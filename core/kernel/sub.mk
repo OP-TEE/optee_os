54e0470881add603f773fa48b8d26a78571815bb
Pascal Brand <pascal.brand@st.com>
2014-10-23 16:38:32 +0200

    Move tee_misc in generic part
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Pascal Brand <pascal.brand@st.com>

--- core/kernel/sub.mk
+++ core/kernel/sub.mk
@@ -2,6 +2,7 @@ srcs-y += assert.c
 srcs-y += tee_compat.c
 srcs-y += tee_dispatch.c
 srcs-y += tee_kta_trace.c
+srcs-y += tee_misc.c
 srcs-y += tee_ta_manager_unpg.c
 srcs-y += panic.c
 srcs-y += handle.c
