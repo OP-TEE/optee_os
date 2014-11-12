OP-TEE/optee_os

commit ffe040395b13924a8bfb106002a3512c0f1024c6

Add crypto provider internal API
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
Tested-by: Pascal Brand <pascal.brand@linaro.org> (STM platform)

core/include/tee/tee_cryp_provider.h
@@ -1,0 +1,265 @@
+/*
+ * Copyright (c) 2014, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * This is the Cryptographic Provider API (CP API).
+ *
+ * This defines how most crypto syscalls that implement the Cryptographic
+ * Operations API can invoke the actual providers of cryptographic algorithms
+ * (such as LibTomCrypt).
+ *
+ * To add a new provider, you need to provide an implementation of this
+ * interface.
+ *
+ * The following parameters are commonly used.
+ *
+ * @ctx: context allocated by the syscall, for later use by the algorithm
+ * @algo: algorithm identifier (TEE_ALG_*)
+ */
+
+
+#ifndef TEE_CRYP_PROVIDER_H
+#define TEE_CRYP_PROVIDER_H
+
+#include <tee_api_types.h>
+
+/* Message digest functions */
+struct hash_ops {
+	TEE_Result (*get_ctx_size)(uint32_t algo, size_t *size);
+	TEE_Result (*init)(void *ctx, uint32_t algo);
+	TEE_Result (*update)(void *ctx, uint32_t algo,
+			     const uint8_t *data, size_t len);
+	TEE_Result (*final)(void *ctx, uint32_t algo, uint8_t *digest,
+			    size_t len);
+};
+
+/* Symmetric ciphers */
+struct cipher_ops {
+	TEE_Result (*get_ctx_size)(uint32_t algo, size_t *size);
+	TEE_Result (*init)(void *ctx, uint32_t algo,
+			    TEE_OperationMode mode,
+			    const uint8_t *key1, size_t key1_len,
+			    const uint8_t *key2, size_t key2_len,
+			    const uint8_t *iv, size_t iv_len);
+	TEE_Result (*update)(void *ctx, uint32_t algo,
+			     TEE_OperationMode mode,
+			     bool last_block, const uint8_t *data,
+			     size_t len, uint8_t *dst);
+	void       (*final)(void *ctx, uint32_t algo);
+	TEE_Result (*get_block_size)(uint32_t algo, size_t *size);
+};
+
+/* Message Authentication Code functions */
+struct mac_ops {
+	TEE_Result (*get_ctx_size)(uint32_t algo, size_t *size);
+	TEE_Result (*init)(void *ctx, uint32_t algo,
+			   const uint8_t *key, size_t len);
+
+	TEE_Result (*update)(void *ctx, uint32_t algo,
+			     const uint8_t *data, size_t len);
+	TEE_Result (*final)(void *ctx, uint32_t algo,
+			    const uint8_t *data, size_t data_len,
+			    uint8_t *digest, size_t digest_len);
+};
+
+/* Authenticated encryption */
+struct authenc_ops {
+	TEE_Result (*get_ctx_size)(uint32_t algo, size_t *size);
+	TEE_Result (*init)(void *ctx, uint32_t algo,
+			   TEE_OperationMode mode,
+			   const uint8_t *key, size_t key_len,
+			   const uint8_t *nonce, size_t nonce_len,
+			   size_t tag_len, size_t aad_len,
+			   size_t payload_len);
+	TEE_Result (*update_aad)(void *ctx, uint32_t algo,
+				 TEE_OperationMode mode,
+				 const uint8_t *data, size_t len);
+	TEE_Result (*update_payload)(void *ctx, uint32_t algo,
+				     TEE_OperationMode mode,
+				     const uint8_t *src_data,
+				     size_t src_len,
+				     uint8_t *dst_data,
+				     size_t *dst_len);
+	TEE_Result (*enc_final)(void *ctx, uint32_t algo,
+				const uint8_t *src_data,
+				size_t src_len, uint8_t *dst_data,
+				size_t *dst_len, uint8_t *dst_tag,
+				size_t *dst_tag_len);
+	TEE_Result (*dec_final)(void *ctx, uint32_t algo,
+				const uint8_t *src_data,
+				size_t src_len, uint8_t *dst_data,
+				size_t *dst_len, const uint8_t *tag,
+				size_t tag_len);
+
+	void       (*final)(void *ctx, uint32_t algo);
+};
+
+/* Implementation-defined big numbers */
+struct bignum_ops {
+	/*
+	 * Allocate a bignum capable of holding an unsigned integer value of
+	 * up to bitsize bits
+	 */
+	struct bignum *(*allocate)(size_t size_bits);
+	TEE_Result (*bin2bn)(const uint8_t *from, size_t fromsize,
+			     struct bignum *to);
+	size_t (*num_bytes)(struct bignum *a);
+	void (*bn2bin)(const struct bignum *from, uint8_t *to);
+	void (*copy)(struct bignum *to, const struct bignum *from);
+	void (*free)(struct bignum *a);
+};
+
+/* Asymmetric algorithms */
+
+struct rsa_keypair {
+	struct bignum *e;	/* Public exponent */
+	struct bignum *d;	/* Private exponent */
+	struct bignum *n;	/* Modulus */
+
+	/* Optional CRT parameters (all NULL if unused) */
+	struct bignum *p;	/* N = pq */
+	struct bignum *q;
+	struct bignum *qp;	/* 1/q mod p */
+	struct bignum *dp;	/* d mod (p-1) */
+	struct bignum *dq;	/* d mod (q-1) */
+};
+
+struct rsa_public_key {
+	struct bignum *e;	/* Public exponent */
+	struct bignum *n;	/* Modulus */
+};
+
+struct dsa_keypair {
+	struct bignum *g;	/* Generator of subgroup (public) */
+	struct bignum *p;	/* Prime number (public) */
+	struct bignum *q;	/* Order of subgroup (public) */
+	struct bignum *y;	/* Public key */
+	struct bignum *x;	/* Private key */
+};
+
+struct dsa_public_key {
+	struct bignum *g;	/* Generator of subgroup (public) */
+	struct bignum *p;	/* Prime number (public) */
+	struct bignum *q;	/* Order of subgroup (public) */
+	struct bignum *y;	/* Public key */
+};
+
+struct dh_keypair {
+	struct bignum *g;	/* Generator of Z_p (shared) */
+	struct bignum *p;	/* Prime modulus (shared) */
+	struct bignum *x;	/* Private key */
+	struct bignum *y;	/* Public key y = g^x */
+
+	/*
+	 * Optional parameters used by key generation.
+	 * When not used, q == NULL and xbits == 0
+	 */
+	struct bignum *q;	/* x must be in the range [2, q-2] */
+	uint32_t xbits;		/* Number of bits in the private key */
+};
+
+struct acipher_ops {
+
+	/*
+	 * Key allocation functions
+	 * Allocate the bignum's inside a key structure.
+	 * TEE core will later use bignum.free().
+	 */
+	TEE_Result (*alloc_rsa_keypair)(struct rsa_keypair *s,
+					size_t key_size_bits);
+	TEE_Result (*alloc_rsa_public_key)(struct rsa_public_key *s,
+					   size_t key_size_bits);
+	TEE_Result (*alloc_dsa_keypair)(struct dsa_keypair *s,
+					size_t key_size_bits);
+	TEE_Result (*alloc_dsa_public_key)(struct dsa_public_key *s,
+					   size_t key_size_bits);
+	TEE_Result (*alloc_dh_keypair)(struct dh_keypair *s,
+				       size_t key_size_bits);
+
+	/*
+	 * Key generation functions
+	 */
+	TEE_Result (*gen_rsa_key)(struct rsa_keypair *key, size_t key_size);
+	TEE_Result (*gen_dsa_key)(struct dsa_keypair *key, size_t key_size);
+	TEE_Result (*gen_dh_key)(struct dh_keypair *key, struct bignum *q,
+				 size_t xbits);
+	TEE_Result (*dh_shared_secret)(struct dh_keypair *private_key,
+				       struct bignum *public_key,
+				       struct bignum *secret);
+
+	TEE_Result (*rsanopad_decrypt)(struct rsa_keypair *key,
+				       const uint8_t *src, size_t src_len,
+				       uint8_t *dst, size_t *dst_len);
+	TEE_Result (*rsanopad_encrypt)(struct rsa_public_key *key,
+				       const uint8_t *src, size_t src_len,
+				       uint8_t *dst, size_t *dst_len);
+	TEE_Result (*rsaes_decrypt)(uint32_t algo, struct rsa_keypair *key,
+				    const uint8_t *label, size_t label_len,
+				    const uint8_t *src, size_t src_len,
+				    uint8_t *dst, size_t *dst_len);
+	TEE_Result (*rsaes_encrypt)(uint32_t algo,
+				    struct rsa_public_key *key,
+				    const uint8_t *label, size_t label_len,
+				    const uint8_t *src, size_t src_len,
+				    uint8_t *dst, size_t *dst_len);
+	/* RSA SSA sign/verify: if salt_len == -1, use default value */
+	TEE_Result (*rsassa_sign)(uint32_t algo, struct rsa_keypair *key,
+				  int salt_len, const uint8_t *msg,
+				  size_t msg_len, uint8_t *sig,
+				  size_t *sig_len);
+	TEE_Result (*rsassa_verify)(uint32_t algo,
+				    struct rsa_public_key *key,
+				    int salt_len, const uint8_t *msg,
+				    size_t msg_len, const uint8_t *sig,
+				    size_t sig_len);
+	TEE_Result (*dsa_sign)(uint32_t algo, struct dsa_keypair *key,
+			       const uint8_t *msg, size_t msg_len,
+			       uint8_t *sig, size_t *sig_len);
+	TEE_Result (*dsa_verify)(uint32_t algo, struct dsa_public_key *key,
+				 const uint8_t *msg, size_t msg_len,
+				 const uint8_t *sig, size_t sig_len);
+};
+
+/* Cryptographic Provider API */
+struct crypto_ops {
+	/* Human-readable provider name */
+	const char *name;
+
+	TEE_Result (*init)(void);
+	struct hash_ops hash;
+	struct cipher_ops cipher;
+	struct mac_ops mac;
+	struct authenc_ops authenc;
+	struct acipher_ops acipher;
+	struct bignum_ops bignum;
+};
+
+extern struct crypto_ops crypto_ops;
+
+TEE_Result tee_cryp_init(void);
+
+#endif /* TEE_CRYP_PROVIDER_H */

core/include/tee/tee_obj.h
@@ -41,6 +41,7 @@ struct tee_obj {
 	uint32_t have_attrs;	/* bitfield identifying set properties */
 	void *data;
 	size_t data_size;
+	void (*finalize)(void *); /* called with data ptr before it's freed */
 	struct tee_pobj *pobj;	/* ptr to persistant object */
 	int fd;
 	uint32_t ds_size;	/* data stream size */

core/tee/tee_obj.c
@@ -62,6 +62,8 @@ void tee_obj_close(struct tee_ta_ctx *ctx, struct tee_obj *o)
 		tee_pobj_release(o->pobj);
 	}
 
+	if (o->finalize)
+		o->finalize(o->data);
 	free(o->data);
 	free(o);
 }

core/arch/arm32/kernel/tee_ta_manager.c
@@ -48,7 +48,6 @@
 #include <kernel/tee_rpc.h>
 #include <kernel/tee_rpc_types.h>
 #include <kernel/mutex.h>
-#include <tee/tee_hash.h>
 #include <tee/tee_obj.h>
 #include <tee/tee_svc_storage.h>
 #include <kernel/tee_time.h>

core/arch/arm32/tee/init.c
@@ -24,7 +24,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <tee_ltc_wrapper.h>
 #include <assert.h>
 #include <malloc.h>		/* required for inits */
 
@@ -35,7 +34,7 @@
 #include <kernel/time_source.h>
 #include <mm/tee_mmu.h>
 #include <tee/tee_fs.h>
-
+#include <tee/tee_cryp_provider.h>
 
 
 
@@ -80,12 +79,20 @@ TEE_Result init_teecore(void)
 	teecore_init_ta_ram();
 	teecore_init_pub_ram();
 
-	/* Libtomcrypt initialization */
-	tee_ltc_init();
+	/* Initialize cryptographic provider */
+	tee_cryp_init();
 
 	/* time initialization */
 	time_source_init();
 
 	IMSG("teecore inits done");
+	return TEE_SUCCESS;
+}
+
+TEE_Result tee_cryp_init(void)
+{
+	if (crypto_ops.init)
+		return crypto_ops.init();
+
 	return TEE_SUCCESS;
 }

core/arch/arm32/tee/tee_rpmb.c
@@ -37,10 +37,9 @@
 #include <kernel/tee_ta_manager.h>
 #include <tee/tee_rpmb.h>
 #include <kernel/chip_services.h>
-#include <tee/tee_hash.h>
 #include <kernel/tee_misc.h>
-#include <tee/tee_hash.h>
-#include <tee/tee_svc_cryp.h>
+#include <tee/tee_cryp_provider.h>
+#include <tee/tee_cryp_utl.h>
 #include <sm/teesmc.h>
 #include <mm/core_mmu.h>
 
@@ -174,17 +173,17 @@ static TEE_Result mac_calc(uint8_t *mac, uint32_t macsize,
 static TEE_Result mac_init(void *ctx, const uint8_t *key __unused,
 			uint32_t keysize __unused)
 {
-	return tee_hash_init(ctx, TEE_ALG_HMAC_SHA256);
+	return crypto_ops.hash.init(ctx, TEE_ALG_HMAC_SHA256);
 }
 
 static TEE_Result mac_update(void *ctx, const uint8_t *data, uint32_t datasize)
 {
-	return tee_hash_update(ctx, TEE_ALG_HMAC_SHA256, data, datasize);
+	return crypto_ops.hash.update(ctx, TEE_ALG_HMAC_SHA256, data, datasize);
 }
 
 static TEE_Result mac_final(void *ctx, uint8_t *mac, uint32_t macsize)
 {
-	return tee_hash_final(ctx, TEE_ALG_HMAC_SHA256, mac, macsize);
+	return crypto_ops.hash.final(ctx, TEE_ALG_HMAC_SHA256, mac, macsize);
 }
 
 /*
@@ -856,7 +855,7 @@ static TEE_Result tee_rpmb_init(uint16_t dev_id, bool writekey, bool commercial)
 		memcpy(rpmb_ctx->cid, dev_info.cid, RPMB_EMMC_CID_SIZE);
 
 		if ((rpmb_ctx->hash_ctx_size == 0) &&
-		    (tee_hash_get_ctx_size(
+		    (crypto_ops.mac.get_ctx_size(
 			    TEE_ALG_HMAC_SHA256,
 			    (size_t *)(&rpmb_ctx->hash_ctx_size)))) {
 			rpmb_ctx->hash_ctx_size = 0;

commit 51835057e8aa7a7ba13bd8e029ac98a22b4d990b

Fix memory leak in tee_svc_cryp_obj_copy()
The following Trusted App would lead to a memory leak in the TEE core:

  TEE_ObjectHandle o1, o2;
  TEE_AllocateTransientObject(TEE_TYPE_RSA_KEYPAIR, 256, &o1);
  TEE_GenerateKey(o1, 256, NULL, 0);
  TEE_AllocateTransientObject(TEE_TYPE_RSA_KEYPAIR, 256, &o2);
  TEE_CopyObjectAttributes(o2, o1);
  TEE_FreeTransientObject(o1);
  TEE_FreeTransientObject(o2);

The leak was introduced by commit ffe040395b13 ("Add crypto provider internal
API").

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
Tested-by: Pascal Brand <pascal.brand@linaro.org> (STM platform)

core/include/tee/tee_cryp_provider.h
@@ -131,6 +131,7 @@ struct bignum_ops {
 	void (*bn2bin)(const struct bignum *from, uint8_t *to);
 	void (*copy)(struct bignum *to, const struct bignum *from);
 	void (*free)(struct bignum *a);
+	void (*clear)(struct bignum *a);
 };
 
 /* Asymmetric algorithms */

core/include/tee/tee_obj.h
@@ -41,7 +41,7 @@ struct tee_obj {
 	uint32_t have_attrs;	/* bitfield identifying set properties */
 	void *data;
 	size_t data_size;
-	void (*finalize)(void *); /* called with data ptr before it's freed */
+	void (*cleanup)(void *data, bool del); /* clear or delete data */
 	struct tee_pobj *pobj;	/* ptr to persistant object */
 	int fd;
 	uint32_t ds_size;	/* data stream size */

core/lib/libtomcrypt/src/tee_ltc_provider.c
@@ -549,6 +549,13 @@ static void bn_free(struct bignum *s)
 	free(s);
 }
 
+static void bn_clear(struct bignum *s)
+{
+	struct mpa_numbase_struct *bn = (struct mpa_numbase_struct *)s;
+
+	memset(bn, 0, bn->alloc);
+}
+
 static bool bn_alloc_max(struct bignum **s)
 {
 	size_t sz = mpa_StaticVarSizeInU32(LTC_MAX_BITS_PER_VARIABLE) *
@@ -2204,5 +2211,6 @@ struct crypto_ops crypto_ops = {
 		.bin2bn = bin2bn,
 		.copy = copy,
 		.free = bn_free,
+		.clear = bn_clear
 	}
 };

core/tee/tee_obj.c
@@ -62,8 +62,8 @@ void tee_obj_close(struct tee_ta_ctx *ctx, struct tee_obj *o)
 		tee_pobj_release(o->pobj);
 	}
 
-	if (o->finalize)
-		o->finalize(o->data);
+	if (o->cleanup)
+		o->cleanup(o->data, true);
 	free(o->data);
 	free(o);
 }

core/tee/tee_svc_cryp.c
@@ -600,93 +600,79 @@ TEE_Result tee_svc_cryp_obj_get_attr(uint32_t obj, uint32_t attr_id,
 					 raw_data, raw_size);
 }
 
-static void bn_free(struct bignum **bn)
+static void bn_cleanup(struct bignum *bn, bool del)
 {
-	if (*bn)
-		crypto_ops.bignum.free(*bn);
-	*bn = NULL;
+	if (del)
+		crypto_ops.bignum.free(bn);
+	else
+		crypto_ops.bignum.clear(bn);
 }
 
-static void free_rsa_keypair(void *p)
+static void cleanup_rsa_keypair(void *p, bool del)
 {
 	struct rsa_keypair *s = (struct rsa_keypair *)p;
 
-	bn_free(&s->e);
-	bn_free(&s->d);
-	bn_free(&s->n);
-	bn_free(&s->p);
-	bn_free(&s->q);
-	bn_free(&s->qp);
-	bn_free(&s->dp);
-	bn_free(&s->dq);
+	bn_cleanup(s->e, del);
+	bn_cleanup(s->d, del);
+	bn_cleanup(s->n, del);
+	bn_cleanup(s->p, del);
+	bn_cleanup(s->q, del);
+	bn_cleanup(s->qp, del);
+	bn_cleanup(s->dp, del);
+	bn_cleanup(s->dq, del);
 }
 
-static void free_dsa_keypair(void *p)
+static void cleanup_dsa_keypair(void *p, bool del)
 {
 	struct dsa_keypair *s = (struct dsa_keypair *)p;
 
-	bn_free(&s->g);
-	bn_free(&s->p);
-	bn_free(&s->q);
-	bn_free(&s->y);
-	bn_free(&s->x);
+	bn_cleanup(s->g, del);
+	bn_cleanup(s->p, del);
+	bn_cleanup(s->q, del);
+	bn_cleanup(s->y, del);
+	bn_cleanup(s->x, del);
 }
 
-static void free_rsa_public_key(void *p)
+static void cleanup_rsa_public_key(void *p, bool del)
 {
 	struct rsa_public_key *s = (struct rsa_public_key *)p;
 
-	bn_free(&s->e);
-	bn_free(&s->n);
+	bn_cleanup(s->e, del);
+	bn_cleanup(s->n, del);
 }
 
-static void free_dsa_public_key(void *p)
+static void cleanup_dsa_public_key(void *p, bool del)
 {
 	struct dsa_public_key *s = (struct dsa_public_key *)p;
 
-	bn_free(&s->g);
-	bn_free(&s->p);
-	bn_free(&s->q);
-	bn_free(&s->y);
+	bn_cleanup(s->g, del);
+	bn_cleanup(s->p, del);
+	bn_cleanup(s->q, del);
+	bn_cleanup(s->y, del);
 }
 
-static void free_dh_keypair(void *p)
+static void cleanup_dh_keypair(void *p, bool del)
 {
 	struct dh_keypair *s = (struct dh_keypair *)p;
 
-	bn_free(&s->g);
-	bn_free(&s->p);
-	bn_free(&s->x);
-	bn_free(&s->y);
-	bn_free(&s->q);
+	bn_cleanup(s->g, del);
+	bn_cleanup(s->p, del);
+	bn_cleanup(s->x, del);
+	bn_cleanup(s->y, del);
+	bn_cleanup(s->q, del);
 	s->xbits = 0;
 }
 
-static TEE_Result copy_rsa_public_key(struct rsa_public_key *to,
-				      const struct rsa_public_key *from)
+static void copy_rsa_public_key(struct rsa_public_key *to,
+				const struct rsa_public_key *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->n);
-	res = crypto_ops.acipher.alloc_rsa_public_key(to, keysize);
-	if (res != TEE_SUCCESS)
-		return res;
 	crypto_ops.bignum.copy(to->e, from->e);
 	crypto_ops.bignum.copy(to->n, from->n);
-	return TEE_SUCCESS;
 }
 
-static TEE_Result copy_rsa_keypair(struct rsa_keypair *to,
-				   const struct rsa_keypair *from)
+static void copy_rsa_keypair(struct rsa_keypair *to,
+			     const struct rsa_keypair *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->n);
-	res = crypto_ops.acipher.alloc_rsa_keypair(to, keysize);
-	if (res != TEE_SUCCESS)
-		return TEE_ERROR_OUT_OF_MEMORY;
 	crypto_ops.bignum.copy(to->e, from->e);
 	crypto_ops.bignum.copy(to->d, from->d);
 	crypto_ops.bignum.copy(to->n, from->n);
@@ -695,94 +681,53 @@ static TEE_Result copy_rsa_keypair(struct rsa_keypair *to,
 	crypto_ops.bignum.copy(to->qp, from->qp);
 	crypto_ops.bignum.copy(to->dp, from->dp);
 	crypto_ops.bignum.copy(to->dq, from->dq);
-	return TEE_SUCCESS;
 }
 
-static TEE_Result copy_dsa_public_key(struct dsa_public_key *to,
-				      const struct dsa_public_key *from)
+static void copy_dsa_public_key(struct dsa_public_key *to,
+				const struct dsa_public_key *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->p);
-	res = crypto_ops.acipher.alloc_dsa_public_key(to, keysize);
-	if (res != TEE_SUCCESS)
-		return TEE_ERROR_OUT_OF_MEMORY;
 	crypto_ops.bignum.copy(to->g, from->g);
 	crypto_ops.bignum.copy(to->p, from->p);
 	crypto_ops.bignum.copy(to->q, from->q);
 	crypto_ops.bignum.copy(to->y, from->y);
-	return TEE_SUCCESS;
 }
 
 
-static TEE_Result copy_dsa_keypair(struct dsa_keypair *to,
-				   const struct dsa_keypair *from)
+static void copy_dsa_keypair(struct dsa_keypair *to,
+			     const struct dsa_keypair *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->p);
-	res = crypto_ops.acipher.alloc_dsa_keypair(to, keysize);
-	if (res != TEE_SUCCESS)
-		return TEE_ERROR_OUT_OF_MEMORY;
 	crypto_ops.bignum.copy(to->g, from->g);
 	crypto_ops.bignum.copy(to->p, from->p);
 	crypto_ops.bignum.copy(to->q, from->q);
 	crypto_ops.bignum.copy(to->y, from->y);
 	crypto_ops.bignum.copy(to->x, from->x);
-	return TEE_SUCCESS;
 }
 
-static TEE_Result copy_dh_keypair(struct dh_keypair *to,
-				  const struct dh_keypair *from)
+static void copy_dh_keypair(struct dh_keypair *to,
+			    const struct dh_keypair *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->p);
-	res = crypto_ops.acipher.alloc_dh_keypair(to, keysize);
-	if (res != TEE_SUCCESS)
-		return TEE_ERROR_OUT_OF_MEMORY;
 	crypto_ops.bignum.copy(to->g, from->g);
 	crypto_ops.bignum.copy(to->p, from->p);
 	crypto_ops.bignum.copy(to->y, from->y);
 	crypto_ops.bignum.copy(to->x, from->x);
 	crypto_ops.bignum.copy(to->q, from->q);
 	to->xbits = from->xbits;
-	return TEE_SUCCESS;
 }
 
-static TEE_Result extract_rsa_public_key(struct rsa_public_key *to,
-					 const struct rsa_keypair *from)
+static void extract_rsa_public_key(struct rsa_public_key *to,
+				   const struct rsa_keypair *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->n);
-	res = crypto_ops.acipher.alloc_rsa_public_key(to, keysize);
-	if (res != TEE_SUCCESS)
-		return TEE_ERROR_OUT_OF_MEMORY;
 	crypto_ops.bignum.copy(to->e, from->e);
 	crypto_ops.bignum.copy(to->n, from->n);
-	return TEE_SUCCESS;
 }
 
-static TEE_Result extract_dsa_public_key(struct dsa_public_key *to,
-					 const struct dsa_keypair *from)
+static void extract_dsa_public_key(struct dsa_public_key *to,
+				   const struct dsa_keypair *from)
 {
-	TEE_Result res;
-	size_t keysize;
-
-	keysize = crypto_ops.bignum.num_bytes(from->p);
-	res = crypto_ops.acipher.alloc_dsa_public_key(to, keysize);
-	if (res != TEE_SUCCESS)
-		return TEE_ERROR_OUT_OF_MEMORY;
 	crypto_ops.bignum.copy(to->g, from->g);
 	crypto_ops.bignum.copy(to->p, from->p);
 	crypto_ops.bignum.copy(to->q, from->q);
 	crypto_ops.bignum.copy(to->y, from->y);
-	return TEE_SUCCESS;
 }
 
 TEE_Result tee_svc_cryp_obj_alloc(TEE_ObjectType obj_type,
@@ -834,7 +779,7 @@ TEE_Result tee_svc_cryp_obj_alloc(TEE_ObjectType obj_type,
 							    max_obj_size)
 				!= TEE_SUCCESS)
 			goto alloc_err;
-		o->finalize = free_rsa_public_key;
+		o->cleanup = cleanup_rsa_public_key;
 		break;
 	case TEE_TYPE_RSA_KEYPAIR:
 		if (!crypto_ops.acipher.alloc_rsa_keypair)
@@ -843,7 +788,7 @@ TEE_Result tee_svc_cryp_obj_alloc(TEE_ObjectType obj_type,
 							 max_obj_size)
 				!= TEE_SUCCESS)
 			goto alloc_err;
-		o->finalize = free_rsa_keypair;
+		o->cleanup = cleanup_rsa_keypair;
 		break;
 	case TEE_TYPE_DSA_PUBLIC_KEY:
 		if (!crypto_ops.acipher.alloc_dsa_public_key)
@@ -852,7 +797,7 @@ TEE_Result tee_svc_cryp_obj_alloc(TEE_ObjectType obj_type,
 							    max_obj_size)
 				!= TEE_SUCCESS)
 			goto alloc_err;
-		o->finalize = free_dsa_public_key;
+		o->cleanup = cleanup_dsa_public_key;
 		break;
 	case TEE_TYPE_DSA_KEYPAIR:
 		if (!crypto_ops.acipher.alloc_dsa_keypair)
@@ -860,7 +805,7 @@ TEE_Result tee_svc_cryp_obj_alloc(TEE_ObjectType obj_type,
 		if (crypto_ops.acipher.alloc_dsa_keypair(o->data, max_obj_size)
 				!= TEE_SUCCESS)
 			goto alloc_err;
-		o->finalize = free_dsa_keypair;
+		o->cleanup = cleanup_dsa_keypair;
 		break;
 	case TEE_TYPE_DH_KEYPAIR:
 		if (!crypto_ops.acipher.alloc_dh_keypair)
@@ -868,7 +813,7 @@ TEE_Result tee_svc_cryp_obj_alloc(TEE_ObjectType obj_type,
 		if (crypto_ops.acipher.alloc_dh_keypair(o->data, max_obj_size)
 				!= TEE_SUCCESS)
 			goto alloc_err;
-		o->finalize = free_dh_keypair;
+		o->cleanup = cleanup_dh_keypair;
 		break;
 	default:
 		break;
@@ -938,9 +883,15 @@ TEE_Result tee_svc_cryp_obj_reset(uint32_t obj)
 		return res;
 
 	if ((o->info.handleFlags & TEE_HANDLE_FLAG_PERSISTENT) == 0) {
-		if (o->finalize)
-			o->finalize(o->data);
-		memset(o->data, 0, o->data_size);
+		if (o->cleanup) {
+			/*
+			 * o->data contains pointers to key data.
+			 * Clear key data, but keep the pointers.
+			 */
+			o->cleanup(o->data, false);
+		} else {
+			memset(o->data, 0, o->data_size);
+		}
 		o->info.objectSize = 0;
 		o->info.objectUsage = TEE_USAGE_DEFAULT;
 	} else {
@@ -1232,39 +1183,26 @@ TEE_Result tee_svc_cryp_obj_copy(uint32_t dst, uint32_t src)
 
 		if (dst_o->data_size != src_o->data_size)
 			return TEE_ERROR_BAD_STATE;
+		if (dst_o->cleanup != src_o->cleanup)
+			return TEE_ERROR_BAD_STATE;
 
 		dst_o->have_attrs = src_o->have_attrs;
 
 		switch (src_o->info.objectType) {
 		case TEE_TYPE_RSA_PUBLIC_KEY:
-			res = copy_rsa_public_key(dst_o->data, src_o->data);
-			if (res != TEE_SUCCESS)
-				return res;
-			dst_o->finalize = free_rsa_public_key;
+			copy_rsa_public_key(dst_o->data, src_o->data);
 			break;
 		case TEE_TYPE_RSA_KEYPAIR:
-			res = copy_rsa_keypair(dst_o->data, src_o->data);
-			if (res != TEE_SUCCESS)
-				return res;
-			dst_o->finalize = free_rsa_keypair;
+			copy_rsa_keypair(dst_o->data, src_o->data);
 			break;
 		case TEE_TYPE_DSA_PUBLIC_KEY:
-			res = copy_dsa_public_key(dst_o->data, src_o->data);
-			if (res != TEE_SUCCESS)
-				return res;
-			dst_o->finalize = free_dsa_public_key;
+			copy_dsa_public_key(dst_o->data, src_o->data);
 			break;
 		case TEE_TYPE_DSA_KEYPAIR:
-			res = copy_dsa_keypair(dst_o->data, src_o->data);
-			if (res != TEE_SUCCESS)
-				return res;
-			dst_o->finalize = free_dsa_keypair;
+			copy_dsa_keypair(dst_o->data, src_o->data);
 			break;
 		case TEE_TYPE_DH_KEYPAIR:
-			res = copy_dh_keypair(dst_o->data, src_o->data);
-			if (res != TEE_SUCCESS)
-				return res;
-			dst_o->finalize = free_dh_keypair;
+			copy_dh_keypair(dst_o->data, src_o->data);
 			break;
 		default:
 			/* Generic case */
@@ -1275,10 +1213,7 @@ TEE_Result tee_svc_cryp_obj_copy(uint32_t dst, uint32_t src)
 		/* Extract public key from RSA key pair */
 		size_t n;
 
-		res = extract_rsa_public_key(dst_o->data, src_o->data);
-		if (res != TEE_SUCCESS)
-			return res;
-		dst_o->finalize = free_rsa_public_key;
+		extract_rsa_public_key(dst_o->data, src_o->data);
 		dst_o->have_attrs = 0;
 		for (n = 0; n < TEE_ARRAY_SIZE(tee_cryp_obj_rsa_pub_key_attrs);
 		     n++)
@@ -1289,17 +1224,15 @@ TEE_Result tee_svc_cryp_obj_copy(uint32_t dst, uint32_t src)
 		/* Extract public key from DSA key pair */
 		size_t n;
 
-		res = extract_dsa_public_key(dst_o->data, src_o->data);
-		if (res != TEE_SUCCESS)
-			return res;
-		dst_o->finalize = free_dsa_public_key;
+		extract_dsa_public_key(dst_o->data, src_o->data);
 		dst_o->have_attrs = 0;
 		for (n = 0; n < TEE_ARRAY_SIZE(tee_cryp_obj_dsa_pub_key_attrs);
 		     n++)
 			dst_o->have_attrs |= 1 << n;
 
-	} else
+	} else {
 		return TEE_ERROR_BAD_PARAMETERS;
+	}
 
 	dst_o->info.handleFlags |= TEE_HANDLE_FLAG_INITIALIZED;
 	dst_o->info.objectSize = src_o->info.objectSize;