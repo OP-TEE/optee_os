c15e5835925664519a7fbd735657df2684334314
Cedric Chaumont <cedric.chaumont@st.com>
2015-04-16 16:55:35 +0200

    GP Internal Core API v1.1 : review internal client and cancellation functions
    update panic reasons
    checkpatch warning fix (else after return)
    
    Signed-off-by: Cedric Chaumont <cedric.chaumont@st.com>
    Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Tested-by: Cedric Chaumont <cedric.chaumont@linaro.org> (STM boards)
    Tested-by: Cedric Chaumont <cedric.chaumont@linaro.org> (ARM Juno board)

--- lib/libutee/tee_api.c
+++ lib/libutee/tee_api.c
@@ -78,9 +78,17 @@ TEE_Result TEE_InvokeTACommand(TEE_TASessionHandle session,
 			       uint32_t commandID, uint32_t paramTypes,
 			       TEE_Param params[4], uint32_t *returnOrigin)
 {
-	return utee_invoke_ta_command(session, cancellationRequestTimeout,
+	TEE_Result res;
+
+	res = utee_invoke_ta_command(session, cancellationRequestTimeout,
 				      commandID, paramTypes, params,
 				      returnOrigin);
+	if (res != TEE_SUCCESS &&
+	    res != TEE_ERROR_OUT_OF_MEMORY &&
+	    res != TEE_ERROR_TARGET_DEAD)
+		TEE_Panic(res);
+
+	return res;
 }
 
 /* System API - Cancellations */
@@ -89,9 +97,8 @@ bool TEE_GetCancellationFlag(void)
 {
 	bool c;
 	TEE_Result res = utee_get_cancellation_flag(&c);
-
 	if (res != TEE_SUCCESS)
-		TEE_Panic(res);
+		c = false;
 	return c;
 }
 
