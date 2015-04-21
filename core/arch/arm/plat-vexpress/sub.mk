57903c16c7dafb08abc57680de73ef4a7c43cb4a
Jerome Forissier <jerome.forissier@linaro.org>
2015-04-21 09:41:46 +0200

    Delete tee_common_otp.c
    Move dummy implementations of tee_otp_get_hw_unique_key() and
    tee_otp_get_die_id() to core/include/kernel/tee_common_otp.h as inline
    functions.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

--- core/arch/arm/plat-vexpress/sub.mk
+++ core/arch/arm/plat-vexpress/sub.mk
@@ -3,7 +3,6 @@ srcs-$(CFG_ARM32_core) += entry_a32.S
 srcs-$(CFG_ARM64_core) += entry_a64.S
 srcs-y += main.c
 srcs-y += plat_tee_func.c
-srcs-y += tee_common_otp.c
 srcs-y += core_bootcfg.c
 srcs-y += core_chip.c
 ifeq ($(PLATFORM_FLAVOR_juno),y)
