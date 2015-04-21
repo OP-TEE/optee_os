c4d467ce9cbd7154bc182b9f6eb9ac16df631243
Jerome Forissier <jerome.forissier@linaro.org>
2015-04-21 09:54:02 +0200

    vexpress: move core_bootcfg.c to plat-common/
    HiKey will use the same implementation, so move it to a common directory.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-common/sub.mk
+++ core/arch/arm/plat-common/sub.mk
@@ -3,3 +3,4 @@ srcs-$(CFG_COMMON_INIT) += init.c
 ifeq ($(CFG_COMMON_INIT),y)
 srcs-$(CFG_ARM32_core) += entry_a32.S
 endif
+srcs-$(CFG_COMMON_BOOTCFG) += core_bootcfg.c
--- core/arch/arm/plat-vexpress/sub.mk
+++ core/arch/arm/plat-vexpress/sub.mk
@@ -1,5 +1,4 @@
 global-incdirs-y += .
 srcs-y += main.c
-srcs-y += core_bootcfg.c
 srcs-y += core_chip.c
 srcs-$(PLATFORM_FLAVOR_juno) += juno_core_pos_a32.S
