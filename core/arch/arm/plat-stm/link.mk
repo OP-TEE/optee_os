483200d5cc2a0ba8d04ff8871831af8592666769
Pascal Brand <pascal.brand@st.com>
2015-05-07 13:03:15 +0200

    plat-stm: going to generic link file
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Pascal Brand <pascal.brand@st.com>

--- core/arch/arm/plat-stm/link.mk
+++ core/arch/arm/plat-stm/link.mk
@@ -1,42 +1,50 @@
 link-out-dir = $(out-dir)/core
 
 link-script = $(platform-dir)/tz-template.lds
-link-script-pp = $(link-out-dir)/tz.lds
+link-script-pp = $(link-out-dir)/kern.ld
+link-script-dep = $(link-out-dir)/.kern.ld.d
 
-all: $(link-out-dir)/tee.elf $(link-out-dir)/tee.dmp $(link-out-dir)/tee.bin
-all: $(link-out-dir)/tee.symb_sizes
-cleanfiles += $(link-out-dir)/tee.elf $(link-out-dir)/tee.dmp $(link-out-dir)/tee.map
-cleanfiles += $(link-out-dir)/tee.bin
-cleanfiles += $(link-out-dir)/tee.symb_sizes
-cleanfiles += $(link-script-pp)
+AWK	 = awk
+
 
 link-ldflags  = $(LDFLAGS)
 link-ldflags += -T $(link-script-pp) -Map=$(link-out-dir)/tee.map
 link-ldflags += --sort-section=alignment
+link-ldflags += --fatal-warnings
+link-ldflags += --print-gc-sections
 
 link-ldadd  = $(LDADD)
-link-ldadd += $(libfiles)
+link-ldadd += $(addprefix -L,$(libdirs))
+link-ldadd += $(addprefix -l,$(libnames))
 ldargs-tee.elf := $(link-ldflags) $(objs) $(link-ldadd) $(libgcccore)
 
 
+cleanfiles += $(link-script-pp)
 $(link-script-pp): $(link-script) $(MAKEFILE_LIST)
 	@$(cmd-echo-silent) '  SED     $@'
 	@mkdir -p $(dir $@)
 	$(q)sed -e "s/%in_TEE_SCATTER_START%/$(TEE_SCATTER_START)/g" < $< > $@
 
-
+all: $(link-out-dir)/tee.elf
+cleanfiles += $(link-out-dir)/tee.elf $(link-out-dir)/tee.map
 $(link-out-dir)/tee.elf: $(objs) $(libdeps) $(link-script-pp)
 	@$(cmd-echo-silent) '  LD      $@'
 	$(q)$(LDcore) $(ldargs-tee.elf) -o $@
 
+all: $(link-out-dir)/tee.dmp
+cleanfiles += $(link-out-dir)/tee.dmp
 $(link-out-dir)/tee.dmp: $(link-out-dir)/tee.elf
 	@$(cmd-echo-silent) '  OBJDUMP $@'
 	$(q)$(OBJDUMPcore) -l -x -d $< > $@
 
+all: $(link-out-dir)/tee.bin
+cleanfiles += $(link-out-dir)/tee.bin
 $(link-out-dir)/tee.bin: $(link-out-dir)/tee.elf
 	@$(cmd-echo-silent) '  OBJCOPY $@'
 	$(q)$(OBJCOPYcore) -O binary $< $@
 
+all: $(link-out-dir)/tee.symb_sizes
+cleanfiles += $(link-out-dir)/tee.symb_sizes
 $(link-out-dir)/tee.symb_sizes: $(link-out-dir)/tee.elf
 	@$(cmd-echo-silent) '  GEN     $@'
 	$(q)$(NMcore) --print-size --reverse-sort --size-sort $< > $@
