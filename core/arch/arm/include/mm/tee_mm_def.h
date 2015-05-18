55093bf460a0fc8c4efc1a3bd0c68eeb95483948
Jens Wiklander <jens.wiklander@linaro.org>
2015-05-18 10:20:57 +0200

    Remove RTT0 and RTT1 support
    Removes the support for RTT0 and RTT1 as we don't have a port to any
    hardware with RTT0 and RTT1.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Pascal Brand <pascal.brand@linaro.org>

--- core/arch/arm/include/mm/tee_mm_def.h
+++ core/arch/arm/include/mm/tee_mm_def.h
@@ -32,37 +32,4 @@
 #define SMALL_PAGE_MASK		0x00000fff
 #define SMALL_PAGE_SIZE		0x00001000
 
-/*
- * Register addresses related to time
- * RTT = Real-Time Timer
- * RTT0 = Real-Time Timer 0
- * RTT1 = Real-Time Timer 1
- */
-#define RTT_CR_EN		0x2
-#define RTT_CR_ENS		0x4
-#define RTT_IMSC_IMSC		0x1
-#define RTT_MIS_MIS		0x1
-
-/* RTT0 definition */
-#define RTT0_REG_START_ADDR	0x80152000
-#define RTT0_CTCR		(RTT0_REG_START_ADDR)
-#define RTT0_IMSC		(RTT0_REG_START_ADDR + 0x04)
-#define RTT0_RIS		(RTT0_REG_START_ADDR + 0x08)
-#define RTT0_MIS		(RTT0_REG_START_ADDR + 0x0C)
-#define RTT0_ICR		(RTT0_REG_START_ADDR + 0x10)
-#define RTT0_DR			(RTT0_REG_START_ADDR + 0x14)
-#define RTT0_LR			(RTT0_REG_START_ADDR + 0x18)
-#define RTT0_CR			(RTT0_REG_START_ADDR + 0x1c)
-
-/* RTT1 definition */
-#define RTT1_REG_START_ADDR	0x80153000
-#define RTT1_CTCR		(RTT1_REG_START_ADDR)
-#define RTT1_IMSC		(RTT1_REG_START_ADDR + 0x04)
-#define RTT1_RIS		(RTT1_REG_START_ADDR + 0x08)
-#define RTT1_MIS		(RTT1_REG_START_ADDR + 0x0C)
-#define RTT1_ICR		(RTT1_REG_START_ADDR + 0x10)
-#define RTT1_DR			(RTT1_REG_START_ADDR + 0x14)
-#define RTT1_LR			(RTT1_REG_START_ADDR + 0x18)
-#define RTT1_CR			(RTT1_REG_START_ADDR + 0x1c)
-
 #endif
