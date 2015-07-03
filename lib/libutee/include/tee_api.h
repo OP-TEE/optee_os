4e9461268aa7ba17bf25117dcc20037f63ab2636
Cedric Chaumont <cedric.chaumont@st.com>
2015-07-03 14:17:19 +0200

    GP11 : TEE Arithmetical API fix/panic reason
    Signed-off-by: Cedric Chaumont <cedric.chaumont@st.com>
    Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Tested-by: Cedric Chaumont <cedric.chaumont@linaro.org> (STM boards)
    Tested-by: Cedric Chaumont <cedric.chaumont@linaro.org> (ARM Juno board)

--- lib/libutee/include/tee_api.h
+++ lib/libutee/include/tee_api.h
@@ -343,116 +343,109 @@ void TEE_GetREETime(TEE_Time *time);
 
 /* TEE Arithmetical API - Memory allocation and size of objects */
 
-size_t TEE_BigIntFMMSizeInU32(uint32_t modulusSizeInBits);
+uint32_t TEE_BigIntFMMSizeInU32(uint32_t modulusSizeInBits);
 
-size_t TEE_BigIntFMMContextSizeInU32(uint32_t modulusSizeInBits);
+uint32_t TEE_BigIntFMMContextSizeInU32(uint32_t modulusSizeInBits);
 
 /* TEE Arithmetical API - Initialization functions */
 
 void TEE_BigIntInit(TEE_BigInt *bigInt, uint32_t len);
 
 void TEE_BigIntInitFMMContext(TEE_BigIntFMMContext *context, uint32_t len,
-			      const TEE_BigInt *modulus);
+			      TEE_BigInt *modulus);
 
-void TEE_BigIntInitFMM(const TEE_BigIntFMM *bigIntFMM, uint32_t len);
+void TEE_BigIntInitFMM(TEE_BigIntFMM *bigIntFMM, uint32_t len);
 
 /* TEE Arithmetical API - Converter functions */
 
 TEE_Result TEE_BigIntConvertFromOctetString(TEE_BigInt *dest,
-					    const uint8_t *buffer,
+					    uint8_t *buffer,
 					    uint32_t bufferLen,
 					    int32_t sign);
 
 TEE_Result TEE_BigIntConvertToOctetString(uint8_t *buffer, uint32_t *bufferLen,
-					  const TEE_BigInt *bigInt);
+					  TEE_BigInt *bigInt);
 
 void TEE_BigIntConvertFromS32(TEE_BigInt *dest, int32_t shortVal);
 
-TEE_Result TEE_BigIntConvertToS32(int32_t *dest, const TEE_BigInt *src);
-
-void TEE_BigIntSetShort(TEE_BigInt *dest, int32_t shortVal);
-
-TEE_Result TEE_BigIntGetShort(int32_t *dest, const TEE_BigInt *src);
+TEE_Result TEE_BigIntConvertToS32(int32_t *dest, TEE_BigInt *src);
 
 /* TEE Arithmetical API - Logical operations */
 
-int32_t TEE_BigIntCmp(const TEE_BigInt *op1, const TEE_BigInt *op2);
+int32_t TEE_BigIntCmp(TEE_BigInt *op1, TEE_BigInt *op2);
 
-int32_t TEE_BigIntCmpS32(const TEE_BigInt *op, int32_t shortVal);
+int32_t TEE_BigIntCmpS32(TEE_BigInt *op, int32_t shortVal);
 
-void TEE_BigIntShiftRight(TEE_BigInt *dest, const TEE_BigInt *op,
+void TEE_BigIntShiftRight(TEE_BigInt *dest, TEE_BigInt *op,
 			  size_t bits);
 
-bool TEE_BigIntGetBit(const TEE_BigInt *src, uint32_t bitIndex);
+bool TEE_BigIntGetBit(TEE_BigInt *src, uint32_t bitIndex);
 
-uint32_t TEE_BigIntGetBitCount(const TEE_BigInt *src);
+uint32_t TEE_BigIntGetBitCount(TEE_BigInt *src);
 
-void TEE_BigIntAdd(TEE_BigInt *dest, const TEE_BigInt *op1,
-		   const TEE_BigInt *op2);
+void TEE_BigIntAdd(TEE_BigInt *dest, TEE_BigInt *op1,
+		   TEE_BigInt *op2);
 
-void TEE_BigIntSub(TEE_BigInt *dest, const TEE_BigInt *op1,
-		   const TEE_BigInt *op2);
+void TEE_BigIntSub(TEE_BigInt *dest, TEE_BigInt *op1,
+		   TEE_BigInt *op2);
 
-void TEE_BigIntNeg(TEE_BigInt *dest, const TEE_BigInt *op);
+void TEE_BigIntNeg(TEE_BigInt *dest, TEE_BigInt *op);
 
-void TEE_BigIntMul(TEE_BigInt *dest, const TEE_BigInt *op1,
-		   const TEE_BigInt *op2);
+void TEE_BigIntMul(TEE_BigInt *dest, TEE_BigInt *op1,
+		   TEE_BigInt *op2);
 
-void TEE_BigIntSquare(TEE_BigInt *dest, const TEE_BigInt *op);
+void TEE_BigIntSquare(TEE_BigInt *dest, TEE_BigInt *op);
 
 void TEE_BigIntDiv(TEE_BigInt *dest_q, TEE_BigInt *dest_r,
-		   const TEE_BigInt *op1, const TEE_BigInt *op2);
-
-void TEE_BigIntReduceMod(TEE_BigInt *dest, const TEE_BigInt *op,
-			 const TEE_BigInt *n);
+		   TEE_BigInt *op1, TEE_BigInt *op2);
 
 /* TEE Arithmetical API - Modular arithmetic operations */
 
-void TEE_BigIntMod(TEE_BigInt *dest, const TEE_BigInt *op,
-		   const TEE_BigInt *n);
+void TEE_BigIntMod(TEE_BigInt *dest, TEE_BigInt *op,
+		   TEE_BigInt *n);
 
-void TEE_BigIntAddMod(TEE_BigInt *dest, const TEE_BigInt *op1,
-		      const TEE_BigInt *op2, const TEE_BigInt *n);
+void TEE_BigIntAddMod(TEE_BigInt *dest, TEE_BigInt *op1,
+		      TEE_BigInt *op2, TEE_BigInt *n);
 
-void TEE_BigIntSubMod(TEE_BigInt *dest, const TEE_BigInt *op1,
-		      const TEE_BigInt *op2, const TEE_BigInt *n);
+void TEE_BigIntSubMod(TEE_BigInt *dest, TEE_BigInt *op1,
+		      TEE_BigInt *op2, TEE_BigInt *n);
 
-void TEE_BigIntMulMod(TEE_BigInt *dest, const TEE_BigInt *op1,
-		      const TEE_BigInt *op2, const TEE_BigInt *n);
+void TEE_BigIntMulMod(TEE_BigInt *dest, TEE_BigInt *op1,
+		      TEE_BigInt *op2, TEE_BigInt *n);
 
-void TEE_BigIntSquareMod(TEE_BigInt *dest, const TEE_BigInt *op,
-			 const TEE_BigInt *n);
+void TEE_BigIntSquareMod(TEE_BigInt *dest, TEE_BigInt *op,
+			 TEE_BigInt *n);
 
-void TEE_BigIntInvMod(TEE_BigInt *dest, const TEE_BigInt *op,
-		      const TEE_BigInt *n);
+void TEE_BigIntInvMod(TEE_BigInt *dest, TEE_BigInt *op,
+		      TEE_BigInt *n);
 
 /* TEE Arithmetical API - Other arithmetic operations */
 
-bool TEE_BigIntRelativePrime(const TEE_BigInt *op1, const TEE_BigInt *op2);
+bool TEE_BigIntRelativePrime(TEE_BigInt *op1, TEE_BigInt *op2);
 
 void TEE_BigIntComputeExtendedGcd(TEE_BigInt *gcd, TEE_BigInt *u,
-				  TEE_BigInt *v, const TEE_BigInt *op1,
-				  const TEE_BigInt *op2);
+				  TEE_BigInt *v, TEE_BigInt *op1,
+				  TEE_BigInt *op2);
 
-int32_t TEE_BigIntIsProbablePrime(const TEE_BigInt *op,
+int32_t TEE_BigIntIsProbablePrime(TEE_BigInt *op,
 				  uint32_t confidenceLevel);
 
 /* TEE Arithmetical API - Fast modular multiplication operations */
 
-void TEE_BigIntConvertToFMM(TEE_BigIntFMM *dest, const TEE_BigInt *src,
-			    const TEE_BigInt *n,
-			    const TEE_BigIntFMMContext *context);
+void TEE_BigIntConvertToFMM(TEE_BigIntFMM *dest, TEE_BigInt *src,
+			    TEE_BigInt *n,
+			    TEE_BigIntFMMContext *context);
 
-void TEE_BigIntConvertFromFMM(TEE_BigInt *dest, const TEE_BigIntFMM *src,
-			      const TEE_BigInt *n,
-			      const TEE_BigIntFMMContext *context);
+void TEE_BigIntConvertFromFMM(TEE_BigInt *dest, TEE_BigIntFMM *src,
+			      TEE_BigInt *n,
+			      TEE_BigIntFMMContext *context);
 
-void TEE_BigIntFMMConvertToBigInt(TEE_BigInt *dest, const TEE_BigIntFMM *src,
-				  const TEE_BigInt *n,
-				  const TEE_BigIntFMMContext *context);
+void TEE_BigIntFMMConvertToBigInt(TEE_BigInt *dest, TEE_BigIntFMM *src,
+				  TEE_BigInt *n,
+				  TEE_BigIntFMMContext *context);
 
-void TEE_BigIntComputeFMM(TEE_BigIntFMM *dest, const TEE_BigIntFMM *op1,
-			  const TEE_BigIntFMM *op2, const TEE_BigInt *n,
-			  const TEE_BigIntFMMContext *context);
+void TEE_BigIntComputeFMM(TEE_BigIntFMM *dest, TEE_BigIntFMM *op1,
+			  TEE_BigIntFMM *op2, TEE_BigInt *n,
+			  TEE_BigIntFMMContext *context);
 
 #endif /* TEE_API_H */
