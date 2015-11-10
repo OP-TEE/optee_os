f74a4a2bbf3f526e2edbdbce4f30459c48b91cda
Jens Wiklander <jens.wiklander@linaro.org>
2015-11-11 00:10:32 +0100

    libutee: arm64 syscalls
    Adds assembly implementations of arm64 syscalls.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU)
    Reviewed-by: Pascal Brand <pascal.brand@linaro.org>

--- lib/libutee/arch/arm/sub.mk
+++ lib/libutee/arch/arm/sub.mk
@@ -2,4 +2,5 @@ cppflags-y += -I$(sub-dir)/../..
 
 srcs-y += user_ta_entry.c
 srcs-y += utee_misc.c
-srcs-y += utee_syscalls_asm.S
+srcs-$(CFG_ARM32_user_ta) += utee_syscalls_a32.S
+srcs-$(CFG_ARM64_user_ta) += utee_syscalls_a64.S
