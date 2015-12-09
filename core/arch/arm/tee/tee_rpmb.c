9d296ebe31ad4ab54a9f1f897004dd1e0a4a8ea2
Jerome Forissier <jerome.forissier@linaro.org>
2015-12-09 11:12:07 +0100

    tee_rpmb.c: allocate two payload buffers for request and response
    It is not valid to allocate a single buffer with
    thread_optee_rpc_alloc_payload(), then split it into two parameters of
    type TEESMC_ATTR_TYPE_MEMREF_* in teesmc32_param. The linux driver
    would not be able to map the second buffer.
    Thus, this commit replaces the single allocation with two smaller ones.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/tee/tee_rpmb.c
+++ core/arch/arm/tee/tee_rpmb.c
@@ -348,10 +348,10 @@ func_exit:
 struct tee_rpmb_mem {
 	struct teesmc32_arg *arg;
 	paddr_t pharg;
-	paddr_t phpayload;
-	paddr_t phpayload_cookie;
 	paddr_t phreq;
+	paddr_t phreq_cookie;
 	paddr_t phresp;
+	paddr_t phresp_cookie;
 	size_t req_size;
 	size_t resp_size;
 };
@@ -380,10 +380,13 @@ static void tee_rpmb_free(struct tee_rpmb_mem *mem)
 		return;
 
 	thread_rpc_free_arg(mem->pharg);
-	thread_optee_rpc_free_payload(mem->phpayload_cookie);
+	thread_optee_rpc_free_payload(mem->phreq_cookie);
+	thread_optee_rpc_free_payload(mem->phresp_cookie);
 	mem->pharg = 0;
-	mem->phpayload = 0;
-	mem->phpayload_cookie = 0;
+	mem->phreq = 0;
+	mem->phreq_cookie = 0;
+	mem->phresp = 0;
+	mem->phresp_cookie = 0;
 }
 
 
@@ -398,15 +401,16 @@ static TEE_Result tee_rpmb_alloc(size_t req_size, size_t resp_size,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	mem->pharg = thread_rpc_alloc_arg(TEESMC32_GET_ARG_SIZE(2));
-	thread_optee_rpc_alloc_payload(req_s + resp_s, &mem->phpayload,
-				       &mem->phpayload_cookie);
-	if (!mem->pharg || !mem->phpayload) {
+	thread_optee_rpc_alloc_payload(req_s,
+				       &mem->phreq,
+				       &mem->phreq_cookie);
+	thread_optee_rpc_alloc_payload(resp_s,
+				       &mem->phresp,
+				       &mem->phresp_cookie);
+	if (!mem->pharg || !mem->phreq || !mem->phresp) {
 		res = TEE_ERROR_OUT_OF_MEMORY;
 		goto out;
 	}
-
-	mem->phreq = mem->phpayload;
-	mem->phresp = mem->phpayload + req_s;
 
 	if (core_pa2va(mem->pharg, &mem->arg) || core_pa2va(mem->phreq, req) ||
 	    core_pa2va(mem->phresp, resp)) {
