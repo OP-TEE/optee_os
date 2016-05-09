5cc4d5f1fa00c2d3fd93f659b28788925394cbcb
Jens Wiklander <jens.wiklander@linaro.org>
2016-05-09 22:11:56 +0200

    plat-mediatek: translate pa base addr before use
    Translates physical addresses used for register base addresses before
    use to be able to handle non-linear mapping of addresses.
    
    Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-mediatek/main.c
+++ core/arch/arm/plat-mediatek/main.c
@@ -30,6 +30,7 @@
 #include <kernel/generic_boot.h>
 #include <kernel/panic.h>
 #include <kernel/pm_stubs.h>
+#include <mm/core_memprot.h>
 #include <platform_config.h>
 #include <stdint.h>
 #include <tee/entry_std.h>
@@ -59,21 +60,34 @@ static void main_fiq(void)
 	panic();
 }
 
+static vaddr_t console_base(void)
+{
+	static void *va;
+
+	if (cpu_mmu_enabled()) {
+		if (!va)
+			va = phys_to_virt(CONSOLE_UART_BASE, MEM_AREA_IO_NSEC);
+		return (vaddr_t)va;
+	}
+	return CONSOLE_UART_BASE;
+}
+
 void console_init(void)
 {
-	serial8250_uart_init(CONSOLE_UART_BASE,
-			     CONSOLE_UART_CLK_IN_HZ,
+	serial8250_uart_init(console_base(), CONSOLE_UART_CLK_IN_HZ,
 			     CONSOLE_BAUDRATE);
 }
 
 void console_putc(int ch)
 {
-	serial8250_uart_putc(ch, CONSOLE_UART_BASE);
+	vaddr_t base = console_base();
+
+	serial8250_uart_putc(ch, base);
 	if (ch == '\n')
-		serial8250_uart_putc('\r', CONSOLE_UART_BASE);
+		serial8250_uart_putc('\r', base);
 }
 
 void console_flush(void)
 {
-	serial8250_uart_flush_tx_fifo(CONSOLE_UART_BASE);
+	serial8250_uart_flush_tx_fifo(console_base());
 }
