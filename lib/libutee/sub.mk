a43532cf9192ff7af3f621b963f1f4dc38f91d39
Jens Wiklander <jens.wiklander@linaro.org>
2016-06-02 14:16:56 +0200

    libutee: move tui related files to tui directory
    Moves all trusted UI related file to a separate tui subdirectory.
    
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU)
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libutee/sub.mk
+++ lib/libutee/sub.mk
@@ -12,32 +12,6 @@ srcs-y += tee_api_objects.c
 srcs-y += tee_api_operations.c
 srcs-y += tee_api_se.c
 srcs-y += tee_api_panic.c
-srcs-y += utf8.c
-srcs-y += image.c
-srcs-y += image_png.c
-srcs-y += font.c
 
-# Need to locate font.h from generated sources
-incdirs-y += .
-
-gensrcs-y += default_bold
-produce-additional-default_bold = default_bold.h
-produce-default_bold = default_bold.c
-depends-default_bold := scripts/render_font.py \
-		$(sub-dir)/amble/Amble-Bold.ttf
-recipe-default_bold := scripts/render_font.py \
-		--font_file $(sub-dir)/amble/Amble-Bold.ttf \
-		--font_size 20 --font_name default_bold \
-		--out_dir $(sub-dir-out)
-
-gensrcs-y += default_regular
-produce-additional-default_regular = default_regular.h
-produce-default_regular = default_regular.c
-depends-default_regular := scripts/render_font.py \
-		$(sub-dir)/amble/Amble-Regular.ttf
-recipe-default_regular := scripts/render_font.py \
-		--font_file $(sub-dir)/amble/Amble-Regular.ttf \
-		--font_size 20 --font_name default_regular \
-		--out_dir $(sub-dir-out)
-
+subdirs-y += tui
 subdirs-y += arch/$(ARCH)
---
+++ lib/libutee/tui/sub.mk
@@ -0,0 +1,29 @@
+srcs-y += utf8.c
+srcs-y += image.c
+srcs-y += image_png.c
+srcs-y += font.c
+
+# Need to locate font.h from generated sources
+incdirs-y += .
+
+gensrcs-y += default_bold
+produce-additional-default_bold = default_bold.h
+produce-default_bold = default_bold.c
+depends-default_bold := scripts/render_font.py \
+		$(sub-dir)/fonts/amble/Amble-Bold.ttf
+recipe-default_bold := scripts/render_font.py \
+		--font_file $(sub-dir)/fonts/amble/Amble-Bold.ttf \
+		--font_size 20 --font_name default_bold \
+		--out_dir $(sub-dir-out)
+
+gensrcs-y += default_regular
+produce-additional-default_regular = default_regular.h
+produce-default_regular = default_regular.c
+depends-default_regular := scripts/render_font.py \
+		$(sub-dir)/fonts/amble/Amble-Regular.ttf
+recipe-default_regular := scripts/render_font.py \
+		--font_file $(sub-dir)/fonts/amble/Amble-Regular.ttf \
+		--font_size 20 --font_name default_regular \
+		--out_dir $(sub-dir-out)
+
+
