290e18b3a06c0d9ac00ea66882054b4198336c83
Soren Brinkmann <soren.brinkmann@xilinx.com>
2016-06-24 13:36:35 -0700

    zynqmp: Add static TA for bitstream loading
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>

---
+++ lib/libutee/include/zynqmp_bsload.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016, Xilinx Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TA_ZYNQMP_BSLOAD_H
+#define TA_ZYNQMP_BSLOAD_H
+
+/*
+ * This UUID is generated with uuidgen
+ * the ITU-T UUID generator at http://www.itu.int/ITU-T/asn1/uuid.html
+ */
+#define ZYNQMP_BSLOAD_UUID { 0xb0bb3f31, 0x907d, 0x4e2f, \
+		{ 0x94, 0xa0, 0xf5, 0xb0, 0x14, 0x55, 0x6f, 0xeb} }
+
+/* The TAFs ID implemented in this TA */
+#define ZYNQMP_BSLOAD_CMD_LOAD_BITSTREAM	0
+
+#endif /*TA_ZYNQMP_BSLOAD_H*/
