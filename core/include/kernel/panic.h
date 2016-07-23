87a092a7d391b75490a2b39f219f09c5a566ba4b
Etienne Carriere <etienne.carriere@linaro.org>
2016-07-23 10:17:41 +0200

    core: panic() macro accepts zero or one string argument.
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jen.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (QEMU)

--- core/include/kernel/panic.h
+++ core/include/kernel/panic.h
@@ -30,8 +30,29 @@
 
 #include <compiler.h>
 
-#define panic()	__panic(__FILE__, __LINE__, __func__)
+/* debug disabled => __FILE__, ... and panic message are not built. */
+#if defined(CFG_TEE_CORE_DEBUG) && CFG_TEE_CORE_DEBUG != 0
+#define __panic(str)	__do_panic(__FILE__, __LINE__, __func__, str)
+#else
+#define __panic(str)	__do_panic((void *)0, 0, (void *)0, (void *)0)
+#endif
 
-void __panic(const char *file, int line, const char *func) __noreturn;
+void __do_panic(const char *file, const int line, const char *func,
+		const char *msg) __noreturn;
+
+/*
+ * Suppress GCC warning on expansion of the panic() macro with no argument:
+ *  'ISO C99 requires at least one argument for the "..." in a variadic macro'
+ * Occurs when '-pedantic' is combined with '-std=gnu99'.
+ * Suppression applies only to this file and the expansion of macros defined in
+ * this file.
+ */
+#pragma GCC system_header
+
+/* panic() can get a string or no argument */
+#define _panic0()	__panic((void *)0)
+#define _panic1(s)	__panic(s)
+#define _panic_fn(a, b, name, ...) name
+#define panic(...) _panic_fn("", ##__VA_ARGS__, _panic1, _panic0)(__VA_ARGS__)
 
 #endif /*KERNEL_PANIC_H*/
--- core/kernel/panic.c
+++ core/kernel/panic.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2016, Linaro Limited
  * Copyright (c) 2014, STMicroelectronics International N.V.
  * All rights reserved.
  *
@@ -29,12 +30,26 @@
 #include <kernel/thread.h>
 #include <trace.h>
 
-void __panic(const char *file __maybe_unused, int line __maybe_unused,
-		const char *func __maybe_unused)
+void __do_panic(const char *file __maybe_unused,
+		const int line __maybe_unused,
+		const char *func __maybe_unused,
+		const char *msg __maybe_unused)
 {
-	uint32_t __unused exceptions = thread_mask_exceptions(THREAD_EXCP_ALL);
+	/* disable prehemption */
+	(void)thread_mask_exceptions(THREAD_EXCP_ALL);
 
-	EMSG_RAW("PANIC: %s %s:%d\n", func, file, line);
+	/* TODO: notify other cores */
+
+	/* trace: Panic ['panic-string-message' ]at FILE:LINE [<FUNCTION>]" */
+	if (!file && !func && !msg)
+		EMSG_RAW("Panic");
+	else
+		EMSG_RAW("Panic %s%s%sat %s:%d %s%s%s",
+			 msg ? "'" : "", msg ? msg : "", msg ? "' " : "",
+			 file ? file : "?", file ? line : 0,
+			 func ? "<" : "", func ? func : "", func ? ">" : "");
+
+	/* abort current execution */
 	while (1)
 		;
 }
