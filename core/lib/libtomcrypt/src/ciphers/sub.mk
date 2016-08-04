34777d462d98cb7a9b370044bee64c89b84cb3e6
Jerome Forissier <jerome.forissier@linaro.org>
2016-08-04 16:44:55 +0200

    arm64: libtomcrypt: rename AES CE files
    aes_arm64_ce.c contains wrapper code for the assembly routines that
    implement AES using the ARMv8-A cryptographic extensions. It is
    currently used only in 64-bit builds, but could be used for 32-bits
    as well. So, rename it to aes_armv8a_ce.c to better reflect its content
    and prepare for 32-bit support. Also rename the 64-bit assembly file
    (aes_modes_arm64_ce_a64.S -> aes_modes_armv8a_ce_a64.S) for
    consistency.
    Note: this naming scheme (xyz_armv8a_ce.c / xyz_armv8a_ce_a{32,64}.S)
    is already used for SHA1 and SHA2.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: David Brown <david.brown@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/ciphers/sub.mk
+++ core/lib/libtomcrypt/src/ciphers/sub.mk
@@ -1,10 +1,10 @@
 cflags-y += -Wno-unused-parameter
 
 ifeq ($(CFG_CRYPTO_AES_ARM64_CE),y)
-srcs-y += aes_arm64_ce.c
-cflags-aes_arm64_ce.c-y += -march=armv8-a+crypto
-srcs-y += aes_modes_arm64_ce_a64.S
-aflags-aes_modes_arm64_ce_a64.S-y += -DINTERLEAVE=4
+srcs-y += aes_armv8a_ce.c
+cflags-aes_armv8a_ce.c-y += -march=armv8-a+crypto
+srcs-y += aes_modes_armv8a_ce_a64.S
+aflags-aes_modes_armv8a_ce_a64.S-y += -DINTERLEAVE=4
 else
 srcs-$(CFG_CRYPTO_AES) += aes.c
 srcs-$(CFG_CRYPTO_AES) += aes_tab.c
