ca70d0b5c51b923005299a31d2a4811b0efe7ecf
Jens Wiklander <jens.wiklander@linaro.org>
2016-08-19 10:44:46 +0200

    core: add late service_init and driver_init
    Adds service_init_late() for init calls that depends on other services.
    Adds driver_init_late() for init calls that depends on other drivers.
    
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/include/initcall.h
+++ core/include/initcall.h
@@ -37,6 +37,8 @@ typedef TEE_Result (*initcall_t)(void);
 	__attribute__((__section__(".initcall" level))) = fn
 
 #define service_init(fn)	__define_initcall("1", fn)
-#define driver_init(fn)		__define_initcall("2", fn)
+#define service_init_late(fn)	__define_initcall("2", fn)
+#define driver_init(fn)		__define_initcall("3", fn)
+#define driver_init_late(fn)	__define_initcall("4", fn)
 
 #endif
