OP-TEE/optee_os

commit 3d34e125a253aad5152b99fd4cce84f6565fa479

Add support for $(cflags-lib-y)
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
Tested-by: Pascal Brand <pascal.brand@linaro.org> (STM platform)

mk/compile.mk
@@ -66,7 +66,7 @@ comp-flags-$2 = $$(filter-out $$(CFLAGS_REMOVE) $$(cflags-remove) \
 			      $$(cflags-remove-$2), \
 		   $$(CFLAGS) $$(CFLAGS_WARNS) \
 		   $$(comp-cflags$$(comp-sm-$2)) $$(cflags$$(comp-sm-$2)) \
-		   $$(cflags-$2))
+		   $$(cflags-lib$$(comp-lib-$2)) $$(cflags-$2))
 else ifeq ($$(filter %.S,$1),$1)
 comp-flags-$2 = -DASM=1 $$(filter-out $$(AFLAGS_REMOVE) $$(aflags-remove) \
 				      $$(aflags-remove-$2), \

mk/subdir.mk
@@ -11,6 +11,7 @@
 #         incdirs-$(oname)
 #         incdirs-lib$(libname)  [if libname is defined]
 #         cppflags-lib$(libname) [if libname is defined]
+#         cflags-lib$(libname)   [if libname is defined]
 # for each file found, oname is the name of the object file for corresponding
 # source file
 
@@ -44,6 +45,7 @@ incdirs-$$(oname)		:= $$(thissubdir-incdirs) $$(addprefix $(sub-dir)/,$$(incdirs
 # in another subdirectory
 cflags-$(1)-y 			:=
 cflags-remove-$(1)-y		:=
+cflags-lib-y			:=
 cppflags-$(1)-y			:=
 cppflags-remove-$(1)-y		:=
 cppflags-lib-y			:=
@@ -62,6 +64,7 @@ incdirs$(sm) := $(incdirs$(sm)) $$(addprefix $1/,$$(global-incdirs-y))
 thissubdir-incdirs := $$(addprefix $1/,$$(incdirs-y))
 ifneq ($$(libname),)
 incdirs-lib$$(libname) := $$(incdirs-lib$$(libname)) $$(addprefix $1/,$$(incdirs-lib-y))
+cflags-lib$$(libname) := $$(cflags-lib$$(libname)) $$(cflags-lib-y)
 cppflags-lib$$(libname) := $$(cppflags-lib$$(libname)) $$(cppflags-lib-y)
 endif
 
@@ -70,6 +73,7 @@ $$(foreach s, $$(srcs-y), $$(eval $$(call process-subdir-srcs-y,$$(s))))
 # Clear flags used when processing current directory
 srcs-y :=
 cflags-y :=
+cflags-lib-y :=
 cppflags-y :=
 cppflags-lib-y :=
 aflags-y :=

commit 606f378c8a2aad962554fe0af00628a82fcdd699

build: include submodule in name of per-library variables
Because a library may be built several times for different submodules
(for example, libutils.a is built differently for kernel and user
mode), the names of the variables that hold library-specific flags
have to uniquely identify the instance of the library that's being
built.

Example use case: to build libutils with flag FOO for the core and
flag BAR for the user mode, add this to lib/libutils/sub.mk:

  ifeq ($(sm),core)
  cppflags-lib-y += -DFOO
  else
  cppflags-lib-y += -DBAR
  endif

Without this fix, the above would fail because all the flags would
be added to all the builds of libutils.

Fixes: 3d34e125a253 ("Add support for $(cflags-lib-y)")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

mk/compile.mk
@@ -63,7 +63,7 @@ objs		+= $2
 comp-dep-$2	:= $$(dir $2).$$(notdir $2).d
 comp-cmd-file-$2:= $$(dir $2).$$(notdir $2).cmd
 comp-sm-$2	:= $(sm)
-comp-lib-$2	:= $(libname)
+comp-lib-$2	:= $(libname)-$(sm)
 
 cleanfiles := $$(cleanfiles) $$(comp-dep-$2) $$(comp-cmd-file-$2) $2
 

mk/subdir.mk
@@ -9,9 +9,9 @@
 #         aflags-$(oname) aflags-remove-$(oname)
 #         cppflags-$(oname) cppflags-remove-$(oname)
 #         incdirs-$(oname)
-#         incdirs-lib$(libname)  [if libname is defined]
-#         cppflags-lib$(libname) [if libname is defined]
-#         cflags-lib$(libname)   [if libname is defined]
+#         incdirs-lib$(libname)-$(sm)  [if libname is defined]
+#         cppflags-lib$(libname)-$(sm) [if libname is defined]
+#         cflags-lib$(libname)-$(sm)   [if libname is defined]
 # for each file found, oname is the name of the object file for corresponding
 # source file
 
@@ -119,9 +119,9 @@ sub-subdirs := $$(addprefix $1/,$$(subdirs-y))
 incdirs$(sm) := $(incdirs$(sm)) $$(addprefix $1/,$$(global-incdirs-y))
 thissubdir-incdirs := $(out-dir)/$(base-prefix)$1 $$(addprefix $1/,$$(incdirs-y))
 ifneq ($$(libname),)
-incdirs-lib$$(libname) := $$(incdirs-lib$$(libname)) $$(addprefix $1/,$$(incdirs-lib-y))
-cflags-lib$$(libname) := $$(cflags-lib$$(libname)) $$(cflags-lib-y)
-cppflags-lib$$(libname) := $$(cppflags-lib$$(libname)) $$(cppflags-lib-y)
+incdirs-lib$$(libname)-$$(sm) := $$(incdirs-lib$$(libname)-$$(sm)) $$(addprefix $1/,$$(incdirs-lib-y))
+cflags-lib$$(libname)-$$(sm) := $$(cflags-lib$$(libname)-$$(sm)) $$(cflags-lib-y)
+cppflags-lib$$(libname)-$$(sm) := $$(cppflags-lib$$(libname)-$$(sm)) $$(cppflags-lib-y)
 endif
 
 # Process files in current directory