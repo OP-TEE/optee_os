OP-TEE/optee_os

commit 287359f44187396c3e12d1bab257ce5918a58cb0

crypto: fix incorrect algo passed to cipher.final()
Fix an error in function tee_svc_cipher_update_helper, and add assert
in function cipher_final to prevent it being called by an algorithm
that is not a symmetric cipher.

Signed-off-by: lackan <liang.guanchao@linaro.org>
[Reword commit subject]
Signed-off-by: Jerome Forissier <jerome.forissier@.linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/lib/libtomcrypt/src/tee_ltc_provider.c
@@ -2219,7 +2219,7 @@ static void cipher_final(void *ctx, uint32_t algo)
 		break;
 #endif
 	default:
-		/* TEE_ERROR_NOT_SUPPORTED; */
+		assert(TEE_ALG_GET_CLASS(algo) == TEE_OPERATION_CIPHER);
 		break;
 	}
 }

core/tee/tee_svc_cryp.c
@@ -2471,7 +2471,7 @@ static TEE_Result tee_svc_cipher_update_helper(unsigned long state,
 	}
 
 	if (last_block && cs->ctx_finalize != NULL) {
-		cs->ctx_finalize(cs->ctx, cs->mode);
+		cs->ctx_finalize(cs->ctx, cs->algo);
 		cs->ctx_finalize = NULL;
 	}
 

commit caabd5feb0d3bc81e215bd7ec6ebf39a379760c3

crypto: ltc: add missing CBC_MAC algorithms to cipher_final()
Make sure that all the code paths leading to cipher_final() are
properly handled. Then, the 'default:' case cannot occur; add an
assert() there. Fixes an assertion failure with xtest 4002 when
CFG_TEE_CORE_DEBUG=y.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reported-by: Zeng Tao <prime.zeng@hisilicon.com>
Fixes: 287359f44187 ("crypto: fix incorrect algo passed to cipher.final()")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Tested-by: Zeng Tao <prime.zeng@hisilicon.com>

core/lib/libtomcrypt/src/tee_ltc_provider.c
@@ -2199,6 +2199,12 @@ static void cipher_final(void *ctx, uint32_t algo)
 	case TEE_ALG_AES_CBC_NOPAD:
 	case TEE_ALG_DES_CBC_NOPAD:
 	case TEE_ALG_DES3_CBC_NOPAD:
+	case TEE_ALG_AES_CBC_MAC_NOPAD:
+	case TEE_ALG_AES_CBC_MAC_PKCS5:
+	case TEE_ALG_DES_CBC_MAC_NOPAD:
+	case TEE_ALG_DES_CBC_MAC_PKCS5:
+	case TEE_ALG_DES3_CBC_MAC_NOPAD:
+	case TEE_ALG_DES3_CBC_MAC_PKCS5:
 		cbc_done(ctx);
 		break;
 #endif
@@ -2219,7 +2225,7 @@ static void cipher_final(void *ctx, uint32_t algo)
 		break;
 #endif
 	default:
-		assert(TEE_ALG_GET_CLASS(algo) == TEE_OPERATION_CIPHER);
+		assert(!"Unhandled algo");
 		break;
 	}
 }