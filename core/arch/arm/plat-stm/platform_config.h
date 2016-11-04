OP-TEE/optee_os

commit 8cd89706f19cdb0f16afa7707263d6dbd4d6d664

plat-stm: beautify platform config
Move platform config to platfom_config.h.
Remove local system_config.mk.

Add generic SCU SAC/NSAC registers bit fields definition.
Add generic PL310 control register enable bit fields definition.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-stm/platform_config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Linaro Limited
+ * Copyright (c) 2014-2016, Linaro Limited
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,17 +28,205 @@
 #ifndef PLATFORM_CONFIG_H
 #define PLATFORM_CONFIG_H
 
-/* Make stacks aligned to data cache line length */
-#define STACK_ALIGNMENT		32
+/* Below are platform/SoC settings specific to stm platform flavors */
+
+#if defined(PLATFORM_FLAVOR_b2260)
 
 #define CFG_TEE_CORE_NB_CORE	2
 
-#ifdef CFG_WITH_PAGER
-#error "Pager not supported for platform STM"
+#ifndef CFG_DDR_START
+#define CFG_DDR_START		0x40000000
+#define CFG_DDR_SIZE		0x40000000
 #endif
-#ifdef CFG_WITH_LPAE
-#error "LPAE not supported for platform STM"
+#ifndef CFG_DDR_TEETZ_RESERVED_START
+#define CFG_DDR_TEETZ_RESERVED_START	0x7E000000
+#define CFG_DDR_TEETZ_RESERVED_SIZE	0x01E00000
 #endif
+
+#define CPU_IOMEM_BASE		0x08760000
+#define CPU_PORT_FILT_START	0x40000000
+#define CPU_PORT_FILT_END	0xC0000000
+#define STXHxxx_LPM_PERIPH_BASE	0x09700000
+#define RNG_BASE		0x08A89000
+
+#define ASC_NUM			21
+#define UART_CONSOLE_BASE	ST_ASC21_REGS_BASE
+
+#elif defined(PLATFORM_FLAVOR_cannes)
+
+#define CFG_TEE_CORE_NB_CORE	2
+
+#ifndef CFG_DDR_START
+#define CFG_DDR_START		0x40000000
+#define CFG_DDR_SIZE		0x80000000
+#endif
+#ifndef CFG_DDR_TEETZ_RESERVED_START
+#define CFG_DDR_TEETZ_RESERVED_START	0x93a00000
+#define CFG_DDR_TEETZ_RESERVED_SIZE	0x01000000
+#endif
+
+#define CPU_IOMEM_BASE		0x08760000
+#define CPU_PORT_FILT_START	0x40000000
+#define CPU_PORT_FILT_END	0xC0000000
+#define STXHxxx_LPM_PERIPH_BASE	0x09300000
+#define RNG_BASE		0x08A89000
+
+#define ASC_NUM			20
+#define UART_CONSOLE_BASE	ST_ASC21_REGS_BASE
+
+#elif defined(PLATFORM_FLAVOR_orly2)
+
+#define CFG_TEE_CORE_NB_CORE	2
+
+#ifndef CFG_DDR_START
+#define CFG_DDR_START		0x40000000
+#define CFG_DDR_SIZE		0x40000000
+#define CFG_DDR1_START		0x80000000
+#define CFG_DDR1_SIZE		0x40000000
+#endif
+#ifndef CFG_DDR_TEETZ_RESERVED_START
+#define CFG_DDR_TEETZ_RESERVED_START	0x8F000000
+#define CFG_DDR_TEETZ_RESERVED_SIZE	0x00800000
+#endif
+
+#define CPU_IOMEM_BASE		0xFFFE0000
+#define CPU_PORT_FILT_START	0x40000000
+#define CPU_PORT_FILT_END	0x80000000
+#define STXHxxx_LPM_PERIPH_BASE	0xFE400000
+#define RNG_BASE		0xFEE80000
+
+#define ASC_NUM			21
+#define UART_CONSOLE_BASE	ST_ASC21_REGS_BASE
+
+#else /* defined(PLATFORM_FLAVOR_xxx) */
+
+#error "Unknown platform flavor"
+
+#endif /* defined(PLATFORM_FLAVOR_xxx) */
+
+/* Below are settings common to stm platform flavors */
+
+/*
+ * CP15 Secure ConTroL Register (SCTLR
+ *
+ * - Round-Robin replac. for icache, btac, i/duTLB (bit14: RoundRobin)
+ */
+#define CPU_SCTLR_INIT			0x00004000
+
+/*
+ * CP15 Auxiliary ConTroL Register (ACTRL)
+ *
+ * - core always in full SMP (FW bit0=1, SMP bit6=1)
+ * - L2 write full line of zero disabled (bit3=0)
+ *   (keep WFLZ low. Will be set once outer L2 is ready)
+ */
+
+#define CPU_ACTLR_INIT			0x00000041
+
+/*
+ * CP15 NonSecure Access Control Register (NSACR)
+ *
+ * - NSec cannot change ACTRL.SMP (NS_SMP bit18=0)
+ * - Nsec can lockdown TLB (TL bit17=1)
+ * - NSec cannot access PLE (PLE bit16=0)
+ * - NSec can use SIMD/VFP (CP10/CP11) (bit15:14=2b00, bit11:10=2b11)
+ */
+#define CPU_NSACR_INIT			0x00020C00
+
+/*
+ * CP15 Power Control Register (PCR)
+ *
+ * - no change latency, enable clk gating
+ */
+#define CPU_PCR_INIT			0x00000001
+
+
+/*
+ * SCU Secure Access Control / NonSecure Access Control
+ *
+ * SAC:  Both secure CPU access SCU (bit[3:0]).
+ * NSAC: Both nonsec cpu access SCU (bit[3:0]), private timers (bit[7:4])
+ *       and global timers (bit[11:8]).
+ */
+#if !defined(SCU_SAC_INIT) || !defined(SCU_NSAC_INIT)
+#define SCU_CPUS_MASK		(SHIFT_U32(1, CFG_TEE_CORE_NB_CORE) - 1)
+
+#define SCU_SAC_INIT	SCU_CPUS_MASK
+#define SCU_NSAC_INIT	(SHIFT_U32(SCU_CPUS_MASK, SCU_NSAC_SCU_SHIFT) | \
+			SHIFT_U32(SCU_CPUS_MASK, SCU_NSAC_PTIMER_SHIFT) | \
+			SHIFT_U32(SCU_CPUS_MASK, SCU_NSAC_GTIMER_SHIFT))
+#endif
+
+/*
+ * PL310 TAG RAM Control Register
+ *
+ * bit[10:8]:1 - 2 cycle of write accesses latency
+ * bit[6:4]:1 - 2 cycle of read accesses latency
+ * bit[2:0]:1 - 2 cycle of setup latency
+ */
+#ifndef PL310_TAG_RAM_CTRL_INIT
+#define PL310_TAG_RAM_CTRL_INIT		0x00000111
+#endif
+
+/*
+ * PL310 DATA RAM Control Register
+ *
+ * bit[10:8]:2 - 3 cycle of write accesses latency
+ * bit[6:4]:2 - 3 cycle of read accesses latency
+ * bit[2:0]:2 - 3 cycle of setup latency
+ */
+#ifndef PL310_DATA_RAM_CTRL_INIT
+#define PL310_DATA_RAM_CTRL_INIT	0x00000222
+#endif
+
+/*
+ * PL310 Auxiliary Control Register
+ *
+ * I/Dcache prefetch enabled (bit29:28=2b11)
+ * NS can access interrupts (bit27=1)
+ * NS can lockown cache lines (bit26=1)
+ * Pseudo-random replacement policy (bit25=0)
+ * Force write allocated (default)
+ * Shared attribute internally ignored (bit22=1, bit13=0)
+ * Parity disabled (bit21=0)
+ * Event monitor disabled (bit20=0)
+ * Platform fmavor specific way config:
+ * - way size (bit19:17)
+ * - way associciativity (bit16)
+ * Store buffer device limitation enabled (bit11=1)
+ * Cacheable accesses have high prio (bit10=0)
+ * Full Line Zero (FLZ) disabled (bit0=0)
+ */
+#ifndef PL310_AUX_CTRL_INIT
+#define PL310_AUX_CTRL_INIT		0x3C480800
+#endif
+
+/*
+ * PL310 Prefetch Control Register
+ *
+ * Double linefill disabled (bit30=0)
+ * I/D prefetch enabled (bit29:28=2b11)
+ * Prefetch drop enabled (bit24=1)
+ * Incr double linefill disable (bit23=0)
+ * Prefetch offset = 7 (bit4:0)
+ */
+#define PL310_PREFETCH_CTRL_INIT	0x31000007
+
+/*
+ * PL310 Power Register
+ *
+ * Dynamic clock gating enabled
+ * Standby mode enabled
+ */
+#define PL310_POWER_CTRL_INIT		0x00000003
+
+/*
+ * SCU Control Register : CTRL = 0x00000065
+ * - ic stanby enable=1
+ * - scu standby enable=1
+ * - scu enable=1
+ */
+#define SCU_CTRL_INIT			0x00000065
 
 /*
  * TEE/TZ RAM layout:
@@ -63,8 +251,12 @@
 #error "Invalid CFG_DDR_TEETZ_RESERVED_SIZE: at least 4MB expected"
 #endif
 
+#ifndef CFG_SHMEM_SIZE
 #define CFG_SHMEM_SIZE		(2 * 1024 * 1024)
+#endif
+#ifndef CFG_TEE_RAM_PH_SIZE
 #define CFG_TEE_RAM_PH_SIZE	(1 * 1024 * 1024)
+#endif
 #define CFG_TA_RAM_SIZE		(CFG_DDR_TEETZ_RESERVED_SIZE - \
 				 CFG_TEE_RAM_PH_SIZE - CFG_SHMEM_SIZE)
 
@@ -77,57 +269,11 @@
 #define CFG_TA_RAM_START	(CFG_TEE_RAM_START + CFG_TEE_RAM_PH_SIZE)
 #define CFG_SHMEM_START		(CFG_TA_RAM_START + CFG_TA_RAM_SIZE)
 
-#if defined(PLATFORM_FLAVOR_b2260)
-
-#define DRAM0_BASE		0x40000000
-#define DRAM0_SIZE		0x40000000
-
-#define CPU_IOMEM_BASE		0x08760000
-#define CPU_PORT_FILT_START	0x40000000
-#define CPU_PORT_FILT_END	0xC0000000
-#define STXHxxx_LPM_PERIPH_BASE	0x09700000
-#define ASC_NUM			21
-#define UART_CONSOLE_BASE	ST_ASC21_REGS_BASE
-#define RNG_BASE		0x08A89000
-
-#define PL310_AUX_CTRL_INIT	0x3C480800
-
-#elif defined(PLATFORM_FLAVOR_cannes)
-
-#define DRAM0_BASE		0x40000000
-#define DRAM0_SIZE		0x80000000
-
-#define CPU_IOMEM_BASE		0x08760000
-#define CPU_PORT_FILT_START	0x40000000
-#define CPU_PORT_FILT_END	0xC0000000
-#define STXHxxx_LPM_PERIPH_BASE	0x09400000
-#define ASC_NUM			20
-#define UART_CONSOLE_BASE	ST_ASC20_REGS_BASE
-#define RNG_BASE		0x08A89000
-
-#define PL310_AUX_CTRL_INIT	0x3C480800
-
-#elif defined(PLATFORM_FLAVOR_orly2)
-
-#define DRAM0_BASE		0x40000000
-#define DRAM0_SIZE		0x40000000
-#define DRAM1_BASE		0x80000000
-#define DRAM1_SIZE		0x40000000
-
-#define CPU_IOMEM_BASE		0xFFFE0000
-#define CPU_PORT_FILT_START	0x40000000
-#define CPU_PORT_FILT_END	0x80000000
-#define STXHxxx_LPM_PERIPH_BASE	0xFE400000
-#define ASC_NUM			21
-#define UART_CONSOLE_BASE	ST_ASC21_REGS_BASE
-#define RNG_BASE		0xFEE80000
-
-#define PL310_AUX_CTRL_INIT	0x3C480800
-
-#else
-
-#error "Unknown platform flavor"
-
+#define DRAM0_BASE		CFG_DDR_START
+#define DRAM0_SIZE		CFG_DDR_SIZE
+#ifdef CFG_DDR1_START
+#define DRAM1_BASE		CFG_DDR1_START
+#define DRAM1_SIZE		CFG_DDR1_SIZE
 #endif
 
 #define CFG_TEE_RAM_VA_SIZE	(1024 * 1024)
@@ -143,22 +289,7 @@
 #define ST_ASC20_REGS_BASE	(STXHxxx_LPM_PERIPH_BASE + 0x00130000)
 #define ST_ASC21_REGS_BASE	(STXHxxx_LPM_PERIPH_BASE + 0x00131000)
 
-#define DEVICE0_PA_BASE		ROUNDDOWN(CPU_IOMEM_BASE, \
-					  CORE_MMU_DEVICE_SIZE)
-#define DEVICE0_VA_BASE		DEVICE0_PA_BASE
-#define DEVICE0_SIZE		CORE_MMU_DEVICE_SIZE
-#define DEVICE0_TYPE		MEM_AREA_IO_SEC
+/* Make stacks aligned to data cache line length */
+#define STACK_ALIGNMENT		32
 
-#define DEVICE1_PA_BASE		ROUNDDOWN(UART_CONSOLE_BASE, \
-					  CORE_MMU_DEVICE_SIZE)
-#define DEVICE1_VA_BASE		DEVICE1_PA_BASE
-#define DEVICE1_SIZE		CORE_MMU_DEVICE_SIZE
-#define DEVICE1_TYPE		MEM_AREA_IO_NSEC
-
-#define DEVICE2_PA_BASE		ROUNDDOWN(RNG_BASE, \
-					  CORE_MMU_DEVICE_SIZE)
-#define DEVICE2_VA_BASE		DEVICE2_PA_BASE
-#define DEVICE2_SIZE		CORE_MMU_DEVICE_SIZE
-#define DEVICE2_TYPE		MEM_AREA_IO_SEC
-
-#endif /*PLATFORM_CONFIG_H*/
+#endif /* PLATFORM_CONFIG_H */

core/arch/arm/include/kernel/tz_ssvce_def.h
@@ -99,6 +99,8 @@
 #define PL310_PREFETCH_CTRL	0xF60
 #define PL310_POWER_CTRL	0xF80
 
+#define PL310_CTRL_ENABLE_BIT	BIT32(0)
+
 /*
  * SCU iomem
  */
@@ -111,6 +113,14 @@
 #define SCU_SAC		0x50
 #define SCU_NSAC	0x54
 #define SCU_ERRATA744369 0x30
+
+#define SCU_ACCESS_CONTROL_CPU0		BIT32(0)
+#define SCU_ACCESS_CONTROL_CPU1		BIT32(1)
+#define SCU_ACCESS_CONTROL_CPU2		BIT32(2)
+#define SCU_ACCESS_CONTROL_CPU3		BIT32(3)
+#define SCU_NSAC_SCU_SHIFT		0
+#define SCU_NSAC_PTIMER_SHIFT		4
+#define SCU_NSAC_GTIMER_SHIFT		8
 
 /*
  * GIC iomem

core/arch/arm/plat-stm/conf.mk
@@ -11,10 +11,13 @@ $(call force,CFG_PL310,y)
 $(call force,CFG_CACHE_API,y)
 $(call force,CFG_PM_STUBS,y)
 $(call force,CFG_GENERIC_BOOT,y)
-$(call force,CFG_BOOT_SYNC_CPU,y)
+$(call force,CFG_WITH_LPAE,n)
 
 ta-targets = ta_arm32
 
+CFG_WITH_PAGER ?= n
+CFG_BOOT_SYNC_CPU ?= y
+CFG_TEE_CORE_EMBED_INTERNAL_TESTS ?= y
 CFG_WITH_STACK_CANARIES ?= y
 CFG_WITH_STATS ?= y
 CFG_WITH_SOFTWARE_PRNG ?= n
@@ -26,5 +29,3 @@ else
 CFG_PL310_LOCKED ?= n
 CFG_TEE_GDB_BOOT ?= y
 endif
-
-include $(platform-dir)/system_config.mk

core/arch/arm/plat-stm/main.c
@@ -42,6 +42,7 @@
 #include <tee/entry_std.h>
 #include <tee/entry_fast.h>
 #include <trace.h>
+#include <util.h>
 
 register_phys_mem(MEM_AREA_IO_SEC, CPU_IOMEM_BASE, CORE_MMU_DEVICE_SIZE);
 register_phys_mem(MEM_AREA_IO_SEC, RNG_BASE, CORE_MMU_DEVICE_SIZE);
@@ -134,71 +135,15 @@ vaddr_t pl310_base(void)
 
 void arm_cl2_config(vaddr_t pl310)
 {
-	uint32_t v;
 	/* pl310 off */
 	write32(0, pl310 + PL310_CTRL);
 
-	/*
-	 * TAG RAM Control Register
-	 *
-	 * bit[10:8]:1 - 2 cycle of write accesses latency
-	 * bit[6:4]:1 - 2 cycle of read accesses latency
-	 * bit[2:0]:1 - 2 cycle of setup latency
-	 */
-	v = read32(pl310 + PL310_TAG_RAM_CTRL);
-	v &= 0xFFFFF888;
-	v |= 0xFFFFF999;
-	write32(v, pl310 + PL310_TAG_RAM_CTRL);
-
-	/*
-	 * DATA RAM Control Register
-	 *
-	 * bit[10:8]:2 - 3 cycle of write accesses latency
-	 * bit[6:4]:2 - 3 cycle of read accesses latency
-	 * bit[2:0]:2 - 3 cycle of setup latency
-	 */
-	v = read32(pl310 + PL310_DATA_RAM_CTRL);
-	v &= 0xFFFFF888;
-	v |= 0xFFFFFAAA;
-	write32(v, pl310 + PL310_DATA_RAM_CTRL);
-
-	/*
-	 * Auxiliary Control Register
-	 *
-	 * I/Dcache prefetch enabled (bit29:28=2b11)
-	 * NS can access interrupts (bit27=1)
-	 * NS can lockown cache lines (bit26=1)
-	 * Pseudo-random replacement policy (bit25=0)
-	 * Force write allocated (default)
-	 * Shared attribute internally ignored (bit22=1, bit13=0)
-	 * Parity disabled (bit21=0)
-	 * Event monitor disabled (bit20=0)
-	 * Set or preserved way config: size (bit19:17), ass (bit16)
-	 * Store buffer device limitation enabled (bit11=1)
-	 * Cacheable accesses have high prio (bit10=0)
-	 * Full Line Zero (FLZ) disabled (bit0=0)
-	 */
-	v = PL310_AUX_CTRL_INIT;
-	write32(v, pl310 + PL310_AUX_CTRL);
-
-	/*
-	 * Prefetch Control Register
-	 *
-	 * Double linefill disabled (bit30=0)
-	 * I/D prefetch enabled (bit29:28=2b11)
-	 * Prefetch drop enabled (bit24=1)
-	 * Incr double linefill disable (bit23=0)
-	 * Prefetch offset = 7 (bit4:0)
-	 */
-	write32(0x31000007, pl310 + PL310_PREFETCH_CTRL);
-
-	/*
-	 * Power Register
-	 *
-	 * Dynamic clock gating enabled
-	 * Standby mode enabled
-	 */
-	write32(0x00000003, pl310 + PL310_POWER_CTRL);
+	/* config PL310 */
+	write32(PL310_TAG_RAM_CTRL_INIT, pl310 + PL310_TAG_RAM_CTRL);
+	write32(PL310_DATA_RAM_CTRL_INIT, pl310 + PL310_DATA_RAM_CTRL);
+	write32(PL310_AUX_CTRL_INIT, pl310 + PL310_AUX_CTRL);
+	write32(PL310_PREFETCH_CTRL_INIT, pl310 + PL310_PREFETCH_CTRL);
+	write32(PL310_POWER_CTRL_INIT, pl310 + PL310_POWER_CTRL);
 
 	/* invalidate all pl310 cache ways */
 	arm_cl2_invbyway(pl310);
@@ -216,31 +161,17 @@ void plat_cpu_reset_late(void)
 	if (get_core_pos())
 		return;
 
-	/* both secure CPU access SCU */
-	write32(3, SCU_BASE + SCU_SAC);
-
-	/* both nonsec cpu access SCU, private and global timer */
-	write32(0x333, SCU_BASE + SCU_NSAC);
-
-	/* SCU Filtering End Address register */
+	write32(SCU_SAC_INIT, SCU_BASE + SCU_SAC);
+	write32(SCU_NSAC_INIT, SCU_BASE + SCU_NSAC);
 	write32(CPU_PORT_FILT_END, SCU_BASE + SCU_FILT_EA);
 	write32(CPU_PORT_FILT_START, SCU_BASE + SCU_FILT_SA);
+	write32(SCU_CTRL_INIT, SCU_BASE + SCU_CTRL);
 
-	/*
-	 * SCU Control Register : CTRL = 0x00000065
-	 * - ic stanby enable=1
-	 * - scu standby enable=1
-	 * - scu enable=1
-	 */
-	write32(0x0065, SCU_BASE + SCU_CTRL);
+	write32(CPU_PORT_FILT_END, pl310_base() + PL310_ADDR_FILT_END);
+	write32(CPU_PORT_FILT_START | PL310_CTRL_ENABLE_BIT,
+				   pl310_base() + PL310_ADDR_FILT_START);
 
-	/*
-	 * - All external interrupts are NonSecure.
-	 */
+	/* default: all SPIs are nonsecure */
 	for (i = 0; i < (31 * 4); i += 4)
 		write32(0xFFFFFFFF, GIC_DIST_BASE + GIC_DIST_ISR1 + i);
-
-	/* PL310 Memory Controller port filtering */
-	write32(CPU_PORT_FILT_END, pl310_base() + PL310_ADDR_FILT_END);
-	write32(CPU_PORT_FILT_START | 1, pl310_base() + PL310_ADDR_FILT_START);
 }

core/arch/arm/plat-stm/system_config.mk
@@ -1,33 +1,0 @@
-# this script defines platform specific settings for TEE armv7 build
-
-ifeq ($(PLATFORM_FLAVOR),b2260)
-
-CFG_DDR_TEETZ_RESERVED_START ?= 0x7E000000
-CFG_DDR_TEETZ_RESERVED_SIZE ?= 0x01E00000
-
-else ifeq ($(PLATFORM_FLAVOR),cannes)
-
-# Static configuration of DDR reserved to TEE/TZ
-# Provide here only start address and size. TEEcore will adapt.
-# Allow these settings to be overridden.
-CFG_DDR_TEETZ_RESERVED_START ?= 0x93a00000
-CFG_DDR_TEETZ_RESERVED_SIZE ?= 0x01000000
-
-else ifeq ($(PLATFORM_FLAVOR),orly2)
-
-# Static configuration of DDR reserved to TEE/TZ
-# Provide here only start address and size. TEEcore will adapt.
-# Allow these settings to be overridden.
-CFG_DDR_TEETZ_RESERVED_START ?= 0x8F000000
-CFG_DDR_TEETZ_RESERVED_SIZE ?= 0x00800000
-
-else
-$(error PLATFORM_FLAVOR=$(PLATFORM_FLAVOR) is not supported)
-endif
-
-ifndef CFG_DDR_TEETZ_RESERVED_START
-$(error "CFG_DDR_TEETZ_RESERVED_START should be set from system_config.in")
-endif
-ifndef CFG_DDR_TEETZ_RESERVED_SIZE
-$(error "CFG_DDR_TEETZ_RESERVED_SIZE should be set from system_config.in")
-endif

core/arch/arm/plat-stm/tz_a9init.S
@@ -77,40 +77,20 @@ END_FUNC arm_cl2_enable
 FUNC plat_cpu_reset_early , :
 UNWIND(	.fnstart)
 
-	/*
-	 * Mandated HW config loaded
-	 *
-	 * SCTLR = 0x00004000
-	 * - Round-Robin replac. for icache, btac, i/duTLB (bit14: RoundRobin)
-	 *
-	 * ACTRL = 0x00000041
-	 * - core always in full SMP (FW bit0=1, SMP bit6=1)
-	 * - L2 write full line of zero disabled (bit3=0)
-	 *   (keep WFLZ low. Will be set once outer L2 is ready)
-	 *
-	 * NSACR = 0x00020C00
-	 * - NSec cannot change ACTRL.SMP (NS_SMP bit18=0)
-	 * - Nsec can lockdown TLB (TL bit17=1)
-	 * - NSec cannot access PLE (PLE bit16=0)
-	 * - NSec can use SIMD/VFP (CP10/CP11) (bit15:14=2b00, bit11:10=2b11)
-	 *
-	 * PCR = 0x00000001
-	 * - no change latency, enable clk gating
-	 */
-	movw r0, #0x4000
-	movt r0, #0x0000
+	movw r0, #(CPU_SCTLR_INIT & 0xFFFF)
+	movt r0, #((CPU_SCTLR_INIT >> 16) & 0xFFFF)
 	write_sctlr r0
 
-	movw r0, #0x0041
-	movt r0, #0x0000
+	movw r0, #(CPU_ACTLR_INIT & 0xFFFF)
+	movt r0, #((CPU_ACTLR_INIT >> 16) & 0xFFFF)
 	write_actlr r0
 
-	movw r0, #0x0C00
-	movt r0, #0x0002
+	movw r0, #(CPU_NSACR_INIT & 0xFFFF)
+	movt r0, #((CPU_NSACR_INIT >> 16) & 0xFFFF)
 	write_nsacr r0
 
-	movw r0, #0x0000
-	movt r0, #0x0001
+	movw r0, #(CPU_PCR_INIT & 0xFFFF)
+	movt r0, #((CPU_PCR_INIT >> 16) & 0xFFFF)
 	write_pcr r0
 
 	/*

commit fe839766bc55ad0d36abac11d0e98bf7ea58aa6e

plat-stm: fix flavor 'cannes'
Fixes: 8cd89706f19c ("plat-stm: beautify platform config")

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-stm/platform_config.h
@@ -68,11 +68,11 @@
 #define CPU_IOMEM_BASE		0x08760000
 #define CPU_PORT_FILT_START	0x40000000
 #define CPU_PORT_FILT_END	0xC0000000
-#define STXHxxx_LPM_PERIPH_BASE	0x09300000
+#define STXHxxx_LPM_PERIPH_BASE	0x09400000
 #define RNG_BASE		0x08A89000
 
 #define ASC_NUM			20
-#define UART_CONSOLE_BASE	ST_ASC21_REGS_BASE
+#define UART_CONSOLE_BASE	ST_ASC20_REGS_BASE
 
 #elif defined(PLATFORM_FLAVOR_orly2)
 