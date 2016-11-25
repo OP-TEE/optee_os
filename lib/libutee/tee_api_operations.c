a9f92a95e17142bece5c60a64fd9db08206fbea2
Jerome Forissier <jerome.forissier@linaro.org>
2016-11-25 10:13:35 +0100

    libutee: TEE_AEEncryptFinal(): do nothing when (!srcData && !srcLen)
    Fixes: Coverity CID:1378616
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libutee/tee_api_operations.c
+++ lib/libutee/tee_api_operations.c
@@ -1413,6 +1413,11 @@ TEE_Result TEE_AEEncryptFinal(TEE_OperationHandle operation,
 		goto out;
 	}
 
+	if (!srcData && !srcLen) {
+		res = TEE_SUCCESS;
+		goto out;
+	}
+
 	/*
 	 * Check that required destLen is big enough before starting to feed
 	 * data to the algorithm. Errors during feeding of data are fatal as we
