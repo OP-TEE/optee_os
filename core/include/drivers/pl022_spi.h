219ef78442a21ab06f3d35bb4633ab2843e13422
Victor Chong <victor.chong@linaro.org>
2016-12-28 22:06:48 +0900

    drivers: pl022: Allow platforms to register CS control function
    If a CS callback function is registered, the system provided one
    will not be used.
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    Tested-by: Victor Chong <victor.chong@linaro.org> (HiKey)
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/include/drivers/pl022_spi.h
+++ core/include/drivers/pl022_spi.h
@@ -34,17 +34,32 @@
 
 #define PL022_REG_SIZE	0x1000
 
+enum pl022_cs_control {
+	PL022_CS_CTRL_AUTO_GPIO,
+	PL022_CS_CTRL_CB,
+	PL022_CS_CTRL_MANUAL
+};
+
+struct pl022_cs_gpio_data {
+	struct gpio_chip	*chip;
+	unsigned int		pin_num;
+};
+
+union pl022_cs_data {
+	struct pl022_cs_gpio_data	gpio_data;
+	void				(*cs_cb)(enum gpio_level value);
+};
+
 struct pl022_data {
+	union pl022_cs_data	cs_data;
 	struct spi_chip		chip;
-	struct gpio_chip	*gpio;
-	vaddr_t		base;
-	vaddr_t		cs_gpio_base;
-	unsigned int	clk_hz;
-	unsigned int	speed_hz;
-	unsigned int	cs_gpio_pin;
-	enum spi_mode	mode;
-	unsigned int	data_size_bits;
-	bool		loopback;
+	vaddr_t			base;
+	enum spi_mode		mode;
+	enum pl022_cs_control	cs_control;
+	unsigned int		clk_hz;
+	unsigned int		speed_hz;
+	unsigned int		data_size_bits;
+	bool			loopback;
 };
 
 void pl022_init(struct pl022_data *pd);
