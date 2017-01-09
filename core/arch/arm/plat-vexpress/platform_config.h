OP-TEE/optee_os

commit 0f8333b888f10f49dbdf646ff12565632d6250f0

plat-vexpress/qemu: correct DRAM layout.
There are really more memory in qemu config, than configured in
platform_config.h
Invalid DRAM layout causes core_pbuf_is(CORE_MEM_EXTRAM) to fail
when it should not.

Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-vexpress/platform_config.h
@@ -28,6 +28,8 @@
 #ifndef PLATFORM_CONFIG_H
 #define PLATFORM_CONFIG_H
 
+#include <stdint.h>
+
 /* Make stacks aligned to data cache line length */
 #define STACK_ALIGNMENT		64
 
@@ -179,28 +181,28 @@
  * QEMU virt specifics.
  */
 
-#define DRAM0_BASE		0x40000000
-#define DRAM0_SIZE		(0x40000000 - DRAM0_TEERES_SIZE)
+#define DRAM0_BASE		UINTPTR_C(0x40000000)
+#define DRAM0_SIZE		(UINTPTR_C(0x42100000) - CFG_SHMEM_SIZE)
 
 #define DRAM0_TEERES_BASE	(DRAM0_BASE + DRAM0_SIZE)
-#define DRAM0_TEERES_SIZE	(33 * 1024 * 1024)
+#define DRAM0_TEERES_SIZE	CFG_SHMEM_SIZE
 
 #ifdef CFG_WITH_PAGER
 
 /* Emulated SRAM */
-#define TZSRAM_BASE		DRAM0_TEERES_BASE
+#define TZSRAM_BASE		0x0e000000
 #define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
 
-#define TZDRAM_BASE		(DRAM0_TEERES_BASE + CFG_TEE_RAM_VA_SIZE)
-#define TZDRAM_SIZE		(DRAM0_TEERES_SIZE - CFG_TEE_RAM_VA_SIZE \
-					- CFG_SHMEM_SIZE)
+#define TZDRAM_BASE		(TZSRAM_BASE + TZSRAM_SIZE)
+#define TZDRAM_SIZE		(0x01000000 - TZSRAM_SIZE)
 
 #else /* CFG_WITH_PAGER */
 
-#define TZDRAM_BASE		DRAM0_TEERES_BASE
-#define TZDRAM_SIZE		(DRAM0_TEERES_SIZE - CFG_SHMEM_SIZE)
+#define TZDRAM_BASE		0x0e000000
+#define TZDRAM_SIZE		0x01000000
 
 #endif /* CFG_WITH_PAGER */
+
 
 #define CFG_TEE_CORE_NB_CORE	2
 
@@ -218,11 +220,11 @@
 #error "Pager not supported for platform vexpress-qemu_armv8a"
 #endif
 
-#define DRAM0_BASE		0x40000000
-#define DRAM0_SIZE		(0x40000000 - DRAM0_TEERES_SIZE)
+#define DRAM0_BASE		UINTPTR_C(0x40000000)
+#define DRAM0_SIZE		(UINTPTR_C(0x42100000) - CFG_SHMEM_SIZE)
 
 #define DRAM0_TEERES_BASE	(DRAM0_BASE + DRAM0_SIZE)
-#define DRAM0_TEERES_SIZE	(33 * 1024 * 1024)
+#define DRAM0_TEERES_SIZE	CFG_SHMEM_SIZE
 
 #define TZDRAM_BASE		0x0e100000
 #define TZDRAM_SIZE		0x00f00000

core/arch/arm/plat-vexpress/conf.mk
@@ -61,7 +61,7 @@ ifeq ($(CFG_CORE_SANITIZE_KADDRESS),y)
 # This is unfortunately currently not possible to do in make so we have to
 # calculate it offline, there's some asserts in
 # core/arch/arm/kernel/generic_boot.c to check that we got it right
-CFG_ASAN_SHADOW_OFFSET=0x6e4038e0
+CFG_ASAN_SHADOW_OFFSET=0xc4e38e0
 endif
 $(call force,CFG_DT,y)
 # SE API is only supported by QEMU Virt platform

commit 608bd28c3b5baa6fbd29ce89d4bee42945e34ccb

core: plat-vexpress: qemu_armv8a: correct DRAM config
The previous fix for DRAM configuration wasn't correct for QEMU virt
ARMv8A. This patch fixes that.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU ARMv8-A)
Fixes: 0f8333b888f1 ("plat-vexpress/qemu: correct DRAM layout")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-vexpress/platform_config.h
@@ -221,7 +221,7 @@
 #endif
 
 #define DRAM0_BASE		UINTPTR_C(0x40000000)
-#define DRAM0_SIZE		(UINTPTR_C(0x42100000) - CFG_SHMEM_SIZE)
+#define DRAM0_SIZE		(UINTPTR_C(0x40000000) - CFG_SHMEM_SIZE)
 
 #define DRAM0_TEERES_BASE	(DRAM0_BASE + DRAM0_SIZE)
 #define DRAM0_TEERES_SIZE	CFG_SHMEM_SIZE