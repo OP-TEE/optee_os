OP-TEE/optee_os

commit 0dcfa5686d022738f9e6372b19e6b30851523dd0

core: introduce MOBJ abstraction
Introduces MOBJ for memory objects, no changes in features except
breaking unsafe-nw-param/mem-multipurpose.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by Volodymyr Babchuk <vlad.babchuk@gmail.com>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/mobj.c
@@ -1,0 +1,281 @@
+/*
+ * Copyright (c) 2016-2017, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <kernel/mutex.h>
+#include <kernel/panic.h>
+#include <kernel/tee_misc.h>
+#include <mm/core_mmu.h>
+#include <mm/mobj.h>
+#include <optee_msg.h>
+#include <sm/optee_smc.h>
+#include <stdlib.h>
+#include <tee_api_types.h>
+#include <types_ext.h>
+#include <util.h>
+
+struct mobj *mobj_sec_ddr;
+
+/*
+ * mobj_phys implementation
+ */
+
+struct mobj_phys {
+	struct mobj mobj;
+	enum buf_is_attr battr;
+	uint32_t cattr; /* Defined by TEE_MATTR_CACHE_* in tee_mmu_types.h */
+	vaddr_t va;
+	paddr_t pa;
+};
+
+static struct mobj_phys *to_mobj_phys(struct mobj *mobj);
+
+static void *mobj_phys_get_va(struct mobj *mobj, size_t offset)
+{
+	struct mobj_phys *moph = to_mobj_phys(mobj);
+
+	if (!moph->va)
+		return NULL;
+
+	return (void *)(moph->va + offset);
+}
+
+static TEE_Result mobj_phys_get_pa(struct mobj *mobj, size_t offs,
+				   size_t granule, paddr_t *pa)
+{
+	struct mobj_phys *moph = to_mobj_phys(mobj);
+	paddr_t p;
+
+	if (!pa)
+		return TEE_ERROR_GENERIC;
+
+	p = moph->pa + offs;
+
+	if (granule) {
+		if (granule != SMALL_PAGE_SIZE &&
+		    granule != CORE_MMU_PGDIR_SIZE)
+			return TEE_ERROR_GENERIC;
+		p &= ~(granule - 1);
+	}
+
+	*pa = p;
+	return TEE_SUCCESS;
+}
+
+static TEE_Result mobj_phys_get_cattr(struct mobj *mobj, uint32_t *cattr)
+{
+	struct mobj_phys *moph = to_mobj_phys(mobj);
+
+	if (!cattr)
+		return TEE_ERROR_GENERIC;
+
+	*cattr = moph->cattr;
+	return TEE_SUCCESS;
+}
+
+static bool mobj_phys_matches(struct mobj *mobj, enum buf_is_attr attr)
+{
+	struct mobj_phys *moph = to_mobj_phys(mobj);
+	enum buf_is_attr a;
+
+	a = moph->battr;
+
+	switch (attr) {
+	case CORE_MEM_SEC:
+		return a == CORE_MEM_SEC || a == CORE_MEM_TEE_RAM ||
+		       a == CORE_MEM_TA_RAM;
+	case CORE_MEM_NON_SEC:
+		return a == CORE_MEM_NSEC_SHM;
+	case CORE_MEM_TEE_RAM:
+	case CORE_MEM_TA_RAM:
+	case CORE_MEM_NSEC_SHM:
+		return attr == a;
+	default:
+		return false;
+	}
+}
+
+static void mobj_phys_free(struct mobj *mobj)
+{
+	struct mobj_phys *moph = to_mobj_phys(mobj);
+
+	free(moph);
+}
+
+static const struct mobj_ops mobj_phys_ops __rodata_unpaged = {
+	.get_va = mobj_phys_get_va,
+	.get_pa = mobj_phys_get_pa,
+	.get_cattr = mobj_phys_get_cattr,
+	.matches = mobj_phys_matches,
+	.free = mobj_phys_free,
+};
+
+static struct mobj_phys *to_mobj_phys(struct mobj *mobj)
+{
+	assert(mobj->ops == &mobj_phys_ops);
+	return container_of(mobj, struct mobj_phys, mobj);
+}
+
+struct mobj *mobj_phys_alloc(paddr_t pa, size_t size, uint32_t cattr,
+			     enum buf_is_attr battr)
+{
+	struct mobj_phys *moph;
+	void *va;
+
+	if ((pa & CORE_MMU_USER_PARAM_MASK) ||
+	    (size & CORE_MMU_USER_PARAM_MASK)) {
+		DMSG("Expect %#x alignment", CORE_MMU_USER_PARAM_SIZE);
+		return NULL;
+	}
+
+	va = phys_to_virt(pa, battr);
+	if (!va)
+		return NULL;
+
+	moph = calloc(1, sizeof(*moph));
+	if (!moph)
+		return NULL;
+
+	moph->battr = battr;
+	moph->cattr = cattr;
+	moph->mobj.size = size;
+	moph->mobj.ops = &mobj_phys_ops;
+	moph->pa = pa;
+	moph->va = (vaddr_t)va;
+
+	return &moph->mobj;
+}
+
+/*
+ * mobj_virt implementation
+ */
+
+static void mobj_virt_assert_type(struct mobj *mobj);
+
+static void *mobj_virt_get_va(struct mobj *mobj, size_t offset)
+{
+	mobj_virt_assert_type(mobj);
+
+	return (void *)(vaddr_t)offset;
+}
+
+static const struct mobj_ops mobj_virt_ops __rodata_unpaged = {
+	.get_va = mobj_virt_get_va,
+};
+
+static void mobj_virt_assert_type(struct mobj *mobj __maybe_unused)
+{
+	assert(mobj->ops == &mobj_virt_ops);
+}
+
+struct mobj mobj_virt = { .ops = &mobj_phys_ops, .size = SIZE_MAX };
+
+/*
+ * mobj_mm implementation
+ */
+
+struct mobj_mm {
+	tee_mm_entry_t *mm;
+	struct mobj *parent_mobj;
+	struct mobj mobj;
+};
+
+static struct mobj_mm *to_mobj_mm(struct mobj *mobj);
+
+static size_t mobj_mm_offs(struct mobj *mobj, size_t offs)
+{
+	tee_mm_entry_t *mm = to_mobj_mm(mobj)->mm;
+
+	return (mm->offset << mm->pool->shift) + offs;
+}
+
+static void *mobj_mm_get_va(struct mobj *mobj, size_t offs)
+{
+	return mobj_get_va(to_mobj_mm(mobj)->parent_mobj,
+			   mobj_mm_offs(mobj, offs));
+}
+
+
+static TEE_Result mobj_mm_get_pa(struct mobj *mobj, size_t offs,
+				    size_t granule, paddr_t *pa)
+{
+	return mobj_get_pa(to_mobj_mm(mobj)->parent_mobj,
+			   mobj_mm_offs(mobj, offs), granule, pa);
+}
+
+static TEE_Result mobj_mm_get_cattr(struct mobj *mobj, uint32_t *cattr)
+{
+	return mobj_get_cattr(to_mobj_mm(mobj)->parent_mobj, cattr);
+}
+
+static bool mobj_mm_matches(struct mobj *mobj, enum buf_is_attr attr)
+{
+	return mobj_matches(to_mobj_mm(mobj)->parent_mobj, attr);
+}
+
+static void mobj_mm_free(struct mobj *mobj)
+{
+	struct mobj_mm *m = to_mobj_mm(mobj);
+
+	tee_mm_free(m->mm);
+	free(m);
+}
+
+static const struct mobj_ops mobj_mm_ops __rodata_unpaged = {
+	.get_va = mobj_mm_get_va,
+	.get_pa = mobj_mm_get_pa,
+	.get_cattr = mobj_mm_get_cattr,
+	.matches = mobj_mm_matches,
+	.free = mobj_mm_free,
+};
+
+static struct mobj_mm *to_mobj_mm(struct mobj *mobj)
+{
+	assert(mobj->ops == &mobj_mm_ops);
+	return container_of(mobj, struct mobj_mm, mobj);
+}
+
+struct mobj *mobj_mm_alloc(struct mobj *mobj_parent, size_t size,
+			      tee_mm_pool_t *pool)
+{
+	struct mobj_mm *m = calloc(1, sizeof(*m));
+
+	if (!m)
+		return NULL;
+
+	m->mm = tee_mm_alloc(pool, size);
+	if (!m->mm) {
+		free(m);
+		return NULL;
+	}
+
+	m->parent_mobj = mobj_parent;
+	m->mobj.size = size;
+	m->mobj.ops = &mobj_mm_ops;
+
+	return &m->mobj;
+}

core/arch/arm/include/mm/core_memprot.h
@@ -50,7 +50,6 @@ enum buf_is_attr {
 	CORE_MEM_TEE_RAM,
 	CORE_MEM_TA_RAM,
 	CORE_MEM_NSEC_SHM,
-	CORE_MEM_MULTPURPOSE,
 	CORE_MEM_EXTRAM,
 	CORE_MEM_INTRAM,
 	CORE_MEM_CACHED,

core/arch/arm/include/mm/core_mmu.h
@@ -28,6 +28,7 @@
 #ifndef CORE_MMU_H
 #define CORE_MMU_H
 
+#include <compiler.h>
 #include <kernel/user_ta.h>
 #include <mm/tee_mmu_types.h>
 #include <types_ext.h>

core/arch/arm/include/mm/mobj.h
@@ -1,0 +1,111 @@
+/*
+ * Copyright (c) 2016-2017, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __MM_MOBJ_H
+#define __MM_MOBJ_H
+
+#include <compiler.h>
+#include <mm/core_memprot.h>
+#include <optee_msg.h>
+#include <sys/queue.h>
+#include <tee_api_types.h>
+#include <types_ext.h>
+
+
+struct mobj {
+	const struct mobj_ops *ops;
+	size_t size;
+};
+
+struct mobj_ops {
+	void *(*get_va)(struct mobj *mobj, size_t offs);
+	TEE_Result (*get_pa)(struct mobj *mobj, size_t offs, size_t granule,
+			     paddr_t *pa);
+	TEE_Result (*get_cattr)(struct mobj *mobj, uint32_t *cattr);
+	bool (*matches)(struct mobj *mobj, enum buf_is_attr attr);
+	void (*free)(struct mobj *mobj);
+};
+
+extern struct mobj mobj_virt;
+extern struct mobj *mobj_sec_ddr;
+
+static inline void *mobj_get_va(struct mobj *mobj, size_t offset)
+{
+	if (mobj && mobj->ops && mobj->ops->get_va)
+		return mobj->ops->get_va(mobj, offset);
+	return NULL;
+}
+
+static inline TEE_Result mobj_get_pa(struct mobj *mobj, size_t offs,
+				     size_t granule, paddr_t *pa)
+{
+	if (mobj && mobj->ops && mobj->ops->get_pa)
+		return mobj->ops->get_pa(mobj, offs, granule, pa);
+	return TEE_ERROR_GENERIC;
+}
+
+static inline TEE_Result mobj_get_cattr(struct mobj *mobj, uint32_t *cattr)
+{
+	if (mobj && mobj->ops && mobj->ops->get_cattr)
+		return mobj->ops->get_cattr(mobj, cattr);
+	return TEE_ERROR_GENERIC;
+}
+
+static inline bool mobj_matches(struct mobj *mobj, enum buf_is_attr attr)
+{
+	if (mobj && mobj->ops && mobj->ops->matches)
+		return mobj->ops->matches(mobj, attr);
+	return false;
+}
+
+static inline void mobj_free(struct mobj *mobj)
+{
+	if (mobj && mobj->ops && mobj->ops->free)
+		mobj->ops->free(mobj);
+}
+
+static inline bool mobj_is_nonsec(struct mobj *mobj)
+{
+	return mobj_matches(mobj, CORE_MEM_NON_SEC);
+}
+
+static inline bool mobj_is_secure(struct mobj *mobj)
+{
+	return mobj_matches(mobj, CORE_MEM_SEC);
+}
+
+static inline bool mobj_is_unsafe(struct mobj *mobj __unused)
+{
+	return false;
+}
+
+struct mobj *mobj_mm_alloc(struct mobj *mobj_parent, size_t size,
+			   tee_mm_pool_t *pool);
+
+struct mobj *mobj_phys_alloc(paddr_t pa, size_t size, uint32_t cattr,
+			     enum buf_is_attr battr);
+#endif /*__MM_MOBJ_H*/

core/arch/arm/kernel/link.mk
@@ -30,6 +30,10 @@ entries-unpaged += sm_init
 entries-unpaged += core_init_mmu_regs
 entries-unpaged += sem_cpu_sync
 entries-unpaged += generic_boot_get_handlers
+# Can't use KEEP_PAGER in mobj.c since it for some reason causes an assert
+# in the AArch64 linker
+entries-unpaged += mobj_phys_get_pa
+entries-unpaged += mobj_mm_get_pa
 
 ldargs-all_objs := -i $(objs) $(link-ldadd) $(libgcccore)
 cleanfiles += $(link-out-dir)/all_objs.o

core/arch/arm/kernel/static_ta.c
@@ -28,6 +28,7 @@
 #include <kernel/static_ta.h>
 #include <kernel/tee_ta_manager.h>
 #include <mm/core_memprot.h>
+#include <mm/mobj.h>
 #include <sm/tee_mon.h>
 #include <stdlib.h>
 #include <string.h>
@@ -35,38 +36,59 @@
 #include <types_ext.h>
 
 /* Maps static TA params */
-static TEE_Result tee_ta_param_pa2va(struct tee_ta_param *param)
+static TEE_Result copy_in_param(struct tee_ta_param *param,
+				     TEE_Param tee_param[TEE_NUM_PARAMS])
 {
 	size_t n;
 	void *va;
-	paddr_t pa;
-
-	/*
-	 * If a static TA is called from another TA the mapping
-	 * of that TA is borrowed and the addresses are already
-	 * virtual.
-	 */
-	if (tee_ta_get_calling_session())
-		return TEE_SUCCESS;
 
 	for (n = 0; n < TEE_NUM_PARAMS; n++) {
 		switch (TEE_PARAM_TYPE_GET(param->types, n)) {
+		case TEE_PARAM_TYPE_VALUE_INPUT:
+		case TEE_PARAM_TYPE_VALUE_OUTPUT:
+		case TEE_PARAM_TYPE_VALUE_INOUT:
+			tee_param[n].value.a = param->u[n].val.a;
+			tee_param[n].value.b = param->u[n].val.b;
+			break;
 		case TEE_PARAM_TYPE_MEMREF_INPUT:
 		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 		case TEE_PARAM_TYPE_MEMREF_INOUT:
-			pa = (paddr_t)param->params[n].memref.buffer;
-			va = phys_to_virt(pa, MEM_AREA_NSEC_SHM);
+			va = mobj_get_va(param->u[n].mem.mobj,
+					 param->u[n].mem.offs);
 			if (!va)
 				return TEE_ERROR_BAD_PARAMETERS;
-			param->params[n].memref.buffer = va;
+			tee_param[n].memref.buffer = va;
+			tee_param[n].memref.size = param->u[n].mem.mobj->size;
 			break;
-
 		default:
-			continue;
+			memset(tee_param + n, 0, sizeof(TEE_Param));
+			break;
 		}
 	}
 
 	return TEE_SUCCESS;
+}
+
+static void update_out_param(TEE_Param tee_param[TEE_NUM_PARAMS],
+			     struct tee_ta_param *param)
+{
+	size_t n;
+
+	for (n = 0; n < TEE_NUM_PARAMS; n++) {
+		switch (TEE_PARAM_TYPE_GET(param->types, n)) {
+		case TEE_PARAM_TYPE_VALUE_OUTPUT:
+		case TEE_PARAM_TYPE_VALUE_INOUT:
+			param->u[n].val.a = tee_param[n].value.a;
+			param->u[n].val.b = tee_param[n].value.b;
+			break;
+		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
+		case TEE_PARAM_TYPE_MEMREF_INOUT:
+			param->u[n].mem.size = tee_param[n].memref.size;
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 static TEE_Result static_ta_enter_open_session(struct tee_ta_session *s,
@@ -74,9 +96,10 @@ static TEE_Result static_ta_enter_open_session(struct tee_ta_session *s,
 {
 	TEE_Result res;
 	struct static_ta_ctx *stc = to_static_ta_ctx(s->ctx);
+	TEE_Param tee_param[TEE_NUM_PARAMS];
 
 	tee_ta_push_current_session(s);
-	res = tee_ta_param_pa2va(param);
+	res = copy_in_param(param, tee_param);
 	if (res != TEE_SUCCESS) {
 		*eo = TEE_ORIGIN_TEE;
 		goto out;
@@ -88,8 +111,9 @@ static TEE_Result static_ta_enter_open_session(struct tee_ta_session *s,
 		if (res != TEE_SUCCESS)
 			goto out;
 	}
-	res = stc->static_ta->open_session_entry_point(param->types,
-					param->params, &s->user_ctx);
+	res = stc->static_ta->open_session_entry_point(param->types, tee_param,
+						       &s->user_ctx);
+	update_out_param(tee_param, param);
 
 out:
 	tee_ta_pop_current_session();
@@ -102,9 +126,10 @@ static TEE_Result static_ta_enter_invoke_cmd(struct tee_ta_session *s,
 {
 	TEE_Result res;
 	struct static_ta_ctx *stc = to_static_ta_ctx(s->ctx);
+	TEE_Param tee_param[TEE_NUM_PARAMS];
 
 	tee_ta_push_current_session(s);
-	res = tee_ta_param_pa2va(param);
+	res = copy_in_param(param, tee_param);
 	if (res != TEE_SUCCESS) {
 		*eo = TEE_ORIGIN_TEE;
 		goto out;
@@ -112,7 +137,9 @@ static TEE_Result static_ta_enter_invoke_cmd(struct tee_ta_session *s,
 
 	*eo = TEE_ORIGIN_TRUSTED_APP;
 	res = stc->static_ta->invoke_command_entry_point(s->user_ctx, cmd,
-					param->types, param->params);
+							 param->types,
+							 tee_param);
+	update_out_param(tee_param, param);
 out:
 	tee_ta_pop_current_session();
 	return res;

core/arch/arm/kernel/tee_ta_manager.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2016, Linaro Limited
  * Copyright (c) 2014, STMicroelectronics International N.V.
  * All rights reserved.
  *
@@ -24,10 +25,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <tee_api_types.h>
+
 #include <kernel/tee_ta_manager.h>
-#include <mm/tee_mmu_types.h>
 #include <mm/core_memprot.h>
+#include <mm/mobj.h>
+#include <mm/tee_mmu_types.h>
+#include <tee_api_types.h>
 
 /**
  * tee_ta_verify_param() - check that the 4 "params" match security
@@ -35,8 +38,6 @@
 TEE_Result tee_ta_verify_param(struct tee_ta_session *sess,
 			       struct tee_ta_param *param)
 {
-	paddr_t p;
-	size_t l;
 	int n;
 
 	for (n = 0; n < TEE_NUM_PARAMS; n++) {
@@ -44,27 +45,10 @@ TEE_Result tee_ta_verify_param(struct tee_ta_session *sess,
 		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 		case TEE_PARAM_TYPE_MEMREF_INOUT:
 		case TEE_PARAM_TYPE_MEMREF_INPUT:
-
-			if (param->param_attr[n] & TEE_MATTR_VIRTUAL) {
-				p = virt_to_phys(
-					param->params[n].memref.buffer);
-				if (!p)
-					return TEE_ERROR_SECURITY;
-			} else {
-				p = (paddr_t)param->params[n].memref.buffer;
-			}
-			l = param->params[n].memref.size;
-
-			if (core_pbuf_is(CORE_MEM_NSEC_SHM, p, l))
-				break;
-			if ((sess->ctx->flags & TA_FLAG_UNSAFE_NW_PARAMS) &&
-				core_pbuf_is(CORE_MEM_MULTPURPOSE, p, l))
-				break;
-			if ((sess->clnt_id.login == TEE_LOGIN_TRUSTED_APP) &&
-				core_pbuf_is(CORE_MEM_TA_RAM, p, l))
-				break;
-
-			return TEE_ERROR_SECURITY;
+			if (mobj_is_unsafe(param->u[n].mem.mobj) &&
+			    !(sess->ctx->flags & TA_FLAG_UNSAFE_NW_PARAMS))
+				return TEE_ERROR_SECURITY;
+			break;
 		default:
 			break;
 		}

core/arch/arm/kernel/user_ta.c
@@ -35,6 +35,7 @@
 #include <kernel/user_ta.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
+#include <mm/mobj.h>
 #include <mm/pgt_cache.h>
 #include <mm/tee_mm.h>
 #include <mm/tee_mmu.h>
@@ -151,12 +152,12 @@ static TEE_Result config_initial_paging(struct user_ta_ctx *utc)
 {
 	size_t n;
 
-	for (n = 0; n < utc->mmu->size; n++) {
-		if (!utc->mmu->table[n].size)
+	for (n = 0; n < ARRAY_SIZE(utc->mmu->regions); n++) {
+		if (!utc->mmu->regions[n].size)
 			continue;
-		utc->mmu->table[n].attr |= TEE_MATTR_PAGED;
-		if (!tee_pager_add_uta_area(utc, utc->mmu->table[n].va,
-					    utc->mmu->table[n].size))
+		utc->mmu->regions[n].attr |= TEE_MATTR_PAGED;
+		if (!tee_pager_add_uta_area(utc, utc->mmu->regions[n].va,
+					    utc->mmu->regions[n].size))
 			return TEE_ERROR_GENERIC;
 	}
 	return TEE_SUCCESS;
@@ -167,28 +168,34 @@ static TEE_Result config_final_paging(struct user_ta_ctx *utc)
 	size_t n;
 	uint32_t flags;
 
-	for (n = 0; n < utc->mmu->size; n++) {
-		if (!utc->mmu->table[n].size)
+	for (n = 0; n < ARRAY_SIZE(utc->mmu->regions); n++) {
+		if (!utc->mmu->regions[n].size)
 			continue;
-		utc->mmu->table[n].attr |= TEE_MATTR_PAGED;
-		flags = utc->mmu->table[n].attr &
+		utc->mmu->regions[n].attr |= TEE_MATTR_PAGED;
+		flags = utc->mmu->regions[n].attr &
 			(TEE_MATTR_PRW | TEE_MATTR_URWX);
-		if (!tee_pager_set_uta_area_attr(utc, utc->mmu->table[n].va,
-						 utc->mmu->table[n].size,
+		if (!tee_pager_set_uta_area_attr(utc, utc->mmu->regions[n].va,
+						 utc->mmu->regions[n].size,
 						 flags))
 			return TEE_ERROR_GENERIC;
 	}
 	return TEE_SUCCESS;
 }
 
-static paddr_t get_stack_pa(struct user_ta_ctx *utc __unused)
+static TEE_Result get_stack_mobj_offs(struct user_ta_ctx *utc __unused,
+				      struct mobj **mobj, size_t *offs)
 {
-	return 0;
+	*mobj = NULL;
+	*offs = 0;
+	return TEE_SUCCESS;
 }
 
-static paddr_t get_code_pa(struct user_ta_ctx *utc __unused)
+static TEE_Result get_code_mobj_offs(struct user_ta_ctx *utc __unused,
+				      struct mobj **mobj, size_t *offs)
 {
-	return 0;
+	*mobj = NULL;
+	*offs = 0;
+	return TEE_SUCCESS;
 }
 #else /*!CFG_PAGED_USER_TA*/
 static TEE_Result config_initial_paging(struct user_ta_ctx *utc __unused)
@@ -201,14 +208,36 @@ static TEE_Result config_final_paging(struct user_ta_ctx *utc __unused)
 	return TEE_SUCCESS;
 }
 
-static paddr_t get_stack_pa(struct user_ta_ctx *utc)
+static TEE_Result get_stack_mobj_offs(struct user_ta_ctx *utc,
+				      struct mobj **mobj, size_t *offs)
 {
-	return tee_mm_get_smem(utc->mm_stack);
+	TEE_Result res;
+	paddr_t pa = tee_mm_get_smem(utc->mm_stack);
+	paddr_t pa2;
+
+	res = mobj_get_pa(mobj_sec_ddr, 0, 0, &pa2);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	*mobj = mobj_sec_ddr;
+	*offs = pa - pa2;
+	return TEE_SUCCESS;
 }
 
-static paddr_t get_code_pa(struct user_ta_ctx *utc)
+static TEE_Result get_code_mobj_offs(struct user_ta_ctx *utc,
+				      struct mobj **mobj, size_t *offs)
 {
-	return tee_mm_get_smem(utc->mm);
+	TEE_Result res;
+	paddr_t pa = tee_mm_get_smem(utc->mm);
+	paddr_t pa2;
+
+	res = mobj_get_pa(mobj_sec_ddr, 0, 0, &pa2);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	*mobj = mobj_sec_ddr;
+	*offs = pa - pa2;
+	return TEE_SUCCESS;
 }
 #endif /*!CFG_PAGED_USER_TA*/
 
@@ -216,22 +245,27 @@ static TEE_Result load_elf_segments(struct user_ta_ctx *utc,
 			struct elf_load_state *elf_state, bool init_attrs)
 {
 	TEE_Result res;
-	paddr_t pa;
 	uint32_t mattr;
 	size_t idx = 0;
+	struct mobj *mobj = NULL;
+	size_t moffs = 0;
 
 	tee_mmu_map_clear(utc);
+
 	/*
 	 * Add stack segment
 	 */
-	pa = get_stack_pa(utc);
-	mattr = elf_flags_to_mattr(PF_W | PF_R, init_attrs);
-	tee_mmu_map_stack(utc, pa, utc->stack_size, mattr);
+	res = get_stack_mobj_offs(utc, &mobj, &moffs);
+	if (res != TEE_SUCCESS)
+		return res;
+	tee_mmu_map_stack(utc, mobj, moffs, utc->stack_size);
 
 	/*
 	 * Add code segment
 	 */
-	pa = get_code_pa(utc);
+	res = get_code_mobj_offs(utc, &mobj, &moffs);
+	if (res != TEE_SUCCESS)
+		return res;
 	while (true) {
 		vaddr_t offs;
 		size_t size;
@@ -245,7 +279,8 @@ static TEE_Result load_elf_segments(struct user_ta_ctx *utc,
 			return res;
 
 		mattr = elf_flags_to_mattr(flags, init_attrs);
-		res = tee_mmu_map_add_segment(utc, pa, offs, size, mattr);
+		res = tee_mmu_map_add_segment(utc, mobj, moffs + offs, size,
+					      mattr);
 		if (res != TEE_SUCCESS)
 			return res;
 	}
@@ -522,7 +557,7 @@ error_return:
 }
 
 static void init_utee_param(struct utee_params *up,
-			const struct tee_ta_param *p)
+			const struct tee_ta_param *p, void *va[TEE_NUM_PARAMS])
 {
 	size_t n;
 
@@ -535,13 +570,13 @@ static void init_utee_param(struct utee_params *up,
 		case TEE_PARAM_TYPE_MEMREF_INPUT:
 		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 		case TEE_PARAM_TYPE_MEMREF_INOUT:
-			a = (uintptr_t)p->params[n].memref.buffer;
-			b = p->params[n].memref.size;
+			a = (uintptr_t)va[n];
+			b = p->u[n].mem.size;
 			break;
 		case TEE_PARAM_TYPE_VALUE_INPUT:
 		case TEE_PARAM_TYPE_VALUE_INOUT:
-			a = p->params[n].value.a;
-			b = p->params[n].value.b;
+			a = p->u[n].val.a;
+			b = p->u[n].val.b;
 			break;
 		default:
 			a = 0;
@@ -564,13 +599,13 @@ static void update_from_utee_param(struct tee_ta_param *p,
 		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 		case TEE_PARAM_TYPE_MEMREF_INOUT:
 			/* See comment for struct utee_params in utee_types.h */
-			p->params[n].memref.size = up->vals[n * 2 + 1];
+			p->u[n].mem.size = up->vals[n * 2 + 1];
 			break;
 		case TEE_PARAM_TYPE_VALUE_OUTPUT:
 		case TEE_PARAM_TYPE_VALUE_INOUT:
 			/* See comment for struct utee_params in utee_types.h */
-			p->params[n].value.a = up->vals[n * 2];
-			p->params[n].value.b = up->vals[n * 2 + 1];
+			p->u[n].val.a = up->vals[n * 2];
+			p->u[n].val.b = up->vals[n * 2 + 1];
 			break;
 		default:
 			break;
@@ -596,12 +631,13 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 	struct user_ta_ctx *utc = to_user_ta_ctx(session->ctx);
 	TEE_ErrorOrigin serr = TEE_ORIGIN_TEE;
 	struct tee_ta_session *s __maybe_unused;
+	void *param_va[TEE_NUM_PARAMS] = { NULL };
 
 	if (!(utc->ctx.flags & TA_FLAG_EXEC_DDR))
 		panic("TA does not exec in DDR");
 
 	/* Map user space memory */
-	res = tee_mmu_map_param(utc, param);
+	res = tee_mmu_map_param(utc, param, param_va);
 	if (res != TEE_SUCCESS)
 		goto cleanup_return;
 
@@ -609,10 +645,10 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 	tee_ta_push_current_session(session);
 
 	/* Make room for usr_params at top of stack */
-	usr_stack = (uaddr_t)utc->mmu->table[0].va + utc->stack_size;
+	usr_stack = (uaddr_t)utc->mmu->regions[0].va + utc->stack_size;
 	usr_stack -= ROUNDUP(sizeof(struct utee_params), STACK_ALIGNMENT);
 	usr_params = (struct utee_params *)usr_stack;
-	init_utee_param(usr_params, param);
+	init_utee_param(usr_params, param, param_va);
 
 	res = thread_enter_user_mode(func, tee_svc_kaddr_to_uref(session),
 				     (vaddr_t)usr_params, cmd, usr_stack,
@@ -757,11 +793,18 @@ static void user_ta_dump_state(struct tee_ta_ctx *ctx)
 	EMSG_RAW("- load addr : 0x%x    ctx-idr: %d",
 		 utc->load_addr, utc->context);
 	EMSG_RAW("- stack: 0x%" PRIxVA " %zu",
-		 utc->mmu->table[0].va, utc->stack_size);
-	for (n = 0; n < utc->mmu->size; n++)
+		 utc->mmu->regions[0].va, utc->stack_size);
+	for (n = 0; n < ARRAY_SIZE(utc->mmu->regions); n++) {
+		paddr_t pa = 0;
+
+		if (utc->mmu->regions[n].mobj)
+			mobj_get_pa(utc->mmu->regions[n].mobj,
+				    utc->mmu->regions[n].offset, 0, &pa);
+
 		EMSG_RAW("sect %zu : va %#" PRIxVA " pa %#" PRIxPA " %#zx",
-			 n, utc->mmu->table[n].va, utc->mmu->table[n].pa,
-			 utc->mmu->table[n].size);
+			 n, utc->mmu->regions[n].va, pa,
+			 utc->mmu->regions[n].size);
+	}
 }
 KEEP_PAGER(user_ta_dump_state);
 

commit 3958b8b2ad9b1c47b2ad7e92922893d4dbc3eed2

core: bugfix mobj_virt
Prior to this patch is mobj_virt using mobj_phys_ops instead of the
expected mobj_virt_ops. This patch fixes mobj_virt to use mobj_virt_ops.

Fixes: 0dcfa5686d02 ("core: introduce MOBJ abstraction")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/mobj.c
@@ -194,7 +194,7 @@ static void mobj_virt_assert_type(struct mobj *mobj __maybe_unused)
 	assert(mobj->ops == &mobj_virt_ops);
 }
 
-struct mobj mobj_virt = { .ops = &mobj_phys_ops, .size = SIZE_MAX };
+struct mobj mobj_virt = { .ops = &mobj_virt_ops, .size = SIZE_MAX };
 
 /*
  * mobj_mm implementation