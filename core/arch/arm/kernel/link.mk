OP-TEE/optee_os

commit aa4cc147a462a8a5effc4ccf5062a5eed38c0a5b

core: fix build error with large tee-init_load_addr
If tee-init_load_addr is higher than 0xffffffff. building failed with:

Traceback (most recent call last):
  File "./scripts/gen_hashed_bin.py", line 148, in <module>
    main()
  File "./scripts/gen_hashed_bin.py", line 132, in main
    write_header(outf, 0, args, 0)
  File "./scripts/gen_hashed_bin.py", line 44, in write_header
    args.init_mem_usage, paged_size))
struct.error: 'I' format requires 0 <= number <= 4294967295
make: *** [out/arm-plat-vexpress/core/tee.bin] Error 1
make: Leaving directory `/home/zh/work/github/optee_os'

Signed-off-by: Zhizhou Zhang <zhizhouzhang@asrmicro.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/link.mk
@@ -220,8 +220,12 @@ $(link-out-dir)/tee.bin: $(link-out-dir)/tee-pager.bin \
 	$(q)./scripts/gen_hashed_bin.py \
 		--arch $(if $(filter y,$(CFG_ARM64_core)),arm64,arm32) \
 		--init_size `cat $(link-out-dir)/tee-init_size.txt` \
+		--init_load_addr_hi \
+			`cat $(link-out-dir)/tee-init_load_addr.txt | \
+			${AWK} '{printf("0x%x\n",rshift(strtonum($$0),32))}'` \
 		--init_load_addr_lo \
-			`cat $(link-out-dir)/tee-init_load_addr.txt` \
+			`cat $(link-out-dir)/tee-init_load_addr.txt | \
+			${AWK} '{printf("0x%x\n",and(strtonum($$0),0xffffffff))}'` \
 		--init_mem_usage `cat $(link-out-dir)/tee-init_mem_usage.txt` \
 		--tee_pager_bin $(link-out-dir)/tee-pager.bin \
 		--tee_pageable_bin $(link-out-dir)/tee-pageable.bin \

commit d6adcd2ba6cbd17acf77e67d7a16f4d38fcb6e99

core: do not assume GNU awk is available
The rshift(), strtonum() and and() functions are GNU extensions to awk.
Therefore, the build will fail if another variant of awk is installed
on the build system.
Replace the awk calls with bash arithmetic to avoid any issues. We know
we can rely on the shell being bash, since the main Makefile has
SHELL = /bin/bash.

Fixes: aa4cc147a462 ("core: fix build error with large tee-init_load_addr")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/link.mk
@@ -213,15 +213,12 @@ $(link-out-dir)/tee.bin: $(link-out-dir)/tee-pager.bin \
 			 $(link-out-dir)/tee-init_mem_usage.txt \
 			./scripts/gen_hashed_bin.py
 	@$(cmd-echo-silent) '  GEN     $@'
-	$(q)./scripts/gen_hashed_bin.py \
+	$(q)load_addr=`cat $(link-out-dir)/tee-init_load_addr.txt` && \
+	./scripts/gen_hashed_bin.py \
 		--arch $(if $(filter y,$(CFG_ARM64_core)),arm64,arm32) \
 		--init_size `cat $(link-out-dir)/tee-init_size.txt` \
-		--init_load_addr_hi \
-			`cat $(link-out-dir)/tee-init_load_addr.txt | \
-			${AWK} '{printf("0x%x\n",rshift(strtonum($$0),32))}'` \
-		--init_load_addr_lo \
-			`cat $(link-out-dir)/tee-init_load_addr.txt | \
-			${AWK} '{printf("0x%x\n",and(strtonum($$0),0xffffffff))}'` \
+		--init_load_addr_hi $$(($$load_addr >> 32 & 0xffffffff)) \
+		--init_load_addr_lo $$(($$load_addr & 0xffffffff)) \
 		--init_mem_usage `cat $(link-out-dir)/tee-init_mem_usage.txt` \
 		--tee_pager_bin $(link-out-dir)/tee-pager.bin \
 		--tee_pageable_bin $(link-out-dir)/tee-pageable.bin \