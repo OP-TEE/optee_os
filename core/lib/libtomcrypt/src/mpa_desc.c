13c9b83130e08ddd53fb3a456a678c7e3040deb9
Joakim Bech <joakim.bech@linaro.org>
2017-02-13 19:40:46 +0100

    ltc: Implement mp_rand for mpa_desc
    When enabling the flag LTC_RSA_BLINDING the code uses the mp_rand()
    function, which isn't implemented for the mpa_desc descriptor. Implement
    it as rand() in mpa_desc and mpa_get_random_digits() in libmpa.
    
    Fixes: OP-TEE-2016-0003 which was reported by Applus+ Laboratories.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey, GP)
    Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (b2260, GP)

--- core/lib/libtomcrypt/src/mpa_desc.c
+++ core/lib/libtomcrypt/src/mpa_desc.c
@@ -602,6 +602,12 @@ static int isprime(void *a, int b, int *c)
 	return CRYPT_OK;
 }
 
+static int rand(void *a, int size)
+{
+	return mpa_get_random_digits(a, size) != size ?
+					CRYPT_ERROR_READPRNG : CRYPT_OK;
+}
+
 ltc_math_descriptor ltc_mp = {
 	.name = "MPA",
 	.bits_per_digit = MPA_WORD_SIZE,
@@ -678,5 +684,6 @@ ltc_math_descriptor ltc_mp = {
 	.rsa_keygen = &rsa_make_key,
 	.rsa_me = &rsa_exptmod,
 #endif
+	.rand = &rand,
 
 };
