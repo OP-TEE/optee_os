9ee4baadf1936edeb121b2eed70afadc113e1046
Jerome Forissier <jerome.forissier@linaro.org>
2017-02-17 17:39:06 +0100

    drivers: convert ns16550 driver to use struct serial_chip
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/drivers/ns16550.c
+++ core/drivers/ns16550.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Freescale Semiconductor, Inc.
+ * Copyright (c) 2017, Linaro Limited
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,6 +28,8 @@
 
 #include <drivers/ns16550.h>
 #include <io.h>
+#include <mm/core_mmu.h>
+#include <util.h>
 
 /* uart register defines */
 #define UART_RBR	0x0
@@ -42,16 +45,46 @@
 /* uart status register bits */
 #define UART_LSR_THRE	0x20 /* Transmit-hold-register empty */
 
-void ns16550_flush(vaddr_t base)
+static vaddr_t base_addr(struct ns16550_data *pd)
 {
+	return cpu_mmu_enabled() ? pd->vbase : pd->pbase;
+}
+
+static void ns16550_flush(struct serial_chip *chip)
+{
+	struct ns16550_data *pd = container_of(chip, struct ns16550_data, chip);
+	vaddr_t base = base_addr(pd);
+
 	while ((read8(base + UART_LSR) & UART_LSR_THRE) == 0)
 		;
 }
 
-void ns16550_putc(int ch, vaddr_t base)
+static void ns16550_putc(struct serial_chip *chip, int ch)
 {
-	ns16550_flush(base);
+	struct ns16550_data *pd = container_of(chip, struct ns16550_data, chip);
+	vaddr_t base = base_addr(pd);
+
+	ns16550_flush(chip);
 
 	/* write out charset to Transmit-hold-register */
 	write8(ch, base + UART_THR);
+}
+
+static const struct serial_ops ns16550_ops = {
+	.flush = ns16550_flush,
+	.putc = ns16550_putc,
+};
+
+void ns16550_init(struct ns16550_data *pd, vaddr_t base)
+{
+	if (cpu_mmu_enabled())
+		pd->vbase = base;
+	else
+		pd->pbase = base;
+	pd->chip.ops = &ns16550_ops;
+
+	/*
+	 * Do nothing, uart driver shared with normal world,
+	 * everything for uart driver intialization is done in bootloader.
+	 */
 }
