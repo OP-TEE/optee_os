9fe4c79784dbccb3eaa561e3ccd81855c675ee12
Jerome Forissier <jerome.forissier@linaro.org>
2017-03-02 17:46:55 +0100

    dt: Add FDT manipulation functions
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/kernel/dt.c
+++ core/kernel/dt.c
@@ -26,6 +26,7 @@
  */
 
 #include <kernel/dt.h>
+#include <libfdt.h>
 #include <string.h>
 
 const struct dt_driver *dt_find_compatible_driver(const void *fdt, int offs)
@@ -50,4 +51,117 @@ const struct dt_driver *__dt_driver_start(void)
 const struct dt_driver *__dt_driver_end(void)
 {
 	return &__rodata_dtdrv_end;
+}
+
+/* Read a physical address (n=1 or 2 cells) */
+static paddr_t _fdt_read_paddr(const uint32_t *cell, int n)
+{
+	paddr_t addr;
+
+	if (n < 1 || n > 2)
+		goto bad;
+
+	addr = fdt32_to_cpu(*cell);
+	cell++;
+	if (n == 2) {
+#ifdef ARM32
+		if (addr) {
+			/* High order 32 bits can't be nonzero */
+			goto bad;
+		}
+		addr = fdt32_to_cpu(*cell);
+#else
+		addr = (addr << 32) | fdt32_to_cpu(*cell);
+#endif
+	}
+
+	if (!addr)
+		goto bad;
+
+	return addr;
+bad:
+	return (paddr_t)-1;
+
+}
+
+paddr_t _fdt_reg_base_address(const void *fdt, int offs)
+{
+	const void *reg;
+	int ncells;
+	int len;
+
+	reg = fdt_getprop(fdt, offs, "reg", &len);
+	if (!reg)
+		return (paddr_t)-1;
+
+	ncells = fdt_address_cells(fdt, offs);
+	if (ncells < 0)
+		return (paddr_t)-1;
+
+	return _fdt_read_paddr(reg, ncells);
+}
+
+ssize_t _fdt_reg_size(const void *fdt, int offs)
+{
+	const uint32_t *reg;
+	uint32_t sz;
+	int n;
+	int len;
+
+	reg = (const uint32_t *)fdt_getprop(fdt, offs, "reg", &len);
+	if (!reg)
+		return -1;
+
+	n = fdt_address_cells(fdt, offs);
+	if (n < 1 || n > 2)
+		return -1;
+
+	reg += n;
+
+	n = fdt_size_cells(fdt, offs);
+	if (n < 1 || n > 2)
+		return -1;
+
+	sz = fdt32_to_cpu(*reg);
+	if (n == 2) {
+		if (sz)
+			return -1;
+		reg++;
+		sz = fdt32_to_cpu(*reg);
+	}
+
+	return sz;
+}
+
+static bool is_okay(const char *st, int len)
+{
+	return !strncmp(st, "ok", len) || !strncmp(st, "okay", len);
+}
+
+int _fdt_get_status(const void *fdt, int offs)
+{
+	const char *prop;
+	int st = 0;
+	int len;
+
+	prop = fdt_getprop(fdt, offs, "status", &len);
+	if (!prop || is_okay(prop, len)) {
+		/* If status is not specified, it defaults to "okay" */
+		st |= DT_STATUS_OK_NSEC;
+	}
+
+	prop = fdt_getprop(fdt, offs, "secure-status", &len);
+	if (!prop) {
+		/*
+		 * When secure-status is not specified it defaults to the same
+		 * value as status
+		 */
+		if (st & DT_STATUS_OK_NSEC)
+			st |= DT_STATUS_OK_SEC;
+	} else {
+		if (is_okay(prop, len))
+			st |= DT_STATUS_OK_SEC;
+	}
+
+	return st;
 }
