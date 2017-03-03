OP-TEE/optee_os

commit 82f97f19fe2d763dfa6c2da85385e6cda5b43b64

prng: call plat_prng_add_jitter_entropy() at PRNG init and before NW RPC
This patch adds the new platform jitter collection API to be called first
at PRNG init, and subsequently on every RPC.

Signed-off-by: Andy Green <andy@warmcat.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/kernel/thread.c
@@ -45,6 +45,7 @@
 #include <sm/optee_smc.h>
 #include <sm/sm.h>
 #include <tee/tee_fs_rpc.h>
+#include <tee/tee_cryp_utl.h>
 #include <trace.h>
 #include <util.h>
 
@@ -1192,6 +1193,8 @@ static uint32_t rpc_cmd_nolock(uint32_t cmd, size_t num_params,
 	size_t n;
 
 	assert(arg && carg && num_params <= THREAD_RPC_MAX_NUM_PARAMS);
+
+	plat_prng_add_jitter_entropy();
 
 	memset(arg, 0, OPTEE_MSG_GET_ARG_SIZE(THREAD_RPC_MAX_NUM_PARAMS));
 	arg->cmd = cmd;

core/lib/libtomcrypt/src/tee_ltc_provider.c
@@ -170,6 +170,9 @@ static TEE_Result tee_ltc_prng_init(struct tee_ltc_prng *prng)
 	}
 
 	prng->index = prng_index;
+
+	plat_prng_add_jitter_entropy();
+
 	return  TEE_SUCCESS;
 }
 

commit 25d6f46ee1fde50fab35f0948efd2e6b8c3e6e97

core: prng: Don't lock a mutex from rpc_cmd_nolock()
Avoids a recursive indirect call to rpc_cmd_nolock() from
plat_prng_add_jitter_entropy_norpc() by not calling
plat_prng_add_jitter_entropy_norpc() for RPC related to mutex
wait/wakeup.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (HiKey)
Fixes: 82f97f19fe2d ("prng: call plat_prng_add_jitter_entropy() at PRNG init and before NW RPC")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/thread.c
@@ -1199,7 +1199,14 @@ static uint32_t rpc_cmd_nolock(uint32_t cmd, size_t num_params,
 
 	assert(arg && carg && num_params <= THREAD_RPC_MAX_NUM_PARAMS);
 
-	plat_prng_add_jitter_entropy_norpc();
+
+	/*
+	 * Break recursion in case plat_prng_add_jitter_entropy_norpc()
+	 * sleeps on a mutex or unlocks a mutex with a sleeper (contended
+	 * mutex).
+	 */
+	if (cmd != OPTEE_MSG_RPC_CMD_WAIT_QUEUE)
+		plat_prng_add_jitter_entropy_norpc();
 
 	memset(arg, 0, OPTEE_MSG_GET_ARG_SIZE(THREAD_RPC_MAX_NUM_PARAMS));
 	arg->cmd = cmd;

core/include/tee/tee_cryp_utl.h
@@ -50,6 +50,10 @@ TEE_Result tee_aes_cbc_cts_update(void *cbc_ctx, void *ecb_ctx,
 
 TEE_Result tee_prng_add_entropy(const uint8_t *in, size_t len);
 void plat_prng_add_jitter_entropy(void);
+/*
+ * The _norpc version must not invoke Normal World, or infinite recursion
+ * may occur. As an exception however, using mutexes is allowed.
+ */
 void plat_prng_add_jitter_entropy_norpc(void);
 
 #endif