e23868cfaab904ffaa9138adfe727fdb502935c8
Joakim Bech <joakim.bech@linaro.org>
2017-03-21 11:37:49 +0100

    ltc: Enable RSA_CRT_HARDENING and RSA_CRT_BLINDING
    Enable the hardening flags by default. This should make it robust to the
    Bellcore attack when using RSA with CRT.
    
    Fixes: OP-TEE-2016-0003 which was reported by Applus+ Laboratories.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey, GP)
    Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (b2260, GP)

--- core/lib/libtomcrypt/sub.mk
+++ core/lib/libtomcrypt/sub.mk
@@ -120,6 +120,7 @@ _CFG_CRYPTO_WITH_ASN1 := $(call cryp-one-enabled, RSA DSA ECC)
 _CFG_CRYPTO_WITH_FORTUNA_PRNG := $(call cryp-all-enabled, AES SHA256)
 
 cppflags-lib-$(CFG_CRYPTO_SIZE_OPTIMIZATION) += -DLTC_SMALL_CODE
+cppflags-lib-y += -DLTC_RSA_CRT_HARDENING -DLTC_RSA_BLINDING
 cflags-lib-$(CFG_CRYPTO_SIZE_OPTIMIZATION) += -Os
 
 global-incdirs-y += include
