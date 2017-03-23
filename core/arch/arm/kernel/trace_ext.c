1d8a391c5e809d3bf09459ee1bbb6bf32a1876eb
Peng Fan <peng.fan@nxp.com>
2017-03-23 09:17:36 +0800

    core: arm: trace: make the trace output atomic
    Use spinlock to make the trace output atomic.
    On SMP cores, different cores may puts out log
    to uart. If there is no lock, the log will be a mess.
    
    When there is a contention, print out a '*'.
    
    Need to disable interrupt, because cpu_spin_lock/unlock/trylock
    will invoke assert with CFG_TEE_CORE_DEBUG=y. So
    mask interrupt before lock, then un-mask interrupt after unlock.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (HiKey)
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/kernel/trace_ext.c
+++ core/arch/arm/kernel/trace_ext.c
@@ -27,21 +27,40 @@
 #include <stdbool.h>
 #include <trace.h>
 #include <console.h>
+#include <kernel/spinlock.h>
 #include <kernel/thread.h>
+#include <mm/core_mmu.h>
 
 const char trace_ext_prefix[] = "TEE-CORE";
 int trace_level = TRACE_LEVEL;
+static unsigned int puts_lock = SPINLOCK_UNLOCK;
 
 void trace_ext_puts(const char *str)
 {
+	uint32_t itr_status = thread_mask_exceptions(THREAD_EXCP_ALL);
+	bool mmu_enabled = cpu_mmu_enabled();
+	bool was_contended = false;
 	const char *p;
 
+	if (mmu_enabled && !cpu_spin_trylock(&puts_lock)) {
+		was_contended = true;
+		cpu_spin_lock(&puts_lock);
+	}
+
 	console_flush();
+
+	if (was_contended)
+		console_putc('*');
 
 	for (p = str; *p; p++)
 		console_putc(*p);
 
 	console_flush();
+
+	if (mmu_enabled)
+		cpu_spin_unlock(&puts_lock);
+
+	thread_unmask_exceptions(itr_status);
 }
 
 int trace_ext_get_thread_id(void)
