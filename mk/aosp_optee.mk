b96f67db7c0b075b0daa3d7d00036c46cecdfcfa
Victor Chong <victor.chong@linaro.org>
2017-03-25 15:55:39 +0000

    mk/aosp_optee.mk: Allow extra flags for aosp builds
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    [jf: rebase on top of master]
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- mk/aosp_optee.mk
+++ mk/aosp_optee.mk
@@ -1,13 +1,14 @@
 ##########################################################
 ## Common mk file used for Android to compile and       ##
-## integrate OP-TEE related components                   ##
-## Following flags need to be defined in device.mk    ##
+## integrate OP-TEE related components                  ##
+## Following flags need to be defined in optee*.mk      ##
 ##    OPTEE_OS_DIR                                      ##
 ##    OPTEE_TA_TARGETS                                  ##
 ##    OPTEE_CFG_ARM64_CORE                              ##
 ##    OPTEE_PLATFORM                                    ##
 ##    OPTEE_PLATFORM_FLAVOR                             ##
-## And BUILD_OPTEE_MK needs to be defined in device.mk  ##
+##    OPTEE_EXTRA_FLAGS (optional)                      ##
+## And BUILD_OPTEE_MK needs to be defined in optee*.mk  ##
 ## to point to this file                                ##
 ##                                                      ##
 ## local_module needs to be defined before including    ##
@@ -50,7 +51,8 @@ BUILD_OPTEE_OS:
 		CFG_ARM64_core=$(OPTEE_CFG_ARM64_CORE) \
 		PLATFORM=$(OPTEE_PLATFORM) \
 		PLATFORM_FLAVOR=$(OPTEE_PLATFORM_FLAVOR) \
-		$(CROSS_COMPILE_LINE)
+		$(CROSS_COMPILE_LINE) \
+		$(OPTEE_EXTRA_FLAGS)
 	@echo "Finished building optee_os..."
 
 endif
