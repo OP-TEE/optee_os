75c6da9db3bd65fa6a84aff1951a32f587b1abce
Andrew F. Davis <afd@ti.com>
2017-04-20 12:09:50 -0500

    plat-ti: Add support for using HUK on DRA7xx/AM57xx
    On DRA7xx/AM57xx the initial secure software will pass OP-TEE a
    Hardware Unique Key (HUK), use this key when requested.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-ti/conf.mk
+++ core/arch/arm/plat-ti/conf.mk
@@ -15,6 +15,7 @@ $(call force,CFG_PL310_LOCKED,y)
 $(call force,CFG_SECURE_TIME_SOURCE_REE,y)
 arm32-platform-cpuarch		:= cortex-a9
 else
+CFG_OTP_SUPPORT ?= y
 $(call force,CFG_HWSUPP_MEM_PERM_PXN,y)
 $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 arm32-platform-cpuarch		:= cortex-a15
