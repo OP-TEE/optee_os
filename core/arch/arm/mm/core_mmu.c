OP-TEE/optee_os

commit aa2ab38be0aa8bd148cfc5ccc2ef580f1acc64c8

core: more flexible static memory mapping
This change allows remove dependency of the static mapping
description on memory type IDs ordering and the requirements
for such memory types to match unique memories. This change
will be required by a later change that will define several
memory types for the TEE RAM (read-only/executable, read-only,
read/write).

The setup of the static memory mapping array is somewhat changed:
- 1st step: fill the array from registered "phys_mem" (unchanged)
- 2nd step: assign "region size" to arrays cells (unchanged)
- 3rd step: sort array by region size. This allows to group
  small page mappings together to prevent wasting xlat tables.
- 4th step: for non LPAE mapping only: separate secure mapping
  from non-secure mapping for the small page mapped areas.
- 5th step: assign virtual addresses for flat-mapped areas.
  This sequence saves the flat mapped areas base address which is
  used in the next step to assign non-flat mapped virtual memories.
- 6th step: assign virtual addresses for the non-flat mapped areas.
  This step considers the cases where non-flat mapped areas are
  mapped before or after flat mapped areas as done before this
  change.
- a last, yet not mandatory step sorts the static mapping
  description array by virtual address ranges.

With this change, the static mapping does not expects memory type
ID value ordering reflect any ordering of the areas virtual address
ranges. Instead, the memory type IDs only reflect the expected
mapping attributes which drive how virtual mapping is built.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -26,12 +26,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
- * This core mmu supports static section mapping (1MByte) and finer mapping
- * with 4k pages.
- *       It should also allow core to map/unmap (and va/pa) at run-time.
- */
-
 #include <arm.h>
 #include <assert.h>
 #include <kernel/generic_boot.h>
@@ -317,11 +311,9 @@ static void add_phys_mem(struct tee_mmap_region *memory_map, size_t num_elems,
 	size_t size;
 
 	/*
-	 * When all entries are added we'd like to have it in a sorted
-	 * array first based on memory type and secondly on physical
-	 * address. If some ranges of memory of the same type overlaps of
-	 * are next to each others they are coalesced into one entry. This
-	 * makes it easier later when building the translation tables.
+	 * If some ranges of memory of the same type do overlap
+	 * each others they are coalesced into one entry. To help this
+	 * added entries are sorted by increasing physical.
 	 *
 	 * Note that it's valid to have the same physical memory as several
 	 * different memory types, for instance the same device memory
@@ -418,9 +410,53 @@ uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 	}
 }
 
-static bool __maybe_unused map_is_tee_ram(struct tee_mmap_region *mm)
+static bool __maybe_unused map_is_tee_ram(const struct tee_mmap_region *mm)
 {
 	return mm->type == MEM_AREA_TEE_RAM;
+}
+
+static bool map_is_flat_mapped(const struct tee_mmap_region *mm)
+{
+	return map_is_tee_ram(mm);
+}
+
+static bool __maybe_unused map_is_secure(const struct tee_mmap_region *mm)
+{
+	return !!(core_mmu_type_to_attr(mm->type) & TEE_MATTR_SECURE);
+}
+
+static bool __maybe_unused map_is_pgdir(const struct tee_mmap_region *mm)
+{
+	return mm->region_size == CORE_MMU_PGDIR_SIZE;
+}
+
+static int cmp_mmap_by_lower_va(const void *a, const void *b)
+{
+	const struct tee_mmap_region *mm_a = a;
+	const struct tee_mmap_region *mm_b = b;
+
+	return mm_a->va - mm_b->va;
+}
+
+static int __maybe_unused cmp_mmap_by_secure_attr(const void *a, const void *b)
+{
+	const struct tee_mmap_region *mm_a = a;
+	const struct tee_mmap_region *mm_b = b;
+
+	/* unmapped areas are special */
+	if (!core_mmu_type_to_attr(mm_a->type) ||
+	    !core_mmu_type_to_attr(mm_b->type))
+		return 0;
+
+	return map_is_secure(mm_b) - map_is_secure(mm_a);
+}
+
+static int cmp_mmap_by_bigger_region_size(const void *a, const void *b)
+{
+	const struct tee_mmap_region *mm_a = a;
+	const struct tee_mmap_region *mm_b = b;
+
+	return mm_b->region_size - mm_a->region_size;
 }
 
 static void dump_mmap_table(struct tee_mmap_region *memory_map)
@@ -432,10 +468,11 @@ static void dump_mmap_table(struct tee_mmap_region *memory_map)
 
 		vstart = map->va + ((vaddr_t)map->pa & (map->region_size - 1));
 		DMSG("type va %d 0x%08" PRIxVA "..0x%08" PRIxVA
-		     " pa 0x%08" PRIxPA "..0x%08" PRIxPA " size %#zx",
+		     " pa 0x%08" PRIxPA "..0x%08" PRIxPA " size %#zx (%s)",
 		     map->type, vstart, vstart + map->size - 1,
 		     map->pa, (paddr_t)(map->pa + map->size - 1),
-		     map->size);
+		     map->size,
+		     map->region_size == SMALL_PAGE_SIZE ? "smallpg" : "pgdir");
 	}
 }
 
@@ -444,8 +481,9 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	const struct core_mmu_phys_mem *mem;
 	struct tee_mmap_region *map;
 	size_t last = 0;
+	size_t __maybe_unused count = 0;
 	vaddr_t va;
-	size_t n;
+	vaddr_t __maybe_unused va_max;
 
 	for (mem = &__start_phys_mem_map_section;
 	     mem < &__end_phys_mem_map_section; mem++) {
@@ -487,49 +525,71 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	}
 
 	/*
-	 * bootcfg_memory_map is sorted in order first by type and last by
-	 * address. This puts TEE_RAM first and TA_RAM second
-	 *
+	 * To ease mapping and lower use of xlat tables, sort mapping
+	 * description moving small-page regions after the pgdir regions.
 	 */
-	map = memory_map;
-	assert(map->type == MEM_AREA_TEE_RAM);
-	map->va = map->pa;
-	map->attr = core_mmu_type_to_attr(map->type);
+	qsort(memory_map, last, sizeof(struct tee_mmap_region),
+		cmp_mmap_by_bigger_region_size);
 
+#if !defined(CFG_WITH_LPAE)
+	/*
+	 * 32bit MMU descriptors cannot mix secure and non-secure mapping in
+	 * the same level2 table. Hence sort secure mapping from non-secure
+	 * mapping.
+	 */
+	for (count = 0, map = memory_map; map_is_pgdir(map); count++, map++)
+		;
 
-	if (core_mmu_place_tee_ram_at_top(map->pa)) {
-		va = map->va;
-		map++;
-		while (map->type != MEM_AREA_NOTYPE) {
+	qsort(memory_map + count, last - count, sizeof(struct tee_mmap_region),
+		cmp_mmap_by_secure_attr);
+#endif
+
+	/*
+	 * Map flat mapped addresses first.
+	 * 'va' will store the lower address of the flat-mapped areas to later
+	 * setup the virtual mapping of the non flat-mapped areas.
+	 */
+	va = (vaddr_t)~0UL;
+	va_max = 0;
+	for (map = memory_map; map->type != MEM_AREA_NOTYPE; map++) {
+		if (!map_is_flat_mapped(map))
+			continue;
+
+		map->attr = core_mmu_type_to_attr(map->type);
+		map->va = map->pa;
+		va = MIN(va, ROUNDDOWN(map->va, map->region_size));
+		va_max = MAX(va_max, ROUNDUP(va + map->size, map->region_size));
+	}
+	assert(va >= CFG_TEE_RAM_START);
+	assert(va_max <= CFG_TEE_RAM_START + CFG_TEE_RAM_VA_SIZE);
+
+	if (core_mmu_place_tee_ram_at_top(va)) {
+		/* Map non-flat mapped addresses below flat mapped addresses */
+		for (map = memory_map; map->type != MEM_AREA_NOTYPE; map++) {
+			if (map_is_flat_mapped(map))
+				continue;
+
 			map->attr = core_mmu_type_to_attr(map->type);
 			va -= map->size;
 			va = ROUNDDOWN(va, map->region_size);
 			map->va = va;
-			map++;
-		}
-		/*
-		 * The memory map should be sorted by virtual address
-		 * when this function returns. As we're assigning va in
-		 * the oposite direction we need to reverse the list.
-		 */
-		for (n = 0; n < last / 2; n++) {
-			struct tee_mmap_region r;
-
-			r = memory_map[last - n - 1];
-			memory_map[last - n - 1] = memory_map[n];
-			memory_map[n] = r;
 		}
 	} else {
-		va = ROUNDUP(map->va + map->size, CORE_MMU_PGDIR_SIZE);
-		map++;
-		while (map->type != MEM_AREA_NOTYPE) {
+		/* Map non-flat mapped addresses above flat mapped addresses */
+		va = ROUNDUP(va + CFG_TEE_RAM_VA_SIZE, CORE_MMU_PGDIR_SIZE);
+		for (map = memory_map; map->type != MEM_AREA_NOTYPE; map++) {
+			if (map_is_flat_mapped(map))
+				continue;
+
 			map->attr = core_mmu_type_to_attr(map->type);
 			va = ROUNDUP(va, map->region_size);
 			map->va = va;
 			va += map->size;
-			map++;
 		}
 	}
+
+	qsort(memory_map, last, sizeof(struct tee_mmap_region),
+		cmp_mmap_by_lower_va);
 
 	dump_mmap_table(memory_map);
 }

commit 2dd446e9fa1f9b5c89a308b2f29b4c5530b70a1f

core: fix test against end address of core virtual memory
Fix the first argument of the ROUNDUP() that should be 'map->va' instead
of 'va' which is the lowest virtual address mapped.

'va_max' is renamed 'end' to help code fitting in 80 char lines.

Fixes: aa2ab38be0aa (core: more flexible static memory mapping)
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -484,7 +484,7 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	size_t last = 0;
 	size_t __maybe_unused count = 0;
 	vaddr_t va;
-	vaddr_t __maybe_unused va_max;
+	vaddr_t __maybe_unused end;
 
 	for (mem = &__start_phys_mem_map_section;
 	     mem < &__end_phys_mem_map_section; mem++) {
@@ -557,7 +557,7 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	 * setup the virtual mapping of the non flat-mapped areas.
 	 */
 	va = (vaddr_t)~0UL;
-	va_max = 0;
+	end = 0;
 	for (map = memory_map; map->type != MEM_AREA_NOTYPE; map++) {
 		if (!map_is_flat_mapped(map))
 			continue;
@@ -565,10 +565,10 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 		map->attr = core_mmu_type_to_attr(map->type);
 		map->va = map->pa;
 		va = MIN(va, ROUNDDOWN(map->va, map->region_size));
-		va_max = MAX(va_max, ROUNDUP(va + map->size, map->region_size));
+		end = MAX(end, ROUNDUP(map->va + map->size, map->region_size));
 	}
 	assert(va >= CFG_TEE_RAM_START);
-	assert(va_max <= CFG_TEE_RAM_START + CFG_TEE_RAM_VA_SIZE);
+	assert(end <= CFG_TEE_RAM_START + CFG_TEE_RAM_VA_SIZE);
 
 	if (core_mmu_place_tee_ram_at_top(va)) {
 		/* Map non-flat mapped addresses below flat mapped addresses */