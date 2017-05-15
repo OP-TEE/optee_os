OP-TEE/optee_os

commit 1472c996cca6291b6cbfca2115e9d307759fb43b

Take some GP types into account when running patch check tool
When running checkpatch on the OP-TEE code, for instance during the
Travis checks, it happens that we get false warnings and errors caused
by the use of GlobalPlatform typedefs. In the following example, the
patch introduces pointers to functions returning TEE_Result, but
checkpatch won't accept it as a valid type:

 $ ./scripts/checkpatch.sh d776721 | tail -1
 total: 1 errors, 5 warnings, 555 lines checked

To address such issues, declare our custom types in a local file
(typedefs.checkpatch) and use the new --typedefsfile option provided by
checkpatch.pl.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

scripts/checkpatch_inc.sh
@@ -8,7 +8,8 @@ _CP_EXCL=$(for p in $CHECKPATCH_IGNORE; do echo ":(exclude)$p" ; done)
 
 function _checkpatch() {
 		$CHECKPATCH --quiet --ignore FILE_PATH_CHANGES \
-				--ignore GERRIT_CHANGE_ID --no-tree -
+				--ignore GERRIT_CHANGE_ID \
+				--typedefsfile typedefs.checkpatch --no-tree -
 }
 
 function checkpatch() {

typedefs.checkpatch
@@ -1,0 +1,2 @@
+TEE_Result
+TEE_UUID

commit c1d47bcf985f2f45ff380239e0d20876a207258b

checkpatch: support tools not supporting argument --typedefsfile
If one relies on a checkpatch tool that does not support argument
--typedefsfile, the script will fail whereas it could simply run
without this extra feature.

Fixes: 1472c996cca6 ("Take some GP types into account when running patch check tool")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

scripts/checkpatch_inc.sh
@@ -7,9 +7,15 @@ CHECKPATCH_IGNORE=$(echo core/lib/lib{fdt,tomcrypt} lib/lib{png,utils,zlib} \
 _CP_EXCL=$(for p in $CHECKPATCH_IGNORE; do echo ":(exclude)$p" ; done)
 
 function _checkpatch() {
+		# Use --typedefsfile if supported by the checkpatch tool
+		typedefs_opt="--typedefsfile typedefs.checkpatch"
+		$CHECKPATCH --help 2>&1 | grep -q -- --typedefsfile || \
+				typedefs_opt="";
+
 		$CHECKPATCH --quiet --ignore FILE_PATH_CHANGES \
-				--ignore GERRIT_CHANGE_ID \
-				--typedefsfile typedefs.checkpatch --no-tree -
+				--ignore GERRIT_CHANGE_ID --no-tree \
+				$typedefs_opt \
+				-
 }
 
 function checkpatch() {