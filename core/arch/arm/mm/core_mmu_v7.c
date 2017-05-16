OP-TEE/optee_os

commit c6c69797168cab3bc54eed3a430f2d9f2d278824

mm: add new VA region for dynamic shared buffers
This region will be used later to dynamically map shared buffers
provided by Normal World.

Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu_v7.c
@@ -776,7 +776,8 @@ void core_init_mmu_tables(struct tee_mmap_region *mm)
 	memset(ttb1, 0, L1_TBL_SIZE);
 
 	for (n = 0; mm[n].size; n++)
-		map_memarea(mm + n, ttb1);
+		if (!core_mmu_is_dynamic_vaspace(mm))
+			map_memarea(mm + n, ttb1);
 }
 
 bool core_mmu_place_tee_ram_at_top(paddr_t paddr)

core/arch/arm/include/mm/core_mmu.h
@@ -96,6 +96,7 @@
  * MEM_AREA_IO_NSEC:  NonSecure HW mapped registers
  * MEM_AREA_IO_SEC:   Secure HW mapped registers
  * MEM_AREA_RES_VASPACE: Reserved virtual memory space
+ * MEM_AREA_SHM_VASPACE: Virtual memory space for dynamic shared memory buffers
  * MEM_AREA_TA_VASPACE: TA va space, only used with phys_to_virt()
  * MEM_AREA_MAXTYPE:  lower invalid 'type' value
  */
@@ -110,6 +111,7 @@ enum teecore_memtypes {
 	MEM_AREA_IO_NSEC,
 	MEM_AREA_IO_SEC,
 	MEM_AREA_RES_VASPACE,
+	MEM_AREA_SHM_VASPACE,
 	MEM_AREA_TA_VASPACE,
 	MEM_AREA_SDP_MEM,
 	MEM_AREA_MAXTYPE
@@ -365,6 +367,18 @@ static inline size_t core_mmu_get_block_offset(
 			struct core_mmu_table_info *tbl_info, paddr_t pa)
 {
 	return pa & ((1 << tbl_info->shift) - 1);
+}
+
+/*
+ * core_mmu_is_dynamic_vaspace() - Check if memory region belongs to
+ *  empty virtual address space that is used for dymanic mappings
+ * @mm:		memory region to be checked
+ * @returns result of the check
+ */
+static inline bool core_mmu_is_dynamic_vaspace(struct tee_mmap_region *mm)
+{
+	return mm->type == MEM_AREA_RES_VASPACE ||
+		mm->type == MEM_AREA_SHM_VASPACE;
 }
 
 /*

core/arch/arm/mm/core_mmu.c
@@ -51,6 +51,7 @@
 
 #define MAX_MMAP_REGIONS	10
 #define RES_VASPACE_SIZE	(CORE_MMU_PGDIR_SIZE * 10)
+#define SHM_VASPACE_SIZE	(1024 * 1024 * 32)
 
 /*
  * These variables are initialized before .bss is cleared. To avoid
@@ -395,6 +396,7 @@ uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 	case MEM_AREA_TA_RAM:
 		return attr | TEE_MATTR_SECURE | cached;
 	case MEM_AREA_NSEC_SHM:
+	case MEM_AREA_SHM_VASPACE:
 		return attr | cached;
 	case MEM_AREA_IO_NSEC:
 		return attr | noncache;
@@ -507,6 +509,9 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	verify_sdp_mem_areas(memory_map, num_elems);
 
 	add_va_space(memory_map, num_elems, MEM_AREA_RES_VASPACE,
+		     RES_VASPACE_SIZE, &last);
+
+	add_va_space(memory_map, num_elems, MEM_AREA_SHM_VASPACE,
 		     RES_VASPACE_SIZE, &last);
 
 	memory_map[last].type = MEM_AREA_NOTYPE;
@@ -654,6 +659,7 @@ void core_init_mmu_map(void)
 		case MEM_AREA_RAM_SEC:
 		case MEM_AREA_RAM_NSEC:
 		case MEM_AREA_RES_VASPACE:
+		case MEM_AREA_SHM_VASPACE:
 			break;
 		default:
 			EMSG("Uhandled memtype %d", map->type);

core/arch/arm/mm/core_mmu_lpae.c
@@ -361,12 +361,15 @@ static struct tee_mmap_region *init_xlation_table(struct tee_mmap_region *mm,
 	do  {
 		uint64_t desc = UNSET_DESC;
 
-		if (mm->va + mm->size <= base_va) {
-			/* Area now after the region so skip it */
+		if ((mm->va + mm->size <= base_va) ||
+		    core_mmu_is_dynamic_vaspace(mm)) {
+			/*
+			 * Area now after the region or area should not be
+			 * mapped at init so skip it
+			 */
 			mm++;
 			continue;
 		}
-
 
 		if (mm->va >= base_va + level_size) {
 			/* Next region is after area so nothing to map yet */

commit 8ebc2babef143b80dec1a959632184228f3dc0b4

core: fix non-LPAE mapping
Fixes: c6c69797168c  ("mm: add new VA region for dynamic shared buffers")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Tested-by: Andrew F. Davis <afd@ti.com> (am43xx)

core/arch/arm/mm/core_mmu_v7.c
@@ -783,7 +783,7 @@ void core_init_mmu_tables(struct tee_mmap_region *mm)
 	memset(ttb1, 0, L1_TBL_SIZE);
 
 	for (n = 0; mm[n].size; n++)
-		if (!core_mmu_is_dynamic_vaspace(mm))
+		if (!core_mmu_is_dynamic_vaspace(mm + n))
 			map_memarea(mm + n, ttb1);
 }
 