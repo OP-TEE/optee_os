1cb4377071b08ce530b7124c0f78631f4873f591
Jens Wiklander <jens.wiklander@linaro.org>
2017-05-24 12:26:32 +0200

    core: sparse fix: pm_panic
    Removes __noreturn attribute from pm_panic() to avoid sparse warnings
    when used to initialize struct thread_handlers.
    
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/include/kernel/pm_stubs.h
+++ core/arch/arm/include/kernel/pm_stubs.h
@@ -31,7 +31,7 @@
 #include <stdint.h>
 #include <compiler.h>
 
-unsigned long pm_panic(unsigned long a0, unsigned long a1) __noreturn;
+unsigned long pm_panic(unsigned long a0, unsigned long a1);
 unsigned long pm_do_nothing(unsigned long a0, unsigned long a1);
 
 #endif /* PM_STUBS_H */
