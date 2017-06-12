8ca80fb8948d777c39d1d72936ada97f7c679eeb
Jerome Forissier <jerome.forissier@linaro.org>
2017-06-12 14:30:44 +0200

    plat-hikey: enable 64-bit paging
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-hikey/platform_config.h
+++ core/arch/arm/plat-hikey/platform_config.h
@@ -31,12 +31,6 @@
 /* Make stacks aligned to data cache line length */
 #define STACK_ALIGNMENT		64
 
-#ifdef ARM64
-#ifdef CFG_WITH_PAGER
-#error "Pager not supported for ARM64"
-#endif
-#endif /* ARM64 */
-
 /* PL011 UART */
 #if defined(CFG_CONSOLE_UART) && (CFG_CONSOLE_UART == 0)
 #define CONSOLE_UART_BASE       0xF8015000
