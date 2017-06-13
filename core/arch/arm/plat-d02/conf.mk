0d9e635854d580553d723395d52e12c394e07433
Jerome Forissier <jerome.forissier@linaro.org>
2017-06-13 14:47:07 +0200

    plat-d02: Use LPAE, increase pager TZSRAM size to 512K and TEE_RAM to 2M
    Fixes a boot error when CFG_WITH_PAGER=y:
    
     INFO:    TEE-CORE:
     INFO:    TEE-CORE: Pager is enabled. Hashes: 512 bytes
     INFO:    TEE-CORE: OP-TEE version: 2.4.0-136-g4ec2358 #25 Tue Jun 13 13:32:21 UTC 2017 arm
     INFO:    TEE-CORE: Shared memory address range: 50500000, 50f00000
     ERROR:   TEE-CORE: Panic at core/lib/libtomcrypt/src/tee_ltc_provider.c:500 <get_mpa_scratch_memory_pool>
    
    Panic occurs because tee_pager_alloc() fails to allocate memory from
    tee_mm_vcore. Fix this by increasing CFG_TEE_RAM_VA_SIZE from 1 to
    2 MiB. This implies to enable LPAE, otherwise the TEE core panics with:
    
     ERROR:   TEE-CORE: Panic 'Unsupported page size in translation table' at core/arch/arm/mm/tee_pager.c:219 <set_alias_area>
    
    Finally, CFG_CORE_TZSRAM_EMUL_SIZE has to be increased to at least
    416 KiB to avoid:
    
       LD      out/arm-plat-d02/core/tee.elf
     /usr/bin/arm-linux-gnueabihf-ld: OP-TEE can't fit init part into available physical memory
    
    We choose 512 KiB because smaller values cause horrible performance.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-d02/conf.mk
+++ core/arch/arm/plat-d02/conf.mk
@@ -4,8 +4,7 @@ CFG_NUM_THREADS ?= 16
 CFG_CRYPTO_WITH_CE ?= y
 CFG_WITH_STACK_CANARIES ?= y
 CFG_WITH_SOFTWARE_PRNG ?= n
-# Override default size of emulated TrustZone protected SRAM, 384 kB
-CFG_CORE_TZSRAM_EMUL_SIZE ?= 393216
+CFG_CORE_TZSRAM_EMUL_SIZE ?= 524288
 # Overrides default in mk/config.mk with 96 kB
 CFG_CORE_HEAP_SIZE ?= 98304
 
@@ -17,6 +16,7 @@ $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 ifneq ($(CFG_WITH_SOFTWARE_PRNG),y)
 $(call force,CFG_HI16XX_RNG,y)
 endif
+$(call force,CFG_WITH_LPAE,y)
 
 # 32-bit flags
 core_arm32-platform-aflags	+= -mfpu=neon
@@ -24,7 +24,6 @@ core_arm32-platform-aflags	+= -mfpu=neon
 ta-targets = ta_arm32
 
 ifeq ($(CFG_ARM64_core),y)
-$(call force,CFG_WITH_LPAE,y)
 ta-targets += ta_arm64
 else
 $(call force,CFG_ARM32_core,y)
