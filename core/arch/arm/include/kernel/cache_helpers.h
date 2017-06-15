b3177a3aa990537a6f04759f7625cc32b716adc0
Jens Wiklander <jens.wiklander@linaro.org>
2017-06-15 15:48:37 +0200

    core: update cache helpers
    Updates AArch64 and ARMv7 cache helpers from lib/aarch32/cache_helpers.S
    and lib/aarch64/cache_helpers.S in ARM-TF,
    https://github.com/ARM-software/arm-trusted-firmware/tree/2bd26faf62411c75111fea4b23c542865383b068
    
    The imported routines only covers the inner cache. Already present ARMv7
    cache routines are replaced by the new equivalent routines. The AArch64
    routines are updated with the resent changes in ARM-TF.
    
    The imported files are modified to better fit into OP-TEE, some
    functions and defines are renamed.
    
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (Hikey AArch{32,64} pager)
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (Juno AArch{32,64} pager)
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/include/kernel/cache_helpers.h
+++ core/arch/arm/include/kernel/cache_helpers.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, STMicroelectronics International N.V.
+ * Copyright (c) 2017, Linaro Limited
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,22 +25,32 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef TZ_SSVCE_H
-#define TZ_SSVCE_H
+#ifndef __KERNEL_CACHE_HELPERS_H
+#define __KERNEL_CACHE_HELPERS_H
 
 #ifndef ASM
-
 #include <types_ext.h>
+#endif
 
-unsigned int secure_get_cpu_id(void);
+/* Data Cache set/way op type defines */
+#define DCACHE_OP_INV		0x0
+#define DCACHE_OP_CLEAN_INV	0x1
+#define DCACHE_OP_CLEAN		0x2
 
-void secure_mmu_datatlbinvall(void);
-void secure_mmu_unifiedtlbinvall(void);
-void secure_mmu_unifiedtlbinvbymva(unsigned long addr);
-void secure_mmu_unifiedtlbinv_curasid(void);
-void secure_mmu_unifiedtlbinv_byasid(unsigned long asid);
+#ifndef ASM
+void dcache_cleaninv_range(void *addr, size_t size);
+void dcache_clean_range(void *addr, size_t size);
+void dcache_inv_range(void *addr, size_t size);
 
-void secure_mmu_disable(void);
+void icache_inv_all(void);
+void icache_inv_range(void *addr, size_t size);
+
+void dcache_op_louis(unsigned long op_type);
+void dcache_op_all(unsigned long op_type);
+
+void dcache_op_level1(unsigned long op_type);
+void dcache_op_level2(unsigned long op_type);
+void dcache_op_level3(unsigned long op_type);
 #endif /*!ASM*/
 
-#endif
+#endif /*__KERNEL_CACHE_HELPERS_H*/
