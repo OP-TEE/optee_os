67a1725b46c1a2b7c3701b4d809f9e0f18920752
Jens Wiklander <jens.wiklander@linaro.org>
2017-07-11 10:50:06 +0200

    core: add matches operation to mobj_paged_ops
    Adds the matches operation to mobj_paged_ops in order to correctly
    classify paged MOBJs.
    
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU)
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/mm/mobj.c
+++ core/arch/arm/mm/mobj.c
@@ -702,8 +702,10 @@ struct mobj *mobj_shm_alloc(paddr_t pa, size_t size)
  */
 
 static void mobj_paged_free(struct mobj *mobj);
+static bool mobj_paged_matches(struct mobj *mobj, enum buf_is_attr attr);
 
 static const struct mobj_ops mobj_paged_ops __rodata_unpaged = {
+	.matches = mobj_paged_matches,
 	.free = mobj_paged_free,
 };
 
@@ -711,6 +713,14 @@ static void mobj_paged_free(struct mobj *mobj)
 {
 	assert(mobj->ops == &mobj_paged_ops);
 	free(mobj);
+}
+
+static bool mobj_paged_matches(struct mobj *mobj __maybe_unused,
+				 enum buf_is_attr attr)
+{
+	assert(mobj->ops == &mobj_paged_ops);
+
+	return attr == CORE_MEM_SEC || attr == CORE_MEM_TEE_RAM;
 }
 
 struct mobj *mobj_paged_alloc(size_t size)
