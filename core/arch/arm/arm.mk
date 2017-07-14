3696447908ad1dda116e5262d0e7dc6051a08883
Jens Wiklander <jens.wiklander@linaro.org>
2017-07-14 14:34:08 +0200

    core: gcc 7 ready
    Remove the armv7 compiler option -mno-apcs-float as it's not supported
    by gcc 7. -apcs-float has be default disabled since at least gcc 4.8.
    
    Reviewed-by: Igor Opaniuk <igor.opaniuk@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/arm.mk
+++ core/arch/arm/arm.mk
@@ -61,7 +61,7 @@ arm32-platform-cppflags += -DARM32=1 -D__ILP32__=1
 platform-cflags-generic ?= -g -ffunction-sections -fdata-sections -pipe
 platform-aflags-generic ?= -g -pipe
 
-arm32-platform-cflags-no-hard-float ?= -mno-apcs-float -mfloat-abi=soft
+arm32-platform-cflags-no-hard-float ?= -mfloat-abi=soft
 arm32-platform-cflags-hard-float ?= -mfloat-abi=hard -funsafe-math-optimizations
 arm32-platform-cflags-generic ?= -mthumb -mthumb-interwork \
 			-fno-short-enums -fno-common -mno-unaligned-access
