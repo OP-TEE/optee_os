c6ac89bc911f82c4a37b63cf005fb7723541c8a3
Peng Fan <peng.fan@nxp.com>
2017-07-18 20:05:36 +0800

    drivers: add snvs srtc support
    Introduce i.MX SNVS SRTC support. The SRTC works with 32.768KHz.
    The SRTC is in SNVS_LP domain. The SNVS_LP is a data storage
    subsystem with enhanced security capabilities. Its purpose is to store
    and protect system data, regardless of the main system power state.
    SNVS_LP is in the always-powered-up domain, which is a separate power
    domain with its own power supply. When the chip power supply domain
    loses power, SNVS_LP continues to operate normally.
    
    Since OP-TEE does not care about calendar time, there is no need
    to update calendar time, we only need to read the counter and
    get out the time.
    
    The plat_prng_add_jitter_entropy is reused from tee_time_arm_cntpct.c.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -12,6 +12,7 @@ srcs-$(CFG_PL022) += pl022_spi.c
 srcs-$(CFG_SUNXI_UART) += sunxi_uart.c
 srcs-$(CFG_8250_UART) += serial8250_uart.c
 srcs-$(CFG_16550_UART) += ns16550.c
+srcs-$(CFG_IMX_SNVS) += imx_snvs.c
 srcs-$(CFG_IMX_UART) += imx_uart.c
 srcs-$(CFG_SPRD_UART) += sprd_uart.c
 srcs-$(CFG_HI16XX_UART) += hi16xx_uart.c
