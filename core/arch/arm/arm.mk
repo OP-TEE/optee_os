0ca352945482dcecaebbd635d94418ed77134fb9
Victor Chong <victor.chong@linaro.org>
2017-07-19 06:37:52 +0100

    arm.mk: export-ta_arm32: Set default value for CROSS_COMPILE
    When building 32-bit optee_os, CROSS_COMPILE32 defaults to CROSS_COMPILE
    if not set, and CROSS_COMPILE defaults to arm-linux-gnueabihf- if not
    set.
    
    When building 32-bit optee_test, CROSS_COMPILE32 also defaults to
    CROSS_COMPILE if not set, but if CROSS_COMPILE is not set, there is no
    default value, causing the native compiler to be used and generating
    errors and confusion.
    
    This patch makes the behavior and expectations consistent across both
    optee_os and optee_test.
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/arm.mk
+++ core/arch/arm/arm.mk
@@ -133,6 +133,7 @@ ta-mk-file-export-vars-ta_arm32 += ta_arm32-platform-cppflags
 ta-mk-file-export-vars-ta_arm32 += ta_arm32-platform-cflags
 ta-mk-file-export-vars-ta_arm32 += ta_arm32-platform-aflags
 
+ta-mk-file-export-add-ta_arm32 += CROSS_COMPILE ?= arm-linux-gnueabihf-_nl_
 ta-mk-file-export-add-ta_arm32 += CROSS_COMPILE32 ?= $$(CROSS_COMPILE)_nl_
 ta-mk-file-export-add-ta_arm32 += CROSS_COMPILE_ta_arm32 ?= $$(CROSS_COMPILE32)_nl_
 endif
