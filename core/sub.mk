2d6d36155f57c036d60831c64eee5994a0a1db2c
Jerome Forissier <jerome.forissier@linaro.org>
2017-07-26 17:41:35 +0200

    core/sub.mk: add missing dependency on the TA key processing script
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/sub.mk
+++ core/sub.mk
@@ -5,7 +5,7 @@ subdirs-y += drivers
 ifeq ($(CFG_WITH_USER_TA)-$(CFG_REE_FS_TA),y-y)
 gensrcs-y += ta_pub_key
 produce-ta_pub_key = ta_pub_key.c
-depends-ta_pub_key = $(TA_SIGN_KEY)
+depends-ta_pub_key = $(TA_SIGN_KEY) scripts/pem_to_pub_c.py
 recipe-ta_pub_key = scripts/pem_to_pub_c.py --prefix ta_pub_key \
 		--key $(TA_SIGN_KEY) --out $(sub-dir-out)/ta_pub_key.c
 cleanfiles += $(sub-dir-out)/ta_pub_key.c
