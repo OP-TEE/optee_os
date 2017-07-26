110da4bc245c43c8a883c1808c00337636c4b0f3
Joseph Chen <chenjh@rock-chips.com>
2017-07-26 20:27:02 +0800

    plat-rockchip: rk322x: add PSCI system suspend
    Support gating clks and power down PLLs.
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-rockchip/common.h
+++ core/arch/arm/plat-rockchip/common.h
@@ -33,4 +33,17 @@
 #define LOCK_ADDR_OFFSET	4
 #define BOOT_ADDR_OFFSET	8
 
+/*
+ * Some register has write-mask bits, it means if you want to set the bits,
+ * you need set the write-mask bits at the same time, the write-mask bits is
+ * in high 16-bits. The following macro definition helps you access register
+ * efficiently.
+ */
+#define REG_MSK_SHIFT		16
+#define WMSK_BIT(nr)		BIT((nr) + REG_MSK_SHIFT)
+#define BIT_WITH_WMSK(nr)	(BIT(nr) | WMSK_BIT(nr))
+#define BITS_WMSK(msk, shift)	SHIFT_U32(msk, (shift) + REG_MSK_SHIFT)
+#define BITS_WITH_WMASK(bits, msk, shift) \
+				(SHIFT_U32(bits, shift) | BITS_WMSK(msk, shift))
+
 #endif
--- core/arch/arm/plat-rockchip/psci_rk322x.c
+++ core/arch/arm/plat-rockchip/psci_rk322x.c
@@ -34,6 +34,7 @@
 #include <kernel/delay.h>
 #include <kernel/generic_boot.h>
 #include <kernel/misc.h>
+#include <kernel/panic.h>
 #include <mm/core_mmu.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
@@ -42,6 +43,178 @@
 #include <stdint.h>
 #include <tee/entry_std.h>
 #include <tee/entry_fast.h>
+
+struct dram_data {
+	uint32_t cru_mode_con;
+	uint32_t cru_clksel0;
+	uint32_t cru_clksel1;
+	uint32_t cru_clksel10;
+	uint32_t cru_clksel21;
+	uint32_t cru_clkgate[CRU_CLKGATE_CON_CNT];
+};
+
+static struct dram_data dram_d;
+
+static const uint32_t clks_gating_table[CRU_CLKGATE_CON_CNT] = {
+	/* gate: 0-3 */
+	0xefb8,
+	0x0ff7,
+	0xfff4,
+	0x887f,
+	/* gate: 4-7 */
+	0x0030,
+	0x00f8,
+	0x07e0,
+	0xc000,
+	/* gate: 8-11 */
+	0xff84,
+	0xb047,
+	0x1ca0,
+	0x57ff,
+	/* gate: 12-15 */
+	0x0000,
+	0x00ff,
+	0x1cc0,
+	0x000f,
+};
+
+static void clks_disable(void)
+{
+	uint32_t i;
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	for (i = 0; i < CRU_CLKGATE_CON_CNT; i++) {
+		dram_d.cru_clkgate[i] = read32(va_base + CRU_CLKGATE_CON(i));
+		write32(BITS_WITH_WMASK(clks_gating_table[i], 0xffff, 0),
+			va_base + CRU_CLKGATE_CON(i));
+	}
+}
+
+static void clks_restore(void)
+{
+	uint32_t i;
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	for (i = 0; i < CRU_CLKGATE_CON_CNT; i++)
+		write32(BITS_WITH_WMASK(dram_d.cru_clkgate[i], 0xffff, 0),
+			va_base + CRU_CLKGATE_CON(i));
+}
+
+static void pll_power_down(uint32_t pll)
+{
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	write32(PLL_SLOW_MODE(pll), va_base + CRU_MODE_CON);
+	write32(PLL_POWER_DOWN, va_base + CRU_PLL_CON1(pll));
+}
+
+static void pll_power_up(uint32_t pll)
+{
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	write32(PLL_POWER_UP, va_base + CRU_PLL_CON1(pll));
+}
+
+static void pll_wait_lock(uint32_t pll)
+{
+	uint32_t loop = 0;
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	while (!(read32(va_base + CRU_PLL_CON1(pll)) & PLL_LOCK) &&
+	       (loop < 500)) {
+		udelay(2);
+		loop++;
+	}
+
+	if (!(read32(va_base + CRU_PLL_CON1(pll)) & PLL_LOCK)) {
+		EMSG("PLL can't lock, index = %" PRIu32, pll);
+		panic();
+	}
+}
+
+/*
+ * Select clock from external 24MHz OSC(slow mode) and power down plls,
+ * then set frequency division of relevant bus to 24MHz.
+ */
+static void plls_power_down(void)
+{
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	dram_d.cru_clksel0 = read32(va_base + CRU_CLKSEL_CON(0));
+	dram_d.cru_clksel1 = read32(va_base + CRU_CLKSEL_CON(1));
+	dram_d.cru_clksel10 = read32(va_base + CRU_CLKSEL_CON(10));
+	dram_d.cru_clksel21 = read32(va_base + CRU_CLKSEL_CON(21));
+	dram_d.cru_mode_con = read32(va_base + CRU_MODE_CON);
+
+	pll_power_down(GPLL_ID);
+	pll_power_down(CPLL_ID);
+	pll_power_down(APLL_ID);
+
+	/* core */
+	write32(BITS_WITH_WMASK(0, 0x1f, 0), va_base + CRU_CLKSEL_CON(0));
+	write32(BITS_WITH_WMASK(0, 0xf, 0) | BITS_WITH_WMASK(0, 0x7, 4),
+		va_base + CRU_CLKSEL_CON(1));
+
+	/* peri aclk, hclk, pclk */
+	write32(BITS_WITH_WMASK(0, 0x1f, 0) | BITS_WITH_WMASK(0, 0x3, 8) |
+		BITS_WITH_WMASK(0, 0x7, 12),
+		va_base + CRU_CLKSEL_CON(10));
+
+	/* pdbus */
+	write32(BITS_WITH_WMASK(0, 0x1f, 8), va_base + CRU_CLKSEL_CON(0));
+	write32(BITS_WITH_WMASK(0, 0x3, 8) | BITS_WITH_WMASK(0, 0x7, 12),
+		va_base + CRU_CLKSEL_CON(1));
+
+	/* hdmi cec 32k */
+	write32(BITS_WITH_WMASK(732, 0x3fff, 0) | BITS_WITH_WMASK(2, 0x3, 14),
+		va_base + CRU_CLKSEL_CON(21));
+}
+
+static void plls_restore(void)
+{
+	vaddr_t va_base = (vaddr_t)phys_to_virt_io(CRU_BASE);
+
+	/* power up plls */
+	pll_power_up(APLL_ID);
+	pll_power_up(GPLL_ID);
+	pll_power_up(CPLL_ID);
+
+	udelay(200);
+
+	/* wait lock*/
+	pll_wait_lock(APLL_ID);
+	pll_wait_lock(GPLL_ID);
+	pll_wait_lock(CPLL_ID);
+
+	/* hdmi cec 32k */
+	write32(dram_d.cru_clksel21 | BITS_WMSK(0x3fff, 0) | BITS_WMSK(0x3, 14),
+		va_base + CRU_CLKSEL_CON(21));
+
+	/* pdbus */
+	write32(dram_d.cru_clksel0 | BITS_WMSK(0x1f, 8),
+		va_base + CRU_CLKSEL_CON(0));
+	write32(dram_d.cru_clksel1 | BITS_WMSK(0x3, 8) | BITS_WMSK(0x7, 12),
+		va_base + CRU_CLKSEL_CON(1));
+
+	/* peri aclk, hclk, pclk */
+	write32(dram_d.cru_clksel10 | BITS_WMSK(0x1f, 0) | BITS_WMSK(0x3, 8) |
+		BITS_WMSK(0x7, 12),
+		va_base + CRU_CLKSEL_CON(10));
+
+	/* core */
+	write32(dram_d.cru_clksel0 | BITS_WMSK(0x1f, 0),
+		va_base + CRU_CLKSEL_CON(0));
+	write32(dram_d.cru_clksel1 | BITS_WMSK(0xf, 0) | BITS_WMSK(0x7, 4),
+		va_base + CRU_CLKSEL_CON(1));
+
+	/* resume plls mode */
+	write32(dram_d.cru_mode_con | BITS_WMSK(0x1, PLL_MODE_BIT(APLL_ID)),
+		va_base + CRU_MODE_CON);
+	write32(dram_d.cru_mode_con | BITS_WMSK(0x1, PLL_MODE_BIT(CPLL_ID)),
+		va_base + CRU_MODE_CON);
+	write32(dram_d.cru_mode_con | BITS_WMSK(0x1, PLL_MODE_BIT(GPLL_ID)),
+		va_base + CRU_MODE_CON);
+}
 
 static bool wait_core_wfe_i(uint32_t core)
 {
@@ -79,6 +252,7 @@ int psci_features(uint32_t psci_fid)
 	case PSCI_VERSION:
 	case PSCI_CPU_ON:
 	case PSCI_CPU_OFF:
+	case PSCI_SYSTEM_SUSPEND:
 	case PSCI_SYSTEM_RESET:
 		return PSCI_RET_SUCCESS;
 	default:
@@ -183,6 +357,24 @@ void psci_system_reset(void)
 	/* Global second reset */
 	write32(CRU_SNDRST_VAL, va_base + CRU_SNDRST_VAL_BASE);
 	dsb();
+}
+
+int psci_system_suspend(uintptr_t entry __unused,
+			uint32_t context_id __unused)
+{
+	DMSG("system suspend");
+
+	clks_disable();
+	plls_power_down();
+
+	cache_op_inner(DCACHE_CLEAN_INV, NULL, 0);
+
+	wfi();
+
+	plls_restore();
+	clks_restore();
+
+	return PSCI_RET_SUCCESS;
 }
 
 /* When SMP bootup, we release cores one by one */
