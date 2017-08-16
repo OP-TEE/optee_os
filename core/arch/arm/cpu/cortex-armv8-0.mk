f460af340b0996079d670993509fdeff384bbb53
Etienne Carriere <etienne.carriere@linaro.org>
2017-08-16 11:17:38 +0200

    core: introduce CFG_ENABLE_SCTLR_RR for ARMv7 cores
    CFG_ENABLE_SCTLR_RR to enable SCTLR[RR] from the generic boot.
    SCTLR[RR] enables the round robin replacement strategy for the
    several caches (icache, dcache, btac, tlb).
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/cpu/cortex-armv8-0.mk
+++ core/arch/arm/cpu/cortex-armv8-0.mk
@@ -1,5 +1,6 @@
 $(call force,CFG_HWSUPP_MEM_PERM_WXN,y)
 $(call force,CFG_HWSUPP_MEM_PERM_PXN,y)
+$(call force,CFG_ENABLE_SCTLR_RR,n)
 # cortex-a53 and cortex-a57 complies on arm32 architectures
 arm32-platform-cpuarch 	:= cortex-a53
 arm32-platform-cflags 	+= -mcpu=$(arm32-platform-cpuarch)
