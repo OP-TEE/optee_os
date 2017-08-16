2e4e94bfad833beedd3804aec92b5b9feba63928
Etienne Carriere <etienne.carriere@linaro.org>
2017-08-16 11:19:30 +0200

    core: default enable program flow prediction on ARMv7 cores
    Introduce CFG_ENABLE_SCTLR_Z to enable SCTLR[Z] from the generic boot.
    SCTLR[Z] enables program flow prediction support from the core.
    
    Early branch prediction may be unsafe against uncontrolled memory
    prefetches that could hit some hard memory access control firewalls.
    It is usually safer to enable after the mmu is enabled.
    
    CFG_ENABLE_SCTLR_Z allows to use vanilla op-tee on development board
    to exercises benchmark and performance tests over the op-tee system.
    
    This feature is no expected on ARMv8 architectures and some recent
    ARMv7 architectures since program flow prediction enable is tight to
    mmu enable.
    
    Suggested-by: Jangseop Shin <jsshin@sor.snu.ac.kr>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/cpu/cortex-armv8-0.mk
+++ core/arch/arm/cpu/cortex-armv8-0.mk
@@ -1,6 +1,7 @@
 $(call force,CFG_HWSUPP_MEM_PERM_WXN,y)
 $(call force,CFG_HWSUPP_MEM_PERM_PXN,y)
 $(call force,CFG_ENABLE_SCTLR_RR,n)
+$(call force,CFG_ENABLE_SCTLR_Z,n)
 # cortex-a53 and cortex-a57 complies on arm32 architectures
 arm32-platform-cpuarch 	:= cortex-a53
 arm32-platform-cflags 	+= -mcpu=$(arm32-platform-cpuarch)
