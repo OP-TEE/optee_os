382e20efc3e3ea3ca4507ae609c96daaf057d538
Peng Fan <peng.fan@nxp.com>
2017-08-26 12:52:27 +0800

    core: imx: simplify code
    Wrap memory registration using macros to make it easy to add new soc/arch
    support.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-imx/imx7.c
+++ core/arch/arm/plat-imx/imx7.c
@@ -47,16 +47,6 @@
 #include <tee/entry_fast.h>
 #include <util.h>
 
-register_phys_mem(MEM_AREA_IO_SEC, SRC_BASE, CORE_MMU_DEVICE_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, IOMUXC_BASE, CORE_MMU_DEVICE_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, CCM_BASE, CORE_MMU_DEVICE_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, ANATOP_BASE, CORE_MMU_DEVICE_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, GPC_BASE, CORE_MMU_DEVICE_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, DDRC_BASE, CORE_MMU_DEVICE_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, AIPS1_BASE, AIPS1_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, AIPS2_BASE, AIPS2_SIZE);
-register_phys_mem(MEM_AREA_IO_SEC, AIPS3_BASE, AIPS3_SIZE);
-
 void plat_cpu_reset_late(void)
 {
 	uintptr_t addr;
