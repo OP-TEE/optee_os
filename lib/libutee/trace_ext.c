OP-TEE/optee_os

commit 6246cc9d957c1bfca0989b970e98ded93b324419

libutee: puts(): add trailing newline
puts() should print the supplied string and a trailing newline. Fix it
accordingly.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/trace_ext.c
@@ -73,8 +73,10 @@ int printf(const char *fmt, ...)
 
 int puts(const char *str)
 {
-	if (trace_get_level() >= TRACE_PRINTF_LEVEL)
+	if (trace_get_level() >= TRACE_PRINTF_LEVEL) {
 		trace_ext_puts(str);
+		trace_ext_puts("\n");
+	}
 	return 1;
 }
 

commit c738b7d17aa9ed7c246fa5426c008f25c690a521

libutee: printf() fix: remove newline added by mistake
puts() was recently modified [1] to always add a trailing newline (\n).
This change has broken printf() which uses puts() internally.

Fix the issue by calling trace_ext_puts() instead.

Fixes: 6246cc9d957c ("libutee: puts(): add trailing newline")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/trace_ext.c
@@ -66,7 +66,7 @@ int printf(const char *fmt, ...)
 	if (s < 0)
 		return s;
 
-	puts(to_format);
+	trace_ext_puts(to_format);
 
 	return s;
 }