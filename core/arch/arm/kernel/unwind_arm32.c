OP-TEE/optee_os

commit 6693786dda1a61cb94d126da00a57abfe4612da0

core: make panic call stack consistent with abort call stack
The call stack is formatted differently when the TEE code panics vs.
when it receives an abort exception. The unfortunate consequence is,
the symbolize.py helper script cannot be used to troubleshoot panics.

Fix the issue by introducing common functions to format the call stack:
print_call_stack_arm32() and print_call_stack_arm64(), and by using
them in the panic and abort paths.

Suggested-by: Zeng Tao <prime.zeng@hisilicon.com>
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/unwind_arm32.c
@@ -356,6 +356,20 @@ bool unwind_stack_arm32(struct unwind_state_arm32 *state, uaddr_t exidx,
 	return !finished;
 }
 
+#if defined(CFG_UNWIND) && (TRACE_LEVEL > 0)
+
+void print_stack_arm32(int level, struct unwind_state_arm32 *state, uaddr_t exidx,
+		       size_t exidx_sz)
+{
+	trace_printf_helper_raw(level, true, "Call stack:");
+	do {
+		trace_printf_helper_raw(level, true, " 0x%08x" PRIx32,
+					state->registers[PC]);
+	} while (unwind_stack_arm32(state, exidx, exidx_sz));
+}
+
+#endif
+
 #if defined(CFG_UNWIND) && defined(ARM32) && (TRACE_LEVEL > 0)
 
 void print_kernel_stack(int level)
@@ -373,24 +387,7 @@ void print_kernel_stack(int level)
 	state.registers[LR] = read_lr();
 	state.registers[PC] = (uint32_t)print_kernel_stack;
 
-	do {
-		switch (level) {
-		case TRACE_FLOW:
-			FMSG_RAW("pc  0x%08" PRIx32, state.registers[PC]);
-			break;
-		case TRACE_DEBUG:
-			DMSG_RAW("pc  0x%08" PRIx32, state.registers[PC]);
-			break;
-		case TRACE_INFO:
-			IMSG_RAW("pc  0x%08" PRIx32, state.registers[PC]);
-			break;
-		case TRACE_ERROR:
-			EMSG_RAW("pc  0x%08" PRIx32, state.registers[PC]);
-			break;
-		default:
-			break;
-		}
-	} while (unwind_stack_arm32(&state, exidx, exidx_sz));
+	print_stack_arm32(level, &state, exidx, exidx_sz);
 }
 
 #endif

core/arch/arm/include/kernel/unwind.h
@@ -72,12 +72,36 @@ bool unwind_stack_arm64(struct unwind_state_arm64 *state, uaddr_t stack,
 #endif /*ARM64*/
 
 #if defined(CFG_UNWIND) && (TRACE_LEVEL > 0)
+
+#ifdef ARM64
+void print_stack_arm64(int level, struct unwind_state_arm64 *state, uaddr_t exidx,
+		       size_t exidx_sz);
+#endif
+void print_stack_arm32(int level, struct unwind_state_arm32 *state, uaddr_t exidx,
+		       size_t exidx_sz);
 void print_kernel_stack(int level);
-#else
-static inline void print_kernel_stack(int level __unused)
+
+#else /* defined(CFG_UNWIND) && (TRACE_LEVEL > 0) */
+
+#ifdef ARM64
+static inline void print_stack_arm64(int level __unused,
+				     struct unwind_state_arm64 *state __unused,
+				     uaddr_t exidx __unused,
+				     size_t exidx_sz __unused)
 {
 }
 #endif
+static inline void print_stack_arm32(int level __unused,
+				     struct unwind_state_arm32 *state __unused,
+				     uaddr_t exidx __unused,
+				     size_t exidx_sz __unused)
+{
+}
+static inline void print_kernel_stack(int level __unused)
+{
+}
+
+#endif /* defined(CFG_UNWIND) && (TRACE_LEVEL > 0) */
 
 #endif /*ASM*/
 

core/arch/arm/kernel/abort.c
@@ -115,10 +115,7 @@ static void __print_stack_unwind_arm32(struct abort_info *ai)
 	state.registers[14] = lr;
 	state.registers[15] = ai->pc;
 
-	EMSG_RAW("Call stack:");
-	do {
-		EMSG_RAW(" 0x%08x", state.registers[15]);
-	} while (exidx && unwind_stack_arm32(&state, exidx, exidx_sz));
+	print_stack_arm32(TRACE_ERROR, &state, exidx, exidx_sz);
 }
 #else /* ARM32 */
 
@@ -151,10 +148,7 @@ static void __print_stack_unwind_arm32(struct abort_info *ai __unused)
 	state.registers[14] = ai->regs->x14;
 	state.registers[15] = ai->pc;
 
-	EMSG_RAW("Call stack:");
-	do {
-		EMSG_RAW(" 0x%08x", state.registers[15]);
-	} while (exidx && unwind_stack_arm32(&state, exidx, exidx_sz));
+	print_stack_arm32(TRACE_ERROR, &state, exidx, exidx_sz);
 }
 #endif /* ARM32 */
 #ifdef ARM64
@@ -186,10 +180,7 @@ static void __print_stack_unwind_arm64(struct abort_info *ai)
 	state.pc = ai->regs->elr;
 	state.fp = ai->regs->x29;
 
-	EMSG_RAW("Call stack:");
-	do {
-		EMSG_RAW(" 0x%016" PRIx64, state.pc);
-	} while (stack && unwind_stack_arm64(&state, stack, stack_size));
+	print_stack_arm64(TRACE_ERROR, &state, stack, stack_size);
 }
 #else
 static void __print_stack_unwind_arm64(struct abort_info *ai __unused)

core/arch/arm/kernel/unwind_arm64.c
@@ -54,6 +54,16 @@ bool unwind_stack_arm64(struct unwind_state_arm64 *frame, uaddr_t stack,
 
 #if defined(CFG_UNWIND) && (TRACE_LEVEL > 0)
 
+void print_stack_arm64(int level, struct unwind_state_arm64 *state,
+		       uaddr_t stack, size_t stack_size)
+{
+	trace_printf_helper_raw(level, true, "Call stack:");
+	do {
+		trace_printf_helper_raw(level, true, " 0x%016" PRIx64,
+					state->pc);
+	} while (stack && unwind_stack_arm64(state, stack, stack_size));
+}
+
 void print_kernel_stack(int level)
 {
 	struct unwind_state_arm64 state;
@@ -64,24 +74,7 @@ void print_kernel_stack(int level)
 	state.pc = read_pc();
 	state.fp = read_fp();
 
-	do {
-		switch (level) {
-		case TRACE_FLOW:
-			FMSG_RAW("pc  0x%016" PRIx64, state.pc);
-			break;
-		case TRACE_DEBUG:
-			DMSG_RAW("pc  0x%016" PRIx64, state.pc);
-			break;
-		case TRACE_INFO:
-			IMSG_RAW("pc  0x%016" PRIx64, state.pc);
-			break;
-		case TRACE_ERROR:
-			EMSG_RAW("pc  0x%016" PRIx64, state.pc);
-			break;
-		default:
-			break;
-		}
-	} while (unwind_stack_arm64(&state, stack, stack_size));
+	print_stack_arm64(level, &state, stack, stack_size);
 }
 
 #endif

commit 776550e0441d3399c9c4d340532b6abfe1b16824

core: arm32: remove extra 'x' in core stack dump
Commit 6693786dda1a ("core: make panic call stack consistent with abort
call stack") has mistakenly introduced an extra 'x' in the TEE core
stack dumps. Remove it.

Fixes: 6693786dda1a ("core: make panic call stack consistent with abort call stack")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/kernel/unwind_arm32.c
@@ -363,7 +363,7 @@ void print_stack_arm32(int level, struct unwind_state_arm32 *state, uaddr_t exid
 {
 	trace_printf_helper_raw(level, true, "Call stack:");
 	do {
-		trace_printf_helper_raw(level, true, " 0x%08x" PRIx32,
+		trace_printf_helper_raw(level, true, " 0x%08" PRIx32,
 					state->registers[PC]);
 	} while (unwind_stack_arm32(state, exidx, exidx_sz));
 }