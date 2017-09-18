77a99ce12f9dcbdd6ccaa8bf1b78099c926942a8
Jens Wiklander <jens.wiklander@linaro.org>
2017-09-18 13:43:48 +0200

    core: generic boot: update fdt with psci
    If configured with device tree and PSCI update the FDT with PSCI nodes
    and other information needed by the kernel to start up the secondary
    cores.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/kernel/generic_boot.c
+++ core/arch/arm/kernel/generic_boot.c
@@ -43,6 +43,7 @@
 #include <mm/tee_mm.h>
 #include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <sm/psci.h>
 #include <sm/tee_mon.h>
 #include <stdio.h>
 #include <tee/tee_cryp_provider.h>
@@ -410,6 +411,104 @@ static int add_optee_dt_node(void *fdt)
 	return 0;
 }
 
+#ifdef CFG_PSCI_ARM32
+static int append_psci_compatible(void *fdt, int offs, const char *str)
+{
+	return fdt_appendprop(fdt, offs, "compatible", str, strlen(str) + 1);
+}
+
+static int dt_add_psci_node(void *fdt)
+{
+	int offs;
+
+	if (fdt_path_offset(fdt, "/psci") >= 0) {
+		DMSG("PSCI Device Tree node already exists!\n");
+		return 0;
+	}
+
+	offs = fdt_path_offset(fdt, "/");
+	if (offs < 0)
+		return -1;
+	offs = fdt_add_subnode(fdt, offs, "psci");
+	if (offs < 0)
+		return -1;
+	if (append_psci_compatible(fdt, offs, "arm,psci-1.0"))
+		return -1;
+	if (append_psci_compatible(fdt, offs, "arm,psci-0.2"))
+		return -1;
+	if (append_psci_compatible(fdt, offs, "arm,psci"))
+		return -1;
+	if (fdt_setprop_string(fdt, offs, "method", "smc"))
+		return -1;
+	if (fdt_setprop_u32(fdt, offs, "cpu_suspend", PSCI_CPU_SUSPEND))
+		return -1;
+	if (fdt_setprop_u32(fdt, offs, "cpu_off", PSCI_CPU_OFF))
+		return -1;
+	if (fdt_setprop_u32(fdt, offs, "cpu_on", PSCI_CPU_ON))
+		return -1;
+	if (fdt_setprop_u32(fdt, offs, "sys_poweroff", PSCI_SYSTEM_OFF))
+		return -1;
+	if (fdt_setprop_u32(fdt, offs, "sys_reset", PSCI_SYSTEM_RESET))
+		return -1;
+	return 0;
+}
+
+static int check_node_compat_prefix(void *fdt, int offs, const char *prefix)
+{
+	const size_t prefix_len = strlen(prefix);
+	size_t l;
+	int plen;
+	const char *prop;
+
+	prop = fdt_getprop(fdt, offs, "compatible", &plen);
+	if (!prop)
+		return -1;
+
+	while (plen > 0) {
+		if (memcmp(prop, prefix, prefix_len) == 0)
+			return 0; /* match */
+
+		l = strlen(prop) + 1;
+		prop += l;
+		plen -= l;
+	}
+
+	return -1;
+}
+
+static int dt_add_psci_cpu_enable_methods(void *fdt)
+{
+	int offs = 0;
+
+	while (1) {
+		offs = fdt_next_node(fdt, offs, NULL);
+		if (offs < 0)
+			break;
+		if (fdt_getprop(fdt, offs, "enable-method", NULL))
+			continue; /* already set */
+		if (check_node_compat_prefix(fdt, offs, "arm,cortex-a"))
+			continue; /* no compatible */
+		if (fdt_setprop_string(fdt, offs, "enable-method", "psci"))
+			return -1;
+		/* Need to restart scanning as offsets may have changed */
+		offs = 0;
+	}
+	return 0;
+}
+
+static int config_psci(void *fdt)
+{
+	if (dt_add_psci_node(fdt))
+		return -1;
+	return dt_add_psci_cpu_enable_methods(fdt);
+}
+#else
+static int config_psci(void *fdt __unused)
+{
+	return 0;
+}
+#endif /*CFG_PSCI_ARM32*/
+
 static void set_dt_val(void *data, uint32_t cell_size, uint64_t val)
 {
 	if (cell_size == 1) {
@@ -613,6 +712,9 @@ static void init_fdt(unsigned long phys_fdt)
 
 	if (add_optee_dt_node(fdt))
 		panic("Failed to add OP-TEE Device Tree node");
+
+	if (config_psci(fdt))
+		panic("Failed to config PSCI");
 
 	if (config_nsmem(fdt))
 		panic("Failed to config non-secure memory");
