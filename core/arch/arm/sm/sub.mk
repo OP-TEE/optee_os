18b611d71817524e0a8f5d388d3f98597a9c8506
Andrew F. Davis <afd@ti.com>
2017-09-19 10:45:26 -0500

    core: arm: psci: Split PM config from PSCI
    Not all platforms need PM when implementing PSCI, also some
    platforms may need PM but do not implement PSCI. As PSCI has
    no direct dependence on PM, split these config options.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/sm/sub.mk
+++ core/arch/arm/sm/sub.mk
@@ -1,3 +1,4 @@
 srcs-y += sm_a32.S
 srcs-y += sm.c
-srcs-$(CFG_PSCI_ARM32) += std_smc.c psci.c pm.c psci-helper.S pm_a32.S
+srcs-$(CFG_PM_ARM32) += pm.c pm_a32.S
+srcs-$(CFG_PSCI_ARM32) += std_smc.c psci.c psci-helper.S
