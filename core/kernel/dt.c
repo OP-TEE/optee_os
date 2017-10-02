34deb103dd636294cfbfbe3adbd5706db9a7ef2e
Peng Fan <peng.fan@nxp.com>
2017-10-02 20:25:59 +0800

    core: dt: fix getting address and size
    According to ePAPR spec.
    
    "
    The #address-cells and #size-cells properties may be used in any device
    node that has children in the device tree hierarchy and describes how
    child device nodes should be addressed. The #address-cells property
    defines the number of <u32> cells used to encode the address field in
    a child node’s reg property. The #size-cells property defines the number
    of <u32> cells used to encode the size field in a child node’s reg
    property.
    
    The #address-cells and #size-cells properties are not inherited from
    ancestors in the device tree. They shall be explicitly defined.
    
    An ePAPR-compliant boot program shall supply #address-cells and #size-cells
    on all nodes that have children.
    
    If missing, a client program should assume a default value of 2 for
    
    An ePAPR-compliant boot program shall supply #address-cells and #size-cells
    on all nodes that have children.
    "
    
    So need to use the parent's address-cells and size-cells property.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/kernel/dt.c
+++ core/kernel/dt.c
@@ -146,12 +146,17 @@ paddr_t _fdt_reg_base_address(const void *fdt, int offs)
 	const void *reg;
 	int ncells;
 	int len;
+	int parent;
+
+	parent = fdt_parent_offset(fdt, offs);
+	if (parent < 0)
+		return (paddr_t)-1;
 
 	reg = fdt_getprop(fdt, offs, "reg", &len);
 	if (!reg)
 		return (paddr_t)-1;
 
-	ncells = fdt_address_cells(fdt, offs);
+	ncells = fdt_address_cells(fdt, parent);
 	if (ncells < 0)
 		return (paddr_t)-1;
 
@@ -164,18 +169,23 @@ ssize_t _fdt_reg_size(const void *fdt, int offs)
 	uint32_t sz;
 	int n;
 	int len;
+	int parent;
+
+	parent = fdt_parent_offset(fdt, offs);
+	if (parent < 0)
+		return (paddr_t)-1;
 
 	reg = (const uint32_t *)fdt_getprop(fdt, offs, "reg", &len);
 	if (!reg)
 		return -1;
 
-	n = fdt_address_cells(fdt, offs);
+	n = fdt_address_cells(fdt, parent);
 	if (n < 1 || n > 2)
 		return -1;
 
 	reg += n;
 
-	n = fdt_size_cells(fdt, offs);
+	n = fdt_size_cells(fdt, parent);
 	if (n < 1 || n > 2)
 		return -1;
 
