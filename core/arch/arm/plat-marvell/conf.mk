a0825ef521f36a2f01f247258428705f5cc72108
Kevin Peng <kevinp@marvell.com>
2017-10-13 14:22:05 +0800

    Add Marvell platform with initial support for ARMADA A7K & A8K
    Only tested 64-bit mode with default configurations:
    
    1. build command
        make PLATFORM=marvell-armada7080 CFG_ARM64_core=y
    2. Passed xtest
    
    Signed-off-by: Kevin Peng <kevinp@marvell.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ core/arch/arm/plat-marvell/conf.mk
@@ -0,0 +1,36 @@
+PLATFORM_FLAVOR ?= armada7k8k
+
+# 32-bit flags
+core_arm32-platform-aflags	+= -mfpu=neon
+
+ifeq ($(PLATFORM_FLAVOR),armada7k8k)
+include core/arch/arm/cpu/cortex-armv8-0.mk
+platform-debugger-arm := 1
+endif
+
+ifeq ($(platform-debugger-arm),1)
+# ARM debugger needs this
+platform-cflags-debug-info = -gdwarf-2
+platform-aflags-debug-info = -gdwarf-2
+endif
+
+$(call force,CFG_WITH_ARM_TRUSTED_FW,y)
+
+$(call force,CFG_GENERIC_BOOT,y)
+$(call force,CFG_GIC,y)
+$(call force,CFG_8250_UART,y)
+$(call force,CFG_PM_STUBS,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_CORE_CLUSTER_SHIFT,1)
+
+ta-targets = ta_arm32
+
+ifeq ($(CFG_ARM64_core),y)
+$(call force,CFG_WITH_LPAE,y)
+ta-targets += ta_arm64
+else
+$(call force,CFG_ARM32_core,y)
+endif
+
+CFG_WITH_STACK_CANARIES ?= y
+CFG_WITH_STATS ?= y
