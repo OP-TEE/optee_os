a3bb2887f166083c11ad85faf5e802787b2a285d
Jerome Forissier <jerome.forissier@linaro.org>
2017-10-18 10:34:05 +0200

    Update minor revision to 6 for release tag 2.6.0-rc1
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- mk/config.mk
+++ mk/config.mk
@@ -99,7 +99,7 @@ TEE_IMPL_VERSION ?= $(shell git describe --always --dirty=-dev 2>/dev/null || ec
 # with limited depth not including any tag, so there is really no guarantee
 # that TEE_IMPL_VERSION contains the major and minor revision numbers.
 CFG_OPTEE_REVISION_MAJOR ?= 2
-CFG_OPTEE_REVISION_MINOR ?= 5
+CFG_OPTEE_REVISION_MINOR ?= 6
 
 # Trusted OS implementation manufacturer name
 CFG_TEE_MANUFACTURER ?= LINARO
