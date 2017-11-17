7d9fabe4f95d290ceab58b29c958dee849981a83
Jens Wiklander <jens.wiklander@linaro.org>
2017-11-17 09:19:23 +0100

    [review] LTC: add GHASH acceleration
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/encauth/gcm/sub.mk
+++ core/lib/libtomcrypt/src/encauth/gcm/sub.mk
@@ -1,14 +1,14 @@
 srcs-y += gcm_add_aad.c
 srcs-y += gcm_add_iv.c
 srcs-y += gcm_done.c
+srcs-y += gcm_gf_mult.c
+srcs-y += gcm_init.c
+srcs-y += gcm_memory.c
 ifeq ($(CFG_CRYPTO_WITH_CE),y)
 srcs-y += gcm_mult_h_arm_ce.c
 else
-srcs-y += gcm_gf_mult.c
-endif
-srcs-y += gcm_init.c
-srcs-y += gcm_memory.c
 srcs-y += gcm_mult_h.c
+endif
 srcs-y += gcm_process.c
 srcs-y += gcm_reset.c
 # srcs-y += gcm_test.c
