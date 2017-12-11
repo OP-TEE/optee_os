4d79574a7cbec72f79825f68ab5d1f3c4db9e91a
Peng Fan <peng.fan@nxp.com>
2017-12-11 13:18:36 +0800

    drivers: imx_wdog: update wdog support for mx7ulp
    Update watchdog for imx7ulp SoC support.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/include/drivers/imx_wdog.h
+++ core/include/drivers/imx_wdog.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2019 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -39,6 +39,25 @@
 #define WDT_WSR		0x02
 #define WDT_SEQ1	0x5555
 #define WDT_SEQ2	0xAAAA
+
+/* 7ULP */
+#define WDOG_CNT	0x4
+#define WDOG_TOVAL	0x8
+
+#define REFRESH_SEQ0	0xA602
+#define REFRESH_SEQ1	0xB480
+#define REFRESH		((REFRESH_SEQ1 << 16) | REFRESH_SEQ0)
+
+#define UNLOCK_SEQ0	0xC520
+#define UNLOCK_SEQ1	0xD928
+#define UNLOCK		((UNLOCK_SEQ1 << 16) | UNLOCK_SEQ0)
+
+#define WDOG_CS			0x0
+#define WDOG_CS_CMD32EN		BIT(13)
+#define WDOG_CS_ULK		BIT(11)
+#define WDOG_CS_RCS		BIT(10)
+#define WDOG_CS_EN		BIT(7)
+#define WDOG_CS_UPDATE		BIT(5)
 
 /* Exposed for psci reset */
 void imx_wdog_restart(void);
