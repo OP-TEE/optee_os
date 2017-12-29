92eecd21554126bcac4d928530393222b7c600bb
Peng Fan <peng.fan@nxp.com>
2017-12-29 11:14:26 +0800

    imx: cleanup conf.mk
    Removed duplicated config and sort config with SoC and Board.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -23,7 +23,6 @@ $(call force,CFG_MX6DL,y)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx6s-flavorlist)))
 $(call force,CFG_MX6S,y)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx6sx-flavorlist)))
-$(call force,CFG_MX6,y)
 $(call force,CFG_MX6SX,y)
 $(call force,CFG_IMX_UART,y)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx7-flavorlist)))
@@ -32,33 +31,23 @@ else
 $(error Unsupported PLATFORM_FLAVOR "$(PLATFORM_FLAVOR)")
 endif
 
-ifneq (,$(filter $(PLATFORM_FLAVOR),mx7dsabresd))
-CFG_DDR_SIZE ?= 0x40000000
-CFG_DT ?= y
-CFG_NS_ENTRY_ADDR ?= 0x80800000
-CFG_PSCI_ARM32 ?= y
-CFG_TEE_CORE_NB_CORE ?= 2
-endif
-
-# Common i.MX6 config
+# Common i.MX config
 $(call force,CFG_GENERIC_BOOT,y)
 $(call force,CFG_GIC,y)
 $(call force,CFG_IMX_UART,y)
 $(call force,CFG_PM_STUBS,y)
 $(call force,CFG_WITH_SOFTWARE_PRNG,y)
 
+CFG_SECURE_TIME_SOURCE_REE ?= y
 CFG_CRYPTO_SIZE_OPTIMIZATION ?= n
 CFG_WITH_STACK_CANARIES ?= y
-
 
 # i.MX6UL/ULL specific config
 ifneq (,$(filter y, $(CFG_MX6UL) $(CFG_MX6ULL)))
 include core/arch/arm/cpu/cortex-a7.mk
 
 $(call force,CFG_MX6,y)
-$(call force,CFG_SECURE_TIME_SOURCE_REE,y)
 endif
-
 
 # i.MX6 Solo/DualLite/Dual/Quad specific config
 ifeq ($(filter y, $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) $(CFG_MX6S) \
@@ -68,19 +57,27 @@ include core/arch/arm/cpu/cortex-a9.mk
 $(call force,CFG_MX6,y)
 $(call force,CFG_PL310,y)
 $(call force,CFG_PL310_LOCKED,y)
-$(call force,CFG_SECURE_TIME_SOURCE_REE,y)
 
 CFG_BOOT_SYNC_CPU ?= n
 CFG_BOOT_SECONDARY_REQUEST ?= y
 CFG_ENABLE_SCTLR_RR ?= y
 endif
 
+# i.MX 7 specific config and Boards
 ifeq ($(filter y, $(CFG_MX7)), y)
 include core/arch/arm/cpu/cortex-a7.mk
 
-$(call force,CFG_SECURE_TIME_SOURCE_REE,y)
+CFG_DT ?= y
 CFG_BOOT_SECONDARY_REQUEST ?= y
+CFG_NS_ENTRY_ADDR ?= 0x80800000
+CFG_PSCI_ARM32 ?= y
+CFG_TEE_CORE_NB_CORE ?= 2
 endif
+
+ifneq (,$(filter $(PLATFORM_FLAVOR),mx7dsabresd))
+CFG_DDR_SIZE ?= 0x40000000
+endif
+# End
 
 # For i.MX6SX and Boards
 ifeq ($(filter y, $(CFG_MX6SX)), y)
