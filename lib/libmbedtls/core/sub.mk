d2e5e90e5e4f73d7284b312e47726d1f8379c16e
Edison Ai <edison.ai@arm.com>
2018-01-09 16:37:54 +0800

    libmbedtls: support symmetrical ciphers
    Adds support for symmetrical ciphers. The XTS mode is not supported in
    mbedTLS and will be dealt with later.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Edison Ai <edison.ai@arm.com>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libmbedtls/core/sub.mk
+++ lib/libmbedtls/core/sub.mk
@@ -5,3 +5,15 @@ srcs-y += tomcrypt.c
 srcs-$(call cfg-one-enabled, CFG_CRYPTO_MD5 CFG_CRYPTO_SHA1 CFG_CRYPTO_SHA224 \
 			     CFG_CRYPTO_SHA256 CFG_CRYPTO_SHA384 \
 			     CFG_CRYPTO_SHA512) += hash.c
+
+ifeq ($(CFG_CRYPTO_AES),y)
+srcs-$(CFG_CRYPTO_ECB) += aes_ecb.c
+srcs-$(CFG_CRYPTO_CBC) += aes_cbc.c
+srcs-$(CFG_CRYPTO_CTR) += aes_ctr.c
+endif
+ifeq ($(CFG_CRYPTO_DES),y)
+srcs-$(CFG_CRYPTO_ECB) += des_ecb.c
+srcs-$(CFG_CRYPTO_ECB) += des3_ecb.c
+srcs-$(CFG_CRYPTO_CBC) += des_cbc.c
+srcs-$(CFG_CRYPTO_CBC) += des3_cbc.c
+endif
