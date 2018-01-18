OP-TEE/optee_os

commit 2d9ed57b6bd854841e099f29a7eb6f6cffe4b531

Define register_sdp_mem() only when CFG_SECURE_DATA_PATH is defined
Suggested-by: Jerome Forissier <jerome.forissier@linaro.org>
Suggested-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Victor Chong <victor.chong@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/include/mm/core_mmu.h
@@ -234,9 +234,13 @@ struct core_mmu_phys_mem {
 		__register_memory1_ul(#addr, (type), (addr), (size), \
 				   phys_mem_map_section, __COUNTER__)
 
+#ifdef CFG_SECURE_DATA_PATH
 #define register_sdp_mem(addr, size) \
 		__register_memory1(#addr, MEM_AREA_SDP_MEM, (addr), (size), \
 				   phys_sdp_mem_section, __COUNTER__)
+#else
+#define register_sdp_mem(addr, size)
+#endif
 
 #define register_nsec_ddr(addr, size) \
 		__register_memory1(#addr, MEM_AREA_RAM_NSEC, (addr), (size), \

commit bfdc879cf71680ff18a77f884a33c6c20671b86c

Fix compiler warning with register_sdp_mem()
Fixes the following warning/error when CFG_SECURE_DATA_PATH is disabled:

 $ make PLATFORM=hikey CFG_SECURE_DATA_PATH=n
 ...
 core/arch/arm/mm/core_mmu.c:90:61: error: ISO C does not allow extra ';' outside of a function [-Werror=pedantic]
  register_sdp_mem(CFG_TEE_SDP_MEM_BASE, CFG_TEE_SDP_MEM_SIZE);
                                                             ^
 cc1: all warnings being treated as errors

Fixes: 2d9ed57b6bd8 ("Define register_sdp_mem() only when CFG_SECURE_DATA_PATH is defined")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/include/mm/core_mmu.h
@@ -239,7 +239,9 @@ struct core_mmu_phys_mem {
 		__register_memory1(#addr, MEM_AREA_SDP_MEM, (addr), (size), \
 				   phys_sdp_mem_section, __COUNTER__)
 #else
-#define register_sdp_mem(addr, size)
+#define register_sdp_mem(addr, size) \
+		static int CONCAT(__register_sdp_mem_unused, __COUNTER__) \
+			__unused
 #endif
 
 #define register_nsec_ddr(addr, size) \