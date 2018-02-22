23de6726efe70b664a120c9fb0867de2bf0a1504
Pankaj Gupta <pankaj.gupta@nxp.com>
2018-02-22 17:00:58 +0530

    plat-ls: Add support for fetching SSK from armv8 platform flavour.
        - PLATFORM = ls-ls1046ardb, ls-ls1043ardb, ls-ls1012ardb
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Reviewed-by: Sumit Garg <sumit.garg@nxp.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-ls/sub.mk
+++ core/arch/arm/plat-ls/sub.mk
@@ -2,4 +2,5 @@ global-incdirs-y += .
 srcs-y += main.c
 srcs-$(CFG_ARM32_core) += ls_core_pos_a32.S
 srcs-$(CFG_ARM64_core) += ls_core_pos_a64.S
+srcs-$(CFG_ARM64_core) += ls_hw_unq_key_a64.S
 srcs-$(CFG_ARM32_core) += plat_init.S
