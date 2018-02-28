8f7357271cc2e36bcd33fc0175839aac63613ef6
Edison Ai <edison.ai@arm.com>
2018-02-28 15:01:47 +0800

    libmbedtls: refine mbedtls license header
    New header format according to this:
    https://optee.readthedocs.io/general/license_headers.html
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Edison Ai <edison.ai@arm.com>
    [jw: removed the now redundant SPDX-License-Identifier]
    [jw: updated the link to readthredocs]
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    [jf: rebased onto mbedtls-2.22.0]
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

--- lib/libmbedtls/mbedtls/include/mbedtls/ctr_drbg.h
+++ lib/libmbedtls/mbedtls/include/mbedtls/ctr_drbg.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: Apache-2.0 */
 /**
  * \file ctr_drbg.h
  *
@@ -23,7 +24,6 @@
  */
 /*
  *  Copyright (C) 2006-2019, Arm Limited (or its affiliates), All Rights Reserved
- *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
  *  not use this file except in compliance with the License.
--- lib/libmbedtls/mbedtls/include/mbedtls/ecp_internal.h
+++ lib/libmbedtls/mbedtls/include/mbedtls/ecp_internal.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: Apache-2.0 */
 /**
  * \file ecp_internal.h
  *
@@ -6,7 +7,6 @@
  */
 /*
  *  Copyright (C) 2016, ARM Limited, All Rights Reserved
- *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
  *  not use this file except in compliance with the License.
