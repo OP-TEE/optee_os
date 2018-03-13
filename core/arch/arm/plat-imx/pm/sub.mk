062c54569ce0f5131d522dceb44917ab4f1e27a0
Peng Fan <peng.fan@nxp.com>
2018-03-13 17:35:00 +0800

    core: imx: use a dedicated sub.mk for pm
    Use a dedicated sub.mk for the directory holding power related
    source code.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ core/arch/arm/plat-imx/pm/sub.mk
@@ -0,0 +1,5 @@
+global-incdirs-y += .
+srcs-y += psci.c gpcv2.c
+srcs-$(CFG_MX7) += pm-imx7.c psci-suspend-imx7.S imx7_suspend.c
+
+cflags-psci.c-y += -Wno-suggest-attribute=noreturn
--- core/arch/arm/plat-imx/sub.mk
+++ core/arch/arm/plat-imx/sub.mk
@@ -5,13 +5,9 @@ srcs-$(CFG_MX6)$(CFG_MX7) += mmdc.c
 
 srcs-$(CFG_PL310) += imx_pl310.c
 ifeq ($(CFG_PSCI_ARM32),y)
-srcs-y += pm/psci.c pm/gpcv2.c
-srcs-$(CFG_MX7) += pm/pm-imx7.c pm/psci-suspend-imx7.S pm/imx7_suspend.c
 $(call force,CFG_PM_ARM32,y)
 asm-defines-y += imx_pm_asm_defines.c
 endif
-
-cflags-pm/psci.c-y += -Wno-suggest-attribute=noreturn
 
 ifneq (,$(filter y, $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) $(CFG_MX6S) \
        $(CFG_MX6SX)))
@@ -24,3 +20,5 @@ srcs-y += imx6ul.c
 endif
 
 srcs-$(CFG_MX7) += imx7.c a7_plat_init.S
+
+subdirs-$(CFG_PSCI_ARM32) += pm
