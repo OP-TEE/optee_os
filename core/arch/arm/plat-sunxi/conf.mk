ebc0ea6c0b86339b132f0ab7b15e049628af2056
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2018-03-13 15:15:52 +0800

    plat-sunxi: Add plat-sunxi
    Initial version support for Allwinner H2+ platform. Specific to Banana Pi
    M2 zero board currently.
    
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paul.liu@linaro.org>

---
+++ core/arch/arm/plat-sunxi/conf.mk
@@ -0,0 +1,32 @@
+PLATFORM_FLAVOR ?= bpi_zero
+
+include core/arch/arm/cpu/cortex-a7.mk
+
+ta-targets = ta_arm32
+
+$(call force,CFG_SUN8I_H2_PLUS,y)
+$(call force,CFG_ARM32_core,y)
+$(call force,CFG_GENERIC_BOOT,y)
+$(call force,CFG_GIC,y)
+$(call force,CFG_8250_UART,y)
+$(call force,CFG_PM_STUBS,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_WITH_LPAE,n)
+$(call force,CFG_WITH_PAGER,n)
+
+CFG_CRYPTO_SIZE_OPTIMIZATION ?= n
+CFG_NUM_THREADS ?= 4
+CFG_TEE_CORE_NB_CORE ?= 4
+CFG_WITH_STACK_CANARIES ?= y
+CFG_BOOT_SECONDARY_REQUEST ?= y
+CFG_PSCI_ARM32 ?= y
+CFG_WITH_STACK_CANARIES ?= y
+CFG_WITH_STATS ?= y
+CFG_NS_ENTRY_ADDR ?= 0x42000000
+CFG_DT ?= y
+CFG_INIT_CNTVOFF ?= y
+CFG_SECONDARY_INIT_CNTFRQ ?= y
+CFG_TZDRAM_START ?= 0x5c000000
+CFG_TZDRAM_SIZE ?= 0x03e00000
+CFG_SHMEM_START ?= 0x5fe00000
+CFG_SHMEM_SIZE ?= 0x00200000
