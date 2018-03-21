2a2c1fbd6c6505b5001e15aa4c508a3c096b269b
Peng Fan <peng.fan@nxp.com>
2018-03-21 13:10:47 +0800

    core: arm: imx7d: add low power cpuidle support
    Add Low power cpuidle support.
    
    The hard point is two cores could runs into low power idle
    at any time. So need to do protection. Here
    we could not use cpu_spin_lock, because
    we need lock in c code and unlock in asm code, and in asm
    code, SMP is turned off.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-imx/pm/sub.mk
+++ core/arch/arm/plat-imx/pm/sub.mk
@@ -1,5 +1,6 @@
 global-incdirs-y += .
 srcs-y += psci.c gpcv2.c
-srcs-$(CFG_MX7) += pm-imx7.c psci-suspend-imx7.S imx7_suspend.c
+srcs-$(CFG_MX7) += pm-imx7.c psci-suspend-imx7.S imx7_suspend.c \
+	cpuidle-imx7d.c psci-cpuidle-imx7.S
 
 cflags-psci.c-y += -Wno-suggest-attribute=noreturn
