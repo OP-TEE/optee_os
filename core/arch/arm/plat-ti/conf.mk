a98b65d9f35db901e8b804ee39819700a0f0e2ac
Etienne Carriere <etienne.carriere@linaro.org>
2018-04-26 21:58:01 +0200

    plat-ti: move some CFG_'s from platform_config.h to conf.mk
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Andrew F. Davis <afd@ti.com>

--- core/arch/arm/plat-ti/conf.mk
+++ core/arch/arm/plat-ti/conf.mk
@@ -11,23 +11,36 @@ else
 CFG_TEE_SDP_MEM_SIZE ?= 0x0
 endif
 
-$(call force,CFG_8250_UART,y)
-$(call force,CFG_ARM32_core,y)
-$(call force,CFG_GENERIC_BOOT,y)
-$(call force,CFG_PM_STUBS,y)
+ifeq ($(PLATFORM_FLAVOR),dra7xx)
+include core/arch/arm/cpu/cortex-a15.mk
+CFG_TEE_CORE_NB_CORE = 2
+CFG_OTP_SUPPORT ?= y
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+endif #dra7xx
+
+ifeq ($(PLATFORM_FLAVOR),am57xx)
+include core/arch/arm/cpu/cortex-a15.mk
+CFG_TEE_CORE_NB_CORE = 2
+CFG_OTP_SUPPORT ?= y
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+endif #am57xx
+
 ifeq ($(PLATFORM_FLAVOR),am43xx)
+include core/arch/arm/cpu/cortex-a9.mk
+CFG_TEE_CORE_NB_CORE = 1
+
 CFG_WITH_SOFTWARE_PRNG = y
 $(call force,CFG_NO_SMP,y)
 $(call force,CFG_PL310,y)
 $(call force,CFG_PL310_LOCKED,y)
 $(call force,CFG_PM_ARM32,y)
 $(call force,CFG_SECURE_TIME_SOURCE_REE,y)
-include core/arch/arm/cpu/cortex-a9.mk
-else
-CFG_OTP_SUPPORT ?= y
-$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
-include core/arch/arm/cpu/cortex-a15.mk
-endif
+endif #am43xx
+
+$(call force,CFG_8250_UART,y)
+$(call force,CFG_ARM32_core,y)
+$(call force,CFG_GENERIC_BOOT,y)
+$(call force,CFG_PM_STUBS,y)
 $(call force,CFG_SM_PLATFORM_HANDLER,y)
 $(call force,CFG_GIC,y)
 ifneq ($(CFG_WITH_SOFTWARE_PRNG),y)
