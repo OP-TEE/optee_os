f66d862dd543172128e6e0733bd7cb46ab8a5c36
Jens Wiklander <jens.wiklander@linaro.org>
2018-05-07 15:48:37 +0200

    libutils: import strcpy() and strncpy()
    Imports strcpy() and strncpy() from newlib with license added
    
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libutils/isoc/newlib/sub.mk
+++ lib/libutils/isoc/newlib/sub.mk
@@ -1,5 +1,6 @@
 cflags-y += -ffreestanding
 cflags-y += -Wno-sign-compare
+cflags-y += -Wno-parentheses
 cflags-remove-y += -Wcast-align
 
 srcs-y += memchr.c
@@ -14,3 +15,5 @@ srcs-y += strnlen.c
 srcs-y += abs.c
 srcs-y += strchr.c
 srcs-y += strstr.c
+srcs-y += strcpy.c
+srcs-y += strncpy.c
