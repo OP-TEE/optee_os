OP-TEE/optee_os

commit ce0d8e2da340980ac3f70aaa1122876308d0f745

util: add macros for arithmetics with overflow checking
Compiler builtin support for checking overflow of addition, subtraction
and multiplication is used if available. If unavailable a fallback
implementation using macros is used instead.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey 32/64)
Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/ext/include/compiler.h
@@ -59,4 +59,130 @@
 #define __compiler_bswap32(x)	__builtin_bswap32((x))
 #define __compiler_bswap16(x)	__builtin_bswap16((x))
 
+#define __GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + \
+		       __GNUC_PATCHLEVEL__)
+
+#if __GCC_VERSION >= 50100
+#define __HAVE_BUILTIN_OVERFLOW 1
+#endif
+
+#ifdef __HAVE_BUILTIN_OVERFLOW
+#define __compiler_add_overflow(a, b, res) \
+	__builtin_add_overflow((a), (b), (res))
+
+#define __compiler_sub_overflow(a, b, res) \
+	__builtin_sub_overflow((a), (b), (res))
+
+#define __compiler_mul_overflow(a, b, res) \
+	__builtin_mul_overflow((a), (b), (res))
+#else /*!__HAVE_BUILTIN_OVERFLOW*/
+
+/*
+ * Copied/inspired from https://www.fefe.de/intof.html
+ */
+#define __INTOF_HALF_MAX_SIGNED(type) ((type)1 << (sizeof(type)*8-2))
+#define __INTOF_MAX_SIGNED(type) (__INTOF_HALF_MAX_SIGNED(type) - 1 + \
+			    __INTOF_HALF_MAX_SIGNED(type))
+#define __INTOF_MIN_SIGNED(type) (-1 - __INTOF_MAX_SIGNED(type))
+
+#define __INTOF_MIN(type) ((type)-1 < 1?__INTOF_MIN_SIGNED(type):(type)0)
+#define __INTOF_MAX(type) ((type)~__INTOF_MIN(type))
+
+#define __INTOF_ASSIGN(dest, src) (__extension__({ \
+	typeof(src) __intof_x = (src); \
+	typeof(dest) __intof_y = __intof_x; \
+	(((uintmax_t)__intof_x == (uintmax_t)__intof_y) && \
+	 ((__intof_x < 1) == (__intof_y < 1)) ? \
+		(void)((dest) = __intof_y) , 0 : 1); \
+}))
+
+#define __INTOF_ADD(c, a, b) (__extension__({ \
+	typeof(a) __intofa_a = (a); \
+	typeof(b) __intofa_b = (b); \
+	\
+	__intofa_b < 1 ? \
+		((__INTOF_MIN(typeof(c)) - __intofa_b <= __intofa_a) ? \
+			__INTOF_ASSIGN((c), __intofa_a + __intofa_b) : 1) : \
+		((__INTOF_MAX(typeof(c)) - __intofa_b >= __intofa_a) ? \
+			__INTOF_ASSIGN((c), __intofa_a + __intofa_b) : 1); \
+}))
+
+#define __INTOF_SUB(c, a, b) (__extension__({ \
+	typeof(a) __intofs_a = a; \
+	typeof(b) __intofs_b = b; \
+	\
+	__intofs_b < 1 ? \
+		((__INTOF_MAX(typeof(c)) + __intofs_b >= __intofs_a) ? \
+			__INTOF_ASSIGN((c), __intofs_a - __intofs_b) : 1) : \
+		((__INTOF_MIN(typeof(c)) + __intofs_b <= __intofs_a) ? \
+			__INTOF_ASSIGN((c), __intofs_a - __intofs_b) : 1); \
+}))
+
+/*
+ * Dealing with detecting overflow in multiplication of integers.
+ *
+ * First step is to remove two corner cases with the minum signed integer
+ * which can't be represented as a positive integer + sign.
+ * Multiply with 0 or 1 can't overflow, no checking needed of the operation,
+ * only if it can be assigned to the result.
+ *
+ * After the corner cases are eliminated we convert the two factors to
+ * positive unsigned values, keeping track of the original in another
+ * variable which is used at the end to determine the sign of the product.
+ *
+ * The two terms (a and b) are divided into upper and lower half (x1 upper
+ * and x0 lower), so the product is:
+ * ((a1 << hshift) + a0) * ((b1 << hshift) + b0)
+ * which also is:
+ * ((a1 * b1) << (hshift * 2)) +				(T1)
+ * ((a1 * b0 + a0 * b1) << hshift) +				(T2)
+ * (a0 * b0)							(T3)
+ *
+ * From this we can tell and (a1 * b1) has to be 0 or we'll overflow, that
+ * is, at least one of a1 or b1 has to be 0. Once this has been checked the
+ * addition: ((a1 * b0) << hshift) + ((a0 * b1) << hshift)
+ * isn't an addition as one of the terms will be 0.
+ *
+ * Since each factor in: (a0 * b0)
+ * only uses half the capicity of the underlaying type it can't overflow
+ *
+ * The addition of T2 and T3 can overflow so we use __INTOF_ADD() to
+ * perform that addition. If the addition succeeds without overflow the
+ * result is assigned the required sign and checked for overflow again.
+ */
+
+#define __intof_mul_negate	((__intof_oa < 1) != (__intof_ob < 1))
+#define __intof_mul_hshift	(sizeof(uintmax_t) * 8 / 2)
+#define __intof_mul_hmask	(UINTMAX_MAX >> __intof_mul_hshift)
+#define __intof_mul_a0		((uintmax_t)(__intof_a) >> __intof_mul_hshift)
+#define __intof_mul_b0		((uintmax_t)(__intof_b) >> __intof_mul_hshift)
+#define __intof_mul_a1		((uintmax_t)(__intof_a) & __intof_mul_hmask)
+#define __intof_mul_b1		((uintmax_t)(__intof_b) & __intof_mul_hmask)
+#define __intof_mul_t		(__intof_mul_a1 * __intof_mul_b0 + \
+				 __intof_mul_a0 * __intof_mul_b1)
+
+#define __INTOF_MUL(c, a, b) (__extension__({ \
+	typeof(a) __intof_oa = (a); \
+	typeof(a) __intof_a = __intof_oa < 1 ? -__intof_oa : __intof_oa; \
+	typeof(b) __intof_ob = (b); \
+	typeof(b) __intof_b = __intof_ob < 1 ? -__intof_ob : __intof_ob; \
+	typeof(c) __intof_c; \
+	\
+	__intof_oa == 0 || __intof_ob == 0 || \
+	__intof_oa == 1 || __intof_ob == 1 ? \
+		__INTOF_ASSIGN((c), __intof_oa * __intof_ob) : \
+	(__intof_mul_a0 && __intof_mul_b0) || \
+	 __intof_mul_t > __intof_mul_hmask ?  1 : \
+	__INTOF_ADD((__intof_c), __intof_mul_t << __intof_mul_hshift, \
+				 __intof_mul_a1 * __intof_mul_b1) ? 1 : \
+	__intof_mul_negate ? __INTOF_ASSIGN((c), -__intof_c) : \
+			     __INTOF_ASSIGN((c), __intof_c); \
+}))
+
+#define __compiler_add_overflow(a, b, res) __INTOF_ADD(*(res), (a), (b))
+#define __compiler_sub_overflow(a, b, res) __INTOF_SUB(*(res), (a), (b))
+#define __compiler_mul_overflow(a, b, res) __INTOF_MUL(*(res), (a), (b))
+
+#endif /*!__HAVE_BUILTIN_OVERFLOW*/
+
 #endif /*COMPILER_H*/

core/arch/arm/pta/core_self_tests.c
@@ -24,9 +24,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <assert.h>
 #include <malloc.h>
 #include <stdbool.h>
 #include <trace.h>
+#include <kernel/panic.h>
+#include <util.h>
 #include "core_self_tests.h"
 
 /*
@@ -37,18 +40,156 @@
  */
 #define LOG(...)
 
-static int self_test_division(void);
-static int self_test_malloc(void);
-
-/* exported entry points for some basic test */
-TEE_Result core_self_tests(uint32_t nParamTypes __unused,
-		TEE_Param pParams[TEE_NUM_PARAMS] __unused)
+static int self_test_add_overflow(void)
 {
-	if (self_test_division() || self_test_malloc()) {
-		EMSG("some self_test_xxx failed! you should enable local LOG");
-		return TEE_ERROR_GENERIC;
-	}
-	return TEE_SUCCESS;
+	uint32_t r_u32;
+	int32_t r_s32;
+
+	if (ADD_OVERFLOW(32U, 30U, &r_u32))
+		return -1;
+	if (r_u32 != 62)
+		return -1;
+	if (!ADD_OVERFLOW(UINT32_MAX, UINT32_MAX, &r_u32))
+		return -1;
+	if (!ADD_OVERFLOW(UINT32_MAX / 2 + 1, UINT32_MAX / 2 + 1, &r_u32))
+		return -1;
+	if (ADD_OVERFLOW(UINT32_MAX / 2, UINT32_MAX / 2 + 1, &r_u32))
+		return -1;
+	if (r_u32 != UINT32_MAX)
+		return -1;
+
+	if (ADD_OVERFLOW(30, -31, &r_s32))
+		return -1;
+	if (r_s32 != -1)
+		return -1;
+	if (ADD_OVERFLOW(INT32_MIN + 1, -1, &r_s32))
+		return -1;
+	if (r_s32 != INT32_MIN)
+		return -1;
+	if (!ADD_OVERFLOW(INT32_MIN, -1, &r_s32))
+		return -1;
+	if (!ADD_OVERFLOW(INT32_MIN + 1, -2, &r_s32))
+		return -1;
+	if (!ADD_OVERFLOW(INT32_MAX, INT32_MAX, &r_s32))
+		return -1;
+	if (!ADD_OVERFLOW(INT32_MAX / 2 + 1, INT32_MAX / 2 + 1, &r_s32))
+		return -1;
+	if (ADD_OVERFLOW(INT32_MAX / 2, INT32_MAX / 2 + 1, &r_s32))
+		return -1;
+	if (r_s32 != INT32_MAX)
+		return -1;
+
+	return 0;
+}
+
+static int self_test_sub_overflow(void)
+{
+	uint32_t r_u32;
+	int32_t r_s32;
+
+	if (SUB_OVERFLOW(32U, 30U, &r_u32))
+		return -1;
+	if (r_u32 != 2)
+		return -1;
+	if (!SUB_OVERFLOW(30U, 31U, &r_u32))
+		return -1;
+
+	if (SUB_OVERFLOW(30, 31, &r_s32))
+		return -1;
+	if (r_s32 != -1)
+		return -1;
+	if (SUB_OVERFLOW(-1, INT32_MAX, &r_s32))
+		return -1;
+	if (r_s32 != INT32_MIN)
+		return -1;
+	if (!SUB_OVERFLOW(-2, INT32_MAX, &r_s32))
+		return -1;
+
+	return 0;
+}
+
+static int self_test_mul_unsigned_overflow(void)
+{
+	const size_t um_half_shift = sizeof(uintmax_t) * 8 / 2;
+	const uintmax_t um_half_mask = UINTMAX_MAX >> um_half_shift;
+	uint32_t r_u32;
+	uintmax_t r_um;
+
+	if (MUL_OVERFLOW(32, 30, &r_u32))
+		return -1;
+	if (r_u32 != 960)
+		return -1;
+	if (MUL_OVERFLOW(-32, -30, &r_u32))
+		return -1;
+	if (r_u32 != 960)
+		return -1;
+
+	if (MUL_OVERFLOW(UINTMAX_MAX, 1, &r_um))
+		return -1;
+	if (r_um != UINTMAX_MAX)
+		return -1;
+	if (MUL_OVERFLOW(UINTMAX_MAX / 4, 4, &r_um))
+		return -1;
+	if (r_um != (UINTMAX_MAX - 3))
+		return -1;
+	if (!MUL_OVERFLOW(UINTMAX_MAX / 4 + 1, 4, &r_um))
+		return -1;
+	if (!MUL_OVERFLOW(UINTMAX_MAX, UINTMAX_MAX, &r_um))
+		return -1;
+	if (!MUL_OVERFLOW(um_half_mask << um_half_shift,
+			  um_half_mask << um_half_shift, &r_um))
+		return -1;
+
+	return 0;
+}
+
+static int self_test_mul_signed_overflow(void)
+{
+	intmax_t r;
+
+	if (MUL_OVERFLOW(32, -30, &r))
+		return -1;
+	if (r != -960)
+		return -1;
+	if (MUL_OVERFLOW(-32, 30, &r))
+		return -1;
+	if (r != -960)
+		return -1;
+	if (MUL_OVERFLOW(32, 30, &r))
+		return -1;
+	if (r != 960)
+		return -1;
+
+	if (MUL_OVERFLOW(INTMAX_MAX, 1, &r))
+		return -1;
+	if (r != INTMAX_MAX)
+		return -1;
+	if (MUL_OVERFLOW(INTMAX_MAX / 4, 4, &r))
+		return -1;
+	if (r != (INTMAX_MAX - 3))
+		return -1;
+	if (!MUL_OVERFLOW(INTMAX_MAX / 4 + 1, 4, &r))
+		return -1;
+	if (!MUL_OVERFLOW(INTMAX_MAX, INTMAX_MAX, &r))
+		return -1;
+	if (MUL_OVERFLOW(INTMAX_MIN + 1, 1, &r))
+		return -1;
+	if (r != INTMAX_MIN + 1)
+		return -1;
+	if (MUL_OVERFLOW(1, INTMAX_MIN + 1, &r))
+		return -1;
+	if (r != INTMAX_MIN + 1)
+		return -1;
+	if (MUL_OVERFLOW(0, INTMAX_MIN, &r))
+		return -1;
+	if (r != 0)
+		return -1;
+	if (MUL_OVERFLOW(1, INTMAX_MIN, &r))
+		return -1;
+	if (r != INTMAX_MIN)
+		return -1;
+
+	return 0;
 }
 
 /* test division support. resulting trace shall be manually checked */
@@ -249,4 +390,17 @@ static int self_test_malloc(void)
 	LOG("malloc test done");
 
 	return ret;
+}
+
+/* exported entry points for some basic test */
+TEE_Result core_self_tests(uint32_t nParamTypes __unused,
+		TEE_Param pParams[TEE_NUM_PARAMS] __unused)
+{
+	if (self_test_mul_signed_overflow() || self_test_add_overflow() ||
+	    self_test_sub_overflow() || self_test_mul_unsigned_overflow() ||
+	    self_test_division() || self_test_malloc()) {
+		EMSG("some self_test_xxx failed! you should enable local LOG");
+		return TEE_ERROR_GENERIC;
+	}
+	return TEE_SUCCESS;
 }

lib/libutils/ext/include/util.h
@@ -27,6 +27,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <compiler.h>
 #include <stdint.h>
 
 #ifndef MAX
@@ -90,5 +91,16 @@
 
 #define GENMASK_64(h, l) \
 	(((~UINT64_C(0)) << (l)) & (~UINT64_C(0) >> (64 - 1 - (h))))
+
+/*
+ * Checking overflow for addition, subtraction and multiplication. Result
+ * of operation is stored in res which is a pointer to some kind of
+ * integer.
+ *
+ * The macros return true if an overflow occurred and *res is undefined.
+ */
+#define ADD_OVERFLOW(a, b, res) __compiler_add_overflow((a), (b), (res))
+#define SUB_OVERFLOW(a, b, res) __compiler_sub_overflow((a), (b), (res))
+#define MUL_OVERFLOW(a, b, res) __compiler_mul_overflow((a), (b), (res))
 
 #endif /*UTIL_H*/

commit 6203f75e10aeca7fa2b635a6e22297c07350f576

util: fix fallback ADD_OVERFLOW() macro
This only applies when the compiler doesn't provide the
__builtin_add_overflow() function (before GCC 5.1).

Fixes problem due to integer promotion in overflow macros when result is
a signed type and the second addend is an unsigned type with value 0.

ADD_OVERFLOW() still doesn't handle all combinations of signed and
unsigned types in the parameters.

Fixes: ce0d8e2da340 ("util: add macros for arithmetics with overflow checking")
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/ext/include/compiler.h
@@ -112,9 +112,17 @@
 #define __INTOF_ADD(c, a, b) (__extension__({ \
 	typeof(a) __intofa_a = (a); \
 	typeof(b) __intofa_b = (b); \
+	int __infoa_b_signed = !!__INTOF_MIN(typeof(b)); \
+	int __infoa_c_signed = !!__INTOF_MIN(typeof(c)); \
 	\
 	__intofa_b < 1 ? \
-		((__INTOF_MIN(typeof(c)) - __intofa_b <= __intofa_a) ? \
+		/* \
+		 * First deal with special case where c is signed and b is \
+		 * unsigned and has the value 0 to avoid a problem due to \
+		 * integer promotion. \
+		 */ \
+		(((__infoa_c_signed && !__infoa_b_signed) || \
+		 (__INTOF_MIN(typeof(c)) - __intofa_b <= __intofa_a)) ? \
 			__INTOF_ASSIGN((c), __intofa_a + __intofa_b) : 1) : \
 		((__INTOF_MAX(typeof(c)) - __intofa_b >= __intofa_a) ? \
 			__INTOF_ASSIGN((c), __intofa_a + __intofa_b) : 1); \