785a98b72e244d46bf1f87fe38e2c1b90a4811ce
Etienne Carriere <etienne.carriere@linaro.org>
2018-05-14 15:30:45 +0200

    plat-mediatek: support generic Ram layout
    Move default secure and non-secure Optee memory locations from
    platform_config.h to conf.mk using the generic_ram_layout.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

--- core/arch/arm/plat-mediatek/conf.mk
+++ core/arch/arm/plat-mediatek/conf.mk
@@ -3,6 +3,10 @@ PLATFORM_FLAVOR ?= mt8173
 include core/arch/arm/cpu/cortex-armv8-0.mk
 
 CFG_TEE_CORE_NB_CORE = 4
+CFG_TZDRAM_START ?= 0xbe000000
+CFG_TZDRAM_SIZE ?= 0x01e00000
+CFG_SHMEM_START ?= 0xbfe00000
+CFG_SHMEM_SIZE ?= 0x00200000
 
 $(call force,CFG_8250_UART,y)
 $(call force,CFG_GENERIC_BOOT,y)
