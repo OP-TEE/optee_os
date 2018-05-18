OP-TEE/optee_os

commit 94a72998bc1d9c60d8c81fa485a0ec32c122f4cd

core: fs_htree: include meta in root hash
Includes the meta data when calculating the hash of the root node to
detect changes in file length while number of blocks is unchanged.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Fixes: https://github.com/OP-TEE/optee_os/issues/2094
Reported-by: Kevin Peng <kevinp@marvell.com>
Tested-by: Kevin Peng <kevinp@marvell.com>
[jf: add Fixes:, Reported-by: and Tested-by: tags]
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

core/tee/fs_htree.c
@@ -397,7 +397,8 @@ static TEE_Result init_tree_from_data(struct tee_fs_htree *ht)
 	return TEE_SUCCESS;
 }
 
-static TEE_Result calc_node_hash(struct htree_node *node, void *ctx,
+static TEE_Result calc_node_hash(struct htree_node *node,
+				 struct tee_fs_htree_meta *meta, void *ctx,
 				 uint8_t *digest)
 {
 	TEE_Result res;
@@ -412,6 +413,12 @@ static TEE_Result calc_node_hash(struct htree_node *node, void *ctx,
 	res = crypto_hash_update(ctx, alg, ndata, nsize);
 	if (res != TEE_SUCCESS)
 		return res;
+
+	if (meta) {
+		res = crypto_hash_update(ctx, alg, (void *)meta, sizeof(meta));
+		if (res != TEE_SUCCESS)
+			return res;
+	}
 
 	if (node->child[0]) {
 		res = crypto_hash_update(ctx, alg, node->child[0]->node.hash,
@@ -563,7 +570,10 @@ static TEE_Result verify_node(struct traverse_arg *targ,
 	TEE_Result res;
 	uint8_t digest[TEE_FS_HTREE_HASH_SIZE];
 
-	res = calc_node_hash(node, ctx, digest);
+	if (node->parent)
+		res = calc_node_hash(node, NULL, ctx, digest);
+	else
+		res = calc_node_hash(node, &targ->ht->imeta.meta, ctx, digest);
 	if (res == TEE_SUCCESS &&
 	    buf_compare_ct(digest, node->node.hash, sizeof(digest)))
 		return TEE_ERROR_CORRUPT_OBJECT;
@@ -598,7 +608,8 @@ static TEE_Result init_root_node(struct tee_fs_htree *ht)
 	ht->root.id = 1;
 	ht->root.dirty = true;
 
-	res = calc_node_hash(&ht->root, ctx, ht->root.node.hash);
+	res = calc_node_hash(&ht->root, &ht->imeta.meta, ctx,
+			     ht->root.node.hash);
 	crypto_hash_free_ctx(ctx, TEE_FS_HTREE_HASH_ALG);
 
 	return res;
@@ -670,6 +681,7 @@ struct tee_fs_htree_meta *tee_fs_htree_get_meta(struct tee_fs_htree *ht)
 void tee_fs_htree_meta_set_dirty(struct tee_fs_htree *ht)
 {
 	ht->dirty = true;
+	ht->root.dirty = true;
 }
 
 static TEE_Result free_node(struct traverse_arg *targ __unused,
@@ -694,6 +706,7 @@ static TEE_Result htree_sync_node_to_storage(struct traverse_arg *targ,
 {
 	TEE_Result res;
 	uint8_t vers;
+	struct tee_fs_htree_meta *meta = NULL;
 
 	/*
 	 * The node can be dirty while the block isn't updated due to
@@ -717,9 +730,10 @@ static TEE_Result htree_sync_node_to_storage(struct traverse_arg *targ,
 		 * writing the header.
 		 */
 		vers = !(targ->ht->head.counter & 1);
+		meta = &targ->ht->imeta.meta;
 	}
 
-	res = calc_node_hash(node, targ->arg, node->node.hash);
+	res = calc_node_hash(node, meta, targ->arg, node->node.hash);
 	if (res != TEE_SUCCESS)
 		return res;
 

commit 3f272a233f14c302c71da44baaa8409b7e7d552d

core: htree: fix meta inclusion in root hash
Prior to this patch was the size of the meta data supplied as the size
of the pointer to meta data. With this patch the size is corrected to be
the size of meta  data itself.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Fixes: https://github.com/OP-TEE/optee_os/issues/2330
Fixes: 94a72998bc1d ("core: fs_htree: include meta in root hash")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/fs_htree.c
@@ -415,7 +415,7 @@ static TEE_Result calc_node_hash(struct htree_node *node,
 		return res;
 
 	if (meta) {
-		res = crypto_hash_update(ctx, alg, (void *)meta, sizeof(meta));
+		res = crypto_hash_update(ctx, alg, (void *)meta, sizeof(*meta));
 		if (res != TEE_SUCCESS)
 			return res;
 	}