03314a3a69b8465e4cb6377115a0baa5e38112b1
Etienne Carriere <etienne.carriere@linaro.org>
2018-05-22 11:49:18 +0200

    plat-vexpress: move to generic RAM layout
    FVP:
    - Secure RAM [0600.0000 0800.0000[ configurable.
    - Static SHM [8300.0000 8320.0000[ configurable.
    
    Juno:
    - Secure RAM [ff00.0000 ffff.8000[ configurable.
      Note trailing 32kByte reserved by SCP for DDR retraining.
    - Static SHM [fee0.0000 ff00.0000[ configurable.
    
    qemu_virt:
    - Secure RAM [0e10.0000 0f00.0000[ configurable.
    - Static SHM [7fe0.0000 8000.0000[ configurable.
    
    qemu_armv8:
    - Secure RAM [0.0e10.0000 0.0f00.0000[ configurable.
    - Static SHM [0.ffe0.0000 1.0000.0000[ configurable.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-vexpress/conf.mk
+++ core/arch/arm/plat-vexpress/conf.mk
@@ -46,12 +46,20 @@ CFG_WITH_STATS ?= y
 
 ifeq ($(PLATFORM_FLAVOR),fvp)
 CFG_TEE_CORE_NB_CORE = 8
+CFG_TZDRAM_START ?= 0x06000000
+CFG_TZDRAM_SIZE  ?= 0x02000000
+CFG_SHMEM_START  ?= 0x83000000
+CFG_SHMEM_SIZE   ?= 0x00200000
 # DRAM1 is defined above 4G
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
 endif
 
 ifeq ($(PLATFORM_FLAVOR),juno)
 CFG_TEE_CORE_NB_CORE = 6
+CFG_TZDRAM_START ?= 0xff000000
+CFG_TZDRAM_SIZE  ?= 0x00ff8000
+CFG_SHMEM_START  ?= 0xfee00000
+CFG_SHMEM_SIZE   ?= 0x00200000
 # DRAM1 is defined above 4G
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
 CFG_CRYPTO_WITH_CE ?= y
@@ -59,6 +67,15 @@ endif
 
 ifeq ($(PLATFORM_FLAVOR),qemu_virt)
 CFG_TEE_CORE_NB_CORE = 4
+# [0e00.0000 0e0f.ffff] is reserved to early boot
+CFG_TZDRAM_START ?= 0x0e100000
+CFG_TZDRAM_SIZE  ?= 0x00f00000
+CFG_SHMEM_START  ?= 0x7fe00000
+CFG_SHMEM_SIZE   ?= 0x00200000
+# When Secure Data Path is enable, last MByte of TZDRAM is SDP test memory.
+CFG_TEE_SDP_MEM_SIZE ?= 0x00400000
+# Set VA space to 2MB for Kasan offset to match LPAE and 32bit MMU configs
+CFG_TEE_RAM_VA_SIZE ?= 0x00200000
 ifeq ($(CFG_CORE_SANITIZE_KADDRESS),y)
 # CFG_ASAN_SHADOW_OFFSET is calculated as:
 # (&__asan_shadow_start - (TEE_RAM_VA_START / 8)
@@ -78,5 +95,14 @@ endif
 
 ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
 CFG_TEE_CORE_NB_CORE = 2
+# [0e00.0000 0e0f.ffff] is reserved to early boot
+CFG_TZDRAM_START ?= 0x0e100000
+CFG_TZDRAM_SIZE  ?= 0x00f00000
+# SHM chosen arbitrary, in a way that it does not interfere
+# with initial location of linux kernel, dtb and initrd.
+CFG_SHMEM_START ?= 0x42000000
+CFG_SHMEM_SIZE  ?= 0x00200000
+# When Secure Data Path is enable, last MByte of TZDRAM is SDP test memory.
+CFG_TEE_SDP_MEM_SIZE ?= 0x00400000
 $(call force,CFG_DT,y)
 endif
