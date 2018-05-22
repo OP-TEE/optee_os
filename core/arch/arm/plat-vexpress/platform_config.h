9e872d2beb4e6b14558cfd28dc2482b580bd0a4c
Etienne Carriere <etienne.carriere@linaro.org>
2018-05-22 11:49:18 +0200

    plat-vexpress: move to generic RAM layout
    FVP:
    - Secure RAM [0600.0000 0800.0000[ configurable.
    - Static SHM [8300.0000 8320.0000[ configurable.
    
    Juno:
    - Secure RAM [ff00.0000 ffff.8000[ configurable.
      Note trailing 32kByte reserved by SCP for DDR retraining.
    - Static SHM [fee0.0000 ff00.0000[ configurable.
    
    qemu_virt:
    - Secure RAM [0e10.0000 0f00.0000[ configurable.
    - Static SHM [7fe0.0000 8000.0000[ configurable.
    
    qemu_armv8:
    - Secure RAM [0.0e10.0000 0.0f00.0000[ configurable.
    - Static SHM [0.ffe0.0000 1.0000.0000[ configurable.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-vexpress/platform_config.h
+++ core/arch/arm/plat-vexpress/platform_config.h
@@ -6,6 +6,7 @@
 #ifndef PLATFORM_CONFIG_H
 #define PLATFORM_CONFIG_H
 
+#include <mm/generic_ram_layout.h>
 #include <stdint.h>
 
 /* Make stacks aligned to data cache line length */
@@ -89,26 +90,6 @@
 #define TZCDRAM_BASE		0xff000000
 #define TZCDRAM_SIZE		0x01000000
 
-#ifdef CFG_WITH_PAGER
-
-/* Emulated SRAM */
-#define TZSRAM_BASE		(0x06000000)
-#define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
-
-#define TZDRAM_BASE		(TZSRAM_BASE + TEE_RAM_VA_SIZE)
-#define TZDRAM_SIZE		(0x02000000 - TEE_RAM_VA_SIZE)
-
-#else /*CFG_WITH_PAGER*/
-
-/* Location of trusted dram on the base fvp */
-#define TZDRAM_BASE		0x06000000
-#define TZDRAM_SIZE		0x02000000
-
-#endif /*CFG_WITH_PAGER*/
-
-#define TEE_SHMEM_START		(DRAM0_BASE + 0x3000000)
-#define TEE_SHMEM_SIZE		0x200000
-
 #define GICC_OFFSET		0x0
 #define GICD_OFFSET		0x3000000
 
@@ -123,28 +104,6 @@
 #define DRAM1_BASE		0x880000000UL
 #define DRAM1_SIZE		0x180000000UL
 
-#ifdef CFG_WITH_PAGER
-
-/* Emulated SRAM */
-#define TZSRAM_BASE		0xFF000000
-#define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
-
-#define TZDRAM_BASE		(TZSRAM_BASE + TEE_RAM_VA_SIZE)
-#define TZDRAM_SIZE		(0x00E00000 - TEE_RAM_VA_SIZE)
-
-#else /*CFG_WITH_PAGER*/
-/*
- * Last part of DRAM is reserved as secure dram, note that the last 32KiB
- * of DRAM0 is used by SCP for DDR retraining.
- */
-#define TZDRAM_BASE		0xFF000000
-#define TZDRAM_SIZE		0x00FF8000
-
-#endif /*CFG_WITH_PAGER*/
-
-#define TEE_SHMEM_START		(DRAM0_BASE + DRAM0_SIZE - TEE_SHMEM_SIZE)
-#define TEE_SHMEM_SIZE		0x200000
-
 #define GICC_OFFSET		0x1f000
 #define GICD_OFFSET		0
 
@@ -154,138 +113,21 @@
  */
 
 #define DRAM0_BASE		UINTPTR_C(0x40000000)
-#define DRAM0_SIZE		(UINTPTR_C(0x40000000) - TEE_SHMEM_SIZE)
-
-#define DRAM0_TEERES_BASE	(DRAM0_BASE + DRAM0_SIZE)
-#define DRAM0_TEERES_SIZE	TEE_SHMEM_SIZE
+#define DRAM0_SIZE		UINTPTR_C(0x40000000)
 
 #define SECRAM_BASE		0x0e000000
-#define SECRAM_SIZE		0x01000000
 #define SECRAM_COHERENT_SIZE	4096
-
-#ifdef CFG_WITH_PAGER
-
-/* Emulated SRAM */
-/* First 1MByte of the secure RAM is reserved to ARM-TF boot services */
-#define TZSRAM_BASE		(SECRAM_BASE + 0x00100000)
-#define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
-
-#define TZDRAM_BASE		(TZSRAM_BASE + TEE_RAM_VA_SIZE)
-#define TZDRAM_SIZE		(SECRAM_SIZE - TEE_RAM_VA_SIZE - \
-					TZSRAM_SIZE - 0x00100000)
-
-#else /* CFG_WITH_PAGER */
-
-#define TZDRAM_BASE		(SECRAM_BASE + 0x00100000)
-#define TZDRAM_SIZE		(SECRAM_SIZE - 0x00100000)
-
-#endif /* CFG_WITH_PAGER */
-
-#define TEE_SHMEM_START		(DRAM0_TEERES_BASE + \
-					(DRAM0_TEERES_SIZE - TEE_SHMEM_SIZE))
-#define TEE_SHMEM_SIZE		0x200000
 
 #define GICD_OFFSET		0
 #define GICC_OFFSET		0x10000
 
-
 #elif defined(PLATFORM_FLAVOR_qemu_armv8a)
 
 #define DRAM0_BASE		UINTPTR_C(0x40000000)
-#define DRAM0_SIZE		(UINTPTR_C(0x40000000) - TEE_SHMEM_SIZE)
-
-#define SECRAM_BASE		0x0e000000
-#define SECRAM_SIZE		0x01000000
-
-#ifdef CFG_WITH_PAGER
-
-/* Emulated SRAM */
-/* First 1MByte of the secure RAM is reserved to ARM-TF runtime services */
-#define TZSRAM_BASE		(SECRAM_BASE + 0x00100000)
-#define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
-
-#define TZDRAM_BASE		(TZSRAM_BASE + TZSRAM_SIZE)
-#define TZDRAM_SIZE		(SECRAM_SIZE - TZSRAM_SIZE - 0x00100000)
-
-#else /* CFG_WITH_PAGER */
-
-/* First 1MByte of the secure RAM is reserved to ARM-TF runtime services */
-#define TZDRAM_BASE		(SECRAM_BASE + 0x00100000)
-#define TZDRAM_SIZE		(SECRAM_SIZE - 0x00100000)
-
-#endif /* CFG_WITH_PAGER */
-
-/*
- * TEE_SHMEM_START chosen arbitrary, in a way that it does not interfere
- * with initial location of linux kernel, dtb and initrd
- */
-#define TEE_SHMEM_START		(DRAM0_BASE + 0x2000000)
-#define TEE_SHMEM_SIZE		0x200000
+#define DRAM0_SIZE		UINTPTR_C(0x40000000)
 
 #else
 #error "Unknown platform flavor"
-#endif
-
-#define TEE_RAM_VA_SIZE		(2 * 1024 * 1024)
-
-#ifdef CFG_TEE_LOAD_ADDR
-#define TEE_LOAD_ADDR			CFG_TEE_LOAD_ADDR
-#else
-#define TEE_LOAD_ADDR			TEE_RAM_START
-#endif
-
-#ifdef CFG_WITH_PAGER
-/*
- * Have TZSRAM either as real physical or emulated by reserving an area
- * somewhere else.
- *
- * +------------------+
- * | TZSRAM | TEE_RAM |
- * +--------+---------+
- * | TZDRAM | TA_RAM  |
- * |        +---------+
- * |        | SDP RAM | (SDP test pool, optional)
- * +--------+---------+
- */
-#define TEE_RAM_PH_SIZE		TZSRAM_SIZE
-#define TEE_RAM_START		TZSRAM_BASE
-#define TA_RAM_START		ROUNDUP(TZDRAM_BASE, CORE_MMU_DEVICE_SIZE)
-
-#else
-/*
- * Assumes that either TZSRAM isn't large enough or TZSRAM doesn't exist,
- * everything is in TZDRAM.
- * +------------------+
- * |        | TEE_RAM |
- * | TZDRAM +---------+
- * |        | TA_RAM  |
- * |        +---------+
- * |        | SDP RAM | (test pool, optional)
- * +--------+---------+
- */
-#define TEE_RAM_PH_SIZE		TEE_RAM_VA_SIZE
-#define TEE_RAM_START		TZDRAM_BASE
-#define TA_RAM_START		ROUNDUP(TZDRAM_BASE + TEE_RAM_VA_SIZE, \
-					CORE_MMU_DEVICE_SIZE)
-#endif
-
-#define TA_RAM_SIZE		ROUNDDOWN(TZDRAM_SIZE - \
-					  (TA_RAM_START - TZDRAM_BASE) - \
-					  TEE_SDP_TEST_MEM_SIZE, \
-					  CORE_MMU_DEVICE_SIZE)
-
-/* SDP enable but no pool defined: reserve 4MB for SDP tests */
-#if defined(CFG_SECURE_DATA_PATH) && !defined(CFG_TEE_SDP_MEM_BASE)
-#ifdef CFG_TEE_SDP_TEST_MEM_SIZE
-#define TEE_SDP_TEST_MEM_SIZE	CFG_TEE_SDP_TEST_MEM_SIZE
-#else
-#define TEE_SDP_TEST_MEM_SIZE	0x00400000
-#endif
-#define TEE_SDP_TEST_MEM_BASE	(TZDRAM_BASE + TZDRAM_SIZE - \
-					TEE_SDP_TEST_MEM_SIZE)
-#endif
-#ifndef TEE_SDP_TEST_MEM_SIZE
-#define TEE_SDP_TEST_MEM_SIZE	0
 #endif
 
 #ifdef GIC_BASE
