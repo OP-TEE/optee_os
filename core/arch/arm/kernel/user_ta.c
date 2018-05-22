OP-TEE/optee_os

commit b072193efb70f8d22ffde61d915bf5930ff1c0f8

arm32: stack unwinding for dynamically linked TAs
Update the ELF loader so that TAs that contain multiple ELF binaries
have a valid exception index table (EXIDX). This table is the entry
point for the call stack unwinding code. When a TA uses shared
libraries, we create a new EXIDX table by joining all the tables found
in each ELF and patching them to account for the new table address.
Information about the ARM unwind tables can be found in [1].

Link: [1] https://wiki.linaro.org/KenWerner/Sandbox/libunwind?action=AttachFile&do=get&target=libunwind-LDS.pdf
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/kernel/user_ta.c
@@ -433,6 +433,7 @@ static void user_ta_ctx_destroy(struct tee_ta_ctx *ctx)
 	TAILQ_FOREACH(elf, &utc->elfs, link)
 		release_ta_memory_by_mobj(elf->mobj_code);
 	release_ta_memory_by_mobj(utc->mobj_stack);
+	release_ta_memory_by_mobj(utc->mobj_exidx);
 
 	/*
 	 * Close sessions opened by this TA
@@ -446,6 +447,7 @@ static void user_ta_ctx_destroy(struct tee_ta_ctx *ctx)
 
 	vm_info_final(utc);
 	mobj_free(utc->mobj_stack);
+	mobj_free(utc->mobj_exidx);
 	free_elfs(&utc->elfs);
 
 	/* Free cryp states created by this TA */
@@ -824,6 +826,94 @@ static TEE_Result set_seg_prot(struct user_ta_ctx *utc,
 	return res;
 }
 
+#ifdef CFG_UNWIND
+
+/*
+ * 32-bit TAs: set the address and size of the exception index table (EXIDX).
+ * If the TA contains only one ELF, we point to its table. Otherwise, a
+ * consolidated table is made by concatenating the tables found in each ELF and
+ * adjusting their content to account for the offset relative to the original
+ * location.
+ */
+static TEE_Result set_exidx(struct user_ta_ctx *utc)
+{
+	struct user_ta_elf *exe;
+	struct user_ta_elf *elf;
+	vaddr_t exidx;
+	size_t exidx_sz = 0;
+	TEE_Result res;
+	uint8_t *p;
+
+	if (!utc->is_32bit)
+		return TEE_SUCCESS;
+
+	exe = TAILQ_FIRST(&utc->elfs);
+	if (!TAILQ_NEXT(exe, link)) {
+		/* We have a single ELF: simply reference its table */
+		utc->exidx_start = exe->exidx_start;
+		utc->exidx_size = exe->exidx_size;
+		return TEE_SUCCESS;
+	}
+
+	TAILQ_FOREACH(elf, &utc->elfs, link)
+		exidx_sz += elf->exidx_size;
+
+	utc->mobj_exidx = alloc_ta_mem(exidx_sz);
+	if (!utc->mobj_exidx)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	exidx = 0;
+	res = vm_map(utc, &exidx, exidx_sz, TEE_MATTR_UR | TEE_MATTR_PRW,
+		     utc->mobj_exidx, 0);
+	if (res)
+		goto err;
+	DMSG("New EXIDX table mapped at 0x%" PRIxVA " size %zu",
+	     exidx, exidx_sz);
+
+	p = (void *)exidx;
+	TAILQ_FOREACH(elf, &utc->elfs, link) {
+		void *e_exidx = (void *)(elf->exidx_start + elf->load_addr);
+		size_t e_exidx_sz = elf->exidx_size;
+		int32_t offs = (int32_t)((vaddr_t)e_exidx - (vaddr_t)p);
+
+		memcpy(p, e_exidx, e_exidx_sz);
+		res = relocate_exidx(p, e_exidx_sz, offs);
+		if (res)
+			goto err;
+		p += e_exidx_sz;
+	}
+
+	/*
+	 * Drop privileged mode permissions. Normally we should keep
+	 * TEE_MATTR_PR because the code that accesses this table runs in
+	 * privileged mode. However, privileged read is always enabled if
+	 * unprivileged read is enabled, so it doesn't matter. For consistency
+	 * with other ELF section mappings, let's clear all the privileged
+	 * permission bits.
+	 */
+	res = vm_set_prot(utc, exidx,
+			  ROUNDUP(exidx_sz, SMALL_PAGE_SIZE),
+			  TEE_MATTR_UR);
+	if (res)
+		goto err;
+
+	utc->exidx_start = exidx - utc->load_addr;
+	utc->exidx_size = exidx_sz;
+
+	return TEE_SUCCESS;
+err:
+	mobj_free(utc->mobj_exidx);
+	return res;
+}
+
+#else /* CFG_UNWIND */
+
+static TEE_Result set_exidx(struct user_ta_ctx *utc __unused)
+{
+	return TEE_SUCCESS;
+}
+
+#endif /* CFG_UNWIND */
+
 TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 				       struct tee_ta_session *s)
 {
@@ -886,8 +976,9 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	}
 
 	utc->load_addr = exe->load_addr;
-	utc->exidx_start = exe->exidx_start;
-	utc->exidx_size = exe->exidx_size;
+	res = set_exidx(utc);
+	if (res)
+		goto err;
 
 	ta_head = (struct ta_head *)(vaddr_t)utc->load_addr;
 

core/arch/arm/include/kernel/unwind.h
@@ -35,6 +35,7 @@
 
 #ifndef ASM
 #include <compiler.h>
+#include <tee_api_types.h>
 #include <types_ext.h>
 
 /* The state of the unwind process (32-bit mode) */
@@ -102,6 +103,17 @@ static inline void print_kernel_stack(int level __unused)
 }
 
 #endif /* defined(CFG_UNWIND) && (TRACE_LEVEL > 0) */
+
+#ifdef CFG_UNWIND
+TEE_Result relocate_exidx(void *exidx, size_t exidx_sz, int32_t offset);
+#else
+static inline TEE_Result relocate_exidx(void *exidx __unused,
+					size_t exidx_sz __unused,
+					int32_t offset __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+#endif /* CFG_UNWIND  */
 
 #endif /*ASM*/
 

core/arch/arm/include/kernel/user_ta.h
@@ -23,6 +23,7 @@ TAILQ_HEAD(user_ta_elf_head, user_ta_elf);
  * @entry_func:		Entry address in TA
  * @exidx_start:	32-bit TA: start of exception handling index table
  * @exidx_size:		32-bit TA: size of of exception handling index table
+ * @mobj_exidx:         32-bit TA: consolidated EXIDX table (if several ELFs)
  * @is_32bit:		True if 32-bit TA, false if 64-bit TA
  * @open_sessions:	List of sessions opened by this TA
  * @cryp_states:	List of cryp states created by this TA
@@ -43,6 +44,7 @@ struct user_ta_ctx {
 	uaddr_t entry_func;
 	uaddr_t exidx_start;
 	size_t exidx_size;
+	struct mobj *mobj_exidx;
 	bool is_32bit;
 	struct tee_ta_session_head open_sessions;
 	struct tee_cryp_state_head cryp_states;

core/arch/arm/kernel/unwind_arm32.c
@@ -35,6 +35,7 @@
 #include <kernel/misc.h>
 #include <kernel/unwind.h>
 #include <string.h>
+#include <tee_api_types.h>
 #include <trace.h>
 
 /* The register names */
@@ -355,6 +356,41 @@ bool unwind_stack_arm32(struct unwind_state_arm32 *state, uaddr_t exidx,
 		finished = true;
 
 	return !finished;
+}
+
+static uint32_t offset_prel31(uint32_t addr, int32_t offset)
+{
+	return (addr + offset) & 0x7FFFFFFFUL;
+}
+
+TEE_Result relocate_exidx(void *exidx, size_t exidx_sz, int32_t offset)
+{
+	size_t num_items = exidx_sz / sizeof(struct unwind_idx);
+	struct unwind_idx *start = (struct unwind_idx *)exidx;
+	size_t n;
+
+	for (n = 0; n < num_items; n++) {
+		struct unwind_idx *item = &start[n];
+
+		if (item->offset & BIT32(31))
+			return TEE_ERROR_BAD_FORMAT;
+
+		/* Offset to the start of the function has to be adjusted */
+		item->offset = offset_prel31(item->offset, offset);
+
+		if (item->insn == EXIDX_CANTUNWIND)
+			continue;
+		if (item->insn & BIT32(31)) {
+			/* insn is a table entry itself */
+			continue;
+		}
+		/*
+		 * insn is an offset to an entry in .ARM.extab so it has to be
+		 * adjusted
+		 */
+		item->insn = offset_prel31(item->insn, offset);
+	}
+	return TEE_SUCCESS;
 }
 
 #if defined(CFG_UNWIND) && (TRACE_LEVEL > 0)

commit 47e4b229b6c8f16579ce0be68cc4ba4d72e5a4ed

user_ta: tee_ta_init_user_ta_session(): free mobj_exidx in error path
Fixes: b072193efb70 ("arm32: stack unwinding for dynamically linked TAs")

Suggested-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/kernel/user_ta.c
@@ -1048,6 +1048,7 @@ err:
 		vm_info_final(utc);
 		free_elf_states(utc);
 		mobj_free(utc->mobj_stack);
+		mobj_free(utc->mobj_exidx);
 		free_elfs(utc);
 		free(utc);
 	}