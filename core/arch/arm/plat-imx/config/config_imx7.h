c1317794c482bf8be5e82f78c7591af0ee3e71db
Jordan Rhee <jordanrh@microsoft.com>
2018-05-23 15:07:22 -0700

    plat-imx: generic RAM layout for IMX7
    PLATFORM=imx-mx7dsabresd
    
    Name                    Before            After
    TEE_RAM_START           be000000          be000000
    TEE_RAM_VA_SIZE         00100000          00100000
    TEE_RAM_PH_SIZE         00100000          00100000
    TA_RAM_START            be100000          be100000
    TA_RAM_SIZE             01d00000          01d00000
    TEE_SHMEM_START         bfe00000          bfe00000
    TEE_SHMEM_SIZE          00200000          00200000
    TZDRAM_BASE             be000000          be000000
    TZDRAM_SIZE             01e00000          01e00000
    TZSRAM_BASE             00000000          00000000
    TZSRAM_SIZE             00000000          00000000
    TEE_LOAD_ADDR           be000000          be000000
    TEE_RAM_VA_SIZE         00100000          00100000
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Tested-by: Jordan Rhee <jordanrh@microsoft.com>
    Signed-off-by: Jordan Rhee <jordanrh@microsoft.com>

--- core/arch/arm/plat-imx/config/config_imx7.h
+++ core/arch/arm/plat-imx/config/config_imx7.h
@@ -16,35 +16,4 @@
 #define DRAM0_BASE		0x80000000
 #define DRAM0_SIZE		CFG_DDR_SIZE
 
-/* Location of trusted dram */
-#define TZDRAM_BASE		(DRAM0_BASE + CFG_DDR_SIZE - 32 * 1024 * 1024)
-#define TZDRAM_SIZE		(30 * 1024 * 1024)
-
-/* Full GlobalPlatform test suite requires TEE_SHMEM_SIZE to be at least 2MB */
-#define TEE_SHMEM_START		(TZDRAM_BASE + TZDRAM_SIZE)
-#define TEE_SHMEM_SIZE		0x200000
-
-#define TEE_RAM_VA_SIZE		(1024 * 1024)
-
-/*
- * Everything is in TZDRAM.
- * +------------------+
- * |        | TEE_RAM |
- * + TZDRAM +---------+
- * |        | TA_RAM  |
- * +--------+---------+
- */
-#define TEE_RAM_PH_SIZE		TEE_RAM_VA_SIZE
-#define TEE_RAM_START		TZDRAM_BASE
-#define TA_RAM_START		ROUNDUP((TZDRAM_BASE + TEE_RAM_VA_SIZE), \
-					CORE_MMU_DEVICE_SIZE)
-#define TA_RAM_SIZE		ROUNDDOWN((TZDRAM_SIZE - TEE_RAM_VA_SIZE), \
-					  CORE_MMU_DEVICE_SIZE)
-
-#ifdef CFG_TEE_LOAD_ADDR
-#define TEE_LOAD_ADDR		CFG_TEE_LOAD_ADDR
-#else
-#define TEE_LOAD_ADDR		TEE_RAM_START
-#endif
-
 #define CONSOLE_UART_BASE	(CFG_UART_BASE)
