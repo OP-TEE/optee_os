aff8e8dc81e0904a57dc83c2db42eb1c17b62487
Jordan Rhee <jordanrh@microsoft.com>
2018-05-23 15:19:11 -0700

    plat-imx: generic RAM layout for MX6UL and MX6ULL
    PLATFORM=imx-mx6ulevk
    
    Name                  Before          After
    TEE_RAM_START         9e000000        9e000000
    TEE_RAM_VA_SIZE       00100000        00100000
    TEE_RAM_PH_SIZE       00100000        00100000
    TA_RAM_START          9e100000        9e100000
    TA_RAM_SIZE           01d00000        01d00000
    TEE_SHMEM_START       9fe00000        9fe00000
    TEE_SHMEM_SIZE        00200000        00200000
    TZDRAM_BASE           9e000000        9e000000
    TZDRAM_SIZE           01e00000        01e00000
    TZSRAM_BASE           00000000        00000000
    TZSRAM_SIZE           00000000        00000000
    TEE_LOAD_ADDR         9e000000        9e000000
    TEE_RAM_VA_SIZE       00100000        00100000
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Tested-by: Jordan Rhee <jordanrh@microsoft.com>
    Signed-off-by: Jordan Rhee <jordanrh@microsoft.com>

--- core/arch/arm/plat-imx/platform_config.h
+++ core/arch/arm/plat-imx/platform_config.h
@@ -52,53 +52,7 @@
 #error "LPAE not supported for now"
 #endif
 
-
 #define CFG_TEE_CORE_NB_CORE		1
-
-#define DDR_PHYS_START			DRAM0_BASE
-#define DDR_SIZE			DRAM0_SIZE
-
-#define CFG_DDR_START			DDR_PHYS_START
-#define CFG_DDR_SIZE			DDR_SIZE
-
-#ifndef CFG_DDR_TEETZ_RESERVED_START
-#define CFG_DDR_TEETZ_RESERVED_START	0x9E000000
-#endif
-
-#define CFG_DDR_TEETZ_RESERVED_SIZE	0x02000000
-
-#define CFG_PUB_RAM_SIZE	(2 * 1024 * 1024)
-
-#define TZDRAM_BASE		(CFG_DDR_TEETZ_RESERVED_START)
-#define TZDRAM_SIZE		(CFG_DDR_TEETZ_RESERVED_SIZE - \
-				 CFG_PUB_RAM_SIZE)
-
-#define TEE_SHMEM_START		(CFG_DDR_TEETZ_RESERVED_START + \
-				 TZDRAM_SIZE)
-/* Full GlobalPlatform test suite requires TEE_SHMEM_SIZE to be at least 2MB */
-#define TEE_SHMEM_SIZE		CFG_PUB_RAM_SIZE
-
-/*
- * Everything is in TZDRAM.
- * +------------------+
- * |        | TEE_RAM |
- * + TZDRAM +---------+
- * |        | TA_RAM  |
- * +--------+---------+
- */
-#define TEE_RAM_VA_SIZE		(1024 * 1024)
-#define TEE_RAM_PH_SIZE		TEE_RAM_VA_SIZE
-#define TEE_RAM_START		TZDRAM_BASE
-#define TA_RAM_START		ROUNDUP((TZDRAM_BASE + TEE_RAM_VA_SIZE), \
-					CORE_MMU_DEVICE_SIZE)
-#define TA_RAM_SIZE		ROUNDDOWN((TZDRAM_SIZE - TEE_RAM_VA_SIZE), \
-					  CORE_MMU_DEVICE_SIZE)
-
-#ifdef CFG_TEE_LOAD_ADDR
-#define TEE_LOAD_ADDR			CFG_TEE_LOAD_ADDR
-#else
-#define TEE_LOAD_ADDR			TEE_RAM_START
-#endif
 
 #define CONSOLE_UART_BASE	(UART1_BASE)
 
