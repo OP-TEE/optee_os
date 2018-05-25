83b4265ae9787e600931dd489b44ee3abe23b7b1
Volodymyr Babchuk <vlad.babchuk@gmail.com>
2018-05-25 19:56:18 +0300

    linker.h: declare __data_start as non-const
    Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/include/kernel/linker.h
+++ core/arch/arm/include/kernel/linker.h
@@ -65,7 +65,7 @@ extern const uint8_t __end[];
 extern const initcall_t __initcall_start;
 extern const initcall_t __initcall_end;
 
-extern const uint8_t __data_start[];
+extern uint8_t __data_start[];
 extern const uint8_t __data_end[];
 extern const uint8_t __rodata_start[];
 extern const uint8_t __rodata_end[];
