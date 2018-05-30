OP-TEE/optee_os

commit 01b8b5ce011dd291c22133d7bb979984e626f913

TA dev kit: when building a shared library, create symlink with UUID
For convenience for use by tools such as scripts/symbolize.py, create
a symbolic link <uuid>.elf -> libfoo.so.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

ta/arch/arm/link_shlib.mk
@@ -8,11 +8,13 @@ TA_SIGN_KEY ?= $(TA_DEV_KIT_DIR)/keys/default_ta.pem
 
 all: $(link-out-dir)/$(shlibname).so $(link-out-dir)/$(shlibname).dmp \
 	$(link-out-dir)/$(shlibname).stripped.so \
+	$(link-out-dir)/$(shlibuuid).elf \
 	$(link-out-dir)/$(shlibuuid).ta
 
 cleanfiles += $(link-out-dir)/$(shlibname).so
 cleanfiles += $(link-out-dir)/$(shlibname).dmp
 cleanfiles += $(link-out-dir)/$(shlibname).stripped.so
+cleanfiles += $(link-out-dir)/$(shlibuuid).elf
 cleanfiles += $(link-out-dir)/$(shlibuuid).ta
 
 shlink-ldflags  = $(LDFLAGS)
@@ -33,6 +35,10 @@ $(link-out-dir)/$(shlibname).dmp: $(link-out-dir)/$(shlibname).so
 $(link-out-dir)/$(shlibname).stripped.so: $(link-out-dir)/$(shlibname).so
 	@$(cmd-echo-silent) '  OBJCOPY $@'
 	$(q)$(OBJCOPY$(sm)) --strip-unneeded $< $@
+
+$(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
+	@$(cmd-echo-silent) '  LN      $@'
+	$(q)ln -s $< $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
 				$(TA_SIGN_KEY)

commit 88d4467fdcac5a9bebf890c1c83a8c1e0e2d1ac1

TA dev kit: shared library: add -f argument to ln
When creating a symbolic link <uuid>.elf -> libname.so, use the -f (force)
argument so that the command won't fail if the link already exists. It is
a very common case: the first time the library is built and the link is
created. Then whenever some source file is modified, the .so is re-linked
but the symbolic link is there already.

Fixes: 01b8b5ce011d ("TA dev kit: when building a shared library, create symlink with UUID")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/arch/arm/link_shlib.mk
@@ -38,7 +38,7 @@ $(link-out-dir)/$(shlibname).stripped.so: $(link-out-dir)/$(shlibname).so
 
 $(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
 	@$(cmd-echo-silent) '  LN      $@'
-	$(q)ln -s $< $@
+	$(q)ln -sf $< $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
 				$(TA_SIGN_KEY)