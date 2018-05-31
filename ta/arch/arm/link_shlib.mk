OP-TEE/optee_os

commit 01b8b5ce011dd291c22133d7bb979984e626f913

TA dev kit: when building a shared library, create symlink with UUID
For convenience for use by tools such as scripts/symbolize.py, create
a symbolic link <uuid>.elf -> libfoo.so.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

ta/arch/arm/link_shlib.mk
@@ -8,11 +8,13 @@ TA_SIGN_KEY ?= $(TA_DEV_KIT_DIR)/keys/default_ta.pem
 
 all: $(link-out-dir)/$(shlibname).so $(link-out-dir)/$(shlibname).dmp \
 	$(link-out-dir)/$(shlibname).stripped.so \
+	$(link-out-dir)/$(shlibuuid).elf \
 	$(link-out-dir)/$(shlibuuid).ta
 
 cleanfiles += $(link-out-dir)/$(shlibname).so
 cleanfiles += $(link-out-dir)/$(shlibname).dmp
 cleanfiles += $(link-out-dir)/$(shlibname).stripped.so
+cleanfiles += $(link-out-dir)/$(shlibuuid).elf
 cleanfiles += $(link-out-dir)/$(shlibuuid).ta
 
 shlink-ldflags  = $(LDFLAGS)
@@ -33,6 +35,10 @@ $(link-out-dir)/$(shlibname).dmp: $(link-out-dir)/$(shlibname).so
 $(link-out-dir)/$(shlibname).stripped.so: $(link-out-dir)/$(shlibname).so
 	@$(cmd-echo-silent) '  OBJCOPY $@'
 	$(q)$(OBJCOPY$(sm)) --strip-unneeded $< $@
+
+$(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
+	@$(cmd-echo-silent) '  LN      $@'
+	$(q)ln -s $< $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
 				$(TA_SIGN_KEY)

commit c75a5e61ff531cb5583e5114ab85e4dac3975879

TA dev kit: shared library: strip directory part
The symbolic link and its target are in the same directory, so we need
to make sure that there is no relative path before the target filename.
The proper Make variable to use is therefore not $< but $(<F).

Fixes: 01b8b5ce011d ("TA dev kit: when building a shared library, create symlink with UUID")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/arch/arm/link_shlib.mk
@@ -38,7 +38,7 @@ $(link-out-dir)/$(shlibname).stripped.so: $(link-out-dir)/$(shlibname).so
 
 $(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
 	@$(cmd-echo-silent) '  LN      $@'
-	$(q)ln -sf $< $@
+	$(q)ln -sf $(<F) $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
 				$(TA_SIGN_KEY)