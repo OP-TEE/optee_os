OP-TEE/optee_os

commit b7ea03ff2963ac4f3e550d4e9d329c93da2c90e1

libutee: fix TA_CreateEntryPoint() and TA_DestroyEntryPoint()
Fixes issues observed with keep alive single instance TAs:
- TA_CreateEntryPoint() must only be called once,
- TA_DestroyEntryPoint() must not be called when the last session is
closed, because the instance is still alive.

While we're at it, simplify the code a bit by not using a separate
variable (ta_ref_count) to track whether we have sessions or not.
Simply use TAILQ_EMPTY() on the session queue instead.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Kevin Peng <kevinp@marvell.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

lib/libutee/arch/arm/user_ta_entry.c
@@ -60,15 +60,30 @@ struct ta_session {
 static TAILQ_HEAD(ta_sessions, ta_session) ta_sessions =
 		TAILQ_HEAD_INITIALIZER(ta_sessions);
 
-static uint32_t ta_ref_count;
-static bool context_init;
+static bool init_done;
 
 /* From user_ta_header.c, built within TA */
 extern uint8_t ta_heap[];
 extern const size_t ta_heap_size;
+extern struct ta_head ta_head;
 
 uint32_t ta_param_types;
 TEE_Param ta_params[TEE_NUM_PARAMS];
+
+static TEE_Result init_instance(void)
+{
+	trace_set_level(tahead_get_trace_level());
+	__utee_gprof_init();
+	malloc_add_pool(ta_heap, ta_heap_size);
+	_TEE_MathAPI_Init();
+	return TA_CreateEntryPoint();
+}
+
+static void uninit_instance(void)
+{
+	__utee_gprof_fini();
+	TA_DestroyEntryPoint();
+}
 
 static void ta_header_save_params(uint32_t param_types,
 				  TEE_Param params[TEE_NUM_PARAMS])
@@ -95,25 +110,15 @@ static struct ta_session *ta_header_get_session(uint32_t session_id)
 static TEE_Result ta_header_add_session(uint32_t session_id)
 {
 	struct ta_session *itr = ta_header_get_session(session_id);
+	TEE_Result res;
 
 	if (itr)
 		return TEE_SUCCESS;
 
-	ta_ref_count++;
-
-	if (ta_ref_count == 1) {
-		TEE_Result res;
-
-		if (!context_init) {
-			trace_set_level(tahead_get_trace_level());
-			__utee_gprof_init();
-			malloc_add_pool(ta_heap, ta_heap_size);
-			_TEE_MathAPI_Init();
-			context_init = true;
-		}
-
-		res = TA_CreateEntryPoint();
-		if (res != TEE_SUCCESS)
+	if (!init_done) {
+		init_done = true;
+		res = init_instance();
+		if (res)
 			return res;
 	}
 
@@ -137,11 +142,9 @@ static void ta_header_remove_session(uint32_t session_id)
 			TAILQ_REMOVE(&ta_sessions, itr, link);
 			TEE_Free(itr);
 
-			ta_ref_count--;
-			if (ta_ref_count == 0) {
-				__utee_gprof_fini();
-				TA_DestroyEntryPoint();
-			}
+			if (TAILQ_EMPTY(&ta_sessions) &&
+			    !(ta_head.flags & TA_FLAG_INSTANCE_KEEP_ALIVE))
+				uninit_instance();
 
 			return;
 		}

commit 5050ee549d84820bbb7a3ab15232f4e9b831d3d7

libutee: Fix the keepalive condition on last session close
Keepalive condition check should involve single instance flag too, since
the keepalive flag is meaningless if the TA is not single instance.
The same fix was done earlier in the core by commit f9a64f12b542 ("core:
fix the keepalive condition in close session").

Fixes: b7ea03ff2963 ("libutee: fix TA_CreateEntryPoint() and TA_DestroyEntryPoint()")
Signed-off-by: Andrew Gabbasov <andrew_gabbasov@mentor.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/arch/arm/user_ta_entry.c
@@ -114,14 +114,17 @@ static TEE_Result ta_header_add_session(uint32_t session_id)
 static void ta_header_remove_session(uint32_t session_id)
 {
 	struct ta_session *itr;
+	bool keep_alive;
 
 	TAILQ_FOREACH(itr, &ta_sessions, link) {
 		if (itr->session_id == session_id) {
 			TAILQ_REMOVE(&ta_sessions, itr, link);
 			TEE_Free(itr);
 
-			if (TAILQ_EMPTY(&ta_sessions) &&
-			    !(ta_head.flags & TA_FLAG_INSTANCE_KEEP_ALIVE))
+			keep_alive =
+				(ta_head.flags & TA_FLAG_SINGLE_INSTANCE) &&
+				(ta_head.flags & TA_FLAG_INSTANCE_KEEP_ALIVE);
+			if (TAILQ_EMPTY(&ta_sessions) && !keep_alive)
 				uninit_instance();
 
 			return;