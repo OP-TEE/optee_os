OP-TEE/optee_os

commit 2f82082fada310cd58bcb8145d677144e9bfccaa

core: add ddr overall register
register_ddr() is used to add overall DDR address range.
SDP memories, static SHM, secure DDR and so on need to fix the
problem that intersect with the overall DDR.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Edison Ai <edison.ai@arm.com>

core/arch/arm/kernel/generic_boot.c
@@ -716,18 +716,10 @@ static struct core_mmu_phys_mem *get_memory(void *fdt, size_t *nelems)
 	return mem;
 }
 
-static int config_nsmem(void *fdt)
+static int mark_static_shm_as_reserved(void *fdt)
 {
-	struct core_mmu_phys_mem *mem;
-	size_t nelems;
 	vaddr_t shm_start;
 	vaddr_t shm_end;
-
-	mem = get_memory(fdt, &nelems);
-	if (mem)
-		core_mmu_set_discovered_nsec_ddr(mem, nelems);
-	else
-		DMSG("No non-secure memory found in FDT");
 
 	core_mmu_get_mem_by_type(MEM_AREA_NSEC_SHM, &shm_start, &shm_end);
 	if (shm_start != shm_end)
@@ -771,23 +763,32 @@ static void init_fdt(unsigned long phys_fdt)
 		panic();
 	}
 
+	dt_blob_addr = fdt;
+}
+
+static void update_fdt(void)
+{
+	void *fdt = get_dt_blob();
+	int ret;
+
+	if (!fdt)
+		return;
+
 	if (add_optee_dt_node(fdt))
 		panic("Failed to add OP-TEE Device Tree node");
 
 	if (config_psci(fdt))
 		panic("Failed to config PSCI");
 
-	if (config_nsmem(fdt))
+	if (mark_static_shm_as_reserved(fdt))
 		panic("Failed to config non-secure memory");
 
 	ret = fdt_pack(fdt);
 	if (ret < 0) {
 		EMSG("Failed to pack Device Tree at 0x%" PRIxPA ": error %d",
-		     phys_fdt, ret);
+		     virt_to_phys(fdt), ret);
 		panic();
 	}
-
-	dt_blob_addr = fdt;
 }
 
 #else
@@ -795,11 +796,58 @@ static void init_fdt(unsigned long phys_fdt __unused)
 {
 }
 
+static void update_fdt(void)
+{
+}
+
 static void reset_dt_references(void)
 {
 }
 
+void *get_dt_blob(void)
+{
+	return NULL;
+}
+
+static struct core_mmu_phys_mem *get_memory(void *fdt __unused,
+					     size_t *nelems __unused)
+{
+	return NULL;
+}
+
 #endif /*!CFG_DT*/
+
+static void discover_nsec_memory(void)
+{
+	struct core_mmu_phys_mem *mem;
+	size_t nelems;
+	void *fdt = get_dt_blob();
+
+	if (fdt) {
+		mem = get_memory(fdt, &nelems);
+		if (mem) {
+			core_mmu_set_discovered_nsec_ddr(mem, nelems);
+			return;
+		}
+
+		DMSG("No non-secure memory found in FDT");
+	}
+
+	nelems = (&__end_phys_ddr_overall_section -
+		  &__start_phys_ddr_overall_section) / sizeof(*mem);
+	if (!nelems)
+		return;
+
+	/* Platform cannot define nsec_ddr && overall_ddr */
+	assert(&__start_phys_nsec_ddr_section == &__end_phys_nsec_ddr_section);
+
+	mem = calloc(nelems, sizeof(*mem));
+	if (!mem)
+		panic();
+
+	memcpy(mem, &__start_phys_ddr_overall_section, sizeof(*mem) * nelems);
+	core_mmu_set_discovered_nsec_ddr(mem, nelems);
+}
 
 static void init_primary_helper(unsigned long pageable_part,
 				unsigned long nsec_entry, unsigned long fdt)
@@ -819,7 +867,9 @@ static void init_primary_helper(unsigned long pageable_part,
 	thread_init_per_cpu();
 	init_sec_mon(nsec_entry);
 	init_fdt(fdt);
-	configure_console_from_dt(fdt);
+	update_fdt();
+	configure_console_from_dt();
+	discover_nsec_memory();
 
 	IMSG("OP-TEE version: %s", core_v_str);
 

core/arch/arm/include/kernel/linker.h
@@ -35,6 +35,8 @@ extern const struct core_mmu_phys_mem __start_phys_mem_map_section;
 extern const struct core_mmu_phys_mem __end_phys_mem_map_section;
 extern const struct core_mmu_phys_mem __start_phys_nsec_ddr_section;
 extern const struct core_mmu_phys_mem __end_phys_nsec_ddr_section;
+extern const struct core_mmu_phys_mem __start_phys_ddr_overall_section;
+extern const struct core_mmu_phys_mem __end_phys_ddr_overall_section;
 
 #define VCORE_UNPG_RX_PA	((unsigned long)__vcore_unpg_rx_start)
 #define VCORE_UNPG_RX_SZ	((size_t)__vcore_unpg_rx_size)

core/arch/arm/include/mm/core_mmu.h
@@ -124,6 +124,7 @@
  * MEM_AREA_RES_VASPACE: Reserved virtual memory space
  * MEM_AREA_SHM_VASPACE: Virtual memory space for dynamic shared memory buffers
  * MEM_AREA_TA_VASPACE: TA va space, only used with phys_to_virt()
+ * MEM_AREA_DDR_OVERALL: Overall DDR address range, candidate to dynamic shm.
  * MEM_AREA_MAXTYPE:  lower invalid 'type' value
  */
 enum teecore_memtypes {
@@ -145,6 +146,7 @@ enum teecore_memtypes {
 	MEM_AREA_TA_VASPACE,
 	MEM_AREA_PAGER_VASPACE,
 	MEM_AREA_SDP_MEM,
+	MEM_AREA_DDR_OVERALL,
 	MEM_AREA_MAXTYPE
 };
 
@@ -169,6 +171,7 @@ static inline const char *teecore_memtype_name(enum teecore_memtypes type)
 		[MEM_AREA_TA_VASPACE] = "TA_VASPACE",
 		[MEM_AREA_PAGER_VASPACE] = "PAGER_VASPACE",
 		[MEM_AREA_SDP_MEM] = "SDP_MEM",
+		[MEM_AREA_DDR_OVERALL] = "DDR_OVERALL"
 	};
 
 	COMPILE_TIME_ASSERT(ARRAY_SIZE(names) == MEM_AREA_MAXTYPE);
@@ -247,6 +250,11 @@ struct core_mmu_phys_mem {
 #define register_dynamic_shm(addr, size) \
 		__register_memory1(#addr, MEM_AREA_RAM_NSEC, (addr), (size), \
 				   phys_nsec_ddr_section, __COUNTER__)
+
+#define register_ddr(addr, size) \
+		__register_memory1(#addr, MEM_AREA_DDR_OVERALL, (addr), \
+				   (size), phys_ddr_overall_section,\
+				   __COUNTER__)
 
 /* Default NSec shared memory allocated from NSec world */
 extern unsigned long default_nsec_shm_paddr;
@@ -568,10 +576,8 @@ void map_memarea_sections(const struct tee_mmap_region *mm, uint32_t *ttb);
  */
 bool core_mmu_nsec_ddr_is_defined(void);
 
-#ifdef CFG_DT
 void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 				      size_t nelems);
-#endif
 
 #ifdef CFG_SECURE_DATA_PATH
 /* Alloc and fill SDP memory objects table - table is NULL terminated */

core/arch/arm/kernel/kern.ld.S
@@ -163,6 +163,10 @@ SECTIONS
 		__start_phys_nsec_ddr_section = . ;
 		KEEP(*(phys_nsec_ddr_section))
 		__end_phys_nsec_ddr_section = . ;
+		. = ALIGN(8);
+		__start_phys_ddr_overall_section = . ;
+		KEEP(*(phys_ddr_overall_section))
+		__end_phys_ddr_overall_section = . ;
 #endif
 		. = ALIGN(8);
 		__rodata_end = .;
@@ -337,6 +341,10 @@ SECTIONS
 		__start_phys_nsec_ddr_section = . ;
 		KEEP(*(phys_nsec_ddr_section))
 		__end_phys_nsec_ddr_section = . ;
+		. = ALIGN(8);
+		__start_phys_ddr_overall_section = . ;
+		KEEP(*(phys_ddr_overall_section))
+		__end_phys_ddr_overall_section = . ;
 		. = ALIGN(8);
 		__rodata_init_end = .;
 	}

core/arch/arm/kernel/link_dummy.ld
@@ -27,6 +27,8 @@ __end = .;
 __end_phys_mem_map_section = .;
 __end_phys_nsec_ddr_section = .;
 __end_phys_sdp_mem_section = .;
+__end_phys_ddr_overall_section = .;
+__start_phys_ddr_overall_section = .;
 __exidx_end = .;
 __exidx_start = .;
 __extab_end = .;

core/arch/arm/mm/core_mmu.c
@@ -226,7 +226,6 @@ static bool pbuf_is_special_mem(paddr_t pbuf, size_t len,
 	return false;
 }
 
-#ifdef CFG_DT
 static void carve_out_phys_mem(struct core_mmu_phys_mem **mem, size_t *nelems,
 			       paddr_t pa, size_t size)
 {
@@ -354,14 +353,6 @@ static bool get_discovered_nsec_ddr(const struct core_mmu_phys_mem **start,
 
 	return true;
 }
-#else /*!CFG_DT*/
-static bool
-get_discovered_nsec_ddr(const struct core_mmu_phys_mem **start __unused,
-			const struct core_mmu_phys_mem **end __unused)
-{
-	return false;
-}
-#endif /*!CFG_DT*/
 
 static bool pbuf_is_nsec_ddr(paddr_t pbuf, size_t len)
 {

core/arch/arm/plat-vexpress/main.c
@@ -82,9 +82,9 @@ register_phys_mem(MEM_AREA_RAM_SEC, TZCDRAM_BASE, TZCDRAM_SIZE);
 register_phys_mem(MEM_AREA_IO_SEC, SECRAM_BASE, SECRAM_COHERENT_SIZE);
 #endif
 register_phys_mem(MEM_AREA_IO_SEC, CONSOLE_UART_BASE, PL011_REG_SIZE);
-register_dynamic_shm(DRAM0_BASE, DRAM0_SIZE);
+register_ddr(DRAM0_BASE, DRAM0_SIZE);
 #ifdef DRAM1_BASE
-register_dynamic_shm(DRAM1_BASE, DRAM1_SIZE);
+register_ddr(DRAM1_BASE, DRAM1_SIZE);
 #endif
 
 const struct thread_handlers *generic_boot_get_handlers(void)

core/include/console.h
@@ -16,9 +16,9 @@ struct serial_chip;
 void register_serial_console(struct serial_chip *chip);
 
 #ifdef CFG_DT
-void configure_console_from_dt(unsigned long phys_fdt);
+void configure_console_from_dt(void);
 #else
-static inline void configure_console_from_dt(unsigned long phys_fdt __unused)
+static inline void configure_console_from_dt(void)
 {}
 #endif /* !CFG_DT */
 

commit 93ef79d8dfb560708af5af76437a0d62cf0a6fd8

core: don't divide by sizeof(*mem) for ddr nsec memory
Since the two addresses are already of type struct core_mmu_phys_mem, do
not divide by sizeof(struct core_mmu_phys_mem). This broke dynamic shared
memory on Juno r0, since nelem would be zero for two slots.

Tested on Juno r0.

Fixes: 2f82082fada3 ("core: add ddr overall register")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

core/arch/arm/kernel/generic_boot.c
@@ -838,7 +838,7 @@ static void discover_nsec_memory(void)
 	}
 
 	nelems = (&__end_phys_ddr_overall_section -
-		  &__start_phys_ddr_overall_section) / sizeof(*mem);
+		  &__start_phys_ddr_overall_section);
 	if (!nelems)
 		return;
 