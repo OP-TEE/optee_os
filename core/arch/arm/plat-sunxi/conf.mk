e59d8fd7da16ebf72d2ce7505a5f0ae28ff226c9
Amit Singh Tomar <amittomer25@gmail.com>
2018-06-22 00:28:32 +0530

    plat-sunxi: Add Allwinner A64 support
    This commit adds support for pine64[1] board based on
    Allwinner's A64 SoC.
    
    1. Build command
    
        # make PLATFORM=sunxi-sun50i_a64
    
    2. Pass optee_example_hello_world and xtest.
    
    [1]: https://www.pine64.org/
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Amit Singh Tomar <amittomer25@gmail.com>

--- core/arch/arm/plat-sunxi/conf.mk
+++ core/arch/arm/plat-sunxi/conf.mk
@@ -1,16 +1,15 @@
 PLATFORM_FLAVOR ?= bpi_zero
 
-include core/arch/arm/cpu/cortex-a7.mk
-
-ta-targets = ta_arm32
-
-$(call force,CFG_SUN8I_H2_PLUS,y)
-$(call force,CFG_ARM32_core,y)
 $(call force,CFG_GENERIC_BOOT,y)
-$(call force,CFG_GIC,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 $(call force,CFG_8250_UART,y)
 $(call force,CFG_PM_STUBS,y)
-$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+
+ifeq ($(PLATFORM_FLAVOR),bpi_zero)
+include core/arch/arm/cpu/cortex-a7.mk
+$(call force,CFG_SUN8I_H2_PLUS,y)
+$(call force,CFG_ARM32_core,y)
+$(call force,CFG_GIC,y)
 $(call force,CFG_WITH_LPAE,n)
 $(call force,CFG_WITH_PAGER,n)
 
@@ -20,8 +19,6 @@ CFG_TEE_CORE_NB_CORE ?= 4
 CFG_WITH_STACK_CANARIES ?= y
 CFG_BOOT_SECONDARY_REQUEST ?= y
 CFG_PSCI_ARM32 ?= y
-CFG_WITH_STACK_CANARIES ?= y
-CFG_WITH_STATS ?= y
 CFG_NS_ENTRY_ADDR ?= 0x42000000
 CFG_DT ?= y
 CFG_INIT_CNTVOFF ?= y
@@ -30,3 +27,27 @@ CFG_TZDRAM_START ?= 0x5c000000
 CFG_TZDRAM_SIZE ?= 0x03e00000
 CFG_SHMEM_START ?= 0x5fe00000
 CFG_SHMEM_SIZE ?= 0x00200000
+endif
+
+ta-targets = ta_arm32
+
+ifeq ($(PLATFORM_FLAVOR),sun50i_a64)
+include core/arch/arm/cpu/cortex-armv8-0.mk
+$(call force,CFG_ARM64_core,y)
+$(call force,CFG_WITH_LPAE,y)
+ta-targets += ta_arm64
+
+CFG_TZDRAM_START ?= 0x40000000
+CFG_TZDRAM_SIZE  ?= 0x2000000
+CFG_SHMEM_START  ?= 0x44000000
+CFG_SHMEM_SIZE   ?= 0x00400000
+CFG_TEE_CORE_NB_CORE ?= 4
+CFG_TZC380 ?= y
+endif
+
+ifeq ($(platform-flavor-armv8),1)
+$(call force,CFG_WITH_ARM_TRUSTED_FW,y)
+endif
+
+CFG_WITH_STACK_CANARIES ?= y
+CFG_WITH_STATS ?= y
--- core/arch/arm/plat-sunxi/platform_config.h
+++ core/arch/arm/plat-sunxi/platform_config.h
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2014, Allwinner Technology Co., Ltd.
  * Copyright (c) 2018, Linaro Limited
+ * Copyright (c) 2018, Amit Singh Tomar <amittomer25@gmail.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,25 +34,29 @@
 #include <mm/generic_ram_layout.h>
 
 /* Make stacks aligned to data cache line length */
-#define STACK_ALIGNMENT		32
+#define STACK_ALIGNMENT		64
 
 /* 16550 UART */
 #define CONSOLE_UART_BASE	0x01c28000 /* UART0 */
 #define CONSOLE_UART_CLK_IN_HZ	24000000
 #define CONSOLE_BAUDRATE	115200
 #define SUNXI_UART_REG_SIZE	0x400
+
+#if defined(PLATFORM_FLAVOR_bpi_zero)
 #define GIC_BASE		0x01c80000
 #define GICC_OFFSET		0x2000
 #define GICD_OFFSET		0x1000
-#define SMC_BASE		0x01c1e000
 #define SUNXI_TZPC_BASE		0x01c23400
 #define SUNXI_TZPC_REG_SIZE	0x400
-#define SUNXI_CPUCFG_BASE		0x01f01c00
+#define SUNXI_CPUCFG_BASE	0x01f01c00
 #define SUNXI_CPUCFG_REG_SIZE	0x400
 #define SUNXI_PRCM_BASE		0x01f01400
 #define SUNXI_PRCM_REG_SIZE	0x400
-#define PRCM_CPU_SOFT_ENTRY_REG   (0x164)
+#define PRCM_CPU_SOFT_ENTRY_REG	0x164
+#endif
 
-#define DRAM0_SIZE		0x20000000
+#if defined(PLATFORM_FLAVOR_sun50i_a64)
+#define SUNXI_SMC_BASE		0x01c1e000
+#endif
 
 #endif /* PLATFORM_CONFIG_H */
