35b4ef75d056aa8e5657df14af016c31f1ad6c0c
Jens Wiklander <jens.wiklander@linaro.org>
2018-07-17 15:47:46 +0200

    ta: prepare for in-tree ta building
    Prepare for in-tree TA building by adding $(sm) to all TA dev-kit
    variables that may cause conflicts either with the core linking or when
    linking multiple TAs.
    
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- ta/arch/arm/link.mk
+++ ta/arch/arm/link.mk
@@ -1,26 +1,26 @@
-link-out-dir = $(out-dir)
+link-script$(sm) = $(ta-dev-kit-dir$(sm))/src/ta.ld.S
+link-script-pp$(sm) = $(link-out-dir$(sm))/ta.lds
+link-script-dep$(sm) = $(link-out-dir$(sm))/.ta.ld.d
 
-link-script = $(TA_DEV_KIT_DIR)/src/ta.ld.S
-link-script-pp = $(link-out-dir)/ta.lds
-link-script-dep = $(link-out-dir)/.ta.ld.d
+SIGN = $(ta-dev-kit-dir$(sm))/scripts/sign.py
+TA_SIGN_KEY ?= $(ta-dev-kit-dir$(sm))/keys/default_ta.pem
 
-SIGN = $(TA_DEV_KIT_DIR)/scripts/sign.py
-TA_SIGN_KEY ?= $(TA_DEV_KIT_DIR)/keys/default_ta.pem
-
-all: $(link-out-dir)/$(binary).elf $(link-out-dir)/$(binary).dmp \
-	$(link-out-dir)/$(binary).stripped.elf $(link-out-dir)/$(binary).ta
-cleanfiles += $(link-out-dir)/$(binary).elf $(link-out-dir)/$(binary).dmp
-cleanfiles += $(link-out-dir)/$(binary).map
-cleanfiles += $(link-out-dir)/$(binary).stripped.elf
-cleanfiles += $(link-out-dir)/$(binary).ta
-cleanfiles += $(link-script-pp) $(link-script-dep)
+all: $(link-out-dir$(sm))/$(binary).dmp \
+	$(link-out-dir$(sm))/$(binary).stripped.elf \
+	$(link-out-dir$(sm))/$(binary).ta
+cleanfiles += $(link-out-dir$(sm))/$(binary).elf
+cleanfiles += $(link-out-dir$(sm))/$(binary).dmp
+cleanfiles += $(link-out-dir$(sm))/$(binary).map
+cleanfiles += $(link-out-dir$(sm))/$(binary).stripped.elf
+cleanfiles += $(link-out-dir$(sm))/$(binary).ta
+cleanfiles += $(link-script-pp$(sm)) $(link-script-dep$(sm))
 
 link-ldflags  = -pie
-link-ldflags += -T $(link-script-pp) -Map=$(link-out-dir)/$(binary).map
+link-ldflags += -T $(link-script-pp$(sm))
+link-ldflags += -Map=$(link-out-dir$(sm))/$(binary).map
 link-ldflags += --sort-section=alignment
 
-link-ldadd  = $(LDADD)
-link-ldadd += $(addprefix -L,$(libdirs))
+link-ldadd  = $(user-ta-ldadd) $(addprefix -L,$(libdirs))
 link-ldadd += --start-group $(addprefix -l,$(libnames)) --end-group
 ldargs-$(binary).elf := $(link-ldflags) $(objs) $(link-ldadd)
 
@@ -28,31 +28,35 @@ ldargs-$(binary).elf := $(link-ldflags) $(objs) $(link-ldadd)
 link-script-cppflags-$(sm) := -DASM=1 \
 	$(filter-out $(CPPFLAGS_REMOVE) $(cppflags-remove), \
 		$(nostdinc$(sm)) $(CPPFLAGS) \
-		$(addprefix -I,$(incdirs$(sm)) $(link-out-dir)) \
+		$(addprefix -I,$(incdirs$(sm)) $(link-out-dir$(sm))) \
 		$(cppflags$(sm)))
 
--include $(link-script-dep)
+-include $(link-script-dep$(sm))
 
-$(link-script-pp): $(link-script) $(MAKEFILE_LIST)
+$(link-script-pp$(sm)): $(link-script$(sm)) $(MAKEFILE_LIST)
 	@$(cmd-echo-silent) '  CPP     $@'
 	$(q)mkdir -p $(dir $@)
-	$(q)$(CPP$(sm)) -Wp,-P,-MT,$@,-MD,$(link-script-dep) \
+	$(q)$(CPP$(sm)) -Wp,-P,-MT,$@,-MD,$(link-script-dep$(sm)) \
 		$(link-script-cppflags-$(sm)) $< > $@
 
-$(link-out-dir)/$(binary).elf: $(objs) $(libdeps) $(link-script-pp)
+$(link-out-dir$(sm))/$(binary).elf: $(objs) $(libdeps) \
+					  $(link-script-pp$(sm))
 	@$(cmd-echo-silent) '  LD      $@'
 	$(q)$(LD$(sm)) $(ldargs-$(binary).elf) -o $@
 
-$(link-out-dir)/$(binary).dmp: $(link-out-dir)/$(binary).elf
+$(link-out-dir$(sm))/$(binary).dmp: \
+			$(link-out-dir$(sm))/$(binary).elf
 	@$(cmd-echo-silent) '  OBJDUMP $@'
 	$(q)$(OBJDUMP$(sm)) -l -x -d $< > $@
 
-$(link-out-dir)/$(binary).stripped.elf: $(link-out-dir)/$(binary).elf
+$(link-out-dir$(sm))/$(binary).stripped.elf: \
+			$(link-out-dir$(sm))/$(binary).elf
 	@$(cmd-echo-silent) '  OBJCOPY $@'
 	$(q)$(OBJCOPY$(sm)) --strip-unneeded $< $@
 
-$(link-out-dir)/$(binary).ta: $(link-out-dir)/$(binary).stripped.elf \
-				$(TA_SIGN_KEY)
+$(link-out-dir$(sm))/$(binary).ta: \
+			$(link-out-dir$(sm))/$(binary).stripped.elf \
+			$(TA_SIGN_KEY)
 	@echo '  SIGN    $@'
 	$(q)$(SIGN) --key $(TA_SIGN_KEY) --uuid $(binary) --version 0 \
 		--in $< --out $@
