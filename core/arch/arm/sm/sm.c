976835541b0950c128936f95a91a78f7f5a429b5
Jens Wiklander <jens.wiklander@linaro.org>
2018-08-14 10:29:17 +0200

    core: arm: sm: optimize padding in  struct sm_ctx
    Removes redundant padding in struct sm_ctx and sub-structs with regards
    to CFG_SM_NO_CYCLE_COUNTING. Saves 4 bytes per core if
    CFG_SM_NO_CYCLE_COUNTING is defined.
    
    Removes assumptions in monitor assembly code about where the padding in
    struct sm_ctx is located.
    
    Adds compile time asserts are added to check that struct sm_ctx is
    properly aligned.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU virt)
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/sm/sm.c
+++ core/arch/arm/sm/sm.c
@@ -17,6 +17,15 @@ bool sm_from_nsec(struct sm_ctx *ctx)
 {
 	uint32_t *nsec_r0 = (uint32_t *)(&ctx->nsec.r0);
 
+	/*
+	 * Check that struct sm_ctx has the different parts properly
+	 * aligned since the stack pointer will be updated to point at
+	 * different parts of this struct.
+	 */
+	COMPILE_TIME_ASSERT(!(offsetof(struct sm_ctx, sec.r0) % 8));
+	COMPILE_TIME_ASSERT(!(offsetof(struct sm_ctx, nsec.r0) % 8));
+	COMPILE_TIME_ASSERT(!(sizeof(struct sm_ctx) % 8));
+
 	if (!sm_platform_handler(ctx))
 		return false;
 
