OP-TEE/optee_os

commit cfa34ec63699e26e3eb674212fe6401fab32e98c

abort.c: manipulate with VFP state only if thread is active
abort_handler() can be called both within and without thread
context. In the latter case it stops on
assert(thread_get_exceptions() & THREAD_EXCP_FOREIGN_INTR)
in thread_kernel_save_vfp() and no information about abort is displayed.

This assert fires during some initialization stages and during
fast SMCs, because they are handled with foreign interrupts disabled.

To fix this, we should call thread_kernel_{save,restore}_vfp() only
when foreign interrupts are enabled.

Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

core/arch/arm/kernel/abort.c
@@ -680,9 +680,11 @@ void abort_handler(uint32_t abort_type, struct thread_abort_regs *regs)
 #endif
 	case FAULT_TYPE_PAGEABLE:
 	default:
-		thread_kernel_save_vfp();
+		if (!thread_foreign_intr_disabled())
+			thread_kernel_save_vfp();
 		handled = tee_pager_handle_fault(&ai);
-		thread_kernel_restore_vfp();
+		if (!thread_foreign_intr_disabled())
+			thread_kernel_restore_vfp();
 		if (!handled) {
 			abort_print_error(&ai);
 			if (!abort_is_user_exception(&ai))

commit 2f0a1a537ce67cb23a4fc8cc7d98281a28d93615

abort.c: always save VFP state with thread context
Saving VFP state requires a thread context, if none is available print
abort info and panic().

Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
Fixes: cfa34ec63699 ("abort.c: manipulate with VFP state only if thread is active")
Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (Hikey AArch64 pager)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/abort.c
@@ -680,11 +680,13 @@ void abort_handler(uint32_t abort_type, struct thread_abort_regs *regs)
 #endif
 	case FAULT_TYPE_PAGEABLE:
 	default:
-		if (!thread_foreign_intr_disabled())
-			thread_kernel_save_vfp();
+		if (thread_get_id_may_fail() < 0) {
+			abort_print_error(&ai);
+			panic("abort outside thread context");
+		}
+		thread_kernel_save_vfp();
 		handled = tee_pager_handle_fault(&ai);
-		if (!thread_foreign_intr_disabled())
-			thread_kernel_restore_vfp();
+		thread_kernel_restore_vfp();
 		if (!handled) {
 			abort_print_error(&ai);
 			if (!abort_is_user_exception(&ai))