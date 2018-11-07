9cc392772c2ae24bf9dbb71810750b08fea054e4
Jens Wiklander <jens.wiklander@linaro.org>
2018-11-07 08:11:29 +0100

    libmbedtls: make mbedtls_mpi_mont*() available
    Makes mbedtls_mpi_montg_init(), mbedtls_mpi_montmul() and
    mbedtls_mpi_montred() available for external use.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    [jf: rebased onto mbedtls-2.22.0]
    [jf: rebased onto mbedtls-2.27.0, keep static functions]
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    [jf: rebased onto mbedtls-2.28.1]
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- lib/libmbedtls/mbedtls/library/bignum.c
+++ lib/libmbedtls/mbedtls/library/bignum.c
@@ -1886,6 +1886,11 @@ static void mpi_montg_init( mbedtls_mpi_uint *mm, const mbedtls_mpi *N )
     *mm = ~x + 1;
 }
 
+void mbedtls_mpi_montg_init( mbedtls_mpi_uint *mm, const mbedtls_mpi *N )
+{
+	mpi_montg_init( mm, N );
+}
+
 /** Montgomery multiplication: A = A * B * R^-1 mod N  (HAC 14.36)
  *
  * \param[in,out]   A   One of the numbers to multiply.
@@ -1955,6 +1960,12 @@ static void mpi_montmul( mbedtls_mpi *A, const mbedtls_mpi *B, const mbedtls_mpi
     mbedtls_ct_mpi_uint_cond_assign( n, A->p, d, (unsigned char) d[n] );
 }
 
+void mbedtls_mpi_montmul( mbedtls_mpi *A, const mbedtls_mpi *B, const mbedtls_mpi *N, mbedtls_mpi_uint mm,
+                          const mbedtls_mpi *T )
+{
+    mpi_montmul( A, B, N, mm, T);
+}
+
 /*
  * Montgomery reduction: A = A * R^-1 mod N
  *
@@ -1970,6 +1981,12 @@ static void mpi_montred( mbedtls_mpi *A, const mbedtls_mpi *N,
     U.p = &z;
 
     mpi_montmul( A, &U, N, mm, T );
+}
+
+void mbedtls_mpi_montred( mbedtls_mpi *A, const mbedtls_mpi *N,
+                          mbedtls_mpi_uint mm, const mbedtls_mpi *T )
+{
+    mpi_montred( A, N, mm, T );
 }
 
 /**
