36395e57399a8205f75c2e990c4eb95e1f6c309a
Jens Wiklander <jens.wiklander@linaro.org>
2018-11-08 11:18:22 +0100

    libmbedtls: add mbedtls_mpi_init_mempool()
    Adds mbedtls_mpi_init_mempool() which initializes a mbedtls_mpi struct
    to use the mempool mbedtls_mpi_mempool if configured for memory
    allocation. All local memory allocation are changed to use
    mbedtls_mpi_init_mempool() instead of mbedtls_mpi_init(). This will give
    a stack like alloc/free pattern for which the mempool is optimized.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    [jf: rebased onto mbedtls-2.22.0]
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

--- lib/libmbedtls/mbedtls/include/mbedtls/bignum.h
+++ lib/libmbedtls/mbedtls/include/mbedtls/bignum.h
@@ -185,11 +185,14 @@ extern "C" {
  */
 typedef struct mbedtls_mpi
 {
-    int s;              /*!<  Sign: -1 if the mpi is negative, 1 otherwise */
+    short s;            /*!<  Sign: -1 if the mpi is negative, 1 otherwise */
+    short use_mempool;
     size_t n;           /*!<  total # of limbs  */
     mbedtls_mpi_uint *p;          /*!<  pointer to limbs  */
 }
 mbedtls_mpi;
+
+extern void *mbedtls_mpi_mempool;
 
 /**
  * \brief           Initialize an MPI context.
@@ -200,6 +203,7 @@ mbedtls_mpi;
  * \param X         The MPI context to initialize. This must not be \c NULL.
  */
 void mbedtls_mpi_init( mbedtls_mpi *X );
+void mbedtls_mpi_init_mempool( mbedtls_mpi *X );
 
 /**
  * \brief          This function frees the components of an MPI context.
