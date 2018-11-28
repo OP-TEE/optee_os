7294172cdf5790c65f22f18c9ae9d68285150da2
Etienne Carriere <etienne.carriere@st.com>
2018-11-28 09:45:11 +0100

    stm32mp1: release constraint on core count
    Minor cleaning and allow CFG_TEE_CORE_NB_CORE to be set to 1.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -3,8 +3,6 @@ PLATFORM_FLAVOR ?= stm32mp157c
 include core/arch/arm/cpu/cortex-a7.mk
 ta-targets = ta_arm32
 
-$(call force,CFG_TEE_CORE_NB_CORE,2)
-$(call force,CFG_ARM32_core,y)
 $(call force,CFG_BOOT_SECONDARY_REQUEST,y)
 $(call force,CFG_GENERIC_BOOT,y)
 $(call force,CFG_GIC,y)
@@ -19,12 +17,13 @@ CFG_TZSRAM_START ?= 0x2ffc0000
 CFG_TZSRAM_SIZE  ?= 0x00040000
 CFG_TZDRAM_START ?= 0xfe000000
 CFG_TZDRAM_SIZE  ?= 0x01e00000
-CFG_SHMEM_SIZE   ?= 0x00200000
 CFG_SHMEM_START  ?= 0xffe00000
+CFG_SHMEM_SIZE   ?= 0x00200000
 
-CFG_WITH_PAGER		?= y
-CFG_WITH_LPAE		?= y
-CFG_WITH_STACK_CANARIES	?= y
+CFG_TEE_CORE_NB_CORE ?= 2
+CFG_WITH_PAGER ?= y
+CFG_WITH_LPAE ?= y
+CFG_WITH_STACK_CANARIES ?= y
 
 CFG_STM32_UART ?= y
 
