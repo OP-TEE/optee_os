OP-TEE/optee_os

commit d276907c8c4f85c3227a28b81f34b74c324806c7

core: drivers: serial8250_uart: Add DT support
Add DT support for serial8250 uart driver. The matchtable currently
supports allwinner H2+ SoC.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

core/drivers/serial8250_uart.c
@@ -9,6 +9,7 @@
 #include <io.h>
 #include <keep.h>
 #include <util.h>
+#include <kernel/dt.h>
 
 /* uart register defines */
 #define UART_RHR	0x0
@@ -98,3 +99,69 @@ void serial8250_uart_init(struct serial8250_uart_data *pd, paddr_t base,
 	 * everything for uart0 is ready now.
 	 */
 }
+
+#ifdef CFG_DT
+
+static struct serial_chip *serial8250_uart_dev_alloc(void)
+{
+	struct serial8250_uart_data *pd = malloc(sizeof(*pd));
+
+	if (!pd)
+		return NULL;
+	return &pd->chip;
+}
+
+static int serial8250_uart_dev_init(struct serial_chip *chip,
+			       const void *fdt,
+			       int offs,
+			       const char *parms)
+{
+	struct serial8250_uart_data *pd =
+		container_of(chip, struct serial8250_uart_data, chip);
+	vaddr_t vbase;
+	paddr_t pbase;
+	size_t size;
+
+	if (parms && parms[0])
+		IMSG("serial8250_uart: device parameters ignored (%s)", parms);
+
+	if (dt_map_dev(fdt, offs, &vbase, &size) < 0)
+		return -1;
+
+	if (size < SERIAL8250_UART_REG_SIZE) {
+		EMSG("serial8250_uart: register size too small: %zx", size);
+		return -1;
+	}
+
+	pbase = virt_to_phys((void *)vbase);
+	serial8250_uart_init(pd, pbase, 0, 0);
+
+	return 0;
+}
+
+static void serial8250_uart_dev_free(struct serial_chip *chip)
+{
+	struct serial8250_uart_data *pd =
+	  container_of(chip,  struct serial8250_uart_data, chip);
+
+	free(pd);
+}
+
+static const struct serial_driver serial8250_driver = {
+	.dev_alloc = serial8250_uart_dev_alloc,
+	.dev_init = serial8250_uart_dev_init,
+	.dev_free = serial8250_uart_dev_free,
+};
+
+static const struct dt_device_match serial8250_match_table[] = {
+	{ .compatible = "snps,dw-apb-uart" },
+	{ 0 }
+};
+
+const struct dt_driver serial8250_dt_driver __dt_driver = {
+	.name = "serial8250_uart",
+	.match_table = serial8250_match_table,
+	.driver = &serial8250_driver,
+};
+
+#endif /* CFG_DT */

commit 04ab6f0f0f273b0a743937c63a13c3293c39c907

core: serial8250_uart_dev_init(): use calloc()
Allocate pl011_data with calloc() instead of malloc() get initialized
memory.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Fixes: d276907c8c4f ("core: drivers: serial8250_uart: Add DT support")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/serial8250_uart.c
@@ -104,7 +104,7 @@ void serial8250_uart_init(struct serial8250_uart_data *pd, paddr_t base,
 
 static struct serial_chip *serial8250_uart_dev_alloc(void)
 {
-	struct serial8250_uart_data *pd = malloc(sizeof(*pd));
+	struct serial8250_uart_data *pd = calloc(1, sizeof(*pd));
 
 	if (!pd)
 		return NULL;