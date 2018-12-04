2a77c64a8d4f374a945b19d5481c4b3dd60ad4d5
Cedric Neveux <cedric.neveux@nxp.com>
2018-12-04 09:39:52 +0100

    core: system drivers object
       Update after review
       - remove drivers data management
       - rename default power state
       - remove debug build flag/macro
       - review comment to be linux kernel style
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/arch/arm/tee/drv_mgt.c
+++ core/arch/arm/tee/drv_mgt.c
@@ -1,12 +1,31 @@
 // SPDX-License-Identifier: BSD-2-Clause
-/**
- * @copyright 2018 NXP
+/*
+ * Copyright 2018 NXP
  *
- * @file    drv_mgt.c
+ * File    drv_mgt.c
  *
- * @brief   Global management of drivers registered with DRIVER_EXPORT
- *          macro and using the struct driver format (driver data and
- *          operations)
+ * Brief   Global management of drivers registered with REGISTER_DRIVER
+ *         macro and using the struct driver format (driver data and
+ *         operations).
+ *
+ * The concept of driver object is introduced in OP-TEE through the
+ * `struct driver` available in the `core/include/drivers/driver.h`
+ * include file.
+ *
+ * This object has been designed to allow power management at driver level,
+ * hence to prepare the driver to enter in power transition.
+ * This mechanism is useful for driver:
+ *   - using DMA (Direct Memory Access) transfers to a memory that will
+ *   be shutdown
+ *   - losing configuration because clocks and/or power domain are shutdown
+ *
+ * Exported APIs to be called in the power management framework (e.g. PSCI)
+ *
+ *  - Switch in a power mode
+ * TEE_Result drivers_pm_enter(enum drv_pwrmode mode, bool wait)
+ *
+ *  - Resume from power mode
+ * void drivers_pm_resume(void)
  */
 
 /* Global includes */
@@ -15,121 +34,48 @@
 #include <kernel/spinlock.h>
 #include <trace.h>
 
-#ifdef DRV_MGT_DEBUG
-#define MGTDRV_TRACE		DMSG
-#else
-#define MGTDRV_TRACE(...)
-#endif
-
-/**
- * @brief spinlock object to operate on the driver list
+/*
+ * spinlock object to operate on the driver list
  */
 unsigned int drv_spinlock = SPINLOCK_UNLOCK;
 
-/**
- * @brief   Driver reference object building a chained
- *          list of drivers
+/*
+ * Driver reference object building a chained
+ * list of drivers
  */
 struct drv_ref {
-	const struct driver  *drv;  ///< Register Driver structure
-	enum drv_pwrmode     mode;  ///< Driver power mode
-	void                 *data; ///< Driver's data if any
+	const struct driver  *drv;  // Register Driver structure
+	enum drv_pwrmode     mode;  // Driver power mode
 
-	SLIST_ENTRY(drv_ref) next;  ///< Link to the next driver in the list
+	SLIST_ENTRY(drv_ref) next;  // Link to the next driver in the list
 };
 
 static SLIST_HEAD(, drv_ref) drv_list = SLIST_HEAD_INITIALIZER(drv_ref);
 
-/**
- * @brief   Allocation of the driver's data. This function is a weak
- *          function that can be overwritten in the platform
- *          implementation
+/*
+ * brief   Installation of all registered drivers.
+ *         Add all drivers in the drivers list in order
+ *         to maintain drivers function of system power.
  *
- * @param[in]  size   Data size to allocate
- *
- * @retval  address of the data buffer allocated
- * @retval  NULL if allocation error
- */
-void __weak *drivers_alloc_data(size_t size)
-{
-#ifdef DRV_MGT_DEBUG
-	MGTDRV_TRACE("Allocate %d bytes for driver", size);
-#endif
-	return malloc(size);
-}
-
-/**
- * @brief   Save the drivers data to enter in a power \a mode.
- *
- * @param[in]  mode   Power mode to enter
- * @param[in]  drv    Driver object
- * @param[in]  data   Driver's data
- *
- * @retval  TEE_SUCCESS        Success
- * @retval  TEE_ERROR_GENERIC  Generic error
- */
-TEE_Result __weak drivers_save_data(enum drv_pwrmode mode __maybe_unused,
-		const struct driver *drv __maybe_unused,
-		void *data __maybe_unused)
-{
-#ifdef DRV_MGT_DEBUG
-	MGTDRV_TRACE(
-	"Driver [%s] save data @0x%"PRIxPTR" (%d bytes) to enter mode %d",
-	drv->name, (uintptr_t)data, drv->data_size, mode);
-#endif
-	return TEE_SUCCESS;
-}
-
-/**
- * @brief   Restore the drivers data after resuning from power \a mode.
- *
- * @param[in]  mode   Power mode to enter
- * @param[in]  drv    Driver object
- * @param[in]  data   Driver's data
- *
- * @retval  TEE_SUCCESS        Success
- * @retval  TEE_ERROR_GENERIC  Generic error
- */
-TEE_Result __weak drivers_restore_data(enum drv_pwrmode mode __maybe_unused,
-		const struct driver *drv __maybe_unused,
-		void *data __maybe_unused)
-{
-#ifdef DRV_MGT_DEBUG
-	MGTDRV_TRACE(
-	"Driver [%s] restore data @0x%"PRIxPTR" (%d bytes) after mode %d",
-	drv->name, (uintptr_t)data, drv->data_size, mode);
-#endif
-	return TEE_SUCCESS;
-}
-
-/**
- * @brief   Installation of all registered drivers.\n
- *          Add all drivers in the drivers list in order
- *          to maintain drivers function of system power.
- *
- * @retval TEE_SUCCESS               Success
- * @retval TEE_ERROR_OUT_OF_MEMORY   Out of memory
+ * return TEE_SUCCESS               Success
+ *        TEE_ERROR_OUT_OF_MEMORY   Out of memory
  */
 static TEE_Result do_install(void)
 {
 	struct drv_ref *new_elem;
+	struct drv_ref *elem;
 	const struct driver *drv = NULL;
 
-	const struct driver *start = SCATTERED_ARRAY_BEGIN(
-						drivers, struct driver);
-	const struct driver *end = SCATTERED_ARRAY_END(
-						drivers, struct driver);
-
-	for (drv = start; drv < end; drv++) {
+	SCATTERED_ARRAY_FOREACH(drv, drivers, struct driver) {
 		if (drv->ops) {
 			if (!drv->ops->init) {
-				MGTDRV_TRACE(
-				"Driver [%s] not install no Init function",
+				DMSG(
+				"Driver [%s] not installed no Init function",
 				drv->name);
 				continue;
 			}
 
-			MGTDRV_TRACE("Add driver [%s] in the list", drv->name);
+			DMSG("Add driver [%s] in the list", drv->name);
 
 			/*
 			 * Allocate a new driver reference element to
@@ -144,23 +90,19 @@ static TEE_Result do_install(void)
 			 * list at the head of the list.
 			 */
 			new_elem->drv  = drv;
-			new_elem->data = NULL;
-			new_elem->mode = OFF;
+			new_elem->mode = STATE_UNKNOWN;
 
-			if (drv->data_size) {
-				new_elem->data = drivers_alloc_data(
-							drv->data_size);
-				if (!new_elem->data)
-					return TEE_ERROR_OUT_OF_MEMORY;
-			}
+			elem = SLIST_FIRST(&drv_list);
+			if (elem) {
+				while (SLIST_NEXT(elem, next))
+					elem = SLIST_NEXT(elem, next);
 
-			SLIST_INSERT_HEAD(&drv_list, new_elem, next);
+				SLIST_INSERT_AFTER(elem, new_elem, next);
+			} else
+				SLIST_INSERT_HEAD(&drv_list, new_elem, next);
+		} else {
+			DMSG("Driver [%s] OPS not defined", drv->name);
 		}
-#ifdef DRV_MGT_DEBUG
-		else
-			MGTDRV_TRACE("Driver [%s] OPS not defined", drv->name);
-#endif
-
 	}
 
 	IMSG("Drivers installation success");
@@ -168,13 +110,13 @@ static TEE_Result do_install(void)
 	return TEE_SUCCESS;
 }
 
-/**
- * @brief   Initialization of all driver in the list.\n
+/*
+ * brief   Initialization of all driver in the list.\n
  *
- * @note    Return TEE_SUCCESS in case there is no driver
+ * note    Return TEE_SUCCESS in case there is no driver
  *
- * @retval TEE_SUCCESS       Success
- * @retval TEE_ERROR_GENERIC Generic error
+ * return TEE_SUCCESS       Success
+ *        TEE_ERROR_GENERIC Generic error
  */
 static TEE_Result do_init(void)
 {
@@ -184,11 +126,11 @@ static TEE_Result do_init(void)
 	struct drv_ref *drv_elem;
 
 	SLIST_FOREACH(drv_elem, &drv_list, next) {
-		ret = drv_elem->drv->ops->init(drv_elem->data);
-		MGTDRV_TRACE("Driver [%s] init ret 0x%"PRIx32"",
+		ret = drv_elem->drv->ops->init();
+		DMSG("Driver [%s] init ret 0x%"PRIx32"",
 				drv_elem->drv->name, ret);
 		if (ret == TEE_SUCCESS)
-			drv_elem->mode = ON;
+			drv_elem->mode = STATE_RUNNING;
 		else {
 			SLIST_REMOVE(&drv_list, drv_elem, drv_ref, next);
 			free(drv_elem);
@@ -199,23 +141,23 @@ static TEE_Result do_init(void)
 	return (error_counter) ? TEE_ERROR_GENERIC : TEE_SUCCESS;
 }
 
-/**
- * @brief   Prepare all drivers to enter in a the given power \a mode.
- *          Function of the \a wait parameter:
- *           - \a wait = TRUE, the driver must complete the power mode
+/*
+ * brief    Prepare all drivers to enter in a the given power mode.
+ *          Function of the wait parameter:
+ *           - wait = TRUE, the driver must complete the power mode
  *           entry procedure before returning.
- *           - \a wait = FALSE, the driver checks if it's ready or not.
+ *           - wait = FALSE, the driver checks if it's ready or not.
  *           If not, it return TEE_ERROR_BUSY, otherwise it switches in
  *           the requested power mode.
  *
- * @note    Return TEE_SUCCESS in case there is no driver
+ * note    Return TEE_SUCCESS in case there is no driver
  *
- * @param[in]  mode   Power mode to switch to
- * @param[in]  wait   TRUE to wait until power mode reach
+ * inputs:  mode   Power mode to switch to
+ *          wait   TRUE to wait until power mode reach
  *
- * @retval TEE_SUCCESS       Success
- * @retval TEE_ERROR_BUSY    At least one driver can not be switched
- * @retval TEE_ERROR_GENERIC Generic error
+ * return TEE_SUCCESS       Success
+ *        TEE_ERROR_BUSY    At least one driver can not be switched
+ *        TEE_ERROR_GENERIC Generic error
  */
 TEE_Result drivers_pm_enter(enum drv_pwrmode mode, bool wait)
 {
@@ -232,21 +174,12 @@ TEE_Result drivers_pm_enter(enum drv_pwrmode mode, bool wait)
 			if (drv_elem->mode == mode)
 				continue;
 
-			ret = drv_elem->drv->ops->pm_enter(drv_elem->data,
-					mode, wait);
-			MGTDRV_TRACE("Driver [%s] switch to %d ret 0x%"PRIx32"",
+			ret = drv_elem->drv->ops->pm_enter(mode, wait);
+			DMSG("Driver [%s] switch to %d ret 0x%"PRIx32"",
 					drv_elem->drv->name, mode, ret);
 
 			switch (ret) {
 			case TEE_SUCCESS:
-				if (drv_elem->data) {
-					ret = drivers_save_data(mode,
-							drv_elem->drv,
-							drv_elem->data);
-					if (ret != TEE_SUCCESS)
-						goto exit_pm_enter;
-				}
-
 				drv_elem->mode = mode;
 				break;
 
@@ -282,15 +215,13 @@ exit_pm_enter:
 	return ret;
 }
 
-/**
- * @brief   Resume all drivers for their power mode.\n
- *          Do check error, driver must react itself in case
+/*
+ * brief    Resume all drivers from their power mode.
+ *          Don't check error, driver must react itself in case
  *          of resume error.
  */
 void drivers_pm_resume(void)
 {
-	TEE_Result ret;
-
 	struct drv_ref *drv_elem;
 
 	cpu_spin_lock(&drv_spinlock);
@@ -298,40 +229,31 @@ void drivers_pm_resume(void)
 	SLIST_FOREACH(drv_elem, &drv_list, next) {
 		if (drv_elem->drv->ops->pm_resume) {
 			/* If state is ON, don't resume */
-			if (drv_elem->mode == ON)
+			if (drv_elem->mode == STATE_RUNNING)
 				continue;
 
-			if (drv_elem->data) {
-				ret = drivers_restore_data(drv_elem->mode,
-						drv_elem->drv, drv_elem->data);
-				if (ret != TEE_SUCCESS)
-					continue;
-			}
+			drv_elem->drv->ops->pm_resume(drv_elem->mode);
+			DMSG("Driver [%s] resume", drv_elem->drv->name);
 
-			drv_elem->drv->ops->pm_resume(drv_elem->data,
-				drv_elem->mode);
-			MGTDRV_TRACE("Driver [%s] resume",
-					drv_elem->drv->name);
-
-			drv_elem->mode = ON;
+			drv_elem->mode = STATE_RUNNING;
 		}
 	}
 
 	cpu_spin_unlock(&drv_spinlock);
 }
 
-/**
- * @brief   Initialization of all registered driver.\n
+/*
+ * brief    Initialization of all registered driver.
  *          Build a list of drivers and if success
  *          call all drivers of the list.
  *
- * @retval TEE_SUCCESS             Success
- * @retval TEE_ERROR_OUT_OF_MEMORY Out of memory
- * @retval TEE_ERROR_GENERIC       Generic error
+ * return TEE_SUCCESS             Success
+ *        TEE_ERROR_OUT_OF_MEMORY Out of memory
+ *        TEE_ERROR_GENERIC       Generic error
  */
 static TEE_Result drivers_init(void)
 {
-	TEE_Result ret;
+	TEE_Result ret = TEE_SUCCESS;
 
 	ret = do_install();
 
--- core/include/drivers/driver.h
+++ core/include/drivers/driver.h
@@ -1,11 +1,11 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
-/**
- * @copyright 2018 NXP
+/*
+ * Copyright 2018 NXP
  *
- * @file    driver.h
+ * File    driver.h
  *
- * @brief   Definition of the generic driver object opened, closed,
- *          suspended and resumed by the system.
+ * Brief   Definition of the generic driver object opened, closed,
+ *         suspended and resumed by the system.
  *
  */
 #ifndef __DRIVER_H__
@@ -13,143 +13,62 @@
 
 #include <scattered_array.h>
 #include <tee_api_types.h>
-#include <trace.h>
-
-#if TRACE_LEVEL >= TRACE_DEBUG
-#define DRV_MGT_DEBUG
-#endif
+#include <util.h>
 
 #ifndef CFG_PLAT_PWR
-/**
- * @brief   Enumerate the driver power mode state
+/*
+ * Default Enumerate the driver power mode state.
+ *
+ * But it's still possible to redefine the `enum drv_pwrmode` to be
+ * platform specific.
+ * For that the include file `platform_pw.h` has to be present in
+ * which the `enum drv_pwrmode` is redefined. Then the platform must
+ * be built with the `CFG_PLAT_PWR` compilation flag.
+ *
+ * Note: The STATE_UNKNOWN and STATE_RUNNING state must be defined
+ *       even if redefined in the platform specific enumerate.
  */
 enum drv_pwrmode {
-	ON = 0,   ///< Power up and running
-	IDLE,     ///< Low Power mode Idle
-	OFF       ///< Power OFF
+	STATE_UNKNOWN = 0,   // Unknown Power state
+	STATE_RUNNING,       // Power up and running
+	STATE_SUSPEND        // System suspend mode
 };
 #else
 #include <platform_pwr.h>
 #endif
 
-/**
- * @brief   Generic driver object operations. Some can be optional.
+/*
+ * Driver object operations. Some can be optional.
  */
 struct driver_ops {
-	/// Driver initialization
-	TEE_Result (*init)(void *drvdata);
-	/// Switch driver in either Idle or OFF mode
-	TEE_Result (*pm_enter)(void *drvdata, enum drv_pwrmode mode, bool wait);
-	/// Resume driver from Idle or Power Off
-	void (*pm_resume)(void *drvdata, enum drv_pwrmode mode);
+	// Driver initialization
+	TEE_Result (*init)(void);
+	// Request driver power state entry
+	TEE_Result (*pm_enter)(enum drv_pwrmode mode, bool wait);
+	// Resume driver to be running mode
+	void (*pm_resume)(enum drv_pwrmode mode);
 };
 
-/**
- * @brief   Generic driver object referencing driver data and operations.
- *          Driver's data can be optional (data_size = 0).
- *          Driver data is allocated by the generic installer
- *          to allow the control of the data's area. Data area can so
- *          be saved/restored in secure place in case of power mode
- *          when normal memory is lost. (e.g System OFF)
+/*
+ * Driver object referencing driver operations.
  */
 struct driver {
-#ifdef DRV_MGT_DEBUG
-	const char              *name;     ///< Name of the driver (debug only)
-#endif
-	const size_t            data_size; ///< Driver's private data size
-	const struct driver_ops *ops;      ///< Driver's operation functions
+	const char              *name;     // Name of the driver
+	const struct driver_ops *ops;      // Driver's operation functions
 };
-
-#define concat(a, b)	a ## b
-#define STR(s)			#s
 
 #define __define_drivers(...) \
 	SCATTERED_ARRAY_DEFINE_PG_ITEM(drivers, struct driver) = \
 		{ __VA_ARGS__ }
 
-#ifdef DRV_MGT_DEBUG
 
 /*
  * Register driver with debug information
  */
-#define REGISTER_DRIVER(_name, _data_size, _ops) \
-	__define_drivers(.name = STR(_name), \
-			.data_size = _data_size, \
-			.ops = _ops)
-#else
+#define REGISTER_DRIVER(_name, _ops) \
+	__define_drivers(.name = TO_STR(_name), .ops = _ops)
 
-/*
- * Register driver without debug information
- */
-#define REGISTER_DRIVER(_name, _data_size, _ops) \
-	__define_drivers(.data_size = _data_size, \
-			.ops = _ops)
-
-#endif
-
-/**
- * @brief   Allocation of the driver's data. This function is a weak
- *          function that can be overwritten in the platform
- *          implementation
- *
- * @param[in]  size   Data size to allocate
- *
- * @retval  address of the data buffer allocated
- * @retval  NULL if allocation error
- */
-void *drivers_alloc_data(size_t size);
-
-/**
- * @brief   Prepare all drivers to Idle or power off mode. Function
- *          of the \a wait parameter:
- *           - \a wait = TRUE, the driver must complete the power mode
- *           entry procedure before returning.
- *           - \a wait = FALSE, the driver checks if it's ready or not.
- *           If not, it return TEE_ERROR_BUSY, otherwise it switches in
- *           the requested power mode.
- *
- * @note    Return TEE_SUCCESS in case there is no driver
- *
- * @param[in]  mode   Power mode to switch to
- * @param[in]  wait   TRUE to wait until power mode reach
- *
- * @retval TEE_SUCCESS       Success
- * @retval TEE_ERROR_BUSY    At least one driver can not be switched
- * @retval TEE_ERROR_GENERIC Generic error
- */
 TEE_Result drivers_pm_enter(enum drv_pwrmode mode, bool wait);
-
-/**
- * @brief   Resume all drivers for their power mode.\n
- *          Do check error, driver must react itself in case
- *          of resume error.
- */
 void drivers_pm_resume(void);
-
-/**
- * @brief   Save the drivers data to enter in a power \a mode.
- *
- * @param[in]  mode   Power mode to enter
- * @param[in]  drv    Driver object
- * @param[in]  data   Driver's data
- *
- * @retval  TEE_SUCCESS        Success
- * @retval  TEE_ERROR_GENERIC  Generic error
- */
-TEE_Result drivers_save_data(enum drv_pwrmode mode,
-		const struct driver *drv, void *data);
-
-/**
- * @brief   Restore the drivers data after resuning from power \a mode.
- *
- * @param[in]  mode   Power mode to enter
- * @param[in]  drv    Driver object
- * @param[in]  data   Driver's data
- *
- * @retval  TEE_SUCCESS        Success
- * @retval  TEE_ERROR_GENERIC  Generic error
- */
-TEE_Result drivers_restore_data(enum drv_pwrmode mode,
-		const struct driver *drv, void *data);
 
 #endif /* __DRIVER_H__ */
