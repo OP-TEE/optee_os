4b1f1189ba8e1d3e4480b8160bfe63f365cd944f
Etienne Carriere <etienne.carriere@st.com>
2018-12-17 17:14:10 +0100

    stm32mp1: RCC support
    RCC if the SoC interface for clocks, reset and some low power features.
    The drivers is very specific to the platform stm32mp1 hence located
    next to the platform specific source files.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

---
+++ core/arch/arm/plat-stm32mp1/drivers/stm32mp1_rcc.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, STMicroelectronics
+ */
+
+#include <drivers/stm32mp1_rcc.h>
+#include <io.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+
+uintptr_t stm32_rcc_base(void)
+{
+	static struct io_pa_va base = { .pa = RCC_BASE };
+
+	return io_pa_or_va(&base);
+}
