b148ed09330bcb7ad6274286c1b88d709afa90bd
Etienne Carriere <etienne.carriere@st.com>
2018-12-17 17:14:48 +0100

    stm32mp1: psci: switch secondary boot cpu offline
    Secondary boot core can be switched OFF from a functional view by
    being reset. To get the core back on line, it shall be woken through
    catching secure SGI0 interrupt then proceeding to same sequence as
    when getting the core online from a cold reset.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-stm32mp1/platform_config.h
+++ core/arch/arm/plat-stm32mp1/platform_config.h
@@ -27,6 +27,14 @@
 #define GICC_OFFSET			0x1000
 #define GICD_OFFSET			0x0000
 
+#define GIC_NON_SEC_SGI_0		0
+#define GIC_SEC_SGI_0			8
+#define GIC_SEC_SGI_1			9
+
+#define TARGET_CPU0_GIC_MASK		BIT(0)
+#define TARGET_CPU1_GIC_MASK		BIT(1)
+#define TARGET_CPUS_GIC_MASK		GENMASK_32(CFG_TEE_CORE_NB_CORE - 1, 0)
+
 /* TAMP resources */
 #define TAMP_BKP_REGISTER_OFF		0x100
 
