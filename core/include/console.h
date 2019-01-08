770b2afacf3342b462629198ad26675b9fb41687
Etienne Carriere <etienne.carriere@linaro.org>
2019-01-08 10:01:51 +0100

    core: more flexible console init from DT
    Introduce get_console_node_from_dt() that collects the references
    to the console configuration in the DTB.
    
    Existing configure_console_from_dt() relies on it when initiating
    the console from the early core inits based on registered matching
    device tables.
    
    get_console_node_from_dt() allows a driver probed from the driver
    initialization sequence (i.e registered from driver_init()) to
    get the console DT directive.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Joakim Bech <joakim.bech@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/include/console.h
+++ core/include/console.h
@@ -7,6 +7,8 @@
 #define CONSOLE_H
 
 #include <compiler.h>
+#include <tee_api_types.h>
+
 
 void console_init(void);
 void console_putc(int ch);
@@ -16,6 +18,32 @@ struct serial_chip;
 void register_serial_console(struct serial_chip *chip);
 
 #ifdef CFG_DT
+/*
+ * Get console info from a reacheable DTB. Check the embedded DTB and fall
+ * back to the external DTB.
+ *
+ * If the DTB does not specify a chosen (or secure-chosen) node, we assume
+ * DTB does not provide specific console directive. Early console may remain.
+ * If the DTB does not specify any console in the chosen (or secure-chosen)
+ * node, we assume there is no console. Early console would be disabled.
+ *
+ * @fdt_out: Output DTB address where console directive is found
+ * @offs_out: Output offset in the DTB where console directive is found
+ * @path_out: Output string configuration of the console from in the DTB
+ * @params_out: Output console parameters found from the DTB
+ *
+ * Return a TEE_Result compliant return value
+ *
+ */
+TEE_Result get_console_node_from_dt(void **fdt_out, int *offs_out,
+				    const char **path_out,
+				    const char **params_out);
+
+/*
+ * Check if the /secure-chosen or /chosen node in the DT contains an
+ * stdout-path value for which we have a compatible driver. If so, switch
+ * the console to this device.
+ */
 void configure_console_from_dt(void);
 #else
 static inline void configure_console_from_dt(void)
