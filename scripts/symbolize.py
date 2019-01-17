b6bc49caed24fe2705767a8783d449b74714586b
Sumit Garg <sumit.garg@linaro.org>
2019-01-17 17:42:14 +0530

    trace: fix core id print if in non-atomic context
    Make "?" print repetitive equivalent to number of digits needed to display
    core id rather than extra spaces as it causes symbolize.py script parsing
    failure for call stack addresses in case number of cores is greater than
    10.
    
    Also change symbolize.py to detect repetitive "?".
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- scripts/symbolize.py
+++ scripts/symbolize.py
@@ -16,7 +16,7 @@ CALL_STACK_RE = re.compile('Call stack:')
 # This gets the address from lines looking like this:
 # E/TC:0  0x001044a8
 STACK_ADDR_RE = re.compile(
-    r'[UEIDFM]/T[AC]:(\?|[0-9]+) [0-9]* +(?P<addr>0x[0-9a-f]+)')
+    r'[UEIDFM]/T[AC]:(\?+|[0-9]+) [0-9]* +(?P<addr>0x[0-9a-f]+)')
 ABORT_ADDR_RE = re.compile(r'-abort at address (?P<addr>0x[0-9a-f]+)')
 REGION_RE = re.compile(r'region [0-9]+: va (?P<addr>0x[0-9a-f]+) '
                        r'pa 0x[0-9a-f]+ size (?P<size>0x[0-9a-f]+)'
