1b08a3d854ee43b68c5a07a8a4ff0dfc4d37acf5
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2019-01-25 21:58:03 +0530

    plat-bcm: Add Broadcom ARMv8-A SoC ns3
    Add base platform support for Broadcom ns3 SoC.
    Broadcom ns3 is ARMv8-A based SoS with Cortex-A72 cores
    and GICv3. It is configured to run with TF-A.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>
    Reviewed-by: Raveendra Padasalagi <raveendra.padasalagi@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Reviewed-by: Pramod Kumar <pramod.kumar@broadcom.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

---
+++ core/arch/arm/plat-bcm/platform_config.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2019 Broadcom.
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+#include <mm/generic_ram_layout.h>
+
+#if defined(PLATFORM_FLAVOR_ns3)
+
+#define STACK_ALIGNMENT		64
+
+#define CONSOLE_UART_CLK_IN_HZ	25000000
+#define CONSOLE_BAUDRATE	115200
+
+#define CONSOLE_UART_BASE	0x68a10000
+#define BCM_DEVICE0_BASE	CONSOLE_UART_BASE
+#define BCM_DEVICE0_SIZE	CORE_MMU_DEVICE_SIZE
+
+#define GICD_BASE		0x63c00000
+#define BCM_DEVICE1_BASE	GICD_BASE
+#define BCM_DEVICE1_SIZE	CORE_MMU_DEVICE_SIZE
+
+#else
+#error "Unknown platform flavor"
+#endif
+#endif /*PLATFORM_CONFIG_H*/
