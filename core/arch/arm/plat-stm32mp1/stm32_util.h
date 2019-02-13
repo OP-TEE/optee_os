9f6d9163836374936970b939e18f1069c34a0f66
Etienne Carriere <etienne.carriere@st.com>
2019-02-13 15:47:04 +0100

    stm32mp1: util for shared resources refcount
    Implement {incr|decr}_shrefcnt(refcount, secure_flag) to provide
    reference counting for secure, non secure and resources used both
    from secure and non secure world.
    
    Functions {incr|decr}_refcnt(refcount) is a secure refcount only
    reference counting.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-stm32mp1/stm32_util.h
+++ core/arch/arm/plat-stm32mp1/stm32_util.h
@@ -86,4 +86,64 @@ struct stm32_bsec_static_cfg {
 
 void stm32mp_get_bsec_static_cfg(struct stm32_bsec_static_cfg *cfg);
 
+/*
+ * Shared reference counter: increments by 2 on secure increment
+ * request, decrements by 2 on secure decrement request. Bit #0
+ * is set to 1 on non-secure increment request and reset to 0 on
+ * non-secure decrement request. These counters initialize to
+ * either 0, 1 or 2 upon their expect default state.
+ * Counters saturate to UINT_MAX / 2.
+ */
+#define SHREFCNT_NONSECURE_FLAG		0x1ul
+#define SHREFCNT_SECURE_STEP		0x2ul
+#define SHREFCNT_MAX			(UINT_MAX / 2)
+
+/* Return 1 if refcnt increments from 0, else return 0 */
+static inline int incr_shrefcnt(unsigned int *refcnt, bool secure)
+{
+	int rc = !*refcnt;
+
+	if (secure) {
+		if (*refcnt < SHREFCNT_MAX) {
+			*refcnt += SHREFCNT_SECURE_STEP;
+			assert(*refcnt < SHREFCNT_MAX);
+		}
+	} else {
+		*refcnt |= SHREFCNT_NONSECURE_FLAG;
+	}
+
+	return rc;
+}
+
+/* Return 1 if refcnt decrements to 0, else return 0 */
+static inline int decr_shrefcnt(unsigned int *refcnt, bool secure)
+{
+	int  rc = 0;
+
+	if (secure) {
+		if (*refcnt < SHREFCNT_MAX) {
+			if (*refcnt < SHREFCNT_SECURE_STEP)
+				panic();
+
+			*refcnt -= SHREFCNT_SECURE_STEP;
+			rc = !*refcnt;
+		}
+	} else {
+		rc = (*refcnt == SHREFCNT_NONSECURE_FLAG);
+		*refcnt &= ~SHREFCNT_NONSECURE_FLAG;
+	}
+
+	return rc;
+}
+
+static inline int incr_refcnt(unsigned int *refcnt)
+{
+	return incr_shrefcnt(refcnt, true);
+}
+
+static inline int decr_refcnt(unsigned int *refcnt)
+{
+	return decr_shrefcnt(refcnt, true);
+}
+
 #endif /*__STM32_UTIL_H__*/
