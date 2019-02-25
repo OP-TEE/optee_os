7c8b181a013779b0f30d40580e4b00be63d1e91e
Jerome Forissier <jerome.forissier@linaro.org>
2019-02-25 15:13:39 +0100

    libutils: add memzero_explicit()
    Adds a new function: memzero_explicit(s, count) which is equivalent to
    memset(s, 0, count) except that it cannot be optimized away by the
    compiler.
    
    memset() being a built-in function, the compiler is free to perform
    optimizations such as simply discarding a call when it considers that the
    call cannot have any observable effect from the program's point of view.
    A typical example is clearing local data before returning from a
    function. memset() is likely to have no effect in this case while
    memzero_explicit() will work as expected.
    
    Calling memset() directly from memzero_explicit() would work as long as
    link time optimization (LTO) is not applied. With LTO however, the
    compiler could inline the call to memzero_explicit() and find out that
    dead store optimization applies. In order to avoid that, we use a method
    mentioned in [1] which consists in using a volatile function pointer.
    This method is considered "effective in practice" with all the commonly
    used compilers.
    
    Link: [1] https://www.usenix.org/system/files/conference/usenixsecurity17/sec17-yang.pdf
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

--- lib/libutils/ext/include/string_ext.h
+++ lib/libutils/ext/include/string_ext.h
@@ -33,4 +33,23 @@ static inline int buf_compare_ct(const void *s1, const void *s2, size_t n)
 /* Variant of strdup() that uses nex_malloc() instead of malloc() */
 char *nex_strdup(const char *s);
 
+/*
+ * Like memset(s, 0, count) but prevents the compiler from optimizing the call
+ * away. Such "dead store elimination" optimizations typically occur when
+ * clearing a *local* variable that is not used after it is cleared; but
+ * link-time optimization (LTO) can also trigger code elimination in other
+ * circumstances. See "Dead Store Elimination (Still) Considered Harmful" [1]
+ * for details and examples (and note that the Cland compiler enables LTO by
+ * default!).
+ *
+ * [1] https://www.usenix.org/system/files/conference/usenixsecurity17/sec17-yang.pdf
+ *
+ * Practically speaking:
+ *
+ * - Use memzero_explicit() to *clear* (as opposed to initialize) *sensitive*
+ *   data (such as keys, passwords, cryptographic state);
+ * - Otherwise, use memset().
+ */
+void memzero_explicit(void *s, size_t count);
+
 #endif /* STRING_EXT_H */
