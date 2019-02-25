9dde212d4794a8315a7509e17c3d3c423afa5920
Etienne Carriere <etienne.carriere@linaro.org>
2019-02-25 14:51:18 +0100

    core: replace TAF with TA invocation in inline comments
    TAF stands for Trusted Application Function. It is deprecated
    since a while. This change replaces the few remaining occurrences
    with TA invocation.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/include/kernel/tee_ta_manager.h
+++ core/include/kernel/tee_ta_manager.h
@@ -93,9 +93,9 @@ struct tee_ta_session {
 	uint32_t id;		/* Session handle (0 is invalid) */
 	struct tee_ta_ctx *ctx;	/* TA context */
 	TEE_Identity clnt_id;	/* Identify of client */
-	bool cancel;		/* True if TAF is cancelled */
+	bool cancel;		/* True if TA invocation is cancelled */
 	bool cancel_mask;	/* True if cancel is masked */
-	TEE_Time cancel_time;	/* Time when to cancel the TAF */
+	TEE_Time cancel_time;	/* Time when to cancel the TA invocation */
 	void *user_ctx;		/* Opaque session handle assigned by PTA */
 	uint32_t ref_count;	/* reference counter */
 	struct condvar refc_cv;	/* CV used to wait for ref_count to be 0 */
