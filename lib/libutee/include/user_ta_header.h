OP-TEE/optee_os

commit 387b0ee39b1b8d5a8a0db68a0a4535d2d621342d

core: deprecate TA property flags EXEC_DDR and USER_MODE
TA property flags TA_FLAG_EXEC_DDR and TA_FLAG_USER_MODE were
not really useful in the OP-TEE and now they are meaningless.

Define the mask of flags a TA may pretend to and assert loaded
TAs do not expect flags set outside of the defined supported bit
flags.

Fix gmon.h against duplicate round macros.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

lib/libutee/include/user_ta_header.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
- * All rights reserved.
+ * Copyright (c) 2018, Linaro Limited.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -30,10 +30,8 @@
 #define USER_TA_HEADER_H
 
 #include <tee_api_types.h>
+#include <util.h>
 
-
-#define TA_FLAG_USER_MODE		(1 << 0)
-#define TA_FLAG_EXEC_DDR		(1 << 1)
 #define TA_FLAG_SINGLE_INSTANCE		(1 << 2)
 #define TA_FLAG_MULTI_SESSION		(1 << 3)
 #define TA_FLAG_INSTANCE_KEEP_ALIVE	(1 << 4) /* remains after last close */
@@ -45,6 +43,12 @@
 	 * (pseudo-TAs only).
 	 */
 #define TA_FLAG_CONCURRENT		(1 << 8)
+
+#define TA_FLAGS_MASK			GENMASK_32(8, 2)
+
+/* Deprecated macros that will be removed in the 3.2 release */
+#define TA_FLAG_USER_MODE		0
+#define TA_FLAG_EXEC_DDR		0
 
 union ta_head_func_ptr {
 	uint64_t ptr64;

core/arch/arm/kernel/elf_load.c
@@ -319,14 +319,7 @@ static TEE_Result load_head(struct elf_load_state *state, size_t head_size)
 	if (ADD_OVERFLOW(phdr.p_vaddr, phdr.p_memsz, &state->vasize))
 		return TEE_ERROR_SECURITY;
 
-	/*
-	 * Read .ta_head from first segment, make sure the segment is large
-	 * enough. We're only interested in seeing that the
-	 * TA_FLAG_EXEC_DDR flag is set. If that's true we set that flag in
-	 * the TA context to enable mapping the TA. Later when this
-	 * function has returned and the hash has been verified the flags
-	 * field will be updated with eventual other flags.
-	 */
+	/* Read .ta_head from first segment if the segment is large enough */
 	if (ptload0.p_filesz < head_size)
 		return TEE_ERROR_BAD_FORMAT;
 	res = alloc_and_copy_to(&p, state, ptload0.p_offset, head_size);

core/arch/arm/kernel/user_ta.c
@@ -194,14 +194,6 @@ static TEE_Result load_elf(struct user_ta_ctx *utc,
 		goto out;
 	}
 
-	/* Currently all TA must execute from DDR */
-	if (!(ta_head->flags & TA_FLAG_EXEC_DDR)) {
-		res = TEE_ERROR_BAD_FORMAT;
-		goto out;
-	}
-	/* Temporary assignment to setup memory mapping */
-	utc->ctx.flags = TA_FLAG_EXEC_DDR;
-
 	/* Ensure proper aligment of stack */
 	utc->mobj_stack = alloc_ta_mem(ROUNDUP(ta_head->stack_size,
 					       STACK_ALIGNMENT));
@@ -279,10 +271,6 @@ static TEE_Result ta_load(const TEE_UUID *uuid,
 			  struct tee_ta_ctx **ta_ctx)
 {
 	TEE_Result res;
-	uint32_t mandatory_flags = TA_FLAG_USER_MODE | TA_FLAG_EXEC_DDR;
-	uint32_t optional_flags = mandatory_flags | TA_FLAG_SINGLE_INSTANCE |
-	    TA_FLAG_MULTI_SESSION | TA_FLAG_SECURE_DATA_PATH |
-	    TA_FLAG_INSTANCE_KEEP_ALIVE | TA_FLAG_CACHE_MAINTENANCE;
 	struct user_ta_ctx *utc = NULL;
 	struct ta_head *ta_head;
 	struct user_ta_store_handle *ta_handle = NULL;
@@ -319,11 +307,9 @@ static TEE_Result ta_load(const TEE_UUID *uuid,
 		goto error_return;
 	}
 
-	/* check input flags bitmask consistency and save flags */
-	if ((ta_head->flags & optional_flags) != ta_head->flags ||
-	    (ta_head->flags & mandatory_flags) != mandatory_flags) {
-		EMSG("TA flag issue: flags=%x optional=%x mandatory=%x",
-		     ta_head->flags, optional_flags, mandatory_flags);
+	if (ta_head->flags & ~TA_FLAGS_MASK) {
+		EMSG("Invalid TA flag(s) 0x%" PRIx32,
+			ta_head->flags & ~TA_FLAGS_MASK);
 		res = TEE_ERROR_BAD_FORMAT;
 		goto error_return;
 	}
@@ -432,9 +418,6 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 	struct tee_ta_session *s __maybe_unused;
 	void *param_va[TEE_NUM_PARAMS] = { NULL };
 
-	if (!(utc->ctx.flags & TA_FLAG_EXEC_DDR))
-		panic("TA does not exec in DDR");
-
 	/* Map user space memory */
 	res = tee_mmu_map_param(utc, param, param_va);
 	if (res != TEE_SUCCESS)
@@ -539,37 +522,28 @@ static void user_ta_dump_state(struct tee_ta_ctx *ctx)
 }
 KEEP_PAGER(user_ta_dump_state);
 
+static void release_ta_memory_by_mobj(struct mobj *mobj)
+{
+	void *va;
+
+	if (!mobj)
+		return;
+
+	va = mobj_get_va(mobj, 0);
+	if (!va)
+		return;
+
+	memset(va, 0, mobj->size);
+	cache_op_inner(DCACHE_AREA_CLEAN, va, mobj->size);
+}
+
 static void user_ta_ctx_destroy(struct tee_ta_ctx *ctx)
 {
 	struct user_ta_ctx *utc = to_user_ta_ctx(ctx);
 
 	tee_pager_rem_uta_areas(utc);
-
-	/*
-	 * Clean all traces of the TA, both RO and RW data.
-	 * No L2 cache maintenance to avoid sync problems
-	 */
-	if (ctx->flags & TA_FLAG_EXEC_DDR) {
-		void *va;
-
-		if (utc->mobj_code) {
-			va = mobj_get_va(utc->mobj_code, 0);
-			if (va) {
-				memset(va, 0, utc->mobj_code->size);
-				cache_op_inner(DCACHE_AREA_CLEAN, va,
-						utc->mobj_code->size);
-			}
-		}
-
-		if (utc->mobj_stack) {
-			va = mobj_get_va(utc->mobj_stack, 0);
-			if (va) {
-				memset(va, 0, utc->mobj_stack->size);
-				cache_op_inner(DCACHE_AREA_CLEAN, va,
-						utc->mobj_stack->size);
-			}
-		}
-	}
+	release_ta_memory_by_mobj(utc->mobj_code);
+	release_ta_memory_by_mobj(utc->mobj_stack);
 
 	/*
 	 * Close sessions opened by this TA

lib/libutee/arch/arm/gprof/gmon.h
@@ -65,6 +65,7 @@
 #define GMON_H
 
 #include <stdint.h>
+#include <util.h>
 
 /* Exported by the TA linker script */
 extern uint8_t __text_start[];
@@ -162,12 +163,6 @@ struct rawarc {
 	unsigned long	raw_selfpc;
 	long		raw_count;
 };
-
-/*
- * General rounding functions.
- */
-#define ROUNDDOWN(x, y)	(((x)/(y))*(y))
-#define ROUNDUP(x, y)	((((x)+(y)-1)/(y))*(y))
 
 /*
  * The profiling data structures are housed in this structure.

ta/arch/arm/user_ta_header.c
@@ -72,7 +72,7 @@ const struct ta_head ta_head __section(".ta_head") = {
 	 * must be enlarged
 	 */
 	.stack_size = TA_STACK_SIZE + TA_FRAMEWORK_STACK_SIZE,
-	.flags = TA_FLAG_USER_MODE | TA_FLAGS,
+	.flags = TA_FLAGS,
 #ifdef __ILP32__
 	/*
 	 * This workaround is neded on 32-bit because it seems we can't

commit dce253778c5cc1dfb4ff01a5e677540cf4b98e5b

core: ignore deprecated TA flags EXEC_DDR and USER_MODE
Commit 387b0ee39b1b ("core: deprecate TA property flags EXEC_DDR and
USER_MODE") removes the requirement for user TAs to set the flags
TA_FLAG_EXEC_DDR (bit 0) and TA_FLAG_USER_MODE (bit 1), the rationale
being that they are meaningless in the current implementation.
The macros are re-defined to be zero to reflect the fact that they have
no use. But, instead of ignoring the previous values, the TEE core now
requires that bits 0 and 1 must *not* be set. This is a problem because
it needlessly breaks backward compatibility. A TA built against OP-TEE
3.0.0 will not work with 3.1.0 or later:

 E/TC:? 0 tee_ta_init_user_ta_session:1040 Invalid TA flag(s) 0x3

This commit changes the acceptable flags mask (TA_FLAGS_MASK) to
include the previous EXEC_DDR and USER_MODE bits, thus restoring
backward compatibility.

Fixes: 387b0ee39b1b ("core: deprecate TA property flags EXEC_DDR and USER_MODE")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

lib/libutee/include/user_ta_header.h
@@ -10,6 +10,8 @@
 #include <tee_api_types.h>
 #include <util.h>
 
+#define TA_FLAG_USER_MODE		0	 /* Deprecated, was (1 << 0) */
+#define TA_FLAG_EXEC_DDR		0	 /* Deprecated, was (1 << 1) */
 #define TA_FLAG_SINGLE_INSTANCE		(1 << 2)
 #define TA_FLAG_MULTI_SESSION		(1 << 3)
 #define TA_FLAG_INSTANCE_KEEP_ALIVE	(1 << 4) /* remains after last close */
@@ -23,11 +25,7 @@
 #define TA_FLAG_CONCURRENT		(1 << 8)
 #define TA_FLAG_DEVICE_ENUM		(1 << 9) /* device enumeration */
 
-#define TA_FLAGS_MASK			GENMASK_32(9, 2)
-
-/* Deprecated macros that will be removed in the 3.2 release */
-#define TA_FLAG_USER_MODE		0
-#define TA_FLAG_EXEC_DDR		0
+#define TA_FLAGS_MASK			GENMASK_32(9, 0)
 
 union ta_head_func_ptr {
 	uint64_t ptr64;