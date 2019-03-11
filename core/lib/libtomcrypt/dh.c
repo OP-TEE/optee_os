4eaf1b78d36b2d7684899145f278fc67f6e3e562
Jens Wiklander <jens.wiklander@linaro.org>
2019-03-11 11:54:41 +0100

    core: ltc: move dh wrappers to separate file
    Moves the DH wrappers in tee_ltc_provider.c to its own file, dh.c.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ core/lib/libtomcrypt/dh.c
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2014-2019, Linaro Limited
+ */
+
+#include <crypto/crypto.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tee_api_types.h>
+#include <tomcrypt.h>
+#include <trace.h>
+#include <utee_defines.h>
+
+#include "acipher_helpers.h"
+
+TEE_Result crypto_acipher_alloc_dh_keypair(struct dh_keypair *s,
+					   size_t key_size_bits __unused)
+{
+	memset(s, 0, sizeof(*s));
+	if (!bn_alloc_max(&s->g))
+		return TEE_ERROR_OUT_OF_MEMORY;
+	if (!bn_alloc_max(&s->p))
+		goto err;
+	if (!bn_alloc_max(&s->y))
+		goto err;
+	if (!bn_alloc_max(&s->x))
+		goto err;
+	if (!bn_alloc_max(&s->q))
+		goto err;
+	return TEE_SUCCESS;
+err:
+	crypto_bignum_free(s->g);
+	crypto_bignum_free(s->p);
+	crypto_bignum_free(s->y);
+	crypto_bignum_free(s->x);
+	return TEE_ERROR_OUT_OF_MEMORY;
+}
+
+TEE_Result crypto_acipher_gen_dh_key(struct dh_keypair *key, struct bignum *q,
+				     size_t xbits)
+{
+	TEE_Result res;
+	dh_key ltc_tmp_key;
+	int ltc_res;
+
+	/* Generate the DH key */
+	ltc_tmp_key.g = key->g;
+	ltc_tmp_key.p = key->p;
+	ltc_res = dh_make_key(NULL, find_prng("prng_mpa"), q, xbits,
+			      &ltc_tmp_key);
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+	} else {
+		ltc_mp.copy(ltc_tmp_key.y,  key->y);
+		ltc_mp.copy(ltc_tmp_key.x,  key->x);
+
+		/* Free the tempory key */
+		dh_free(&ltc_tmp_key);
+		res = TEE_SUCCESS;
+	}
+	return res;
+}
+
+TEE_Result crypto_acipher_dh_shared_secret(struct dh_keypair *private_key,
+					   struct bignum *public_key,
+					   struct bignum *secret)
+{
+	int err;
+	dh_key pk = {
+		.type = PK_PRIVATE,
+		.g = private_key->g,
+		.p = private_key->p,
+		.y = private_key->y,
+		.x = private_key->x
+	};
+
+	err = dh_shared_secret(&pk, public_key, secret);
+	return ((err == CRYPT_OK) ? TEE_SUCCESS : TEE_ERROR_BAD_PARAMETERS);
+}
--- core/lib/libtomcrypt/sub.mk
+++ core/lib/libtomcrypt/sub.mk
@@ -18,3 +18,4 @@ srcs-$(CFG_CRYPTO_AES_GCM_FROM_CRYPTOLIB) += gcm.c
 srcs-$(CFG_CRYPTO_DSA) += dsa.c
 srcs-$(CFG_CRYPTO_ECC) += ecc.c
 srcs-$(CFG_CRYPTO_RSA) += rsa.c
+srcs-$(CFG_CRYPTO_DH) += dh.c
