6230dd0f07e25e5c026d3817d48df2f59d443ab4
Silvano di Ninno <silvano.dininno@nxp.com>
2019-03-18 15:39:27 +0100

    core: imx: update PL310 settings
    Move PL310 settings for mx6q, 6dl, 6solo to a dedicated file in plat-imx
    config folder.
    Update PL310 settings.
    
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- core/arch/arm/plat-imx/platform_config.h
+++ core/arch/arm/plat-imx/platform_config.h
@@ -3,6 +3,7 @@
  * Copyright (C) 2015 Freescale Semiconductor, Inc.
  * Copyright (c) 2016, Wind River Systems.
  * All rights reserved.
+ * Copyright 2019 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -57,95 +58,7 @@
 
 #elif defined(CFG_MX6Q) || defined(CFG_MX6D) || defined(CFG_MX6DL) || \
 	defined(CFG_MX6S)
-/*
- * PL310 TAG RAM Control Register
- *
- * bit[10:8]:1 - 2 cycle of write accesses latency
- * bit[6:4]:1 - 2 cycle of read accesses latency
- * bit[2:0]:1 - 2 cycle of setup latency
- */
-#ifndef PL310_TAG_RAM_CTRL_INIT
-#define PL310_TAG_RAM_CTRL_INIT		0x00000111
-#endif
-
-/*
- * PL310 DATA RAM Control Register
- *
- * bit[10:8]:2 - 3 cycle of write accesses latency
- * bit[6:4]:2 - 3 cycle of read accesses latency
- * bit[2:0]:2 - 3 cycle of setup latency
- */
-#ifndef PL310_DATA_RAM_CTRL_INIT
-#define PL310_DATA_RAM_CTRL_INIT	0x00000222
-#endif
-
-/*
- * PL310 Auxiliary Control Register
- *
- * I/Dcache prefetch enabled (bit29:28=2b11)
- * NS can access interrupts (bit27=1)
- * NS can lockown cache lines (bit26=1)
- * Pseudo-random replacement policy (bit25=0)
- * Force write allocated (default)
- * Shared attribute internally ignored (bit22=1, bit13=0)
- * Parity disabled (bit21=0)
- * Event monitor disabled (bit20=0)
- * Platform fmavor specific way config (dual / quad):
- * - 64kb way size (bit19:17=3b011)
- * - 16-way associciativity (bit16=1)
- * Platform fmavor specific way config (dual lite / solo):
- * - 32kb way size (bit19:17=3b010)
- * - no 16-way associciativity (bit16=0)
- * Store buffer device limitation enabled (bit11=1)
- * Cacheable accesses have high prio (bit10=0)
- * Full Line Zero (FLZ) disabled (bit0=0)
- */
-#ifndef PL310_AUX_CTRL_INIT
-#if defined(CFG_MX6Q) || defined(CFG_MX6D)
-#define PL310_AUX_CTRL_INIT		0x3C470800
-#else
-#define PL310_AUX_CTRL_INIT		0x3C440800
-#endif
-#endif
-
-/*
- * PL310 Prefetch Control Register
- *
- * Double linefill disabled (bit30=0)
- * I/D prefetch enabled (bit29:28=2b11)
- * Prefetch drop enabled (bit24=1)
- * Incr double linefill disable (bit23=0)
- * Prefetch offset = 7 (bit4:0)
- */
-#define PL310_PREFETCH_CTRL_INIT	0x31000007
-
-/*
- * PL310 Power Register
- *
- * Dynamic clock gating enabled
- * Standby mode enabled
- */
-#define PL310_POWER_CTRL_INIT		0x00000003
-
-/*
- * SCU Invalidate Register
- *
- * Invalidate all registers
- */
-#define	SCU_INV_CTRL_INIT		0xFFFFFFFF
-
-/*
- * SCU Access Register
- * - both secure CPU access SCU
- */
-#define SCU_SAC_CTRL_INIT		0x0000000F
-
-/*
- * SCU NonSecure Access Register
- * - both nonsec cpu access SCU, private and global timer
- */
-#define SCU_NSAC_CTRL_INIT		0x00000FFF
-
+#include <config/imx6qdlsolo.h>
 #endif
 
 #endif /*PLATFORM_CONFIG_H*/
