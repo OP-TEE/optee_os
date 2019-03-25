f0d1690983e8abcf1c5fe7a435b151fcd83157f8
Jerome Forissier <jerome.forissier@linaro.org>
2019-03-25 10:55:22 +0100

    libutils: ctype.h: define functions as builtins only when supported
    Our <ctype.h> defines a number of functions as macros that evaluate to
    the builtin function of the same name, for instance:
    
     #define isalpha(__c) __builtin_isalpha(__c)
    
    The goal is to allow the compiler to recognize the functions as
    builtins, therefore enabling some optimizations.
    
    In preparation for supporting Clang, which does not implement all the
    builtins that GCC recognizes, we need to test builtin support more
    precisely.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- lib/libutils/isoc/include/ctype.h
+++ lib/libutils/isoc/include/ctype.h
@@ -5,19 +5,75 @@
 #ifndef __CTYPE_H
 #define __CTYPE_H
 
-#define isalpha(__c)	__builtin_isalpha(__c)
-#define isupper(__c)	__builtin_isupper(__c)
-#define islower(__c)	__builtin_islower(__c)
-#define isdigit(__c)	__builtin_isdigit(__c)
-#define isxdigit(__c)	__builtin_isxdigit(__c)
-#define isspace(__c)	__builtin_isspace(__c)
-#define ispunct(__c)	__builtin_ispunct(__c)
-#define isalnum(__c)	__builtin_isalnum(__c)
-#define isprint(__c)	__builtin_isprint(__c)
-#define isgraph(__c)	__builtin_isgraph(__c)
-#define iscntrl(__c)	__builtin_iscntrl(__c)
+#ifndef __has_builtin
+/* GCC */
+#define __has_builtin(x) 1
+#endif
 
-#define toupper(__c)	__builtin_toupper(__c)
-#define tolower(__c)	__builtin_tolower(__c)
+#if __has_builtin(__builtin_isalnum)
+#define isalnum(__c) __builtin_isalnum(__c)
+#else
+int isalnum(int c);
+#endif
+#if __has_builtin(__builtin_isalpha)
+#define isalpha(__c) __builtin_isalpha(__c)
+#else
+int isalpha(int c);
+#endif
+#if __has_builtin(__builtin_iscntrl)
+#define iscntrl(__c) __builtin_iscntrl(__c)
+#else
+int iscntrl(int c);
+#endif
+#if __has_builtin(__builtin_isdigit)
+#define isdigit(__c) __builtin_isdigit(__c)
+#else
+int isdigit(int c);
+#endif
+#if __has_builtin(__builtin_isgraph)
+#define isgraph(__c) __builtin_isgraph(__c)
+#else
+int isgraph(int c);
+#endif
+#if __has_builtin(__builtin_islower)
+#define islower(__c) __builtin_islower(__c)
+#else
+int islower(int c);
+#endif
+#if __has_builtin(__builtin_isprint)
+#define isprint(__c) __builtin_isprint(__c)
+#else
+int isprint(int c);
+#endif
+#if __has_builtin(__builtin_ispunct)
+#define ispunct(__c) __builtin_ispunct(__c)
+#else
+int ispunct(int c);
+#endif
+#if __has_builtin(__builtin_isspace)
+#define isspace(__c) __builtin_isspace(__c)
+#else
+int isspace(int c);
+#endif
+#if __has_builtin(__builtin_isupper)
+#define isupper(__c) __builtin_isupper(__c)
+#else
+int isupper(int c);
+#endif
+#if __has_builtin(__builtin_isxdigit)
+#define isxdigit(__c) __builtin_isxdigit(__c)
+#else
+int isxdigit(int c);
+#endif
+#if __has_builtin(__builtin_tolower)
+#define tolower(__c) __builtin_tolower(__c)
+#else
+int tolower(int c);
+#endif
+#if __has_builtin(__builtin_toupper)
+#define toupper(__c) __builtin_toupper(__c)
+#else
+int toupper(int c);
+#endif
 
 #endif /*__CTYPE_H*/
