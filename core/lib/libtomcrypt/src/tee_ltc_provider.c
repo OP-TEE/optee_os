25cc72e017f4914b3b4ab7d5f5c40067d97f0e1b
Jens Wiklander <jens.wiklander@linaro.org>
2019-03-26 20:31:53 +0100

    core: ltc: move remaining exported aes functions
    Moves remaining exported aes functions to aes.c
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/tee_ltc_provider.c
+++ core/lib/libtomcrypt/src/tee_ltc_provider.c
@@ -144,32 +144,3 @@ void tomcrypt_arm_neon_disable(struct tomcrypt_arm_neon_state *state)
 	thread_kernel_disable_vfp(state->state);
 }
 #endif
-
-TEE_Result crypto_aes_expand_enc_key(const void *key, size_t key_len,
-				     void *enc_key, size_t enc_keylen,
-				     unsigned int *rounds)
-{
-	symmetric_key skey;
-
-	if (enc_keylen < sizeof(skey.rijndael.eK))
-		return TEE_ERROR_BAD_PARAMETERS;
-
-	if (aes_setup(key, key_len, 0, &skey))
-		return TEE_ERROR_BAD_PARAMETERS;
-
-	memcpy(enc_key, skey.rijndael.eK, sizeof(skey.rijndael.eK));
-	*rounds = skey.rijndael.Nr;
-	return TEE_SUCCESS;
-}
-
-void crypto_aes_enc_block(const void *enc_key, size_t enc_keylen __maybe_unused,
-			  unsigned int rounds, const void *src, void *dst)
-{
-	symmetric_key skey;
-
-	assert(enc_keylen >= sizeof(skey.rijndael.eK));
-	memcpy(skey.rijndael.eK, enc_key, sizeof(skey.rijndael.eK));
-	skey.rijndael.Nr = rounds;
-	if (aes_ecb_encrypt(src, dst, &skey))
-		panic();
-}
