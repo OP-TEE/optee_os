78887e6053f32b2a050ae5871d9c287f7d51d686
Jens Wiklander <jens.wiklander@linaro.org>
2019-03-26 20:36:52 +0100

    core: ltc: move remaining external files from src
    Moves the remaining external source files from tomcrypt src directory.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/sub.mk
+++ core/lib/libtomcrypt/src/sub.mk
@@ -1,16 +1,3 @@
-ifdef _CFG_CRYPTO_WITH_ACIPHER
-ifeq ($(CFG_CORE_MBEDTLS_MPI),y)
-srcs-y += mpi_desc.c
-else
-srcs-y += mpa_desc.c
-# Get mpa.h which normally is an internal .h file
-cppflags-mpa_desc.c-y += -Ilib/libmpa
-cflags-mpa_desc.c-y += -Wno-unused-parameter
-endif
-endif
-
-srcs-y += tee_ltc_provider.c
-
 subdirs-$(_CFG_CRYPTO_WITH_CIPHER) += ciphers
 subdirs-$(_CFG_CRYPTO_WITH_AUTHENC) += encauth
 subdirs-y += hashes
