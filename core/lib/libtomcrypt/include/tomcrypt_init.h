a70f4504e3fa19452f19948e6fe6e139f74c31a4
Jens Wiklander <jens.wiklander@linaro.org>
2019-03-27 17:01:37 +0100

    core: ltc: add tomcrypt_init() when not crypto lib
    Adds tomcrypt_init() which is only available when LTC isn't configured
    as the crypto lib. tomcrypt_init() performs the same initialization as
    crypto_init() does when LTC is configured as the crypto lib.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ core/lib/libtomcrypt/include/tomcrypt_init.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2015, Linaro Limited
+ */
+
+#ifndef __TOMCRYPT_INIT_H
+#define __TOMCRYPT_INIT_H
+
+void tomcrypt_init(void);
+
+#endif /*__TOMCRYPT_INIT_H*/
