f7859906faa00a29398cf4a9f5f703dbf310fb63
Jens Wiklander <jens.wiklander@linaro.org>
2019-03-27 15:11:59 +0100

    core: ltc: dsa_import() use inlen instead of MAX_RSA_SIZE
    Upstream has changed dsa_import() to use inlen instead of MAX_RSA_SIZE.
    This is needed when compiling LTC with DSA but without RSA support.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/pk/dsa/dsa_import.c
+++ core/lib/libtomcrypt/src/pk/dsa/dsa_import.c
@@ -115,7 +115,7 @@ int dsa_import(const unsigned char *in, unsigned long inlen, dsa_key *key)
        key->type = PK_PRIVATE;
    } else { /* public */
       ltc_asn1_list params[3];
-      unsigned long tmpbuf_len = MAX_RSA_SIZE*8;
+      unsigned long tmpbuf_len = inlen;
 
       LTC_SET_ASN1(params, 0, LTC_ASN1_INTEGER, key->p, 1UL);
       LTC_SET_ASN1(params, 1, LTC_ASN1_INTEGER, key->q, 1UL);
