OP-TEE/optee_os

commit 64718c9361636e9750c29da790ddb3a8bfa55f6d

Allow building libutils etc. as shared libraries
When CFG_ULIBS_SHARED=y (default n), build libutils, libmpa/libmbedtls
and libutee as shared libraries (.so). The static archives (.a) are
still produced. The linker uses shared libraries by default when
present so TAs just need to be re-built to use the shared libraries.

For the time being, in-tree TAs are always linked statically for
practical reasons. Indeed, in-tree TAs (such as ta/avb) are likely
to be used as "early TAs". If such a TA was linked against shared
libraries, then those libraries would need to be installed in the
"early TA" area, too. While this works fine technically, it requires
some more steps in the Makefiles to make sure the proper dependencies
are installed and also it would mean that those libraries take
precedence over the ones potentially installed in the REE FS etc.
In other words, it would raise questions that we do not want nor need
to address now.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

mk/config.mk
@@ -346,6 +346,16 @@ $(error Cannot instrument user libraries if user mode profiling is disabled)
 endif
 endif
 
+# Build libutee, libutils, libmpa/libmbedtls as shared libraries.
+# - Static libraries are still generated when this is enabled, but TAs will use
+# the shared libraries unless explicitly linked with the -static flag.
+# - Shared libraries are made of two files: for example, libutee is
+#   libutee.so and 527f1a47-b92c-4a74-95bd-72f19f4a6f74.ta. The '.so' file
+#   is a totally standard shared object, and should be used to link against.
+#   The '.ta' file is a signed version of the '.so' and should be installed
+#   in the same way as TAs so that they can be found at runtime.
+CFG_ULIBS_SHARED ?= n
+
 # CFG_GP_SOCKETS
 # Enable Global Platform Sockets support
 CFG_GP_SOCKETS ?= y

ta/arch/arm/link.mk
@@ -21,6 +21,7 @@ link-ldflags += -Map=$(link-out-dir$(sm))/$(user-ta-uuid).map
 link-ldflags += --sort-section=alignment
 link-ldflags += -z max-page-size=4096 # OP-TEE always uses 4K alignment
 link-ldflags += --as-needed # Do not add dependency on unused shlib
+link-ldflags += $(link-ldflags$(sm))
 
 link-ldadd  = $(user-ta-ldadd) $(addprefix -L,$(libdirs))
 link-ldadd += --start-group $(addprefix -l,$(libnames)) --end-group

ta/mk/build-user-ta.mk
@@ -24,6 +24,11 @@ cppflags$(sm)	:= $(cppflags$(ta-target)) -I$(ta-dev-kit-dir$(sm))/include
 cflags$(sm)	:= $(cflags$(ta-target))
 aflags$(sm)	:= $(aflags$(ta-target))
 
+ifeq ($(CFG_ULIBS_SHARED),y)
+# For now, do not link in-tree TAs against shared libraries
+link-ldflags$(sm) := -static
+endif
+
 libdirs  = $(ta-dev-kit-dir$(sm))/lib
 libnames = utils utee
 ifneq ($(CFG_TA_MBEDTLS_MPI),y)

ta/ta.mk
@@ -48,27 +48,36 @@ base-prefix := $(sm)-
 
 libname = utils
 libdir = lib/libutils
+libuuid = 71855bba-6055-4293-a63f-b0963a737360
 include mk/lib.mk
 
 CFG_TA_MBEDTLS_MPI ?= y
 ifeq ($(CFG_TA_MBEDTLS_MPI),y)
+mplib-for-utee = mbedtls
 $(call force,CFG_TA_MBEDTLS,y)
 else
+mplib-for-utee = mpa
 libname = mpa
 libdir = lib/libmpa
+libuuid = 39b498d9-1e1f-4ae0-a9e1-6d1caf8ec731
+libl = utils
 include mk/lib.mk
 endif
-
-libname = utee
-libdir = lib/libutee
-include mk/lib.mk
 
 ifeq ($(CFG_TA_MBEDTLS),y)
 libname = mbedtls
 libdir = lib/libmbedtls
+libuuid = 87bb6ae8-4b1d-49fe-9986-2b966132c309
+libl = utils
 include mk/lib.mk
 ta-mk-file-export-vars-$(sm) += CFG_TA_MBEDTLS
 endif
+
+libname = utee
+libdir = lib/libutee
+libuuid = 527f1a47-b92c-4a74-95bd-72f19f4a6f74
+libl = $(mplib-for-utee) utils
+include mk/lib.mk
 
 base-prefix :=
 
@@ -92,7 +101,7 @@ $2/$$(notdir $1): $1
 	@set -e; \
 	mkdir -p $$(dir $$@) ; \
 	$(cmd-echo-silent) '  INSTALL $$@' ; \
-	cp $$< $$@
+	cp -P $$< $$@
 
 cleanfiles += $2/$$(notdir $1)
 ta_dev_kit: $2/$$(notdir $1)

commit d5fd2d42cdb02143f7fcd59310a19ac567eb9656

Error out if CFG_ULIBS_GPROF and CFG_ULIBS_SHARED are both enabled
When CFG_ULIBS_GPROF=y, the TA linker script references the
__utee_mcount symbol, which is external to the TA if libutee is built
as a shared library. This causes the following link error:

 ta.lds:19: unresolvable symbol `__utee_mcount' referenced in expression

It should quite easy to get rid of the allocation of the profiling
buffer in the TA's .bss segment, once we have a syscall (or a PTA
command) to allocate and map TA memory from the TEE core. Until then,
let's just prevent the incompatible configuration. Profiling is just a
debugging tool, so it should not be a problem to force static linking in
that case.

Fixes: 64718c936163 ("Allow building libutils etc. as shared libraries")
Reported-by: Sumit Garg <sumit.garg@linaro.org>
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

mk/config.mk
@@ -370,6 +370,17 @@ endif
 #   in the same way as TAs so that they can be found at runtime.
 CFG_ULIBS_SHARED ?= n
 
+ifeq (yy,$(CFG_ULIBS_GPROF)$(CFG_ULIBS_SHARED))
+# FIXME:
+# If both are enabled, the linker will report an error when linking a TA:
+#  ta.lds:19: unresolvable symbol `__utee_mcount' referenced in expression
+# The allocation of the profiling buffer should probably be done at runtime
+# via a new syscall/PTA call instead of having it pre-allocated in .bss by the
+# linker, which is hackish. When the TA link script is processed, there is no
+# way the linker can tell if some file in some shared library uses -pg anyway.
+$(error CFG_ULIBS_GPROF and CFG_ULIBS_SHARED are incompatible)
+endif
+
 # CFG_GP_SOCKETS
 # Enable Global Platform Sockets support
 CFG_GP_SOCKETS ?= y