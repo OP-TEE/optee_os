2b174fd1ac5bf21def76ec4700039f1aa382dc4c
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2019-04-12 21:21:45 +0530

    drivers: bcm_hwrng: add HWRNG driver
    low level driver for Broadcom random number generator IP.
    
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Bharat Kumar Reddy Gooty <bharat.gooty@broadcom.com>
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -24,3 +24,4 @@ srcs-$(CFG_STM32_ETZPC) += stm32_etzpc.c
 srcs-$(CFG_STM32_GPIO) += stm32_gpio.c
 srcs-$(CFG_STM32_UART) += stm32_uart.c
 srcs-$(CFG_STM32_I2C) += stm32_i2c.c
+srcs-$(CFG_BCM_HWRNG) += bcm_hwrng.c
