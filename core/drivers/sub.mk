df12e09d0e96b80484ea6fb1e3fbd1ccab263fec
Sandeep Tripathy <sandeep.tripathy@broadcom.com>
2019-04-12 21:34:12 +0530

    drivers: bcm_sotp: add SOTP driver
    low level driver for Broadcom SOTP controller.
    
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Rahul Gupta <rahul.gupta@broadcom.com>
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@broadcom.com>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -25,3 +25,4 @@ srcs-$(CFG_STM32_GPIO) += stm32_gpio.c
 srcs-$(CFG_STM32_UART) += stm32_uart.c
 srcs-$(CFG_STM32_I2C) += stm32_i2c.c
 srcs-$(CFG_BCM_HWRNG) += bcm_hwrng.c
+srcs-$(CFG_BCM_SOTP) += bcm_sotp.c
