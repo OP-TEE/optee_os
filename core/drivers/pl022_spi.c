OP-TEE/optee_os

commit 918bb3a5f3e473ec252ff2dfb71d666108dd22f4

core: upgrade from write32() to io_write32() and friends
Replace use of readX() and writeX() with io_readX() and io_writeX().
The former function are about to be deprecated in favor to the later.

This change upgrades core generic code and drivers.
At some place, io_clrbitsX(), io_setbitsX() and io_clrsetbitsX()
replace the writeX(readX() ...) operations when obvious.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/pl022_spi.c
@@ -151,25 +151,25 @@ static enum spi_result pl022_txrx8(struct spi_chip *chip, uint8_t *wdat,
 
 	if (wdat)
 		while (i < num_pkts) {
-			if (read8(pd->base + SSPSR) & SSPSR_TNF) {
+			if (io_read8(pd->base + SSPSR) & SSPSR_TNF) {
 				/* tx 1 packet */
-				write8(wdat[i++], pd->base + SSPDR);
+				io_write8(pd->base + SSPDR, wdat[i++]);
 			}
 
 			if (rdat)
-				if (read8(pd->base + SSPSR) & SSPSR_RNE) {
+				if (io_read8(pd->base + SSPSR) & SSPSR_RNE) {
 					/* rx 1 packet */
-					rdat[j++] = read8(pd->base + SSPDR);
+					rdat[j++] = io_read8(pd->base + SSPDR);
 				}
 		}
 
 	/* Capture remaining rdat not read above */
 	if (rdat) {
 		while ((j < num_pkts) &&
-			(read8(pd->base + SSPSR) & SSPSR_BSY))
-			if (read8(pd->base + SSPSR) & SSPSR_RNE) {
+			(io_read8(pd->base + SSPSR) & SSPSR_BSY))
+			if (io_read8(pd->base + SSPSR) & SSPSR_RNE) {
 				/* rx 1 packet */
-				rdat[j++] = read8(pd->base + SSPDR);
+				rdat[j++] = io_read8(pd->base + SSPDR);
 			}
 
 		if (j < num_pkts) {
@@ -197,25 +197,25 @@ static enum spi_result pl022_txrx16(struct spi_chip *chip, uint16_t *wdat,
 
 	if (wdat)
 		while (i < num_pkts) {
-			if (read8(pd->base + SSPSR) & SSPSR_TNF) {
+			if (io_read8(pd->base + SSPSR) & SSPSR_TNF) {
 				/* tx 1 packet */
-				write16(wdat[i++], pd->base + SSPDR);
+				io_write16(pd->base + SSPDR, wdat[i++]);
 			}
 
 			if (rdat)
-				if (read8(pd->base + SSPSR) & SSPSR_RNE) {
+				if (io_read8(pd->base + SSPSR) & SSPSR_RNE) {
 					/* rx 1 packet */
-					rdat[j++] = read16(pd->base + SSPDR);
+					rdat[j++] = io_read8(pd->base + SSPDR);
 				}
 		}
 
 	/* Capture remaining rdat not read above */
 	if (rdat) {
 		while ((j < num_pkts) &&
-			(read8(pd->base + SSPSR) & SSPSR_BSY))
-			if (read8(pd->base + SSPSR) & SSPSR_RNE) {
+			(io_read8(pd->base + SSPSR) & SSPSR_BSY))
+			if (io_read8(pd->base + SSPSR) & SSPSR_RNE) {
 				/* rx 1 packet */
-				rdat[j++] = read16(pd->base + SSPDR);
+				rdat[j++] = io_read8(pd->base + SSPDR);
 			}
 
 		if (j < num_pkts) {
@@ -232,20 +232,20 @@ static void pl022_print_peri_id(struct pl022_data *pd __maybe_unused)
 {
 	DMSG("Expected: 0x 22 10 ?4 00");
 	DMSG("Read: 0x %02x %02x %02x %02x",
-		read32(pd->base + SSPPeriphID0),
-		read32(pd->base + SSPPeriphID1),
-		read32(pd->base + SSPPeriphID2),
-		read32(pd->base + SSPPeriphID3));
+		io_read8(pd->base + SSPPeriphID0),
+		io_read8(pd->base + SSPPeriphID1),
+		io_read8(pd->base + SSPPeriphID2),
+		io_read8(pd->base + SSPPeriphID3));
 }
 
 static void pl022_print_cell_id(struct pl022_data *pd __maybe_unused)
 {
 	DMSG("Expected: 0x 0d f0 05 b1");
 	DMSG("Read: 0x %02x %02x %02x %02x",
-		read32(pd->base + SSPPCellID0),
-		read32(pd->base + SSPPCellID1),
-		read32(pd->base + SSPPCellID2),
-		read32(pd->base + SSPPCellID3));
+		io_read8(pd->base + SSPPCellID0),
+		io_read8(pd->base + SSPPCellID1),
+		io_read8(pd->base + SSPPCellID2),
+		io_read8(pd->base + SSPPCellID3));
 }
 
 static void pl022_sanity_check(struct pl022_data *pd)
@@ -271,7 +271,7 @@ static void pl022_sanity_check(struct pl022_data *pd)
 
 	#ifdef PLATFORM_hikey
 	DMSG("SSPB2BTRANS: Expected: 0x2. Read: 0x%x",
-		read32(pd->base + SSPB2BTRANS));
+		io_read8(pd->base + SSPB2BTRANS));
 	#endif
 	pl022_print_peri_id(pd);
 	pl022_print_cell_id(pd);
@@ -289,9 +289,9 @@ static void pl022_control_cs(struct spi_chip *chip, enum gpio_level value)
 
 	switch (pd->cs_control) {
 	case PL022_CS_CTRL_AUTO_GPIO:
-		if (read8(pd->base + SSPSR) & SSPSR_BSY)
+		if (io_read8(pd->base + SSPSR) & SSPSR_BSY)
 			DMSG("pl022 busy - do NOT set CS!");
-		while (read8(pd->base + SSPSR) & SSPSR_BSY)
+		while (io_read8(pd->base + SSPSR) & SSPSR_BSY)
 			;
 		DMSG("pl022 done - set CS!");
 
@@ -359,11 +359,11 @@ static void pl022_flush_fifo(struct pl022_data *pd)
 	uint32_t __maybe_unused rdat;
 
 	do {
-		while (read32(pd->base + SSPSR) & SSPSR_RNE) {
-			rdat = read32(pd->base + SSPDR);
+		while (io_read32(pd->base + SSPSR) & SSPSR_RNE) {
+			rdat = io_read32(pd->base + SSPDR);
 			DMSG("rdat: 0x%x", rdat);
 		}
-	} while (read32(pd->base + SSPSR) & SSPSR_BSY);
+	} while (io_read32(pd->base + SSPSR) & SSPSR_BSY);
 }
 
 static void pl022_configure(struct spi_chip *chip)

core/arch/arm/mm/tee_pager.c
@@ -232,8 +232,8 @@ static uint32_t pager_lock_check_stack(size_t stack_size)
 		 * the thread.
 		 */
 		for (n = 0; n < stack_size; n += SMALL_PAGE_SIZE)
-			write8(1, (vaddr_t)buf + n);
-		write8(1, (vaddr_t)buf + stack_size - 1);
+			io_write8((vaddr_t)buf + n, 1);
+		io_write8((vaddr_t)buf + stack_size - 1, 1);
 	}
 
 	return pager_lock(NULL);

core/drivers/atmel_uart.c
@@ -61,7 +61,7 @@ static void atmel_uart_flush(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	while (!(read32(base + ATMEL_UART_SR) & ATMEL_SR_TXEMPTY))
+	while (!(io_read32(base + ATMEL_UART_SR) & ATMEL_SR_TXEMPTY))
 		;
 }
 
@@ -69,20 +69,20 @@ static int atmel_uart_getchar(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	while (read32(base + ATMEL_UART_SR) & ATMEL_SR_RXRDY)
+	while (io_read32(base + ATMEL_UART_SR) & ATMEL_SR_RXRDY)
 		;
 
-	return read32(base + ATMEL_UART_RHR);
+	return io_read32(base + ATMEL_UART_RHR);
 }
 
 static void atmel_uart_putc(struct serial_chip *chip, int ch)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	while (!(read32(base + ATMEL_UART_SR) & ATMEL_SR_TXRDY))
+	while (!(io_read32(base + ATMEL_UART_SR) & ATMEL_SR_TXRDY))
 		;
 
-	write32(ch, base + ATMEL_UART_THR);
+	io_write32(base + ATMEL_UART_THR, ch);
 }
 
 static const struct serial_ops atmel_uart_ops = {

core/drivers/cdns_uart.c
@@ -67,7 +67,7 @@ static void cdns_uart_flush(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	while (!(read32(base + CDNS_UART_CHANNEL_STATUS) &
+	while (!(io_read32(base + CDNS_UART_CHANNEL_STATUS) &
 		 CDNS_UART_CHANNEL_STATUS_TEMPTY))
 		;
 }
@@ -76,8 +76,8 @@ static bool cdns_uart_have_rx_data(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	return !(read32(base + CDNS_UART_CHANNEL_STATUS) &
-			CDNS_UART_CHANNEL_STATUS_REMPTY);
+	return !(io_read32(base + CDNS_UART_CHANNEL_STATUS) &
+		 CDNS_UART_CHANNEL_STATUS_REMPTY);
 }
 
 static int cdns_uart_getchar(struct serial_chip *chip)
@@ -86,7 +86,7 @@ static int cdns_uart_getchar(struct serial_chip *chip)
 
 	while (!cdns_uart_have_rx_data(chip))
 		;
-	return read32(base + CDNS_UART_FIFO) & 0xff;
+	return io_read32(base + CDNS_UART_FIFO) & 0xff;
 }
 
 static void cdns_uart_putc(struct serial_chip *chip, int ch)
@@ -94,12 +94,12 @@ static void cdns_uart_putc(struct serial_chip *chip, int ch)
 	vaddr_t base = chip_to_base(chip);
 
 	/* Wait until there is space in the FIFO */
-	while (read32(base + CDNS_UART_CHANNEL_STATUS) &
-			CDNS_UART_CHANNEL_STATUS_TFUL)
+	while (io_read32(base + CDNS_UART_CHANNEL_STATUS) &
+	       CDNS_UART_CHANNEL_STATUS_TFUL)
 		;
 
 	/* Send the character */
-	write32(ch, base + CDNS_UART_FIFO);
+	io_write32(base + CDNS_UART_FIFO, ch);
 }
 
 
@@ -125,8 +125,8 @@ void cdns_uart_init(struct cdns_uart_data *pd, paddr_t base, uint32_t uart_clk,
 		return;
 
 	/* Enable UART and RX/TX */
-	write32(CDNS_UART_CONTROL_RXEN | CDNS_UART_CONTROL_TXEN,
-		base + CDNS_UART_CONTROL);
+	io_write32(base + CDNS_UART_CONTROL,
+		   CDNS_UART_CONTROL_RXEN | CDNS_UART_CONTROL_TXEN);
 
 	cdns_uart_flush(&pd->chip);
 }

core/drivers/dra7_rng.c
@@ -87,29 +87,30 @@ uint8_t hw_get_random_byte(void)
 
 	if (!pos) {
 		/* Is the result ready (available)? */
-		while (!(read32(rng + RNG_STATUS) & RNG_READY)) {
+		while (!(io_read32(rng + RNG_STATUS) & RNG_READY)) {
 			/* Is the shutdown threshold reached? */
-			if (read32(rng + RNG_STATUS) & SHUTDOWN_OFLO) {
-				uint32_t alarm = read32(rng + RNG_ALARMSTOP);
-				uint32_t tuning = read32(rng + RNG_FRODETUNE);
+			if (io_read32(rng + RNG_STATUS) & SHUTDOWN_OFLO) {
+				uint32_t alarm = io_read32(rng + RNG_ALARMSTOP);
+				uint32_t tune = io_read32(rng + RNG_FRODETUNE);
+
 				/* Clear the alarm events */
-				write32(0x0, rng + RNG_ALARMMASK);
-				write32(0x0, rng + RNG_ALARMSTOP);
+				io_write32(rng + RNG_ALARMMASK, 0x0);
+				io_write32(rng + RNG_ALARMSTOP, 0x0);
 				/* De-tune offending FROs */
-				write32(tuning ^ alarm, rng + RNG_FRODETUNE);
+				io_write32(rng + RNG_FRODETUNE, tune ^ alarm);
 				/* Re-enable the shut down FROs */
-				write32(RNG_FRO_MASK, rng + RNG_FROENABLE);
+				io_write32(rng + RNG_FROENABLE, RNG_FRO_MASK);
 				/* Clear the shutdown overflow event */
-				write32(SHUTDOWN_OFLO, rng + RNG_INTACK);
+				io_write32(rng + RNG_INTACK, SHUTDOWN_OFLO);
 
 				DMSG("Fixed FRO shutdown\n");
 			}
 		}
 		/* Read random value */
-		random.val[0] = read32(rng + RNG_OUTPUT_L);
-		random.val[1] = read32(rng + RNG_OUTPUT_H);
+		random.val[0] = io_read32(rng + RNG_OUTPUT_L);
+		random.val[1] = io_read32(rng + RNG_OUTPUT_H);
 		/* Acknowledge read complete */
-		write32(RNG_READY, rng + RNG_INTACK);
+		io_write32(rng + RNG_INTACK, RNG_READY);
 	}
 
 	ret = random.byte[pos];
@@ -128,14 +129,14 @@ static TEE_Result dra7_rng_init(void)
 	uint32_t val;
 
 	/* Execute a software reset */
-	write32(RNG_SOFT_RESET, rng + RNG_SOFT_RESET_REG);
+	io_write32(rng + RNG_SOFT_RESET_REG, RNG_SOFT_RESET);
 
 	/* Wait for the software reset completion by polling */
-	while (read32(rng + RNG_SOFT_RESET_REG) & RNG_SOFT_RESET)
+	while (io_read32(rng + RNG_SOFT_RESET_REG) & RNG_SOFT_RESET)
 		;
 
 	/* Switch to low-power operating mode */
-	write32(RNG_AUTOIDLE, rng + RNG_SYS_CONFIG_REG);
+	io_write32(rng + RNG_SYS_CONFIG_REG, RNG_AUTOIDLE);
 
 	/*
 	 * Select the number of clock input cycles to the
@@ -148,13 +149,13 @@ static TEE_Result dra7_rng_init(void)
 			RNG_CONFIG_MIN_REFIL_CYCLES_SHIFT;
 	val |= RNG_CONFIG_MAX_REFIL_CYCLES <<
 			RNG_CONFIG_MAX_REFIL_CYCLES_SHIFT;
-	write32(val, rng + RNG_CONFIG);
+	io_write32(rng + RNG_CONFIG, val);
 
 	/* Configure the desired FROs */
-	write32(0x0, rng + RNG_FRODETUNE);
+	io_write32(rng + RNG_FRODETUNE, 0x0);
 
 	/* Enable all FROs */
-	write32(0xffffff, rng + RNG_FROENABLE);
+	io_write32(rng + RNG_FROENABLE, 0xffffff);
 
 	/*
 	 * Select the maximum number of samples after
@@ -168,12 +169,12 @@ static TEE_Result dra7_rng_init(void)
 	 * allowed to be shut downed
 	 */
 	val |= RNG_SHUTDOWN_THRESHOLD << RNG_ALARMCNT_SHUTDOWN_TH_SHIFT;
-	write32(val, rng + RNG_ALARMCNT);
+	io_write32(rng + RNG_ALARMCNT, val);
 
 	/* Enable the RNG module */
 	val = RNG_CONTROL_STARTUP_CYCLES << RNG_CONTROL_STARTUP_CYCLES_SHIFT;
 	val |= ENABLE_TRNG;
-	write32(val, rng + RNG_CONTROL);
+	io_write32(rng + RNG_CONTROL, val);
 
 	IMSG("DRA7x TRNG initialized");
 

core/drivers/gic.c
@@ -99,18 +99,18 @@ static size_t probe_max_it(vaddr_t gicc_base __maybe_unused, vaddr_t gicd_base)
 	old_ctlr = read_icc_ctlr();
 	write_icc_ctlr(0);
 #else
-	old_ctlr = read32(gicc_base + GICC_CTLR);
-	write32(0, gicc_base + GICC_CTLR);
+	old_ctlr = io_read32(gicc_base + GICC_CTLR);
+	io_write32(gicc_base + GICC_CTLR, 0);
 #endif
 	for (i = max_regs; i >= 0; i--) {
 		uint32_t old_reg;
 		uint32_t reg;
 		int b;
 
-		old_reg = read32(gicd_base + GICD_ISENABLER(i));
-		write32(0xffffffff, gicd_base + GICD_ISENABLER(i));
-		reg = read32(gicd_base + GICD_ISENABLER(i));
-		write32(~old_reg, gicd_base + GICD_ICENABLER(i));
+		old_reg = io_read32(gicd_base + GICD_ISENABLER(i));
+		io_write32(gicd_base + GICD_ISENABLER(i), 0xffffffff);
+		reg = io_read32(gicd_base + GICD_ISENABLER(i));
+		io_write32(gicd_base + GICD_ICENABLER(i), ~old_reg);
 		for (b = NUM_INTS_PER_REG - 1; b >= 0; b--) {
 			if (BIT32(b) & reg) {
 				ret = i * NUM_INTS_PER_REG + b;
@@ -122,7 +122,7 @@ out:
 #if defined(CFG_ARM_GICV3)
 	write_icc_ctlr(old_ctlr);
 #else
-	write32(old_ctlr, gicc_base + GICC_CTLR);
+	io_write32(gicc_base + GICC_CTLR, old_ctlr);
 #endif
 	return ret;
 }
@@ -140,7 +140,7 @@ void gic_cpu_init(struct gic_data *gd)
 	 * ID8-ID15(SGI)  for Secure interrupts.
 	 * All PPI config as Non-secure interrupts.
 	 */
-	write32(0xffff00ff, gd->gicd_base + GICD_IGROUPR(0));
+	io_write32(gd->gicd_base + GICD_IGROUPR(0), 0xffff00ff);
 
 	/* Set the priority mask to permit Non-secure interrupts, and to
 	 * allow the Non-secure world to adjust the priority mask itself
@@ -150,11 +150,12 @@ void gic_cpu_init(struct gic_data *gd)
 	write_icc_ctlr(GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 |
 		       GICC_CTLR_FIQEN);
 #else
-	write32(0x80, gd->gicc_base + GICC_PMR);
+	io_write32(gd->gicc_base + GICC_PMR, 0x80);
 
 	/* Enable GIC */
-	write32(GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 | GICC_CTLR_FIQEN,
-		gd->gicc_base + GICC_CTLR);
+	io_write32(gd->gicc_base + GICC_CTLR,
+		   GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 |
+		   GICC_CTLR_FIQEN);
 #endif
 }
 
@@ -167,10 +168,10 @@ void gic_init(struct gic_data *gd, vaddr_t gicc_base __maybe_unused,
 
 	for (n = 0; n <= gd->max_it / NUM_INTS_PER_REG; n++) {
 		/* Disable interrupts */
-		write32(0xffffffff, gd->gicd_base + GICD_ICENABLER(n));
+		io_write32(gd->gicd_base + GICD_ICENABLER(n), 0xffffffff);
 
 		/* Make interrupts non-pending */
-		write32(0xffffffff, gd->gicd_base + GICD_ICPENDR(n));
+		io_write32(gd->gicd_base + GICD_ICPENDR(n), 0xffffffff);
 
 		/* Mark interrupts non-secure */
 		if (n == 0) {
@@ -179,9 +180,9 @@ void gic_init(struct gic_data *gd, vaddr_t gicc_base __maybe_unused,
                          * ID8-ID15(SGI)  for Secure interrupts.
                          * All PPI config as Non-secure interrupts.
 			 */
-			write32(0xffff00ff, gd->gicd_base + GICD_IGROUPR(n));
+			io_write32(gd->gicd_base + GICD_IGROUPR(n), 0xffff00ff);
 		} else {
-			write32(0xffffffff, gd->gicd_base + GICD_IGROUPR(n));
+			io_write32(gd->gicd_base + GICD_IGROUPR(n), 0xffffffff);
 		}
 	}
 
@@ -193,14 +194,14 @@ void gic_init(struct gic_data *gd, vaddr_t gicc_base __maybe_unused,
 	write_icc_ctlr(GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 |
 		       GICC_CTLR_FIQEN);
 #else
-	write32(0x80, gd->gicc_base + GICC_PMR);
+	io_write32(gd->gicc_base + GICC_PMR, 0x80);
 
 	/* Enable GIC */
-	write32(GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1 | GICC_CTLR_FIQEN,
-		gd->gicc_base + GICC_CTLR);
+	io_write32(gd->gicc_base + GICC_CTLR, GICC_CTLR_FIQEN |
+		   GICC_CTLR_ENABLEGRP0 | GICC_CTLR_ENABLEGRP1);
 #endif
-	write32(read32(gd->gicd_base + GICD_CTLR) | GICD_CTLR_ENABLEGRP0 |
-		GICD_CTLR_ENABLEGRP1, gd->gicd_base + GICD_CTLR);
+	io_setbits32(gd->gicd_base + GICD_CTLR,
+		     GICD_CTLR_ENABLEGRP0 | GICD_CTLR_ENABLEGRP1);
 }
 
 void gic_init_base_addr(struct gic_data *gd, vaddr_t gicc_base __maybe_unused,
@@ -218,12 +219,11 @@ static void gic_it_add(struct gic_data *gd, size_t it)
 	uint32_t mask = 1 << (it % NUM_INTS_PER_REG);
 
 	/* Disable the interrupt */
-	write32(mask, gd->gicd_base + GICD_ICENABLER(idx));
+	io_write32(gd->gicd_base + GICD_ICENABLER(idx), mask);
 	/* Make it non-pending */
-	write32(mask, gd->gicd_base + GICD_ICPENDR(idx));
+	io_write32(gd->gicd_base + GICD_ICPENDR(idx), mask);
 	/* Assign it to group0 */
-	write32(read32(gd->gicd_base + GICD_IGROUPR(idx)) & ~mask,
-			gd->gicd_base + GICD_IGROUPR(idx));
+	io_clrbits32(gd->gicd_base + GICD_IGROUPR(idx), mask);
 }
 
 static void gic_it_set_cpu_mask(struct gic_data *gd, size_t it,
@@ -232,22 +232,20 @@ static void gic_it_set_cpu_mask(struct gic_data *gd, size_t it,
 	size_t idx __maybe_unused = it / NUM_INTS_PER_REG;
 	uint32_t mask __maybe_unused = 1 << (it % NUM_INTS_PER_REG);
 	uint32_t target, target_shift;
+	vaddr_t itargetsr = gd->gicd_base +
+			    GICD_ITARGETSR(it / NUM_TARGETS_PER_REG);
 
 	/* Assigned to group0 */
-	assert(!(read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
+	assert(!(io_read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
 
 	/* Route it to selected CPUs */
-	target = read32(gd->gicd_base +
-			GICD_ITARGETSR(it / NUM_TARGETS_PER_REG));
+	target = io_read32(itargetsr);
 	target_shift = (it % NUM_TARGETS_PER_REG) * ITARGETSR_FIELD_BITS;
 	target &= ~(ITARGETSR_FIELD_MASK << target_shift);
 	target |= cpu_mask << target_shift;
-	DMSG("cpu_mask: writing 0x%x to 0x%" PRIxVA,
-	     target, gd->gicd_base + GICD_ITARGETSR(it / NUM_TARGETS_PER_REG));
-	write32(target,
-		gd->gicd_base + GICD_ITARGETSR(it / NUM_TARGETS_PER_REG));
-	DMSG("cpu_mask: 0x%x\n",
-	     read32(gd->gicd_base + GICD_ITARGETSR(it / NUM_TARGETS_PER_REG)));
+	DMSG("cpu_mask: writing 0x%x to 0x%" PRIxVA, target, itargetsr);
+	io_write32(itargetsr, target);
+	DMSG("cpu_mask: 0x%x", io_read32(itargetsr));
 }
 
 static void gic_it_set_prio(struct gic_data *gd, size_t it, uint8_t prio)
@@ -256,31 +254,32 @@ static void gic_it_set_prio(struct gic_data *gd, size_t it, uint8_t prio)
 	uint32_t mask __maybe_unused = 1 << (it % NUM_INTS_PER_REG);
 
 	/* Assigned to group0 */
-	assert(!(read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
+	assert(!(io_read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
 
 	/* Set prio it to selected CPUs */
 	DMSG("prio: writing 0x%x to 0x%" PRIxVA,
 		prio, gd->gicd_base + GICD_IPRIORITYR(0) + it);
-	write8(prio, gd->gicd_base + GICD_IPRIORITYR(0) + it);
+	io_write8(gd->gicd_base + GICD_IPRIORITYR(0) + it, prio);
 }
 
 static void gic_it_enable(struct gic_data *gd, size_t it)
 {
 	size_t idx = it / NUM_INTS_PER_REG;
 	uint32_t mask = 1 << (it % NUM_INTS_PER_REG);
+	vaddr_t base = gd->gicd_base;
 
 	/* Assigned to group0 */
-	assert(!(read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
+	assert(!(io_read32(base + GICD_IGROUPR(idx)) & mask));
 	if (it >= NUM_SGI) {
 		/*
 		 * Not enabled yet, except Software Generated Interrupt
 		 * which is implementation defined
 		 */
-		assert(!(read32(gd->gicd_base + GICD_ISENABLER(idx)) & mask));
+		assert(!(io_read32(base + GICD_ISENABLER(idx)) & mask));
 	}
 
 	/* Enable the interrupt */
-	write32(mask, gd->gicd_base + GICD_ISENABLER(idx));
+	io_write32(base + GICD_ISENABLER(idx), mask);
 }
 
 static void gic_it_disable(struct gic_data *gd, size_t it)
@@ -289,10 +288,10 @@ static void gic_it_disable(struct gic_data *gd, size_t it)
 	uint32_t mask = 1 << (it % NUM_INTS_PER_REG);
 
 	/* Assigned to group0 */
-	assert(!(read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
+	assert(!(io_read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
 
 	/* Disable the interrupt */
-	write32(mask, gd->gicd_base + GICD_ICENABLER(idx));
+	io_write32(gd->gicd_base + GICD_ICENABLER(idx), mask);
 }
 
 static void gic_it_set_pending(struct gic_data *gd, size_t it)
@@ -303,10 +302,10 @@ static void gic_it_set_pending(struct gic_data *gd, size_t it)
 	/* Should be Peripheral Interrupt */
 	assert(it >= NUM_SGI);
 	/* Assigned to group0 */
-	assert(!(read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
+	assert(!(io_read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask));
 
 	/* Raise the interrupt */
-	write32(mask, gd->gicd_base + GICD_ISPENDR(idx));
+	io_write32(gd->gicd_base + GICD_ISPENDR(idx), mask);
 }
 
 static void gic_it_raise_sgi(struct gic_data *gd, size_t it,
@@ -322,7 +321,7 @@ static void gic_it_raise_sgi(struct gic_data *gd, size_t it,
 	assert(it < NUM_SGI);
 
 	/* Raise the interrupt */
-	write32(mask, gd->gicd_base + GICD_SGIR);
+	io_write32(gd->gicd_base + GICD_SGIR, mask);
 }
 
 static uint32_t gic_read_iar(struct gic_data *gd __maybe_unused)
@@ -330,7 +329,7 @@ static uint32_t gic_read_iar(struct gic_data *gd __maybe_unused)
 #if defined(CFG_ARM_GICV3)
 	return read_icc_iar1();
 #else
-	return read32(gd->gicc_base + GICC_IAR);
+	return io_read32(gd->gicc_base + GICC_IAR);
 #endif
 }
 
@@ -339,7 +338,7 @@ static void gic_write_eoir(struct gic_data *gd __maybe_unused, uint32_t eoir)
 #if defined(CFG_ARM_GICV3)
 	write_icc_eoir1(eoir);
 #else
-	write32(eoir, gd->gicc_base + GICC_EOIR);
+	io_write32(gd->gicc_base + GICC_EOIR, eoir);
 #endif
 }
 
@@ -347,14 +346,14 @@ static bool gic_it_is_enabled(struct gic_data *gd, size_t it)
 {
 	size_t idx = it / NUM_INTS_PER_REG;
 	uint32_t mask = 1 << (it % NUM_INTS_PER_REG);
-	return !!(read32(gd->gicd_base + GICD_ISENABLER(idx)) & mask);
+	return !!(io_read32(gd->gicd_base + GICD_ISENABLER(idx)) & mask);
 }
 
 static bool __maybe_unused gic_it_get_group(struct gic_data *gd, size_t it)
 {
 	size_t idx = it / NUM_INTS_PER_REG;
 	uint32_t mask = 1 << (it % NUM_INTS_PER_REG);
-	return !!(read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask);
+	return !!(io_read32(gd->gicd_base + GICD_IGROUPR(idx)) & mask);
 }
 
 static uint32_t __maybe_unused gic_it_get_target(struct gic_data *gd, size_t it)
@@ -363,11 +362,9 @@ static uint32_t __maybe_unused gic_it_get_target(struct gic_data *gd, size_t it)
 	uint32_t target_shift = (it % NUM_TARGETS_PER_REG) *
 				ITARGETSR_FIELD_BITS;
 	uint32_t target_mask = ITARGETSR_FIELD_MASK << target_shift;
-	uint32_t target =
-		read32(gd->gicd_base + GICD_ITARGETSR(reg_idx)) & target_mask;
+	uint32_t target = io_read32(gd->gicd_base + GICD_ITARGETSR(reg_idx));
 
-	target = target >> target_shift;
-	return target;
+	return (target & target_mask) >> target_shift;
 }
 
 void gic_dump_state(struct gic_data *gd)
@@ -377,9 +374,9 @@ void gic_dump_state(struct gic_data *gd)
 #if defined(CFG_ARM_GICV3)
 	DMSG("GICC_CTLR: 0x%x", read_icc_ctlr());
 #else
-	DMSG("GICC_CTLR: 0x%x", read32(gd->gicc_base + GICC_CTLR));
+	DMSG("GICC_CTLR: 0x%x", io_read32(gd->gicc_base + GICC_CTLR));
 #endif
-	DMSG("GICD_CTLR: 0x%x", read32(gd->gicd_base + GICD_CTLR));
+	DMSG("GICD_CTLR: 0x%x", io_read32(gd->gicd_base + GICD_CTLR));
 
 	for (i = 0; i < (int)gd->max_it; i++) {
 		if (gic_it_is_enabled(gd, i)) {

core/drivers/hi16xx_rng.c
@@ -44,17 +44,17 @@ static TEE_Result hi16xx_rng_init(void)
 	TEE_Time time;
 
 	/* ALG sub-controller must allow RNG out of reset */
-	write32(ALG_SC_SRST_DREQ_RNG, alg + ALG_SC_RNG_RESET_DREQ);
+	io_write32(alg + ALG_SC_RNG_RESET_DREQ, ALG_SC_SRST_DREQ_RNG);
 
 	/* Set initial seed */
 	tee_time_get_sys_time(&time);
-	write32(time.seconds * 1000 + time.millis, rng + RNG_SEED);
+	io_write32(rng + RNG_SEED, time.seconds * 1000 + time.millis);
 
 	/*
 	 * Enable RNG and configure it to re-seed automatically from the
 	 * internal ring oscillator
 	 */
-	write32(RNG_EN | RNG_RING_EN | RNG_SEED_SEL, rng + RNG_CTRL);
+	io_write32(rng + RNG_CTRL, RNG_EN | RNG_RING_EN | RNG_SEED_SEL);
 
 	IMSG("Hi16xx RNG initialized");
 	return TEE_SUCCESS;
@@ -77,7 +77,7 @@ uint8_t hw_get_random_byte(void)
 		r = (vaddr_t)phys_to_virt(RNG_BASE, MEM_AREA_IO_SEC) + RNG_NUM;
 
 	if (!pos)
-		random.val = read32(r);
+		random.val = io_read32(r);
 
 	ret = random.byte[pos++];
 

core/drivers/hi16xx_uart.c
@@ -70,7 +70,7 @@ static void hi16xx_uart_flush(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	while (!(read32(base + UART_USR) & UART_USR_TFE_BIT))
+	while (!(io_read32(base + UART_USR) & UART_USR_TFE_BIT))
 		;
 }
 
@@ -79,18 +79,18 @@ static void hi16xx_uart_putc(struct serial_chip *chip, int ch)
 	vaddr_t base = chip_to_base(chip);
 
 	/* Wait until TX FIFO is empty */
-	while (!(read32(base + UART_USR) & UART_USR_TFE_BIT))
+	while (!(io_read32(base + UART_USR) & UART_USR_TFE_BIT))
 		;
 
 	/* Put character into TX FIFO */
-	write32(ch & 0xFF, base + UART_THR);
+	io_write32(base + UART_THR, ch & 0xFF);
 }
 
 static bool hi16xx_uart_have_rx_data(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
-	return (read32(base + UART_USR) & UART_USR_RFNE_BIT);
+	return (io_read32(base + UART_USR) & UART_USR_RFNE_BIT);
 }
 
 static int hi16xx_uart_getchar(struct serial_chip *chip)
@@ -99,7 +99,7 @@ static int hi16xx_uart_getchar(struct serial_chip *chip)
 
 	while (!hi16xx_uart_have_rx_data(chip))
 		;
-	return read32(base + UART_RBR) & 0xFF;
+	return io_read32(base + UART_RBR) & 0xFF;
 }
 
 static const struct serial_ops hi16xx_uart_ops = {
@@ -119,22 +119,22 @@ void hi16xx_uart_init(struct hi16xx_uart_data *pd, paddr_t base,
 	pd->chip.ops = &hi16xx_uart_ops;
 
 	/* Enable (and clear) FIFOs */
-	write32(UART_FCR_FIFO_EN, base + UART_FCR);
+	io_write32(base + UART_FCR, UART_FCR_FIFO_EN);
 
 	/* Enable access to _DLL and _DLH */
-	write32(UART_LCR_DLAB, base + UART_LCR);
+	io_write32(base + UART_LCR, UART_LCR_DLAB);
 
 	/* Calculate and set UART_DLL */
-	write32(freq_div & 0xFF, base + UART_DLL);
+	io_write32(base + UART_DLL, freq_div & 0xFF);
 
 	/* Calculate and set UART_DLH */
-	write32((freq_div >> 8) & 0xFF, base + UART_DLH);
+	io_write32(base + UART_DLH, (freq_div >> 8) & 0xFF);
 
 	/* Clear _DLL/_DLH access bit, set data size (8 bits), parity etc. */
-	write32(UART_LCR_DLS8, base + UART_LCR);
+	io_write32(base + UART_LCR, UART_LCR_DLS8);
 
 	/* Disable interrupt mode */
-	write32(0, base + UART_IEL);
+	io_write32(base + UART_IEL, 0);
 
 	hi16xx_uart_flush(&pd->chip);
 }

commit c6d43e9d515293e361dafa506084a60a89c60b6b

pl022_spi: fix non-trivial typo
read16() was replaced with io_read8() instead of io_read16() so fix it.

Fixes: 918bb3a5 ("core: upgrade from write32() to io_write32() and friends")

Signed-off-by: Victor Chong <victor.chong@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/pl022_spi.c
@@ -204,7 +204,7 @@ static enum spi_result pl022_txrx16(struct spi_chip *chip, uint16_t *wdat,
 			if (rdat)
 				if (io_read8(pd->base + SSPSR) & SSPSR_RNE) {
 					/* rx 1 packet */
-					rdat[j++] = io_read8(pd->base + SSPDR);
+					rdat[j++] = io_read16(pd->base + SSPDR);
 				}
 		}
 
@@ -213,7 +213,7 @@ static enum spi_result pl022_txrx16(struct spi_chip *chip, uint16_t *wdat,
 		while ((j < num_pkts) &&
 		       (io_read8(pd->base + SSPSR) & SSPSR_RNE)) {
 			/* rx 1 packet */
-			rdat[j++] = io_read8(pd->base + SSPDR);
+			rdat[j++] = io_read16(pd->base + SSPDR);
 		}
 
 		if (j < num_pkts) {