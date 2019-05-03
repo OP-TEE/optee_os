b8fa2c7da0e43b4619a44e21a3268f8e7ebd659d
Etienne Carriere <etienne.carriere@st.com>
2019-05-03 10:18:24 +0200

    stm32mp1: common SoC registers protections
    Helper functions for stm32mp1 platform to access a SoC interface
    register that can be accessed from several drivers and services.
    They all use a common spinlock to ensure atomic update of the
    register content.
    
    Helpers: io_mask32_stm32shregs(), io_setbits32_stm32shregs(),
    io_clrbits32_stm32shregs() and io_clrsetbits32_stm32shregs().
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-stm32mp1/shared_resources.c
+++ core/arch/arm/plat-stm32mp1/shared_resources.c
@@ -27,6 +27,40 @@
 static bool registering_locked;
 
 /*
+ * Shared register access: upon shared resource lock
+ */
+static unsigned int shregs_lock = SPINLOCK_UNLOCK;
+
+/* Shared resource lock: assume not required if MMU is disabled */
+static uint32_t lock_stm32shregs(void)
+{
+	return may_spin_lock(&shregs_lock);
+}
+
+static void unlock_stm32shregs(uint32_t exceptions)
+{
+	may_spin_unlock(&shregs_lock, exceptions);
+}
+
+void io_mask32_stm32shregs(vaddr_t va, uint32_t value, uint32_t mask)
+{
+	uint32_t exceptions = lock_stm32shregs();
+
+	io_mask32(va, value, mask);
+
+	unlock_stm32shregs(exceptions);
+}
+
+void io_clrsetbits32_stm32shregs(vaddr_t va, uint32_t clr, uint32_t set)
+{
+	uint32_t exceptions = lock_stm32shregs();
+
+	io_clrsetbits32(va, clr, set);
+
+	unlock_stm32shregs(exceptions);
+}
+
+/*
  * Shared peripherals and resources registration
  *
  * Each resource assignation is stored in a table. The state defaults
