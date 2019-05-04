628f5b726a8b7e7d7fcef5b210df66a9b09d299a
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2019-05-05 00:45:17 +0100

    core: imx: Add simple CAAM permissions set routine
    When we transition to secure-world certain parts of the CAAM become opaque
    to normal world. This patch adds a simple routine to set CAAM job-ring
    permissions to normal-world by default.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

---
+++ core/arch/arm/plat-imx/imx_caam.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2019 Bryan O'Donoghue
+ *
+ * Bryan O'Donoghue <bryan.odonoghue@linaro.org>
+ */
+
+#include <io.h>
+#include <imx_caam.h>
+#include <kernel/generic_boot.h>
+#include <platform_config.h>
+#include <stdint.h>
+
+void init_caam(void)
+{
+	struct imx_caam_ctrl *caam = (struct caam_ctrl *)CAAM_BASE;
+	uint32_t reg;
+	int i;
+
+	/*
+	 * Set job-ring ownership to non-secure by default.
+	 * A Linux kernel that runs after OP-TEE will run in normal-world
+	 * so we want to enable that kernel to have total ownership of the
+	 * CAAM job-rings.
+	 *
+	 * It is possible to use CAAM job-rings inside of OP-TEE i.e. in
+	 * secure world code but, to do that OP-TEE and kernel should agree
+	 * via a DTB which job-rings are owned by OP-TEE and which are
+	 * owned by Kernel, something that the OP-TEE CAAM driver should
+	 * set up.
+	 *
+	 * This code below simply sets a default for the case where no
+	 * runtime OP-TEE CAAM code will be run
+	 */
+	for (i = 0; i < CAAM_NUM_JOB_RINGS; i++) {
+		reg = io_read32((vaddr_t)&caam->jr[i].jrmidr_ms);
+		reg |= JROWN_NS | JROWN_MID;
+		io_write32((vaddr_t)&caam->jr[i].jrmidr_ms, reg);
+	}
+}
