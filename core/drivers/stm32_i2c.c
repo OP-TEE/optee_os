OP-TEE/optee_os

commit c75303f777b76d6b61a6ccaddb81d7b544c731dc

stm32_i2c: handle pinctrl
Get pinctrl support from stm32_gpio.h into STM32 I2C driver.
When device tree content defines pins related to an I2C interface,
the I2C driver saves the pins configuration instances and set the
registered pins in the expected power mode at runtime.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/stm32_i2c.c
@@ -648,7 +648,9 @@ static int i2c_config_analog_filter(struct i2c_handle_s *hi2c,
 }
 
 int stm32_i2c_get_setup_from_fdt(void *fdt, int node,
-				 struct stm32_i2c_init_s *init)
+				 struct stm32_i2c_init_s *init,
+				 struct stm32_pinctrl **pinctrl,
+				 size_t *pinctrl_count)
 {
 	const fdt32_t *cuint = NULL;
 	struct dt_node_info info = { .status = 0 };
@@ -693,6 +695,24 @@ int stm32_i2c_get_setup_from_fdt(void *fdt, int node,
 	} else {
 		init->speed_mode = STM32_I2C_SPEED_DEFAULT;
 	}
+
+	count = stm32_pinctrl_fdt_get_pinctrl(fdt, node, NULL, 0);
+	if (count <= 0) {
+		*pinctrl = NULL;
+		*pinctrl_count = 0;
+		return count;
+	}
+
+	if (count > 2)
+		panic("Too many PINCTRLs found");
+
+	*pinctrl = calloc(count, sizeof(**pinctrl));
+	if (!*pinctrl)
+		panic();
+
+	*pinctrl_count = stm32_pinctrl_fdt_get_pinctrl(fdt, node,
+						       *pinctrl, count);
+	assert(*pinctrl_count == (unsigned int)count);
 
 	return 0;
 }
@@ -1381,8 +1401,10 @@ void stm32_i2c_resume(struct i2c_handle_s *hi2c)
 	    (hi2c->i2c_state != I2C_STATE_SUSPENDED))
 		panic();
 
+	stm32_pinctrl_load_active_cfg(hi2c->pinctrl, hi2c->pinctrl_count);
+
 	if (hi2c->i2c_state == I2C_STATE_RESET) {
-		/* This is no valid I2C configuration loaded yet */
+		/* There is no valid I2C configuration to be loaded yet */
 		return;
 	}
 
@@ -1400,6 +1422,7 @@ void stm32_i2c_suspend(struct i2c_handle_s *hi2c)
 		panic();
 
 	save_cfg(hi2c, &hi2c->sec_cfg);
+	stm32_pinctrl_load_standby_cfg(hi2c->pinctrl, hi2c->pinctrl_count);
 
 	hi2c->i2c_state = I2C_STATE_SUSPENDED;
 }

core/include/drivers/stm32_i2c.h
@@ -6,6 +6,7 @@
 #ifndef __STM32_I2C_H
 #define __STM32_I2C_H
 
+#include <drivers/stm32_gpio.h>
 #include <mm/core_memprot.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -89,6 +90,8 @@ struct i2c_cfg {
  * @i2c_state: Driver state ID I2C_STATE_*
  * @i2c_err: Last error code I2C_ERROR_*
  * @sec_cfg: I2C regsiters configuration storage
+ * @pinctrl: PINCTRLs configuration for the I2C PINs
+ * @pinctrl_count: Number of PINCTRLs elements
  */
 struct i2c_handle_s {
 	struct io_pa_va base;
@@ -96,6 +99,8 @@ struct i2c_handle_s {
 	enum i2c_state_e i2c_state;
 	uint32_t i2c_err;
 	struct i2c_cfg sec_cfg;
+	struct stm32_pinctrl *pinctrl;
+	size_t pinctrl_count;
 };
 
 /* STM32 specific defines */
@@ -112,10 +117,14 @@ struct i2c_handle_s {
  * @fdt: Reference to DT
  * @node: Target I2C node in the DT
  * @init: Output stm32_i2c_init_s structure
+ * @pinctrl: Reference to output pinctrl array
+ * @pinctrl_count: Input @pinctrl array size, output expected size
  * Return 0 on success else a negative value
  */
 int stm32_i2c_get_setup_from_fdt(void *fdt, int node,
-				 struct stm32_i2c_init_s *init);
+				 struct stm32_i2c_init_s *init,
+				 struct stm32_pinctrl **pinctrl,
+				 size_t *pinctrl_count);
 
 /*
  * Initialize I2C bus handle from input configuration directives

commit 78d33888c02e9693115dfde3bed1ead894e43dc0

stm32_i2c: fix bug in device tree support
Correct missing local variable in stm32_i2c_get_setup_from_fdt().

Fixes: c75303f777b7 ("stm32_i2c: handle pinctrl")

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

core/drivers/stm32_i2c.c
@@ -654,6 +654,7 @@ int stm32_i2c_get_setup_from_fdt(void *fdt, int node,
 {
 	const fdt32_t *cuint = NULL;
 	struct dt_node_info info = { .status = 0 };
+	int count = 0;
 
 	/* Default STM32 specific configs caller may need to overwrite */
 	memset(init, 0, sizeof(*init));