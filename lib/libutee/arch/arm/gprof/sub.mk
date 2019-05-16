d6cd5328b2deb1f3235b501dcc4b485bd5bd9f9c
Sumit Garg <sumit.garg@linaro.org>
2019-05-16 18:49:30 +0530

    Rename CFG_ULIBS_GPROF to CFG_ULIBS_MCOUNT
    CFG_ULIBS_GPROF builds the user mode libraries with -pg, which adds
    instrumentation to all functions in the form of a call to mcount().
    This was historically used by gprof, but other tools can benefit from
    this instrumentation. Therefore, rename the config flag as well as a
    couple of source files to remove the reference to gprof.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey960)

--- lib/libutee/arch/arm/gprof/sub.mk
+++ lib/libutee/arch/arm/gprof/sub.mk
@@ -3,5 +3,3 @@ cppflags-y += -I$(sub-dir)/../../..
 srcs-$(CFG_TA_GPROF_SUPPORT) += gprof.c
 srcs-$(CFG_TA_GPROF_SUPPORT) += gprof_pta.c
 cflags-remove-gprof.c-y += -Wcast-align
-srcs-$(CFG_ARM32_$(sm)) += gprof_a32.S
-srcs-$(CFG_ARM64_$(sm)) += gprof_a64.S
