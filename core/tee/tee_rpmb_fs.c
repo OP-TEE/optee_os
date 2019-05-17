OP-TEE/optee_os

commit ea81076f7896de7278dcd62b47b99d5dc3351caf

core: RPMB FS: check for potential overflows
This commit deals with a number of potential integer overflows in the
RPMB FS code.

rpmb_fs_init() requests device information from the REE. The RPMB size
is returned in struct rpmb_dev_info (field rpmb_size_mult) and is used
in a multiplication that could overflow. Use MUL_OVERFLOW() to deal with
this case.

Some overflow checks are also added in the read and write paths.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reported-by: Bastien Simondi <bsimondi@netflix.com> [2.12]
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

core/tee/tee_rpmb_fs.c
@@ -1117,8 +1117,13 @@ static TEE_Result tee_rpmb_init(uint16_t dev_id)
 			goto func_exit;
 		}
 
-		rpmb_ctx->max_blk_idx = (dev_info.rpmb_size_mult *
-					 RPMB_SIZE_SINGLE / RPMB_DATA_SIZE) - 1;
+		if (MUL_OVERFLOW(dev_info.rpmb_size_mult,
+				 RPMB_SIZE_SINGLE / RPMB_DATA_SIZE,
+				 &rpmb_ctx->max_blk_idx)) {
+			res = TEE_ERROR_BAD_PARAMETERS;
+			goto func_exit;
+		}
+		rpmb_ctx->max_blk_idx--;
 
 		memcpy(rpmb_ctx->cid, dev_info.cid, RPMB_EMMC_CID_SIZE);
 
@@ -1192,6 +1197,10 @@ static TEE_Result tee_rpmb_read(uint16_t dev_id, uint32_t addr, uint8_t *data,
 	blk_idx = addr / RPMB_DATA_SIZE;
 	byte_offset = addr % RPMB_DATA_SIZE;
 
+	if (len + byte_offset + RPMB_DATA_SIZE < RPMB_DATA_SIZE) {
+		/* Overflow */
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
 	blkcnt =
 	    ROUNDUP(len + byte_offset, RPMB_DATA_SIZE) / RPMB_DATA_SIZE;
 	res = tee_rpmb_init(dev_id);
@@ -2049,8 +2058,14 @@ static TEE_Result rpmb_fs_write_primitive(struct rpmb_file_handle *fh,
 	if (fh->fat_entry.flags & FILE_IS_LAST_ENTRY)
 		panic("invalid last entry flag");
 
-	end = pos + size;
-	start_addr = fh->fat_entry.start_address + pos;
+	if (ADD_OVERFLOW(pos, size, &end)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+	if (ADD_OVERFLOW(fh->fat_entry.start_address, pos, &start_addr)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
 
 	if (end <= fh->fat_entry.data_size &&
 	    tee_rpmb_write_is_atomic(CFG_RPMB_FS_DEV_ID, start_addr, size)) {

commit 45efe1cd021e3dca8d225a2210f618baf2d91568

core: RPMB FS: fix panic when RPMB partition size is 16 MiB
The overflow check used when computing the number of the last block in
the RPMB parition is incorrect. It causes an overflow when
rpmb_size_mult is 128, that is, when the partition size is 16 MiB.
Indeed, max_blk_idx is a uint16_t and we are trying to store 65536
(= 128 * (128 * 1024) / 256).

Fix this by using a 32-bit temporary variable to hold the result of the
multiplication (the number of blocks), then subtract 1 to get the last
block number using SUB_OVERFLOW().

Fixes: ea81076f7896 ("core: RPMB FS: check for potential overflows")
Fixes: https://github.com/OP-TEE/optee_os/issues/3012
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reported-by: Pengguang Zhu <zpghao@163.com>
Suggested-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_rpmb_fs.c
@@ -1041,6 +1041,7 @@ static TEE_Result tee_rpmb_init(uint16_t dev_id)
 {
 	TEE_Result res = TEE_SUCCESS;
 	struct rpmb_dev_info dev_info;
+	uint32_t nblocks = 0;
 
 	if (!rpmb_ctx) {
 		rpmb_ctx = calloc(1, sizeof(struct tee_rpmb_ctx));
@@ -1071,12 +1072,11 @@ static TEE_Result tee_rpmb_init(uint16_t dev_id)
 		}
 
 		if (MUL_OVERFLOW(dev_info.rpmb_size_mult,
-				 RPMB_SIZE_SINGLE / RPMB_DATA_SIZE,
-				 &rpmb_ctx->max_blk_idx)) {
+				 RPMB_SIZE_SINGLE / RPMB_DATA_SIZE, &nblocks) ||
+		    SUB_OVERFLOW(nblocks, 1, &rpmb_ctx->max_blk_idx)) {
 			res = TEE_ERROR_BAD_PARAMETERS;
 			goto func_exit;
 		}
-		rpmb_ctx->max_blk_idx--;
 
 		memcpy(rpmb_ctx->cid, dev_info.cid, RPMB_EMMC_CID_SIZE);
 