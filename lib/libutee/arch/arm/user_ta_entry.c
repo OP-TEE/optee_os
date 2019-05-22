OP-TEE/optee_os

commit eeb866c431db3068e83d277c814ac503d1f3b91f

Add TA entry point function: __ta_entry()
Symbol __utee_entry  may be undefined in a TA in case libutee is
built as a shared library (CFG_ULIBS_SHARED=y). Add a wrapper function
to avoid this issue.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

ta/arch/arm/user_ta_header.c
@@ -28,6 +28,15 @@ void __utee_entry(unsigned long func, unsigned long session_id,
 			struct utee_params *up, unsigned long cmd_id)
 			__noreturn;
 
+void __noreturn __ta_entry(unsigned long func, unsigned long session_id,
+			   struct utee_params *up, unsigned long cmd_id);
+
+void __noreturn __ta_entry(unsigned long func, unsigned long session_id,
+			   struct utee_params *up, unsigned long cmd_id)
+{
+	__utee_entry(func, session_id, up, cmd_id);
+}
+
 /*
  * According to GP Internal API, TA_STACK_SIZE corresponds to the stack
  * size used by the TA code itself and does not include stack space

ta/arch/arm/link.mk
@@ -15,7 +15,7 @@ cleanfiles += $(link-out-dir$(sm))/$(user-ta-uuid).stripped.elf
 cleanfiles += $(link-out-dir$(sm))/$(user-ta-uuid).ta
 cleanfiles += $(link-script-pp$(sm)) $(link-script-dep$(sm))
 
-link-ldflags  = -e__utee_entry -pie
+link-ldflags  = -e__ta_entry -pie
 link-ldflags += -T $(link-script-pp$(sm))
 link-ldflags += -Map=$(link-out-dir$(sm))/$(user-ta-uuid).map
 link-ldflags += --sort-section=alignment

commit fa3b5c9584ab89eee48a24ab08675df8b2775bad

Remove redundant __noreturn from __utee_entry()
As __ta_entry() acts as function entry point, it makes sense to
logically return from this api only via utee_return(). So remove
redundant __noreturn from __utee_entry().

Fixes: eeb866c431db ("Add TA entry point function: __ta_entry()")
Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/arch/arm/user_ta_entry.c
@@ -182,7 +182,7 @@ static TEE_Result entry_invoke_command(unsigned long session_id,
 	return res;
 }
 
-void __noreturn __utee_entry(unsigned long func, unsigned long session_id,
+TEE_Result __utee_entry(unsigned long func, unsigned long session_id,
 			struct utee_params *up, unsigned long cmd_id)
 {
 	TEE_Result res;
@@ -211,5 +211,6 @@ void __noreturn __utee_entry(unsigned long func, unsigned long session_id,
 		break;
 	}
 	ta_header_save_params(0, NULL);
-	utee_return(res);
+
+	return res;
 }

lib/libutee/tee_api_private.h
@@ -18,7 +18,7 @@ void __utee_from_param(struct utee_params *up, uint32_t param_types,
 void __utee_to_param(TEE_Param params[TEE_NUM_PARAMS],
 			uint32_t *param_types, const struct utee_params *up);
 
-void __utee_entry(unsigned long func, unsigned long session_id,
+TEE_Result __utee_entry(unsigned long func, unsigned long session_id,
 			struct utee_params *up, unsigned long cmd_id);
 
 

ta/arch/arm/user_ta_header.c
@@ -5,9 +5,10 @@
 #include <compiler.h>
 #include <tee_ta_api.h>
 #include <tee_internal_api_extensions.h>
+#include <trace.h>
 #include <user_ta_header.h>
 #include <user_ta_header_defines.h>
-#include <trace.h>
+#include <utee_syscalls.h>
 
 int trace_level = TRACE_LEVEL;
 
@@ -24,9 +25,8 @@ const char trace_ext_prefix[]  = "TA";
 /* exprted to user_ta_header.c, built within TA */
 struct utee_params;
 
-void __utee_entry(unsigned long func, unsigned long session_id,
-			struct utee_params *up, unsigned long cmd_id)
-			__noreturn;
+TEE_Result __utee_entry(unsigned long func, unsigned long session_id,
+			struct utee_params *up, unsigned long cmd_id);
 
 void __noreturn __ta_entry(unsigned long func, unsigned long session_id,
 			   struct utee_params *up, unsigned long cmd_id);
@@ -34,7 +34,11 @@ void __noreturn __ta_entry(unsigned long func, unsigned long session_id,
 void __noreturn __ta_entry(unsigned long func, unsigned long session_id,
 			   struct utee_params *up, unsigned long cmd_id)
 {
-	__utee_entry(func, session_id, up, cmd_id);
+	TEE_Result res = TEE_SUCCESS;
+
+	res = __utee_entry(func, session_id, up, cmd_id);
+
+	utee_return(res);
 }
 
 /*