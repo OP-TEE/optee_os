66ac790b494bddaa95e1443c1de742b11d6e1837
Jens Wiklander <jens.wiklander@linaro.org>
2019-05-23 17:42:10 +0200

    core: add define TEE_MATTR_LDELF
    Adds the define TEE_MATTR_LDELF for temporary mappings needed to load
    the ldelf binary.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/include/mm/tee_mmu_types.h
+++ core/include/mm/tee_mmu_types.h
@@ -47,6 +47,8 @@
 #define TEE_MATTR_PERMANENT		BIT(17)
 /* Tags TA mappings that may be shared with other TAs. */
 #define TEE_MATTR_SHAREABLE		BIT(18)
+/* Tags temporary mappings added to load the ldelf binary */
+#define TEE_MATTR_LDELF			BIT(19)
 
 #ifdef CFG_CORE_UNMAP_CORE_AT_EL0
 #define TEE_MMU_UMAP_KCODE_IDX	0
