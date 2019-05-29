871944dedfdcd99a9162b62df3793375cf532a06
Joakim Bech <joakim.bech@linaro.org>
2019-05-29 10:50:44 +0200

    PTA: add support for deriving device and TA unique keys
    Enable derivation of device and Trusted Application unique keys that can
    be used by different Trusted Applications directly. An example of use
    case could be when you need to encrypt some data in a Trusted App and
    then give it back to normal world.
    
    By default device unique properties (HUK and TA UUID) will be used when
    deriving a key. However, the one calling the PTA derive key function
    also have the ability to provide some extra data that will be mixed in
    together with existing device unique properties. That gives the ability
    to derive keys that are not only device and Trusted Application unique,
    but also tied to some additional data, it could for example be a
    password or something similar.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libutee/include/pta_system.h
+++ lib/libutee/include/pta_system.h
@@ -14,6 +14,17 @@
 			 0x9c, 0x2d, 0xfa, 0x7a, 0xe0, 0x1b, 0xbe, 0xbc } }
 
 /*
+ * Having keys with too few bits impose a potential security risk, hence set a
+ * lower bound of 128 bits.
+ */
+#define TA_DERIVED_KEY_MIN_SIZE		16
+
+/* Same value as max in huk_subkey_derive */
+#define TA_DERIVED_KEY_MAX_SIZE		32
+
+#define TA_DERIVED_EXTRA_DATA_MAX_SIZE	1024
+
+/*
  * Add (re-seed) caller-provided entropy to the RNG pool. Keymaster
  * implementations need to securely mix the provided entropy into their pool,
  * which also must contain internally-generated entropy from a hardware random
@@ -22,5 +33,18 @@
  * [in]     memref[0]: entropy input data
  */
 #define PTA_SYSTEM_ADD_RNG_ENTROPY	0
+
+/*
+ * Derives a device and TA unique key. The caller can also provide extra data
+ * that will be mixed together with existing device unique properties. If no
+ * extra data is provided, then the derived key will only use device unique
+ * properties and caller TA UUID.
+ *
+ * [in]  params[0].memref.buffer     Buffer for extra data
+ * [in]  params[0].memref.size       Size of extra data (max 1024 bytes)
+ * [out] params[1].memref.buffer     Buffer for the derived key
+ * [out] params[1].memref.size       Size of the derived key (16 to 32 bytes)
+ */
+#define PTA_SYSTEM_DERIVE_TA_UNIQUE_KEY 1
 
 #endif /* __PTA_SYSTEM_H */
