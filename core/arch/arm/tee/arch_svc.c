OP-TEE/optee_os

commit c0bc8d0e7d7202457ae14d470610f44713843174

core: print TA stack dump from thread context
Instead of printing TA stack dump in abort mode, save the required
information and print it from user_ta_enter() in thread context. This
allows dumping the stack also for paged TAs.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/tee/arch_svc.c
@@ -225,36 +225,32 @@ void __weak tee_svc_handler(struct thread_svc_regs *regs)
 #ifdef ARM32
 #ifdef CFG_UNWIND
 /* Get register values pushed onto the stack by utee_panic() */
-static void get_panic_regs_a32_ta(uint32_t *pushed, struct abort_info *ai)
+static void save_panic_regs_a32_ta(struct thread_specific_data *tsd,
+				  uint32_t *pushed)
 {
-	struct thread_abort_regs *ar = ai->regs;
-
-	ai->abort_type = ABORT_TYPE_TA_PANIC;
-
-	memset(ar, 0, sizeof(*ar));
-	ai->pc = pushed[0];
-	ar->r0 = pushed[1];
-	ar->r1 = pushed[2];
-	ar->r2 = pushed[3];
-	ar->r3 = pushed[4];
-	ar->r4 = pushed[5];
-	ar->r5 = pushed[6];
-	ar->r6 = pushed[7];
-	ar->r7 = pushed[8];
-	ar->r8 = pushed[9];
-	ar->r9 = pushed[10];
-	ar->r10 = pushed[11];
-	ar->r11 = pushed[12];
-	ar->usr_sp = (uint32_t)pushed;
-	ar->usr_lr = pushed[13];
-	ar->spsr = read_spsr();
+	tsd->abort_regs = (struct thread_abort_regs){
+		.elr = pushed[0],
+		.r0 = pushed[1],
+		.r1 = pushed[2],
+		.r2 = pushed[3],
+		.r3 = pushed[4],
+		.r4 = pushed[5],
+		.r5 = pushed[6],
+		.r6 = pushed[7],
+		.r7 = pushed[8],
+		.r8 = pushed[9],
+		.r9 = pushed[10],
+		.r10 = pushed[11],
+		.r11 = pushed[12],
+		.usr_sp = (uint32_t)pushed,
+		.usr_lr = pushed[13],
+		.spsr = read_spsr(),
+	};
 }
 
-static void print_panic_stack(struct thread_svc_regs *regs)
+static void save_panic_stack(struct thread_svc_regs *regs)
 {
-	struct abort_info ai;
-	struct thread_abort_regs ar;
-	uint32_t *pregs = (uint32_t *)regs->r1;
+	struct thread_specific_data *tsd = thread_get_tsd();
 	struct tee_ta_session *s;
 
 	if (tee_ta_get_current_session(&s))
@@ -263,22 +259,22 @@ static void print_panic_stack(struct thread_svc_regs *regs)
 	if (tee_mmu_check_access_rights(to_user_ta_ctx(s->ctx),
 					TEE_MEMORY_ACCESS_READ |
 					TEE_MEMORY_ACCESS_WRITE,
-					(uaddr_t)pregs,
+					(uaddr_t)regs->r1,
 					TA32_CONTEXT_MAX_SIZE)) {
 		TAMSG_RAW("");
 		TAMSG_RAW("Can't unwind invalid user stack 0x%" PRIxUA,
-				(uaddr_t)pregs);
+				(uaddr_t)regs->r1);
 		return;
 	}
 
-	memset(&ai, 0, sizeof(ai));
-	ai.regs = &ar;
-	get_panic_regs_a32_ta(pregs, &ai);
+	tsd->abort_type = ABORT_TYPE_TA_PANIC;
+	tsd->abort_descr = 0;
+	tsd->abort_va = 0;
 
-	abort_print_error(&ai);
+	save_panic_regs_a32_ta(tsd, (uint32_t *)regs->r1);
 }
 #else /* CFG_UNWIND */
-static void print_panic_stack(struct thread_svc_regs *regs __unused)
+static void save_panic_stack(struct thread_svc_regs *regs __unused)
 {
 }
 #endif
@@ -289,7 +285,7 @@ uint32_t tee_svc_sys_return_helper(uint32_t ret, bool panic,
 	if (panic) {
 		TAMSG_RAW("");
 		TAMSG_RAW("TA panicked with code 0x%" PRIx32, panic_code);
-		print_panic_stack(regs);
+		save_panic_stack(regs);
 	}
 
 	regs->r1 = panic;
@@ -302,53 +298,45 @@ uint32_t tee_svc_sys_return_helper(uint32_t ret, bool panic,
 #ifdef ARM64
 #ifdef CFG_UNWIND
 /* Get register values pushed onto the stack by utee_panic() (32-bit TA) */
-static void get_panic_regs_a32_ta(uint32_t *pushed, struct abort_info *ai)
+static void save_panic_regs_a32_ta(struct thread_specific_data *tsd,
+				   uint32_t *pushed)
 {
-	struct thread_abort_regs *ar = ai->regs;
-
-	ai->abort_type = ABORT_TYPE_TA_PANIC;
-
-	memset(ar, 0, sizeof(*ar));
-	ai->pc = pushed[0];
-	ar->x0 = pushed[1];
-	ar->x1 = pushed[2];
-	ar->x2 = pushed[3];
-	ar->x3 = pushed[4];
-	ar->x4 = pushed[5];
-	ar->x5 = pushed[6];
-	ar->x6 = pushed[7];
-	ar->x7 = pushed[8];
-	ar->x8 = pushed[9];
-	ar->x9 = pushed[10];
-	ar->x10 = pushed[11];
-	ar->x11 = pushed[12];
-	ar->x13 = (uint64_t)pushed;
-	ar->x14 = pushed[13];
-	ar->spsr = (SPSR_MODE_RW_32 << SPSR_MODE_RW_SHIFT);
+	tsd->abort_regs = (struct thread_abort_regs){
+		.elr = pushed[0],
+		.x0 = pushed[1],
+		.x1 = pushed[2],
+		.x2 = pushed[3],
+		.x3 = pushed[4],
+		.x4 = pushed[5],
+		.x5 = pushed[6],
+		.x6 = pushed[7],
+		.x7 = pushed[8],
+		.x8 = pushed[9],
+		.x9 = pushed[10],
+		.x10 = pushed[11],
+		.x11 = pushed[12],
+		.x13 = (uint64_t)pushed,
+		.x14 = pushed[13],
+		.spsr = (SPSR_MODE_RW_32 << SPSR_MODE_RW_SHIFT),
+	};
 }
 
 /* Get register values pushed onto the stack by utee_panic() (64-bit TA) */
-static void get_panic_regs_a64_ta(uint64_t *pushed, struct abort_info *ai)
+static void save_panic_regs_a64_ta(struct thread_specific_data *tsd,
+				   uint64_t *pushed)
 {
-	struct thread_abort_regs *ar = ai->regs;
-
-	ai->abort_type = ABORT_TYPE_TA_PANIC;
-
-	memset(ar, 0, sizeof(*ar));
-	ar->x29 = pushed[0];
-	ar->elr = pushed[1];
-	ar->spsr = (SPSR_64_MODE_EL0 << SPSR_64_MODE_EL_SHIFT);
+	tsd->abort_regs = (struct thread_abort_regs){
+		.x29 = pushed[0],
+		.elr = pushed[1],
+		.spsr = (SPSR_64_MODE_EL0 << SPSR_64_MODE_EL_SHIFT),
+	};
 }
 
-static void print_panic_stack(struct thread_svc_regs *regs)
+static void save_panic_stack(struct thread_svc_regs *regs)
 {
-	struct tee_ta_session *s;
-	struct user_ta_ctx *utc;
-	struct abort_info ai;
-	struct thread_abort_regs ar;
-
-	memset(&ai, 0, sizeof(ai));
-	ai.regs = &ar;
+	struct thread_specific_data *tsd = thread_get_tsd();
+	struct tee_ta_session *s = NULL;
+	struct user_ta_ctx *utc = NULL;
 
 	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
 		panic();
@@ -367,15 +355,17 @@ static void print_panic_stack(struct thread_svc_regs *regs)
 		return;
 	}
 
-	if (utc->is_32bit)
-		get_panic_regs_a32_ta((uint32_t *)regs->x1, &ai);
-	else
-		get_panic_regs_a64_ta((uint64_t *)regs->x1, &ai);
+	tsd->abort_type = ABORT_TYPE_TA_PANIC;
+	tsd->abort_descr = 0;
+	tsd->abort_va = 0;
 
-	abort_print_error(&ai);
+	if (utc->is_32bit)
+		save_panic_regs_a32_ta(tsd, (uint32_t *)regs->x1);
+	else
+		save_panic_regs_a64_ta(tsd, (uint64_t *)regs->x1);
 }
 #else /* CFG_UNWIND */
-static void print_panic_stack(struct thread_svc_regs *regs __unused)
+static void save_panic_stack(struct thread_svc_regs *regs __unused)
 {
 }
 #endif /* CFG_UNWIND */
@@ -386,7 +376,7 @@ uint32_t tee_svc_sys_return_helper(uint32_t ret, bool panic,
 	if (panic) {
 		TAMSG_RAW("");
 		TAMSG_RAW("TA panicked with code 0x%" PRIx32, panic_code);
-		print_panic_stack(regs);
+		save_panic_stack(regs);
 	}
 
 	regs->x1 = panic;

core/arch/arm/include/kernel/abort.h
@@ -33,6 +33,9 @@ void abort_handler(uint32_t abort_type, struct thread_abort_regs *regs);
 
 bool abort_is_user_exception(struct abort_info *ai);
 
+/* Called from a normal thread */
+void abort_print_current_ta(void);
+
 #endif /*ASM*/
 #endif /*KERNEL_ABORT_H*/
 

core/arch/arm/include/kernel/thread.h
@@ -63,15 +63,6 @@ struct thread_vector_table {
 };
 extern struct thread_vector_table thread_vector_table;
 
-struct thread_specific_data {
-	TAILQ_HEAD(, tee_ta_session) sess_stack;
-	struct tee_ta_ctx *ctx;
-	struct pgt_cache pgt_cache;
-	void *rpc_fs_payload;
-	struct mobj *rpc_fs_payload_mobj;
-	size_t rpc_fs_payload_size;
-};
-
 struct thread_user_vfp_state {
 	struct vfp_state vfp;
 	bool lazy_saved;
@@ -202,6 +193,22 @@ struct thread_svc_regs {
 	uint64_t pad;
 } __aligned(16);
 #endif /*ARM64*/
+
+struct thread_specific_data {
+	TAILQ_HEAD(, tee_ta_session) sess_stack;
+	struct tee_ta_ctx *ctx;
+	struct pgt_cache pgt_cache;
+	void *rpc_fs_payload;
+	struct mobj *rpc_fs_payload_mobj;
+	size_t rpc_fs_payload_size;
+
+	uint32_t abort_type;
+	uint32_t abort_descr;
+	vaddr_t abort_va;
+	unsigned int abort_core;
+	struct thread_abort_regs abort_regs;
+};
+
 #endif /*ASM*/
 
 #ifndef ASM

core/arch/arm/kernel/abort.c
@@ -154,22 +154,15 @@ static void __print_stack_unwind_arm32(struct abort_info *ai __unused)
 /* Kernel or user mode unwind (64-bit execution state) */
 static void __print_stack_unwind_arm64(struct abort_info *ai)
 {
-	struct unwind_state_arm64 state;
-	bool kernel_stack;
-	uaddr_t stack;
-	size_t stack_size;
+	struct unwind_state_arm64 state = { };
+	bool kernel_stack = false;
+	uaddr_t stack = 0;
+	size_t stack_size = 0;
 
 	if (abort_is_user_exception(ai)) {
-		struct tee_ta_session *s;
-		struct user_ta_ctx *utc;
-
-		if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
-			panic();
-
-		utc = to_user_ta_ctx(s->ctx);
 		/* User stack */
-		stack = utc->stack_addr;
-		stack_size = utc->mobj_stack->size;
+		stack = 0;
+		stack_size = 0;
 		kernel_stack = false;
 	} else {
 		/* Kernel stack */
@@ -178,7 +171,6 @@ static void __print_stack_unwind_arm64(struct abort_info *ai)
 		kernel_stack = true;
 	}
 
-	memset(&state, 0, sizeof(state));
 	state.pc = ai->regs->elr;
 	state.fp = ai->regs->x29;
 
@@ -234,19 +226,28 @@ static __maybe_unused void
 __print_abort_info(struct abort_info *ai __maybe_unused,
 		   const char *ctx __maybe_unused)
 {
+	__maybe_unused size_t core_pos = 0;
 #ifdef ARM32
 	uint32_t mode = ai->regs->spsr & CPSR_MODE_MASK;
-	__maybe_unused uint32_t sp;
-	__maybe_unused uint32_t lr;
+	__maybe_unused uint32_t sp = 0;
+	__maybe_unused uint32_t lr = 0;
 
 	if (mode == CPSR_MODE_USR || mode == CPSR_MODE_SYS) {
 		sp = ai->regs->usr_sp;
 		lr = ai->regs->usr_lr;
+		core_pos = thread_get_tsd()->abort_core;
 	} else {
 		sp = read_mode_sp(mode);
 		lr = read_mode_lr(mode);
+		core_pos = get_core_pos();
 	}
 #endif /*ARM32*/
+#ifdef ARM64
+	if (abort_is_user_exception(ai))
+		core_pos = thread_get_tsd()->abort_core;
+	else
+		core_pos = get_core_pos();
+#endif /*ARM64*/
 
 	EMSG_RAW("");
 	EMSG_RAW("%s %s-abort at address 0x%" PRIxVA "%s",
@@ -257,7 +258,7 @@ __print_abort_info(struct abort_info *ai __maybe_unused,
 		 ai->fault_descr, read_ttbr0(), read_ttbr1(),
 		 read_contextidr());
 	EMSG_RAW(" cpu #%zu          cpsr 0x%08x",
-		 get_core_pos(), ai->regs->spsr);
+		 core_pos, ai->regs->spsr);
 	EMSG_RAW(" r0 0x%08x      r4 0x%08x    r8 0x%08x   r12 0x%08x",
 		 ai->regs->r0, ai->regs->r4, ai->regs->r8, ai->regs->ip);
 	EMSG_RAW(" r1 0x%08x      r5 0x%08x    r9 0x%08x    sp 0x%08x",
@@ -272,7 +273,7 @@ __print_abort_info(struct abort_info *ai __maybe_unused,
 		 "   cidr 0x%X", ai->fault_descr, read_ttbr0_el1(),
 		 read_ttbr1_el1(), read_contextidr_el1());
 	EMSG_RAW(" cpu #%zu          cpsr 0x%08x",
-		 get_core_pos(), (uint32_t)ai->regs->spsr);
+		 core_pos, (uint32_t)ai->regs->spsr);
 	EMSG_RAW(" x0  %016" PRIx64 " x1  %016" PRIx64,
 		 ai->regs->x0, ai->regs->x1);
 	EMSG_RAW(" x2  %016" PRIx64 " x3  %016" PRIx64,
@@ -309,58 +310,24 @@ __print_abort_info(struct abort_info *ai __maybe_unused,
 #endif /*ARM64*/
 }
 
-#if defined(ARM32)
-static const bool kernel_is32bit = true;
-#elif defined(ARM64)
-static const bool kernel_is32bit;
-#endif
-
 /*
  * Print abort info and (optionally) stack dump to the console
- * @ai user-mode or kernel-mode abort info. If user mode, the current session
- * must be the one of the TA that caused the abort.
+ * @ai kernel-mode abort info.
  * @stack_dump true to show a stack trace
  */
 static void __abort_print(struct abort_info *ai, bool stack_dump)
 {
-	bool is_32bit;
-	bool paged_ta_abort = false;
+	assert(!abort_is_user_exception(ai));
 
-	if (abort_is_user_exception(ai)) {
-		struct tee_ta_session *s;
-		struct user_ta_ctx *utc;
+	__print_abort_info(ai, "Core");
 
-		if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
-			panic();
-
-		utc = to_user_ta_ctx(s->ctx);
-		is_32bit = utc->is_32bit;
-#ifdef CFG_PAGED_USER_TA
-		/*
-		 * It is not safe to unwind paged TAs that received an abort,
-		 * because we currently don't handle page faults that could
-		 * occur when accessing the TA memory (unwind tables for
-		 * instance).
-		 */
-		if (ai->abort_type != ABORT_TYPE_TA_PANIC)
-			paged_ta_abort = true;
-#endif
-		if (ai->abort_type != ABORT_TYPE_TA_PANIC)
-			__print_abort_info(ai, "User TA");
-		tee_ta_dump_current();
-	} else {
-		is_32bit = kernel_is32bit;
-
-		__print_abort_info(ai, "Core");
-	}
-
-	if (!stack_dump || paged_ta_abort)
-		return;
-
-	if (is_32bit)
+	if (stack_dump) {
+#if defined(ARM32)
 		__print_stack_unwind_arm32(ai);
-	else
+#else
 		__print_stack_unwind_arm64(ai);
+#endif
+	}
 }
 
 void abort_print(struct abort_info *ai)
@@ -371,6 +338,43 @@ void abort_print(struct abort_info *ai)
 void abort_print_error(struct abort_info *ai)
 {
 	__abort_print(ai, true);
+}
+
+/* This function must be called from a normal thread */
+void abort_print_current_ta(void)
+{
+	struct thread_specific_data *tsd = thread_get_tsd();
+	struct abort_info ai = { };
+	struct tee_ta_session *s = NULL;
+
+	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
+		panic();
+
+	ai.abort_type = tsd->abort_type;
+	ai.fault_descr = tsd->abort_descr;
+	ai.va = tsd->abort_va;
+	ai.pc = tsd->abort_regs.elr;
+	ai.regs = &tsd->abort_regs;
+
+	if (ai.abort_type != ABORT_TYPE_TA_PANIC)
+		__print_abort_info(&ai, "User TA");
+	tee_ta_dump_current();
+
+	if (to_user_ta_ctx(s->ctx)->is_32bit)
+		__print_stack_unwind_arm32(&ai);
+	else
+		__print_stack_unwind_arm64(&ai);
+}
+
+static void save_abort_info_in_tsd(struct abort_info *ai)
+{
+	struct thread_specific_data *tsd = thread_get_tsd();
+
+	tsd->abort_type = ai->abort_type;
+	tsd->abort_descr = ai->fault_descr;
+	tsd->abort_va = ai->va;
+	tsd->abort_regs = *ai->regs;
+	tsd->abort_core = get_core_pos();
 }
 
 #ifdef ARM32
@@ -582,7 +586,8 @@ static enum fault_type get_fault_type(struct abort_info *ai)
 		break;
 
 	case CORE_MMU_FAULT_DEBUG_EVENT:
-		abort_print(ai);
+		if (!abort_is_user_exception(ai))
+			abort_print(ai);
 		DMSG("[abort] Ignoring debug event!");
 		return FAULT_TYPE_IGNORE;
 
@@ -592,13 +597,15 @@ static enum fault_type get_fault_type(struct abort_info *ai)
 		return FAULT_TYPE_PAGEABLE;
 
 	case CORE_MMU_FAULT_ASYNC_EXTERNAL:
-		abort_print(ai);
+		if (!abort_is_user_exception(ai))
+			abort_print(ai);
 		DMSG("[abort] Ignoring async external abort!");
 		return FAULT_TYPE_IGNORE;
 
 	case CORE_MMU_FAULT_OTHER:
 	default:
-		abort_print(ai);
+		if (!abort_is_user_exception(ai))
+			abort_print(ai);
 		DMSG("[abort] Unhandled fault!");
 		return FAULT_TYPE_IGNORE;
 	}
@@ -616,7 +623,7 @@ void abort_handler(uint32_t abort_type, struct thread_abort_regs *regs)
 		break;
 	case FAULT_TYPE_USER_TA_PANIC:
 		DMSG("[abort] abort in User mode (TA will panic)");
-		abort_print_error(&ai);
+		save_abort_info_in_tsd(&ai);
 		vfp_disable();
 		handle_user_ta_panic(&ai);
 		break;
@@ -635,10 +642,12 @@ void abort_handler(uint32_t abort_type, struct thread_abort_regs *regs)
 		handled = tee_pager_handle_fault(&ai);
 		thread_kernel_restore_vfp();
 		if (!handled) {
-			abort_print_error(&ai);
-			if (!abort_is_user_exception(&ai))
+			if (!abort_is_user_exception(&ai)) {
+				abort_print_error(&ai);
 				panic("unhandled pageable abort");
+			}
 			DMSG("[abort] abort in User mode (TA will panic)");
+			save_abort_info_in_tsd(&ai);
 			vfp_disable();
 			handle_user_ta_panic(&ai);
 		}

core/arch/arm/kernel/user_ta.c
@@ -359,6 +359,7 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 	serr = TEE_ORIGIN_TRUSTED_APP;
 
 	if (utc->ctx.panicked) {
+		abort_print_current_ta();
 		DMSG("tee_user_ta_enter: TA panicked with code 0x%x",
 		     utc->ctx.panic_code);
 		serr = TEE_ORIGIN_TEE;
@@ -486,7 +487,6 @@ static void user_ta_dump_state(struct tee_ta_ctx *ctx)
 	}
 	show_elfs(utc);
 }
-KEEP_PAGER(user_ta_dump_state);
 
 static void release_ta_memory_by_mobj(struct mobj *mobj)
 {

core/arch/arm/mm/tee_pager.c
@@ -1252,7 +1252,8 @@ bool tee_pager_handle_fault(struct abort_info *ai)
 	bool ret;
 
 #ifdef TEE_PAGER_DEBUG_PRINT
-	abort_print(ai);
+	if (!abort_is_user_exception(ai))
+		abort_print(ai);
 #endif
 
 	/*

commit b8e1180b3ed0deb826c0a34da593338afd667114

core: fix core panic when CFG_UNWIND=n
This change fixes a core panic occurrence when a TA panics while core
is built with CFG_TEE_CORE_DEBUG=y and CFG_UNWIND=n.

When a TA panics while CFG_UNWIND=n, the thread specific data holding
abort_type was not loaded prior this change. The abort sequence that
print information to the console dumps now does not attemp to dump
un-relevant CPU register content.

Prior this change, ARM32 code could panic since reading an invalid SPSR
value when printing CPU state to the console, with an error trace like:

E/TC:? 0 TA panicked with code 0xbeef
E/TC:? 0 assertion 'thread_get_exceptions() & THREAD_EXCP_FOREIGN_INTR' failed at core/arch/arm/include/kernel/misc.h:22 <get_core_pos>
E/TC:0 0 Panic at core/kernel/assert.c:28 <_assert_break>


Prior this change ARM64 code printed irrelevant CPU state information
as below:

E/TC:? 0 TA panicked with code 0xbeef
E/TC:? 0
E/TC:? 0 User TA undef-abort at address 0x0
E/TC:? 0  esr 0x00000000  ttbr0 0x200000e173020   ttbr1 0x00000000   cidr 0x0
E/TC:? 0  cpu #0          cpsr 0x00000000
E/TC:? 0  x0  0000000000000000 x1  0000000000000000
(...)
E/TC:? 0  x30 0000000000000000 elr 0000000000000000
E/TC:? 0  sp_el0 0000000000000000

Fixes: c0bc8d0e7d72 ("core: print TA stack dump from thread context")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/tee/arch_svc.c
@@ -276,6 +276,9 @@ static void save_panic_stack(struct thread_svc_regs *regs)
 #else /* CFG_UNWIND */
 static void save_panic_stack(struct thread_svc_regs *regs __unused)
 {
+	struct thread_specific_data *tsd = thread_get_tsd();
+
+	tsd->abort_type = ABORT_TYPE_TA_PANIC;
 }
 #endif
 
@@ -367,6 +370,9 @@ static void save_panic_stack(struct thread_svc_regs *regs)
 #else /* CFG_UNWIND */
 static void save_panic_stack(struct thread_svc_regs *regs __unused)
 {
+	struct thread_specific_data *tsd = thread_get_tsd();
+
+	tsd->abort_type = ABORT_TYPE_TA_PANIC;
 }
 #endif /* CFG_UNWIND */
 