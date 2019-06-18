OP-TEE/optee_os

commit aea0999e2360b6f1c124169fbfeb544e356ebc85

core: explicit return value for sm_platform_handler()
Changes sm_platform_handler() to explicitly return whether the SMC
was handled or if it shall be relayed to another layer. Prior this
change the function returned false when the SMC had been handled and
true otherwise which are not obvious return values.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-ti/sm_platform_handler_a15.c
@@ -33,7 +33,7 @@
 bool sm_platform_handler(struct sm_ctx *ctx)
 {
 	if (ctx->nsec.r12 == 0x200)
-		return true;
+		return SM_HANDLER_PENDING_SMC;
 
 	switch (ctx->nsec.r12) {
 	case API_MONITOR_ACTLR_SETREGISTER_INDEX:
@@ -51,5 +51,5 @@ bool sm_platform_handler(struct sm_ctx *ctx)
 		break;
 	}
 
-	return false;
+	return SM_HANDLER_SMC_HANDLED;
 }

core/arch/arm/plat-ti/sm_platform_handler_a9.c
@@ -42,7 +42,7 @@ uint32_t suspend_regs[16];
 bool sm_platform_handler(struct sm_ctx *ctx)
 {
 	if (ctx->nsec.r12 == 0x200)
-		return true;
+		return SM_HANDLER_PENDING_SMC;
 
 	switch (ctx->nsec.r12) {
 	case 0x0:
@@ -93,5 +93,5 @@ bool sm_platform_handler(struct sm_ctx *ctx)
 		break;
 	}
 
-	return false;
+	return SM_HANDLER_SMC_HANDLED;
 }

core/arch/arm/include/sm/sm.h
@@ -107,17 +107,23 @@ void *sm_get_sp(void);
  */
 void sm_init(vaddr_t stack_pointer);
 
+enum sm_handler_ret {
+	SM_HANDLER_SMC_HANDLED = 0,
+	SM_HANDLER_PENDING_SMC,
+};
+
 #ifndef CFG_SM_PLATFORM_HANDLER
-/*
- * Returns false if we handled the monitor service and should now return
- * back to the non-secure state
- */
-static inline bool sm_platform_handler(__unused struct sm_ctx *ctx)
+/* No platform handler in secure monitor, SMC shall reach OP-TEE core */
+static inline enum sm_handler_ret sm_platform_handler(__unused struct sm_ctx *c)
 {
-	return true;
+	return SM_HANDLER_PENDING_SMC;
 }
 #else
-bool sm_platform_handler(struct sm_ctx *ctx);
+/*
+ * Returns whether SMC was handled from platform handler in secure monitor
+ * or if it shall reach OP-TEE core .
+ */
+enum sm_handler_ret sm_platform_handler(struct sm_ctx *ctx);
 #endif
 
 void sm_save_unbanked_regs(struct sm_unbanked_regs *regs);

core/arch/arm/plat-imx/sm_platform_handler.c
@@ -10,7 +10,7 @@
 #include "imx_sip.h"
 #include "imx_pl310.h"
 
-static bool imx_sip_handler(struct thread_smc_args *smc_args)
+static enum sm_handler_ret imx_sip_handler(struct thread_smc_args *smc_args)
 {
 	uint16_t sip_func = OPTEE_SMC_FUNC_NUM(smc_args->a0);
 
@@ -38,10 +38,10 @@ static bool imx_sip_handler(struct thread_smc_args *smc_args)
 		break;
 	}
 
-	return false;
+	return SM_HANDLER_SMC_HANDLED;
 }
 
-bool sm_platform_handler(struct sm_ctx *ctx)
+enum sm_handler_ret sm_platform_handler(struct sm_ctx *ctx)
 {
 	uint32_t *nsec_r0 = (uint32_t *)(&ctx->nsec.r0);
 	uint16_t smc_owner = OPTEE_SMC_OWNER_NUM(*nsec_r0);
@@ -50,6 +50,6 @@ bool sm_platform_handler(struct sm_ctx *ctx)
 	case OPTEE_SMC_OWNER_SIP:
 		return imx_sip_handler((struct thread_smc_args *)nsec_r0);
 	default:
-		return true;
+		return SM_HANDLER_PENDING_SMC;
 	}
 }

core/arch/arm/sm/sm.c
@@ -13,6 +13,11 @@
 #include <string.h>
 #include "sm_private.h"
 
+/*
+ * Return false/0 if secure monitor shall retrun to non-secure and
+ * true/1 if secure monitor shall enter OP-TEE core to proceed
+ * invocation.
+ */
 bool sm_from_nsec(struct sm_ctx *ctx)
 {
 	uint32_t *nsec_r0 = (uint32_t *)(&ctx->nsec.r0);
@@ -26,7 +31,7 @@ bool sm_from_nsec(struct sm_ctx *ctx)
 	COMPILE_TIME_ASSERT(!(offsetof(struct sm_ctx, nsec.r0) % 8));
 	COMPILE_TIME_ASSERT(!(sizeof(struct sm_ctx) % 8));
 
-	if (!sm_platform_handler(ctx))
+	if (sm_platform_handler(ctx) == SM_HANDLER_SMC_HANDLED)
 		return false;
 
 #ifdef CFG_PSCI_ARM32

commit 430640a69b59d1f50635e7e3a4c798350404d871

plat-ti: fix build errors
Fixes build error with PLATFORM=ti-dra7xx and PLATFORM=ti-am43xx:

 error: conflicting types for ‘sm_platform_handler’
  bool sm_platform_handler(struct sm_ctx *ctx)
       ^~~~~~~~~~~~~~~~~~~

Fixes: aea0999e2360 ("core: explicit return value for sm_platform_handler()")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

core/arch/arm/plat-ti/sm_platform_handler_a15.c
@@ -30,7 +30,7 @@
 #include <sm/sm.h>
 #include "api_monitor_index_a15.h"
 
-bool sm_platform_handler(struct sm_ctx *ctx)
+enum sm_handler_ret sm_platform_handler(struct sm_ctx *ctx)
 {
 	if (ctx->nsec.r12 == 0x200)
 		return SM_HANDLER_PENDING_SMC;

core/arch/arm/plat-ti/sm_platform_handler_a9.c
@@ -39,7 +39,7 @@
 
 uint32_t suspend_regs[16];
 
-bool sm_platform_handler(struct sm_ctx *ctx)
+enum sm_handler_ret sm_platform_handler(struct sm_ctx *ctx)
 {
 	if (ctx->nsec.r12 == 0x200)
 		return SM_HANDLER_PENDING_SMC;