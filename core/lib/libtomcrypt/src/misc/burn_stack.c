4c36c9a83ace3343f7ed4df61df2e8703718caa0
Sumit Garg <sumit.garg@linaro.org>
2019-06-27 19:19:59 +0530

    core: crypto: libtomcrypt: fix LTC_CLEAN_STACK bug
    LTC_CLEAN_STACK uses burn_stack() API that uses a recursive call which
    leads to approx. double the size of stack cleaned than expected on ARM64,
    because it consumes stack space in 32-byte chunks and assumes only buf
    is pushed onto the stack while ignoring any other data such as lr, fp,
    etc.. This causes stack overflow corrupting canaries in case we perform
    a SHA512 hash operation which utilizes maximum stack as compared to other
    libtomcrypt APIs. So get rid of this recursive call via using variable
    length array to clean stack.
    
    Also, convert zeromem() API as a wrapper to call memzero_explicit().
    
    Fixes: ad565116a0d7 ("core: crypto: libtomcrypt: enable LTC_CLEAN_STACK")
    Suggested-by: Daniel Thompson <daniel.thompson@linaro.org>
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    [jf: pick 03121b2ca4]
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/lib/libtomcrypt/src/misc/burn_stack.c
+++ core/lib/libtomcrypt/src/misc/burn_stack.c
@@ -20,11 +20,8 @@
 */
 void burn_stack(unsigned long len)
 {
-   unsigned char buf[32];
+   unsigned char buf[len];
    zeromem(buf, sizeof(buf));
-   if (len > (unsigned long)sizeof(buf)) {
-      burn_stack(len - sizeof(buf));
-   }
 }
 
 
