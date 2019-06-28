OP-TEE/optee_os

commit bae0f170e3217a9322f4c36879a65bc12f50e96a

core: file: refactor interface to be thread safe
Adds file_lock() and file_unlock() to change the lock state of a file.

file_new() is removed, the only way to create a file is with
file_get_by_tag() which will return an empty newly allocated file if
none can be found.

file_add_slice() is added to add slices to a file, one by one.

With this can multiple threads try to load a TA at once, the first one
taking the lock will add all the slices to the file.

The code in user_ta.c and elf_load.c using the <file.h> interface can
be optimized to allow more operations in parallel.

Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/file.c
@@ -13,6 +13,11 @@
 #include <types_ext.h>
 #include <util.h>
 
+struct file_slice_elem {
+	struct file_slice slice;
+	SLIST_ENTRY(file_slice_elem) link;
+};
+
 /*
  * struct file - file resources
  * @tag:	Tag or hash uniquely identifying a file
@@ -31,8 +36,8 @@ struct file {
 	unsigned int taglen;
 	struct refcount refc;
 	TAILQ_ENTRY(file) link;
-	unsigned int num_slices;
-	struct file_slice slices[];
+	struct mutex mu;
+	SLIST_HEAD(, file_slice_elem) slice_head;
 };
 
 static struct mutex file_mu = MUTEX_INITIALIZER;
@@ -60,61 +65,45 @@ static struct file *file_find_tag_unlocked(const uint8_t *tag,
 
 static void file_free(struct file *f)
 {
-	size_t n = 0;
+	mutex_destroy(&f->mu);
 
-	for (n = 0; n < f->num_slices; n++)
-		fobj_put(f->slices[n].fobj);
+	while (!SLIST_EMPTY(&f->slice_head)) {
+		struct file_slice_elem *fse = SLIST_FIRST(&f->slice_head);
+
+		SLIST_REMOVE_HEAD(&f->slice_head, link);
+		fobj_put(fse->slice.fobj);
+		free(fse);
+	}
 
 	free(f);
 }
 
-struct file *file_new(uint8_t *tag, unsigned int taglen,
-		      struct file_slice *slices, unsigned int num_slices)
+TEE_Result file_add_slice(struct file *f, struct fobj *fobj,
+			  unsigned int page_offset)
 {
+	struct file_slice_elem *fse = NULL;
 	unsigned int s = 0;
-	unsigned int n = 0;
-	struct file *f = NULL;
-	bool did_insert = false;
 
-	if (taglen > sizeof(f->tag))
-		return NULL;
-	if (MUL_OVERFLOW(num_slices, sizeof(*slices), &s))
-		return NULL;
-	if (ADD_OVERFLOW(s, sizeof(*f), &s))
-		return NULL;
-	f = calloc(1, s);
-	if (!f)
-		return NULL;
+	/* Check for conflicts */
+	if (file_find_slice(f, page_offset))
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	memcpy(f->tag, tag, taglen);
-	f->taglen = taglen;
-	refcount_set(&f->refc, 1);
-	for (n = 0; n < num_slices; n++) {
-		if (file_find_slice(f, slices[n].page_offset))
-			goto err;
+	fse = calloc(1, sizeof(*fse));
+	if (!fse)
+		return TEE_ERROR_OUT_OF_MEMORY;
 
-		f->slices[n].fobj = fobj_get(slices[n].fobj);
-		f->num_slices = n + 1;
-		if (!f->slices[n].fobj ||
-		    ADD_OVERFLOW(slices[n].page_offset,
-				 slices[n].fobj->num_pages, &s))
-			goto err;
-		f->slices[n].page_offset = slices[n].page_offset;
+	fse->slice.fobj = fobj_get(fobj);
+	if (!fse->slice.fobj ||
+	    ADD_OVERFLOW(page_offset, fse->slice.fobj->num_pages, &s)) {
+		fobj_put(fse->slice.fobj);
+		free(fse);
+		return TEE_ERROR_BAD_PARAMETERS;
 	}
 
-	mutex_lock(&file_mu);
-	if (!file_find_tag_unlocked(tag, taglen)) {
-		TAILQ_INSERT_TAIL(&file_head, f, link);
-		did_insert = true;
-	}
-	mutex_unlock(&file_mu);
+	fse->slice.page_offset = page_offset;
+	SLIST_INSERT_HEAD(&f->slice_head, fse, link);
 
-	if (did_insert)
-		return f;
-err:
-	file_free(f);
-
-	return NULL;
+	return TEE_SUCCESS;
 }
 
 struct file *file_get(struct file *f)
@@ -125,12 +114,28 @@ struct file *file_get(struct file *f)
 	return f;
 }
 
-struct file *file_get_by_tag(uint8_t *tag, unsigned int taglen)
+struct file *file_get_by_tag(const uint8_t *tag, unsigned int taglen)
 {
 	struct file *f = NULL;
 
+	if (taglen > sizeof(f->tag))
+		return NULL;
+
 	mutex_lock(&file_mu);
 	f = file_get(file_find_tag_unlocked(tag, taglen));
+	if (f)
+		goto out;
+	f = calloc(1, sizeof(*f));
+	if (!f)
+		goto out;
+	memcpy(f->tag, tag, taglen);
+	f->taglen = taglen;
+	refcount_set(&f->refc, 1);
+	mutex_init(&f->mu);
+	SLIST_INIT(&f->slice_head);
+	TAILQ_INSERT_TAIL(&file_head, f, link);
+
+out:
 	mutex_unlock(&file_mu);
 
 	return f;
@@ -138,31 +143,24 @@ struct file *file_get_by_tag(uint8_t *tag, unsigned int taglen)
 
 void file_put(struct file *f)
 {
-	bool did_remove = false;
-
-	if (!f)
-		return;
-
-	mutex_lock(&file_mu);
-	if (refcount_dec(&f->refc)) {
+	if (f && refcount_dec(&f->refc)) {
+		mutex_lock(&file_mu);
 		TAILQ_REMOVE(&file_head, f, link);
-		did_remove = true;
-	}
-	mutex_unlock(&file_mu);
+		mutex_unlock(&file_mu);
 
-	if (did_remove)
 		file_free(f);
+	}
+
 }
 
 struct file_slice *file_find_slice(struct file *f, unsigned int page_offset)
 {
-	size_t n = 0;
+	struct file_slice_elem *fse = NULL;
 
-	if (!f)
-		return NULL;
+	assert(f->mu.state);
 
-	for (n = 0; n < f->num_slices; n++) {
-		struct file_slice *fs = f->slices + n;
+	SLIST_FOREACH(fse, &f->slice_head, link) {
+		struct file_slice *fs = &fse->slice;
 
 		if (page_offset >= fs->page_offset &&
 		    page_offset < fs->page_offset + fs->fobj->num_pages)
@@ -170,4 +168,19 @@ struct file_slice *file_find_slice(struct file *f, unsigned int page_offset)
 	}
 
 	return NULL;
+}
+
+void file_lock(struct file *f)
+{
+	mutex_lock(&f->mu);
+}
+
+bool file_trylock(struct file *f)
+{
+	return mutex_trylock(&f->mu);
+}
+
+void file_unlock(struct file *f)
+{
+	mutex_unlock(&f->mu);
 }

core/arch/arm/kernel/elf_load.c
@@ -95,8 +95,6 @@ TEE_Result elf_load_init(const struct user_ta_store_ops *ta_store,
 			 struct elf_load_state **ret_state)
 {
 	struct elf_load_state *state;
-	uint8_t tag[FILE_TAG_SIZE] = { 0 };
-	unsigned int tag_size = sizeof(tag);
 	TEE_Result res;
 
 	state = calloc(1, sizeof(*state));
@@ -112,11 +110,6 @@ TEE_Result elf_load_init(const struct user_ta_store_ops *ta_store,
 	if (res)
 		goto err;
 
-	res = ta_store->get_tag(ta_handle, tag, &tag_size);
-	if (res)
-		goto err;
-	state->file = file_get_by_tag(tag, tag_size);
-
 	*ret_state = state;
 	return TEE_SUCCESS;
 err:
@@ -124,9 +117,21 @@ err:
 	return res;
 }
 
-struct file *elf_load_get_file(struct elf_load_state *state)
+TEE_Result elf_load_get_file(struct elf_load_state *state, struct file **file)
 {
-	return file_get(state->file);
+	TEE_Result res = TEE_SUCCESS;
+	uint8_t tag[FILE_TAG_SIZE] = { 0 };
+	unsigned int tag_size = sizeof(tag);
+
+	res = state->ta_store->get_tag(state->ta_handle, tag, &tag_size);
+	if (res)
+		return res;
+	state->file = file_get_by_tag(tag, tag_size);
+	if (!state->file)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	*file = file_get(state->file);
+	return TEE_SUCCESS;
 }
 
 static TEE_Result e32_load_ehdr(struct elf_load_state *state, Elf32_Ehdr *ehdr,
@@ -615,8 +620,9 @@ TEE_Result elf_load_body(struct elf_load_state *state, vaddr_t vabase)
 			e_p_hdr_sz = 0;
 		}
 
-		fs = file_find_slice(state->file,
-				     phdr.p_vaddr / SMALL_PAGE_SIZE);
+		if (state->file)
+			fs = file_find_slice(state->file,
+					     phdr.p_vaddr / SMALL_PAGE_SIZE);
 		if (fs) {
 			/*
 			 * If we've found a fobj in state->file it has to

core/arch/arm/kernel/elf_load.h
@@ -10,6 +10,7 @@
 #include <types_ext.h>
 #include <kernel/user_ta_store.h>
 
+struct file;
 struct elf_load_state;
 struct user_ta_elf_head;
 
@@ -20,7 +21,7 @@ TEE_Result elf_load_init(const struct user_ta_store_ops *ta_store,
 						   const char *, uintptr_t *),
 			 struct elf_load_state **state);
 
-struct file *elf_load_get_file(struct elf_load_state *state);
+TEE_Result elf_load_get_file(struct elf_load_state *state, struct file **file);
 
 TEE_Result elf_load_head(struct elf_load_state *state, size_t head_size,
 			void **head, size_t *vasize, bool *is_32bit,

core/arch/arm/kernel/user_ta.c
@@ -812,61 +812,43 @@ static TEE_Result add_deps(struct user_ta_ctx *utc __unused,
 #endif
 
 static TEE_Result register_ro_slices(struct user_ta_ctx *utc,
-				     struct file **file,
-				     const struct user_ta_store_ops *ta_store,
-				     struct user_ta_store_handle *handle,
+				     struct elf_load_state *state,
 				     struct load_seg *segs, size_t num_segs)
 {
 	TEE_Result res = TEE_SUCCESS;
-	struct file *f = NULL;
-	struct file_slice *fs = NULL;
-	size_t num_slices = 0;
+	unsigned int page_offset = 0;
+	struct fobj *fobj = NULL;
 	size_t n = 0;
-	uint8_t tag[FILE_TAG_SIZE] = { 0 };
-	unsigned int tag_len = sizeof(tag);
-
-	assert(!*file);
-
-	res = ta_store->get_tag(handle, tag, &tag_len);
-	if (res)
-		return res;
 
 	for (n = 0; n < num_segs; n++) {
-		if (!(segs[n].flags & PF_W)) {
-			res = vm_set_prot(utc, segs[n].va, segs[n].size,
-					  elf_flags_to_mattr(segs[n].flags));
-			if (res)
-				return res;
-			num_slices++;
-		}
+		if (segs[n].flags & PF_W)
+			continue;
+
+		res = vm_set_prot(utc, segs[n].va, segs[n].size,
+				  elf_flags_to_mattr(segs[n].flags));
+		if (res)
+			return res;
+
+		if (!state->file)
+			continue;
+
+		page_offset = (segs[n].va - segs[0].va) / SMALL_PAGE_SIZE;
+		if (file_find_slice(state->file, page_offset))
+			continue;
+
+		fobj = mobj_get_fobj(segs[n].mobj);
+		res = file_add_slice(state->file, fobj, page_offset);
+
+		/*
+		 * The fobjs is guaranteed to exist now, and
+		 * file_add_slice() has called fobj_get() on the
+		 * supplied fobj.
+		 */
+		fobj_put(fobj);
+		if (res)
+			return res;
 	}
 
-	fs = calloc(num_slices, sizeof(*fs));
-	if (!fs)
-		return TEE_ERROR_OUT_OF_MEMORY;
-
-	num_slices = 0;
-	for (n = 0; n < num_segs; n++) {
-		if (!(segs[n].flags & PF_W)) {
-			fs[num_slices].fobj = mobj_get_fobj(segs[n].mobj);
-			fs[num_slices].page_offset = (segs[n].va - segs[0].va) /
-						     SMALL_PAGE_SIZE;
-			/*
-			 * The fobjs is guaranteed to exist now, and
-			 * file_new() will call fobj_get() on all supplied
-			 * fobjs later.
-			 */
-			fobj_put(fs[num_slices].fobj);
-			num_slices++;
-		}
-	}
-
-	f = file_new(tag, tag_len, fs, num_slices);
-	free(fs);
-	if (!f)
-		return TEE_ERROR_OUT_OF_MEMORY;
-
-	*file = f;
 	return TEE_SUCCESS;
 }
 
@@ -963,7 +945,10 @@ static TEE_Result load_elf_from_store(const TEE_UUID *uuid,
 		DMSG("Using deprecated TA entry via ta_head");
 		utc->entry_func = ta_head->depr_entry;
 	} else {
-		file = elf_load_get_file(elf_state);
+		res = elf_load_get_file(elf_state, &file);
+		if (res)
+			goto out;
+		file_lock(file);
 	}
 
 	if (elf == exe) {
@@ -1025,11 +1010,11 @@ static TEE_Result load_elf_from_store(const TEE_UUID *uuid,
 					  &segs[n].mobj);
 			if (res)
 				goto out;
-			/*
-			 * Note that segs[n].mobj can still be NULL if
-			 * corresponding fobj isn't found.
-			 */
 		}
+		/*
+		 * Note that segs[n].mobj can still be NULL if
+		 * corresponding fobj isn't found.
+		 */
 		if (!segs[n].mobj) {
 			segs[n].mobj = alloc_ta_mem(segs[n].size);
 			if (!segs[n].mobj) {
@@ -1063,21 +1048,20 @@ static TEE_Result load_elf_from_store(const TEE_UUID *uuid,
 	 * Legacy TAs can't share read-only memory due to the way
 	 * relocation is updated.
 	 */
-	if (!file && ta_head->depr_entry == UINT64_MAX) {
-		res = register_ro_slices(utc, &file, ta_store, handle,
-					 segs, num_segs);
-		if (res)
-			goto out;
-	}
+	res = register_ro_slices(utc, elf_state, segs, num_segs);
+	if (res)
+		goto out;
 
 	/* Find any external dependency (dynamically linked libraries) */
 	res = add_deps(utc, elf_state, elf->load_addr);
 out:
+	if (file)
+		file_unlock(file);
 	if (res) {
 		file_put(file);
 		free_segs(segs, num_segs);
 	} else {
-		elf->file = file;
+		elf->file = elf_state->file;
 		elf->segs = segs;
 		elf->num_segs = num_segs;
 	}

core/include/mm/file.h
@@ -6,6 +6,7 @@
 #ifndef __MM_FILE_H
 #define __MM_FILE_H
 
+#include <tee_api_types.h>
 #include <types_ext.h>
 #include <utee_defines.h>
 
@@ -26,17 +27,43 @@ struct file_slice {
 struct file;
 
 /*
- * file_new() - allocate a new struct file
- * @tag:	Tag of the file
- * @taglen:	Length of @tag
- * @slices:	An array of file slices
- * @num_slices:	Number of elements in the @slices array
+ * file_lock() - Lock the file
+ * @f:		File pointer
  *
- * Returns a newly allocated file with the reference counters of the fobjs
- * in all the slices increased on success. Returns NULL on failure.
+ * Waits until the file can be locked and with the file put in locked state.
  */
-struct file *file_new(uint8_t *tag, unsigned int taglen,
-		      struct file_slice *slices, unsigned int num_slices);
+void file_lock(struct file *f);
+
+/*
+ * file_lock() - Try to lock the file without blocking
+ * @f:		File pointer
+ *
+ * Returns false if file cannot be locked without blocking.
+ * Returns true if the file has been put in locked state.
+ */
+bool file_trylock(struct file *f);
+
+/*
+ * file_unlock() - Unlock the file
+ * @f:		File pointer
+ *
+ * File must be in locked state. Releases the previous lock and returns.
+ */
+void file_unlock(struct file *f);
+
+/*
+ * file_add_slice() - Add a slice to a file
+ * @f:		 File pointer
+ * @fobj:	 Fobj holding the data of this slice
+ * @page_offset: Offset in pages into the file (@f) where the @fobj is
+ *		 located.
+ *
+ * File must be in locked state.
+ *
+ * Returns TEE_SUCCESS on success or a TEE_ERROR_* code on failure.
+ */
+TEE_Result file_add_slice(struct file *f, struct fobj *fobj,
+			  unsigned int page_offset);
 
 /*
  * file_get() - Increase file reference counter
@@ -51,10 +78,11 @@ struct file *file_get(struct file *f);
  * @tag:	Tag of the file
  * @taglen:	Length of @tag
  *
- * Returns a file with an increased reference counter if found, or NULL if
- * not found.
+ * If a file doesn't exist it's created with the supplied tag.
+ *
+ * Returns a file with an increased reference counter, or NULL on failure.
  */
-struct file *file_get_by_tag(uint8_t *tag, unsigned int taglen);
+struct file *file_get_by_tag(const uint8_t *tag, unsigned int taglen);
 
 /*
  * file_put() - Decrease reference counter of file
@@ -70,6 +98,8 @@ void file_put(struct file *f);
  * file_find_slice() - Find a slice covering the @page_offset
  * @f:		 File pointer
  * @page_offset: Offset that must be covered
+ *
+ * File must be in locked state.
  *
  * If a matching file slice is found it is returned, else NULL is returned.
  */

commit d842648e5404d6c2b2f7647a90a1232facd7ae4b

core: fix file_get_by_tag()
Fixes a race in file_get_by_tag() where a file is just about to be freed
but hasn't been removed from the linked list yet.

Fixes: bae0f170e321 ("core: file: refactor interface to be thread safe")
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/file.c
@@ -122,9 +122,32 @@ struct file *file_get_by_tag(const uint8_t *tag, unsigned int taglen)
 		return NULL;
 
 	mutex_lock(&file_mu);
-	f = file_get(file_find_tag_unlocked(tag, taglen));
-	if (f)
+
+	/*
+	 * If file is found and reference counter can be increased, we're done.
+	 * If file can't be found, it doesn't exist so it has to be added.
+	 * If it's found but reference counter is 0, the situation is
+	 * a bit complicated:
+	 * - file_put() is about to free the file as soon as it can obtain the
+	 *   mutex.
+	 * - Unless there's a mismatch between file_get() and file_put() only
+	 *   one thread calling file_put() is about to free the file.
+	 *
+	 * There's a window of opportunity where file_put() is called
+	 * (without a mutex being held, which is quite OK) while we're
+	 * holding the mutex here and are searching for the file and it's
+	 * found, but just after file_put() has decreased the reference
+	 * counter.
+	 *
+	 * To keep it simple we're adding a new file at the head (so new
+	 * searches finds this file instead of the old being freed) instead
+	 * of complicating file_put() by trying to rescue the file and
+	 * possibly hiding a case of mismatching file_put() and file_get().
+	 */
+	f = file_find_tag_unlocked(tag, taglen);
+	if (f && refcount_inc(&f->refc))
 		goto out;
+
 	f = calloc(1, sizeof(*f));
 	if (!f)
 		goto out;
@@ -133,7 +156,7 @@ struct file *file_get_by_tag(const uint8_t *tag, unsigned int taglen)
 	refcount_set(&f->refc, 1);
 	mutex_init(&f->mu);
 	SLIST_INIT(&f->slice_head);
-	TAILQ_INSERT_TAIL(&file_head, f, link);
+	TAILQ_INSERT_HEAD(&file_head, f, link);
 
 out:
 	mutex_unlock(&file_mu);