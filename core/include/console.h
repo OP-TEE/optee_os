OP-TEE/optee_os

commit 770b2afacf3342b462629198ad26675b9fb41687

core: more flexible console init from DT
Introduce get_console_node_from_dt() that collects the references
to the console configuration in the DTB.

Existing configure_console_from_dt() relies on it when initiating
the console from the early core inits based on registered matching
device tables.

get_console_node_from_dt() allows a driver probed from the driver
initialization sequence (i.e registered from driver_init()) to
get the console DT directive.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Joakim Bech <joakim.bech@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/include/console.h
@@ -7,6 +7,8 @@
 #define CONSOLE_H
 
 #include <compiler.h>
+#include <tee_api_types.h>
+
 
 void console_init(void);
 void console_putc(int ch);
@@ -16,6 +18,32 @@ struct serial_chip;
 void register_serial_console(struct serial_chip *chip);
 
 #ifdef CFG_DT
+/*
+ * Get console info from a reacheable DTB. Check the embedded DTB and fall
+ * back to the external DTB.
+ *
+ * If the DTB does not specify a chosen (or secure-chosen) node, we assume
+ * DTB does not provide specific console directive. Early console may remain.
+ * If the DTB does not specify any console in the chosen (or secure-chosen)
+ * node, we assume there is no console. Early console would be disabled.
+ *
+ * @fdt_out: Output DTB address where console directive is found
+ * @offs_out: Output offset in the DTB where console directive is found
+ * @path_out: Output string configuration of the console from in the DTB
+ * @params_out: Output console parameters found from the DTB
+ *
+ * Return a TEE_Result compliant return value
+ *
+ */
+TEE_Result get_console_node_from_dt(void **fdt_out, int *offs_out,
+				    const char **path_out,
+				    const char **params_out);
+
+/*
+ * Check if the /secure-chosen or /chosen node in the DT contains an
+ * stdout-path value for which we have a compatible driver. If so, switch
+ * the console to this device.
+ */
 void configure_console_from_dt(void);
 #else
 static inline void configure_console_from_dt(void)

core/kernel/console.c
@@ -55,23 +55,18 @@ static int find_chosen_node(void *fdt)
 	return offset;
 }
 
-/*
- * Check if the /secure-chosen node in the DT contains an stdout-path value
- * for which we have a compatible driver. If so, switch the console to
- * this device.
- */
-void configure_console_from_dt(void)
+TEE_Result get_console_node_from_dt(void **fdt_out, int *offs_out,
+				    const char **path_out,
+				    const char **params_out)
 {
-	const struct dt_driver *dt_drv;
-	const struct serial_driver *sdrv;
 	const struct fdt_property *prop;
-	struct serial_chip *dev;
-	char *stdout_data;
 	const char *uart;
 	const char *parms = NULL;
 	void *fdt;
 	int offs;
+	char *stdout_data;
 	char *p;
+	TEE_Result rc = TEE_ERROR_GENERIC;
 
 	/* Probe console from secure DT and fallback to non-secure DT */
 	fdt = get_embedded_dt();
@@ -82,7 +77,7 @@ void configure_console_from_dt(void)
 	}
 	if (offs < 0) {
 		DMSG("No console directive from DTB");
-		return;
+		return TEE_ERROR_ITEM_NOT_FOUND;
 	}
 
 	prop = fdt_get_property(fdt, offs, "stdout-path", NULL);
@@ -93,12 +88,12 @@ void configure_console_from_dt(void)
 		 */
 		IMSG("Switching off console");
 		register_serial_console(NULL);
-		return;
+		return TEE_ERROR_ITEM_NOT_FOUND;
 	}
 
 	stdout_data = strdup(prop->data);
 	if (!stdout_data)
-		return;
+		panic();
 	p = strchr(stdout_data, ':');
 	if (p) {
 		*p = '\0';
@@ -112,19 +107,49 @@ void configure_console_from_dt(void)
 		uart = stdout_data;
 	}
 	offs = fdt_path_offset(fdt, uart);
-	if (offs < 0)
-		goto out;
+	if (offs >= 0) {
+		if (fdt_out)
+			*fdt_out = fdt;
+		if (offs_out)
+			*offs_out = offs;
+		if (params_out)
+			*params_out = parms;
+		if (path_out)
+			*path_out = uart;
+
+		rc = TEE_SUCCESS;
+	}
+
+	free(stdout_data);
+
+	return rc;
+}
+
+void configure_console_from_dt(void)
+{
+	const struct dt_driver *dt_drv;
+	const struct serial_driver *sdrv;
+	struct serial_chip *dev;
+	const char *uart;
+	const char *parms;
+	void *fdt;
+	int offs;
+
+	if (get_console_node_from_dt(&fdt, &offs, &uart, &parms))
+		return;
 
 	dt_drv = dt_find_compatible_driver(fdt, offs);
 	if (!dt_drv)
-		goto out;
+		return;
 
 	sdrv = (const struct serial_driver *)dt_drv->driver;
 	if (!sdrv)
-		goto out;
+		return;
+
 	dev = sdrv->dev_alloc();
 	if (!dev)
-		goto out;
+		return;
+
 	/*
 	 * If the console is the same as the early console, dev_init() might
 	 * clear pending data. Flush to avoid that.
@@ -132,13 +157,11 @@ void configure_console_from_dt(void)
 	console_flush();
 	if (sdrv->dev_init(dev, fdt, offs, parms) < 0) {
 		sdrv->dev_free(dev);
-		goto out;
+		return;
 	}
 
 	IMSG("Switching console to device: %s", uart);
 	register_serial_console(dev);
-out:
-	free(stdout_data);
 }
 
 #endif /* CFG_DT */

commit bb52a807f975a7abe07ad9d66b671549a3a39006

core: console: fix use after free when CFG_DT=y
Commit 770b2afacf33 ("core: more flexible console init from DT") has
split configure_console_from_dt() in two parts, the first one being
moved to a new function: get_console_node_from_dt(). Unfortunately,
this function may return pointers to a freed buffer.

Fix the problem by allocating each output string on the heap and letting
the caller clean on return.

Fixes: 770b2afacf33 ("core: more flexible console init from DT")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Rouven Czerwinski <r.czerwinksi@pengutronix.de>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/include/console.h
@@ -29,15 +29,16 @@ void register_serial_console(struct serial_chip *chip);
  *
  * @fdt_out: Output DTB address where console directive is found
  * @offs_out: Output offset in the DTB where console directive is found
- * @path_out: Output string configuration of the console from in the DTB
- * @params_out: Output console parameters found from the DTB
+ * @path_out: Output string configuration of the console from the DTB.
+ * (*path_out) shall be freed using nex_free().
+ * @params_out: Output console parameters found from the DTB.
+ * (*params_out) shall be freed using nex_free().
  *
  * Return a TEE_Result compliant return value
  *
  */
 TEE_Result get_console_node_from_dt(void *fdt, int *offs_out,
-				    const char **path_out,
-				    const char **params_out);
+				    char **path_out, char **params_out);
 
 /*
  * Check if the /secure-chosen or /chosen node in the DT contains an

core/kernel/console.c
@@ -57,8 +57,7 @@ static int find_chosen_node(void *fdt)
 }
 
 TEE_Result get_console_node_from_dt(void *fdt, int *offs_out,
-				    const char **path_out,
-				    const char **params_out)
+				    char **path_out, char **params_out)
 {
 	const struct fdt_property *prop;
 	const char *uart;
@@ -106,9 +105,9 @@ TEE_Result get_console_node_from_dt(void *fdt, int *offs_out,
 		if (offs_out)
 			*offs_out = offs;
 		if (params_out)
-			*params_out = parms;
+			*params_out = parms ? nex_strdup(parms) : NULL;
 		if (path_out)
-			*path_out = uart;
+			*path_out = uart ? nex_strdup(uart) : NULL;
 
 		rc = TEE_SUCCESS;
 	}
@@ -123,8 +122,8 @@ void configure_console_from_dt(void)
 	const struct dt_driver *dt_drv;
 	const struct serial_driver *sdrv;
 	struct serial_chip *dev;
-	const char *uart;
-	const char *parms;
+	char *uart = NULL;
+	char *parms = NULL;
 	void *fdt;
 	int offs;
 
@@ -134,15 +133,15 @@ void configure_console_from_dt(void)
 
 	dt_drv = dt_find_compatible_driver(fdt, offs);
 	if (!dt_drv)
-		return;
+		goto out;
 
 	sdrv = (const struct serial_driver *)dt_drv->driver;
 	if (!sdrv)
-		return;
+		goto out;
 
 	dev = sdrv->dev_alloc();
 	if (!dev)
-		return;
+		goto out;
 
 	/*
 	 * If the console is the same as the early console, dev_init() might
@@ -151,11 +150,14 @@ void configure_console_from_dt(void)
 	console_flush();
 	if (sdrv->dev_init(dev, fdt, offs, parms) < 0) {
 		sdrv->dev_free(dev);
-		return;
+		goto out;
 	}
 
 	IMSG("Switching console to device: %s", uart);
 	register_serial_console(dev);
+out:
+	nex_free(uart);
+	nex_free(parms);
 }
 
 #endif /* CFG_DT */