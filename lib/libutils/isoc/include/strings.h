31df052c314b170ec130c8c72167c5571d651833
Jerome Forissier <jerome.forissier@linaro.org>
2019-07-05 11:40:53 +0200

    libutils: import bcmp() from newlib
    Recent versions of Clang (later than 8.0.0) may replace calls to
    memcmp() by bcmp(). Therefore, provide an implementation of this
    function to avoid unresolved symbol errors.
    The same issue was fixed in a similar way in the Linux kernel [1].
    
    Link: [1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=5f074f3e192f10c9fade898b9b3b8812e3d83342
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ lib/libutils/isoc/include/strings.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2019, Linaro Limited
+ */
+#ifndef __STRINGS_H
+#define __STRINGS_H
+
+int bcmp(const void *s1, const void *s2, size_t n);
+
+#endif /*__STRINGS_H*/
