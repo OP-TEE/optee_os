d0afa6320e9c46bf7c5913ca9d4c002c6a3bf779
Clement Faure <clement.faure@nxp.com>
2019-07-25 16:59:21 +0200

    core: imx: split registers declaration
    Split imx registers declaration into separate files for imx6, imx7 and
    imx8m.
    Move it to a separate registers folder.
    
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
    Signed-off-by: Alessandro Di Chiara <alessandro.dichiara@nxp.com>
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ core/arch/arm/plat-imx/registers/imx8m.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2017-2019 NXP
+ */
+
+#ifndef __IMX8M_H__
+#define __IMX8M_H__
+
+#define GICD_BASE	0x38800000
+#define GICR_BASE	0x38880000
+#define UART1_BASE	0x30860000
+#define UART2_BASE	0x30890000
+#define UART3_BASE	0x30880000
+#define UART4_BASE	0x30A60000
+#define TZASC_BASE	0x32F80000
+#define CAAM_BASE	0x30900000
+#define ANATOP_BASE	0x30360000
+
+#endif /* __IMX8M_H__ */
