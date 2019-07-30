OP-TEE/optee_os

commit 2616b103629134f4c586c9bbdd681b32db7fcc57

core: add prot arg to tee_pager_add_uta_area()
Adds a prot argument to tee_pager_add_uta_area() to set the initial
protection instead of the previous TEE_MATTR_PRW | TEE_MATTR_URWX;

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/tee_mmu.c
@@ -276,7 +276,7 @@ TEE_Result vm_map(struct user_ta_ctx *utc, vaddr_t *va, size_t len,
 			goto err_rem_reg;
 		}
 
-		res = tee_pager_add_uta_area(utc, reg->va, fobj);
+		res = tee_pager_add_uta_area(utc, reg->va, fobj, prot);
 		fobj_put(fobj);
 		if (res)
 			goto err_rem_reg;

core/arch/arm/include/mm/tee_pager.h
@@ -108,11 +108,11 @@ void tee_pager_add_core_area(vaddr_t base, enum tee_pager_area_type type,
  */
 #ifdef CFG_PAGED_USER_TA
 TEE_Result tee_pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
-				  struct fobj *fobj);
+				  struct fobj *fobj, uint32_t prot);
 #else
 static inline TEE_Result
 tee_pager_add_uta_area(struct user_ta_ctx *utc __unused, vaddr_t base __unused,
-		       struct fobj *fobj __unused)
+		       struct fobj *fobj __unused, uint32_t prot __unused)
 {
 	return TEE_ERROR_NOT_SUPPORTED;
 }

core/arch/arm/mm/mobj.c
@@ -493,7 +493,8 @@ struct mobj *mobj_seccpy_shm_alloc(size_t size)
 
 	m->fobj = fobj_rw_paged_alloc(ROUNDUP(size, SMALL_PAGE_SIZE) /
 				      SMALL_PAGE_SIZE);
-	if (tee_pager_add_uta_area(utc, va, m->fobj))
+	if (tee_pager_add_uta_area(utc, va, m->fobj,
+				   TEE_MATTR_PRW | TEE_MATTR_URW))
 		goto bad;
 
 	m->va = va;

core/arch/arm/mm/tee_pager.c
@@ -678,7 +678,7 @@ static void free_area(struct tee_pager_area *area)
 }
 
 static TEE_Result pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
-				     struct fobj *fobj)
+				     struct fobj *fobj, uint32_t prot)
 {
 	struct tee_pager_area *area;
 	vaddr_t b = base;
@@ -709,7 +709,7 @@ static TEE_Result pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
 		area->type = PAGER_AREA_TYPE_RW;
 		area->base = b;
 		area->size = s2;
-		area->flags = TEE_MATTR_PRW | TEE_MATTR_URWX;
+		area->flags = prot;
 
 		TAILQ_INSERT_TAIL(utc->areas, area, link);
 		TAILQ_INSERT_TAIL(&fobj->areas, area, fobj_link);
@@ -722,7 +722,7 @@ static TEE_Result pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
 }
 
 TEE_Result tee_pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
-			    struct fobj *fobj)
+			    struct fobj *fobj, uint32_t prot)
 {
 	TEE_Result res = TEE_SUCCESS;
 	struct thread_specific_data *tsd = thread_get_tsd();
@@ -734,7 +734,7 @@ TEE_Result tee_pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
 		 * Changes are to an utc that isn't active. Just add the
 		 * areas page tables will be dealt with later.
 		 */
-		return pager_add_uta_area(utc, base, fobj);
+		return pager_add_uta_area(utc, base, fobj, prot);
 	}
 
 	/*
@@ -742,7 +742,7 @@ TEE_Result tee_pager_add_uta_area(struct user_ta_ctx *utc, vaddr_t base,
 	 * are newly added and should be removed in case of failure.
 	 */
 	tee_pager_assign_uta_tables(utc);
-	res = pager_add_uta_area(utc, base, fobj);
+	res = pager_add_uta_area(utc, base, fobj, prot);
 	if (res) {
 		struct tee_pager_area *next_a;
 

commit 81fb54fbeb914d1f6010b7a23e50e2eddff17eb4

core: only pass prot bits to tee_pager_add_uta_area()
Fixes vm_map_pad() to only pass the prot bits (TEE_MATTR_PROT_MASK) to
tee_pager_add_uta_area().

Fixes: 2616b1036291 ("core: add prot arg to tee_pager_add_uta_area()")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/tee_mmu.c
@@ -301,7 +301,8 @@ TEE_Result vm_map_pad(struct user_ta_ctx *utc, vaddr_t *va, size_t len,
 			goto err_rem_reg;
 		}
 
-		res = tee_pager_add_uta_area(utc, reg->va, fobj, prot);
+		res = tee_pager_add_uta_area(utc, reg->va, fobj,
+					     prot & TEE_MATTR_PROT_MASK);
 		fobj_put(fobj);
 		if (res)
 			goto err_rem_reg;