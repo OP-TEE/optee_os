1b2370072606347b92ed2932143a1eb2b2eb6e4f
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2019-08-01 14:59:04 +0200

    plat-imx: register definitions for CAAM
    Register Definitions in the same style as used by the jobring allocation
    code, by extending the structure definitions and inserting padding if
    the registers are undocumented.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- core/arch/arm/plat-imx/drivers/imx_caam.h
+++ core/arch/arm/plat-imx/drivers/imx_caam.h
@@ -34,6 +34,41 @@ struct imx_caam_job_ring {
 #define CCM_CCGR0_CAAM_WRAPPER_ACLK	SHIFT_U32(3, 10)
 #define CCM_CCGR6_EMI_SLOW		SHIFT_U32(3, 10)
 
+/* jr configuration registers */
+struct imx_caam_jr_ctrl {
+	uint32_t	irbar;
+	uint32_t	irbar_ls;
+	uint8_t		padding[4];
+	uint32_t	irsr;
+	uint8_t		padding2[4];
+	uint32_t	irsar;
+	uint8_t		padding3[4];
+	uint32_t	irjar;
+	uint32_t	orbar;
+	uint32_t	orbar_ls;
+	uint8_t		padding4[4];
+	uint32_t	orsr;
+	uint8_t		padding5[4];
+	uint32_t	orjrr;
+	uint8_t		padding6[4];
+	uint32_t	orsfr;
+	uint8_t		padding7[4];
+	uint32_t	jrstar;
+	uint8_t		padding8[4];
+	uint32_t	jrintr;
+	uint32_t	jrcfgr_ms;
+	uint32_t	jrcfgr_ls;
+	uint8_t		padding9[4];
+	uint32_t	irrir;
+	uint8_t		padding10[4];
+	uint32_t	orwir;
+	uint8_t		padding11[4];
+	uint32_t	jrcr;
+	uint8_t		padding12[1688];
+	uint32_t	jraav;
+	uint8_t		padding13[2292];
+} __packed;
+
 /* A basic sub-set of the CAAM */
 struct imx_caam_ctrl {
 	uint32_t			res0;
@@ -41,6 +76,11 @@ struct imx_caam_ctrl {
 	uint32_t			res1;
 	uint32_t			scfgr;
 	struct imx_caam_job_ring	jr[CAAM_NUM_JOB_RINGS];
+	uint8_t				padding[36];
+	uint32_t			debugctl;
+	uint32_t			jrstartr;
+	uint8_t				padding2[4004];
+	struct imx_caam_jr_ctrl		jrcfg[CAAM_NUM_JOB_RINGS];
 } __packed;
 
 #endif /* __IMX_CAAM_H__ */
