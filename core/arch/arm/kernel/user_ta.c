OP-TEE/optee_os

commit 153479acb74e111d651b4a8b3217f8fb1ff94671

core: dump ftrace data with ldelf
Uses ldelf to dump ftrace data from a TA.

Reviewed-by: Sumit Garg <sumit.garg@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey960)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/user_ta.c
@@ -11,7 +11,6 @@
 #include <elf_common.h>
 #include <initcall.h>
 #include <keep.h>
-#include <kernel/ftrace.h>
 #include <kernel/panic.h>
 #include <kernel/tee_misc.h>
 #include <kernel/tee_ta_manager.h>
@@ -28,6 +27,7 @@
 #include <mm/tee_mm.h>
 #include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <optee_rpc_cmd.h>
 #include <printk.h>
 #include <signed_hdr.h>
 #include <stdio.h>
@@ -300,6 +300,9 @@ static TEE_Result init_with_ldelf(struct tee_ta_session *sess,
 	utc->stack_ptr = arg->stack_ptr;
 	utc->ctx.flags = arg->flags;
 	utc->dump_entry_func = arg->dump_entry;
+#ifdef CFG_TA_FTRACE_SUPPORT
+	utc->ftrace_entry_func = arg->ftrace_entry;
+#endif
 
 out:
 	s = tee_ta_pop_current_session();
@@ -500,10 +503,109 @@ static void user_ta_dump_state(struct tee_ta_ctx *ctx)
 	dump_state_no_ldelf_dbg(utc);
 }
 
-static void user_ta_dump_state_buffer(struct tee_ta_ctx *ctx __unused,
-				      char *buf __unused, size_t *sz __unused)
+#ifdef CFG_TA_FTRACE_SUPPORT
+static TEE_Result dump_ftrace(struct user_ta_ctx *utc, void *buf, size_t *blen)
 {
+	uaddr_t usr_stack = utc->ldelf_stack_ptr;
+	TEE_Result res = TEE_SUCCESS;
+	uint32_t panic_code = 0;
+	uint32_t panicked = 0;
+	size_t *arg = NULL;
+
+	if (!utc->ftrace_entry_func)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	usr_stack -= ROUNDUP(sizeof(*arg), STACK_ALIGNMENT);
+	arg = (size_t *)usr_stack;
+
+	res = tee_mmu_check_access_rights(utc, TEE_MEMORY_ACCESS_READ |
+					  TEE_MEMORY_ACCESS_ANY_OWNER,
+					  (uaddr_t)arg, sizeof(*arg));
+	if (res) {
+		EMSG("ldelf stack is inaccessible!");
+		return res;
+	}
+
+	*arg = *blen;
+
+	res = thread_enter_user_mode((vaddr_t)buf, (vaddr_t)arg, 0, 0,
+				     usr_stack, utc->ftrace_entry_func,
+				     is_arm32, &panicked, &panic_code);
+	clear_vfp_state(utc);
+	if (panicked) {
+		utc->ftrace_entry_func = 0;
+		EMSG("ldelf ftrace function panicked");
+		abort_print_current_ta();
+		res = TEE_ERROR_TARGET_DEAD;
+	}
+
+	if (!res) {
+		if (*arg > *blen)
+			res = TEE_ERROR_SHORT_BUFFER;
+		*blen = *arg;
+	}
+
+	return res;
 }
+
+static void user_ta_dump_ftrace(struct tee_ta_ctx *ctx)
+{
+	uint32_t prot = TEE_MATTR_URW | TEE_MATTR_EPHEMERAL;
+	struct user_ta_ctx *utc = to_user_ta_ctx(ctx);
+	struct thread_param params[3] = { };
+	TEE_Result res = TEE_SUCCESS;
+	struct mobj *mobj = NULL;
+	uint8_t *ubuf = NULL;
+	void *buf = NULL;
+	size_t pl_sz = 0;
+	size_t blen = 0;
+	vaddr_t va = 0;
+
+	res = dump_ftrace(utc, NULL, &blen);
+	if (res != TEE_ERROR_SHORT_BUFFER)
+		return;
+
+	pl_sz = ROUNDUP(SMALL_PAGE_SIZE, blen + sizeof(TEE_UUID));
+
+	mobj = thread_rpc_alloc_payload(pl_sz);
+	if (!mobj) {
+		EMSG("Ftrace thread_rpc_alloc_payload failed");
+		return;
+	}
+
+	buf = mobj_get_va(mobj, 0);
+	if (!buf)
+		goto out_free_pl;
+
+	res = vm_map(utc, &va, mobj->size, prot, mobj, 0);
+	if (res)
+		goto out_free_pl;
+
+	ubuf = (uint8_t *)va + mobj_get_phys_offs(mobj, mobj->phys_granule);
+	memcpy(ubuf, &ctx->uuid, sizeof(TEE_UUID));
+	ubuf += sizeof(TEE_UUID);
+
+	res = dump_ftrace(utc, ubuf, &blen);
+	if (res) {
+		EMSG("Ftrace dump failed: %#"PRIx32, res);
+		goto out_unmap_pl;
+	}
+
+	params[0] = THREAD_PARAM_VALUE(INOUT, 0, 0, 0);
+	params[1] = THREAD_PARAM_MEMREF(IN, mobj, 0, sizeof(TEE_UUID));
+	params[2] = THREAD_PARAM_MEMREF(IN, mobj, sizeof(TEE_UUID), blen);
+
+	res = thread_rpc_cmd(OPTEE_RPC_CMD_FTRACE, 3, params);
+	if (res)
+		EMSG("Ftrace thread_rpc_cmd res: %#"PRIx32, res);
+
+out_unmap_pl:
+	res = vm_unmap(utc, va, mobj->size);
+	assert(!res);
+out_free_pl:
+	thread_rpc_free_payload(mobj);
+}
+#endif /*CFG_TA_FTRACE_SUPPORT*/
 
 static void free_utc(struct user_ta_ctx *utc)
 {
@@ -546,7 +648,9 @@ static const struct tee_ta_ops user_ta_ops __rodata_unpaged = {
 	.enter_invoke_cmd = user_ta_enter_invoke_cmd,
 	.enter_close_session = user_ta_enter_close_session,
 	.dump_state = user_ta_dump_state,
-	.dump_state_buffer = user_ta_dump_state_buffer,
+#ifdef CFG_TA_FTRACE_SUPPORT
+	.dump_ftrace = user_ta_dump_ftrace,
+#endif
 	.destroy = user_ta_ctx_destroy,
 	.get_instance_id = user_ta_get_instance_id,
 };

core/arch/arm/include/kernel/ftrace.h
@@ -1,55 +1,0 @@
-/* SPDX-License-Identifier: BSD-2-Clause */
-/*
- * Copyright (C) 2019, Linaro Limited
- */
-
-#ifndef __KERNEL_FTRACE_H
-#define __KERNEL_FTRACE_H
-
-#include <kernel/tee_ta_manager.h>
-#include <user_ta_header.h>
-
-struct elf_load_state;
-
-/*
- * ta_fbuf_init() - Initialize TA ftrace buffer
- * @load_addr:	user TA load address
- * @s:		user TA session
- * @state:	elf state for TA executable
- */
-#ifdef CFG_TA_FTRACE_SUPPORT
-void ta_fbuf_init(vaddr_t load_addr, struct tee_ta_session *s,
-		  struct elf_load_state *state);
-#else
-static inline void ta_fbuf_init(vaddr_t load_addr __unused,
-				struct tee_ta_session *s __unused,
-				struct elf_load_state *state __unused)
-{
-}
-#endif
-
-/*
- * ta_fbuf_dump() - Dump TA ftrace buffer to normal world
- * @s:		user TA session
- */
-#ifdef CFG_TA_FTRACE_SUPPORT
-void ta_fbuf_dump(struct tee_ta_session *s);
-#else
-static inline void ta_fbuf_dump(struct tee_ta_session *s __unused)
-{
-}
-#endif
-
-/*
- * ftrace_ta_map_lr() - Get original lr in case modified by ftrace
- * @lr:		pointer to lr
- */
-#ifdef CFG_TA_FTRACE_SUPPORT
-void ftrace_ta_map_lr(uint64_t *lr);
-#else
-static inline void ftrace_ta_map_lr(uint64_t *lr __unused)
-{
-}
-#endif
-
-#endif /* __KERNEL_FTRACE_H */

core/arch/arm/include/kernel/user_ta.h
@@ -25,6 +25,7 @@ SLIST_HEAD(load_seg_head, load_seg);
  * @entry_func:		Entry address in TA
  * @dump_entry_func:	Entry address in TA for dumping address mappings
  *			and stack trace
+ * @ftrace_entry_func:	Entry address in ldelf for dumping ftrace data
  * @ldelf_stack_ptr:	Stack pointer used for dumping address mappings and
  *			stack trace
  * @is_32bit:		True if 32-bit TA, false if 64-bit TA
@@ -44,6 +45,9 @@ SLIST_HEAD(load_seg_head, load_seg);
 struct user_ta_ctx {
 	uaddr_t entry_func;
 	uaddr_t dump_entry_func;
+#ifdef CFG_TA_FTRACE_SUPPORT
+	uaddr_t ftrace_entry_func;
+#endif
 	uaddr_t ldelf_stack_ptr;
 	bool is_32bit;
 	bool is_initializing;

core/arch/arm/kernel/abort.c
@@ -5,7 +5,6 @@
 
 #include <arm.h>
 #include <kernel/abort.h>
-#include <kernel/ftrace.h>
 #include <kernel/linker.h>
 #include <kernel/misc.h>
 #include <kernel/panic.h>
@@ -298,7 +297,8 @@ void abort_print_current_ta(void)
 
 	s->ctx->ops->dump_state(s->ctx);
 
-	ta_fbuf_dump(s);
+	if (s->ctx->ops->dump_ftrace)
+		s->ctx->ops->dump_ftrace(s->ctx);
 }
 
 static void save_abort_info_in_tsd(struct abort_info *ai)

core/arch/arm/kernel/ftrace.c
@@ -1,162 +1,0 @@
-// SPDX-License-Identifier: BSD-2-Clause
-/*
- * Copyright (C) 2019, Linaro Limited
- */
-
-#include <assert.h>
-#include <kernel/ftrace.h>
-#include <kernel/panic.h>
-#include <kernel/thread.h>
-#include <kernel/tee_ta_manager.h>
-#include <mm/mobj.h>
-#include <mm/tee_mmu.h>
-#include <optee_rpc_cmd.h>
-#include <printk.h>
-#include <string.h>
-#include <trace.h>
-#include "elf_load_dyn.h"
-
-#define MIN_FTRACE_BUF_SIZE	1024
-#define MAX_HEADER_STRLEN	128
-
-#define MAX_STATE_BUF_SIZE	2048
-
-void ta_fbuf_init(vaddr_t load_addr, struct tee_ta_session *s,
-		  struct elf_load_state *state)
-{
-	struct __ftrace_info *finfo = NULL;
-	struct ftrace_buf *fbuf = NULL;
-	struct user_ta_ctx *utc = to_user_ta_ctx(s->ctx);
-	size_t fbuf_size = 0;
-	uintptr_t val = 0;
-	int count = 0;
-	TEE_Result res = TEE_SUCCESS;
-
-	if (state->is_32bit) {
-		DMSG("Current ftrace doesn't support 32 bit TAs");
-		return;
-	}
-
-	res = elf_resolve_symbol(state, "__ftrace_info", &val);
-	if (res != TEE_SUCCESS || !val) {
-		DMSG("ftrace info not found");
-		return;
-	}
-
-	finfo = (struct __ftrace_info *)(val + load_addr);
-
-	if (!tee_mmu_is_vbuf_inside_ta_private(utc, finfo, sizeof(*finfo))) {
-		EMSG("Incorrect struct __ftrace_info addr");
-		return;
-	}
-
-	if (finfo->buf_end > finfo->buf_start)
-		fbuf_size = finfo->buf_end - finfo->buf_start;
-
-	if (!tee_mmu_is_vbuf_inside_ta_private(utc, (void *)finfo->buf_start,
-					       fbuf_size)) {
-		EMSG("Incorrect struct ftrace_buf addr");
-		return;
-	}
-
-	if (fbuf_size >= MIN_FTRACE_BUF_SIZE) {
-		fbuf = (struct ftrace_buf *)finfo->buf_start;
-		fbuf->head_off = sizeof(struct ftrace_buf);
-		count = snprintk((char *)fbuf + fbuf->head_off,
-				 MAX_HEADER_STRLEN,
-				 "Function graph for TA: %pUl @ %lx\n",
-				 (void *)&s->ctx->uuid, load_addr);
-		assert(count < MAX_HEADER_STRLEN);
-
-		fbuf->ret_func_ptr = finfo->ret_ptr;
-		fbuf->ret_idx = 0;
-		fbuf->lr_idx = 0;
-		fbuf->buf_off = fbuf->head_off + count;
-		fbuf->curr_size = 0;
-		fbuf->max_size = fbuf_size - sizeof(struct ftrace_buf) - count;
-
-		s->fbuf = fbuf;
-	} else {
-		DMSG("ftrace buffer too small");
-
-		s->fbuf = NULL;
-	}
-}
-
-void ta_fbuf_dump(struct tee_ta_session *s)
-{
-	struct ftrace_buf *fbuf = NULL;
-	struct mobj *mobj = NULL;
-	char *va = NULL;
-	uint32_t ret = 0;
-	size_t dump_size = 0;
-	size_t state_buf_size = MAX_STATE_BUF_SIZE;
-	struct thread_param params[3] = { };
-
-	if (!s->fbuf)
-		return;
-
-	fbuf = s->fbuf;
-
-	if (!fbuf->curr_size)
-		DMSG("Ftrace buffer empty (no TA file compiled with -pg?)");
-
-	dump_size = fbuf->buf_off - fbuf->head_off + fbuf->curr_size;
-
-	mobj = thread_rpc_alloc_payload(sizeof(TEE_UUID) + state_buf_size +
-					dump_size);
-	if (!mobj) {
-		EMSG("Ftrace thread_rpc_alloc_payload failed");
-		return;
-	}
-
-	va = mobj_get_va(mobj, 0);
-	if (!va)
-		goto exit;
-
-	memcpy(va, &s->ctx->uuid, sizeof(TEE_UUID));
-	s->ctx->ops->dump_state_buffer(s->ctx, va + sizeof(TEE_UUID),
-				       &state_buf_size);
-	memcpy(va + sizeof(TEE_UUID) + state_buf_size,
-	       (char *)fbuf + fbuf->head_off, dump_size);
-
-	params[0] = THREAD_PARAM_VALUE(INOUT, 0, 0, 0);
-	params[1] = THREAD_PARAM_MEMREF(IN, mobj, 0, sizeof(TEE_UUID));
-	params[2] = THREAD_PARAM_MEMREF(IN, mobj, sizeof(TEE_UUID),
-					state_buf_size + dump_size);
-
-	ret = thread_rpc_cmd(OPTEE_RPC_CMD_FTRACE, 3, params);
-	if (ret)
-		EMSG("Ftrace thread_rpc_cmd ret: %08x", ret);
-
-exit:
-	thread_rpc_free_payload(mobj);
-}
-
-void ftrace_ta_map_lr(uint64_t *lr)
-{
-	struct ftrace_buf *fbuf = NULL;
-	struct tee_ta_session *s = NULL;
-
-	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
-		panic();
-
-	if (!s->fbuf)
-		return;
-
-	fbuf = s->fbuf;
-
-	/*
-	 * Function tracer inserts return hook (addr: fbuf->ret_func_ptr)
-	 * via modifying lr values in the stack frames. And during aborts,
-	 * stack trace picks these modified lr values which needs to be
-	 * replaced with original lr value. So here we use the ftrace return
-	 * stack to retrieve original lr value but we need to first check if
-	 * it has actually been modified or not in case TA is profiled
-	 * partially.
-	 */
-	if ((*lr == fbuf->ret_func_ptr) && (fbuf->lr_idx < fbuf->ret_idx)) {
-		fbuf->lr_idx++;
-		*lr = fbuf->ret_stack[fbuf->ret_idx - fbuf->lr_idx];
-	}
-}

core/arch/arm/kernel/sub.mk
@@ -56,8 +56,6 @@ endif
 
 srcs-$(CFG_VIRTUALIZATION) += virtualization.c
 
-srcs-$(CFG_TA_FTRACE_SUPPORT) += ftrace.c
-
 srcs-y += link_dummies.c
 
 asm-defines-y += asm-defines.c

core/arch/arm/kernel/unwind_arm64.c
@@ -30,7 +30,6 @@
  */
 
 #include <arm.h>
-#include <kernel/ftrace.h>
 #include <kernel/thread.h>
 #include <kernel/unwind.h>
 #include <kernel/tee_misc.h>
@@ -67,9 +66,6 @@ bool unwind_stack_arm64(struct unwind_state_arm64 *frame, bool kernel_stack,
 	if (!copy_in_reg(&frame->pc, fp + 8, kernel_stack))
 		return false;
 
-	if (!kernel_stack)
-		ftrace_ta_map_lr(&frame->pc);
-
 	frame->pc -= 4;
 
 	return true;
@@ -82,8 +78,6 @@ void print_stack_arm64(int level, struct unwind_state_arm64 *state,
 {
 	trace_printf_helper_raw(level, true, "Call stack:");
 
-	if (!kernel_stack)
-		ftrace_ta_map_lr(&state->pc);
 	do {
 		trace_printf_helper_raw(level, true, " 0x%016" PRIx64,
 					state->pc);

core/include/kernel/tee_ta_manager.h
@@ -56,8 +56,7 @@ struct tee_ta_ops {
 			struct tee_ta_param *param, TEE_ErrorOrigin *eo);
 	void (*enter_close_session)(struct tee_ta_session *s);
 	void (*dump_state)(struct tee_ta_ctx *ctx);
-	void (*dump_state_buffer)(struct tee_ta_ctx *ctx, char *buf,
-				  size_t *sz);
+	void (*dump_ftrace)(struct tee_ta_ctx *ctx);
 	void (*destroy)(struct tee_ta_ctx *ctx);
 	uint32_t (*get_instance_id)(struct tee_ta_ctx *ctx);
 };
@@ -107,9 +106,6 @@ struct tee_ta_session {
 	bool unlink;		/* True if session is to be unlinked */
 #if defined(CFG_TA_GPROF_SUPPORT)
 	struct sample_buf *sbuf; /* Profiling data (PC sampling) */
-#endif
-#if defined(CFG_TA_FTRACE_SUPPORT)
-	struct ftrace_buf *fbuf; /* ftrace buffer */
 #endif
 };
 

commit 7e91bfe6304de8e080336a6f13895df6e34c6f3c

core: ftrace: fix incorrect usage of ROUNDUP
Fix incorrect usage of ROUNDUP macro during ftrace data dump to normal
world. It leads to random crashes dependent on ftrace buffer size.

Fixes: 153479acb74e ("core: dump ftrace data with ldelf")
Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/user_ta.c
@@ -516,7 +516,7 @@ static void user_ta_dump_ftrace(struct tee_ta_ctx *ctx)
 	if (res != TEE_ERROR_SHORT_BUFFER)
 		return;
 
-	pl_sz = ROUNDUP(SMALL_PAGE_SIZE, blen + sizeof(TEE_UUID));
+	pl_sz = ROUNDUP(blen + sizeof(TEE_UUID), SMALL_PAGE_SIZE);
 
 	mobj = thread_rpc_alloc_payload(pl_sz);
 	if (!mobj) {