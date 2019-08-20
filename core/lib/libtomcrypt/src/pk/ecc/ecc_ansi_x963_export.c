0f0bb223af7696d689b38af27de6aa1398fdb95f
Jerome Forissier <jerome.forissier@linaro.org>
2019-08-20 15:02:08 +0200

    Import LibTomCrypt v1.18.2 branch "develop" (Aug 19, 2019)
    Imports LibTomcrypt (src directory only).
    
     $ cd work
     $ git clone -b develop https://github.com/libtom/libtomcrypt.git
     $ cd libtomcrypt
     $ git describe
     v1.18.2-490-ge01e4c5c
     $ cd .../optee_os/core/lib/libtomcrypt
     $ cp -a ~/work/libtomcrypt/src .
     $ git add src
    
    Follow-up commits will re-introduce the modifications done locally
    in core/lib/libtomcrypt/{src,include} and update the integration code
    to support the imported version.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

---
+++ core/lib/libtomcrypt/src/pk/ecc/ecc_ansi_x963_export.c
@@ -0,0 +1,34 @@
+/* LibTomCrypt, modular cryptographic library -- Tom St Denis
+ *
+ * LibTomCrypt is a library that provides various cryptographic
+ * algorithms in a highly modular and flexible manner.
+ *
+ * The library is free for all purposes without any express
+ * guarantee it works.
+ */
+
+#include "tomcrypt_private.h"
+
+/**
+  @file ecc_ansi_x963_export.c
+  ECC Crypto, Tom St Denis
+*/
+
+#ifdef LTC_MECC
+
+/** ECC X9.63 (Sec. 4.3.6) uncompressed export
+  @param key     Key to export
+  @param out     [out] destination of export
+  @param outlen  [in/out]  Length of destination and final output size
+  Return CRYPT_OK on success
+*/
+int ecc_ansi_x963_export(const ecc_key *key, unsigned char *out, unsigned long *outlen)
+{
+   return ecc_get_key(out, outlen, PK_PUBLIC, key);
+}
+
+#endif
+
+/* ref:         $Format:%D$ */
+/* git commit:  $Format:%H$ */
+/* commit time: $Format:%ai$ */
