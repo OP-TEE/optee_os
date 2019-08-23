15c897fbe000d26fd5e5c26b9807ed0c84601844
Jerome Forissier <jerome.forissier@linaro.org>
2019-08-23 14:02:53 +0200

    core: ltc: fix 'switch case misses default'
    Add default: statements where missing.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/pk/asn1/der/choice/der_decode_choice.c
+++ core/lib/libtomcrypt/src/pk/asn1/der/choice/der_decode_choice.c
@@ -211,6 +211,7 @@ int der_decode_choice(const unsigned char *in,   unsigned long *inlen,
 
            case LTC_ASN1_CHOICE:
            case LTC_ASN1_EOL:
+           default:
                return CRYPT_INVALID_ARG;
        }
    }
