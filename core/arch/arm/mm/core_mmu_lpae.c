OP-TEE/optee_os

commit 4c4ae21077159402070eaa4b51c96f9b5f23dda5

mmu: replace _prepare_small_page_mapping with _entry_to_finer_grained
core_mmu_prepare_small_page_mapping() just prepares table for the next
level if there was no mappings already.
core_mmu_entry_to_finer_grained() will do the same even if there is are
something mapped there.

Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/mm/core_mmu_lpae.c
@@ -702,37 +702,44 @@ out:
 	return ret;
 }
 
-bool core_mmu_prepare_small_page_mapping(struct core_mmu_table_info *tbl_info,
-					 unsigned int idx, bool __unused secure)
+bool core_mmu_entry_to_finer_grained(struct core_mmu_table_info *tbl_info,
+				     unsigned int idx, bool secure __unused)
 {
 	uint64_t *new_table;
 	uint64_t *entry;
+	int i;
+	paddr_t pa;
+	uint64_t attr;
+	paddr_t block_size_on_next_lvl = L1_XLAT_ADDRESS_SHIFT -
+		tbl_info->level * XLAT_TABLE_ENTRIES_SHIFT;
 
-	if (tbl_info->level >= 3)
-		return false;
-
-	if (next_xlat >= MAX_XLAT_TABLES)
-		return false;
-
-	if (tbl_info->level == 1 && idx >= NUM_L1_ENTRIES)
-		return false;
-
-	if (tbl_info->level > 1 && idx >= XLAT_TABLE_ENTRIES)
+	if (tbl_info->level >= 3 || idx > tbl_info->num_entries)
 		return false;
 
 	entry = (uint64_t *)tbl_info->table + idx;
 
-	if ((*entry & DESC_ENTRY_TYPE_MASK) == BLOCK_DESC)
+	if ((*entry & DESC_ENTRY_TYPE_MASK) == TABLE_DESC)
 		return true;
-	if (*entry)
+
+	if (next_xlat >= MAX_XLAT_TABLES)
 		return false;
 
 	new_table = xlat_tables[next_xlat++];
-	if (next_xlat > MAX_XLAT_TABLES)
-		panic("running out of xlat tables");
-	memset(new_table, 0, XLAT_TABLE_SIZE);
 
-	*entry = TABLE_DESC | (uint64_t)(uintptr_t)new_table;
+	DMSG("xlat tables used %d / %d\n", next_xlat, MAX_XLAT_TABLES);
+
+	if (*entry) {
+		pa = *entry & OUTPUT_ADDRESS_MASK;
+		attr = *entry & ~(OUTPUT_ADDRESS_MASK | DESC_ENTRY_TYPE_MASK);
+		for (i = 0; i < XLAT_TABLE_ENTRIES; i++) {
+			new_table[i] = pa | attr | TABLE_DESC;
+			pa += block_size_on_next_lvl;
+		}
+	} else {
+		memset(new_table, 0, XLAT_TABLE_ENTRIES * XLAT_ENTRY_SIZE);
+	}
+
+	*entry = virt_to_phys(new_table) | TABLE_DESC;
 
 	return true;
 }

core/arch/arm/include/mm/core_mmu.h
@@ -384,15 +384,15 @@ bool core_mmu_find_table(vaddr_t va, unsigned max_level,
 		struct core_mmu_table_info *tbl_info);
 
 /*
- * core_mmu_prepare_small_page_mapping() - prepare target small page parent
- *	pgdir so that each of its entry can be used to map a page.
+ * core_mmu_entry_to_finer_grained() - divide mapping at current level into
+ *     smaller ones so memory can be mapped with finer granularity
  * @tbl_info:	table where target record located
  * @idx:	index of record for which a pdgir must be setup.
  * @secure:	true/false if pgdir maps secure/non-secure memory (32bit mmu)
  * @return true on successful, false on error
  */
-bool core_mmu_prepare_small_page_mapping(struct core_mmu_table_info *tbl_info,
-					 unsigned int idx, bool secure);
+bool core_mmu_entry_to_finer_grained(struct core_mmu_table_info *tbl_info,
+				     unsigned int idx, bool secure);
 
 void core_mmu_set_entry_primitive(void *table, size_t level, size_t idx,
 				  paddr_t pa, uint32_t attr);

core/arch/arm/mm/core_mmu.c
@@ -1343,8 +1343,8 @@ TEE_Result core_mmu_map_pages(vaddr_t vstart, paddr_t *pages, size_t num_pages,
 				break;
 
 			/* This is supertable. Need to divide it. */
-			if (!core_mmu_prepare_small_page_mapping(&tbl_info, idx,
-								 secure))
+			if (!core_mmu_entry_to_finer_grained(&tbl_info, idx,
+							     secure))
 				panic("Failed to spread pgdir on small tables");
 		}
 

core/arch/arm/mm/core_mmu_v7.c
@@ -520,48 +520,6 @@ bool core_mmu_find_table(vaddr_t va, unsigned max_level,
 	return true;
 }
 
-bool core_mmu_prepare_small_page_mapping(struct core_mmu_table_info *tbl_info,
-					 unsigned int idx, bool secure)
-{
-	uint32_t *new_table;
-	uint32_t *entry;
-	uint32_t new_table_desc;
-	uint32_t attr;
-
-	if (tbl_info->level != 1)
-		return false;
-
-	if (idx >= NUM_L1_ENTRIES)
-		return false;
-
-	entry = (uint32_t *)tbl_info->table + idx;
-	if (*entry && get_desc_type(1, *entry) != DESC_TYPE_SECTION)
-		return false;
-
-	attr = desc_to_mattr(1, *entry);
-
-	if (attr) {
-		/* If pgdir maps something, check the secure attribute fits */
-		return secure == (attr & TEE_MATTR_SECURE);
-	}
-
-	if (secure)
-		attr = TEE_MATTR_SECURE;
-
-	new_table = core_mmu_alloc_l2(NUM_L2_ENTRIES * SMALL_PAGE_SIZE);
-	if (!new_table)
-		return false;
-
-	new_table_desc = SECTION_PT_PT | (uint32_t)new_table;
-	if (!secure)
-		new_table_desc |= SECTION_PT_NOTSECURE;
-
-	/* Update descriptor at current level */
-	*entry = new_table_desc;
-
-	return true;
-}
-
 void core_mmu_set_entry_primitive(void *table, size_t level, size_t idx,
 				  paddr_t pa, uint32_t attr)
 {
@@ -597,6 +555,60 @@ static paddr_t desc_to_pa(unsigned level, uint32_t desc)
 	}
 
 	return desc & ~((1 << shift_mask) - 1);
+}
+
+bool core_mmu_entry_to_finer_grained(struct core_mmu_table_info *tbl_info,
+				     unsigned int idx, bool secure)
+{
+	uint32_t *new_table;
+	uint32_t *entry;
+	uint32_t new_table_desc;
+	uint32_t attr;
+	uint32_t desc;
+	paddr_t pa;
+	int i;
+
+	if (tbl_info->level != 1)
+		return false;
+
+	if (idx >= NUM_L1_ENTRIES)
+		return false;
+
+	entry = (uint32_t *)tbl_info->table + idx;
+	attr = desc_to_mattr(1, *entry);
+
+	if (*entry && get_desc_type(1, *entry) == DESC_TYPE_PAGE_TABLE) {
+		/*
+		 * If there is page table already,
+		 * check the secure attribute fits
+		 */
+		return secure == (bool)(attr & TEE_MATTR_SECURE);
+	}
+
+	/* If there is something mapped, check the secure access flag */
+	if (attr && secure != (bool)(attr & TEE_MATTR_SECURE))
+		return false;
+
+	new_table = core_mmu_alloc_l2(NUM_L2_ENTRIES * SMALL_PAGE_SIZE);
+	if (!new_table)
+		return false;
+
+	new_table_desc = SECTION_PT_PT | (uint32_t)new_table;
+
+	if (!secure)
+		new_table_desc |= SECTION_PT_NOTSECURE;
+
+	if (*entry) {
+		pa = desc_to_pa(1, *entry);
+		desc = mattr_to_desc(2, attr);
+		for (i = 0; i < NUM_L2_ENTRIES; i++, pa += SMALL_PAGE_SIZE)
+			new_table[i] = desc | pa;
+	}
+
+	/* Update descriptor at current level */
+	*entry = new_table_desc;
+
+	return true;
 }
 
 void core_mmu_get_entry_primitive(const void *table, size_t level,

commit 81d80da279ef65f8e420fa13749400681d1f43cb

core: LPAE finer grain remap support
Correct core_mmu_entry_to_finer_grained() when remapping an already
mapped area. Prior this change were the page descriptors filled with
corrupted data: physical address was badly computed and the table/block
mapping tag was wrong.

Fixes: 4c4ae2107715 ("mmu: replace _prepare_small_page_mapping with _entry_to_finer_grained")
Signed-off-by: Jeffrey Lin <linjunemail@gmail.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/mm/core_mmu_lpae.c
@@ -767,8 +767,8 @@ bool core_mmu_entry_to_finer_grained(struct core_mmu_table_info *tbl_info,
 	int i;
 	paddr_t pa;
 	uint64_t attr;
-	paddr_t block_size_on_next_lvl = L1_XLAT_ADDRESS_SHIFT -
-		tbl_info->level * XLAT_TABLE_ENTRIES_SHIFT;
+	paddr_t block_size_on_next_lvl = 1 << (L1_XLAT_ADDRESS_SHIFT -
+		tbl_info->level * XLAT_TABLE_ENTRIES_SHIFT);
 	struct mmu_partition *prtn;
 
 #ifdef CFG_VIRTUALIZATION
@@ -798,7 +798,7 @@ bool core_mmu_entry_to_finer_grained(struct core_mmu_table_info *tbl_info,
 		pa = *entry & OUTPUT_ADDRESS_MASK;
 		attr = *entry & ~(OUTPUT_ADDRESS_MASK | DESC_ENTRY_TYPE_MASK);
 		for (i = 0; i < XLAT_TABLE_ENTRIES; i++) {
-			new_table[i] = pa | attr | TABLE_DESC;
+			new_table[i] = pa | attr | BLOCK_DESC;
 			pa += block_size_on_next_lvl;
 		}
 	} else {