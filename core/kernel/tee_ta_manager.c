OP-TEE/optee_os

commit f5df167c2ffb2b26de30bebe6e7a0674924b4de7

ftrace: Add function execution time support
Add support to dump function execution time at corresponding function
exit points in output ftrace buffer.

To achieve proper function execution time we need to exclude TA suspend
time from timestamps, so add corresponding support in TEE core.

Also user mapping must be active to access ftrace buffer, so do that
during TA resume.

Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (QEMU)
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/tee_ta_manager.c
@@ -29,6 +29,7 @@
 #include <tee/tee_svc_storage.h>
 #include <tee_api_types.h>
 #include <trace.h>
+#include <user_ta_header.h>
 #include <utee_types.h>
 #include <util.h>
 
@@ -876,12 +877,14 @@ struct tee_ta_session *tee_ta_get_calling_session(void)
 	return s;
 }
 
+#if defined(CFG_TA_GPROF_SUPPORT) || defined(CFG_TA_FTRACE_SUPPORT)
+
 #if defined(CFG_TA_GPROF_SUPPORT)
 void tee_ta_gprof_sample_pc(vaddr_t pc)
 {
-	struct tee_ta_session *s;
-	struct sample_buf *sbuf;
-	size_t idx;
+	struct tee_ta_session *s = NULL;
+	struct sample_buf *sbuf = NULL;
+	size_t idx = 0;
 
 	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
 		return;
@@ -895,23 +898,15 @@ void tee_ta_gprof_sample_pc(vaddr_t pc)
 	sbuf->count++;
 }
 
-/*
- * Update user-mode CPU time for the current session
- * @suspend: true if session is being suspended (leaving user mode), false if
- * it is resumed (entering user mode)
- */
-static void tee_ta_update_session_utime(bool suspend)
+static void gprof_update_session_utime(bool suspend, struct tee_ta_session *s,
+				       uint64_t now)
 {
-	struct tee_ta_session *s;
-	struct sample_buf *sbuf;
-	uint64_t now;
+	struct sample_buf *sbuf = NULL;
 
-	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
-		return;
 	sbuf = s->sbuf;
 	if (!sbuf)
 		return;
-	now = read_cntpct();
+
 	if (suspend) {
 		assert(sbuf->usr_entered);
 		sbuf->usr += now - sbuf->usr_entered;
@@ -922,6 +917,50 @@ static void tee_ta_update_session_utime(bool suspend)
 			now++; /* 0 is reserved */
 		sbuf->usr_entered = now;
 	}
+}
+#endif
+
+#if defined(CFG_TA_FTRACE_SUPPORT)
+static void ftrace_update_session_utime(bool suspend, struct tee_ta_session *s,
+					uint64_t now)
+{
+	struct ftrace_buf *fbuf = NULL;
+	uint32_t i = 0;
+
+	fbuf = s->fbuf;
+	if (!fbuf)
+		return;
+
+	if (suspend) {
+		fbuf->suspend_time = now;
+	} else {
+		for (i = 0; i <= fbuf->ret_idx; i++)
+			fbuf->begin_time[i] += now - fbuf->suspend_time;
+	}
+}
+#endif
+
+/*
+ * Update user-mode CPU time for the current session
+ * @suspend: true if session is being suspended (leaving user mode), false if
+ * it is resumed (entering user mode)
+ */
+static void tee_ta_update_session_utime(bool suspend)
+{
+	struct tee_ta_session *s = NULL;
+	uint64_t now = 0;
+
+	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
+		return;
+
+	now = read_cntpct();
+
+#if defined(CFG_TA_GPROF_SUPPORT)
+	gprof_update_session_utime(suspend, s, now);
+#endif
+#if defined(CFG_TA_FTRACE_SUPPORT)
+	ftrace_update_session_utime(suspend, s, now);
+#endif
 }
 
 void tee_ta_update_session_utime_suspend(void)

ldelf/ftrace.c
@@ -53,6 +53,7 @@ bool ftrace_init(struct ftrace_buf **fbuf_ptr)
 	fbuf->ret_func_ptr = finfo->ret_ptr.ptr64;
 	fbuf->ret_idx = 0;
 	fbuf->lr_idx = 0;
+	fbuf->suspend_time = 0;
 	fbuf->buf_off = fbuf->head_off + count;
 	fbuf->curr_size = 0;
 	fbuf->max_size = fbuf_size - sizeof(struct ftrace_buf) - count;

lib/libutee/include/user_ta_header.h
@@ -55,6 +55,8 @@ struct ftrace_buf {
 	uint64_t ret_stack[FTRACE_RETFUNC_DEPTH]; /* Return stack */
 	uint32_t ret_idx;	/* Return stack index */
 	uint32_t lr_idx;	/* lr index used for stack unwinding */
+	uint64_t begin_time[FTRACE_RETFUNC_DEPTH]; /* Timestamp */
+	uint64_t suspend_time;	/* Suspend timestamp */
 	uint32_t curr_size;	/* Size of ftrace buffer */
 	uint32_t max_size;	/* Max allowed size of ftrace buffer */
 	uint32_t head_off;	/* Ftrace buffer header offset */

lib/libutee/arch/arm/ftrace/ftrace.c
@@ -10,10 +10,13 @@
  * profiled too.
  */
 
+#include <arm_user_sysreg.h>
 #include <setjmp.h>
 #include <user_ta_header.h>
 #include <utee_syscalls.h>
 #include "ftrace.h"
+
+#define DURATION_MAX_LEN		16
 
 static const char hex_str[] = "0123456789abcdef";
 
@@ -40,8 +43,11 @@ static size_t __noprof to_func_enter_fmt(char *buf, uint32_t ret_idx,
 	uint32_t addr_size = 2 * sizeof(unsigned long);
 	uint32_t i = 0;
 
-	for (i = 0; i < ret_idx; i++)
-		*str++ = ' ';
+	for (i = 0; i < (DURATION_MAX_LEN + ret_idx); i++)
+		if (i == (DURATION_MAX_LEN - 2))
+			*str++ = '|';
+		else
+			*str++ = ' ';
 
 	*str++ = '0';
 	*str++ = 'x';
@@ -69,7 +75,8 @@ void __noprof ftrace_enter(unsigned long pc, unsigned long *lr)
 	if (!fbuf->buf_off || !fbuf->max_size)
 		return;
 
-	dump_size = fbuf->ret_idx + (2 * sizeof(unsigned long)) + 8;
+	dump_size = DURATION_MAX_LEN + fbuf->ret_idx +
+			(2 * sizeof(unsigned long)) + 8;
 
 	/*
 	 * Check if we have enough space in ftrace buffer. If not then just
@@ -85,6 +92,7 @@ void __noprof ftrace_enter(unsigned long pc, unsigned long *lr)
 
 	if (fbuf->ret_idx < FTRACE_RETFUNC_DEPTH) {
 		fbuf->ret_stack[fbuf->ret_idx] = *lr;
+		fbuf->begin_time[fbuf->ret_idx] = read_cntpct();
 		fbuf->ret_idx++;
 	} else {
 		/*
@@ -97,11 +105,42 @@ void __noprof ftrace_enter(unsigned long pc, unsigned long *lr)
 	*lr = (unsigned long)&__ftrace_return;
 }
 
+static void __noprof ftrace_duration(char *buf, uint64_t start, uint64_t end)
+{
+	uint32_t cntfrq = read_cntfrq();
+	uint64_t ticks = end - start;
+	uint32_t us = 0;
+	uint32_t ns = 0;
+	uint32_t i = 0;
+
+	ticks = ticks * 1000000000 / cntfrq;
+
+	us = (ticks % 1000000000) / 1000;
+	ns = ticks % 1000;
+
+	*buf-- = 's';
+	*buf-- = 'u';
+	*buf-- = ' ';
+
+	for (i = 0; i < 3; i++) {
+		*buf-- = hex_str[ns % 10];
+		ns /= 10;
+	}
+
+	*buf-- = '.';
+
+	while (us) {
+		*buf-- = hex_str[us % 10];
+		us /= 10;
+	}
+}
+
 unsigned long __noprof ftrace_return(void)
 {
 	struct ftrace_buf *fbuf = NULL;
 	size_t dump_size = 0;
 	char *curr_buf = NULL;
+	char *dur_loc = NULL;
 	uint32_t i = 0;
 
 	fbuf = &__ftrace_buf_start;
@@ -124,21 +163,33 @@ unsigned long __noprof ftrace_return(void)
 		*(curr_buf - 2) = '\n';
 		*(curr_buf - 1) = '\0';
 		fbuf->curr_size -= 1;
+
+		dur_loc = curr_buf - (fbuf->ret_idx +
+				      (2 * sizeof(unsigned long)) + 11);
+		ftrace_duration(dur_loc, fbuf->begin_time[fbuf->ret_idx],
+				read_cntpct());
 	} else {
-		dump_size = fbuf->ret_idx + 3;
+		dump_size = DURATION_MAX_LEN + fbuf->ret_idx + 3;
 		if ((fbuf->curr_size + dump_size) > fbuf->max_size)
 			fbuf_shift(fbuf, dump_size);
 
 		curr_buf = (char *)fbuf + fbuf->buf_off + fbuf->curr_size;
 
-		for (i = 0; i < fbuf->ret_idx; i++)
-			*curr_buf++ = ' ';
+		for (i = 0; i < (DURATION_MAX_LEN + fbuf->ret_idx); i++)
+			if (i == (DURATION_MAX_LEN - 2))
+				*curr_buf++ = '|';
+			else
+				*curr_buf++ = ' ';
 
 		*curr_buf++ = '}';
 		*curr_buf++ = '\n';
 		*curr_buf = '\0';
 
 		fbuf->curr_size += dump_size - 1;
+
+		dur_loc = curr_buf - fbuf->ret_idx - 6;
+		ftrace_duration(dur_loc, fbuf->begin_time[fbuf->ret_idx],
+				read_cntpct());
 	}
 
 	return fbuf->ret_stack[fbuf->ret_idx];

core/arch/arm/kernel/thread.c
@@ -533,11 +533,11 @@ static void thread_resume_from_rpc(struct thread_smc_args *args)
 
 	l->curr_thread = n;
 
-	if (is_user_mode(&threads[n].regs))
-		tee_ta_update_session_utime_resume();
-
 	if (threads[n].have_user_map)
 		core_mmu_set_user_map(&threads[n].user_map);
+
+	if (is_user_mode(&threads[n].regs))
+		tee_ta_update_session_utime_resume();
 
 	/*
 	 * Return from RPC to request service of a foreign interrupt must not

core/arch/arm/kernel/user_ta.c
@@ -252,6 +252,7 @@ static TEE_Result init_with_ldelf(struct tee_ta_session *sess,
 	utc->dump_entry_func = arg->dump_entry;
 #ifdef CFG_TA_FTRACE_SUPPORT
 	utc->ftrace_entry_func = arg->ftrace_entry;
+	sess->fbuf = arg->fbuf;
 #endif
 
 out:

core/include/kernel/tee_ta_manager.h
@@ -107,6 +107,9 @@ struct tee_ta_session {
 #if defined(CFG_TA_GPROF_SUPPORT)
 	struct sample_buf *sbuf; /* Profiling data (PC sampling) */
 #endif
+#if defined(CFG_TA_FTRACE_SUPPORT)
+	struct ftrace_buf *fbuf; /* ftrace buffer */
+#endif
 };
 
 /* Registered contexts */
@@ -160,14 +163,17 @@ struct tee_ta_session *tee_ta_get_session(uint32_t id, bool exclusive,
 
 void tee_ta_put_session(struct tee_ta_session *sess);
 
-#if defined(CFG_TA_GPROF_SUPPORT)
-void tee_ta_gprof_sample_pc(vaddr_t pc);
+#if defined(CFG_TA_GPROF_SUPPORT) || defined(CFG_TA_FTRACE_SUPPORT)
 void tee_ta_update_session_utime_suspend(void);
 void tee_ta_update_session_utime_resume(void);
 #else
-static inline void tee_ta_gprof_sample_pc(vaddr_t pc __unused) {}
 static inline void tee_ta_update_session_utime_suspend(void) {}
 static inline void tee_ta_update_session_utime_resume(void) {}
+#endif
+#if defined(CFG_TA_GPROF_SUPPORT)
+void tee_ta_gprof_sample_pc(vaddr_t pc);
+#else
+static inline void tee_ta_gprof_sample_pc(vaddr_t pc __unused) {}
 #endif
 
 #endif

commit 035115e8d4e246dfe9f4487a7c4c10c054e1ad70

ftrace: use wall clock time
The current ftrace timestamping code for TAs ignores the time spent
outside user space. While this has positive aspects, such as avoiding
noise due to context switching, it can be misleading. I also suspect it
will not work well with combined kernel + user space tracing (when
syscall ftrace is implemented).

This commit removes the code that was specially inserted to ignore
non-user time. As a result, the durations shown in the dump are computed
from the wall clock times taken on function entries and exits.
In my opinion, this gives a much better view of the actual execution.
Here is an example with xtest 1009 (only the part that calls
TEE_Wait(2000)), built for QEMU with:

 $ make CFG_TA_FTRACE_SUPPORT=y CFG_ULIBS_MCOUNT=y \
        CFLAGS_ta_arm32="-O0 -pg"

 $ cat ftrace-5b9e0e40-2636-11e1-ad9e0002a5d5c51b.out | \
   ./optee_os/scripts/symbolize.py -d out-br/build/optee_test-1.0/ta/*/out

 Function graph for TA: 5b9e0e40-2636-11e1-ad9e-0002a5d5c51b @ 170000
 ...
              | __ta_entry_c() {
              |  __utee_entry() {
              |   entry_invoke_command() {
    21.120 us |    ta_header_get_session();
    25.040 us |    from_utee_params();
              |    ta_header_save_params() {
     1.072 us |     memcpy();
    13.728 us |    }
              |    TA_InvokeCommandEntryPoint() {
              |     ta_entry_wait() {
              |      printf() {
    11.888 us |       trace_get_level();
              |       vsnprintf() {
              |        __vsnprintf() {
   387.984 us |         kprintf();
   451.840 us |        }
   508.128 us |       }
              |       trace_ext_puts() {
    56.192 us |        strlen();
   371.440 us |        utee_log();
   528.960 us |       }
  1143.376 us |      }
              |      TEE_Wait() {
  2026.760 ms |       utee_wait();
  2027.216 ms |      }
  2028.504 ms |     }
  2028.646 ms |    }
   257.312 us |    to_utee_params();
  2029.316 ms |   }
              |   ta_header_save_params() {
     9.216 us |    memset();
    94.528 us |   }
  2029.599 ms |  }
  2029.670 ms | }
 ...

Fixes: f5df167c2ffb ("ftrace: Add function execution time support")

core/kernel/tee_ta_manager.c
@@ -921,26 +921,6 @@ static void gprof_update_session_utime(bool suspend, struct tee_ta_session *s,
 }
 #endif
 
-#if defined(CFG_TA_FTRACE_SUPPORT)
-static void ftrace_update_session_utime(bool suspend, struct tee_ta_session *s,
-					uint64_t now)
-{
-	struct ftrace_buf *fbuf = NULL;
-	uint32_t i = 0;
-
-	fbuf = s->fbuf;
-	if (!fbuf)
-		return;
-
-	if (suspend) {
-		fbuf->suspend_time = now;
-	} else {
-		for (i = 0; i <= fbuf->ret_idx; i++)
-			fbuf->begin_time[i] += now - fbuf->suspend_time;
-	}
-}
-#endif
-
 /*
  * Update user-mode CPU time for the current session
  * @suspend: true if session is being suspended (leaving user mode), false if
@@ -958,9 +938,6 @@ static void tee_ta_update_session_utime(bool suspend)
 
 #if defined(CFG_TA_GPROF_SUPPORT)
 	gprof_update_session_utime(suspend, s, now);
-#endif
-#if defined(CFG_TA_FTRACE_SUPPORT)
-	ftrace_update_session_utime(suspend, s, now);
 #endif
 }
 

ldelf/ftrace.c
@@ -53,7 +53,6 @@ bool ftrace_init(struct ftrace_buf **fbuf_ptr)
 	fbuf->ret_func_ptr = finfo->ret_ptr.ptr64;
 	fbuf->ret_idx = 0;
 	fbuf->lr_idx = 0;
-	fbuf->suspend_time = 0;
 	fbuf->buf_off = fbuf->head_off + count;
 	fbuf->curr_size = 0;
 	fbuf->max_size = fbuf_size - sizeof(struct ftrace_buf) - count;

lib/libutee/include/user_ta_header.h
@@ -56,7 +56,6 @@ struct ftrace_buf {
 	uint32_t ret_idx;	/* Return stack index */
 	uint32_t lr_idx;	/* lr index used for stack unwinding */
 	uint64_t begin_time[FTRACE_RETFUNC_DEPTH]; /* Timestamp */
-	uint64_t suspend_time;	/* Suspend timestamp */
 	uint32_t curr_size;	/* Size of ftrace buffer */
 	uint32_t max_size;	/* Max allowed size of ftrace buffer */
 	uint32_t head_off;	/* Ftrace buffer header offset */