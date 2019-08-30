e2d25104fcce9c70cc3db653fe0e18af46b63c4e
Sumit Garg <sumit.garg@linaro.org>
2019-08-30 12:57:13 +0530

    ftrace: move ftrace code from libutee to libutils
    Since TEE core and TA can share most of ftrace library code, so move
    ftrace code from libutee to libutils library which is shared among TEE
    core and TA.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

--- lib/libutils/ext/arch/arm/sub.mk
+++ lib/libutils/ext/arch/arm/sub.mk
@@ -1,3 +1,7 @@
 srcs-$(CFG_ARM32_$(sm)) += aeabi_unwind.c
 srcs-$(CFG_ARM32_$(sm)) += atomic_a32.S
 srcs-$(CFG_ARM64_$(sm)) += atomic_a64.S
+ifeq ($(filter $(sm), core ldelf),) # TA
+srcs-$(CFG_ARM32_$(sm)) += mcount_a32.S
+srcs-$(CFG_ARM64_$(sm)) += mcount_a64.S
+endif
--- lib/libutils/ext/ftrace/sub.mk
+++ lib/libutils/ext/ftrace/sub.mk
@@ -1,3 +1,5 @@
 cppflags-y += -I$(sub-dir)/../../..
 
+ifeq ($(filter $(sm), core ldelf),) # TA
 srcs-$(CFG_TA_FTRACE_SUPPORT) += ftrace.c
+endif
