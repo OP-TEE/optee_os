03200b5a20401e5cedcfb721ad65b117e567b1d1
Jerome Forissier <jerome@forissier.org>
2019-09-13 10:06:38 +0200

    core: move core/arch/arm/tee/pta_socket.c to core/arch/arm/pta/socket.c
    Pseudo-TAs belong in core/arch/arm/pta/. In addition, the pta_ prefix is
    useless and not used systematically so remove it.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/pta/sub.mk
+++ core/arch/arm/pta/sub.mk
@@ -14,5 +14,6 @@ srcs-$(CFG_WITH_STATS) += stats.c
 srcs-$(CFG_TA_GPROF_SUPPORT) += gprof.c
 srcs-$(CFG_TEE_BENCHMARK) += benchmark.c
 srcs-$(CFG_SDP_PTA) += sdp_pta.c
+srcs-$(CFG_GP_SOCKETS) += socket.c
 srcs-$(CFG_SYSTEM_PTA) += system.c
 srcs-$(CFG_DEVICE_ENUM_PTA) += device.c
