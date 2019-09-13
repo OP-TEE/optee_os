02184b7cd6e0bb07b52ca3be7e577e67b0d8c08c
Jerome Forissier <jerome@forissier.org>
2019-09-13 14:36:43 +0200

    core: move PTAs from core/arch/arm/pta to core/pta
    All pseudo-TAs in core/arch/arm/pta are not architecture-
    specific so move them out of the arch directory.
    
    sdp_pta.c is renamed sdp.c since _pta is redundant.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/arm.mk
+++ core/arch/arm/arm.mk
@@ -84,7 +84,7 @@ endif
 
 core-platform-cppflags	+= -I$(arch-dir)/include
 core-platform-subdirs += \
-	$(addprefix $(arch-dir)/, kernel crypto mm tee pta) $(platform-dir)
+	$(addprefix $(arch-dir)/, kernel crypto mm tee) $(platform-dir)
 
 ifneq ($(CFG_WITH_ARM_TRUSTED_FW),y)
 core-platform-subdirs += $(arch-dir)/sm
--- core/pta/sub.mk
+++ core/pta/sub.mk
@@ -1,1 +1,11 @@
 subdirs-$(CFG_TEE_CORE_EMBED_INTERNAL_TESTS) += tests
+
+srcs-$(CFG_TEE_BENCHMARK) += benchmark.c
+srcs-$(CFG_DEVICE_ENUM_PTA) += device.c
+srcs-$(CFG_TA_GPROF_SUPPORT) += gprof.c
+srcs-$(CFG_SDP_PTA) += sdp.c
+ifeq ($(CFG_WITH_USER_TA),y)
+srcs-$(CFG_SECSTOR_TA_MGMT_PTA) += secstor_ta_mgmt.c
+endif
+srcs-$(CFG_WITH_STATS) += stats.c
+srcs-$(CFG_SYSTEM_PTA) += system.c
