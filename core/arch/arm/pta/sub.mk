5843bb755f152c7961130e90bba969b8a1ce8f64
Jerome Forissier <jerome@forissier.org>
2019-09-13 14:36:43 +0200

    core: move PTAs from core/arch/arm/pta to core/pta
    All pseudo-TAs in core/arch/arm/pta are not architecture-
    specific so move them out of the arch directory.
    
    sdp_pta.c is renamed sdp.c since _pta is redundant.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/pta/sub.mk
+++
@@ -1,9 +0,0 @@
-ifeq ($(CFG_WITH_USER_TA),y)
-srcs-$(CFG_SECSTOR_TA_MGMT_PTA) += secstor_ta_mgmt.c
-endif
-srcs-$(CFG_WITH_STATS) += stats.c
-srcs-$(CFG_TA_GPROF_SUPPORT) += gprof.c
-srcs-$(CFG_TEE_BENCHMARK) += benchmark.c
-srcs-$(CFG_SDP_PTA) += sdp_pta.c
-srcs-$(CFG_SYSTEM_PTA) += system.c
-srcs-$(CFG_DEVICE_ENUM_PTA) += device.c
