bd1edb25bfe930611fa62496d008b2ecb18b49e4
Jens Wiklander <jens.wiklander@linaro.org>
2019-09-18 17:28:15 +0200

    core: move struct thread_ctx_regs to thread.h
    Moves definition of struct thread_ctx_regs from thread_private.h to
    <kernel/thread.h>.
    
    Acked-by: Pipat Methavanitpong <pipat.methavanitpong@linaro.org>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/kernel/thread_private.h
+++ core/arch/arm/kernel/thread_private.h
@@ -21,40 +21,6 @@ enum thread_state {
 	THREAD_STATE_ACTIVE,
 };
 
-#ifdef ARM32
-struct thread_ctx_regs {
-	uint32_t r0;
-	uint32_t r1;
-	uint32_t r2;
-	uint32_t r3;
-	uint32_t r4;
-	uint32_t r5;
-	uint32_t r6;
-	uint32_t r7;
-	uint32_t r8;
-	uint32_t r9;
-	uint32_t r10;
-	uint32_t r11;
-	uint32_t r12;
-	uint32_t usr_sp;
-	uint32_t usr_lr;
-	uint32_t svc_spsr;
-	uint32_t svc_sp;
-	uint32_t svc_lr;
-	uint32_t pc;
-	uint32_t cpsr;
-};
-#endif /*ARM32*/
-
-#ifdef ARM64
-struct thread_ctx_regs {
-	uint64_t sp;
-	uint64_t pc;
-	uint64_t cpsr;
-	uint64_t x[31];
-};
-#endif /*ARM64*/
-
 #ifdef ARM64
 struct thread_user_mode_rec {
 	uint64_t ctx_regs_ptr;
