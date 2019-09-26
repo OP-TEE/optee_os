OP-TEE/optee_os

commit 6b4fc6752b3c3a8885c88f1f6c9dccb43033b8af

symbolize.py: disable terminal local echo
When scripts/symbolize.py is used interactively and the input data is
copied and pasted into the terminal, both the input and output text are
displayed on the same screen. There is nothing preventing both text
streams from mixing up because it depends on buffering and how Python
reads and flushes its buffers.

Things usually go well with Python 2, but I have observed that Python 3
is more problematic. So the issue needs to be properly addressed. This
patch temporarily disables local echo so that only the processed text is
shown.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

scripts/symbolize.py
@@ -12,6 +12,7 @@ import os
 import re
 import subprocess
 import sys
+import termios
 
 CALL_STACK_RE = re.compile('Call stack:')
 # This gets the address from lines looking like this:
@@ -479,9 +480,17 @@ def main():
         args.dirs = []
     symbolizer = Symbolizer(sys.stdout, args.dirs, args.strip_path)
 
-    for line in sys.stdin:
-        symbolizer.write(line)
-    symbolizer.flush()
+    fd = sys.stdin.fileno()
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+    new[3] = new[3] & ~termios.ECHO  # lflags
+    try:
+        termios.tcsetattr(fd, termios.TCSADRAIN, new)
+        for line in sys.stdin:
+            symbolizer.write(line)
+    finally:
+        symbolizer.flush()
+        termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
 
 if __name__ == "__main__":

commit 0ff63349f5a4f8ef5598378540a63f9ca1e893bc

symbolize.py: fix exception when stdin is not a terminal
Commit 6b4fc6752b3c ("symbolize.py: disable terminal local echo") uses
termios functions on stdin unconditionally. Unfortunately, this will
cause an exception when stdin is not a terminal, for instance:

 $ echo Hello | ./script/symbolize.py
 Traceback (most recent call last):
   File "./scripts/symbolize.py", line 497, in <module>
     main()
   File "./scripts/symbolize.py", line 484, in main
     old = termios.tcgetattr(fd)
 termios.error: (25, 'Inappropriate ioctl for device')

Fix the issue by making sure stdin is a TTY before using the termios
functions.

Fixes: 6b4fc6752b3c ("symbolize.py: disable terminal local echo")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

scripts/symbolize.py
@@ -481,16 +481,20 @@ def main():
     symbolizer = Symbolizer(sys.stdout, args.dirs, args.strip_path)
 
     fd = sys.stdin.fileno()
-    old = termios.tcgetattr(fd)
-    new = termios.tcgetattr(fd)
-    new[3] = new[3] & ~termios.ECHO  # lflags
+    isatty = os.isatty(fd)
+    if isatty:
+        old = termios.tcgetattr(fd)
+        new = termios.tcgetattr(fd)
+        new[3] = new[3] & ~termios.ECHO  # lflags
     try:
-        termios.tcsetattr(fd, termios.TCSADRAIN, new)
+        if isatty:
+            termios.tcsetattr(fd, termios.TCSADRAIN, new)
         for line in sys.stdin:
             symbolizer.write(line)
     finally:
         symbolizer.flush()
-        termios.tcsetattr(fd, termios.TCSADRAIN, old)
+        if isatty:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
 
 if __name__ == "__main__":