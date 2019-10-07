OP-TEE/optee_os

commit 8fd4d26f6e226e56f93cb208ef4b2298d46ce297

plat-hikey: support generic RAM layout
Move default secure and non-secure Optee memory locations from
platform_config.h to conf.mk using the generic_ram_layout.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey960)

core/arch/arm/plat-hikey/conf.mk
@@ -56,3 +56,8 @@ ifeq ($(PLATFORM_FLAVOR),hikey960)
 CFG_CONSOLE_UART ?= 6
 CFG_DRAM_SIZE_GB ?= 3
 endif
+
+CFG_TZDRAM_START ?= 0x3F000000
+CFG_TZDRAM_SIZE ?= 0x01000000
+CFG_SHMEM_START ?= 0x3EE00000
+CFG_SHMEM_SIZE ?= 0x00200000

core/arch/arm/plat-hikey/platform_config.h
@@ -6,6 +6,8 @@
 #ifndef PLATFORM_CONFIG_H
 #define PLATFORM_CONFIG_H
 
+#include <mm/generic_ram_layout.h>
+
 /* Make stacks aligned to data cache line length */
 #define STACK_ALIGNMENT		64
 
@@ -43,6 +45,8 @@
 
 /*
  * HiKey and HiKey960 memory map
+ *
+ * Refer to the default configuration from conf.mk for description below.
  *
  * TZDRAM is secured (firewalled) by the DDR controller, see ARM-TF, but note
  * that security of this type of memory is weak for two reasons:
@@ -125,47 +129,5 @@
 #else /* PLATFORM_FLAVOR_hikey */
 #error Unknown HiKey PLATFORM_FLAVOR
 #endif /* PLATFORM_FLAVOR_hikey */
-
-
-#ifdef CFG_WITH_PAGER
-
-#define TZSRAM_BASE		0x3F000000
-#define TZSRAM_SIZE		CFG_CORE_TZSRAM_EMUL_SIZE
-
-#define TZDRAM_BASE		0x3F200000
-#define TZDRAM_SIZE		(14 * 1024 * 1024)
-
-#else /* CFG_WITH_PAGER */
-
-#define TZDRAM_BASE		0x3F000000
-#define TZDRAM_SIZE		(16 * 1024 * 1024)
-
-#endif /* CFG_WITH_PAGER */
-
-#define TEE_SHMEM_START		0x3EE00000
-#define TEE_SHMEM_SIZE		(2 * 1024 * 1024)
-
-#define TEE_RAM_VA_SIZE		(2 * 1024 * 1024)
-
-#define TEE_LOAD_ADDR		0x3F000000
-
-#ifdef CFG_WITH_PAGER
-
-#define TEE_RAM_START		TZSRAM_BASE
-#define TEE_RAM_PH_SIZE		TZSRAM_SIZE
-#define TA_RAM_START		ROUNDUP(TZDRAM_BASE, CORE_MMU_DEVICE_SIZE)
-#define TA_RAM_SIZE		ROUNDDOWN(TZDRAM_SIZE, CORE_MMU_DEVICE_SIZE)
-
-#else /* CFG_WITH_PAGER */
-
-#define TEE_RAM_PH_SIZE		TEE_RAM_VA_SIZE
-#define TEE_RAM_START		TZDRAM_BASE
-#define TA_RAM_START		ROUNDUP((TZDRAM_BASE + TEE_RAM_VA_SIZE), \
-					CORE_MMU_DEVICE_SIZE)
-
-#define TA_RAM_SIZE		ROUNDDOWN((TZDRAM_SIZE - TEE_RAM_VA_SIZE),\
-					  CORE_MMU_DEVICE_SIZE)
-
-#endif /* CFG_WITH_PAGER */
 
 #endif /* PLATFORM_CONFIG_H */

commit bd04f398025e4a5ce00c280bff2e1cde5d64d2c9

hikey, hikey960: set CFG_TEE_RAM_VA_SIZE to 2 MiB
Commit 8fd4d26f6e22 ("plat-hikey: support generic RAM layout") has
inadvertently removed the platform-specific definition of
TEE_RAM_VA_SIZE for HiKey platforms. It was 2 MiB before, and became
1 MiB (the default). This commit restores the proper value.

Fixes the following panic on boot (HiKey960, 32-bit TEE core with pager
enabled):

 I/TC: Pager is enabled. Hashes: 1824 bytes
 I/TC: Pager pool size: 252kB
 I/TC: OP-TEE version: 3.6.0-182-g2d7a8964df-dev (gcc version 6.2.1 20161016 (Linaro GCC 6.2-2016.11)) #5 Mon 07 Oct 2019 08:22:21 AM UTC arm
 E/TC:0 0 Panic at core/lib/libtomcrypt/mpi_desc.c:39 <get_mp_scratch_memory_pool>
 E/TC:0 0 Call stack:
 E/TC:0 0  0x3f003a4d

Fixes: 8fd4d26f6e22 ("plat-hikey: support generic RAM layout")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-hikey/conf.mk
@@ -62,5 +62,6 @@ CFG_TZDRAM_START ?= 0x3F000000
 CFG_TZDRAM_SIZE ?= 0x01000000
 CFG_SHMEM_START ?= 0x3EE00000
 CFG_SHMEM_SIZE ?= 0x00200000
+CFG_TEE_RAM_VA_SIZE ?= 0x00200000
 
 CFG_IN_TREE_EARLY_TAS += avb/023f8f1a-292a-432b-8fc4-de8471358067