8354ece4e45ea589af4cc2aafaa2399cd9e56ae6
Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
2019-10-10 00:53:04 +0200

    plat-rockchip: add rk3399 and px30 flavors
    Add support for the both the 6-core rk3399 as well as the 4-core px30
    Rockchip socs to be used as secure payload together with trusted firmware.
    
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
    Acked-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-rockchip/platform_config.h
+++ core/arch/arm/plat-rockchip/platform_config.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
  * Copyright (C) 2017, Fuzhou Rockchip Electronics Co., Ltd.
+ * Copyright (C) 2019, Theobroma Systems Design und Consulting GmbH
  */
 
 #ifndef PLATFORM_CONFIG_H
@@ -39,6 +40,49 @@
 /* Internal SRAM */
 #define ISRAM_BASE		0x10080000
 #define ISRAM_SIZE		SIZE_K(8)
+
+#elif defined(PLATFORM_FLAVOR_rk3399)
+
+#define MMIO_BASE		0xF8000000
+
+#define GIC_BASE		(MMIO_BASE + 0x06E00000)
+#define GIC_SIZE		SIZE_M(2)
+#define GICD_BASE		GIC_BASE
+#define GICR_BASE		(GIC_BASE + SIZE_M(1))
+
+#define UART0_BASE		(MMIO_BASE + 0x07180000)
+#define UART0_SIZE		SIZE_K(64)
+
+#define UART1_BASE		(MMIO_BASE + 0x07190000)
+#define UART1_SIZE		SIZE_K(64)
+
+#define UART2_BASE		(MMIO_BASE + 0x071A0000)
+#define UART2_SIZE		SIZE_K(64)
+
+#define UART3_BASE		(MMIO_BASE + 0x071B0000)
+#define UART3_SIZE		SIZE_K(64)
+
+#define SGRF_BASE		(MMIO_BASE + 0x07330000)
+#define SGRF_SIZE		SIZE_K(64)
+
+#elif defined(PLATFORM_FLAVOR_px30)
+
+#define GIC_BASE		0xff130000
+#define GIC_SIZE		SIZE_K(64)
+#define GICD_BASE		(GIC_BASE + 0x1000)
+#define GICC_BASE		(GIC_BASE + 0x2000)
+
+#define UART1_BASE		0xff158000
+#define UART1_SIZE		SIZE_K(64)
+
+#define UART2_BASE		0xff160000
+#define UART2_SIZE		SIZE_K(64)
+
+#define UART5_BASE		0xff178000
+#define UART5_SIZE		SIZE_K(64)
+
+#define FIREWALL_DDR_BASE	0xff534000
+#define FIREWALL_DDR_SIZE	SIZE_K(16)
 
 #else
 #error "Unknown platform flavor"
--- core/arch/arm/plat-rockchip/sub.mk
+++ core/arch/arm/plat-rockchip/sub.mk
@@ -1,7 +1,9 @@
 global-incdirs-y += .
 srcs-y += main.c
 srcs-y += platform.c
+srcs-$(PLATFORM_FLAVOR_px30) += platform_px30.c
 srcs-$(PLATFORM_FLAVOR_rk322x) += platform_rk322x.c
+srcs-$(PLATFORM_FLAVOR_rk3399) += platform_rk3399.c
 
 ifeq ($(PLATFORM_FLAVOR),rk322x)
 srcs-y += plat_init.S
