de5333edb926768505c5db3a2585b23a696eb74d
Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
2019-10-10 00:53:04 +0200

    plat-rockchip: add rk3399 and px30 flavors
    Add support for the both the 6-core rk3399 as well as the 4-core px30
    Rockchip socs to be used as secure payload together with trusted firmware.
    
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
    Acked-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-rockchip/conf.mk
+++ core/arch/arm/plat-rockchip/conf.mk
@@ -30,3 +30,31 @@ CFG_EARLY_CONSOLE_CLK_IN_HZ ?= 24000000
 endif
 
 CFG_EARLY_CONSOLE ?= n
+
+ifeq ($(PLATFORM_FLAVOR),rk3399)
+include core/arch/arm/cpu/cortex-armv8-0.mk
+$(call force,CFG_TEE_CORE_NB_CORE,6)
+$(call force,CFG_ARM_GICV3,y)
+
+CFG_TZDRAM_START ?= 0x30000000
+CFG_TZDRAM_SIZE  ?= 0x02000000
+CFG_SHMEM_START  ?= 0x32000000
+CFG_SHMEM_SIZE   ?= 0x00400000
+endif
+
+ifeq ($(PLATFORM_FLAVOR),px30)
+include core/arch/arm/cpu/cortex-armv8-0.mk
+$(call force,CFG_TEE_CORE_NB_CORE,4)
+
+CFG_TZDRAM_START ?= 0x30000000
+CFG_TZDRAM_SIZE  ?= 0x02000000
+CFG_SHMEM_START  ?= 0x32000000
+CFG_SHMEM_SIZE   ?= 0x00400000
+endif
+
+ifeq ($(platform-flavor-armv8),1)
+$(call force,CFG_ARM64_core,y)
+$(call force,CFG_WITH_ARM_TRUSTED_FW,y)
+$(call force,CFG_WITH_LPAE,y)
+ta-targets = ta_arm64
+endif
