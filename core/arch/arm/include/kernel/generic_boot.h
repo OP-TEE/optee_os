ef262691fe0eb512bdb0744a6968d2bcb924798f
Jens Wiklander <jens.wiklander@linaro.org>
2019-10-15 13:31:19 +0200

    core: add get_aslr_seed()
    Adds get_aslr_seed() which reads "kaslr-seed" from "/secure-chosen" in
    FDT. The seed is intended to use as input to ASLR, also known as,
    randomized address space layout.
    
    Once successfully read the seed is zeroed out in the FDT to minimize the
    risk of leaking the seed.
    
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/include/kernel/generic_boot.h
+++ core/arch/arm/include/kernel/generic_boot.h
@@ -53,4 +53,6 @@ void *get_embedded_dt(void);
 /* Returns external DTB if present, otherwise NULL */
 void *get_external_dt(void);
 
+unsigned long get_aslr_seed(void *fdt);
+
 #endif /* KERNEL_GENERIC_BOOT_H */
