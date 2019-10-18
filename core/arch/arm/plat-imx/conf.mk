30ae24b7d7ad7c62707ff6b514e02cad0ab9952f
Clement Faure <clement.faure@nxp.com>
2019-10-18 10:43:39 +0200

    core: imx: fix CFG_DRAM_BASE for i.MX7ulp
    Change CFG_DRAM_BASE to 0x60000000 for i.MX7ulp
    
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -331,7 +331,7 @@ endif
 
 ifeq ($(filter y, $(CFG_MX7ULP)), y)
 CFG_INIT_CNTVOFF ?= y
-CFG_DRAM_BASE ?= 0x80000000
+CFG_DRAM_BASE ?= UL(0x60000000)
 $(call force,CFG_IMX_LPUART,y)
 $(call force,CFG_BOOT_SECONDARY_REQUEST,n)
 endif
