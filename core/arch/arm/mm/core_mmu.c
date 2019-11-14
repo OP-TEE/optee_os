OP-TEE/optee_os

commit 1385854b72c9e6e43786a8054787eec8c668810d

core: Add core memory type MEM_AREA_IDENTITY_MAP_RX
Adds another memory type, MEM_AREA_IDENTITY_MAP_RX, to enum
teecore_memtypes.  MEM_AREA_IDENTITY_MAP_RX is used to represent memory
which is additionally identity mapped while OP-TEE is mapped at a
non-identity mapped location. This is needed to support CFG_CORE_ASLR=y.

The link script is updated to collect functions and read-only data in
between __identity_map_init_start and __identity_map_init_end.

Reviewed-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -645,6 +645,7 @@ uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 	case MEM_AREA_TEE_RAM:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRWX | cached;
 	case MEM_AREA_TEE_RAM_RX:
+	case MEM_AREA_IDENTITY_MAP_RX:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRX | cached;
 	case MEM_AREA_TEE_RAM_RO:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PR | cached;

core/arch/arm/include/kernel/linker.h
@@ -54,6 +54,9 @@ extern const uint8_t __vcore_init_ro_size[];
 extern const uint8_t __text_start[];
 extern const uint8_t __end[];
 
+extern const uint8_t __identity_map_init_start[];
+extern const uint8_t __identity_map_init_end[];
+
 extern uint8_t __data_start[];
 extern const uint8_t __data_end[];
 extern const uint8_t __rodata_start[];

core/arch/arm/include/mm/core_mmu.h
@@ -93,6 +93,7 @@
  * MEM_AREA_NEX_RAM_RW: nexus private r/w/non-executable memory (secure)
  * MEM_AREA_TEE_COHERENT: teecore coherent RAM (secure, reserved to TEE)
  * MEM_AREA_TEE_ASAN: core address sanitizer RAM (secure, reserved to TEE)
+ * MEM_AREA_IDENTITY_MAP_RX: core identity mapped r/o executable memory (secure)
  * MEM_AREA_TA_RAM:   Secure RAM where teecore loads/exec TA instances.
  * MEM_AREA_NSEC_SHM: NonSecure shared RAM between NSec and TEE.
  * MEM_AREA_RAM_NSEC: NonSecure RAM storing data
@@ -115,6 +116,7 @@ enum teecore_memtypes {
 	MEM_AREA_NEX_RAM_RW,
 	MEM_AREA_TEE_COHERENT,
 	MEM_AREA_TEE_ASAN,
+	MEM_AREA_IDENTITY_MAP_RX,
 	MEM_AREA_TA_RAM,
 	MEM_AREA_NSEC_SHM,
 	MEM_AREA_RAM_NSEC,
@@ -141,6 +143,7 @@ static inline const char *teecore_memtype_name(enum teecore_memtypes type)
 		[MEM_AREA_TEE_RAM_RW] = "TEE_RAM_RW",
 		[MEM_AREA_NEX_RAM_RW] = "NEX_RAM_RW",
 		[MEM_AREA_TEE_ASAN] = "TEE_ASAN",
+		[MEM_AREA_IDENTITY_MAP_RX] = "IDENTITY_MAP_RX",
 		[MEM_AREA_TEE_COHERENT] = "TEE_COHERENT",
 		[MEM_AREA_TA_RAM] = "TA_RAM",
 		[MEM_AREA_NSEC_SHM] = "NSEC_SHM",

core/arch/arm/kernel/kern.ld.S
@@ -85,6 +85,15 @@ SECTIONS
 
 	.text : {
 		KEEP(*(.text._start))
+		__identity_map_init_start = .;
+		*(.identity_map .identity_map.* \
+			/*
+			 * The one below is needed because it's a weak
+			 * symbol that may be overridden by platform
+			 * specific code.
+			 */
+		  .text.get_core_pos_mpidr)
+		__identity_map_init_end = .;
 		KEEP(*(.text.init .text.plat_cpu_reset_early \
 		       .text.reset .text.reset_primary .text.unhandled_cpu \
 		       .text.__assert_flat_mapped_range))

core/arch/arm/kernel/link_dummy.ld
@@ -13,25 +13,22 @@ SECTIONS
 }
 
 __asan_map_end = .;
-__asan_map_start = .;
 __asan_map_size = .;
+__asan_map_start = .;
 __asan_shadow_end = .;
-__asan_shadow_start = .;
 __asan_shadow_size = .;
+__asan_shadow_start = .;
 __bss_end = .;
 __bss_start = .;
-__nex_bss_end = .;
-__nex_bss_start = .;
 __ctor_end = .;
 __ctor_list = .;
-__data_start = .;
 __data_end = .;
+__data_start = .;
 __end = .;
+__end_phys_ddr_overall_section = .;
 __end_phys_mem_map_section = .;
 __end_phys_nsec_ddr_section = .;
 __end_phys_sdp_mem_section = .;
-__end_phys_ddr_overall_section = .;
-__start_phys_ddr_overall_section = .;
 __exidx_end = .;
 __exidx_start = .;
 __extab_end = .;
@@ -40,13 +37,17 @@ __heap1_end = .;
 __heap1_start = .;
 __heap2_end = .;
 __heap2_start = .;
-__nex_heap_end = .;
-__nex_heap_start = .;
+__identity_map_init_end = .;
+__identity_map_init_start = .;
 __initcall_end = .;
 __initcall_start = .;
 __init_end = .;
 __init_size = .;
 __init_start = .;
+__nex_bss_end = .;
+__nex_bss_start = .;
+__nex_heap_end = .;
+__nex_heap_start = .;
 __nozi_end = .;
 __nozi_stack_end = .;
 __nozi_stack_start = .;
@@ -57,10 +58,11 @@ __pageable_part_start = .;
 __pageable_start = .;
 __rodata_dtdrv_end = .;
 __rodata_dtdrv_start = .;
-__rodata_early_ta_start = .;
 __rodata_early_ta_end = .;
+__rodata_early_ta_start = .;
 __rodata_end = .;
 __rodata_start = .;
+__start_phys_ddr_overall_section = .;
 __start_phys_nsec_ddr_section = .;
 __text_init_start = .;
 __text_start = .;
@@ -71,14 +73,14 @@ __vcore_init_ro_size = .;
 __vcore_init_ro_start = .;
 __vcore_init_rx_size = .;
 __vcore_init_rx_start = .;
+__vcore_nex_rw_size = .;
+__vcore_nex_rw_start = .;
 __vcore_unpg_ro_size = .;
 __vcore_unpg_ro_start = .;
 __vcore_unpg_rw_size = .;
 __vcore_unpg_rw_start = .;
 __vcore_unpg_rx_size = .;
 __vcore_unpg_rx_start = .;
-__vcore_nex_rw_size = .;
-__vcore_nex_rw_start = .;
 PROVIDE(core_v_str = 0);
 PROVIDE(tee_entry_std = 0);
 PROVIDE(tee_svc_handler = 0);

commit 6ce399a7bf4b28e6f8541913ce21c4a8b8c58b12

core: fix check_mem_map() vs MEM_AREA_IDENTITY_MAP_RX
This patch updates check_mem_map() to recognize MEM_AREA_IDENTITY_MAP_RX
as part of secure only memory.

This fix is only needed with CFG_CORE_ASLR=y and prevents an error like:
E/TC:0 0 check_mem_map:1166 Uhandled memtype 8
E/TC:0 0 Panic at core/arch/arm/mm/core_mmu.c:1167 <check_mem_map>

Fixes: 1385854b72c9 ("core: Add core memory type MEM_AREA_IDENTITY_MAP_RX")
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Tested-by: Jerome Forissier <jerome@forissier.org> (QEMU)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -1140,6 +1140,7 @@ static void check_mem_map(struct tee_mmap_region *map)
 		case MEM_AREA_TEE_RAM_RO:
 		case MEM_AREA_TEE_RAM_RW:
 		case MEM_AREA_NEX_RAM_RW:
+		case MEM_AREA_IDENTITY_MAP_RX:
 			if (!pbuf_is_inside(secure_only, m->pa, m->size))
 				panic("TEE_RAM can't fit in secure_only");
 			break;