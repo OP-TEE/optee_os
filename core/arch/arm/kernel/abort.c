17cb320e4f5bb65a2a28f9e29a2188cc276c794e
Jens Wiklander <jens.wiklander@linaro.org>
2019-11-15 09:14:50 +0100

    core: move VFP state into struct user_ta_ctx
    Moves the VFP state from struct user_ta_ctx to struct user_mode_ctx to make
    user mode handling a bit more generic.
    
    Acked-by: Pipat Methavanitpong <pipat.methavanitpong@linaro.org>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/kernel/abort.c
+++ core/arch/arm/kernel/abort.c
@@ -10,7 +10,7 @@
 #include <kernel/panic.h>
 #include <kernel/tee_ta_manager.h>
 #include <kernel/unwind.h>
-#include <kernel/user_ta.h>
+#include <kernel/user_mode_ctx.h>
 #include <mm/core_mmu.h>
 #include <mm/mobj.h>
 #include <mm/tee_pager.h>
@@ -377,7 +377,7 @@ static void handle_user_ta_vfp(void)
 	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
 		panic();
 
-	thread_user_enable_vfp(&to_user_ta_ctx(s->ctx)->vfp);
+	thread_user_enable_vfp(&to_user_mode_ctx(s->ctx)->vfp);
 }
 #endif /*CFG_WITH_VFP*/
 
--- core/include/kernel/user_mode_ctx_struct.h
+++ core/include/kernel/user_mode_ctx_struct.h
@@ -7,11 +7,15 @@
 #define __KERNEL_USER_MODE_CTX_STRUCT_H
 
 #include <kernel/tee_ta_manager.h>
+#include <kernel/thread.h>
 #include <mm/tee_mmu_types.h>
 
 struct user_mode_ctx {
 	struct vm_info vm_info;
 	struct tee_pager_area_head *areas;
+#if defined(CFG_WITH_VFP)
+	struct thread_user_vfp_state vfp;
+#endif
 	struct tee_ta_ctx ctx;
 };
 #endif /*__KERNEL_USER_MODE_CTX_STRUCT_H*/
