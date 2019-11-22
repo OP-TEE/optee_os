OP-TEE/optee_os

commit 170e9084a84fe215c806b1df3ae09703f79aa0ac

core: add support for CFG_CORE_ASLR
Adds support for CFG_CORE_ASLR to load TEE Core at a random address.
ASLR makes the exploitation of memory corruption vulnerabilities more
difficult.

Paging is currently not supported with CFG_CORE_ASLR=y.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/generic_entry_a32.S
@@ -6,6 +6,7 @@
 #include <arm32_macros.S>
 #include <arm.h>
 #include <asm.S>
+#include <elf_common.h>
 #include <generated/asm-defines.h>
 #include <keep.h>
 #include <kernel/asan.h>
@@ -440,13 +441,37 @@ shadow_stack_access_ok:
 	bl	arm_cl2_enable
 #endif
 
-	ldr	r0, =boot_mmu_config
+#ifdef CFG_CORE_ASLR
+	mov	r0, r6
+	bl	get_aslr_seed
+#else
+	mov	r0, #0
+#endif
+
+	ldr	r1, =boot_mmu_config
 	bl	core_init_mmu_map
+
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Process relocation information for updating with the new offset.
+	 * We're doing this now before MMU is enabled as some of the memory
+	 * will become write protected.
+	 */
+	ldr	r0, =boot_mmu_config
+	ldr	r0, [r0, #CORE_MMU_CONFIG_LOAD_OFFSET]
+	bl	relocate
+#endif
 
 	bl	__get_core_pos
 	bl	enable_mmu
-	/* Virtual addresses may have changed, reinitialize console */
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Reinitialize console, since register_serial_console() has
+	 * previously registered a PA and with ASLR the VA is different
+	 * from the PA.
+	 */
 	bl	console_init
+#endif
 
 	mov	r0, r4		/* pageable part address */
 	mov	r1, r5		/* ns-entry address */
@@ -489,8 +514,10 @@ shadow_stack_access_ok:
 	bl 	thread_clr_boot_thread
 
 #if defined(CFG_WITH_ARM_TRUSTED_FW)
+	ldr	r0, =boot_mmu_config
+	ldr	r0, [r0, #CORE_MMU_CONFIG_LOAD_OFFSET]
 	/* Pass the vector address returned from main_init */
-	mov	r1, r4
+	sub	r1, r4, r0
 #else
 	/* realy standard bootarg #1 and #2 to non secure entry */
 	mov	r4, #0
@@ -512,10 +539,48 @@ UNWIND(	.fnstart)
 UNWIND(	.fnend)
 END_FUNC unhandled_cpu
 
+#ifdef CFG_CORE_ASLR
+LOCAL_FUNC relocate , :
+	push	{r4-r5}
+	/* r0 holds load offset */
+	ldr	r1, =__rel_start
+	ldr	r2, =__rel_end
+
+	b	2f
+	/* Loop over the relocations (Elf32_Rel) and process all entries */
+1:	ldmia	r1!, {r4-r5}		/* r4 = r_offset, r5 = r_info */
+	/* We're only expecting R_ARM_RELATIVE relocations. */
+	cmp	r5, #R_ARM_RELATIVE
+	bne	.
+
+	/*
+	 * Update the pointer at r_offset + load_offset.
+	 *
+	 * Note that we're skipping the load_offset for the address to
+	 * update, this is because that memory isn't mapped at that address
+	 * yet.
+	 */
+	ldr	r12, [r4]
+	add	r12, r12, r0
+	str	r12, [r4]
+
+2:	cmp	r1, r2
+	bne	1b
+
+	pop	{r4-r5}
+	bx	lr
+END_FUNC relocate
+#endif
+
 /*
  * void enable_mmu(unsigned long core_pos);
+ *
+ * This function depends on being mapped with in the identity map where
+ * physical address and virtual address is the same. After MMU has been
+ * enabled the instruction pointer will be updated to execute as the new
+ * offset instead. Stack pointers and the return address are updated.
  */
-LOCAL_FUNC enable_mmu , :
+LOCAL_FUNC enable_mmu , : , .identity_map
 	/* r0 = core pos */
 	adr	r1, boot_mmu_config
 
@@ -537,10 +602,10 @@ LOCAL_FUNC enable_mmu , :
 	/*
 	 * ttbr0_el1 = ttbr0_base + ttbr0_core_offset * core_pos
 	 */
-	mla	r1, r0, r3, r2
-	write_ttbr0 r1
-	mov	r1, #0
-	write_ttbr1 r1
+	mla	r12, r0, r3, r2
+	write_ttbr0 r12
+	mov	r12, #0
+	write_ttbr1 r12
 #else
 	ldm	r1!, {r2, r3}
 	/*
@@ -563,9 +628,11 @@ LOCAL_FUNC enable_mmu , :
 	write_ttbr0 r2
 	write_ttbr1 r2
 
-	mov	r1, #0
-	write_contextidr r1
+	mov	r2, #0
+	write_contextidr r2
 #endif
+	ldm	r1!, {r2}
+	/* r2 = load_offset (always 0 if CFG_CORE_ASLR=n) */
 	isb
 
 	/* Invalidate TLB */
@@ -585,6 +652,12 @@ LOCAL_FUNC enable_mmu , :
 	orr	r0, r0, #(SCTLR_AFE | SCTLR_TRE)
 #endif
 	write_sctlr r0
+	isb
+
+	/* Update vbar */
+	read_vbar r1
+	add	r1, r1, r2
+	write_vbar r1
 	isb
 
 	/* Invalidate instruction cache and branch predictor */
@@ -608,6 +681,10 @@ LOCAL_FUNC enable_mmu , :
 	write_sctlr r0
 	isb
 
+	/* Adjust stack pointer and return address */
+	add	sp, sp, r2
+	add	lr, lr, r2
+
 	bx	lr
 END_FUNC enable_mmu
 
@@ -630,10 +707,11 @@ UNWIND(	.cantunwind)
 	write_vbar r0
 
 	mov	r4, lr
-	set_sp
 
 	bl	__get_core_pos
 	bl	enable_mmu
+
+	set_sp
 
 	mov	r0, r4
 	mov	r1, r5

core/arch/arm/arm.mk
@@ -136,6 +136,10 @@ core-platform-cflags += $(platform-cflags-debug-info)
 core-platform-aflags += $(platform-aflags-generic)
 core-platform-aflags += $(platform-aflags-debug-info)
 
+ifeq ($(CFG_CORE_ASLR),y)
+core-platform-cflags += -fpie
+endif
+
 ifeq ($(CFG_ARM64_core),y)
 arch-bits-core := 64
 core-platform-cppflags += $(arm64-platform-cppflags)

core/arch/arm/include/kernel/generic_boot.h
@@ -9,6 +9,7 @@
 #include <types_ext.h>
 
 extern uint8_t embedded_secure_dtb[];
+extern const struct core_mmu_config boot_mmu_config;
 
 #if defined(CFG_WITH_ARM_TRUSTED_FW)
 unsigned long cpu_on_handler(unsigned long a0, unsigned long a1);

core/arch/arm/include/mm/core_mmu.h
@@ -279,21 +279,24 @@ struct core_mmu_config {
 	uint64_t mair_el1;
 	uint64_t ttbr0_el1_base;
 	uint64_t ttbr0_core_offset;
+	uint64_t load_offset;
 #elif defined(CFG_WITH_LPAE)
 	uint32_t ttbcr;
 	uint32_t mair0;
 	uint32_t ttbr0_base;
 	uint32_t ttbr0_core_offset;
+	uint32_t load_offset;
 #else
 	uint32_t prrr;
 	uint32_t nmrr;
 	uint32_t dacr;
 	uint32_t ttbcr;
 	uint32_t ttbr;
+	uint32_t load_offset;
 #endif
 };
 
-void core_init_mmu_map(struct core_mmu_config *cfg);
+void core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg);
 void core_init_mmu_regs(struct core_mmu_config *cfg);
 
 bool core_mmu_place_tee_ram_at_top(paddr_t paddr);

core/arch/arm/kernel/asm-defines.c
@@ -94,4 +94,6 @@ DEFINES
 
 	/* struct core_mmu_config */
 	DEFINE(CORE_MMU_CONFIG_SIZE, sizeof(struct core_mmu_config));
+	DEFINE(CORE_MMU_CONFIG_LOAD_OFFSET,
+	       offsetof(struct core_mmu_config, load_offset));
 }

core/arch/arm/kernel/generic_boot.c
@@ -1095,6 +1095,10 @@ static void init_primary_helper(unsigned long pageable_part,
 	configure_console_from_dt();
 
 	IMSG("OP-TEE version: %s", core_v_str);
+#ifdef CFG_CORE_ASLR
+	DMSG("Executing at offset %#lx with virtual load address %#"PRIxVA,
+	     (unsigned long)boot_mmu_config.load_offset, VCORE_START_VA);
+#endif
 
 	main_init_gic();
 	init_vfp_nsec();

core/arch/arm/kernel/generic_entry_a64.S
@@ -8,6 +8,7 @@
 #include <arm64_macros.S>
 #include <arm.h>
 #include <asm.S>
+#include <elf_common.h>
 #include <generated/asm-defines.h>
 #include <keep.h>
 #include <sm/optee_smc.h>
@@ -118,6 +119,21 @@ clear_nex_bss:
 	b.lt	clear_nex_bss
 #endif
 
+#ifdef CFG_CORE_ASLR
+	/*
+	 * The code is supposed to be linked against the address where
+	 * binary was loaded (0 load offset). However, since AArch64 uses
+	 * relocations of the new Rela type, relocations has to be
+	 * processed to guarantee that all updated locations holds the
+	 * correct value.
+	 *
+	 * N.b, it seems that some toolchains already provides expected
+	 * values at the target locations.
+	 */
+	mov	x0, #0
+	bl	relocate
+#endif
+
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
 	set_sp
 
@@ -138,13 +154,36 @@ clear_nex_bss:
 	/* Enable Console */
 	bl	console_init
 
-	adr	x0, boot_mmu_config
+#ifdef CFG_CORE_ASLR
+	mov	x0, x20
+	bl	get_aslr_seed
+#else
+	mov	x0, #0
+#endif
+
+	adr	x1, boot_mmu_config
 	bl	core_init_mmu_map
+
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Process relocation information again updating for the new
+	 * offset. We're doing this now before MMU is enabled as some of
+	 * the memory will become write protected.
+	 */
+	ldr	x0, boot_mmu_config + CORE_MMU_CONFIG_LOAD_OFFSET
+	bl	relocate
+#endif
 
 	bl	__get_core_pos
 	bl	enable_mmu
-	/* Virtual addresses may have changed, reinitialize console */
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Reinitialize console, since register_serial_console() has
+	 * previously registered a PA and with ASLR the VA is different
+	 * from the PA.
+	 */
 	bl	console_init
+#endif
 
 	mov	x0, x19		/* pagable part address */
 	mov	x1, #-1
@@ -178,26 +217,72 @@ clear_nex_bss:
 	bl 	thread_clr_boot_thread
 #endif
 
-	/* Pass the vector address returned from main_init */
-	mov	x1, x19
+	/*
+	 * Pass the vector address returned from main_init
+	 * Compensate for the load offset since cpu_on_handler() is
+	 * called with MMU off.
+	 */
+	ldr	x0, boot_mmu_config + CORE_MMU_CONFIG_LOAD_OFFSET
+	sub	x1, x19, x0
 	mov	x0, #TEESMC_OPTEED_RETURN_ENTRY_DONE
 	smc	#0
 	b	.	/* SMC should not return */
 END_FUNC _start
 KEEP_INIT _start
 
+#ifdef CFG_CORE_ASLR
+LOCAL_FUNC relocate , :
+	/* x0 holds load offset */
+	ldr	x1, =__rela_start
+	ldr	x2, =__rela_end
+
+	b	2f
+	/* Loop over the relocations (Elf64_Rela) and process all entries */
+1:	ldp	x3, x4, [x1], #16	/* x3 = r_offset, x4 = r_info */
+	ldr	x5, [x1], #8		/* x5 = r_addend */
+	/*
+	 * We're only expecting R_AARCH64_NONE and R_AARCH64_RELATIVE
+	 * relocations.
+	 */
+	cmp	w4, #R_AARCH64_NONE
+	b.eq	2f			/* Skip R_AARCH64_NONE */
+	cmp	w4, #R_AARCH64_RELATIVE
+	b.ne	.
+	/*
+	 * Update the pointer at r_offset + load_offset with r_addend +
+	 * load_offset.
+	 */
+	add	x5, x5, x0
+	/*
+	 * Note that we're skipping the load_offset for the address to
+	 * update, this is because that memory isn't mapped at that address
+	 * yet.
+	 */
+	str	x5, [x3]
+2:	cmp	x1, x2
+	b.ne	1b
+	ret
+END_FUNC relocate
+#endif
+
 /*
  * void enable_mmu(unsigned long core_pos);
+ *
+ * This function depends on being mapped with in the identity map where
+ * physical address and virtual address is the same. After MMU has been
+ * enabled the instruction pointer will be updated to execute as the new
+ * offset instead. Stack pointers and the return address are updated.
  */
-LOCAL_FUNC enable_mmu , :
+LOCAL_FUNC enable_mmu , : , .identity_map
 	adr	x1, boot_mmu_config
-	load_xregs x1, 0, 2, 5
+	load_xregs x1, 0, 2, 6
 	/*
 	 * x0 = core_pos
 	 * x2 = tcr_el1
 	 * x3 = mair_el1
 	 * x4 = ttbr0_el1_base
 	 * x5 = ttbr0_core_offset
+	 * x6 = load_offset
 	 */
 	msr	tcr_el1, x2
 	msr	mair_el1, x3
@@ -226,6 +311,12 @@ LOCAL_FUNC enable_mmu , :
 	msr	sctlr_el1, x1
 	isb
 
+	/* Update vbar */
+	mrs	x1, vbar_el1
+	add	x1, x1, x6
+	msr	vbar_el1, x1
+	isb
+
 	/* Invalidate instruction cache and branch predictor */
 	ic	iallu
 	isb
@@ -236,6 +327,13 @@ LOCAL_FUNC enable_mmu , :
 	orr	x1, x1, #SCTLR_C
 	msr	sctlr_el1, x1
 	isb
+
+	/* Adjust stack pointers and return address */
+	msr	spsel, #1
+	add	sp, sp, x6
+	msr	spsel, #0
+	add	sp, sp, x6
+	add	x30, x30, x6
 
 	ret
 END_FUNC enable_mmu
@@ -256,14 +354,14 @@ FUNC cpu_on_handler , :
 	set_sctlr_el1
 	isb
 
-	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
-	set_sp
-
 	/* Enable aborts now that we can receive exceptions */
 	msr	daifclr, #DAIFBIT_ABT
 
 	bl	__get_core_pos
 	bl	enable_mmu
+
+	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
+	set_sp
 
 	mov	x0, x19
 	mov	x1, x20
@@ -289,9 +387,9 @@ END_FUNC unhandled_cpu
 	  .endif
 	.endm
 
-	.section .text.reset_vect_table
+	.section .identity_map
 	.align	11
-LOCAL_FUNC reset_vect_table , :
+LOCAL_FUNC reset_vect_table , :, .identity_map
 	/* -----------------------------------------------------
 	 * Current EL with SP0 : 0x0 - 0x180
 	 * -----------------------------------------------------

core/arch/arm/kernel/link.mk
@@ -10,6 +10,9 @@ link-script-dep = $(link-out-dir)/.kern.ld.d
 AWK	 = awk
 
 link-ldflags  = $(LDFLAGS)
+ifeq ($(CFG_CORE_ASLR),y)
+link-ldflags += -pie -z notext
+endif
 link-ldflags += -T $(link-script-pp) -Map=$(link-out-dir)/tee.map
 link-ldflags += --sort-section=alignment
 link-ldflags += --fatal-warnings

commit a87269f5ec1b102b5c7a1a4e20a49daffe4b6591

core: arm32: bugfix booting second cpu with ASLR
Fixes crashing second cpu when booting with ASLR enabled.

Fixes: 170e9084a84f ("core: add support for CFG_CORE_ASLR")
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (b2260)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/generic_entry_a32.S
@@ -77,9 +77,9 @@ END_FUNC plat_cpu_reset_early
 KEEP_PAGER plat_cpu_reset_early
 .weak plat_cpu_reset_early
 
-	.section .text.reset_vect_table
+	.section .identity_map, "ax"
 	.align 5
-LOCAL_FUNC reset_vect_table , :
+LOCAL_FUNC reset_vect_table , : , .identity_map
 	b	.
 	b	.	/* Undef */
 	b	.	/* Syscall */
@@ -264,12 +264,34 @@ KEEP_INIT _start
 		cmp	r0, #CFG_TEE_CORE_NB_CORE
 		/* Unsupported CPU, park it before it breaks something */
 		bge	unhandled_cpu
-		ldr	r1, =stack_tmp_stride
-		ldr	r1, [r1]
+
+		/*
+		 * stack_tmp_stride and stack_tmp_stride_rel are the
+		 * equivalent of:
+		 * extern const u32 stack_tmp_stride;
+		 * u32 stack_tmp_stride_rel = (u32)&stack_tmp_stride -
+		 *			      (u32)&stack_tmp_stride_rel 
+		 *
+		 * To load the value of stack_tmp_stride we do the equivalent
+		 * of:
+		 * *(u32 *)(stack_tmp_stride + (u32)&stack_tmp_stride_rel)
+		 */
+		adr	r3, stack_tmp_stride_rel
+		ldr	r1, [r3]
+		ldr	r1, [r1, r3]
+
+		/* Same pattern as for stack_tmp_stride above */
+		adr	r3, stack_tmp_export_rel
+		ldr	r2, [r3]
+		ldr	r2, [r2, r3]
+
+		/*
+		 * r0 is core pos
+		 * r1 is value of stack_tmp_stride
+		 * r2 is value of stack_tmp_export
+		 */
 		mul	r1, r0, r1
-		ldr	r0, =stack_tmp_export
-		ldr	r0, [r0]
-		add	sp, r1, r0
+		add	sp, r1, r2
 	.endm
 
 	/*
@@ -325,7 +347,7 @@ KEEP_INIT _start
 #define flush_cpu_semaphores
 #endif
 
-LOCAL_FUNC reset_primary , :, .identity_map
+LOCAL_FUNC reset_primary , : , .identity_map
 UNWIND(	.fnstart)
 UNWIND(	.cantunwind)
 
@@ -688,12 +710,20 @@ LOCAL_FUNC enable_mmu , : , .identity_map
 	bx	lr
 END_FUNC enable_mmu
 
+LOCAL_DATA stack_tmp_export_rel , :
+	.word	stack_tmp_export - stack_tmp_export_rel
+END_DATA stack_tmp_export_rel
+
+LOCAL_DATA stack_tmp_stride_rel , :
+	.word	stack_tmp_stride - stack_tmp_stride_rel
+END_DATA stack_tmp_stride_rel
+
 DATA boot_mmu_config , : /* struct core_mmu_config */
 	.skip	CORE_MMU_CONFIG_SIZE
 END_DATA boot_mmu_config
 
 #if defined(CFG_WITH_ARM_TRUSTED_FW)
-FUNC cpu_on_handler , :
+FUNC cpu_on_handler , : , .identity_map
 UNWIND(	.fnstart)
 UNWIND(	.cantunwind)
 	mov	r4, r0
@@ -703,7 +733,7 @@ UNWIND(	.cantunwind)
 	set_sctlr
 	isb
 
-	ldr	r0, =reset_vect_table
+	adr	r0, reset_vect_table
 	write_vbar r0
 
 	mov	r4, lr
@@ -727,12 +757,23 @@ KEEP_PAGER cpu_on_handler
 LOCAL_FUNC reset_secondary , : , .identity_map
 UNWIND(	.fnstart)
 UNWIND(	.cantunwind)
-	ldr	r0, =reset_vect_table
+	adr	r0, reset_vect_table
 	write_vbar r0
 
 	wait_primary
 
 	set_sp
+#ifdef CFG_CORE_ASLR
+	/*
+	 * stack_tmp_export which is used as base when initializing sp has
+	 * been relocated to the new offset. Since MMU isn't enabled on
+	 * this CPU yet we need to restore the corresponding physical
+	 * address.
+	 */
+	adr	r0, boot_mmu_config
+	ldr	r0, [r0, #CORE_MMU_CONFIG_LOAD_OFFSET]
+	sub	sp, sp, r0
+#endif
 
 #if defined (CFG_BOOT_SECONDARY_REQUEST)
 	/* if L1 is not invalidated before, do it here */

core/arch/arm/kernel/thread.c
@@ -104,9 +104,11 @@ DECLARE_STACK(stack_abt, CFG_TEE_CORE_NB_CORE, STACK_ABT_SIZE, static);
 DECLARE_STACK(stack_thread, CFG_NUM_THREADS, STACK_THREAD_SIZE, static);
 #endif
 
-const void *stack_tmp_export = (uint8_t *)stack_tmp + sizeof(stack_tmp[0]) -
-			       (STACK_TMP_OFFS + STACK_CANARY_SIZE / 2);
-const uint32_t stack_tmp_stride = sizeof(stack_tmp[0]);
+const void *stack_tmp_export __section(".identity_map.stack_tmp_export") =
+	(uint8_t *)stack_tmp + sizeof(stack_tmp[0]) -
+	(STACK_TMP_OFFS + STACK_CANARY_SIZE / 2);
+const uint32_t stack_tmp_stride __section(".identity_map.stack_tmp_stride") =
+	sizeof(stack_tmp[0]);
 
 /*
  * These stack setup info are required by secondary boot cores before they