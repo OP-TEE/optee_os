33eb245deedb1e5a73e41470ed923f893a8879aa
Jens Wiklander <jens.wiklander@linaro.org>
2019-11-25 10:17:25 +0100

    core: link.mk: Make sure to link without relro
    Passes -z norelro to linker to make sure that the relro option isn't
    enabled.  With relro enabled all relro sections has to be contiguous
    with each other. This would prevent us from removing .dynamic from the
    binary created with scripts/gen_tee_bin.py. Regardless of the relro
    option OP-TEE itself uses the equivalent of relro when mapping its
    memory.
    
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/kernel/link.mk
+++ core/arch/arm/kernel/link.mk
@@ -11,7 +11,7 @@ AWK	 = awk
 
 link-ldflags  = $(LDFLAGS)
 ifeq ($(CFG_CORE_ASLR),y)
-link-ldflags += -pie -z notext
+link-ldflags += -pie -z notext -z norelro
 endif
 link-ldflags += -T $(link-script-pp) -Map=$(link-out-dir)/tee.map
 link-ldflags += --sort-section=alignment
