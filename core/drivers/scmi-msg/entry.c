ae8c8068098d291e6e55744dbc237ec39fd9840a
Etienne Carriere <etienne.carriere@linaro.org>
2019-12-01 21:57:35 +0100

    drivers/scmi-msg: driver for processing scmi messages
    This change introduces drivers to allow a platform to create a SCMI
    service and register handlers for client request (SCMI agent) on
    system resources. This is the first piece of the drivers: an entry
    function, the SCMI base protocol support and helpers for create
    the response message.
    
    With this change, scmi_process_message() is the entry function to
    process an incoming SCMI message. The function expect the message
    is already copied from shared memory into secure memory. The message
    structure stores message reference and output buffer reference where
    response message shall be stored.
    
    scmi_process_message() calls the SCMI protocol driver according to
    the protocol ID in the message. The SCMI protocol driver will call
    defined platform handlers according to the message content.
    
    This change introduces only the SCMI base protocol as defined in
    SCMI specification v2.0 [1]. Not all the messages defined
    in the specification are supported.
    
    SCMI resource in this implementation are dumped or inspired by the
    SCP-firmware implementation [2] of the SCMI protocol, server side.
    
    Link: [1] http://infocenter.arm.com/help/topic/com.arm.doc.den0056a/DEN0056A_System_Control_and_Management_Interface.pdf
    Link: [2] https://github.com/ARM-software/SCP-firmware.git
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ core/drivers/scmi-msg/entry.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2015-2019, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2019, Linaro Limited
+ */
+#include <speculation_barrier.h>
+#include <drivers/scmi-msg.h>
+#include <drivers/scmi.h>
+#include <trace.h>
+
+#include "base.h"
+#include "common.h"
+
+void scmi_status_response(struct scmi_msg *msg, int32_t status)
+{
+	assert(msg->out && msg->out_size >= sizeof(int32_t));
+
+	memcpy(msg->out, &status, sizeof(int32_t));
+	msg->out_size_out = sizeof(int32_t);
+}
+
+void scmi_write_response(struct scmi_msg *msg, void *payload, size_t size)
+{
+	/*
+	 * Output payload shall be at least the size of the status
+	 * Output buffer shall be at least be the size of the status
+	 * Output paylaod shall fit in output buffer
+	 */
+	assert(payload && size >= sizeof(int32_t) && size <= msg->out_size &&
+	       msg->out && msg->out_size >= sizeof(int32_t));
+
+	memcpy(msg->out, payload, size);
+	msg->out_size_out = size;
+}
+
+void scmi_process_message(struct scmi_msg *msg)
+{
+	scmi_msg_handler_t handler = NULL;
+
+	switch (msg->protocol_id) {
+	case SCMI_PROTOCOL_ID_BASE:
+		handler = scmi_msg_get_base_handler(msg);
+		break;
+	default:
+		break;
+	}
+
+	if (handler) {
+		handler(msg);
+		return;
+	}
+
+	DMSG("Agent %u Protocol %#x Message %#x: not supported",
+	     msg->agent_id, msg->protocol_id, msg->message_id);
+
+	scmi_status_response(msg, SCMI_NOT_SUPPORTED);
+}
