OP-TEE/optee_os

commit 5dd1570ac5b0f6563b1a9c074533a19107b8222d

core: add embedded data region
Until this patch hashes has been supplied as a single blob following the
init part when configured for paging. To facilitate storing additional
data when OP-TEE is initializing a struct boot_embdata is added. This
struct  is populated gen_tee_bin.py and later interpreted by
assembly boot code and init_runtime().

Previous memory allocation for hashes in the linker script is replaced
by this new mechanism.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/kern.ld.S
@@ -162,6 +162,7 @@ SECTIONS
 	ASSERT(__rela_end == __rela_start, "Relocation entries not expected")
 #endif
 
+
 	.got : { *(.got.plt) *(.got) }
 	.dynamic : { *(.dynamic) }
 	.plt : { *(.plt) }
@@ -388,23 +389,10 @@ SECTIONS
 	__pageable_start = __init_start;
 	__pageable_end = __pageable_part_end;
 
-	/*
-	 * Assign a safe spot to store the hashes of the pages before
-	 * heap is initialized.
-	 */
-	__tmp_hashes_start = __init_end;
-	__tmp_hashes_size = ((__pageable_end - __pageable_start) /
-				SMALL_PAGE_SIZE) * 32;
-	__tmp_hashes_end = __tmp_hashes_start + __tmp_hashes_size;
-
-	__init_mem_usage = __tmp_hashes_end - TEE_TEXT_VA_START;
-
 	ASSERT(TEE_LOAD_ADDR >= TEE_RAM_START,
 		"Load address before start of physical memory")
 	ASSERT(TEE_LOAD_ADDR < (TEE_RAM_START + TEE_RAM_PH_SIZE),
 		"Load address after end of physical memory")
-	ASSERT(__tmp_hashes_end < (TEE_RAM_VA_START + TEE_RAM_PH_SIZE),
-		"OP-TEE can't fit init part into available physical memory")
 	ASSERT((TEE_RAM_VA_START + TEE_RAM_PH_SIZE - __init_end) >
 		SMALL_PAGE_SIZE, "Too few free pages to initialize paging")
 
@@ -426,7 +414,6 @@ SECTIONS
 
 #ifndef CFG_WITH_PAGER
 	__init_size = __data_end - TEE_TEXT_VA_START;
-	__init_mem_usage = __end - TEE_TEXT_VA_START;
 #endif
 	/*
 	 * Guard against moving the location counter backwards in the assignment

scripts/gen_tee_bin.py
@@ -35,10 +35,28 @@ small_page_size = 4 * 1024
 elffile_symbols = None
 tee_pageable_bin = None
 tee_pager_bin = None
+tee_embdata_bin = None
 
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def round_up(n, m):
+    if n == 0:
+        return 0
+    else:
+        return (((n - 1) // m) + 1) * m
+
+
+def get_arch_id(elffile):
+    e_machine = elffile.header['e_machine']
+    if e_machine == 'EM_ARM':
+        return 0
+    if e_machine == 'EM_AARCH64':
+        return 1
+    eprint('Unknown e_machine "%s"' % e_machine)
+    sys.exit(1)
 
 
 def get_symbol(elffile, name):
@@ -59,14 +77,13 @@ def get_symbol(elffile, name):
         sys.exit(1)
 
 
-def get_sections(elffile, pad_to, skip_names, dump_names):
+def get_sections(elffile, pad_to, dump_names):
     last_end = 0
     bin_data = bytearray()
 
     for section in elffile.iter_sections():
         if (section['sh_type'] == 'SHT_NOBITS' or
                 not (section['sh_flags'] & SH_FLAGS.SHF_ALLOC) or
-                skip_names.match(section.name) or
                 not dump_names.match(section.name)):
             continue
 
@@ -90,10 +107,8 @@ def get_pageable_bin(elffile):
     global tee_pageable_bin
     if tee_pageable_bin is None:
         pad_to = 0
-        skip_names = re.compile(r'^$')
         dump_names = re.compile(r'^\..*_(pageable|init)$')
-        tee_pageable_bin = get_sections(elffile, pad_to, skip_names,
-                                        dump_names)
+        tee_pageable_bin = get_sections(elffile, pad_to, dump_names)
     return tee_pageable_bin
 
 
@@ -101,11 +116,57 @@ def get_pager_bin(elffile):
     global tee_pager_bin
     if tee_pager_bin is None:
         pad_to = get_symbol(elffile, '__data_end')['st_value']
-        skip_names = re.compile(r'^\..*_(pageable|init)$')
-        dump_names = re.compile(r'')
-        tee_pager_bin = get_sections(elffile, pad_to, skip_names, dump_names)
+        dump_names = re.compile(
+            r'^\.(text|rodata|got|data|ARM\.exidx|ARM\.extab|rel|rela)$')
+        tee_pager_bin = get_sections(elffile, pad_to, dump_names)
 
     return tee_pager_bin
+
+
+def get_hashes_bin(elffile):
+    pageable_bin = get_pageable_bin(elffile)
+    if len(pageable_bin) % small_page_size != 0:
+        eprint("pageable size not a multiple of 4K: "
+               "{}".format(paged_area_size))
+        sys.exit(1)
+
+    data = bytearray()
+    for n in range(0, len(pageable_bin), small_page_size):
+        page = pageable_bin[n:n + small_page_size]
+        data += hashlib.sha256(page).digest()
+
+    return data
+
+
+def get_embdata_bin(elffile):
+    global tee_embdata_bin
+    if tee_embdata_bin is None:
+        hashes_bin = get_hashes_bin(elffile)
+
+        num_entries = 1
+        hash_offs = 2 * 4 + num_entries * (2 * 4)
+        hash_pad = round_up(len(hashes_bin), 8) - len(hashes_bin)
+        total_len = hash_offs + len(hashes_bin) + hash_pad
+
+        tee_embdata_bin = struct.pack('<IIII', total_len, num_entries,
+                                      hash_offs, len(hashes_bin))
+        tee_embdata_bin += hashes_bin + bytearray(hash_pad)
+
+    # The embedded data region is designed to be easy to extend when
+    # needed, it's formatted as:
+    # +--------------------------------------------------------+
+    # | uint32_t: Length of entire area including this field   |
+    # +--------------------------------------------------------+
+    # | uint32_t: Number of entries "1"                        |
+    # +--------------------------------------------------------+
+    # | uint32_t: Offset of hashes from beginning of table     |
+    # +--------------------------------------------------------+
+    # | uint32_t: Length of hashes                             |
+    # +--------------------------------------------------------+
+    # | Data of hashes + eventual padding                      |
+    # +--------------------------------------------------------+
+
+    return tee_embdata_bin
 
 
 def output_pager_bin(elffile, outf):
@@ -114,16 +175,6 @@ def output_pager_bin(elffile, outf):
 
 def output_pageable_bin(elffile, outf):
     outf.write(get_pageable_bin(elffile))
-
-
-def get_arch_id(elffile):
-    e_machine = elffile.header['e_machine']
-    if e_machine == 'EM_ARM':
-        return 0
-    if e_machine == 'EM_AARCH64':
-        return 1
-    eprint('Unknown e_machine "%s"' % e_machine)
-    sys.exit(1)
 
 
 def get_init_load_addr(elffile):
@@ -137,22 +188,19 @@ def output_header_v1(elffile, outf):
     arch_id = get_arch_id(elffile)
     pager_bin = get_pager_bin(elffile)
     pageable_bin = get_pageable_bin(elffile)
-    init_mem_usage = get_symbol(elffile, '__init_mem_usage')['st_value']
+    embdata_bin = get_embdata_bin(elffile)
     init_load_addr = get_init_load_addr(elffile)
     init_bin_size = get_symbol(elffile, '__init_size')['st_value']
     pager_bin_size = len(pager_bin)
     paged_area_size = len(pageable_bin)
-    hash_size = (paged_area_size // small_page_size *
-                 hashlib.sha256().digest_size)
+
+    init_mem_usage = (get_symbol(elffile, '__init_end')['st_value'] -
+                      get_symbol(elffile, '__text_start')['st_value'] +
+                      len(embdata_bin))
 
     init_size = (pager_bin_size + min(init_bin_size, paged_area_size) +
-                 hash_size)
+                 len(embdata_bin))
     paged_size = paged_area_size - min(init_bin_size, paged_area_size)
-
-    if paged_area_size % small_page_size != 0:
-        eprint("pageable size not a multiple of 4K: "
-               "{}".format(paged_area_size))
-        sys.exit(1)
 
     magic = 0x4554504f  # 'OPTE'
     version = 1
@@ -162,9 +210,7 @@ def output_header_v1(elffile, outf):
                            init_mem_usage, paged_size))
     outf.write(pager_bin)
     outf.write(pageable_bin[:init_bin_size])
-    for n in range(0, len(pageable_bin), small_page_size):
-        page = pageable_bin[n:n + small_page_size]
-        outf.write(hashlib.sha256(page).digest())
+    outf.write(embdata_bin)
     outf.write(pageable_bin[init_bin_size:])
 
 
@@ -174,16 +220,10 @@ def output_header_v2(elffile, outf):
     init_bin_size = get_symbol(elffile, '__init_size')['st_value']
     pager_bin_size = len(get_pager_bin(elffile))
     paged_area_size = len(get_pageable_bin(elffile))
-    hash_size = (paged_area_size // small_page_size *
-                 hashlib.sha256().digest_size)
-
-    if paged_area_size % small_page_size != 0:
-        eprint("pageable size not a multiple of 4K: "
-               "{}".format(paged_area_size))
-        sys.exit(1)
+    embdata_bin_size = len(get_embdata_bin(elffile))
 
     init_size = (pager_bin_size + min(init_bin_size, paged_area_size) +
-                 hash_size)
+                 embdata_bin_size)
     paged_size = paged_area_size - min(init_bin_size, paged_area_size)
 
     magic = 0x4554504f  # 'OPTE'
@@ -200,18 +240,13 @@ def output_header_v2(elffile, outf):
 
 def output_pager_v2(elffile, outf):
     init_bin_size = get_symbol(elffile, '__init_size')['st_value']
+    pager_bin = get_pager_bin(elffile)
     pageable_bin = get_pageable_bin(elffile)
+    embdata_bin = get_embdata_bin(elffile)
 
-    if len(pageable_bin) % small_page_size != 0:
-        eprint("pageable size not a multiple of 4K: "
-               "{}".format(paged_area_size))
-        sys.exit(1)
-
-    outf.write(get_pager_bin(elffile))
+    outf.write(pager_bin)
     outf.write(pageable_bin[:init_bin_size])
-    for n in range(0, len(pageable_bin), small_page_size):
-        page = pageable_bin[n:n + small_page_size]
-        outf.write(hashlib.sha256(page).digest())
+    outf.write(embdata_bin)
 
 
 def output_pageable_v2(elffile, outf):

core/arch/arm/include/kernel/generic_boot.h
@@ -8,6 +8,33 @@
 #include <initcall.h>
 #include <types_ext.h>
 
+/*
+ * struct boot_embdata - Embedded boot data
+ * @total_len: Total length of the embedded boot data
+ * @num_blobs: Number of blobs in the embedded boot data, always 1 even if
+ *	       one blob is empty
+ * @hashes_offset: Offset of hashes from start of this struct
+ * @hashes_len: Length of hashes
+ *
+ * This struct is initialized by scripts/gen_tee_bin.py and must be kept
+ * in sync with that script. The struct and the following data is loaded
+ * at different addresses at boot depending on CFG_WITH_PAGER.
+ *
+ * If configured with CFG_WITH_PAGER=y the struct with data is following
+ * init part, this is together with the init part moved by the primary CPU
+ * so it ends up at __init_end. Whatever need to be saved for later need to
+ * be copied to a safe location in init_runtime().
+ *
+ * If configured with CFG_WITH_PAGER=n following the struct with data is
+ * __data_end, this is moved by the primary CPU so it ends up at __end.
+ */
+struct boot_embdata {
+	uint32_t total_len;
+	uint32_t num_blobs;
+	uint32_t hashes_offset;
+	uint32_t hashes_len;
+};
+
 extern uint8_t embedded_secure_dtb[];
 extern const struct core_mmu_config boot_mmu_config;
 

core/arch/arm/include/kernel/linker.h
@@ -68,9 +68,7 @@ extern const uint8_t __nozi_end[];
 extern const uint8_t __nozi_stack_start[];
 extern const uint8_t __nozi_stack_end[];
 extern const uint8_t __init_start[];
-extern const uint8_t __init_size[];
-extern const uint8_t __tmp_hashes_start[];
-extern const uint8_t __tmp_hashes_size[];
+extern const uint8_t __init_end[];
 
 extern uint8_t __heap1_start[];
 extern const uint8_t __heap1_end[];

core/arch/arm/kernel/asm-defines.c
@@ -4,6 +4,7 @@
  */
 
 #include <gen-asm-defines.h>
+#include <kernel/generic_boot.h>
 #include <kernel/thread.h>
 #include <sm/pm.h>
 #include <sm/sm.h>
@@ -96,4 +97,10 @@ DEFINES
 	DEFINE(CORE_MMU_CONFIG_SIZE, sizeof(struct core_mmu_config));
 	DEFINE(CORE_MMU_CONFIG_LOAD_OFFSET,
 	       offsetof(struct core_mmu_config, load_offset));
+
+	/* struct boot_embdata */
+	DEFINE(BOOT_EMBDATA_HASHES_OFFSET,
+	       offsetof(struct boot_embdata, hashes_offset));
+	DEFINE(BOOT_EMBDATA_HASHES_LEN,
+	       offsetof(struct boot_embdata, hashes_len));
 }

core/arch/arm/kernel/generic_boot.c
@@ -331,20 +331,27 @@ static void init_vcore(tee_mm_pool_t *mm_vcore)
 static void init_runtime(unsigned long pageable_part)
 {
 	size_t n;
-	size_t init_size = (size_t)__init_size;
+	size_t init_size = (size_t)(__init_end - __init_start);
 	size_t pageable_start = (size_t)__pageable_start;
 	size_t pageable_end = (size_t)__pageable_end;
 	size_t pageable_size = pageable_end - pageable_start;
 	size_t tzsram_end = TZSRAM_BASE + TZSRAM_SIZE;
 	size_t hash_size = (pageable_size / SMALL_PAGE_SIZE) *
 			   TEE_SHA256_HASH_SIZE;
+	const struct boot_embdata *embdata = (const void *)__init_end;
+	const void *tmp_hashes = NULL;
 	tee_mm_entry_t *mm = NULL;
 	struct fobj *fobj = NULL;
 	uint8_t *paged_store = NULL;
 	uint8_t *hashes = NULL;
 
 	assert(pageable_size % SMALL_PAGE_SIZE == 0);
-	assert(hash_size == (size_t)__tmp_hashes_size);
+	assert(embdata->total_len >= embdata->hashes_offset +
+				     embdata->hashes_len);
+	assert(hash_size == embdata->hashes_len);
+
+	tmp_hashes = __init_end + embdata->hashes_offset;
+
 
 	/*
 	 * This needs to be initialized early to support address lookup
@@ -361,7 +368,7 @@ static void init_runtime(unsigned long pageable_part)
 	IMSG_RAW("\n");
 	IMSG("Pager is enabled. Hashes: %zu bytes", hash_size);
 	assert(hashes);
-	asan_memcpy_unchecked(hashes, __tmp_hashes_start, hash_size);
+	asan_memcpy_unchecked(hashes, tmp_hashes, hash_size);
 
 	/*
 	 * Need tee_mm_sec_ddr initialized to be able to allocate secure

core/arch/arm/kernel/generic_entry_a32.S
@@ -63,7 +63,7 @@ END_FUNC __assert_flat_mapped_range
 
 	/* panic if mmu is enable and vaddr != paddr (scratch lr) */
 	.macro assert_flat_mapped_range va, line
-		ldr	r0, =(\va)
+		ldr	r0, \va
 		ldr	r1, =\line
 		bl	__assert_flat_mapped_range
 	.endm
@@ -283,12 +283,12 @@ KEEP_INIT _start
 #if defined(CFG_PL310) && !defined(CFG_PL310_SIP_PROTOCOL)
 		assert_flat_mapped_range (\vbase), (\line)
 		bl	pl310_base
-		ldr	r1, =(\vbase)
-		ldr	r2, =(\vend)
+		ldr	r1, \vbase
+		ldr	r2, \vend
 		bl	arm_cl2_invbypa
 #endif
-		ldr	r0, =(\vbase)
-		ldr	r1, =(\vend)
+		ldr	r0, \vbase
+		ldr	r1, \vend
 		sub	r1, r1, r0
 		bl	dcache_inv_range
 	.endm
@@ -296,31 +296,30 @@ KEEP_INIT _start
 	.macro __flush_cache_vrange vbase, vend, line
 #if defined(CFG_PL310) && !defined(CFG_PL310_SIP_PROTOCOL)
 		assert_flat_mapped_range (\vbase), (\line)
-		ldr	r0, =(\vbase)
-		ldr	r1, =(\vend)
+		ldr	r0, \vbase
+		ldr	r1, \vend
 		sub	r1, r1, r0
 		bl	dcache_clean_range
 		bl	pl310_base
-		ldr	r1, =(\vbase)
-		ldr	r2, =(\vend)
+		ldr	r1, \vbase
+		ldr	r2, \vend
 		bl	arm_cl2_cleaninvbypa
 #endif
-		ldr	r0, =(\vbase)
-		ldr	r1, =(\vend)
+		ldr	r0, \vbase
+		ldr	r1, \vend
 		sub	r1, r1, r0
 		bl	dcache_cleaninv_range
 	.endm
 
 #define inval_cache_vrange(vbase, vend) \
-		__inval_cache_vrange (vbase), ((vend) - 1), __LINE__
+		__inval_cache_vrange vbase, vend, __LINE__
 
 #define flush_cache_vrange(vbase, vend) \
-		__flush_cache_vrange (vbase), ((vend) - 1), __LINE__
+		__flush_cache_vrange vbase, vend, __LINE__
 
 #ifdef CFG_BOOT_SYNC_CPU
 #define flush_cpu_semaphores \
-		flush_cache_vrange(sem_cpu_sync, \
-				   (sem_cpu_sync + (CFG_TEE_CORE_NB_CORE << 2)))
+		flush_cache_vrange(sem_cpu_sync_start, sem_cpu_sync_end)
 #else
 #define flush_cpu_semaphores
 #endif
@@ -339,17 +338,42 @@ UNWIND(	.cantunwind)
 	 * The binary is built as:
 	 * [Pager code, rodata and data] : In correct location
 	 * [Init code and rodata] : Should be copied to __init_start
-	 * [Hashes] : Should be saved before initializing pager
-	 *
+	 * [struct boot_embdata + data] : Should be saved before
+	 * initializing pager, first uint32_t tells the length of the data
 	 */
 	ldr	r0, =__init_start	/* dst */
 	ldr	r1, =__data_end 	/* src */
-	ldr	r2, =__tmp_hashes_end	/* dst limit */
+	ldr	r2, =__init_end
+	sub	r2, r2, r0		/* init len */
+	ldr	r12, [r1, r2]		/* length of hashes etc */
+	add	r2, r2, r12		/* length of init and hashes etc */
 	/* Copy backwards (as memmove) in case we're overlapping */
-	sub	r2, r2, r0		/* len */
+	add	r0, r0, r2		/* __init_start + len */
+	add	r1, r1, r2		/* __data_end + len */
+	str	r1, cached_mem_end
+	ldr	r2, =__init_start
+copy_init:
+	ldmdb	r1!, {r3, r8-r12}
+	stmdb	r0!, {r3, r8-r12}
+	cmp	r0, r2
+	bgt	copy_init
+#else
+	/*
+	 * The binary is built as:
+	 * [Core, rodata and data] : In correct location
+	 * [struct boot_embdata + data] : Should be moved to __end, first
+	 * uint32_t tells the length of the struct + data
+	 */
+	ldr	r0, =__end		/* dst */
+	ldr	r1, =__data_end		/* src */
+	ldr	r2, [r1]		/* struct boot_embdata::total_len */
+	/* Copy backwards (as memmove) in case we're overlapping */
 	add	r0, r0, r2
 	add	r1, r1, r2
-	ldr	r2, =__init_start
+	adr	r3, cached_mem_end
+	str	r1, [r3]
+	ldr	r2, =__end
+
 copy_init:
 	ldmdb	r1!, {r3, r8-r12}
 	stmdb	r0!, {r3, r8-r12}
@@ -429,11 +453,7 @@ shadow_stack_access_ok:
 	 * invalidate memory not used by OP-TEE since we may invalidate
 	 * entries used by for instance ARM Trusted Firmware.
 	 */
-#ifdef CFG_WITH_PAGER
-	inval_cache_vrange(__text_start, __tmp_hashes_end)
-#else
-	inval_cache_vrange(__text_start, __end)
-#endif
+	inval_cache_vrange(cached_mem_start, cached_mem_end)
 
 #if defined(CFG_PL310) && !defined(CFG_PL310_SIP_PROTOCOL)
 	/* Enable PL310 if not yet enabled */
@@ -484,11 +504,7 @@ shadow_stack_access_ok:
 	 * they have turned on their D-cache, clean and invalidate the
 	 * D-cache before exiting to normal world.
 	 */
-#ifdef CFG_WITH_PAGER
-	flush_cache_vrange(__text_start, __init_end)
-#else
-	flush_cache_vrange(__text_start, __end)
-#endif
+	flush_cache_vrange(cached_mem_start, cached_mem_end)
 
 	/* release secondary boot cores and sync with them */
 	cpu_is_ready
@@ -531,6 +547,24 @@ shadow_stack_access_ok:
 	b	.	/* SMC should not return */
 UNWIND(	.fnend)
 END_FUNC reset_primary
+
+#ifdef CFG_BOOT_SYNC_CPU
+LOCAL_DATA sem_cpu_sync_start , :
+	.word	sem_cpu_sync
+END_DATA sem_cpu_sync_start
+
+LOCAL_DATA sem_cpu_sync_end , :
+	.word	sem_cpu_sync + (CFG_TEE_CORE_NB_CORE << 2)
+END_DATA sem_cpu_sync_end
+#endif
+
+LOCAL_DATA cached_mem_start , :
+	.word	__text_start
+END_DATA cached_mem_start
+
+LOCAL_DATA cached_mem_end , :
+	.skip	4
+END_DATA cached_mem_end
 
 LOCAL_FUNC unhandled_cpu , :
 UNWIND(	.fnstart)

core/arch/arm/kernel/generic_entry_a64.S
@@ -77,17 +77,43 @@ FUNC _start , :
 	 * The binary is built as:
 	 * [Pager code, rodata and data] : In correct location
 	 * [Init code and rodata] : Should be copied to __init_start
-	 * [Hashes] : Should be saved before initializing pager
-	 *
+	 * [struct boot_embdata + data] : Should be saved before
+	 * initializing pager, first uint32_t tells the length of the data
 	 */
 	adr	x0, __init_start	/* dst */
 	adr	x1, __data_end		/* src */
-	adr	x2, __tmp_hashes_end	/* dst limit */
+	adr	x2, __init_end
+	sub	x2, x2, x0		/* init len */
+	ldr	w4, [x1, x2]		/* length of hashes etc */
+	add	x2, x2, x4		/* length of init and hashes etc */
 	/* Copy backwards (as memmove) in case we're overlapping */
-	sub	x2, x2, x0		/* len */
-	add	x0, x0, x2		/* __init_start + len = __init_end */
+	add	x0, x0, x2		/* __init_start + len */
 	add	x1, x1, x2		/* __data_end + len */
+	adr	x3, cached_mem_end
+	str	x1, [x3]
 	adr	x2, __init_start
+copy_init:
+	ldp	x3, x4, [x1, #-16]!
+	stp	x3, x4, [x0, #-16]!
+	cmp	x0, x2
+	b.gt	copy_init
+#else
+	/*
+	 * The binary is built as:
+	 * [Core, rodata and data] : In correct location
+	 * [struct boot_embdata + data] : Should be moved to __end, first
+	 * uint32_t tells the length of the data.
+	 */
+	adr	x0, __end		/* dst */
+	adr	x1, __data_end		/* src */
+	ldr	w2, [x1]		/* struct boot_embdata::total_len */
+	/* Copy backwards (as memmove) in case we're overlapping */
+	add	x0, x0, x2
+	add	x1, x1, x2
+	adr	x3, cached_mem_end
+	str	x1, [x3]
+	adr	x2, __end
+
 copy_init:
 	ldp	x3, x4, [x1, #-16]!
 	stp	x3, x4, [x0, #-16]!
@@ -141,13 +167,7 @@ clear_nex_bss:
 	msr	daifclr, #DAIFBIT_ABT
 
 	adr_l	x0, __text_start
-#ifdef CFG_WITH_PAGER
-	adrp	x1, __tmp_hashes_end
-	add	x1, x1, :lo12:__tmp_hashes_end
-#else
-	adrp	x1, __end
-	add	x1, x1, :lo12:__end
-#endif
+	ldr	x1, cached_mem_end
 	sub	x1, x1, x0
 	bl	dcache_inv_range
 
@@ -197,13 +217,7 @@ clear_nex_bss:
 	 */
 	mov	x19, x0
 	adr_l	x0, __text_start
-#ifdef CFG_WITH_PAGER
-	adrp	x1, __tmp_hashes_end
-	add	x1, x1, :lo12:__tmp_hashes_end
-#else
-	adrp	x1, __end
-	add	x1, x1, :lo12:__end
-#endif
+	ldr	x1, cached_mem_end
 	sub	x1, x1, x0
 	bl	dcache_cleaninv_range
 
@@ -229,6 +243,11 @@ clear_nex_bss:
 	b	.	/* SMC should not return */
 END_FUNC _start
 KEEP_INIT _start
+
+	.balign	8
+LOCAL_DATA cached_mem_end , :
+	.skip	8
+END_DATA cached_mem_end
 
 #ifdef CFG_CORE_ASLR
 LOCAL_FUNC relocate , :

commit fa80991e905eab7f43b77431532c9ea50f9cbca4

core: fix generation of tee.bin
Prior to this patch generation of tee.bin (CFG_WITH_PAGER=n) fails with:
  GEN     out/core/tee.bin
Cannot find symbol __init_end
core/arch/arm/kernel/link.mk:183: recipe for target 'out/core/tee.bin' failed

Introduce a special __get_tee_init_end to fix this and also avoid
confusion with __init_end used in the code for the pager case.

Fixes: 5dd1570ac5b0 ("core: add embedded data region")
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/kern.ld.S
@@ -334,6 +334,7 @@ SECTIONS
 	__rodata_init_end = .;
 
 	__init_end = ROUNDUP(__rodata_init_end, SMALL_PAGE_SIZE);
+	__get_tee_init_end = __init_end;
 	__init_size = __init_end - __init_start;
 
 	/* vcore flat map stops here. No need to page align, rodata follows. */
@@ -406,6 +407,7 @@ SECTIONS
 
 #ifndef CFG_WITH_PAGER
 	__flatmap_unpg_rw_size = _end_of_ram - __flatmap_unpg_rw_start;
+	__get_tee_init_end = .;
 #endif
 
 	/*

scripts/gen_tee_bin.py
@@ -270,7 +270,7 @@ def output_header_v1(elffile, outf):
     pager_bin_size = len(pager_bin)
     paged_area_size = len(pageable_bin)
 
-    init_mem_usage = (get_symbol(elffile, '__init_end')['st_value'] -
+    init_mem_usage = (get_symbol(elffile, '__get_tee_init_end')['st_value'] -
                       get_symbol(elffile, '__text_start')['st_value'] +
                       len(embdata_bin))
 