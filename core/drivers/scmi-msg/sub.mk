e4688a06e16a41c4e3fc22c9ee2fc4a8ad06f019
Etienne Carriere <etienne.carriere@linaro.org>
2019-12-17 15:34:49 +0100

    [review] drivers/scmi-msg: support for clock protocol
    32bit and 64bit implementations for platform_weak.S
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/scmi-msg/sub.mk
+++ core/drivers/scmi-msg/sub.mk
@@ -1,6 +1,7 @@
 srcs-y += base.c
 srcs-$(CFG_SCMI_MSG_CLOCK) += clock.c
 srcs-y += entry.c
-srcs-y += platform_weaks.S
+srcs-$(CFG_ARM32_core) += platform_weaks_a32.S
+srcs-$(CFG_ARM64_core) += platform_weaks_a64.S
 srcs-$(CFG_SCMI_MSG_RESET_DOMAIN) += reset_domain.c
 srcs-$(CFG_SCMI_MSG_SMT) += smt.c
