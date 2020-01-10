4f562c571b01ea38974018164ecf02385503ffd5
Fangsuo Wu <fangsuowu@asrmicro.com>
2020-01-10 10:25:30 +0800

    core: fix typo error in nsec ddr discovery
    In core_mmu_set_discovered_nsec_ddr(), core_mmap_is_end_of_table
    always returns false and the loop body cannot be executed, which
    is unexpected.
    
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Fangsuo Wu <fangsuowu@asrmicro.com>

--- core/arch/arm/mm/core_mmu.c
+++ core/arch/arm/mm/core_mmu.c
@@ -377,7 +377,7 @@ void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 		carve_out_phys_mem(&m, &num_elems, pmem->addr, pmem->size);
 #endif
 
-	for (map = static_memory_map; core_mmap_is_end_of_table(map); map++) {
+	for (map = static_memory_map; !core_mmap_is_end_of_table(map); map++) {
 		if (map->type == MEM_AREA_NSEC_SHM)
 			carve_out_phys_mem(&m, &num_elems, map->pa, map->size);
 		else if (map->type != MEM_AREA_EXT_DT)
