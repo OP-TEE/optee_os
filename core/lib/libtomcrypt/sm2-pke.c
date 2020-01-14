f34f6512ed9776a9b58e71a930663e31f2b735e6
Jerome Forissier <jerome@forissier.org>
2020-01-14 11:35:18 +0100

    core: ltc: SM2 PKE: export sm2_kdf()
    The Key Derivation Function used by the SM2 Public Key Encryption
    algorithm is also used by the Key Exchange Protocol. Move it to its
    file in order to be able to re-use it.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/sm2-pke.c
+++ core/lib/libtomcrypt/sm2-pke.c
@@ -89,62 +89,6 @@ static TEE_Result sm2_bytes_to_point(ecc_point *p, const ltc_ecc_dp *dp,
 	return TEE_ERROR_GENERIC;
 }
 
-/*
- * GM/T 0003.1‒2012 Part 4 Sections 5.4.2 and 5.4.3
- * Key derivation function based on the SM3 hash function
- */
-static TEE_Result sm2_kdf(const uint8_t *Z, size_t Z_len, uint8_t *t,
-			  size_t tlen)
-{
-	TEE_Result res = TEE_SUCCESS;
-	size_t remain = tlen;
-	uint32_t count = 1;
-	uint32_t be_count = 0;
-	void *ctx = NULL;
-	uint8_t *out = t;
-
-	res = crypto_hash_alloc_ctx(&ctx, TEE_ALG_SM3);
-	if (res)
-		return res;
-
-	while (remain) {
-		uint8_t tmp[TEE_SM3_HASH_SIZE] = { };
-		uint8_t *buf = NULL;
-
-		if (remain >= TEE_SM3_HASH_SIZE)
-			buf = out;
-		else
-			buf = tmp;
-
-		put_be32(&be_count, count);
-		res = crypto_hash_init(ctx);
-		if (res)
-			goto out;
-		res = crypto_hash_update(ctx, Z, Z_len);
-		if (res)
-			goto out;
-		res = crypto_hash_update(ctx, (const uint8_t *)&be_count,
-					 sizeof(be_count));
-		if (res)
-			goto out;
-		res = crypto_hash_final(ctx, buf, TEE_SM3_HASH_SIZE);
-		if (res)
-			goto out;
-
-		if (remain < TEE_SM3_HASH_SIZE) {
-			memcpy(out, tmp, remain);
-			break;
-		}
-
-		out += TEE_SM3_HASH_SIZE;
-		remain -= TEE_SM3_HASH_SIZE;
-		count++;
-	}
-out:
-	crypto_hash_free_ctx(ctx);
-	return res;
-}
-
 static bool is_zero(const uint8_t *buf, size_t size)
 {
 	uint8_t v = 0;
