OP-TEE/optee_os

commit 520860f658be37b08f173b12db4594cc16351871

core: generic_entry: add enable_mmu()
Adds the assembly function enable_mmu() which as the name suggests enables
MMU. The function writes configuration which has previously been prepared
by core_init_mmu_regs().

The now obsolete assembly functions cpu_mmu_enable(),
cpu_mmu_enable_icache() and cpu_mmu_enable_dcache() are removed since
they are fully covered by enable_mmu().

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -1088,7 +1088,7 @@ static void check_mem_map(struct tee_mmap_region *map)
  *
  * If an error happened: core_init_mmu_map is expected to panic.
  */
-void core_init_mmu_map(void)
+void core_init_mmu_map(struct core_mmu_config *cfg)
 {
 	check_sec_nsec_mem_config();
 
@@ -1098,6 +1098,7 @@ void core_init_mmu_map(void)
 	check_mem_map(static_memory_map);
 	core_init_mmu(static_memory_map);
 	dump_xlat_table(0x0, 1);
+	core_init_mmu_regs(cfg);
 }
 
 bool core_mmu_mattr_is_ok(uint32_t mattr)

core/arch/arm/mm/core_mmu_lpae.c
@@ -529,30 +529,26 @@ bool core_mmu_place_tee_ram_at_top(paddr_t paddr)
 }
 
 #ifdef ARM32
-void core_init_mmu_regs(void)
+void core_init_mmu_regs(struct core_mmu_config *cfg)
 {
-	uint32_t ttbcr = TTBCR_EAE;
-	uint32_t mair;
-	paddr_t ttbr0;
+	uint32_t ttbcr = 0;
+	uint32_t mair = 0;
 
-	ttbr0 = virt_to_phys(l1_xlation_table[0][get_core_pos()]);
+	cfg->ttbr0_base = virt_to_phys(l1_xlation_table[0][0]);
+	cfg->ttbr0_core_offset = sizeof(l1_xlation_table[0][0]);
 
 	mair  = MAIR_ATTR_SET(ATTR_DEVICE, ATTR_DEVICE_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR, ATTR_IWBWA_OWBWA_NTR_INDEX);
-	write_mair0(mair);
+	cfg->mair0 = mair;
 
+	ttbcr = TTBCR_EAE;
 	ttbcr |= TTBCR_XRGNX_WBWA << TTBCR_IRGN0_SHIFT;
 	ttbcr |= TTBCR_XRGNX_WBWA << TTBCR_ORGN0_SHIFT;
 	ttbcr |= TTBCR_SHX_ISH << TTBCR_SH0_SHIFT;
-
-	/* Disable the use of TTBR1 */
-	ttbcr |= TTBCR_EPD1;
+	ttbcr |= TTBCR_EPD1;	/* Disable the use of TTBR1 */
 
 	/* TTBCR.A1 = 0 => ASID is stored in TTBR0 */
-
-	write_ttbcr(ttbcr);
-	write_ttbr0_64bit(ttbr0);
-	write_ttbr1_64bit(0);
+	cfg->ttbcr = ttbcr;
 }
 #endif /*ARM32*/
 
@@ -586,18 +582,18 @@ static unsigned int calc_physical_addr_size_bits(uint64_t max_addr)
 	return TCR_PS_BITS_4GB;
 }
 
-void core_init_mmu_regs(void)
+void core_init_mmu_regs(struct core_mmu_config *cfg)
 {
-	uint64_t mair;
-	uint64_t tcr;
-	paddr_t ttbr0;
 	uint64_t ips = calc_physical_addr_size_bits(max_pa);
+	uint64_t mair = 0;
+	uint64_t tcr = 0;
 
-	ttbr0 = virt_to_phys(l1_xlation_table[0][get_core_pos()]);
+	cfg->ttbr0_el1_base = virt_to_phys(l1_xlation_table[0][0]);
+	cfg->ttbr0_core_offset = sizeof(l1_xlation_table[0][0]);
 
 	mair  = MAIR_ATTR_SET(ATTR_DEVICE, ATTR_DEVICE_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR, ATTR_IWBWA_OWBWA_NTR_INDEX);
-	write_mair_el1(mair);
+	cfg->mair_el1 = mair;
 
 	tcr = TCR_RES1;
 	tcr |= TCR_XRGNX_WBWA << TCR_IRGN0_SHIFT;
@@ -613,10 +609,7 @@ void core_init_mmu_regs(void)
 	 * TCR.A1 = 0 => ASID is stored in TTBR0
 	 * TCR.AS = 0 => Same ASID size as in Aarch32/ARMv7
 	 */
-
-	write_tcr_el1(tcr);
-	write_ttbr0_el1(ttbr0);
-	write_ttbr1_el1(0);
+	cfg->tcr_el1 = tcr;
 }
 
 void core_mmu_set_max_pa(paddr_t pa)
@@ -641,8 +634,6 @@ void core_mmu_set_max_pa(paddr_t pa)
 	tlbi_all();
 }
 #endif /*ARM64*/
-
-KEEP_PAGER(core_init_mmu_regs);
 
 void core_mmu_set_info_table(struct core_mmu_table_info *tbl_info,
 		unsigned level, vaddr_t va_base, void *table)

core/arch/arm/include/mm/core_mmu.h
@@ -267,8 +267,31 @@ extern unsigned long default_nsec_shm_paddr;
 extern unsigned long default_nsec_shm_size;
 #endif
 
-void core_init_mmu_map(void);
-void core_init_mmu_regs(void);
+/*
+ * Assembly code in enable_mmu() depends on the layout of this struct.
+ */
+struct core_mmu_config {
+#if defined(ARM64)
+	uint64_t tcr_el1;
+	uint64_t mair_el1;
+	uint64_t ttbr0_el1_base;
+	uint64_t ttbr0_core_offset;
+#elif defined(CFG_WITH_LPAE)
+	uint32_t ttbcr;
+	uint32_t mair0;
+	uint32_t ttbr0_base;
+	uint32_t ttbr0_core_offset;
+#else
+	uint32_t prrr;
+	uint32_t nmrr;
+	uint32_t dacr;
+	uint32_t ttbcr;
+	uint32_t ttbr;
+#endif
+};
+
+void core_init_mmu_map(struct core_mmu_config *cfg);
+void core_init_mmu_regs(struct core_mmu_config *cfg);
 
 bool core_mmu_place_tee_ram_at_top(paddr_t paddr);
 

core/arch/arm/kernel/asm-defines.c
@@ -92,4 +92,6 @@ DEFINES
 	DEFINE(THREAD_CORE_LOCAL_ABT_STACK_VA_END,
 		offsetof(struct thread_core_local, abt_stack_va_end));
 
+	/* struct core_mmu_config */
+	DEFINE(CORE_MMU_CONFIG_SIZE, sizeof(struct core_mmu_config));
 }

core/arch/arm/kernel/generic_entry_a32.S
@@ -6,6 +6,7 @@
 #include <arm32_macros.S>
 #include <arm.h>
 #include <asm.S>
+#include <generated/asm-defines.h>
 #include <keep.h>
 #include <kernel/asan.h>
 #include <kernel/cache_helpers.h>
@@ -162,26 +163,6 @@ END_FUNC reset_vect_table
 		orr	r0, r0, #SCTLR_RR
 #endif
 		write_sctlr r0
-	.endm
-
-	/*
-	 * enable_branch_prediction : manually enable branch prediction
-	 *
-	 * This macro targets only ARMv7 architecture and hence conditionned
-	 * by configuration directive CFG_ENABLE_SCTLR_Z. For recent
-	 * architectures, the program flow prediction is automatically enable
-	 * upon MMU enablement.
-	 */
-	.macro enable_branch_prediction
-#if defined(CFG_ENABLE_SCTLR_Z)
-		read_sctlr r0
-		/* Some ARMv7 architectures need btac flush with post synchro */
-		write_bpiall
-		dsb
-		isb
-		orr	r0, r0, #SCTLR_Z
-		write_sctlr r0
-#endif
 	.endm
 
 	/*
@@ -459,13 +440,13 @@ shadow_stack_access_ok:
 	bl	arm_cl2_enable
 #endif
 
+	ldr	r0, =boot_mmu_config
 	bl	core_init_mmu_map
-	bl	core_init_mmu_regs
-	bl	cpu_mmu_enable
-	bl	cpu_mmu_enable_icache
-	bl	cpu_mmu_enable_dcache
 
-	enable_branch_prediction
+	bl	__get_core_pos
+	bl	enable_mmu
+	/* Virtual addresses may have changed, reinitialize console */
+	bl	console_init
 
 	mov	r0, r4		/* pageable part address */
 	mov	r1, r5		/* ns-entry address */
@@ -531,6 +512,109 @@ UNWIND(	.fnstart)
 UNWIND(	.fnend)
 END_FUNC unhandled_cpu
 
+/*
+ * void enable_mmu(unsigned long core_pos);
+ */
+LOCAL_FUNC enable_mmu , :
+	/* r0 = core pos */
+	adr	r1, boot_mmu_config
+
+#ifdef CFG_WITH_LPAE
+	ldm	r1!, {r2, r3}
+	/*
+	 * r2 = ttbcr
+	 * r3 = mair0
+	 */
+	write_ttbcr r2
+	write_mair0 r3
+
+	ldm	r1!, {r2, r3}
+	/*
+	 * r2 = ttbr0_base
+	 * r3 = ttbr0_core_offset
+	 */
+
+	/*
+	 * ttbr0_el1 = ttbr0_base + ttbr0_core_offset * core_pos
+	 */
+	mla	r1, r0, r3, r2
+	write_ttbr0 r1
+	mov	r1, #0
+	write_ttbr1 r1
+#else
+	ldm	r1!, {r2, r3}
+	/*
+	 * r2 = prrr
+	 * r3 = nmrr
+	 */
+	write_prrr r2
+	write_nmrr r3
+
+	ldm	r1!, {r2, r3}
+	/*
+	 * r2 = dacr
+	 * r3 = ttbcr
+	 */
+	write_dacr r2
+	write_ttbcr r3
+
+	ldm	r1!, {r2}
+	/* r2 = ttbr */
+	write_ttbr0 r2
+	write_ttbr1 r2
+
+	mov	r1, #0
+	write_contextidr r1
+#endif
+	isb
+
+	/* Invalidate TLB */
+	write_tlbiall
+
+	/*
+	 * Make sure translation table writes have drained into memory and
+	 * the TLB invalidation is complete.
+	 */
+	dsb	sy
+	isb
+
+	read_sctlr r0
+	orr	r0, r0, #SCTLR_M
+#ifndef CFG_WITH_LPAE
+	/* Enable Access flag (simplified access permissions) and TEX remap */
+	orr	r0, r0, #(SCTLR_AFE | SCTLR_TRE)
+#endif
+	write_sctlr r0
+	isb
+
+	/* Invalidate instruction cache and branch predictor */
+	write_iciallu
+	write_bpiall
+	isb
+
+	read_sctlr r0
+	/* Enable I and D cache */
+	orr	r0, r0, #SCTLR_I
+	orr	r0, r0, #SCTLR_C
+#if defined(CFG_ENABLE_SCTLR_Z)
+	/*
+	 * This is only needed on ARMv7 architecture and hence conditionned
+	 * by configuration directive CFG_ENABLE_SCTLR_Z. For recent
+	 * architectures, the program flow prediction is automatically
+	 * enabled upon MMU enablement.
+	 */
+	orr	r0, r0, #SCTLR_Z
+#endif
+	write_sctlr r0
+	isb
+
+	bx	lr
+END_FUNC enable_mmu
+
+DATA boot_mmu_config , : /* struct core_mmu_config */
+	.skip	CORE_MMU_CONFIG_SIZE
+END_DATA boot_mmu_config
+
 #if defined(CFG_WITH_ARM_TRUSTED_FW)
 FUNC cpu_on_handler , :
 UNWIND(	.fnstart)
@@ -548,12 +632,8 @@ UNWIND(	.cantunwind)
 	mov	r4, lr
 	set_sp
 
-	bl	core_init_mmu_regs
-	bl	cpu_mmu_enable
-	bl	cpu_mmu_enable_icache
-	bl	cpu_mmu_enable_dcache
-
-	enable_branch_prediction
+	bl	__get_core_pos
+	bl	enable_mmu
 
 	mov	r0, r4
 	mov	r1, r5
@@ -584,12 +664,8 @@ UNWIND(	.cantunwind)
 	bl	dcache_op_level1
 #endif
 
-	bl	core_init_mmu_regs
-	bl	cpu_mmu_enable
-	bl	cpu_mmu_enable_icache
-	bl	cpu_mmu_enable_dcache
-
-	enable_branch_prediction
+	bl	__get_core_pos
+	bl	enable_mmu
 
 	cpu_is_ready
 

core/arch/arm/kernel/generic_entry_a64.S
@@ -8,6 +8,7 @@
 #include <arm64_macros.S>
 #include <arm.h>
 #include <asm.S>
+#include <generated/asm-defines.h>
 #include <keep.h>
 #include <sm/optee_smc.h>
 #include <sm/teesmc_opteed.h>
@@ -137,11 +138,13 @@ clear_nex_bss:
 	/* Enable Console */
 	bl	console_init
 
+	adr	x0, boot_mmu_config
 	bl	core_init_mmu_map
-	bl	core_init_mmu_regs
-	bl	cpu_mmu_enable
-	bl	cpu_mmu_enable_icache
-	bl	cpu_mmu_enable_dcache
+
+	bl	__get_core_pos
+	bl	enable_mmu
+	/* Virtual addresses may have changed, reinitialize console */
+	bl	console_init
 
 	mov	x0, x19		/* pagable part address */
 	mov	x1, #-1
@@ -183,6 +186,63 @@ clear_nex_bss:
 END_FUNC _start
 KEEP_INIT _start
 
+/*
+ * void enable_mmu(unsigned long core_pos);
+ */
+LOCAL_FUNC enable_mmu , :
+	adr	x1, boot_mmu_config
+	load_xregs x1, 0, 2, 5
+	/*
+	 * x0 = core_pos
+	 * x2 = tcr_el1
+	 * x3 = mair_el1
+	 * x4 = ttbr0_el1_base
+	 * x5 = ttbr0_core_offset
+	 */
+	msr	tcr_el1, x2
+	msr	mair_el1, x3
+
+	/*
+	 * ttbr0_el1 = ttbr0_el1_base + ttbr0_core_offset * core_pos
+	 */
+	madd	x1, x5, x0, x4
+	msr	ttbr0_el1, x1
+	msr	ttbr1_el1, xzr
+	isb
+
+	/* Invalidate TLB */
+	tlbi	vmalle1
+
+	/*
+	 * Make sure translation table writes have drained into memory and
+	 * the TLB invalidation is complete.
+	 */
+	dsb	sy
+	isb
+
+	/* Enable the MMU */
+	mrs	x1, sctlr_el1
+	orr	x1, x1, #SCTLR_M
+	msr	sctlr_el1, x1
+	isb
+
+	/* Invalidate instruction cache and branch predictor */
+	ic	iallu
+	isb
+
+	/* Enable I and D cache */
+	mrs	x1, sctlr_el1
+	orr	x1, x1, #SCTLR_I
+	orr	x1, x1, #SCTLR_C
+	msr	sctlr_el1, x1
+	isb
+
+	ret
+END_FUNC enable_mmu
+
+DATA boot_mmu_config , : /* struct core_mmu_config */
+	.skip	CORE_MMU_CONFIG_SIZE
+END_DATA boot_mmu_config
 
 FUNC cpu_on_handler , :
 	mov	x19, x0
@@ -202,10 +262,8 @@ FUNC cpu_on_handler , :
 	/* Enable aborts now that we can receive exceptions */
 	msr	daifclr, #DAIFBIT_ABT
 
-	bl	core_init_mmu_regs
-	bl	cpu_mmu_enable
-	bl	cpu_mmu_enable_icache
-	bl	cpu_mmu_enable_dcache
+	bl	__get_core_pos
+	bl	enable_mmu
 
 	mov	x0, x19
 	mov	x1, x20

core/arch/arm/kernel/proc_a32.S
@@ -1,97 +1,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-/*
- * Copyright (c) 2016, ARM Limited and Contributors. All rights reserved.
- * Copyright (c) 2014, STMicroelectronics International N.V.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * Neither the name of ARM nor the names of its contributors may be used
- * to endorse or promote products derived from this software without specific
- * prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <arm.h>
-#include <arm32_macros.S>
-#include <asm.S>
-#include <keep.h>
-#include <kernel/unwind.h>
-
-/*
- * void cpu_mmu_enable(void) - enable MMU
- *
- * TLBs are invalidated before MMU is enabled.
- * An DSB and ISB insures MMUs is enabled before routine returns
- */
-FUNC cpu_mmu_enable , :
-UNWIND(	.fnstart)
-	/* Invalidate TLB */
-	write_tlbiall
-
-	/* Enable the MMU */
-	read_sctlr r0
-	orr	r0, r0, #SCTLR_M
-	write_sctlr r0
-
-	dsb
-	isb
-
-	bx	lr
-UNWIND(	.fnend)
-END_FUNC cpu_mmu_enable
-KEEP_PAGER cpu_mmu_enable
-
-/* void cpu_mmu_enable_icache(void) - enable instruction cache */
-FUNC cpu_mmu_enable_icache , :
-UNWIND(	.fnstart)
-	/* Invalidate instruction cache and branch predictor */
-	write_iciallu
-	write_bpiall
-
-	/* Enable the instruction cache */
-	read_sctlr r1
-	orr	r1, r1, #SCTLR_I
-	write_sctlr r1
-
-	dsb
-	isb
-
-	bx	lr
-UNWIND(	.fnend)
-END_FUNC cpu_mmu_enable_icache
-KEEP_PAGER cpu_mmu_enable_icache
-
-/* void cpu_mmu_enable_dcache(void) - enable data cache */
-FUNC cpu_mmu_enable_dcache , :
-UNWIND(	.fnstart)
-	read_sctlr r0
-	orr	r0, r0, #SCTLR_C
-	write_sctlr r0
-
-	dsb
-	isb
-
-	bx	lr
-UNWIND(	.fnend)
-END_FUNC cpu_mmu_enable_dcache
-KEEP_PAGER cpu_mmu_enable_dcache

core/arch/arm/kernel/proc_a64.S
@@ -1,49 +1,0 @@
-/* SPDX-License-Identifier: BSD-2-Clause */
-/*
- * Copyright (c) 2015, Linaro Limited
- */
-#include <arm64.h>
-#include <asm.S>
-
-/* void cpu_mmu_enable(void) */
-FUNC cpu_mmu_enable , :
-	/* Invalidate TLB */
-	tlbi	vmalle1
-
-	/*
-	 * Make sure translation table writes have drained into memory and
-	 * the TLB invalidation is complete.
-	 */
-	dsb	sy
-	isb
-
-	/* Enable the MMU */
-	mrs	x0, sctlr_el1
-	orr	x0, x0, #SCTLR_M
-	msr	sctlr_el1, x0
-	isb
-
-	ret
-END_FUNC cpu_mmu_enable
-
-/* void cpu_mmu_enable_icache(void) */
-FUNC cpu_mmu_enable_icache , :
-	/* Invalidate instruction cache and branch predictor */
-	ic	iallu
-	isb
-	mrs	x0, sctlr_el1
-	orr	x0, x0, #SCTLR_I
-	msr	sctlr_el1, x0
-	isb
-	ret
-END_FUNC cpu_mmu_enable_icache
-
-
-/* void cpu_mmu_enable_dcache(void) */
-FUNC cpu_mmu_enable_dcache , :
-	mrs	x0, sctlr_el1
-	orr	x0, x0, #SCTLR_C
-	msr	sctlr_el1, x0
-	isb
-	ret
-END_FUNC cpu_mmu_enable_dcache

commit ad351ed5655f3c86db404abe034121d006e75601

core: arm64: introduce CFG_CORE_ARM64_PA_BITS
Introduces CFG_CORE_ARM64_PA_BITS which replaces the max_pa global
variable which was used to configure TCR_EL1.IPS.

Prior to 520860f ("core: generic_entry: add enable_mmu()") TCR_EL1.IPS
was calculated and even updated later in the boot flow to automatically
cover the needed physical address space. But now it's calculated before
MMU is enabled and once MMU it's kept in read only memory.

With CFG_CORE_ARM64_PA_BITS TCR_EL1.IPS can be determined early and
later it is enough to check that physical addresses to be mapped are
covered by CFG_CORE_ARM64_PA_BITS.

Fixes: 520860f658be ("core: generic_entry: add enable_mmu()")
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/arm.mk
@@ -10,6 +10,13 @@ CFG_RESERVED_VASPACE_SIZE ?= (1024 * 1024 * 10)
 ifeq ($(CFG_ARM64_core),y)
 CFG_KERN_LINKER_FORMAT ?= elf64-littleaarch64
 CFG_KERN_LINKER_ARCH ?= aarch64
+# TCR_EL1.IPS needs to be initialized according to the largest physical
+# address that we need to map.
+# Physical address size
+# 32 bits, 4GB.
+# 36 bits, 64GB.
+# (etc.)
+CFG_CORE_ARM64_PA_BITS ?= 32
 else
 ifeq ($(CFG_ARM32_core),y)
 CFG_KERN_LINKER_FORMAT ?= elf32-littlearm

core/arch/arm/mm/core_mmu.c
@@ -353,7 +353,6 @@ void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 	size_t num_elems = nelems;
 	struct tee_mmap_region *map = static_memory_map;
 	const struct core_mmu_phys_mem __maybe_unused *pmem;
-	paddr_t pa;
 
 	assert(!discovered_nsec_ddr_start);
 	assert(m && num_elems);
@@ -387,9 +386,9 @@ void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 	discovered_nsec_ddr_start = m;
 	discovered_nsec_ddr_nelems = num_elems;
 
-	if (ADD_OVERFLOW(m[num_elems - 1].addr, m[num_elems - 1].size - 1, &pa))
+	if (!core_mmu_check_end_pa(m[num_elems - 1].addr,
+				   m[num_elems - 1].size - 1))
 		panic();
-	core_mmu_set_max_pa(pa);
 }
 
 static bool get_discovered_nsec_ddr(const struct core_mmu_phys_mem **start,
@@ -1881,6 +1880,9 @@ bool core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	if (!len)
 		return true;
 
+	if (!core_mmu_check_end_pa(addr, len))
+		return false;
+
 	/* Check if the memory is already mapped */
 	map = find_map_by_type_and_pa(type, addr);
 	if (map && pbuf_inside_map_area(addr, len, map))
@@ -1934,9 +1936,6 @@ bool core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	map->region_size = granule;
 	map->attr = core_mmu_type_to_attr(type);
 	map->pa = p;
-
-	/* Update TCR_EL1 with possiable maximum physical address change */
-	core_mmu_set_max_pa(map->pa + map->size);
 
 	set_region(&tbl_info, map);
 

core/arch/arm/mm/core_mmu_lpae.c
@@ -137,13 +137,6 @@
 #define TCR_PS_BITS_16TB	(0x4)
 #define TCR_PS_BITS_256TB	(0x5)
 
-#define ADDR_MASK_48_TO_63	0xFFFF000000000000ULL
-#define ADDR_MASK_44_TO_47	0x0000F00000000000ULL
-#define ADDR_MASK_42_TO_43	0x00000C0000000000ULL
-#define ADDR_MASK_40_TO_41	0x0000030000000000ULL
-#define ADDR_MASK_36_TO_39	0x000000F000000000ULL
-#define ADDR_MASK_32_TO_35	0x0000000F00000000ULL
-
 #define UNSET_DESC		((uint64_t)-1)
 
 #define FOUR_KB_SHIFT		12
@@ -216,7 +209,6 @@ static xlat_tbl_t xlat_tables[MAX_XLAT_TABLES]
 static xlat_tbl_t xlat_tables_ul1[CFG_NUM_THREADS]
 	__aligned(XLAT_TABLE_SIZE) __section(".nozi.mmu.l2");
 
-static paddr_t max_pa __nex_bss;
 static int user_va_idx __nex_data = -1;
 
 struct mmu_partition {
@@ -365,15 +357,13 @@ static uint64_t mattr_to_desc(unsigned level, uint32_t attr)
 	return desc;
 }
 
-static paddr_t get_nsec_ddr_max_pa(void)
+static void check_nsec_ddr_max_pa(void)
 {
-	paddr_t pa = 0;
 	const struct core_mmu_phys_mem *mem;
 
 	for (mem = phys_nsec_ddr_begin; mem < phys_nsec_ddr_end; mem++)
-		pa = MAX(pa, mem->addr + mem->size);
-
-	return pa;
+		if (!core_mmu_check_end_pa(mem->addr, mem->size))
+			panic();
 }
 
 #ifdef CFG_VIRTUALIZATION
@@ -493,7 +483,7 @@ void core_init_mmu(struct tee_mmap_region *mm)
 #endif
 	COMPILE_TIME_ASSERT(XLAT_TABLES_SIZE == sizeof(xlat_tables));
 
-	max_pa = get_nsec_ddr_max_pa();
+	check_nsec_ddr_max_pa();
 
 	/* Initialize default pagetables */
 	core_init_mmu_prtn(&default_partition, mm);
@@ -504,13 +494,8 @@ void core_init_mmu(struct tee_mmap_region *mm)
 #endif
 
 	for (n = 0; !core_mmap_is_end_of_table(mm + n); n++) {
-		paddr_t pa_end;
-		vaddr_t va_end;
+		vaddr_t va_end = mm[n].va + mm[n].size - 1;
 
-		pa_end = mm[n].pa + mm[n].size - 1;
-		va_end = mm[n].va + mm[n].size - 1;
-		if (pa_end > max_pa)
-			max_pa = pa_end;
 		if (va_end > max_va)
 			max_va = va_end;
 	}
@@ -560,38 +545,45 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 #endif /*ARM32*/
 
 #ifdef ARM64
-static unsigned int calc_physical_addr_size_bits(uint64_t max_addr)
+static unsigned int get_physical_addr_size_bits(void)
 {
-	/* Physical address can't exceed 48 bits */
-	if (max_addr & ADDR_MASK_48_TO_63)
-		panic();
+	/*
+	 * Intermediate Physical Address Size.
+	 * 0b000      32 bits, 4GB.
+	 * 0b001      36 bits, 64GB.
+	 * 0b010      40 bits, 1TB.
+	 * 0b011      42 bits, 4TB.
+	 * 0b100      44 bits, 16TB.
+	 * 0b101      48 bits, 256TB.
+	 * 0b110      52 bits, 4PB (not supported)
+	 */
 
-	/* 48 bits address */
-	if (max_addr & ADDR_MASK_44_TO_47)
-		return TCR_PS_BITS_256TB;
+	COMPILE_TIME_ASSERT(CFG_CORE_ARM64_PA_BITS >= 32);
 
-	/* 44 bits address */
-	if (max_addr & ADDR_MASK_42_TO_43)
-		return TCR_PS_BITS_16TB;
+	if (CFG_CORE_ARM64_PA_BITS <= 32)
+		return TCR_PS_BITS_4GB;
 
-	/* 42 bits address */
-	if (max_addr & ADDR_MASK_40_TO_41)
-		return TCR_PS_BITS_4TB;
-
-	/* 40 bits address */
-	if (max_addr & ADDR_MASK_36_TO_39)
-		return TCR_PS_BITS_1TB;
-
-	/* 36 bits address */
-	if (max_addr & ADDR_MASK_32_TO_35)
+	if (CFG_CORE_ARM64_PA_BITS <= 36)
 		return TCR_PS_BITS_64GB;
 
-	return TCR_PS_BITS_4GB;
+	if (CFG_CORE_ARM64_PA_BITS <= 40)
+		return TCR_PS_BITS_1TB;
+
+	if (CFG_CORE_ARM64_PA_BITS <= 42)
+		return TCR_PS_BITS_4TB;
+
+	if (CFG_CORE_ARM64_PA_BITS <= 44)
+		return TCR_PS_BITS_16TB;
+
+	/* Physical address can't exceed 48 bits */
+	COMPILE_TIME_ASSERT(CFG_CORE_ARM64_PA_BITS <= 48);
+	/* CFG_CORE_ARM64_PA_BITS <= 48 */
+	return TCR_PS_BITS_256TB;
 }
 
 void core_init_mmu_regs(struct core_mmu_config *cfg)
 {
-	uint64_t ips = calc_physical_addr_size_bits(max_pa);
+	uint64_t ips = get_physical_addr_size_bits();
 	uint64_t mair = 0;
 	uint64_t tcr = 0;
 
@@ -617,28 +609,6 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 	 * TCR.AS = 0 => Same ASID size as in Aarch32/ARMv7
 	 */
 	cfg->tcr_el1 = tcr;
-}
-
-void core_mmu_set_max_pa(paddr_t pa)
-{
-	uint64_t tcr;
-	uint64_t ips;
-
-	if (pa <= max_pa)
-		return;
-
-	max_pa = pa;
-	ips = calc_physical_addr_size_bits(max_pa);
-	tcr = read_tcr_el1();
-	tcr &= ~(TCR_EL1_IPS_MASK << TCR_EL1_IPS_SHIFT);
-	tcr |= ips << TCR_EL1_IPS_SHIFT;
-	write_tcr_el1(tcr);
-	/*
-	 * MMU is already enabled at this stage and TCR state may be cached
-	 * in TLB.
-	 */
-	isb();
-	tlbi_all();
 }
 #endif /*ARM64*/
 

core/arch/arm/mm/core_mmu_private.h
@@ -23,13 +23,26 @@ static inline bool core_mmap_is_end_of_table(const struct tee_mmap_region *mm)
 	return mm->type == MEM_AREA_END;
 }
 
-#ifdef ARM64
-void core_mmu_set_max_pa(paddr_t pa);
-#else
-static inline void core_mmu_set_max_pa(paddr_t pa __unused)
+static inline bool core_mmu_check_max_pa(paddr_t pa __maybe_unused)
 {
-}
+#if defined(ARM64)
+	return pa <= (BIT64(CFG_CORE_ARM64_PA_BITS) - 1);
+#elif defined(CFG_CORE_LARGE_PHYS_ADDR)
+	return pa <= (BIT64(40) - 1);
+#else
+	COMPILE_TIME_ASSERT(sizeof(paddr_t) == sizeof(uint32_t));
+	return true;
 #endif
+}
+
+static inline bool core_mmu_check_end_pa(paddr_t pa, size_t len)
+{
+	paddr_t end_pa = 0;
+
+	if (ADD_OVERFLOW(pa, len, &end_pa))
+		return false;
+	return core_mmu_check_max_pa(end_pa);
+}
 
 #endif /*CORE_MMU_PRIVATE_H*/
 

core/arch/arm/plat-rcar/conf.mk
@@ -8,6 +8,7 @@ $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_SCIF,y)
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 
 ifeq ($(PLATFORM_FLAVOR),salvator_h3)
 $(call force,CFG_TEE_CORE_NB_CORE,8)

core/arch/arm/plat-vexpress/conf.mk
@@ -52,6 +52,7 @@ CFG_SHMEM_START  ?= 0x83000000
 CFG_SHMEM_SIZE   ?= 0x00200000
 # DRAM1 is defined above 4G
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 endif
 
 ifeq ($(PLATFORM_FLAVOR),juno)
@@ -62,6 +63,7 @@ CFG_SHMEM_START  ?= 0xfee00000
 CFG_SHMEM_SIZE   ?= 0x00200000
 # DRAM1 is defined above 4G
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 CFG_CRYPTO_WITH_CE ?= y
 endif
 