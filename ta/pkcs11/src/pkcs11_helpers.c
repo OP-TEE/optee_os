047a0a12f2f3bc1c4ef3dd191c75f8a90e63e5f4
Etienne Carriere <etienne.carriere@linaro.org>
2020-01-20 10:13:33 +0100

    ta: pkcs11: string debug trace for ta command ids
    ck_helper.c/.h provide will helper functions for IDs. This change
    starts with providing a string ID for a numerical command ID.
    
    Matching IDs a strings are stored in a constant array. Macros
    PKCS11_ID() ease definition of ID/string conversion arrays content.
    Function id2str() finds the string for a IDs possibly skip a given
    prefix, i.e. printing "ENCRYPT" instead of "PKCS11_CKFM_ENCRYPT".
    
    TA command IDs are the first introduced ID/string conversion util.
    Function id2str_ta_cmd() return string "PKCS11_CMD_..." for a known
    command ID.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ ta/pkcs11/src/pkcs11_helpers.c
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2018-2020, Linaro Limited
+ */
+
+#include <pkcs11_ta.h>
+#include <string.h>
+#include <tee_internal_api.h>
+#include <util.h>
+
+#include "pkcs11_helpers.h"
+
+static const char __maybe_unused unknown[] = "<unknown-identifier>";
+
+struct any_id {
+	uint32_t id;
+#if CFG_TEE_TA_LOG_LEVEL > 0
+	const char *string;
+#endif
+};
+
+/*
+ * Macro PKCS11_ID() can be used to define cells in ID list arrays
+ * or ID/string conversion arrays.
+ */
+#if CFG_TEE_TA_LOG_LEVEL > 0
+#define PKCS11_ID(_id)		{ .id = _id, .string = #_id }
+#else
+#define PKCS11_ID(_id)		{ .id = _id }
+#endif
+
+#define ID2STR(id, table, prefix)	\
+	id2str(id, table, ARRAY_SIZE(table), prefix)
+
+/* Convert a PKCS11 ID into its label string */
+static const char __maybe_unused *id2str(uint32_t id,
+					 const struct any_id *table,
+					 size_t count, const char *prefix)
+{
+	size_t n = 0;
+	const char *str = NULL;
+
+	for (n = 0; n < count; n++) {
+		if (id != table[n].id)
+			continue;
+
+		str = table[n].string;
+
+		/* Skip prefix provided matches found */
+		if (prefix && !TEE_MemCompare(str, prefix, strlen(prefix)))
+			str += strlen(prefix);
+
+		return str;
+	}
+
+	return unknown;
+}
+
+/*
+ * TA command IDs: used only as ID/string conversion for debug trace support
+ */
+static const struct any_id __maybe_unused string_ta_cmd[] = {
+	PKCS11_ID(PKCS11_CMD_PING),
+};
+
+#if CFG_TEE_TA_LOG_LEVEL > 0
+const char *id2str_ta_cmd(uint32_t id)
+{
+	return ID2STR(id, string_ta_cmd, NULL);
+}
+#endif /*CFG_TEE_TA_LOG_LEVEL*/
---
+++ ta/pkcs11/src/pkcs11_helpers.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2018-2020, Linaro Limited
+ */
+
+#ifndef PKCS11_HELPERS_H
+#define PKCS11_HELPERS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#if CFG_TEE_TA_LOG_LEVEL > 0
+/* Id-to-string conversions only for trace support */
+const char *id2str_ta_cmd(uint32_t id);
+#endif
+#endif /*PKCS11_HELPERS_H*/
