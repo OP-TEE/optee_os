30b940db4881cd5530b0f65c22898d75ba001bcf
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2020-01-22 19:57:29 +0200

    plat: rcar: generate .srec file using gen_tee_bin
    After recent changes, we are not able to use raw binary generated
    from the elf file. Instead we need to use gen_tee_bin script to
    generate the header-less binary with the correct layout.
    
    This change also generates tee-raw.bin as byproduct. This file is
    usable also, because it allows to flash OP-TEE using JTAG.
    
    Fixes: 5dd1570ac5b ("core: add embedded data region")
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-rcar/link.mk
+++ core/arch/arm/plat-rcar/link.mk
@@ -2,6 +2,14 @@ include core/arch/arm/kernel/link.mk
 
 all: $(link-out-dir)/tee.srec
 cleanfiles += $(link-out-dir)/tee.srec
-$(link-out-dir)/tee.srec: $(link-out-dir)/tee.elf
+
+cleanfiles += $(link-out-dir)/tee-raw.bin
+$(link-out-dir)/tee-raw.bin: $(link-out-dir)/tee.elf scripts/gen_tee_bin.py
 	@$(cmd-echo-silent) '  GEN     $@'
-	$(q)$(OBJCOPYcore) -O srec $< $@
+	$(q)scripts/gen_tee_bin.py --input $< --out_tee_raw_bin $@
+
+cleanfiles += $(link-out-dir)/tee.srec
+$(link-out-dir)/tee.srec: $(link-out-dir)/tee-raw.bin
+	@$(cmd-echo-silent) '  GEN     $@'
+	$(q)$(OBJCOPYcore) -I binary -O srec $< $@
+
