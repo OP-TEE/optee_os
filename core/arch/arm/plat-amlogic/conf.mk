ca576217e9ca8c798d441ec737ab9e72ecc1b81a
Carlo Caione <ccaione@baylibre.com>
2020-01-27 12:53:32 +0100

    plat-amlogic: Add initial support for Amlogic platforms
    This is the initial support for the Amlogic platforms.
    
    Tested 64-bin mode on A113D (AXG) board using upstream TF-A BL31.
    
    * xtest results (-l 15):
    | 44074 subtests of which 0 failed
    | 96 test cases of which 0 failed
    | 0 test cases were skipped
    | TEE test application done!
    
    * Compiled with:
    | make PLATFORM=amlogic
    
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

---
+++ core/arch/arm/plat-amlogic/conf.mk
@@ -0,0 +1,22 @@
+PLATFORM_FLAVOR ?= axg
+
+include core/arch/arm/cpu/cortex-armv8-0.mk
+
+$(call force,CFG_TEE_CORE_NB_CORE,4)
+
+CFG_TZDRAM_START ?= 0x05300000
+CFG_TZDRAM_SIZE ?= 0x00c00000
+CFG_SHMEM_START ?= 0x05000000
+CFG_SHMEM_SIZE ?= 0x00100000
+
+$(call force,CFG_GENERIC_BOOT,y)
+$(call force,CFG_PM_STUBS,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_WITH_ARM_TRUSTED_FW,y)
+$(call force,CFG_AMLOGIC_UART,y)
+
+$(call force,CFG_WITH_PAGER,n)
+$(call force,CFG_ARM64_core,y)
+$(call force,CFG_WITH_LPAE,y)
+
+CFG_WITH_STACK_CANARIES ?= y
