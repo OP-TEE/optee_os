802ddb5fad4d017cc1021a40a890806e0371c521
Andrew Davis <afd@ti.com>
2020-02-03 12:07:52 -0500

    plat-ti: Add SM handler for setting AMBA IF in the WUGEN
    This mirrors what is available as a ROM API. We implement this here as
    OP-TEE's monitor replaces the ROM's monitor functions and we would like
    to keep feature parity.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-ti/sm_platform_handler_a15.c
+++ core/arch/arm/plat-ti/sm_platform_handler_a15.c
@@ -27,12 +27,38 @@
  */
 
 #include <arm32.h>
+#include <io.h>
 #include <kernel/thread.h>
+#include <mm/core_memprot.h>
+#include <mm/core_mmu.h>
 #include <sm/optee_smc.h>
 #include <sm/sm.h>
 #include <trace.h>
 
 #include "api_monitor_index_a15.h"
+
+#define WUGEN_MPU_AMBA_IF_MODE 0x80c
+
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, WUGEN_MPU_BASE, WUGEN_MPU_SIZE);
+
+static vaddr_t wugen_mpu_base(void)
+{
+	static void *va;
+
+	if (cpu_mmu_enabled()) {
+		if (!va)
+			va = phys_to_virt(WUGEN_MPU_BASE, MEM_AREA_IO_SEC,
+					  WUGEN_MPU_SIZE);
+		return (vaddr_t)va;
+	}
+
+	return WUGEN_MPU_BASE;
+}
+
+static void write_wugen_mpu_amba_if_mode(uint32_t val)
+{
+	io_write32(wugen_mpu_base() + WUGEN_MPU_AMBA_IF_MODE, val);
+}
 
 static enum sm_handler_ret ti_sip_handler(struct thread_smc_args *smc_args)
 {
@@ -47,6 +73,10 @@ static enum sm_handler_ret ti_sip_handler(struct thread_smc_args *smc_args)
 	case API_MONITOR_TIMER_SETCNTFRQ_INDEX:
 		write_cntfrq(smc_args->a1);
 		isb();
+		smc_args->a0 = OPTEE_SMC_RETURN_OK;
+		break;
+	case API_MONITOR_WUGEN_MPU_SETAMBAIF_INDEX:
+		write_wugen_mpu_amba_if_mode(smc_args->a1);
 		smc_args->a0 = OPTEE_SMC_RETURN_OK;
 		break;
 	default:
