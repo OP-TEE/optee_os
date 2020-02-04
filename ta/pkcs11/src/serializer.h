4f8a354fe12895c806e678d626f85ab07297d7be
Etienne Carriere <etienne.carriere@linaro.org>
2020-02-04 19:40:17 +0100

    ta: pkcs11: de-serialization of command arguments
    Add serialize.c|.h that provide functions to extract a sized data
    in a serialized buffer as used for command serial arguments.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ ta/pkcs11/src/serializer.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#ifndef PKCS11_TA_SERIALIZER_H
+#define PKCS11_TA_SERIALIZER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/*
+ * Util routines for serializes unformated arguments in a client memref
+ */
+struct serialargs {
+	char *start;
+	char *next;
+	size_t size;
+};
+
+void serialargs_init(struct serialargs *args, void *in, size_t size);
+
+uint32_t serialargs_get(struct serialargs *args, void *out, size_t sz);
+
+uint32_t serialargs_get_ptr(struct serialargs *args, void **out, size_t size);
+
+uint32_t serialargs_alloc_and_get(struct serialargs *args,
+				  void **out, size_t size);
+
+bool serialargs_remaining_bytes(struct serialargs *args);
+
+#endif /*PKCS11_TA_SERIALIZER_H*/
