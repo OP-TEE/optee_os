a4c45fe676ba72d2ff816219e590659d93278b75
Etienne Carriere <etienne.carriere@linaro.org>
2020-02-04 14:43:40 +0100

    ta: pkcs11: persistent database for the pkcs11 tokens
    Initialize token(s) state from a persistent database. If no
    persistent database is found in the secure storage, initialize
    it to a default state and save the database in secure storage.
    
    PKCS11 TA may implement several tokens each related to its own
    database.
    
    A token persistent database is stored in several part in TEE secure
    storage. The main database stores token label, flags and PINs status.
    Another database stores the UUIDs of the TEE persistent objects
    used to store the token PKCS11 objects allowing the token to find
    back PKCS11 persistent objects. This object database is out of the
    scope of this change.
    
    At runtime, a token instance is reference by a struct ck_token instance
    in RAM which stores the state of the token and references to the
    resources the token as loaded as PIN cipher keys (see paragraph below),
    session states and the volatile copy of the persistent databases.
    
    Among data saved in persistent database is the reference to
    the keys used to cipher the PINs that will be used. A symmetric
    encryption scheme is used using keys PKCS11 TA does not have access
    to. This allows PKCS11 TA to save in RAM an encrypted value of
    the owners PINs.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ ta/pkcs11/src/pkcs11_token.h
@@ -0,0 +1,76 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+#ifndef PKCS11_TA_PKCS11_TOKEN_H
+#define PKCS11_TA_PKCS11_TOKEN_H
+
+#include <sys/queue.h>
+#include <tee_api_types.h>
+#include <tee_internal_api.h>
+
+enum pkcs11_token_state {
+	PKCS11_TOKEN_RESET = 0,
+	PKCS11_TOKEN_READ_WRITE,
+	PKCS11_TOKEN_READ_ONLY,
+};
+
+#define PKCS11_MAX_USERS		2
+#define PKCS11_TOKEN_PIN_SIZE		128
+
+/*
+ * Persistent state of the token
+ *
+ * @version - currently unused...
+ * @label - pkcs11 formatted token label, set by client
+ * @flags - pkcs11 token flags
+ * @so_pin_count - counter on security officer login failure
+ * @so_pin_size - byte size of the provisioned SO PIN
+ * @so_pin - stores the SO PIN
+ * @user_pin_count - counter on user login failure
+ * @user_pin_size - byte size of the provisioned user PIN
+ * @user_pin - stores the user PIN
+ */
+struct token_persistent_main {
+	uint32_t version;
+	uint8_t label[PKCS11_TOKEN_LABEL_SIZE];
+	uint32_t flags;
+	uint32_t so_pin_count;
+	uint32_t so_pin_size;
+	uint8_t so_pin[PKCS11_TOKEN_PIN_SIZE];
+	uint32_t user_pin_count;
+	uint32_t user_pin_size;
+	uint8_t user_pin[PKCS11_TOKEN_PIN_SIZE];
+};
+
+/*
+ * Runtime state of the token, complies with pkcs11
+ *
+ * @state - Pkcs11 login is public, user, SO or custom
+ * @session_count - Counter for opened Pkcs11 sessions
+ * @rw_session_count - Count for opened Pkcs11 read/write sessions
+ * @db_main - Volatile copy of the persistent main database
+ */
+struct ck_token {
+	enum pkcs11_token_state state;
+	uint32_t session_count;
+	uint32_t rw_session_count;
+	/* Copy in RAM of the persistent database */
+	struct token_persistent_main *db_main;
+};
+
+/* Initialize static token instance(s) from default/persistent database */
+TEE_Result pkcs11_init(void);
+void pkcs11_deinit(void);
+
+/* Return token instance from token identifier */
+struct ck_token *get_token(unsigned int token_id);
+
+/* Return token identified from token instance address */
+unsigned int get_token_id(struct ck_token *token);
+
+/* Access to persistent database */
+struct ck_token *init_persistent_db(unsigned int token_id);
+void close_persistent_db(struct ck_token *token);
+
+#endif /*PKCS11_TA_PKCS11_TOKEN_H*/
--- ta/pkcs11/src/sub.mk
+++ ta/pkcs11/src/sub.mk
@@ -1,2 +1,4 @@
 srcs-y += entry.c
+srcs-y += persistent_token.c
 srcs-y += pkcs11_helpers.c
+srcs-y += pkcs11_token.c
