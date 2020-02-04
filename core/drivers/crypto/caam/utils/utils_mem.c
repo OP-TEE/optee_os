8d02ec6d2530fbadbf20bc1ddffd6cd54e0abf10
Cedric Neveux <cedric.neveux@nxp.com>
2020-02-04 11:07:48 +0100

    drivers: caam: change CAAM debug trace method
    Change the method to enable the CAAM debug.
    Split the debug compilation 32 bits word to 3x32 bits words, one for the
    trace, one for the descriptor dump and one for the buffer dump.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/drivers/crypto/caam/utils/utils_mem.c
+++ core/drivers/crypto/caam/utils/utils_mem.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2020 NXP
  *
  * Brief   Memory management utilities.
  *         Primitive to allocate, free memory.
@@ -62,7 +62,7 @@ static uint32_t read_cacheline_size(void)
 #define MEM_TYPE_ZEROED	BIT(0) /* Buffer filled with 0's */
 #define MEM_TYPE_ALIGN	BIT(1) /* Address and size aligned on a cache line */
 
-#if (CFG_CAAM_DBG & DBG_TRACE_MEM)
+#if CAAM_DBG_TRACE(MEM)
 #define DEBUG_INFO_SIZE
 #endif
 
