OP-TEE/optee_os

commit a702f5e71e791925251574339bd60c29cca5547d

core: split thread_enter_user_mode
* elf_load_head() returns if it's a 32-bit or 64-bit TA (only 32-bit
  for now)
* The bool is_32bit is propagated to the C function thread_enter_user_mode()
  which initializes a value for SPSR used to enter TA

Reviewed-by: Pascal Brand <pascal.brand@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/thread.c
@@ -1015,6 +1015,56 @@ void thread_kernel_disable_vfp(uint32_t state)
 }
 #endif /*CFG_WITH_VFP*/
 
+#ifdef ARM32
+static bool get_spsr(bool is_32bit, unsigned long entry_func, uint32_t *spsr)
+{
+	uint32_t s;
+
+	if (!is_32bit)
+		return false;
+
+	s = read_spsr();
+	s &= ~(CPSR_MODE_MASK | CPSR_T | CPSR_IT_MASK1 | CPSR_IT_MASK2);
+	s |= CPSR_MODE_USR;
+	if (entry_func & 1)
+		s |= CPSR_T;
+	*spsr = s;
+	return true;
+}
+#endif
+
+#ifdef ARM64
+static bool get_spsr(bool is_32bit, unsigned long entry_func, uint32_t *spsr)
+{
+	uint32_t s;
+
+	if (!is_32bit)
+		return false;
+
+	s = read_daif() & (SPSR_32_AIF_MASK << SPSR_32_AIF_SHIFT);
+	s |= SPSR_MODE_RW_32 << SPSR_MODE_RW_SHIFT;
+	s |= (entry_func & SPSR_32_T_MASK) << SPSR_32_T_SHIFT;
+	*spsr = s;
+	return true;
+}
+#endif
+
+uint32_t thread_enter_user_mode(unsigned long a0, unsigned long a1,
+		unsigned long a2, unsigned long a3, unsigned long user_sp,
+		unsigned long entry_func, bool is_32bit,
+		uint32_t *exit_status0, uint32_t *exit_status1)
+{
+	uint32_t spsr;
+
+	if (!get_spsr(is_32bit, entry_func, &spsr)) {
+		*exit_status0 = 1; /* panic */
+		*exit_status1 = 0xbadbadba;
+		return 0;
+	}
+	return __thread_enter_user_mode(a0, a1, a2, a3, user_sp, entry_func,
+					spsr, exit_status0, exit_status1);
+}
+
 void thread_add_mutex(struct mutex *m)
 {
 	struct thread_core_local *l = thread_get_core_local();

core/arch/arm/include/kernel/thread.h
@@ -393,6 +393,7 @@ void thread_kernel_disable_vfp(uint32_t state);
  * @a3:		Passed in r/x3 for user_func
  * @user_sp:	Assigned sp value in user mode
  * @user_func:	Function to execute in user mode
+ * @is_32bit:   True if TA should execute in Aarch32, false if Aarch64
  * @exit_status0: Pointer to opaque exit staus 0
  * @exit_status1: Pointer to opaque exit staus 1
  *
@@ -403,9 +404,9 @@ void thread_kernel_disable_vfp(uint32_t state);
  *
  * @Returns what's passed in "ret" to thread_unwind_user_mode()
  */
-
-uint32_t thread_enter_user_mode(uint32_t a0, uint32_t a1, uint32_t a2,
-		uint32_t a3, vaddr_t user_sp, vaddr_t user_func,
+uint32_t thread_enter_user_mode(unsigned long a0, unsigned long a1,
+		unsigned long a2, unsigned long a3, unsigned long user_sp,
+		unsigned long entry_func, bool is_32bit,
 		uint32_t *exit_status0, uint32_t *exit_status1);
 
 /*

core/arch/arm/kernel/elf_load.c
@@ -140,7 +140,7 @@ TEE_Result elf_load_init(void *hash_ctx, uint32_t hash_algo, uint8_t *nwdata,
 }
 
 TEE_Result elf_load_head(struct elf_load_state *state, size_t head_size,
-			void **head, size_t *vasize)
+			void **head, size_t *vasize, bool *is_32bit)
 {
 	TEE_Result res;
 	size_t n;
@@ -232,6 +232,7 @@ TEE_Result elf_load_head(struct elf_load_state *state, size_t head_size,
 	state->ta_head = p;
 	state->ta_head_size = head_size;
 
+	*is_32bit = true;
 	*head = state->ta_head;
 	*vasize = state->vasize;
 	return TEE_SUCCESS;

core/arch/arm/kernel/elf_load.h
@@ -35,7 +35,7 @@ struct elf_load_state;
 TEE_Result elf_load_init(void *hash_ctx, uint32_t hash_algo, uint8_t *nwdata,
 			size_t nwdata_len, struct elf_load_state **state);
 TEE_Result elf_load_head(struct elf_load_state *state, size_t head_size,
-			void **head, size_t *vasize);
+			void **head, size_t *vasize, bool *is_32bit);
 TEE_Result elf_load_body(struct elf_load_state *state, vaddr_t vabase);
 void elf_load_final(struct elf_load_state *state);
 

core/arch/arm/kernel/tee_ta_manager.c
@@ -541,7 +541,8 @@ static TEE_Result tee_ta_load_elf(struct tee_ta_ctx *ctx, struct shdr *shdr,
 	if (res != TEE_SUCCESS)
 		goto out;
 
-	res = elf_load_head(elf_state, sizeof(struct ta_head), &p, &vasize);
+	res = elf_load_head(elf_state, sizeof(struct ta_head), &p, &vasize,
+			    &ctx->is_32bit);
 	if (res != TEE_SUCCESS)
 		goto out;
 	ta_head = p;
@@ -887,8 +888,8 @@ static TEE_Result tee_user_ta_enter(TEE_ErrorOrigin *err,
 
 	res = thread_enter_user_mode(func, tee_svc_kaddr_to_uref(session),
 				     params_uaddr, cmd, stack_uaddr,
-				     ctx->entry_func, &ctx->panicked,
-				     &ctx->panic_code);
+				     ctx->entry_func, ctx->is_32bit,
+				     &ctx->panicked, &ctx->panic_code);
 	/*
 	 * According to GP spec the origin should allways be set to the
 	 * TA after TA execution

core/arch/arm/kernel/thread_a32.S
@@ -407,13 +407,13 @@ END_FUNC thread_init_vbar
  */
 
 /*
- * TEE_Result thread_enter_user_mode(
- *              uint32_t a0, uint32_t a1, uint32_t a2, uint32_t a3,
- *              tee_uaddr_t sp, tee_uaddr_t user_func,
- *              uint32_t *panicked, uint32_t *panic_code);
- * See description in thread.h
+ * uint32_t __thread_enter_user_mode(unsigned long a0, unsigned long a1,
+ *               unsigned long a2, unsigned long a3, unsigned long user_sp,
+ *               unsigned long user_func, unsigned long spsr,
+ *               uint32_t *exit_status0, uint32_t *exit_status1)
+ *
  */
-FUNC thread_enter_user_mode , :
+FUNC __thread_enter_user_mode , :
 	/*
 	 * Save all registers to allow syscall_return() to resume execution
 	 * as if this function would have returned. This is also used in
@@ -426,7 +426,14 @@ FUNC thread_enter_user_mode , :
 
 	ldr     r4, [sp, #(10 * 0x4)]   /* user stack pointer */
 	ldr     r5, [sp, #(11 * 0x4)]   /* user function */
+	ldr     r6, [sp, #(12 * 0x4)]   /* spsr */
 
+	/*
+	 * Set the saved Processors Status Register to user mode to allow
+	 * entry of user mode through movs below.
+	 */
+	msr     spsr_cxsf, r6
+	
 	/*
 	 * Save old user sp and set new user sp.
 	 */
@@ -437,28 +444,13 @@ FUNC thread_enter_user_mode , :
 	push	{r6}
 
 	/*
-	 * Set the saved Processors Status Register to user mode to allow
-	 * entry of user mode through movs below. Also update thumbstate
-	 * since movs doesn't do that automatically.
-	 */
-	mrs     r6, cpsr
-	bic     r6, #CPSR_MODE_MASK
-	orr	r6, #CPSR_MODE_USR
-	tst     r5, #1	/* If it's odd we should switch to thumb mode */
-	orrne   r6, #CPSR_T		/* Enable thumb mode */
-	biceq   r6, #CPSR_T		/* Disable thumb mode */
-	bicne   r6, #CPSR_IT_MASK1	/* Clear IT state for thumb mode */
-	bicne   r6, #CPSR_IT_MASK2	/* Clear IT state for thumb mode */
-	msr     spsr_cxsf, r6
-
-	/*
 	* Don't allow return from this function, return is done through
 	* thread_unwind_user_mode() below.
 	*/
 	mov     lr, #0
 	/* Call the user function with its arguments */
 	movs    pc, r5
-END_FUNC thread_enter_user_mode
+END_FUNC __thread_enter_user_mode
 
 /*
  * void thread_unwind_user_mode(uint32_t ret, uint32_t exit_status0,
@@ -466,9 +458,9 @@ END_FUNC thread_enter_user_mode
  * See description in thread.h
  */
 FUNC thread_unwind_user_mode , :
-	ldr     ip, [sp, #(13 * 0x4)]   /* &ctx->panicked */
+	ldr     ip, [sp, #(14 * 0x4)]   /* &ctx->panicked */
 	str	r1, [ip]
-	ldr     ip, [sp, #(14 * 0x4)]   /* &ctx->panic_code */
+	ldr     ip, [sp, #(15 * 0x4)]   /* &ctx->panic_code */
 	str	r2, [ip]
 
 	/* Restore old user sp */

core/arch/arm/kernel/thread_a64.S
@@ -260,26 +260,26 @@ FUNC thread_init_vbar , :
 END_FUNC thread_init_vbar
 
 /*
- * uint32_t thread_enter_user_mode(uint32_t a0, uint32_t a1, uint32_t a2,
- * 		uint32_t a3, vaddr_t user_sp, vaddr_t user_func,
- * 		uint32_t *exit_status0, uint32_t *exit_status1);
- * See description in thread.h
+ * uint32_t __thread_enter_user_mode(unsigned long a0, unsigned long a1,
+ *               unsigned long a2, unsigned long a3, unsigned long user_sp,
+ *               unsigned long user_func, unsigned long spsr,
+ *               uint32_t *exit_status0, uint32_t *exit_status1)
+ *
  */
-FUNC thread_enter_user_mode , :
+FUNC __thread_enter_user_mode , :
+	ldr	x8, [sp]
 	/*
 	 * Create the and fill in the struct thread_user_mode_rec
 	 */
 	sub	sp, sp, #THREAD_USER_MODE_REC_SIZE
-	store_xregs sp, THREAD_USER_MODE_REC_EXIT_STATUS0_PTR_OFFSET, 6, 7
+	store_xregs sp, THREAD_USER_MODE_REC_EXIT_STATUS0_PTR_OFFSET, 7, 8
 	store_xregs sp, THREAD_USER_MODE_REC_X_OFFSET(19), 19, 30
 
 	/*
 	 * Switch to SP_EL1
-	 * Save interrupt bits in x23
 	 * Disable exceptions
 	 * Save kern sp in x19
 	 */
-	mrs	x23, daif
 	msr	daifset, #DAIFBIT_ALL
 	mov	x19, sp
 	msr	spsel, #1
@@ -295,23 +295,16 @@ FUNC thread_enter_user_mode , :
 	/*
 	 * Initialize SPSR, ELR_EL1, and SP_EL0 to enter user mode
 	 */
-	/* Keep only the AIF bits */
-	and	x23, x23, #(SPSR_32_AIF_MASK << SPSR_32_AIF_SHIFT)
-	/* Set Aarch32 */
-	orr	x23, x23, #(SPSR_MODE_RW_32 << SPSR_MODE_RW_SHIFT)
-	/* Set thumb mode for thumb function */
-	and	x24, x5, #SPSR_32_T_MASK
-	orr	x23, x23, x24, lsl #SPSR_32_T_SHIFT
-	msr	spsr_el1, x23
+	msr	spsr_el1, x6
 	/* Set user sp */
-	mov	x13, x4
-	msr	sp_el0, x4 /* TODO remove, only here to invalidate sp_el0 */
+	mov	x13, x4		/* Used when running TA in Aarch32 */
+	msr	sp_el0, x4	/* Used when running TA in Aarch64 */
 	/* Set user function */
 	msr	elr_el1, x5
 
 	/* Jump into user mode */
 	eret
-END_FUNC thread_enter_user_mode
+END_FUNC __thread_enter_user_mode
 
 /*
  * void thread_unwind_user_mode(uint32_t ret, uint32_t exit_status0,
@@ -478,7 +471,8 @@ LOCAL_FUNC el0_sync_a32_svc , :
 
 	/*
 	 * Unmask FIQ, Serror, and debug exceptions since we have nothing
-	 * left in sp_el1.
+	 * left in sp_el1. Note that the SVC handler is excepted to
+	 * re-enable IRQs by itself.
 	 */
 	msr	daifclr, #(DAIFBIT_FIQ | DAIFBIT_ABT | DAIFBIT_DBG)
 

core/arch/arm/kernel/thread_private.h
@@ -198,6 +198,11 @@ struct thread_core_local *thread_get_core_local(void);
  */
 void thread_resume(struct thread_ctx_regs *regs);
 
+uint32_t __thread_enter_user_mode(unsigned long a0, unsigned long a1,
+		unsigned long a2, unsigned long a3, unsigned long user_sp,
+		unsigned long user_func, unsigned long spsr,
+		uint32_t *exit_status0, uint32_t *exit_status1);
+
 /*
  * Private functions made available for thread_asm.S
  */

commit cc3a026628c50ab3ace9bb0a8fdd041fa0415b82

core: arm32: derive spsr from cpsr
When entering user mode, the bits of SPSR should be derived from CPSR.

Fixes: a702f5e71e79 "core: split thread_enter_user_mode"

Signed-off-by: Ali Zhang <alizhang@google.com>

core/arch/arm/kernel/thread.c
@@ -1210,7 +1210,7 @@ static bool get_spsr(bool is_32bit, unsigned long entry_func, uint32_t *spsr)
 	if (!is_32bit)
 		return false;
 
-	s = read_spsr();
+	s = read_cpsr();
 	s &= ~(CPSR_MODE_MASK | CPSR_T | CPSR_IT_MASK1 | CPSR_IT_MASK2);
 	s |= CPSR_MODE_USR;
 	if (entry_func & 1)
@@ -1284,6 +1284,7 @@ uint32_t thread_enter_user_mode(unsigned long a0, unsigned long a1,
 
 	tee_ta_update_session_utime_resume();
 
+	/* Derive SPSR from current CPSR/PSTATE readout. */
 	if (!get_spsr(is_32bit, entry_func, &spsr)) {
 		*exit_status0 = 1; /* panic */
 		*exit_status1 = 0xbadbadba;