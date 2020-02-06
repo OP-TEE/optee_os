OP-TEE/optee_os

commit 6ea5aa6cf4571812f0a0237349993ffee86f7ff2

ta: pkcs11: invocation command PKCS11
Introduce a first invocation command for the TA: PKCS11_CMD_PING
can be used the check TA presence and possibly retrieve TA version
information if client provides an output buffer.

Add helpers to trace command and parameters configuration.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Markus S. Wamser <markus.wamser@mixed-mode.de>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/pkcs11/src/entry.c
@@ -4,6 +4,7 @@
  */
 
 #include <compiler.h>
+#include <pkcs11_ta.h>
 #include <tee_internal_api.h>
 
 TEE_Result TA_CreateEntryPoint(void)
@@ -29,13 +30,110 @@ void TA_CloseSessionEntryPoint(void *session __unused)
 }
 
 /*
+ * Entry point for invocation command PKCS11_CMD_PING
+ *
+ * @ctrl - param memref[0] or NULL: expected NULL
+ * @in - param memref[1] or NULL: expected NULL
+ * @out - param memref[2] or NULL
+ *
+ * Return a TEE_Result value
+ */
+static TEE_Result entry_ping(TEE_Param *ctrl, TEE_Param *in, TEE_Param *out)
+{
+	const uint32_t ver[] = {
+		PKCS11_TA_VERSION_MAJOR,
+		PKCS11_TA_VERSION_MINOR,
+		PKCS11_TA_VERSION_PATCH,
+	};
+	size_t size = 0;
+
+	if (ctrl || in)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (!out)
+		return TEE_SUCCESS;
+
+	size = out->memref.size;
+	out->memref.size = sizeof(ver);
+
+	if (size < sizeof(ver))
+		return TEE_ERROR_SHORT_BUFFER;
+
+	TEE_MemMove(out->memref.buffer, ver, sizeof(ver));
+
+	return TEE_SUCCESS;
+}
+
+/*
  * Entry point for PKCS11 TA commands
  */
 TEE_Result TA_InvokeCommandEntryPoint(void *tee_session __unused, uint32_t cmd,
-				      uint32_t ptypes __unused,
-				      TEE_Param params[TEE_NUM_PARAMS] __unused)
+				      uint32_t ptypes,
+				      TEE_Param params[TEE_NUM_PARAMS])
 {
-	EMSG("Command 0x%"PRIx32" is not supported", cmd);
+	TEE_Param *ctrl = NULL;
+	TEE_Param *p1_in = NULL;
+	TEE_Param __maybe_unused *p2_in = NULL;
+	TEE_Param *p2_out = NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
 
-	return TEE_ERROR_NOT_SUPPORTED;
+	/* Param#0: none or in-out buffer with serialized arguments */
+	switch (TEE_PARAM_TYPE_GET(ptypes, 0)) {
+	case TEE_PARAM_TYPE_NONE:
+		break;
+	case TEE_PARAM_TYPE_MEMREF_INOUT:
+		ctrl = &params[0];
+		break;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	/* Param#1: none or input data buffer */
+	switch (TEE_PARAM_TYPE_GET(ptypes, 1)) {
+	case TEE_PARAM_TYPE_NONE:
+		break;
+	case TEE_PARAM_TYPE_MEMREF_INPUT:
+		p1_in = &params[1];
+		break;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	/* Param#2: none or input data buffer */
+	switch (TEE_PARAM_TYPE_GET(ptypes, 2)) {
+	case TEE_PARAM_TYPE_NONE:
+		break;
+	case TEE_PARAM_TYPE_MEMREF_INPUT:
+		p2_in = &params[2];
+		break;
+	case TEE_PARAM_TYPE_MEMREF_OUTPUT:
+		p2_out = &params[2];
+		break;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	/* Param#3: currently unused */
+	switch (TEE_PARAM_TYPE_GET(ptypes, 3)) {
+	case TEE_PARAM_TYPE_NONE:
+		break;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	switch (cmd) {
+	case PKCS11_CMD_PING:
+		res = entry_ping(ctrl, p1_in, p2_out);
+		break;
+
+	default:
+		EMSG("Command 0x%"PRIx32" is not supported", cmd);
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	/* Currently no output data stored in output param#0 */
+	if (TEE_PARAM_TYPE_GET(ptypes, 0) == TEE_PARAM_TYPE_MEMREF_INOUT)
+		ctrl->memref.size = 0;
+
+	return res;
 }

ta/pkcs11/include/pkcs11_ta.h
@@ -12,6 +12,11 @@
 #define PKCS11_TA_UUID { 0xfd02c9da, 0x306c, 0x48c7, \
 			 { 0xa4, 0x9c, 0xbb, 0xd8, 0x27, 0xae, 0x86, 0xee } }
 
+/* PKCS11 trusted application version information */
+#define PKCS11_TA_VERSION_MAJOR			0
+#define PKCS11_TA_VERSION_MINOR			1
+#define PKCS11_TA_VERSION_PATCH			0
+
 /* Attribute specific values */
 #define PKCS11_UNAVAILABLE_INFORMATION		UINT32_C(0xFFFFFFFF)
 #define PKCS11_UNDEFINED_ID			PKCS11_UNAVAILABLE_INFORMATION
@@ -29,7 +34,7 @@
  * and for providing a PKCS#11 compliant status code for the request command.
  * Param#0 is an in/out memory reference (aka memref[0]). The input buffer
  * stores the command arguments serialized inside. The output buffer will
- * store the 32bit TA retrun code for the command. Client shall get this
+ * store the 32bit TA return code for the command. Client shall get this
  * return code and override the GPD TEE Client API legacy TEE_Result value.
  *
  * Param#1 is used for input data arguments of the invoked command.
@@ -43,4 +48,16 @@
  *
  * Param#3 is currently unused and reserved for evolution of the API.
  */
+
+/*
+ * PKCS11_CMD_PING		Acknowledge TA presence and return version info
+ *
+ * Optinal invocation parameter (if none, command simply returns with success)
+ * [out]        memref[2] = [
+ *                      32bit version major value,
+ *                      32bit version minor value
+ *                      32bit version patch value
+ *              ]
+ */
+#define PKCS11_CMD_PING				0
 #endif /*PKCS11_TA_H*/

commit 8d02e6afe28f5b2699a9509abfb5b87ed75dfcc8

ta: pkcs11: fix command parameter types
Relax parameters types for TA command invocation.
Allow param#0 to be an output only memory reference.
Allow param#2 to be an input/output memory reference.

Fixes: 6ea5aa6cf457 ("ta: pkcs11: invocation command PKCS11")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

ta/pkcs11/src/entry.c
@@ -77,10 +77,11 @@ TEE_Result TA_InvokeCommandEntryPoint(void *tee_session __unused, uint32_t cmd,
 	TEE_Param *p2_out = NULL;
 	TEE_Result res = TEE_ERROR_GENERIC;
 
-	/* Param#0: none or in-out buffer with serialized arguments */
+	/* Param#0: none or output or in-out buffer with serialized arguments */
 	switch (TEE_PARAM_TYPE_GET(ptypes, 0)) {
 	case TEE_PARAM_TYPE_NONE:
 		break;
+	case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 	case TEE_PARAM_TYPE_MEMREF_INOUT:
 		ctrl = &params[0];
 		break;
@@ -99,7 +100,7 @@ TEE_Result TA_InvokeCommandEntryPoint(void *tee_session __unused, uint32_t cmd,
 		return TEE_ERROR_BAD_PARAMETERS;
 	}
 
-	/* Param#2: none or input data buffer */
+	/* Param#2: none or input/output/in-out data buffer */
 	switch (TEE_PARAM_TYPE_GET(ptypes, 2)) {
 	case TEE_PARAM_TYPE_NONE:
 		break;
@@ -107,6 +108,10 @@ TEE_Result TA_InvokeCommandEntryPoint(void *tee_session __unused, uint32_t cmd,
 		p2_in = &params[2];
 		break;
 	case TEE_PARAM_TYPE_MEMREF_OUTPUT:
+		p2_out = &params[2];
+		break;
+	case TEE_PARAM_TYPE_MEMREF_INOUT:
+		p2_in = &params[2];
 		p2_out = &params[2];
 		break;
 	default: