9d1a383e8fded8fb655b25396b88478a2994215b
Fabien Parent <fparent@baylibre.com>
2020-02-13 17:43:20 +0100

    plat-mediatek: Add support for GIC
    Add the support for the GIC for the MediaTek platforms.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-mediatek/main.c
+++ core/arch/arm/plat-mediatek/main.c
@@ -4,7 +4,9 @@
  */
 
 #include <console.h>
+#include <drivers/gic.h>
 #include <drivers/serial8250_uart.h>
+#include <kernel/boot.h>
 #include <kernel/panic.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
@@ -14,6 +16,37 @@ register_phys_mem_pgdir(MEM_AREA_IO_NSEC,
 			CONSOLE_UART_BASE, SERIAL8250_UART_REG_SIZE);
 
 static struct serial8250_uart_data console_data;
+
+#ifdef CFG_GIC
+static struct gic_data gic_data;
+
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, GIC_BASE + GICD_OFFSET,
+			CORE_MMU_PGDIR_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, GIC_BASE + GICC_OFFSET,
+			CORE_MMU_PGDIR_SIZE);
+
+void main_init_gic(void)
+{
+	vaddr_t gicc_base = 0;
+	vaddr_t gicd_base = 0;
+
+	gicc_base = (vaddr_t)phys_to_virt(GIC_BASE + GICC_OFFSET,
+					  MEM_AREA_IO_SEC);
+	gicd_base = (vaddr_t)phys_to_virt(GIC_BASE + GICD_OFFSET,
+					  MEM_AREA_IO_SEC);
+	if (!gicc_base || !gicd_base)
+		panic();
+
+	gic_init_base_addr(&gic_data, gicc_base, gicd_base);
+
+	itr_init(&gic_data.chip);
+}
+
+void itr_core_handler(void)
+{
+	gic_it_handle(&gic_data);
+}
+#endif
 
 void console_init(void)
 {
