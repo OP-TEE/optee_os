OP-TEE/optee_os

commit 4518cdc1ff64614942239f40568baa603fb1d101

core: arm64: introduce CFG_CORE_ARM64_PA_BITS
Introduces CFG_CORE_ARM64_PA_BITS which replaces the max_pa global
variable which was used to configure TCR_EL1.IPS.

Prior to 520860f ("core: generic_entry: add enable_mmu()") TCR_EL1.IPS
was calculated and even updated later in the boot flow to automatically
cover the needed physical address space. But now it's calculated before
MMU is enabled and once MMU it's kept in read only memory.

With CFG_CORE_ARM64_PA_BITS TCR_EL1.IPS can be determined early and
later it is enough to check that physical addresses to be mapped are
covered by CFG_CORE_ARM64_PA_BITS.

Fixes: 520860f658be ("core: generic_entry: add enable_mmu()")
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -353,7 +353,6 @@ void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 	size_t num_elems = nelems;
 	struct tee_mmap_region *map = static_memory_map;
 	const struct core_mmu_phys_mem __maybe_unused *pmem;
-	paddr_t pa;
 
 	assert(!discovered_nsec_ddr_start);
 	assert(m && num_elems);
@@ -387,9 +386,9 @@ void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 	discovered_nsec_ddr_start = m;
 	discovered_nsec_ddr_nelems = num_elems;
 
-	if (ADD_OVERFLOW(m[num_elems - 1].addr, m[num_elems - 1].size - 1, &pa))
+	if (!core_mmu_check_end_pa(m[num_elems - 1].addr,
+				   m[num_elems - 1].size - 1))
 		panic();
-	core_mmu_set_max_pa(pa);
 }
 
 static bool get_discovered_nsec_ddr(const struct core_mmu_phys_mem **start,
@@ -1881,6 +1880,9 @@ bool core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	if (!len)
 		return true;
 
+	if (!core_mmu_check_end_pa(addr, len))
+		return false;
+
 	/* Check if the memory is already mapped */
 	map = find_map_by_type_and_pa(type, addr);
 	if (map && pbuf_inside_map_area(addr, len, map))
@@ -1934,9 +1936,6 @@ bool core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	map->region_size = granule;
 	map->attr = core_mmu_type_to_attr(type);
 	map->pa = p;
-
-	/* Update TCR_EL1 with possiable maximum physical address change */
-	core_mmu_set_max_pa(map->pa + map->size);
 
 	set_region(&tbl_info, map);
 

core/arch/arm/mm/core_mmu_private.h
@@ -23,13 +23,26 @@ static inline bool core_mmap_is_end_of_table(const struct tee_mmap_region *mm)
 	return mm->type == MEM_AREA_END;
 }
 
-#ifdef ARM64
-void core_mmu_set_max_pa(paddr_t pa);
-#else
-static inline void core_mmu_set_max_pa(paddr_t pa __unused)
+static inline bool core_mmu_check_max_pa(paddr_t pa __maybe_unused)
 {
-}
+#if defined(ARM64)
+	return pa <= (BIT64(CFG_CORE_ARM64_PA_BITS) - 1);
+#elif defined(CFG_CORE_LARGE_PHYS_ADDR)
+	return pa <= (BIT64(40) - 1);
+#else
+	COMPILE_TIME_ASSERT(sizeof(paddr_t) == sizeof(uint32_t));
+	return true;
 #endif
+}
+
+static inline bool core_mmu_check_end_pa(paddr_t pa, size_t len)
+{
+	paddr_t end_pa = 0;
+
+	if (ADD_OVERFLOW(pa, len, &end_pa))
+		return false;
+	return core_mmu_check_max_pa(end_pa);
+}
 
 #endif /*CORE_MMU_PRIVATE_H*/
 

core/arch/arm/arm.mk
@@ -10,6 +10,13 @@ CFG_RESERVED_VASPACE_SIZE ?= (1024 * 1024 * 10)
 ifeq ($(CFG_ARM64_core),y)
 CFG_KERN_LINKER_FORMAT ?= elf64-littleaarch64
 CFG_KERN_LINKER_ARCH ?= aarch64
+# TCR_EL1.IPS needs to be initialized according to the largest physical
+# address that we need to map.
+# Physical address size
+# 32 bits, 4GB.
+# 36 bits, 64GB.
+# (etc.)
+CFG_CORE_ARM64_PA_BITS ?= 32
 else
 ifeq ($(CFG_ARM32_core),y)
 CFG_KERN_LINKER_FORMAT ?= elf32-littlearm

core/arch/arm/mm/core_mmu_lpae.c
@@ -137,13 +137,6 @@
 #define TCR_PS_BITS_16TB	(0x4)
 #define TCR_PS_BITS_256TB	(0x5)
 
-#define ADDR_MASK_48_TO_63	0xFFFF000000000000ULL
-#define ADDR_MASK_44_TO_47	0x0000F00000000000ULL
-#define ADDR_MASK_42_TO_43	0x00000C0000000000ULL
-#define ADDR_MASK_40_TO_41	0x0000030000000000ULL
-#define ADDR_MASK_36_TO_39	0x000000F000000000ULL
-#define ADDR_MASK_32_TO_35	0x0000000F00000000ULL
-
 #define UNSET_DESC		((uint64_t)-1)
 
 #define FOUR_KB_SHIFT		12
@@ -216,7 +209,6 @@ static xlat_tbl_t xlat_tables[MAX_XLAT_TABLES]
 static xlat_tbl_t xlat_tables_ul1[CFG_NUM_THREADS]
 	__aligned(XLAT_TABLE_SIZE) __section(".nozi.mmu.l2");
 
-static paddr_t max_pa __nex_bss;
 static int user_va_idx __nex_data = -1;
 
 struct mmu_partition {
@@ -365,15 +357,13 @@ static uint64_t mattr_to_desc(unsigned level, uint32_t attr)
 	return desc;
 }
 
-static paddr_t get_nsec_ddr_max_pa(void)
+static void check_nsec_ddr_max_pa(void)
 {
-	paddr_t pa = 0;
 	const struct core_mmu_phys_mem *mem;
 
 	for (mem = phys_nsec_ddr_begin; mem < phys_nsec_ddr_end; mem++)
-		pa = MAX(pa, mem->addr + mem->size);
-
-	return pa;
+		if (!core_mmu_check_end_pa(mem->addr, mem->size))
+			panic();
 }
 
 #ifdef CFG_VIRTUALIZATION
@@ -493,7 +483,7 @@ void core_init_mmu(struct tee_mmap_region *mm)
 #endif
 	COMPILE_TIME_ASSERT(XLAT_TABLES_SIZE == sizeof(xlat_tables));
 
-	max_pa = get_nsec_ddr_max_pa();
+	check_nsec_ddr_max_pa();
 
 	/* Initialize default pagetables */
 	core_init_mmu_prtn(&default_partition, mm);
@@ -504,13 +494,8 @@ void core_init_mmu(struct tee_mmap_region *mm)
 #endif
 
 	for (n = 0; !core_mmap_is_end_of_table(mm + n); n++) {
-		paddr_t pa_end;
-		vaddr_t va_end;
+		vaddr_t va_end = mm[n].va + mm[n].size - 1;
 
-		pa_end = mm[n].pa + mm[n].size - 1;
-		va_end = mm[n].va + mm[n].size - 1;
-		if (pa_end > max_pa)
-			max_pa = pa_end;
 		if (va_end > max_va)
 			max_va = va_end;
 	}
@@ -560,38 +545,45 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 #endif /*ARM32*/
 
 #ifdef ARM64
-static unsigned int calc_physical_addr_size_bits(uint64_t max_addr)
+static unsigned int get_physical_addr_size_bits(void)
 {
-	/* Physical address can't exceed 48 bits */
-	if (max_addr & ADDR_MASK_48_TO_63)
-		panic();
+	/*
+	 * Intermediate Physical Address Size.
+	 * 0b000      32 bits, 4GB.
+	 * 0b001      36 bits, 64GB.
+	 * 0b010      40 bits, 1TB.
+	 * 0b011      42 bits, 4TB.
+	 * 0b100      44 bits, 16TB.
+	 * 0b101      48 bits, 256TB.
+	 * 0b110      52 bits, 4PB (not supported)
+	 */
 
-	/* 48 bits address */
-	if (max_addr & ADDR_MASK_44_TO_47)
-		return TCR_PS_BITS_256TB;
+	COMPILE_TIME_ASSERT(CFG_CORE_ARM64_PA_BITS >= 32);
 
-	/* 44 bits address */
-	if (max_addr & ADDR_MASK_42_TO_43)
-		return TCR_PS_BITS_16TB;
+	if (CFG_CORE_ARM64_PA_BITS <= 32)
+		return TCR_PS_BITS_4GB;
 
-	/* 42 bits address */
-	if (max_addr & ADDR_MASK_40_TO_41)
-		return TCR_PS_BITS_4TB;
-
-	/* 40 bits address */
-	if (max_addr & ADDR_MASK_36_TO_39)
-		return TCR_PS_BITS_1TB;
-
-	/* 36 bits address */
-	if (max_addr & ADDR_MASK_32_TO_35)
+	if (CFG_CORE_ARM64_PA_BITS <= 36)
 		return TCR_PS_BITS_64GB;
 
-	return TCR_PS_BITS_4GB;
+	if (CFG_CORE_ARM64_PA_BITS <= 40)
+		return TCR_PS_BITS_1TB;
+
+	if (CFG_CORE_ARM64_PA_BITS <= 42)
+		return TCR_PS_BITS_4TB;
+
+	if (CFG_CORE_ARM64_PA_BITS <= 44)
+		return TCR_PS_BITS_16TB;
+
+	/* Physical address can't exceed 48 bits */
+	COMPILE_TIME_ASSERT(CFG_CORE_ARM64_PA_BITS <= 48);
+	/* CFG_CORE_ARM64_PA_BITS <= 48 */
+	return TCR_PS_BITS_256TB;
 }
 
 void core_init_mmu_regs(struct core_mmu_config *cfg)
 {
-	uint64_t ips = calc_physical_addr_size_bits(max_pa);
+	uint64_t ips = get_physical_addr_size_bits();
 	uint64_t mair = 0;
 	uint64_t tcr = 0;
 
@@ -617,28 +609,6 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 	 * TCR.AS = 0 => Same ASID size as in Aarch32/ARMv7
 	 */
 	cfg->tcr_el1 = tcr;
-}
-
-void core_mmu_set_max_pa(paddr_t pa)
-{
-	uint64_t tcr;
-	uint64_t ips;
-
-	if (pa <= max_pa)
-		return;
-
-	max_pa = pa;
-	ips = calc_physical_addr_size_bits(max_pa);
-	tcr = read_tcr_el1();
-	tcr &= ~(TCR_EL1_IPS_MASK << TCR_EL1_IPS_SHIFT);
-	tcr |= ips << TCR_EL1_IPS_SHIFT;
-	write_tcr_el1(tcr);
-	/*
-	 * MMU is already enabled at this stage and TCR state may be cached
-	 * in TLB.
-	 */
-	isb();
-	tlbi_all();
 }
 #endif /*ARM64*/
 

core/arch/arm/plat-rcar/conf.mk
@@ -8,6 +8,7 @@ $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_SCIF,y)
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 
 ifeq ($(PLATFORM_FLAVOR),salvator_h3)
 $(call force,CFG_TEE_CORE_NB_CORE,8)

core/arch/arm/plat-vexpress/conf.mk
@@ -52,6 +52,7 @@ CFG_SHMEM_START  ?= 0x83000000
 CFG_SHMEM_SIZE   ?= 0x00200000
 # DRAM1 is defined above 4G
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 endif
 
 ifeq ($(PLATFORM_FLAVOR),juno)
@@ -62,6 +63,7 @@ CFG_SHMEM_START  ?= 0xfee00000
 CFG_SHMEM_SIZE   ?= 0x00200000
 # DRAM1 is defined above 4G
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 CFG_CRYPTO_WITH_CE ?= y
 endif
 

commit bebe1f4931a9efc4db58961dce7eceb1604af990

core: mmu: fix check of memory physical end address
Correct test on memory physical end address that was not consistent
in use of core_mmu_check_end_pa().

This change fixes core_mmu_set_discovered_nsec_ddr() where
core_mmu_check_end_pa() was called with a byte length argument
decreased by one whereas it should provide the effective byte size
 of the memory area.

This change fixes core_mmu_check_end_pa() so that it computes
the end address as start address plus byte size minus one to
obtain the inclusive end address core_mmu_check_max_pa() expects
as input argument.

Fixes: 4518cdc1ff64 ("core: arm64: introduce CFG_CORE_ARM64_PA_BITS")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -390,7 +390,7 @@ void core_mmu_set_discovered_nsec_ddr(struct core_mmu_phys_mem *start,
 	discovered_nsec_ddr_nelems = num_elems;
 
 	if (!core_mmu_check_end_pa(m[num_elems - 1].addr,
-				   m[num_elems - 1].size - 1))
+				   m[num_elems - 1].size))
 		panic();
 }
 

core/arch/arm/mm/core_mmu_private.h
@@ -39,7 +39,7 @@ static inline bool core_mmu_check_end_pa(paddr_t pa, size_t len)
 {
 	paddr_t end_pa = 0;
 
-	if (ADD_OVERFLOW(pa, len, &end_pa))
+	if (ADD_OVERFLOW(pa, len - 1, &end_pa))
 		return false;
 	return core_mmu_check_max_pa(end_pa);
 }