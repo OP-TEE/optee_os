OP-TEE/optee_os

commit b1042535dc3ecf287d5d18a8a7f79eebad60b395

rpmb: function to block rpmb write per platform
Often the RPMB should only be written if the platform is in the correct
state where the Hardware Unique Key (HUK) for the platform works
correctly. As an example on imx platforms with HUK derivation from the
OTPMK, the OTPMK is only accessible from the secure or trusted state.
Add a function and weak implementation for platforms to indicate that
the device is in the correct state to allow RPMB key writes.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Cedric Neveux <cedric.neveux@nxp.com>

core/tee/tee_rpmb_fs.c
@@ -1020,6 +1020,11 @@ static TEE_Result tee_rpmb_write_and_verify_key(uint16_t dev_id)
 {
 	TEE_Result res;
 
+	if (!plat_rpmb_key_is_ready) {
+		DMSG("RPMB INIT: platform indicates RPMB key is not ready");
+		return TEE_ERROR_BAD_STATE;
+	}
+
 	DMSG("RPMB INIT: Writing Key value:");
 	DHEXDUMP(rpmb_ctx->key, RPMB_KEY_MAC_SIZE);
 
@@ -2614,4 +2619,9 @@ TEE_Result tee_rpmb_fs_raw_open(const char *fname, bool create,
 	}
 
 	return res;
+}
+
+bool __weak plat_rpmb_key_is_ready(void)
+{
+	return true;
 }

core/include/tee/tee_fs.h
@@ -58,6 +58,13 @@ extern const struct tee_file_operations rpmb_fs_ops;
 
 TEE_Result tee_rpmb_fs_raw_open(const char *fname, bool create,
 				struct tee_file_handle **fh);
+
+/**
+ * Weak function which can be overridden by platforms to indicate that the RPMB
+ * key is ready to be written. Defaults to true, platforms can return false to
+ * prevent a RPMB key write in the wrong state.
+ */
+bool plat_rpmb_key_is_ready(void);
 #endif
 
 #endif /*TEE_FS_H*/

commit 151950de9fc94378f82c62f2e80774218d459af9

rpmb: fix call to plat_rpmb_key_is_ready()
In tee_rpmb_write_and_verify_key() a call was recently added to check if
the RPMB key was ready to be retrieved. But the function wasn't called
in the new if statement, instead was just the address of the function
tested to be non-NULL. So with this patch add the missing () to make it
a function call.

Fixes: b1042535dc3e ("rpmb: function to block rpmb write per platform")
Reviewed-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_rpmb_fs.c
@@ -1020,7 +1020,7 @@ static TEE_Result tee_rpmb_write_and_verify_key(uint16_t dev_id)
 {
 	TEE_Result res;
 
-	if (!plat_rpmb_key_is_ready) {
+	if (!plat_rpmb_key_is_ready()) {
 		DMSG("RPMB INIT: platform indicates RPMB key is not ready");
 		return TEE_ERROR_BAD_STATE;
 	}