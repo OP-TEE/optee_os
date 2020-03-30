0d9e74defbc26a6c107598adfeabd59e7068f41b
Jens Wiklander <jens.wiklander@linaro.org>
2020-03-30 14:36:14 +0200

    core: ltc: remove Arm SHA1 CE routines
    Removes the Arm CE routines accelerating SHA1 in the LTC library.
    
    This will later be added in common code to be shared with other crypto
    libraries etc.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/hashes/sub.mk
+++ core/lib/libtomcrypt/src/hashes/sub.mk
@@ -1,14 +1,7 @@
 srcs-$(_CFG_CORE_LTC_MD5) += md5.c
 
 ifeq ($(_CFG_CORE_LTC_SHA1),y)
-SHA1_CE := $(call cfg-one-enabled, _CFG_CORE_LTC_SHA1_ARM32_CE _CFG_CORE_LTC_SHA1_ARM64_CE)
-ifeq ($(SHA1_CE),y)
-srcs-y += sha1_armv8a_ce.c
-srcs-$(_CFG_CORE_LTC_SHA1_ARM32_CE) += sha1_armv8a_ce_a32.S
-srcs-$(_CFG_CORE_LTC_SHA1_ARM64_CE) += sha1_armv8a_ce_a64.S
-else
 srcs-y += sha1.c
-endif
 endif
 
 subdirs-y += helper
