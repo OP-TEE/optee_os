9cd2e73b6a86b2946142306c135647c7f7a9ed85
Jens Wiklander <jens.wiklander@linaro.org>
2020-03-30 14:37:05 +0200

    core: optimize AArch32 AES-GCM routines
    In AArch32 there are not enough SIMD registers to make a fused GHASH and
    AES-CTR assembly function. But we can do better than using the default
    implementation. By carefully using the GHASH and AES primitive assembly
    functions there's some gain in performance.
    
    Before this patch throughput was around 12MiB/s to now a bit more than
    110MiB/s with blocks of 4096 bytes.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/crypto/aes-gcm-ce.c
+++ core/arch/arm/crypto/aes-gcm-ce.c
@@ -14,6 +14,8 @@
 #include <string.h>
 #include <types_ext.h>
 
+#include "aes_armv8a_ce.h"
+
 static void get_be_block(void *dst, const void *src)
 {
 	uint64_t *d = dst;
@@ -62,6 +64,18 @@ void internal_aes_gcm_set_key(struct internal_aes_gcm_state *state,
 	ghash_reflect(state->ghash_key.h4, h);
 }
 
+static void pmull_ghash_update(int num_blocks, uint64_t dg[2],
+			       const uint8_t *src,
+			       const struct internal_ghash_key *ghash_key,
+			       const uint8_t *head)
+{
+#ifdef CFG_HWSUPP_PMULT_64
+	pmull_ghash_update_p64(num_blocks, dg, src, ghash_key, head);
+#else
+	pmull_ghash_update_p8(num_blocks, dg, src, ghash_key, head);
+#endif
+}
+
 void internal_aes_gcm_ghash_update(struct internal_aes_gcm_state *state,
 				   const void *head, const void *data,
 				   size_t num_blocks)
@@ -73,11 +87,8 @@ void internal_aes_gcm_ghash_update(struct internal_aes_gcm_state *state,
 
 	vfp_state = thread_kernel_enable_vfp();
 
-#ifdef CFG_HWSUPP_PMULT_64
-	pmull_ghash_update_p64(num_blocks, dg, data, &state->ghash_key, head);
-#else
-	pmull_ghash_update_p8(num_blocks, dg, data, &state->ghash_key, head);
-#endif
+	pmull_ghash_update(num_blocks, dg, data, &state->ghash_key, head);
+
 	thread_kernel_disable_vfp(vfp_state);
 
 	put_be_block(state->hash_state, dg);
@@ -164,3 +175,66 @@ internal_aes_gcm_update_payload_blocks(struct internal_aes_gcm_state *state,
 	}
 }
 #endif /*ARM64*/
+
+#ifdef ARM32
+static void encrypt_pl(struct internal_aes_gcm_state *state,
+		       const struct internal_aes_gcm_key *ek, uint64_t dg[2],
+		       const uint8_t *src, size_t num_blocks, uint8_t *dst)
+{
+	void *buf_cryp = state->buf_cryp;
+
+	while (num_blocks) {
+		ce_aes_xor_block(buf_cryp, buf_cryp, src);
+
+		pmull_ghash_update(1, dg, buf_cryp, &state->ghash_key, NULL);
+		memcpy(dst, buf_cryp, TEE_AES_BLOCK_SIZE);
+
+		ce_aes_ecb_encrypt(buf_cryp, (const uint8_t *)state->ctr,
+				   (const uint8_t *)ek->data, ek->rounds,
+				   1, 1);
+		internal_aes_gcm_inc_ctr(state);
+
+		src += TEE_AES_BLOCK_SIZE;
+		dst += TEE_AES_BLOCK_SIZE;
+		num_blocks--;
+	}
+}
+
+static void decrypt_pl(struct internal_aes_gcm_state *state,
+		       const struct internal_aes_gcm_key *ek, uint64_t dg[2],
+		       const uint8_t *src, size_t num_blocks, uint8_t *dst)
+{
+	while (num_blocks) {
+		ce_aes_ctr_encrypt(dst, src, (const uint8_t *)ek->data,
+				   ek->rounds, 1, (uint8_t *)state->ctr, 1);
+		pmull_ghash_update(1, dg, src, &state->ghash_key, NULL);
+
+		src += TEE_AES_BLOCK_SIZE;
+		dst += TEE_AES_BLOCK_SIZE;
+		num_blocks--;
+	}
+}
+
+/* Overriding the __weak function */
+void
+internal_aes_gcm_update_payload_blocks(struct internal_aes_gcm_state *state,
+				       const struct internal_aes_gcm_key *ek,
+				       TEE_OperationMode mode, const void *src,
+				       size_t num_blocks, void *dst)
+{
+	uint64_t dg[2] = { 0 };
+	uint32_t vfp_state = 0;
+
+	assert(!state->buf_pos && num_blocks);
+	get_be_block(dg, state->hash_state);
+	vfp_state = thread_kernel_enable_vfp();
+
+	if (mode == TEE_MODE_ENCRYPT)
+		encrypt_pl(state, ek, dg, src, num_blocks, dst);
+	else
+		decrypt_pl(state, ek, dg, src, num_blocks, dst);
+
+	thread_kernel_disable_vfp(vfp_state);
+	put_be_block(state->hash_state, dg);
+}
+#endif
