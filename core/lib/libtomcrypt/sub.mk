40277c5b4162aef7a1060c167b6804cb40cafc49
Jens Wiklander <jens.wiklander@linaro.org>
2020-03-30 14:36:20 +0200

    core: ltc: use AES crypto accelerated routines
    Uses the recently provided accelerated AES crypto routines in LTC.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/sub.mk
+++ core/lib/libtomcrypt/sub.mk
@@ -125,6 +125,7 @@ srcs-$(_CFG_CORE_LTC_ECC) += ecc.c
 srcs-$(_CFG_CORE_LTC_RSA) += rsa.c
 srcs-$(_CFG_CORE_LTC_DH) += dh.c
 srcs-$(_CFG_CORE_LTC_AES) += aes.c
+srcs-$(_CFG_CORE_LTC_AES_ACCEL) += aes_accel.c
 srcs-$(_CFG_CORE_LTC_SM2_DSA) += sm2-dsa.c
 srcs-$(_CFG_CORE_LTC_SM2_PKE) += sm2-pke.c
 srcs-$(_CFG_CORE_LTC_SM2_KEP) += sm2-kep.c
