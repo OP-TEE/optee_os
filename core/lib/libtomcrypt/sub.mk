a828d70fe87df96e1206f6ffedcb66d2cc59ce67
Jens Wiklander <jens.wiklander@linaro.org>
2020-04-02 14:51:18 +0200

    core: ltc: use SHA-256 crypto accelerated function
    Uses the recently provided accelerated SHA-256 function in LTC.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/sub.mk
+++ core/lib/libtomcrypt/sub.mk
@@ -127,6 +127,9 @@ srcs-$(_CFG_CORE_LTC_DH) += dh.c
 srcs-$(_CFG_CORE_LTC_AES) += aes.c
 srcs-$(_CFG_CORE_LTC_AES_ACCEL) += aes_accel.c
 srcs-$(_CFG_CORE_LTC_SHA1_ACCEL) += sha1_accel.c
+ifeq ($(_CFG_CORE_LTC_SHA256_DESC),y)
+srcs-$(_CFG_CORE_LTC_SHA256_ACCEL) += sha256_accel.c
+endif
 srcs-$(_CFG_CORE_LTC_SM2_DSA) += sm2-dsa.c
 srcs-$(_CFG_CORE_LTC_SM2_PKE) += sm2-pke.c
 srcs-$(_CFG_CORE_LTC_SM2_KEP) += sm2-kep.c
