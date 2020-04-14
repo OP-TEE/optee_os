OP-TEE/optee_os

commit d9c569c9c765ed6f0fe6e131d1a33c9c32c0ba08

plat-stm32mp1: prepare for SiP SMC services
Implement secure monitor platform handlers foundations for
platform stm32mp1 to handle SiP SMC services.

Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-stm32mp1/nsec-service/stm32mp1_svc_setup.c
@@ -1,0 +1,53 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2017-2018, STMicroelectronics
+ */
+
+#include <kernel/thread.h>
+#include <sm/optee_smc.h>
+#include <sm/sm.h>
+
+#include "stm32mp1_smc.h"
+
+#define SM_NSEC_CTX_OFFSET(_reg)	(offsetof(struct sm_nsec_ctx, _reg) - \
+					 offsetof(struct sm_nsec_ctx, r0))
+#define SMC_ARGS_OFFSET(_reg)		offsetof(struct thread_smc_args, _reg)
+
+static enum sm_handler_ret sip_service(struct sm_ctx *ctx __unused,
+				       struct thread_smc_args *args)
+{
+	switch (OPTEE_SMC_FUNC_NUM(args->a0)) {
+	case STM32_SIP_SVC_FUNC_CALL_COUNT:
+		args->a0 = STM32_SIP_SVC_FUNCTION_COUNT;
+		break;
+	case STM32_SIP_SVC_FUNC_VERSION:
+		args->a0 = STM32_SIP_SVC_VERSION_MAJOR;
+		args->a1 = STM32_SIP_SVC_VERSION_MINOR;
+		break;
+	case STM32_SIP_SVC_FUNC_UID:
+		args->a0 = STM32_SIP_SVC_UID_0;
+		args->a1 = STM32_SIP_SVC_UID_1;
+		args->a2 = STM32_SIP_SVC_UID_2;
+		args->a3 = STM32_SIP_SVC_UID_3;
+		break;
+	default:
+		return SM_HANDLER_PENDING_SMC;
+	}
+
+	return SM_HANDLER_SMC_HANDLED;
+}
+
+enum sm_handler_ret sm_platform_handler(struct sm_ctx *ctx)
+{
+	struct thread_smc_args *args = (void *)&ctx->nsec.r0;
+
+	if (!OPTEE_SMC_IS_FAST_CALL(args->a0))
+		return SM_HANDLER_PENDING_SMC;
+
+	switch (OPTEE_SMC_OWNER_NUM(args->a0)) {
+	case OPTEE_SMC_OWNER_SIP:
+		return sip_service(ctx, args);
+	default:
+		return SM_HANDLER_PENDING_SMC;
+	}
+}

core/arch/arm/plat-stm32mp1/conf.mk
@@ -15,6 +15,7 @@ $(call force,CFG_PM_STUBS,y)
 $(call force,CFG_PSCI_ARM32,y)
 $(call force,CFG_SECONDARY_INIT_CNTFRQ,y)
 $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_SM_PLATFORM_HANDLER,y)
 $(call force,CFG_WITH_SOFTWARE_PRNG,y)
 
 ifneq ($(filter $(CFG_EMBED_DTB_SOURCE_FILE),$(flavorlist-512M)),)

core/arch/arm/plat-stm32mp1/nsec-service/stm32mp1_smc.h
@@ -1,0 +1,77 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2016-2018, STMicroelectronics
+ * Copyright (c) 2018, Linaro Limited
+ */
+#ifndef __STM32MP1_SMC_H__
+#define __STM32MP1_SMC_H__
+
+/*
+ * SIP Functions
+ */
+#define STM32_SIP_SVC_VERSION_MAJOR	0x0
+#define STM32_SIP_SVC_VERSION_MINOR	0x1
+
+#define STM32_SIP_SVC_FUNCTION_COUNT	0x0
+
+/*
+ * SMC function IDs for STM32 Service queries
+ * STM32 SMC services use the space between 0x82000000 and 0x8200FFFF
+ * like this is defined in SMC calling Convention by ARM
+ * for SiP (Silicon Partner)
+ * http://infocenter.arm.com/help/topic/com.arm.doc.den0028a/index.html
+ */
+
+/*
+ * SIP function STM32_SIP_FUNC_CALL_COUNT
+ *
+ * Argument a0: (input) SMCC ID
+ *		(output) Count of defined function IDs
+ */
+#define STM32_SIP_SVC_FUNC_CALL_COUNT		0xff00
+
+/*
+ * Return the following UID if using API specified in this file without
+ * further extensions:
+ * 50aa78a7-9bf4-4a14-8a5e-264d5994c214.
+ *
+ * Represented in 4 32-bit words in STM32_SIP_UID_0, STM32_SIP_UID_1,
+ * STM32_SIP_UID_2, STM32_SIP_UID_3.
+ */
+#define STM32_SIP_SVC_UID_0			0x50aa78a7
+#define STM32_SIP_SVC_UID_1			0x9bf44a14
+#define STM32_SIP_SVC_UID_2			0x8a5e264d
+#define STM32_SIP_SVC_UID_3			0x5994c214
+
+/*
+ * SIP function STM32_SIP_SVC_FUNC_UID
+ *
+ * Argument a0: (input) SMCC ID
+ *		(output) Lowest 32bit of the stm32mp1 SIP service UUID
+ * Argument a1: (output) Next 32bit of the stm32mp1 SIP service UUID
+ * Argument a2: (output) Next 32bit of the stm32mp1 SIP service UUID
+ * Argument a3: (output) Last 32bit of the stm32mp1 SIP service UUID
+ */
+#define STM32_SIP_SVC_FUNC_UID			0xff01
+
+/*
+ * SIP function STM32_SIP_FUNC_VERSION
+ *
+ * Argument a0: (input) SMCC ID
+ *		(output) STM32 SIP service major
+ * Argument a1: (output) STM32 SIP service minor
+ */
+#define STM32_SIP_SVC_FUNC_VERSION		0xff03
+
+/*
+ * SIP function STM32_SIP_SVC_FUNC_SCMI_AGENT0
+ * SIP function STM32_SIP_SVC_FUNC_SCMI_AGENT1
+ *
+ * Process SCMI message pending in related SCMI shared memory buffer.
+ *
+ * Argument a0: (input) SMCC ID
+ */
+#define STM32_SIP_SVC_FUNC_SCMI_AGENT0		0x2000
+#define STM32_SIP_SVC_FUNC_SCMI_AGENT1		0x2001
+
+#endif /* __STM32MP1_SMC_H__*/

core/arch/arm/plat-stm32mp1/nsec-service/sub.mk
@@ -1,0 +1,3 @@
+global-incdirs-y += .
+
+srcs-y += stm32mp1_svc_setup.c

core/arch/arm/plat-stm32mp1/sub.mk
@@ -5,4 +5,5 @@ srcs-y += reset.S
 srcs-y += shared_resources.c
 
 subdirs-y += drivers
+subdirs-y += nsec-service
 subdirs-y += pm

commit 478682d21c009c2111e3b3aed4d3ed0635cc4f40

plat-stm32mp1: remove useless macros in SMC SiP handler
Remove unused macros in stm32mp1 platform SMC SiP handler source file.

Fixes: d9c569c9c765 ("plat-stm32mp1: prepare for SiP SMC services")
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-stm32mp1/nsec-service/stm32mp1_svc_setup.c
@@ -9,10 +9,6 @@
 
 #include "stm32mp1_smc.h"
 
-#define SM_NSEC_CTX_OFFSET(_reg)	(offsetof(struct sm_nsec_ctx, _reg) - \
-					 offsetof(struct sm_nsec_ctx, r0))
-#define SMC_ARGS_OFFSET(_reg)		offsetof(struct thread_smc_args, _reg)
-
 static enum sm_handler_ret sip_service(struct sm_ctx *ctx __unused,
 				       struct thread_smc_args *args)
 {