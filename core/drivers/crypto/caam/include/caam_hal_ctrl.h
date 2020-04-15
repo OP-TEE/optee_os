012e31cd7e19dd8491b6fccaccae766c913ea181
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2020-04-15 12:18:47 +0200

    caam: hal: add function to increment priblob
    Add a common hal function to increment the priblob. This can
    conditionally be enabled with CFG_CAAM_INC_PRIBLOB to use this as a
    workaround on SoCs which can't distinguish secure and normal world in
    the CAAM, i.e. i.MX6Q.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Clement Faure <clement.faure@nxp.com>

--- core/drivers/crypto/caam/include/caam_hal_ctrl.h
+++ core/drivers/crypto/caam/include/caam_hal_ctrl.h
@@ -51,4 +51,11 @@ bool caam_hal_ctrl_splitkey_support(vaddr_t baseaddr);
  * @baseaddr  Controller base address
  */
 uint8_t caam_hal_ctrl_era(vaddr_t baseaddr);
+
+/*
+ * Increment the CAAM PRIBLOB field
+ *
+ * @baseaddr  Controller base address
+ */
+void caam_hal_ctrl_inc_priblob(vaddr_t baseaddr);
 #endif /* __CAAM_HAL_CTRL_H__ */
