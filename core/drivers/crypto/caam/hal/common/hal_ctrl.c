8e97f8b68ee275349b59463de08a3ca799510ef9
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2020-04-15 12:18:47 +0200

    caam: hal: add function to increment priblob
    Add a common hal function to increment the priblob. This can
    conditionally be enabled with CFG_CAAM_INC_PRIBLOB to use this as a
    workaround on SoCs which can't distinguish secure and normal world in
    the CAAM, i.e. i.MX6Q.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Clement Faure <clement.faure@nxp.com>

--- core/drivers/crypto/caam/hal/common/hal_ctrl.c
+++ core/drivers/crypto/caam/hal/common/hal_ctrl.c
@@ -7,8 +7,11 @@
  */
 #include <caam_hal_ctrl.h>
 #include <caam_io.h>
+#include <caam_trace.h>
+#include <config.h>
 #include <registers/ctrl_regs.h>
 #include <registers/version_regs.h>
+#include <kernel/panic.h>
 
 uint8_t caam_hal_ctrl_jrnum(vaddr_t baseaddr)
 {
@@ -63,4 +66,39 @@ uint8_t caam_hal_ctrl_era(vaddr_t baseaddr)
 	val = io_caam_read32(baseaddr + CCBVID);
 
 	return GET_CCBVID_CAAM_ERA(val);
+}
+
+#define PRIBLOB_MASK	GENMASK_32(1, 0)
+
+void caam_hal_ctrl_inc_priblob(vaddr_t baseaddr)
+{
+	uint32_t val = 0;
+	uint32_t blob = 0;
+
+	if (!IS_ENABLED(CFG_CAAM_INC_PRIBLOB))
+		return;
+
+	val = io_caam_read32(baseaddr + SCFGR);
+	val &= PRIBLOB_MASK;
+	CTRL_TRACE("Reading CAAM PRIBLOB: 0x%"PRIx32, val);
+
+	if (val == 0 || val == 2)
+		blob = val + 1;
+	else if (val == 1)
+		blob = val + 2;
+	else
+		panic("Error locking PRIBLOB, PRIBLOB =3");
+
+	CTRL_TRACE("New CAAM PRIBLOB value: 0x%"PRIx32, blob);
+
+	val = io_caam_read32(baseaddr + SCFGR);
+	val |= blob;
+	io_caam_write32(baseaddr + SCFGR, val);
+
+	val = io_caam_read32(baseaddr + SCFGR);
+	val &= PRIBLOB_MASK;
+	CTRL_TRACE("Checking: CAAM PRIBLOB: 0x%"PRIx32 " want: 0x%"PRIx32, val,
+		   blob);
+	if (val != blob)
+		panic("Written PRIBLOB and read PRIBLOB do not match!");
 }
