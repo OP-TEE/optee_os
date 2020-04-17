38dde91e13dd8b5c55b5d6dd73c48130dc63d419
Ruchika Gupta <ruchika.gupta@nxp.com>
2020-04-17 19:59:54 +0530

    drivers: caam: Add register map changes for Era 10
    Era 10 changes the register map.
    
    The updates that affect the drivers:
    -new version registers are added
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
    Reviewed-by: Sahil Malhotra <sahil.malhotra@nxp.com>
    Reviewed-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/caam/hal/common/hal_ctrl.c
+++ core/drivers/crypto/caam/hal/common/hal_ctrl.c
@@ -13,30 +13,59 @@
 #include <registers/version_regs.h>
 #include <kernel/panic.h>
 
+uint8_t caam_hal_ctrl_era(vaddr_t baseaddr)
+{
+	/* Read the number of instance */
+	uint32_t val = io_caam_read32(baseaddr + CCBVID);
+
+	return GET_CCBVID_CAAM_ERA(val);
+}
+
 uint8_t caam_hal_ctrl_jrnum(vaddr_t baseaddr)
 {
 	uint32_t val = 0;
+	uint8_t jrnum = 0;
 
-	val = io_caam_read32(baseaddr + CHANUM_MS);
+	if (caam_hal_ctrl_era(baseaddr) < 10) {
+		val = io_caam_read32(baseaddr + CHANUM_MS);
+		jrnum = GET_CHANUM_MS_JRNUM(val);
+	} else {
+		val = io_caam_read32(baseaddr + JR_VERSION);
+		jrnum = GET_JR_VERSION_JRNUM(val);
+	}
 
-	return GET_CHANUM_MS_JRNUM(val);
+	return jrnum;
 }
 
 uint8_t caam_hal_ctrl_hash_limit(vaddr_t baseaddr)
 {
 	uint32_t val = 0;
 
-	/* Read the number of instance */
-	val = io_caam_read32(baseaddr + CHANUM_LS);
+	if (caam_hal_ctrl_era(baseaddr) < 10) {
+		/* Read the number of instance */
+		val = io_caam_read32(baseaddr + CHANUM_LS);
 
-	if (GET_CHANUM_LS_MDNUM(val)) {
-		/* Hashing is supported */
-		val = io_caam_read32(baseaddr + CHAVID_LS);
-		val &= BM_CHAVID_LS_MDVID;
-		if (val == CHAVID_LS_MDVID_LP256)
-			return TEE_MAIN_ALGO_SHA256;
+		if (GET_CHANUM_LS_MDNUM(val)) {
+			/* Hashing is supported */
+			val = io_caam_read32(baseaddr + CHAVID_LS);
+			val &= BM_CHAVID_LS_MDVID;
+			if (val == CHAVID_LS_MDVID_LP256)
+				return TEE_MAIN_ALGO_SHA256;
 
-		return TEE_MAIN_ALGO_SHA512;
+			return TEE_MAIN_ALGO_SHA512;
+		}
+	} else {
+		/* Read the number of instance */
+		val = io_caam_read32(baseaddr + MDHA_VERSION);
+
+		if (GET_MDHA_VERSION_MDNUM(val)) {
+			/* Hashing is supported */
+			val &= BM_MDHA_VERSION_MDVID;
+			if (val == MDHA_VERSION_MDVID_LP256)
+				return TEE_MAIN_ALGO_SHA256;
+
+			return TEE_MAIN_ALGO_SHA512;
+		}
 	}
 
 	return UINT8_MAX;
@@ -52,20 +81,17 @@ bool caam_hal_ctrl_splitkey_support(vaddr_t baseaddr)
 uint8_t caam_hal_ctrl_pknum(vaddr_t baseaddr)
 {
 	uint32_t val = 0;
+	uint8_t pknum = 0;
 
-	val = io_caam_read32(baseaddr + CHANUM_LS);
+	if (caam_hal_ctrl_era(baseaddr) < 10) {
+		val = io_caam_read32(baseaddr + CHANUM_LS);
+		pknum = GET_CHANUM_LS_PKNUM(val);
+	} else {
+		val = io_caam_read32(baseaddr + PKHA_VERSION);
+		pknum = GET_PKHA_VERSION_PKNUM(val);
+	}
 
-	return GET_CHANUM_LS_PKNUM(val);
-}
-
-uint8_t caam_hal_ctrl_era(vaddr_t baseaddr)
-{
-	uint32_t val = 0;
-
-	/* Read the number of instance */
-	val = io_caam_read32(baseaddr + CCBVID);
-
-	return GET_CCBVID_CAAM_ERA(val);
+	return pknum;
 }
 
 #define PRIBLOB_MASK	GENMASK_32(1, 0)
