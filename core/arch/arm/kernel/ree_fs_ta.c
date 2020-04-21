9bf51c11b3fbce089a38ea3e0add1bd58670bdab
Jerome Forissier <jerome@forissier.org>
2020-04-21 16:06:46 +0200

    core: ree_fs_ta.c: initialize structs with '= { };'
    Initialize structs with '= { };' rather than '= {0};' because (1) it is
    the recommended style and (2) it fixes the following warning with Clang
    9:
    
       CC      out/arm/core/arch/arm/kernel/ree_fs_ta.o
     core/arch/arm/kernel/ree_fs_ta.c:325:40: warning: suggest braces around initialization of subobject [-Wmissing-braces]
             struct shdr_bootstrap_ta hdr_entry = {0};
                                                   ^
                                                   {}
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/kernel/ree_fs_ta.c
+++ core/arch/arm/kernel/ree_fs_ta.c
@@ -322,14 +322,14 @@ out:
 
 static TEE_Result check_update_version(struct shdr_bootstrap_ta *hdr)
 {
-	struct shdr_bootstrap_ta hdr_entry = {0};
+	struct shdr_bootstrap_ta hdr_entry = { };
 	const struct tee_file_operations *ops = NULL;
 	struct tee_file_handle *fh = NULL;
 	TEE_Result res = TEE_SUCCESS;
 	bool entry_found = false;
 	size_t len = 0;
 	unsigned int i = 0;
-	struct ta_ver_db_hdr db_hdr = {0};
+	struct ta_ver_db_hdr db_hdr = { };
 	struct tee_pobj pobj = {
 		.obj_id = (void *)ta_ver_db_obj_id,
 		.obj_id_len = sizeof(ta_ver_db_obj_id)
