5946a07dce112e73a652c25b54604ee91bc89763
Etienne Carriere <etienne.carriere@linaro.org>
2020-04-28 20:29:15 +0200

    core: sm: support SMCCC v1.1 specification
    SMCCC v1.1 specification: support defined function IDs with weak
    handlers platform can override, as other PSCI function handler.
    We could state we support v1.2 but Linux kernel v5.7-rc1 expects
    strict v1.1 support.
    
    unsigned long arm_arch_version(void);
        returns SMCCC_V_1_1
    
    unsigned long arm_arch_feature(unsigned long a1);
        default supports version only
    
    unsigned long arm_arch_soc_id(void);
    unsigned long arm_arch_workaround_1(void);
    unsigned long arm_arch_workaround_2(void);
        default return ARM_SMCCC_RET_NOT_SUPPORTED
    
    This helper is needed by Linux kernel (U-Boot) drivers that rely on
    arm_smccc_v1_1() supports.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/include/sm/std_smc.h
+++ core/arch/arm/include/sm/std_smc.h
@@ -10,6 +10,21 @@
 /*                                      0x8400ff02 is reserved */
 #define ARM_STD_SVC_VERSION		0x8400ff03
 
+#define ARM_SMCCC_VERSION		0x80000000
+#define ARM_SMCCC_ARCH_FEATURES		0x80000001
+#define ARM_SMCCC_ARCH_SOC_ID		0x80000002
+#define ARM_SMCCC_ARCH_WORKAROUND_1	0x80008000
+#define ARM_SMCCC_ARCH_WORKAROUND_2	0x80007fff
+
+#define ARM_SMCCC_RET_SUCCESS		0
+#define ARM_SMCCC_RET_NOT_SUPPORTED	0xffffffff
+#define ARM_SMCCC_RET_NOT_REQUIRED	0xfffffffe
+#define ARM_SMCCC_RET_INVALID_PARAMETER	0xfffffffd
+
+#define SMCCC_V_1_0			0x10000
+#define SMCCC_V_1_1			0x10001
+#define SMCCC_V_1_2			0x10002
+
 /* ARM Standard Service Calls version numbers */
 #define STD_SVC_VERSION_MAJOR		0x0
 #define STD_SVC_VERSION_MINOR		0x1
