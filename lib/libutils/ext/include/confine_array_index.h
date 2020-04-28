OP-TEE/optee_os

commit 2b6dd0df52b4d44f568892310f90409a595ce0af

confine_array_index.h: add A32 and T32 versions of confine_array_index()
Adds inline assembly implementations for the A32 and T32 instruction
sets. The implementation is based on __load_no_speculate1() in
<speculation_barrier.h>.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Tested-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/ext/include/confine_array_index.h
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2020 Linaro Limited */
 // Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -82,6 +83,39 @@ static inline size_t confine_array_index(size_t index, size_t size) {
   return safe_index;
 }
 #endif
+#ifdef __arm__
+static inline size_t confine_array_index(size_t index, size_t size)
+{
+	size_t safe_index = 0;
+
+	/*
+	 * For the ARMv7/AArch32 case we're basing the select and barrier
+	 * code on __load_no_speculate1() in <speculation_barrier.h> as we
+	 * lack the csel instruction.
+	 */
+
+#ifdef __thumb2__
+      asm volatile (
+	".syntax unified\n"
+	"cmp	%1, %2\n" /* %1 holds the unsanitized index */
+	"it	cs\n"
+	"movcs	%1, #0\n"
+	".inst.n 0xf3af\t@ CSDB\n"
+	".inst.n 0x8014\t@ CSDB"
+	: "=r" (safe_index) : "r" (index), "r" (size) : "cc");
+#else
+      asm volatile (
+	".syntax unified\n"
+	"cmp	%1, %2\n" /* %1 holds the unsanitized index */
+	"movcs	%1, #0\n"
+	".inst	0xe320f014\t@ CSDB"
+	: "=r" (safe_index) : "r" (index), "r" (size) : "cc");
+#endif
+
+	return safe_index;
+}
+#endif /* __arm__ */
+
 #ifdef __x86_64__
 static inline size_t confine_array_index(size_t index, size_t size) {
   size_t safe_index = 0;

commit a888902f8ae1a3b097b4a87c6d961b59a628fc6f

libutils: confine_array_index: fix 32bit modes implementation
Fix implementation of confine_array_index() for 32bit Arm and Thumb2
modes as previous implementation did not set the return value
explicitly and relied on luck with the layout of the machine code
to return the correct value.

Fixes: https://github.com/OP-TEE/optee_os/issues/3799
Fixes: 2b6dd0df52b4 ("confine_array_index.h: add A32 and T32 versions of confine_array_index()")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (qemu,qemu_v8)
Acked-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>

lib/libutils/ext/include/confine_array_index.h
@@ -86,7 +86,7 @@ static inline size_t confine_array_index(size_t index, size_t size) {
 #ifdef __arm__
 static inline size_t confine_array_index(size_t index, size_t size)
 {
-	size_t safe_index = 0;
+	size_t ret_val = index;
 
 	/*
 	 * For the ARMv7/AArch32 case we're basing the select and barrier
@@ -97,22 +97,22 @@ static inline size_t confine_array_index(size_t index, size_t size)
 #ifdef __thumb2__
       asm volatile (
 	".syntax unified\n"
-	"cmp	%1, %2\n" /* %1 holds the unsanitized index */
+	"cmp	%0, %1\n"
 	"it	cs\n"
-	"movcs	%1, #0\n"
+	"movcs	%0, #0\n"
 	".inst.n 0xf3af\t@ CSDB\n"
 	".inst.n 0x8014\t@ CSDB"
-	: "=r" (safe_index) : "r" (index), "r" (size) : "cc");
+	: "+r" (ret_val) : "r" (size) : "cc");
 #else
       asm volatile (
 	".syntax unified\n"
-	"cmp	%1, %2\n" /* %1 holds the unsanitized index */
-	"movcs	%1, #0\n"
+	"cmp	%0, %1\n" /* %0 holds the unsanitized index */
+	"movcs	%0, #0\n"
 	".inst	0xe320f014\t@ CSDB"
-	: "=r" (safe_index) : "r" (index), "r" (size) : "cc");
+	: "+r" (ret_val) : "r" (size) : "cc");
 #endif
 
-	return safe_index;
+	return ret_val;
 }
 #endif /* __arm__ */
 