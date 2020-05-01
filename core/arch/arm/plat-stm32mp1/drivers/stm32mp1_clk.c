ed835ce4da014885128ee2fcca38678cecd8e3ce
Etienne Carriere <etienne.carriere@st.com>
2020-05-01 14:51:12 +0200

    plat-stm32mp1: clock: enable some secure clocks at initialization
    With this change some system clocks are enabled by Core at
    boot time and have a reference counter synchronized with
    the clock hardware state. RTCAPB must be enabled for secondary
    cores to boot, if any.
    
    This change also ensures these secure clocks are derived from
    secure clocks.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-stm32mp1/drivers/stm32mp1_clk.c
+++ core/arch/arm/plat-stm32mp1/drivers/stm32mp1_clk.c
@@ -1285,6 +1285,24 @@ static void get_osc_freq_from_dt(void *fdt)
 	}
 }
 
+static void enable_static_secure_clocks(void)
+{
+	unsigned int idx = 0;
+	static const unsigned long secure_enable[] = {
+		DDRC1, DDRC1LP, DDRC2, DDRC2LP, DDRPHYC, DDRPHYCLP, DDRCAPB,
+		AXIDCG, DDRPHYCAPB, DDRPHYCAPBLP, TZPC, TZC1, TZC2, STGEN_K,
+		BSEC,
+	};
+
+	for (idx = 0; idx < ARRAY_SIZE(secure_enable); idx++) {
+		stm32_clock_enable(secure_enable[idx]);
+		stm32mp_register_clock_parents_secure(secure_enable[idx]);
+	}
+
+	if (CFG_TEE_CORE_NB_CORE > 1)
+		stm32_clock_enable(RTCAPB);
+}
+
 static TEE_Result stm32mp1_clk_early_init(void)
 {
 	void *fdt = NULL;
@@ -1346,6 +1364,8 @@ static TEE_Result stm32mp1_clk_early_init(void)
 
 	if (ignored != 0)
 		IMSG("DT clock tree configurations were ignored");
+
+	enable_static_secure_clocks();
 
 	return TEE_SUCCESS;
 }
