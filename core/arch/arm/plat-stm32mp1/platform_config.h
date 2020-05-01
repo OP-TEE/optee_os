59c253f92c6c09f4aefb0f841a280663b399e0d4
Etienne Carriere <etienne.carriere@st.com>
2020-05-01 17:56:15 +0200

    plat-stm32mp1: check TZC400 configuration
    Core checks TZC400 configuration during initialization to ensure
    DDR firewall expectations are satisfied.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-stm32mp1/platform_config.h
+++ core/arch/arm/plat-stm32mp1/platform_config.h
@@ -41,6 +41,7 @@
 #define SPI6_BASE			0x5c001000
 #define SYSRAM_BASE			0x2ffc0000
 #define TAMP_BASE			0x5c00a000
+#define TZC_BASE			0x5c006000
 #define UART1_BASE			0x5c000000
 #define UART2_BASE			0x4000e000
 #define UART3_BASE			0x4000f000
@@ -116,6 +117,21 @@
 
 /* TAMP resources */
 #define TAMP_BKP_REGISTER_OFF		0x100
+
+/* TZC resources */
+#define STM32MP1_IRQ_TZC		36
+
+#define STM32MP1_TZC_A7_ID		0
+#define STM32MP1_TZC_M4_ID		1
+#define STM32MP1_TZC_LCD_ID		3
+#define STM32MP1_TZC_GPU_ID		4
+#define STM32MP1_TZC_MDMA_ID		5
+#define STM32MP1_TZC_DMA_ID		6
+#define STM32MP1_TZC_USB_HOST_ID	7
+#define STM32MP1_TZC_USB_OTG_ID		8
+#define STM32MP1_TZC_SDMMC_ID		9
+#define STM32MP1_TZC_ETH_ID		10
+#define STM32MP1_TZC_DAP_ID		15
 
 /* USART/UART resources */
 #define USART1_BASE			UART1_BASE
