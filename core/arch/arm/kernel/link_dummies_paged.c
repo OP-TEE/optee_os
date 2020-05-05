a9a03ac4f271513310c0fcf39aab09c3ec7335a2
Etienne Carriere <etienne.carriere@linaro.org>
2020-05-05 18:23:05 +0200

    core: boot: relax init sections from DT related resources
    Move the initialization sequences that deal with device tree parsing
    support out of the text_init and rodata_init sections to relax memory
    pressure on them. To do so this change splits init_primary_helper()
    in 2 helper functions: generic_init_primary() for resources expected
    in the init sections and paged_init_primary() for resources that move
    the pageable sections.
    
    Updates inline comments to state generic_boot_init_primary() lies in
    the init area, not in the unpaged area as wrongly stated prior this
    change.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (stm32mp1)

--- core/arch/arm/kernel/link_dummies_paged.c
+++ core/arch/arm/kernel/link_dummies_paged.c
@@ -3,6 +3,7 @@
  * Copyright (c) 2017, Linaro Limited
  */
 #include <compiler.h>
+#include <kernel/generic_boot.h>
 #include <kernel/thread.h>
 #include <kernel/wait_queue.h>
 #include <sm/tee_mon.h>
@@ -20,6 +21,11 @@ thread_svc_handler(struct thread_svc_regs *regs __unused)
 TEE_Result __section(".text.dummy.init_teecore") init_teecore(void)
 {
 	return TEE_SUCCESS;
+}
+
+void __section(".text.dummy.paged_init_primary")
+paged_init_primary(unsigned long fdt __unused)
+{
 }
 
 uint32_t __section(".text.dummy.__thread_std_smc_entry")
