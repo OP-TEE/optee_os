827cb2cba095f52dfaad30a56f870c81d9ba57f1
Jens Wiklander <jens.wiklander@linaro.org>
2020-05-15 12:06:19 +0200

    core: make core_is_buffer_*() paddr_t compatible
    The core_is_buffer_*() helpers are sometimes used with physical
    addresses (type paddr_t). This can cause problem on platforms where
    sizeof(paddr_t) > sizeof(vaddr_t), that is on ARM32 systems with
    CFG_CORE_LARGE_PHYS_ADDR=y. The FVP platform compiled for AArch32 is one
    such system which as a consequence fails with:
    E/TC:0 0 check_phys_mem_is_outside:335 Non-sec mem (0x880000000:0x180000000) ove
    rlaps map (type 12 0xff000000:0x1000000)
    E/TC:0 0 Panic at core/arch/arm/mm/core_mmu.c:336 <check_phys_mem_is_outside>
    
    This patch fixes this problem by taking input addresses as paddr_t and
    sizes as paddr_ssize_t instead. The wrapper macros which did some
    automatic casting removed. The requires updates at some of the places
    where these functions are called.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/include/kernel/tee_misc.h
+++ core/include/kernel/tee_misc.h
@@ -42,20 +42,11 @@ uint32_t tee_hs2b(uint8_t *hs, uint8_t *b, uint32_t hslen, uint32_t blen);
  * @a - memory area start address (handled has an unsigned offset)
  * @al - memory area length (in byte)
  */
-#define core_is_buffer_inside(b, bl, a, al) \
-	_core_is_buffer_inside((vaddr_t)(b), (size_t)(bl), \
-				(vaddr_t)(a), (size_t)(al))
-
-#define core_is_buffer_outside(b, bl, a, al) \
-	_core_is_buffer_outside((vaddr_t)(b), (size_t)(bl), \
-				(vaddr_t)(a), (size_t)(al))
-
-#define core_is_buffer_intersect(b, bl, a, al) \
-	_core_is_buffer_intersect((vaddr_t)(b), (size_t)(bl), \
-				(vaddr_t)(a), (size_t)(al))
-
-bool _core_is_buffer_inside(vaddr_t b, size_t bl, vaddr_t a, size_t al);
-bool _core_is_buffer_outside(vaddr_t b, size_t bl, vaddr_t a, size_t al);
-bool _core_is_buffer_intersect(vaddr_t b, size_t bl, vaddr_t a, size_t al);
+bool core_is_buffer_inside(paddr_t b, paddr_size_t bl,
+			   paddr_t a, paddr_size_t al);
+bool core_is_buffer_outside(paddr_t b, paddr_size_t bl,
+			    paddr_t a, paddr_size_t al);
+bool core_is_buffer_intersect(paddr_t b, paddr_size_t bl,
+			      paddr_t a, paddr_size_t al);
 
 #endif /* TEE_MISC_H */
