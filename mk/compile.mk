OP-TEE/optee_os

commit 989ac108b0efce902bd40a5a5678ec1fbb4a9663

mk/compile.mk: add cc-option macro
Adds a macro to test the availability of a compiler option. For example:

 $(call cc-option,-opt1,-opt2)

...will evaluate to '-opt1' if the C compiler supports option -opt1, or
'-opt2' otherwise. The second argument (-opt2) is optional.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

mk/compile.mk
@@ -17,6 +17,15 @@ objs		:=
 # Disable all builtin rules
 .SUFFIXES:
 
+__cc-option = $(if $(shell $(CC$(sm)) $(1) -c -x c /dev/null -o /dev/null 2>&1 >/dev/null),$(2),$(1))
+_cc-opt-cached-var-name = cached-cc-option$(subst =,~,$(strip $(1)))$(subst $(empty) $(empty),,$(CC$(sm)))
+define _cc-option
+$(eval _cached := $(call _cc-opt-cached-var-name,$1))
+$(eval $(_cached) := $(if $(filter $(origin $(_cached)),undefined),$(call __cc-option,$(1),$(2)),$($(_cached))))
+$($(_cached))
+endef
+cc-option = $(strip $(call _cc-option,$(1),$(2)))
+
 comp-cflags$(sm) = -std=gnu99
 comp-aflags$(sm) =
 comp-cppflags$(sm) =

commit a3c5bb3d0a53535ba3bf0b48f5d836bb549c07f4

mk/compile.mk: fix cc-option macro
There are (at least) three issues with the cc-option macro:

1. When COMPILER=clang: when presented with a supported but unused
   option, Clang emits a warning to stderr (and returns a success code
   of 0). Therefore it is incorrect to check stderr to determine if an
   option is supported or not; we should rely on the return status
   instead.
2. When COMPILER=clang, the compile command $(CC$(sm)) contains an
   equal sign (e.g., clang --target=arm-linux-gnueabihf). This is not
   expected in the cc-option macro, currently only flags are allowed to
   potentially contain an equal sign. This messes with the caching of
   the test result.
3. The macro should not cache the return value when an option is not
   supported. For instance, if we have:
     A := $(call cc-option,--not-supported,a)
     B := $(call cc-option,--not-supported,b)
   ...we expect A to be "a" and B to be "b". The current implementation
   returns "a" in both cases.

This commit fixes the above problems.

Fixes: 989ac108b0ef ("mk/compile.mk: add cc-option macro")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

mk/compile.mk
@@ -17,12 +17,12 @@ objs		:=
 # Disable all builtin rules
 .SUFFIXES:
 
-__cc-option = $(if $(shell $(CC$(sm)) $(1) -c -x c /dev/null -o /dev/null 2>&1 >/dev/null),$(2),$(1))
-_cc-opt-cached-var-name = cached-cc-option$(subst =,~,$(strip $(1)))$(subst $(empty) $(empty),,$(CC$(sm)))
+_cc-option-supported = $(if $(shell $(CC$(sm)) $(1) -c -x c /dev/null -o /dev/null 2>/dev/null >/dev/null || echo "Not supported"),,1)
+_cc-opt-cached-var-name = $(subst =,~,$(strip cached-cc-option-$(1)-$(subst $(empty) $(empty),,$(CC$(sm)))))
 define _cc-option
-$(eval _cached := $(call _cc-opt-cached-var-name,$1))
-$(eval $(_cached) := $(if $(filter $(origin $(_cached)),undefined),$(call __cc-option,$(1),$(2)),$($(_cached))))
-$($(_cached))
+$(eval _var_name := $(call _cc-opt-cached-var-name,$(1)))
+$(eval $(_var_name) := $(if $(filter $(origin $(_var_name)),undefined),$(call _cc-option-supported,$(1)),$($(_var_name))))
+$(if $($(_var_name)),$(1),$(2))
 endef
 cc-option = $(strip $(call _cc-option,$(1),$(2)))
 