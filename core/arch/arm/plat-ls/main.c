ad964e3e0fcf1f5ec99bc1397e3f895457a8aecc
Manish Tomar <manish.tomar@nxp.com>
2020-06-03 11:45:51 +0530

    core: ls: add LX2160A-QDS platform
    Add support for Layerscape® LX2160A-QDS from NXP.
    Dynamic shared memory is also enabled.
    
    Signed-off-by: Manish Tomar <manish.tomar@nxp.com>
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/plat-ls/main.c
+++ core/arch/arm/plat-ls/main.c
@@ -63,7 +63,7 @@ register_phys_mem_pgdir(MEM_AREA_IO_NSEC, CONSOLE_UART_BASE,
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GIC_BASE, CORE_MMU_PGDIR_SIZE);
 #endif
 
-#if defined(PLATFORM_FLAVOR_lx2160ardb)
+#if defined(PLATFORM_FLAVOR_lx2160ardb) || defined(PLATFORM_FLAVOR_lx2160aqds)
 register_ddr(CFG_DRAM0_BASE, (CFG_TZDRAM_START - CFG_DRAM0_BASE));
 #ifdef CFG_DRAM1_BASE
 register_ddr(CFG_DRAM1_BASE, CFG_DRAM1_SIZE);
--- core/arch/arm/plat-ls/platform_config.h
+++ core/arch/arm/plat-ls/platform_config.h
@@ -103,4 +103,12 @@
 #define CAAM_BASE			0x08000000
 #endif
 
+#if defined(PLATFORM_FLAVOR_lx2160aqds)
+/*  DUART 1 */
+#define UART0_BASE                      0x021C0000
+#define GIC_BASE                        0x06000000
+#define GICC_OFFSET                     0x0
+#define GICD_OFFSET                     0x0
+#endif
+
 #endif /*PLATFORM_CONFIG_H*/
