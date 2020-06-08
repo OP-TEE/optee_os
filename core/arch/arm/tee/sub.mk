6146c647805acb3486b2ad2a8d5a00dc3e856a54
Jens Wiklander <jens.wiklander@linaro.org>
2020-06-08 09:49:42 +0200

    Remove unused core/arch/arm/tee/svc_dummy.c
    The function tee_svc_handler() doesn't exist any longer so no need for a
    dummy implementation. Removes references to tee_svc_handler() and where
    applicable adds references to user_ta_handle_svc() instead.
    
    Fixes: 7c732ee481c6 ("core: get svc handler from the context of current session")
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/tee/sub.mk
+++ core/arch/arm/tee/sub.mk
@@ -3,8 +3,6 @@ srcs-$(CFG_ARM32_core) += arch_svc_a32.S
 srcs-$(CFG_ARM64_core) += arch_svc_a64.S
 srcs-$(CFG_CACHE_API) += svc_cache.c
 srcs-y += arch_svc.c
-else
-srcs-y += svc_dummy.c
 endif
 srcs-y += entry_std.c
 srcs-y += entry_fast.c
