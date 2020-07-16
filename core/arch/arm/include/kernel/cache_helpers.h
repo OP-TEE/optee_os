ec64f5ab4f6c69446bee0feb6dcde59a2ca6dcc3
Clement Faure <clement.faure@nxp.com>
2020-07-16 13:49:07 +0200

    core: add dcache_get_line_size() function
    Add dcache_get_line_size() function to get cache line size for ARM32 and
    ARM64 platforms.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/include/kernel/cache_helpers.h
+++ core/arch/arm/include/kernel/cache_helpers.h
@@ -7,6 +7,7 @@
 #define __KERNEL_CACHE_HELPERS_H
 
 #ifndef __ASSEMBLER__
+#include <arm.h>
 #include <types_ext.h>
 #endif
 
@@ -31,6 +32,17 @@ void dcache_op_all(unsigned long op_type);
 void dcache_op_level1(unsigned long op_type);
 void dcache_op_level2(unsigned long op_type);
 void dcache_op_level3(unsigned long op_type);
+
+/*
+ * Get system cache line size from ARM system configuration registers.
+ */
+static inline unsigned int dcache_get_line_size(void)
+{
+	uint32_t value = read_ctr();
+
+	return CTR_WORD_SIZE <<
+		((value >> CTR_DMINLINE_SHIFT) & CTR_DMINLINE_MASK);
+}
 #endif /*!__ASSEMBLER__*/
 
 #endif /*__KERNEL_CACHE_HELPERS_H*/
