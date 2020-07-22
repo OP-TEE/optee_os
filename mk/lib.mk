b4faf4806c6748fe4e53699d46c9bfb448d54731
Dick Olsson <hi@senzilla.io>
2020-07-22 08:49:12 +0200

    mk: core: ta: Configurable Python interpreter
    Build systems that manage multiple different python interpreters need
    explicit control over which version of the interpreter to use.
    This patch enables one to override the default interpreter with the path
    to a specific one.
    
    Signed-off-by: Dick Olsson <hi@senzilla.io>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

--- mk/lib.mk
+++ mk/lib.mk
@@ -72,7 +72,7 @@ $(lib-shlibstrippedfile): $(lib-shlibfile)
 
 $(lib-shlibtafile): $(lib-shlibstrippedfile) $(TA_SIGN_KEY)
 	@$(cmd-echo-silent) '  SIGN    $$@'
-	$$(q)$$(SIGN) --key $(TA_SIGN_KEY) --uuid $(libuuid) --in $$< --out $$@
+	$$(q)$$(PYTHON3) $$(SIGN) --key $(TA_SIGN_KEY) --uuid $(libuuid) --in $$< --out $$@
 
 $(lib-libuuidln): $(lib-shlibfile)
 	@$(cmd-echo-silent) '  LN      $$@'
