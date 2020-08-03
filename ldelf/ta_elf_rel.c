OP-TEE/optee_os

commit 97c5ac19427b84c7704e46c1b236c521107a8113

ldelf: check ranges in __resolve_sym()
Adds checks in __resolve_sym() to see that the offset of the name and
location of a symbol is in range.

Reviewed-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf_rel.c
@@ -31,23 +31,35 @@ static uint32_t elf_hash(const char *name)
 	return h;
 }
 
-static bool __resolve_sym(struct ta_elf *elf, unsigned int bind,
-			  size_t st_shndx, size_t st_name, size_t st_value,
-			  const char *name, vaddr_t *val)
+static bool __resolve_sym(struct ta_elf *elf, unsigned int st_bind,
+			  unsigned int st_type, size_t st_shndx,
+			  size_t st_name, size_t st_value, const char *name,
+			  vaddr_t *val)
 {
-	if (bind != STB_GLOBAL)
+	if (st_bind != STB_GLOBAL)
 		return false;
 	if (st_shndx == SHN_UNDEF || st_shndx == SHN_XINDEX)
 		return false;
 	if (!st_name)
 		return false;
 	if (st_name > elf->dynstr_size)
-		err(TEE_ERROR_BAD_FORMAT, "Symbol out of range");
+		err(TEE_ERROR_BAD_FORMAT, "Symbol name out of range");
 
 	if (strcmp(name, elf->dynstr + st_name))
 		return false;
 
-	*val = st_value + elf->load_addr;
+	if (st_value > (elf->max_addr - elf->load_addr))
+		err(TEE_ERROR_BAD_FORMAT, "Symbol location out of range");
+
+	switch (st_type) {
+	case STT_OBJECT:
+	case STT_FUNC:
+		*val = st_value + elf->load_addr;
+		break;
+	default:
+		err(TEE_ERROR_NOT_SUPPORTED, "Symbol type not supported");
+	}
+
 	return true;
 }
 
@@ -74,6 +86,7 @@ static TEE_Result resolve_sym_helper(uint32_t hash, const char *name,
 				    "Index out of range");
 			if (__resolve_sym(elf,
 					  ELF32_ST_BIND(sym[n].st_info),
+					  ELF32_ST_TYPE(sym[n].st_info),
 					  sym[n].st_shndx,
 					  sym[n].st_name,
 					  sym[n].st_value, name, val))
@@ -88,6 +101,7 @@ static TEE_Result resolve_sym_helper(uint32_t hash, const char *name,
 				    "Index out of range");
 			if (__resolve_sym(elf,
 					  ELF64_ST_BIND(sym[n].st_info),
+					  ELF64_ST_TYPE(sym[n].st_info),
 					  sym[n].st_shndx,
 					  sym[n].st_name,
 					  sym[n].st_value, name, val))

commit 5a68740e844cd1818aec7dd16e58cb95210cc9b3

ldelf: __resolve_sym(): support STT_NOTYPE
Symbols defined in a linker script are assigned type STT_NOTYPE, but
the __resolve_sym() function in ldelf only supports STT_OBJECT and
STT_FUNCTION. As a result, it is impossible to resolve STT_NOTYPE
symbols at runtime. This causes an error in shared libraries when
ftrace is enabled:

  # Platform: QEMU
  $ make CFG_FTRACE_SUPPORT=y CFLAGS_ta_arm32=-pg run

  $ xtest 1019

  D/LD:  ldelf:134 Loading TA 5b9e0e40-2636-11e1-ad9e-0002a5d5c51b
  E/LD:  __resolve_sym:61 Symbol type not supported
  E/TC:? 0 init_with_ldelf:232 ldelf failed with res: 0xffff000a

  * regression_1019 Test dynamically linked TA
  regression_1000.c:1502: [...] TEEC_ERROR_NOT_SUPPORTED

This commit adds STT_NOTYPE to the supported types, handled the same way
as STT_OBJECT and STT_FUNCTION.

Fixes: 97c5ac19427b ("ldelf: check ranges in __resolve_sym()")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf_rel.c
@@ -53,6 +53,7 @@ static bool __resolve_sym(struct ta_elf *elf, unsigned int st_bind,
 		err(TEE_ERROR_BAD_FORMAT, "Symbol location out of range");
 
 	switch (st_type) {
+	case STT_NOTYPE:
 	case STT_OBJECT:
 	case STT_FUNC:
 		*val = st_value + elf->load_addr;