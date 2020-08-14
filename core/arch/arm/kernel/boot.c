OP-TEE/optee_os

commit b166fabf3e8c35de38ccc77a870b58941f73cb83

core: initialize thread_core_local::curr_thread to -1
The struct thread_core_local for each CPU is global, hence initialized
to zero when .bss is cleared. So before threads are initialized we have
a seemingly valid curr_thread value (0) when we should really have -1.
thread_get_id_may_fail() can return 0 although there is no current
thread.

Fix this by setting curr_thread to -1 earlier in the boot.

Note: this moves code out of thread_init_threads(), which is called by
virt_guest_created() when virtualization is enabled. I think it is the
right thing to do because I see no reason why the thread_core_local
structures should be cleared when a guest is added (the data belong to
.nex_bss).

Signed-off-by: Jerome Forissier <jerome@forissier.org>
CC: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/boot.c
@@ -1146,6 +1146,13 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
 	thread_set_exceptions(THREAD_EXCP_ALL);
 	primary_save_cntfrq();
 	init_vfp_sec();
+	/*
+	 * Pager: init_runtime() calls thread_kernel_enable_vfp() so we must
+	 * set a current thread right now to avoid a chicken-and-egg problem
+	 * (thread_init_boot_thread() sets the current thread but needs
+	 * things set by init_runtime()).
+	 */
+	thread_get_core_local()->curr_thread = 0;
 	init_runtime(pageable_part);
 
 #ifndef CFG_VIRTUALIZATION

core/arch/arm/include/kernel/thread.h
@@ -280,6 +280,9 @@ bool thread_init_stack(uint32_t stack_id, vaddr_t sp);
  */
 void thread_init_threads(void);
 
+/* Set thread_core_local::curr_thread = -1 for all CPUs */
+void thread_clr_thread_core_local(void);
+
 /*
  * Initializes a thread to be used during boot
  */

core/arch/arm/kernel/entry_a32.S
@@ -457,6 +457,9 @@ shadow_stack_access_ok:
 
 	set_sp
 
+	/* curr_thread needs to be -1 until threads are properly initialized */
+	bl	thread_clr_thread_core_local
+
 	/* complete ARM secure MP common configuration */
 	bl	plat_primary_init_early
 

core/arch/arm/kernel/entry_a64.S
@@ -148,6 +148,9 @@ clear_nex_bss:
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
 	set_sp
 
+	/* curr_thread needs to be -1 until threads are properly initialized */
+	bl	thread_clr_thread_core_local
+
 	/* Enable aborts now that we can receive exceptions */
 	msr	daifclr, #DAIFBIT_ABT
 

core/arch/arm/kernel/thread.c
@@ -892,7 +892,7 @@ static void init_user_kcode(void)
 
 void thread_init_threads(void)
 {
-	size_t n;
+	size_t n = 0;
 
 	init_thread_stacks();
 	pgt_init();
@@ -903,6 +903,11 @@ void thread_init_threads(void)
 		TAILQ_INIT(&threads[n].tsd.sess_stack);
 		SLIST_INIT(&threads[n].tsd.pgt_cache);
 	}
+}
+
+void thread_clr_thread_core_local(void)
+{
+	size_t n = 0;
 
 	for (n = 0; n < CFG_TEE_CORE_NB_CORE; n++)
 		thread_core_local[n].curr_thread = -1;

commit 79e8c69b270b50c73a333dc615aa70000b896ff1

virt: clear current thread id during initialization
When OP-TEE is built with CFG_VIRTUALIZATION=y, it does not call
`thread_clr_boot_thread()` during boot because the threads are
allocated in "tee" memory area, which is not available when there is
no virtual guests.

So, in this case local core state is left in erroneous state, which
causes assertion violation in thread_alloc_and_run(), when guests
calls OP-TEE for the first time from boot core.

Fixes: b166fabf3e8c ("core: initialize thread_core_local::curr_thread to -1")
Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/kernel/boot.c
@@ -1153,9 +1153,17 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
 	thread_get_core_local()->curr_thread = 0;
 	init_runtime(pageable_part);
 
-#ifndef CFG_VIRTUALIZATION
-	thread_init_boot_thread();
-#endif
+	if (IS_ENABLED(CFG_VIRTUALIZATION)) {
+		/*
+		 * Virtualization: We can't initialize threads right now because
+		 * threads belong to "tee" part and will be initialized
+		 * separately per each new virtual guest. So, we'll clear
+		 * "curr_thread" and call it done.
+		 */
+		thread_get_core_local()->curr_thread = -1;
+	} else {
+		thread_init_boot_thread();
+	}
 	thread_init_primary();
 	thread_init_per_cpu();
 	init_sec_mon(nsec_entry);