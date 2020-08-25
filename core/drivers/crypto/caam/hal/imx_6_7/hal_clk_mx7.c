fb1849d7c97655a6210fc5910787a1982e8bb8e6
Cedric Neveux <cedric.neveux@nxp.com>
2020-08-25 15:41:17 +0200

    drivers: caam: Fix hal clock registers read/write
    Fix CAAM Clock registers access to use io_read/write() functions and
    not io_caam_read/write().
    
    Fixes: 2d7a896 ("driver: implement CAAM driver")
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Acked-by: Ruchika Gupta <ruchika.gupta@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7.c
+++ core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7.c
@@ -5,7 +5,7 @@
  * Brief   CAAM Clock functions.
  */
 #include <caam_hal_clk.h>
-#include <caam_io.h>
+#include <io.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
@@ -14,10 +14,10 @@ void caam_hal_clk_enable(bool enable)
 	vaddr_t ccm_base = (vaddr_t)phys_to_virt(CCM_BASE, MEM_AREA_IO_SEC, 1);
 
 	if (enable) {
-		io_caam_write32(ccm_base + CCM_CCGRx_SET(CCM_CLOCK_DOMAIN_CAAM),
-				CCM_CCGRx_ALWAYS_ON(0));
+		io_write32(ccm_base + CCM_CCGRx_SET(CCM_CLOCK_DOMAIN_CAAM),
+			   CCM_CCGRx_ALWAYS_ON(0));
 	} else {
-		io_caam_write32(ccm_base + CCM_CCGRx_CLR(CCM_CLOCK_DOMAIN_CAAM),
-				CCM_CCGRx_ALWAYS_ON(0));
+		io_write32(ccm_base + CCM_CCGRx_CLR(CCM_CLOCK_DOMAIN_CAAM),
+			   CCM_CCGRx_ALWAYS_ON(0));
 	}
 }
