ef5f758488a1229f578125b9079b73a9c4c66ad7
Etienne Carriere <etienne.carriere@linaro.org>
2020-08-28 15:21:02 +0200

    ta: pkcs11: fix trace_attributes() to support empty attribute arrays
    Fix trace_attributes() to not print meaningless data when an attribute
    array is empty.
    
    Also fix a local variable that is not initialized in trace_attributes().
    
    Fixes: 63f89caa9022 ("ta: pkcs11: attribute helper functions")
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- ta/pkcs11/src/attributes.c
+++ ta/pkcs11/src/attributes.c
@@ -254,7 +254,9 @@ static void __trace_attributes(char *prefix, void *src, void *end)
 		case PKCS11_CKA_WRAP_TEMPLATE:
 		case PKCS11_CKA_UNWRAP_TEMPLATE:
 		case PKCS11_CKA_DERIVE_TEMPLATE:
-			trace_attributes(prefix2, cur + sizeof(pkcs11_ref));
+			if (pkcs11_ref.size)
+				trace_attributes(prefix2,
+						 cur + sizeof(pkcs11_ref));
 			break;
 		default:
 			break;
@@ -270,10 +272,13 @@ static void __trace_attributes(char *prefix, void *src, void *end)
 
 void trace_attributes(const char *prefix, void *ref)
 {
-	struct obj_attrs head;
+	struct obj_attrs head = { };
 	char *pre = NULL;
 
 	TEE_MemMove(&head, ref, sizeof(head));
+
+	if (!head.attrs_count)
+		return;
 
 	pre = TEE_Malloc(prefix ? strlen(prefix) + 2 : 2, TEE_MALLOC_FILL_ZERO);
 	if (!pre) {
