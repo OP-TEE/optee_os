OP-TEE/optee_os

commit 2667e1359e516ed45b71b7857da750f531a8b16f

core: fix offset calculation in param_mem_to_user_va()
In param_mem_to_user_va() the offset of a memory parameter is used to
check if a particular struct vm_region will cover that parameter.
struct vm_region always uses offsets from the beginning of the first
physical page while a memory parameter contains only the offset from the
beginning of a MOBJ. Consequently the two offset cannot be compared
directly.

Until this patch the two offset where compared directly so fix it by
adding the phys_offs of the MOBJ to the offset of the memory parameter.

Note that this doesn't change the computed virtual address, it only
fails to find a matching struct vm_region under certain circumstances.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/tee_mmu.c
@@ -837,13 +837,15 @@ static TEE_Result param_mem_to_user_va(struct user_mode_ctx *uctx,
 			continue;
 		if (mem->mobj != region->mobj)
 			continue;
-		if (mem->offs < region->offset)
-			continue;
-		if (mem->offs >= (region->offset + region->size))
-			continue;
+
 		phys_offs = mobj_get_phys_offs(mem->mobj,
 					       CORE_MMU_USER_PARAM_SIZE);
-		va = region->va + mem->offs + phys_offs - region->offset;
+		phys_offs += mem->offs;
+		if (phys_offs < region->offset)
+			continue;
+		if (phys_offs >= (region->offset  - region->size))
+			continue;
+		va = region->va + phys_offs - region->offset;
 		*user_va = (void *)va;
 		return TEE_SUCCESS;
 	}

commit 2bbd90925e9e694630f56cee488aa4a252222fe5

core: mm: fix region lookup in param_mem_to_user_va()
The test whether a memory parameter is located in a region may fail
because of a typo in the comparison. The region size must be
added to the start address, not subtracted.

Fixes: 2667e1359e51 ("core: fix offset calculation in param_mem_to_user_va()")
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Wolfgang Ocker <weo@reccoware.de>

core/arch/arm/mm/tee_mmu.c
@@ -843,7 +843,7 @@ static TEE_Result param_mem_to_user_va(struct user_mode_ctx *uctx,
 		phys_offs += mem->offs;
 		if (phys_offs < region->offset)
 			continue;
-		if (phys_offs >= (region->offset  - region->size))
+		if (phys_offs >= (region->offset + region->size))
 			continue;
 		va = region->va + phys_offs - region->offset;
 		*user_va = (void *)va;