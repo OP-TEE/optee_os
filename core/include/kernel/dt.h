OP-TEE/optee_os

commit c0cfb36c9054e09bdb4249aaea36d3a4c316c63f

core: dt: introduce _fdt_fill_device_info()
_fdt_fill_device_info() gets some generic properties from a given
node in a single always successful sequence.

Retrieved device information from the DT:
- The status/secure-status state as per DT_STATUS_*,
- The first register base address found or DT_INFO_INVALID_REG (zero).
  If there are several register base addresses others are ignored.
- The first clock identifier found or DT_INFO_INVALID_CLOCK (negative).
- This first reset identifier found or DT_INFO_INVALID_RESET (negative).

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Joakim Bech <joakim.bech@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/include/kernel/dt.h
@@ -7,6 +7,7 @@
 #define KERNEL_DT_H
 
 #include <compiler.h>
+#include <kernel/panic.h>
 #include <stdint.h>
 #include <types_ext.h>
 #include <util.h>
@@ -15,9 +16,25 @@
  * Bitfield to reflect status and secure-status values ("okay", "disabled"
  * or not present)
  */
-#define DT_STATUS_DISABLED 0
-#define DT_STATUS_OK_NSEC  BIT(0)
-#define DT_STATUS_OK_SEC   BIT(1)
+#define DT_STATUS_DISABLED		0
+#define DT_STATUS_OK_NSEC		BIT(0)
+#define DT_STATUS_OK_SEC		BIT(1)
+
+#define DT_INFO_INVALID_REG		((paddr_t)-1)
+#define DT_INFO_INVALID_CLOCK		-1
+#define DT_INFO_INVALID_RESET		-1
+/*
+ * @status: Bit mask for DT_STATUS_*
+ * @reg: Device register physical base address or DT_INFO_INVALID_REG
+ * @clock: Device identifier (positive value) or DT_INFO_INVALID_CLOCK
+ * @reset: Device reset identifier (positive value) or DT_INFO_INVALID_CLOCK
+ */
+struct dt_node_info {
+	unsigned int status;
+	paddr_t reg;
+	int clock;
+	int reset;
+};
 
 #if defined(CFG_DT)
 
@@ -100,6 +117,16 @@ ssize_t _fdt_reg_size(const void *fdt, int offs);
  */
 int _fdt_get_status(const void *fdt, int offs);
 
+/*
+ * fdt_fill_device_info - Get generic device info from a node
+ *
+ * This function fills the generic information from a given node.
+ * Currently supports a single base register, a single clock,
+ * a single reset ID line and a single interrupt ID.
+ * Default DT_INFO_* macros are used when the relate property is not found.
+ */
+void _fdt_fill_device_info(void *fdt, struct dt_node_info *info, int node);
+
 #else /* !CFG_DT */
 
 static inline const struct dt_driver *__dt_driver_start(void)
@@ -142,6 +169,12 @@ static inline int _fdt_get_status(const void *fdt __unused, int offs __unused)
 	return -1;
 }
 
+static inline void _fdt_fill_device_info(void *fdt __unused,
+					 struct dt_node_info *info __unused,
+					 int node __unused)
+{
+	panic();
+}
 #endif /* !CFG_DT */
 
 #define for_each_dt_driver(drv) \

core/kernel/dt.c
@@ -66,7 +66,7 @@ int dt_map_dev(const void *fdt, int offs, vaddr_t *base, size_t *size)
 		return -1;
 
 	pbase = _fdt_reg_base_address(fdt, offs);
-	if (pbase == (paddr_t)-1)
+	if (pbase == DT_INFO_INVALID_REG)
 		return -1;
 	sz = _fdt_reg_size(fdt, offs);
 	if (sz < 0)
@@ -121,7 +121,7 @@ static paddr_t _fdt_read_paddr(const uint32_t *cell, int n)
 
 	return addr;
 bad:
-	return (paddr_t)-1;
+	return DT_INFO_INVALID_REG;
 
 }
 
@@ -134,15 +134,15 @@ paddr_t _fdt_reg_base_address(const void *fdt, int offs)
 
 	parent = fdt_parent_offset(fdt, offs);
 	if (parent < 0)
-		return (paddr_t)-1;
+		return DT_INFO_INVALID_REG;
 
 	reg = fdt_getprop(fdt, offs, "reg", &len);
 	if (!reg)
-		return (paddr_t)-1;
+		return DT_INFO_INVALID_REG;
 
 	ncells = fdt_address_cells(fdt, parent);
 	if (ncells < 0)
-		return (paddr_t)-1;
+		return DT_INFO_INVALID_REG;
 
 	return _fdt_read_paddr(reg, ncells);
 }
@@ -157,7 +157,7 @@ ssize_t _fdt_reg_size(const void *fdt, int offs)
 
 	parent = fdt_parent_offset(fdt, offs);
 	if (parent < 0)
-		return (paddr_t)-1;
+		return DT_INFO_INVALID_REG;
 
 	reg = (const uint32_t *)fdt_getprop(fdt, offs, "reg", &len);
 	if (!reg)
@@ -215,4 +215,32 @@ int _fdt_get_status(const void *fdt, int offs)
 	}
 
 	return st;
+}
+
+void _fdt_fill_device_info(void *fdt, struct dt_node_info *info, int offs)
+{
+	struct dt_node_info dinfo = {
+		.reg = DT_INFO_INVALID_REG,
+		.clock = DT_INFO_INVALID_CLOCK,
+		.reset = DT_INFO_INVALID_RESET,
+	};
+	const fdt32_t *cuint;
+
+	dinfo.reg = _fdt_reg_base_address(fdt, offs);
+
+	cuint = fdt_getprop(fdt, offs, "clocks", NULL);
+	if (cuint) {
+		cuint++;
+		dinfo.clock = (int)fdt32_to_cpu(*cuint);
+	}
+
+	cuint = fdt_getprop(fdt, offs, "resets", NULL);
+	if (cuint) {
+		cuint++;
+		dinfo.reset = (int)fdt32_to_cpu(*cuint);
+	}
+
+	dinfo.status = _fdt_get_status(fdt, offs);
+
+	*info = dinfo;
 }

commit 17eb87a2b1b68d6ead0644554a66a0216af89c94

core: dt: Repair type in _fdt_reg_size()
The function returns ssize_t , while DT_INFO_INVALID_REG is paddr_t,
fix the type.

Fixes: c0cfb36c ("core: dt: introduce _fdt_fill_device_info()")
Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>

core/include/kernel/dt.h
@@ -21,6 +21,7 @@
 #define DT_STATUS_OK_SEC		BIT(1)
 
 #define DT_INFO_INVALID_REG		((paddr_t)-1)
+#define DT_INFO_INVALID_REG_SIZE	((ssize_t)-1)
 #define DT_INFO_INVALID_CLOCK		-1
 #define DT_INFO_INVALID_RESET		-1
 #define DT_INFO_INVALID_INTERRUPT	-1

core/kernel/dt.c
@@ -222,7 +222,7 @@ ssize_t _fdt_reg_size(const void *fdt, int offs)
 
 	parent = fdt_parent_offset(fdt, offs);
 	if (parent < 0)
-		return DT_INFO_INVALID_REG;
+		return DT_INFO_INVALID_REG_SIZE;
 
 	reg = (const uint32_t *)fdt_getprop(fdt, offs, "reg", &len);
 	if (!reg)