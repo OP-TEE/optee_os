OP-TEE/optee_os

commit 721619e8890ea6d270cdd1aaee82c0f23ee2536e

core: Parse all memory DT nodes
The current code for parsing /memory* DT nodes does not work at all
on systems with multiple memory nodes. The code cannot handle e.g.

/ {
  memory@480000000 {
    reg = <0x00000000 0x48000000 0x00000000 0x78000000>;
    device_type = "memory";
  };
  memory@600000000 {
    reg = <0x00000006 0x00000000 0x00000000 0x80000000>;
    device_type = "memory";
  };
};

This patch fixes the code such that it iterates over all enabled
memory nodes instead of reading out the first /memory node only.
The code iterates over the DT twice, which is faster than constant
calls to realloc() to allocate more entries in core_mmu_phys_mem().

Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/kernel/boot.c
@@ -914,58 +914,82 @@ static uint64_t get_dt_val_and_advance(const void *data, size_t *offs,
 	return rv;
 }
 
-static struct core_mmu_phys_mem *get_memory(void *fdt, size_t *nelems)
+static int get_memory_all(void *fdt, struct core_mmu_phys_mem *mem)
 {
-	int offs = 0;
+	const uint8_t *prop = NULL;
+	uint64_t a = 0;
+	uint64_t l = 0;
+	size_t prop_offs = 0;
+	size_t prop_len = 0;
+	int elems_total = 0;
 	int addr_size = 0;
 	int len_size = 0;
-	size_t prop_len = 0;
-	const uint8_t *prop = NULL;
-	size_t prop_offs = 0;
+	int offs = 0;
 	size_t n = 0;
-	struct core_mmu_phys_mem *mem = NULL;
+	int len = 0;
 
-	offs = fdt_subnode_offset(fdt, 0, "memory");
-	if (offs < 0)
-		return NULL;
-
-	prop = fdt_getprop(fdt, offs, "reg", &addr_size);
-	if (!prop)
-		return NULL;
-
-	prop_len = addr_size;
 	addr_size = fdt_address_cells(fdt, 0);
 	if (addr_size < 0)
-		return NULL;
+		return 0;
 
 	len_size = fdt_size_cells(fdt, 0);
 	if (len_size < 0)
-		return NULL;
+		return 0;
 
-	for (n = 0, prop_offs = 0; prop_offs < prop_len; n++) {
-		get_dt_val_and_advance(prop, &prop_offs, addr_size);
-		if (prop_offs >= prop_len) {
-			n--;
+	while (true) {
+		offs = fdt_node_offset_by_prop_value(fdt, offs, "device_type",
+						     "memory",
+						     sizeof("memory"));
+		if (offs < 0)
 			break;
+
+		if (_fdt_get_status(fdt, offs) <= 0)
+			continue;
+
+		prop = fdt_getprop(fdt, offs, "reg", &len);
+		if (!prop)
+			continue;
+
+		prop_len = len;
+		for (n = 0, prop_offs = 0; prop_offs < prop_len; n++) {
+			a = get_dt_val_and_advance(prop, &prop_offs, addr_size);
+			if (prop_offs >= prop_len) {
+				n--;
+				break;
+			}
+
+			l = get_dt_val_and_advance(prop, &prop_offs, len_size);
+			if (mem) {
+				mem->type = MEM_AREA_RAM_NSEC;
+				mem->addr = a;
+				mem->size = l;
+				mem++;
+			}
 		}
-		get_dt_val_and_advance(prop, &prop_offs, len_size);
+
+		elems_total += n;
 	}
 
-	if (!n)
+	return elems_total;
+}
+
+static struct core_mmu_phys_mem *get_memory(void *fdt, size_t *nelems)
+{
+	struct core_mmu_phys_mem *mem = NULL;
+	int elems_total = 0;
+
+	elems_total = get_memory_all(fdt, NULL);
+	if (elems_total <= 0)
 		return NULL;
 
-	*nelems = n;
-	mem = nex_calloc(n, sizeof(*mem));
+	mem = nex_calloc(elems_total, sizeof(*mem));
 	if (!mem)
 		panic();
 
-	for (n = 0, prop_offs = 0; n < *nelems; n++) {
-		mem[n].type = MEM_AREA_RAM_NSEC;
-		mem[n].addr = get_dt_val_and_advance(prop, &prop_offs,
-						     addr_size);
-		mem[n].size = get_dt_val_and_advance(prop, &prop_offs,
-						     len_size);
-	}
+	elems_total = get_memory_all(fdt, mem);
+	assert(elems_total > 0);
+
+	*nelems = elems_total;
 
 	return mem;
 }

commit 6590ab8653207558d739d156893b6b6b88b82f6f

core: dt: discover_nsec_memory(): consider only non-secure memory
QEMU has a /secram@e000000 DT node with secure-status = "okay" and
status = "disabled", in other words: some secure-only memory is
described in the DT. Memory that is not accessible from non-secure
world should not be added to the nsec map. Unfortunately, the commit
referenced below inadvertently added such memory, resulting in a panic
on boot:

 I/TC: Non-secure external DT found
 E/TC:0 0 check_phys_mem_is_outside:332 Non-sec mem (0xe000000:0x100000) overlaps map (type 14 0xe000000:0x100000)
 E/TC:0 0 Panic at core/arch/arm/mm/core_mmu.c:336 <check_phys_mem_is_outside>

Change the DT status test to consider only memory accessible from non-
secure and secure world. Also, rename a couple of functions to make their
purpose clear.

Fixes: 721619e8890e ("core: Parse all memory DT nodes")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/kernel/boot.c
@@ -914,7 +914,12 @@ static uint64_t get_dt_val_and_advance(const void *data, size_t *offs,
 	return rv;
 }
 
-static int get_memory_all(void *fdt, struct core_mmu_phys_mem *mem)
+/*
+ * Find all non-secure memory from DT. Memory marked inaccessible by Secure
+ * World is ignored since it could not be mapped to be used as dynamic shared
+ * memory.
+ */
+static int get_nsec_memory_helper(void *fdt, struct core_mmu_phys_mem *mem)
 {
 	const uint8_t *prop = NULL;
 	uint64_t a = 0;
@@ -943,7 +948,8 @@ static int get_memory_all(void *fdt, struct core_mmu_phys_mem *mem)
 		if (offs < 0)
 			break;
 
-		if (_fdt_get_status(fdt, offs) <= 0)
+		if (_fdt_get_status(fdt, offs) != (DT_STATUS_OK_NSEC |
+						   DT_STATUS_OK_SEC))
 			continue;
 
 		prop = fdt_getprop(fdt, offs, "reg", &len);
@@ -973,12 +979,12 @@ static int get_memory_all(void *fdt, struct core_mmu_phys_mem *mem)
 	return elems_total;
 }
 
-static struct core_mmu_phys_mem *get_memory(void *fdt, size_t *nelems)
+static struct core_mmu_phys_mem *get_nsec_memory(void *fdt, size_t *nelems)
 {
 	struct core_mmu_phys_mem *mem = NULL;
 	int elems_total = 0;
 
-	elems_total = get_memory_all(fdt, NULL);
+	elems_total = get_nsec_memory_helper(fdt, NULL);
 	if (elems_total <= 0)
 		return NULL;
 
@@ -986,7 +992,7 @@ static struct core_mmu_phys_mem *get_memory(void *fdt, size_t *nelems)
 	if (!mem)
 		panic();
 
-	elems_total = get_memory_all(fdt, mem);
+	elems_total = get_nsec_memory_helper(fdt, mem);
 	assert(elems_total > 0);
 
 	*nelems = elems_total;
@@ -1098,8 +1104,8 @@ static void update_external_dt(void)
 }
 
 #ifdef CFG_CORE_DYN_SHM
-static struct core_mmu_phys_mem *get_memory(void *fdt __unused,
-					    size_t *nelems __unused)
+static struct core_mmu_phys_mem *get_nsec_memory(void *fdt __unused,
+						 size_t *nelems __unused)
 {
 	return NULL;
 }
@@ -1114,7 +1120,7 @@ static void discover_nsec_memory(void)
 	void *fdt = get_external_dt();
 
 	if (fdt) {
-		mem = get_memory(fdt, &nelems);
+		mem = get_nsec_memory(fdt, &nelems);
 		if (mem) {
 			core_mmu_set_discovered_nsec_ddr(mem, nelems);
 			return;