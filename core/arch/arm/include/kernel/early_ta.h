015722216b3720f74b06922933d510871c706c37
Jens Wiklander <jens.wiklander@linaro.org>
2020-10-15 13:02:18 +0200

    early_ta: use scattered array helpers
    Simplifies the core linker script by replacing the hard coded
    .rodata.early_ta section with use of SCATTERED_ARRAY_DEFINE_PG_ITEM()
    instead.
    
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/include/kernel/early_ta.h
+++ core/arch/arm/include/kernel/early_ta.h
@@ -1,31 +1,24 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2017, Linaro Limited
+ * Copyright (c) 2017, 2020, Linaro Limited
  */
 #ifndef KERNEL_EARLY_TA_H
 #define KERNEL_EARLY_TA_H
 
-#include <compiler.h>
-#include <kernel/linker.h>
+#include <scattered_array.h>
 #include <stdint.h>
 #include <tee_api_types.h>
-#include <util.h>
 
 struct early_ta {
 	uint32_t flags;
 	TEE_UUID uuid;
 	uint32_t size;
 	uint32_t uncompressed_size; /* 0: not compressed */
-	const uint8_t ta[]; /* @size bytes */
+	const uint8_t *ta; /* @size bytes */
 };
 
-#define __early_ta __section(".rodata.early_ta" __SECTION_FLAGS_RODATA)
-
 #define for_each_early_ta(_ta) \
-	for (_ta = &__rodata_early_ta_start; _ta < &__rodata_early_ta_end; \
-	     _ta = (const struct early_ta *)				   \
-		   ROUNDUP((vaddr_t)_ta + sizeof(*_ta) + _ta->size,	   \
-			   __alignof__(struct early_ta)))
+	SCATTERED_ARRAY_FOREACH(_ta, early_tas, struct early_ta)
 
 #endif /* KERNEL_EARLY_TA_H */
 
