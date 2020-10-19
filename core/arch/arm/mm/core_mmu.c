292c2fe04c5d5d400fa6567f38a2b7c423e1844e
Jens Wiklander <jens.wiklander@linaro.org>
2020-10-19 23:46:08 +0200

    core: move teecore_init_pub_ram()
    Moves teecore_init_pub_ram() from tee_mmu.c to core_mmu.c.
    
    Acked-by: Joakim Bech <joakim.bech@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/mm/core_mmu.c
+++ core/arch/arm/mm/core_mmu.c
@@ -2287,3 +2287,34 @@ vaddr_t io_pa_or_va_nsec(struct io_pa_va *p)
 	}
 	return p->pa;
 }
+
+#ifdef CFG_CORE_RESERVED_SHM
+static TEE_Result teecore_init_pub_ram(void)
+{
+	vaddr_t s = 0;
+	vaddr_t e = 0;
+
+	/* get virtual addr/size of NSec shared mem allocated from teecore */
+	core_mmu_get_mem_by_type(MEM_AREA_NSEC_SHM, &s, &e);
+
+	if (s >= e || s & SMALL_PAGE_MASK || e & SMALL_PAGE_MASK)
+		panic("invalid PUB RAM");
+
+	/* extra check: we could rely on core_mmu_get_mem_by_type() */
+	if (!tee_vbuf_is_non_sec(s, e - s))
+		panic("PUB RAM is not non-secure");
+
+#ifdef CFG_PL310
+	/* Allocate statically the l2cc mutex */
+	tee_l2cc_store_mutex_boot_pa(virt_to_phys((void *)s));
+	s += sizeof(uint32_t);			/* size of a pl310 mutex */
+	s = ROUNDUP(s, SMALL_PAGE_SIZE);	/* keep required alignment */
+#endif
+
+	default_nsec_shm_paddr = virt_to_phys((void *)s);
+	default_nsec_shm_size = e - s;
+
+	return TEE_SUCCESS;
+}
+early_init(teecore_init_pub_ram);
+#endif /*CFG_CORE_RESERVED_SHM*/
