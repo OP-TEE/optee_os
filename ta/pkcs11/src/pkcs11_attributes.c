39fc24fabd9c31a22f35f79e6924685e50641dc7
Etienne Carriere <etienne.carriere@linaro.org>
2020-11-03 17:39:03 +0100

    ta: pkcs11: fix for spec WRAP_WITH_TRUSTED/ALWAYS_AUTHENTICATE defaults
    Set PKCS11_CKA_WRAP_WITH_TRUSTED and PKCS11_CKA_ALWAYS_AUTHENTICATE
    default values in the static values list of the specification.
    No functional change.
    
    Reviewed-by: Ruchika Gupta <ruchika.gupta@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- ta/pkcs11/src/pkcs11_attributes.c
+++ ta/pkcs11/src/pkcs11_attributes.c
@@ -134,7 +134,8 @@ static uint8_t *pkcs11_object_default_boolprop(uint32_t attribute)
 		return (uint8_t *)&bool_true;
 	case PKCS11_CKA_TOKEN:
 	case PKCS11_CKA_PRIVATE:
-	/* symkey false, privkey: token specific */
+	case PKCS11_CKA_WRAP_WITH_TRUSTED:
+	case PKCS11_CKA_ALWAYS_AUTHENTICATE:
 	case PKCS11_CKA_SENSITIVE:
 		return (uint8_t *)&bool_false;
 	/* Token specific default value */
@@ -149,8 +150,6 @@ static uint8_t *pkcs11_object_default_boolprop(uint32_t attribute)
 	case PKCS11_CKA_WRAP:
 	case PKCS11_CKA_UNWRAP:
 	case PKCS11_CKA_EXTRACTABLE:
-	case PKCS11_CKA_WRAP_WITH_TRUSTED:
-	case PKCS11_CKA_ALWAYS_AUTHENTICATE:
 	case PKCS11_CKA_TRUSTED:
 		return (uint8_t *)&bool_false;
 	default:
