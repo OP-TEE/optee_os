nxp-imx/imx-optee-os

core: driver: generic resources for crypto device driver - ECC
Add a generic cryptographic ECC driver interface connecting
TEE Crypto generic APIs to HW driver interface

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/drivers/crypto/crypto_api/include/drvcrypt.h
@@ -30,6 +30,7 @@ struct cryptobuf {
 enum drvcrypt_algo_id {
 	CRYPTO_HASH = 0, /* Hash driver */
 	CRYPTO_CIPHER,   /* Cipher driver */
+	CRYPTO_ECC,      /* Assymetric ECC driver */
 	CRYPTO_MAX_ALGO  /* Maximum number of algo supported */
 };
 

OP-TEE/optee_os

Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/crypto_api/include/drvcrypt.h
@@ -54,6 +54,7 @@ enum drvcrypt_algo_id {
 	CRYPTO_RSA,      /* Asymmetric RSA driver */
 	CRYPTO_MATH,	 /* Mathematical driver */
 	CRYPTO_CIPHER,   /* Cipher driver */
+	CRYPTO_ECC,      /* Asymmetric ECC driver */
 	CRYPTO_MAX_ALGO  /* Maximum number of algo supported */
 };
 