OP-TEE/optee_os

commit 02d307b7db909c7973fd54431c9bb7335f103ede

core: use libunw
Reduce core/arch/arm/kernel/unwind_arm{32,64}.c and use common code from
libunw instead.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/kernel/abort.c
@@ -5,17 +5,16 @@
 
 #include <arm.h>
 #include <kernel/abort.h>
-#include <kernel/linker.h>
 #include <kernel/misc.h>
 #include <kernel/panic.h>
 #include <kernel/tee_ta_manager.h>
-#include <kernel/unwind.h>
 #include <kernel/user_mode_ctx.h>
 #include <mm/core_mmu.h>
 #include <mm/mobj.h>
 #include <mm/tee_pager.h>
 #include <tee/tee_svc.h>
 #include <trace.h>
+#include <unw/unwind.h>
 
 #include "thread_private.h"
 
@@ -35,8 +34,6 @@ enum fault_type {
 static void __print_stack_unwind(struct abort_info *ai)
 {
 	struct unwind_state_arm32 state = { };
-	vaddr_t exidx = (vaddr_t)__exidx_start;
-	size_t exidx_sz = (vaddr_t)__exidx_end - (vaddr_t)__exidx_start;
 	uint32_t mode = ai->regs->spsr & CPSR_MODE_MASK;
 	uint32_t sp = 0;
 	uint32_t lr = 0;
@@ -68,8 +65,7 @@ static void __print_stack_unwind(struct abort_info *ai)
 	state.registers[14] = lr;
 	state.registers[15] = ai->pc;
 
-	print_stack_arm32(&state, exidx, exidx_sz, thread_stack_start(),
-			  thread_stack_size());
+	print_stack_arm32(&state, thread_stack_start(), thread_stack_size());
 }
 #endif /* ARM32 */
 

core/arch/arm/include/kernel/unwind.h
@@ -1,109 +1,21 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
 /*-
- * Copyright (c) 2015, Linaro Limited
- * Copyright (c) 2000, 2001 Ben Harris
- * Copyright (c) 1996 Scott K. Stevens
- *
- * Mach Operating System
- * Copyright (c) 1991,1990 Carnegie Mellon University
- * All Rights Reserved.
- *
- * Permission to use, copy, modify and distribute this software and its
- * documentation is hereby granted, provided that both the copyright
- * notice and this permission notice appear in all copies of the
- * software, derivative works or modified versions, and any portions
- * thereof, and that both notices appear in supporting documentation.
- *
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
- * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
- * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- *
- * Carnegie Mellon requests users of this software to return to
- *
- *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
- *  School of Computer Science
- *  Carnegie Mellon University
- *  Pittsburgh PA 15213-3890
- *
- * any improvements or extensions that they make and grant Carnegie the
- * rights to redistribute these changes.
- *
- * $FreeBSD$
+ * Copyright (c) 2015-2019 Linaro Limited
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
  */
 
 #ifndef KERNEL_UNWIND
 #define KERNEL_UNWIND
 
-#include <compiler.h>
-#include <tee_api_types.h>
 #include <types_ext.h>
 
-/* The state of the unwind process (32-bit mode) */
-struct unwind_state_arm32 {
-	uint32_t registers[16];
-	uint32_t start_pc;
-	vaddr_t insn;
-	unsigned entries;
-	unsigned byte;
-	uint16_t update_mask;
-};
-
-/*
- * Unwind a 32-bit user or kernel stack.
- * @exidx, @exidx_sz: address and size of the binary search index table
- * (.ARM.exidx section).
- * @stack, @stack_size: the bottom of the stack and its size, respectively.
- */
-bool unwind_stack_arm32(struct unwind_state_arm32 *state, vaddr_t exidx,
-			size_t exidx_sz, vaddr_t stack, size_t stack_size);
-
-#ifdef ARM64
-/* The state of the unwind process (64-bit mode) */
-struct unwind_state_arm64 {
-	uint64_t fp;
-	uint64_t sp;
-	uint64_t pc;
-};
-
-/*
- * Unwind a 64-bit kernel stack.
- * @stack, @stack_size: the bottom of the stack and its size, respectively.
- */
-bool unwind_stack_arm64(struct unwind_state_arm64 *state,
-			vaddr_t stack, size_t stack_size);
-#endif /*ARM64*/
-
 #if defined(CFG_UNWIND) && (TRACE_LEVEL > 0)
-
-#ifdef ARM64
-void print_stack_arm64(struct unwind_state_arm64 *state,
-		       vaddr_t stack, size_t stack_size);
-#endif
-void print_stack_arm32(struct unwind_state_arm32 *state,
-		       vaddr_t exidx, size_t exidx_sz,
-		       vaddr_t stack, size_t stack_size);
 void print_kernel_stack(void);
-
-#else /* defined(CFG_UNWIND) && (TRACE_LEVEL > 0) */
-
-#ifdef ARM64
-static inline void print_stack_arm64(struct unwind_state_arm64 *state __unused,
-				     vaddr_t stack __unused,
-				     size_t stack_size __unused)
-{
-}
-#endif
-static inline void print_stack_arm32(struct unwind_state_arm32 *state __unused,
-				     uaddr_t exidx __unused,
-				     size_t exidx_sz __unused,
-				     vaddr_t stack __unused,
-				     size_t stack_size __unused)
-{
-}
-
+#else
 static inline void print_kernel_stack(void)
 {
 }
-#endif /* defined(CFG_UNWIND) && (TRACE_LEVEL > 0) */
+#endif
 
 #ifdef CFG_UNWIND
 /* Get current call stack as an array allocated on the heap */

core/arch/arm/kernel/sub.mk
@@ -55,7 +55,7 @@ srcs-$(CFG_ARM32_core) += entry_a32.S
 srcs-$(CFG_ARM64_core) += entry_a64.S
 
 ifeq ($(CFG_UNWIND),y)
-srcs-y += unwind_arm32.c
+srcs-$(CFG_ARM32_core) += unwind_arm32.c
 srcs-$(CFG_ARM64_core) += unwind_arm64.c
 endif
 

core/arch/arm/kernel/unwind_arm32.c
@@ -32,14 +32,10 @@
 
 #include <arm.h>
 #include <kernel/linker.h>
-#include <kernel/misc.h>
-#include <kernel/tee_misc.h>
+#include <kernel/thread.h>
 #include <kernel/unwind.h>
-#include <string.h>
-#include <tee_api_types.h>
-#include <tee/tee_svc.h>
 #include <trace.h>
-#include <util.h>
+#include <unw/unwind.h>
 
 #include "unwind_private.h"
 
@@ -49,405 +45,13 @@
 #define	LR	14
 #define	PC	15
 
-/*
- * Definitions for the instruction interpreter.
- *
- * The ARM EABI specifies how to perform the frame unwinding in the
- * Exception Handling ABI for the ARM Architecture document. To perform
- * the unwind we need to know the initial frame pointer, stack pointer,
- * link register and program counter. We then find the entry within the
- * index table that points to the function the program counter is within.
- * This gives us either a list of three instructions to process, a 31-bit
- * relative offset to a table of instructions, or a value telling us
- * we can't unwind any further.
- *
- * When we have the instructions to process we need to decode them
- * following table 4 in section 9.3. This describes a collection of bit
- * patterns to encode that steps to take to update the stack pointer and
- * link register to the correct values at the start of the function.
- */
-
-/* A special case when we are unable to unwind past this function */
-#define	EXIDX_CANTUNWIND	1
-
-/*
- * Entry types.
- * These are the only entry types that have been seen in the kernel.
- */
-#define	ENTRY_MASK	0xff000000
-#define	ENTRY_ARM_SU16	0x80000000
-#define	ENTRY_ARM_LU16	0x81000000
-
-/* Instruction masks. */
-#define	INSN_VSP_MASK		0xc0
-#define	INSN_VSP_SIZE_MASK	0x3f
-#define	INSN_STD_MASK		0xf0
-#define	INSN_STD_DATA_MASK	0x0f
-#define	INSN_POP_TYPE_MASK	0x08
-#define	INSN_POP_COUNT_MASK	0x07
-#define	INSN_VSP_LARGE_INC_MASK	0xff
-
-/* Instruction definitions */
-#define	INSN_VSP_INC		0x00
-#define	INSN_VSP_DEC		0x40
-#define	INSN_POP_MASKED		0x80
-#define	INSN_VSP_REG		0x90
-#define	INSN_POP_COUNT		0xa0
-#define	INSN_FINISH		0xb0
-#define	INSN_POP_REGS		0xb1
-#define	INSN_VSP_LARGE_INC	0xb2
-
-/* An item in the exception index table */
-struct unwind_idx {
-	uint32_t offset;
-	uint32_t insn;
-};
-
-/* Expand a 31-bit signed value to a 32-bit signed value */
-static int32_t expand_prel31(uint32_t prel31)
+bool find_exidx(vaddr_t addr __unused, vaddr_t *idx_start, vaddr_t *idx_end)
 {
-	return prel31 | SHIFT_U32(prel31 & BIT32(30), 1);
-}
-
-/*
- * Perform a binary search of the index table to find the function
- * with the largest address that doesn't exceed addr.
- */
-static struct unwind_idx *find_index(uint32_t addr, vaddr_t exidx,
-				     size_t exidx_sz)
-{
-	vaddr_t idx_start, idx_end;
-	unsigned int min, mid, max;
-	struct unwind_idx *start;
-	struct unwind_idx *item;
-	int32_t prel31_addr;
-	vaddr_t func_addr;
-
-	start = (struct unwind_idx *)exidx;
-	idx_start = exidx;
-	idx_end = exidx + exidx_sz;
-
-	min = 0;
-	max = (idx_end - idx_start) / sizeof(struct unwind_idx);
-
-	while (min != max) {
-		mid = min + (max - min + 1) / 2;
-
-		item = &start[mid];
-
-		prel31_addr = expand_prel31(item->offset);
-		func_addr = (vaddr_t)&item->offset + prel31_addr;
-
-		if (func_addr <= addr) {
-			min = mid;
-		} else {
-			max = mid - 1;
-		}
-	}
-
-	return &start[min];
-}
-
-/* Reads the next byte from the instruction list */
-static bool unwind_exec_read_byte(struct unwind_state_arm32 *state,
-				  uint32_t *ret_insn)
-{
-	uint32_t insn;
-
-	memcpy(&insn, (void *)state->insn, sizeof(insn));
-
-	/* Read the unwind instruction */
-	*ret_insn = (insn >> (state->byte * 8)) & 0xff;
-
-	/* Update the location of the next instruction */
-	if (state->byte == 0) {
-		state->byte = 3;
-		state->insn += sizeof(uint32_t);
-		state->entries--;
-	} else
-		state->byte--;
-
+	*idx_start = (vaddr_t)__exidx_start;
+	*idx_end = (vaddr_t)__exidx_end;
 	return true;
 }
 
-static bool pop_vsp(uint32_t *reg, vaddr_t *vsp,
-		    vaddr_t stack, size_t stack_size)
-{
-	if (!core_is_buffer_inside(*vsp, sizeof(*reg), stack, stack_size))
-		return false;
-
-	memcpy(reg, (void *)*vsp, sizeof(*reg));
-	(*vsp) += sizeof(*reg);
-	return true;
-}
-
-/* Executes the next instruction on the list */
-static bool unwind_exec_insn(struct unwind_state_arm32 *state,
-			     vaddr_t stack, size_t stack_size)
-{
-	uint32_t insn;
-	vaddr_t vsp = state->registers[SP];
-	int update_vsp = 0;
-
-	/* Read the next instruction */
-	if (!unwind_exec_read_byte(state, &insn))
-		return false;
-
-	if ((insn & INSN_VSP_MASK) == INSN_VSP_INC) {
-		state->registers[SP] += ((insn & INSN_VSP_SIZE_MASK) << 2) + 4;
-
-	} else if ((insn & INSN_VSP_MASK) == INSN_VSP_DEC) {
-		state->registers[SP] -= ((insn & INSN_VSP_SIZE_MASK) << 2) + 4;
-
-	} else if ((insn & INSN_STD_MASK) == INSN_POP_MASKED) {
-		uint32_t mask;
-		unsigned int reg;
-
-		/* Load the mask */
-		if (!unwind_exec_read_byte(state, &mask))
-			return false;
-		mask |= (insn & INSN_STD_DATA_MASK) << 8;
-
-		/* We have a refuse to unwind instruction */
-		if (mask == 0)
-			return false;
-
-		/* Update SP */
-		update_vsp = 1;
-
-		/* Load the registers */
-		for (reg = 4; mask && reg < 16; mask >>= 1, reg++) {
-			if (mask & 1) {
-				if (!pop_vsp(&state->registers[reg], &vsp,
-					     stack, stack_size))
-					return false;
-				state->update_mask |= 1 << reg;
-
-				/* If we have updated SP kep its value */
-				if (reg == SP)
-					update_vsp = 0;
-			}
-		}
-
-	} else if ((insn & INSN_STD_MASK) == INSN_VSP_REG &&
-	    ((insn & INSN_STD_DATA_MASK) != 13) &&
-	    ((insn & INSN_STD_DATA_MASK) != 15)) {
-		/* sp = register */
-		state->registers[SP] =
-		    state->registers[insn & INSN_STD_DATA_MASK];
-
-	} else if ((insn & INSN_STD_MASK) == INSN_POP_COUNT) {
-		unsigned int count, reg;
-
-		/* Read how many registers to load */
-		count = insn & INSN_POP_COUNT_MASK;
-
-		/* Update sp */
-		update_vsp = 1;
-
-		/* Pop the registers */
-		for (reg = 4; reg <= 4 + count; reg++) {
-			if (!pop_vsp(&state->registers[reg], &vsp,
-				     stack, stack_size))
-				return false;
-			state->update_mask |= 1 << reg;
-		}
-
-		/* Check if we are in the pop r14 version */
-		if ((insn & INSN_POP_TYPE_MASK) != 0) {
-			if (!pop_vsp(&state->registers[14], &vsp,
-				     stack, stack_size))
-				return false;
-		}
-
-	} else if (insn == INSN_FINISH) {
-		/* Stop processing */
-		state->entries = 0;
-
-	} else if (insn == INSN_POP_REGS) {
-		uint32_t mask;
-		unsigned int reg;
-
-		if (!unwind_exec_read_byte(state, &mask))
-			return false;
-		if (mask == 0 || (mask & 0xf0) != 0)
-			return false;
-
-		/* Update SP */
-		update_vsp = 1;
-
-		/* Load the registers */
-		for (reg = 0; mask && reg < 4; mask >>= 1, reg++) {
-			if (mask & 1) {
-				if (!pop_vsp(&state->registers[reg], &vsp,
-					     stack, stack_size))
-					return false;
-				state->update_mask |= 1 << reg;
-			}
-		}
-
-	} else if ((insn & INSN_VSP_LARGE_INC_MASK) == INSN_VSP_LARGE_INC) {
-		uint32_t uleb128;
-
-		/* Read the increment value */
-		if (!unwind_exec_read_byte(state, &uleb128))
-			return false;
-
-		state->registers[SP] += 0x204 + (uleb128 << 2);
-
-	} else {
-		/* We hit a new instruction that needs to be implemented */
-		DMSG("Unhandled instruction %.2x", insn);
-		return false;
-	}
-
-	if (update_vsp)
-		state->registers[SP] = vsp;
-
-	return true;
-}
-
-/* Performs the unwind of a function */
-static bool unwind_tab(struct unwind_state_arm32 *state,
-		       vaddr_t stack, size_t stack_size)
-{
-	uint32_t entry;
-	uint32_t insn;
-
-	/* Set PC to a known value */
-	state->registers[PC] = 0;
-
-	memcpy(&insn, (void *)state->insn, sizeof(insn));
-
-	/* Read the personality */
-	entry = insn & ENTRY_MASK;
-
-	if (entry == ENTRY_ARM_SU16) {
-		state->byte = 2;
-		state->entries = 1;
-	} else if (entry == ENTRY_ARM_LU16) {
-		state->byte = 1;
-		state->entries = ((insn >> 16) & 0xFF) + 1;
-	} else {
-		DMSG("Unknown entry: %x", entry);
-		return true;
-	}
-
-	while (state->entries > 0) {
-		if (!unwind_exec_insn(state, stack, stack_size))
-			return true;
-	}
-
-	/*
-	 * The program counter was not updated, load it from the link register.
-	 */
-	if (state->registers[PC] == 0) {
-		state->registers[PC] = state->registers[LR];
-
-		/*
-		 * If the program counter changed, flag it in the update mask.
-		 */
-		if (state->start_pc != state->registers[PC])
-			state->update_mask |= 1 << PC;
-	}
-
-	return false;
-}
-
-bool unwind_stack_arm32(struct unwind_state_arm32 *state, vaddr_t exidx,
-			size_t exidx_sz, vaddr_t stack, size_t stack_size)
-{
-	struct unwind_idx *index;
-	bool finished;
-
-	if (!exidx_sz)
-		return false;
-
-	/* Reset the mask of updated registers */
-	state->update_mask = 0;
-
-	/* The pc value is correct and will be overwritten, save it */
-	state->start_pc = state->registers[PC];
-
-	/*
-	 * Find the item to run. Subtract 2 from PC to make sure that we're
-	 * still inside the calling function in case a __no_return function
-	 * (typically panic()) is called unconditionally and may cause LR and
-	 * thus this PC to point into the next and entirely unrelated function.
-	 */
-	index = find_index(state->start_pc - 2, exidx, exidx_sz);
-
-	finished = false;
-	if (index->insn != EXIDX_CANTUNWIND) {
-		if (index->insn & (1U << 31)) {
-			/* The data is within the instruction */
-			state->insn = (vaddr_t)&index->insn;
-		} else {
-			/* A prel31 offset to the unwind table */
-			state->insn = (vaddr_t)&index->insn +
-				      expand_prel31(index->insn);
-		}
-
-		/* Run the unwind function */
-		finished = unwind_tab(state, stack, stack_size);
-	}
-
-	/* This is the top of the stack, finish */
-	if (index->insn == EXIDX_CANTUNWIND)
-		finished = true;
-
-	return !finished;
-}
-
-#if (TRACE_LEVEL > 0)
-
-void print_stack_arm32(struct unwind_state_arm32 *state,
-		       vaddr_t exidx, size_t exidx_sz,
-		       vaddr_t stack, size_t stack_size)
-{
-	trace_printf_helper_raw(TRACE_ERROR, true,
-				"TEE load address @ %#"PRIxVA, VCORE_START_VA);
-	trace_printf_helper_raw(TRACE_ERROR, true, "Call stack:");
-	do {
-		trace_printf_helper_raw(TRACE_ERROR, true, " 0x%08" PRIx32,
-					state->registers[PC]);
-	} while (unwind_stack_arm32(state, exidx, exidx_sz, stack, stack_size));
-}
-
-#endif
-
-#if defined(ARM32) && (TRACE_LEVEL > 0)
-
-void print_kernel_stack(void)
-{
-	struct unwind_state_arm32 state = {};
-	uaddr_t exidx = (vaddr_t)__exidx_start;
-	size_t exidx_sz = (vaddr_t)__exidx_end - (vaddr_t)__exidx_start;
-	vaddr_t stack_start = 0;
-	vaddr_t stack_end = 0;
-
-	/* r7: Thumb-style frame pointer */
-	state.registers[7] = read_r7();
-	/* r11: ARM-style frame pointer */
-	state.registers[FP] = read_fp();
-	state.registers[SP] = read_sp();
-	state.registers[LR] = read_lr();
-
-	/*
-	 * Add 4 to make sure that we have an address well inside this function.
-	 * This is needed because we're subtracting 2 from PC when calling
-	 * find_index() above. See a comment there for more details.
-	 */
-	state.registers[PC] = (uint32_t)print_kernel_stack + 4;
-
-	get_stack_hard_limits(&stack_start, &stack_end);
-	print_stack_arm32(&state, exidx, exidx_sz, stack_start,
-			  stack_end - stack_start);
-}
-
-#endif
-
-#if defined(ARM32)
 vaddr_t *unw_get_kernel_stack(void)
 {
 	size_t n = 0;
@@ -456,7 +60,6 @@ vaddr_t *unw_get_kernel_stack(void)
 	vaddr_t *tmp = NULL;
 	vaddr_t *addr = NULL;
 	struct unwind_state_arm32 state = { };
-	uaddr_t exidx = (vaddr_t)__exidx_start;
 	vaddr_t stack = thread_stack_start();
 	size_t stack_size = thread_stack_size();
 
@@ -478,7 +81,7 @@ vaddr_t *unw_get_kernel_stack(void)
 	 */
 	state.registers[PC] = (uint32_t)unw_get_kernel_stack + 4;
 
-	while (unwind_stack_arm32(&state, exidx, exidx_sz, stack, stack_size)) {
+	while (unwind_stack_arm32(&state, stack, stack_size)) {
 		tmp = unw_grow(addr, &size, (n + 1) * sizeof(vaddr_t));
 		if (!tmp)
 			goto err;
@@ -498,7 +101,33 @@ vaddr_t *unw_get_kernel_stack(void)
 	return addr;
 err:
 	EMSG("Out of memory");
-	free(addr);
 	return NULL;
+}
+
+#if (TRACE_LEVEL > 0)
+void print_kernel_stack(void)
+{
+	struct unwind_state_arm32 state = { };
+	vaddr_t stack_start = 0;
+	vaddr_t stack_end = 0;
+
+	/* r7: Thumb-style frame pointer */
+	state.registers[7] = read_r7();
+	/* r11: ARM-style frame pointer */
+	state.registers[FP] = read_fp();
+	state.registers[SP] = read_sp();
+	state.registers[LR] = read_lr();
+
+	/*
+	 * Add 4 to make sure that we have an address well inside this function.
+	 * This is needed because we're subtracting 2 from PC when calling
+	 * find_index() above. See a comment there for more details.
+	 */
+	state.registers[PC] = (uint32_t)print_kernel_stack + 4;
+
+	trace_printf_helper_raw(TRACE_ERROR, true,
+				"TEE load address @ %#"PRIxVA, VCORE_START_VA);
+	get_stack_hard_limits(&stack_start, &stack_end);
+	print_stack_arm32(&state, stack_start, stack_end - stack_start);
 }
 #endif

core/arch/arm/kernel/unwind_arm64.c
@@ -31,109 +31,11 @@
 
 #include <arm.h>
 #include <kernel/linker.h>
-#include <kernel/tee_misc.h>
-#include <kernel/tee_ta_manager.h>
 #include <kernel/thread.h>
 #include <kernel/unwind.h>
-#include <string.h>
-#include <tee/tee_svc.h>
-#include <trace.h>
-#include <user_ta_header.h>
-#include <util.h>
+#include <unw/unwind.h>
 
 #include "unwind_private.h"
-
-static void copy_in_reg(uint64_t *reg, vaddr_t addr)
-{
-	memcpy(reg, (void *)addr, sizeof(*reg));
-}
-
-#ifdef CFG_SYSCALL_FTRACE
-static void ftrace_core_map_lr(uint64_t *lr)
-{
-	struct ftrace_buf *fbuf = NULL;
-	struct tee_ta_session *s = NULL;
-
-	if (tee_ta_get_current_session(&s) != TEE_SUCCESS)
-		return;
-
-	if (!s->fbuf)
-		return;
-
-	fbuf = s->fbuf;
-
-	/*
-	 * Function tracer inserts return hook (addr: &__ftrace_return)
-	 * via modifying lr values in the stack frames. And during aborts,
-	 * stack trace picks these modified lr values which needs to be
-	 * replaced with original lr value. So here we use the ftrace return
-	 * stack to retrieve original lr value but we need to first check if
-	 * it has actually been modified or not in case TA is profiled
-	 * partially.
-	 */
-	if ((*lr == (uint64_t)&__ftrace_return) &&
-	    fbuf->lr_idx < fbuf->ret_idx) {
-		fbuf->lr_idx++;
-		*lr = fbuf->ret_stack[fbuf->ret_idx - fbuf->lr_idx];
-	}
-}
-#else
-static void ftrace_core_map_lr(uint64_t *lr __unused)
-{
-}
-#endif
-
-bool unwind_stack_arm64(struct unwind_state_arm64 *frame,
-			vaddr_t stack, size_t stack_size)
-{
-	vaddr_t fp = frame->fp;
-
-	if (!core_is_buffer_inside(fp, sizeof(uint64_t) * 3,
-				   stack, stack_size))
-		return false;
-
-	frame->sp = fp + 0x10;
-	/* FP to previous frame (X29) */
-	copy_in_reg(&frame->fp, fp);
-	/* LR (X30) */
-	copy_in_reg(&frame->pc, fp + 8);
-
-	ftrace_core_map_lr(&frame->pc);
-
-	frame->pc -= 4;
-
-	return true;
-}
-
-#if (TRACE_LEVEL > 0)
-
-void print_stack_arm64(struct unwind_state_arm64 *state,
-		       vaddr_t stack, size_t stack_size)
-{
-	trace_printf_helper_raw(TRACE_ERROR, true,
-				"TEE load address @ %#"PRIxVA, VCORE_START_VA);
-	trace_printf_helper_raw(TRACE_ERROR, true, "Call stack:");
-
-	do {
-		trace_printf_helper_raw(TRACE_ERROR, true, " 0x%016" PRIx64,
-					state->pc);
-	} while (unwind_stack_arm64(state, stack, stack_size));
-}
-
-void print_kernel_stack(void)
-{
-	struct unwind_state_arm64 state = {};
-	vaddr_t stack_start = 0;
-	vaddr_t stack_end = 0;
-
-	state.pc = read_pc();
-	state.fp = read_fp();
-
-	get_stack_hard_limits(&stack_start, &stack_end);
-	print_stack_arm64(&state, stack_start, stack_end - stack_start);
-}
-
-#endif
 
 vaddr_t *unw_get_kernel_stack(void)
 {
@@ -141,12 +43,12 @@ vaddr_t *unw_get_kernel_stack(void)
 	size_t size = 0;
 	vaddr_t *tmp = NULL;
 	vaddr_t *addr = NULL;
-	struct unwind_state_arm64 state = { 0 };
 	uaddr_t stack = thread_stack_start();
 	size_t stack_size = thread_stack_size();
-
-	state.pc = read_pc();
-	state.fp = read_fp();
+	struct unwind_state_arm64 state = {
+		.pc = read_pc(),
+		.fp = read_fp()
+	};
 
 	while (unwind_stack_arm64(&state, stack, stack_size)) {
 		tmp = unw_grow(addr, &size, (n + 1) * sizeof(vaddr_t));
@@ -170,4 +72,19 @@ err:
 	EMSG("Out of memory");
 	free(addr);
 	return NULL;
+}
+
+void print_kernel_stack(void)
+{
+	struct unwind_state_arm64 state = { };
+	vaddr_t stack_start = 0;
+	vaddr_t stack_end = 0;
+
+	state.pc = read_pc();
+	state.fp = read_fp();
+
+	trace_printf_helper_raw(TRACE_ERROR, true,
+				"TEE load address @ %#"PRIxVA, VCORE_START_VA);
+	get_stack_hard_limits(&stack_start, &stack_end);
+	print_stack_arm64(&state, stack_start, stack_end - stack_start);
 }

core/arch/arm/kernel/unwind_private.h
@@ -8,11 +8,14 @@
 
 static inline void *unw_grow(void *p, size_t *cur_size, size_t new_size)
 {
+	size_t rounded_size = 0;
+	void *tmp = NULL;
+
 	if (*cur_size >= new_size)
 		return p;
 
-	size_t rounded_size = ROUNDUP(new_size, 16 * sizeof(vaddr_t));
-	void *tmp = realloc(p, rounded_size);
+	rounded_size = ROUNDUP(new_size, 16 * sizeof(vaddr_t));
+	tmp = realloc(p, rounded_size);
 
 	if (tmp)
 		*cur_size = rounded_size;

core/core.mk
@@ -142,6 +142,10 @@ libdir = core/lib/zlib
 include mk/lib.mk
 endif
 
+libname = unw
+libdir = lib/libunw
+include mk/lib.mk
+
 #
 # Do main source
 #

commit d37884e59c4d0c205a4d8e5a690e83d852b81c66

core: print "TEE load address" message on abort
Commit 02d307b7db90 ("core: use libunw") has involuntarily removed the
"TEE load address @ ..." message when a TEE core abort occurs. This
information is essential to be able to resolve function addresses when
ASLR is enabled, and scripts/symbolize.py needs this line. Add it back.

Fixes: 02d307b7db90 ("core: use libunw")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/abort.c
@@ -5,6 +5,7 @@
 
 #include <arm.h>
 #include <kernel/abort.h>
+#include <kernel/linker.h>
 #include <kernel/misc.h>
 #include <kernel/panic.h>
 #include <kernel/tee_ta_manager.h>
@@ -217,8 +218,12 @@ static void __abort_print(struct abort_info *ai, bool stack_dump)
 
 	__print_abort_info(ai, "Core");
 
-	if (stack_dump)
+	if (stack_dump) {
+		trace_printf_helper_raw(TRACE_ERROR, true,
+					"TEE load address @ %#"PRIxVA,
+					VCORE_START_VA);
 		__print_stack_unwind(ai);
+	}
 }
 
 void abort_print(struct abort_info *ai)