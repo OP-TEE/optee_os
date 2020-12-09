784d32ef2978f5ea9af9e40376dae02eb3f28b8c
Jerome Forissier <jerome@forissier.org>
2020-12-09 15:17:13 +0100

    core: remove unused TEE_TIME_BOOT_TICKS_HZ
    The TEE_TIME_BOOT_TICKS_HZ macro is not used, remove it.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/include/kernel/tee_time.h
+++ core/include/kernel/tee_time.h
@@ -8,8 +8,6 @@
 
 #include "tee_api_types.h"
 
-#define TEE_TIME_BOOT_TICKS_HZ  10UL
-
 TEE_Result tee_time_get_sys_time(TEE_Time *time);
 uint32_t tee_time_get_sys_time_protection_level(void);
 TEE_Result tee_time_get_ta_time(const TEE_UUID *uuid, TEE_Time *time);
