OP-TEE/optee_os

commit 512cbf1d30ddce3513abf2b08d4063d5c8415a40

ta: pkcs11: adds support for symmetric AES cipher modes
Adds support for the AES modes ECB, CBC, CTR and CTS.

Much infrastructure is added to handle mechanisms.

Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/pkcs11/src/pkcs11_token.c
@@ -14,6 +14,7 @@
 #include <tee_internal_api_extensions.h>
 #include <util.h>
 
+#include "attributes.h"
 #include "pkcs11_helpers.h"
 #include "pkcs11_token.h"
 #include "serializer.h"
@@ -148,6 +149,63 @@ void pkcs11_deinit(void)
 
 	for (id = 0; id < TOKEN_COUNT; id++)
 		close_persistent_db(get_token(id));
+}
+
+/*
+ * Currently no support for dual operations.
+ */
+enum pkcs11_rc set_processing_state(struct pkcs11_session *session,
+				    enum processing_func function,
+				    struct pkcs11_object *obj1,
+				    struct pkcs11_object *obj2)
+{
+	enum pkcs11_proc_state state = PKCS11_SESSION_READY;
+	struct active_processing *proc = NULL;
+
+	if (session->processing)
+		return PKCS11_CKR_OPERATION_ACTIVE;
+
+	switch (function) {
+	case PKCS11_FUNCTION_ENCRYPT:
+		state = PKCS11_SESSION_ENCRYPTING;
+		break;
+	case PKCS11_FUNCTION_DECRYPT:
+		state = PKCS11_SESSION_DECRYPTING;
+		break;
+	case PKCS11_FUNCTION_SIGN:
+		state = PKCS11_SESSION_SIGNING;
+		break;
+	case PKCS11_FUNCTION_VERIFY:
+		state = PKCS11_SESSION_VERIFYING;
+		break;
+	case PKCS11_FUNCTION_DIGEST:
+		state = PKCS11_SESSION_DIGESTING;
+		break;
+	case PKCS11_FUNCTION_DERIVE:
+		state = PKCS11_SESSION_READY;
+		break;
+	default:
+		TEE_Panic(function);
+		return -1;
+	}
+
+	proc = TEE_Malloc(sizeof(*proc), TEE_MALLOC_FILL_ZERO);
+	if (!proc)
+		return PKCS11_CKR_DEVICE_MEMORY;
+
+	/* Boolean are default to false and pointers to NULL */
+	proc->state = state;
+	proc->tee_op_handle = TEE_HANDLE_NULL;
+
+	if (obj1 && get_bool(obj1->attributes, PKCS11_CKA_ALWAYS_AUTHENTICATE))
+		proc->always_authen = true;
+
+	if (obj2 && get_bool(obj2->attributes, PKCS11_CKA_ALWAYS_AUTHENTICATE))
+		proc->always_authen = true;
+
+	session->processing = proc;
+
+	return PKCS11_CKR_OK;
 }
 
 enum pkcs11_rc entry_ck_slot_list(uint32_t ptypes, TEE_Param *params)

ta/pkcs11/src/entry.c
@@ -13,6 +13,7 @@
 #include "object.h"
 #include "pkcs11_helpers.h"
 #include "pkcs11_token.h"
+#include "processing.h"
 
 TEE_Result TA_CreateEntryPoint(void)
 {
@@ -206,6 +207,45 @@ TEE_Result TA_InvokeCommandEntryPoint(void *tee_session, uint32_t cmd,
 		break;
 	case PKCS11_CMD_DESTROY_OBJECT:
 		rc = entry_destroy_object(client, ptypes, params);
+		break;
+
+	case PKCS11_CMD_ENCRYPT_INIT:
+		rc = entry_processing_init(client, ptypes, params,
+					   PKCS11_FUNCTION_ENCRYPT);
+		break;
+	case PKCS11_CMD_DECRYPT_INIT:
+		rc = entry_processing_init(client, ptypes, params,
+					   PKCS11_FUNCTION_DECRYPT);
+		break;
+	case PKCS11_CMD_ENCRYPT_UPDATE:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_ENCRYPT,
+					   PKCS11_FUNC_STEP_UPDATE);
+		break;
+	case PKCS11_CMD_DECRYPT_UPDATE:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DECRYPT,
+					   PKCS11_FUNC_STEP_UPDATE);
+		break;
+	case PKCS11_CMD_ENCRYPT_ONESHOT:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_ENCRYPT,
+					   PKCS11_FUNC_STEP_ONESHOT);
+		break;
+	case PKCS11_CMD_DECRYPT_ONESHOT:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DECRYPT,
+					   PKCS11_FUNC_STEP_ONESHOT);
+		break;
+	case PKCS11_CMD_ENCRYPT_FINAL:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_ENCRYPT,
+					   PKCS11_FUNC_STEP_FINAL);
+		break;
+	case PKCS11_CMD_DECRYPT_FINAL:
+		rc = entry_processing_step(client, ptypes, params,
+					   PKCS11_FUNCTION_DECRYPT,
+					   PKCS11_FUNC_STEP_FINAL);
 		break;
 
 	default:

ta/pkcs11/src/pkcs11_attributes.c
@@ -24,6 +24,100 @@
 /* Byte size of CKA_ID attribute when generated locally */
 #define PKCS11_CKA_DEFAULT_SIZE		16
 
+static uint32_t pkcs11_func2ckfm(enum processing_func function)
+{
+	switch (function) {
+	case PKCS11_FUNCTION_DIGEST:
+		return PKCS11_CKFM_DIGEST;
+	case PKCS11_FUNCTION_GENERATE:
+		return PKCS11_CKFM_GENERATE;
+	case PKCS11_FUNCTION_GENERATE_PAIR:
+		return PKCS11_CKFM_GENERATE_KEY_PAIR;
+	case PKCS11_FUNCTION_DERIVE:
+		return PKCS11_CKFM_DERIVE;
+	case PKCS11_FUNCTION_WRAP:
+		return PKCS11_CKFM_WRAP;
+	case PKCS11_FUNCTION_UNWRAP:
+		return PKCS11_CKFM_UNWRAP;
+	case PKCS11_FUNCTION_ENCRYPT:
+		return PKCS11_CKFM_ENCRYPT;
+	case PKCS11_FUNCTION_DECRYPT:
+		return PKCS11_CKFM_DECRYPT;
+	case PKCS11_FUNCTION_SIGN:
+		return PKCS11_CKFM_SIGN;
+	case PKCS11_FUNCTION_VERIFY:
+		return PKCS11_CKFM_VERIFY;
+	case PKCS11_FUNCTION_SIGN_RECOVER:
+		return PKCS11_CKFM_SIGN_RECOVER;
+	case PKCS11_FUNCTION_VERIFY_RECOVER:
+		return PKCS11_CKFM_VERIFY_RECOVER;
+	default:
+		return 0;
+	}
+}
+
+enum pkcs11_rc
+check_mechanism_against_processing(struct pkcs11_session *session,
+				   enum pkcs11_mechanism_id mechanism_type,
+				   enum processing_func function,
+				   enum processing_step step)
+{
+	bool allowed = false;
+
+	switch (step) {
+	case PKCS11_FUNC_STEP_INIT:
+		switch (function) {
+		case PKCS11_FUNCTION_IMPORT:
+		case PKCS11_FUNCTION_COPY:
+		case PKCS11_FUNCTION_MODIFY:
+		case PKCS11_FUNCTION_DESTROY:
+			return PKCS11_CKR_OK;
+		default:
+			break;
+		}
+		/*
+		 * Check that the returned PKCS11_CKFM_* flag from
+		 * pkcs11_func2ckfm() is among the ones from
+		 * mechanism_supported_flags().
+		 */
+		allowed = mechanism_supported_flags(mechanism_type) &
+			  pkcs11_func2ckfm(function);
+		break;
+
+	case PKCS11_FUNC_STEP_ONESHOT:
+	case PKCS11_FUNC_STEP_UPDATE:
+		if (session->processing->always_authen &&
+		    !session->processing->relogged)
+			return PKCS11_CKR_USER_NOT_LOGGED_IN;
+
+		if (!session->processing->updated)
+			allowed = true;
+		else
+			allowed = !mechanism_is_one_shot_only(mechanism_type);
+		break;
+
+	case PKCS11_FUNC_STEP_FINAL:
+		if (session->processing->always_authen &&
+		    !session->processing->relogged)
+			return PKCS11_CKR_USER_NOT_LOGGED_IN;
+
+		return PKCS11_CKR_OK;
+
+	default:
+		TEE_Panic(step);
+		break;
+	}
+
+	if (!allowed) {
+		EMSG("Processing %#x/%s not permitted (%u/%u)",
+		     (unsigned int)mechanism_type, id2str_proc(mechanism_type),
+		     function, step);
+		return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
+	}
+
+	return PKCS11_CKR_OK;
+}
+
 /*
  * Object default boolean attributes as per PKCS#11
  */
@@ -691,6 +785,35 @@ static enum pkcs11_rc check_attrs_misc_integrity(struct obj_attrs *head)
 }
 
 /*
+ * Check access to object against authentication to token
+ */
+enum pkcs11_rc check_access_attrs_against_token(struct pkcs11_session *session,
+						struct obj_attrs *head)
+{
+	bool private = true;
+
+	switch (get_class(head)) {
+	case PKCS11_CKO_SECRET_KEY:
+	case PKCS11_CKO_PUBLIC_KEY:
+	case PKCS11_CKO_DATA:
+		private = get_bool(head, PKCS11_CKA_PRIVATE);
+		break;
+	case PKCS11_CKO_PRIVATE_KEY:
+		break;
+	default:
+		return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
+	}
+
+	if (private && pkcs11_session_is_public(session)) {
+		DMSG("Private object access from a public session");
+
+		return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
+	}
+
+	return PKCS11_CKR_OK;
+}
+
+/*
  * Check the attributes of a to-be-created object matches the token state
  */
 enum pkcs11_rc check_created_attrs_against_token(struct pkcs11_session *session,
@@ -769,6 +892,172 @@ enum pkcs11_rc check_created_attrs_against_processing(uint32_t proc_id,
 	default:
 		TEE_Panic(proc_id);
 		break;
+	}
+
+	return PKCS11_CKR_OK;
+}
+
+static void get_key_min_max_sizes(enum pkcs11_key_type key_type,
+				  uint32_t *min_key_size,
+				  uint32_t *max_key_size)
+{
+	enum pkcs11_mechanism_id mechanism = PKCS11_CKM_UNDEFINED_ID;
+
+	switch (key_type) {
+	case PKCS11_CKK_AES:
+		mechanism = PKCS11_CKM_AES_KEY_GEN;
+		break;
+	default:
+		TEE_Panic(key_type);
+		break;
+	}
+
+	mechanism_supported_key_sizes(mechanism, min_key_size,
+				      max_key_size);
+}
+
+enum pkcs11_rc check_created_attrs(struct obj_attrs *key1,
+				   struct obj_attrs *key2)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	struct obj_attrs *secret = NULL;
+	uint32_t max_key_size = 0;
+	uint32_t min_key_size = 0;
+	uint32_t key_length = 0;
+
+	switch (get_class(key1)) {
+	case PKCS11_CKO_SECRET_KEY:
+		secret = key1;
+		break;
+	default:
+		return PKCS11_CKR_ATTRIBUTE_VALUE_INVALID;
+	}
+
+	if (key2)
+		return PKCS11_CKR_ATTRIBUTE_VALUE_INVALID;
+
+	if (secret) {
+		switch (get_key_type(secret)) {
+		case PKCS11_CKK_AES:
+		case PKCS11_CKK_GENERIC_SECRET:
+		case PKCS11_CKK_MD5_HMAC:
+		case PKCS11_CKK_SHA_1_HMAC:
+		case PKCS11_CKK_SHA224_HMAC:
+		case PKCS11_CKK_SHA256_HMAC:
+		case PKCS11_CKK_SHA384_HMAC:
+		case PKCS11_CKK_SHA512_HMAC:
+			break;
+		default:
+			return PKCS11_CKR_TEMPLATE_INCONSISTENT;
+		}
+
+		/* Get key size */
+		rc = get_u32_attribute(secret, PKCS11_CKA_VALUE_LEN,
+				       &key_length);
+		if (rc)
+			return PKCS11_CKR_TEMPLATE_INCONSISTENT;
+	}
+
+	get_key_min_max_sizes(get_key_type(key1), &min_key_size, &max_key_size);
+	if (key_length < min_key_size || key_length > max_key_size) {
+		EMSG("Length %"PRIu32" vs range [%"PRIu32" %"PRIu32"]",
+		     key_length, min_key_size, max_key_size);
+
+		return PKCS11_CKR_KEY_SIZE_RANGE;
+	}
+
+	return PKCS11_CKR_OK;
+}
+
+/* Check processing ID against attribute ALLOWED_MECHANISMS if any */
+static bool parent_key_complies_allowed_processings(uint32_t proc_id,
+						    struct obj_attrs *head)
+{
+	char *attr = NULL;
+	uint32_t size = 0;
+	uint32_t proc = 0;
+	size_t count = 0;
+
+	/* Check only if restricted allowed mechanisms list is defined */
+	if (get_attribute_ptr(head, PKCS11_CKA_ALLOWED_MECHANISMS,
+			      (void *)&attr, &size) != PKCS11_CKR_OK) {
+		return true;
+	}
+
+	for (count = size / sizeof(uint32_t); count; count--) {
+		TEE_MemMove(&proc, attr, sizeof(uint32_t));
+		attr += sizeof(uint32_t);
+
+		if (proc == proc_id)
+			return true;
+	}
+
+	DMSG("can't find %s in allowed list", id2str_proc(proc_id));
+	return false;
+}
+
+static enum pkcs11_attr_id func_to_attr(enum processing_func func)
+{
+	switch (func) {
+	case PKCS11_FUNCTION_ENCRYPT:
+		return PKCS11_CKA_ENCRYPT;
+	case PKCS11_FUNCTION_DECRYPT:
+		return PKCS11_CKA_DECRYPT;
+	case PKCS11_FUNCTION_SIGN:
+		return PKCS11_CKA_SIGN;
+	case PKCS11_FUNCTION_VERIFY:
+		return PKCS11_CKA_VERIFY;
+	case PKCS11_FUNCTION_WRAP:
+		return PKCS11_CKA_WRAP;
+	case PKCS11_FUNCTION_UNWRAP:
+		return PKCS11_CKA_UNWRAP;
+	case PKCS11_FUNCTION_DERIVE:
+		return PKCS11_CKA_DERIVE;
+	default:
+		return PKCS11_CKA_UNDEFINED_ID;
+	}
+}
+
+enum pkcs11_rc
+check_parent_attrs_against_processing(enum pkcs11_mechanism_id proc_id,
+				      enum processing_func function,
+				      struct obj_attrs *head)
+{
+	enum pkcs11_class_id key_class = get_class(head);
+	enum pkcs11_key_type key_type = get_key_type(head);
+	enum pkcs11_attr_id attr = func_to_attr(function);
+
+	if (!get_bool(head, attr)) {
+		DMSG("%s not permitted", id2str_attr(attr));
+		return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
+	}
+
+	/* Check processing complies with parent key family */
+	switch (proc_id) {
+	case PKCS11_CKM_AES_ECB:
+	case PKCS11_CKM_AES_CBC:
+	case PKCS11_CKM_AES_CBC_PAD:
+	case PKCS11_CKM_AES_CTS:
+	case PKCS11_CKM_AES_CTR:
+		if (key_class == PKCS11_CKO_SECRET_KEY &&
+		    key_type == PKCS11_CKK_AES)
+			break;
+
+		DMSG("%s invalid key %s/%s", id2str_proc(proc_id),
+		     id2str_class(key_class), id2str_key_type(key_type));
+
+		return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
+
+	default:
+		DMSG("Invalid processing %#"PRIx32"/%s", proc_id,
+		     id2str_proc(proc_id));
+
+		return PKCS11_CKR_MECHANISM_INVALID;
+	}
+
+	if (!parent_key_complies_allowed_processings(proc_id, head)) {
+		DMSG("Allowed mechanism failed");
+		return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
 	}
 
 	return PKCS11_CKR_OK;

ta/pkcs11/src/pkcs11_attributes.h
@@ -134,4 +134,29 @@ enum pkcs11_rc check_created_attrs_against_token(struct pkcs11_session *session,
 enum pkcs11_rc check_created_attrs_against_processing(uint32_t proc_id,
 						      struct obj_attrs *head);
 
+enum pkcs11_rc check_created_attrs(struct obj_attrs *key1,
+				   struct obj_attrs *key2);
+
+/*
+ * Check the attributes of the parent secret (key) used in the processing
+ * do match the target processing.
+ *
+ * @proc_id - PKCS11_CKM_xxx
+ * @func - identifier of the processing function operated with @proc_id.
+ * @head - head of the attributes of parent object.
+ */
+enum pkcs11_rc
+check_parent_attrs_against_processing(enum pkcs11_mechanism_id proc_id,
+				      enum processing_func func,
+				      struct obj_attrs *head);
+
+enum pkcs11_rc check_access_attrs_against_token(struct pkcs11_session *session,
+						struct obj_attrs *head);
+
+enum pkcs11_rc
+check_mechanism_against_processing(struct pkcs11_session *session,
+				   enum pkcs11_mechanism_id mechanism_type,
+				   enum processing_func function,
+				   enum processing_step step);
+
 #endif /*PKCS11_TA_PKCS11_ATTRIBUTES_H*/

ta/pkcs11/src/pkcs11_helpers.c
@@ -9,6 +9,7 @@
 #include <util.h>
 
 #include "attributes.h"
+#include "object.h"
 #include "pkcs11_attributes.h"
 #include "pkcs11_helpers.h"
 
@@ -152,6 +153,14 @@ static const struct any_id __maybe_unused string_ta_cmd[] = {
 	PKCS11_ID(PKCS11_CMD_LOGOUT),
 	PKCS11_ID(PKCS11_CMD_CREATE_OBJECT),
 	PKCS11_ID(PKCS11_CMD_DESTROY_OBJECT),
+	PKCS11_ID(PKCS11_CMD_ENCRYPT_INIT),
+	PKCS11_ID(PKCS11_CMD_DECRYPT_INIT),
+	PKCS11_ID(PKCS11_CMD_ENCRYPT_UPDATE),
+	PKCS11_ID(PKCS11_CMD_DECRYPT_UPDATE),
+	PKCS11_ID(PKCS11_CMD_ENCRYPT_FINAL),
+	PKCS11_ID(PKCS11_CMD_DECRYPT_FINAL),
+	PKCS11_ID(PKCS11_CMD_ENCRYPT_ONESHOT),
+	PKCS11_ID(PKCS11_CMD_DECRYPT_ONESHOT),
 };
 
 static const struct any_id __maybe_unused string_slot_flags[] = {
@@ -438,6 +447,46 @@ int pkcs11_attr2boolprop_shift(uint32_t attr)
 			return (int)pos;
 
 	return -1;
+}
+
+/* Initialize a TEE attribute for a target PKCS11 TA attribute in an object */
+bool pkcs2tee_load_attr(TEE_Attribute *tee_ref, uint32_t tee_id,
+			struct pkcs11_object *obj,
+			enum pkcs11_attr_id pkcs11_id)
+{
+	void *a_ptr = NULL;
+	uint32_t a_size = 0;
+
+	if (get_attribute_ptr(obj->attributes, pkcs11_id, &a_ptr, &a_size))
+		return false;
+
+	TEE_InitRefAttribute(tee_ref, tee_id, a_ptr, a_size);
+
+	return true;
+}
+
+/* Easy conversion between PKCS11 TA function of TEE crypto mode */
+void pkcs2tee_mode(uint32_t *tee_id, enum processing_func function)
+{
+	switch (function) {
+	case PKCS11_FUNCTION_ENCRYPT:
+		*tee_id = TEE_MODE_ENCRYPT;
+		break;
+	case PKCS11_FUNCTION_DECRYPT:
+		*tee_id = TEE_MODE_DECRYPT;
+		break;
+	case PKCS11_FUNCTION_SIGN:
+		*tee_id = TEE_MODE_SIGN;
+		break;
+	case PKCS11_FUNCTION_VERIFY:
+		*tee_id = TEE_MODE_VERIFY;
+		break;
+	case PKCS11_FUNCTION_DERIVE:
+		*tee_id = TEE_MODE_DERIVE;
+		break;
+	default:
+		TEE_Panic(function);
+	}
 }
 
 #if CFG_TEE_TA_LOG_LEVEL > 0

ta/pkcs11/src/pkcs11_helpers.h
@@ -9,7 +9,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include <pkcs11_attributes.h>
 #include <token_capabilities.h>
+
+struct pkcs11_object;
 
 /*
  * TEE invocation parameter#0 is an in/out buffer of at least 32bit
@@ -49,6 +52,14 @@ bool key_type_is_asymm_key(uint32_t key_type_id);
 
 /* Boolprop flag shift position if @attribute_id is boolean, else -1 */
 int pkcs11_attr2boolprop_shift(uint32_t attribute_id);
+
+/* Convert PKCS11 TA function ID into a TEE crypto operation mode */
+void pkcs2tee_mode(uint32_t *tee_id, enum processing_func function);
+
+/* Load TEE operation attributes from a PKCS11 object, return false on error */
+bool pkcs2tee_load_attr(TEE_Attribute *tee_ref, uint32_t tee_id,
+			struct pkcs11_object *obj,
+			enum pkcs11_attr_id pkcs11_id);
 
 /* Return true if attribute is a boolean, false otherwise */
 static inline bool pkcs11_attr_is_boolean(enum pkcs11_attr_id id)

ta/pkcs11/src/pkcs11_token.h
@@ -101,6 +101,48 @@ struct ck_token {
 };
 
 /*
+ * A session can enter a processing state (encrypt, decrypt, digest, ...)
+ * only from the initialized state. A session must return the initialized
+ * state (from a processing finalization request) before entering another
+ * processing state.
+ */
+enum pkcs11_proc_state {
+	PKCS11_SESSION_READY = 0,		/* No active processing */
+	PKCS11_SESSION_ENCRYPTING,
+	PKCS11_SESSION_DECRYPTING,
+	PKCS11_SESSION_DIGESTING,
+	PKCS11_SESSION_DIGESTING_ENCRYPTING,	/* case C_DigestEncryptUpdate */
+	PKCS11_SESSION_DECRYPTING_DIGESTING,	/* case C_DecryptDigestUpdate */
+	PKCS11_SESSION_SIGNING,
+	PKCS11_SESSION_SIGNING_ENCRYPTING,	/* case C_SignEncryptUpdate */
+	PKCS11_SESSION_VERIFYING,
+	PKCS11_SESSION_DECRYPTING_VERIFYING,	/* case C_DecryptVerifyUpdate */
+	PKCS11_SESSION_SIGNING_RECOVER,
+	PKCS11_SESSION_VERIFYING_RECOVER,
+};
+
+/*
+ * Context of the active processing in the session
+ *
+ * @state - ongoing active processing function or ready state
+ * @mecha_type - mechanism type of the active processing
+ * @always_authen - true if user need to login before each use
+ * @relogged - true once client logged since last operation update
+ * @updated - true once an active operation is updated
+ * @tee_op_handle - handle on active crypto operation or TEE_HANDLE_NULL
+ * @extra_ctx - context for the active processing
+ */
+struct active_processing {
+	enum pkcs11_proc_state state;
+	uint32_t mecha_type;
+	bool always_authen;
+	bool relogged;
+	bool updated;
+	TEE_OperationHandle tee_op_handle;
+	void *extra_ctx;
+};
+
+/*
  * Structure tracking the PKCS#11 sessions
  *
  * @link - List of the session belonging to a client
@@ -110,15 +152,17 @@ struct ck_token {
  * @object_list - Entry of the session objects list
  * @object_handle_db - Database for object handles published by the session
  * @state - R/W SO, R/W user, RO user, R/W public, RO public.
+ * @processing - Reference to initialized processing context if any
  */
 struct pkcs11_session {
 	TAILQ_ENTRY(pkcs11_session) link;
 	struct pkcs11_client *client;
 	struct ck_token *token;
-	uint32_t handle;
+	enum pkcs11_mechanism_id handle;
 	struct object_list object_list;
 	struct handle_db object_handle_db;
 	enum pkcs11_session_state state;
+	struct active_processing *processing;
 };
 
 /* Initialize static token instance(s) from default/persistent database */
@@ -163,6 +207,16 @@ void unregister_client(struct pkcs11_client *client);
 
 struct pkcs11_session *pkcs11_handle2session(uint32_t handle,
 					     struct pkcs11_client *client);
+
+static inline bool session_is_active(struct pkcs11_session *session)
+{
+	return session->processing;
+}
+
+enum pkcs11_rc set_processing_state(struct pkcs11_session *session,
+				    enum processing_func function,
+				    struct pkcs11_object *obj1,
+				    struct pkcs11_object *obj2);
 
 static inline bool pkcs11_session_is_read_write(struct pkcs11_session *session)
 {

ta/pkcs11/src/processing.c
@@ -1,0 +1,340 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#include <assert.h>
+#include <pkcs11_ta.h>
+#include <string.h>
+#include <tee_api_defines.h>
+#include <tee_internal_api.h>
+#include <tee_internal_api_extensions.h>
+#include <util.h>
+
+#include "attributes.h"
+#include "object.h"
+#include "pkcs11_attributes.h"
+#include "pkcs11_helpers.h"
+#include "pkcs11_token.h"
+#include "processing.h"
+#include "serializer.h"
+
+static enum pkcs11_rc get_ready_session(struct pkcs11_session *session)
+{
+	if (session_is_active(session))
+		return PKCS11_CKR_OPERATION_ACTIVE;
+
+	return PKCS11_CKR_OK;
+}
+
+static bool func_matches_state(enum processing_func function,
+			       enum pkcs11_proc_state state)
+{
+	switch (function) {
+	case PKCS11_FUNCTION_ENCRYPT:
+		return state == PKCS11_SESSION_ENCRYPTING ||
+		       state == PKCS11_SESSION_DIGESTING_ENCRYPTING ||
+		       state == PKCS11_SESSION_SIGNING_ENCRYPTING;
+	case PKCS11_FUNCTION_DECRYPT:
+		return state == PKCS11_SESSION_DECRYPTING ||
+		       state == PKCS11_SESSION_DECRYPTING_DIGESTING ||
+		       state == PKCS11_SESSION_DECRYPTING_VERIFYING;
+	case PKCS11_FUNCTION_DIGEST:
+		return state == PKCS11_SESSION_DIGESTING ||
+		       state == PKCS11_SESSION_DIGESTING_ENCRYPTING;
+	case PKCS11_FUNCTION_SIGN:
+		return state == PKCS11_SESSION_SIGNING ||
+		       state == PKCS11_SESSION_SIGNING_ENCRYPTING;
+	case PKCS11_FUNCTION_VERIFY:
+		return state == PKCS11_SESSION_VERIFYING ||
+		       state == PKCS11_SESSION_DECRYPTING_VERIFYING;
+	case PKCS11_FUNCTION_SIGN_RECOVER:
+		return state == PKCS11_SESSION_SIGNING_RECOVER;
+	case PKCS11_FUNCTION_VERIFY_RECOVER:
+		return state == PKCS11_SESSION_SIGNING_RECOVER;
+	default:
+		TEE_Panic(function);
+		return false;
+	}
+}
+
+static enum pkcs11_rc get_active_session(struct pkcs11_session *session,
+					 enum processing_func function)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_OPERATION_NOT_INITIALIZED;
+
+	if (session->processing &&
+	    func_matches_state(function, session->processing->state))
+		rc = PKCS11_CKR_OK;
+
+	return rc;
+}
+
+void release_active_processing(struct pkcs11_session *session)
+{
+	if (!session->processing)
+		return;
+
+	switch (session->processing->mecha_type) {
+	case PKCS11_CKM_AES_CTR:
+		tee_release_ctr_operation(session->processing);
+		break;
+	default:
+		break;
+	}
+
+	if (session->processing->tee_op_handle != TEE_HANDLE_NULL) {
+		TEE_FreeOperation(session->processing->tee_op_handle);
+		session->processing->tee_op_handle = TEE_HANDLE_NULL;
+	}
+
+	TEE_Free(session->processing);
+	session->processing = NULL;
+}
+
+size_t get_object_key_bit_size(struct pkcs11_object *obj)
+{
+	uint32_t a_size = 0;
+	struct obj_attrs *attrs = obj->attributes;
+
+	switch (get_key_type(attrs)) {
+	case PKCS11_CKK_AES:
+		if (get_attribute_ptr(attrs, PKCS11_CKA_VALUE, NULL, &a_size))
+			return 0;
+
+		return a_size * 8;
+	default:
+		TEE_Panic(0);
+		return 0;
+	}
+}
+
+/*
+ * entry_processing_init - Generic entry for initializing a processing
+ *
+ * @client = client reference
+ * @ptype = Invocation parameter types
+ * @params = Invocation parameters reference
+ * @function - encrypt, decrypt, sign, verify, digest, ...
+ */
+enum pkcs11_rc entry_processing_init(struct pkcs11_client *client,
+				     uint32_t ptypes, TEE_Param *params,
+				     enum processing_func function)
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INOUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	TEE_Param *ctrl = params;
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	struct serialargs ctrlargs = { };
+	struct pkcs11_session *session = NULL;
+	struct pkcs11_attribute_head *proc_params = NULL;
+	uint32_t key_handle = 0;
+	struct pkcs11_object *obj = NULL;
+
+	if (!client || ptypes != exp_pt)
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	serialargs_init(&ctrlargs, ctrl->memref.buffer, ctrl->memref.size);
+
+	rc = serialargs_get_session_from_handle(&ctrlargs, client, &session);
+	if (rc)
+		return rc;
+
+	rc = serialargs_get(&ctrlargs, &key_handle, sizeof(uint32_t));
+	if (rc)
+		return rc;
+
+	rc = serialargs_alloc_get_one_attribute(&ctrlargs, &proc_params);
+	if (rc)
+		return rc;
+
+	if (serialargs_remaining_bytes(&ctrlargs)) {
+		rc = PKCS11_CKR_ARGUMENTS_BAD;
+		goto out;
+	}
+
+	rc = get_ready_session(session);
+	if (rc)
+		goto out;
+
+	obj = pkcs11_handle2object(key_handle, session);
+	if (!obj) {
+		rc = PKCS11_CKR_KEY_HANDLE_INVALID;
+		goto out;
+	}
+
+	rc = set_processing_state(session, function, obj, NULL);
+	if (rc)
+		goto out;
+
+	rc = check_mechanism_against_processing(session, proc_params->id,
+						function,
+						PKCS11_FUNC_STEP_INIT);
+	if (rc)
+		goto out;
+
+	rc = check_parent_attrs_against_processing(proc_params->id, function,
+						   obj->attributes);
+	if (rc)
+		goto out;
+
+	rc = check_access_attrs_against_token(session, obj->attributes);
+	if (rc)
+		goto out;
+
+	if (processing_is_tee_symm(proc_params->id))
+		rc = init_symm_operation(session, function, proc_params, obj);
+	else
+		rc = PKCS11_CKR_MECHANISM_INVALID;
+
+	if (rc == PKCS11_CKR_OK) {
+		session->processing->mecha_type = proc_params->id;
+		DMSG("PKCS11 session %"PRIu32": init processing %s %s",
+		     session->handle, id2str_proc(proc_params->id),
+		     id2str_function(function));
+	}
+
+out:
+	if (rc && session)
+		release_active_processing(session);
+
+	TEE_Free(proc_params);
+
+	return rc;
+}
+
+/*
+ * entry_processing_step - Generic entry on active processing
+ *
+ * @client = client reference
+ * @ptype = Invocation parameter types
+ * @params = Invocation parameters reference
+ * @function - encrypt, decrypt, sign, verify, digest, ...
+ * @step - update, oneshot, final
+ */
+enum pkcs11_rc entry_processing_step(struct pkcs11_client *client,
+				     uint32_t ptypes, TEE_Param *params,
+				     enum processing_func function,
+				     enum processing_step step)
+{
+	TEE_Param *ctrl = params;
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	struct serialargs ctrlargs = { };
+	struct pkcs11_session *session = NULL;
+	enum pkcs11_mechanism_id mecha_type = PKCS11_CKM_UNDEFINED_ID;
+
+	if (!client ||
+	    TEE_PARAM_TYPE_GET(ptypes, 0) != TEE_PARAM_TYPE_MEMREF_INOUT)
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	serialargs_init(&ctrlargs, ctrl->memref.buffer, ctrl->memref.size);
+
+	rc = serialargs_get_session_from_handle(&ctrlargs, client, &session);
+	if (rc)
+		return rc;
+
+	if (serialargs_remaining_bytes(&ctrlargs))
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	rc = get_active_session(session, function);
+	if (rc)
+		return rc;
+
+	mecha_type = session->processing->mecha_type;
+	rc = check_mechanism_against_processing(session, mecha_type,
+						function, step);
+	if (rc)
+		goto out;
+
+	if (processing_is_tee_symm(mecha_type))
+		rc = step_symm_operation(session, function, step,
+					 ptypes, params);
+	else
+		rc = PKCS11_CKR_MECHANISM_INVALID;
+
+	if (rc == PKCS11_CKR_OK) {
+		session->processing->updated = true;
+		DMSG("PKCS11 session%"PRIu32": processing %s %s",
+		     session->handle, id2str_proc(mecha_type),
+		     id2str_function(function));
+	}
+
+out:
+	switch (step) {
+	case PKCS11_FUNC_STEP_UPDATE:
+		if (rc != PKCS11_CKR_OK && rc != PKCS11_CKR_BUFFER_TOO_SMALL)
+			release_active_processing(session);
+		break;
+	default:
+		/* ONESHOT and FINAL terminates processing on success */
+		if (rc != PKCS11_CKR_BUFFER_TOO_SMALL)
+			release_active_processing(session);
+		break;
+	}
+
+	return rc;
+}
+
+/*
+ * entry_verify_oneshot - Run a single part verification processing
+ *
+ * @client = client reference
+ * @ptype = Invocation parameter types
+ * @params = Invocation parameters reference
+ * @function - encrypt, decrypt, sign, verify, digest, ...
+ * @step - update, oneshot, final
+ */
+enum pkcs11_rc entry_verify_oneshot(struct pkcs11_client *client,
+				    uint32_t ptypes, TEE_Param *params,
+				    enum processing_func function,
+				    enum processing_step step)
+
+{
+	TEE_Param *ctrl = params;
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	struct serialargs ctrlargs = { };
+	struct pkcs11_session *session = NULL;
+	enum pkcs11_mechanism_id mecha_type = PKCS11_CKM_UNDEFINED_ID;
+
+	assert(function == PKCS11_FUNCTION_VERIFY);
+	if (!client ||
+	    TEE_PARAM_TYPE_GET(ptypes, 0) != TEE_PARAM_TYPE_MEMREF_INOUT)
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	serialargs_init(&ctrlargs, ctrl->memref.buffer, ctrl->memref.size);
+
+	rc = serialargs_get_session_from_handle(&ctrlargs, client, &session);
+	if (rc)
+		return rc;
+
+	if (serialargs_remaining_bytes(&ctrlargs))
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	rc = get_active_session(session, function);
+	if (rc)
+		return rc;
+
+	mecha_type = session->processing->mecha_type;
+	rc = check_mechanism_against_processing(session, mecha_type,
+						function, step);
+	if (rc)
+		goto out;
+
+	if (processing_is_tee_symm(mecha_type))
+		rc = step_symm_operation(session, function, step,
+					 ptypes, params);
+	else
+		rc = PKCS11_CKR_MECHANISM_INVALID;
+
+	DMSG("PKCS11 session %"PRIu32": verify %s %s: %s", session->handle,
+	     id2str_proc(mecha_type), id2str_function(function),
+	     id2str_rc(rc));
+
+out:
+	if (rc != PKCS11_CKR_BUFFER_TOO_SMALL)
+		release_active_processing(session);
+
+	return rc;
+}

commit 2403ee5cf4730a3aa44b1737a90b1c9439c721cb

ta: pkcs11: release active process on session closure
Release the possible active processing resources when closing a session.

Fixes: 512cbf1d30dd ("ta: pkcs11: adds support for symmetric AES cipher modes")
Reviewed-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

ta/pkcs11/src/pkcs11_token.c
@@ -17,6 +17,7 @@
 #include "attributes.h"
 #include "pkcs11_helpers.h"
 #include "pkcs11_token.h"
+#include "processing.h"
 #include "serializer.h"
 
 /* Provide 3 slots/tokens, ID is token index */
@@ -650,6 +651,8 @@ enum pkcs11_rc entry_ck_open_session(struct pkcs11_client *client,
 
 static void close_ck_session(struct pkcs11_session *session)
 {
+	release_active_processing(session);
+
 	/* No need to put object handles, the whole database is destroyed */
 	while (!LIST_EMPTY(&session->object_list))
 		destroy_object(session,