cd65da48ee14e7377c923e124331d248003d2d18
Jorge Ramirez-Ortiz <jorge@foundries.io>
2020-12-15 18:58:53 +0100

    crypto: se050: adaptors: elliptic curve
    APDU and utility functions required to support elliptic curve
    cryptography.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/se050/adaptors/include/se050_utils.h
+++ core/drivers/crypto/se050/adaptors/include/se050_utils.h
@@ -33,8 +33,10 @@ struct s050_scp_rotate_cmd {
 sss_status_t se050_get_oid(sss_key_object_mode_t type, uint32_t *val);
 
 struct rsa_keypair;
+struct ecc_keypair;
 
 uint32_t se050_rsa_keypair_from_nvm(struct rsa_keypair *key);
+uint32_t se050_ecc_keypair_from_nvm(struct ecc_keypair *key);
 uint64_t se050_generate_private_key(uint32_t oid);
 
 void se050_refcount_init_ctx(uint8_t **cnt);
