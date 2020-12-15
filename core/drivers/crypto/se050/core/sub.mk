4c69b1f137d643f739fafd8652e7e35148d22fc9
Jorge Ramirez-Ortiz <jorge@foundries.io>
2020-12-15 19:00:05 +0100

    crypto: se050: build: elliptic curve
    Makefile changes to compile-in and enable elliptic curve support.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/se050/core/sub.mk
+++ core/drivers/crypto/se050/core/sub.mk
@@ -10,6 +10,7 @@ incdirs-y += include
 srcs-y += scp03.c
 srcs-y += storage.c
 srcs-$(CFG_NXP_SE05X_RSA_DRV) += rsa.c
+srcs-$(CFG_NXP_SE05X_ECC_DRV) += ecc.c
 srcs-$(CFG_NXP_SE05X_CTR_DRV) += ctr.c
 srcs-$(CFG_NXP_SE05X_HUK_DRV) += huk.c
 srcs-$(CFG_NXP_SE05X_RNG_DRV) += rng.c
