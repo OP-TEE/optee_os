4c3354e3e1d639a27b24a8c787aa37798b131437
Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
2020-12-25 21:00:39 +0200

    ta: pkcs11: Allocate command ID for key pair generation
    Allocate command ID for C_GenerateKeyPair() functionality.
    
    Reviewed-by: Ricardo Salveti <ricardo@foundries.io>
    Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

--- ta/pkcs11/include/pkcs11_ta.h
+++ ta/pkcs11/include/pkcs11_ta.h
@@ -657,6 +657,28 @@ enum pkcs11_ta_cmd {
 	 * This command relates to the PKCS#11 API function C_Digest().
 	 */
 	PKCS11_CMD_DIGEST_ONESHOT = 49,
+
+	/*
+	 * PKCS11_CMD_GENERATE_KEY_PAIR - Generate an asymmetric key pair
+	 *
+	 * [in]  memref[0] = [
+	 *              32bit session handle,
+	 *              (struct pkcs11_attribute_head)mechanism + mecha params,
+	 *              (struct pkcs11_object_head)public key attribs +
+	 *              attributes data,
+	 *              (struct pkcs11_object_head)private key attribs +
+	 *              attributes data
+	 *	 ]
+	 * [out] memref[0] = 32bit return code, enum pkcs11_rc
+	 * [out] memref[2] = [
+	 *              32bit public key object handle,
+	 *              32bit private key object handle
+	 *	 ]
+	 *
+	 * This command relates to the PKCS#11 API function
+	 * C_GenerateKeyPair().
+	 */
+	PKCS11_CMD_GENERATE_KEY_PAIR = 50,
 };
 
 /*
