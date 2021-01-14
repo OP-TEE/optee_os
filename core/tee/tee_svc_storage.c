OP-TEE/optee_os

commit 928efd065222491b7c6255e3ec98aab18dc9925a

core: syscall_storage_next_enum() use live pobj
Instead of using a fake pobj in syscall_storage_next_enum() retrieve the
shared pobj instead in order to get the flags of an already opened
object.

TEE_POBJ_USAGE_ENUM is supplied to tee_pobj_get() to avoid checking for
conflicts with how the pobj is already used.

Reviewed-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc_storage.c
@@ -686,28 +686,6 @@ TEE_Result syscall_storage_reset_enum(unsigned long obj_enum)
 	return TEE_SUCCESS;
 }
 
-static TEE_Result tee_svc_storage_set_enum(struct tee_fs_dirent *d,
-			const struct tee_file_operations *fops,
-			struct tee_obj *o)
-{
-	o->info.handleFlags =
-	    TEE_HANDLE_FLAG_PERSISTENT | TEE_HANDLE_FLAG_INITIALIZED;
-	o->info.objectUsage = TEE_USAGE_DEFAULT;
-
-	if (d->oidlen > TEE_OBJECT_ID_MAX_LEN)
-		return TEE_ERROR_CORRUPT_OBJECT;
-
-	o->pobj->obj_id = malloc(d->oidlen);
-	if (!o->pobj->obj_id)
-		return TEE_ERROR_OUT_OF_MEMORY;
-
-	memcpy(o->pobj->obj_id, d->oid, d->oidlen);
-	o->pobj->obj_id_len = d->oidlen;
-	o->pobj->fops = fops;
-
-	return TEE_SUCCESS;
-}
-
 TEE_Result syscall_storage_start_enum(unsigned long obj_enum,
 				      unsigned long storage_id)
 {
@@ -791,18 +769,14 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 		res = TEE_ERROR_OUT_OF_MEMORY;
 		goto exit;
 	}
-	o->info.handleFlags = TEE_DATA_FLAG_SHARE_READ;
 
-	o->pobj = calloc(1, sizeof(struct tee_pobj));
-	if (!o->pobj) {
-		res = TEE_ERROR_OUT_OF_MEMORY;
+	res = tee_pobj_get(&sess->ctx->uuid, d->oid, d->oidlen, 0,
+			   TEE_POBJ_USAGE_ENUM, e->fops, &o->pobj);
+	if (res)
 		goto exit;
-	}
 
-	o->pobj->uuid = sess->ctx->uuid;
-	res = tee_svc_storage_set_enum(d, e->fops, o);
-	if (res != TEE_SUCCESS)
-		goto exit;
+	o->info.handleFlags = o->pobj->flags | TEE_HANDLE_FLAG_PERSISTENT |
+			      TEE_HANDLE_FLAG_INITIALIZED;
 
 	res = tee_svc_storage_read_head(o);
 	if (res != TEE_SUCCESS)
@@ -816,12 +790,7 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 
 exit:
 	if (o) {
-		if (o->pobj) {
-			if (o->pobj->fops)
-				o->pobj->fops->close(&o->fh);
-			free(o->pobj->obj_id);
-		}
-		free(o->pobj);
+		tee_pobj_release(o->pobj);
 		tee_obj_free(o);
 	}
 

core/include/tee/tee_pobj.h
@@ -27,6 +27,7 @@ enum tee_pobj_usage {
 	TEE_POBJ_USAGE_OPEN,
 	TEE_POBJ_USAGE_RENAME,
 	TEE_POBJ_USAGE_CREATE,
+	TEE_POBJ_USAGE_ENUM,
 };
 
 TEE_Result tee_pobj_get(TEE_UUID *uuid, void *obj_id, uint32_t obj_id_len,

core/tee/tee_pobj.c
@@ -80,6 +80,10 @@ TEE_Result tee_pobj_get(TEE_UUID *uuid, void *obj_id, uint32_t obj_id_len,
 	}
 
 	if (*obj) {
+		if (usage == TEE_POBJ_USAGE_ENUM) {
+			(*obj)->refcnt++;
+			goto out;
+		}
 		if (temporary != (*obj)->temporary) {
 			res = TEE_ERROR_ACCESS_CONFLICT;
 			goto out;

commit 0d1820b4f03ed83fee1328e816f9724b14064098

core: fix file handle leakage in syscall_storage_next_enum()
Prior to this patch was syscall_storage_next_enum() opening a file
handle with tee_svc_storage_read_head() but never freeing the handle.
Fix this by closing the file handle as part of cleaning up before
returning.

Fixes: 928efd065222 ("core: syscall_storage_next_enum() use live pobj")
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Tested-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc_storage.c
@@ -756,7 +756,10 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 
 exit:
 	if (o) {
-		tee_pobj_release(o->pobj);
+		if (o->pobj) {
+			o->pobj->fops->close(&o->fh);
+			tee_pobj_release(o->pobj);
+		}
 		tee_obj_free(o);
 	}
 