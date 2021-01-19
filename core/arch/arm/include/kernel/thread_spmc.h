c185655eafaab6b4b27759812cd6633e1da9db12
Jelle Sels <jelle.sels@arm.com>
2021-01-19 13:48:15 +0100

    core: Initiate and load Secure Partitions
    Secure Partitions (SP) are S-El0 execution service defined in the Arm
    FF-A specification.
    The Secure Partitions are loaded as the last part of the boot process.
    A Secure Partitions can be added to image using the SP_PATHS build
    option. The SPs are loaded using ldelf.
    
    Signed-off-by: Jelle Sels <jelle.sels@arm.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ core/arch/arm/include/kernel/thread_spmc.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, Arm Limited.
+ */
+#ifndef __KERNEL_THREAD_SPMC_H
+#define __KERNEL_THREAD_SPMC_H
+
+/* FF-A endpoint base ID when OP-TEE is used as a S-EL1 endpoint */
+#define SPMC_ENDPOINT_ID        0x8001
+
+#endif /* __KERNEL_THREAD_SPMC_H */
