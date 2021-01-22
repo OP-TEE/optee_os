ade0994c57b388cf0ce5799a5118fb2daf1c155b
Jerome Forissier <jerome@forissier.org>
2021-01-22 16:30:41 +0100

    libmbedtls: add SM2 curve
    When MBEDTLS_ECP_DP_SM2_ENABLED is set, adds support for the ECC curve
    defined for the Chinese SM2 algorithm (G/MT 0003 Part 5, [1]).
    
    Link: [1] http://www.gmbz.org.cn/upload/2018-07-24/1532401863206085511.pdf
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    [jf: rebased onto mbedtls-2.27.0]
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

--- lib/libmbedtls/mbedtls/include/mbedtls/ecp.h
+++ lib/libmbedtls/mbedtls/include/mbedtls/ecp.h
@@ -71,7 +71,8 @@
     defined(MBEDTLS_ECP_DP_BP512R1_ENABLED) || \
     defined(MBEDTLS_ECP_DP_SECP192K1_ENABLED) || \
     defined(MBEDTLS_ECP_DP_SECP224K1_ENABLED) || \
-    defined(MBEDTLS_ECP_DP_SECP256K1_ENABLED)
+    defined(MBEDTLS_ECP_DP_SECP256K1_ENABLED) || \
+    defined(MBEDTLS_ECP_DP_SM2_ENABLED)
 #define MBEDTLS_ECP_SHORT_WEIERSTRASS_ENABLED
 #endif
 #if defined(MBEDTLS_ECP_DP_CURVE25519_ENABLED) || \
@@ -123,6 +124,7 @@ typedef enum
     MBEDTLS_ECP_DP_SECP224K1,      /*!< Domain parameters for 224-bit "Koblitz" curve. */
     MBEDTLS_ECP_DP_SECP256K1,      /*!< Domain parameters for 256-bit "Koblitz" curve. */
     MBEDTLS_ECP_DP_CURVE448,       /*!< Domain parameters for Curve448. */
+    MBEDTLS_ECP_DP_SM2,            /*!< Domain parameters for SM2. */
 } mbedtls_ecp_group_id;
 
 /**
@@ -191,6 +193,8 @@ mbedtls_ecp_point;
 #elif defined(MBEDTLS_ECP_DP_SECP256K1_ENABLED)
 #define MBEDTLS_ECP_MAX_BITS_MIN 256
 #elif defined(MBEDTLS_ECP_DP_SECP256R1_ENABLED)
+#define MBEDTLS_ECP_MAX_BITS_MIN 256
+#elif defined(MBEDTLS_ECP_DP_SM2_ENABLED)
 #define MBEDTLS_ECP_MAX_BITS_MIN 256
 #elif defined(MBEDTLS_ECP_DP_CURVE25519_ENABLED)
 #define MBEDTLS_ECP_MAX_BITS_MIN 255
