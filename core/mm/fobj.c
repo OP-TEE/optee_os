aad1cf6bbe80ed145f48e8650b96119d835ee0a0
Jens Wiklander <jens.wiklander@linaro.org>
2021-01-25 11:39:12 +0100

    core: fobj_rw_paged_alloc() store tags and IVs in paged area
    fobj_rw_paged_alloc() is updated to store tags and IVs in a designated
    paged area instead of storing them in the heap. This avoids large heap
    allocations which also would suffer from a fragmented heap.
    
    The previous ops_rw_paged and struct fobj_rwp are now replaced by
    ops_rwp_unpaged_iv and struct fobj_rwp_unpaged_iv respectively. These
    are now only used to support the area where other tags and IVs are
    stored.
    
    A new ops_rwp_paged_iv and struct fobj_rwp_paged_iv are added for
    using the designated paged area.
    
    A fobj based on the ops_rwp_unpaged_iv ops is allocated and registered
    with the pager via a callback registered with driver_init_late().
    
    This effectively enables paging of IV and tags for pages mapping a TA.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/mm/fobj.c
+++ core/mm/fobj.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2019, Linaro Limited
+ * Copyright (c) 2019-2021, Linaro Limited
  */
 
 #include <crypto/crypto.h>
@@ -33,36 +33,34 @@ struct rwp_state {
 	uint8_t tag[RWP_AES_GCM_TAG_LEN];
 };
 
-struct fobj_rwp {
+/*
+ * Note that this struct is padded to a size which is a power of 2, this
+ * guarantees that this state will not span two pages. This avoids a corner
+ * case in the pager when making the state available.
+ */
+struct rwp_state_padded {
+	struct rwp_state state;
+	uint64_t pad;
+};
+
+struct fobj_rwp_unpaged_iv {
 	uint8_t *store;
 	struct rwp_state *state;
 	struct fobj fobj;
 };
 
-static const struct fobj_ops ops_rw_paged;
+struct fobj_rwp_paged_iv {
+	size_t idx;
+	struct fobj fobj;
+};
+
+static const struct fobj_ops ops_rwp_paged_iv;
+static const struct fobj_ops ops_rwp_unpaged_iv;
 
 static struct internal_aes_gcm_key rwp_ae_key;
 
-/*
- * fobj_generate_authenc_key() - Generate authentication key
- *
- * Generates the authentication key used in all fobjs allocated with
- * fobj_rw_paged_alloc().
- */
-static TEE_Result fobj_generate_authenc_key(void)
-{
-	uint8_t key[RWP_AE_KEY_BITS / 8] = { 0 };
-
-	if (crypto_rng_read(key, sizeof(key)) != TEE_SUCCESS)
-		panic("failed to generate random");
-	if (crypto_aes_expand_enc_key(key, sizeof(key), rwp_ae_key.data,
-				      sizeof(rwp_ae_key.data),
-				      &rwp_ae_key.rounds))
-		panic("failed to expand key");
-
-	return TEE_SUCCESS;
-}
-driver_init_late(fobj_generate_authenc_key);
+static struct rwp_state_padded *rwp_state_base;
+static uint8_t *rwp_store_base;
 
 static void fobj_init(struct fobj *fobj, const struct fobj_ops *ops,
 		      unsigned int num_pages)
@@ -80,77 +78,16 @@ static void fobj_uninit(struct fobj *fobj)
 	tee_pager_invalidate_fobj(fobj);
 }
 
-struct fobj *fobj_rw_paged_alloc(unsigned int num_pages)
+static TEE_Result rwp_load_page(void *va, struct rwp_state *state,
+				const uint8_t *src)
 {
-	tee_mm_entry_t *mm = NULL;
-	struct fobj_rwp *rwp = NULL;
-	size_t size = 0;
-
-	assert(num_pages);
-
-	rwp = calloc(1, sizeof(*rwp));
-	if (!rwp)
-		return NULL;
-
-	rwp->state = calloc(num_pages, sizeof(*rwp->state));
-	if (!rwp->state)
-		goto err;
-
-	if (MUL_OVERFLOW(num_pages, SMALL_PAGE_SIZE, &size))
-		goto err;
-	mm = tee_mm_alloc(&tee_mm_sec_ddr, size);
-	if (!mm)
-		goto err;
-	rwp->store = phys_to_virt(tee_mm_get_smem(mm), MEM_AREA_TA_RAM);
-	assert(rwp->store); /* to assist debugging if it would ever happen */
-	if (!rwp->store)
-		goto err;
-
-	fobj_init(&rwp->fobj, &ops_rw_paged, num_pages);
-
-	return &rwp->fobj;
-
-err:
-	tee_mm_free(mm);
-	free(rwp->state);
-	free(rwp);
-
-	return NULL;
-}
-
-static struct fobj_rwp *to_rwp(struct fobj *fobj)
-{
-	assert(fobj->ops == &ops_rw_paged);
-
-	return container_of(fobj, struct fobj_rwp, fobj);
-}
-
-static void rwp_free(struct fobj *fobj)
-{
-	struct fobj_rwp *rwp = to_rwp(fobj);
-
-	fobj_uninit(fobj);
-	tee_mm_free(tee_mm_find(&tee_mm_sec_ddr, virt_to_phys(rwp->store)));
-	free(rwp->state);
-	free(rwp);
-}
-
-static TEE_Result rwp_load_page(struct fobj *fobj, unsigned int page_idx,
-				void *va)
-{
-	struct fobj_rwp *rwp = to_rwp(fobj);
-	struct rwp_state *state = rwp->state + page_idx;
-	uint8_t *src = rwp->store + page_idx * SMALL_PAGE_SIZE;
 	struct rwp_aes_gcm_iv iv = {
 		.iv = { (vaddr_t)state, state->iv >> 32, state->iv }
 	};
 
-	assert(refcount_val(&fobj->refc));
-	assert(page_idx < fobj->num_pages);
-
 	if (!state->iv) {
 		/*
-		 * iv still zero which means that this is previously unused
+		 * IV still zero which means that this is previously unused
 		 * page.
 		 */
 		memset(va, 0, SMALL_PAGE_SIZE);
@@ -161,18 +98,106 @@ static TEE_Result rwp_load_page(struct fobj *fobj, unsigned int page_idx,
 				    NULL, 0, src, SMALL_PAGE_SIZE, va,
 				    state->tag, sizeof(state->tag));
 }
-DECLARE_KEEP_PAGER(rwp_load_page);
 
-static TEE_Result rwp_save_page(struct fobj *fobj, unsigned int page_idx,
-				const void *va)
+static TEE_Result rwp_save_page(const void *va, struct rwp_state *state,
+				uint8_t *dst)
 {
-	struct fobj_rwp *rwp = to_rwp(fobj);
-	struct rwp_state *state = rwp->state + page_idx;
 	size_t tag_len = sizeof(state->tag);
-	uint8_t *dst = rwp->store + page_idx * SMALL_PAGE_SIZE;
-	struct rwp_aes_gcm_iv iv;
+	struct rwp_aes_gcm_iv iv = { };
 
-	memset(&iv, 0, sizeof(iv));
+	assert(state->iv + 1 > state->iv);
+
+	state->iv++;
+
+	/*
+	 * IV is constructed as recommended in section "8.2.1 Deterministic
+	 * Construction" of "Recommendation for Block Cipher Modes of
+	 * Operation: Galois/Counter Mode (GCM) and GMAC",
+	 * http://csrc.nist.gov/publications/nistpubs/800-38D/SP-800-38D.pdf
+	 */
+	iv.iv[0] = (vaddr_t)state;
+	iv.iv[1] = state->iv >> 32;
+	iv.iv[2] = state->iv;
+
+	return internal_aes_gcm_enc(&rwp_ae_key, &iv, sizeof(iv),
+				    NULL, 0, va, SMALL_PAGE_SIZE, dst,
+				    state->tag, &tag_len);
+}
+
+static struct rwp_state_padded *idx_to_state_padded(size_t idx)
+{
+	assert(rwp_state_base);
+	return rwp_state_base + idx;
+}
+
+static uint8_t *idx_to_store(size_t idx)
+{
+	assert(rwp_store_base);
+	return rwp_store_base + idx * SMALL_PAGE_SIZE;
+}
+
+struct fobj *fobj_rw_paged_alloc(unsigned int num_pages)
+{
+	struct fobj_rwp_paged_iv *rwp = NULL;
+	tee_mm_entry_t *mm = NULL;
+	size_t size = 0;
+
+	COMPILE_TIME_ASSERT(IS_POWER_OF_TWO(sizeof(struct rwp_state_padded)));
+	assert(num_pages);
+
+	rwp = calloc(1, sizeof(*rwp));
+	if (!rwp)
+		return NULL;
+
+	if (MUL_OVERFLOW(num_pages, SMALL_PAGE_SIZE, &size))
+		goto err;
+	mm = tee_mm_alloc(&tee_mm_sec_ddr, size);
+	if (!mm)
+		goto err;
+	rwp->idx = (tee_mm_get_smem(mm) - tee_mm_sec_ddr.lo) / SMALL_PAGE_SIZE;
+
+	memset(idx_to_state_padded(rwp->idx), 0,
+	       num_pages * sizeof(struct rwp_state_padded));
+
+	fobj_init(&rwp->fobj, &ops_rwp_paged_iv, num_pages);
+
+	return &rwp->fobj;
+err:
+	tee_mm_free(mm);
+	free(rwp);
+
+	return NULL;
+}
+
+static struct fobj_rwp_paged_iv *to_rwp_paged_iv(struct fobj *fobj)
+{
+	assert(fobj->ops == &ops_rwp_paged_iv);
+
+	return container_of(fobj, struct fobj_rwp_paged_iv, fobj);
+}
+
+static TEE_Result rwp_paged_iv_load_page(struct fobj *fobj,
+					 unsigned int page_idx, void *va)
+{
+	struct fobj_rwp_paged_iv *rwp = to_rwp_paged_iv(fobj);
+	uint8_t *src = idx_to_store(rwp->idx) + page_idx * SMALL_PAGE_SIZE;
+	struct rwp_state_padded *st = idx_to_state_padded(rwp->idx + page_idx);
+
+	assert(refcount_val(&fobj->refc));
+	assert(page_idx < fobj->num_pages);
+
+	return rwp_load_page(va, &st->state, src);
+}
+DECLARE_KEEP_PAGER(rwp_paged_iv_load_page);
+
+static TEE_Result rwp_paged_iv_save_page(struct fobj *fobj,
+					 unsigned int page_idx, const void *va)
+{
+	struct fobj_rwp_paged_iv *rwp = to_rwp_paged_iv(fobj);
+	uint8_t *dst = idx_to_store(rwp->idx) + page_idx * SMALL_PAGE_SIZE;
+	struct rwp_state_padded *st = idx_to_state_padded(rwp->idx + page_idx);
+
+	assert(page_idx < fobj->num_pages);
 
 	if (!refcount_val(&fobj->refc)) {
 		/*
@@ -183,32 +208,150 @@ static TEE_Result rwp_save_page(struct fobj *fobj, unsigned int page_idx,
 		return TEE_SUCCESS;
 	}
 
-	assert(page_idx < fobj->num_pages);
-	assert(state->iv + 1 > state->iv);
+	return rwp_save_page(va, &st->state, dst);
+}
+DECLARE_KEEP_PAGER(rwp_paged_iv_save_page);
 
-	state->iv++;
+static void rwp_paged_iv_free(struct fobj *fobj)
+{
+	struct fobj_rwp_paged_iv *rwp = to_rwp_paged_iv(fobj);
+	paddr_t pa = rwp->idx * SMALL_PAGE_SIZE + tee_mm_sec_ddr.lo;
+	tee_mm_entry_t *mm = tee_mm_find(&tee_mm_sec_ddr, pa);
+
+	assert(mm);
+
+	fobj_uninit(fobj);
+	tee_mm_free(mm);
+	free(rwp);
+}
+
+static vaddr_t rwp_paged_iv_get_iv_vaddr(struct fobj *fobj,
+					 unsigned int page_idx)
+{
+	struct fobj_rwp_paged_iv *rwp = to_rwp_paged_iv(fobj);
+	struct rwp_state_padded *st = idx_to_state_padded(rwp->idx + page_idx);
+
+	assert(page_idx < fobj->num_pages);
+	return (vaddr_t)&st->state & ~SMALL_PAGE_MASK;
+}
+DECLARE_KEEP_PAGER(rwp_paged_iv_get_iv_vaddr);
+
+static const struct fobj_ops ops_rwp_paged_iv __rodata_unpaged = {
+	.free = rwp_paged_iv_free,
+	.load_page = rwp_paged_iv_load_page,
+	.save_page = rwp_paged_iv_save_page,
+	.get_iv_vaddr = rwp_paged_iv_get_iv_vaddr,
+};
+
+static struct fobj_rwp_unpaged_iv *to_rwp_unpaged_iv(struct fobj *fobj)
+{
+	assert(fobj->ops == &ops_rwp_unpaged_iv);
+
+	return container_of(fobj, struct fobj_rwp_unpaged_iv, fobj);
+}
+
+static TEE_Result rwp_unpaged_iv_load_page(struct fobj *fobj,
+					   unsigned int page_idx, void *va)
+{
+	struct fobj_rwp_unpaged_iv *rwp = to_rwp_unpaged_iv(fobj);
+	uint8_t *src = rwp->store + page_idx * SMALL_PAGE_SIZE;
+
+	assert(refcount_val(&fobj->refc));
+	assert(page_idx < fobj->num_pages);
+
+	return rwp_load_page(va, rwp->state + page_idx, src);
+}
+DECLARE_KEEP_PAGER(rwp_unpaged_iv_load_page);
+
+static TEE_Result rwp_unpaged_iv_save_page(struct fobj *fobj,
+					   unsigned int page_idx,
+					   const void *va)
+{
+	struct fobj_rwp_unpaged_iv *rwp = to_rwp_unpaged_iv(fobj);
+	uint8_t *dst = rwp->store + page_idx * SMALL_PAGE_SIZE;
+
+	assert(page_idx < fobj->num_pages);
+
+	if (!refcount_val(&fobj->refc)) {
+		/*
+		 * This fobj is being teared down, it just hasn't had the time
+		 * to call tee_pager_invalidate_fobj() yet.
+		 */
+		assert(TAILQ_EMPTY(&fobj->areas));
+		return TEE_SUCCESS;
+	}
+
+	return rwp_save_page(va, rwp->state + page_idx, dst);
+}
+DECLARE_KEEP_PAGER(rwp_unpaged_iv_save_page);
+
+static void rwp_unpaged_iv_free(struct fobj *fobj __unused)
+{
+	panic();
+}
+
+static const struct fobj_ops ops_rwp_unpaged_iv __rodata_unpaged = {
+	.free = rwp_unpaged_iv_free,
+	.load_page = rwp_unpaged_iv_load_page,
+	.save_page = rwp_unpaged_iv_save_page,
+};
+
+static TEE_Result rwp_init(void)
+{
+	uint8_t key[RWP_AE_KEY_BITS / 8] = { 0 };
+	struct fobj_rwp_unpaged_iv *rwp = NULL;
+	tee_mm_entry_t *mm = NULL;
+	size_t num_pool_pages = 0;
+	size_t num_fobj_pages = 0;
+	size_t sz = 0;
+
+	if (crypto_rng_read(key, sizeof(key)) != TEE_SUCCESS)
+		panic("failed to generate random");
+	if (crypto_aes_expand_enc_key(key, sizeof(key), rwp_ae_key.data,
+				      sizeof(rwp_ae_key.data),
+				      &rwp_ae_key.rounds))
+		panic("failed to expand key");
+
+	assert(tee_mm_sec_ddr.hi > tee_mm_sec_ddr.lo);
+	sz = tee_mm_sec_ddr.hi - tee_mm_sec_ddr.lo;
+	assert(!(sz & SMALL_PAGE_SIZE));
+
+	num_pool_pages = sz / SMALL_PAGE_SIZE;
+	num_fobj_pages = ROUNDUP(num_pool_pages * sizeof(*rwp_state_base),
+				 SMALL_PAGE_SIZE) / SMALL_PAGE_SIZE;
+
 	/*
-	 * IV is constructed as recommended in section "8.2.1 Deterministic
-	 * Construction" of "Recommendation for Block Cipher Modes of
-	 * Operation: Galois/Counter Mode (GCM) and GMAC",
-	 * http://csrc.nist.gov/publications/nistpubs/800-38D/SP-800-38D.pdf
+	 * Each page in the pool needs a struct rwp_state.
+	 *
+	 * This isn't entirely true, the pages not used by
+	 * fobj_rw_paged_alloc() don't need any. A future optimization
+	 * may try to avoid allocating for such pages.
 	 */
 
-	iv.iv[0] = (vaddr_t)state;
-	iv.iv[1] = state->iv >> 32;
-	iv.iv[2] = state->iv;
+	rwp = calloc(1, sizeof(*rwp));
+	if (!rwp)
+		panic();
 
-	return internal_aes_gcm_enc(&rwp_ae_key, &iv, sizeof(iv),
-				    NULL, 0, va, SMALL_PAGE_SIZE, dst,
-				    state->tag, &tag_len);
+	rwp->state = calloc(num_fobj_pages, sizeof(*rwp->state));
+	if (!rwp->state)
+		panic();
+	mm = tee_mm_alloc(&tee_mm_sec_ddr, num_fobj_pages * SMALL_PAGE_SIZE);
+	if (!mm)
+		panic();
+	rwp->store = phys_to_virt(tee_mm_get_smem(mm), MEM_AREA_TA_RAM);
+	assert(rwp->store);
+
+	fobj_init(&rwp->fobj, &ops_rwp_unpaged_iv, num_fobj_pages);
+
+	rwp_state_base = (void *)tee_pager_init_iv_area(&rwp->fobj);
+	assert(rwp_state_base);
+
+	rwp_store_base = phys_to_virt(tee_mm_sec_ddr.lo, MEM_AREA_TA_RAM);
+	assert(rwp_store_base);
+
+	return TEE_SUCCESS;
 }
-DECLARE_KEEP_PAGER(rwp_save_page);
-
-static const struct fobj_ops ops_rw_paged __rodata_unpaged = {
-	.free = rwp_free,
-	.load_page = rwp_load_page,
-	.save_page = rwp_save_page,
-};
+driver_init_late(rwp_init);
 
 struct fobj_rop {
 	uint8_t *hashes;
