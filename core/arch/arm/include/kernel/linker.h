OP-TEE/optee_os

commit 170e9084a84fe215c806b1df3ae09703f79aa0ac

core: add support for CFG_CORE_ASLR
Adds support for CFG_CORE_ASLR to load TEE Core at a random address.
ASLR makes the exploitation of memory corruption vulnerabilities more
difficult.

Paging is currently not supported with CFG_CORE_ASLR=y.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/core_mmu.c
@@ -51,8 +51,11 @@ unsigned long default_nsec_shm_size __nex_bss;
 unsigned long default_nsec_shm_paddr __nex_bss;
 #endif
 
-static struct tee_mmap_region
-	static_memory_map[CFG_MMAP_REGIONS + 1] __nex_bss;
+static struct tee_mmap_region static_memory_map[CFG_MMAP_REGIONS
+#ifdef CFG_CORE_ASLR
+						+ 1
+#endif
+						+ 1] __nex_bss;
 
 /* Define the platform's memory layout. */
 struct memaccess_area {
@@ -1018,8 +1021,63 @@ static int cmp_init_mem_map(const void *a, const void *b)
 	return rc;
 }
 
-static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
+static unsigned int get_va_width(void)
 {
+#ifdef ARM64
+	return 64 - __builtin_ctzl(CFG_LPAE_ADDR_SPACE_SIZE);
+#else
+	return 32;
+#endif
+}
+
+static bool mem_map_add_id_map(struct tee_mmap_region *memory_map,
+			       size_t num_elems, size_t *last,
+			       vaddr_t id_map_start, vaddr_t id_map_end)
+{
+	struct tee_mmap_region *map = NULL;
+	vaddr_t start = ROUNDDOWN(id_map_start, SMALL_PAGE_SIZE);
+	vaddr_t end = ROUNDUP(id_map_end, SMALL_PAGE_SIZE);
+	size_t len = end - start;
+
+	if (*last >= num_elems - 1) {
+		EMSG("Out of entries (%zu) in memory map", num_elems);
+		panic();
+	}
+
+	for (map = memory_map; !core_mmap_is_end_of_table(map); map++)
+		if (core_is_buffer_intersect(map->va, map->size, start, len))
+			return false;
+
+	*map = (struct tee_mmap_region){
+		.type = MEM_AREA_IDENTITY_MAP_RX,
+		/*
+		 * Could use CORE_MMU_PGDIR_SIZE to potentially save a
+		 * translation table, at the increased risk of clashes with
+		 * the rest of the memory map.
+		 */
+		.region_size = SMALL_PAGE_SIZE,
+		.pa = start,
+		.va = start,
+		.size = len,
+		.attr = core_mmu_type_to_attr(MEM_AREA_IDENTITY_MAP_RX),
+	};
+
+	(*last)++;
+
+	return true;
+}
+
+static unsigned long init_mem_map(struct tee_mmap_region *memory_map,
+				  size_t num_elems, unsigned long seed)
+{
+	/*
+	 * @id_map_start and @id_map_end describes a physical memory range
+	 * that must be mapped Read-Only eXecutable at identical virtual
+	 * addresses.
+	 */
+	vaddr_t id_map_start = (vaddr_t)__identity_map_init_start;
+	vaddr_t id_map_end = (vaddr_t)__identity_map_init_end;
+	unsigned long offs = 0;
 	size_t last = 0;
 
 	last = collect_mem_ranges(memory_map, num_elems);
@@ -1033,13 +1091,42 @@ static void init_mem_map(struct tee_mmap_region *memory_map, size_t num_elems)
 	      cmp_init_mem_map);
 
 	add_pager_vaspace(memory_map, num_elems, &last);
+	if (IS_ENABLED(CFG_CORE_ASLR) && seed) {
+		vaddr_t base_addr = TEE_RAM_START + seed;
+		const unsigned int va_width = get_va_width();
+		const vaddr_t va_mask = GENMASK_64(va_width - 1,
+						   SMALL_PAGE_SHIFT);
+		vaddr_t ba = base_addr;
+		size_t n = 0;
+
+		for (n = 0; n < 3; n++) {
+			if (n)
+				ba = base_addr ^ BIT64(va_width - n);
+			ba &= va_mask;
+			if (assign_mem_va(ba, memory_map) &&
+			    mem_map_add_id_map(memory_map, num_elems, &last,
+					       id_map_start, id_map_end)) {
+				offs = ba - TEE_RAM_START;
+				DMSG("Mapping core at %#"PRIxVA" offs %#lx",
+				     ba, offs);
+				goto out;
+			} else {
+				DMSG("Failed to map core at %#"PRIxVA, ba);
+			}
+		}
+		EMSG("Failed to map core with seed %#lx", seed);
+	}
+
 	if (!assign_mem_va(TEE_RAM_START, memory_map))
 		panic();
 
+out:
 	qsort(memory_map, last, sizeof(struct tee_mmap_region),
 	      cmp_mmap_by_lower_va);
 
 	dump_mmap_table(memory_map);
+
+	return offs;
 }
 
 static void check_mem_map(struct tee_mmap_region *map)
@@ -1082,24 +1169,60 @@ static void check_mem_map(struct tee_mmap_region *map)
 	}
 }
 
+static struct tee_mmap_region *get_tmp_mmap(void)
+{
+	struct tee_mmap_region *tmp_mmap = (void *)__heap1_start;
+
+#ifdef CFG_WITH_PAGER
+	if (__heap1_end - __heap1_start < (ptrdiff_t)sizeof(static_memory_map))
+		tmp_mmap = (void *)__heap2_start;
+#endif
+
+	memset(tmp_mmap, 0, sizeof(static_memory_map));
+
+	return tmp_mmap;
+}
+
 /*
- * core_init_mmu_map - init tee core default memory mapping
+ * core_init_mmu_map() - init tee core default memory mapping
  *
- * This routine sets the static default TEE core mapping.
+ * This routine sets the static default TEE core mapping. If @seed is > 0
+ * and configured with CFG_CORE_ASLR it will map tee core at a location
+ * based on the seed and return the offset from the link address.
  *
  * If an error happened: core_init_mmu_map is expected to panic.
  */
-void core_init_mmu_map(struct core_mmu_config *cfg)
+void core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg)
 {
+	vaddr_t start = ROUNDDOWN((vaddr_t)__nozi_start, SMALL_PAGE_SIZE);
+	vaddr_t len = ROUNDUP((vaddr_t)__nozi_end, SMALL_PAGE_SIZE) - start;
+	struct tee_mmap_region *tmp_mmap = get_tmp_mmap();
+	unsigned long offs = 0;
+
 	check_sec_nsec_mem_config();
 
-	COMPILE_TIME_ASSERT(CFG_MMAP_REGIONS >= 13);
-	init_mem_map(static_memory_map, ARRAY_SIZE(static_memory_map));
+	/*
+	 * Add a entry covering the translation tables which will be
+	 * involved in some virt_to_phys() and phys_to_virt() conversions.
+	 */
+	static_memory_map[0] = (struct tee_mmap_region){
+		.type = MEM_AREA_TEE_RAM,
+		.region_size = SMALL_PAGE_SIZE,
+		.pa = start,
+		.va = start,
+		.size = len,
+		.attr = core_mmu_type_to_attr(MEM_AREA_IDENTITY_MAP_RX),
+	};
 
-	check_mem_map(static_memory_map);
-	core_init_mmu(static_memory_map);
+	COMPILE_TIME_ASSERT(CFG_MMAP_REGIONS >= 13);
+	offs = init_mem_map(tmp_mmap, ARRAY_SIZE(static_memory_map), seed);
+
+	check_mem_map(tmp_mmap);
+	core_init_mmu(tmp_mmap);
 	dump_xlat_table(0x0, 1);
 	core_init_mmu_regs(cfg);
+	cfg->load_offset = offs;
+	memcpy(static_memory_map, tmp_mmap, sizeof(static_memory_map));
 }
 
 bool core_mmu_mattr_is_ok(uint32_t mattr)
@@ -1859,7 +1982,7 @@ static void check_pa_matches_va(void *va, paddr_t pa)
 	}
 #ifdef CFG_WITH_PAGER
 	if (is_unpaged(va)) {
-		if (v != pa)
+		if (v - boot_mmu_config.load_offset != pa)
 			panic("issue in linear address space");
 		return;
 	}
@@ -1958,7 +2081,7 @@ static void *phys_to_virt_ta_vaspace(paddr_t pa)
 static void *phys_to_virt_tee_ram(paddr_t pa)
 {
 	if (pa >= TEE_LOAD_ADDR && pa < get_linear_map_end())
-		return (void *)(vaddr_t)pa;
+		return (void *)(vaddr_t)(pa + boot_mmu_config.load_offset);
 	return tee_pager_phys_to_virt(pa);
 }
 #else

core/arch/arm/arm.mk
@@ -136,6 +136,10 @@ core-platform-cflags += $(platform-cflags-debug-info)
 core-platform-aflags += $(platform-aflags-generic)
 core-platform-aflags += $(platform-aflags-debug-info)
 
+ifeq ($(CFG_CORE_ASLR),y)
+core-platform-cflags += -fpie
+endif
+
 ifeq ($(CFG_ARM64_core),y)
 arch-bits-core := 64
 core-platform-cppflags += $(arm64-platform-cppflags)

core/arch/arm/include/kernel/generic_boot.h
@@ -9,6 +9,7 @@
 #include <types_ext.h>
 
 extern uint8_t embedded_secure_dtb[];
+extern const struct core_mmu_config boot_mmu_config;
 
 #if defined(CFG_WITH_ARM_TRUSTED_FW)
 unsigned long cpu_on_handler(unsigned long a0, unsigned long a1);

core/arch/arm/include/mm/core_mmu.h
@@ -279,21 +279,24 @@ struct core_mmu_config {
 	uint64_t mair_el1;
 	uint64_t ttbr0_el1_base;
 	uint64_t ttbr0_core_offset;
+	uint64_t load_offset;
 #elif defined(CFG_WITH_LPAE)
 	uint32_t ttbcr;
 	uint32_t mair0;
 	uint32_t ttbr0_base;
 	uint32_t ttbr0_core_offset;
+	uint32_t load_offset;
 #else
 	uint32_t prrr;
 	uint32_t nmrr;
 	uint32_t dacr;
 	uint32_t ttbcr;
 	uint32_t ttbr;
+	uint32_t load_offset;
 #endif
 };
 
-void core_init_mmu_map(struct core_mmu_config *cfg);
+void core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg);
 void core_init_mmu_regs(struct core_mmu_config *cfg);
 
 bool core_mmu_place_tee_ram_at_top(paddr_t paddr);

core/arch/arm/kernel/asm-defines.c
@@ -94,4 +94,6 @@ DEFINES
 
 	/* struct core_mmu_config */
 	DEFINE(CORE_MMU_CONFIG_SIZE, sizeof(struct core_mmu_config));
+	DEFINE(CORE_MMU_CONFIG_LOAD_OFFSET,
+	       offsetof(struct core_mmu_config, load_offset));
 }

core/arch/arm/kernel/generic_boot.c
@@ -1095,6 +1095,10 @@ static void init_primary_helper(unsigned long pageable_part,
 	configure_console_from_dt();
 
 	IMSG("OP-TEE version: %s", core_v_str);
+#ifdef CFG_CORE_ASLR
+	DMSG("Executing at offset %#lx with virtual load address %#"PRIxVA,
+	     (unsigned long)boot_mmu_config.load_offset, VCORE_START_VA);
+#endif
 
 	main_init_gic();
 	init_vfp_nsec();

core/arch/arm/kernel/generic_entry_a32.S
@@ -6,6 +6,7 @@
 #include <arm32_macros.S>
 #include <arm.h>
 #include <asm.S>
+#include <elf_common.h>
 #include <generated/asm-defines.h>
 #include <keep.h>
 #include <kernel/asan.h>
@@ -440,13 +441,37 @@ shadow_stack_access_ok:
 	bl	arm_cl2_enable
 #endif
 
-	ldr	r0, =boot_mmu_config
+#ifdef CFG_CORE_ASLR
+	mov	r0, r6
+	bl	get_aslr_seed
+#else
+	mov	r0, #0
+#endif
+
+	ldr	r1, =boot_mmu_config
 	bl	core_init_mmu_map
+
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Process relocation information for updating with the new offset.
+	 * We're doing this now before MMU is enabled as some of the memory
+	 * will become write protected.
+	 */
+	ldr	r0, =boot_mmu_config
+	ldr	r0, [r0, #CORE_MMU_CONFIG_LOAD_OFFSET]
+	bl	relocate
+#endif
 
 	bl	__get_core_pos
 	bl	enable_mmu
-	/* Virtual addresses may have changed, reinitialize console */
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Reinitialize console, since register_serial_console() has
+	 * previously registered a PA and with ASLR the VA is different
+	 * from the PA.
+	 */
 	bl	console_init
+#endif
 
 	mov	r0, r4		/* pageable part address */
 	mov	r1, r5		/* ns-entry address */
@@ -489,8 +514,10 @@ shadow_stack_access_ok:
 	bl 	thread_clr_boot_thread
 
 #if defined(CFG_WITH_ARM_TRUSTED_FW)
+	ldr	r0, =boot_mmu_config
+	ldr	r0, [r0, #CORE_MMU_CONFIG_LOAD_OFFSET]
 	/* Pass the vector address returned from main_init */
-	mov	r1, r4
+	sub	r1, r4, r0
 #else
 	/* realy standard bootarg #1 and #2 to non secure entry */
 	mov	r4, #0
@@ -512,10 +539,48 @@ UNWIND(	.fnstart)
 UNWIND(	.fnend)
 END_FUNC unhandled_cpu
 
+#ifdef CFG_CORE_ASLR
+LOCAL_FUNC relocate , :
+	push	{r4-r5}
+	/* r0 holds load offset */
+	ldr	r1, =__rel_start
+	ldr	r2, =__rel_end
+
+	b	2f
+	/* Loop over the relocations (Elf32_Rel) and process all entries */
+1:	ldmia	r1!, {r4-r5}		/* r4 = r_offset, r5 = r_info */
+	/* We're only expecting R_ARM_RELATIVE relocations. */
+	cmp	r5, #R_ARM_RELATIVE
+	bne	.
+
+	/*
+	 * Update the pointer at r_offset + load_offset.
+	 *
+	 * Note that we're skipping the load_offset for the address to
+	 * update, this is because that memory isn't mapped at that address
+	 * yet.
+	 */
+	ldr	r12, [r4]
+	add	r12, r12, r0
+	str	r12, [r4]
+
+2:	cmp	r1, r2
+	bne	1b
+
+	pop	{r4-r5}
+	bx	lr
+END_FUNC relocate
+#endif
+
 /*
  * void enable_mmu(unsigned long core_pos);
+ *
+ * This function depends on being mapped with in the identity map where
+ * physical address and virtual address is the same. After MMU has been
+ * enabled the instruction pointer will be updated to execute as the new
+ * offset instead. Stack pointers and the return address are updated.
  */
-LOCAL_FUNC enable_mmu , :
+LOCAL_FUNC enable_mmu , : , .identity_map
 	/* r0 = core pos */
 	adr	r1, boot_mmu_config
 
@@ -537,10 +602,10 @@ LOCAL_FUNC enable_mmu , :
 	/*
 	 * ttbr0_el1 = ttbr0_base + ttbr0_core_offset * core_pos
 	 */
-	mla	r1, r0, r3, r2
-	write_ttbr0 r1
-	mov	r1, #0
-	write_ttbr1 r1
+	mla	r12, r0, r3, r2
+	write_ttbr0 r12
+	mov	r12, #0
+	write_ttbr1 r12
 #else
 	ldm	r1!, {r2, r3}
 	/*
@@ -563,9 +628,11 @@ LOCAL_FUNC enable_mmu , :
 	write_ttbr0 r2
 	write_ttbr1 r2
 
-	mov	r1, #0
-	write_contextidr r1
+	mov	r2, #0
+	write_contextidr r2
 #endif
+	ldm	r1!, {r2}
+	/* r2 = load_offset (always 0 if CFG_CORE_ASLR=n) */
 	isb
 
 	/* Invalidate TLB */
@@ -585,6 +652,12 @@ LOCAL_FUNC enable_mmu , :
 	orr	r0, r0, #(SCTLR_AFE | SCTLR_TRE)
 #endif
 	write_sctlr r0
+	isb
+
+	/* Update vbar */
+	read_vbar r1
+	add	r1, r1, r2
+	write_vbar r1
 	isb
 
 	/* Invalidate instruction cache and branch predictor */
@@ -608,6 +681,10 @@ LOCAL_FUNC enable_mmu , :
 	write_sctlr r0
 	isb
 
+	/* Adjust stack pointer and return address */
+	add	sp, sp, r2
+	add	lr, lr, r2
+
 	bx	lr
 END_FUNC enable_mmu
 
@@ -630,10 +707,11 @@ UNWIND(	.cantunwind)
 	write_vbar r0
 
 	mov	r4, lr
-	set_sp
 
 	bl	__get_core_pos
 	bl	enable_mmu
+
+	set_sp
 
 	mov	r0, r4
 	mov	r1, r5

core/arch/arm/kernel/generic_entry_a64.S
@@ -8,6 +8,7 @@
 #include <arm64_macros.S>
 #include <arm.h>
 #include <asm.S>
+#include <elf_common.h>
 #include <generated/asm-defines.h>
 #include <keep.h>
 #include <sm/optee_smc.h>
@@ -118,6 +119,21 @@ clear_nex_bss:
 	b.lt	clear_nex_bss
 #endif
 
+#ifdef CFG_CORE_ASLR
+	/*
+	 * The code is supposed to be linked against the address where
+	 * binary was loaded (0 load offset). However, since AArch64 uses
+	 * relocations of the new Rela type, relocations has to be
+	 * processed to guarantee that all updated locations holds the
+	 * correct value.
+	 *
+	 * N.b, it seems that some toolchains already provides expected
+	 * values at the target locations.
+	 */
+	mov	x0, #0
+	bl	relocate
+#endif
+
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
 	set_sp
 
@@ -138,13 +154,36 @@ clear_nex_bss:
 	/* Enable Console */
 	bl	console_init
 
-	adr	x0, boot_mmu_config
+#ifdef CFG_CORE_ASLR
+	mov	x0, x20
+	bl	get_aslr_seed
+#else
+	mov	x0, #0
+#endif
+
+	adr	x1, boot_mmu_config
 	bl	core_init_mmu_map
+
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Process relocation information again updating for the new
+	 * offset. We're doing this now before MMU is enabled as some of
+	 * the memory will become write protected.
+	 */
+	ldr	x0, boot_mmu_config + CORE_MMU_CONFIG_LOAD_OFFSET
+	bl	relocate
+#endif
 
 	bl	__get_core_pos
 	bl	enable_mmu
-	/* Virtual addresses may have changed, reinitialize console */
+#ifdef CFG_CORE_ASLR
+	/*
+	 * Reinitialize console, since register_serial_console() has
+	 * previously registered a PA and with ASLR the VA is different
+	 * from the PA.
+	 */
 	bl	console_init
+#endif
 
 	mov	x0, x19		/* pagable part address */
 	mov	x1, #-1
@@ -178,26 +217,72 @@ clear_nex_bss:
 	bl 	thread_clr_boot_thread
 #endif
 
-	/* Pass the vector address returned from main_init */
-	mov	x1, x19
+	/*
+	 * Pass the vector address returned from main_init
+	 * Compensate for the load offset since cpu_on_handler() is
+	 * called with MMU off.
+	 */
+	ldr	x0, boot_mmu_config + CORE_MMU_CONFIG_LOAD_OFFSET
+	sub	x1, x19, x0
 	mov	x0, #TEESMC_OPTEED_RETURN_ENTRY_DONE
 	smc	#0
 	b	.	/* SMC should not return */
 END_FUNC _start
 KEEP_INIT _start
 
+#ifdef CFG_CORE_ASLR
+LOCAL_FUNC relocate , :
+	/* x0 holds load offset */
+	ldr	x1, =__rela_start
+	ldr	x2, =__rela_end
+
+	b	2f
+	/* Loop over the relocations (Elf64_Rela) and process all entries */
+1:	ldp	x3, x4, [x1], #16	/* x3 = r_offset, x4 = r_info */
+	ldr	x5, [x1], #8		/* x5 = r_addend */
+	/*
+	 * We're only expecting R_AARCH64_NONE and R_AARCH64_RELATIVE
+	 * relocations.
+	 */
+	cmp	w4, #R_AARCH64_NONE
+	b.eq	2f			/* Skip R_AARCH64_NONE */
+	cmp	w4, #R_AARCH64_RELATIVE
+	b.ne	.
+	/*
+	 * Update the pointer at r_offset + load_offset with r_addend +
+	 * load_offset.
+	 */
+	add	x5, x5, x0
+	/*
+	 * Note that we're skipping the load_offset for the address to
+	 * update, this is because that memory isn't mapped at that address
+	 * yet.
+	 */
+	str	x5, [x3]
+2:	cmp	x1, x2
+	b.ne	1b
+	ret
+END_FUNC relocate
+#endif
+
 /*
  * void enable_mmu(unsigned long core_pos);
+ *
+ * This function depends on being mapped with in the identity map where
+ * physical address and virtual address is the same. After MMU has been
+ * enabled the instruction pointer will be updated to execute as the new
+ * offset instead. Stack pointers and the return address are updated.
  */
-LOCAL_FUNC enable_mmu , :
+LOCAL_FUNC enable_mmu , : , .identity_map
 	adr	x1, boot_mmu_config
-	load_xregs x1, 0, 2, 5
+	load_xregs x1, 0, 2, 6
 	/*
 	 * x0 = core_pos
 	 * x2 = tcr_el1
 	 * x3 = mair_el1
 	 * x4 = ttbr0_el1_base
 	 * x5 = ttbr0_core_offset
+	 * x6 = load_offset
 	 */
 	msr	tcr_el1, x2
 	msr	mair_el1, x3
@@ -226,6 +311,12 @@ LOCAL_FUNC enable_mmu , :
 	msr	sctlr_el1, x1
 	isb
 
+	/* Update vbar */
+	mrs	x1, vbar_el1
+	add	x1, x1, x6
+	msr	vbar_el1, x1
+	isb
+
 	/* Invalidate instruction cache and branch predictor */
 	ic	iallu
 	isb
@@ -236,6 +327,13 @@ LOCAL_FUNC enable_mmu , :
 	orr	x1, x1, #SCTLR_C
 	msr	sctlr_el1, x1
 	isb
+
+	/* Adjust stack pointers and return address */
+	msr	spsel, #1
+	add	sp, sp, x6
+	msr	spsel, #0
+	add	sp, sp, x6
+	add	x30, x30, x6
 
 	ret
 END_FUNC enable_mmu
@@ -256,14 +354,14 @@ FUNC cpu_on_handler , :
 	set_sctlr_el1
 	isb
 
-	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
-	set_sp
-
 	/* Enable aborts now that we can receive exceptions */
 	msr	daifclr, #DAIFBIT_ABT
 
 	bl	__get_core_pos
 	bl	enable_mmu
+
+	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
+	set_sp
 
 	mov	x0, x19
 	mov	x1, x20
@@ -289,9 +387,9 @@ END_FUNC unhandled_cpu
 	  .endif
 	.endm
 
-	.section .text.reset_vect_table
+	.section .identity_map
 	.align	11
-LOCAL_FUNC reset_vect_table , :
+LOCAL_FUNC reset_vect_table , :, .identity_map
 	/* -----------------------------------------------------
 	 * Current EL with SP0 : 0x0 - 0x180
 	 * -----------------------------------------------------

commit 95da3dfad2c39661779b51be1d5146949cc7a540

link: make section size definitions relocation-proof
Value of define VCORE_UNPG_RW_SZ is determined by linker script and
provided to C code as a symbol value (__vcore_unpg_rw_size). This is a
standard way of sharing linker variables with C code, which is described in
ld manual.

Problem is that linker sometimes makes those symbols relocatable and ASLR
code then moves them to random places with rest of the OP-TEE image.

For example, on build for RCAR platform I am getting those entries in
relocation section:

[...]
000000004415b120 R_AARCH64_RELATIVE  *ABS*+0x0000000044100180
000000004415af60 R_AARCH64_RELATIVE  *ABS*+0x000000004415fc48
000000004415afb0 R_AARCH64_RELATIVE  *ABS*+0x00000000000a4000 <======
000000004415aef8 R_AARCH64_RELATIVE  *ABS*+0x000000004415c000
[...]

From programmer's point of view this looks like "constant" VCORE_UNPG_RW_SZ
has random value every boot.

Obvious approach is to provide section end address and then calculate size
on C side:

 #define VCORE_UNPG_RW_SZ ((size_t)(__vcore_unpg_rx_end -
				    __vcore_unpg_rx_start))

But with this approach compiler can't initialize constant values in
definitions like

 register_phys_mem_ul(MEM_AREA_TEE_RAM_RW, VCORE_UNPG_RW_PA,
					   VCORE_UNPG_RW_SZ);

from core_mmu.c.

Basically, this leads to following constraints:

1. If we calculate section size in linker script, then compiler can use
   it as a constant expression, but this value may be mangled by ASLR
   at run-time.

2. We can't calculate section size in C code, because this value can't be
   used as a constant expression.

This patch provides a workaround around this issue by providing two sets of
definitions: old _SZ definition is renamed to _SZ_UNSAFE and it should be
used only in places where a constant expression is required and provided it
is referenced only before dynamic relocations have been applied, while the
new _SZ definition can be used in all other situations.

Value of _new SZ is obtained by deducting section start address from end
address. Additional linker symbols are introduced to provide section end
addresses.

Fixes: 170e9084a84f ("core: add support for CFG_CORE_ASLR")

core/arch/arm/include/kernel/linker.h
@@ -22,33 +22,56 @@ extern const uint8_t __exidx_end[];
 extern const uint8_t __extab_start[];
 extern const uint8_t __extab_end[];
 
+/*
+ * Warning! (ASLR) Do not use any of the _SZ_UNSAFE defines or _size[] variables
+ * below in code that runs after dynamic relocations have been applied. Their
+ * value could be *wrong* after relocation (for instance Aarch64 GCC 9.2
+ * generates dynamic relocations for the _size[] symbols, but Clang 11.0.0 does
+ * not). The _SZ_UNSAFE macros are mostly here for use in register_phys_mem()
+ * where the _SZ macros would be rejected by the compiler ('initializer element
+ * is not constant'). Other uses are likely to be incorrect.
+ */
+
 #define VCORE_UNPG_RX_PA	((unsigned long)__vcore_unpg_rx_start)
-#define VCORE_UNPG_RX_SZ	((size_t)__vcore_unpg_rx_size)
+#define VCORE_UNPG_RX_SZ_UNSAFE	((size_t)__vcore_unpg_rx_size)
+#define VCORE_UNPG_RX_SZ	((size_t)(__vcore_unpg_rx_end - \
+					  __vcore_unpg_rx_start))
 #define VCORE_UNPG_RO_PA	((unsigned long)__vcore_unpg_ro_start)
-#define VCORE_UNPG_RO_SZ	((size_t)__vcore_unpg_ro_size)
+#define VCORE_UNPG_RO_SZ_UNSAFE	((size_t)__vcore_unpg_ro_size)
+#define VCORE_UNPG_RO_SZ	((size_t)(__vcore_unpg_ro_end - \
+					  __vcore_unpg_ro_start))
 #define VCORE_UNPG_RW_PA	((unsigned long)__vcore_unpg_rw_start)
-#define VCORE_UNPG_RW_SZ	((size_t)__vcore_unpg_rw_size)
+#define VCORE_UNPG_RW_SZ_UNSAFE	((size_t)__vcore_unpg_rw_size)
+#define VCORE_UNPG_RW_SZ	((size_t)(__vcore_unpg_rw_end - \
+					  __vcore_unpg_rw_start))
 #define VCORE_NEX_RW_PA	((unsigned long)__vcore_nex_rw_start)
-#define VCORE_NEX_RW_SZ	((size_t)__vcore_nex_rw_size)
+#define VCORE_NEX_RW_SZ_UNSAFE	((size_t)__vcore_nex_rw_size)
+#define VCORE_NEX_RW_SZ	((size_t)__vcore_nex_rw_end - \
+					 __vcore_nex_rw_start)
 #define VCORE_INIT_RX_PA	((unsigned long)__vcore_init_rx_start)
-#define VCORE_INIT_RX_SZ	((size_t)__vcore_init_rx_size)
+#define VCORE_INIT_RX_SZ_UNSAFE	((size_t)__vcore_init_rx_size)
+#define VCORE_INIT_RX_SZ	((size_t)(__vcore_init_rx_end - \
+					  __vcore_init_rx_start))
 #define VCORE_INIT_RO_PA	((unsigned long)__vcore_init_ro_start)
-#define VCORE_INIT_RO_SZ	((size_t)__vcore_init_ro_size)
+#define VCORE_INIT_RO_SZ_UNSAFE	((size_t)__vcore_init_ro_size)
+#define VCORE_INIT_RO_SZ	((size_t)(__vcore_init_ro_end - \
+					  __vcore_init_ro_start))
 
 #define VCORE_START_VA		((vaddr_t)__text_start)
 
-extern const uint8_t __vcore_unpg_rx_start[];
-extern const uint8_t __vcore_unpg_rx_size[];
-extern const uint8_t __vcore_unpg_ro_start[];
-extern const uint8_t __vcore_unpg_ro_size[];
-extern const uint8_t __vcore_unpg_rw_start[];
-extern const uint8_t __vcore_unpg_rw_size[];
-extern const uint8_t __vcore_nex_rw_start[];
-extern const uint8_t __vcore_nex_rw_size[];
-extern const uint8_t __vcore_init_rx_start[];
-extern const uint8_t __vcore_init_rx_size[];
-extern const uint8_t __vcore_init_ro_start[];
-extern const uint8_t __vcore_init_ro_size[];
+#define EMIT_SECTION_INFO_SYMBOLS(section_name) \
+	extern const uint8_t __vcore_ ## section_name ## _start[]; \
+	extern const uint8_t __vcore_ ## section_name ## _end[]; \
+	extern const uint8_t __vcore_ ## section_name ## _size[]
+
+EMIT_SECTION_INFO_SYMBOLS(unpg_rx);
+EMIT_SECTION_INFO_SYMBOLS(unpg_ro);
+EMIT_SECTION_INFO_SYMBOLS(unpg_rw);
+EMIT_SECTION_INFO_SYMBOLS(nex_rw);
+EMIT_SECTION_INFO_SYMBOLS(init_ro);
+EMIT_SECTION_INFO_SYMBOLS(init_rx);
+
+#undef EMIT_SECTION_INFO_SYMBOLS
 
 extern const uint8_t __text_start[];
 extern const uint8_t __end[];

core/arch/arm/kernel/kern.ld.S
@@ -443,15 +443,19 @@ __vcore_unpg_ro_size = __flatmap_unpg_ro_size;
 __vcore_unpg_rx_size = __flatmap_unpg_rx_size + __flatmap_unpg_ro_size;
 __vcore_unpg_ro_size = 0;
 #endif
+__vcore_unpg_rx_end = __vcore_unpg_rx_start + __vcore_unpg_rx_size;
+__vcore_unpg_ro_end = __vcore_unpg_ro_start + __vcore_unpg_ro_size;
 
 /* Unpaged read-write memory */
 __vcore_unpg_rw_start = __flatmap_unpg_rw_start;
 __vcore_unpg_rw_size = __flatmap_unpg_rw_size;
+__vcore_unpg_rw_end = __vcore_unpg_rw_start + __vcore_unpg_rw_size;
 
 #ifdef CFG_VIRTUALIZATION
 /* Nexus read-write memory */
 __vcore_nex_rw_start = __flatmap_nex_rw_start;
 __vcore_nex_rw_size = __flatmap_nex_rw_size;
+__vcore_nex_rw_end = __vcore_nex_rw_start + __vcore_nex_rw_size;
 #endif
 
 #ifdef CFG_WITH_PAGER
@@ -475,6 +479,8 @@ __vcore_init_rx_size = __flatmap_init_rx_size + __flatmap_init_ro_size +
 		       __FLATMAP_PAGER_TRAILING_SPACE;
 __vcore_init_ro_size = 0;
 #endif /* CFG_CORE_RODATA_NOEXEC */
+__vcore_init_rx_end = __vcore_init_rx_start + __vcore_init_rx_size;
+__vcore_init_ro_end = __vcore_init_ro_start + __vcore_init_ro_size;
 #endif /* CFG_WITH_PAGER */
 
 #ifdef CFG_CORE_SANITIZE_KADDRESS

core/arch/arm/kernel/link_dummy.ld
@@ -73,16 +73,22 @@ __text_start = .;
 __tmp_hashes_end = .;
 __tmp_hashes_size = .;
 __tmp_hashes_start = .;
+__vcore_init_ro_end = .;
 __vcore_init_ro_size = .;
 __vcore_init_ro_start = .;
+__vcore_init_rx_end = .;
 __vcore_init_rx_size = .;
 __vcore_init_rx_start = .;
+__vcore_nex_rw_end = .;
 __vcore_nex_rw_size = .;
 __vcore_nex_rw_start = .;
+__vcore_unpg_ro_end = .;
 __vcore_unpg_ro_size = .;
 __vcore_unpg_ro_start = .;
+__vcore_unpg_rw_end = .;
 __vcore_unpg_rw_size = .;
 __vcore_unpg_rw_start = .;
+__vcore_unpg_rx_end = .;
 __vcore_unpg_rx_size = .;
 __vcore_unpg_rx_start = .;
 PROVIDE(core_v_str = 0);

core/arch/arm/mm/core_mmu.c
@@ -90,19 +90,26 @@ register_sdp_mem(TEE_SDP_TEST_MEM_BASE, TEE_SDP_TEST_MEM_SIZE);
 #ifdef CFG_CORE_RWDATA_NOEXEC
 register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, TEE_RAM_START,
 		     VCORE_UNPG_RX_PA - TEE_RAM_START);
-register_phys_mem_ul(MEM_AREA_TEE_RAM_RX, VCORE_UNPG_RX_PA, VCORE_UNPG_RX_SZ);
-register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, VCORE_UNPG_RO_PA, VCORE_UNPG_RO_SZ);
+register_phys_mem_ul(MEM_AREA_TEE_RAM_RX, VCORE_UNPG_RX_PA,
+		     VCORE_UNPG_RX_SZ_UNSAFE);
+register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, VCORE_UNPG_RO_PA,
+		     VCORE_UNPG_RO_SZ_UNSAFE);
 
 #ifdef CFG_VIRTUALIZATION
-register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, VCORE_UNPG_RW_PA, VCORE_UNPG_RW_SZ);
-register_phys_mem_ul(MEM_AREA_NEX_RAM_RW, VCORE_NEX_RW_PA, VCORE_NEX_RW_SZ);
+register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, VCORE_UNPG_RW_PA,
+		     VCORE_UNPG_RW_SZ_UNSAFE);
+register_phys_mem_ul(MEM_AREA_NEX_RAM_RW, VCORE_NEX_RW_PA,
+		     VCORE_NEX_RW_SZ_UNSAFE);
 #else
-register_phys_mem_ul(MEM_AREA_TEE_RAM_RW, VCORE_UNPG_RW_PA, VCORE_UNPG_RW_SZ);
+register_phys_mem_ul(MEM_AREA_TEE_RAM_RW, VCORE_UNPG_RW_PA,
+		     VCORE_UNPG_RW_SZ_UNSAFE);
 #endif
 
 #ifdef CFG_WITH_PAGER
-register_phys_mem_ul(MEM_AREA_TEE_RAM_RX, VCORE_INIT_RX_PA, VCORE_INIT_RX_SZ);
-register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, VCORE_INIT_RO_PA, VCORE_INIT_RO_SZ);
+register_phys_mem_ul(MEM_AREA_TEE_RAM_RX, VCORE_INIT_RX_PA,
+		     VCORE_INIT_RX_SZ_UNSAFE);
+register_phys_mem_ul(MEM_AREA_TEE_RAM_RO, VCORE_INIT_RO_PA,
+		     VCORE_INIT_RO_SZ_UNSAFE);
 #endif /*CFG_WITH_PAGER*/
 #else /*!CFG_CORE_RWDATA_NOEXEC*/
 register_phys_mem(MEM_AREA_TEE_RAM, TEE_RAM_START, TEE_RAM_PH_SIZE);