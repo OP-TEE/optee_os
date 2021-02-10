b6ca7e5dd226f2c3691d798ab81d1cf35dfec33e
Alexander Zakharov <uglym8@gmail.com>
2021-02-10 16:48:37 +0100

    core: libmbedtls: add 3DES CMAC implementation
    Add 3DES CMAC (NIST SP800-38B).
    
    Signed-off-by: Alexander Zakharov <uglym8@gmail.com>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    [jf: add commit description]
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

--- lib/libmbedtls/core/sub.mk
+++ lib/libmbedtls/core/sub.mk
@@ -19,6 +19,7 @@ endif
 
 srcs-$(CFG_CRYPTO_HMAC) += hmac.c
 srcs-$(CFG_CRYPTO_CMAC) += aes_cmac.c
+srcs-$(CFG_CRYPTO_CMAC) += des3_cmac.c
 
 ifneq ($(CFG_CRYPTO_DSA),y)
 srcs-$(call cfg-one-enabled, CFG_CRYPTO_RSA  CFG_CRYPTO_DH \
