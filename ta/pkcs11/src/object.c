72484d1e84d374831d82c46d57273b08f13fe577
Ruchika Gupta <ruchika.gupta@linaro.org>
2021-02-12 16:04:16 +0530

    ta: pkcs11: Correct the return type of functions and typos
    Correct return types of few functions and few typos.
    
    Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>

--- ta/pkcs11/src/object.c
+++ ta/pkcs11/src/object.c
@@ -348,7 +348,7 @@ enum pkcs11_rc entry_create_object(struct pkcs11_client *client,
 
 	/*
 	 * Now obj_handle (through the related struct pkcs11_object
-	 * instance) owns the serialised buffer that holds the object
+	 * instance) owns the serialized buffer that holds the object
 	 * attributes. We clear reference in head to NULL as the serializer
 	 * object is now referred from obj_handle. This allows smooth pass
 	 * through free at function exit.
@@ -657,8 +657,8 @@ void release_session_find_obj_context(struct pkcs11_session *session)
 	session->find_ctx = NULL;
 }
 
-uint32_t entry_find_objects_final(struct pkcs11_client *client,
-				  uint32_t ptypes, TEE_Param *params)
+enum pkcs11_rc entry_find_objects_final(struct pkcs11_client *client,
+					uint32_t ptypes, TEE_Param *params)
 {
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INOUT,
 						TEE_PARAM_TYPE_NONE,
@@ -689,8 +689,8 @@ uint32_t entry_find_objects_final(struct pkcs11_client *client,
 	return PKCS11_CKR_OK;
 }
 
-uint32_t entry_get_attribute_value(struct pkcs11_client *client,
-				   uint32_t ptypes, TEE_Param *params)
+enum pkcs11_rc entry_get_attribute_value(struct pkcs11_client *client,
+					 uint32_t ptypes, TEE_Param *params)
 {
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INOUT,
 						TEE_PARAM_TYPE_NONE,
@@ -853,8 +853,8 @@ out:
 	return rc;
 }
 
-uint32_t entry_get_object_size(struct pkcs11_client *client,
-			       uint32_t ptypes, TEE_Param *params)
+enum pkcs11_rc entry_get_object_size(struct pkcs11_client *client,
+				     uint32_t ptypes, TEE_Param *params)
 {
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INOUT,
 						TEE_PARAM_TYPE_NONE,
@@ -1156,7 +1156,7 @@ enum pkcs11_rc entry_copy_object(struct pkcs11_client *client, uint32_t ptypes,
 
 	/*
 	 * Now obj_handle (through the related struct pkcs11_object
-	 * instance) owns the serialised buffer that holds the object
+	 * instance) owns the serialized buffer that holds the object
 	 * attributes. We clear reference in head to NULL as the serializer
 	 * object is now referred from obj_handle. This allows smooth pass
 	 * through free at function exit.
