f0cbeefada45be69242577427718188a6707c3e3
Jorge Ramirez-Ortiz <jorge@foundries.io>
2021-02-12 16:37:49 +0100

    drivers: crypto: se050: improve maintainability
    Replace explicit c-flags duplication across makefiles with single
    shared definition.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

---
+++ core/drivers/crypto/se050/cflags.mk
@@ -0,0 +1,5 @@
+cflags-y += -DAX_EMBEDDED=1
+cflags-y += -DVERBOSE_APDU_LOGS=0
+cflags-y += -DT1oI2C_UM11225
+cflags-y += -DT1oI2C
+cflags-y += -DSSS_USE_FTR_FILE
--- core/drivers/crypto/se050/sub.mk
+++ core/drivers/crypto/se050/sub.mk
@@ -1,10 +1,5 @@
 core-platform-cflags += "-I${CFG_NXP_SE05X_PLUG_AND_TRUST}/optee_lib/include"
-
-cflags-y += -DAX_EMBEDDED=1
-cflags-y += -DVERBOSE_APDU_LOGS=0
-cflags-y += -DT1oI2C_UM11225
-cflags-y += -DT1oI2C
-cflags-y += -DSSS_USE_FTR_FILE
+include core/drivers/crypto/se050/cflags.mk
 
 incdirs-y += adaptors/include
 
