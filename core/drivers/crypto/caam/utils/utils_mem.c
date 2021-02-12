f6578facce56a7d3108ce0aaaabb8de0be2f2a4b
Clement Faure <clement.faure@nxp.com>
2021-02-12 18:05:50 +0100

    drivers: caam: fix allocated buffer size
    When a buffer is allocated for output CAAM operations,
    the output buffer cache is invalidated beforehand.
    
    To avoid data loss, an allocated buffer size should be a
    multiple data cacheline size.
    
    Fixes: b22795b ("drivers: caam: make use of generic memalign() implementation")
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/drivers/crypto/caam/utils/utils_mem.c
+++ core/drivers/crypto/caam/utils/utils_mem.c
@@ -71,13 +71,18 @@ static uint32_t read_cacheline_size(void)
 static void *mem_alloc(size_t size, uint8_t type)
 {
 	void *ptr = NULL;
+	size_t cacheline_size = dcache_get_line_size();
+	size_t alloc_size = size;
 
 	MEM_TRACE("alloc %zu bytes of type %" PRIu8, size, type);
 
+	if (ROUNDUP_OVERFLOW(alloc_size, cacheline_size, &alloc_size))
+		return NULL;
+
 	if (type & MEM_TYPE_ALIGN)
-		ptr = memalign(dcache_get_line_size(), size);
+		ptr = memalign(cacheline_size, alloc_size);
 	else
-		ptr = malloc(size);
+		ptr = malloc(alloc_size);
 
 	if (!ptr) {
 		MEM_TRACE("alloc Error - NULL");
@@ -85,7 +90,7 @@ static void *mem_alloc(size_t size, uint8_t type)
 	}
 
 	if (type & MEM_TYPE_ZEROED)
-		memset(ptr, 0, size);
+		memset(ptr, 0, alloc_size);
 
 	MEM_TRACE("alloc returned %p", ptr);
 	return ptr;
