31c7c37fc4aacb0538de6b006d44e06ea4a6c48f
Dhananjay Phadke <dphadke@linux.microsoft.com>
2021-03-14 22:18:08 +0000

    pta: bcm: add separate config for hwrng pta
    CFG_BCM_HWRNG selects both hwrng driver and PTA.
    Add separate config CFG_PTA_BCM_HWRNG for PTA so that driver
    can be used with generic crypto framework.
    
    Signed-off-by: Dhananjay Phadke <dphadke@linux.microsoft.com>

--- core/arch/arm/plat-bcm/conf.mk
+++ core/arch/arm/plat-bcm/conf.mk
@@ -25,6 +25,7 @@ $(call force,CFG_SP805_WDT,y)
 $(call force,CFG_BCM_HWRNG,y)
 $(call force,CFG_BCM_SOTP,y)
 $(call force,CFG_BCM_GPIO,y)
++CFG_PTA_BCM_HWRNG ?= y
 CFG_BNXT_FW ?= y
 CFG_BCM_ELOG_DUMP ?= y
 endif
