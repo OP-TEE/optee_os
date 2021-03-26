5bf80bb42398704d2abf060c3e6904f3eff7ac32
Sughosh Ganu <sughosh.ganu@linaro.org>
2021-03-26 16:34:45 +0530

    core: mm: Use nexus memory allocation api's in carve_out_phys_mem()
    During discovery of the non-secure memory, the memory attributes like
    address and size are stored as part of the core_mmu_phys_mem
    structure. Memory for this structure is allocated on the nexus heap
    area. Subsequently, when memory for this structure is reallocated,
    this is done using the plain realloc call. Use the nex_realloc api for
    the reallocation.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- core/arch/arm/mm/core_mmu.c
+++ core/arch/arm/mm/core_mmu.c
@@ -304,7 +304,7 @@ static void carve_out_phys_mem(struct core_mmu_phys_mem **mem, size_t *nelems,
 		/* Remove this entry */
 		(*nelems)--;
 		memmove(m + n, m + n + 1, sizeof(*m) * (*nelems - n));
-		m = realloc(m, sizeof(*m) * *nelems);
+		m = nex_realloc(m, sizeof(*m) * *nelems);
 		if (!m)
 			panic();
 		*mem = m;
@@ -315,7 +315,7 @@ static void carve_out_phys_mem(struct core_mmu_phys_mem **mem, size_t *nelems,
 		m[n].size -= size;
 	} else {
 		/* Need to split the memory entry */
-		m = realloc(m, sizeof(*m) * (*nelems + 1));
+		m = nex_realloc(m, sizeof(*m) * (*nelems + 1));
 		if (!m)
 			panic();
 		*mem = m;
