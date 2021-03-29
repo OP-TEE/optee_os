OP-TEE/optee_os

commit d6ad67f674e5d0a08cc2f96d5bd66485d1e70d2c

core: mm: change vm_pa2va() to return a virtual address
Change vm_pa2va() to return target virtual address or NULL if the
physical address cannot be resolved which can happen when pager is
enabled and the target physical page belongs to the pager page pool.
This change makes vm_pa2va() helper function simpler and its only caller
doesn't differentiate error return codes.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/vm.c
@@ -1179,9 +1179,8 @@ TEE_Result vm_va2pa(const struct user_mode_ctx *uctx, void *ua, paddr_t *pa)
 	return tee_mmu_user_va2pa_attr(uctx, ua, pa, NULL);
 }
 
-TEE_Result vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa, void **va)
+void *vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa)
 {
-	TEE_Result res = TEE_SUCCESS;
 	paddr_t p = 0;
 	struct vm_region *region = NULL;
 
@@ -1199,6 +1198,8 @@ TEE_Result vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa, void **va)
 		assert(!granule || IS_POWER_OF_TWO(granule));
 
 		for (ofs = region->offset; ofs < region->size; ofs += size) {
+			if (mobj_get_pa(region->mobj, ofs, granule, &p))
+				continue;
 
 			if (granule) {
 				/* From current offset to buffer/granule end */
@@ -1206,12 +1207,9 @@ TEE_Result vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa, void **va)
 
 				if (size > (region->size - ofs))
 					size = region->size - ofs;
-			} else
+			} else {
 				size = region->size;
-
-			res = mobj_get_pa(region->mobj, ofs, granule, &p);
-			if (res != TEE_SUCCESS)
-				return res;
+			}
 
 			if (core_is_buffer_inside(pa, 1, p, size)) {
 				/* Remove region offset (mobj phys offset) */
@@ -1219,13 +1217,12 @@ TEE_Result vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa, void **va)
 				/* Get offset-in-granule */
 				p = pa - p;
 
-				*va = (void *)(region->va + ofs + (vaddr_t)p);
-				return TEE_SUCCESS;
+				return (void *)(region->va + ofs + (vaddr_t)p);
 			}
 		}
 	}
 
-	return TEE_ERROR_ACCESS_DENIED;
+	return NULL;
 }
 
 TEE_Result vm_check_access_rights(const struct user_mode_ctx *uctx,

core/arch/arm/mm/core_mmu.c
@@ -2197,16 +2197,10 @@ static void check_va_matches_pa(paddr_t pa __unused, void *va __unused)
 
 static void *phys_to_virt_ta_vaspace(paddr_t pa)
 {
-	TEE_Result res = TEE_ERROR_GENERIC;
-	void *va = NULL;
-
 	if (!core_mmu_user_mapping_is_active())
 		return NULL;
 
-	res = vm_pa2va(to_user_mode_ctx(thread_get_tsd()->ctx), pa, &va);
-	if (res != TEE_SUCCESS)
-		return NULL;
-	return va;
+	return vm_pa2va(to_user_mode_ctx(thread_get_tsd()->ctx), pa);
 }
 
 #ifdef CFG_WITH_PAGER

core/include/mm/vm.h
@@ -99,7 +99,7 @@ TEE_Result vm_va2pa(const struct user_mode_ctx *uctx, void *ua, paddr_t *pa);
  * given the user context.
  * Interface is deprecated, use phys_to_virt() instead.
  *---------------------------------------------------------------------------*/
-TEE_Result vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa, void **va);
+void *vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa);
 
 TEE_Result vm_check_access_rights(const struct user_mode_ctx *uctx,
 				  uint32_t flags, uaddr_t uaddr, size_t len);

commit 761f8d2baa7f9053385be58454e212e3cae81871

core: mm: fix infinite loop in vm_pa2va()
Commit d6ad67f674e5 ("core: mm: change vm_pa2va() to return a virtual
address") moved the call to mobj_get_pa() up in a 'for' loop and added
a 'continue' statement. Moving it was wrong because at this point 'size'
is not yet updated which causes an infinite loop when the PA is not
found.

Move the call back to its original location but keep the 'continue'
which looks correct.

Fixes: d6ad67f674e5 ("core: mm: change vm_pa2va() to return a virtual address")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/mm/vm.c
@@ -1198,8 +1198,6 @@ void *vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa)
 		assert(!granule || IS_POWER_OF_TWO(granule));
 
 		for (ofs = region->offset; ofs < region->size; ofs += size) {
-			if (mobj_get_pa(region->mobj, ofs, granule, &p))
-				continue;
 
 			if (granule) {
 				/* From current offset to buffer/granule end */
@@ -1210,6 +1208,9 @@ void *vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa)
 			} else {
 				size = region->size;
 			}
+
+			if (mobj_get_pa(region->mobj, ofs, granule, &p))
+				continue;
 
 			if (core_is_buffer_inside(pa, 1, p, size)) {
 				/* Remove region offset (mobj phys offset) */