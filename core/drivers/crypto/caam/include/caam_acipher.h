ba7c81e90b21aaa8dc4872269bd18b6ecf84aba6
Clement Faure <clement.faure@nxp.com>
2021-03-30 14:43:47 +0200

    drivers: caam: implement NXP CAAM Driver - DH
    Add DH CAAM driver.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/caam/include/caam_acipher.h
+++ core/drivers/crypto/caam/include/caam_acipher.h
@@ -23,6 +23,20 @@ static inline enum caam_status caam_rsa_init(vaddr_t ctrl_addr __unused)
 }
 #endif /* CFG_NXP_CAAM_RSA_DRV */
 
+#ifdef CFG_NXP_CAAM_DH_DRV
+/*
+ * Initialize the DH module
+ *
+ * @ctrl_addr   Controller base address
+ */
+enum caam_status caam_dh_init(vaddr_t ctrl_addr);
+#else
+static inline enum caam_status caam_dh_init(vaddr_t ctrl_addr __unused)
+{
+	return CAAM_NO_ERROR;
+}
+#endif /* CFG_NXP_CAAM_DH_DRV */
+
 #ifdef CFG_NXP_CAAM_ACIPHER_DRV
 /*
  * Initialize the MATH module
--- core/drivers/crypto/caam/include/caam_trace.h
+++ core/drivers/crypto/caam/include/caam_trace.h
@@ -38,6 +38,7 @@
 #define DBG_TRACE_BLOB   BIT32(10) /* BLOB trace */
 #define DBG_TRACE_DMAOBJ BIT32(11) /* DMA Object trace */
 #define DBG_TRACE_ECC    BIT32(12) /* ECC trace */
+#define DBG_TRACE_DH	 BIT32(13) /* DH trace */
 
 /* HAL */
 #if CAAM_DBG_TRACE(HAL)
@@ -205,6 +206,29 @@
 #define ECC_TRACE(...) do { } while (0)
 #define ECC_DUMPDESC(desc) do { } while (0)
 #define ECC_DUMPBUF(...) do { } while (0)
+#endif
+
+/* DH */
+#if CAAM_DBG_TRACE(DH)
+#define DH_TRACE DRV_TRACE
+#if CAAM_DBG_DESC(DH)
+#define DH_DUMPDESC(desc)                                                      \
+	{                                                                      \
+		DH_TRACE("DH Descriptor");                                     \
+		DRV_DUMPDESC(desc);                                            \
+	}
+#else
+#define DH_DUMPDESC(desc)
+#endif
+#if CAAM_DBG_BUF(DH)
+#define DH_DUMPBUF DRV_DUMPBUF
+#else
+#define DH_DUMPBUF(...)
+#endif
+#else
+#define DH_TRACE(...)
+#define DH_DUMPDESC(desc)
+#define DH_DUMPBUF(...)
 #endif
 
 #if (TRACE_LEVEL >= TRACE_DEBUG)
