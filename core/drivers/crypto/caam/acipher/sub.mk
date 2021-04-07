8bdff4a4c3ac0005b0668ea887528b96110be104
Clement Faure <clement.faure@nxp.com>
2021-04-07 16:15:43 +0200

    drivers: caam: append RSA to CAAM prime driver
    Rename caam_prime.c to caam_prime_rsa.c
    Rename struct prime_data {} to struct prime_data_rsa {}
    Rename caam_prime_gen() to caam_prime_rsa_gen()
    
    The reason why specifying the RSA algorithm for the current CAAM prime
    implementation is to prepare the merge for DSA algorithm that will
    feature a different prime generation implementation.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

--- core/drivers/crypto/caam/acipher/sub.mk
+++ core/drivers/crypto/caam/acipher/sub.mk
@@ -2,5 +2,5 @@ incdirs-y += ../include
 
 srcs-$(CFG_NXP_CAAM_RSA_DRV) += caam_rsa.c
 srcs-$(CFG_NXP_CAAM_DH_DRV)  += caam_dh.c
-srcs-y += caam_prime.c caam_math.c
+srcs-y += caam_prime_rsa.c caam_math.c
 srcs-$(CFG_NXP_CAAM_ECC_DRV) += caam_ecc.c
