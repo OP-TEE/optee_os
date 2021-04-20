17c6055ed549f2ea59c4c048e5482a8b1ddf5e24
Usama Arif <usama.arif@arm.com>
2021-04-20 15:24:36 +0100

    plat-totalcompute: Introduce TC1 platform
    The different platform parameters, such as GIC, UART, etc
    are similar between TC0 and TC1. tc0_spmc_pm.c is also renamed to
    tc_spmc_pm.c as its common to both platforms.
    
    Signed-off-by: Usama Arif <usama.arif@arm.com>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/plat-totalcompute/platform_config.h
+++ core/arch/arm/plat-totalcompute/platform_config.h
@@ -11,7 +11,7 @@
 /* Make stacks aligned to data cache line length */
 #define STACK_ALIGNMENT		64
 
-#if defined(PLATFORM_FLAVOR_tc0)
+#if defined(PLATFORM_FLAVOR_tc0) || defined(PLATFORM_FLAVOR_tc1)
 #ifndef CFG_CORE_SEL2_SPMC
 #define GIC_BASE		0x30000000
 #define GICD_OFFSET		0x0
