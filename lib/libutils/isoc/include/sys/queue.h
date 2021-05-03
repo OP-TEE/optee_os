0965314d8d33b72d0a54b3808808aaa0e90ad737
Jens Wiklander <jens.wiklander@linaro.org>
2021-05-03 14:24:43 +0200

    libutils: sys/queue.h: add SLIST_FOREACH_SAFE()
    Import macro SLIST_FOREACH_SAFE from FreeBSD.
    
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libutils/isoc/include/sys/queue.h
+++ lib/libutils/isoc/include/sys/queue.h
@@ -237,6 +237,11 @@ struct {								\
 #define	SLIST_FOREACH(var, head, field)					\
 	for((var) = (head)->slh_first; (var); (var) = (var)->field.sle_next)
 
+#define	SLIST_FOREACH_SAFE(var, head, field, tvar)			\
+	for ((var) = SLIST_FIRST((head));				\
+	    (var) && ((tvar) = SLIST_NEXT((var), field), 1);		\
+	    (var) = (tvar))
+
 /*
  * Singly-linked List access methods.
  */
