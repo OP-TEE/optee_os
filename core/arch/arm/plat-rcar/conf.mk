ceefea12576e70a03ba433a81ba8b2d7d93ba10d
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2021-05-05 19:21:31 +0300

    plat: rcar: disable aarch32 support for OP-TEE core
    While it is theoretically possible to boot RCar Gen3 SoC in aarch32
    mode, it isn't supported by Renesas BSP. ARM TF provided by Renesas
    boots only in aarch64 mode. Also it lacks aarch32 assembler code, so
    it is not possible to built it for aarch32 at all.
    
    Therefore, there is a little sense in supporting aarch32 in
    OP-TEE (for plat-rcar, of course) - user just can't boot it. On other
    hand it requires additional efforts to maintain aarch32 assembly code
    that newer will be used.
    
    This patch enforces CFG_ARM64_core build option and removes all
    aarch32 related parts.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/plat-rcar/conf.mk
+++ core/arch/arm/plat-rcar/conf.mk
@@ -17,15 +17,12 @@ $(call force,CFG_CORE_ARM64_PA_BITS,36)
 $(call force,CFG_CORE_ASLR,n)
 
 $(call force,CFG_TEE_CORE_NB_CORE,8)
+$(call force,CFG_ARM64_core,y)
+$(call force,CFG_WITH_LPAE,y)
 
 CFG_TZDRAM_START ?= 0x44100000
 CFG_TZDRAM_SIZE	 ?= 0x03D00000
 CFG_TEE_RAM_VA_SIZE ?= 0x100000
-ifeq ($(CFG_ARM64_core),y)
-$(call force,CFG_WITH_LPAE,y)
 supported-ta-targets = ta_arm64
-else
-$(call force,CFG_ARM32_core,y)
-endif
 
 CFG_DT ?= y
