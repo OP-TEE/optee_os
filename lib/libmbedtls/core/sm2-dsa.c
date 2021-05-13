OP-TEE/optee_os

commit c84eee6397bb8ae0745d9aa24b5228a58793378b

core: add support for SM2 using MBed TLS
The SM2 algorithms (PKE, KEP and DSA) are currently implemented using
LibTomCrypt. They are automatically disabled when MBed TLS is selected
as the core crypto library (that is, when CFG_CRYPTOLIB_NAME=mbedtls
CFG_CRYPTOLIB_DIR=lib/libmbedtls).

This commit removes this restriction by porting the relevant files
(core/lib/libtomcrypt/sm2-{dsa,kep,pke}.c) over to the MBed TLS API in
lib/libmbedtls/core.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

lib/libmbedtls/core/sm2-dsa.c
@@ -1,0 +1,266 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2019-2021 Huawei Technologies Co., Ltd
+ */
+
+#include <compiler.h>
+#include <crypto/crypto.h>
+#include <mbedtls/bignum.h>
+#include <mbedtls/ecp.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tee_api_types.h>
+#include <util.h>
+#include <utee_defines.h>
+
+#include "mbed_helpers.h"
+#include "sm2-dsa.h"
+
+/* SM2 uses 256 bit unsigned integers in big endian format */
+#define SM2_INT_SIZE_BYTES 32
+
+/*
+ * GM/T 0003.1‒2012 Part1 2 Section 6.1
+ */
+TEE_Result sm2_mbedtls_dsa_sign(uint32_t algo __unused, struct ecc_keypair *key,
+				const uint8_t *msg, size_t msg_len,
+				uint8_t *sig, size_t *sig_len)
+{
+	TEE_Result res = TEE_SUCCESS;
+	mbedtls_ecp_group grp = { };
+	mbedtls_ecp_point x1y1p = { };
+	int mres = 0;
+	mbedtls_mpi k = { };
+	mbedtls_mpi e = { };
+	mbedtls_mpi r = { };
+	mbedtls_mpi s = { };
+	mbedtls_mpi tmp = { };
+
+	if (*sig_len < 2 * SM2_INT_SIZE_BYTES) {
+		*sig_len = 64;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	mbedtls_mpi_init(&k);
+	mbedtls_mpi_init(&e);
+	mbedtls_mpi_init(&r);
+	mbedtls_mpi_init(&s);
+	mbedtls_mpi_init(&tmp);
+
+	mbedtls_ecp_point_init(&x1y1p);
+
+	mbedtls_ecp_group_init(&grp);
+	mres = mbedtls_ecp_group_load(&grp, MBEDTLS_ECP_DP_SM2);
+	if (mres) {
+		res = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	/*
+	 * Steps A1 and A2 are the generation of the hash value e from user
+	 * information (ZA) and the message to be signed (M). There are not done
+	 * here since @msg is expected to be the hash value e already.
+	 */
+
+	/* Step A3: generate random number 1 <= k < n */
+	do {
+		res = mbed_gen_random_upto(&k, &grp.N);
+		if (res)
+			goto out;
+
+		res = TEE_ERROR_BAD_STATE;
+
+		/* Step A4: compute (x1, y1) = [k]G */
+
+		mres = mbedtls_ecp_mul(&grp, &x1y1p, &k, &grp.G, mbd_rand,
+				       NULL);
+		if (mres)
+			goto out;
+
+		/* Step A5: compute r = (e + x1) mod n */
+
+		mbedtls_mpi_read_binary(&e, (unsigned char *)msg, msg_len);
+		mres = mbedtls_mpi_add_mpi(&r, &e, &x1y1p.X);
+		if (mres)
+			goto out;
+		mres = mbedtls_mpi_mod_mpi(&r, &r, &grp.N);
+		if (mres)
+			goto out;
+
+		/* Step A5 (continued): return to A3 if r = 0 or r + k = n */
+
+		mres = mbedtls_mpi_add_mpi(&tmp, &r, &k);
+		if (mres)
+			goto out;
+	} while (!mbedtls_mpi_cmp_int(&r, 0) ||
+		 !mbedtls_mpi_cmp_mpi(&tmp, &grp.N));
+
+	/* Step A6: compute s = ((1 + dA)^-1 * (k - r*dA)) mod n */
+
+	mres = mbedtls_mpi_add_int(&s, (mbedtls_mpi *)key->d, 1);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_inv_mod(&s, &s, &grp.N);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_mul_mpi(&tmp, &r, (mbedtls_mpi *)key->d);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_mod_mpi(&tmp, &tmp, &grp.N);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_sub_mpi(&tmp, &k, &tmp);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_mul_mpi(&s, &s, &tmp);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_mod_mpi(&s, &s, &grp.N);
+	if (mres)
+		goto out;
+
+	/* Step A7: convert (r, s) to binary for output */
+
+	*sig_len = 2 * SM2_INT_SIZE_BYTES;
+	memset(sig, 0, *sig_len);
+	mres = mbedtls_mpi_write_binary(&r, sig, SM2_INT_SIZE_BYTES);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_write_binary(&s, sig + SM2_INT_SIZE_BYTES,
+					SM2_INT_SIZE_BYTES);
+	if (mres)
+		goto out;
+
+	res = TEE_SUCCESS;
+out:
+	mbedtls_ecp_point_free(&x1y1p);
+	mbedtls_mpi_free(&k);
+	mbedtls_mpi_free(&e);
+	mbedtls_mpi_free(&r);
+	mbedtls_mpi_free(&s);
+	mbedtls_mpi_free(&tmp);
+	return res;
+}
+
+/*
+ * GM/T 0003.1‒2012 Part1 2 Section 7.1
+ */
+TEE_Result sm2_mbedtls_dsa_verify(uint32_t algo __unused,
+				  struct ecc_public_key *key,
+				  const uint8_t *msg, size_t msg_len,
+				  const uint8_t *sig, size_t sig_len)
+{
+	TEE_Result res = TEE_ERROR_BAD_STATE;
+	mbedtls_ecp_group grp = { };
+	mbedtls_mpi rprime = { };
+	mbedtls_mpi sprime = { };
+	mbedtls_mpi t = { };
+	mbedtls_mpi eprime = { };
+	mbedtls_mpi R = { };
+	mbedtls_ecp_point x1y1p = { };
+	mbedtls_ecp_point PA = { };
+	int mres = 0;
+
+	if (sig_len != 64)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	mbedtls_mpi_init(&rprime);
+	mbedtls_mpi_init(&sprime);
+	mbedtls_mpi_init(&t);
+	mbedtls_mpi_init(&eprime);
+	mbedtls_mpi_init(&R);
+
+	mbedtls_ecp_point_init(&x1y1p);
+	mbedtls_ecp_point_init(&PA);
+
+	mbedtls_ecp_group_init(&grp);
+	mres = mbedtls_ecp_group_load(&grp, MBEDTLS_ECP_DP_SM2);
+	if (mres) {
+		res = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	mres = mbedtls_mpi_read_binary(&rprime, sig, 32);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_read_binary(&sprime, sig + 32, 32);
+	if (mres)
+		goto out;
+
+	/* Step B1: verify r' in [1, n - 1] */
+
+	if (mbedtls_mpi_cmp_int(&rprime, 1) < 0 ||
+	    mbedtls_mpi_cmp_mpi(&rprime, &grp.N) >= 0) {
+		res = TEE_ERROR_SIGNATURE_INVALID;
+		goto out;
+	}
+
+	/* Step B2: verify s' in [1, n - 1] */
+
+	if (mbedtls_mpi_cmp_int(&sprime, 1) < 0 ||
+	    mbedtls_mpi_cmp_mpi(&sprime, &grp.N) >= 0) {
+		res = TEE_ERROR_SIGNATURE_INVALID;
+		goto out;
+	}
+
+	/*
+	 * Steps B3: M'bar = (ZA || M') and B4: e' = Hv(M'bar) are not done here
+	 * because @msg is supposed to contain the hash value e' already.
+	 */
+
+	/* Step B5: t = (r' + s') mod n and check t != 0 */
+
+	mres = mbedtls_mpi_add_mpi(&t, &rprime, &sprime);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_mod_mpi(&t, &t, &grp.N);
+	if (mres)
+		goto out;
+	if (!mbedtls_mpi_cmp_int(&t, 0)) {
+		res = TEE_ERROR_SIGNATURE_INVALID;
+		goto out;
+	}
+
+	/* Step B6: (x1', y1') = [s']G + [t]PA */
+
+	mres = mbedtls_mpi_copy(&PA.X, (mbedtls_mpi *)key->x);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_copy(&PA.Y, (mbedtls_mpi *)key->y);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_lset(&PA.Z, 1);
+	if (mres)
+		goto out;
+
+	mres = mbedtls_ecp_muladd(&grp, &x1y1p, &sprime, &grp.G, &t, &PA);
+	if (mres)
+		goto out;
+
+	/* Step B7: compute R = (e' + x1') mod n and verify R == r' */
+
+	mres = mbedtls_mpi_read_binary(&eprime, msg, msg_len);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_add_mpi(&R, &eprime, &x1y1p.X);
+	if (mres)
+		goto out;
+	mres = mbedtls_mpi_mod_mpi(&R, &R, &grp.N);
+	if (mres)
+		goto out;
+	if (mbedtls_mpi_cmp_mpi(&R, &rprime)) {
+		res = TEE_ERROR_SIGNATURE_INVALID;
+		goto out;
+	}
+
+	res = TEE_SUCCESS;
+out:
+	mbedtls_ecp_point_free(&x1y1p);
+	mbedtls_ecp_point_free(&PA);
+	mbedtls_mpi_free(&rprime);
+	mbedtls_mpi_free(&sprime);
+	mbedtls_mpi_free(&t);
+	mbedtls_mpi_free(&eprime);
+	mbedtls_mpi_free(&R);
+	return res;
+}

lib/libmbedtls/core/sm2-pke.c
@@ -1,0 +1,450 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2019-2021 Huawei Technologies Co., Ltd
+ */
+
+#include <crypto/crypto.h>
+#include <crypto/sm2-kdf.h>
+#include <mbedtls/bignum.h>
+#include <mbedtls/ecp.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string_ext.h>
+#include <tee_api_types.h>
+#include <util.h>
+#include <utee_defines.h>
+
+#include "mbed_helpers.h"
+#include "sm2-pke.h"
+
+/* SM2 uses 256 bit unsigned integers in big endian format */
+#define SM2_INT_SIZE_BYTES 32
+
+static TEE_Result
+sm2_uncompressed_bytes_to_point(const mbedtls_ecp_group *grp,
+				mbedtls_ecp_point *p, const uint8_t *x1y1,
+				size_t max_size, size_t *consumed)
+{
+	uint8_t *ptr = (uint8_t *)x1y1;
+	int mres = 0;
+
+	if (max_size < (size_t)(2 * SM2_INT_SIZE_BYTES))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	mres = mbedtls_mpi_read_binary(&p->X, ptr, SM2_INT_SIZE_BYTES);
+	if (mres)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	ptr += SM2_INT_SIZE_BYTES;
+
+	mres = mbedtls_mpi_read_binary(&p->Y, ptr, SM2_INT_SIZE_BYTES);
+	if (mres)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	mres = mbedtls_mpi_lset(&p->Z, 1);
+	if (mres)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	mres = mbedtls_ecp_check_pubkey(grp, p);
+	if (mres)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	*consumed = 2 * SM2_INT_SIZE_BYTES + 1; /* PC */
+
+	return TEE_SUCCESS;
+}
+
+/*
+ * GM/T 0003.1‒2012 Part 1 Section 4.2.9
+ * Conversion of a byte string @buf to a point @p. Makes sure @p is on the curve
+ * defined by domain parameters @dp.
+ * Note: only the uncompressed form is supported. Uncompressed and hybrid forms
+ * are TBD.
+ */
+static TEE_Result sm2_bytes_to_point(const mbedtls_ecp_group *grp,
+				     mbedtls_ecp_point *p, const uint8_t *buf,
+				     size_t max_size, size_t *consumed)
+{
+	uint8_t PC = 0;
+
+	if (!max_size)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	PC = buf[0];
+
+	switch (PC) {
+	case 0x02:
+	case 0x03:
+		/* Compressed form */
+		return TEE_ERROR_NOT_SUPPORTED;
+	case 0x04:
+		/* Uncompressed form */
+		return sm2_uncompressed_bytes_to_point(grp, p, buf + 1,
+						       max_size - 1, consumed);
+	case 0x06:
+	case 0x07:
+		/* Hybrid form */
+		return TEE_ERROR_NOT_SUPPORTED;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	return TEE_ERROR_GENERIC;
+}
+
+static bool is_zero(const uint8_t *buf, size_t size)
+{
+	uint8_t v = 0;
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+		v |= buf[i];
+
+	return !v;
+}
+
+/*
+ * GM/T 0003.1‒2012 Part 4 Section 7.1
+ * Decryption algorithm
+ */
+TEE_Result sm2_mbedtls_pke_decrypt(struct ecc_keypair *key, const uint8_t *src,
+				   size_t src_len, uint8_t *dst,
+				   size_t *dst_len)
+{
+	TEE_Result res = TEE_SUCCESS;
+	uint8_t x2y2[64] = { };
+	mbedtls_ecp_point C1 = { };
+	size_t C1_len = 0;
+	mbedtls_ecp_point x2y2p = { };
+	mbedtls_ecp_group grp = { };
+	void *ctx = NULL;
+	int mres = 0;
+	uint8_t *t = NULL;
+	size_t C2_len = 0;
+	size_t i = 0;
+	size_t out_len = 0;
+	uint8_t *eom = NULL;
+	uint8_t u[TEE_SM3_HASH_SIZE] = { };
+
+	/*
+	 * Input buffer src is (C1 || C2 || C3)
+	 * - C1 represents a point (should be on the curve)
+	 * - C2 is the encrypted message
+	 * - C3 is a SM3 hash
+	 */
+
+	mbedtls_ecp_point_init(&C1);
+	mbedtls_ecp_point_init(&x2y2p);
+
+	mbedtls_ecp_group_init(&grp);
+	mres = mbedtls_ecp_group_load(&grp, MBEDTLS_ECP_DP_SM2);
+	if (mres) {
+		res = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	/* Step B1: read and validate point C1 from encrypted message */
+
+	res = sm2_bytes_to_point(&grp, &C1, src, src_len, &C1_len);
+	if (res)
+		goto out;
+
+	/*
+	 * Step B2: S = [h]C1, the cofactor h is 1 for SM2 so S == C1.
+	 * The fact that S is on the curve has already been checked in
+	 * sm2_bytes_to_point().
+	 */
+
+	/* Step B3: (x2, y2) = [dB]C1 */
+
+	mres = mbedtls_ecp_mul(&grp, &x2y2p, (mbedtls_mpi *)key->d, &C1,
+			       mbd_rand, NULL);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	if (mbedtls_mpi_size(&x2y2p.X) > SM2_INT_SIZE_BYTES ||
+	    mbedtls_mpi_size(&x2y2p.Y) > SM2_INT_SIZE_BYTES) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	mres = mbedtls_mpi_write_binary(&x2y2p.X, x2y2, SM2_INT_SIZE_BYTES);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+	mres = mbedtls_mpi_write_binary(&x2y2p.Y, x2y2 + SM2_INT_SIZE_BYTES,
+					SM2_INT_SIZE_BYTES);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	/* Step B4: t = KDF(x2 || y2, klen) */
+
+	/* C = C1 || C2 || C3 */
+	if (src_len <= C1_len + TEE_SM3_HASH_SIZE) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+
+	C2_len = src_len - C1_len - TEE_SM3_HASH_SIZE;
+
+	t = calloc(1, C2_len);
+	if (!t) {
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
+	res = sm2_kdf(x2y2, sizeof(x2y2), t, C2_len);
+	if (res)
+		goto out;
+
+	if (is_zero(t, C2_len)) {
+		res = TEE_ERROR_CIPHERTEXT_INVALID;
+		goto out;
+	}
+
+	/* Step B5: get C2 from C and compute Mprime = C2 (+) t */
+
+	out_len = MIN(*dst_len, C2_len);
+	for (i = 0; i < out_len; i++)
+		dst[i] = src[C1_len + i] ^ t[i];
+	*dst_len = out_len;
+	if (out_len < C2_len) {
+		eom = calloc(1, C2_len - out_len);
+		if (!eom) {
+			res = TEE_ERROR_OUT_OF_MEMORY;
+			goto out;
+		}
+		for (i = out_len; i < C2_len; i++)
+		       eom[i - out_len] = src[C1_len + i] ^ t[i];
+	}
+
+	/* Step B6: compute u = Hash(x2 || M' || y2) and compare with C3 */
+
+	res = crypto_hash_alloc_ctx(&ctx, TEE_ALG_SM3);
+	if (res)
+		goto out;
+	res = crypto_hash_init(ctx);
+	if (res)
+		goto out;
+	res = crypto_hash_update(ctx, x2y2, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
+	res = crypto_hash_update(ctx, dst, out_len);
+	if (res)
+		goto out;
+	if (out_len < C2_len) {
+		res = crypto_hash_update(ctx, eom, C2_len - out_len);
+		if (res)
+			goto out;
+	}
+	res = crypto_hash_update(ctx, x2y2 + SM2_INT_SIZE_BYTES,
+				 SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
+	res = crypto_hash_final(ctx, u, sizeof(u));
+	if (res)
+		goto out;
+
+	if (consttime_memcmp(u, src + C1_len + C2_len, TEE_SM3_HASH_SIZE)) {
+		res = TEE_ERROR_CIPHERTEXT_INVALID;
+		goto out;
+	}
+out:
+	free(eom);
+	free(t);
+	crypto_hash_free_ctx(ctx);
+	mbedtls_ecp_point_free(&C1);
+	mbedtls_ecp_point_free(&x2y2p);
+	mbedtls_ecp_group_free(&grp);
+	return res;
+}
+
+/*
+ * GM/T 0003.1‒2012 Part 1 Section 4.2.8
+ * Conversion of point @p to a byte string @buf (uncompressed form).
+ */
+static TEE_Result sm2_point_to_bytes(uint8_t *buf, size_t *size,
+				     const mbedtls_ecp_point *p)
+{
+	size_t xsize = mbedtls_mpi_size(&p->X);
+	size_t ysize = mbedtls_mpi_size(&p->Y);
+	size_t sz = 2 * SM2_INT_SIZE_BYTES + 1;
+	int mres = 0;
+
+	if (xsize > SM2_INT_SIZE_BYTES || ysize > SM2_INT_SIZE_BYTES ||
+	    *size < sz)
+		return TEE_ERROR_BAD_STATE;
+
+	memset(buf, 0, sz);
+	buf[0] = 0x04;  /* Uncompressed form indicator */
+	mres = mbedtls_mpi_write_binary(&p->X, buf + 1, SM2_INT_SIZE_BYTES);
+	if (mres)
+		return TEE_ERROR_BAD_STATE;
+	mres = mbedtls_mpi_write_binary(&p->Y, buf + 1 + SM2_INT_SIZE_BYTES,
+					SM2_INT_SIZE_BYTES);
+	if (mres)
+		return TEE_ERROR_BAD_STATE;
+
+	*size = sz;
+
+	return TEE_SUCCESS;
+}
+
+/*
+ * GM/T 0003.1‒2012 Part 4 Section 6.1
+ * Encryption algorithm
+ */
+TEE_Result sm2_mbedtls_pke_encrypt(struct ecc_public_key *key,
+				   const uint8_t *src, size_t src_len,
+				   uint8_t *dst, size_t *dst_len)
+{
+	TEE_Result res = TEE_SUCCESS;
+	mbedtls_ecp_group grp = { };
+	mbedtls_ecp_point x2y2p = { };
+	mbedtls_ecp_point PB = { };
+	mbedtls_ecp_point C1 = { };
+	uint8_t x2y2[64] = { };
+	uint8_t *t = NULL;
+	int mres = 0;
+	mbedtls_mpi k = { };
+	size_t C1_len = 0;
+	void *ctx = NULL;
+	size_t i = 0;
+
+	mbedtls_mpi_init(&k);
+
+	mbedtls_ecp_point_init(&x2y2p);
+	mbedtls_ecp_point_init(&PB);
+	mbedtls_ecp_point_init(&C1);
+
+	mbedtls_ecp_group_init(&grp);
+	mres = mbedtls_ecp_group_load(&grp, MBEDTLS_ECP_DP_SM2);
+	if (mres) {
+		res = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	/* Step A1: generate random number 1 <= k < n */
+
+	res = mbed_gen_random_upto(&k, &grp.N);
+	if (res)
+		goto out;
+
+	/* Step A2: compute C1 = [k]G */
+
+	mres = mbedtls_ecp_mul(&grp, &C1, &k, &grp.G, mbd_rand, NULL);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	/*
+	 * Step A3: compute S = [h]PB and check for infinity.
+	 * The cofactor h is 1 for SM2 so S == PB, nothing to do.
+	 */
+
+	/* Step A4: compute (x2, y2) = [k]PB */
+
+	mbedtls_mpi_copy(&PB.X, (mbedtls_mpi *)key->x);
+	mbedtls_mpi_copy(&PB.Y, (mbedtls_mpi *)key->y);
+	mbedtls_mpi_lset(&PB.Z, 1);
+
+	mres = mbedtls_ecp_mul(&grp, &x2y2p, &k, &PB, mbd_rand, NULL);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	if (mbedtls_mpi_size(&x2y2p.X) > SM2_INT_SIZE_BYTES ||
+	    mbedtls_mpi_size(&x2y2p.Y) > SM2_INT_SIZE_BYTES) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	mres = mbedtls_mpi_write_binary(&x2y2p.X, x2y2, SM2_INT_SIZE_BYTES);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+	mres = mbedtls_mpi_write_binary(&x2y2p.Y, x2y2 + SM2_INT_SIZE_BYTES,
+					SM2_INT_SIZE_BYTES);
+	if (mres) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	/* Step A5: compute t = KDF(x2 || y2, klen) */
+
+	t = calloc(1, src_len);
+	if (!t) {
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
+	res = sm2_kdf(x2y2, sizeof(x2y2), t, src_len);
+	if (res)
+		goto out;
+
+	if (is_zero(t, src_len)) {
+		res = TEE_ERROR_CIPHERTEXT_INVALID;
+		goto out;
+	}
+
+	/*
+	 * Steps A6, A7, A8:
+	 * Compute C2 = M (+) t
+	 * Compute C3 = Hash(x2 || M || y2)
+	 * Output C = C1 || C2 || C3
+	 */
+
+	/* C1 */
+	C1_len = *dst_len;
+	res = sm2_point_to_bytes(dst, &C1_len, &C1);
+	if (res)
+		goto out;
+
+	if (*dst_len < C1_len + src_len + TEE_SM3_HASH_SIZE) {
+		*dst_len = C1_len + src_len + TEE_SM3_HASH_SIZE;
+		res = TEE_ERROR_SHORT_BUFFER;
+		goto out;
+	}
+
+	/* C2 */
+	for (i = 0; i < src_len; i++)
+		dst[i + C1_len] = src[i] ^ t[i];
+
+	/* C3 */
+        res = crypto_hash_alloc_ctx(&ctx, TEE_ALG_SM3);
+        if (res)
+                goto out;
+        res = crypto_hash_init(ctx);
+        if (res)
+                goto out;
+        res = crypto_hash_update(ctx, x2y2, SM2_INT_SIZE_BYTES);
+        if (res)
+                goto out;
+        res = crypto_hash_update(ctx, src, src_len);
+        if (res)
+                goto out;
+        res = crypto_hash_update(ctx, x2y2 + SM2_INT_SIZE_BYTES,
+				 SM2_INT_SIZE_BYTES);
+        if (res)
+                goto out;
+        res = crypto_hash_final(ctx, dst + C1_len + src_len, TEE_SM3_HASH_SIZE);
+        if (res)
+                goto out;
+
+	*dst_len = C1_len + src_len + TEE_SM3_HASH_SIZE;
+out:
+	crypto_hash_free_ctx(ctx);
+	free(t);
+	mbedtls_ecp_point_free(&x2y2p);
+	mbedtls_ecp_point_free(&PB);
+	mbedtls_ecp_point_free(&C1);
+	return res;
+}

core/crypto.mk
@@ -39,20 +39,9 @@ CFG_CRYPTO_RSA ?= y
 CFG_CRYPTO_DH ?= y
 # ECC includes ECDSA and ECDH
 CFG_CRYPTO_ECC ?= y
-ifeq ($(CFG_CRYPTOLIB_NAME),tomcrypt)
 CFG_CRYPTO_SM2_PKE ?= y
 CFG_CRYPTO_SM2_DSA ?= y
 CFG_CRYPTO_SM2_KEP ?= y
-endif
-ifeq ($(CFG_CRYPTOLIB_NAME)-$(CFG_CRYPTO_SM2_PKE),mbedtls-y)
-$(error Error: CFG_CRYPTO_SM2_PKE=y requires CFG_CRYPTOLIB_NAME=tomcrypt)
-endif
-ifeq ($(CFG_CRYPTOLIB_NAME)-$(CFG_CRYPTO_SM2_DSA),mbedtls-y)
-$(error Error: CFG_CRYPTO_SM2_DSA=y requires CFG_CRYPTOLIB_NAME=tomcrypt)
-endif
-ifeq ($(CFG_CRYPTOLIB_NAME)-$(CFG_CRYPTO_SM2_KEP),mbedtls-y)
-$(error Error: CFG_CRYPTO_SM2_KEP=y requires CFG_CRYPTOLIB_NAME=tomcrypt)
-endif
 
 # Authenticated encryption
 CFG_CRYPTO_CCM ?= y

lib/libmbedtls/core/ecc.c
@@ -5,7 +5,7 @@
  */
 
 #include <assert.h>
-#include <compiler.h>
+#include <config.h>
 #include <crypto/crypto_impl.h>
 #include <mbedtls/ctr_drbg.h>
 #include <mbedtls/ecdh.h>
@@ -17,6 +17,8 @@
 #include <string.h>
 
 #include "mbed_helpers.h"
+#include "sm2-dsa.h"
+#include "sm2-pke.h"
 
 /* Translate mbedtls result to TEE result */
 static TEE_Result get_tee_result(int lmd_res)
@@ -349,20 +351,53 @@ static const struct crypto_ecc_keypair_ops ecc_keypair_ops = {
 	.shared_secret = ecc_shared_secret,
 };
 
+static const struct crypto_ecc_keypair_ops sm2_pke_keypair_ops = {
+	.generate = ecc_generate_keypair,
+	.decrypt = sm2_mbedtls_pke_decrypt,
+};
+
+static const struct crypto_ecc_keypair_ops sm2_kep_keypair_ops = {
+	.generate = ecc_generate_keypair,
+};
+
+static const struct crypto_ecc_keypair_ops sm2_dsa_keypair_ops = {
+	.generate = ecc_generate_keypair,
+	.sign = sm2_mbedtls_dsa_sign,
+};
+
 TEE_Result crypto_asym_alloc_ecc_keypair(struct ecc_keypair *s,
 					 uint32_t key_type,
 					 size_t key_size_bits)
 {
+	memset(s, 0, sizeof(*s));
+
 	switch (key_type) {
-	case TEE_TYPE_SM2_DSA_KEYPAIR:
-	case TEE_TYPE_SM2_PKE_KEYPAIR:
-	case TEE_TYPE_SM2_KEP_KEYPAIR:
-		return TEE_ERROR_NOT_IMPLEMENTED;
-	default:
+	case TEE_TYPE_ECDSA_KEYPAIR:
+	case TEE_TYPE_ECDH_KEYPAIR:
+		s->ops = &ecc_keypair_ops;
 		break;
+	case TEE_TYPE_SM2_DSA_KEYPAIR:
+		if (!IS_ENABLED(CFG_CRYPTO_SM2_DSA))
+			return TEE_ERROR_NOT_IMPLEMENTED;
+
+		s->ops = &sm2_dsa_keypair_ops;
+		break;
+	case TEE_TYPE_SM2_PKE_KEYPAIR:
+		if (!IS_ENABLED(CFG_CRYPTO_SM2_PKE))
+			return TEE_ERROR_NOT_IMPLEMENTED;
+
+		s->ops = &sm2_pke_keypair_ops;
+		break;
+	case TEE_TYPE_SM2_KEP_KEYPAIR:
+		if (!IS_ENABLED(CFG_CRYPTO_SM2_KEP))
+			return TEE_ERROR_NOT_IMPLEMENTED;
+
+		s->ops = &sm2_kep_keypair_ops;
+		break;
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
 	}
 
-	memset(s, 0, sizeof(*s));
 	s->d = crypto_bignum_allocate(key_size_bits);
 	if (!s->d)
 		goto err;
@@ -372,8 +407,6 @@ TEE_Result crypto_asym_alloc_ecc_keypair(struct ecc_keypair *s,
 	s->y = crypto_bignum_allocate(key_size_bits);
 	if (!s->y)
 		goto err;
-
-	s->ops = &ecc_keypair_ops;
 
 	return TEE_SUCCESS;
 
@@ -389,28 +422,59 @@ static const struct crypto_ecc_public_ops ecc_public_key_ops = {
 	.verify = ecc_verify,
 };
 
+static const struct crypto_ecc_public_ops sm2_pke_public_key_ops = {
+	.free = ecc_free_public_key,
+	.encrypt = sm2_mbedtls_pke_encrypt,
+};
+
+static const struct crypto_ecc_public_ops sm2_kep_public_key_ops = {
+	.free = ecc_free_public_key,
+};
+
+static const struct crypto_ecc_public_ops sm2_dsa_public_key_ops = {
+	.free = ecc_free_public_key,
+	.verify = sm2_mbedtls_dsa_verify,
+};
+
 TEE_Result crypto_asym_alloc_ecc_public_key(struct ecc_public_key *s,
 					    uint32_t key_type,
 					    size_t key_size_bits)
 {
+	memset(s, 0, sizeof(*s));
+
 	switch (key_type) {
-	case TEE_TYPE_SM2_DSA_PUBLIC_KEY:
-	case TEE_TYPE_SM2_PKE_PUBLIC_KEY:
-	case TEE_TYPE_SM2_KEP_PUBLIC_KEY:
-		return TEE_ERROR_NOT_IMPLEMENTED;
-	default:
+	case TEE_TYPE_ECDSA_PUBLIC_KEY:
+	case TEE_TYPE_ECDH_PUBLIC_KEY:
+		s->ops = &ecc_public_key_ops;
 		break;
+	case TEE_TYPE_SM2_DSA_PUBLIC_KEY:
+		if (!IS_ENABLED(CFG_CRYPTO_SM2_DSA))
+			return TEE_ERROR_NOT_IMPLEMENTED;
+
+		s->ops = &sm2_dsa_public_key_ops;
+		break;
+	case TEE_TYPE_SM2_PKE_PUBLIC_KEY:
+		if (!IS_ENABLED(CFG_CRYPTO_SM2_PKE))
+			return TEE_ERROR_NOT_IMPLEMENTED;
+
+		s->ops = &sm2_pke_public_key_ops;
+		break;
+	case TEE_TYPE_SM2_KEP_PUBLIC_KEY:
+		if (!IS_ENABLED(CFG_CRYPTO_SM2_KEP))
+			return TEE_ERROR_NOT_IMPLEMENTED;
+
+		s->ops = &sm2_kep_public_key_ops;
+		break;
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
 	}
 
-	memset(s, 0, sizeof(*s));
 	s->x = crypto_bignum_allocate(key_size_bits);
 	if (!s->x)
 		goto err;
 	s->y = crypto_bignum_allocate(key_size_bits);
 	if (!s->y)
 		goto err;
-
-	s->ops = &ecc_public_key_ops;
 
 	return TEE_SUCCESS;
 

lib/libmbedtls/core/mbed_helpers.c
@@ -1,0 +1,33 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2021 Huawei Technologies Co., Ltd
+ */
+
+#include <compiler.h>
+#include <crypto/crypto.h>
+#include <mbedtls/bignum.h>
+#include <stddef.h>
+#include <tee_api_defines.h>
+#include <tee_api_types.h>
+
+#include "mbed_helpers.h"
+
+/* Generate random number 1 <= n < max */
+TEE_Result mbed_gen_random_upto(mbedtls_mpi *n, mbedtls_mpi *max)
+{
+	size_t sz = mbedtls_mpi_size(max);
+	bool found = false;
+	int mres = 0;
+
+	do {
+		mres = mbedtls_mpi_fill_random(n, sz, mbd_rand, NULL);
+		if (mres)
+			return TEE_ERROR_BAD_STATE;
+		if (mbedtls_mpi_bitlen(n) != 0 &&
+		    mbedtls_mpi_cmp_mpi(n, max) == -1)
+			found = true;
+	} while (!found);
+
+	return TEE_SUCCESS;
+}
+

lib/libmbedtls/core/mbed_helpers.h
@@ -9,7 +9,9 @@
 
 #include <crypto/crypto.h>
 #include <mbedtls/aes.h>
+#include <mbedtls/bignum.h>
 #include <mbedtls/ctr_drbg.h>
+#include <tee_api_types.h>
 
 static inline int mbd_rand(void *rng_state __unused, unsigned char *output,
 			size_t len)
@@ -36,4 +38,5 @@ static inline void mbed_copy_mbedtls_aes_context(mbedtls_aes_context *dst,
 #endif
 }
 
+TEE_Result mbed_gen_random_upto(mbedtls_mpi *n, mbedtls_mpi *max);
 #endif /*MBED_HELPERS_H*/

lib/libmbedtls/core/sm2-dsa.h
@@ -1,0 +1,20 @@
+
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2021 Huawei Technologies Co., Ltd
+ */
+
+#ifndef _SM2_DSA_H_
+
+#include <crypto/crypto.h>
+#include <stdint.h>
+#include <tee_api_types.h>
+
+TEE_Result sm2_mbedtls_dsa_sign(uint32_t algo, struct ecc_keypair *key,
+				const uint8_t *msg, size_t msg_len,
+				uint8_t *sig, size_t *sig_len);
+
+TEE_Result sm2_mbedtls_dsa_verify(uint32_t algo, struct ecc_public_key *key,
+				  const uint8_t *msg, size_t msg_len,
+				  const uint8_t *sig, size_t sig_len);
+#endif /* _SM2_DSA_H_ */

commit 52c2ff9488ee3c40a8bc5fe5f5cd9ddefa5644bc

core: Fix memory leaks in sm2 of Mbedtls lib
When enable mbedtls in optee-os , xtest run twice will be fail.
Memory leakage exists in three functions.
1. grp and mpi in sm2_mbedtls_pke_encrypt
2. grp in sm2_mbedtls_dsa_sign
3. grp in sm2_mbedtls_dsa_verify

Fixes: c84eee6397bb ("core: add support for SM2 using MBed TLS")
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: lubing <lubing@eswin.com>

lib/libmbedtls/core/sm2-dsa.c
@@ -139,6 +139,7 @@ out:
 	mbedtls_mpi_free(&r);
 	mbedtls_mpi_free(&s);
 	mbedtls_mpi_free(&tmp);
+	mbedtls_ecp_group_free(&grp);
 	return res;
 }
 
@@ -262,5 +263,6 @@ out:
 	mbedtls_mpi_free(&t);
 	mbedtls_mpi_free(&eprime);
 	mbedtls_mpi_free(&R);
+	mbedtls_ecp_group_free(&grp);
 	return res;
 }

lib/libmbedtls/core/sm2-pke.c
@@ -446,5 +446,7 @@ out:
 	mbedtls_ecp_point_free(&x2y2p);
 	mbedtls_ecp_point_free(&PB);
 	mbedtls_ecp_point_free(&C1);
+	mbedtls_ecp_group_free(&grp);
+	mbedtls_mpi_free(&k);
 	return res;
 }