858f7f938471b2c2d165062db2eb6ed6ce030955
Jelle Sels <jelle.sels@arm.com>
2021-05-18 16:26:05 +0200

    fixup! core: Add FFA_PARTITION_INFO
    fix core: Add FFA_PARTITION_INFO
    
    Signed-off-by: Jelle Sels <jelle.sels@arm.com>

--- core/arch/arm/kernel/secure_partition.c
+++ core/arch/arm/kernel/secure_partition.c
@@ -93,20 +93,19 @@ TEE_Result sp_get_partitions_info(struct ffa_partition_info *fpi,
 	uint32_t count = 0;
 
 	TAILQ_FOREACH(s, &open_sp_sessions, link) {
-		if (count < in_elements) {
-			if (s->state != sp_dead) {
-				spmc_fill_partition_entry(fpi, s->endpoint_id);
-				fpi++;
-			}
-		}
 		if (s->state != sp_dead)
-			count++;
+			continue;
+		count++;
+		if (count < in_elements) {
+			spmc_fill_partition_entry(fpi, s->endpoint_id, 1);
+			fpi++;
+		}
 	}
 
-	if (count > in_elements) {
-		*elements = count;
+	if (count >= in_elements)
 		return TEE_ERROR_SHORT_BUFFER;
-	}
+
+	*elements = count;
 
 	return TEE_SUCCESS;
 }
