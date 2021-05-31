9781fbd252c95b1b2f34a02974193d20044df44a
Clement Faure <clement.faure@nxp.com>
2021-05-31 17:09:00 +0200

    core: imx: add support for i.MX8ULP
    Add support for i.MX8ULP platform.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Jerome Forissier <jerome@forissier.org>

---
+++ core/arch/arm/plat-imx/registers/imx8ulp-crm.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2021 NXP
+ */
+#ifndef __IMX8ULP_CRM_H__
+#define __IMX8ULP_CRM_H__
+
+#include <util.h>
+
+#define PCC_CGC_BIT_SHIFT 30
+#define PCC_ENABLE_CLOCK  BIT32(PCC_CGC_BIT_SHIFT)
+#define PCC_CAAM	  0xB8
+
+#endif /* __IMX8ULP_CRM_H__ */
