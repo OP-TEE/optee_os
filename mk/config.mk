804e32d7cfeac9a625473215d03158720b8e57fc
Clément Léger <clement.leger@bootlin.com>
2021-06-14 11:32:04 +0200

    drivers: clk: add fixed-clock driver
    fixed-clock are a really common clock types used in device tree and
    when there is a clock hierarchy, they are needed to query the clock
    rate. This driver is build by default when CFG_DRIVERS_CLK_DT is
    enabled.
    
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- mk/config.mk
+++ mk/config.mk
@@ -718,7 +718,10 @@ CFG_PREALLOC_RPC_CACHE ?= y
 # This clock framework allows to describe clock tree and provides functions to
 # get and configure the clocks.
 # CFG_DRIVERS_CLK_DT embeds devicetree clock parsing support
+# CFG_DRIVERS_CLK_FIXED add support for "fixed-clock" compatible clocks
 CFG_DRIVERS_CLK ?= n
 CFG_DRIVERS_CLK_DT ?= $(call cfg-all-enabled,CFG_DRIVERS_CLK CFG_DT)
+CFG_DRIVERS_CLK_FIXED ?= $(CFG_DRIVERS_CLK_DT)
 
 $(eval $(call cfg-depends-all,CFG_DRIVERS_CLK_DT,CFG_DRIVERS_CLK CFG_DT))
+$(eval $(call cfg-depends-all,CFG_DRIVERS_CLK_FIXED,CFG_DRIVERS_CLK_DT))
