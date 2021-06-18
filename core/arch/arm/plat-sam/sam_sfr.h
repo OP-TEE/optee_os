82444cc2340f06653b94a22e45d91dfb4c0ec25f
Clément Léger <clement.leger@bootlin.com>
2021-06-18 17:08:46 +0200

    drivers: sam: add at91_utmi clock driver
    Add driver for UTMI clock.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Nicolas Ferre <nicolas.ferre@microchip.com>
    Acked-by: Boris Brezillon <bbrezillon@kernel.org>
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/sam_sfr.h
+++ core/arch/arm/plat-sam/sam_sfr.h
@@ -8,10 +8,15 @@
 
 #include <util.h>
 
+/* UTMI Clock Trimming Register */
+#define AT91_SFR_UTMICKTRIM	0x30
 /* L2 cache RAM used as an internal SRAM */
 #define AT91_SFR_L2CC_HRAMC		0x58
 /* I2SC Register */
 #define AT91_SFR_I2SCLKSEL	0x90
+
+/* Field definitions */
+#define AT91_UTMICKTRIM_FREQ			GENMASK_32(1, 0)
 
 vaddr_t sam_sfr_base(void);
 
