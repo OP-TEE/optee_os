OP-TEE/optee_os

commit 38923d487567e1f0b9e627a88b61d8c748406dad

drivers: caam: implement CAAM DMA Object
Implementation of a CAAM DMA object to:
  - create a DMA object (SGT/buffer) based on input/output buffers
  - reallocate a new buffer accessible from the CAAM address space
  - ensure buffer is cache aligned (for the output)

Implementation of CAAM DMA object functions to:
  - cache maintenance
  - free CAAM DMA object

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/caam/include/caam_utils_dmaobj.h
@@ -1,0 +1,195 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2020-2021 NXP
+ *
+ * CAAM DMA data object utilities include file.
+ */
+#ifndef __CAAM_UTILS_DMAOBJ_H__
+#define __CAAM_UTILS_DMAOBJ_H__
+
+#include <caam_types.h>
+#include <caam_utils_sgt.h>
+#include <tee_api_types.h>
+
+/*
+ * CAAM DMA Object type
+ * @priv   Private object data not used externally.
+ * @orig   Original data buffer
+ * @sgtbuf CAAM SGT/Buffer object
+ */
+struct caamdmaobj {
+	void *priv;
+	struct caambuf orig;
+	struct caamsgtbuf sgtbuf;
+};
+
+/*
+ * Initialize a CAAM DMA object of type input data.
+ * If necessary, a new CAAM Buffer will be reallocated if given @data is not
+ * accessible by the CAAM DMA and input data copied into.
+ *
+ * @obj     [out] CAAM DMA object initialized
+ * @data    Input data pointer
+ * @length  Length in bytes of the input data
+ */
+TEE_Result caam_dmaobj_init_input(struct caamdmaobj *obj, const void *data,
+				  size_t len);
+/*
+ * Initialize and build the SGT/Buffer Object of a CAAM DMA object of type
+ * input data.
+ * Function call the caam_dmaobj_init_input function and if success
+ * the caam_dmaobj_sgtbuf_build function. If the full size of the input
+ * data can't be handled in the SGT/Buffer Object, returns in error.
+ *
+ * @obj     [out] CAAM DMA object initialized
+ * @data    Input data pointer
+ * @length  Length in bytes of the input data
+ */
+TEE_Result caam_dmaobj_input_sgtbuf(struct caamdmaobj *obj, const void *data,
+				    size_t len);
+/*
+ * Initialize a CAAM DMA object of type output data.
+ * If necessary, a new CAAM Buffer will be reallocated if given @data is not
+ * accessible by the CAAM DMA or if the given @length is lower than
+ * @min_length requested for the CAAM operation.
+ *
+ * @obj         [out] CAAM DMA object initialized
+ * @data        Output data pointer
+ * @length      Length in bytes of the output data
+ * @min_length  Minimum length in bytes needed for the output data
+ */
+TEE_Result caam_dmaobj_init_output(struct caamdmaobj *obj, void *data,
+				   size_t length, size_t min_length);
+
+/*
+ * Initialize and build the SGT/Buffer Object of a CAAM DMA object of type
+ * output data.
+ * Function call the caam_dmaobj_init_output function and if success
+ * the caam_dmaobj_sgtbuf_build function. If the full size of the output
+ * data can't be handled in the SGT/Buffer Object, returns in error.
+ *
+ * Note: to allocate a output buffer, set @data = NULL and length = 0, the
+ * buffer size allocated will be the @min_length size. Caution, the field
+ * orig of the @obj is kept empty.
+ *
+ * @obj         [out] CAAM DMA object initialized
+ * @data        Output data pointer
+ * @length      Length in bytes of the output data
+ * @min_length  Minimum length in bytes needed for the output data
+ */
+TEE_Result caam_dmaobj_output_sgtbuf(struct caamdmaobj *obj, void *data,
+				     size_t length, size_t min_length);
+
+/*
+ * Push the data to physical memory with a cache clean or flush depending
+ * on the type of data, respectively input or output.
+ *
+ * @obj     CAAM DMA object
+ */
+void caam_dmaobj_cache_push(struct caamdmaobj *obj);
+
+/*
+ * Copy the CAAM DMA object buffer to the original data buffer.
+ * Return the number of bytes copied.
+ *
+ * @obj     CAAM DMA object
+ */
+size_t caam_dmaobj_copy_to_orig(struct caamdmaobj *obj);
+
+/*
+ * Copy the CAAM DMA object buffer to the original data buffer removing
+ * non-significant first zeros (left zeros).
+ * If all DMA object buffer is zero, left only one zero in the destination.
+ * Return the number of bytes copied.
+ *
+ * @obj    CAAM DMA object
+ */
+size_t caam_dmaobj_copy_ltrim_to_orig(struct caamdmaobj *obj);
+
+/*
+ * Free the CAAM DMA object.
+ * If a buffer has been reallocated, free it.
+ * Free the sgtbuf object.
+ *
+ * @obj     CAAM DMA object
+ */
+void caam_dmaobj_free(struct caamdmaobj *obj);
+
+/*
+ * Create a CAAM DMA object SGT type with the block buffer @block first and
+ * the CAAM DMA Object after
+ *
+ * @res     CAAM DMA object resulting
+ * @block   CAAM Block buffer to add first
+ * @obj     CAAM DMA object to add secondly
+ */
+TEE_Result caam_dmaobj_add_first_block(struct caamdmaobj *obj,
+				       struct caamblock *block);
+
+/*
+ * Derive a CAAM DMA object's sgtbuf object to a new DMA object.
+ * The @from CAAM DMA object sgtbuf must have to be created first to
+ * allocate the DMA Buffers.
+ *
+ * @obj     [out] CAAM DMA object derived
+ * @from    Original CAAM DMA object
+ * @offset  Offset to start from
+ * @length  Length in bytes of the data
+ */
+TEE_Result caam_dmaobj_derive_sgtbuf(struct caamdmaobj *obj,
+				     const struct caamdmaobj *from,
+				     size_t offset, size_t length);
+
+/*
+ * Build the CAAM DMA Object's sgtbuf input and output with the same data
+ * length.
+ * First try to build input sgtbuf of maximum @length starting at @offset.
+ * Then build output sgtbuf with same input data length built start at @offset.
+ * If output sgtbuf built data length is not the same as the input's one,
+ * rebuild the input with same output data length.
+ *
+ * If the both input and output length are not equal returns an error.
+ *
+ * @input   CAAM DMA Input object
+ * @output  CAAM DMA Output object
+ * @length  [in/out] maximum length to do/done
+ * @off     Starting offset
+ * @align   Buffer allocation alignment
+ */
+TEE_Result caam_dmaobj_sgtbuf_inout_build(struct caamdmaobj *input,
+					  struct caamdmaobj *output,
+					  size_t *length, size_t off,
+					  size_t align);
+
+/*
+ * Prepare input/output CAAM DMA Object's by allocating the DMA Buffers
+ * if needed.
+ * If @input or @output is NULL, allocates DMA buffer of given object.
+ * Else if both objects are set, allocates DMA buffer of the same
+ * size for the @input and @output objects.
+ * Minimum DMA Buffer size allocated is the @min_size value. Even if this
+ * minimum size allocation failed, returns an error.
+ *
+ * @input     CAAM DMA object input
+ * @output    CAAM DMA object output
+ * @min_size  Mimimum length to allocate
+ */
+TEE_Result caam_dmaobj_prepare(struct caamdmaobj *input,
+			       struct caamdmaobj *output, size_t min_size);
+
+/*
+ * Build the CAAM DMA Object's sgtbuf object. Try to build a sgtbuf of
+ * maximum @length starting at @offset.
+ * Return the @length mapped in the sgtbuf object. If a new data buffer
+ * must be reallocated, allocated it to the maximum possible round to @align
+ * value.
+ *
+ * @obj     CAAM DMA object
+ * @length  [in/out] maximum length to do/done
+ * @off     Starting offset
+ * @align   Buffer allocation alignment
+ */
+TEE_Result caam_dmaobj_sgtbuf_build(struct caamdmaobj *obj, size_t *length,
+				    size_t off, size_t align);
+
+#endif /* __CAAM_UTILS_DMAOBJ_H__ */

core/arch/arm/plat-imx/crypto_conf.mk
@@ -19,6 +19,7 @@
 # DBG_RSA    BIT32(8)  // RSA trace
 # DBG_CIPHER BIT32(9)  // Cipher trace
 # DBG_BLOB   BIT32(10) // BLOB trace
+# DBG_DMAOBJ BIT32(11) // DMA Object Trace
 CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0

core/arch/arm/plat-ls/crypto_conf.mk
@@ -18,6 +18,7 @@
 # DBG_HASH   BIT32(7)  // Hash trace
 # DBG_RSA    BIT32(8)  // RSA trace
 # DBG_CIPHER BIT32(9)  // Cipher trace
+# DBG_DMAOBJ BIT32(11) // DMA Object Trace
 CFG_DBG_CAAM_TRACE ?= BIT32(1)
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0

core/drivers/crypto/caam/caam_desc.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2019 NXP
+ * Copyright 2019, 2021 NXP
  *
  * Brief   Descriptor construction functions.
  */
@@ -57,7 +57,11 @@ void caam_desc_add_ptr(uint32_t *desc, paddr_t ptr)
 #ifdef CFG_CAAM_64BIT
 	struct ptr_addr *ptr_addr = (struct ptr_addr *)(uintptr_t)last;
 
+#ifdef CFG_ARM64_core
 	caam_write_val32(&ptr_addr->high, ptr >> 32);
+#else
+	caam_write_val32(&ptr_addr->high, 0);
+#endif /* CFG_ARM64_core */
 	caam_write_val32(&ptr_addr->low, ptr);
 	inc++;
 #else
@@ -80,7 +84,8 @@ void caam_desc_push(struct caam_inring_entry *in_entry, paddr_t paddr)
 
 paddr_t caam_desc_pop(struct caam_outring_entry *out_entry)
 {
-	const uint32_t *a32 = (const uint32_t *)(&out_entry->desc);
+	const uintptr_t v_desc = (uintptr_t)&out_entry->desc;
+	const uint32_t *a32 = (const uint32_t *)v_desc;
 
 #ifdef CFG_CAAM_BIG_ENDIAN
 	return SHIFT_U64(get_be32(&a32[0]), 32) | get_be32(&a32[1]);

core/drivers/crypto/caam/include/caam_common.h
@@ -1,9 +1,8 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2019, 2021 NXP
  *
- * Brief   CAAM driver common include file.
- *         Definition of the internal driver status codes.
+ * CAAM driver common include file.
  */
 
 #ifndef __CAAM_COMMON_H__
@@ -12,7 +11,7 @@
 #include <caam_desc_helper.h>
 #include <caam_status.h>
 #include <caam_trace.h>
-#include <types_ext.h>
+#include <caam_types.h>
 
 /*
  * Definition of the number of CAAM Jobs to manage in JR queues
@@ -42,71 +41,7 @@ enum caam_jr_owner {
 	JROWN_ARM_NS = 0x4,                  /* Non-Secure ARM */
 	JROWN_ARM_S = JROWNER_SECURE | 0x4,  /* Secure ARM */
 };
-#endif
 
-/*
- * Definition of a CAAM buffer type
- */
-struct caambuf {
-	uint8_t *data;   /* Data buffer */
-	paddr_t paddr;   /* Physical address of the buffer */
-	size_t length;   /* Number of bytes in the data buffer */
-	uint8_t nocache; /* =1 if buffer is not cacheable, 0 otherwise */
-};
-
-/*
- * Definition of a CAAM Block buffer. Buffer used to store
- * user source data to build a full algorithm block buffer
- */
-struct caamblock {
-	struct caambuf buf; /* Data buffer */
-	size_t filled;      /* Current length filled in the buffer */
-	size_t max;         /* Maximum size of the block */
-};
-
-/*
- * Definition of key size
- */
-struct caamdefkey {
-	uint8_t min; /* Minimum size */
-	uint8_t max; /* Maximum size */
-	uint8_t mod; /* Key modulus */
-};
-
-/*
- * Scatter/Gather Table type for input and output data
- */
-struct caamsgt {
-#if defined(CFG_CAAM_64BIT) && defined(CFG_CAAM_LITTLE_ENDIAN)
-	/* Word 0 */
-	uint32_t ptr_ls;   /* Address pointer (LS 32 bits) */
-
-	/* Word 1 */
-	uint32_t ptr_ms;   /* Address pointer (MS 8 LSBs) */
-#else
-	/* Word 0 */
-	uint32_t ptr_ms;   /* Address pointer (MS 8 LSBs) */
-
-	/* Word 1 */
-	uint32_t ptr_ls;   /* Address pointer (LS 32 bits) */
-#endif
-
-	/* Word 2 */
-	uint32_t len_f_e;  /* Length 30bits + 1bit Final + 1bit Extension) */
-
-	/* Word 3 */
-	uint32_t offset;   /* Offset in memory buffer (13 LSBs) */
-};
-
-/*
- * Data buffer encoded in SGT format
- */
-struct caamsgtbuf {
-	struct caamsgt *sgt; /* SGT Array */
-	struct caambuf *buf; /* Buffer Array */
-	unsigned int number; /* Number of SGT/Buf */
-	size_t length;       /* Total length of the data encoded */
-	bool sgt_type;       /* Define the data format */
-};
+#endif /* CFG_MX7ULP */
 
 #endif /* __CAAM_COMMON_H__ */

core/drivers/crypto/caam/include/caam_io.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2019 NXP
+ * Copyright 2019, 2021 NXP
  *
  * Brief   Specific Macro used to read/write value with a specific
  *         format (BE/LE, 32/64 bits) to be updated for future platform
@@ -28,6 +28,9 @@
 /* Little Endian 32 bits Value access */
 #define caam_read_val32(a)	get_le32(a)
 #define caam_write_val32(a, v)	put_le32(a, v)
+
+#define caam_read_val64(a)     get_le64(a)
+#define caam_write_val64(a, v) put_le64(a, v)
 #endif
 
 #endif /* __CAAM_IO_H__ */

core/drivers/crypto/caam/include/caam_jr_status.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2020 NXP
+ * Copyright 2018-2021 NXP
  *
  * Brief   CAAM Job Ring Status definition header.
  */
@@ -26,6 +26,7 @@
 #define JRSTA_CCB_CHAID_RNG       SHIFT_U32(0x5, 4)
 #define JRSTA_CCB_ERRID_HW        SHIFT_U32(0xB, 0)
 #define JRSTA_DECO_ERRID_FORMAT   SHIFT_U32(0x88, 0)
+#define JRSTA_DECO_INV_SIGNATURE  SHIFT_U32(0x86, 0)
 
 /* Return the Halt User status else 0 if not a Jump Halt User */
 #define JRSTA_GET_HALT_USER(status)                                            \

commit 7c14a1f09e5bd64922311188a6fd1d3a323c2def

drivers: caam: fix DMA object when only output reallocated
Use case:
 - cipher block to encrypt/decrypt is more than 1 Kbytes (e.g. 1232
bytes)
 - input data are accessible from CAAM (no reallocation)
 - output data is not accessible from CAAM (reallocation of DMA buffer).

In case of cipher operation, the input and output CAAM SGT/Buffer
are built in same time through function caam_dmaobj_sgtbuf_inout_build()
to ensure that both SGT/Buffer do the same cipher block size.
Function caam_dmaobj_sgtbuf_inout_build() calls the function
caam_dmaobj_sgtbuf_build():
 - first to build the input data SGT/Buffer. Length returned is
whole cipher buffer size (i.e. 1232 bytes).
 - secondly to build the output data SGT/Buffer. Length return is
whole cipher buffer size (i.e. 1232 bytes) whereas it must be 1024 bytes
because output data must use the reallocation DMA buffer (max 1KBytes).

Fix consist in returning the SGT/Buffer length effectively mapped and
not the maximum length that is the input data SGT/Buffer length.

Consequence of this fix, AES CMAC update loop has to be fixed.

Fixes: 38923d487567 ("drivers: caam: implement CAAM DMA Object")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/caam/cipher/caam_cipher_mac.c
@@ -485,6 +485,16 @@ static TEE_Result do_update_cmac(struct drvcrypt_cipher_update *dupdate)
 
 	for (offset = 0; size_todo;
 	     offset += size_done, size_todo -= size_done) {
+		/*
+		 * At least one block is to be done.
+		 * At first iteration, we can have less than one block
+		 * data available from previous update operation which
+		 * was not block modulus.
+		 * Remove the previous saved data (blockbuf) from the data to
+		 * take from input data.
+		 * Next iteration, blockbuf will be empty.
+		 */
+		size_todo -= ctx->blockbuf.filled;
 		size_done = size_todo;
 
 		if (size_inmade) {
@@ -492,26 +502,16 @@ static TEE_Result do_update_cmac(struct drvcrypt_cipher_update *dupdate)
 						       ctx->alg->size_block);
 			if (ret)
 				goto end_cmac;
-		}
 
-		CIPHER_TRACE("Do input %zu bytes, offset %zu", size_done,
-			     offset);
+			CIPHER_TRACE("Do input %zu bytes, offset %zu",
+				     size_done, offset);
 
-		/*
-		 * Need to re-adjust the length of the data if the
-		 * posted data block is not empty and the SGT/Buffer
-		 * is part of the full input data to do.
-		 */
-		if (ctx->blockbuf.filled && size_done < size_todo) {
-			size_done -= ctx->blockbuf.filled;
-			size_todo -= ctx->blockbuf.filled;
-			src.sgtbuf.length = size_done;
-		}
-
-		if (size_inmade)
 			ret = run_cmac_desc(ctx, &src, NULL, false);
-		else
+		} else {
+			CIPHER_TRACE("Do saved blockbuf %zu bytes (done = %zu)",
+				     ctx->blockbuf.filled, size_done);
 			ret = run_cmac_desc(ctx, NULL, NULL, false);
+		}
 
 		if (ret)
 			goto end_cmac;

core/drivers/crypto/caam/include/caam_utils_dmaobj.h
@@ -180,9 +180,7 @@ TEE_Result caam_dmaobj_prepare(struct caamdmaobj *input,
 /*
  * Build the CAAM DMA Object's sgtbuf object. Try to build a sgtbuf of
  * maximum @length starting at @offset.
- * Return the @length mapped in the sgtbuf object. If a new data buffer
- * must be reallocated, allocated it to the maximum possible round to @align
- * value.
+ * Return the @length mapped in the sgtbuf object.
  *
  * @obj     CAAM DMA object
  * @length  [in/out] maximum length to do/done

core/drivers/crypto/caam/utils/utils_dmaobj.c
@@ -1394,9 +1394,9 @@ TEE_Result caam_dmaobj_sgtbuf_build(struct caamdmaobj *obj, size_t *length,
 		obj->sgtbuf.paddr = obj->sgtbuf.buf->paddr;
 	}
 
-	*length = max_length;
+	*length = obj->sgtbuf.length;
 	ret = TEE_SUCCESS;
 out:
-	DMAOBJ_TRACE("SGTBUF returns 0x%" PRIx32, ret);
+	DMAOBJ_TRACE("SGTBUF (%zu) returns 0x%" PRIx32, *length, ret);
 	return ret;
 }