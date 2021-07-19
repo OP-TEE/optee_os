924b28bd20d926ba0467e7a32b7bf347a6ffe00c
Clément Léger <clement.leger@bootlin.com>
2021-07-19 19:52:47 +0200

    plat-sam: enable PSCI reset support
    Enable PSCI reset support using atmel reset controller driver.
    
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/pm/psci.c
+++ core/arch/arm/plat-sam/pm/psci.c
@@ -3,16 +3,38 @@
  * Copyright (c) 2021, Microchip
  */
 
+#include <drivers/atmel_rstc.h>
+#include <kernel/panic.h>
 #include <sm/psci.h>
 #include <sm/std_smc.h>
 #include <stdint.h>
+#include <trace.h>
+
+void __noreturn psci_system_reset(void)
+{
+	if (!atmel_rstc_available())
+		panic();
+
+	atmel_rstc_reset();
+}
 
 int psci_features(uint32_t psci_fid)
 {
 	switch (psci_fid) {
 	case ARM_SMCCC_VERSION:
+	case PSCI_PSCI_FEATURES:
+	case PSCI_VERSION:
 		return PSCI_RET_SUCCESS;
+	case PSCI_SYSTEM_RESET:
+		if (atmel_rstc_available())
+			return PSCI_RET_SUCCESS;
+		return PSCI_RET_NOT_SUPPORTED;
 	default:
 		return PSCI_RET_NOT_SUPPORTED;
 	}
+}
+
+uint32_t psci_version(void)
+{
+	return PSCI_VERSION_1_0;
 }
