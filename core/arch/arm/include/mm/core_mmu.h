OP-TEE/optee_os

commit ff01e2452169e6c72d309067ce9f9ac84d88d1e1

mm: split mobj_tee_ram onto rw/rx parts
Now mobj_tee_ram memory abstraction contains both TEE_RAM_RX and TEE_RAM_RW
regions joined together. This patch splits it to mobj_tee_ram_rx and
mobj_tee_ram_rw to manage RX/RW memory objects separately.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Anton Rybakov <a.rybakov@omp.ru>

core/mm/mobj.c
@@ -4,6 +4,7 @@
  */
 
 #include <assert.h>
+#include <config.h>
 #include <initcall.h>
 #include <keep.h>
 #include <kernel/linker.h>
@@ -24,7 +25,8 @@
 #include <util.h>
 
 struct mobj *mobj_sec_ddr;
-struct mobj *mobj_tee_ram;
+struct mobj *mobj_tee_ram_rx;
+struct mobj *mobj_tee_ram_rw;
 
 /*
  * mobj_phys implementation
@@ -133,34 +135,16 @@ static struct mobj_phys *to_mobj_phys(struct mobj *mobj)
 	return container_of(mobj, struct mobj_phys, mobj);
 }
 
-struct mobj *mobj_phys_alloc(paddr_t pa, size_t size, uint32_t cattr,
-			     enum buf_is_attr battr)
+static struct mobj *mobj_phys_init(paddr_t pa, size_t size, uint32_t cattr,
+				   enum buf_is_attr battr,
+				   enum teecore_memtypes area_type)
 {
-	struct mobj_phys *moph;
-	enum teecore_memtypes area_type;
-	void *va;
+	void *va = NULL;
+	struct mobj_phys *moph = NULL;
 
 	if ((pa & CORE_MMU_USER_PARAM_MASK) ||
 	    (size & CORE_MMU_USER_PARAM_MASK)) {
 		DMSG("Expect %#x alignment", CORE_MMU_USER_PARAM_SIZE);
-		return NULL;
-	}
-
-	switch (battr) {
-	case CORE_MEM_TEE_RAM:
-		area_type = MEM_AREA_TEE_RAM_RW_DATA;
-		break;
-	case CORE_MEM_TA_RAM:
-		area_type = MEM_AREA_TA_RAM;
-		break;
-	case CORE_MEM_NSEC_SHM:
-		area_type = MEM_AREA_NSEC_SHM;
-		break;
-	case CORE_MEM_SDP_MEM:
-		area_type = MEM_AREA_SDP_MEM;
-		break;
-	default:
-		DMSG("can't allocate with specified attribute");
 		return NULL;
 	}
 
@@ -182,6 +166,32 @@ struct mobj *mobj_phys_alloc(paddr_t pa, size_t size, uint32_t cattr,
 	moph->va = (vaddr_t)va;
 
 	return &moph->mobj;
+}
+
+struct mobj *mobj_phys_alloc(paddr_t pa, size_t size, uint32_t cattr,
+			     enum buf_is_attr battr)
+{
+	enum teecore_memtypes area_type;
+
+	switch (battr) {
+	case CORE_MEM_TEE_RAM:
+		area_type = MEM_AREA_TEE_RAM_RW_DATA;
+		break;
+	case CORE_MEM_TA_RAM:
+		area_type = MEM_AREA_TA_RAM;
+		break;
+	case CORE_MEM_NSEC_SHM:
+		area_type = MEM_AREA_NSEC_SHM;
+		break;
+	case CORE_MEM_SDP_MEM:
+		area_type = MEM_AREA_SDP_MEM;
+		break;
+	default:
+		DMSG("can't allocate with specified attribute");
+		return NULL;
+	}
+
+	return mobj_phys_init(pa, size, cattr, battr, area_type);
 }
 
 /*
@@ -674,13 +684,36 @@ static TEE_Result mobj_init(void)
 	if (!mobj_sec_ddr)
 		panic("Failed to register secure ta ram");
 
-	mobj_tee_ram = mobj_phys_alloc(TEE_RAM_START,
-				       VCORE_UNPG_RW_PA + VCORE_UNPG_RW_SZ -
-						TEE_RAM_START,
-				       TEE_MATTR_CACHE_CACHED,
-				       CORE_MEM_TEE_RAM);
-	if (!mobj_tee_ram)
-		panic("Failed to register tee ram");
+	if (IS_ENABLED(CFG_CORE_RWDATA_NOEXEC)) {
+		mobj_tee_ram_rx = mobj_phys_init(TEE_RAM_START,
+						 VCORE_UNPG_RX_SZ,
+						 TEE_MATTR_CACHE_CACHED,
+						 CORE_MEM_TEE_RAM,
+						 MEM_AREA_TEE_RAM_RX);
+		if (!mobj_tee_ram_rx)
+			panic("Failed to register tee ram rx");
+
+		mobj_tee_ram_rw = mobj_phys_init(TEE_RAM_START +
+						 VCORE_UNPG_RX_SZ,
+						 VCORE_UNPG_RW_SZ,
+						 TEE_MATTR_CACHE_CACHED,
+						 CORE_MEM_TEE_RAM,
+						 MEM_AREA_TEE_RAM_RW_DATA);
+		if (!mobj_tee_ram_rw)
+			panic("Failed to register tee ram rw");
+	} else {
+		mobj_tee_ram_rw = mobj_phys_init(TEE_RAM_START,
+						 VCORE_UNPG_RW_PA +
+						 VCORE_UNPG_RW_SZ -
+						 TEE_RAM_START,
+						 TEE_MATTR_CACHE_CACHED,
+						 CORE_MEM_TEE_RAM,
+						 MEM_AREA_TEE_RAM_RW_DATA);
+		if (!mobj_tee_ram_rw)
+			panic("Failed to register tee ram");
+
+		mobj_tee_ram_rx = mobj_tee_ram_rw;
+	}
 
 	return TEE_SUCCESS;
 }

core/arch/arm/kernel/thread.c
@@ -1506,8 +1506,8 @@ void thread_get_user_kcode(struct mobj **mobj, size_t *offset,
 			   vaddr_t *va, size_t *sz)
 {
 	core_mmu_get_user_va_range(va, NULL);
-	*mobj = mobj_tee_ram;
-	*offset = thread_user_kcode_va - VCORE_START_VA;
+	*mobj = mobj_tee_ram_rx;
+	*offset = thread_user_kcode_va - (vaddr_t)mobj_get_va(*mobj, 0);
 	*sz = thread_user_kcode_size;
 }
 #endif
@@ -1521,8 +1521,9 @@ void thread_get_user_kdata(struct mobj **mobj, size_t *offset,
 
 	core_mmu_get_user_va_range(&v, NULL);
 	*va = v + thread_user_kcode_size;
-	*mobj = mobj_tee_ram;
-	*offset = (vaddr_t)thread_user_kdata_page - VCORE_START_VA;
+	*mobj = mobj_tee_ram_rw;
+	*offset = (vaddr_t)thread_user_kdata_page -
+		  (vaddr_t)mobj_get_va(*mobj, 0);
 	*sz = sizeof(thread_user_kdata_page);
 }
 #endif

core/include/mm/mobj.h
@@ -40,7 +40,8 @@ struct mobj_ops {
 
 extern struct mobj mobj_virt;
 extern struct mobj *mobj_sec_ddr;
-extern struct mobj *mobj_tee_ram;
+extern struct mobj *mobj_tee_ram_rx;
+extern struct mobj *mobj_tee_ram_rw;
 
 static inline void *mobj_get_va(struct mobj *mobj, size_t offset)
 {

commit 99f09fd56060569b04eb3c10abc1218de0291662

mm: fix mobj split by adding core_mmu_find_mapping_exclusive() helper
Fixes: ff01e2452169 ("mm: split mobj_tee_ram onto rw/rx parts")

This fixes mobj splitting onto RX/RW parts. Now split can be done
incorrectly if RX and RW regions doesn`t mapped contiguosly. Added helper
core_mmu_find_mapping_exclusive() allows to find unique mapping for
specified type and length independently of their order, so then RX/RW
regions for mobjects should be determined correctly.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Anton Rybakov <a.rybakov@omp.ru>

core/arch/arm/include/mm/core_mmu.h
@@ -631,6 +631,17 @@ void *core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr,
 			   size_t len);
 
 /*
+ * core_mmu_find_mapping_exclusive() - Find mapping of specified type and
+ *				       length. If more than one mapping of
+ *				       specified type is present, NULL will be
+ *				       returned.
+ * @type:	memory type
+ * @len:	length in bytes
+ */
+struct tee_mmap_region *
+core_mmu_find_mapping_exclusive(enum teecore_memtypes type, size_t len);
+
+/*
  * tlbi_mva_range() - Invalidate TLB for virtual address range
  * @va:		start virtual address, must be a multiple of @granule
  * @len:	length in bytes of range, must be a multiple of @granule

core/arch/arm/mm/core_mmu.c
@@ -1936,6 +1936,31 @@ TEE_Result core_mmu_remove_mapping(enum teecore_memtypes type, void *addr,
 	return TEE_SUCCESS;
 }
 
+struct tee_mmap_region *
+core_mmu_find_mapping_exclusive(enum teecore_memtypes type, size_t len)
+{
+	struct tee_mmap_region *map = NULL;
+	struct tee_mmap_region *map_found = NULL;
+
+	if (!len)
+		return NULL;
+
+	for (map = get_memory_map(); !core_mmap_is_end_of_table(map); map++) {
+		if (map->type != type)
+			continue;
+
+		if (map_found)
+			return NULL;
+
+		map_found = map;
+	}
+
+	if (!map_found || map_found->size < len)
+		return NULL;
+
+	return map_found;
+}
+
 void *core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 {
 	struct core_mmu_table_info tbl_info;

core/mm/mobj.c
@@ -141,6 +141,7 @@ static struct mobj *mobj_phys_init(paddr_t pa, size_t size, uint32_t cattr,
 {
 	void *va = NULL;
 	struct mobj_phys *moph = NULL;
+	struct tee_mmap_region *map = NULL;
 
 	if ((pa & CORE_MMU_USER_PARAM_MASK) ||
 	    (size & CORE_MMU_USER_PARAM_MASK)) {
@@ -148,8 +149,18 @@ static struct mobj *mobj_phys_init(paddr_t pa, size_t size, uint32_t cattr,
 		return NULL;
 	}
 
+	if (pa) {
+		va = phys_to_virt(pa, area_type);
+	} else {
+		map = core_mmu_find_mapping_exclusive(area_type, size);
+		if (!map)
+			return NULL;
+
+		pa = map->pa;
+		va = (void *)map->va;
+	}
+
 	/* Only SDP memory may not have a virtual address */
-	va = phys_to_virt(pa, area_type);
 	if (!va && battr != CORE_MEM_SDP_MEM)
 		return NULL;
 
@@ -685,7 +696,7 @@ static TEE_Result mobj_init(void)
 		panic("Failed to register secure ta ram");
 
 	if (IS_ENABLED(CFG_CORE_RWDATA_NOEXEC)) {
-		mobj_tee_ram_rx = mobj_phys_init(TEE_RAM_START,
+		mobj_tee_ram_rx = mobj_phys_init(0,
 						 VCORE_UNPG_RX_SZ,
 						 TEE_MATTR_CACHE_CACHED,
 						 CORE_MEM_TEE_RAM,
@@ -693,8 +704,7 @@ static TEE_Result mobj_init(void)
 		if (!mobj_tee_ram_rx)
 			panic("Failed to register tee ram rx");
 
-		mobj_tee_ram_rw = mobj_phys_init(TEE_RAM_START +
-						 VCORE_UNPG_RX_SZ,
+		mobj_tee_ram_rw = mobj_phys_init(0,
 						 VCORE_UNPG_RW_SZ,
 						 TEE_MATTR_CACHE_CACHED,
 						 CORE_MEM_TEE_RAM,