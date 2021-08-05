3e22121e24d7a5852539566c1fb3ac593ab472a7
Clément Léger <clement.leger@bootlin.com>
2021-08-05 14:58:51 +0200

    plat-sam: allow using sama5d2_xplained instead of sama5d2xult
    sama5d2_xplained is used in various software (U-Boot, Linux, etc). Allow
    using it and deprecate sama5d2xult.
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/conf.mk
+++ core/arch/arm/plat-sam/conf.mk
@@ -1,7 +1,12 @@
 PLATFORM_FLAVOR ?= sama5d27_som1_ek
 
 flavor_dts_file-sama5d2xult = at91-sama5d2_xplained.dts
+flavor_dts_file-sama5d2_xplained = at91-sama5d2_xplained.dts
 flavor_dts_file-sama5d27_som1_ek = at91-sama5d27_som1_ek.dts
+
+ifeq ($(PLATFORM_FLAVOR),sama5d2xult)
+$(warning "sama5d2xult is deprecated, please use sama5d2_xplained")
+endif
 
 ifeq ($(flavor_dts_file-$(PLATFORM_FLAVOR)),)
 $(error Invalid platform flavor $(PLATFORM_FLAVOR))
