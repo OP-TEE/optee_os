06bc8d197a3c61c09212f004168601bafe2b0acc
Etienne Carriere <etienne.carriere@linaro.org>
2021-08-25 15:28:18 +0200

    ta: pkcs11: move default config to ta sub.mk
    Move PKCS#11 TA default configuration settings from mk/config.mk
    to ta/pkcs11/sub.mk.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

--- ta/pkcs11/sub.mk
+++ ta/pkcs11/sub.mk
@@ -1,3 +1,9 @@
+# Enable PKCS#11 TA's C_DigestKey support
+CFG_PKCS11_TA_ALLOW_DIGEST_KEY ?= y
+
+# Enable PKCS#11 TA's TEE Identity based authentication support
+CFG_PKCS11_TA_AUTH_TEE_IDENTITY ?= y
+
 global-incdirs-y += include
 global-incdirs-y += src
 subdirs-y += src
