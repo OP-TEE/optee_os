OP-TEE/optee_os

commit fb279d8b608ec28f6c0a1d362c1a5a9751db5076

ta: pkcs11: Add support for elliptic curve signing & verification
Add support for performing elliptic curve signing & verification
operations for:

- ECDSA with supplied hash value
- Multi stage SHA-1
- Multi stage SHA-224
- Multi stage SHA-256
- Multi stage SHA-384
- Multi stage SHA-512

Specified in:
PKCS #11 Cryptographic Token Interface Current Mechanisms Specification
Version 2.40 Plus Errata 01
2.3 Elliptic Curve

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Ricardo Salveti <ricardo@foundries.io>
Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

ta/pkcs11/src/processing_asymm.c
@@ -1,0 +1,561 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2018-2020, Linaro Limited
+ */
+
+#include <assert.h>
+#include <compiler.h>
+#include <tee_api_defines.h>
+#include <tee_internal_api.h>
+#include <tee_internal_api_extensions.h>
+
+#include "attributes.h"
+#include "pkcs11_helpers.h"
+#include "pkcs11_token.h"
+#include "processing.h"
+#include "serializer.h"
+
+bool processing_is_tee_asymm(uint32_t proc_id)
+{
+	switch (proc_id) {
+	/* EC flavors */
+	case PKCS11_CKM_ECDSA:
+	case PKCS11_CKM_ECDSA_SHA1:
+	case PKCS11_CKM_ECDSA_SHA224:
+	case PKCS11_CKM_ECDSA_SHA256:
+	case PKCS11_CKM_ECDSA_SHA384:
+	case PKCS11_CKM_ECDSA_SHA512:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static enum pkcs11_rc
+pkcs2tee_algorithm(uint32_t *tee_id, uint32_t *tee_hash_id,
+		   enum processing_func function __unused,
+		   struct pkcs11_attribute_head *proc_params,
+		   struct pkcs11_object *obj)
+{
+	static const struct {
+		enum pkcs11_mechanism_id mech_id;
+		uint32_t tee_id;
+		uint32_t tee_hash_id;
+	} pkcs2tee_algo[] = {
+		/* EC flavors (Must find key size from the object) */
+		{ PKCS11_CKM_ECDSA, 1, 0 },
+		{ PKCS11_CKM_ECDSA_SHA1, 1, TEE_ALG_SHA1 },
+		{ PKCS11_CKM_ECDSA_SHA224, 1, TEE_ALG_SHA224 },
+		{ PKCS11_CKM_ECDSA_SHA256, 1, TEE_ALG_SHA256 },
+		{ PKCS11_CKM_ECDSA_SHA384, 1, TEE_ALG_SHA384 },
+		{ PKCS11_CKM_ECDSA_SHA512, 1, TEE_ALG_SHA512 },
+	};
+	size_t n = 0;
+	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
+
+	for (n = 0; n < ARRAY_SIZE(pkcs2tee_algo); n++) {
+		if (pkcs2tee_algo[n].mech_id == proc_params->id) {
+			*tee_id = pkcs2tee_algo[n].tee_id;
+			*tee_hash_id = pkcs2tee_algo[n].tee_hash_id;
+			break;
+		}
+	}
+
+	if (n == ARRAY_SIZE(pkcs2tee_algo))
+		return PKCS11_RV_NOT_IMPLEMENTED;
+
+	switch (proc_params->id) {
+	case PKCS11_CKM_ECDSA:
+	case PKCS11_CKM_ECDSA_SHA1:
+	case PKCS11_CKM_ECDSA_SHA224:
+	case PKCS11_CKM_ECDSA_SHA256:
+	case PKCS11_CKM_ECDSA_SHA384:
+	case PKCS11_CKM_ECDSA_SHA512:
+		rc = pkcs2tee_algo_ecdsa(tee_id, proc_params, obj);
+		break;
+	default:
+		rc = PKCS11_CKR_OK;
+		break;
+	}
+
+	return rc;
+}
+
+static enum pkcs11_rc pkcs2tee_key_type(uint32_t *tee_type,
+					struct pkcs11_object *obj,
+					enum processing_func function)
+{
+	enum pkcs11_class_id class = get_class(obj->attributes);
+	enum pkcs11_key_type type = get_key_type(obj->attributes);
+
+	switch (class) {
+	case PKCS11_CKO_PUBLIC_KEY:
+	case PKCS11_CKO_PRIVATE_KEY:
+		break;
+	default:
+		TEE_Panic(class);
+		break;
+	}
+
+	switch (type) {
+	case PKCS11_CKK_EC:
+		assert(function != PKCS11_FUNCTION_DERIVE);
+
+		if (class == PKCS11_CKO_PRIVATE_KEY)
+			*tee_type = TEE_TYPE_ECDSA_KEYPAIR;
+		else
+			*tee_type = TEE_TYPE_ECDSA_PUBLIC_KEY;
+		break;
+	default:
+		TEE_Panic(type);
+		break;
+	}
+
+	return PKCS11_CKR_OK;
+}
+
+static enum pkcs11_rc
+allocate_tee_operation(struct pkcs11_session *session,
+		       enum processing_func function,
+		       struct pkcs11_attribute_head *params,
+		       struct pkcs11_object *obj)
+{
+	uint32_t size = (uint32_t)get_object_key_bit_size(obj);
+	uint32_t algo = 0;
+	uint32_t hash_algo = 0;
+	uint32_t mode = 0;
+	uint32_t hash_mode = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct active_processing *processing = session->processing;
+
+	assert(processing->tee_op_handle == TEE_HANDLE_NULL);
+	assert(processing->tee_hash_op_handle == TEE_HANDLE_NULL);
+
+	if (pkcs2tee_algorithm(&algo, &hash_algo, function, params, obj))
+		return PKCS11_CKR_FUNCTION_FAILED;
+
+	pkcs2tee_mode(&mode, function);
+
+	if (hash_algo) {
+		pkcs2tee_mode(&hash_mode, PKCS11_FUNCTION_DIGEST);
+
+		res = TEE_AllocateOperation(&processing->tee_hash_op_handle,
+					    hash_algo, hash_mode, 0);
+		if (res) {
+			EMSG("TEE_AllocateOp. failed %#"PRIx32" %#"PRIx32,
+			     hash_algo, hash_mode);
+
+			if (res == TEE_ERROR_NOT_SUPPORTED)
+				return PKCS11_CKR_MECHANISM_INVALID;
+			return tee2pkcs_error(res);
+		}
+		processing->tee_hash_algo = hash_algo;
+	}
+
+	res = TEE_AllocateOperation(&processing->tee_op_handle,
+				    algo, mode, size);
+	if (res)
+		EMSG("TEE_AllocateOp. failed %#"PRIx32" %#"PRIx32" %#"PRIx32,
+		     algo, mode, size);
+
+	if (res == TEE_ERROR_NOT_SUPPORTED)
+		return PKCS11_CKR_MECHANISM_INVALID;
+
+	if (res != TEE_SUCCESS &&
+	    processing->tee_hash_op_handle != TEE_HANDLE_NULL) {
+		TEE_FreeOperation(session->processing->tee_hash_op_handle);
+		processing->tee_hash_op_handle = TEE_HANDLE_NULL;
+		processing->tee_hash_algo = 0;
+	}
+
+	return tee2pkcs_error(res);
+}
+
+static enum pkcs11_rc load_tee_key(struct pkcs11_session *session,
+				   struct pkcs11_object *obj,
+				   enum processing_func function)
+{
+	TEE_Attribute *tee_attrs = NULL;
+	size_t tee_attrs_count = 0;
+	size_t object_size = 0;
+	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	enum pkcs11_class_id __maybe_unused class = get_class(obj->attributes);
+	enum pkcs11_key_type type = get_key_type(obj->attributes);
+
+	assert(class == PKCS11_CKO_PUBLIC_KEY ||
+	       class == PKCS11_CKO_PRIVATE_KEY);
+
+	if (obj->key_handle != TEE_HANDLE_NULL) {
+		switch (type) {
+		case PKCS11_CKK_EC:
+			/* Reuse EC TEE key only if already DSA or DH */
+			switch (obj->key_type) {
+			case TEE_TYPE_ECDSA_PUBLIC_KEY:
+			case TEE_TYPE_ECDSA_KEYPAIR:
+				if (function != PKCS11_FUNCTION_DERIVE)
+					goto key_ready;
+				break;
+			default:
+				assert(0);
+				break;
+			}
+			break;
+		default:
+			assert(0);
+			break;
+		}
+
+		TEE_CloseObject(obj->key_handle);
+		obj->key_handle = TEE_HANDLE_NULL;
+	}
+
+	rc = pkcs2tee_key_type(&obj->key_type, obj, function);
+	if (rc)
+		return rc;
+
+	object_size = get_object_key_bit_size(obj);
+	if (!object_size)
+		return PKCS11_CKR_GENERAL_ERROR;
+
+	switch (type) {
+	case PKCS11_CKK_EC:
+		rc = load_tee_ec_key_attrs(&tee_attrs, &tee_attrs_count, obj);
+		break;
+	default:
+		break;
+	}
+	if (rc)
+		return rc;
+
+	res = TEE_AllocateTransientObject(obj->key_type, object_size,
+					  &obj->key_handle);
+	if (res) {
+		DMSG("TEE_AllocateTransientObject failed, %#"PRIx32, res);
+
+		return tee2pkcs_error(res);
+	}
+
+	res = TEE_PopulateTransientObject(obj->key_handle,
+					  tee_attrs, tee_attrs_count);
+
+	TEE_Free(tee_attrs);
+
+	if (res) {
+		DMSG("TEE_PopulateTransientObject failed, %#"PRIx32, res);
+
+		goto error;
+	}
+
+key_ready:
+	res = TEE_SetOperationKey(session->processing->tee_op_handle,
+				  obj->key_handle);
+	if (res) {
+		DMSG("TEE_SetOperationKey failed, %#"PRIx32, res);
+
+		goto error;
+	}
+
+	return PKCS11_CKR_OK;
+
+error:
+	TEE_FreeTransientObject(obj->key_handle);
+	obj->key_handle = TEE_HANDLE_NULL;
+	return tee2pkcs_error(res);
+}
+
+static enum pkcs11_rc
+init_tee_operation(struct pkcs11_session *session __unused,
+		   struct pkcs11_attribute_head *proc_params __unused)
+{
+	return PKCS11_CKR_OK;
+}
+
+enum pkcs11_rc init_asymm_operation(struct pkcs11_session *session,
+				    enum processing_func function,
+				    struct pkcs11_attribute_head *proc_params,
+				    struct pkcs11_object *obj)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
+
+	assert(processing_is_tee_asymm(proc_params->id));
+
+	rc = allocate_tee_operation(session, function, proc_params, obj);
+	if (rc)
+		return rc;
+
+	rc = load_tee_key(session, obj, function);
+	if (rc)
+		return rc;
+
+	return init_tee_operation(session, proc_params);
+}
+
+/*
+ * step_sym_step - step (update/oneshot/final) on a symmetric crypto operation
+ *
+ * @session - current session
+ * @function - processing function (encrypt, decrypt, sign, ...)
+ * @step - step ID in the processing (oneshot, update, final)
+ * @ptypes - invocation parameter types
+ * @params - invocation parameter references
+ */
+enum pkcs11_rc step_asymm_operation(struct pkcs11_session *session,
+				    enum processing_func function,
+				    enum processing_step step,
+				    uint32_t ptypes, TEE_Param *params)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	void *in_buf = NULL;
+	void *in2_buf = NULL;
+	void *out_buf = NULL;
+	void *hash_buf = NULL;
+	uint32_t in_size = 0;
+	uint32_t in2_size = 0;
+	uint32_t out_size = 0;
+	uint32_t hash_size = 0;
+	TEE_Attribute *tee_attrs = NULL;
+	size_t tee_attrs_count = 0;
+	bool output_data = false;
+	struct active_processing *proc = session->processing;
+	size_t sz = 0;
+
+	if (TEE_PARAM_TYPE_GET(ptypes, 1) == TEE_PARAM_TYPE_MEMREF_INPUT) {
+		in_buf = params[1].memref.buffer;
+		in_size = params[1].memref.size;
+		if (in_size && !in_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+	}
+	if (TEE_PARAM_TYPE_GET(ptypes, 2) == TEE_PARAM_TYPE_MEMREF_INPUT) {
+		in2_buf = params[2].memref.buffer;
+		in2_size = params[2].memref.size;
+		if (in2_size && !in2_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+	}
+	if (TEE_PARAM_TYPE_GET(ptypes, 2) == TEE_PARAM_TYPE_MEMREF_OUTPUT) {
+		out_buf = params[2].memref.buffer;
+		out_size = params[2].memref.size;
+		if (out_size && !out_buf)
+			return PKCS11_CKR_ARGUMENTS_BAD;
+	}
+	if (TEE_PARAM_TYPE_GET(ptypes, 3) != TEE_PARAM_TYPE_NONE)
+		return PKCS11_CKR_ARGUMENTS_BAD;
+
+	switch (step) {
+	case PKCS11_FUNC_STEP_ONESHOT:
+	case PKCS11_FUNC_STEP_UPDATE:
+	case PKCS11_FUNC_STEP_FINAL:
+		break;
+	default:
+		return PKCS11_CKR_GENERAL_ERROR;
+	}
+
+	/*
+	 * Handle multi stage update step for mechas needing hash
+	 * calculation
+	 */
+	if (step == PKCS11_FUNC_STEP_UPDATE) {
+		switch (proc->mecha_type) {
+		case PKCS11_CKM_ECDSA_SHA1:
+		case PKCS11_CKM_ECDSA_SHA224:
+		case PKCS11_CKM_ECDSA_SHA256:
+		case PKCS11_CKM_ECDSA_SHA384:
+		case PKCS11_CKM_ECDSA_SHA512:
+			assert(proc->tee_hash_op_handle != TEE_HANDLE_NULL);
+
+			TEE_DigestUpdate(proc->tee_hash_op_handle, in_buf,
+					 in_size);
+			break;
+		default:
+			/*
+			 * Other mechanism do not expect multi stage
+			 * operation
+			 */
+			rc = PKCS11_CKR_GENERAL_ERROR;
+			break;
+		}
+
+		goto out;
+	}
+
+	/*
+	 * Handle multi stage one shot and final steps for mechas needing hash
+	 * calculation
+	 */
+	switch (proc->mecha_type) {
+	case PKCS11_CKM_ECDSA_SHA1:
+	case PKCS11_CKM_ECDSA_SHA224:
+	case PKCS11_CKM_ECDSA_SHA256:
+	case PKCS11_CKM_ECDSA_SHA384:
+	case PKCS11_CKM_ECDSA_SHA512:
+		assert(proc->tee_hash_op_handle != TEE_HANDLE_NULL);
+
+		hash_size = TEE_ALG_GET_DIGEST_SIZE(proc->tee_hash_algo);
+		hash_buf = TEE_Malloc(hash_size, 0);
+		if (!hash_buf)
+			return PKCS11_CKR_DEVICE_MEMORY;
+
+		res = TEE_DigestDoFinal(proc->tee_hash_op_handle,
+					in_buf, in_size, hash_buf,
+					&hash_size);
+
+		rc = tee2pkcs_error(res);
+		if (rc != PKCS11_CKR_OK)
+			goto out;
+
+		break;
+	default:
+		break;
+	}
+
+	/*
+	 * Finalize either provided hash or calculated hash with signing
+	 * operation
+	 */
+
+	/* First determine amount of bytes for signing operation */
+	switch (proc->mecha_type) {
+	case PKCS11_CKM_ECDSA:
+		sz = ecdsa_get_input_max_byte_size(proc->tee_op_handle);
+		if (!in_size || !sz) {
+			rc = PKCS11_CKR_FUNCTION_FAILED;
+			goto out;
+		}
+
+		/*
+		 * Note 3) Input the entire raw digest. Internally, this will
+		 * be truncated to the appropriate number of bits.
+		 */
+		if (in_size > sz)
+			in_size = sz;
+
+		if (function == PKCS11_FUNCTION_VERIFY && in2_size != 2 * sz) {
+			rc = PKCS11_CKR_SIGNATURE_LEN_RANGE;
+			goto out;
+		}
+		break;
+	case PKCS11_CKM_ECDSA_SHA1:
+	case PKCS11_CKM_ECDSA_SHA224:
+	case PKCS11_CKM_ECDSA_SHA256:
+	case PKCS11_CKM_ECDSA_SHA384:
+	case PKCS11_CKM_ECDSA_SHA512:
+		/* Get key size in bytes */
+		sz = ecdsa_get_input_max_byte_size(proc->tee_op_handle);
+		if (!sz) {
+			rc = PKCS11_CKR_FUNCTION_FAILED;
+			goto out;
+		}
+
+		if (function == PKCS11_FUNCTION_VERIFY &&
+		    in2_size != 2 * sz) {
+			rc = PKCS11_CKR_SIGNATURE_LEN_RANGE;
+			goto out;
+		}
+		break;
+	default:
+		break;
+	}
+
+	/* Next perform actual signing operation */
+	switch (proc->mecha_type) {
+	case PKCS11_CKM_ECDSA:
+		/* For operations using provided input data */
+		switch (function) {
+		case PKCS11_FUNCTION_ENCRYPT:
+			res = TEE_AsymmetricEncrypt(proc->tee_op_handle,
+						    tee_attrs, tee_attrs_count,
+						    in_buf, in_size,
+						    out_buf, &out_size);
+			output_data = true;
+			rc = tee2pkcs_error(res);
+			break;
+
+		case PKCS11_FUNCTION_DECRYPT:
+			res = TEE_AsymmetricDecrypt(proc->tee_op_handle,
+						    tee_attrs, tee_attrs_count,
+						    in_buf, in_size,
+						    out_buf, &out_size);
+			output_data = true;
+			rc = tee2pkcs_error(res);
+			break;
+
+		case PKCS11_FUNCTION_SIGN:
+			res = TEE_AsymmetricSignDigest(proc->tee_op_handle,
+						       tee_attrs,
+						       tee_attrs_count,
+						       in_buf, in_size,
+						       out_buf, &out_size);
+			output_data = true;
+			rc = tee2pkcs_error(res);
+			break;
+
+		case PKCS11_FUNCTION_VERIFY:
+			res = TEE_AsymmetricVerifyDigest(proc->tee_op_handle,
+							 tee_attrs,
+							 tee_attrs_count,
+							 in_buf, in_size,
+							 in2_buf, in2_size);
+			rc = tee2pkcs_error(res);
+			break;
+
+		default:
+			TEE_Panic(function);
+			break;
+		}
+		break;
+	case PKCS11_CKM_ECDSA_SHA1:
+	case PKCS11_CKM_ECDSA_SHA224:
+	case PKCS11_CKM_ECDSA_SHA256:
+	case PKCS11_CKM_ECDSA_SHA384:
+	case PKCS11_CKM_ECDSA_SHA512:
+		/* For operations having hash operation use calculated hash */
+		switch (function) {
+		case PKCS11_FUNCTION_SIGN:
+			res = TEE_AsymmetricSignDigest(proc->tee_op_handle,
+						       tee_attrs,
+						       tee_attrs_count,
+						       hash_buf, hash_size,
+						       out_buf, &out_size);
+			output_data = true;
+			rc = tee2pkcs_error(res);
+			break;
+
+		case PKCS11_FUNCTION_VERIFY:
+			res = TEE_AsymmetricVerifyDigest(proc->tee_op_handle,
+							 tee_attrs,
+							 tee_attrs_count,
+							 hash_buf, hash_size,
+							 in2_buf, in2_size);
+			rc = tee2pkcs_error(res);
+			break;
+
+		default:
+			TEE_Panic(function);
+			break;
+		}
+		break;
+	default:
+		TEE_Panic(proc->mecha_type);
+		break;
+	}
+
+out:
+	if (output_data &&
+	    (rc == PKCS11_CKR_OK || rc == PKCS11_CKR_BUFFER_TOO_SMALL)) {
+		switch (TEE_PARAM_TYPE_GET(ptypes, 2)) {
+		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
+		case TEE_PARAM_TYPE_MEMREF_INOUT:
+			params[2].memref.size = out_size;
+			break;
+		default:
+			rc = PKCS11_CKR_GENERAL_ERROR;
+			break;
+		}
+	}
+
+	TEE_Free(hash_buf);
+	TEE_Free(tee_attrs);
+
+	return rc;
+}

ta/pkcs11/include/pkcs11_ta.h
@@ -1157,6 +1157,12 @@ enum pkcs11_mechanism_id {
 	PKCS11_CKM_SHA512_HMAC			= 0x00271,
 	PKCS11_CKM_GENERIC_SECRET_KEY_GEN	= 0x00350,
 	PKCS11_CKM_EC_KEY_PAIR_GEN		= 0x01040,
+	PKCS11_CKM_ECDSA			= 0x01041,
+	PKCS11_CKM_ECDSA_SHA1			= 0x01042,
+	PKCS11_CKM_ECDSA_SHA224			= 0x01043,
+	PKCS11_CKM_ECDSA_SHA256			= 0x01044,
+	PKCS11_CKM_ECDSA_SHA384			= 0x01045,
+	PKCS11_CKM_ECDSA_SHA512			= 0x01046,
 	PKCS11_CKM_AES_KEY_GEN			= 0x01080,
 	PKCS11_CKM_AES_ECB			= 0x01081,
 	PKCS11_CKM_AES_CBC			= 0x01082,

ta/pkcs11/src/pkcs11_attributes.c
@@ -1463,6 +1463,27 @@ check_parent_attrs_against_processing(enum pkcs11_mechanism_id proc_id,
 		}
 		break;
 
+	case PKCS11_CKM_ECDSA:
+	case PKCS11_CKM_ECDSA_SHA1:
+	case PKCS11_CKM_ECDSA_SHA224:
+	case PKCS11_CKM_ECDSA_SHA256:
+	case PKCS11_CKM_ECDSA_SHA384:
+	case PKCS11_CKM_ECDSA_SHA512:
+		if (key_type != PKCS11_CKK_EC) {
+			EMSG("Invalid key %s for mechanism %s",
+			     id2str_type(key_type, key_class),
+			     id2str_proc(proc_id));
+
+			return PKCS11_CKR_KEY_TYPE_INCONSISTENT;
+		}
+		if (key_class != PKCS11_CKO_PUBLIC_KEY &&
+		    key_class != PKCS11_CKO_PRIVATE_KEY) {
+			EMSG("Invalid key class for mechanism %s",
+			     id2str_proc(proc_id));
+
+			return PKCS11_CKR_KEY_FUNCTION_NOT_PERMITTED;
+		}
+		break;
 	default:
 		DMSG("Invalid processing %#"PRIx32"/%s", proc_id,
 		     id2str_proc(proc_id));

ta/pkcs11/src/pkcs11_helpers.c
@@ -12,6 +12,7 @@
 #include "object.h"
 #include "pkcs11_attributes.h"
 #include "pkcs11_helpers.h"
+#include "processing.h"
 
 static const char __maybe_unused unknown[] = "<unknown-identifier>";
 
@@ -505,7 +506,91 @@ bool pkcs2tee_load_attr(TEE_Attribute *tee_ref, uint32_t tee_id,
 			enum pkcs11_attr_id pkcs11_id)
 {
 	void *a_ptr = NULL;
+	uint8_t *der_ptr = NULL;
 	uint32_t a_size = 0;
+	uint32_t data32 = 0;
+	size_t hsize = 0;
+	size_t qsize = 0;
+
+	switch (tee_id) {
+	case TEE_ATTR_ECC_PUBLIC_VALUE_X:
+	case TEE_ATTR_ECC_PUBLIC_VALUE_Y:
+	case TEE_ATTR_ECC_CURVE:
+		if (get_attribute_ptr(obj->attributes, PKCS11_CKA_EC_PARAMS,
+				      &a_ptr, &a_size) || !a_ptr) {
+			EMSG("Missing EC_PARAMS attribute");
+			return false;
+		}
+
+		if (tee_id == TEE_ATTR_ECC_CURVE) {
+			data32 = ec_params2tee_curve(a_ptr, a_size);
+			TEE_InitValueAttribute(tee_ref, TEE_ATTR_ECC_CURVE,
+					       data32, 0);
+			return true;
+		}
+
+		data32 = (ec_params2tee_keysize(a_ptr, a_size) + 7) / 8;
+
+		if (get_attribute_ptr(obj->attributes, PKCS11_CKA_EC_POINT,
+				      &a_ptr, &a_size)) {
+			/*
+			 * Public X/Y is required for both TEE keypair and
+			 * public key, so abort if EC_POINT is not provided
+			 * during object import.
+			 */
+
+			EMSG("Missing EC_POINT attribute");
+			return false;
+		}
+
+		der_ptr = (uint8_t *)a_ptr;
+
+		if (der_ptr[0] != 0x04) {
+			EMSG("Unsupported DER type");
+			return false;
+		}
+
+		if ((der_ptr[1] & 0x80) == 0) {
+			/* DER short definitive form up to 127 bytes */
+			qsize = der_ptr[1] & 0x7F;
+			hsize = 2 /* der */ + 1 /* point compression */;
+		} else if (der_ptr[1] == 0x81) {
+			/* DER long definitive form up to 255 bytes */
+			qsize = der_ptr[2];
+			hsize = 3 /* der */ + 1 /* point compression */;
+		} else {
+			EMSG("Unsupported DER long form");
+			return false;
+		}
+
+		if (der_ptr[hsize - 1] != 0x04) {
+			EMSG("Unsupported EC_POINT compression");
+			return false;
+		}
+
+		if (a_size != (hsize - 1) + qsize) {
+			EMSG("Invalid EC_POINT attribute");
+			return false;
+		}
+
+		if (a_size != hsize + 2 * data32) {
+			EMSG("Invalid EC_POINT attribute");
+			return false;
+		}
+
+		if (tee_id == TEE_ATTR_ECC_PUBLIC_VALUE_X)
+			TEE_InitRefAttribute(tee_ref, tee_id,
+					     der_ptr + hsize, data32);
+		else
+			TEE_InitRefAttribute(tee_ref, tee_id,
+					     der_ptr + hsize + data32,
+					     data32);
+
+		return true;
+
+	default:
+		break;
+	}
 
 	if (get_attribute_ptr(obj->attributes, pkcs11_id, &a_ptr, &a_size))
 		return false;

ta/pkcs11/src/pkcs11_token.c
@@ -218,6 +218,8 @@ enum pkcs11_rc set_processing_state(struct pkcs11_session *session,
 	/* Boolean are default to false and pointers to NULL */
 	proc->state = state;
 	proc->tee_op_handle = TEE_HANDLE_NULL;
+	proc->tee_hash_algo = 0;
+	proc->tee_hash_op_handle = TEE_HANDLE_NULL;
 
 	if (obj1 && get_bool(obj1->attributes, PKCS11_CKA_ALWAYS_AUTHENTICATE))
 		proc->always_authen = true;

ta/pkcs11/src/pkcs11_token.h
@@ -136,6 +136,9 @@ enum pkcs11_proc_state {
  * @relogged - true once client logged since last operation update
  * @updated - true once an active operation is updated
  * @tee_op_handle - handle on active crypto operation or TEE_HANDLE_NULL
+ * @tee_hash_algo - hash algorithm identifier.
+ * @tee_hash_op_handle - handle on active hashing crypto operation or
+ * TEE_HANDLE_NULL
  * @extra_ctx - context for the active processing
  */
 struct active_processing {
@@ -145,6 +148,8 @@ struct active_processing {
 	bool relogged;
 	bool updated;
 	TEE_OperationHandle tee_op_handle;
+	uint32_t tee_hash_algo;
+	TEE_OperationHandle tee_hash_op_handle;
 	void *extra_ctx;
 };
 

ta/pkcs11/src/processing.c
@@ -104,6 +104,12 @@ void release_active_processing(struct pkcs11_session *session)
 	if (!session->processing)
 		return;
 
+	if (session->processing->tee_hash_op_handle != TEE_HANDLE_NULL) {
+		TEE_FreeOperation(session->processing->tee_hash_op_handle);
+		session->processing->tee_hash_op_handle = TEE_HANDLE_NULL;
+		session->processing->tee_hash_algo = 0;
+	}
+
 	if (session->processing->tee_op_handle != TEE_HANDLE_NULL) {
 		TEE_FreeOperation(session->processing->tee_op_handle);
 		session->processing->tee_op_handle = TEE_HANDLE_NULL;
@@ -117,6 +123,7 @@ void release_active_processing(struct pkcs11_session *session)
 
 size_t get_object_key_bit_size(struct pkcs11_object *obj)
 {
+	void *a_ptr = NULL;
 	uint32_t a_size = 0;
 	struct obj_attrs *attrs = obj->attributes;
 
@@ -133,6 +140,12 @@ size_t get_object_key_bit_size(struct pkcs11_object *obj)
 			return 0;
 
 		return a_size * 8;
+	case PKCS11_CKK_EC:
+		if (get_attribute_ptr(attrs, PKCS11_CKA_EC_PARAMS,
+				      &a_ptr, &a_size) || !a_ptr)
+			return 0;
+
+		return ec_params2tee_keysize(a_ptr, a_size);
 	default:
 		TEE_Panic(0);
 		return 0;
@@ -632,6 +645,8 @@ enum pkcs11_rc entry_processing_init(struct pkcs11_client *client,
 
 	if (processing_is_tee_symm(proc_params->id))
 		rc = init_symm_operation(session, function, proc_params, obj);
+	else if (processing_is_tee_asymm(proc_params->id))
+		rc = init_asymm_operation(session, function, proc_params, obj);
 	else if (processing_is_tee_digest(proc_params->id))
 		rc = init_digest_operation(session, proc_params);
 	else
@@ -726,6 +741,9 @@ enum pkcs11_rc entry_processing_step(struct pkcs11_client *client,
 	if (processing_is_tee_symm(mecha_type))
 		rc = step_symm_operation(session, function, step,
 					 ptypes, params);
+	else if (processing_is_tee_asymm(mecha_type))
+		rc = step_asymm_operation(session, function, step,
+					  ptypes, params);
 	else if (processing_is_tee_digest(mecha_type))
 		rc = step_digest_operation(session, step, obj, ptypes, params);
 	else

ta/pkcs11/src/processing.h
@@ -57,6 +57,19 @@ enum pkcs11_rc tee2pkcs_add_attribute(struct obj_attrs **head,
 				      TEE_ObjectHandle tee_obj,
 				      uint32_t tee_id);
 
+/* Asymmetric key operations util */
+bool processing_is_tee_asymm(uint32_t proc_id);
+
+enum pkcs11_rc init_asymm_operation(struct pkcs11_session *session,
+				    enum processing_func function,
+				    struct pkcs11_attribute_head *proc_params,
+				    struct pkcs11_object *obj);
+
+enum pkcs11_rc step_asymm_operation(struct pkcs11_session *session,
+				    enum processing_func function,
+				    enum processing_step step,
+				    uint32_t ptypes, TEE_Param *params);
+
 /*
  * Symmetric crypto algorithm specific functions
  */
@@ -93,12 +106,22 @@ enum pkcs11_rc step_digest_operation(struct pkcs11_session *session,
 /*
  * Elliptic curve crypto algorithm specific functions
  */
+enum pkcs11_rc load_tee_ec_key_attrs(TEE_Attribute **tee_attrs,
+				     size_t *tee_count,
+				     struct pkcs11_object *obj);
+
 size_t ec_params2tee_keysize(void *attr, size_t size);
 
 uint32_t ec_params2tee_curve(void *attr, size_t size);
 
+enum pkcs11_rc pkcs2tee_algo_ecdsa(uint32_t *tee_id,
+				   struct pkcs11_attribute_head *proc_params,
+				   struct pkcs11_object *obj);
+
 enum pkcs11_rc generate_ec_keys(struct pkcs11_attribute_head *proc_params,
 				struct obj_attrs **pub_head,
 				struct obj_attrs **priv_head);
+
+size_t ecdsa_get_input_max_byte_size(TEE_OperationHandle op);
 
 #endif /*PKCS11_TA_PROCESSING_H*/

commit 8c4e0659e877616773d07a2c68ac2225c46c0b43

ta: pkcs11: fix error code in asymmetric signature update sequence
Correct return code in asymmetric update sequence when digest of
the input data is updated on a multi-stage operation. Prior this
change, the implementation returned CKR_GENERAL_ERROR instead of
CKR_OK because the expected success return value was loaded for
that stage.

Fixes: fb279d8b608e ("ta: pkcs11: Add support for elliptic curve signing & verification")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Ruchika Gupta <ruchika.gupta@linaro.org>

ta/pkcs11/src/processing_asymm.c
@@ -526,6 +526,7 @@ enum pkcs11_rc step_asymm_operation(struct pkcs11_session *session,
 
 			TEE_DigestUpdate(proc->tee_hash_op_handle, in_buf,
 					 in_size);
+			rc = PKCS11_CKR_OK;
 			break;
 		default:
 			/*