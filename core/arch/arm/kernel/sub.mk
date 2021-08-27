4a7ea1e993f8b7b0f343c645d5fac7a4a7df80db
Jerome Forissier <jerome@forissier.org>
2021-08-27 11:02:47 +0200

    core: arm: implement cpu_idle()
    Implement cpu_idle() on arm and arm64, based on wfi(). With this patch,
    a panicked CPU no longer burns cycles needlessly.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/kernel/sub.mk
+++ core/arch/arm/kernel/sub.mk
@@ -3,6 +3,7 @@ srcs-y += tee_time.c
 srcs-y += rpc_io_i2c.c
 srcs-y += otp_stubs.c
 srcs-y += delay.c
+srcs-y += idle.c
 
 srcs-$(CFG_SECURE_TIME_SOURCE_CNTPCT) += tee_time_arm_cntpct.c
 srcs-$(CFG_SECURE_TIME_SOURCE_REE) += tee_time_ree.c
