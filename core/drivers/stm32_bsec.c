cfada897a5c8585742c1c5becfaef42879722877
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2021-09-06 10:11:07 -0500

    plat-stm32mp1: Don't call get_embedded_dt() without CFG_EMBED_DT
    Several pieces of stm32mp1 code call get_embedded_dt(), then use the
    resulting pointer without checks, or initiate a panic if it is NULL.
    Thus hitting this code results in a non-working binary. For example:
    
        "PLATFORM=stm32mp1 CFG_DT=y"
    
    The get_embedded_dt() uses were #ifdef'd out based on CFG_DT. However,
    as shown, this is problematic, as the calls assumed a valid fdt must
    be returned. A non-NULL fdt can be guaranteed with CFG_EMBED_DT, so
    use this as the basis for the #ifdefs.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/stm32_bsec.c
+++ core/drivers/stm32_bsec.c
@@ -546,7 +546,7 @@ bool stm32_bsec_nsec_can_access_otp(uint32_t otp_id)
 	       nsec_access_granted(otp_id - otp_upper_base());
 }
 
-#ifdef CFG_DT
+#ifdef CFG_EMBED_DTB
 static void enable_nsec_access(unsigned int otp_id)
 {
 	unsigned int idx = (otp_id - otp_upper_base()) / BITS_PER_WORD;
@@ -647,7 +647,7 @@ static void initialize_bsec_from_dt(void)
 static void initialize_bsec_from_dt(void)
 {
 }
-#endif /*CFG_DT*/
+#endif /*CFG_EMBED_DTB*/
 
 static TEE_Result initialize_bsec(void)
 {
