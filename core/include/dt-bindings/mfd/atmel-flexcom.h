dd7e18453419c23bf622415a9ba8c060d56d1a23
Clément Léger <clement.leger@bootlin.com>
2021-09-16 17:53:02 +0200

    dts: at91: add device trees for sama5d2_xplained
    Import device-tree from linux for sama5d2 and relicense them with dual
    GPL/BSD 3-Clause
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Nicolas Ferre <nicolas.ferre@microchip.com>
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

---
+++ core/include/dt-bindings/mfd/atmel-flexcom.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 or BSD-3-Clause */
+/*
+ * This header provides macros for Atmel Flexcom DT bindings.
+ *
+ * Copyright (C) 2015 Cyrille Pitchen <cyrille.pitchen@atmel.com>
+ */
+
+#ifndef __DT_BINDINGS_MFD_ATMEL_FLEXCOM_H__
+#define __DT_BINDINGS_MFD_ATMEL_FLEXCOM_H__
+
+#define ATMEL_FLEXCOM_MODE_USART	1
+#define ATMEL_FLEXCOM_MODE_SPI		2
+#define ATMEL_FLEXCOM_MODE_TWI		3
+
+#endif /* __DT_BINDINGS_MFD_ATMEL_FLEXCOM_H__ */
