OP-TEE/optee_os

commit 247f081a95625a7abd1565084c995b3d2c3d70eb

core: imx: re-work SoC version detection
Move DIGPROG_OFFSET to appropriate registers header files.
Store cpu type and SoC revision in global variables instead of
reading ANATOP registers each time.
imx_digproc() now gets also the minor SoC revision.

Signed-off-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-imx/imx-common.c
@@ -13,34 +13,47 @@
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
-static uint32_t imx_digproc(void)
+static int imx_cpu_type = -1;
+static int imx_soc_revision = -1;
+
+#define CPU_TYPE(reg)		((reg & 0x00FF0000) >> 16)
+#define SOC_REV_MAJOR(reg)	(((reg & 0x0000FF00) >> 8) + 1)
+#define SOC_REV_MINOR(reg)	(reg & 0x0000000F)
+
+static void imx_digproc(void)
 {
-	static uint32_t reg;
-	vaddr_t anatop_addr;
+	uint32_t digprog = 0;
+	vaddr_t __maybe_unused anatop_addr = 0;
 
-	if (!reg) {
-		anatop_addr = core_mmu_get_va(ANATOP_BASE, MEM_AREA_IO_SEC);
+	anatop_addr = core_mmu_get_va(ANATOP_BASE, MEM_AREA_IO_SEC);
 
-#if defined(CFG_MX7)
-		reg = io_read32(anatop_addr + OFFSET_DIGPROG_IMX7D);
-#elif defined(CFG_MX6SL)
-		reg = io_read32(anatop_addr + OFFSET_DIGPROG_IMX6SL);
-#else
-		reg = io_read32(anatop_addr + OFFSET_DIGPROG);
-#endif
-	}
+	if (!anatop_addr)
+		return;
 
-	return reg;
+	digprog = io_read32(anatop_addr + DIGPROG_OFFSET);
+
+	/* Set the CPU type */
+	imx_cpu_type = CPU_TYPE(digprog);
+
+	/* Set the SOC revision: = (Major + 1)[11:4] | (Minor[3:0]) */
+	imx_soc_revision =
+		(SOC_REV_MAJOR(digprog) << 4) | SOC_REV_MINOR(digprog);
 }
 
 static uint32_t imx_soc_rev_major(void)
 {
-	return ((imx_digproc() & 0xff00) >> 8) + 1;
+	if (imx_soc_revision < 0)
+		imx_digproc();
+
+	return imx_soc_revision >> 4;
 }
 
-uint32_t imx_soc_type(void)
+static uint32_t imx_soc_type(void)
 {
-	return (imx_digproc() >> 16) & 0xff;
+	if (imx_cpu_type < 0)
+		imx_digproc();
+
+	return imx_cpu_type;
 }
 
 bool soc_is_imx6sl(void)

core/arch/arm/plat-imx/imx.h
@@ -32,7 +32,7 @@ bool soc_is_imx6sdl(void);
 bool soc_is_imx6dq(void);
 bool soc_is_imx6dqp(void);
 bool soc_is_imx7ds(void);
-uint32_t imx_soc_type(void);
+uint16_t soc_revision(void);
 void imx_gpcv2_set_core1_pdn_by_software(void);
 void imx_gpcv2_set_core1_pup_by_software(void);
 void imx_gpcv2_set_core_pgc(bool enable, uint32_t offset);

core/arch/arm/plat-imx/registers/imx6.h
@@ -129,4 +129,10 @@
 #define IOMUXC_GPR10_OCRAM_TZ_ADDR_LOCK_OFFSET_6UL	(27)
 #define IOMUXC_GPR10_OCRAM_TZ_ADDR_LOCK_MASK_6UL	GENMASK_32(31, 27)
 
+#ifdef CFG_MX6SL
+#define DIGPROG_OFFSET	0x280
+#else
+#define DIGPROG_OFFSET	0x260
+#endif
+
 #endif /* __IMX6_H__ */

core/arch/arm/plat-imx/registers/imx7.h
@@ -63,4 +63,6 @@
 #define IOMUXC_GPR11_OCRAM_S_TZ_EN_LOCK_MASK		GENMASK_32(26, 26)
 #define IOMUXC_GPR11_OCRAM_S_TZ_ADDR_LOCK_OFFSET	GENMASK_32(29, 27)
 
+#define DIGPROG_OFFSET	0x800
+
 #endif /* __IMX7_H__ */

core/arch/arm/plat-imx/registers/imx8m.h
@@ -16,4 +16,11 @@
 #define CAAM_BASE	0x30900000
 #define ANATOP_BASE	0x30360000
 
+#ifdef CFG_IMX8MQ
+#define DIGPROG_OFFSET	0x06c
+#endif
+#ifdef CFG_IMX8MM
+#define DIGPROG_OFFSET	0x800
+#endif
+
 #endif /* __IMX8M_H__ */

core/arch/arm/plat-imx/imx-regs.h
@@ -66,10 +66,6 @@
 
 #define WCR_OFF				0
 
-#define OFFSET_DIGPROG			0x260
-#define OFFSET_DIGPROG_IMX6SL		0x280
-#define OFFSET_DIGPROG_IMX7D		0x800
-
 /* GPC V2 */
 #define GPC_PGC_C1			0x840
 #define GPC_PGC_C1_PUPSCR		0x844

core/arch/arm/plat-imx/sub.mk
@@ -1,7 +1,7 @@
 global-incdirs-y += .
-srcs-y += main.c
+srcs-y += main.c imx-common.c
 
-srcs-$(CFG_MX6)$(CFG_MX7) += imx_src.c mmdc.c imx-common.c
+srcs-$(CFG_MX6)$(CFG_MX7) += imx_src.c mmdc.c
 
 srcs-$(CFG_PL310) += imx_pl310.c
 ifeq ($(CFG_PSCI_ARM32),y)

commit ddca7618aae7242d745fe4101610246f40160503

core: imx: rework digprog driver
Rework digprog read value for all i.MX platforms.
Add the distinction between i.MX8MQ B0 and B1 platforms.
Add soc_is_*() functions for all i.MX8mscale platforms.

Fixes: 247f081a95 ("core: imx: re-work SoC version detection")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-imx/imx-common.c
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2017-2019 NXP
+ * Copyright 2017-2019, 2021 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
 
+#include <config.h>
 #include <console.h>
 #include <io.h>
 #include <imx.h>
@@ -13,59 +14,84 @@
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
-static int imx_cpu_type = -1;
-static int imx_soc_revision = -1;
+#define SOC_TYPE(reg)	       (((reg) & (0x00FF0000)) >> 16)
+#define SOC_REV_MAJOR(reg)     (((reg) & (0x0000FF00)) >> 8)
+#define SOC_REV_MINOR(reg)     ((reg) & (0x0000000F))
+#define SOC_REV_MINOR_MX7(reg) ((reg) & (0x000000FF))
 
-#define CPU_TYPE(reg)		((reg & 0x00FF0000) >> 16)
-#define SOC_REV_MAJOR(reg)	(((reg & 0x0000FF00) >> 8) + 1)
-#define SOC_REV_MINOR(reg)	(reg & 0x0000000F)
+static uint32_t imx_digprog;
 
-static void imx_digproc(void)
+#ifdef ANATOP_BASE
+uint32_t imx_get_digprog(void)
 {
-	uint32_t digprog = 0;
-	vaddr_t __maybe_unused anatop_addr = 0;
+	vaddr_t addr = 0;
 
-#if defined(CFG_MX7ULP)
-	digprog = SOC_MX7ULP << 16;
-#elif defined(CFG_MX8QX)
-	digprog = SOC_MX8QX << 16;
-#elif defined(CFG_MX8QM)
-	digprog = SOC_MX8QM << 16;
-#else
-	anatop_addr = core_mmu_get_va(ANATOP_BASE, MEM_AREA_IO_SEC,
-				      DIGPROG_OFFSET + sizeof(uint32_t));
+	if (imx_digprog)
+		return imx_digprog;
 
-	if (!anatop_addr)
-		return;
+	addr = core_mmu_get_va(ANATOP_BASE, MEM_AREA_IO_SEC, 0x1000);
+	if (!addr)
+		return 0;
 
-	digprog = io_read32(anatop_addr + DIGPROG_OFFSET);
-#endif
-	/* Set the CPU type */
-	imx_cpu_type = CPU_TYPE(digprog);
+	imx_digprog = io_read32(addr + DIGPROG_OFFSET);
 
-#ifdef CFG_MX7
-	imx_soc_revision = digprog & 0xFF;
-#else
-	/* Set the SOC revision: = (Major + 1)[11:4] | (Minor[3:0]) */
-	imx_soc_revision =
-		(SOC_REV_MAJOR(digprog) << 4) | SOC_REV_MINOR(digprog);
-#endif
+#ifdef CFG_MX8MQ
+	/*
+	 * On the i.MX8MQ, the minor revision number must be updated to make
+	 * the difference between B0 chip and the newer chips.
+	 */
+	addr = core_mmu_get_va(OCOTP_BASE, MEM_AREA_IO_SEC, OCOTP_SIZE);
+	if (!addr)
+		return 0;
+
+	if (io_read32(addr + OCOTP_SW_INFO_B1) == OCOTP_SW_MAGIC_B1)
+		imx_digprog |= BIT32(0);
+#endif /* CFG_MX8MQ */
+
+	return imx_digprog;
+}
+#else  /* ANATOP_BASE */
+uint32_t imx_get_digprog(void)
+{
+	if (imx_digprog)
+		return imx_digprog;
+
+	if (IS_ENABLED(CFG_MX7ULP))
+		imx_digprog = SOC_MX7ULP << 16;
+	else if (IS_ENABLED(CFG_MX8QX))
+		imx_digprog = SOC_MX8QX << 16;
+	else if (IS_ENABLED(CFG_MX8QM))
+		imx_digprog = SOC_MX8QM << 16;
+
+	return imx_digprog;
+}
+#endif /* ANATOP_BASE */
+
+uint32_t imx_soc_rev_major(void)
+{
+	if (imx_digprog == 0)
+		imx_get_digprog();
+
+	return SOC_REV_MAJOR(imx_digprog);
 }
 
-static uint32_t imx_soc_rev_major(void)
+uint32_t imx_soc_rev_minor(void)
 {
-	if (imx_soc_revision < 0)
-		imx_digproc();
+	if (imx_digprog == 0)
+		imx_get_digprog();
 
-	return imx_soc_revision >> 4;
+	if (IS_ENABLED(CFG_MX7))
+		return SOC_REV_MINOR_MX7(imx_digprog);
+	else
+		return SOC_REV_MINOR(imx_digprog);
 }
 
-static uint32_t imx_soc_type(void)
+uint32_t imx_soc_type(void)
 {
-	if (imx_cpu_type < 0)
-		imx_digproc();
+	if (imx_digprog == 0)
+		imx_get_digprog();
 
-	return imx_cpu_type;
+	return SOC_TYPE(imx_digprog);
 }
 
 bool soc_is_imx6sl(void)
@@ -100,12 +126,12 @@ bool soc_is_imx6sdl(void)
 
 bool soc_is_imx6dq(void)
 {
-	return (imx_soc_type() == SOC_MX6Q) && (imx_soc_rev_major() == 1);
+	return (imx_soc_type() == SOC_MX6Q) && (imx_soc_rev_major() == 0);
 }
 
 bool soc_is_imx6dqp(void)
 {
-	return (imx_soc_type() == SOC_MX6Q) && (imx_soc_rev_major() == 2);
+	return (imx_soc_type() == SOC_MX6Q) && (imx_soc_rev_major() == 1);
 }
 
 bool soc_is_imx6(void)
@@ -125,4 +151,38 @@ bool soc_is_imx7ds(void)
 bool soc_is_imx7ulp(void)
 {
 	return imx_soc_type() == SOC_MX7ULP;
+}
+
+bool soc_is_imx8mq(void)
+{
+	return imx_soc_type() == SOC_MX8M && imx_soc_rev_major() == 0x40;
+}
+
+bool soc_is_imx8mm(void)
+{
+	return imx_soc_type() == SOC_MX8M && imx_soc_rev_major() == 0x41;
+}
+
+bool soc_is_imx8mn(void)
+{
+	return imx_soc_type() == SOC_MX8M && imx_soc_rev_major() == 0x42;
+}
+
+bool soc_is_imx8mp(void)
+{
+	return imx_soc_type() == SOC_MX8M && imx_soc_rev_major() == 0x43;
+}
+
+bool soc_is_imx8m(void)
+{
+	return soc_is_imx8mq() || soc_is_imx8mm() || soc_is_imx8mn() ||
+	       soc_is_imx8mp();
+}
+
+bool soc_is_imx8mq_b0_layer(void)
+{
+	if (soc_is_imx8mq() && imx_soc_rev_minor() == 0x0)
+		return true;
+	else
+		return false;
 }

core/arch/arm/plat-imx/imx.h
@@ -22,6 +22,7 @@
 #define SOC_MX7ULP	0xE1
 #define SOC_MX8QX	0xE2
 #define SOC_MX8QM	0xE3
+#define SOC_MX8M	0x82
 
 #ifndef __ASSEMBLER__
 uint32_t imx_get_src_gpr(int cpu);
@@ -37,7 +38,16 @@ bool soc_is_imx6dq(void);
 bool soc_is_imx6dqp(void);
 bool soc_is_imx7ds(void);
 bool soc_is_imx7ulp(void);
-uint16_t soc_revision(void);
+bool soc_is_imx8m(void);
+bool soc_is_imx8mq(void);
+bool soc_is_imx8mm(void);
+bool soc_is_imx8mn(void);
+bool soc_is_imx8mp(void);
+bool soc_is_imx8mq_b0_layer(void);
+uint32_t imx_soc_type(void);
+uint32_t imx_soc_rev_major(void);
+uint32_t imx_soc_rev_minor(void);
+uint32_t imx_get_digprog(void);
 void imx_gpcv2_set_core1_pdn_by_software(void);
 void imx_gpcv2_set_core1_pup_by_software(void);
 void imx_gpcv2_set_core_pgc(bool enable, uint32_t offset);

core/arch/arm/plat-imx/registers/imx6.h
@@ -79,6 +79,7 @@
 #define IRAM_BASE			0x00900000
 
 #define OCOTP_BASE			0x021BC000
+#define OCOTP_SIZE			0x4000
 
 #define GIC_BASE			0x00A00000
 #define GICD_OFFSET			0x1000

core/arch/arm/plat-imx/registers/imx7.h
@@ -31,9 +31,11 @@
 #define IOMUXC_BASE		0x30330000
 #define IOMUXC_GPR_BASE		0x30340000
 #define OCOTP_BASE		0x30350000
+#define OCOTP_SIZE		0x10000
 #define ANATOP_BASE		0x30360000
 #define SNVS_BASE		0x30370000
 #define CCM_BASE		0x30380000
+#define CCM_SIZE		0x10000
 #define SRC_BASE		0x30390000
 #define GPC_BASE		0x303A0000
 #define CSU_BASE		0x303E0000

core/arch/arm/plat-imx/registers/imx7ulp.h
@@ -39,6 +39,8 @@
 #define PCC3_BASE		0x40b30000
 #define PMC0_BASE		0x410a1000
 #define SIM_BASE		0x410a3000
+#define OCOTP_BASE		0x410A6000
+#define OCOTP_SIZE		0x4000
 
 #define CAAM_BASE		0x40240000
 #define UART4_BASE		0x402d0000

core/arch/arm/plat-imx/registers/imx8m.h
@@ -17,12 +17,17 @@
 #define TZASC_BASE	0x32F80000
 #define CAAM_BASE	0x30900000
 #define CCM_BASE	0x30380000
+#define CCM_SIZE	0x10000
 #define ANATOP_BASE	0x30360000
 #define IOMUXC_BASE	0x30330000
+#define OCOTP_BASE	0x30350000
+#define OCOTP_SIZE	0x10000
 #define SNVS_BASE	0x30370000
 
 #ifdef CFG_MX8MQ
-#define DIGPROG_OFFSET	0x06c
+#define DIGPROG_OFFSET	  0x06c
+#define OCOTP_SW_INFO_B1  0x40
+#define OCOTP_SW_MAGIC_B1 0xFF0055AA
 #endif
 #if defined(CFG_MX8MM) || defined(CFG_MX8MN) || defined(CFG_MX8MP)
 #define DIGPROG_OFFSET	0x800