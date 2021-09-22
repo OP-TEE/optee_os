OP-TEE/optee_os

commit 2d7a8964dfcbe6c3494a0889831818dd8f153f4a

driver: implement CAAM driver
Add the NXP CAAM drivers:
  - Random generator (instantiation and random generation)
  - Hash

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx6.c
@@ -1,0 +1,42 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2019 NXP
+ *
+ * Brief   CAAM Clock functions.
+ */
+#include <caam_hal_clk.h>
+#include <caam_io.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+
+void caam_hal_clk_enable(bool enable)
+{
+	vaddr_t ccm_base = (vaddr_t)phys_to_virt(CCM_BASE, MEM_AREA_IO_SEC);
+	uint32_t reg = 0;
+	uint32_t mask = 0;
+
+	reg = io_caam_read32(ccm_base + CCM_CCGR0);
+
+	mask = BM_CCM_CCGR0_CAAM_WRAPPER_IPG | BM_CCM_CCGR0_CAAM_WRAPPER_ACLK |
+	       BM_CCM_CCGR0_CAAM_SECURE_MEM;
+
+	if (enable)
+		reg |= mask;
+	else
+		reg &= ~mask;
+
+	io_caam_write32(ccm_base + CCM_CCGR0, reg);
+
+	if (!soc_is_imx6ul()) {
+		/* EMI slow clk */
+		reg = io_caam_read32(ccm_base + CCM_CCGR6);
+		mask = BM_CCM_CCGR6_EMI_SLOW;
+
+		if (enable)
+			reg |= mask;
+		else
+			reg &= ~mask;
+
+		io_caam_write32(ccm_base + CCM_CCGR6, reg);
+	}
+}

core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7.c
@@ -1,0 +1,23 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2019 NXP
+ *
+ * Brief   CAAM Clock functions.
+ */
+#include <caam_hal_clk.h>
+#include <caam_io.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+
+void caam_hal_clk_enable(bool enable)
+{
+	vaddr_t ccm_base = (vaddr_t)phys_to_virt(CCM_BASE, MEM_AREA_IO_SEC);
+
+	if (enable) {
+		io_caam_write32(ccm_base + CCM_CCGRx_SET(CCM_CLOCK_DOMAIN_CAAM),
+				CCM_CCGRx_ALWAYS_ON(0));
+	} else {
+		io_caam_write32(ccm_base + CCM_CCGRx_CLR(CCM_CLOCK_DOMAIN_CAAM),
+				CCM_CCGRx_ALWAYS_ON(0));
+	}
+}

core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7ulp.c
@@ -1,0 +1,20 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2019 NXP
+ *
+ * Brief   CAAM Clock functions.
+ */
+#include <caam_hal_clk.h>
+#include <caam_io.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+
+void caam_hal_clk_enable(bool enable)
+{
+	vaddr_t pcc2_base = (vaddr_t)phys_to_virt(PCC2_BASE, MEM_AREA_IO_SEC);
+
+	if (enable)
+		io_caam_write32(pcc2_base + PCC_CAAM, PCC_ENABLE_CLOCK);
+	else
+		io_caam_write32(pcc2_base + PCC_CAAM, PCC_DISABLE_CLOCK);
+}

commit 6240b202df8fb3436ba142e3a94b548bf1a548ef

drivers: caam: Fix hal clock registers read/write
Fix CAAM Clock registers access to use io_read/write() functions and
not io_caam_read/write().

Fixes: 2d7a896 ("driver: implement CAAM driver")
Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx6.c
@@ -5,7 +5,7 @@
  * Brief   CAAM Clock functions.
  */
 #include <caam_hal_clk.h>
-#include <caam_io.h>
+#include <io.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
@@ -16,7 +16,7 @@ void caam_hal_clk_enable(bool enable)
 	uint32_t reg = 0;
 	uint32_t mask = 0;
 
-	reg = io_caam_read32(ccm_base + CCM_CCGR0);
+	reg = io_read32(ccm_base + CCM_CCGR0);
 
 	mask = BM_CCM_CCGR0_CAAM_WRAPPER_IPG | BM_CCM_CCGR0_CAAM_WRAPPER_ACLK |
 	       BM_CCM_CCGR0_CAAM_SECURE_MEM;
@@ -26,11 +26,11 @@ void caam_hal_clk_enable(bool enable)
 	else
 		reg &= ~mask;
 
-	io_caam_write32(ccm_base + CCM_CCGR0, reg);
+	io_write32(ccm_base + CCM_CCGR0, reg);
 
 	if (!soc_is_imx6ul()) {
 		/* EMI slow clk */
-		reg = io_caam_read32(ccm_base + CCM_CCGR6);
+		reg = io_read32(ccm_base + CCM_CCGR6);
 		mask = BM_CCM_CCGR6_EMI_SLOW;
 
 		if (enable)
@@ -38,6 +38,6 @@ void caam_hal_clk_enable(bool enable)
 		else
 			reg &= ~mask;
 
-		io_caam_write32(ccm_base + CCM_CCGR6, reg);
+		io_write32(ccm_base + CCM_CCGR6, reg);
 	}
 }

core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7.c
@@ -5,7 +5,7 @@
  * Brief   CAAM Clock functions.
  */
 #include <caam_hal_clk.h>
-#include <caam_io.h>
+#include <io.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
@@ -14,10 +14,10 @@ void caam_hal_clk_enable(bool enable)
 	vaddr_t ccm_base = (vaddr_t)phys_to_virt(CCM_BASE, MEM_AREA_IO_SEC, 1);
 
 	if (enable) {
-		io_caam_write32(ccm_base + CCM_CCGRx_SET(CCM_CLOCK_DOMAIN_CAAM),
-				CCM_CCGRx_ALWAYS_ON(0));
+		io_write32(ccm_base + CCM_CCGRx_SET(CCM_CLOCK_DOMAIN_CAAM),
+			   CCM_CCGRx_ALWAYS_ON(0));
 	} else {
-		io_caam_write32(ccm_base + CCM_CCGRx_CLR(CCM_CLOCK_DOMAIN_CAAM),
-				CCM_CCGRx_ALWAYS_ON(0));
+		io_write32(ccm_base + CCM_CCGRx_CLR(CCM_CLOCK_DOMAIN_CAAM),
+			   CCM_CCGRx_ALWAYS_ON(0));
 	}
 }

core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7ulp.c
@@ -5,7 +5,7 @@
  * Brief   CAAM Clock functions.
  */
 #include <caam_hal_clk.h>
-#include <caam_io.h>
+#include <io.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
@@ -15,7 +15,7 @@ void caam_hal_clk_enable(bool enable)
 						  PCC_CAAM + sizeof(uint32_t));
 
 	if (enable)
-		io_caam_write32(pcc2_base + PCC_CAAM, PCC_ENABLE_CLOCK);
+		io_write32(pcc2_base + PCC_CAAM, PCC_ENABLE_CLOCK);
 	else
-		io_caam_write32(pcc2_base + PCC_CAAM, PCC_DISABLE_CLOCK);
+		io_write32(pcc2_base + PCC_CAAM, PCC_DISABLE_CLOCK);
 }