OP-TEE/optee_os

commit d3bf580a67db3bd017051e253b72c3d087de4a1c

core: imx: add support for i.MX 8QxP
Add support for i.MX 8QxP
Add board flavors:
 * imx8qxpmek

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Signed-off-by: Alessandro Di Chiara <alessandro.dichiara@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

core/arch/arm/plat-imx/conf.mk
@@ -61,6 +61,9 @@ imx8mq-flavorlist = \
 imx8mm-flavorlist = \
 	imx8mmevk
 
+imx8qx-flavorlist = \
+	imx8qxpmek \
+
 ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx6ul-flavorlist)))
 $(call force,CFG_MX6,y)
 $(call force,CFG_MX6UL,y)
@@ -130,6 +133,12 @@ else ifneq (,$(filter $(PLATFORM_FLAVOR),$(imx8mm-flavorlist)))
 $(call force,CFG_IMX8MM,y)
 $(call force,CFG_ARM64_core,y)
 CFG_IMX_UART ?= y
+CFG_DRAM_BASE ?= 0x40000000
+CFG_TEE_CORE_NB_CORE ?= 4
+else ifneq (,$(filter $(PLATFORM_FLAVOR),$(imx8qx-flavorlist)))
+$(call force,CFG_IMX8QX,y)
+$(call force,CFG_ARM64_core,y)
+CFG_IMX_LPUART ?= y
 CFG_DRAM_BASE ?= 0x40000000
 CFG_TEE_CORE_NB_CORE ?= 4
 else
@@ -254,6 +263,11 @@ endif
 ifneq (,$(filter $(PLATFORM_FLAVOR),imx8mmevk))
 CFG_DDR_SIZE ?= 0x80000000
 CFG_UART_BASE ?= UART2_BASE
+endif
+
+ifneq (,$(filter $(PLATFORM_FLAVOR),imx8qxpmek))
+CFG_DDR_SIZE ?= 0x80000000
+CFG_UART_BASE ?= UART0_BASE
 endif
 
 # i.MX6 Solo/SL/SoloX/DualLite/Dual/Quad specific config

.shippable.yml
@@ -80,6 +80,7 @@ build:
     - _make PLATFORM=imx-mx7ulpevk
     - _make PLATFORM=imx-imx8mmevk
     - _make PLATFORM=imx-imx8mqevk
+    - _make PLATFORM=imx-imx8qxpmek
     - _make PLATFORM=k3-j721e
     - _make PLATFORM=k3-j721e CFG_ARM64_core=y
     - _make PLATFORM=k3-am65x

core/arch/arm/plat-imx/imx-common.c
@@ -25,8 +25,10 @@ static void imx_digproc(void)
 	uint32_t digprog = 0;
 	vaddr_t __maybe_unused anatop_addr = 0;
 
-#ifdef CFG_MX7ULP
+#if defined(CFG_MX7ULP)
 	digprog = SOC_MX7ULP << 16;
+#elif defined(CFG_IMX8QX)
+	digprog = SOC_MX8QX << 16;
 #else
 	anatop_addr = core_mmu_get_va(ANATOP_BASE, MEM_AREA_IO_SEC);
 

core/arch/arm/plat-imx/imx-regs.h
@@ -38,8 +38,10 @@
 #include <registers/imx7ulp.h>
 #elif defined(CFG_IMX8MQ) || defined(CFG_IMX8MM)
 #include <registers/imx8m.h>
+#elif defined(CFG_IMX8QX)
+#include <registers/imx8q.h>
 #else
-#error "CFG_MX6/7/7ULP or CFG_IMX8MQ/8MM is not defined"
+#error "CFG_MX6/7/7ULP or CFG_IMX8MQ/8MM/8QX is not defined"
 #endif
 
 #define IOMUXC_GPR4_OFFSET	0x10

core/arch/arm/plat-imx/imx.h
@@ -20,6 +20,7 @@
 #define SOC_MX6D	0x6A
 #define SOC_MX7D	0x72
 #define SOC_MX7ULP	0xE1
+#define SOC_MX8QX	0xE2
 
 uint32_t imx_get_src_gpr(int cpu);
 void imx_set_src_gpr(int cpu, uint32_t val);

core/arch/arm/plat-imx/registers/imx8q.h
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2019 NXP
+ */
+
+#ifndef __IMX8Q_H__
+#define __IMX8Q_H__
+
+#define GICD_BASE	0x51a00000
+#define GICR_BASE	0x51b00000
+#define UART0_BASE	0x5a060000
+#define UART1_BASE	0x5a070000
+#define UART2_BASE	0x5a080000
+#define UART3_BASE	0x5a090000
+#define CAAM_BASE	0x30900000
+
+#endif /* __IMX8Q_H__ */

commit 4a28f797406e31b318374b2c90fc6709748eaf06

core: imx: disable SNVS for imx8qx
Disable SNVS as it is not supported by i.MX8QX platforms.

Fixes: d3bf580a67 ("core: imx: add support for i.MX 8QxP")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-imx/conf.mk
@@ -188,6 +188,7 @@ $(call force,CFG_NXP_CAAM,n)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8qx-flavorlist)))
 $(call force,CFG_MX8QX,y)
 $(call force,CFG_ARM64_core,y)
+$(call force,CFG_IMX_SNVS,n)
 CFG_IMX_LPUART ?= y
 CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 4