OP-TEE/optee_os

commit 2d7a8964dfcbe6c3494a0889831818dd8f153f4a

driver: implement CAAM driver
Add the NXP CAAM drivers:
  - Random generator (instantiation and random generation)
  - Hash

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/caam/hal/common/hal_rng.c
@@ -1,0 +1,124 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2019 NXP
+ *
+ * Brief   CAAM Random Number Generator Hardware Abstration Layer.
+ *         Implementation of primitives to access HW.
+ */
+#include <caam_hal_rng.h>
+#include <caam_io.h>
+#include <caam_status.h>
+#include <registers/rng_regs.h>
+#include <registers/version_regs.h>
+
+bool caam_hal_rng_instantiated(vaddr_t baseaddr)
+{
+	uint32_t chavid_ls = 0;
+	uint32_t nb_sh = 0;
+	uint32_t status = 0;
+
+	chavid_ls = io_caam_read32(baseaddr + CHAVID_LS);
+
+	/* RNG version < 4 and RNG state handle is already instantiated */
+	if (GET_CHAVID_LS_RNGVID(chavid_ls) < 4)
+		return true;
+
+	/* Get the Number of State Handles */
+	nb_sh = caam_hal_rng_get_nb_sh(baseaddr);
+
+	/* Read the RNG Status and checks if all channels are instantiatied */
+	status = caam_hal_rng_get_sh_status(baseaddr);
+
+	if (status != GENMASK_32(nb_sh - 1, 0))
+		return false;
+
+	return true;
+}
+
+uint32_t caam_hal_rng_get_nb_sh(vaddr_t baseaddr)
+{
+	uint32_t reg = 0;
+
+	reg = io_caam_read32(baseaddr + CTPR_MS);
+
+	return GET_CTPR_MS_RNG_I(reg);
+}
+
+uint32_t caam_hal_rng_get_sh_status(vaddr_t baseaddr)
+{
+	return io_caam_read32(baseaddr + RNG_STA) & (RNG_STA_IF1 | RNG_STA_IF0);
+}
+
+bool caam_hal_rng_key_loaded(vaddr_t baseaddr)
+{
+	return io_caam_read32(baseaddr + RNG_STA) & RNG_STA_SKVN;
+}
+
+enum caam_status caam_hal_rng_kick(vaddr_t baseaddr, uint32_t inc_delay)
+{
+	uint32_t val = 0;
+	uint32_t ent_delay = TRNG_SDCTL_ENT_DLY_MIN + inc_delay;
+
+	if (ent_delay > TRNG_SDCTL_ENT_DLY_MAX)
+		return CAAM_OUT_OF_BOUND;
+
+	/*
+	 * Switch RNG in program mode
+	 * Setting both RTMCTL:PRGM and RTMCTL:TRNG_ACC causes TRNG to
+	 * properly invalidate the entropy in the entropy register and
+	 * force re-generation
+	 */
+	io_setbits32(baseaddr + TRNG_MCTL, TRNG_MCTL_PRGM | TRNG_MCTL_ACC);
+
+	/*
+	 * Configure the RNG Entropy Delay
+	 * Performance-wise, it does not make sense to
+	 * set the delay to a value that is lower
+	 * than the last one that worked (i.e. the state handles
+	 * were instantiated correctly). Thus, instead of wasting
+	 * time trying to set the values controlling the sample
+	 * frequency, the function simply returns.
+	 */
+	val = io_caam_read32(baseaddr + TRNG_SDCTL);
+	val = GET_TRNG_SDCTL_ENT_DLY(val);
+
+	if (ent_delay < val) {
+		/*
+		 * In this case do the programmation anyway because on some
+		 * device the other registers value can be wrong.
+		 */
+		ent_delay = val;
+	}
+
+	val = io_caam_read32(baseaddr + TRNG_SDCTL);
+	val &= ~BM_TRNG_SDCTL_ENT_DLY;
+	val |= TRNG_SDCTL_ENT_DLY(ent_delay);
+	io_caam_write32(baseaddr + TRNG_SDCTL, val);
+
+	/* min. freq. count, equal to 1/4 of the entropy sample length */
+	io_caam_write32(baseaddr + TRNG_FRQMIN, ent_delay >> 2);
+
+	/* max. freq. count, equal to 16 times the entropy sample length */
+	io_caam_write32(baseaddr + TRNG_FRQMAX, ent_delay << 4);
+
+	val = io_caam_read32(baseaddr + TRNG_MCTL);
+	/*
+	 * Select raw sampling in both entropy shifter
+	 * and statistical checker
+	 */
+	val &= ~BM_TRNG_MCTL_SAMP_MODE;
+	val |= TRNG_MCTL_SAMP_MODE_RAW_ES_SC;
+	/* Put RNG4 into run mode with handling CAAM/RNG4-TRNG Errata */
+	val &= ~(TRNG_MCTL_PRGM | TRNG_MCTL_ACC);
+	io_caam_write32(baseaddr + TRNG_MCTL, val);
+
+	/*
+	 * Clear the ERR bit in RTMCTL if set. The TRNG error can occur when
+	 * the RNG clock is not within 1/2x to 8x the system clock.
+	 * This error is possible if ROM code does not initialize the system
+	 * PLLs immediately after PoR.
+	 */
+	io_setbits32(baseaddr + TRNG_MCTL, TRNG_MCTL_ERR);
+
+	return CAAM_NO_ERROR;
+}

core/drivers/crypto/caam/hal/common/registers/rng_regs.h
@@ -1,0 +1,56 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2017-2019 NXP
+ *
+ * Brief   Random Number Generator Registers.
+ */
+#ifndef __RNG_REGS_H__
+#define __RNG_REGS_H__
+
+#include <util.h>
+
+/*
+ * RNG Test Registers
+ */
+/* Miscellaneous Control */
+#define TRNG_MCTL						0x0600
+#define TRNG_MCTL_PRGM					BIT32(16)
+#define TRNG_MCTL_ERR					BIT32(12)
+#define TRNG_MCTL_ACC					BIT32(5)
+#define BM_TRNG_MCTL_SAMP_MODE			SHIFT_U32(0x3, 0)
+/* Use raw data in both entropy shifter and statistical checker */
+#define TRNG_MCTL_SAMP_MODE_RAW_ES_SC	SHIFT_U32(1, 0)
+
+/* Seed Control */
+#define TRNG_SDCTL					0x0610
+#define BM_TRNG_SDCTL_ENT_DLY		SHIFT_U32(0xFFFF, 16)
+#define GET_TRNG_SDCTL_ENT_DLY(val)	(((val) & BM_TRNG_SDCTL_ENT_DLY) >> 16)
+#define TRNG_SDCTL_ENT_DLY(val)		SHIFT_U32(((val) & 0xFFFF), 16)
+
+#ifdef CFG_MX6SX
+/*
+ * After experimentation on i.MX6SX, the minimal Delay value
+ * allowing the RNG instantiation is 4800
+ */
+#define TRNG_SDCTL_ENT_DLY_MIN		(3200 + 1600)
+#else
+#define TRNG_SDCTL_ENT_DLY_MIN		3200
+#endif
+#define TRNG_SDCTL_ENT_DLY_MAX		12800
+
+/* Frequency Count Minimum Limit */
+#define TRNG_FRQMIN			0x0618
+/* Frequency Count Maximum Limit */
+#define TRNG_FRQMAX			0x061C
+
+/*
+ * RNG Registers
+ */
+/* Status */
+#define RNG_STA				0x06C0
+
+#define RNG_STA_SKVN		BIT32(30)
+#define RNG_STA_IF1			BIT32(1)
+#define RNG_STA_IF0			BIT32(0)
+
+#endif /* __RNG_REGS_H__ */

commit 7d607c274cde1f8facef02a2c51e62a36b7ff515

drivers: caam: hwrng fix perf drop after suspend/resume
RNG performance drops after a suspend/resume cycle on parts where caam
loses power, since the initial U-boot settings are not restored by OP-TEE
when resuming.
Modifying the TRNG "sample size" (the total number of entropy samples that
will be taken during entropy generation) from it's default conservative
value of 2500 to be more in line with the "sample size" that the caam
driver in U-boot/Linux kernel select(512) will solve the performance hit.

Changed the default minimum entropy delay value for I.MX 6SX from 4800
to 3200 to be in line with U-boot and Kernel setting for this value.
The higher default entropy delay value for OP-TEE would have caused a
perceived performance hit after/suspend resume.

Fixes: 2d7a896 ("driver: implement CAAM driver")
Signed-off-by: Dragos Rosioru <dragos.rosioru@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/caam/hal/common/hal_rng.c
@@ -99,16 +99,33 @@ enum caam_status caam_hal_rng_kick(vaddr_t baseaddr, uint32_t inc_delay)
 		ent_delay = val;
 	}
 
-	val = io_caam_read32(baseaddr + TRNG_SDCTL);
-	val &= ~BM_TRNG_SDCTL_ENT_DLY;
-	val |= TRNG_SDCTL_ENT_DLY(ent_delay);
-	io_caam_write32(baseaddr + TRNG_SDCTL, val);
+	io_caam_write32(baseaddr + TRNG_SDCTL, TRNG_SDCTL_ENT_DLY(ent_delay) |
+					       TRNG_SDCTL_SAMP_SIZE(512));
 
 	/* min. freq. count, equal to 1/4 of the entropy sample length */
 	io_caam_write32(baseaddr + TRNG_FRQMIN, ent_delay >> 2);
 
 	/* max. freq. count, equal to 16 times the entropy sample length */
 	io_caam_write32(baseaddr + TRNG_FRQMAX, ent_delay << 4);
+
+	io_caam_write32(baseaddr + TRNG_RTSCMISC,
+			TRNG_RTSCMISC_RTY_CNT(2) | TRNG_RTSCMISC_LRUN_MAX(32));
+	io_caam_write32(baseaddr + TRNG_RTPKRRNG, TRNG_RTPKRRNG_PKR_RNG(570));
+	io_caam_write32(baseaddr + TRNG_RTPKRMAX, TRNG_RTPKRMAX_PKR_MAX(1600));
+	io_caam_write32(baseaddr + TRNG_RTSCML,
+			TRNG_RTSCML_MONO_RNG(122) | TRNG_RTSCML_MONO_MAX(317));
+	io_caam_write32(baseaddr + TRNG_RTSCR1L,
+			TRNG_RTSCR1L_RUN1_RNG(80) | TRNG_RTSCR1L_RUN1_MAX(107));
+	io_caam_write32(baseaddr + TRNG_RTSCR2L,
+			TRNG_RTSCR2L_RUN2_RNG(57) | TRNG_RTSCR2L_RUN2_MAX(62));
+	io_caam_write32(baseaddr + TRNG_RTSCR3L,
+			TRNG_RTSCR3L_RUN3_RNG(39) | TRNG_RTSCR3L_RUN3_MAX(39));
+	io_caam_write32(baseaddr + TRNG_RTSCR4L,
+			TRNG_RTSCR4L_RUN4_RNG(27) | TRNG_RTSCR4L_RUN4_MAX(26));
+	io_caam_write32(baseaddr + TRNG_RTSCR5L,
+			TRNG_RTSCR5L_RUN5_RNG(19) | TRNG_RTSCR5L_RUN5_MAX(18));
+	io_caam_write32(baseaddr + TRNG_RTSCR6PL,
+			TRNG_RTSCR5L_RUN5_RNG(18) | TRNG_RTSCR5L_RUN5_MAX(17));
 
 	val = io_caam_read32(baseaddr + TRNG_MCTL);
 	/*

core/drivers/crypto/caam/hal/common/registers/rng_regs.h
@@ -22,26 +22,85 @@
 #define TRNG_MCTL_SAMP_MODE_RAW_ES_SC	SHIFT_U32(1, 0)
 
 /* Seed Control */
-#define TRNG_SDCTL					0x0610
+#define TRNG_SDCTL			0x0610
 #define BM_TRNG_SDCTL_ENT_DLY		SHIFT_U32(0xFFFF, 16)
 #define GET_TRNG_SDCTL_ENT_DLY(val)	(((val) & BM_TRNG_SDCTL_ENT_DLY) >> 16)
 #define TRNG_SDCTL_ENT_DLY(val)		SHIFT_U32(((val) & 0xFFFF), 16)
+#define TRNG_SDCTL_SAMP_SIZE(val)	((val) & 0xFFFF)
 
-#ifdef CFG_MX6SX
-/*
- * After experimentation on i.MX6SX, the minimal Delay value
- * allowing the RNG instantiation is 4800
- */
-#define TRNG_SDCTL_ENT_DLY_MIN		(3200 + 1600)
-#else
 #define TRNG_SDCTL_ENT_DLY_MIN		3200
-#endif
 #define TRNG_SDCTL_ENT_DLY_MAX		12800
 
 /* Frequency Count Minimum Limit */
 #define TRNG_FRQMIN			0x0618
 /* Frequency Count Maximum Limit */
 #define TRNG_FRQMAX			0x061C
+
+/* Statistical Check Miscellaneous */
+#define TRNG_RTSCMISC		    0x0604
+#define BM_TRNG_RTSCMISC_RTY_CNT    SHIFT_U32(0xF, 16)
+#define TRNG_RTSCMISC_RTY_CNT(val)  SHIFT_U32(((val) & (0xF)), 16)
+#define BM_TRNG_RTSCMISC_LRUN_MAX   SHIFT_U32(0xFF, 0)
+#define TRNG_RTSCMISC_LRUN_MAX(val) SHIFT_U32(((val) & (0xFF)), 0)
+
+/* Poker Range */
+#define TRNG_RTPKRRNG		   0x0608
+#define BM_TRNG_RTPKRRNG_PKR_RNG   SHIFT_U32(0xFFFF, 0)
+#define TRNG_RTPKRRNG_PKR_RNG(val) SHIFT_U32(((val) & (0xFFFF)), 0)
+
+/* Poker Maximum Limit */
+#define TRNG_RTPKRMAX		   0x060C
+#define BM_TRNG_RTPKRMAX_PKR_MAX   SHIFT_U32(0xFFFFFF, 0)
+#define TRNG_RTPKRMAX_PKR_MAX(val) SHIFT_U32(((val) & (0xFFFFFF)), 0)
+
+/* Statistical Check Monobit Limit */
+#define TRNG_RTSCML		  0x0620
+#define BM_TRNG_RTSCML_MONO_RNG	  SHIFT_U32(0xFFFF, 16)
+#define TRNG_RTSCML_MONO_RNG(val) SHIFT_U32(((val) & (0xFFFF)), 16)
+#define BM_TRNG_RTSCML_MONO_MAX	  SHIFT_U32(0xFFFF, 0)
+#define TRNG_RTSCML_MONO_MAX(val) SHIFT_U32(((val) & (0xFFFF)), 0)
+
+/* Statistical Check Run Length 1 Limit */
+#define TRNG_RTSCR1L		   0x0624
+#define BM_TRNG_RTSCR1L_RUN1_RNG   SHIFT_U32(0x7FFF, 16)
+#define TRNG_RTSCR1L_RUN1_RNG(val) SHIFT_U32(((val) & (0x7FFF)), 16)
+#define BM_TRNG_RTSCR1L_RUN1_MAX   SHIFT_U32(0x7FFF, 0)
+#define TRNG_RTSCR1L_RUN1_MAX(val) SHIFT_U32(((val) & (0x7FFF)), 0)
+
+/* Statistical Check Run Length 2 Limit */
+#define TRNG_RTSCR2L		   0x0628
+#define BM_TRNG_RTSCR2L_RUN2_RNG   SHIFT_U32(0x3FFF, 16)
+#define TRNG_RTSCR2L_RUN2_RNG(val) SHIFT_U32(((val) & (0x3FFF)), 16)
+#define BM_TRNG_RTSCR2L_RUN2_MAX   SHIFT_U32(0x3FFF, 0)
+#define TRNG_RTSCR2L_RUN2_MAX(val) SHIFT_U32(((val) & (0x3FFF)), 0)
+
+/* Statistical Check Run Length 3 Limit */
+#define TRNG_RTSCR3L		   0x062C
+#define BM_TRNG_RTSCR3L_RUN3_RNG   SHIFT_U32(0x1FFF, 16)
+#define TRNG_RTSCR3L_RUN3_RNG(val) SHIFT_U32(((val) & (0x1FFF)), 16)
+#define BM_TRNG_RTSCR3L_RUN3_MAX   SHIFT_U32(0x1FFF, 0)
+#define TRNG_RTSCR3L_RUN3_MAX(val) SHIFT_U32(((val) & (0x1FFF)), 0)
+
+/* Statistical Check Run Length 4 Limit */
+#define TRNG_RTSCR4L		   0x0630
+#define BM_TRNG_RTSCR4L_RUN4_RNG   SHIFT_U32(0xFFF, 16)
+#define TRNG_RTSCR4L_RUN4_RNG(val) SHIFT_U32(((val) & (0xFFF)), 16)
+#define BM_TRNG_RTSCR4L_RUN4_MAX   SHIFT_U32(0xFFF, 0)
+#define TRNG_RTSCR4L_RUN4_MAX(val) SHIFT_U32(((val) & (0xFFF)), 0)
+
+/* Statistical Check Run Length 5 Limit */
+#define TRNG_RTSCR5L		   0x0634
+#define BM_TRNG_RTSCR5L_RUN5_RNG   SHIFT_U32(0x7FF, 16)
+#define TRNG_RTSCR5L_RUN5_RNG(val) SHIFT_U32(((val) & (0x7FF)), 16)
+#define BM_TRNG_RTSCR5L_RUN5_MAX   SHIFT_U32(0x7FF, 0)
+#define TRNG_RTSCR5L_RUN5_MAX(val) SHIFT_U32(((val) & (0x7FF)), 0)
+
+/* Statistical Check Run Length 6+ Limit */
+#define TRNG_RTSCR6PL		     0x0638
+#define BM_TRNG_RTSCR6PL_RUN6P_RNG   SHIFT_U32(0x7FF, 16)
+#define TRNG_RTSCR6PL_RUN6P_RNG(val) SHIFT_U32(((val) & (0x7FF)), 16)
+#define BM_TRNG_RTSCR6PL_RUN6P_MAX   SHIFT_U32(0x7FF, 0)
+#define TRNG_RTSCR6PL_RUN6P_MAX(val) SHIFT_U32(((val) & (0x7FF)), 0)
 
 /*
  * RNG Registers