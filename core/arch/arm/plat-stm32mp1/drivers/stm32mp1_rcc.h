c84886a5571a5389ce9bdac7062ceb5e14c317e2
Etienne Carriere <etienne.carriere@linaro.org>
2021-09-24 12:10:52 +0200

    plat-stm32mp1: clk: new compatible st,stm32mp1-rcc-secure
    Update to Linux v5.14 DT bindings on RCC clock driver. Legacy compatible
    "st,stm32mp1-rcc" relates to RCC with security hardening disabled. New
    compatible "st,stm32mp1-rcc-secure" relates to platforms where RCC
    security hardening is enabled. The new compatible was introduced in
    Linux kernel v5.14 from [1].
    
    Link: [1] https://lore.kernel.org/r/20210617051814.12018-11-gabriel.fernandez@foss.st.com
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- core/arch/arm/plat-stm32mp1/drivers/stm32mp1_rcc.h
+++ core/arch/arm/plat-stm32mp1/drivers/stm32mp1_rcc.h
@@ -541,7 +541,8 @@
 #define RCC_MP_IWDGFZSETR_IWDG1			BIT(0)
 #define RCC_MP_IWDGFZSETR_IWDG2			BIT(1)
 
-#define DT_RCC_CLK_COMPAT	"st,stm32mp1-rcc"
+#define DT_RCC_CLK_COMPAT		"st,stm32mp1-rcc"
+#define DT_RCC_SECURE_CLK_COMPAT	"st,stm32mp1-rcc-secure"
 
 #ifndef __ASSEMBLER__
 vaddr_t stm32_rcc_base(void);
