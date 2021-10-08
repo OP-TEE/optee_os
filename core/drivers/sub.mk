fa145fd3afb3acf59f70942995375932348897a4
Jorge Ramirez-Ortiz <jorge@foundries.io>
2021-10-08 09:46:13 +0200

    zynqmp: drivers: generate HUK from PUF KEK
    If authenticated boot was disabled we allow generating the HUK using
    the SHA-256 of the DNA unique identifier.
    
    If authenticated boot was enabled, use the PUK KEK to generate the
    HUK instead. The PUF KEK must be registered while securing the board
    using the Xilinx tools. In this case, the HUK is generated by reading
    the DNA eFuses. This 96 bits value is used to generate a 16 byte
    digest which is then AES-GCM encrypted using the PUF KEK. The
    resulting 16 byte value is the HUK. To prevent the HUK from being
    leaked, the AES-GCM module must be reserved.
    
    The HUK generation was validated on Zynqmp zu3cg using the Xilinx
    Lightweight Provisioning Tool to enable authenticated boot and to
    provision the PUF (burning a number of eFuses in the process).
    
    Tested-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Tested-by: Ricardo Salveti <ricardo@foundries.io>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -42,6 +42,7 @@ srcs-$(CFG_ZYNQMP_CSU_PUF) += zynqmp_csu_puf.c
 srcs-$(CFG_ZYNQMP_CSUDMA) += zynqmp_csudma.c
 srcs-$(CFG_ZYNQMP_CSU_AES) += zynqmp_csu_aes.c
 srcs-$(CFG_ZYNQMP_PM) += zynqmp_pm.c
+srcs-$(CFG_ZYNQMP_HUK) += zynqmp_huk.c
 
 subdirs-y += crypto
 subdirs-$(CFG_BNXT_FW) += bnxt
