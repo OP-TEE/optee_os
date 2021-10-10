f57e4036edaebe33a08d2ec6d785fb5141aefb48
Jorge Ramirez-Ortiz <jorge@foundries.io>
2021-10-10 16:43:14 +0200

    zynqmp: platform: use HUK derived from PUF KEK for RPMB
    Enable the RPMB key when the HUK is generated from the PUF KEK.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Tested-by: Ricardo Salveti <ricardo@foundries.io>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-zynqmp/conf.mk
+++ core/arch/arm/plat-zynqmp/conf.mk
@@ -30,6 +30,10 @@ CFG_CRYPTO_WITH_CE ?= y
 
 CFG_ZYNQMP_PM ?= $(CFG_ARM64_core)
 
+ifeq ($(CFG_RPMB_FS),y)
+$(call force,CFG_ZYNQMP_HUK,y,Mandated by CFG_RPMB_FS)
+endif
+
 ifeq ($(CFG_ZYNQMP_HUK),y)
 $(call force,CFG_ZYNQMP_CSU_AES,y,Mandated by CFG_ZYNQMP_HUK)
 $(call force,CFG_ZYNQMP_CSU_PUF,y,Mandated by CFG_ZYNQMP_HUK)
