OP-TEE/optee_os

commit 9faf0da7b8543690df88623c30a8130234c25bc4

mk: add library common makefile support
It is not always suitable to place the third party library source in
the optee-os directory, provide a common library makefile here, the
usage is similar as TA, the only difference is as follow:
TA Makefile:
BINARY :=  xxx
LIB Makefile:
LIBNAME := libxxx
And xxx.ta or libxxx.a is the target.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Zeng Tao <prime.zeng@huawei.com>

ta/mk/ta_dev_kit.mk
@@ -8,6 +8,17 @@ all:
 include $(ta-dev-kit-dir)/mk/conf.mk
 
 binary := $(BINARY)
+libname := $(LIBNAME)
+
+ifneq ($(BINARY),)
+ifneq ($(LIBNAME),)
+$(error You can only specify one of BINARY or LIBNAME)
+endif
+else
+ifeq ($(LIBNAME),)
+$(error You must specify one of BINARY or LIBNAME)
+endif
+endif
 
 ifneq ($O,)
 out-dir := $O
@@ -65,9 +76,24 @@ clean:
 
 subdirs = .
 include  $(ta-dev-kit-dir)/mk/subdir.mk
+
+#the build target is ta
+ifneq ($(binary),)
 vpath %.c $(ta-dev-kit-dir)/src
 srcs += user_ta_header.c
+endif
 
 include  $(ta-dev-kit-dir)/mk/gcc.mk
 include  $(ta-dev-kit-dir)/mk/compile.mk
+ifneq ($(binary),)
 include  $(ta-dev-kit-dir)/mk/link.mk
+else
+ifneq ($(libname),)
+all: $(libname).a
+cleanfiles += $(libname).a
+
+$(libname).a: $(objs)
+	@echo '  AR      $@'
+	$(q)rm -f $@ && $(AR$(sm)) rcs -o $@ $^
+endif
+endif

commit ea0d439a169b297caeadd8faca4342fecb228916

ta_dev_kit.mk: remove extra -o when creating static library
The archiver command: $(AR$(sm)) is not supposed to take a -o argument
to specify its output, contrary to the linker for instance.

When GNU ar is used, -o is simply ignored. However when LLVM ar is used
an error is printed. Therefore remove this unwanted -o.

Fixes: 9faf0da7b854 ("mk: add library common makefile support")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/mk/ta_dev_kit.mk
@@ -143,7 +143,7 @@ cleanfiles += $(libname).a
 
 $(libname).a: $(objs)
 	@echo '  AR      $@'
-	$(q)rm -f $@ && $(AR$(sm)) rcs -o $@ $^
+	$(q)rm -f $@ && $(AR$(sm)) rcs $@ $^
 endif
 
 ifneq (,$(shlibname))