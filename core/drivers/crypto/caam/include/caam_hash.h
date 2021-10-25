c7220b5c4fc81580ed21c10231426e4faa7b5054
Clement Faure <clement.faure@nxp.com>
2021-10-25 17:13:43 +0200

    drivers: caam: hash: initialize driver with the JR base address
    Instead of systematically using the job ring 0 registers to initialize
    the CAAM hash driver, use the job ring allocated to OPTEE OS.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- core/drivers/crypto/caam/include/caam_hash.h
+++ core/drivers/crypto/caam/include/caam_hash.h
@@ -1,21 +1,24 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2021 NXP
  *
  * Brief   CAAM Hash manager header.
  */
 #ifndef __CAAM_HASH_H__
 #define __CAAM_HASH_H__
 
+#include <caam_jr.h>
+
 #ifdef CFG_NXP_CAAM_HASH_DRV
 /*
  * Initialize the Hash module
  *
- * @ctrl_addr   Controller base address
+ * @caam_jrcfg	JR configuration structure
  */
-enum caam_status caam_hash_init(vaddr_t ctrl_addr);
+enum caam_status caam_hash_init(struct caam_jrcfg *caam_jrcfg);
 #else
-static inline enum caam_status caam_hash_init(vaddr_t ctrl_addr __unused)
+static inline enum caam_status
+caam_hash_init(struct caam_jrcfg *caam_jrcfg __unused)
 {
 	return CAAM_NO_ERROR;
 }
@@ -25,11 +28,12 @@ static inline enum caam_status caam_hash_init(vaddr_t ctrl_addr __unused)
 /*
  * Initialize the HMAC module
  *
- * @ctrl_addr   Controller base address
+ * @caam_jrcfg	JR configuration structure
  */
-enum caam_status caam_hmac_init(vaddr_t ctrl_addr);
+enum caam_status caam_hmac_init(struct caam_jrcfg *caam_jrcfg);
 #else
-static inline enum caam_status caam_hmac_init(vaddr_t ctrl_addr __unused)
+static inline enum caam_status
+caam_hmac_init(struct caam_jrcfg *caam_jrcfg __unused)
 {
 	return CAAM_NO_ERROR;
 }
