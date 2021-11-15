4da83166249ff73174be0de711857c84d0f603e7
Jerome Forissier <jerome@forissier.org>
2021-11-15 17:29:22 +0100

    scripts/pem_to_c.py: add --private option to output private key
    When --private is passed to pem_to_c.py, output the private key in
    addition to the public key. This is useful to perform signing
    operations.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>

--- scripts/pem_to_c.py
+++ scripts/pem_to_c.py
@@ -10,17 +10,31 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--prefix', required=True,
-        help='Prefix for the public key exponent and modulus in c file')
+        help='Prefix for the public key exponent and modulus in C file')
     parser.add_argument(
         '--out', required=True,
-        help='Name of c file for the public key')
+        help='Name of C file for the key')
     parser.add_argument('--key', required=True, help='Name of key file')
+    parser.add_argument(
+        '--private', required=False, action='store_true',
+        help='Extract the private key, not the public one')
 
     return parser.parse_args()
 
 
-def main():
+def hexprint(f, buf):
     import array
+    i = 0
+    for x in array.array("B", buf):
+        f.write("0x" + '{0:02x}'.format(x) + ",")
+        i = i + 1
+        if i % 8 == 0:
+            f.write("\n")
+        else:
+            f.write(" ")
+
+
+def main():
     from cryptography.hazmat.backends import default_backend
     from cryptography.hazmat.primitives import serialization
     from cryptography.hazmat.primitives.asymmetric import rsa
@@ -33,16 +47,19 @@ def main():
         try:
             key = serialization.load_pem_private_key(data, password=None,
                                                      backend=default_backend())
-            key = key.public_key()
+            pubkey = key.public_key()
         except ValueError:
-            key = serialization.load_pem_public_key(data,
+            if args.private:
+                raise
+            pubkey = serialization.load_pem_public_key(
+                                                    data,
                                                     backend=default_backend())
 
     # Refuse public exponent with more than 32 bits. Otherwise the C
     # compiler may simply truncate the value and proceed.
     # This will lead to TAs seemingly having invalid signatures with a
     # possible security issue for any e = k*2^32 + 1 (for any integer k).
-    if key.public_numbers().e > 0xffffffff:
+    if pubkey.public_numbers().e > 0xffffffff:
         raise ValueError(
             'Unsupported large public exponent detected. ' +
             'OP-TEE handles only public exponents up to 2^32 - 1.')
@@ -51,20 +68,21 @@ def main():
         f.write("#include <stdint.h>\n")
         f.write("#include <stddef.h>\n\n")
         f.write("const uint32_t " + args.prefix + "_exponent = " +
-                str(key.public_numbers().e) + ";\n\n")
+                str(pubkey.public_numbers().e) + ";\n\n")
         f.write("const uint8_t " + args.prefix + "_modulus[] = {\n")
-        i = 0
-        nbuf = key.public_numbers().n.to_bytes(key.key_size >> 3, 'big')
-        for x in array.array("B", nbuf):
-            f.write("0x" + '{0:02x}'.format(x) + ",")
-            i = i + 1
-            if i % 8 == 0:
-                f.write("\n")
-            else:
-                f.write(" ")
+        nbuf = pubkey.public_numbers().n.to_bytes(pubkey.key_size >> 3, 'big')
+        hexprint(f, nbuf)
         f.write("};\n")
         f.write("const size_t " + args.prefix + "_modulus_size = sizeof(" +
                 args.prefix + "_modulus);\n")
+        if args.private:
+            f.write("const uint8_t " + args.prefix +
+                    "_private_exponent[] = {\n")
+            dbuf = key.private_numbers().d.to_bytes(key.key_size >> 3, 'big')
+            hexprint(f, dbuf)
+            f.write("};\n")
+            f.write("const size_t " + args.prefix + "_private_exponent_size = "
+                    "sizeof(" + args.prefix + "_private_exponent);\n")
 
 
 if __name__ == "__main__":
