OP-TEE/optee_os

commit 8c499324e457245e3781df0d7b87421f1f26939a

ta: pkcs11: Add function to set key data
Earlier derive_key_by_symm_enc() was used to derive key
by cipher operation and set the derived key value in
the object attributes. Simplify it to just derive the
key and return the derived key value to calling function.
Separate function is created to add this derived key value
in the key object.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>

ta/pkcs11/src/processing_symm.c
@@ -798,71 +798,38 @@ out:
 }
 
 enum pkcs11_rc derive_key_by_symm_enc(struct pkcs11_session *session,
-				      struct obj_attrs **head)
+				      void **out_buf, uint32_t *out_size)
 {
 	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
 	TEE_Result res = TEE_ERROR_GENERIC;
 	struct active_processing *proc = session->processing;
 	struct input_data_ref *input = proc->extra_ctx;
 	void *in_buf = NULL;
-	void *out_buf = NULL;
-	uint32_t out_size = 0;
 	uint32_t in_size = 0;
-	uint32_t size = sizeof(uint32_t);
-	uint32_t key_length = 0;
 
-	if (!proc->extra_ctx)
-		return PKCS11_CKR_ARGUMENTS_BAD;
+	switch (proc->mecha_type) {
+	case PKCS11_CKM_AES_ECB_ENCRYPT_DATA:
+	case PKCS11_CKM_AES_CBC_ENCRYPT_DATA:
+		if (!proc->extra_ctx)
+			return PKCS11_CKR_ARGUMENTS_BAD;
 
-	in_buf = input->data;
-	in_size = input->size;
+		in_buf = input->data;
+		in_size = input->size;
 
-	out_size = in_size;
-	if (!out_size)
-		return PKCS11_CKR_ARGUMENTS_BAD;
+		*out_size = in_size;
+		*out_buf = TEE_Malloc(*out_size, 0);
+		if (!*out_buf)
+			return PKCS11_CKR_DEVICE_MEMORY;
 
-	out_buf = TEE_Malloc(out_size, 0);
-	if (!out_buf)
-		return PKCS11_CKR_DEVICE_MEMORY;
-
-	res = TEE_CipherDoFinal(proc->tee_op_handle, in_buf, in_size, out_buf,
-				&out_size);
-	rc = tee2pkcs_error(res);
-	if (rc)
-		goto out;
-
-	/* Get key size if present in template */
-	rc = get_attribute(*head, PKCS11_CKA_VALUE_LEN, &key_length, &size);
-	if (rc && rc != PKCS11_RV_NOT_FOUND)
-		goto out;
-
-	if (key_length) {
-		/* Derived key is smaller than the required size */
-		if (out_size < key_length) {
-			rc = PKCS11_CKR_DATA_LEN_RANGE;
-			goto out;
-		}
-	} else {
-		key_length = out_size;
-		rc = set_attribute(head, PKCS11_CKA_VALUE_LEN, &key_length,
-				   sizeof(uint32_t));
+		res = TEE_CipherDoFinal(proc->tee_op_handle, in_buf, in_size,
+					*out_buf, out_size);
+		rc = tee2pkcs_error(res);
 		if (rc)
-			goto out;
+			TEE_Free(*out_buf);
+		break;
+	default:
+		return PKCS11_CKR_MECHANISM_INVALID;
 	}
 
-	/* Now we can check the VALUE_LEN field */
-	rc = check_created_attrs(*head, NULL);
-	if (rc)
-		goto out;
-
-	/* Remove the default empty value attribute if found */
-	rc = remove_empty_attribute(head, PKCS11_CKA_VALUE);
-	if (rc != PKCS11_CKR_OK && rc != PKCS11_RV_NOT_FOUND)
-		return PKCS11_CKR_GENERAL_ERROR;
-
-	rc = add_attribute(head, PKCS11_CKA_VALUE, out_buf, key_length);
-
-out:
-	TEE_Free(out_buf);
 	return rc;
 }

ta/pkcs11/src/pkcs11_attributes.c
@@ -1603,3 +1603,50 @@ enum pkcs11_rc check_attrs_against_modification(struct pkcs11_session *session,
 
 	return PKCS11_CKR_OK;
 }
+
+static enum pkcs11_rc set_secret_key_data(struct obj_attrs **head, void *data,
+					  size_t key_size)
+{
+	uint32_t size = sizeof(uint32_t);
+	uint32_t key_length = 0;
+	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
+
+	/* Get key size if present in template */
+	rc = get_attribute(*head, PKCS11_CKA_VALUE_LEN, &key_length, &size);
+	if (rc && rc != PKCS11_RV_NOT_FOUND)
+		return rc;
+
+	if (key_length) {
+		if (key_size < key_length)
+			return PKCS11_CKR_DATA_LEN_RANGE;
+	} else {
+		key_length = key_size;
+		rc = set_attribute(head, PKCS11_CKA_VALUE_LEN, &key_length,
+				   sizeof(uint32_t));
+		if (rc)
+			return rc;
+	}
+
+	/* Now we can check the VALUE_LEN field */
+	rc = check_created_attrs(*head, NULL);
+	if (rc)
+		return rc;
+
+	/* Remove the default empty value attribute if found */
+	rc = remove_empty_attribute(head, PKCS11_CKA_VALUE);
+	if (rc != PKCS11_CKR_OK && rc != PKCS11_RV_NOT_FOUND)
+		return PKCS11_CKR_GENERAL_ERROR;
+
+	return add_attribute(head, PKCS11_CKA_VALUE, data, key_length);
+}
+
+enum pkcs11_rc set_key_data(struct obj_attrs **head, void *data,
+			    size_t key_size)
+{
+	switch (get_class(*head)) {
+	case PKCS11_CKO_SECRET_KEY:
+		return set_secret_key_data(head, data, key_size);
+	default:
+		return PKCS11_CKR_GENERAL_ERROR;
+	}
+}

ta/pkcs11/src/pkcs11_attributes.h
@@ -183,4 +183,7 @@ enum pkcs11_rc check_attrs_against_modification(struct pkcs11_session *session,
 						struct pkcs11_object *obj,
 						enum processing_func function);
 
+enum pkcs11_rc set_key_data(struct obj_attrs **head, void *data,
+			    size_t key_size);
+
 #endif /*PKCS11_TA_PKCS11_ATTRIBUTES_H*/

ta/pkcs11/src/processing.c
@@ -467,6 +467,8 @@ enum pkcs11_rc entry_derive_key(struct pkcs11_client *client,
 	struct pkcs11_object *parent = NULL;
 	struct obj_attrs *head = NULL;
 	size_t template_size = 0;
+	void *out_buf = NULL;
+	uint32_t out_size = 0;
 	enum processing_func function = PKCS11_FUNCTION_DERIVE;
 
 	if (!client || ptypes != exp_pt ||
@@ -560,14 +562,7 @@ enum pkcs11_rc entry_derive_key(struct pkcs11_client *client,
 	if (rc)
 		goto out;
 
-	/*
-	 * Execute target processing and add value as attribute
-	 * PKCS11_CKA_VALUE. Symm key generation: depends on target
-	 * processing to be used.
-	 */
-	switch (proc_params->id) {
-	case PKCS11_CKM_AES_ECB_ENCRYPT_DATA:
-	case PKCS11_CKM_AES_CBC_ENCRYPT_DATA:
+	if (processing_is_tee_symm(proc_params->id)) {
 		/*
 		 * These derivation mechanism require encryption to be
 		 * performed on the data passed in proc_params by parent
@@ -579,14 +574,22 @@ enum pkcs11_rc entry_derive_key(struct pkcs11_client *client,
 		if (rc)
 			goto out;
 
-		rc = derive_key_by_symm_enc(session, &head);
+		session->processing->mecha_type = proc_params->id;
+
+		rc = derive_key_by_symm_enc(session, &out_buf, &out_size);
 		if (rc)
 			goto out;
-		break;
-	default:
+	} else {
 		rc = PKCS11_CKR_MECHANISM_INVALID;
 		goto out;
 	}
+
+	rc = set_key_data(&head, out_buf, out_size);
+	if (rc)
+		goto out;
+
+	TEE_Free(out_buf);
+	out_buf = NULL;
 
 	TEE_Free(proc_params);
 	proc_params = NULL;
@@ -618,6 +621,7 @@ out_free:
 	TEE_Free(proc_params);
 	TEE_Free(template);
 	TEE_Free(head);
+	TEE_Free(out_buf);
 
 	return rc;
 }

ta/pkcs11/src/processing.h
@@ -59,6 +59,6 @@ enum pkcs11_rc tee_init_ctr_operation(struct active_processing *processing,
 				      void *proc_params, size_t params_size);
 
 enum pkcs11_rc derive_key_by_symm_enc(struct pkcs11_session *session,
-				      struct obj_attrs **head);
+				      void **out_buf, uint32_t *out_sz);
 
 #endif /*PKCS11_TA_PROCESSING_H*/

commit 4cc938a853607fa0e00c76b12a8b2f14b3615945

ta: pkcs11: fix a double-free
entry_processing_key() frees out_buf on error from
derive_key_by_symm_enc(). Before this change, that led to a double-free.

Fixes: 8c499324e457 ("ta: pkcs11: Add function to set key data")
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Elvira Khabirova <e.khabirova@omp.ru>

ta/pkcs11/src/processing_symm.c
@@ -999,6 +999,7 @@ enum pkcs11_rc derive_key_by_symm_enc(struct pkcs11_session *session,
 	struct active_processing *proc = session->processing;
 	struct input_data_ref *input = proc->extra_ctx;
 	void *in_buf = NULL;
+	void *dest_buf = NULL;
 	uint32_t in_size = 0;
 
 	switch (proc->mecha_type) {
@@ -1011,15 +1012,19 @@ enum pkcs11_rc derive_key_by_symm_enc(struct pkcs11_session *session,
 		in_size = input->size;
 
 		*out_size = in_size;
-		*out_buf = TEE_Malloc(*out_size, 0);
-		if (!*out_buf)
+		dest_buf = TEE_Malloc(*out_size, 0);
+		if (!dest_buf)
 			return PKCS11_CKR_DEVICE_MEMORY;
 
 		res = TEE_CipherDoFinal(proc->tee_op_handle, in_buf, in_size,
-					*out_buf, out_size);
+					dest_buf, out_size);
 		rc = tee2pkcs_error(res);
-		if (rc)
-			TEE_Free(*out_buf);
+		if (rc) {
+			TEE_Free(dest_buf);
+			return rc;
+		}
+
+		*out_buf = dest_buf;
 		break;
 	default:
 		return PKCS11_CKR_MECHANISM_INVALID;