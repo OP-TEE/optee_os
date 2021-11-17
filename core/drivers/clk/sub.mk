b0ba6f5250c6cc96e4feb2703f1f11c5be6d2d93
Etienne Carriere <etienne.carriere@linaro.org>
2021-11-17 17:49:40 +0100

    drivers: clk: move stm32mp1 clock driver to clock drivers directory
    Moves stm32mp15 clock driver to core/drivers/clk and adds configuration
    switch CFG_STM32MP15_CLK to embed or not the driver. Platform stm32mp1
    mandates CFG_STM32MP15_CLK=y.
    
    Reviewed-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Acked-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/clk/sub.mk
+++ core/drivers/clk/sub.mk
@@ -1,5 +1,6 @@
 srcs-y += clk.c
 srcs-$(CFG_DRIVERS_CLK_DT) += clk_dt.c
 srcs-$(CFG_DRIVERS_CLK_FIXED) += fixed_clk.c
+srcs-$(CFG_STM32MP15_CLK) += clk-stm32mp15.c
 
 subdirs-$(CFG_DRIVERS_SAM_CLK) += sam
