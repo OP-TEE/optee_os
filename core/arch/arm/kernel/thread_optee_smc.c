OP-TEE/optee_os

commit 2786f1438fc88315dbbea7ae3a8bbd9fbfd2f8d2

core: thread: separate old SMC interface handling
Moves the old SMC interface handing parts for thread*.S into separate
files to make way for SPCI which will provide another interface towards
normal world.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/thread_optee_smc.c
@@ -1,0 +1,492 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2019, Linaro Limited
+ */
+
+#include <assert.h>
+#include <compiler.h>
+#include <kernel/misc.h>
+#include <kernel/msg_param.h>
+#include <kernel/thread.h>
+#include <kernel/virtualization.h>
+#include <mm/core_mmu.h>
+#include <optee_msg.h>
+#include <optee_rpc_cmd.h>
+#include <sm/optee_smc.h>
+#include <sm/sm.h>
+#include <string.h>
+#include <tee/tee_cryp_utl.h>
+#include <tee/tee_fs_rpc.h>
+
+#include "thread_private.h"
+
+static bool thread_prealloc_rpc_cache;
+static unsigned int thread_rpc_pnum;
+
+void thread_handle_fast_smc(struct thread_smc_args *args)
+{
+	thread_check_canaries();
+
+#ifdef CFG_VIRTUALIZATION
+	if (!virt_set_guest(args->a7)) {
+		args->a0 = OPTEE_SMC_RETURN_ENOTAVAIL;
+		goto out;
+	}
+#endif
+
+	thread_fast_smc_handler_ptr(args);
+
+#ifdef CFG_VIRTUALIZATION
+	virt_unset_guest();
+#endif
+	/* Fast handlers must not unmask any exceptions */
+out:
+	__maybe_unused;
+	assert(thread_get_exceptions() == THREAD_EXCP_ALL);
+}
+
+void thread_handle_std_smc(struct thread_smc_args *args)
+{
+	thread_check_canaries();
+
+#ifdef CFG_VIRTUALIZATION
+	if (!virt_set_guest(args->a7)) {
+		args->a0 = OPTEE_SMC_RETURN_ENOTAVAIL;
+		return;
+	}
+#endif
+
+	/*
+	 * thread_resume_from_rpc() and thread_alloc_and_run() only return
+	 * on error. Successful return is done via thread_exit() or
+	 * thread_rpc().
+	 */
+	if (args->a0 == OPTEE_SMC_CALL_RETURN_FROM_RPC) {
+		thread_resume_from_rpc(args->a0, args->a1, args->a2, args->a3,
+				       args->a4, args->a5);
+		args->a0 = OPTEE_SMC_RETURN_ERESUME;
+	} else {
+		thread_alloc_and_run(args->a0, args->a1, args->a2, args->a3);
+		args->a0 = OPTEE_SMC_RETURN_ETHREAD_LIMIT;
+	}
+
+#ifdef CFG_VIRTUALIZATION
+	virt_unset_guest();
+#endif
+}
+
+/**
+ * Free physical memory previously allocated with thread_rpc_alloc_arg()
+ *
+ * @cookie:	cookie received when allocating the buffer
+ */
+static void thread_rpc_free_arg(uint64_t cookie)
+{
+	if (cookie) {
+		uint32_t rpc_args[THREAD_RPC_NUM_ARGS] = {
+			OPTEE_SMC_RETURN_RPC_FREE
+		};
+
+		reg_pair_from_64(cookie, rpc_args + 1, rpc_args + 2);
+		thread_rpc(rpc_args);
+	}
+}
+
+/*
+ * Helper routine for the assembly function thread_std_smc_entry()
+ *
+ * Note: this function is weak just to make it possible to exclude it from
+ * the unpaged area.
+ */
+void __weak __thread_std_smc_entry(struct thread_smc_args *args)
+{
+#ifdef CFG_VIRTUALIZATION
+	virt_on_stdcall();
+#endif
+	thread_std_smc_handler_ptr(args);
+
+	if (args->a0 == OPTEE_SMC_RETURN_OK) {
+		struct thread_ctx *thr = threads + thread_get_id();
+
+		tee_fs_rpc_cache_clear(&thr->tsd);
+		if (!thread_prealloc_rpc_cache) {
+			thread_rpc_free_arg(mobj_get_cookie(thr->rpc_mobj));
+			mobj_free(thr->rpc_mobj);
+			thr->rpc_arg = 0;
+			thr->rpc_mobj = NULL;
+		}
+	}
+}
+
+bool thread_disable_prealloc_rpc_cache(uint64_t *cookie)
+{
+	bool rv;
+	size_t n;
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+
+	thread_lock_global();
+
+	for (n = 0; n < CFG_NUM_THREADS; n++) {
+		if (threads[n].state != THREAD_STATE_FREE) {
+			rv = false;
+			goto out;
+		}
+	}
+
+	rv = true;
+	for (n = 0; n < CFG_NUM_THREADS; n++) {
+		if (threads[n].rpc_arg) {
+			*cookie = mobj_get_cookie(threads[n].rpc_mobj);
+			mobj_free(threads[n].rpc_mobj);
+			threads[n].rpc_arg = NULL;
+			goto out;
+		}
+	}
+
+	*cookie = 0;
+	thread_prealloc_rpc_cache = false;
+out:
+	thread_unlock_global();
+	thread_unmask_exceptions(exceptions);
+	return rv;
+}
+
+bool thread_enable_prealloc_rpc_cache(void)
+{
+	bool rv;
+	size_t n;
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+
+	thread_lock_global();
+
+	for (n = 0; n < CFG_NUM_THREADS; n++) {
+		if (threads[n].state != THREAD_STATE_FREE) {
+			rv = false;
+			goto out;
+		}
+	}
+
+	rv = true;
+	thread_prealloc_rpc_cache = true;
+out:
+	thread_unlock_global();
+	thread_unmask_exceptions(exceptions);
+	return rv;
+}
+
+/**
+ * Allocates data for struct optee_msg_arg.
+ *
+ * @size:	size in bytes of struct optee_msg_arg
+ *
+ * @returns	mobj that describes allocated buffer or NULL on error
+ */
+static struct mobj *thread_rpc_alloc_arg(size_t size)
+{
+	paddr_t pa;
+	uint64_t co;
+	uint32_t rpc_args[THREAD_RPC_NUM_ARGS] = {
+		OPTEE_SMC_RETURN_RPC_ALLOC, size
+	};
+	struct mobj *mobj = NULL;
+
+	thread_rpc(rpc_args);
+
+	pa = reg_pair_to_64(rpc_args[1], rpc_args[2]);
+	co = reg_pair_to_64(rpc_args[4], rpc_args[5]);
+
+	if (!ALIGNMENT_IS_OK(pa, struct optee_msg_arg))
+		goto err;
+
+	/* Check if this region is in static shared space */
+	if (core_pbuf_is(CORE_MEM_NSEC_SHM, pa, size))
+		mobj = mobj_shm_alloc(pa, size, co);
+#ifdef CFG_CORE_DYN_SHM
+	else if ((!(pa & SMALL_PAGE_MASK)) && size <= SMALL_PAGE_SIZE)
+		mobj = mobj_mapped_shm_alloc(&pa, 1, 0, co);
+#endif
+
+	if (!mobj)
+		goto err;
+
+	return mobj;
+err:
+	thread_rpc_free_arg(co);
+	mobj_free(mobj);
+	return NULL;
+}
+
+static bool set_rmem(struct optee_msg_param *param,
+		     struct thread_param *tpm)
+{
+	param->attr = tpm->attr - THREAD_PARAM_ATTR_MEMREF_IN +
+		      OPTEE_MSG_ATTR_TYPE_RMEM_INPUT;
+	param->u.rmem.offs = tpm->u.memref.offs;
+	param->u.rmem.size = tpm->u.memref.size;
+	if (tpm->u.memref.mobj) {
+		param->u.rmem.shm_ref = mobj_get_cookie(tpm->u.memref.mobj);
+		if (!param->u.rmem.shm_ref)
+			return false;
+	} else {
+		param->u.rmem.shm_ref = 0;
+	}
+
+	return true;
+}
+
+static bool set_tmem(struct optee_msg_param *param,
+		     struct thread_param *tpm)
+{
+	paddr_t pa = 0;
+	uint64_t shm_ref = 0;
+	struct mobj *mobj = tpm->u.memref.mobj;
+
+	param->attr = tpm->attr - THREAD_PARAM_ATTR_MEMREF_IN +
+		      OPTEE_MSG_ATTR_TYPE_TMEM_INPUT;
+	if (mobj) {
+		shm_ref = mobj_get_cookie(mobj);
+		if (!shm_ref)
+			return false;
+		if (mobj_get_pa(mobj, tpm->u.memref.offs, 0, &pa))
+			return false;
+	}
+
+	param->u.tmem.size = tpm->u.memref.size;
+	param->u.tmem.buf_ptr = pa;
+	param->u.tmem.shm_ref = shm_ref;
+
+	return true;
+}
+
+static uint32_t get_rpc_arg(uint32_t cmd, size_t num_params,
+			    struct thread_param *params, void **arg_ret,
+			    uint64_t *carg_ret)
+{
+	struct thread_ctx *thr = threads + thread_get_id();
+	struct optee_msg_arg *arg = thr->rpc_arg;
+	size_t sz = OPTEE_MSG_GET_ARG_SIZE(THREAD_RPC_MAX_NUM_PARAMS);
+
+	if (num_params > THREAD_RPC_MAX_NUM_PARAMS)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (!arg) {
+		struct mobj *mobj = thread_rpc_alloc_arg(sz);
+
+		if (!mobj)
+			return TEE_ERROR_OUT_OF_MEMORY;
+
+		arg = mobj_get_va(mobj, 0);
+		if (!arg) {
+			thread_rpc_free_arg(mobj_get_cookie(mobj));
+			return TEE_ERROR_OUT_OF_MEMORY;
+		}
+
+		thr->rpc_arg = arg;
+		thr->rpc_mobj = mobj;
+	}
+
+	memset(arg, 0, OPTEE_MSG_GET_ARG_SIZE(num_params));
+	arg->cmd = cmd;
+	arg->num_params = num_params;
+	arg->ret = TEE_ERROR_GENERIC; /* in case value isn't updated */
+
+	for (size_t n = 0; n < num_params; n++) {
+		switch (params[n].attr) {
+		case THREAD_PARAM_ATTR_NONE:
+			arg->params[n].attr = OPTEE_MSG_ATTR_TYPE_NONE;
+			break;
+		case THREAD_PARAM_ATTR_VALUE_IN:
+		case THREAD_PARAM_ATTR_VALUE_OUT:
+		case THREAD_PARAM_ATTR_VALUE_INOUT:
+			arg->params[n].attr = params[n].attr -
+					      THREAD_PARAM_ATTR_VALUE_IN +
+					      OPTEE_MSG_ATTR_TYPE_VALUE_INPUT;
+			arg->params[n].u.value.a = params[n].u.value.a;
+			arg->params[n].u.value.b = params[n].u.value.b;
+			arg->params[n].u.value.c = params[n].u.value.c;
+			break;
+		case THREAD_PARAM_ATTR_MEMREF_IN:
+		case THREAD_PARAM_ATTR_MEMREF_OUT:
+		case THREAD_PARAM_ATTR_MEMREF_INOUT:
+			if (!params[n].u.memref.mobj ||
+			    mobj_matches(params[n].u.memref.mobj,
+					 CORE_MEM_NSEC_SHM)) {
+				if (!set_tmem(arg->params + n, params + n))
+					return TEE_ERROR_BAD_PARAMETERS;
+			} else  if (mobj_matches(params[n].u.memref.mobj,
+						 CORE_MEM_REG_SHM)) {
+				if (!set_rmem(arg->params + n, params + n))
+					return TEE_ERROR_BAD_PARAMETERS;
+			} else {
+				return TEE_ERROR_BAD_PARAMETERS;
+			}
+			break;
+		default:
+			return TEE_ERROR_BAD_PARAMETERS;
+		}
+	}
+
+	*arg_ret = arg;
+	*carg_ret = mobj_get_cookie(thr->rpc_mobj);
+
+	return TEE_SUCCESS;
+}
+
+static uint32_t get_rpc_arg_res(struct optee_msg_arg *arg, size_t num_params,
+				struct thread_param *params)
+{
+	for (size_t n = 0; n < num_params; n++) {
+		switch (params[n].attr) {
+		case THREAD_PARAM_ATTR_VALUE_OUT:
+		case THREAD_PARAM_ATTR_VALUE_INOUT:
+			params[n].u.value.a = arg->params[n].u.value.a;
+			params[n].u.value.b = arg->params[n].u.value.b;
+			params[n].u.value.c = arg->params[n].u.value.c;
+			break;
+		case THREAD_PARAM_ATTR_MEMREF_OUT:
+		case THREAD_PARAM_ATTR_MEMREF_INOUT:
+			/*
+			 * rmem.size and tmem.size is the same type and
+			 * location.
+			 */
+			params[n].u.memref.size = arg->params[n].u.rmem.size;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return arg->ret;
+}
+
+uint32_t thread_rpc_cmd(uint32_t cmd, size_t num_params,
+			struct thread_param *params)
+{
+	uint32_t rpc_args[THREAD_RPC_NUM_ARGS] = { OPTEE_SMC_RETURN_RPC_CMD };
+	void *arg = NULL;
+	uint64_t carg = 0;
+	uint32_t ret = 0;
+
+	/* The source CRYPTO_RNG_SRC_JITTER_RPC is safe to use here */
+	plat_prng_add_jitter_entropy(CRYPTO_RNG_SRC_JITTER_RPC,
+				     &thread_rpc_pnum);
+
+	ret = get_rpc_arg(cmd, num_params, params, &arg, &carg);
+	if (ret)
+		return ret;
+
+	reg_pair_from_64(carg, rpc_args + 1, rpc_args + 2);
+	thread_rpc(rpc_args);
+
+	return get_rpc_arg_res(arg, num_params, params);
+}
+
+/**
+ * Free physical memory previously allocated with thread_rpc_alloc()
+ *
+ * @cookie:	cookie received when allocating the buffer
+ * @bt:		must be the same as supplied when allocating
+ * @mobj:	mobj that describes allocated buffer
+ *
+ * This function also frees corresponding mobj.
+ */
+static void thread_rpc_free(unsigned int bt, uint64_t cookie, struct mobj *mobj)
+{
+	uint32_t rpc_args[THREAD_RPC_NUM_ARGS] = { OPTEE_SMC_RETURN_RPC_CMD };
+	void *arg = NULL;
+	uint64_t carg = 0;
+	struct thread_param param = THREAD_PARAM_VALUE(IN, bt, cookie, 0);
+	uint32_t ret = get_rpc_arg(OPTEE_RPC_CMD_SHM_FREE, 1, &param,
+				   &arg, &carg);
+
+	mobj_free(mobj);
+
+	if (!ret) {
+		reg_pair_from_64(carg, rpc_args + 1, rpc_args + 2);
+		thread_rpc(rpc_args);
+	}
+}
+
+static struct mobj *get_rpc_alloc_res(struct optee_msg_arg *arg,
+				      unsigned int bt)
+{
+	struct mobj *mobj = NULL;
+	uint64_t cookie = 0;
+
+	if (arg->ret || arg->num_params != 1)
+		return NULL;
+
+	if (arg->params[0].attr == OPTEE_MSG_ATTR_TYPE_TMEM_OUTPUT) {
+		cookie = arg->params[0].u.tmem.shm_ref;
+		mobj = mobj_shm_alloc(arg->params[0].u.tmem.buf_ptr,
+				      arg->params[0].u.tmem.size,
+				      cookie);
+	} else if (arg->params[0].attr == (OPTEE_MSG_ATTR_TYPE_TMEM_OUTPUT |
+					   OPTEE_MSG_ATTR_NONCONTIG)) {
+		paddr_t p = arg->params[0].u.tmem.buf_ptr;
+		size_t sz = arg->params[0].u.tmem.size;
+
+		cookie = arg->params[0].u.tmem.shm_ref;
+		mobj = msg_param_mobj_from_noncontig(p, sz, cookie, true);
+	} else {
+		return NULL;
+	}
+
+	if (!mobj) {
+		thread_rpc_free(bt, cookie, mobj);
+		return NULL;
+	}
+
+	assert(mobj_is_nonsec(mobj));
+
+	return mobj;
+}
+
+/**
+ * Allocates shared memory buffer via RPC
+ *
+ * @size:	size in bytes of shared memory buffer
+ * @align:	required alignment of buffer
+ * @bt:		buffer type OPTEE_RPC_SHM_TYPE_*
+ *
+ * Returns a pointer to MOBJ for the memory on success, or NULL on failure.
+ */
+static struct mobj *thread_rpc_alloc(size_t size, size_t align, unsigned int bt)
+{
+	uint32_t rpc_args[THREAD_RPC_NUM_ARGS] = { OPTEE_SMC_RETURN_RPC_CMD };
+	void *arg = NULL;
+	uint64_t carg = 0;
+	struct thread_param param = THREAD_PARAM_VALUE(IN, bt, size, align);
+	uint32_t ret = get_rpc_arg(OPTEE_RPC_CMD_SHM_ALLOC, 1, &param,
+				   &arg, &carg);
+
+	if (ret)
+		return NULL;
+
+	reg_pair_from_64(carg, rpc_args + 1, rpc_args + 2);
+	thread_rpc(rpc_args);
+
+	return get_rpc_alloc_res(arg, bt);
+}
+
+struct mobj *thread_rpc_alloc_payload(size_t size)
+{
+	return thread_rpc_alloc(size, 8, OPTEE_RPC_SHM_TYPE_APPL);
+}
+
+void thread_rpc_free_payload(struct mobj *mobj)
+{
+	thread_rpc_free(OPTEE_RPC_SHM_TYPE_APPL, mobj_get_cookie(mobj),
+			mobj);
+}
+
+struct mobj *thread_rpc_alloc_global_payload(size_t size)
+{
+	return thread_rpc_alloc(size, 8, OPTEE_RPC_SHM_TYPE_GLOBAL);
+}
+
+void thread_rpc_free_global_payload(struct mobj *mobj)
+{
+	thread_rpc_free(OPTEE_RPC_SHM_TYPE_GLOBAL, mobj_get_cookie(mobj),
+			mobj);
+}

core/arch/arm/kernel/sub.mk
@@ -28,6 +28,9 @@ srcs-$(CFG_PL310) += tee_l2cc_mutex.c
 srcs-$(CFG_ARM32_core) += thread_a32.S
 srcs-$(CFG_ARM64_core) += thread_a64.S
 srcs-y += thread.c
+srcs-y += thread_optee_smc.c
+srcs-$(CFG_ARM32_core) += thread_optee_smc_a32.S
+srcs-$(CFG_ARM64_core) += thread_optee_smc_a64.S
 srcs-y += abort.c
 srcs-$(CFG_WITH_VFP) += vfp.c
 ifeq ($(CFG_WITH_VFP),y)

core/arch/arm/kernel/thread_a32.S
@@ -14,9 +14,6 @@
 #include <kernel/thread_defs.h>
 #include <kernel/unwind.h>
 #include <mm/core_mmu.h>
-#include <sm/optee_smc.h>
-#include <sm/teesmc_opteed.h>
-#include <sm/teesmc_opteed_macros.h>
 
 #include "thread_private.h"
 
@@ -30,155 +27,6 @@
 		 */
 		tst	\reg, #0x0f
 	.endm
-
-LOCAL_FUNC vector_std_smc_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	push	{r0-r7}
-	mov	r0, sp
-	bl	thread_handle_std_smc
-	/*
-	 * Normally thread_handle_std_smc() should return via
-	 * thread_exit(), thread_rpc(), but if thread_handle_std_smc()
-	 * hasn't switched stack (error detected) it will do a normal "C"
-	 * return.
-	 */
-	pop	{r1-r8}
-	ldr	r0, =TEESMC_OPTEED_RETURN_CALL_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_std_smc_entry
-
-LOCAL_FUNC vector_fast_smc_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	push	{r0-r7}
-	mov	r0, sp
-	bl	thread_handle_fast_smc
-	pop	{r1-r8}
-	ldr	r0, =TEESMC_OPTEED_RETURN_CALL_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_fast_smc_entry
-
-LOCAL_FUNC vector_fiq_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
- 	/* Secure Monitor received a FIQ and passed control to us. */
-	bl	thread_check_canaries
-	ldr	lr, =thread_nintr_handler_ptr
- 	ldr	lr, [lr]
- 	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_FIQ_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_fiq_entry
-
-LOCAL_FUNC vector_cpu_on_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	ldr	lr, =thread_cpu_on_handler_ptr
-	ldr	lr, [lr]
-	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_ON_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_cpu_on_entry
-
-LOCAL_FUNC vector_cpu_off_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	ldr	lr, =thread_cpu_off_handler_ptr
-	ldr	lr, [lr]
-	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_OFF_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_cpu_off_entry
-
-LOCAL_FUNC vector_cpu_suspend_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	ldr	lr, =thread_cpu_suspend_handler_ptr
-	ldr	lr, [lr]
-	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_SUSPEND_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_cpu_suspend_entry
-
-LOCAL_FUNC vector_cpu_resume_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	ldr	lr, =thread_cpu_resume_handler_ptr
-	ldr	lr, [lr]
-	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_RESUME_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_cpu_resume_entry
-
-LOCAL_FUNC vector_system_off_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	ldr	lr, =thread_system_off_handler_ptr
-	ldr	lr, [lr]
-	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_SYSTEM_OFF_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_system_off_entry
-
-LOCAL_FUNC vector_system_reset_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	ldr	lr, =thread_system_reset_handler_ptr
-	ldr	lr, [lr]
-	blx	lr
-	mov	r1, r0
-	ldr	r0, =TEESMC_OPTEED_RETURN_SYSTEM_RESET_DONE
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC vector_system_reset_entry
-
-/*
- * Vector table supplied to ARM Trusted Firmware (ARM-TF) at
- * initialization.  Also used when compiled with the internal monitor, but
- * the cpu_*_entry and system_*_entry are not used then.
- *
- * Note that ARM-TF depends on the layout of this vector table, any change
- * in layout has to be synced with ARM-TF.
- */
-FUNC thread_vector_table , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	b	vector_std_smc_entry
-	b	vector_fast_smc_entry
-	b	vector_cpu_on_entry
-	b	vector_cpu_off_entry
-	b	vector_cpu_resume_entry
-	b	vector_cpu_suspend_entry
-	b	vector_fiq_entry
-	b	vector_system_off_entry
-	b	vector_system_reset_entry
-UNWIND(	.fnend)
-END_FUNC thread_vector_table
-KEEP_PAGER thread_vector_table
 
 FUNC thread_set_abt_sp , :
 UNWIND(	.fnstart)
@@ -311,7 +159,7 @@ END_FUNC thread_save_state_fiq
  * Disables IRQ and FIQ and saves state of thread, returns original
  * CPSR.
  */
-LOCAL_FUNC thread_save_state , :
+FUNC thread_save_state , :
 UNWIND(	.fnstart)
 UNWIND(	.cantunwind)
 	push	{r12, lr}
@@ -358,94 +206,6 @@ UNWIND(	.cantunwind)
 	bx	lr
 UNWIND(	.fnend)
 END_FUNC thread_save_state
-
-FUNC thread_std_smc_entry , :
-UNWIND(	.fnstart)
-UNWIND(	.cantunwind)
-	/* Pass r0-r7 in a struct thread_smc_args */
-	push	{r0-r7}
-	mov	r0, sp
-	bl	__thread_std_smc_entry
-	/*
-	 * Load the returned r0-r3 into preserved registers and skip the
-	 * "returned" r4-r7 since they will not be returned to normal
-	 * world.
-	 */
-	pop	{r4-r7}
-	add	sp, #(4 * 4)
-
-	/* Disable interrupts before switching to temporary stack */
-	cpsid	aif
-	bl	thread_get_tmp_sp
-	mov	sp, r0
-
-	bl	thread_state_free
-
-	ldr	r0, =TEESMC_OPTEED_RETURN_CALL_DONE
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	mov	r4, r7
-	smc	#0
-	b	.	/* SMC should not return */
-UNWIND(	.fnend)
-END_FUNC thread_std_smc_entry
-
-
-/* void thread_rpc(uint32_t rv[THREAD_RPC_NUM_ARGS]) */
-FUNC thread_rpc , :
-/*
- * r0-r2 are used to pass parameters to normal world
- * r0-r5 are used to pass return vaule back from normal world
- *
- * note that r3 is used to pass "resume information", that is, which
- * thread it is that should resume.
- *
- * Since the this function is following AAPCS we need to preserve r4-r5
- * which are otherwise modified when returning back from normal world.
- */
-UNWIND(	.fnstart)
-	push	{r4-r5, lr}
-UNWIND(	.save	{r4-r5, lr})
-	push	{r0}
-UNWIND(	.save	{r0})
-
-	bl	thread_save_state
-	mov	r4, r0			/* Save original CPSR */
-
-	/*
- 	 * Switch to temporary stack and SVC mode. Save CPSR to resume into.
-	 */
-	bl	thread_get_tmp_sp
-	ldr	r5, [sp]		/* Get pointer to rv[] */
-	cps	#CPSR_MODE_SVC		/* Change to SVC mode */
-	mov	sp, r0			/* Switch to tmp stack */
-
-	mov	r0, #THREAD_FLAGS_COPY_ARGS_ON_RETURN
-	mov	r1, r4			/* CPSR to restore */
-	ldr	r2, =.thread_rpc_return
-	bl	thread_state_suspend
-	mov	r4, r0			/* Supply thread index */
-	ldr	r0, =TEESMC_OPTEED_RETURN_CALL_DONE
-	ldm	r5, {r1-r3}		/* Load rv[] into r0-r2 */
-	smc	#0
-	b	.	/* SMC should not return */
-
-.thread_rpc_return:
-	/*
-	 * At this point has the stack pointer been restored to the value
-	 * it had when thread_save_state() was called above.
-	 *
-	 * Jumps here from thread_resume above when RPC has returned. The
-	 * IRQ and FIQ bits are restored to what they where when this
-	 * function was originally entered.
-	 */
-	pop	{r12}			/* Get pointer to rv[] */
-	stm	r12, {r0-r5}		/* Store r0-r5 into rv[] */
-	pop	{r4-r5, pc}
-UNWIND(	.fnend)
-END_FUNC thread_rpc
-KEEP_PAGER thread_rpc
 
 /*
  * unsigned long thread_smc(unsigned long func_id, unsigned long a1,
@@ -737,21 +497,6 @@ END_FUNC thread_unwind_user_mode
 	bl	thread_save_state
 	.endif
 
-	mov	r0, #THREAD_FLAGS_EXIT_ON_FOREIGN_INTR
-	mrs	r1, spsr
-	pop	{r2}
-	pop	{r12}
-	blx	thread_state_suspend
-	mov	r4, r0		/* Supply thread index */
-
-	/*
-	 * Switch to SVC mode and copy current stack pointer as it already
-	 * is the tmp stack.
-	 */
-	mov	r0, sp
-	cps	#CPSR_MODE_SVC
-	mov	sp, r0
-
 #ifdef CFG_CORE_WORKAROUND_NSITR_CACHE_PRIME
 	/*
 	 * Prevent leaking information about which entries has been used in
@@ -763,13 +508,22 @@ END_FUNC thread_unwind_user_mode
 	write_iciallu
 #endif
 
-	ldr	r0, =TEESMC_OPTEED_RETURN_CALL_DONE
-	ldr	r1, =OPTEE_SMC_RETURN_RPC_FOREIGN_INTR
-	mov	r2, #0
-	mov	r3, #0
-	/* r4 is already filled in above */
-	smc	#0
-	b	.	/* SMC should not return */
+	mov	r0, #THREAD_FLAGS_EXIT_ON_FOREIGN_INTR
+	mrs	r1, spsr
+	pop	{r2}
+	pop	{r12}
+	blx	thread_state_suspend
+
+	/*
+	 * Switch to SVC mode and copy current stack pointer as it already
+	 * is the tmp stack.
+	 */
+	mov	r1, sp
+	cps	#CPSR_MODE_SVC
+	mov	sp, r1
+
+	/* Passing thread index in r0 */
+	b	thread_foreign_intr_exit
 .endm
 
 	.section .text.thread_excp_vect

commit 4ed45027f507fb7693ff43b01b9c3299d5440eb0

core: verify size of allocated shared memory
Makes sure that normal world cannot change the size of allocated shared
memory, resulting in a smaller buffer being allocated.

This reintroduces cc6bc5f94210 ("core: verify size of allocated shared
memory") which was lost with Fixes: 2786f1438fc8 ("core: thread:
separate old SMC interface handling"). In addition is the READ_ONCE()
macro is used when reading the returned size from non-secure shared
memory.

Since then we have a separate set of functions to deal with RPC when
communicating with FF-A. A corresponding size check is added for the
FF-A version of thread_rpc_alloc().

Reported-by: Patrik Lantz <patrik.lantz@axis.com>
Reviewed-by: Patrik Lantz <patrik.lantz@axis.com>
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/thread_optee_smc.c
@@ -531,7 +531,7 @@ static void thread_rpc_free(unsigned int bt, uint64_t cookie, struct mobj *mobj)
 }
 
 static struct mobj *get_rpc_alloc_res(struct optee_msg_arg *arg,
-				      unsigned int bt)
+				      unsigned int bt, size_t size)
 {
 	struct mobj *mobj = NULL;
 	uint64_t cookie = 0;
@@ -547,8 +547,10 @@ static struct mobj *get_rpc_alloc_res(struct optee_msg_arg *arg,
 		return NULL;
 
 	p = arg->params[0].u.tmem.buf_ptr;
-	sz = arg->params[0].u.tmem.size;
+	sz = READ_ONCE(arg->params[0].u.tmem.size);
 	cookie = arg->params[0].u.tmem.shm_ref;
+	if (sz < size)
+		return NULL;
 
 	if (arg->params[0].attr == OPTEE_MSG_ATTR_TYPE_TMEM_OUTPUT)
 		mobj = rpc_shm_mobj_alloc(p, sz, cookie);
@@ -589,7 +591,7 @@ static struct mobj *thread_rpc_alloc(size_t size, size_t align, unsigned int bt)
 	reg_pair_from_64(carg, rpc_args + 1, rpc_args + 2);
 	thread_rpc(rpc_args);
 
-	return get_rpc_alloc_res(arg, bt);
+	return get_rpc_alloc_res(arg, bt, size);
 }
 
 struct mobj *thread_rpc_alloc_payload(size_t size)

core/arch/arm/kernel/thread_spmc.c
@@ -1236,6 +1236,12 @@ static struct mobj *thread_rpc_alloc(size_t size, size_t align, unsigned int bt)
 
 	assert(mobj_is_nonsec(mobj));
 
+	if (mobj->size < size) {
+		DMSG("Mobj %#"PRIx64": wrong size", cookie);
+		mobj_put(mobj);
+		return NULL;
+	}
+
 	if (mobj_inc_map(mobj)) {
 		DMSG("mobj_inc_map(%#"PRIx64"): failed", cookie);
 		mobj_put(mobj);