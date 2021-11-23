OP-TEE/optee_os

commit f1cf4b7994dec9c8aa013e68f7188dd4f062702c

Add support for Renesas RZ/N1 platform
Add support for RZ/N1 platform from Renasas (PLATFORM=rzn1):
- Cortex-A7 based dual core processor.

This platform supports TrustZone based IO register access control, so
add corresponding OEM service based implementation.

Link: https://www.renesas.com/us/en/products/microcontrollers-microprocessors/rz/rzn/rzn1d.html
Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-rzn1/sm_platform_handler.c
@@ -1,0 +1,98 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+#include <console.h>
+#include <io.h>
+#include <kernel/tee_misc.h>
+#include <mm/core_memprot.h>
+#include <sm/optee_smc.h>
+#include <sm/sm.h>
+
+#include "rzn1_regauth.h"
+
+#define RZN1_OEM_CONSOLE_PUTC		0x01
+#define RZN1_OEM_SYSREG_AUTH		0x10
+
+static const struct regauth_t regauth_pass = {.rmask = ~0U, .wmask = ~0U};
+
+static const struct regauth_t *get_regauth(unsigned long paddr)
+{
+	unsigned int idx = 0;
+	unsigned int len = ARRAY_SIZE(regauth);
+
+	while (idx < len) {
+		if (core_is_buffer_inside(paddr, sizeof(uint32_t),
+					  regauth[idx].paddr,
+					  regauth[idx].size))
+			return &regauth[idx];
+		idx++;
+	}
+
+	return NULL;
+}
+
+static uint32_t oem_sysreg(uint32_t addr, uint32_t mask, uint32_t *pvalue)
+{
+	vaddr_t reg = 0;
+	const struct regauth_t *auth = get_regauth(addr);
+
+	/* Allow operations on registers not in the list */
+	if (!auth)
+		auth = &regauth_pass;
+
+	reg = core_mmu_get_va(addr, MEM_AREA_IO_SEC);
+
+	if (mask) {
+		/* Write operation */
+		mask &= auth->wmask;
+		if (!reg || !mask)
+			DMSG("Blocking write of 0x%"PRIx32" to register 0x%"
+			     PRIx32" (0x%"PRIxVA")", *pvalue, addr, reg);
+		else
+			io_mask32(reg, *pvalue, mask);
+	} else {
+		/* Read operation */
+		if (!reg || !auth->rmask)
+			DMSG("Blocking read of register 0x%"PRIx32" (0x%"
+			     PRIxVA")", addr, reg);
+		else
+			*pvalue = io_read32(reg) & auth->rmask;
+	}
+
+	return 0;
+}
+
+static enum sm_handler_ret oem_service(struct sm_ctx *ctx __unused,
+				       struct thread_smc_args *args)
+{
+	switch (OPTEE_SMC_FUNC_NUM(args->a0)) {
+	case RZN1_OEM_SYSREG_AUTH:
+		args->a0 = oem_sysreg(args->a1, args->a2, &args->a3);
+		args->a1 = args->a3;
+		break;
+	case RZN1_OEM_CONSOLE_PUTC:
+		console_putc(args->a1);
+		break;
+	default:
+		return SM_HANDLER_PENDING_SMC;
+	}
+
+	return SM_HANDLER_SMC_HANDLED;
+}
+
+enum sm_handler_ret sm_platform_handler(struct sm_ctx *ctx)
+{
+	struct thread_smc_args *args = (void *)&ctx->nsec.r0;
+
+	if (!OPTEE_SMC_IS_FAST_CALL(args->a0))
+		return SM_HANDLER_PENDING_SMC;
+
+	switch (OPTEE_SMC_OWNER_NUM(args->a0)) {
+	case OPTEE_SMC_OWNER_OEM:
+		return oem_service(ctx, args);
+	default:
+		return SM_HANDLER_PENDING_SMC;
+	}
+}

.shippable.yml
@@ -150,5 +150,6 @@ build:
     - _make PLATFORM=bcm-ns3 CFG_ARM64_core=y
     - _make PLATFORM=hisilicon-hi3519av100_demo
     - _make PLATFORM=amlogic
+    - _make PLATFORM=rzn1
 
     - upload_cache

MAINTAINERS
@@ -163,6 +163,12 @@ R:	[@OP-TEE/plat-rcar]
 S:	Maintained
 F:	core/arch/arm/plat-rcar/
 
+Renesas RZ/N1
+R:	Sumit Garg <sumit.garg@linaro.org> [@b49020]
+R:	Linaro <op-tee@linaro.org> [@OP-TEE/linaro]
+S:	Maintained
+F:	core/arch/arm/plat-rzn1/
+
 Rockchip RK322X
 R:	Rockchip <op-tee@rock-chips.com>
 R:	[@OP-TEE/plat-rockchip]

core/arch/arm/plat-rzn1/a7_plat_init.S
@@ -1,0 +1,68 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2016, Wind River Systems.
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+/*
+ * Entry points for the A7 init.
+ *
+ * Assumptions:
+ * - No stack is available when these routines are called.
+ * - Each routine is called with return address in LR and
+ *   with ARM registers R0, R1, R2, R3 being scratchable.
+ */
+
+#include <arm32.h>
+#include <arm32_macros.S>
+#include <asm.S>
+#include <kernel/unwind.h>
+#include <platform_config.h>
+
+.section .text
+.balign 4
+.code 32
+
+FUNC plat_cpu_reset_early , :
+UNWIND( .fnstart)
+
+	/*
+	 * SCR = 0x00000020
+	 * - FW: Disallow NSec to mask FIQ [bit4=0]
+	 * - AW: Allow NSec to manage Imprecise Abort [bit5=1]
+	 * - EA: Imprecise Abort trapped to Abort Mode [bit3=0]
+	 * - FIQ: In Sec world, FIQ trapped to FIQ Mode [bit2=0]
+	 * - IRQ: IRQ always trapped to IRQ Mode [bit1=0]
+	 * - NS: Secure World [bit0=0]
+	 */
+	mov r0, #SCR_AW
+	write_scr r0
+
+	mov_imm r0, 0x00000000
+	write_sctlr r0
+
+	/*
+	 * ACTRL = 0x00006040
+	 * - DDI: Disable dual issue [bit28=0]
+	 * - DDVM: Disable Distributed Virtual Memory transactions [bit15=0]
+	 * - L1PCTL: L1 Data prefetch control [bit14:13=2b11]
+	 * - L1RADIS: L1 Data Cache read-allocate mode disable [bit12=0]
+	 * - L2RADIS: L2 Data Cache read-allocate mode disable [bit11=0]
+	 * - DODMBS: Disable optimized data memory barrier behavior [bit10=0]
+	 * - SMP: Enables coherent requests to the processor [bit6=0]
+	 */
+	mov_imm r0, 0x00006040
+	write_actlr r0
+
+	/*
+	 * NSACR = 0x00000C00
+	 * - NS_SMP: Non-secure mode cannot change ACTRL.SMP (bit18=0)
+	 * - NSASEDIS/NSD32DIS/CP10/CP11: Non-secure mode can use SIMD/VFP
+	 *                                (bit15:14=2b00, bit11:10=2b11)
+	 */
+	mov_imm r0, 0x00000C00
+	write_nsacr r0
+
+	mov pc, lr
+UNWIND( .fnend)
+END_FUNC plat_cpu_reset_early

core/arch/arm/plat-rzn1/conf.mk
@@ -1,0 +1,26 @@
+PLATFORM_FLAVOR ?= rzn1
+
+include core/arch/arm/cpu/cortex-a7.mk
+
+$(call force,CFG_ARM32_core,y)
+$(call force,CFG_TEE_CORE_NB_CORE,2)
+$(call force,CFG_BOOT_SECONDARY_REQUEST,y)
+$(call force,CFG_SECONDARY_INIT_CNTFRQ,y)
+$(call force,CFG_PSCI_ARM32,y)
+$(call force,CFG_16550_UART,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_WITH_PAGER,n)
+$(call force,CFG_GIC,y)
+$(call force,CFG_SM_PLATFORM_HANDLER,y)
+$(call force,CFG_TA_FLOAT_SUPPORT,n)
+
+ta-targets = ta_arm32
+
+CFG_TZDRAM_START ?= 0x88000000
+CFG_TZDRAM_SIZE ?= 0x00A00000
+CFG_SHMEM_START ?= 0x87C00000
+CFG_SHMEM_SIZE  ?= 0x00400000
+CFG_TEE_RAM_VA_SIZE ?= 0x00200000
+
+CFG_NUM_THREADS ?= 4
+CFG_NS_ENTRY_ADDR ?= 0x87A00000

core/arch/arm/plat-rzn1/link.mk
@@ -1,0 +1,8 @@
+include core/arch/arm/kernel/link.mk
+
+all: $(link-out-dir)/tee-raw.bin
+
+cleanfiles += $(link-out-dir)/tee-raw.bin
+$(link-out-dir)/tee-raw.bin: $(link-out-dir)/tee.elf scripts/gen_tee_bin.py
+	@$(cmd-echo-silent) '  GEN     $@'
+	$(q)scripts/gen_tee_bin.py --input $< --out_tee_raw_bin $@

core/arch/arm/plat-rzn1/main.c
@@ -1,0 +1,72 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2017, Schneider Electric
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+#include <arm.h>
+#include <console.h>
+#include <drivers/gic.h>
+#include <drivers/ns16550.h>
+#include <kernel/boot.h>
+#include <kernel/interrupt.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+#include <mm/core_mmu.h>
+#include <platform_config.h>
+#include <rzn1_tz.h>
+
+static struct gic_data gic_data;
+static struct ns16550_data console_data;
+
+register_phys_mem(MEM_AREA_IO_SEC, GIC_BASE, CORE_MMU_PGDIR_SIZE);
+register_phys_mem(MEM_AREA_IO_SEC, PERIPH_REG_BASE, CORE_MMU_PGDIR_SIZE);
+register_ddr(DRAM_BASE, DRAM_SIZE);
+
+void console_init(void)
+{
+	ns16550_init(&console_data, CONSOLE_UART_BASE, IO_WIDTH_U32, 2);
+	register_serial_console(&console_data.chip);
+}
+
+void main_init_gic(void)
+{
+	vaddr_t gicc_base = 0;
+	vaddr_t gicd_base = 0;
+
+	gicc_base = (vaddr_t)phys_to_virt(GICC_BASE, MEM_AREA_IO_SEC);
+	gicd_base = (vaddr_t)phys_to_virt(GICD_BASE, MEM_AREA_IO_SEC);
+	if (!gicc_base || !gicd_base)
+		panic();
+
+	gic_init(&gic_data, gicc_base, gicd_base);
+
+	itr_init(&gic_data.chip);
+}
+
+void main_secondary_init_gic(void)
+{
+	gic_cpu_init(&gic_data);
+}
+
+static TEE_Result rzn1_tz_init(void)
+{
+	vaddr_t tza_init_reg = 0;
+	vaddr_t tza_targ_reg = 0;
+
+	tza_init_reg = core_mmu_get_va(FW_STATIC_TZA_INIT, MEM_AREA_IO_SEC);
+	tza_targ_reg = core_mmu_get_va(FW_STATIC_TZA_TARG, MEM_AREA_IO_SEC);
+
+	/* TZ initiator ports */
+	io_write32(tza_init_reg, TZ_INIT_CSA_SEC | TZ_INIT_YS_SEC |
+				 TZ_INIT_YC_SEC | TZ_INIT_YD_SEC);
+
+	/* TZ target ports */
+	io_write32(tza_targ_reg, TZ_TARG_PC_SEC | TZ_TARG_QB_SEC |
+				 TZ_TARG_QA_SEC | TZ_TARG_UB_SEC |
+				 TZ_TARG_UA_SEC);
+
+	return TEE_SUCCESS;
+}
+
+service_init(rzn1_tz_init);

core/arch/arm/plat-rzn1/platform_config.h
@@ -1,0 +1,32 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017, Schneider Electric
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+#include <mm/generic_ram_layout.h>
+
+/* DRAM */
+#define DRAM_BASE			0x80000000
+#define DRAM_SIZE			0x10000000
+
+/* GIC */
+#define GIC_BASE			0x44100000
+#define GICD_OFFSET			0x1000
+#define GICC_OFFSET			0x2000
+#define GICD_BASE			(GIC_BASE + GICD_OFFSET)
+#define GICC_BASE			(GIC_BASE + GICC_OFFSET)
+
+/* Peripheral memory map */
+#define PERIPH_REG_BASE			0x40000000
+
+/* System Control */
+#define SYSCTRL_BASE			0x4000C000
+
+/* UART */
+#define CONSOLE_UART_BASE		0x40060000
+
+#endif /* PLATFORM_CONFIG_H */

commit 03b56a9c59af9deb943de5db1a2dcda793d6cd5c

plat-rzn1: fix unmasked register writes
When writing all 32 bits of a register, there is no need for iomask_32
which performs a read-modify-write operation. Not only is it faster, but
certain hardware registers are write-only or have side effects on read.

Using iomask_32 was found to cause the following issues on RZ/N1:
- accessing the I2C EEPROM gives timeout errors on read
- serial console drops characters on input (eg. when pasting)
Switching to io_write32 for non-masked writes fixes the issues.

Fixes: f1cf4b79 ("Add support for Renesas RZ/N1 platform")
Signed-off-by: Ralph Siemsen <ralph.siemsen@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Sumit Garg <sumit.garg@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-rzn1/sm_platform_handler.c
@@ -50,6 +50,8 @@ static uint32_t oem_sysreg(uint32_t addr, uint32_t mask, uint32_t *pvalue)
 		if (!reg || !mask)
 			DMSG("Blocking write of 0x%"PRIx32" to register 0x%"
 			     PRIx32" (0x%"PRIxVA")", *pvalue, addr, reg);
+		else if (mask == ~0UL)
+			io_write32(reg, *pvalue);
 		else
 			io_mask32(reg, *pvalue, mask);
 	} else {