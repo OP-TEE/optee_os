16ac67a785f5e93c99a4bac33fd65b7a1c9b240e
Jens Wiklander <jens.wiklander@linaro.org>
2021-11-24 12:13:20 +0100

    libmbedtls: disable -Wdeclaration-after-statement
    Disabled -Wdeclaration-after-statement when compiling the mbedtls
    library in order to avoid that kind of warnings from upstream code.
    
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libmbedtls/sub.mk
+++ lib/libmbedtls/sub.mk
@@ -124,6 +124,7 @@ srcs-$(sm-$(ta-target)) += $(addprefix mbedtls/library/, $(SRCS_TLS))
 
 cflags-lib-y += -Wno-redundant-decls
 cflags-lib-y += -Wno-switch-default
+cflags-lib-y += -Wno-declaration-after-statement
 
 ifeq ($(CFG_CRYPTOLIB_NAME_mbedtls),y)
 subdirs-$(sm-core) += core
