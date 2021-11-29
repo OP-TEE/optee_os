3b83f117d7548b898e65ae19ac9f190d7572d904
Jerome Forissier <jerome@forissier.org>
2021-11-29 18:57:43 +0100

    core: move FS files out of CFG_WITH_USER_TA
    Some files which are currently guarded with CFG_WITH_USER_TA should be
    guarded with _CFG_WITH_SECURE_STORAGE or FS-specific configs (CFG_RPMB_FS,
    CFG_RPMB_FS). This will allow the use of secure storage from kernel code
    when CFG_WITH_USER_TA=n.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/tee/sub.mk
+++ core/tee/sub.mk
@@ -24,25 +24,27 @@ srcs-$(CFG_CRYPTO_CONCAT_KDF) += tee_cryp_concat_kdf.c
 srcs-$(CFG_CRYPTO_PBKDF2) += tee_cryp_pbkdf2.c
 
 ifeq ($(CFG_WITH_USER_TA),y)
-
+srcs-y += tee_obj.c
 srcs-y += tee_svc.c
-cppflags-tee_svc.c-y += -DTEE_IMPL_VERSION=$(TEE_IMPL_VERSION)
 srcs-y += tee_svc_cryp.c
 srcs-y += tee_svc_storage.c
-srcs-$(CFG_RPMB_FS) += tee_rpmb_fs.c
-srcs-$(CFG_REE_FS) += tee_ree_fs.c
-srcs-$(CFG_REE_FS) += fs_htree.c
-srcs-$(CFG_REE_FS) += fs_dirfile.c
-srcs-$(CFG_REE_FS) += tee_fs_rpc.c
-srcs-y += tee_fs_key_manager.c
-srcs-y += tee_obj.c
-srcs-y += tee_pobj.c
+cppflags-tee_svc.c-y += -DTEE_IMPL_VERSION=$(TEE_IMPL_VERSION)
 srcs-y += tee_time_generic.c
 srcs-$(CFG_SECSTOR_TA) += tadb.c
 srcs-$(CFG_GP_SOCKETS) += socket.c
 srcs-y += tee_ta_enc_manager.c
-
 endif #CFG_WITH_USER_TA,y
+
+srcs-$(_CFG_WITH_SECURE_STORAGE) += tee_fs_key_manager.c
+srcs-$(CFG_RPMB_FS) += tee_rpmb_fs.c
+srcs-$(CFG_REE_FS) += tee_ree_fs.c
+srcs-$(CFG_REE_FS) += fs_dirfile.c
+srcs-$(CFG_REE_FS) += fs_htree.c
+srcs-$(CFG_REE_FS) += tee_fs_rpc.c
+
+ifeq ($(call cfg-one-enabled,CFG_WITH_USER_TA _CFG_WITH_SECURE_STORAGE),y)
+srcs-y += tee_pobj.c
+endif
 
 srcs-y += uuid.c
 srcs-y += tee_supp_plugin_rpc.c
