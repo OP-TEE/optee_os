OP-TEE/optee_os

commit f498c4042931108699ecc3d692cd2250700f8756

core: dt_driver: factorize clk_get_provider_by_*()
Move rename clk_get_provider_by_{node|phandle}() to
dt_driver_get_provider_by_*().

Acked-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Clément Léger <clement.leger@bootlin.com>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/clk/clk_dt.c
@@ -13,28 +13,6 @@
 #include <libfdt.h>
 #include <stddef.h>
 
-static struct dt_driver_provider *clk_get_provider_by_node(int nodeoffset)
-{
-	struct dt_driver_provider *prv = NULL;
-
-	SLIST_FOREACH(prv, &dt_driver_provider_list, link)
-		if (prv->nodeoffset == nodeoffset)
-			return prv;
-
-	return NULL;
-}
-
-static struct dt_driver_provider *clk_get_provider_by_phandle(uint32_t phandle)
-{
-	struct dt_driver_provider *prv = NULL;
-
-	SLIST_FOREACH(prv, &dt_driver_provider_list, link)
-		if (prv->phandle == phandle)
-			return prv;
-
-	return NULL;
-}
-
 static struct clk *clk_dt_get_from_provider(struct dt_driver_provider *prv,
 					    unsigned int clock_cells,
 					    const uint32_t *prop)
@@ -91,7 +69,7 @@ static struct clk *clk_dt_get_by_idx_prop(const char *prop_name,
 		idx32 = idx / sizeof(uint32_t);
 		phandle = fdt32_to_cpu(prop[idx32]);
 
-		prv = clk_get_provider_by_phandle(phandle);
+		prv = dt_driver_get_provider_by_node(phandle);
 		if (!prv)
 			return NULL;
 
@@ -232,7 +210,7 @@ static TEE_Result clk_probe_clock_provider_node(const void *fdt, int node)
 		return TEE_ERROR_ITEM_NOT_FOUND;
 
 	/* Check if node has already been probed */
-	if (clk_get_provider_by_node(node))
+	if (dt_driver_get_provider_by_node(node))
 		return TEE_SUCCESS;
 
 	/* Check if the node has a clock property first to probe parent */

core/include/kernel/dt_driver.h
@@ -80,6 +80,16 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
 				       void *data, enum dt_driver_type type);
 
 /*
+ * Return driver provider reference from its node offset value in the FDT
+ */
+struct dt_driver_provider *dt_driver_get_provider_by_node(int nodeoffset);
+
+/*
+ * Return driver provider reference from its phandle value in the FDT
+ */
+struct dt_driver_provider *dt_driver_get_provider_by_phandle(uint32_t phandle);
+
+/*
  * Return number cells used for phandle arguments by a driver provider
  */
 unsigned int dt_driver_provider_cells(struct dt_driver_provider *prv);

core/kernel/dt_driver.c
@@ -104,3 +104,25 @@ unsigned int dt_driver_provider_cells(struct dt_driver_provider *prv)
 {
 	return prv->provider_cells;
 }
+
+struct dt_driver_provider *dt_driver_get_provider_by_node(int nodeoffset)
+{
+	struct dt_driver_provider *prv = NULL;
+
+	SLIST_FOREACH(prv, &dt_driver_provider_list, link)
+		if (prv->nodeoffset == nodeoffset)
+			return prv;
+
+	return NULL;
+}
+
+struct dt_driver_provider *dt_driver_get_provider_by_phandle(uint32_t phandle)
+{
+	struct dt_driver_provider *prv = NULL;
+
+	SLIST_FOREACH(prv, &dt_driver_provider_list, link)
+		if (prv->phandle == phandle)
+			return prv;
+
+	return NULL;
+}

commit 9ade3190c0ba2e679b3ce12b535e164f515eeaa1

core: dt_driver: use driver type when finding a driver provider
Adds driver type argument to functions dt_driver_get_provider_by_node(),
and dt_driver_get_provider_by_phandle() to differentiate driver provider
references when a single DT node relates to several driver providers
that are of different type by DT binding definition. For example, a DT
node may describe a device that acts both as a as a clock provider and
a reset controller, for which two driver references are needed in the
driver provider list.

Updates dt_driver_device_from_node_idx_prop() accordingly.

Fixes: f498c4042931 ("core: dt_driver: factorize clk_get_provider_by_*()")
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/clk/clk_dt.c
@@ -33,6 +33,7 @@ static struct clk *clk_dt_get_by_idx_prop(const char *prop_name,
 {
 	void *device = dt_driver_device_from_node_idx_prop(prop_name, fdt,
 							   nodeoffset, clk_idx,
+							   DT_DRIVER_CLK,
 							   res);
 
 	return (struct clk *)device;
@@ -100,7 +101,7 @@ static TEE_Result clk_probe_clock_provider_node(const void *fdt, int node)
 		return TEE_ERROR_ITEM_NOT_FOUND;
 
 	/* Check if node has already been probed */
-	if (dt_driver_get_provider_by_node(node))
+	if (dt_driver_get_provider_by_node(node, DT_DRIVER_CLK))
 		return TEE_SUCCESS;
 
 	/* Check if the node has a clock property first to probe parent */

core/include/kernel/dt_driver.h
@@ -68,6 +68,7 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
  * @fdt: FDT base address
  * @nodeoffset: node offset in the FDT
  * @prop_idx: index of the phandle data in the property
+ * @type: Driver type
  * @res: Output result code of the operation: TEE_ERROR_BUSY is target
  *	device is not yet initialized, otherwise any other compliant code.
  *
@@ -77,17 +78,20 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
 					  unsigned int prop_idx,
+					  enum dt_driver_type type,
 					  TEE_Result *res);
 
 /*
  * Return driver provider reference from its node offset value in the FDT
  */
-struct dt_driver_provider *dt_driver_get_provider_by_node(int nodeoffset);
+struct dt_driver_provider *
+dt_driver_get_provider_by_node(int nodeoffset, enum dt_driver_type type);
 
 /*
  * Return driver provider reference from its phandle value in the FDT
  */
-struct dt_driver_provider *dt_driver_get_provider_by_phandle(uint32_t phandle);
+struct dt_driver_provider *
+dt_driver_get_provider_by_phandle(uint32_t phandle, enum dt_driver_type type);
 
 /*
  * Return number cells used for phandle arguments by a driver provider

core/kernel/dt_driver.c
@@ -182,23 +182,25 @@ unsigned int dt_driver_provider_cells(struct dt_driver_provider *prv)
 	return prv->provider_cells;
 }
 
-struct dt_driver_provider *dt_driver_get_provider_by_node(int nodeoffset)
+struct dt_driver_provider *
+dt_driver_get_provider_by_node(int nodeoffset, enum dt_driver_type type)
 {
 	struct dt_driver_provider *prv = NULL;
 
 	SLIST_FOREACH(prv, &dt_driver_provider_list, link)
-		if (prv->nodeoffset == nodeoffset)
+		if (prv->nodeoffset == nodeoffset && prv->type == type)
 			return prv;
 
 	return NULL;
 }
 
-struct dt_driver_provider *dt_driver_get_provider_by_phandle(uint32_t phandle)
+struct dt_driver_provider *
+dt_driver_get_provider_by_phandle(uint32_t phandle, enum dt_driver_type type)
 {
 	struct dt_driver_provider *prv = NULL;
 
 	SLIST_FOREACH(prv, &dt_driver_provider_list, link)
-		if (prv->phandle == phandle)
+		if (prv->phandle == phandle && prv->type == type)
 			return prv;
 
 	return NULL;
@@ -233,6 +235,7 @@ static void *device_from_provider_prop(struct dt_driver_provider *prv,
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
 					  unsigned int prop_idx,
+					  enum dt_driver_type type,
 					  TEE_Result *res)
 {
 	int len = 0;
@@ -253,7 +256,7 @@ void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 		idx32 = idx / sizeof(uint32_t);
 		phandle = fdt32_to_cpu(prop[idx32]);
 
-		prv = dt_driver_get_provider_by_phandle(phandle);
+		prv = dt_driver_get_provider_by_phandle(phandle, type);
 		if (!prv) {
 			*res = TEE_ERROR_GENERIC;
 			return NULL;