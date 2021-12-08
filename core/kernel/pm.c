OP-TEE/optee_os

commit 5920ec258f16b84f301ae95a37ffc14802e4f8a6

core: kernel: pm: refine suspend/resume sequence order
Change PM generic implementation to call handlers for suspend in the
reserve order in which they were registered and to call handlers for
resume in the opposite order, that is the order in which the handlers
where registered.

This change allows to reuse the driver initialization ordering which is
expected to match device drivers dependencies. This change will be even
more useful when using probe support and dependencies resolution.

The change does not affect the 2 PM registration levels: core and drivers.
Core handlers are still suspends after drivers and resumed before
drivers.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Gatien Chevallier <gatien.chevallier@st.com>

core/kernel/pm.c
@@ -6,8 +6,8 @@
 #include <keep.h>
 #include <kernel/panic.h>
 #include <kernel/pm.h>
+#include <malloc.h>
 #include <mm/core_memprot.h>
-#include <stdlib.h>
 #include <string.h>
 #include <types_ext.h>
 
@@ -59,29 +59,59 @@ void register_pm_cb(struct pm_callback_handle *pm_hdl)
 	pm_cb_ref = ref;
 }
 
+static TEE_Result do_pm_callback(enum pm_op op, uint32_t pm_hint,
+				 struct pm_callback_handle *hdl)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	bool suspending = op == PM_OP_SUSPEND;
+
+	if (suspending == (bool)(hdl->flags | PM_FLAG_SUSPENDED))
+		return TEE_SUCCESS;
+
+	DMSG("%s %s", suspending ? "Suspend" : "Resume", hdl->name);
+
+	res = hdl->callback(op, pm_hint, hdl);
+	if (res) {
+		EMSG("%s %s (%p) failed: %#"PRIx32, suspending ? "Suspend" :
+		     "Resume", hdl->name, (void *)(vaddr_t)hdl->callback, res);
+		return res;
+	}
+
+	if (suspending)
+		hdl->flags |= PM_FLAG_SUSPENDED;
+	else
+		hdl->flags &= ~PM_FLAG_SUSPENDED;
+
+	return TEE_SUCCESS;
+}
+
 static TEE_Result call_callbacks(enum pm_op op, uint32_t pm_hint,
 				 enum pm_callback_order order)
 {
-	struct pm_callback_handle *hdl;
-	size_t n;
-	TEE_Result res;
+	struct pm_callback_handle *hdl = NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	size_t n = 0;
 
-	for (n = 0, hdl = pm_cb_ref; n < pm_cb_count; n++, hdl++) {
-		if (hdl->order != order ||
-		    (hdl->flags & PM_FLAG_SUSPENDED) == (op == PM_OP_SUSPEND))
-			continue;
+	/*
+	 * Suspend first the last registered instances.
+	 * Resume first the first registered instances.
+	 */
+	if (op == PM_OP_SUSPEND)
+		hdl = pm_cb_ref + pm_cb_count - 1;
+	else
+		hdl = pm_cb_ref;
 
-		DMSG("%s %s (%p)", op == PM_OP_SUSPEND ? "Suspend" : "Resume",
-		     hdl->name, (void *)hdl->callback);
-
-		res = hdl->callback(op, pm_hint, hdl);
-		if (res)
-			return res;
+	for (n = 0; n < pm_cb_count; n++) {
+		if (hdl->order == order) {
+			res = do_pm_callback(op, pm_hint, hdl);
+			if (res)
+				return res;
+		}
 
 		if (op == PM_OP_SUSPEND)
-			hdl->flags |= PM_FLAG_SUSPENDED;
+			hdl--;
 		else
-			hdl->flags &= ~PM_FLAG_SUSPENDED;
+			hdl++;
 	}
 
 	return TEE_SUCCESS;

commit bd6928fc456ee1eea947847bd20ef5bacf90152b

core: pm: fix incorrect flag check in pm_callback
Fix test error check that always return true with current
condition. The check must be done to identify if the
SUSPENDED flag has been set.

Fixes: 5920ec258f16 ("core: kernel: pm: refine suspend/resume sequence order")
Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/pm.c
@@ -65,7 +65,7 @@ static TEE_Result do_pm_callback(enum pm_op op, uint32_t pm_hint,
 	TEE_Result res = TEE_ERROR_GENERIC;
 	bool suspending = op == PM_OP_SUSPEND;
 
-	if (suspending == (bool)(hdl->flags | PM_FLAG_SUSPENDED))
+	if (suspending == (bool)(hdl->flags & PM_FLAG_SUSPENDED))
 		return TEE_SUCCESS;
 
 	DMSG("%s %s", suspending ? "Suspend" : "Resume", hdl->name);