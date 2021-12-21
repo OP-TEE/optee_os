b224894f4c344bd7340c51cf268e8797d8730fd4
Sumit Garg <sumit.garg@linaro.org>
2021-12-21 17:06:13 +0530

    libutils: provide __sprintf_chk implementation
    While building optee_test CXX test-cases natively on aarch64, OP-TEE
    build relies on toolchain provided by buildroot. The buildroot toolchain
    is built with flag: -fstack-protector-strong which requires
    __sprintf_chk symbol provided by standard glibc. For OP-TEE we use a
    customized libc which leads to below error:
    
    ...
    CC out/init.o
    CC out/os_test.o
    CC out/ta_entry.o
    CXX out/cxx_tests.o
    CC out/user_ta_header.o
    CPP out/ta.lds
    LD out/5b9e0e40-2636-11e1-ad9e-0002a5d5c51b.elf
    /home/sumit/optee_br/build/../toolchains/aarch64/bin/aarch64-linux-ld.bfd: /home/sumit/optee_br/toolchains/aarch64/bin/../lib/gcc/aarch64-buildroot-linux-gnu/10.3.0/../../../../aarch64-buildroot-linux-gnu/lib/../lib64/libstdc++.a(cp-demangle.o): in function d_append_num': cp-demangle.c:(.text+0x830): undefined reference to __sprintf_chk'
    ...
    
    Fix this issue by providing __sprintf_chk implementation.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libutils/isoc/sprintf.c
+++ lib/libutils/isoc/sprintf.c
@@ -3,8 +3,10 @@
  * Copyright (c) 2020, Huawei Technologies Co., Ltd
  */
 
+#include <compiler.h>
 #include <printk.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int sprintf(char *str, const char *fmt, ...)
 {
@@ -14,6 +16,25 @@ int sprintf(char *str, const char *fmt, ...)
 	va_start(ap, fmt);
 	retval = __vsprintf(str, fmt, ap);
 	va_end(ap);
+
+	return retval;
+}
+
+int __sprintf_chk(char *str, int flag __unused, size_t slen,
+		  const char *fmt, ...)
+{
+	int retval;
+	va_list ap;
+
+	if (slen == 0)
+		abort();
+
+	va_start(ap, fmt);
+	retval = __vsnprintf(str, slen, fmt, ap, false);
+	va_end(ap);
+
+	if (retval > 0 && (size_t)retval >= slen)
+		abort();
 
 	return retval;
 }
