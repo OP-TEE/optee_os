c73f274f1ecacd7553e7ec4e56fcd1c343edc566
Jens Wiklander <jens.wiklander@linaro.org>
2022-01-10 12:01:43 +0100

    Update CHANGELOG for 3.16.0
    Update CHANGELOG for 3.16.0 and collect Tested-by tags.
    
    Tested-by: Jerome Forissier <jerome@forissier.org> (vexpress-qemu_armv8a)
    Tested-by: Jerome Forissier <jerome@forissier.org> (vexpress-qemu_armv8a, aarch64 host)
    Tested-by: Jerome Forissier <jerome@forissier.org> (hikey-hikey)
    Tested-by: Jerome Forissier <jerome@forissier.org> (hikey-hikey960)
    Tested-by: Igor Opaniuk <igor.opaniuk@gmail.com> (Poplar)
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (Juno)
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (imx-mx8mqevk)
    Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (FVP)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LS1012A-RDB)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LS1028A-RDB)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LS1043A-RDB)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LS1046A-RDB)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LS1088A-RDB)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LS2088A-RDB)
    Tested-by: Sahil Malhotra <sahil.malhotra@nxp.com> (LX2160A-RDB)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6dlsabreauto)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6dlsabresd)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6qpsabreauto)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6qsabresd)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6sllevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6ulevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6ullevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6ulzevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx6sxsabresd)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx7dsabresd)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx7ulpevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8mmevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8mnevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8mqevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8mpevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8qmmek)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8qxpmek)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8dxlevk)
    Tested-by: Clement Faure <clement.faure@nxp.com> (imx-mx8ulpevk)
    Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (stm32mp1, gp pkcs11)
    Tested-by: Ricardo Salveti <ricardo@foundries.io> (ZynqMP)
    Tested-by: Etienne Carriere <etienne.carriere@linaro.org> (vexpress-qemu_virt, gp pkcs11)
    Tested-by: Sumit Garg <sumit.garg@linaro.org> (Developerbox)
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- mk/config.mk
+++ mk/config.mk
@@ -127,7 +127,7 @@ CFG_OS_REV_REPORTS_GIT_SHA1 ?= y
 # with limited depth not including any tag, so there is really no guarantee
 # that TEE_IMPL_VERSION contains the major and minor revision numbers.
 CFG_OPTEE_REVISION_MAJOR ?= 3
-CFG_OPTEE_REVISION_MINOR ?= 15
+CFG_OPTEE_REVISION_MINOR ?= 16
 
 # Trusted OS implementation version
 TEE_IMPL_VERSION ?= $(shell git describe --always --dirty=-dev 2>/dev/null || \
