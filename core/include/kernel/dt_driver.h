OP-TEE/optee_os

commit d8b14b46af9d2c7cee7f875e389e1a659bd123da

core: dt_driver: get return code when querying a device
Change dt_driver_device_from_provider_prop() function and friends to
output return a TEE_Result code if failing to find target device
instance. Return code TEE_ERROR_DEFER_DRIVER_INIT reports that requested
device is not yet registered and initialized in the dt_driver provider
list.

This change will be used to probed device drivers and allow probe
deferral when a device depends on another device driver that is
not yet initialized.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/include/kernel/dt_driver.h
@@ -32,12 +32,14 @@ struct dt_driver_phandle_args {
  *
  * @parg: phandle argument(s) referencing the device in the FDT.
  * @data: driver private data registered in struct dt_driver.
+ * @res: Output result code of the operation: TEE_ERROR_BUSY is target
+ *	device is not yet initialized, otherwise any other compliant code.
  *
  * Return a device opaque reference, e.g. a struct clk pointer for a clock
- * driver, or NULL if not found.
+ * driver, or NULL if not found in which case @res provides the error code.
  */
 typedef void *(*get_of_device_func)(struct dt_driver_phandle_args *parg,
-				    void *data);
+				    void *data, TEE_Result *res);
 
 /**
  * dt_driver_register_provider - Register a driver provider
@@ -64,13 +66,16 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
  * @fdt: FDT base address
  * @nodeoffset: node offset in the FDT
  * @prop_idx: index of the phandle data in the property
+ * @res: Output result code of the operation: TEE_ERROR_BUSY is target
+ *	device is not yet initialized, otherwise any other compliant code.
  *
  * Return a device opaque reference, e.g. a struct clk pointer for a clock
- * driver, or NULL if not found.
+ * driver, or NULL if not found in which case @res provides the error code.
  */
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
-					  unsigned int prop_idx);
+					  unsigned int prop_idx,
+					  TEE_Result *res);
 
 /*
  * Return driver provider reference from its node offset value in the FDT

core/drivers/clk/clk_dt.c
@@ -14,31 +14,34 @@
 #include <stddef.h>
 
 struct clk *clk_dt_get_by_name(const void *fdt, int nodeoffset,
-			       const char *name)
+			       const char *name, TEE_Result *res)
 {
 	int clk_id = 0;
 
 	clk_id = fdt_stringlist_search(fdt, nodeoffset, "clock-names", name);
-	if (clk_id < 0)
+	if (clk_id < 0) {
+		*res = TEE_ERROR_GENERIC;
 		return NULL;
+	}
 
-	return clk_dt_get_by_idx(fdt, nodeoffset, clk_id);
+	return clk_dt_get_by_idx(fdt, nodeoffset, clk_id, res);
 }
 
 static struct clk *clk_dt_get_by_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
-					  unsigned int clk_idx)
+					  unsigned int clk_idx, TEE_Result *res)
 {
 	void *device = dt_driver_device_from_node_idx_prop(prop_name, fdt,
-							   nodeoffset, clk_idx);
+							   nodeoffset, clk_idx,
+							   res);
 
 	return (struct clk *)device;
 }
 
 struct clk *clk_dt_get_by_idx(const void *fdt, int nodeoffset,
-			      unsigned int clk_idx)
+			      unsigned int clk_idx, TEE_Result *res)
 {
-	return clk_dt_get_by_idx_prop("clocks", fdt, nodeoffset, clk_idx);
+	return clk_dt_get_by_idx_prop("clocks", fdt, nodeoffset, clk_idx, res);
 }
 
 /* Recursively called from parse_clock_property() */
@@ -134,6 +137,7 @@ static void parse_assigned_clock(const void *fdt, int nodeoffset)
 	unsigned long rate = 0;
 	struct clk *parent = NULL;
 	const uint32_t *rate_prop = NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
 
 	rate_prop = fdt_getprop(fdt, nodeoffset, "assigned-clock-rates",
 				&rate_len);
@@ -141,12 +145,12 @@ static void parse_assigned_clock(const void *fdt, int nodeoffset)
 
 	while (1) {
 		clk = clk_dt_get_by_idx_prop("assigned-clocks", fdt, nodeoffset,
-					     clock_idx);
+					     clock_idx, &res);
 		if (!clk)
 			return;
 
 		parent = clk_dt_get_by_idx_prop("assigned-clock-parents", fdt,
-						nodeoffset, clock_idx);
+						nodeoffset, clock_idx, &res);
 		if (parent) {
 			if (clk_set_parent(clk, parent)) {
 				EMSG("Could not set clk %s parent to clock %s",

core/include/drivers/clk_dt.h
@@ -35,11 +35,17 @@
  * @fdt: Device tree to work on
  * @nodeoffset: Node offset of the subnode containing a clock property
  * @clk_idx: Clock index to get
+ * @res: Output result code of the operation:
+ *	TEE_SUCCESS in case of success
+ *	TEE_ERROR_DEFER_DRIVER_INIT if clock is not initialized
+ *	Any TEE_Result compliant code in case of error.
+ *
  * Returns a clk struct pointer matching the clock at index clk_idx in clocks
- * property or NULL if no clock match the given index.
+ * property or NULL if no clock match the given index in which case @res
+ * provides the error code.
  */
 struct clk *clk_dt_get_by_idx(const void *fdt, int nodeoffset,
-			      unsigned int clk_idx);
+			      unsigned int clk_idx, TEE_Result *res);
 
 /**
  * clk_dt_get_by_name - Get a clock matching a name in the "clock-names"
@@ -48,23 +54,34 @@ struct clk *clk_dt_get_by_idx(const void *fdt, int nodeoffset,
  * @fdt: Device tree to work on
  * @nodeoffset: Node offset of the subnode containing a clock property
  * @name: Clock name to get
+ * @res: Output result code of the operation:
+ *	TEE_SUCCESS in case of success
+ *	TEE_ERROR_DEFER_DRIVER_INIT if clock is not initialized
+ *	Any TEE_Result compliant code in case of error.
+ *
  * Returns a clk struct pointer matching the name in "clock-names" property or
- * NULL if no clock match the given name.
+ * NULL if no clock match the given name in which case @res provides the
+ * error code.
  */
 struct clk *clk_dt_get_by_name(const void *fdt, int nodeoffset,
-			       const char *name);
+			       const char *name, TEE_Result *res);
 
 /**
  * clk_dt_get_func - Typedef of function to get clock from devicetree properties
  *
  * @args: Pointer to devicetree description of the clock to parse
  * @data: Pointer to data given at clk_dt_register_clk_provider() call
+ * @res: Output result code of the operation:
+ *	TEE_SUCCESS in case of success
+ *	TEE_ERROR_DEFER_DRIVER_INIT if clock is not initialized
+ *	Any TEE_Result compliant code in case of error.
  *
  * Returns a clk struct pointer pointing to a clock matching the devicetree
- * description or NULL if invalid description.
+ * description or NULL if invalid description in which case @res provides the
+ * error code.
  */
 typedef struct clk *(*clk_dt_get_func)(struct dt_driver_phandle_args *args,
-				       void *data);
+				       void *data, TEE_Result *res);
 
 /**
  * clk_dt_register_clk_provider - Register a clock provider
@@ -90,8 +107,10 @@ TEE_Result clk_dt_register_clk_provider(const void *fdt, int nodeoffset,
  */
 static inline
 struct clk *clk_dt_get_simple_clk(struct dt_driver_phandle_args *args __unused,
-				  void *data)
+				  void *data, TEE_Result *res)
 {
+	*res = TEE_SUCCESS;
+
 	return data;
 }
 

core/kernel/dt_driver.c
@@ -151,7 +151,8 @@ struct dt_driver_provider *dt_driver_get_provider_by_phandle(uint32_t phandle)
 }
 
 static void *device_from_provider_prop(struct dt_driver_provider *prv,
-				       const uint32_t *prop)
+					  const uint32_t *prop,
+					  TEE_Result *res)
 {
 	struct dt_driver_phandle_args *pargs = NULL;
 	unsigned int n = 0;
@@ -159,14 +160,16 @@ static void *device_from_provider_prop(struct dt_driver_provider *prv,
 
 	pargs = calloc(1, prv->provider_cells * sizeof(uint32_t *) +
 		       sizeof(*pargs));
-	if (!pargs)
+	if (!pargs) {
+		*res = TEE_ERROR_OUT_OF_MEMORY;
 		return NULL;
+	}
 
 	pargs->args_count = prv->provider_cells;
 	for (n = 0; n < prv->provider_cells; n++)
 		pargs->args[n] = fdt32_to_cpu(prop[n + 1]);
 
-	device = prv->get_of_device(pargs, prv->priv_data);
+	device = prv->get_of_device(pargs, prv->priv_data, res);
 
 	free(pargs);
 
@@ -175,7 +178,8 @@ static void *device_from_provider_prop(struct dt_driver_provider *prv,
 
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
-					  unsigned int prop_idx)
+					  unsigned int prop_idx,
+					  TEE_Result *res)
 {
 	int len = 0;
 	int idx = 0;
@@ -186,16 +190,20 @@ void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 	struct dt_driver_provider *prv = NULL;
 
 	prop = fdt_getprop(fdt, nodeoffset, prop_name, &len);
-	if (!prop)
+	if (!prop) {
+		*res = TEE_ERROR_GENERIC;
 		return NULL;
+	}
 
 	while (idx < len) {
 		idx32 = idx / sizeof(uint32_t);
 		phandle = fdt32_to_cpu(prop[idx32]);
 
 		prv = dt_driver_get_provider_by_phandle(phandle);
-		if (!prv)
+		if (!prv) {
+			*res = TEE_ERROR_GENERIC;
 			return NULL;
+		}
 
 		prv_cells = dt_driver_provider_cells(prv);
 		if (prop_idx) {
@@ -204,9 +212,10 @@ void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 			continue;
 		}
 
-		return device_from_provider_prop(prv, prop + idx32);
+		return device_from_provider_prop(prv, prop + idx32, res);
 	}
 
+	*res = TEE_ERROR_GENERIC;
 	return NULL;
 }
 

commit f83354bebae766aedafc2ca280ce0a68b424ed38

core: dt_driver: fix result argument description get_of_device_func
Fixes inline description of type get_of_device_func that falsely mentions
TEE_ERROR_BUSY instead of TEE_ERROR_DEFER_DRIVER_INIT when expected
resource requests deferral of the driver probing.

Fixes: d8b14b46af9d ("core: dt_driver: get return code when querying a device")
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/include/kernel/dt_driver.h
@@ -34,8 +34,10 @@ struct dt_driver_phandle_args {
  *
  * @parg: phandle argument(s) referencing the device in the FDT.
  * @data: driver private data registered in struct dt_driver.
- * @res: Output result code of the operation: TEE_ERROR_BUSY is target
- *	device is not yet initialized, otherwise any other compliant code.
+ * @res: Output result code of the operation:
+ *	TEE_SUCCESS in case of success
+ *	TEE_ERROR_DEFER_DRIVER_INIT if clock is not initialized
+ *	Any TEE_Result compliant code in case of error.
  *
  * Return a device opaque reference, e.g. a struct clk pointer for a clock
  * driver, or NULL if not found in which case @res provides the error code.
@@ -69,8 +71,10 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
  * @nodeoffset: node offset in the FDT
  * @prop_idx: index of the phandle data in the property
  * @type: Driver type
- * @res: Output result code of the operation: TEE_ERROR_BUSY is target
- *	device is not yet initialized, otherwise any other compliant code.
+ * @res: Output result code of the operation:
+ *	TEE_SUCCESS in case of success
+ *	TEE_ERROR_DEFER_DRIVER_INIT if clock is not initialized
+ *	Any TEE_Result compliant code in case of error.
  *
  * Return a device opaque reference, e.g. a struct clk pointer for a clock
  * driver, or NULL if not found in which case @res provides the error code.