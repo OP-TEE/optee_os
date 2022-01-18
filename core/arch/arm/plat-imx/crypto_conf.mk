OP-TEE/optee_os

commit 2d7a8964dfcbe6c3494a0889831818dd8f153f4a

driver: implement CAAM driver
Add the NXP CAAM drivers:
  - Random generator (instantiation and random generation)
  - Hash

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-imx/crypto_conf.mk
@@ -1,0 +1,59 @@
+#
+# Define the cryptographic algorithm to be built
+#
+
+#
+# CAAM Debug Trace
+#
+# DBG_TRACE_HAL  BIT32(0)  // HAL trace
+# DBG_TRACE_CTRL BIT32(1)  // Controller trace
+# DBG_TRACE_MEM  BIT32(2)  // Memory utility trace
+# DBG_TRACE_PWR  BIT32(3)  // Power trace
+# DBG_TRACE_JR   BIT32(4)  // Job Ring trace
+# DBG_DESC_JR    BIT32(5)  // Job Ring dump descriptor
+# DBG_TRACE_RNG  BIT32(6)  // RNG trace
+# DBG_DESC_RNG   BIT32(7)  // RNG dump descriptor
+# DBG_TRACE_HASH BIT32(8)  // Hash trace
+# DBG_DESC_HASH  BIT32(9)  // Hash dump descriptor
+# DBG_BUF_HASH   BIT32(10) // Hash dump Buffer
+CFG_CAAM_DBG ?= 0x2
+
+#
+# CAAM Job Ring configuration
+#  - Normal boot settings
+#  - HAB support boot settings
+#
+$(call force, CFG_JR_BLOCK_SIZE,0x1000)
+
+$(call force, CFG_JR_INDEX,0)  # Default JR index used
+$(call force, CFG_JR_INT,137)  # Default JR IT Number (105 + 32) = 137
+
+#
+# Configuration of the Crypto Driver
+#
+ifeq ($(CFG_CRYPTO_DRIVER), y)
+
+$(call force, CFG_NXP_CAAM_RUNTIME_JR, y)
+
+#
+# Definition of all HW accelerations for all i.MX
+#
+$(call force, CFG_NXP_CAAM_RNG_DRV, y)
+$(call force, CFG_WITH_SOFTWARE_PRNG,n)
+
+# Force to 'y' the CFG_NXP_CAAM_xxx_DRV to enable the CAAM HW driver
+# and enable the associated CFG_CRYPTO_DRV_xxx Crypto driver
+# API
+#
+# Example: Enable CFG_CRYPTO_DRV_HASH and CFG_NXP_CAAM_HASH_DRV
+#     $(eval $(call cryphw-enable-drv-hw, HASH))
+define cryphw-enable-drv-hw
+_var := $(strip $(1))
+$$(call force, CFG_NXP_CAAM_$$(_var)_DRV, y)
+$$(call force, CFG_CRYPTO_DRV_$$(_var), y)
+endef
+
+# Definition of the HW and Cryto Driver Algorithm supported by all i.MX
+$(eval $(call cryphw-enable-drv-hw, HASH))
+
+endif

commit 295eb14db891cabe3cb0dbc985a241000e32f710

core: imx: crypto_conf: change CAAM default job ring for mscale platforms
Change the default CAAM job ring index from 0 to 2 for the following
platforms:
 * imx8mmevk
 * imx8mnevk
 * imx8mpevk
 * imx8mqevk
It leaves JR0 available for the HAB to authenticate and decrypt boot
images.

Fixes: 2d7a8964 ("driver: implement CAAM driver")
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-imx/crypto_conf.mk
@@ -41,6 +41,11 @@ $(call force, CFG_CAAM_SIZE_ALIGN,4)
 $(call force, CFG_JR_BLOCK_SIZE,0x10000)
 $(call force,CFG_JR_INDEX,3)
 $(call force,CFG_JR_INT,486)
+else ifneq (,$(filter y, $(CFG_MX8MM) $(CFG_MX8MN) $(CFG_MX8MP) $(CFG_MX8MQ)))
+$(call force,CFG_CAAM_SIZE_ALIGN,1)
+$(call force,CFG_JR_BLOCK_SIZE,0x1000)
+$(call force,CFG_JR_INDEX,2)
+$(call force,CFG_JR_INT,146)
 else
 $(call force, CFG_CAAM_SIZE_ALIGN,1)
 $(call force, CFG_JR_BLOCK_SIZE,0x1000)