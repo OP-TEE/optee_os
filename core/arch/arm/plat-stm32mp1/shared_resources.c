68467d165b79ae363cb7f872f26cebf23aa757db
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-01-18 11:54:41 +0100

    plat-stm32mp1: allow non-secure to read DDR registers
    Changes stm32mp1 ETZPC configuration to allow non-secure read access
    to DDRCTRL and DDRPHYC interface register. This change will be needed
    by next U-Boot release v2022.04 since merge of [1] in order to
    dynamically compute the DDR size.
    
    Link: [1] https://source.denx.de/u-boot/u-boot/-/commit/d72e7bbe7c2841f161848d57b723495a731d0121
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-stm32mp1/shared_resources.c
+++ core/arch/arm/plat-stm32mp1/shared_resources.c
@@ -620,8 +620,8 @@ static void set_etzpc_secure_configuration(void)
 	/* Some peripherals shall be secure */
 	config_lock_decprot(STM32MP1_ETZPC_STGENC_ID, ETZPC_DECPROT_S_RW);
 	config_lock_decprot(STM32MP1_ETZPC_BKPSRAM_ID, ETZPC_DECPROT_S_RW);
-	config_lock_decprot(STM32MP1_ETZPC_DDRCTRL_ID, ETZPC_DECPROT_S_RW);
-	config_lock_decprot(STM32MP1_ETZPC_DDRPHYC_ID, ETZPC_DECPROT_S_RW);
+	config_lock_decprot(STM32MP1_ETZPC_DDRCTRL_ID, ETZPC_DECPROT_NS_R_S_W);
+	config_lock_decprot(STM32MP1_ETZPC_DDRPHYC_ID, ETZPC_DECPROT_NS_R_S_W);
 
 	/* Configure ETZPC with peripheral registering */
 	config_lock_decprot(STM32MP1_ETZPC_IWDG1_ID,
