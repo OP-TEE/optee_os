OP-TEE/optee_os

commit 61bdedea9452abc1ec1582075fdd9cb9f81953a6

core: define DT drivers using scattered arrays
Replace the specific mechanism used to define and enumerate DT drivers
with scattered arrays. Doing so simplifies the TEE linker file a bit.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Suggested-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/dt.h
@@ -9,6 +9,7 @@
 #include <compiler.h>
 #include <kernel/interrupt.h>
 #include <kernel/panic.h>
+#include <scattered_array.h>
 #include <stdint.h>
 #include <tee_api_types.h>
 #include <types_ext.h>
@@ -100,7 +101,8 @@ struct dt_driver {
 	TEE_Result (*probe)(const void *fdt, int node, const void *compat_data);
 };
 
-#define __dt_driver __section(".rodata.dtdrv" __SECTION_FLAGS_RODATA)
+#define DEFINE_DT_DRIVER(name) \
+		SCATTERED_ARRAY_DEFINE_ITEM(dt_drivers, struct dt_driver)
 
 /*
  * Find a driver that is suitable for the given DT node, that is, with
@@ -110,10 +112,6 @@ struct dt_driver {
  * @offs: node offset
  */
 const struct dt_driver *dt_find_compatible_driver(const void *fdt, int offs);
-
-const struct dt_driver *__dt_driver_start(void);
-
-const struct dt_driver *__dt_driver_end(void);
 
 /*
  * Map a device into secure or non-secure memory and return the base VA and
@@ -197,16 +195,6 @@ void _fdt_fill_device_info(const void *fdt, struct dt_node_info *info,
 
 #else /* !CFG_DT */
 
-static inline const struct dt_driver *__dt_driver_start(void)
-{
-	return NULL;
-}
-
-static inline const struct dt_driver *__dt_driver_end(void)
-{
-	return NULL;
-}
-
 static inline const struct dt_driver *dt_find_compatible_driver(
 					const void *fdt __unused,
 					int offs __unused)
@@ -247,6 +235,8 @@ static inline void _fdt_fill_device_info(const void *fdt __unused,
 #endif /* !CFG_DT */
 
 #define for_each_dt_driver(drv) \
-	for (drv = __dt_driver_start(); drv < __dt_driver_end(); drv++)
+	for (drv = SCATTERED_ARRAY_BEGIN(dt_drivers, struct dt_driver); \
+	     drv < SCATTERED_ARRAY_END(dt_drivers, struct dt_driver); \
+	     drv++)
 
 #endif /* KERNEL_DT_H */

core/arch/arm/kernel/kern.ld.S
@@ -130,11 +130,6 @@ SECTIONS
 		*(.rodata .rodata.__unpaged .rodata.__unpaged.*)
 #include <rodata_unpaged.ld.S>
 #else
-#ifdef CFG_DT
-		__rodata_dtdrv_start = .;
-		KEEP(*(.rodata.dtdrv))
-		__rodata_dtdrv_end = .;
-#endif
 		*(.rodata .rodata.*)
 		. = ALIGN(8);
 		KEEP(*(SORT(.scattered_array*)));
@@ -341,11 +336,6 @@ SECTIONS
 	__flatmap_init_ro_size = __init_end - __flatmap_init_ro_start;
 
 	.rodata_pageable : ALIGN(8) {
-#ifdef CFG_DT
-		__rodata_dtdrv_start = .;
-		KEEP(*(.rodata.dtdrv))
-		__rodata_dtdrv_end = .;
-#endif
 		*(.rodata*)
 	}
 

core/drivers/atmel_rstc.c
@@ -54,7 +54,7 @@ static const struct dt_device_match atmel_rstc_match_table[] = {
 	{ }
 };
 
-const struct dt_driver atmel_rstc_dt_driver __dt_driver = {
+DEFINE_DT_DRIVER(atmel_rstc_dt_driver) = {
 	.name = "atmel_rstc",
 	.type = DT_DRIVER_NOTYPE,
 	.match_table = atmel_rstc_match_table,

core/drivers/atmel_shdwc.c
@@ -174,7 +174,7 @@ static const struct dt_device_match atmel_shdwc_match_table[] = {
 	{ }
 };
 
-const struct dt_driver atmel_shdwc_dt_driver __dt_driver = {
+DEFINE_DT_DRIVER(atmel_shdwc_dt_driver) = {
 	.name = "atmel_shdwc",
 	.type = DT_DRIVER_NOTYPE,
 	.match_table = atmel_shdwc_match_table,

core/drivers/atmel_trng.c
@@ -147,7 +147,7 @@ static const struct dt_device_match atmel_trng_match_table[] = {
 	{ }
 };
 
-const struct dt_driver atmel_trng_dt_driver __dt_driver = {
+DEFINE_DT_DRIVER(atmel_trng_dt_driver) = {
 	.name = "atmel_trng",
 	.type = DT_DRIVER_NOTYPE,
 	.match_table = atmel_trng_match_table,

core/drivers/imx_lpuart.c
@@ -125,7 +125,7 @@ static const struct dt_device_match imx_match_table[] = {
 	{ 0 }
 };
 
-const struct dt_driver imx_dt_driver __dt_driver = {
+DEFINE_DT_DRIVER(imx_dt_driver) = {
 	.name = "imx_lpuart",
 	.type = DT_DRIVER_UART,
 	.match_table = imx_match_table,

core/drivers/imx_uart.c
@@ -192,7 +192,7 @@ static const struct dt_device_match imx_match_table[] = {
 	{ 0 }
 };
 
-const struct dt_driver imx_dt_driver __dt_driver = {
+DEFINE_DT_DRIVER(imx_dt_driver) = {
 	.name = "imx_uart",
 	.type = DT_DRIVER_UART,
 	.match_table = imx_match_table,

core/drivers/pl011.c
@@ -222,7 +222,7 @@ static const struct dt_device_match pl011_match_table[] = {
 	{ 0 }
 };
 
-const struct dt_driver pl011_dt_driver __dt_driver = {
+DEFINE_DT_DRIVER(pl011_dt_driver) = {
 	.name = "pl011",
 	.type = DT_DRIVER_UART,
 	.match_table = pl011_match_table,

commit de10969709416c939b710f93e4fb1807f32a5353

core: use a paged scattered array for DT drivers list
Fixes macro DEFINE_DT_DRIVER() to use SCATTERED_ARRAY_DEFINE_PG_ITEM()
that defines a pageable resources whereas SCATTERED_ARRAY_DEFINE_ITEM()
assumes array and its dependencies must be linked in the rodata_init
section. Indeed DT driver probing is done after pager is initialized.

Fixes: 61bdedea9452 ("core: define DT drivers using scattered arrays")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/include/kernel/dt.h
@@ -103,7 +103,7 @@ struct dt_driver {
 };
 
 #define DEFINE_DT_DRIVER(name) \
-		SCATTERED_ARRAY_DEFINE_ITEM(dt_drivers, struct dt_driver)
+		SCATTERED_ARRAY_DEFINE_PG_ITEM(dt_drivers, struct dt_driver)
 
 /*
  * Find a driver that is suitable for the given DT node, that is, with