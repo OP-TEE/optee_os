OP-TEE/optee_os

commit 41985789f3bd48fa0c1ae3d87d947043c8b54265

plat-vexpress: disable uart IT with TF-A and GICv3
Disables uart interrupts if compiled for TF-A and GICv3 since TF-A
doesn't know which interrupts OP-TEE will handle.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-vexpress/main.c
@@ -119,7 +119,16 @@ void console_init(void)
 	register_serial_console(&console_data.chip);
 }
 
-#ifdef IT_CONSOLE_UART
+#if defined(IT_CONSOLE_UART) && \
+	!(defined(CFG_WITH_ARM_TRUSTED_FW) && defined(CFG_ARM_GICV3))
+/*
+ * This cannot be enabled with TF-A and GICv3 because TF-A then need to
+ * assign the interrupt number of the UART to OP-TEE (S-EL1). Currently
+ * there's no way of TF-A to know which interrupts that OP-TEE will serve.
+ * If TF-A doesn't assign the interrupt we're enabling below to OP-TEE it
+ * will hang in EL3 since the interrupt will just be delivered again and
+ * again.
+ */
 static enum itr_return console_itr_cb(struct itr_handler *h __unused)
 {
 	struct serial_chip *cons = &console_data.chip;

commit 0ae59a48be7a44b4a8e2434677031650bf2ab92c

plat-vexpress: enable uart IT with TF-A and GICv3
With GICv3 TF-A doesn't need to know which interrupts are destined to
S-EL1 since this is programmed directly in the GICv3. On GICv2 that's
not possible so fix the ifdefs for this.

Always keep uart interrupts disabled with CFG_VIRTUALIZATION=y since
that requires more work to sort out.

Fixes: 41985789f3bd ("plat-vexpress: disable uart IT with TF-A and GICv3")
Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-vexpress/main.c
@@ -91,8 +91,8 @@ void console_init(void)
 	register_serial_console(&console_data.chip);
 }
 
-#if defined(IT_CONSOLE_UART) && \
-	!(defined(CFG_WITH_ARM_TRUSTED_FW) && defined(CFG_ARM_GICV3))
+#if defined(IT_CONSOLE_UART) && !defined(CFG_VIRTUALIZATION) && \
+	!(defined(CFG_WITH_ARM_TRUSTED_FW) && defined(CFG_ARM_GICV2))
 /*
  * This cannot be enabled with TF-A and GICv3 because TF-A then need to
  * assign the interrupt number of the UART to OP-TEE (S-EL1). Currently