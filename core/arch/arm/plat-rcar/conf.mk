68c566424e20b9b2d78196678792b59a0407b209
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2022-02-10 02:28:38 +0200

    plat: rcar: enable GIC support
    Enable GIC support in the same as it is done in other platforms.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-rcar/conf.mk
+++ core/arch/arm/plat-rcar/conf.mk
@@ -5,6 +5,7 @@ include core/arch/arm/cpu/cortex-armv8-0.mk
 $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_SCIF,y)
+$(call force,CFG_GIC,y)
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
 $(call force,CFG_CORE_ARM64_PA_BITS,36)
 $(call force,CFG_TEE_CORE_NB_CORE,8)
