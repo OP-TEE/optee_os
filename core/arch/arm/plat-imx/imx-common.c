OP-TEE/optee_os

commit 16e73240d00bc9c21722a552dc62bea0af25a5f2

core: imx: add CSU module
Centralize CSU settings in one file. Update it as a driver module.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

core/arch/arm/plat-imx/imx-common.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2017 NXP
+ * Copyright 2017-2019 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -42,6 +42,11 @@ uint32_t imx_soc_type(void)
 	return (imx_digproc() >> 16) & 0xff;
 }
 
+bool soc_is_imx6sx(void)
+{
+	return imx_soc_type() == SOC_MX6SX;
+}
+
 bool soc_is_imx6ul(void)
 {
 	return imx_soc_type() == SOC_MX6UL;
@@ -65,6 +70,15 @@ bool soc_is_imx6dq(void)
 bool soc_is_imx6dqp(void)
 {
 	return (imx_soc_type() == SOC_MX6Q) && (imx_soc_rev_major() == 2);
+}
+
+bool soc_is_imx6(void)
+{
+	return ((imx_soc_type() == SOC_MX6SX) ||
+			(imx_soc_type() == SOC_MX6UL) ||
+			(imx_soc_type() == SOC_MX6ULL) ||
+			(imx_soc_type() == SOC_MX6DL) ||
+			(imx_soc_type() == SOC_MX6Q));
 }
 
 bool soc_is_imx7ds(void)

core/arch/arm/plat-imx/conf.mk
@@ -220,6 +220,7 @@ CFG_DT ?= y
 CFG_PAGEABLE_ADDR ?= 0
 CFG_PSCI_ARM32 ?= y
 CFG_SECURE_TIME_SOURCE_REE ?= y
+CFG_CSU ?= y
 CFG_UART_BASE ?= UART1_BASE
 CFG_IMX_CAAM ?= y
 endif

core/arch/arm/plat-imx/drivers/imx_csu.c
@@ -1,0 +1,89 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2017-2019 NXP
+ *
+ */
+
+#include <imx.h>
+#include <initcall.h>
+#include <io.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+
+struct csu_setting {
+	int csu_index;
+	uint32_t value;
+};
+
+const struct csu_setting csu_setting_imx6[] = {
+	{13, 0xFF0033},		/* Protect ROMCP */
+	{16, 0x330033},		/* Protect TZASC */
+	{26, 0xFF0033},		/* Protect OCRAM */
+	{(-1), 0},
+};
+
+const struct csu_setting csu_setting_imx6ul[] = {
+	{13, 0xFF0033},		/* Protect ROMCP */
+	{16, 0x3300FF},		/* Protect TZASC */
+	{39, 0x3300FF},		/* Protect OCRAM */
+	{(-1), 0},
+};
+
+const struct csu_setting csu_setting_imx6sx[] = {
+	{13, 0xFF0033},		/* Protect ROMCP */
+	{15, 0xFF0033},		/* Protect RDC   */
+	{16, 0x3300FF},		/* Protect TZASC */
+	{34, 0x3300FF},		/* Protect OCRAM */
+	{(-1), 0},
+};
+
+const struct csu_setting csu_setting_imx7ds[] = {
+	{14, 0x3300FF},		/* Protect RDC     */
+	{15, 0xFF0033},		/* Protect CSU     */
+	{28, 0xFF0033},		/* Protect TZASC   */
+	{59, 0x3300FF},		/* Protect OCRAM_S */
+	{(-1), 0},
+};
+
+static TEE_Result csu_init(void)
+{
+	vaddr_t csu_base;
+	vaddr_t offset;
+	const struct csu_setting *csu_setting = NULL;
+
+	csu_base = core_mmu_get_va(CSU_BASE, MEM_AREA_IO_SEC);
+	if (!csu_base)
+		panic();
+
+	if (soc_is_imx6sx())
+		csu_setting = csu_setting_imx6sx;
+	else if (soc_is_imx6ul() || soc_is_imx6ull())
+		csu_setting = csu_setting_imx6ul;
+	else if (soc_is_imx6())
+		csu_setting = csu_setting_imx6;
+	else if (soc_is_imx7ds())
+		csu_setting = csu_setting_imx7ds;
+	else
+		return TEE_SUCCESS;
+
+	/* first grant all peripherals */
+	for (offset = CSU_CSL_START; offset < CSU_CSL_END; offset += 4)
+		io_write32(csu_base + offset, CSU_ACCESS_ALL);
+
+	while (csu_setting->csu_index > 0) {
+		io_write32(csu_base + (csu_setting->csu_index * 4),
+				csu_setting->value);
+
+		csu_setting++;
+	}
+
+	/* lock the settings */
+	for (offset = CSU_CSL_START; offset < CSU_CSL_END; offset += 4) {
+		io_write32(csu_base + offset,
+			io_read32(csu_base + offset) | CSU_SETTING_LOCK);
+	}
+
+	return TEE_SUCCESS;
+}
+
+driver_init(csu_init);

core/arch/arm/plat-imx/drivers/sub.mk
@@ -1,2 +1,3 @@
 srcs-$(CFG_TZC380) 	+= tzc380.c
 srcs-$(CFG_IMX_CAAM) 	+= imx_caam.c
+srcs-$(CFG_CSU) 	+= imx_csu.c

core/arch/arm/plat-imx/imx-regs.h
@@ -3,6 +3,7 @@
  * Copyright (C) 2015 Freescale Semiconductor, Inc.
  * Copyright (c) 2016, Wind River Systems.
  * All rights reserved.
+ * Copyright 2019 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -44,6 +45,7 @@
 #define GPC_BASE			0x020DC000
 #define GPC_SIZE			0x4000
 #define WDOG_BASE			0x020BC000
+#define CSU_BASE			0x021C0000
 #define SEMA4_BASE			0x02290000
 #define SEMA4_SIZE			0x4000
 #define MMDC_P0_BASE			0x021B0000
@@ -86,12 +88,8 @@
 #define GIC_DIST_BASE			(GIC_BASE + GICD_OFFSET)
 
 /* Central Security Unit register values */
-#define CSU_BASE			0x021C0000
 #define CSU_CSL_START			0x0
 #define CSU_CSL_END			0xA0
-#define CSU_CSL5			0x14
-#define CSU_CSL15			0x3C
-#define CSU_CSL16			0x40
 #define	CSU_ACCESS_ALL			0x00FF00FF
 #define CSU_SETTING_LOCK		0x01000100
 
@@ -168,6 +166,7 @@
 #define CCM_BASE		0x30380000
 #define SRC_BASE		0x30390000
 #define GPC_BASE		0x303A0000
+#define CSU_BASE		0x303E0000
 #define TZASC_BASE		0x30780000
 #define DDRC_PHY_BASE		0x30790000
 #define MMDC_P0_BASE		0x307A0000
@@ -175,12 +174,8 @@
 #define IRAM_BASE		0x00900000
 #define IRAM_S_BASE		0x00180000
 
-#define CSU_CSL_START		0x303E0000
-#define CSU_CSL_END		0x303E0100
-#define CSU_CSL_59		(0x303E0000 + 59 * 4)
-#define CSU_CSL_28		(0x303E0000 + 28 * 4)
-#define CSU_CSL_15		(0x303E0000 + 15 * 4)
-#define CSU_CSL_12		(0x303E0000 + 12 * 4)
+#define CSU_CSL_START		0x0
+#define CSU_CSL_END		0x100
 #define	CSU_ACCESS_ALL		0x00FF00FF
 #define CSU_SETTING_LOCK	0x01000100
 

core/arch/arm/plat-imx/imx.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2019 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -23,6 +23,8 @@
 uint32_t imx_get_src_gpr(int cpu);
 void imx_set_src_gpr(int cpu, uint32_t val);
 
+bool soc_is_imx6(void);
+bool soc_is_imx6sx(void);
 bool soc_is_imx6ul(void);
 bool soc_is_imx6ull(void);
 bool soc_is_imx6sdl(void);

core/arch/arm/plat-imx/imx6.c
@@ -27,14 +27,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <imx.h>
 
-#include <compiler.h>
-#include <drivers/gic.h>
+#include <imx.h>
 #include <io.h>
 #include <kernel/generic_boot.h>
 #include <kernel/misc.h>
-#include <kernel/tz_ssvce_pl310.h>
+#include <kernel/tz_ssvce_def.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
 #include <platform_config.h>
@@ -43,7 +41,6 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC, SRC_BASE, CORE_MMU_PGDIR_SIZE);
 
 void plat_cpu_reset_late(void)
 {
-	uintptr_t addr;
 	uint32_t pa __maybe_unused;
 
 	if (!get_core_pos()) {
@@ -66,19 +63,6 @@ void plat_cpu_reset_late(void)
 		/* SCU enable */
 		io_setbits32(SCU_BASE + SCU_CTRL, 0x1);
 
-		/* configure imx6 CSU */
-
-		/* first grant all peripherals */
-		for (addr = CSU_BASE + CSU_CSL_START;
-			 addr != CSU_BASE + CSU_CSL_END;
-			 addr += 4)
-			io_write32(addr, CSU_ACCESS_ALL);
-
-		/* lock the settings */
-		for (addr = CSU_BASE + CSU_CSL_START;
-		     addr != CSU_BASE + CSU_CSL_END;
-		     addr += 4)
-			io_setbits32(addr, CSU_SETTING_LOCK);
 		imx_configure_tzasc();
 	}
 }

core/arch/arm/plat-imx/imx6ul.c
@@ -1,36 +1,0 @@
-// SPDX-License-Identifier: BSD-2-Clause
-/*
- * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2019 NXP
- *
- * Peng Fan <peng.fan@nxp.com>
- */
-
-#include <arm32.h>
-#include <io.h>
-#include <kernel/generic_boot.h>
-#include <platform_config.h>
-#include <stdint.h>
-
-static void init_csu(void)
-{
-	uintptr_t addr;
-
-	/* first grant all peripherals */
-	for (addr = CSU_BASE + CSU_CSL_START;
-	     addr != CSU_BASE + CSU_CSL_END;
-	     addr += 4)
-		io_write32(addr, CSU_ACCESS_ALL);
-
-	/* lock the settings */
-	for (addr = CSU_BASE + CSU_CSL_START;
-	     addr != CSU_BASE + CSU_CSL_END;
-	     addr += 4)
-		io_setbits32(addr, CSU_SETTING_LOCK);
-}
-
-/* MMU not enabled now */
-void plat_cpu_reset_late(void)
-{
-	init_csu();
-}

commit b548c00f773e6e5537f0a31375988dc465e7d482

core: imx: add missing imx6 SoC IDs to soc_is_imx6()
Add the following SoC IDs to soc_is_imx6()
 - SOC_MX6SL
 - SOC_MX6SLL
 - SOC_MX6D

Fixes: 16e73240d ("core: imx: add CSU module")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-imx/imx-common.c
@@ -140,11 +140,12 @@ bool soc_is_imx6dqp(void)
 
 bool soc_is_imx6(void)
 {
-	return ((imx_soc_type() == SOC_MX6SX) ||
-			(imx_soc_type() == SOC_MX6UL) ||
-			(imx_soc_type() == SOC_MX6ULL) ||
-			(imx_soc_type() == SOC_MX6DL) ||
-			(imx_soc_type() == SOC_MX6Q));
+	uint32_t soc = imx_soc_type();
+
+	return (soc == SOC_MX6SLL) || (soc == SOC_MX6SL) ||
+	       (soc == SOC_MX6D) || (soc == SOC_MX6SX) ||
+	       (soc == SOC_MX6UL) || (soc == SOC_MX6ULL) ||
+	       (soc == SOC_MX6DL) || (soc == SOC_MX6Q);
 }
 
 bool soc_is_imx7ds(void)