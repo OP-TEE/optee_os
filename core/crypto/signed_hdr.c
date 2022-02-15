OP-TEE/optee_os

commit 064663e8bd2781d693bc04b6118efa3bbf5ee9c2

core: crypto: add struct shdr helper functions
Adds struct shdr helper functions to allocate and verify a struct shdr.

Reviewed-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/crypto/signed_hdr.c
@@ -1,0 +1,81 @@
+/*
+ * Copyright (c) 2015-2017, Linaro Limited
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <crypto/crypto.h>
+#include <signed_hdr.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ta_pub_key.h>
+#include <tee_api_types.h>
+#include <tee/tee_cryp_utl.h>
+#include <utee_defines.h>
+
+struct shdr *shdr_alloc_and_copy(const struct shdr *img, size_t img_size)
+{
+	size_t shdr_size;
+	struct shdr *shdr;
+
+	if (img_size < sizeof(struct shdr))
+		NULL;
+	shdr_size = SHDR_GET_SIZE(img);
+	if (img_size < shdr_size)
+		NULL;
+
+	shdr = malloc(shdr_size);
+	if (!shdr)
+		return NULL;
+	memcpy(shdr, img, shdr_size);
+
+	/* Check that the data wasn't modified before the copy was completed */
+	if (shdr_size != SHDR_GET_SIZE(shdr)) {
+		free(shdr);
+		return NULL;
+	}
+
+	return shdr;
+}
+
+TEE_Result shdr_verify_signature(const struct shdr *shdr)
+{
+	struct rsa_public_key key;
+	TEE_Result res;
+	uint32_t e = TEE_U32_TO_BIG_ENDIAN(ta_pub_key_exponent);
+	size_t hash_size;
+
+	if (shdr->magic != SHDR_MAGIC)
+		return TEE_ERROR_SECURITY;
+
+	if (TEE_ALG_GET_MAIN_ALG(shdr->algo) != TEE_MAIN_ALGO_RSA)
+		return TEE_ERROR_SECURITY;
+
+	res = tee_hash_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(shdr->algo),
+				       &hash_size);
+	if (res)
+		return TEE_ERROR_SECURITY;
+	if (hash_size != shdr->hash_size)
+		return TEE_ERROR_SECURITY;
+
+	res = crypto_acipher_alloc_rsa_public_key(&key, shdr->sig_size);
+	if (res)
+		return TEE_ERROR_SECURITY;
+
+	res = crypto_bignum_bin2bn((uint8_t *)&e, sizeof(e), key.e);
+	if (res)
+		goto out;
+	res = crypto_bignum_bin2bn(ta_pub_key_modulus, ta_pub_key_modulus_size,
+				   key.n);
+	if (res)
+		goto out;
+
+	res = crypto_acipher_rsassa_verify(shdr->algo, &key, -1,
+					   SHDR_GET_HASH(shdr), shdr->hash_size,
+					   SHDR_GET_SIG(shdr), shdr->sig_size);
+out:
+	crypto_acipher_free_rsa_public_key(&key);
+	if (res)
+		return TEE_ERROR_SECURITY;
+	return TEE_SUCCESS;
+}

core/crypto/sub.mk
@@ -6,3 +6,4 @@ srcs-y += aes-gcm-ghash-tbl.c
 else
 srcs-y += aes-gcm-ghash.c
 endif
+srcs-$(CFG_WITH_USER_TA) += signed_hdr.c

core/include/signed_hdr.h
@@ -29,6 +29,7 @@
 
 #include <inttypes.h>
 #include <tee_api_types.h>
+#include <stdlib.h>
 
 enum shdr_img_type {
 	SHDR_TA = 0,
@@ -78,5 +79,26 @@ struct shdr_bootstrap_ta {
 	uint32_t version;
 };
 
-#endif /*SIGNED_HDR_H*/
+/*
+ * Allocates a struct shdr large enough to hold the entire header,
+ * excluding a subheader like struct shdr_bootstrap_ta.
+ */
+struct shdr *shdr_alloc_and_copy(const struct shdr *img, size_t img_size);
 
+/* Frees a previously allocated struct shdr */
+static inline void shdr_free(struct shdr *shdr)
+{
+	free(shdr);
+}
+
+/*
+ * Verifies the signature in the @shdr.
+ *
+ * Note that the static part of struct shdr and payload still need to be
+ * checked against the hash contained in the header.
+ *
+ * Returns TEE_SUCCESS on success or TEE_ERROR_SECURITY on failure
+ */
+TEE_Result shdr_verify_signature(const struct shdr *shdr);
+
+#endif /*SIGNED_HDR_H*/

core/sub.mk
@@ -3,7 +3,7 @@ subdirs-y += crypto
 subdirs-y += tee
 subdirs-y += drivers
 
-ifeq ($(CFG_WITH_USER_TA)-$(CFG_REE_FS_TA),y-y)
+ifeq ($(CFG_WITH_USER_TA),y)
 gensrcs-y += ta_pub_key
 produce-ta_pub_key = ta_pub_key.c
 depends-ta_pub_key = $(TA_SIGN_KEY) scripts/pem_to_pub_c.py

commit c28441902ba6d035b6c1aba8dc3997835c3843d7

core: crypto: fix the RSA public key size for TA signature verification
The size of the RSA public key allocation should be based on the modulus
size in bits.

Fixes: 064663e8b ("core: crypto: add struct shdr helper functions")
Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/crypto/signed_hdr.c
@@ -64,7 +64,8 @@ TEE_Result shdr_verify_signature(const struct shdr *shdr)
 	if (hash_size != shdr->hash_size)
 		return TEE_ERROR_SECURITY;
 
-	res = crypto_acipher_alloc_rsa_public_key(&key, shdr->sig_size);
+	res = crypto_acipher_alloc_rsa_public_key(&key,
+						  ta_pub_key_modulus_size * 8);
 	if (res)
 		return TEE_ERROR_SECURITY;
 