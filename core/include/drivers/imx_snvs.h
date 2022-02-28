916e56edb0be783873e4c3974d1e3e5cd6b15fe5
Clement Faure <clement.faure@nxp.com>
2022-02-28 17:38:55 +0100

    core: drivers: merge i.MX SNVS driver files
    Move the implementation of plat_rpmb_key_is_ready() from
    plat-imx/drivers/imx_snvs.c to drivers/imx_snvs.c
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/include/drivers/imx_snvs.h
+++ core/include/drivers/imx_snvs.h
@@ -6,37 +6,4 @@
 #ifndef __DRIVERS_IMX_SNVS_H
 #define __DRIVERS_IMX_SNVS_H
 
-#include <tee_api_types.h>
-
-#define SNVS_HPSR	0x14
-
-#define HPSR_SSM_ST_MASK			GENMASK_32(11, 8)
-#define HPSR_SSM_ST_SHIFT			8
-
-#define SNVS_HPSR_SYS_SECURITY_BAD BIT(14)
-#define SNVS_HPSR_SYS_SECURITY_CLOSED BIT(13)
-#define SNVS_HPSR_SYS_SECURITY_OPEN BIT(12)
-
-enum snvs_ssm_mode {
-	SNVS_SSM_MODE_INIT,
-	SNVS_SSM_MODE_HARD_FAIL,
-	SNVS_SSM_MODE_SOFT_FAIL = 3,
-	SNVS_SSM_MODE_INIT_INTERMEDIATE = 8,
-	SNVS_SSM_MODE_CHECK,
-	SNVS_SSM_MODE_NON_SECURE = 11,
-	SNVS_SSM_MODE_TRUSTED = 13,
-	SNVS_SSM_MODE_SECURE,
-};
-
-enum snvs_security_cfg {
-	SNVS_SECURITY_CFG_FAB,
-	SNVS_SECURITY_CFG_OPEN,
-	SNVS_SECURITY_CFG_CLOSED,
-	SNVS_SECURITY_CFG_FIELD_RETURN,
-};
-
-enum snvs_ssm_mode snvs_get_ssm_mode(void);
-
-enum snvs_security_cfg snvs_get_security_cfg(void);
-
 #endif /* __DRIVERS_IMX_SNVS_H */
