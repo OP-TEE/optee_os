2f4eaf251c48d466efb8b4a5079897e16d313f46
Clement Faure <clement.faure@nxp.com>
2022-03-01 11:09:56 +0100

    drivers: imx_snvs: add master key selection
    Select the OTPMK as the SNVS master key when the platforms is in closed
    state.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/drivers/imx_snvs.c
+++ core/drivers/imx_snvs.c
@@ -5,6 +5,7 @@
  * Copyright 2022 NXP
  */
 
+#include <drivers/imx_snvs.h>
 #include <io.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
@@ -13,7 +14,11 @@
 #include <types_ext.h>
 #include <trace.h>
 
-#define SNVS_HPSR 0x14
+#define SNVS_HPLR   0x00
+#define SNVS_HPCOMR 0x04
+#define SNVS_HPSR   0x14
+#define SNVS_LPLR   0x34
+#define SNVS_LPMKCR 0x3C
 
 #define HPSR_SSM_ST_MASK  GENMASK_32(11, 8)
 #define HPSR_SSM_ST_SHIFT 8
@@ -21,6 +26,17 @@
 #define SNVS_HPSR_SYS_SECURITY_BAD    BIT(14)
 #define SNVS_HPSR_SYS_SECURITY_CLOSED BIT(13)
 #define SNVS_HPSR_SYS_SECURITY_OPEN   BIT(12)
+#define SNVS_HPSR_OTPMK_SYND	      GENMASK_32(24, 16)
+#define SNVS_HPSR_OTPMK_ZERO	      BIT(27)
+
+#define SNVS_HPLR_MKS_SL BIT32(9)
+
+#define SNVS_LPLR_MKS_HL BIT32(9)
+
+#define SNVS_HPCOMR_MKS_EN   BIT32(13)
+#define SNVS_HPCOMR_NPSWA_EN BIT32(31)
+
+#define SNVS_LPMKCR_MKCR_MKS_SEL GENMASK_32(1, 0)
 
 enum snvs_ssm_mode {
 	SNVS_SSM_MODE_INIT,
@@ -40,7 +56,63 @@ enum snvs_security_cfg {
 	SNVS_SECURITY_CFG_FIELD_RETURN,
 };
 
-#ifdef CFG_RPMB_FS
+/*
+ * Return true if the master key is OTPMK, false otherwise.
+ */
+static bool is_otpmk_selected(void)
+{
+	uint32_t hp_mks = 0;
+	vaddr_t base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC, SNVS_SIZE);
+
+	hp_mks = io_read32(base + SNVS_HPCOMR);
+
+	/*
+	 * The master key selection might be done by the MASTER_KEY_SEL field
+	 * of LPMKCR instead.
+	 */
+	if (hp_mks & SNVS_HPCOMR_MKS_EN) {
+		uint32_t lp_mks = io_read32(base + SNVS_LPMKCR);
+
+		if (lp_mks & SNVS_LPMKCR_MKCR_MKS_SEL)
+			return false;
+	}
+
+	return true;
+}
+
+/*
+ * Return true if the master key selection is locked, false otherwise.
+ */
+static bool is_mks_locked(void)
+{
+	vaddr_t base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC, SNVS_SIZE);
+
+	return io_read32(base + SNVS_HPLR) & SNVS_HPLR_MKS_SL ||
+	       io_read32(base + SNVS_LPLR) & SNVS_LPLR_MKS_HL;
+}
+
+/* Set the Master key to use OTPMK and lock it. */
+static void set_mks_otpmk(void)
+{
+	vaddr_t base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC, SNVS_SIZE);
+
+	io_setbits32(base + SNVS_HPCOMR, SNVS_HPCOMR_MKS_EN);
+	io_clrbits32(base + SNVS_LPMKCR, SNVS_LPMKCR_MKCR_MKS_SEL);
+	io_clrbits32(base + SNVS_HPLR, SNVS_HPLR_MKS_SL);
+	io_setbits32(base + SNVS_LPLR, SNVS_LPLR_MKS_HL);
+}
+
+/*
+ * Return true if OTPMK is valid, false otherwise.
+ */
+static bool is_otpmk_valid(void)
+{
+	vaddr_t base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC, SNVS_SIZE);
+	uint32_t status = io_read32(base + SNVS_HPSR);
+
+	return !(status & (SNVS_HPSR_OTPMK_ZERO | SNVS_HPSR_OTPMK_SYND));
+}
+
 static enum snvs_security_cfg snvs_get_security_cfg(void)
 {
 	vaddr_t snvs = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC,
@@ -61,6 +133,12 @@ static enum snvs_security_cfg snvs_get_security_cfg(void)
 	return SNVS_SECURITY_CFG_FAB;
 }
 
+bool snvs_is_device_closed(void)
+{
+	return (snvs_get_security_cfg() == SNVS_SECURITY_CFG_CLOSED);
+}
+
+#ifdef CFG_RPMB_FS
 static enum snvs_ssm_mode snvs_get_ssm_mode(void)
 {
 	vaddr_t snvs = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC,
@@ -96,3 +174,20 @@ bool plat_rpmb_key_is_ready(void)
 	return ssm_secure && (security == SNVS_SECURITY_CFG_CLOSED);
 }
 #endif /* CFG_RPMB_FS */
+
+TEE_Result imx_snvs_set_master_otpmk(void)
+{
+	if (is_otpmk_valid())
+		return TEE_ERROR_BAD_STATE;
+
+	if (is_mks_locked()) {
+		if (is_otpmk_selected())
+			return TEE_SUCCESS;
+
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	set_mks_otpmk();
+
+	return TEE_SUCCESS;
+}
