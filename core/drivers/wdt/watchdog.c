48b0b1dd97faff83d0d146277b1f455421eda628
Etienne Carriere <etienne.carriere@linaro.org>
2022-03-02 23:47:18 +0100

    drivers: wdt: implementation supports a single watchdog
    Fixes watchdog_register() to fail when a watchdog instance is already
    registered.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/wdt/watchdog.c
+++ core/drivers/wdt/watchdog.c
@@ -9,6 +9,11 @@ struct wdt_chip *wdt_chip;
 
 TEE_Result watchdog_register(struct wdt_chip *chip)
 {
+	if (wdt_chip) {
+		DMSG("Cannot register several watchdog instances");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
 	if (!chip->ops->start || !chip->ops->ping  || !chip->ops->set_timeout)
 		return TEE_ERROR_BAD_PARAMETERS;
 
