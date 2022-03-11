f96cfa819876f3f10b82d5a0679f4259b6c1f763
Clément Léger <clement.leger@bootlin.com>
2022-03-11 14:05:27 +0100

    plat-sam: nsec-service: handle SMC to set USB suspend
    Handle the SMC that allows Linux to set USB suspend mode using the SFR.
    
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- core/arch/arm/plat-sam/nsec-service/sm_platform_handler.c
+++ core/arch/arm/plat-sam/nsec-service/sm_platform_handler.c
@@ -8,6 +8,7 @@
 #include <io.h>
 #include <kernel/tee_misc.h>
 #include <mm/core_memprot.h>
+#include <sam_sfr.h>
 #include <sm/optee_smc.h>
 #include <sm/sm.h>
 #include <smc_ids.h>
@@ -15,6 +16,10 @@
 static enum sm_handler_ret sam_sip_handler(struct thread_smc_args *args)
 {
 	switch (OPTEE_SMC_FUNC_NUM(args->a0)) {
+	case SAMA5_SMC_SIP_SFR_SET_USB_SUSPEND:
+		atmel_sfr_set_usb_suspend(args->a1);
+		args->a0 = SAMA5_SMC_SIP_RETURN_SUCCESS;
+		break;
 	case SAMA5_SMC_SIP_SET_SUSPEND_MODE:
 		return at91_pm_set_suspend_mode(args);
 	case SAMA5_SMC_SIP_GET_SUSPEND_MODE:
