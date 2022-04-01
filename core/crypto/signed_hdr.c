223d0d671fd23b90d2072e0d1b721d87edc2d915
Jens Wiklander <jens.wiklander@linaro.org>
2022-04-01 17:45:59 +0200

    core: add fault mitigations to shdr_verify_signature()
    Adds fault mitigations to shdr_verify_signature().
    shdr_verify_signature() is called using the wrapper FTMN_CALL_FUNC()
    which verifies that the correct function was called and that the return
    value hasn't been tampered with.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/crypto/signed_hdr.c
+++ core/crypto/signed_hdr.c
@@ -12,6 +12,7 @@
 #include <tee/tee_cryp_utl.h>
 #include <utee_defines.h>
 #include <util.h>
+#include <fault_mitigation.h>
 
 struct shdr *shdr_alloc_and_copy(const struct shdr *img, size_t img_size)
 {
@@ -46,43 +47,56 @@ struct shdr *shdr_alloc_and_copy(const struct shdr *img, size_t img_size)
 
 TEE_Result shdr_verify_signature(const struct shdr *shdr)
 {
-	struct rsa_public_key key;
+	struct rsa_public_key key = { };
 	TEE_Result res;
 	uint32_t e = TEE_U32_TO_BIG_ENDIAN(ta_pub_key_exponent);
+	struct ftmn ftmn = { };
+	unsigned int err_incr = 2;
 	size_t hash_size;
 
 	if (shdr->magic != SHDR_MAGIC)
-		return TEE_ERROR_SECURITY;
+		goto err;
 
 	if (TEE_ALG_GET_MAIN_ALG(shdr->algo) != TEE_MAIN_ALGO_RSA)
-		return TEE_ERROR_SECURITY;
+		goto err;
 
 	res = tee_alg_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(shdr->algo),
 				      &hash_size);
 	if (res)
-		return TEE_ERROR_SECURITY;
+		goto err;
 	if (hash_size != shdr->hash_size)
-		return TEE_ERROR_SECURITY;
+		goto err;
 
 	res = crypto_acipher_alloc_rsa_public_key(&key,
 						  ta_pub_key_modulus_size * 8);
 	if (res)
-		return TEE_ERROR_SECURITY;
+		goto err;
 
 	res = crypto_bignum_bin2bn((uint8_t *)&e, sizeof(e), key.e);
 	if (res)
-		goto out;
+		goto err;
 	res = crypto_bignum_bin2bn(ta_pub_key_modulus, ta_pub_key_modulus_size,
 				   key.n);
 	if (res)
-		goto out;
+		goto err;
 
+	FTMN_PUSH_LINKED_CALL(&ftmn,
+			      FTMN_FUNC_HASH("crypto_acipher_rsassa_verify"));
 	res = crypto_acipher_rsassa_verify(shdr->algo, &key, shdr->hash_size,
 					   SHDR_GET_HASH(shdr), shdr->hash_size,
 					   SHDR_GET_SIG(shdr), shdr->sig_size);
+	FTMN_SET_CHECK_RES_FROM_CALL(&ftmn, FTMN_INCR0, res);
+	FTMN_POP_LINKED_CALL(&ftmn);
+	if (!res) {
+		ftmn_checkpoint(&ftmn, FTMN_INCR0);
+		goto out;
+	}
+	err_incr = 1;
+err:
+	res = TEE_ERROR_SECURITY;
+	FTMN_SET_CHECK_RES_NOT_ZERO(&ftmn, err_incr * FTMN_INCR0, res);
 out:
+	FTMN_CALLEE_DONE_CHECK(&ftmn, FTMN_INCR0, FTMN_STEP_COUNT(2), res);
 	crypto_acipher_free_rsa_public_key(&key);
-	if (res)
-		return TEE_ERROR_SECURITY;
-	return TEE_SUCCESS;
+	return res;
 }
