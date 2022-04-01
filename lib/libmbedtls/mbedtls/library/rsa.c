44c458d92fcaa574f015fb05e81c7e0f70204bd9
Jens Wiklander <jens.wiklander@linaro.org>
2022-04-01 17:45:56 +0200

    libmbedtls: add fault mitigation in mbedtls_rsa_rsassa_pkcs1_v15_verify()
    Adds fault mitigation in mbedtls_rsa_rsassa_pkcs1_v15_verify() by using
    the macro FTMN_CALLEE_DONE_MEMCMP() instead of just
    mbedtls_safer_memcmp() when checking that the hash in the RSA signature
    is matching the expected value.
    
    FTMN_CALLEE_DONE_MEMCMP() saves on success the result in a thread local
    storage if fault mitigations was enabled when the function was called.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    [tve: rebased onto mbedtls-3.6.0]
    Signed-off-by: Tom Van Eyck <tom.vaneyck@kuleuven.be>
    [sby: rebased onto mbedtls-3.6.2]
    Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
    [eca: rebased onto mbedtls-3.6.3]
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
    [tbo: rebased onto mbedtls-3.6.4]
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
    [yga: rebased onto mbedtls-3.6.5]
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>

--- lib/libmbedtls/mbedtls/library/rsa.c
+++ lib/libmbedtls/mbedtls/library/rsa.c
@@ -2778,8 +2778,9 @@ int mbedtls_rsa_rsassa_pkcs1_v15_verify(mbedtls_rsa_context *ctx,
      * Compare
      */
 
-    if ((ret = mbedtls_ct_memcmp(encoded, encoded_expected,
-                                 sig_len)) != 0) {
+    ret = FTMN_CALLEE_DONE_MEMCMP(mbedtls_ct_memcmp, encoded,
+                                  encoded_expected, sig_len);
+    if (ret != 0) {
         ret = MBEDTLS_ERR_RSA_VERIFY_FAILED;
         goto cleanup;
     }
