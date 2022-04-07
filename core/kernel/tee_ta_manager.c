OP-TEE/optee_os

commit 82061b8d7b34f09553f2526b9661036a69b5bb84

core: store TA params in session struct
Stores TA params and error origin in struct tee_ta_session instead of
passing them as parameters to enter_open_session() and
enter_invoke_cmd() in struct ts_ops. This makes struct ts_ops less TA
specific.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/tee_ta_manager.c
@@ -718,9 +718,9 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	s->clnt_id = *clnt_id;
 
 	if (tee_ta_try_set_busy(ctx)) {
+		s->param = param;
 		set_invoke_timeout(s, cancel_req_to);
-		res = ctx->ts_ctx.ops->enter_open_session(&s->ts_sess, param,
-							  err);
+		res = ctx->ts_ctx.ops->enter_open_session(&s->ts_sess);
 		tee_ta_clear_busy(ctx);
 	} else {
 		/* Deadlock avoided */
@@ -729,6 +729,7 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	}
 
 	panicked = ctx->panicked;
+	s->param = NULL;
 
 	tee_ta_put_session(s);
 	if (panicked || (res != TEE_SUCCESS))
@@ -740,6 +741,8 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	 */
 	if (panicked || was_busy)
 		*err = TEE_ORIGIN_TEE;
+	else
+		*err = s->err_origin;
 
 	if (res != TEE_SUCCESS)
 		EMSG("Failed. Return error 0x%x", res);
@@ -778,10 +781,11 @@ TEE_Result tee_ta_invoke_command(TEE_ErrorOrigin *err,
 
 	tee_ta_set_busy(ta_ctx);
 
+	sess->param = param;
 	set_invoke_timeout(sess, cancel_req_to);
-	res = ta_ctx->ts_ctx.ops->enter_invoke_cmd(&sess->ts_sess, cmd, param,
-						   err);
+	res = ta_ctx->ts_ctx.ops->enter_invoke_cmd(&sess->ts_sess, cmd);
 
+	sess->param = NULL;
 	tee_ta_clear_busy(ta_ctx);
 
 	if (ta_ctx->panicked) {
@@ -789,6 +793,8 @@ TEE_Result tee_ta_invoke_command(TEE_ErrorOrigin *err,
 		*err = TEE_ORIGIN_TEE;
 		return TEE_ERROR_TARGET_DEAD;
 	}
+
+	*err = sess->err_origin;
 
 	/* Short buffer is not an effective error case */
 	if (res != TEE_SUCCESS && res != TEE_ERROR_SHORT_BUFFER)

core/arch/arm/kernel/pseudo_ta.c
@@ -140,17 +140,16 @@ static void unmap_mapped_param(struct tee_ta_param *param,
 	}
 }
 
-static TEE_Result pseudo_ta_enter_open_session(struct ts_session *s,
-					       struct tee_ta_param *param,
-					       TEE_ErrorOrigin *eo)
+static TEE_Result pseudo_ta_enter_open_session(struct ts_session *s)
 {
 	TEE_Result res = TEE_SUCCESS;
 	struct pseudo_ta_ctx *stc = to_pseudo_ta_ctx(s->ctx);
-	TEE_Param tee_param[TEE_NUM_PARAMS];
+	struct tee_ta_session *ta_sess = to_ta_session(s);
+	TEE_Param tee_param[TEE_NUM_PARAMS] = { };
 	bool did_map[TEE_NUM_PARAMS] = { false };
 
 	ts_push_current_session(s);
-	*eo = TEE_ORIGIN_TRUSTED_APP;
+	ta_sess->err_origin = TEE_ORIGIN_TRUSTED_APP;
 
 	if (stc->ctx.ref_count == 1 && stc->pseudo_ta->create_entry_point) {
 		res = stc->pseudo_ta->create_entry_point();
@@ -159,20 +158,27 @@ static TEE_Result pseudo_ta_enter_open_session(struct ts_session *s,
 	}
 
 	if (stc->pseudo_ta->open_session_entry_point) {
-		void **user_ctx = &to_ta_session(s)->user_ctx;
+		void **user_ctx = &ta_sess->user_ctx;
+		uint32_t param_types = 0;
 
-		res = copy_in_param(s, param, tee_param, did_map);
-		if (res != TEE_SUCCESS) {
-			unmap_mapped_param(param, did_map);
-			*eo = TEE_ORIGIN_TEE;
-			goto out;
+		if (ta_sess->param) {
+			res = copy_in_param(s, ta_sess->param, tee_param,
+					    did_map);
+			if (res != TEE_SUCCESS) {
+				unmap_mapped_param(ta_sess->param, did_map);
+				ta_sess->err_origin = TEE_ORIGIN_TEE;
+				goto out;
+			}
+			param_types = ta_sess->param->types;
 		}
 
-		res = stc->pseudo_ta->open_session_entry_point(param->types,
+		res = stc->pseudo_ta->open_session_entry_point(param_types,
 							       tee_param,
 							       user_ctx);
-		update_out_param(tee_param, param);
-		unmap_mapped_param(param, did_map);
+		if (ta_sess->param) {
+			update_out_param(tee_param, ta_sess->param);
+			unmap_mapped_param(ta_sess->param, did_map);
+		}
 	}
 
 out:
@@ -180,31 +186,34 @@ out:
 	return res;
 }
 
-static TEE_Result pseudo_ta_enter_invoke_cmd(struct ts_session *s,
-					     uint32_t cmd,
-					     struct tee_ta_param *param,
-					     TEE_ErrorOrigin *eo)
+static TEE_Result pseudo_ta_enter_invoke_cmd(struct ts_session *s, uint32_t cmd)
 {
 	TEE_Result res = TEE_SUCCESS;
 	struct pseudo_ta_ctx *stc = to_pseudo_ta_ctx(s->ctx);
-	void *user_ctx = to_ta_session(s)->user_ctx;
+	struct tee_ta_session *ta_sess = to_ta_session(s);
+	uint32_t param_types = 0;
 	TEE_Param tee_param[TEE_NUM_PARAMS] = { };
 	bool did_map[TEE_NUM_PARAMS] = { false };
 
 	ts_push_current_session(s);
-	res = copy_in_param(s, param, tee_param, did_map);
-	if (res != TEE_SUCCESS) {
-		unmap_mapped_param(param, did_map);
-		*eo = TEE_ORIGIN_TEE;
-		goto out;
+	if (ta_sess->param) {
+		res = copy_in_param(s, ta_sess->param, tee_param, did_map);
+		if (res != TEE_SUCCESS) {
+			unmap_mapped_param(ta_sess->param, did_map);
+			ta_sess->err_origin = TEE_ORIGIN_TEE;
+			goto out;
+		}
+		param_types = ta_sess->param->types;
 	}
 
-	*eo = TEE_ORIGIN_TRUSTED_APP;
-	res = stc->pseudo_ta->invoke_command_entry_point(user_ctx, cmd,
-							 param->types,
+	ta_sess->err_origin = TEE_ORIGIN_TRUSTED_APP;
+	res = stc->pseudo_ta->invoke_command_entry_point(ta_sess->user_ctx, cmd,
+							 param_types,
 							 tee_param);
-	update_out_param(tee_param, param);
-	unmap_mapped_param(param, did_map);
+	if (ta_sess->param) {
+		update_out_param(tee_param, ta_sess->param);
+		unmap_mapped_param(ta_sess->param, did_map);
+	}
 out:
 	ts_pop_current_session();
 	return res;

core/arch/arm/kernel/secure_partition.c
@@ -299,33 +299,31 @@ TEE_Result sec_part_init_session(const TEE_UUID *uuid,
 	return TEE_SUCCESS;
 }
 
-static TEE_Result stmm_enter_open_session(struct ts_session *s,
-					  struct tee_ta_param *param,
-					  TEE_ErrorOrigin *eo)
+static TEE_Result stmm_enter_open_session(struct ts_session *s)
 {
 	struct sec_part_ctx *spc = to_sec_part_ctx(s->ctx);
+	struct tee_ta_session *ta_sess = to_ta_session(s);
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE,
 						TEE_PARAM_TYPE_NONE);
 
-	if (param->types != exp_pt)
+	if (ta_sess->param->types != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	if (spc->is_initializing) {
 		/* StMM is initialized in sec_part_init_session() */
-		*eo = TEE_ORIGIN_TEE;
+		ta_sess->err_origin = TEE_ORIGIN_TEE;
 		return TEE_ERROR_BAD_STATE;
 	}
 
 	return TEE_SUCCESS;
 }
 
-static TEE_Result stmm_enter_invoke_cmd(struct ts_session *s, uint32_t cmd,
-					struct tee_ta_param *param,
-					TEE_ErrorOrigin *eo __unused)
+static TEE_Result stmm_enter_invoke_cmd(struct ts_session *s, uint32_t cmd)
 {
 	struct sec_part_ctx *spc = to_sec_part_ctx(s->ctx);
+	struct tee_ta_session *ta_sess = to_ta_session(s);
 	TEE_Result res = TEE_SUCCESS;
 	TEE_Result __maybe_unused tmp_res = TEE_SUCCESS;
 	unsigned int ns_buf_size = 0;
@@ -339,10 +337,10 @@ static TEE_Result stmm_enter_invoke_cmd(struct ts_session *s, uint32_t cmd,
 	if (cmd != PTA_STMM_CMD_COMMUNICATE)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	if (param->types != exp_pt)
+	if (ta_sess->param->types != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	mem = &param->u[0].mem;
+	mem = &ta_sess->param->u[0].mem;
 	ns_buf_size = mem->size;
 	if (ns_buf_size > spc->ns_comm_buf_size) {
 		mem->size = spc->ns_comm_buf_size;
@@ -380,7 +378,7 @@ static TEE_Result stmm_enter_invoke_cmd(struct ts_session *s, uint32_t cmd,
 	 * Copy the SPM response from secure partition back to the non-secure
 	 * buffer of the client that called us.
 	 */
-	param->u[1].val.a = spc->regs.x[4];
+	ta_sess->param->u[1].val.a = spc->regs.x[4];
 
 	memcpy(va, (void *)spc->ns_comm_buf_addr, ns_buf_size);
 

core/arch/arm/kernel/user_ta.c
@@ -144,16 +144,14 @@ static void dec_recursion(void)
 	tsd->syscall_recursion--;
 }
 
-static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
-				struct ts_session *session,
-				enum utee_entry_func func, uint32_t cmd,
-				struct tee_ta_param *param)
+static TEE_Result user_ta_enter(struct ts_session *session,
+				enum utee_entry_func func, uint32_t cmd)
 {
 	TEE_Result res = TEE_SUCCESS;
 	struct utee_params *usr_params = NULL;
 	uaddr_t usr_stack = 0;
 	struct user_ta_ctx *utc = to_user_ta_ctx(session->ctx);
-	TEE_ErrorOrigin serr = TEE_ORIGIN_TEE;
+	struct tee_ta_session *ta_sess = to_ta_session(session);
 	struct ts_session *ts_sess __maybe_unused = NULL;
 	void *param_va[TEE_NUM_PARAMS] = { NULL };
 
@@ -162,11 +160,12 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 		res = TEE_ERROR_OUT_OF_MEMORY;
 		goto out_clr_cancel;
 	}
-
-	/* Map user space memory */
-	res = tee_mmu_map_param(&utc->uctx, param, param_va);
-	if (res != TEE_SUCCESS)
-		goto out;
+	if (ta_sess->param) {
+		/* Map user space memory */
+		res = tee_mmu_map_param(&utc->uctx, ta_sess->param, param_va);
+		if (res != TEE_SUCCESS)
+			goto out;
+	}
 
 	/* Switch to user ctx */
 	ts_push_current_session(session);
@@ -175,7 +174,10 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 	usr_stack = utc->stack_ptr;
 	usr_stack -= ROUNDUP(sizeof(struct utee_params), STACK_ALIGNMENT);
 	usr_params = (struct utee_params *)usr_stack;
-	init_utee_param(usr_params, param, param_va);
+	if (ta_sess->param)
+		init_utee_param(usr_params, ta_sess->param, param_va);
+	else
+		memset(usr_params, 0, sizeof(*usr_params));
 
 	res = thread_enter_user_mode(func, kaddr_to_uref(session),
 				     (vaddr_t)usr_params, cmd, usr_stack,
@@ -184,28 +186,31 @@ static TEE_Result user_ta_enter(TEE_ErrorOrigin *err,
 				     &utc->uctx.ctx.panic_code);
 
 	clear_vfp_state(utc);
-	/*
-	 * According to GP spec the origin should allways be set to the
-	 * TA after TA execution
-	 */
-	serr = TEE_ORIGIN_TRUSTED_APP;
 
 	if (utc->uctx.ctx.panicked) {
 		abort_print_current_ta();
 		DMSG("tee_user_ta_enter: TA panicked with code 0x%x",
 		     utc->uctx.ctx.panic_code);
-		serr = TEE_ORIGIN_TEE;
 		res = TEE_ERROR_TARGET_DEAD;
+	} else {
+		/*
+		 * According to GP spec the origin should allways be set to
+		 * the TA after TA execution
+		 */
+		ta_sess->err_origin = TEE_ORIGIN_TRUSTED_APP;
 	}
 
-	/* Copy out value results */
-	update_from_utee_param(param, usr_params);
+	if (ta_sess->param) {
+		/* Copy out value results */
+		update_from_utee_param(ta_sess->param, usr_params);
 
-	/*
-	 * Clear out the parameter mappings added with tee_mmu_map_param()
-	 * above.
-	 */
-	tee_mmu_clean_param(&utc->uctx);
+		/*
+		 * Clear out the parameter mappings added with
+		 * tee_mmu_map_param() above.
+		 */
+		tee_mmu_clean_param(&utc->uctx);
+	}
+
 
 	ts_sess = ts_pop_current_session();
 	assert(ts_sess == session);
@@ -218,13 +223,7 @@ out_clr_cancel:
 	 * time the TA will be invoked will be with a new operation and should
 	 * not have an old cancellation pending.
 	 */
-	to_ta_session(session)->cancel = false;
-
-	/*
-	 * Can't update *err until now since it may point to an address
-	 * mapped for the user mode TA.
-	 */
-	*err = serr;
+	ta_sess->cancel = false;
 
 	return res;
 }
@@ -286,29 +285,21 @@ static TEE_Result init_with_ldelf(struct ts_session *sess __maybe_unused,
 	return TEE_SUCCESS;
 }
 
-static TEE_Result user_ta_enter_open_session(struct ts_session *s,
-					     struct tee_ta_param *param,
-					     TEE_ErrorOrigin *eo)
+static TEE_Result user_ta_enter_open_session(struct ts_session *s)
 {
-	return user_ta_enter(eo, s, UTEE_ENTRY_FUNC_OPEN_SESSION, 0, param);
+	return user_ta_enter(s, UTEE_ENTRY_FUNC_OPEN_SESSION, 0);
 }
 
-static TEE_Result user_ta_enter_invoke_cmd(struct ts_session *s, uint32_t cmd,
-					   struct tee_ta_param *param,
-					   TEE_ErrorOrigin *eo)
+static TEE_Result user_ta_enter_invoke_cmd(struct ts_session *s, uint32_t cmd)
 {
-	return user_ta_enter(eo, s, UTEE_ENTRY_FUNC_INVOKE_COMMAND, cmd, param);
+	return user_ta_enter(s, UTEE_ENTRY_FUNC_INVOKE_COMMAND, cmd);
 }
 
 static void user_ta_enter_close_session(struct ts_session *s)
 {
 	/* Only if the TA was fully initialized by ldelf */
-	if (!to_user_ta_ctx(s->ctx)->is_initializing) {
-		TEE_ErrorOrigin eo = TEE_ORIGIN_TEE;
-		struct tee_ta_param param = { };
-
-		user_ta_enter(&eo, s, UTEE_ENTRY_FUNC_CLOSE_SESSION, 0, &param);
-	}
+	if (!to_user_ta_ctx(s->ctx)->is_initializing)
+		user_ta_enter(s, UTEE_ENTRY_FUNC_CLOSE_SESSION, 0);
 }
 
 static void dump_state_no_ldelf_dbg(struct user_ta_ctx *utc)

core/include/kernel/tee_ta_manager.h
@@ -81,6 +81,8 @@ struct tee_ta_session {
 	struct ts_session ts_sess;
 	uint32_t id;		/* Session handle (0 is invalid) */
 	TEE_Identity clnt_id;	/* Identify of client */
+	struct tee_ta_param *param;
+	TEE_ErrorOrigin err_origin;
 	bool cancel;		/* True if TA invocation is cancelled */
 	bool cancel_mask;	/* True if cancel is masked */
 	TEE_Time cancel_time;	/* Time when to cancel the TA invocation */

core/include/kernel/ts_manager.h
@@ -26,15 +26,10 @@ struct ts_session {
 #endif
 };
 
-struct tee_ta_param;
 struct thread_svc_regs;
 struct ts_ops {
-	TEE_Result (*enter_open_session)(struct ts_session *s,
-					 struct tee_ta_param *param,
-					 TEE_ErrorOrigin *eo);
-	TEE_Result (*enter_invoke_cmd)(struct ts_session *s, uint32_t cmd,
-				       struct tee_ta_param *param,
-				       TEE_ErrorOrigin *eo);
+	TEE_Result (*enter_open_session)(struct ts_session *s);
+	TEE_Result (*enter_invoke_cmd)(struct ts_session *s, uint32_t cmd);
 	void (*enter_close_session)(struct ts_session *s);
 	void (*dump_state)(struct ts_ctx *ctx);
 	void (*dump_ftrace)(struct ts_ctx *ctx);

commit 8919799c081fb3aa9709b7ce6d7581f720f92387

core: fix use after free in tee_ta_open_session()
Fixes a use after free where the session pointer 's' was used after
tee_ta_close_session() while recovering from an error.

Fixes: 82061b8d7b34 ("core: store TA params in session struct")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/tee_ta_manager.c
@@ -750,10 +750,6 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	panicked = ctx->panicked;
 	s->param = NULL;
 
-	tee_ta_put_session(s);
-	if (panicked || (res != TEE_SUCCESS))
-		tee_ta_close_session(s, open_sessions, KERN_IDENTITY);
-
 	/*
 	 * Origin error equal to TEE_ORIGIN_TRUSTED_APP for "regular" error,
 	 * apart from panicking.
@@ -762,6 +758,10 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 		*err = TEE_ORIGIN_TEE;
 	else
 		*err = s->err_origin;
+
+	tee_ta_put_session(s);
+	if (panicked || res != TEE_SUCCESS)
+		tee_ta_close_session(s, open_sessions, KERN_IDENTITY);
 
 	if (res != TEE_SUCCESS)
 		EMSG("Failed. Return error 0x%x", res);