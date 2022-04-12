OP-TEE/optee_os

commit 3f45afc31955b3f7366ef3f8c5dfb193a353f28b

drivers: caam: disable the use of interrupts for some platforms
On some i.MX platforms, all CAAM JRs share the same line of interrupts.
To avoid conflicts with the other job ring owners, skip the
enable/disable of job ring interruptions in OP-TEE CAAM driver.

Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/drivers/crypto/caam/hal/common/hal_jr.c
@@ -136,6 +136,7 @@ void caam_hal_jr_del_job(vaddr_t baseaddr)
 	io_caam_write32(baseaddr + JRX_ORJRR, 1);
 }
 
+#ifdef CFG_CAAM_NO_ITR
 void caam_hal_jr_disable_itr(vaddr_t baseaddr)
 {
 	io_setbits32(baseaddr + JRX_JRCFGR_LS, JRX_JRCFGR_LS_IMSK);
@@ -147,6 +148,10 @@ void caam_hal_jr_enable_itr(vaddr_t baseaddr)
 	io_mask32(baseaddr + JRX_JRCFGR_LS, ~JRX_JRCFGR_LS_IMSK,
 		  JRX_JRCFGR_LS_IMSK);
 }
+#else
+void caam_hal_jr_disable_itr(vaddr_t baseaddr __unused) {}
+void caam_hal_jr_enable_itr(vaddr_t baseaddr __unused) {}
+#endif /* CFG_CAAM_NO_ITR */
 
 bool caam_hal_jr_check_ack_itr(vaddr_t baseaddr)
 {

commit 889d9f50402698d03658c53a5d7965fd041098d7

drivers: caam: fix function definition when CFG_CAAM_NO_ITR=y
There is a bug in the CAAM JR interruption enablement logic. When
CFG_CAAM_NO_ITR=y, the JR interruptions are used and when
CFG_CAAM_NO_ITR=n, the JR interruptions are not used.

Even with this wrong logic, the CAAM is still able to enqueue jobs.
When no JR interruptions are received, the CAAM will manually dequeue
jobs from the jobring by checking the number of jobs done in the output
ring slots full register.

CAAM JR interruptions are not mandatory for the CAAM to work properly
but it makes the dequeuing faster than polling the output ring slot full
register.

To avoid confusion, replace CFG_CAAM_NO_ITR with CFG_CAAM_ITR. The
CFG_CAAM_ITR is enabled by default and platforms not using the JR
interruptions would have this flag disabled instead.

Fixes: 3f45afc31 ("drivers: caam: disable the use of interrupts for some platforms")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-imx/crypto_conf.mk
@@ -51,13 +51,16 @@ $(call force,CFG_CAAM_SIZE_ALIGN,1)
 $(call force,CFG_JR_BLOCK_SIZE,0x1000)
 $(call force,CFG_JR_INDEX,2)
 $(call force,CFG_JR_INT,114)
-$(call force,CFG_CAAM_NO_ITR,y)
+$(call force,CFG_CAAM_ITR,n)
 else
 $(call force, CFG_CAAM_SIZE_ALIGN,1)
 $(call force, CFG_JR_BLOCK_SIZE,0x1000)
 $(call force, CFG_JR_INDEX,0)
 $(call force, CFG_JR_INT,137)
 endif
+
+# Enable JR interruptions
+CFG_CAAM_ITR ?= y
 
 #
 # Configuration of the Crypto Driver

core/drivers/crypto/caam/hal/common/hal_jr.c
@@ -136,7 +136,7 @@ void caam_hal_jr_del_job(vaddr_t baseaddr)
 	io_caam_write32(baseaddr + JRX_ORJRR, 1);
 }
 
-#ifdef CFG_CAAM_NO_ITR
+#ifdef CFG_CAAM_ITR
 void caam_hal_jr_disable_itr(vaddr_t baseaddr)
 {
 	io_setbits32(baseaddr + JRX_JRCFGR_LS, JRX_JRCFGR_LS_IMSK);
@@ -151,7 +151,7 @@ void caam_hal_jr_enable_itr(vaddr_t baseaddr)
 #else
 void caam_hal_jr_disable_itr(vaddr_t baseaddr __unused) {}
 void caam_hal_jr_enable_itr(vaddr_t baseaddr __unused) {}
-#endif /* CFG_CAAM_NO_ITR */
+#endif /* CFG_CAAM_ITR */
 
 bool caam_hal_jr_check_ack_itr(vaddr_t baseaddr)
 {