OP-TEE/optee_os

commit 38923d487567e1f0b9e627a88b61d8c748406dad

drivers: caam: implement CAAM DMA Object
Implementation of a CAAM DMA object to:
  - create a DMA object (SGT/buffer) based on input/output buffers
  - reallocate a new buffer accessible from the CAAM address space
  - ensure buffer is cache aligned (for the output)

Implementation of CAAM DMA object functions to:
  - cache maintenance
  - free CAAM DMA object

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/caam/utils/utils_mem.c
@@ -9,41 +9,29 @@
 #include <caam_common.h>
 #include <caam_trace.h>
 #include <caam_utils_mem.h>
+#include <io.h>
 #include <kernel/cache_helpers.h>
 #include <mm/core_memprot.h>
 #include <string.h>
 
-/*
- * CAAM Descriptor address alignment
- */
-#ifdef ARM64
-#define DESC_START_ALIGN	(64 / 8)
-#else
-#define DESC_START_ALIGN	(32 / 8)
-#endif
-
-/*
- * Check if pointer p is aligned with align
- */
-#define IS_PTR_ALIGN(p, align)	(((uintptr_t)(p) & ((align) - 1)) == 0)
-
-/*
- * Check if size is aligned with align
- */
-#define IS_SIZE_ALIGN(size, align)                                             \
-	({                                                                     \
-		__typeof__(size) _size = (size);                               \
-		__typeof__(size) _sizeup = 0;                                  \
-									       \
-		_sizeup = ROUNDUP(_size, align);                               \
-		(_sizeup == _size) ? 1 : 0;                                    \
-	})
-
+#define MEM_TYPE_NORMAL 0      /* Normal allocation */
 #define MEM_TYPE_ZEROED	BIT(0) /* Buffer filled with 0's */
 #define MEM_TYPE_ALIGN	BIT(1) /* Address and size aligned on a cache line */
 
 /*
- * Allocate an area of given size in bytes
+ * Read the first byte at the given @addr to ensure that
+ * virtual page is mapped before getting its physical address.
+ *
+ * @addr: address to read
+ */
+static inline void touch_page(vaddr_t addr)
+{
+	io_read8(addr);
+}
+
+/*
+ * Allocate an area of given size in bytes. Add the memory allocator
+ * information in the newly allocated area.
  *
  * @size   Size in bytes to allocate
  * @type   Type of area to allocate (refer to MEM_TYPE_*)
@@ -117,6 +105,11 @@ static enum caam_status mem_alloc_buf(struct caambuf *buf, size_t size,
 	return CAAM_NO_ERROR;
 }
 
+void *caam_alloc(size_t size)
+{
+	return mem_alloc(size, MEM_TYPE_NORMAL);
+}
+
 void *caam_calloc(size_t size)
 {
 	return mem_alloc(size, MEM_TYPE_ZEROED);
@@ -142,6 +135,11 @@ void caam_free_desc(uint32_t **ptr)
 {
 	mem_free(*ptr);
 	*ptr = NULL;
+}
+
+enum caam_status caam_alloc_buf(struct caambuf *buf, size_t size)
+{
+	return mem_alloc_buf(buf, size, MEM_TYPE_NORMAL);
 }
 
 enum caam_status caam_calloc_buf(struct caambuf *buf, size_t size)
@@ -173,41 +171,6 @@ void caam_free_buf(struct caambuf *buf)
 	}
 }
 
-void caam_sgtbuf_free(struct caamsgtbuf *data)
-{
-	if (data->sgt_type)
-		caam_free(data->sgt);
-	else
-		caam_free(data->buf);
-
-	data->sgt = NULL;
-	data->buf = NULL;
-}
-
-enum caam_status caam_sgtbuf_alloc(struct caamsgtbuf *data)
-{
-	if (!data)
-		return CAAM_BAD_PARAM;
-
-	if (data->sgt_type) {
-		data->sgt =
-			caam_calloc(data->number * (sizeof(struct caamsgt) +
-						    sizeof(struct caambuf)));
-		data->buf = (void *)(((uint8_t *)data->sgt) +
-				     (data->number * sizeof(struct caamsgt)));
-	} else {
-		data->buf = caam_calloc(data->number * sizeof(struct caambuf));
-		data->sgt = NULL;
-	}
-
-	if (!data->buf || (!data->sgt && data->sgt_type)) {
-		caam_sgtbuf_free(data);
-		return CAAM_OUT_MEMORY;
-	}
-
-	return CAAM_NO_ERROR;
-}
-
 bool caam_mem_is_cached_buf(void *buf, size_t size)
 {
 	enum teecore_memtypes mtype = MEM_AREA_MAXTYPE;
@@ -226,43 +189,6 @@ bool caam_mem_is_cached_buf(void *buf, size_t size)
 		is_cached = core_vbuf_is(CORE_MEM_CACHED, buf, size);
 
 	return is_cached;
-}
-
-enum caam_status caam_set_or_alloc_align_buf(void *orig, struct caambuf *dst,
-					     size_t size, bool *realloc)
-{
-	uint32_t cacheline_size = 0;
-	enum caam_status retstatus = CAAM_FAILURE;
-
-	if (caam_mem_is_cached_buf(orig, size)) {
-		/*
-		 * Check if either orig pointer or size are aligned on the
-		 * cache line size.
-		 * If not, reallocate a buffer aligned on cache line.
-		 */
-		cacheline_size = dcache_get_line_size();
-		if (!IS_PTR_ALIGN(orig, cacheline_size) ||
-		    !IS_SIZE_ALIGN(size, cacheline_size)) {
-			retstatus = caam_alloc_align_buf(dst, size);
-			if (retstatus == CAAM_NO_ERROR)
-				*realloc = true;
-
-			return retstatus;
-		}
-		dst->nocache = 0;
-	} else {
-		dst->nocache = 1;
-	}
-
-	dst->data = orig;
-	dst->paddr = virt_to_phys(dst->data);
-	if (!dst->paddr)
-		return CAAM_OUT_MEMORY;
-
-	dst->length = size;
-
-	*realloc = false;
-	return CAAM_NO_ERROR;
 }
 
 enum caam_status caam_cpy_block_src(struct caamblock *block,
@@ -341,6 +267,8 @@ int caam_mem_get_pa_area(struct caambuf *buf, struct caambuf **out_pabufs)
 	 */
 	va = (vaddr_t)buf->data;
 	pa = virt_to_phys((void *)va);
+	if (!pa)
+		return -1;
 
 	nb_pa_area = 0;
 	if (pabufs) {
@@ -356,10 +284,20 @@ int caam_mem_get_pa_area(struct caambuf *buf, struct caambuf **out_pabufs)
 		len_tohandle =
 			MIN(SMALL_PAGE_SIZE - (va & SMALL_PAGE_MASK), len);
 		next_va = va + len_tohandle;
-		next_pa = virt_to_phys((void *)next_va);
-
 		if (pabufs)
 			pabufs[nb_pa_area].length += len_tohandle;
+
+		/*
+		 * Reaches the end of buffer, exits here because
+		 * the next virtual address is out of scope.
+		 */
+		if (len == len_tohandle)
+			break;
+
+		touch_page(next_va);
+		next_pa = virt_to_phys((void *)next_va);
+		if (!next_pa)
+			return -1;
 
 		if (next_pa != (pa + len_tohandle)) {
 			nb_pa_area++;

core/arch/arm/plat-imx/crypto_conf.mk
@@ -19,6 +19,7 @@
 # DBG_RSA    BIT32(8)  // RSA trace
 # DBG_CIPHER BIT32(9)  // Cipher trace
 # DBG_BLOB   BIT32(10) // BLOB trace
+# DBG_DMAOBJ BIT32(11) // DMA Object Trace
 CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0

core/arch/arm/plat-ls/crypto_conf.mk
@@ -18,6 +18,7 @@
 # DBG_HASH   BIT32(7)  // Hash trace
 # DBG_RSA    BIT32(8)  // RSA trace
 # DBG_CIPHER BIT32(9)  // Cipher trace
+# DBG_DMAOBJ BIT32(11) // DMA Object Trace
 CFG_DBG_CAAM_TRACE ?= BIT32(1)
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0

core/drivers/crypto/caam/caam_desc.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2019 NXP
+ * Copyright 2019, 2021 NXP
  *
  * Brief   Descriptor construction functions.
  */
@@ -57,7 +57,11 @@ void caam_desc_add_ptr(uint32_t *desc, paddr_t ptr)
 #ifdef CFG_CAAM_64BIT
 	struct ptr_addr *ptr_addr = (struct ptr_addr *)(uintptr_t)last;
 
+#ifdef CFG_ARM64_core
 	caam_write_val32(&ptr_addr->high, ptr >> 32);
+#else
+	caam_write_val32(&ptr_addr->high, 0);
+#endif /* CFG_ARM64_core */
 	caam_write_val32(&ptr_addr->low, ptr);
 	inc++;
 #else
@@ -80,7 +84,8 @@ void caam_desc_push(struct caam_inring_entry *in_entry, paddr_t paddr)
 
 paddr_t caam_desc_pop(struct caam_outring_entry *out_entry)
 {
-	const uint32_t *a32 = (const uint32_t *)(&out_entry->desc);
+	const uintptr_t v_desc = (uintptr_t)&out_entry->desc;
+	const uint32_t *a32 = (const uint32_t *)v_desc;
 
 #ifdef CFG_CAAM_BIG_ENDIAN
 	return SHIFT_U64(get_be32(&a32[0]), 32) | get_be32(&a32[1]);

core/drivers/crypto/caam/include/caam_common.h
@@ -1,9 +1,8 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2019, 2021 NXP
  *
- * Brief   CAAM driver common include file.
- *         Definition of the internal driver status codes.
+ * CAAM driver common include file.
  */
 
 #ifndef __CAAM_COMMON_H__
@@ -12,7 +11,7 @@
 #include <caam_desc_helper.h>
 #include <caam_status.h>
 #include <caam_trace.h>
-#include <types_ext.h>
+#include <caam_types.h>
 
 /*
  * Definition of the number of CAAM Jobs to manage in JR queues
@@ -42,71 +41,7 @@ enum caam_jr_owner {
 	JROWN_ARM_NS = 0x4,                  /* Non-Secure ARM */
 	JROWN_ARM_S = JROWNER_SECURE | 0x4,  /* Secure ARM */
 };
-#endif
 
-/*
- * Definition of a CAAM buffer type
- */
-struct caambuf {
-	uint8_t *data;   /* Data buffer */
-	paddr_t paddr;   /* Physical address of the buffer */
-	size_t length;   /* Number of bytes in the data buffer */
-	uint8_t nocache; /* =1 if buffer is not cacheable, 0 otherwise */
-};
-
-/*
- * Definition of a CAAM Block buffer. Buffer used to store
- * user source data to build a full algorithm block buffer
- */
-struct caamblock {
-	struct caambuf buf; /* Data buffer */
-	size_t filled;      /* Current length filled in the buffer */
-	size_t max;         /* Maximum size of the block */
-};
-
-/*
- * Definition of key size
- */
-struct caamdefkey {
-	uint8_t min; /* Minimum size */
-	uint8_t max; /* Maximum size */
-	uint8_t mod; /* Key modulus */
-};
-
-/*
- * Scatter/Gather Table type for input and output data
- */
-struct caamsgt {
-#if defined(CFG_CAAM_64BIT) && defined(CFG_CAAM_LITTLE_ENDIAN)
-	/* Word 0 */
-	uint32_t ptr_ls;   /* Address pointer (LS 32 bits) */
-
-	/* Word 1 */
-	uint32_t ptr_ms;   /* Address pointer (MS 8 LSBs) */
-#else
-	/* Word 0 */
-	uint32_t ptr_ms;   /* Address pointer (MS 8 LSBs) */
-
-	/* Word 1 */
-	uint32_t ptr_ls;   /* Address pointer (LS 32 bits) */
-#endif
-
-	/* Word 2 */
-	uint32_t len_f_e;  /* Length 30bits + 1bit Final + 1bit Extension) */
-
-	/* Word 3 */
-	uint32_t offset;   /* Offset in memory buffer (13 LSBs) */
-};
-
-/*
- * Data buffer encoded in SGT format
- */
-struct caamsgtbuf {
-	struct caamsgt *sgt; /* SGT Array */
-	struct caambuf *buf; /* Buffer Array */
-	unsigned int number; /* Number of SGT/Buf */
-	size_t length;       /* Total length of the data encoded */
-	bool sgt_type;       /* Define the data format */
-};
+#endif /* CFG_MX7ULP */
 
 #endif /* __CAAM_COMMON_H__ */

core/drivers/crypto/caam/include/caam_io.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2019 NXP
+ * Copyright 2019, 2021 NXP
  *
  * Brief   Specific Macro used to read/write value with a specific
  *         format (BE/LE, 32/64 bits) to be updated for future platform
@@ -28,6 +28,9 @@
 /* Little Endian 32 bits Value access */
 #define caam_read_val32(a)	get_le32(a)
 #define caam_write_val32(a, v)	put_le32(a, v)
+
+#define caam_read_val64(a)     get_le64(a)
+#define caam_write_val64(a, v) put_le64(a, v)
 #endif
 
 #endif /* __CAAM_IO_H__ */

core/drivers/crypto/caam/include/caam_jr_status.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2020 NXP
+ * Copyright 2018-2021 NXP
  *
  * Brief   CAAM Job Ring Status definition header.
  */
@@ -26,6 +26,7 @@
 #define JRSTA_CCB_CHAID_RNG       SHIFT_U32(0x5, 4)
 #define JRSTA_CCB_ERRID_HW        SHIFT_U32(0xB, 0)
 #define JRSTA_DECO_ERRID_FORMAT   SHIFT_U32(0x88, 0)
+#define JRSTA_DECO_INV_SIGNATURE  SHIFT_U32(0x86, 0)
 
 /* Return the Halt User status else 0 if not a Jump Halt User */
 #define JRSTA_GET_HALT_USER(status)                                            \

core/drivers/crypto/caam/include/caam_status.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2019, 2021 NXP
  *
  * Brief   CAAM driver internal status definition
  */
@@ -16,6 +16,7 @@ enum caam_status {
 	CAAM_FAILURE,        /* General failure */
 	CAAM_OUT_MEMORY,     /* Out of memory */
 	CAAM_BAD_PARAM,      /* Bad parameters */
+	CAAM_SHORT_BUFFER,   /* Buffer is too short */
 	CAAM_BUSY,           /* Operation is not possible, system busy */
 	CAAM_PENDING,        /* Operation is pending */
 	CAAM_TIMEOUT,        /* Operation timeout */

commit 7c1ddf0c63c0384a1691139e263e10ede5d5f1f6

drivers: caam: fix potential memory leak
Free CAAM buffer pabufs before exiting the function in case of an error.

Fixes: 38923d487 ("drivers: caam: implement CAAM DMA Object")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/caam/utils/utils_mem.c
@@ -272,7 +272,7 @@ int caam_mem_get_pa_area(struct caambuf *buf, struct caambuf **out_pabufs)
 	va = (vaddr_t)buf->data;
 	pa = virt_to_phys((void *)va);
 	if (!pa)
-		return -1;
+		goto err;
 
 	nb_pa_area = 0;
 	if (pabufs) {
@@ -301,7 +301,7 @@ int caam_mem_get_pa_area(struct caambuf *buf, struct caambuf **out_pabufs)
 		touch_page(next_va);
 		next_pa = virt_to_phys((void *)next_va);
 		if (!next_pa)
-			return -1;
+			goto err;
 
 		if (next_pa != (pa + len_tohandle)) {
 			nb_pa_area++;
@@ -324,6 +324,10 @@ int caam_mem_get_pa_area(struct caambuf *buf, struct caambuf **out_pabufs)
 
 	MEM_TRACE("Nb Physical Area %d", nb_pa_area + 1);
 	return nb_pa_area + 1;
+
+err:
+	free(pabufs);
+	return -1;
 }
 
 void caam_mem_cpy_ltrim_buf(struct caambuf *dst, struct caambuf *src)