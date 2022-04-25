OP-TEE/optee_os

commit e43ab7a8557a87604fc4fccc0ac03b0bcac81b83

core: driver: generic resources for crypto cipher driver
Add a generic cryptographic Cipher driver interface connecting
TEE Crypto generic APIs to HW driver interface

To make Cipher HW driver generic and to reduce the amount of function
parameters, add structure data for the initialization and update
function driver call.
Checks the Cipher operation's parameter in the generic part before
calling the HW driver.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/crypto_api/cipher/cipher.c
@@ -1,0 +1,200 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * Crypto Cipher interface implementation to enable HW driver.
+ */
+#include <assert.h>
+#include <crypto/crypto.h>
+#include <crypto/crypto_impl.h>
+#include <drvcrypt.h>
+#include <drvcrypt_cipher.h>
+#include <kernel/panic.h>
+#include <malloc.h>
+#include <utee_defines.h>
+#include <util.h>
+
+static const struct crypto_cipher_ops cipher_ops;
+
+/*
+ * Returns the reference to the driver context
+ *
+ * @ctx    Reference the API context pointer
+ */
+static struct crypto_cipher *to_cipher_ctx(struct crypto_cipher_ctx *ctx)
+{
+	assert(ctx && ctx->ops == &cipher_ops);
+
+	return container_of(ctx, struct crypto_cipher, cipher_ctx);
+}
+
+/*
+ * Free cipher context
+ *
+ * @ctx    Reference the API context pointer
+ */
+static void cipher_free_ctx(struct crypto_cipher_ctx *ctx)
+{
+	struct crypto_cipher *cipher = to_cipher_ctx(ctx);
+
+	if (cipher->op && cipher->op->free_ctx)
+		cipher->op->free_ctx(cipher->ctx);
+
+	free(cipher);
+}
+
+/*
+ * Copy cipher context
+ *
+ * @dst_ctx  [out] Reference the API context pointer destination
+ * @src_ctx  Reference the API context pointer source
+ */
+static void cipher_copy_state(struct crypto_cipher_ctx *dst_ctx,
+			      struct crypto_cipher_ctx *src_ctx)
+{
+	struct crypto_cipher *cipher_src = to_cipher_ctx(src_ctx);
+	struct crypto_cipher *cipher_dst = to_cipher_ctx(dst_ctx);
+
+	if (cipher_src->op && cipher_src->op->copy_state)
+		cipher_src->op->copy_state(cipher_dst->ctx, cipher_src->ctx);
+}
+
+/*
+ * Initialization of the cipher operation
+ *
+ * @ctx      Reference the API context pointer
+ * @mode     Operation mode
+ * @key1     First Key
+ * @key1_len Length of the first key
+ * @key2     Second Key
+ * @key2_len Length of the second key
+ * @iv       Initial Vector
+ * @iv_len   Length of the IV
+ */
+static TEE_Result cipher_init(struct crypto_cipher_ctx *ctx,
+			      TEE_OperationMode mode, const uint8_t *key1,
+			      size_t key1_len, const uint8_t *key2,
+			      size_t key2_len, const uint8_t *iv, size_t iv_len)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct crypto_cipher *cipher = to_cipher_ctx(ctx);
+
+	if ((!key1 && key1_len) || (!key2 && key2_len) || (!iv && iv_len)) {
+		CRYPTO_TRACE("One of the key is not correct");
+		CRYPTO_TRACE("key1 @%p-%zu bytes", key1, key1_len);
+		CRYPTO_TRACE("key2 @%p-%zu bytes", key1, key1_len);
+		CRYPTO_TRACE("iv   @%p-%zu bytes", iv, iv_len);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (cipher->op && cipher->op->init) {
+		struct drvcrypt_cipher_init dinit = {
+			.ctx = cipher->ctx,
+			.encrypt = (mode == TEE_MODE_ENCRYPT),
+			.key1.data = (uint8_t *)key1,
+			.key1.length = key1_len,
+			.key2.data = (uint8_t *)key2,
+			.key2.length = key2_len,
+			.iv.data = (uint8_t *)iv,
+			.iv.length = iv_len,
+		};
+
+		ret = cipher->op->init(&dinit);
+	}
+
+	CRYPTO_TRACE("cipher ret 0x%" PRIX32, ret);
+	return ret;
+}
+
+/*
+ * Update of the cipher operation
+ *
+ * @ctx        Reference the API context pointer
+ * @last_block True if last block to handle
+ * @data       Data to encrypt/decrypt
+ * @len        Length of the input data and output result
+ * @dst        [out] Output data of the operation
+ */
+static TEE_Result cipher_update(struct crypto_cipher_ctx *ctx, bool last_block,
+				const uint8_t *data, size_t len, uint8_t *dst)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct crypto_cipher *cipher = to_cipher_ctx(ctx);
+
+	if (!dst) {
+		CRYPTO_TRACE("Destination buffer error");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (!data && len) {
+		CRYPTO_TRACE("Bad data data @%p-%zu bytes", data, len);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (cipher->op && cipher->op->update) {
+		struct drvcrypt_cipher_update dupdate = {
+			.ctx = cipher->ctx,
+			.last = last_block,
+			.src.data = (uint8_t *)data,
+			.src.length = len,
+			.dst.data = dst,
+			.dst.length = len,
+		};
+
+		ret = cipher->op->update(&dupdate);
+	}
+
+	CRYPTO_TRACE("cipher ret 0x%" PRIX32, ret);
+	return ret;
+}
+
+/*
+ * Finalize the cipher operation
+ *
+ * @ctx   Reference the API context pointer
+ */
+static void cipher_final(struct crypto_cipher_ctx *ctx)
+{
+	struct crypto_cipher *cipher = to_cipher_ctx(ctx);
+
+	if (cipher->op && cipher->op->final)
+		cipher->op->final(cipher->ctx);
+}
+
+static const struct crypto_cipher_ops cipher_ops = {
+	.init = cipher_init,
+	.update = cipher_update,
+	.final = cipher_final,
+	.free_ctx = cipher_free_ctx,
+	.copy_state = cipher_copy_state,
+};
+
+TEE_Result drvcrypt_cipher_alloc_ctx(struct crypto_cipher_ctx **ctx,
+				     uint32_t algo)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct crypto_cipher *cipher = NULL;
+
+	CRYPTO_TRACE("Cipher alloc_ctx algo 0x%" PRIX32, algo);
+
+	assert(ctx);
+
+	cipher = calloc(1, sizeof(*cipher));
+	if (!cipher)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	cipher->op = drvcrypt_get_ops(CRYPTO_CIPHER);
+	if (cipher->op && cipher->op->alloc_ctx)
+		ret = cipher->op->alloc_ctx(&cipher->ctx, algo);
+
+	if (ret != TEE_SUCCESS) {
+		free(cipher);
+	} else {
+		cipher->cipher_ctx.ops = &cipher_ops;
+		*ctx = &cipher->cipher_ctx;
+	}
+
+	CRYPTO_TRACE("Cipher alloc_ctx ret 0x%" PRIX32, ret);
+
+	return ret;
+}

core/crypto/crypto.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2017, Linaro Limited
+ * Copyright 2020 NXP
  */
 
 #include <assert.h>
@@ -18,7 +19,7 @@ TEE_Result crypto_hash_alloc_ctx(void **ctx, uint32_t algo)
 	struct crypto_hash_ctx *c = NULL;
 
 	/*
-	 * Use default cryptographic implement if no matching
+	 * Use default cryptographic implementation if no matching
 	 * drvcrypt device.
 	 */
 	res = drvcrypt_hash_alloc_ctx(&c, algo);
@@ -94,48 +95,56 @@ TEE_Result crypto_hash_final(void *ctx, uint8_t *digest, size_t len)
 
 TEE_Result crypto_cipher_alloc_ctx(void **ctx, uint32_t algo)
 {
-	TEE_Result res = TEE_SUCCESS;
+	TEE_Result res = TEE_ERROR_NOT_IMPLEMENTED;
 	struct crypto_cipher_ctx *c = NULL;
 
-	switch (algo) {
-	case TEE_ALG_AES_ECB_NOPAD:
-		res = crypto_aes_ecb_alloc_ctx(&c);
-		break;
-	case TEE_ALG_AES_CBC_NOPAD:
-		res = crypto_aes_cbc_alloc_ctx(&c);
-		break;
-	case TEE_ALG_AES_CTR:
-		res = crypto_aes_ctr_alloc_ctx(&c);
-		break;
-	case TEE_ALG_AES_CTS:
-		res = crypto_aes_cts_alloc_ctx(&c);
-		break;
-	case TEE_ALG_AES_XTS:
-		res = crypto_aes_xts_alloc_ctx(&c);
-		break;
-	case TEE_ALG_DES_ECB_NOPAD:
-		res = crypto_des_ecb_alloc_ctx(&c);
-		break;
-	case TEE_ALG_DES3_ECB_NOPAD:
-		res = crypto_des3_ecb_alloc_ctx(&c);
-		break;
-	case TEE_ALG_DES_CBC_NOPAD:
-		res = crypto_des_cbc_alloc_ctx(&c);
-		break;
-	case TEE_ALG_DES3_CBC_NOPAD:
-		res = crypto_des3_cbc_alloc_ctx(&c);
-		break;
-	case TEE_ALG_SM4_ECB_NOPAD:
-		res = crypto_sm4_ecb_alloc_ctx(&c);
-		break;
-	case TEE_ALG_SM4_CBC_NOPAD:
-		res = crypto_sm4_cbc_alloc_ctx(&c);
-		break;
-	case TEE_ALG_SM4_CTR:
-		res = crypto_sm4_ctr_alloc_ctx(&c);
-		break;
-	default:
-		return TEE_ERROR_NOT_IMPLEMENTED;
+	/*
+	 * Use default cryptographic implementation if no matching
+	 * drvcrypt device.
+	 */
+	res = drvcrypt_cipher_alloc_ctx(&c, algo);
+
+	if (res == TEE_ERROR_NOT_IMPLEMENTED) {
+		switch (algo) {
+		case TEE_ALG_AES_ECB_NOPAD:
+			res = crypto_aes_ecb_alloc_ctx(&c);
+			break;
+		case TEE_ALG_AES_CBC_NOPAD:
+			res = crypto_aes_cbc_alloc_ctx(&c);
+			break;
+		case TEE_ALG_AES_CTR:
+			res = crypto_aes_ctr_alloc_ctx(&c);
+			break;
+		case TEE_ALG_AES_CTS:
+			res = crypto_aes_cts_alloc_ctx(&c);
+			break;
+		case TEE_ALG_AES_XTS:
+			res = crypto_aes_xts_alloc_ctx(&c);
+			break;
+		case TEE_ALG_DES_ECB_NOPAD:
+			res = crypto_des_ecb_alloc_ctx(&c);
+			break;
+		case TEE_ALG_DES3_ECB_NOPAD:
+			res = crypto_des3_ecb_alloc_ctx(&c);
+			break;
+		case TEE_ALG_DES_CBC_NOPAD:
+			res = crypto_des_cbc_alloc_ctx(&c);
+			break;
+		case TEE_ALG_DES3_CBC_NOPAD:
+			res = crypto_des3_cbc_alloc_ctx(&c);
+			break;
+		case TEE_ALG_SM4_ECB_NOPAD:
+			res = crypto_sm4_ecb_alloc_ctx(&c);
+			break;
+		case TEE_ALG_SM4_CBC_NOPAD:
+			res = crypto_sm4_cbc_alloc_ctx(&c);
+			break;
+		case TEE_ALG_SM4_CTR:
+			res = crypto_sm4_ctr_alloc_ctx(&c);
+			break;
+		default:
+			return TEE_ERROR_NOT_IMPLEMENTED;
+		}
 	}
 
 	if (!res)

core/drivers/crypto/crypto_api/cipher/sub.mk
@@ -1,0 +1,1 @@
+srcs-y += cipher.c

core/drivers/crypto/crypto_api/include/drvcrypt.h
@@ -51,6 +51,7 @@ enum drvcrypt_algo_id {
 	CRYPTO_HASH = 0, /* Hash driver */
 	CRYPTO_RSA,      /* Asymmetric RSA driver */
 	CRYPTO_MATH,	 /* Mathematical driver */
+	CRYPTO_CIPHER,   /* Cipher driver */
 	CRYPTO_MAX_ALGO  /* Maximum number of algo supported */
 };
 

core/drivers/crypto/crypto_api/include/drvcrypt_cipher.h
@@ -1,0 +1,72 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * Cipher interface calling the crypto driver.
+ */
+#ifndef __DRVCRYPT_CIPHER_H__
+#define __DRVCRYPT_CIPHER_H__
+
+#include <crypto/crypto_impl.h>
+#include <tee_api_types.h>
+
+/*
+ * Cipher operation context
+ */
+struct crypto_cipher {
+	struct crypto_cipher_ctx cipher_ctx; /* Crypto cipher API context */
+	void *ctx;                           /* Cipher context */
+	struct drvcrypt_cipher *op;          /* Reference to the operation */
+};
+
+/*
+ * Cipher algorithm initialization data
+ */
+struct drvcrypt_cipher_init {
+	void *ctx;		  /* Software context */
+	bool encrypt;		  /* Encrypt or decrypt direction */
+	struct drvcrypt_buf key1; /* First key */
+	struct drvcrypt_buf key2; /* Second key */
+	struct drvcrypt_buf iv;	  /* Initial vector */
+};
+
+/*
+ * Cipher algorithm update data
+ */
+struct drvcrypt_cipher_update {
+	void *ctx;		 /* Software context */
+	bool encrypt;		 /* Encrypt or decrypt direction */
+	bool last;		 /* Last block to handle */
+	struct drvcrypt_buf src; /* Buffer source (message or cipher) */
+	struct drvcrypt_buf dst; /* Buffer dest (message or cipher) */
+};
+
+/*
+ * Crypto library cipher driver operations
+ */
+struct drvcrypt_cipher {
+	/* Allocate context */
+	TEE_Result (*alloc_ctx)(void **ctx, uint32_t algo);
+	/* Free context */
+	void (*free_ctx)(void *ctx);
+	/* Initialize the cipher operation */
+	TEE_Result (*init)(struct drvcrypt_cipher_init *dinit);
+	/* Update the cipher operation */
+	TEE_Result (*update)(struct drvcrypt_cipher_update *dupdate);
+	/* Finalize the cipher operation */
+	void (*final)(void *ctx);
+	/* Copy cipher context */
+	void (*copy_state)(void *dst_ctx, void *src_ctx);
+};
+
+/*
+ * Register a cipher processing driver in the crypto API
+ *
+ * @ops - Driver operations
+ */
+static inline TEE_Result drvcrypt_register_cipher(struct drvcrypt_cipher *ops)
+{
+	return drvcrypt_register(CRYPTO_CIPHER, (void *)ops);
+}
+
+#endif /* __DRVCRYPT_CIPHER_H__ */

core/drivers/crypto/crypto_api/sub.mk
@@ -5,3 +5,4 @@ subdirs-y += math
 subdirs-$(CFG_CRYPTO_DRV_HASH)    += hash
 subdirs-$(CFG_CRYPTO_DRV_ACIPHER) += acipher
 subdirs-$(CFG_CRYPTO_DRV_ACIPHER) += oid
+subdirs-$(CFG_CRYPTO_DRV_CIPHER) += cipher

core/include/crypto/crypto_impl.h
@@ -272,5 +272,17 @@ drvcrypt_hash_alloc_ctx(struct crypto_hash_ctx **ctx __unused,
 {
 	return TEE_ERROR_NOT_IMPLEMENTED;
 }
-#endif
+#endif /* CFG_CRYPTO_DRV_HASH */
+
+#ifdef CFG_CRYPTO_DRV_CIPHER
+TEE_Result drvcrypt_cipher_alloc_ctx(struct crypto_cipher_ctx **ctx,
+				     uint32_t algo);
+#else
+static inline TEE_Result
+drvcrypt_cipher_alloc_ctx(struct crypto_cipher_ctx **ctx __unused,
+			  uint32_t algo __unused)
+{
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+#endif /* CFG_CRYPTO_DRV_CIPHER */
 #endif /*__CRYPTO_CRYPTO_IMPL_H*/

commit 190546c1021c1927ef960a5f8211767ee11c3320

drivers: crypto: remove unnecessary header
Remove the following header:
 * utee_defines.h

Fixes: e43ab7a8 ("core: driver: generic resources for crypto cipher driver")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/crypto_api/cipher/cipher.c
@@ -11,7 +11,6 @@
 #include <drvcrypt_cipher.h>
 #include <kernel/panic.h>
 #include <malloc.h>
-#include <utee_defines.h>
 #include <util.h>
 
 static const struct crypto_cipher_ops cipher_ops;