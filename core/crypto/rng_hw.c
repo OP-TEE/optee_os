4a3e6b90d93a4fac9e5261a976b278c97fb3d6e3
Andrew Davis <afd@ti.com>
2022-04-29 17:59:13 -0500

    core: rng_hw: Remove hw_get_random_byte()
    Now that all everyone is moved over to hw_get_random_bytes()
    we can remove the stub hw_get_random_byte() and the weak default
    hw_get_random_bytes().
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/crypto/rng_hw.c
+++ core/crypto/rng_hw.c
@@ -8,29 +8,6 @@
 #include <tee/tee_cryp_utl.h>
 #include <types_ext.h>
 
-/*
- * This is only here to keep the compiler happy while we convert over
- * platforms. Either hw_get_random_bytes() is overridden or this
- * function is, in either case this is never called.
- */
-uint8_t __weak hw_get_random_byte(void)
-{
-	panic();
-	return 4; // chosen by fair dice roll.
-		  // guaranteed to be random.
-}
-
-TEE_Result __weak hw_get_random_bytes(void *buf, size_t blen)
-{
-	uint8_t *b = buf;
-	size_t n = 0;
-
-	for (n = 0; n < blen; n++)
-		b[n] = hw_get_random_byte();
-
-	return TEE_SUCCESS;
-}
-
 /* This is a HW RNG, no need for seeding */
 TEE_Result __weak crypto_rng_init(const void *data __unused,
 				  size_t dlen __unused)
