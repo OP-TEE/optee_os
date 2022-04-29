60296dff4e3d3adc4bafd5afe6d40b05363f9dd4
Andrew Davis <afd@ti.com>
2022-04-29 11:10:35 -0500

    core: rng_hw: Add dummy hw_get_random_byte()
    This is only used so we can start removing hw_get_random_byte() from
    platforms without causing compile errors. It is never called.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/crypto/rng_hw.c
+++ core/crypto/rng_hw.c
@@ -3,9 +3,22 @@
 
 #include <compiler.h>
 #include <crypto/crypto.h>
+#include <kernel/panic.h>
 #include <rng_support.h>
 #include <tee/tee_cryp_utl.h>
 #include <types_ext.h>
+
+/*
+ * This is only here to keep the compiler happy while we convert over
+ * platforms. Either hw_get_random_bytes() is overridden or this
+ * function is, in either case this is never called.
+ */
+uint8_t __weak hw_get_random_byte(void)
+{
+	panic();
+	return 4; // chosen by fair dice roll.
+		  // guaranteed to be random.
+}
 
 TEE_Result __weak hw_get_random_bytes(void *buf, size_t blen)
 {
