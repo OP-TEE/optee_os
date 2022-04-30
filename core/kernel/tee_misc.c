4682bf0f1aa5ba48403466207c186c12986ac8df
Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
2022-04-30 10:44:34 +0300

    core: add allocator for cache aligned memory
    Provides new common maximum cache line aligned allocator for allocating
    memory to be used when communicating with different peripherals within the
    CPU.
    
    Allocated memory can be readily used with cache maintenance operations.
    
    This is based on core/drivers/imx/dcp/dcp_utils.c.
    
    Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/kernel/tee_misc.c
+++ core/kernel/tee_misc.c
@@ -2,10 +2,12 @@
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
  */
+#include <kernel/cache_helpers.h>
 #include <kernel/chip_services.h>
 #include <kernel/tee_common_otp.h>
 #include <kernel/tee_common.h>
 #include <kernel/tee_misc.h>
+#include <malloc.h>
 #include <mm/core_memprot.h>
 #include <stdio.h>
 #include <trace.h>
@@ -117,4 +119,23 @@ bool core_is_buffer_intersect(paddr_t b, paddr_size_t bl,
 	if ((b + bl - 1 < a) || (b > a + al - 1))
 		return false;
 	return true;
+}
+
+void *alloc_cache_aligned(size_t size)
+{
+	void *ptr = NULL;
+	size_t alloc_size = 0;
+	uint32_t cacheline_size = 0;
+
+	cacheline_size = cache_get_max_line_size();
+	if (ROUNDUP_OVERFLOW(size, cacheline_size, &alloc_size))
+		return NULL;
+
+	ptr = memalign(cacheline_size, alloc_size);
+	if (!ptr)
+		return NULL;
+
+	memset(ptr, 0, size);
+
+	return ptr;
 }
