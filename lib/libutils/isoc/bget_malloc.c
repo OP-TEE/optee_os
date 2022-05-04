OP-TEE/optee_os

commit 08a5c4f9ae421384e52b87107283181e3fddf056

libutils: add MTE support in malloc() and friends
Adds support for memory tagging using the hardware assisted Armv8.5-A
Memory Tagging Extension in malloc() and friends.

When allocating memory the acquired memory range in tagged a special
pointer with the tag coded in the top bits of the pointer. Accessing
this memory without the coded tag will result in a Tag Check Exception.

MTE is enabled for both OP-TEE core and TAs at the same time with
CFG_MEMTAG=y.

When the memory is freed it's untagged again. All accesses to heap
memory using old/stale pointers will be caught. This is an efficient
method to catch use-after-free and buffer overruns on the heap. The
buffer overruns are limited in granularity to blocks of 16 bytes.
However, the heap itself is also managed in blocks of 16 bytes so
smaller buffer overruns will not damage the heap.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/isoc/bget_malloc.c
@@ -81,6 +81,7 @@
 #include <compiler.h>
 #include <config.h>
 #include <malloc.h>
+#include <memtag.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib_ext.h>
@@ -213,6 +214,20 @@ static void *maybe_tag_buf(void *buf, size_t __maybe_unused requested_size)
 	if (!buf)
 		return NULL;
 
+	COMPILE_TIME_ASSERT(MEMTAG_GRANULE_SIZE <= SizeQuant);
+
+	if (MEMTAG_IS_ENABLED) {
+		size_t sz = ROUNDUP(requested_size, MEMTAG_GRANULE_SIZE);
+
+		/*
+		 * Allocated buffer can be larger than requested when
+		 * allocating with memalign(), but we should never tag more
+		 * than allocated.
+		 */
+		assert(bget_buf_size(buf) >= sz);
+		return memtag_set_random_tags(buf, sz);
+	}
+
 #if defined(__KERNEL__)
 	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS))
 		asan_tag_access(buf, (uint8_t *)buf + requested_size);
@@ -225,10 +240,25 @@ static void *maybe_untag_buf(void *buf)
 	if (!buf)
 		return NULL;
 
+	if (MEMTAG_IS_ENABLED) {
+		size_t sz = 0;
+
+		memtag_assert_tag(buf); /* Trying to catch double free early */
+		sz = bget_buf_size(memtag_strip_tag(buf));
+		return memtag_set_tags(buf, sz, 0);
+	}
+
 #if defined(__KERNEL__)
 	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS))
 		asan_tag_heap_free(buf, (uint8_t *)buf + bget_buf_size(buf));
 #endif
+	return buf;
+}
+
+static void *strip_tag(void *buf)
+{
+	if (MEMTAG_IS_ENABLED)
+		return memtag_strip_tag(buf);
 	return buf;
 }
 
@@ -840,7 +870,7 @@ static bool gen_malloc_buffer_is_within_alloced(struct malloc_ctx *ctx,
 {
 	struct bpool_iterator itr;
 	void *b;
-	uint8_t *start_buf = buf;
+	uint8_t *start_buf = strip_tag(buf);
 	uint8_t *end_buf = start_buf + len;
 	bool ret = false;
 	uint32_t exceptions = malloc_lock(ctx);

commit 4cebb3a7cd21dda679ded00ee47a06d8e8a2ec08

libutils: fix MTE error in realloc()
With memory tagging realloc() should only untag the old buffer if the
allocation of a new buffer is successful.

The implementation of realloc() prior to this patch is based on bgetr().
This is not so convenient with MTE enabled since the old buffer must be
untagged before calling bgetr(). If realloc() fails the memory tags must
be restored, but this was not done.

Restoring the tags is made somewhat more complicated by the fact that
the size reported by bget_buf_size() may actually be larger than the
size of the tagged buffer. Memalign() is sometimes forced to allocate
buffers larger than requested in order to honor the requested alignment.

So instead of using bgetr(), realloc() is implemented using bget(),
memcpy(), memset() and brel() as needed. This is the same thing that
bgetr() did, except that the old buffer is not untagged until a new
buffer has been successfully allocated.

Fixes: 08a5c4f9ae42 ("libutils: add MTE support in malloc() and friends")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (vexpress-qemu_armv8a gp_20056)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/isoc/bget_malloc.c
@@ -499,7 +499,24 @@ void *raw_realloc(void *ptr, size_t hdr_size, size_t ftr_size,
 	if (!s)
 		s++;
 
-	p = bgetr(maybe_untag_buf(ptr), 0, 0, s, &ctx->poolset);
+	p = bget(0, 0, s, &ctx->poolset);
+
+	if (p && ptr) {
+		void *old_ptr = maybe_untag_buf(ptr);
+		bufsize old_sz = bget_buf_size(old_ptr);
+
+		if (old_sz < s) {
+			memcpy(p, old_ptr, old_sz);
+#ifndef __KERNEL__
+			/* User space reallocations are always zeroed */
+			memset((uint8_t *)p + old_sz, 0, s - old_sz);
+#endif
+		} else {
+			memcpy(p, old_ptr, s);
+		}
+
+		brel(old_ptr, &ctx->poolset, false /*!wipe*/);
+	}
 out:
 	return raw_malloc_return_hook(p, pl_size, ctx);
 }