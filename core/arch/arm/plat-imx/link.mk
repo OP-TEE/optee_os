60ee4b123a80e2f826769c84908e3c46ebccd61b
Clement Faure <clement.faure@nxp.com>
2022-05-11 17:50:03 +0200

    core: imx: link: generate entry_point_address.txt for uTee image
    For some platforms like mx7ulpevk, the `UL` attribute for CFG_DRAM_BASE
    is necessary to avoid the following error:
    
    $ PLATFORM=imx-mx7ulpevk make uTee
    core/mm/mobj.c: In function ‘mobj_init’:
    ./out/arm-plat-imx/include/generated/conf.h:222:64: warning: integer overflow in expression of type ‘int’ results in ‘-1644167168’ [-Woverflow]
      222 | #define CFG_TZDRAM_START (0x60000000 - 0x01e00000 - 0x00200000 + 0x40000000)
          |                                                                ^
    
    However, this `UL` attribute prevents the shell from properly computing
    the CFG_TZDRAM_START value :
    
    $ PLATFORM=imx-mx7ulpevk make uTee
    bash: (UL(0x60000000) - 0x01e00000 - 0x00200000 + 0x40000000): missing `)' (error token is "(0x60000000) - 0x01e00000 - 0x00200000 + 0x40000000)")
    
    To address both issues, remove the `UL` attribute from CFG_TZDRAM_START
    with Makefile string substitution.
    
    Fixes: 0f8347dcafe ("core: imx: generate uImage for imx6 and imx7 platforms")
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-imx/link.mk
+++ core/arch/arm/plat-imx/link.mk
@@ -7,5 +7,5 @@ uTee: $(link-out-dir)/uTee
 cleanfiles += $(link-out-dir)/uTee
 $(link-out-dir)/uTee: $(link-out-dir)/tee-raw.bin
 	@$(cmd-echo-silent) '  MKIMAGE $@'
-	$(q)ADDR=`printf 0x%x $$(($(CFG_TZDRAM_START)))`; \
+	$(q)ADDR=`printf 0x%x $$(($(subst UL,,$(CFG_TZDRAM_START))))`; \
 		mkimage -A arm -O linux -C none -a $$ADDR -e $$ADDR -d $< $@
