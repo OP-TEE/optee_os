8d09211bb6fe88f414525e4bba6d5ef1a234ed51
Gatien Chevallier <gatien.chevallier@foss.st.com>
2022-06-07 11:19:46 +0200

    plat-stm32mp1: conf: allow BSEC writing in debug mode
    Default embed support for burning fuses when in debug build configuration.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -160,6 +160,9 @@ endif
 CFG_ENABLE_EMBEDDED_TESTS ?= y
 CFG_WITH_STATS ?= y
 
+# Enable to allow debug
+CFG_STM32_BSEC_WRITE ?= $(CFG_TEE_CORE_DEBUG)
+
 # Default disable some support for pager memory size constraint
 ifeq ($(CFG_WITH_PAGER),y)
 CFG_TEE_CORE_DEBUG ?= n
