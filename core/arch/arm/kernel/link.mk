OP-TEE/optee_os

commit 3e2b963515c128dda914cf16feefda94529601a5

core: use C locale when generating the build date
The build date included in the version string depends on the current
locale (language), which is not very good. Force LANG=C so that english
abbreviations are used for the day and month.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/kernel/link.mk
@@ -128,10 +128,9 @@ version-o-cflags = $(filter-out -g3,$(core-platform-cflags) \
 			$(platform-cflags) $(cflagscore))
 # SOURCE_DATE_EPOCH defined for reproducible builds
 ifneq ($(SOURCE_DATE_EPOCH),)
-DATE_STR = `date -u -d @$(SOURCE_DATE_EPOCH)`
-else
-DATE_STR = `date -u`
+date-opts = -d @$(SOURCE_DATE_EPOCH)
 endif
+DATE_STR = `LANG=C date -u $(date-opts)`
 BUILD_COUNT_STR = `cat $(link-out-dir)/.buildcount`
 CORE_CC_VERSION = `$(CCcore) -v 2>&1 | grep "version " | sed 's/ *$$//'`
 define gen-version-o

commit 5ef2423254df6ebaf50b651b06b12c1b1134dfb0

core: make sure build date is always in English
Setting LANG=C before invoking the date command doesn't always result in
the "C" (English) locale being selected. The correct way is to set
LC_ALL. As explained in the locale(7) man page:

 If the second argument to setlocale(3) is an empty string, "", for the
 default locale, it is determined using the following steps:

 1. If there is a non-null environment variable LC_ALL, the value of
    LC_ALL is used.

 2. If an environment variable with the same name as one of the
    categories above exists and is non-null, its value is used for that
    category.

 3. If there is a non-null environment variable LANG, the value of LANG
    is used.

Fixes: 3e2b963515c1 ("core: use C locale when generating the build date")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Igor Opaniuk <igor.opaniuk@foundries.io>
Tested-by: Igor Opaniuk <igor.opaniuk@foundries.io>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/link.mk
@@ -148,7 +148,7 @@ version-o-cflags = $(filter-out -g3,$(core-platform-cflags) \
 ifneq ($(SOURCE_DATE_EPOCH),)
 date-opts = -d @$(SOURCE_DATE_EPOCH)
 endif
-DATE_STR = `LANG=C date -u $(date-opts)`
+DATE_STR = `LC_ALL=C date -u $(date-opts)`
 BUILD_COUNT_STR = `cat $(link-out-dir)/.buildcount`
 CORE_CC_VERSION = `$(CCcore) -v 2>&1 | grep "version " | sed 's/ *$$//'`
 define gen-version-o