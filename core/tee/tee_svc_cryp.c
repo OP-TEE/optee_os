OP-TEE/optee_os

commit ef142203a36b34925c47cd347197ce45153466fc

core: syscalls: strip tags from user space pointers
Strips the tags from user space pointers before using them.
We're relying on TCR_EL1.TCMA0=1 to make the accesses unchecked.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc_cryp.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
- * Copyright (c) 2020, Linaro Limited
+ * Copyright (c) 2020, 2022 Linaro Limited
  */
 
 #include <assert.h>
@@ -11,6 +11,7 @@
 #include <crypto/crypto.h>
 #include <kernel/tee_ta_manager.h>
 #include <kernel/user_access.h>
+#include <memtag.h>
 #include <mm/vm.h>
 #include <stdlib_ext.h>
 #include <string_ext.h>
@@ -1427,6 +1428,8 @@ static TEE_Result copy_in_attrs(struct user_ta_ctx *utc,
 	if (MUL_OVERFLOW(sizeof(struct utee_attribute), attr_count, &size))
 		return TEE_ERROR_OVERFLOW;
 
+	usr_attrs = memtag_strip_tag_const(usr_attrs);
+
 	res = vm_check_access_rights(&utc->uctx,
 				     TEE_MEMORY_ACCESS_READ |
 				     TEE_MEMORY_ACCESS_ANY_OWNER,
@@ -1444,6 +1447,8 @@ static TEE_Result copy_in_attrs(struct user_ta_ctx *utc,
 			size_t len = usr_attrs[n].b;
 			uint32_t flags = TEE_MEMORY_ACCESS_READ |
 					 TEE_MEMORY_ACCESS_ANY_OWNER;
+
+			buf = memtag_strip_tag_vaddr((void *)buf);
 
 			res = vm_check_access_rights(&utc->uctx, flags, buf,
 						     len);
@@ -2447,6 +2452,8 @@ TEE_Result syscall_hash_update(unsigned long state, const void *chunk,
 	if (!chunk_size)
 		return TEE_SUCCESS;
 
+	chunk = memtag_strip_tag_const(chunk);
+
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |
 				     TEE_MEMORY_ACCESS_ANY_OWNER,
@@ -2492,6 +2499,9 @@ TEE_Result syscall_hash_final(unsigned long state, const void *chunk,
 	/* No data, but size provided isn't valid parameters. */
 	if (!chunk && chunk_size)
 		return TEE_ERROR_BAD_PARAMETERS;
+
+	chunk = memtag_strip_tag_const(chunk);
+	hash = memtag_strip_tag(hash);
 
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -2587,6 +2597,8 @@ TEE_Result syscall_cipher_init(unsigned long state, const void *iv,
 	if (TEE_ALG_GET_CLASS(cs->algo) != TEE_OPERATION_CIPHER)
 		return TEE_ERROR_BAD_STATE;
 
+	iv = memtag_strip_tag_const(iv);
+
 	res = vm_check_access_rights(&utc->uctx,
 				     TEE_MEMORY_ACCESS_READ |
 				     TEE_MEMORY_ACCESS_ANY_OWNER,
@@ -2641,6 +2653,9 @@ static TEE_Result tee_svc_cipher_update_helper(unsigned long state,
 
 	if (cs->state != CRYP_STATE_INITIALIZED)
 		return TEE_ERROR_BAD_STATE;
+
+	src = memtag_strip_tag_const(src);
+	dst = memtag_strip_tag(dst);
 
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3240,6 +3255,8 @@ TEE_Result syscall_cryp_random_number_generate(void *buf, size_t blen)
 	struct ts_session *sess = ts_get_current_session();
 	TEE_Result res = TEE_SUCCESS;
 
+	buf = memtag_strip_tag(buf);
+
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_WRITE,
 				     (uaddr_t)buf, blen);
@@ -3262,6 +3279,8 @@ TEE_Result syscall_authenc_init(unsigned long state, const void *nonce,
 	struct tee_cryp_state *cs = NULL;
 	TEE_Result res = TEE_SUCCESS;
 	struct tee_obj *o = NULL;
+
+	nonce = memtag_strip_tag_const(nonce);
 
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3299,6 +3318,8 @@ TEE_Result syscall_authenc_update_aad(unsigned long state,
 	struct ts_session *sess = ts_get_current_session();
 	TEE_Result res = TEE_SUCCESS;
 	struct tee_cryp_state *cs = NULL;
+
+	aad_data = memtag_strip_tag_const(aad_data);
 
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3344,6 +3365,9 @@ TEE_Result syscall_authenc_update_payload(unsigned long state,
 
 	if (TEE_ALG_GET_CLASS(cs->algo) != TEE_OPERATION_AE)
 		return TEE_ERROR_BAD_STATE;
+
+	src_data = memtag_strip_tag_const(src_data);
+	dst_data = memtag_strip_tag(dst_data);
 
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3406,6 +3430,10 @@ TEE_Result syscall_authenc_enc_final(unsigned long state, const void *src_data,
 
 	if (TEE_ALG_GET_CLASS(cs->algo) != TEE_OPERATION_AE)
 		return TEE_ERROR_BAD_STATE;
+
+	src_data = memtag_strip_tag_const(src_data);
+	dst_data = memtag_strip_tag(dst_data);
+	tag = memtag_strip_tag(tag);
 
 	res = vm_check_access_rights(uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3490,6 +3518,10 @@ TEE_Result syscall_authenc_dec_final(unsigned long state,
 
 	if (TEE_ALG_GET_CLASS(cs->algo) != TEE_OPERATION_AE)
 		return TEE_ERROR_BAD_STATE;
+
+	src_data = memtag_strip_tag_const(src_data);
+	dst_data = memtag_strip_tag(dst_data);
+	tag = memtag_strip_tag_const(tag);
 
 	res = vm_check_access_rights(uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3581,6 +3613,9 @@ TEE_Result syscall_asymm_operate(unsigned long state,
 	res = tee_svc_cryp_get_state(sess, uref_to_vaddr(state), &cs);
 	if (res != TEE_SUCCESS)
 		return res;
+
+	src_data = memtag_strip_tag_const(src_data);
+	dst_data = memtag_strip_tag(dst_data);
 
 	res = vm_check_access_rights(&utc->uctx,
 				     TEE_MEMORY_ACCESS_READ |
@@ -3759,6 +3794,9 @@ TEE_Result syscall_asymm_verify(unsigned long state,
 
 	if (cs->mode != TEE_MODE_VERIFY)
 		return TEE_ERROR_BAD_PARAMETERS;
+
+	data = memtag_strip_tag_const(data);
+	sig = memtag_strip_tag_const(sig);
 
 	res = vm_check_access_rights(&utc->uctx,
 				     TEE_MEMORY_ACCESS_READ |

core/kernel/user_access.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
- * Copyright (c) 2015-2020 Linaro Limited
+ * Copyright (c) 2015-2020, 2022 Linaro Limited
  */
 
 #include <initcall.h>
@@ -14,18 +14,21 @@
 #include <tee_api_types.h>
 #include <types_ext.h>
 
-static TEE_Result check_access(uint32_t flags, vaddr_t va, size_t len)
+static TEE_Result check_access(uint32_t flags, const void *uaddr, size_t len)
 {
 	struct ts_session *s = ts_get_current_session();
 
-	return vm_check_access_rights(to_user_mode_ctx(s->ctx), flags, va, len);
+	return vm_check_access_rights(to_user_mode_ctx(s->ctx), flags,
+				      (vaddr_t)uaddr, len);
 }
 
 TEE_Result copy_from_user(void *kaddr, const void *uaddr, size_t len)
 {
 	uint32_t flags = TEE_MEMORY_ACCESS_READ | TEE_MEMORY_ACCESS_ANY_OWNER;
-	TEE_Result res = check_access(flags, (vaddr_t)uaddr, len);
+	TEE_Result res = TEE_SUCCESS;
 
+	uaddr = memtag_strip_tag_const(uaddr);
+	res = check_access(flags, uaddr, len);
 	if (!res)
 		memcpy(kaddr, uaddr, len);
 
@@ -35,8 +38,10 @@ TEE_Result copy_from_user(void *kaddr, const void *uaddr, size_t len)
 TEE_Result copy_to_user(void *uaddr, const void *kaddr, size_t len)
 {
 	uint32_t flags = TEE_MEMORY_ACCESS_WRITE | TEE_MEMORY_ACCESS_ANY_OWNER;
-	TEE_Result res = check_access(flags, (vaddr_t)uaddr, len);
+	TEE_Result res = TEE_SUCCESS;
 
+	uaddr = memtag_strip_tag(uaddr);
+	res = check_access(flags, uaddr, len);
 	if (!res)
 		memcpy(uaddr, kaddr, len);
 
@@ -46,8 +51,10 @@ TEE_Result copy_to_user(void *uaddr, const void *kaddr, size_t len)
 TEE_Result copy_from_user_private(void *kaddr, const void *uaddr, size_t len)
 {
 	uint32_t flags = TEE_MEMORY_ACCESS_READ;
-	TEE_Result res = check_access(flags, (vaddr_t)uaddr, len);
+	TEE_Result res = TEE_SUCCESS;
 
+	uaddr = memtag_strip_tag_const(uaddr);
+	res = check_access(flags, uaddr, len);
 	if (!res)
 		memcpy(kaddr, uaddr, len);
 
@@ -57,8 +64,10 @@ TEE_Result copy_from_user_private(void *kaddr, const void *uaddr, size_t len)
 TEE_Result copy_to_user_private(void *uaddr, const void *kaddr, size_t len)
 {
 	uint32_t flags = TEE_MEMORY_ACCESS_WRITE;
-	TEE_Result res = check_access(flags, (vaddr_t)uaddr, len);
+	TEE_Result res = TEE_SUCCESS;
 
+	uaddr = memtag_strip_tag(uaddr);
+	res = check_access(flags, uaddr, len);
 	if (!res)
 		memcpy(uaddr, kaddr, len);
 

core/tee/tee_svc.c
@@ -585,7 +585,7 @@ static TEE_Result utee_param_to_param(struct user_ta_ctx *utc,
 			flags |= TEE_MEMORY_ACCESS_WRITE;
 			fallthrough;
 		case TEE_PARAM_TYPE_MEMREF_INPUT:
-			p->u[n].mem.offs = a;
+			p->u[n].mem.offs = memtag_strip_tag_vaddr((void *)a);
 			p->u[n].mem.size = b;
 
 			if (!p->u[n].mem.offs) {
@@ -667,6 +667,8 @@ static TEE_Result tee_svc_copy_param(struct ts_session *sess,
 	void *va = NULL;
 	size_t n = 0;
 	size_t s = 0;
+
+	callee_params = memtag_strip_tag(callee_params);
 
 	/* fill 'param' input struct with caller params description buffer */
 	if (!callee_params) {
@@ -981,7 +983,7 @@ TEE_Result syscall_check_access_rights(unsigned long flags, const void *buf,
 	struct ts_session *s = ts_get_current_session();
 
 	return vm_check_access_rights(&to_user_ta_ctx(s->ctx)->uctx, flags,
-				      (uaddr_t)buf, len);
+				      memtag_strip_tag_vaddr(buf), len);
 }
 
 TEE_Result syscall_get_cancellation_flag(uint32_t *cancel)

core/tee/tee_svc_storage.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
- * Copyright (c) 2020, Linaro Limited
+ * Copyright (c) 2020, 2022 Linaro Limited
  */
 
 #include <config.h>
@@ -11,6 +11,7 @@
 #include <kernel/tee_ta_manager.h>
 #include <kernel/ts_manager.h>
 #include <kernel/user_access.h>
+#include <memtag.h>
 #include <mm/vm.h>
 #include <string.h>
 #include <tee_api_defines_extensions.h>
@@ -192,6 +193,7 @@ TEE_Result syscall_storage_obj_open(unsigned long storage_id, void *object_id,
 		goto exit;
 	}
 
+	object_id = memtag_strip_tag(object_id);
 	if (object_id_len) {
 		res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
 					     (uaddr_t)object_id, object_id_len);
@@ -334,6 +336,9 @@ TEE_Result syscall_storage_obj_create(unsigned long storage_id, void *object_id,
 
 	if (object_id_len > TEE_OBJECT_ID_MAX_LEN)
 		return TEE_ERROR_BAD_PARAMETERS;
+
+	object_id = memtag_strip_tag(object_id);
+	data = memtag_strip_tag(data);
 
 	if (object_id_len) {
 		res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
@@ -492,6 +497,7 @@ TEE_Result syscall_storage_obj_rename(unsigned long obj, void *object_id,
 		goto exit;
 	}
 
+	object_id = memtag_strip_tag(object_id);
 	if (object_id_len) {
 		res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
 					     (uaddr_t)object_id, object_id_len);
@@ -621,6 +627,9 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 	if (res != TEE_SUCCESS)
 		goto exit;
 
+	info = memtag_strip_tag(info);
+	obj_id = memtag_strip_tag(obj_id);
+
 	/* check rights of the provided buffers */
 	res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_WRITE,
 				     (uaddr_t)info, sizeof(TEE_ObjectInfo));
@@ -708,6 +717,7 @@ TEE_Result syscall_storage_obj_read(unsigned long obj, void *data, size_t len,
 		goto exit;
 	}
 
+	data = memtag_strip_tag(data);
 	/* check rights of the provided buffer */
 	res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_WRITE,
 				     (uaddr_t)data, len);
@@ -764,6 +774,7 @@ TEE_Result syscall_storage_obj_write(unsigned long obj, void *data, size_t len)
 		goto exit;
 	}
 
+	data = memtag_strip_tag(data);
 	/* check rights of the provided buffer */
 	res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
 				     (uaddr_t)data, len);

commit ecd1007b0f791b3a93c374e2ca60649667a45108

core: crypto: mte: strip tag before calling vm_check_access_rights()
op_attr_bignum_to_user() lacks a call to memtag_strip_tag() before it
calls vm_check_access_rights(). This results in the memory buffer not
being found and the function returning TEE_ERROR_ACCESS_DENIED.

Test case: xtest pkcs11_1019 on QEMUv8, build command:

 make CFG_PKCS11_TA=y CFG_USER_TA_TARGET_pkcs11=ta_arm64 MEMTAG=y run

Fixes: ef142203a36b ("core: syscalls: strip tags from user space pointers")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc_cryp.c
@@ -755,6 +755,8 @@ static TEE_Result op_attr_bignum_to_user(void *attr,
 	if (s < req_size || !buffer)
 		return TEE_ERROR_SHORT_BUFFER;
 
+	buffer = memtag_strip_tag(buffer);
+
 	/* Check we can access data using supplied user mode pointer */
 	res = vm_check_access_rights(&to_user_ta_ctx(sess->ctx)->uctx,
 				     TEE_MEMORY_ACCESS_READ |