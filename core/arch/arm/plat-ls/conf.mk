69ecfb92ced86283dd3fc0b41455acdde4937f80
Sahil Malhotra <sahil.malhotra@nxp.com>
2022-07-07 17:38:36 +0530

    core: plat-ls: set CFG_NUM_THREADS ?= 2 for LS1012ARDB
    xtest regression_1009.3 fails on LS1012ARDB because the test needs
    at least two threads but the default configuration for the platform
    enables only one. Set CFG_NUM_THREADS ?= 2 to fix the issue.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-ls/conf.mk
+++ core/arch/arm/plat-ls/conf.mk
@@ -33,6 +33,7 @@ include core/arch/arm/cpu/cortex-armv8-0.mk
 $(call force,CFG_TEE_CORE_NB_CORE,1)
 $(call force,CFG_DRAM0_SIZE,0x40000000)
 $(call force,CFG_CORE_CLUSTER_SHIFT,2)
+CFG_NUM_THREADS ?= 2
 CFG_SHMEM_SIZE ?= 0x00200000
 endif
 
