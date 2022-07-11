38df614f7f43a4aabaad8f3e9d4a6517c0ea7cb4
Lionel Debieve <lionel.debieve@foss.st.com>
2022-07-11 16:00:40 +0200

    drivers: stm32_bsec: keep stm32_bsec_permanent_lock_otp() under flag
    Keep the function to access the OTP lock under the CFG_STM32_BSEC_WRITE
    flag to align with the write function.
    
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>

--- core/drivers/stm32_bsec.c
+++ core/drivers/stm32_bsec.c
@@ -401,7 +401,6 @@ out:
 
 	return result;
 }
-#endif /*CFG_STM32_BSEC_WRITE*/
 
 TEE_Result stm32_bsec_permanent_lock_otp(uint32_t otp_id)
 {
@@ -476,6 +475,7 @@ out:
 
 	return result;
 }
+#endif /*CFG_STM32_BSEC_WRITE*/
 
 TEE_Result stm32_bsec_write_debug_conf(uint32_t value)
 {
