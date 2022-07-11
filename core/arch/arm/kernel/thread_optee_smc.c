OP-TEE/optee_os

commit feb290a51087026ce7aef515e5d5f00d16728856

core: add OPTEE_SMC_CALL_WITH_RPC_ARG
Adds OPTEE_SMC_CALL_WITH_RPC_ARG and OPTEE_SMC_CALL_WITH_REGD_ARG where
the struct optee_msg_arg to be used for RPC is appended in the memory
following the normal argument struct optee_msg_arg.
OPTEE_SMC_CALL_WITH_REGD_ARG only works with registered shared memory, a
cookie and an offset is used instead a physical address.

The presence OPTEE_SMC_CALL_WITH_RPC_ARG and
OPTEE_SMC_CALL_WITH_REGD_ARG is indicated by the new
OPTEE_SMC_SEC_CAP_RPC_ARG bit returned by
OPTEE_SMC_EXCHANGE_CAPABILITIES. OPTEE_SMC_EXCHANGE_CAPABILITIES also
reports the number of arguments that the RPC argument struct must have
room for.

OPTEE_SMC_CALL_WITH_RPC_ARG, OPTEE_SMC_CALL_WITH_ARG and
OPTEE_SMC_CALL_WITH_REGD_ARG can be used interleaved.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/thread_optee_smc.c
@@ -97,106 +97,192 @@ static void thread_rpc_free_arg(uint64_t cookie)
 	}
 }
 
-static struct mobj *get_cmd_buffer(paddr_t parg, uint32_t *num_params)
+static uint32_t get_msg_arg(struct mobj *mobj, size_t offset,
+			    size_t *num_params, struct optee_msg_arg **arg,
+			    struct optee_msg_arg **rpc_arg)
 {
-	struct optee_msg_arg *arg;
-	size_t args_size;
+	void *p = NULL;
+	size_t sz = 0;
 
-	arg = phys_to_virt(parg, MEM_AREA_NSEC_SHM,
-			   sizeof(struct optee_msg_arg));
-	if (!arg)
-		return NULL;
-
-	*num_params = READ_ONCE(arg->num_params);
-	if (*num_params > OPTEE_MSG_MAX_NUM_PARAMS)
-		return NULL;
-
-	args_size = OPTEE_MSG_GET_ARG_SIZE(*num_params);
-
-	return mobj_shm_alloc(parg, args_size, 0);
-}
-
-#ifdef CFG_CORE_DYN_SHM
-static struct mobj *map_cmd_buffer(paddr_t parg, uint32_t *num_params)
-{
-	struct mobj *mobj;
-	struct optee_msg_arg *arg;
-	size_t args_size;
-
-	assert(!(parg & SMALL_PAGE_MASK));
-	/* mobj_mapped_shm_alloc checks if parg resides in nonsec ddr */
-	mobj = mobj_mapped_shm_alloc(&parg, 1, 0, 0);
 	if (!mobj)
-		return NULL;
+		return OPTEE_SMC_RETURN_EBADADDR;
 
-	arg = mobj_get_va(mobj, 0, SMALL_PAGE_SIZE);
-	if (!arg)
-		goto err;
+	p = mobj_get_va(mobj, offset, sizeof(struct optee_msg_arg));
+	if (!p || !IS_ALIGNED_WITH_TYPE(p, struct optee_msg_arg))
+		return OPTEE_SMC_RETURN_EBADADDR;
 
-	*num_params = READ_ONCE(arg->num_params);
+	*arg = p;
+	*num_params = READ_ONCE((*arg)->num_params);
 	if (*num_params > OPTEE_MSG_MAX_NUM_PARAMS)
-		goto err;
+		return OPTEE_SMC_RETURN_EBADADDR;
 
-	args_size = OPTEE_MSG_GET_ARG_SIZE(*num_params);
-	if (args_size > SMALL_PAGE_SIZE) {
-		EMSG("Command buffer spans across page boundary");
-		goto err;
+	sz = OPTEE_MSG_GET_ARG_SIZE(*num_params);
+	if (!mobj_get_va(mobj, offset, sz))
+		return OPTEE_SMC_RETURN_EBADADDR;
+
+	if (rpc_arg) {
+		size_t rpc_sz = 0;
+
+		rpc_sz = OPTEE_MSG_GET_ARG_SIZE(THREAD_RPC_MAX_NUM_PARAMS);
+		p = mobj_get_va(mobj, offset + sz, rpc_sz);
+		if (!p)
+			return OPTEE_SMC_RETURN_EBADADDR;
+		*rpc_arg = p;
 	}
 
-	return mobj;
-err:
-	mobj_put(mobj);
-	return NULL;
+	return OPTEE_SMC_RETURN_OK;
 }
-#else
-static struct mobj *map_cmd_buffer(paddr_t pargi __unused,
-				   uint32_t *num_params __unused)
-{
-	return NULL;
-}
-#endif /*CFG_CORE_DYN_SHM*/
 
-static uint32_t std_smc_entry(uint32_t a0, uint32_t a1, uint32_t a2,
-			      uint32_t a3 __unused)
+static void maybe_clear_prealloc_rpc_cache(struct thread_ctx *thr)
 {
-	paddr_t parg = 0;
-	struct optee_msg_arg *arg = NULL;
-	uint32_t num_params = 0;
-	struct mobj *mobj = NULL;
+	if (IS_ENABLED(CFG_PREALLOC_RPC_CACHE) && thread_prealloc_rpc_cache) {
+		thread_rpc_free_arg(mobj_get_cookie(thr->rpc_mobj));
+		mobj_put(thr->rpc_mobj);
+		thr->rpc_arg = NULL;
+		thr->rpc_mobj = NULL;
+	}
+}
+
+static uint32_t call_entry_std(struct optee_msg_arg *arg, size_t num_params,
+			       struct optee_msg_arg *rpc_arg)
+{
+	struct thread_ctx *thr = threads + thread_get_id();
 	uint32_t rv = 0;
 
-	if (a0 != OPTEE_SMC_CALL_WITH_ARG) {
-		EMSG("Unknown SMC 0x%"PRIx32, a0);
-		DMSG("Expected 0x%x", OPTEE_SMC_CALL_WITH_ARG);
-		return OPTEE_SMC_RETURN_EBADCMD;
-	}
-	parg = reg_pair_to_64(a1, a2);
-
-	/* Check if this region is in static shared space */
-	if (core_pbuf_is(CORE_MEM_NSEC_SHM, parg,
-			 sizeof(struct optee_msg_arg))) {
-		mobj = get_cmd_buffer(parg, &num_params);
-	} else {
-		if (parg & SMALL_PAGE_MASK)
-			return OPTEE_SMC_RETURN_EBADADDR;
-		mobj = map_cmd_buffer(parg, &num_params);
+	if (rpc_arg) {
+		/*
+		 * In case the prealloc RPC arg cache is enabled, clear the
+		 * cached object for this thread.
+		 *
+		 * Normally it doesn't make sense to have the prealloc RPC
+		 * arg cache enabled together with a supplied RPC arg
+		 * struct. But if it is we must use the supplied struct and
+		 * at the same time make sure to not break anything.
+		 */
+		maybe_clear_prealloc_rpc_cache(thr);
+		thr->rpc_arg = rpc_arg;
 	}
 
-	if (!mobj || !IS_ALIGNED_WITH_TYPE(parg, struct optee_msg_arg)) {
-		EMSG("Bad arg address 0x%" PRIxPA, parg);
-		mobj_put(mobj);
-		return OPTEE_SMC_RETURN_EBADADDR;
-	}
-
-	arg = mobj_get_va(mobj, 0, OPTEE_MSG_GET_ARG_SIZE(num_params));
-	assert(arg && mobj_is_nonsec(mobj));
 	if (tee_entry_std(arg, num_params))
 		rv = OPTEE_SMC_RETURN_EBADCMD;
 	else
 		rv = OPTEE_SMC_RETURN_OK;
+
+	thread_rpc_shm_cache_clear(&thr->shm_cache);
+	if (rpc_arg)
+		thr->rpc_arg = NULL;
+
+	if (rv == OPTEE_SMC_RETURN_OK)
+		maybe_clear_prealloc_rpc_cache(thr);
+
+	return rv;
+}
+
+static uint32_t std_entry_with_parg(paddr_t parg, bool with_rpc_arg)
+{
+	size_t sz = sizeof(struct optee_msg_arg);
+	struct optee_msg_arg *rpc_arg = NULL;
+	struct optee_msg_arg *arg = NULL;
+	struct mobj *mobj = NULL;
+	size_t num_params = 0;
+	uint32_t rv = 0;
+
+	/* Check if this region is in static shared space */
+	if (core_pbuf_is(CORE_MEM_NSEC_SHM, parg, sz)) {
+		if (!IS_ALIGNED_WITH_TYPE(parg, struct optee_msg_arg))
+			goto bad_addr;
+
+		arg = phys_to_virt(parg, MEM_AREA_NSEC_SHM,
+				   sizeof(struct optee_msg_arg));
+		if (!arg)
+			goto bad_addr;
+
+		num_params = READ_ONCE(arg->num_params);
+		if (num_params > OPTEE_MSG_MAX_NUM_PARAMS)
+			return OPTEE_SMC_RETURN_EBADADDR;
+
+		sz = OPTEE_MSG_GET_ARG_SIZE(num_params);
+		if (with_rpc_arg) {
+			rpc_arg = (void *)((uint8_t *)arg + sz);
+			sz += OPTEE_MSG_GET_ARG_SIZE(THREAD_RPC_MAX_NUM_PARAMS);
+		}
+		if (!core_pbuf_is(CORE_MEM_NSEC_SHM, parg, sz))
+			goto bad_addr;
+
+		return call_entry_std(arg, num_params, rpc_arg);
+	} else {
+		if (parg & SMALL_PAGE_MASK)
+			goto bad_addr;
+		/*
+		 * mobj_mapped_shm_alloc checks if parg resides in nonsec
+		 * ddr.
+		 */
+		mobj = mobj_mapped_shm_alloc(&parg, 1, 0, 0);
+		if (!mobj)
+			goto bad_addr;
+		if (with_rpc_arg)
+			rv = get_msg_arg(mobj, 0, &num_params, &arg, &rpc_arg);
+		else
+			rv = get_msg_arg(mobj, 0, &num_params, &arg, NULL);
+		if (!rv)
+			rv = call_entry_std(arg, num_params, rpc_arg);
+		mobj_put(mobj);
+		return rv;
+	}
+
+bad_addr:
+	EMSG("Bad arg address 0x%"PRIxPA, parg);
+	return OPTEE_SMC_RETURN_EBADADDR;
+}
+
+static uint32_t std_entry_with_regd_arg(uint64_t cookie, size_t offset)
+{
+	struct optee_msg_arg *rpc_arg = NULL;
+	struct optee_msg_arg *arg = NULL;
+	size_t num_params = 0;
+	struct mobj *mobj = NULL;
+	uint32_t rv = 0;
+
+	mobj = mobj_reg_shm_get_by_cookie(cookie);
+	if (!mobj) {
+		EMSG("Bad arg cookie 0x%"PRIx64, cookie);
+		return OPTEE_SMC_RETURN_EBADADDR;
+	}
+
+	if (mobj_inc_map(mobj)) {
+		rv = OPTEE_SMC_RETURN_ENOMEM;
+		goto out;
+	}
+
+	rv = get_msg_arg(mobj, offset, &num_params, &arg, &rpc_arg);
+	if (!rv)
+		rv = call_entry_std(arg, num_params, rpc_arg);
+
+	mobj_dec_map(mobj);
+out:
 	mobj_put(mobj);
 
 	return rv;
+}
+
+static uint32_t std_smc_entry(uint32_t a0, uint32_t a1, uint32_t a2,
+			      uint32_t a3 __unused)
+{
+	const bool with_rpc_arg = true;
+
+	switch (a0) {
+	case OPTEE_SMC_CALL_WITH_ARG:
+		return std_entry_with_parg(reg_pair_to_64(a1, a2),
+					   !with_rpc_arg);
+	case OPTEE_SMC_CALL_WITH_RPC_ARG:
+		return std_entry_with_parg(reg_pair_to_64(a1, a2),
+					   with_rpc_arg);
+	case OPTEE_SMC_CALL_WITH_REGD_ARG:
+		return std_entry_with_regd_arg(reg_pair_to_64(a1, a2), a3);
+	default:
+		EMSG("Unknown SMC 0x%"PRIx32, a0);
+		return OPTEE_SMC_RETURN_EBADCMD;
+	}
 }
 
 /*
@@ -209,26 +295,10 @@ uint32_t __weak __thread_std_smc_entry(uint32_t a0, uint32_t a1, uint32_t a2,
 				       uint32_t a3, uint32_t a4 __unused,
 				       uint32_t a5 __unused)
 {
-	uint32_t rv = 0;
-
 	if (IS_ENABLED(CFG_VIRTUALIZATION))
 		virt_on_stdcall();
 
-	rv = std_smc_entry(a0, a1, a2, a3);
-
-	if (rv == OPTEE_SMC_RETURN_OK) {
-		struct thread_ctx *thr = threads + thread_get_id();
-
-		thread_rpc_shm_cache_clear(&thr->shm_cache);
-		if (!thread_prealloc_rpc_cache) {
-			thread_rpc_free_arg(mobj_get_cookie(thr->rpc_mobj));
-			mobj_put(thr->rpc_mobj);
-			thr->rpc_arg = NULL;
-			thr->rpc_mobj = NULL;
-		}
-	}
-
-	return rv;
+	return std_smc_entry(a0, a1, a2, a3);
 }
 
 bool thread_disable_prealloc_rpc_cache(uint64_t *cookie)

core/arch/arm/include/sm/optee_smc.h
@@ -135,19 +135,35 @@
 /*
  * Call with struct optee_msg_arg as argument
  *
- * When calling this function normal world has a few responsibilities:
+ * When called with OPTEE_SMC_CALL_WITH_RPC_ARG or
+ * OPTEE_SMC_CALL_WITH_REGD_ARG in a0 there is one RPC struct optee_msg_arg
+ * following after the first struct optee_msg_arg. The RPC struct
+ * optee_msg_arg has reserved space for the number of RPC parameters as
+ * returned by OPTEE_SMC_EXCHANGE_CAPABILITIES.
+ *
+ * When calling these functions normal world has a few responsibilities:
  * 1. It must be able to handle eventual RPCs
  * 2. Non-secure interrupts should not be masked
- * 3. If asynchronous notifications has be negotiated successfully, then
+ * 3. If asynchronous notifications has been negotiated successfully, then
  *    the interrupt for asynchronous notifications should be unmasked
  *    during this call.
  *
- * Call register usage:
- * a0	SMC Function ID, OPTEE_SMC*CALL_WITH_ARG
+ * Call register usage, OPTEE_SMC_CALL_WITH_ARG and
+ * OPTEE_SMC_CALL_WITH_RPC_ARG:
+ * a0	SMC Function ID, OPTEE_SMC_CALL_WITH_ARG or OPTEE_SMC_CALL_WITH_RPC_ARG
  * a1	Upper 32 bits of a 64-bit physical pointer to a struct optee_msg_arg
  * a2	Lower 32 bits of a 64-bit physical pointer to a struct optee_msg_arg
  * a3	Cache settings, not used if physical pointer is in a predefined shared
  *	memory area else per OPTEE_SMC_SHM_*
+ * a4-6	Not used
+ * a7	Hypervisor Client ID register
+ *
+ * Call register usage, OPTEE_SMC_CALL_WITH_REGD_ARG:
+ * a0	SMC Function ID, OPTEE_SMC_CALL_WITH_REGD_ARG
+ * a1	Upper 32 bits of a 64-bit shared memory cookie
+ * a2	Lower 32 bits of a 64-bit shared memory cookie
+ * a3	Offset of the struct optee_msg_arg in the shared memory with the
+ *	supplied cookie
  * a4-6	Not used
  * a7	Hypervisor Client ID register
  *
@@ -183,6 +199,10 @@
 #define OPTEE_SMC_FUNCID_CALL_WITH_ARG OPTEE_MSG_FUNCID_CALL_WITH_ARG
 #define OPTEE_SMC_CALL_WITH_ARG \
 	OPTEE_SMC_STD_CALL_VAL(OPTEE_SMC_FUNCID_CALL_WITH_ARG)
+#define OPTEE_SMC_CALL_WITH_RPC_ARG \
+	OPTEE_SMC_STD_CALL_VAL(OPTEE_SMC_FUNCID_CALL_WITH_RPC_ARG)
+#define OPTEE_SMC_CALL_WITH_REGD_ARG \
+	OPTEE_SMC_STD_CALL_VAL(OPTEE_SMC_FUNCID_CALL_WITH_REGD_ARG)
 
 /*
  * Get Shared Memory Config
@@ -266,6 +286,10 @@
  * a0	OPTEE_SMC_RETURN_OK
  * a1	bitfield of secure world capabilities OPTEE_SMC_SEC_CAP_*
  * a2	The maximum secure world notification number
+ * a3	Bit[7:0]: Number of parameters needed for RPC to be supplied
+ *		  as the second MSG arg struct for
+ *		  OPTEE_SMC_CALL_WITH_ARG
+ *	Bit[31:8]: Reserved (MBZ)
  * a3-7	Preserved
  *
  * Error return register usage:
@@ -290,6 +314,8 @@
 #define OPTEE_SMC_SEC_CAP_MEMREF_NULL		BIT(4)
 /* Secure world supports asynchronous notification of normal world */
 #define OPTEE_SMC_SEC_CAP_ASYNC_NOTIF		BIT(5)
+/* Secure world supports pre-allocating RPC arg struct */
+#define OPTEE_SMC_SEC_CAP_RPC_ARG		BIT(6)
 
 #define OPTEE_SMC_FUNCID_EXCHANGE_CAPABILITIES	U(9)
 #define OPTEE_SMC_EXCHANGE_CAPABILITIES \
@@ -490,7 +516,7 @@
  * OP-TEE keeps a record of all posted values. When an interrupt is
  * received which indicates that there are posted values this function
  * should be called until all pended values have been retrieved. When a
- * value is retrieved it's cleared from the record in secure world.
+ * value is retrieved, it's cleared from the record in secure world.
  *
  * It is expected that this function is called from an interrupt handler
  * in normal world.
@@ -526,6 +552,12 @@
 #define OPTEE_SMC_FUNCID_GET_ASYNC_NOTIF_VALUE	17
 #define OPTEE_SMC_GET_ASYNC_NOTIF_VALUE \
 	OPTEE_SMC_FAST_CALL_VAL(OPTEE_SMC_FUNCID_GET_ASYNC_NOTIF_VALUE)
+
+/* See OPTEE_SMC_CALL_WITH_RPC_ARG above */
+#define OPTEE_SMC_FUNCID_CALL_WITH_RPC_ARG	U(18)
+
+/* See OPTEE_SMC_CALL_WITH_REGD_ARG above */
+#define OPTEE_SMC_FUNCID_CALL_WITH_REGD_ARG	U(19)
 
 /*
  * Resume from RPC (for example after processing a foreign interrupt)

core/arch/arm/tee/entry_fast.c
@@ -107,6 +107,9 @@ static void tee_entry_exchange_capabilities(struct thread_smc_args *args)
 #endif
 
 	DMSG("Dynamic shared memory is %sabled", dyn_shm_en ? "en" : "dis");
+
+	args->a1 |= OPTEE_SMC_SEC_CAP_RPC_ARG;
+	args->a3 = THREAD_RPC_MAX_NUM_PARAMS;
 }
 
 static void tee_entry_disable_shm_cache(struct thread_smc_args *args)

commit 47200ef1a58bb190db1e86732fe192a3ba725407

core: arm: kernel: fix rpc shm free when prealloc is disable
Fixes commit [1] that changed implementation of the standard invocation
SMC command to introduce invocation with RPC shared memory reference.
A wrong logic was implemented to free RPC buffer on standard invocation
completion. This change fixes that by freeing the cached shared memory
when pre-allocation is disable by config switch or runtime service.

Fixes: feb290a51087 ("core: add OPTEE_SMC_CALL_WITH_RPC_ARG")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/kernel/thread_optee_smc.c
@@ -133,14 +133,12 @@ static uint32_t get_msg_arg(struct mobj *mobj, size_t offset,
 	return OPTEE_SMC_RETURN_OK;
 }
 
-static void maybe_clear_prealloc_rpc_cache(struct thread_ctx *thr)
+static void clear_prealloc_rpc_cache(struct thread_ctx *thr)
 {
-	if (IS_ENABLED(CFG_PREALLOC_RPC_CACHE) && thread_prealloc_rpc_cache) {
-		thread_rpc_free_arg(mobj_get_cookie(thr->rpc_mobj));
-		mobj_put(thr->rpc_mobj);
-		thr->rpc_arg = NULL;
-		thr->rpc_mobj = NULL;
-	}
+	thread_rpc_free_arg(mobj_get_cookie(thr->rpc_mobj));
+	mobj_put(thr->rpc_mobj);
+	thr->rpc_arg = NULL;
+	thr->rpc_mobj = NULL;
 }
 
 static uint32_t call_entry_std(struct optee_msg_arg *arg, size_t num_params,
@@ -159,7 +157,9 @@ static uint32_t call_entry_std(struct optee_msg_arg *arg, size_t num_params,
 		 * struct. But if it is we must use the supplied struct and
 		 * at the same time make sure to not break anything.
 		 */
-		maybe_clear_prealloc_rpc_cache(thr);
+		if (IS_ENABLED(CFG_PREALLOC_RPC_CACHE) &&
+		    thread_prealloc_rpc_cache)
+			clear_prealloc_rpc_cache(thr);
 		thr->rpc_arg = rpc_arg;
 	}
 
@@ -172,8 +172,9 @@ static uint32_t call_entry_std(struct optee_msg_arg *arg, size_t num_params,
 	if (rpc_arg)
 		thr->rpc_arg = NULL;
 
-	if (rv == OPTEE_SMC_RETURN_OK)
-		maybe_clear_prealloc_rpc_cache(thr);
+	if (rv == OPTEE_SMC_RETURN_OK &&
+	    !(IS_ENABLED(CFG_PREALLOC_RPC_CACHE) && thread_prealloc_rpc_cache))
+		clear_prealloc_rpc_cache(thr);
 
 	return rv;
 }