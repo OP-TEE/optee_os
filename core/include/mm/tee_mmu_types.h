OP-TEE/optee_os

commit 7c3ab7744d38842cfc4fd2cc095b6a489a2c28bb

core: mm: add TEE_MATTR_MEM_TYPE_TAGGED
Adds TEE_MATTR_MEM_TYPE_TAGGED used to map tagged memory as defined in
Armv8.5-A Memory Tagging Extension (MTE).

All OP-TEE core memory should be mapped as tagged memory when supported.

Memory potentially shared with non-secure world or other firmware should
not be mapped as tagged since we don't have control over the tags then.

The mappings used by TEE_MATTR_MEM_TYPE_TAGGED is replaced by
TEE_MATTR_MEM_TYPE_CACHED if MTE isn't supported or configured.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/mm/tee_mmu_types.h
@@ -48,6 +48,7 @@
 #define TEE_MATTR_MEM_TYPE_DEV	        U(0) /* Device-nGnRE */
 #define TEE_MATTR_MEM_TYPE_CACHED	U(1)
 #define TEE_MATTR_MEM_TYPE_STRONGLY_O	U(2) /* Device-nGnRnE  */
+#define TEE_MATTR_MEM_TYPE_TAGGED	U(3)
 
 #define TEE_MATTR_GUARDED		BIT(15)
 

core/arch/arm/mm/core_mmu_lpae.c
@@ -121,11 +121,14 @@
 #define ATTR_DEVICE_nGnRE_INDEX		0x0
 #define ATTR_IWBWA_OWBWA_NTR_INDEX	0x1
 #define ATTR_DEVICE_nGnRnE_INDEX	0x2
+#define ATTR_TAGGED_NORMAL_MEM_INDEX	0x3
 #define ATTR_INDEX_MASK			0x7
 
 #define ATTR_DEVICE_nGnRnE		(0x0)
 #define ATTR_DEVICE_nGnRE		(0x4)
 #define ATTR_IWBWA_OWBWA_NTR		(0xff)
+/* Same as ATTR_IWBWA_OWBWA_NTR but with memory tagging.  */
+#define ATTR_TAGGED_NORMAL_MEM		(0xf0)
 
 #define MAIR_ATTR_SET(attr, index)	(((uint64_t)attr) << ((index) << 3))
 
@@ -336,6 +339,8 @@ static uint32_t desc_to_mattr(unsigned level, uint64_t desc)
 	COMPILE_TIME_ASSERT(ATTR_DEVICE_nGnRE_INDEX == TEE_MATTR_MEM_TYPE_DEV);
 	COMPILE_TIME_ASSERT(ATTR_IWBWA_OWBWA_NTR_INDEX ==
 			    TEE_MATTR_MEM_TYPE_CACHED);
+	COMPILE_TIME_ASSERT(ATTR_TAGGED_NORMAL_MEM_INDEX ==
+			    TEE_MATTR_MEM_TYPE_TAGGED);
 
 	a |= ((desc & LOWER_ATTRS(ATTR_INDEX_MASK)) >> LOWER_ATTRS_SHIFT) <<
 	     TEE_MATTR_MEM_TYPE_SHIFT;
@@ -404,6 +409,9 @@ static uint64_t mattr_to_desc(unsigned level, uint32_t attr)
 		break;
 	case TEE_MATTR_MEM_TYPE_CACHED:
 		desc |= LOWER_ATTRS(ATTR_IWBWA_OWBWA_NTR_INDEX | ISH);
+		break;
+	case TEE_MATTR_MEM_TYPE_TAGGED:
+		desc |= LOWER_ATTRS(ATTR_TAGGED_NORMAL_MEM_INDEX | ISH);
 		break;
 	default:
 		/*
@@ -838,6 +846,12 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 	mair  = MAIR_ATTR_SET(ATTR_DEVICE_nGnRE, ATTR_DEVICE_nGnRE_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR, ATTR_IWBWA_OWBWA_NTR_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_DEVICE_nGnRnE, ATTR_DEVICE_nGnRnE_INDEX);
+	/*
+	 * Tagged memory isn't supported in 32-bit mode, map tagged memory
+	 * as normal memory instead.
+	 */
+	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR,
+			      ATTR_TAGGED_NORMAL_MEM_INDEX);
 	cfg->mair0 = mair;
 
 	ttbcr = TTBCR_EAE;
@@ -900,6 +914,12 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 	mair  = MAIR_ATTR_SET(ATTR_DEVICE_nGnRE, ATTR_DEVICE_nGnRE_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR, ATTR_IWBWA_OWBWA_NTR_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_DEVICE_nGnRnE, ATTR_DEVICE_nGnRnE_INDEX);
+	/*
+	 * MEMTAG isn't enabled yet, map tagged memory as normal memory
+	 * instead.
+	 */
+	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR,
+			      ATTR_TAGGED_NORMAL_MEM_INDEX);
 	cfg->mair_el1 = mair;
 
 	tcr = TCR_RES1;

core/arch/arm/mm/core_mmu_v7.c
@@ -97,6 +97,7 @@
 #define SECTION_NORMAL			SECTION_TEXCB(ATTR_DEVICE_INDEX)
 #define SECTION_NORMAL_CACHED		SECTION_TEXCB(ATTR_NORMAL_CACHED_INDEX)
 #define SECTION_STRONG_O		SECTION_TEXCB(ATTR_STRONG_O_INDEX)
+#define SECTION_TAGGED_CACHED		SECTION_TEXCB(ATTR_TAGGED_CACHED_INDEX)
 
 #define SECTION_XN			(1 << 4)
 #define SECTION_PXN			(1 << 0)
@@ -115,8 +116,11 @@
 					 (((texcb) & 0x1) << 2))
 #define SMALL_PAGE_DEVICE		SMALL_PAGE_TEXCB(ATTR_DEVICE_INDEX)
 #define SMALL_PAGE_NORMAL		SMALL_PAGE_TEXCB(ATTR_DEVICE_INDEX)
-#define SMALL_PAGE_NORMAL_CACHED	SMALL_PAGE_TEXCB(ATTR_NORMAL_CACHED_INDEX)
+#define SMALL_PAGE_NORMAL_CACHED \
+	SMALL_PAGE_TEXCB(ATTR_NORMAL_CACHED_INDEX)
 #define SMALL_PAGE_STRONG_O		SMALL_PAGE_TEXCB(ATTR_STRONG_O_INDEX)
+#define SMALL_PAGE_TAGGED_CACHED \
+	SMALL_PAGE_TEXCB(ATTR_TAGGED_CACHED_INDEX)
 #define SMALL_PAGE_ACCESS_FLAG		(1 << 4)
 #define SMALL_PAGE_UNPRIV		(1 << 5)
 #define SMALL_PAGE_RO			(1 << 9)
@@ -127,6 +131,8 @@
 #define ATTR_DEVICE_INDEX		0x0
 #define ATTR_NORMAL_CACHED_INDEX	0x1
 #define ATTR_STRONG_O_INDEX		0x2
+/* Compat with TEE_MATTR_MEM_TYPE_TAGGED */
+#define ATTR_TAGGED_CACHED_INDEX	0x3
 
 #define PRRR_IDX(idx, tr, nos)		(((tr) << (2 * (idx))) | \
 					 ((uint32_t)(nos) << ((idx) + 24)))
@@ -146,9 +152,13 @@
 #ifndef CFG_NO_SMP
 #define ATTR_NORMAL_CACHED_PRRR		PRRR_IDX(ATTR_NORMAL_CACHED_INDEX, 2, 1)
 #define ATTR_NORMAL_CACHED_NMRR		NMRR_IDX(ATTR_NORMAL_CACHED_INDEX, 1, 1)
+#define ATTR_TAGGED_CACHED_PRRR		PRRR_IDX(ATTR_TAGGED_CACHED_INDEX, 2, 1)
+#define ATTR_TAGGED_CACHED_NMRR		NMRR_IDX(ATTR_TAGGED_CACHED_INDEX, 1, 1)
 #else
 #define ATTR_NORMAL_CACHED_PRRR		PRRR_IDX(ATTR_NORMAL_CACHED_INDEX, 2, 0)
 #define ATTR_NORMAL_CACHED_NMRR		NMRR_IDX(ATTR_NORMAL_CACHED_INDEX, 3, 3)
+#define ATTR_TAGGED_CACHED_PRRR		PRRR_IDX(ATTR_TAGGED_CACHED_INDEX, 2, 0)
+#define ATTR_TAGGED_CACHED_NMRR		NMRR_IDX(ATTR_TAGGED_CACHED_INDEX, 3, 3)
 #endif
 
 #define NUM_L1_ENTRIES		4096
@@ -315,6 +325,8 @@ static uint32_t texcb_to_mattr(uint32_t texcb)
 			    TEE_MATTR_MEM_TYPE_CACHED);
 	COMPILE_TIME_ASSERT(ATTR_STRONG_O_INDEX ==
 			    TEE_MATTR_MEM_TYPE_STRONGLY_O);
+	COMPILE_TIME_ASSERT(ATTR_TAGGED_CACHED_INDEX ==
+			    TEE_MATTR_MEM_TYPE_TAGGED);
 
 	return texcb << TEE_MATTR_MEM_TYPE_SHIFT;
 }
@@ -762,9 +774,9 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 		    TEE_MMU_DEFAULT_ATTRS;
 
 	cfg->prrr = ATTR_DEVICE_PRRR | ATTR_NORMAL_CACHED_PRRR |
-		    ATTR_STRONGLY_O_PRRR;
+		    ATTR_STRONGLY_O_PRRR | ATTR_TAGGED_CACHED_PRRR;
 	cfg->nmrr = ATTR_DEVICE_NMRR | ATTR_NORMAL_CACHED_NMRR |
-		    ATTR_STRONGLY_O_NMRR;
+		    ATTR_STRONGLY_O_NMRR | ATTR_TAGGED_CACHED_NMRR;
 
 	cfg->prrr |= PRRR_NS1 | PRRR_DS1;
 

core/mm/core_mmu.c
@@ -616,6 +616,8 @@ static void add_va_space(struct tee_mmap_region *memory_map, size_t num_elems,
 uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 {
 	const uint32_t attr = TEE_MATTR_VALID_BLOCK;
+	const uint32_t tagged = TEE_MATTR_MEM_TYPE_TAGGED <<
+				TEE_MATTR_MEM_TYPE_SHIFT;
 	const uint32_t cached = TEE_MATTR_MEM_TYPE_CACHED <<
 				TEE_MATTR_MEM_TYPE_SHIFT;
 	const uint32_t noncache = TEE_MATTR_MEM_TYPE_DEV <<
@@ -623,19 +625,19 @@ uint32_t core_mmu_type_to_attr(enum teecore_memtypes t)
 
 	switch (t) {
 	case MEM_AREA_TEE_RAM:
-		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRWX | cached;
+		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRWX | tagged;
 	case MEM_AREA_TEE_RAM_RX:
 	case MEM_AREA_INIT_RAM_RX:
 	case MEM_AREA_IDENTITY_MAP_RX:
-		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRX | cached;
+		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRX | tagged;
 	case MEM_AREA_TEE_RAM_RO:
 	case MEM_AREA_INIT_RAM_RO:
-		return attr | TEE_MATTR_SECURE | TEE_MATTR_PR | cached;
+		return attr | TEE_MATTR_SECURE | TEE_MATTR_PR | tagged;
 	case MEM_AREA_TEE_RAM_RW:
 	case MEM_AREA_NEX_RAM_RO: /* This has to be r/w during init runtime */
 	case MEM_AREA_NEX_RAM_RW:
 	case MEM_AREA_TEE_ASAN:
-		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRW | cached;
+		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRW | tagged;
 	case MEM_AREA_TEE_COHERENT:
 		return attr | TEE_MATTR_SECURE | TEE_MATTR_PRWX | noncache;
 	case MEM_AREA_TA_RAM:
@@ -1294,6 +1296,7 @@ bool core_mmu_mattr_is_ok(uint32_t mattr)
 	case TEE_MATTR_MEM_TYPE_DEV:
 	case TEE_MATTR_MEM_TYPE_STRONGLY_O:
 	case TEE_MATTR_MEM_TYPE_CACHED:
+	case TEE_MATTR_MEM_TYPE_TAGGED:
 		return true;
 	default:
 		return false;

commit c2a7dc6d4e949c9e8b22e0a16f45272f4a2532ef

core: mm: return true for mattr_is_cached() and TEE_MATTR_MEM_TYPE_TAGGED
Memory areas tagged with TEE_MATTR_MEM_TYPE_TAGGED attributes are
cached. Modify mattr_is_cached() accordingly.

Fixes: 7c3ab7744d ("core: mm: add TEE_MATTR_MEM_TYPE_TAGGED")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/mm/tee_mmu_types.h
@@ -145,6 +145,7 @@ static inline bool mattr_is_cached(uint32_t mattr)
 	uint32_t mem_type = (mattr >> TEE_MATTR_MEM_TYPE_SHIFT) &
 			    TEE_MATTR_MEM_TYPE_MASK;
 
-	return mem_type == TEE_MATTR_MEM_TYPE_CACHED;
+	return mem_type == TEE_MATTR_MEM_TYPE_CACHED ||
+	       mem_type == TEE_MATTR_MEM_TYPE_TAGGED;
 }
 #endif