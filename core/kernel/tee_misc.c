f6b4561a4981563e81cc82eb962fab078f68a3f1
Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
2022-07-29 13:42:01 +0300

    core: sort includes in tee_misc.c
    Sort includes to keep it clean.
    
    Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/kernel/tee_misc.c
+++ core/kernel/tee_misc.c
@@ -2,12 +2,12 @@
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
  */
-#include <stdio.h>
-#include <kernel/tee_common.h>
 #include <kernel/chip_services.h>
+#include <kernel/tee_common_otp.h>
+#include <kernel/tee_common.h>
 #include <kernel/tee_misc.h>
 #include <mm/core_memprot.h>
-#include <kernel/tee_common_otp.h>
+#include <stdio.h>
 #include <trace.h>
 
 static uint8_t tee_b2hs_add_base(uint8_t in)
