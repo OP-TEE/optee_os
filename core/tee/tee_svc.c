OP-TEE/optee_os

commit a0e8ffe9ba8f9312d23b447eb1f161df7328387b

core: add support for MTE
Adds support for the Armv8.5-A Memory Tagging Extension with
CFG_MEMTAG=y.

A memtag.h API is introduced to handle this extension. If CFG_MEMTAG=n
the API doesn't add any overhead and the behaviour is unchanged. With
CFG_MEMTAG=y a check is performed to see if the platform can support MTE
and the API is dynamically configured accordingly. This means that it's
safe to have CFG_MEMTAG=y even for platforms not supporting MTE. There
will be some minimal overhead then, but likely not noticeable.

An entry is also added in the TEE_PROPSET_TEE_IMPLEMENTATION for a u32
property "org.trustedfirmware.optee.cpu.feat_memtag_implemented". The
property is set to a non-zero value only if CFG_CORE_MEMTAG is
configured and the underlying CPU supports FEAT_MTE.

This commit still only uses the default tag with the value 0 resulting
in unchanged pointers when accessing memory. However, all plumbing is in
place allowing for instance tagging of the heap in a later commit.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
- * Copyright (c) 2020, Linaro Limited
+ * Copyright (c) 2020-2022 Linaro Limited
  */
 
 #include <compiler.h>
@@ -13,6 +13,7 @@
 #include <kernel/tee_time.h>
 #include <kernel/trace_ta.h>
 #include <kernel/user_access.h>
+#include <memtag.h>
 #include <mm/core_memprot.h>
 #include <mm/mobj.h>
 #include <mm/tee_mm.h>
@@ -225,6 +226,25 @@ get_prop_feat_pauth_implemented(struct ts_session *sess __unused, void *buf,
 }
 #endif
 
+#if MEMTAG_IS_ENABLED
+static TEE_Result
+get_prop_feat_memtag_implemented(struct ts_session *sess __unused, void *buf,
+				 size_t *blen)
+{
+	uint32_t v = 0;
+
+	if (*blen < sizeof(v)) {
+		*blen = sizeof(v);
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+	*blen = sizeof(v);
+	if (memtag_is_enabled())
+		v = feat_mte_implemented();
+
+	return copy_to_user(buf, &v, sizeof(v));
+}
+#endif
+
 /* Properties of the set TEE_PROPSET_CURRENT_CLIENT */
 const struct tee_props tee_propset_client[] = {
 	{
@@ -345,6 +365,13 @@ const struct tee_props tee_propset_tee[] = {
 		.name = "org.trustedfirmware.optee.cpu.feat_pauth_implemented",
 		.prop_type = USER_TA_PROP_TYPE_BOOL,
 		.get_prop_func = get_prop_feat_pauth_implemented
+	}
+#endif
+#if MEMTAG_IS_ENABLED
+	{
+		.name = "org.trustedfirmware.optee.cpu.feat_memtag_implemented",
+		.prop_type = USER_TA_PROP_TYPE_U32,
+		.get_prop_func = get_prop_feat_memtag_implemented
 	}
 #endif
 

core/arch/arm/arm.mk
@@ -167,6 +167,11 @@ arm64-platform-cflags-no-hard-float ?= -mgeneral-regs-only
 arm64-platform-cflags-hard-float ?=
 arm64-platform-cflags-generic := -mstrict-align $(call cc-option,-mno-outline-atomics,)
 
+ifeq ($(CFG_MEMTAG),y)
+arm64-platform-cflags += -march=armv8.5-a+memtag
+arm64-platform-aflags += -march=armv8.5-a+memtag
+endif
+
 platform-cflags-optimization ?= -O$(CFG_CC_OPT_LEVEL)
 
 ifeq ($(CFG_DEBUG_INFO),y)

core/arch/arm/kernel/boot.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2015-2021, Linaro Limited
+ * Copyright (c) 2015-2022, Linaro Limited
  */
 
 #include <arm.h>
@@ -23,6 +23,7 @@
 #include <kernel/tpm.h>
 #include <libfdt.h>
 #include <malloc.h>
+#include <memtag.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
 #include <mm/fobj.h>
@@ -282,6 +283,15 @@ static void init_asan(void)
 {
 }
 #endif /*CFG_CORE_SANITIZE_KADDRESS*/
+
+#if defined(CFG_MEMTAG)
+/* Called from entry_a64.S only when MEMTAG is configured */
+void boot_init_memtag(void)
+{
+	memtag_init_ops(feat_mte_implemented());
+	memtag_set_tags((void *)TEE_RAM_START, TEE_RAM_PH_SIZE, 0);
+}
+#endif
 
 #ifdef CFG_WITH_PAGER
 
@@ -1303,6 +1313,9 @@ void __weak boot_init_primary_late(unsigned long fdt)
 	DMSG("Executing at offset %#lx with virtual load address %#"PRIxVA,
 	     (unsigned long)boot_mmu_config.load_offset, VCORE_START_VA);
 #endif
+	if (IS_ENABLED(CFG_MEMTAG))
+		DMSG("Memory tagging %s",
+		     memtag_is_enabled() ?  "enabled" : "disabled");
 
 	main_init_gic();
 	init_vfp_nsec();

core/arch/arm/kernel/entry_a64.S
@@ -45,6 +45,11 @@
 		msr	spsel, #0
 	.endm
 
+	.macro read_feat_mte reg
+		mrs	\reg, id_aa64pfr1_el1
+		ubfx	\reg, \reg, #ID_AA64PFR1_EL1_MTE_SHIFT, #4
+	.endm
+
 	.macro set_sctlr_el1
 		mrs	x0, sctlr_el1
 		orr	x0, x0, #SCTLR_I
@@ -58,7 +63,65 @@
 #else
 		bic	x0, x0, #SCTLR_A
 #endif
+#ifdef CFG_MEMTAG
+		read_feat_mte x1
+		cmp	w1, #1
+		b.ls	111f
+		orr	x0, x0, #(SCTLR_ATA | SCTLR_ATA0)
+		bic	x0, x0, #SCTLR_TCF_MASK
+		bic	x0, x0, #SCTLR_TCF0_MASK
+111:
+#endif
 		msr	sctlr_el1, x0
+	.endm
+
+	.macro init_memtag_per_cpu
+		read_feat_mte x0
+		cmp	w0, #1
+		b.ls	11f
+
+#ifdef CFG_TEE_CORE_DEBUG
+		/*
+		 * This together with GCR_EL1.RRND = 0 will make the tags
+		 * acquired with the irg instruction deterministic.
+		 */
+		mov_imm	x0, 0xcafe00
+		msr	rgsr_el1, x0
+		/* Avoid tag = 0x0 and 0xf */
+		mov	x0, #0
+#else
+		/*
+		 * Still avoid tag = 0x0 and 0xf as we use that tag for
+		 * everything which isn't explicitly tagged. Setting
+		 * GCR_EL1.RRND = 1 to allow an implementation specific
+		 * method of generating the tags.
+		 */
+		mov	x0, #GCR_EL1_RRND
+#endif
+		orr	x0, x0, #1
+		orr	x0, x0, #(1 << 15)
+		msr	gcr_el1, x0
+
+		/*
+		 * Enable the tag checks on the current CPU.
+		 *
+		 * Depends on boot_init_memtag() having cleared tags for
+		 * TEE core memory. Well, not really, addresses with the
+		 * tag value 0b0000 will use unchecked access due to
+		 * TCR_TCMA0.
+		 */
+		mrs	x0, tcr_el1
+		orr	x0, x0, #TCR_TBI0
+		orr	x0, x0, #TCR_TCMA0
+		msr	tcr_el1, x0
+
+		mrs	x0, sctlr_el1
+		orr	x0, x0, #SCTLR_TCF_SYNC
+		orr	x0, x0, #SCTLR_TCF0_SYNC
+		msr	sctlr_el1, x0
+
+		isb
+11:
 	.endm
 
 FUNC _start , :
@@ -184,6 +247,15 @@ clear_nex_bss:
 	/* Enable Console */
 	bl	console_init
 
+#ifdef CFG_MEMTAG
+	/*
+	 * If FEAT_MTE2 is available, initializes the memtag callbacks.
+	 * Tags for OP-TEE core memory are then cleared to make it safe to
+	 * enable MEMTAG below.
+	 */
+	bl	boot_init_memtag
+#endif
+
 #ifdef CFG_CORE_ASLR
 	mov	x0, x20
 	bl	get_aslr_seed
@@ -234,6 +306,11 @@ clear_nex_bss:
 	mov	x0, x19		/* pagable part address */
 	mov	x1, #-1
 	bl	boot_init_primary_early
+
+#ifdef CFG_MEMTAG
+	init_memtag_per_cpu
+#endif
+
 #ifndef CFG_VIRTUALIZATION
 	mov	x21, sp
 	adr_l	x0, threads
@@ -455,6 +532,10 @@ FUNC cpu_on_handler , :
 
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
 	set_sp
+
+#ifdef CFG_MEMTAG
+	init_memtag_per_cpu
+#endif
 
 	mov	x0, x19
 	mov	x1, x20

core/arch/arm/mm/core_mmu_lpae.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: (BSD-2-Clause AND BSD-3-Clause)
 /*
- * Copyright (c) 2015-2016, Linaro Limited
+ * Copyright (c) 2015-2016, 2022 Linaro Limited
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -69,7 +69,7 @@
 #include <kernel/panic.h>
 #include <kernel/thread.h>
 #include <kernel/tlb_helpers.h>
-#include <mm/core_memprot.h>
+#include <memtag.h>
 #include <mm/core_memprot.h>
 #include <mm/pgt_cache.h>
 #include <string.h>
@@ -915,11 +915,15 @@ void core_init_mmu_regs(struct core_mmu_config *cfg)
 	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR, ATTR_IWBWA_OWBWA_NTR_INDEX);
 	mair |= MAIR_ATTR_SET(ATTR_DEVICE_nGnRnE, ATTR_DEVICE_nGnRnE_INDEX);
 	/*
-	 * MEMTAG isn't enabled yet, map tagged memory as normal memory
+	 * If MEMTAG isn't enabled, map tagged memory as normal memory
 	 * instead.
 	 */
-	mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR,
-			      ATTR_TAGGED_NORMAL_MEM_INDEX);
+	if (memtag_is_enabled())
+		mair |= MAIR_ATTR_SET(ATTR_TAGGED_NORMAL_MEM,
+				      ATTR_TAGGED_NORMAL_MEM_INDEX);
+	else
+		mair |= MAIR_ATTR_SET(ATTR_IWBWA_OWBWA_NTR,
+				      ATTR_TAGGED_NORMAL_MEM_INDEX);
 	cfg->mair_el1 = mair;
 
 	tcr = TCR_RES1;

core/include/kernel/boot.h
@@ -47,6 +47,7 @@ extern const struct core_mmu_config boot_mmu_config;
 void boot_init_primary_early(unsigned long pageable_part,
 			     unsigned long nsec_entry);
 void boot_init_primary_late(unsigned long fdt);
+void boot_init_memtag(void);
 
 void __panic_at_smc_return(void) __noreturn;
 

core/kernel/user_access.c
@@ -8,6 +8,7 @@
 #include <kernel/linker.h>
 #include <kernel/user_access.h>
 #include <kernel/user_mode_ctx.h>
+#include <memtag.h>
 #include <mm/vm.h>
 #include <string.h>
 #include <tee_api_types.h>
@@ -73,11 +74,29 @@ TEE_Result copy_kaddr_to_uref(uint32_t *uref, void *kaddr)
 
 uint32_t kaddr_to_uref(void *kaddr)
 {
+	if (MEMTAG_IS_ENABLED) {
+		unsigned int uref_tag_shift = 32 - MEMTAG_TAG_WIDTH;
+		vaddr_t uref = memtag_strip_tag_vaddr(kaddr);
+
+		uref -= VCORE_START_VA;
+		assert(uref < (UINT32_MAX >> MEMTAG_TAG_WIDTH));
+		uref |= memtag_get_tag(kaddr) << uref_tag_shift;
+		return uref;
+	}
+
 	assert(((vaddr_t)kaddr - VCORE_START_VA) < UINT32_MAX);
 	return (vaddr_t)kaddr - VCORE_START_VA;
 }
 
 vaddr_t uref_to_vaddr(uint32_t uref)
 {
+	if (MEMTAG_IS_ENABLED) {
+		vaddr_t u = uref & (UINT32_MAX >> MEMTAG_TAG_WIDTH);
+		unsigned int uref_tag_shift = 32 - MEMTAG_TAG_WIDTH;
+		uint8_t tag = uref >> uref_tag_shift;
+
+		return memtag_insert_tag_vaddr(VCORE_START_VA + u, tag);
+	}
+
 	return VCORE_START_VA + uref;
 }

core/mm/fobj.c
@@ -9,6 +9,7 @@
 #include <initcall.h>
 #include <kernel/boot.h>
 #include <kernel/panic.h>
+#include <memtag.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
 #include <mm/fobj.h>
@@ -766,7 +767,7 @@ struct fobj *fobj_sec_mem_alloc(unsigned int num_pages)
 	if (!va)
 		goto err;
 
-	memset(va, 0, size);
+	memtag_clear_mem(va, size);
 	f->fobj.ops = &ops_sec_mem;
 	f->fobj.num_pages = num_pages;
 	refcount_set(&f->fobj.refc, 1);

commit f08cea51b706d290253c1493108b320903410241

core: tee_svc.c: add missing comma
Add missing comma to fix the following error:

 $ make -s PLATFORM=vexpress-qemu_armv8a CFG_TA_PAUTH=y CFG_MEMTAG=y
 core/tee/tee_svc.c:371:9: error: expected ‘}’ before ‘{’ token
   371 |         {
       |         ^
 core/tee/tee_svc.c:280:44: note: to match this ‘{’
   280 | const struct tee_props tee_propset_tee[] = {
       |                                            ^

Fixes: a0e8ffe9ba8f ("core: add support for MTE")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc.c
@@ -365,7 +365,7 @@ const struct tee_props tee_propset_tee[] = {
 		.name = "org.trustedfirmware.optee.cpu.feat_pauth_implemented",
 		.prop_type = USER_TA_PROP_TYPE_BOOL,
 		.get_prop_func = get_prop_feat_pauth_implemented
-	}
+	},
 #endif
 #if MEMTAG_IS_ENABLED
 	{