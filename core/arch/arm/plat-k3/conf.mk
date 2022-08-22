OP-TEE/optee_os

commit dfd994436ac3b67ebfe78dd813fe35fbed9b211f

plat-k3: Add DDR setup in k3 platform
This patch introduces DDR setup for possible use of
CFG_CORE_DYN_SHM/dynamic shared memory on k3 platform.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Manorit Chawdhry <m-chawdhry@ti.com>
Signed-off-by: Ivan Mikhaylov <ivan.mikhaylov@siemens.com>

core/arch/arm/plat-k3/main.c
@@ -36,6 +36,7 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_DATA_BASE,
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_SCFG_BASE,
 			SEC_PROXY_SCFG_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_RT_BASE, SEC_PROXY_RT_SIZE);
+register_ddr(DRAM0_BASE, DRAM0_SIZE);
 
 void main_init_gic(void)
 {

core/arch/arm/plat-k3/platform_config.h
@@ -15,6 +15,9 @@
 #define CONSOLE_BAUDRATE        115200
 #define CONSOLE_UART_CLK_IN_HZ  48000000
 
+#define DRAM0_BASE      0x80000000
+#define DRAM0_SIZE      0x80000000
+
 #define SCU_BASE        0x01800000
 #if defined(PLATFORM_FLAVOR_j721e)
 #define GICC_OFFSET     0x100000

commit b5a42f2b72f8b629698a092569bf3375ae405613

plat-k3: Add high DDR memory region
K3 devices support more than 2GB of DRAM, the extra is placed at a highmem
address of 0x880000000. If memory from this area is passed to OP-TEE
one will get the following error:

E/TC:1 0 std_entry_with_parg:235 Bad arg address 0x881585000

Add the highmem area to fix this.

Fixes: dfd994436ac3 ("plat-k3: Add DDR setup in k3 platform")
Signed-off-by: Andrew Davis <afd@ti.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-k3/conf.mk
@@ -15,6 +15,8 @@ $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_GIC,y)
 $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_CORE_CLUSTER_SHIFT,1)
+$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
 
 ifneq (,$(filter ${PLATFORM_FLAVOR},am65x j721e am64x))
 CFG_WITH_SOFTWARE_PRNG ?= n

core/arch/arm/plat-k3/main.c
@@ -34,6 +34,7 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_SCFG_BASE,
 			SEC_PROXY_SCFG_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_RT_BASE, SEC_PROXY_RT_SIZE);
 register_ddr(DRAM0_BASE, DRAM0_SIZE);
+register_ddr(DRAM1_BASE, DRAM1_SIZE);
 
 void main_init_gic(void)
 {

core/arch/arm/plat-k3/platform_config.h
@@ -18,6 +18,9 @@
 #define DRAM0_BASE      0x80000000
 #define DRAM0_SIZE      0x80000000
 
+#define DRAM1_BASE      0x880000000
+#define DRAM1_SIZE      0x80000000
+
 #define SCU_BASE        0x01800000
 #if defined(PLATFORM_FLAVOR_j721e)
 #define GICC_OFFSET     0x100000