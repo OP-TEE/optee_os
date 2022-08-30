7bf5e91c1b78bd07a8f8a0e4b52431a0d958d195
Sahil Malhotra <sahil.malhotra@nxp.com>
2022-08-30 16:54:23 +0530

    core: plat-ls: remove OP-TEE support for LS1021A-QDS platform
    LS1021A-QDS does not support OP-TEE anymore, removing its
    support.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-ls/conf.mk
+++ core/arch/arm/plat-ls/conf.mk
@@ -8,16 +8,6 @@ $(call force,CFG_LS,y)
 $(call force,CFG_DRAM0_BASE,0x80000000)
 $(call force,CFG_TEE_OS_DRAM0_SIZE,0x4000000)
 
-ifeq ($(PLATFORM_FLAVOR),ls1021aqds)
-include core/arch/arm/cpu/cortex-a7.mk
-$(call force,CFG_TEE_CORE_NB_CORE,2)
-$(call force,CFG_DRAM0_SIZE,0x80000000)
-$(call force,CFG_CORE_CLUSTER_SHIFT,2)
-CFG_SHMEM_SIZE ?= 0x00100000
-CFG_BOOT_SYNC_CPU ?= y
-CFG_BOOT_SECONDARY_REQUEST ?= y
-endif
-
 ifeq ($(PLATFORM_FLAVOR),ls1012ardb)
 include core/arch/arm/cpu/cortex-armv8-0.mk
 $(call force,CFG_TEE_CORE_NB_CORE,1)
