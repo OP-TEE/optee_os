1ee59ecb8d7135ef57dacab78fe73247d634e151
Sahil Malhotra <sahil.malhotra@nxp.com>
2022-08-30 16:54:23 +0530

    core: plat-ls: remove OP-TEE support for LS1021A-QDS platform
    LS1021A-QDS does not support OP-TEE anymore, removing its
    support.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-ls/platform_config.h
+++ core/arch/arm/plat-ls/platform_config.h
@@ -38,31 +38,6 @@
 #define CONSOLE_UART_BASE		UART0_BASE
 
 /* Platform specific defines */
-#if defined(PLATFORM_FLAVOR_ls1021aqds)
-/*  DUART 1 */
-#define UART0_BASE			0x021C0500
-#define DCFG_BASE			0x01EE0000
-#define DCFG_CCSR_BRR			0xE4
-#define DCFG_SCRATCHRW1			0x200
-
-#define CSU_BASE			0x01510000
-#define CSU_CSL_START			0x0
-#define CSU_CSL_END			0xE8
-#define CSU_CSL30			0x78
-#define CSU_CSL37			0x94
-
-/* Central Security Unit register values */
-#define	CSU_ACCESS_ALL			0x00FF00FF
-#define	CSU_ACCESS_SEC_ONLY		0x003F003F
-#define CSU_SETTING_LOCK		0x01000100
-
-#define GIC_BASE			0x01400000
-#define GICC_OFFSET			0x2000
-#define GICD_OFFSET			0x1000
-#define CAAM_BASE			0x01700000
-#define CAAM_SIZE			0x100000
-#endif
-
 #if defined(PLATFORM_FLAVOR_ls1012ardb)
 /*  DUART 1 */
 #define UART0_BASE			0x021C0500
