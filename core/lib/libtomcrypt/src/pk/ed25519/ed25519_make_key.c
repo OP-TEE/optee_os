d3040d8bc691add1a4c3d2027cadc58341db5639
Jerome Forissier <jerome.forissier@linaro.org>
2022-10-05 17:45:12 +0200

    libtomcrypt: Remove prng_state* NULL pointer check from ed25519_make_key()
    For the same reasons as in commit 2d7740f6f44c ("core: libtomcrypt:
    Remove prng_state* NULL pointer check from x25519_make_key()"), remove
    the NULL pointer check in ed25519_make_key().
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/lib/libtomcrypt/src/pk/ed25519/ed25519_make_key.c
+++ core/lib/libtomcrypt/src/pk/ed25519/ed25519_make_key.c
@@ -20,7 +20,6 @@ int ed25519_make_key(prng_state *prng, int wprng, curve25519_key *key)
 {
    int err;
 
-   LTC_ARGCHK(prng != NULL);
    LTC_ARGCHK(key  != NULL);
 
    if ((err = tweetnacl_crypto_sign_keypair(prng, wprng, key->pub, key->priv)) != CRYPT_OK) {
