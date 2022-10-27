OP-TEE/optee_os

commit b1df82f10633e8f452b30698cbef2062ddba71f6

core: use set_um_region() to update translation tables
Adds an internal function in core/mm/vm.c which is called when
translation tables needs to be updated.

With a cache for recently used translation tables
core_mmu_populate_user_map() will only update translation tables which
are new and not populated yet.

Each user space context has a linked list of struct vm_region describing
the logical memory map. To ensure that this logical memory map is kept
in sync with the translation tables in use set_um_region() must be used
to copy the content of a struct vm_region into translation tables as
needed.

If the current context is updated then the pgts currently in use are
updated. However, if the context isn't current then the cached tables
are updated instead. When cached tables are updated some of the needed
translation tables may actually be missing. This is ignored at this
stage and later taken care of by core_mmu_populate_user_map() since
those tables will be new and have the "populated" entry set to false.
Once core_mmu_populate_user_map() has initialized tables "populated" is
set to true for each table.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/vm.c
@@ -164,6 +164,73 @@ static void rem_um_region(struct user_mode_ctx *uctx, struct vm_region *r)
 	pgt_flush_ctx_range(pgt_cache, uctx->ts_ctx, r->va, r->va + r->size);
 }
 
+static void set_pa_range(struct core_mmu_table_info *ti, vaddr_t va,
+			 paddr_t pa, size_t size, uint32_t attr)
+{
+	unsigned int end = core_mmu_va2idx(ti, va + size);
+	unsigned int idx = core_mmu_va2idx(ti, va);
+
+	while (idx < end) {
+		core_mmu_set_entry(ti, idx, pa, attr);
+		idx++;
+		pa += BIT64(ti->shift);
+	}
+}
+
+static void set_reg_in_table(struct core_mmu_table_info *ti,
+			     struct vm_region *r)
+{
+	vaddr_t va = MAX(r->va, ti->va_base);
+	vaddr_t end = MIN(r->va + r->size, ti->va_base + CORE_MMU_PGDIR_SIZE);
+	size_t sz = MIN(end - va, mobj_get_phys_granule(r->mobj));
+	size_t granule = BIT(ti->shift);
+	size_t offset = 0;
+	paddr_t pa = 0;
+
+	while (va < end) {
+		offset = va - r->va + r->offset;
+		if (mobj_get_pa(r->mobj, offset, granule, &pa))
+			panic("Failed to get PA");
+		set_pa_range(ti, va, pa, sz, r->attr);
+		va += sz;
+	}
+}
+
+static void set_um_region(struct user_mode_ctx *uctx, struct vm_region *r)
+{
+	struct pgt *p = SLIST_FIRST(&uctx->pgt_cache);
+	struct core_mmu_table_info ti = { };
+
+	assert(!mobj_is_paged(r->mobj));
+
+	core_mmu_set_info_table(&ti, CORE_MMU_PGDIR_LEVEL, 0, NULL);
+
+	if (p) {
+		/* All the pgts are already allocated, update in place */
+		do {
+			ti.va_base = p->vabase;
+			ti.table = p->tbl;
+			set_reg_in_table(&ti, r);
+			p = SLIST_NEXT(p, link);
+		} while (p);
+	} else {
+		/*
+		 * We may have a few pgts in the cache list, update the
+		 * ones found.
+		 */
+		for (ti.va_base = ROUNDDOWN(r->va, CORE_MMU_PGDIR_SIZE);
+		     ti.va_base < r->va + r->size;
+		     ti.va_base += CORE_MMU_PGDIR_SIZE) {
+			p = pgt_pop_from_cache_list(ti.va_base, uctx->ts_ctx);
+			if (!p)
+				continue;
+			ti.table = p->tbl;
+			set_reg_in_table(&ti, r);
+			pgt_push_to_cache_list(p);
+		}
+	}
+}
+
 static TEE_Result umap_add_region(struct vm_info *vmi, struct vm_region *reg,
 				  size_t pad_begin, size_t pad_end,
 				  size_t align)
@@ -277,6 +344,8 @@ TEE_Result vm_map_pad(struct user_mode_ctx *uctx, vaddr_t *va, size_t len,
 		fobj_put(fobj);
 		if (res)
 			goto err_rem_reg;
+	} else {
+		set_um_region(uctx, reg);
 	}
 
 	/*
@@ -537,13 +606,17 @@ TEE_Result vm_remap(struct user_mode_ctx *uctx, vaddr_t *new_va, vaddr_t old_va,
 			res = umap_add_region(&uctx->vm_info, r, pad_begin,
 					      pad_end + len - r->size, 0);
 		}
-		if (!res)
+		if (!res) {
 			r_last = r;
-		if (!res)
 			res = alloc_pgt(uctx);
-		if (fobj && !res)
-			res = tee_pager_add_um_region(uctx, r->va, fobj,
-						      r->attr);
+		}
+		if (!res) {
+			if (!fobj)
+				set_um_region(uctx, r);
+			else
+				res = tee_pager_add_um_region(uctx, r->va, fobj,
+							      r->attr);
+		}
 
 		if (res) {
 			/*
@@ -596,8 +669,12 @@ err_restore_map:
 			panic("Cannot restore mapping");
 		if (alloc_pgt(uctx))
 			panic("Cannot restore mapping");
-		if (fobj && tee_pager_add_um_region(uctx, r->va, fobj, r->attr))
-			panic("Cannot restore mapping");
+		if (fobj) {
+			if (tee_pager_add_um_region(uctx, r->va, fobj, r->attr))
+				panic("Cannot restore mapping");
+		} else {
+			set_um_region(uctx, r);
+		}
 	}
 	fobj_put(fobj);
 	vm_set_ctx(uctx->ts_ctx);
@@ -684,16 +761,23 @@ TEE_Result vm_set_prot(struct user_mode_ctx *uctx, vaddr_t va, size_t len,
 		if (r->attr & (TEE_MATTR_UW | TEE_MATTR_PW))
 			was_writeable = true;
 
-		if (!mobj_is_paged(r->mobj))
-			need_sync = true;
-
 		r->attr &= ~TEE_MATTR_PROT_MASK;
 		r->attr |= prot;
-	}
 
-	if (need_sync) {
-		/* Synchronize changes to translation tables */
-		vm_set_ctx(uctx->ts_ctx);
+		if (!mobj_is_paged(r->mobj)) {
+			need_sync = true;
+			set_um_region(uctx, r);
+			/*
+			 * Normally when set_um_region() is called we
+			 * change from no mapping to some mapping, but in
+			 * this case we change the permissions on an
+			 * already present mapping so some TLB invalidation
+			 * is needed. We also depend on the dsb() performed
+			 * as part of the TLB invalidation.
+			 */
+			tlbi_mva_range_asid(r->va, r->size, SMALL_PAGE_SIZE,
+					    uctx->vm_info.asid);
+		}
 	}
 
 	for (r = r0; r; r = TAILQ_NEXT(r, link)) {

core/mm/core_mmu.c
@@ -1610,7 +1610,7 @@ static void set_pg_region(struct core_mmu_table_info *dir_info,
 		r.size = MIN(CORE_MMU_PGDIR_SIZE - (r.va - pg_info->va_base),
 			     end - r.va);
 
-		if (!mobj_is_paged(region->mobj)) {
+		if (!(*pgt)->populated  && !mobj_is_paged(region->mobj)) {
 			size_t granule = BIT(pg_info->shift);
 			size_t offset = r.va - region->va + region->offset;
 
@@ -1893,6 +1893,7 @@ void core_mmu_populate_user_map(struct core_mmu_table_info *dir_info,
 	struct core_mmu_table_info pg_info = { };
 	struct pgt_cache *pgt_cache = &uctx->pgt_cache;
 	struct pgt *pgt = NULL;
+	struct pgt *p = NULL;
 	struct vm_region *r = NULL;
 
 	if (TAILQ_EMPTY(&uctx->vm_info.regions))
@@ -1908,6 +1909,13 @@ void core_mmu_populate_user_map(struct core_mmu_table_info *dir_info,
 
 	TAILQ_FOREACH(r, &uctx->vm_info.regions, link)
 		set_pg_region(dir_info, r, &pgt, &pg_info);
+	/* Record that the translation tables now are populated. */
+	SLIST_FOREACH(p, pgt_cache, link) {
+		p->populated = true;
+		if (p == pgt)
+			break;
+	}
+	assert(p == pgt);
 }
 
 TEE_Result core_mmu_remove_mapping(enum teecore_memtypes type, void *addr,

commit aa96154a73873440f5364f6933e5ad4ebebca6d8

core: mm: fix error flushing unused pgt's
With commit b1df82f10633 ("core: use set_um_region() to update
translation tables") a "populated" value is used for each pgt
(translation table) to tell if it's up to date or if
core_mmu_populate_user_map() should initialize it.

When a pgt becomes unused it must be marked as unused.  Prior to this
patch an error in the logic prevented pgt's to be marked as unused
properly. This can prevent core_mmu_populate_user_map() from
initializing a pgt. This can cause some new mappings to not be
established properly.

So fix this by giving the correct arguments for pgt_flush_range() in
rem_um_region() and core_is_buffer_inside() in pgt_entry_matches().

Fixes: b1df82f10633 ("core: use set_um_region() to update translation tables")
Acked-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/pgt_cache.c
@@ -660,7 +660,7 @@ static bool pgt_entry_matches(struct pgt *p, void *ctx, vaddr_t begin,
 		return false;
 	if (last <= begin)
 		return false;
-	if (!core_is_buffer_inside(p->vabase, SMALL_PAGE_SIZE, begin,
+	if (!core_is_buffer_inside(p->vabase, CORE_MMU_PGDIR_SIZE, begin,
 				   last - begin))
 		return false;
 

core/mm/vm.c
@@ -146,6 +146,14 @@ static void rem_um_region(struct user_mode_ctx *uctx, struct vm_region *r)
 				    uctx->vm_info.asid);
 	}
 
+	/*
+	 * Figure out how much virtual memory on a CORE_MMU_PGDIR_SIZE
+	 * grunalarity can be freed. Only completely unused
+	 * CORE_MMU_PGDIR_SIZE ranges can be supplied to pgt_flush_range().
+	 *
+	 * Note that there's is no margin for error here, both flushing too
+	 * many or too few translation tables can be fatal.
+	 */
 	r2 = TAILQ_NEXT(r, link);
 	if (r2)
 		last = MIN(last, ROUNDDOWN(r2->va, CORE_MMU_PGDIR_SIZE));
@@ -155,10 +163,8 @@ static void rem_um_region(struct user_mode_ctx *uctx, struct vm_region *r)
 		begin = MAX(begin,
 			    ROUNDUP(r2->va + r2->size, CORE_MMU_PGDIR_SIZE));
 
-	/* If there's no unused page tables, there's nothing left to do */
-	if (begin >= last)
-		return;
-	pgt_flush_range(uctx, r->va, r->va + r->size);
+	if (begin < last)
+		pgt_flush_range(uctx, begin, last);
 }
 
 static void set_pa_range(struct core_mmu_table_info *ti, vaddr_t va,