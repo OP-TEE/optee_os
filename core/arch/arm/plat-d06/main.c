45426538a9b5695490b2a8cc72c8628024e9eed3
Xiaoxu Zeng <zengxiaoxu@huawei.com>
2022-10-31 11:07:44 +0800

    core: Add support for Hisilicon D06 (PLATFORM=d06)
    D06 is a server-class development board equipped with a Hisilicon
    Phosphor processor.
    
    Signed-off-by: Xiaoxu Zeng <zengxiaoxu@huawei.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ core/arch/arm/plat-d06/main.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2015, Linaro Limited
+ * Copyright (c) 2022, Huawei Technologies Co., Ltd
+ */
+#include <console.h>
+
+void console_init(void)
+{
+}
---
+++ core/arch/arm/plat-d06/platform_config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2015, Linaro Limited
+ * Copyright (c) 2022, Huawei Technologies Co., Ltd
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+#include <mm/generic_ram_layout.h>
+
+/* Make stacks aligned to data cache line length */
+#define STACK_ALIGNMENT		64
+
+#endif /* PLATFORM_CONFIG_H */
