5cc04a3ee27cdfdbe5dc9dfb40bbcfef2a472991
Etienne Carriere <etienne.carriere@linaro.org>
2022-11-23 09:02:18 +0100

    libutils: prefix system header guard with 2 underscore chars
    Adds prefix "__" to standard header files implemented in libutils. This
    is applicable as these header guards macro are system macros. This
    change prevents conflicts with external component as faced with
    SCP-firmware [1] that implements wrapper headers with #include_next for
    assert.h and stdlib.h using ASSERT_H [2] and STDLIB_H [3] as header
    guards as in libutils.
    
    Prior this change did stdint.h both define STDINT_H and _STDINT_H but
    guards only upon STDINT_H. This change removes STDINT_H.
    
    Link: [1] https://github.com/ARM-software/SCP-firmware.git
    Link: [2] https://github.com/ARM-software/SCP-firmware/blob/v2.11.0/framework/include/assert.h#L8-L9
    Link: [3] https://github.com/ARM-software/SCP-firmware/blob/v2.11.0/framework/include/stdlib.h#L8-L9
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- lib/libutils/isoc/include/malloc.h
+++ lib/libutils/isoc/include/malloc.h
@@ -2,8 +2,8 @@
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
  */
-#ifndef MALLOC_H
-#define MALLOC_H
+#ifndef __MALLOC_H
+#define __MALLOC_H
 
 #include <stddef.h>
 #include <types_ext.h>
@@ -173,4 +173,4 @@ void raw_malloc_add_pool(struct malloc_ctx *ctx, void *buf, size_t len);
 void raw_malloc_get_stats(struct malloc_ctx *ctx, struct malloc_stats *stats);
 #endif
 
-#endif /* MALLOC_H */
+#endif /* __MALLOC_H */
