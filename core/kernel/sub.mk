20573b71fbf3664a41777e5a570503d727ff2b9e
maroueneboubakri <maroueneboubakri@gmail.com>
2022-12-01 12:23:32 +0000

    core: kernel: move tee_time.c and tee_time_ree.c to core/kernel
    tee_time.c and tee_time_ree.c are architecture-independant code therefore
    move them from core/arch/arm/kernel to core/kernel.
    
    Signed-off-by: Marouene Boubakri <marouene.boubakri@nxp.com>

--- core/kernel/sub.mk
+++ core/kernel/sub.mk
@@ -17,6 +17,8 @@ endif
 srcs-y += panic.c
 srcs-y += trace_ext.c
 srcs-y += refcount.c
+srcs-y += tee_time.c
+srcs-$(CFG_SECURE_TIME_SOURCE_REE) += tee_time_ree.c
 srcs-y += tee_misc.c
 srcs-y += tee_ta_manager.c
 srcs-y += ts_manager.c
