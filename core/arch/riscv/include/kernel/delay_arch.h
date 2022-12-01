eabe3107e7b8cd10ae8db448c066574ce61a7e7b
maroueneboubakri <maroueneboubakri@gmail.com>
2022-12-01 14:19:17 +0000

    riscv: kernel: include: add delay_arch.h
    Implement timeout_init_us() and timeout_elapsed().
    
    Signed-off-by: Marouene Boubakri <marouene.boubakri@nxp.com>

---
+++ core/arch/riscv/include/kernel/delay_arch.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __KERNEL_DELAY_ARCH_H
+#define __KERNEL_DELAY_ARCH_H
+
+#include <platform_config.h>
+#include <riscv.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+static inline uint64_t timeout_init_us(uint32_t us)
+{
+	return read_time() + (((uint64_t)us * RISCV_MTIME_RATE) / 1000000ULL);
+}
+
+static inline bool timeout_elapsed(uint64_t expire)
+{
+	return read_time() > expire;
+}
+
+#endif /*__KERNEL_DELAY_ARCH_H*/
