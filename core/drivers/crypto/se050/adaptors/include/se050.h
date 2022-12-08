0c7364db3d410d1a194a5d7ed3b50cf4561def9f
Jorge Ramirez-Ortiz <jorge@foundries.io>
2022-12-08 11:07:38 +0100

    crypto: drivers: se050: adaptor: provide the oefid interface
    Not all the NXP SE05X secure elements provide the same level of
    cryptographic support. This interface allows runtime identification
    of the device under control
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/se050/adaptors/include/se050.h
+++ core/drivers/crypto/se050/adaptors/include/se050.h
@@ -16,11 +16,35 @@
 #include <se050_utils.h>
 #include <tee_api_types.h>
 #include <trace.h>
+#include <util.h>
+
+/* Supported Devices*/
+#define SE050A1_ID 0xA204
+#define SE050A2_ID 0xA205
+#define SE050B1_ID 0xA202
+#define SE050B2_ID 0xA203
+#define SE050C1_ID 0xA200
+#define SE050C2_ID 0xA201
+#define SE050DV_ID 0xA1F4
+#define SE051A2_ID 0xA565
+#define SE051C2_ID 0xA564
+#define SE050F2_ID 0xA77E
+#define SE050E_ID 0xA921
+#define SE051A_ID 0xA920
+#define SE051C_ID 0xA8FA
+#define SE051W_ID 0xA739
+#define SE050F_ID 0xA92A
 
 TEE_Result se050_core_early_init(struct se050_scp_key *keys);
 
 extern sss_se05x_key_store_t *se050_kstore;
 extern sss_se05x_session_t *se050_session;
 extern struct sss_se05x_ctx se050_ctx;
+
+static inline uint32_t se050_get_oefid(void)
+{
+	return SHIFT_U32(se050_ctx.se_info.oefid[0], 8) |
+		SHIFT_U32(se050_ctx.se_info.oefid[1], 0);
+}
 
 #endif /* SE050_H_ */
