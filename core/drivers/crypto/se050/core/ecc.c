a0c1bc47767d0f916fdf04bbac254c8b05d6a645
Jorge Ramirez-Ortiz <jorge@foundries.io>
2022-12-08 11:26:39 +0100

    crypto: drivers: se050: ecc: fallback to softw-ops
    Operations that require a public key might fallback to a software
    based implementation.
    
    Operations that require a private key might fallback to a software
    based implementation as long as the private key is not in the secure
    element.
    
    Use CFG_NXP_SE05X_ECC_DRV_FALLBACK to enable this feature.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/crypto/se050/core/ecc.c
+++ core/drivers/crypto/se050/core/ecc.c
@@ -5,6 +5,8 @@
  */
 
 #include <assert.h>
+#include <config.h>
+#include <crypto/crypto_impl.h>
 #include <der.h>
 #include <drvcrypt.h>
 #include <drvcrypt_acipher.h>
@@ -17,6 +19,19 @@
 #include <tee_api_defines_extensions.h>
 #include <utee_defines.h>
 #include <util.h>
+
+static const struct crypto_ecc_keypair_ops *pair_ops;
+static const struct crypto_ecc_public_ops *pub_ops;
+
+static bool oefid_key_supported(size_t bits __unused)
+{
+	return true;
+}
+
+static bool oefid_algo_supported(uint32_t algo __unused)
+{
+	return true;
+}
 
 static uint32_t algo_tee2se050(uint32_t algo)
 {
@@ -141,8 +156,14 @@ static TEE_Result ecc_get_key_size(uint32_t curve, uint32_t algo,
 			return TEE_ERROR_BAD_PARAMETERS;
 		break;
 	default:
-		return TEE_ERROR_NOT_SUPPORTED;
+		return TEE_ERROR_NOT_IMPLEMENTED;
 	}
+
+	if (!oefid_key_supported(*bits))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	if (!oefid_algo_supported(algo))
+		return TEE_ERROR_NOT_IMPLEMENTED;
 
 	return TEE_SUCCESS;
 }
@@ -345,6 +366,61 @@ static TEE_Result se050_inject_keypair(sss_se05x_object_t *k_object,
 	return TEE_SUCCESS;
 }
 
+static TEE_Result gen_fallback(struct ecc_keypair *key, size_t len)
+{
+	if (!IS_ENABLED(CFG_NXP_SE05X_ECC_DRV_FALLBACK))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	DMSG("se050: debug: ECC software fallback: KEYGEN");
+	return pair_ops->generate(key, len);
+}
+
+static TEE_Result shared_secret_fallback(struct ecc_keypair *private_key,
+					 struct ecc_public_key *public_key,
+					 void *secret, size_t *secret_len)
+{
+	const struct crypto_ecc_keypair_ops *ops = NULL;
+
+	if (!IS_ENABLED(CFG_NXP_SE05X_ECC_DRV_FALLBACK))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	if (se050_ecc_keypair_from_nvm(private_key))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	ops = crypto_asym_get_ecc_keypair_ops(TEE_TYPE_ECDH_KEYPAIR);
+	if (!ops)
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	DMSG("se050: debug: ECC software fallback: ECDH");
+	return ops->shared_secret(private_key, public_key,
+				  secret, (unsigned long *)secret_len);
+}
+
+static TEE_Result verify_fallback(uint32_t algo, struct ecc_public_key *key,
+				  const uint8_t *msg, size_t msg_len,
+				  const uint8_t *sig, size_t sig_len)
+{
+	if (!IS_ENABLED(CFG_NXP_SE05X_ECC_DRV_FALLBACK))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	DMSG("se050: debug: ECC software fallback: VERIFY");
+	return pub_ops->verify(algo, key, msg, msg_len, sig, sig_len);
+}
+
+static TEE_Result sign_fallback(uint32_t algo, struct ecc_keypair *key,
+				const uint8_t *msg, size_t msg_len,
+				uint8_t *sig, size_t *sig_len)
+{
+	if (!IS_ENABLED(CFG_NXP_SE05X_ECC_DRV_FALLBACK))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	if (se050_ecc_keypair_from_nvm(key))
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	DMSG("se050: debug: ECC software fallback: SIGN");
+	return pair_ops->sign(algo, key, msg, msg_len, sig, sig_len);
+}
+
 static TEE_Result shared_secret(struct ecc_keypair *private_key,
 				struct ecc_public_key *public_key,
 				void *secret, size_t *secret_len)
@@ -361,8 +437,13 @@ static TEE_Result shared_secret(struct ecc_keypair *private_key,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	ret = ecc_get_key_size(private_key->curve, 0, &key_bytes, &key_bits);
-	if (ret != TEE_SUCCESS)
-		return TEE_ERROR_BAD_PARAMETERS;
+	if (ret) {
+		if (ret != TEE_ERROR_NOT_IMPLEMENTED)
+			return ret;
+
+		return shared_secret_fallback(private_key, public_key,
+					      secret, secret_len);
+	}
 
 	ret = se050_inject_keypair(&kobject, private_key, key_bytes);
 	if (ret != TEE_SUCCESS)
@@ -420,8 +501,12 @@ static TEE_Result sign(uint32_t algo, struct ecc_keypair *key,
 	uint8_t *p = NULL;
 
 	res = ecc_get_key_size(key->curve, algo, &key_bytes, &key_bits);
-	if (res != TEE_SUCCESS)
-		goto exit;
+	if (res) {
+		if (res != TEE_ERROR_NOT_IMPLEMENTED)
+			goto exit;
+
+		return sign_fallback(algo, key, msg, msg_len, sig, sig_len);
+	}
 
 	/* allocate temporary buffer to retrieve the signature in DER format */
 	sig_der_len = 2 * key_bytes + DER_SIGNATURE_SZ;
@@ -493,8 +578,12 @@ static TEE_Result verify(uint32_t algo, struct ecc_public_key *key,
 	uint8_t *p = NULL;
 
 	res = ecc_get_key_size(key->curve, algo, &key_bytes, &key_bits);
-	if (res != TEE_SUCCESS)
-		goto exit;
+	if (res) {
+		if (res != TEE_ERROR_NOT_IMPLEMENTED)
+			goto exit;
+
+		return verify_fallback(algo, key, msg, msg_len, sig, sig_len);
+	}
 
 	/* truncate or pad the message as needed */
 	res = ecc_prepare_msg(algo_tee2se050(algo), msg, &msg_len, &p);
@@ -533,7 +622,7 @@ exit:
 	return res;
 }
 
-static TEE_Result gen_keypair(struct ecc_keypair *key, size_t key_size __unused)
+static TEE_Result gen_keypair(struct ecc_keypair *key, size_t key_size)
 {
 	sss_status_t st = kStatus_SSS_Fail;
 	sss_se05x_object_t k_object = { };
@@ -545,8 +634,12 @@ static TEE_Result gen_keypair(struct ecc_keypair *key, size_t key_size __unused)
 	size_t bits = 0;
 
 	ret = ecc_get_key_size(key->curve, 0, &bytes, &bits);
-	if (ret)
-		return ret;
+	if (ret) {
+		if (ret != TEE_ERROR_NOT_IMPLEMENTED)
+			return ret;
+
+		return gen_fallback(key, key_size);
+	}
 
 	st = sss_se05x_key_object_init(&k_object, se050_kstore);
 	if (st != kStatus_SSS_Success)
@@ -680,6 +773,14 @@ static struct drvcrypt_ecc driver_ecc = {
 
 static TEE_Result ecc_init(void)
 {
+	pub_ops = crypto_asym_get_ecc_public_ops(TEE_TYPE_ECDSA_PUBLIC_KEY);
+	if (!pub_ops)
+		return TEE_ERROR_GENERIC;
+
+	pair_ops = crypto_asym_get_ecc_keypair_ops(TEE_TYPE_ECDSA_KEYPAIR);
+	if (!pair_ops)
+		return TEE_ERROR_GENERIC;
+
 	return drvcrypt_register_ecc(&driver_ecc);
 }
 
