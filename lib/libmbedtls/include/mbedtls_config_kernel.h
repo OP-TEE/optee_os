1d11ab221a28c847f4450e50919981957633530e
Jens Wiklander <jens.wiklander@linaro.org>
2022-12-27 10:16:22 +0100

    core: mbedtls: use SHA-512 crypto accelerated routines
    Uses the recently provided accelerated SHA-512 routine.
    
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libmbedtls/include/mbedtls_config_kernel.h
+++ lib/libmbedtls/include/mbedtls_config_kernel.h
@@ -39,6 +39,9 @@
 #if defined(CFG_CRYPTO_SHA384) || defined(CFG_CRYPTO_SHA512)
 #define MBEDTLS_SHA512_C
 #define MBEDTLS_MD_C
+#if defined(CFG_CORE_CRYPTO_SHA512_ACCEL)
+#define MBEDTLS_SHA512_PROCESS_ALT
+#endif
 #endif
 
 #if defined(CFG_CRYPTO_HMAC)
