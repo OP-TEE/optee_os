OP-TEE/optee_os

commit 2fc5dc95a949d166a489704b0bac3823090ce282

core: mbedtls: use SHA-256 crypto accelerated routines
Uses the recently provided accelerated SHA-256 routine.

Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libmbedtls/include/mbedtls_config_kernel.h
@@ -31,6 +31,9 @@
 #if defined(CFG_CRYPTO_SHA224) || defined(CFG_CRYPTO_SHA256)
 #define MBEDTLS_SHA256_C
 #define MBEDTLS_MD_C
+#if defined(CFG_CRYPTO_SHA256_ACCEL)
+#define MBEDTLS_SHA256_PROCESS_ALT
+#endif
 #endif
 
 #if defined(CFG_CRYPTO_SHA384) || defined(CFG_CRYPTO_SHA512)

lib/libmbedtls/core/hash.c
@@ -210,3 +210,18 @@ int mbedtls_internal_sha1_process(mbedtls_sha1_context *ctx,
 	return 0;
 }
 #endif /*MBEDTLS_SHA1_PROCESS_ALT*/
+
+#if defined(MBEDTLS_SHA256_PROCESS_ALT)
+int mbedtls_internal_sha256_process(mbedtls_sha256_context *ctx,
+				    const unsigned char data[64])
+{
+	MBEDTLS_INTERNAL_VALIDATE_RET(ctx != NULL,
+				      MBEDTLS_ERR_SHA256_BAD_INPUT_DATA);
+	MBEDTLS_INTERNAL_VALIDATE_RET((const unsigned char *)data != NULL,
+				      MBEDTLS_ERR_SHA256_BAD_INPUT_DATA);
+
+	crypto_accel_sha256_compress(ctx->state, data, 1);
+
+	return 0;
+}
+#endif /*MBEDTLS_SHA256_PROCESS_ALT*/

commit 7e87b9c4701d3ff37992b972d4e1d0b34b097abd

core: mbedtls: fix use of SHA-256 crypto accelerated routines
The wrong name of a configuration was used to test in mbedtls if the
accelerated SHA-256 routines should be used. Fix this by using the
correct name CFG_CORE_CRYPTO_SHA256_ACCEL instead.

Fixes: 2fc5dc95a949 ("core: mbedtls: use SHA-256 crypto accelerated routines")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libmbedtls/include/mbedtls_config_kernel.h
@@ -31,7 +31,7 @@
 #if defined(CFG_CRYPTO_SHA224) || defined(CFG_CRYPTO_SHA256)
 #define MBEDTLS_SHA256_C
 #define MBEDTLS_MD_C
-#if defined(CFG_CRYPTO_SHA256_ACCEL)
+#if defined(CFG_CORE_CRYPTO_SHA256_ACCEL)
 #define MBEDTLS_SHA256_PROCESS_ALT
 #endif
 #endif