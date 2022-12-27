36716037d7ef362c7296938a6bb923ff158b3c10
Jens Wiklander <jens.wiklander@linaro.org>
2022-12-27 13:14:36 +0100

    core: arm64: SM3 using ARMv8.2-A cryptographic extensions
    Import SM3 assembly code from the Linux kernel (Linaro contribution).
    Enabled with CFG_CRYPTO_SM3_ARM64_CE=y, set by default if
    CFG_CRYPTO_WITH_CE82=y.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/crypto/sub.mk
+++ core/arch/arm/crypto/sub.mk
@@ -27,3 +27,8 @@ ifeq ($(CFG_CRYPTO_SHA512_ARM_CE),y)
 srcs-y += sha512_armv8a_ce.c
 srcs-$(CFG_ARM64_core) += sha512_armv8a_ce_a64.S
 endif
+
+ifeq ($(CFG_CRYPTO_SM3_ARM_CE),y)
+srcs-y += sm3_armv8a_ce.c
+srcs-$(CFG_ARM64_core) += sm3_armv8a_ce_a64.S
+endif
