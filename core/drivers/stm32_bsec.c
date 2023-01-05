OP-TEE/optee_os

commit a638030bce844d6c654a6945f1db55ae5e696328

drivers: stm32_bsec: remove unused functions
Removes unused functions stm32_bsec_otp_lock() and
stm32_bsec_shadow_register().

Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/stm32_bsec.c
@@ -241,7 +241,7 @@ static TEE_Result power_down_safmem(void)
 	return TEE_ERROR_GENERIC;
 }
 
-TEE_Result stm32_bsec_shadow_register(uint32_t otp_id)
+static TEE_Result stm32_bsec_shadow_register(uint32_t otp_id)
 {
 	TEE_Result result = 0;
 	uint32_t exceptions = 0;
@@ -569,30 +569,6 @@ TEE_Result stm32_bsec_read_sp_lock(uint32_t otp_id, bool *locked)
 TEE_Result stm32_bsec_read_permanent_lock(uint32_t otp_id, bool *locked)
 {
 	return read_bsec_lock(otp_id, locked, BSEC_WRLOCK_OFF);
-}
-
-TEE_Result stm32_bsec_otp_lock(uint32_t service)
-{
-	vaddr_t addr = bsec_base() + BSEC_OTP_LOCK_OFF;
-
-	if (state_is_invalid_mode())
-		return TEE_ERROR_SECURITY;
-
-	switch (service) {
-	case BSEC_LOCK_UPPER_OTP:
-		io_write32(addr, BIT(BSEC_LOCK_UPPER_OTP));
-		break;
-	case BSEC_LOCK_DEBUG:
-		io_write32(addr, BIT(BSEC_LOCK_DEBUG));
-		break;
-	case BSEC_LOCK_PROGRAM:
-		io_write32(addr, BIT(BSEC_LOCK_PROGRAM));
-		break;
-	default:
-		return TEE_ERROR_BAD_PARAMETERS;
-	}
-
-	return TEE_SUCCESS;
 }
 
 static size_t nsec_access_array_size(void)

core/include/drivers/stm32_bsec.h
@@ -39,13 +39,6 @@ enum stm32_bsec_sec_state {
 TEE_Result stm32_bsec_shadow_read_otp(uint32_t *value, uint32_t otp_id);
 
 /*
- * Copy SAFMEM OTP to BSEC data.
- * @otp_id: OTP number.
- * Return a TEE_Result compliant return value
- */
-TEE_Result stm32_bsec_shadow_register(uint32_t otp_id);
-
-/*
  * Read an OTP data value
  * @value: Output read value
  * @otp_id: OTP number
@@ -153,13 +146,6 @@ TEE_Result stm32_bsec_read_sp_lock(uint32_t otp_id, bool *locked);
  * Return a TEE_Result compliant return value
  */
 TEE_Result stm32_bsec_read_permanent_lock(uint32_t otp_id, bool *locked);
-
-/*
- * Lock Upper OTP or Global programming or debug enable
- * @service: Service to lock, see header file
- * Return a TEE_Result compliant return value
- */
-TEE_Result stm32_bsec_otp_lock(uint32_t service);
 
 /*
  * Return true if OTP can be read, false otherwise

commit 59771cb35b0a467fc7a406081fd8e74f3421a446

plat-stm32mp1: fix visibility of stm32_bsec_shadow_register()
Makes stm32_bsec_shadow_register() function a visible driver API
function as it is needed when CFG_STM32_BSEC_SIP is enabled.
Fixed commit made it a local function which was wrong.

Fixes: a638030bce84 ("drivers: stm32_bsec: remove unused functions")
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/stm32_bsec.c
@@ -241,7 +241,7 @@ static TEE_Result power_down_safmem(void)
 	return TEE_ERROR_GENERIC;
 }
 
-static TEE_Result stm32_bsec_shadow_register(uint32_t otp_id)
+TEE_Result stm32_bsec_shadow_register(uint32_t otp_id)
 {
 	TEE_Result result = 0;
 	uint32_t exceptions = 0;

core/include/drivers/stm32_bsec.h
@@ -39,6 +39,13 @@ enum stm32_bsec_sec_state {
 TEE_Result stm32_bsec_shadow_read_otp(uint32_t *value, uint32_t otp_id);
 
 /*
+ * Copy SAFMEM OTP to BSEC data.
+ * @otp_id: OTP number.
+ * Return a TEE_Result compliant return value
+ */
+TEE_Result stm32_bsec_shadow_register(uint32_t otp_id);
+
+/*
  * Read an OTP data value
  * @value: Output read value
  * @otp_id: OTP number