OP-TEE/optee_os

commit eab9487631ccefad235fcb162f405ae06bccbdf8

plat-stm32mp1: deprecate BSEC SIP services
As the interface is now managed using PTA BSEC, the
SMC SIP services can be set as deprecated.

It can be removed in few OP-TEE releases.

Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-stm32mp1/conf.mk
@@ -191,7 +191,7 @@ CFG_WDT ?= $(CFG_STM32_IWDG)
 CFG_STM32MP_PANIC_ON_TZC_PERM_VIOLATION ?= y
 
 # SiP/OEM service for non-secure world
-CFG_STM32_BSEC_SIP ?= y
+CFG_STM32_BSEC_SIP ?= n
 CFG_STM32MP1_SCMI_SIP ?= n
 ifeq ($(CFG_STM32MP1_SCMI_SIP),y)
 $(call force,CFG_SCMI_MSG_DRIVERS,y,Mandated by CFG_STM32MP1_SCMI_SIP)

core/arch/arm/plat-stm32mp1/nsec-service/stm32mp1_smc.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright (c) 2016-2020, STMicroelectronics
+ * Copyright (c) 2016-2022, STMicroelectronics
  * Copyright (c) 2018, Linaro Limited
  */
 #ifndef __STM32MP1_SMC_H__
@@ -72,7 +72,7 @@
 #define STM32_SIP_SVC_FUNC_VERSION		0xff03
 
 /*
- * SIP functions STM32_SIP_SVC_FUNC_BSEC
+ * SIP functions STM32_SIP_SVC_FUNC_BSEC - Deprecated
  *
  * Argument a0: (input) SMCCC function ID
  *		(output) status return code

commit b2268a28f14558910e625a2d42435bd8239a59fd

plat-stm32mp1: restore BSEC SIP services on STM32MP15
U-Boot and Linux kernel use BSEC OP-TEE services on STM32MP15 but do
not yet integrate BSEC PTA drivers for OTP accesses and still rely on
OP-TEE BSEC SMC SiP service. Therefore restore the service for STM32MP15
platform flavors. The service will be default disabled once U-Boot and
Linux kernel are ready.

Fixes: eab9487631cc ("plat-stm32mp1: deprecate BSEC SIP services")
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-stm32mp1/conf.mk
@@ -107,6 +107,7 @@ $(call force,CFG_STM32MP1_SHARED_RESOURCES,y)
 $(call force,CFG_STM32MP15_CLK,y)
 CFG_CORE_RESERVED_SHM ?= y
 CFG_EXTERNAL_DT ?= y
+CFG_STM32_BSEC_SIP ?= y
 CFG_TEE_CORE_NB_CORE ?= 2
 CFG_WITH_PAGER ?= y
 endif # CFG_STM32MP15