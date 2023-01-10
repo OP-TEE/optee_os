a90f9e4f38ecb096c24441aa981a1e4ec755ef71
Etienne Carriere <etienne.carriere@linaro.org>
2023-01-10 14:05:21 +0100

    plat-stm32mp1: remove code for when DT is not supported
    This change removes implementation managing cases when CFG_EMBED_DTB is
    disabled. This change aims to simplify source files and is related to
    commit [1] from which stm32mp1 platform requires DTB for the drivers
    configuration.
    
    Link: [1] 474ad1856b56 ("plat-stm32mp1: conf: mandate the use of device tree on STM32MP1x platforms")
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/arch/arm/plat-stm32mp1/shared_resources.c
+++ core/arch/arm/plat-stm32mp1/shared_resources.c
@@ -127,7 +127,6 @@ static __maybe_unused const char *shres2str_state(enum shres_state id)
 }
 
 /* GPIOZ bank pin count depends on SoC variants */
-#ifdef CFG_EMBED_DTB
 /* A light count routine for unpaged context to not depend on DTB support */
 static int gpioz_nbpin = -1;
 
@@ -155,12 +154,6 @@ static TEE_Result set_gpioz_nbpin_from_dt(void)
 }
 /* Get GPIOZ pin count before drivers initialization, hence service_init() */
 service_init(set_gpioz_nbpin_from_dt);
-#else
-static unsigned int get_gpioz_nbpin(void)
-{
-	return STM32MP1_GPIOZ_PIN_MAX_COUNT;
-}
-#endif
 
 static void register_periph(enum stm32mp_shres id, enum shres_state state)
 {
