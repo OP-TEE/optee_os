OP-TEE/optee_os

commit 7ff454421a8c996b183a100eb17db87e89070ad7

core: pta: scmi: support SCP-firmware SCMI resources
Updates SCMI PTA to use SCP-firmware resources when CFG_SCMI_SCPFW=y.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/pta/scmi.c
@@ -6,6 +6,7 @@
 #include <compiler.h>
 #include <config.h>
 #include <drivers/scmi-msg.h>
+#include <scmi/scmi_server.h>
 #include <kernel/pseudo_ta.h>
 #include <pta_scmi_client.h>
 #include <stdint.h>
@@ -65,6 +66,9 @@ static TEE_Result cmd_process_smt_channel(uint32_t ptypes,
 		return TEE_SUCCESS;
 	}
 
+	if (IS_ENABLED(CFG_SCMI_SCPFW))
+		return scmi_server_smt_process_thread(channel_id);
+
 	return TEE_ERROR_NOT_SUPPORTED;
 }
 
@@ -123,8 +127,11 @@ static TEE_Result cmd_process_msg_channel(uint32_t ptypes,
 	if (ptypes != exp_pt || !in_buf || !out_buf)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	if (IS_ENABLED(CFG_SCMI_MSG_SHM_MSG)) {
+	if (IS_ENABLED(CFG_SCMI_MSG_DRIVERS)) {
 		struct scmi_msg_channel *channel = NULL;
+
+		if (!IS_ENABLED(CFG_SCMI_MSG_SHM_MSG))
+			return TEE_ERROR_NOT_SUPPORTED;
 
 		channel = plat_scmi_get_channel(channel_id);
 		if (!channel)
@@ -134,6 +141,21 @@ static TEE_Result cmd_process_msg_channel(uint32_t ptypes,
 					      out_buf, &out_size);
 		if (!res)
 			params[2].memref.size = out_size;
+
+		return res;
+	}
+
+	if (IS_ENABLED(CFG_SCMI_SCPFW)) {
+		if (!in_buf || !out_buf)
+			return TEE_ERROR_BAD_PARAMETERS;
+
+		res = scmi_server_msg_process_thread(channel_id, in_buf,
+						     in_size, out_buf,
+						     &out_size);
+		if (!res) {
+			params[2].memref.size = (uint32_t)out_size;
+			IMSG("scmi optee shm: out %zu", out_size);
+		}
 
 		return res;
 	}
@@ -170,6 +192,11 @@ static TEE_Result cmd_get_channel_handle(uint32_t ptypes,
 		return TEE_SUCCESS;
 	}
 
+	if (IS_ENABLED(CFG_SCMI_SCPFW)) {
+		/* Only check the channel ID is known from SCP-firwmare */
+		return scmi_server_get_channel(channel_id, NULL);
+	}
+
 	return TEE_ERROR_NOT_SUPPORTED;
 }
 
@@ -186,7 +213,7 @@ static TEE_Result pta_scmi_open_session(uint32_t ptypes __unused,
 		return TEE_ERROR_ACCESS_DENIED;
 	}
 
-	if (IS_ENABLED(CFG_SCMI_MSG_DRVIERS))
+	if (IS_ENABLED(CFG_SCMI_MSG_DRVIERS) || IS_ENABLED(CFG_SCMI_SCPFW))
 		return TEE_SUCCESS;
 
 	return TEE_ERROR_NOT_SUPPORTED;

commit a9c45602f946d261f4873bb6ae08d890e3c42b23

core: pta: scmi: fix typo in open session test
Fixes a typo in config switch name CFG_SCMI_MSG_DRIVERS in SCMI PTA
open session function.

Fixes: 7ff454421a8c ("core: pta: scmi: support SCP-firmware SCMI resources")
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/pta/scmi.c
@@ -213,7 +213,7 @@ static TEE_Result pta_scmi_open_session(uint32_t ptypes __unused,
 		return TEE_ERROR_ACCESS_DENIED;
 	}
 
-	if (IS_ENABLED(CFG_SCMI_MSG_DRVIERS) || IS_ENABLED(CFG_SCMI_SCPFW))
+	if (IS_ENABLED(CFG_SCMI_MSG_DRIVERS) || IS_ENABLED(CFG_SCMI_SCPFW))
 		return TEE_SUCCESS;
 
 	return TEE_ERROR_NOT_SUPPORTED;