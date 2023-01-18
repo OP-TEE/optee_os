OP-TEE/optee_os

commit 4583de067b5da0cbcbdb341e7a4d9d0cdec2d033

pta: stm32mp: add BSEC PTA
Add BSEC PTA to offer an interface with One Time Programmed resources
(OTPs) of stm32mp1x platforms.

This interface allows non-secure world clients to get the state of
the BSEC, and read and write the OTPs. The REE has restricted
access on OTPs, the policy is defined in the embedded DT.

Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

lib/libutee/include/pta_stm32mp_bsec.h
@@ -1,0 +1,88 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2022, STMicroelectronics - All Rights Reserved
+ */
+
+#ifndef __PTA_STM32MP_BSEC_H
+#define __PTA_STM32MP_BSEC_H
+
+#define PTA_BSEC_UUID { 0x94cf71ad, 0x80e6, 0x40b5, \
+	{ 0xa7, 0xc6, 0x3d, 0xc5, 0x01, 0xeb, 0x28, 0x03 } }
+
+/**
+ * Read OTP memory
+ *
+ * [in]		value[0].a		OTP start offset in byte
+ * [in]		value[0].b		Access type, see PTA_BSEC_TYPE_*
+ * [out]	memref[1].buffer	Output buffer to store read values
+ * [out]	memref[1].size		Size of OTP to be read
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_BSEC_CMD_READ_OTP		0x0
+
+/**
+ * Write OTP memory
+ *
+ * [in]		value[0].a		OTP start offset in byte
+ * [in]		value[0].b		Access type (0 : shadow,
+ *					1 : fuse, 2 : lock)
+ * [in]		memref[1].buffer	Input buffer to read values
+ * [in]		memref[1].size		Size of OTP to be written
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ * TEE_ERROR_ACCESS_DENIED - OTP not accessible by caller
+ */
+#define PTA_BSEC_CMD_WRITE_OTP		0x1
+
+/**
+ * Get BSEC state
+ * Return the chip security level by reading the BSEC state
+ *
+ * [out]	value[0].a		One of PTA_BSEC_STATE_*
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_BSEC_CMD_GET_STATE		0x3
+
+enum stm32_bsec_pta_sec_state {
+	PTA_BSEC_STATE_SEC_OPEN = 0,
+	PTA_BSEC_STATE_SEC_CLOSE = 1,
+	PTA_BSEC_STATE_INVALID = 3
+};
+
+/*
+ * Access types identifiers for PTA_BSEC_CMD_READ_OTP and
+ * PTA_BSEC_CMD_WRITE_OTP = value[in].b.
+ *
+ * PTA_BSEC_SHADOW_ACCESS	Access OTP shadow memory
+ * PTA_BSEC_FUSE_ACCESS	Access	OTP fuse memory
+ * PTA_BSEC_LOCKS_ACCESS	Access OTP locks. The locks value read/written
+ *				in memref[1] 32bit words are related to bit flag
+ *				masks PTA_BSEC_LOCK_*.
+ */
+#define PTA_BSEC_SHADOW_ACCESS		0
+#define PTA_BSEC_FUSE_ACCESS		1
+#define PTA_BSEC_LOCKS_ACCESS		2
+
+/*
+ * PTA_BSEC_LOCK_* - Bit mask of OTP locks in memref[1]
+ *
+ * PTA_BSEC_LOCK_PERM		Fuse programming permanent lock
+ * PTA_BSEC_LOCK_SHADOW_R	Shadow programming (from fuse) lock
+ * PTA_BSEC_LOCK_SHADOW_W	Shadow memory write lock
+ * PTA_BSEC_LOCK_SHADOW_P	Fuse programming sticky lock
+ * PTA_BSEC_LOCK_ERROR		Flag indicating an error in lock access
+ */
+#define PTA_BSEC_LOCK_PERM			BIT(30)
+#define PTA_BSEC_LOCK_SHADOW_R			BIT(29)
+#define PTA_BSEC_LOCK_SHADOW_W			BIT(28)
+#define PTA_BSEC_LOCK_SHADOW_P			BIT(27)
+#define PTA_BSEC_LOCK_ERROR			BIT(26)
+
+#endif /* __PTA_STM32MP_BSEC_H */

core/pta/stm32mp/bsec_pta.c
@@ -1,0 +1,301 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2022, STMicroelectronics - All Rights Reserved
+ */
+
+#include <config.h>
+#include <drivers/stm32_bsec.h>
+#include <kernel/pseudo_ta.h>
+#include <kernel/user_access.h>
+#include <kernel/user_mode_ctx.h>
+#include <kernel/user_ta.h>
+#include <mm/vm.h>
+#include <pta_stm32mp_bsec.h>
+#include <string.h>
+#include <util.h>
+
+static_assert(IS_ENABLED(CFG_STM32_BSEC));
+
+#define PTA_NAME "bsec.pta"
+
+static TEE_Result bsec_check_access(uint32_t otp_id)
+{
+	if (!stm32_bsec_nsec_can_access_otp(otp_id))
+		return TEE_ERROR_ACCESS_DENIED;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result bsec_read_mem(uint32_t pt, TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_MEMREF_OUTPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	uint32_t *buf = (uint32_t *)params[1].memref.buffer;
+	uint32_t otp_start = 0;
+	size_t otp_length = 0;
+	uint32_t otp_id = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	size_t size = params[1].memref.size;
+	bool locked = false;
+	unsigned int otp_base_offset = params[0].value.a;
+	unsigned int bsec_command = params[0].value.b;
+
+	if (pt != exp_pt || !buf || !size ||
+	    !IS_ALIGNED_WITH_TYPE(params[1].memref.buffer, uint32_t))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	/* Check 32bits alignment */
+	if (otp_base_offset % BSEC_BYTES_PER_WORD ||
+	    size % BSEC_BYTES_PER_WORD)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	otp_start = otp_base_offset / BSEC_BYTES_PER_WORD;
+	otp_length = size / BSEC_BYTES_PER_WORD;
+
+	for (otp_id = otp_start; otp_id < otp_start + otp_length;
+	     otp_id++, buf++) {
+		res = bsec_check_access(otp_id);
+		switch (bsec_command) {
+		case PTA_BSEC_SHADOW_ACCESS:
+			if (res) {
+				/* Force 0 when access is not allowed */
+				*buf = 0x0;
+				continue;
+			}
+			/* Read shadow register */
+			res = stm32_bsec_read_otp(buf, otp_id);
+			FMSG("Read shadow %"PRIu32" val: %#"PRIx32, otp_id,
+			     *buf);
+			break;
+		case PTA_BSEC_FUSE_ACCESS:
+			/* Check access */
+			if (res)
+				goto out;
+			/* Read fuse value */
+			res = stm32_bsec_shadow_read_otp(buf, otp_id);
+			FMSG("Read fuse %"PRIu32" val: %#"PRIx32, otp_id, *buf);
+			break;
+		case PTA_BSEC_LOCKS_ACCESS:
+			if (res) {
+				/* Force error when access is not allowed */
+				*buf = PTA_BSEC_LOCK_ERROR;
+				continue;
+			}
+			*buf = 0;
+			/* Read lock value */
+			res = stm32_bsec_read_permanent_lock(otp_id, &locked);
+			if (res)
+				goto out;
+
+			if (locked)
+				*buf |= PTA_BSEC_LOCK_PERM;
+
+			res = stm32_bsec_read_sr_lock(otp_id, &locked);
+			if (res)
+				goto out;
+
+			if (locked)
+				*buf |= PTA_BSEC_LOCK_SHADOW_R;
+
+			res = stm32_bsec_read_sw_lock(otp_id, &locked);
+			if (res)
+				goto out;
+
+			if (locked)
+				*buf |= PTA_BSEC_LOCK_SHADOW_W;
+
+			res = stm32_bsec_read_sp_lock(otp_id, &locked);
+			if (res)
+				goto out;
+
+			if (locked)
+				*buf |= PTA_BSEC_LOCK_SHADOW_P;
+
+			FMSG("Read lock %"PRIu32" val: %#"PRIx32, otp_id, *buf);
+			break;
+		default:
+			FMSG("%"PRIu32" invalid operation: %"PRIu32, otp_id,
+			     bsec_command);
+			res = TEE_ERROR_BAD_PARAMETERS;
+		}
+
+		if (res)
+			goto out;
+	}
+
+	FMSG("Buffer orig %p, size %zu", buf, size);
+
+	res = TEE_SUCCESS;
+out:
+	return res;
+}
+
+static TEE_Result bsec_write_mem(uint32_t pt, TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+
+	uint32_t *buf = (uint32_t *)params[1].memref.buffer;
+	size_t size = params[1].memref.size;
+	uint32_t otp_start = 0;
+	size_t otp_length = 0;
+	uint32_t otp_id = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	unsigned int otp_base_offset = params[0].value.a;
+	unsigned int bsec_command = params[0].value.b;
+
+	if (pt != exp_pt || !buf || !size ||
+	    !IS_ALIGNED_WITH_TYPE(params[1].memref.buffer, uint32_t))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	/* Check 32bits alignment */
+	if (otp_base_offset % BSEC_BYTES_PER_WORD ||
+	    size % BSEC_BYTES_PER_WORD)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	otp_start = otp_base_offset / BSEC_BYTES_PER_WORD;
+	otp_length = size / BSEC_BYTES_PER_WORD;
+
+	/* Initial check to ensure that all BSEC words are available */
+	for (otp_id = otp_start; otp_id < otp_start + otp_length; otp_id++) {
+		res = bsec_check_access(otp_id);
+		if (res)
+			return res;
+	}
+
+	for (otp_id = otp_start; otp_id < otp_start + otp_length;
+	     otp_id++, buf++) {
+		switch (bsec_command) {
+		case PTA_BSEC_SHADOW_ACCESS:
+			/* Write shadow register */
+			FMSG("Write shadow %"PRIx32" : %"PRIx32,
+			     otp_id, *buf);
+			res = stm32_bsec_write_otp(*buf, otp_id);
+			break;
+
+		case PTA_BSEC_FUSE_ACCESS:
+			/* Write fuse value */
+			FMSG("Write fuse %"PRIx32" : %08"PRIx32,
+			     otp_id, *buf);
+			res = stm32_bsec_program_otp(*buf, otp_id);
+			break;
+
+		case PTA_BSEC_LOCKS_ACCESS:
+			if (*buf & PTA_BSEC_LOCK_PERM) {
+				FMSG("Perm lock access OTP: %u", otp_id);
+				res = stm32_bsec_permanent_lock_otp(otp_id);
+				if (res)
+					break;
+			}
+
+			if (*buf & PTA_BSEC_LOCK_SHADOW_R) {
+				FMSG("Shadow read lock");
+				res = stm32_bsec_set_sr_lock(otp_id);
+				if (res)
+					break;
+			}
+
+			if (*buf & PTA_BSEC_LOCK_SHADOW_W) {
+				FMSG("Shadow write lock detected");
+				res = stm32_bsec_set_sw_lock(otp_id);
+				if (res)
+					break;
+			}
+
+			if (*buf & PTA_BSEC_LOCK_SHADOW_P) {
+				FMSG("Shadow programming lock detected");
+				res = stm32_bsec_set_sp_lock(otp_id);
+			}
+
+			break;
+
+		default:
+			FMSG("OTP %"PRIx32" invalid operation: %"PRIx32,
+			     otp_id, bsec_command);
+			res = TEE_ERROR_BAD_PARAMETERS;
+		}
+
+		if (res)
+			return res;
+	}
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result bsec_pta_state(uint32_t pt, TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_OUTPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	TEE_Result res = TEE_ERROR_GENERIC;
+	enum stm32_bsec_sec_state state = BSEC_STATE_INVALID;
+	enum stm32_bsec_pta_sec_state pta_state = PTA_BSEC_STATE_INVALID;
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = stm32_bsec_get_state(&state);
+	if (res)
+		return res;
+
+	switch (state) {
+	case BSEC_STATE_SEC_CLOSED:
+		pta_state = PTA_BSEC_STATE_SEC_CLOSE;
+		break;
+	case BSEC_STATE_SEC_OPEN:
+		pta_state = PTA_BSEC_STATE_SEC_OPEN;
+		break;
+	default:
+		pta_state = PTA_BSEC_STATE_INVALID;
+		break;
+	}
+
+	params[0].value.a = pta_state;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result bsec_pta_invoke_command(void *pSessionContext __unused,
+					  uint32_t cmd_id,
+					  uint32_t param_types,
+					  TEE_Param params[TEE_NUM_PARAMS])
+{
+	FMSG(PTA_NAME" command %#"PRIx32" ptypes %#"PRIx32,
+	     cmd_id, param_types);
+
+	switch (cmd_id) {
+	case PTA_BSEC_CMD_READ_OTP:
+		return bsec_read_mem(param_types, params);
+	case PTA_BSEC_CMD_WRITE_OTP:
+		return bsec_write_mem(param_types, params);
+	case PTA_BSEC_CMD_GET_STATE:
+		return bsec_pta_state(param_types, params);
+	default:
+		break;
+	}
+
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+
+static TEE_Result pta_bsec_open_session(uint32_t ptypes __unused,
+					TEE_Param par[TEE_NUM_PARAMS] __unused,
+					void **session __unused)
+{
+	uint32_t login = to_ta_session(ts_get_current_session())->clnt_id.login;
+
+	if (login == TEE_LOGIN_REE_KERNEL)
+		return TEE_SUCCESS;
+
+	return TEE_ERROR_ACCESS_DENIED;
+}
+
+pseudo_ta_register(.uuid = PTA_BSEC_UUID, .name = PTA_NAME,
+		   .flags = PTA_DEFAULT_FLAGS | TA_FLAG_CONCURRENT |
+			    TA_FLAG_DEVICE_ENUM,
+		   .open_session_entry_point = pta_bsec_open_session,
+		   .invoke_command_entry_point = bsec_pta_invoke_command);

core/pta/stm32mp/sub.mk
@@ -1,0 +1,1 @@
+srcs-$(CFG_STM32_BSEC_PTA) += bsec_pta.c

core/pta/sub.mk
@@ -16,3 +16,4 @@ srcs-$(CFG_HWRNG_PTA) += hwrng.c
 srcs-$(CFG_RTC_PTA) += rtc.c
 
 subdirs-y += bcm
+subdirs-y += stm32mp

commit 809aa1e69250a313a439214049d14ebe1062c18b

pta: stm32mp: mention access denied error code in BSEC PTA API
Fixes BSEC PTA API header file that did not mention possible error code
TEE_ERROR_ACCESS_DENIED for command PTA_BSEC_CMD_READ_OTP.

Fixes: 4583de067b5d ("pta: stm32mp: add BSEC PTA")
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

lib/libutee/include/pta_stm32mp_bsec.h
@@ -20,6 +20,7 @@
  * Return codes:
  * TEE_SUCCESS - Invoke command success
  * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ * TEE_ERROR_ACCESS_DENIED - OTP not accessible by caller
  */
 #define PTA_BSEC_CMD_READ_OTP		0x0
 