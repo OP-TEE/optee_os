b5937c3ba536cc4bdd8502d95067300ec50a9caf
Jerome Forissier <jerome.forissier@linaro.org>
2023-01-30 11:33:05 +0100

    libutils: mte: add missing calls to strip_tag()
    Add missing calls to strip_tag() in raw_malloc_buffer_overlaps_heap()
    and raw_malloc_buffer_is_within_alloced(). Without them pointer
    arithmetic cannot work. Fixes xtest 1001.1 (make check MEMTAG=y).
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- lib/libutils/isoc/bget_malloc.c
+++ lib/libutils/isoc/bget_malloc.c
@@ -953,7 +953,7 @@ bool raw_malloc_buffer_overlaps_heap(struct malloc_ctx *ctx,
 	raw_malloc_validate_pools(ctx);
 
 	for (n = 0; n < ctx->pool_len; n++) {
-		uintptr_t pool_start = (uintptr_t)ctx->pool[n].buf;
+		uintptr_t pool_start = (uintptr_t)strip_tag(ctx->pool[n].buf);
 		uintptr_t pool_end = pool_start + ctx->pool[n].len;
 
 		if (buf_start > buf_end || pool_start > pool_end)
@@ -986,7 +986,7 @@ bool raw_malloc_buffer_is_within_alloced(struct malloc_ctx *ctx,
 		uint8_t *end_b = NULL;
 		size_t s = 0;
 
-		start_b = get_payload_start_size(b, &s);
+		start_b = strip_tag(get_payload_start_size(b, &s));
 		end_b = start_b + s;
 		if (start_buf >= start_b && end_buf <= end_b)
 			return true;
