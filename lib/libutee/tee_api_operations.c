OP-TEE/optee_os

commit fe2fd3ff46c0bada3dc9b2ae8c1025bfd8f57f3f

GP131: Add TEE_ALG_ECDH_DERIVE_SHARED_SECRET and TEE_ALG_ECDSA_SHA*
Adds the defines
- TEE_ALG_ECDH_DERIVE_SHARED_SECRET
- TEE_ALG_ECDSA_SHA1
- TEE_ALG_ECDSA_SHA224
- TEE_ALG_ECDSA_SHA256
- TEE_ALG_ECDSA_SHA386
- TEE_ALG_ECDSA_SHA512
from TEE Internal Core API version 1.3.1 to replace the previous now
deprecated defines:
- TEE_ALG_ECDSA_P192
- TEE_ALG_ECDSA_P224
- TEE_ALG_ECDSA_P256
- TEE_ALG_ECDSA_P384
- TEE_ALG_ECDSA_P521
- TEE_ALG_ECDH_P192
- TEE_ALG_ECDH_P224
- TEE_ALG_ECDH_P256
- TEE_ALG_ECDH_P384
- TEE_ALG_ECDH_P521

The new defines have new values and the values from the old defines are
not reused.

The syscall ABI maintains compatibility by handling both the old and new
values in parallel from now on.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/tee_api_operations.c
@@ -69,20 +69,23 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_P192:
-	case TEE_ALG_ECDH_P192:
+	case TEE_ALG_ECDSA_SHA1:
+	case __OPTEE_ALG_ECDSA_P192:
+	case __OPTEE_ALG_ECDH_P192:
 		if (maxKeySize != 192)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_P224:
-	case TEE_ALG_ECDH_P224:
+	case TEE_ALG_ECDSA_SHA224:
+	case __OPTEE_ALG_ECDSA_P224:
+	case __OPTEE_ALG_ECDH_P224:
 		if (maxKeySize != 224)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_P256:
-	case TEE_ALG_ECDH_P256:
+	case TEE_ALG_ECDSA_SHA256:
+	case __OPTEE_ALG_ECDSA_P256:
+	case __OPTEE_ALG_ECDH_P256:
 	case TEE_ALG_SM2_PKE:
 	case TEE_ALG_SM2_DSA_SM3:
 		if (maxKeySize != 256)
@@ -95,17 +98,25 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_P384:
-	case TEE_ALG_ECDH_P384:
+	case TEE_ALG_ECDSA_SHA384:
+	case __OPTEE_ALG_ECDSA_P384:
+	case __OPTEE_ALG_ECDH_P384:
 		if (maxKeySize != 384)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_P521:
-	case TEE_ALG_ECDH_P521:
+	case TEE_ALG_ECDSA_SHA512:
+	case __OPTEE_ALG_ECDSA_P521:
+	case __OPTEE_ALG_ECDH_P521:
 		if (maxKeySize != 521)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
+
+	case TEE_ALG_ECDH_DERIVE_SHARED_SECRET:
+		if (maxKeySize > 521)
+			return TEE_ERROR_NOT_SUPPORTED;
+		break;
+
 	case TEE_ALG_ED25519:
 	case TEE_ALG_X25519:
 		if (maxKeySize != 256)
@@ -166,11 +177,16 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 	case TEE_ALG_DSA_SHA1:
 	case TEE_ALG_DSA_SHA224:
 	case TEE_ALG_DSA_SHA256:
-	case TEE_ALG_ECDSA_P192:
-	case TEE_ALG_ECDSA_P224:
-	case TEE_ALG_ECDSA_P256:
-	case TEE_ALG_ECDSA_P384:
-	case TEE_ALG_ECDSA_P521:
+	case TEE_ALG_ECDSA_SHA1:
+	case TEE_ALG_ECDSA_SHA224:
+	case TEE_ALG_ECDSA_SHA256:
+	case TEE_ALG_ECDSA_SHA384:
+	case TEE_ALG_ECDSA_SHA512:
+	case __OPTEE_ALG_ECDSA_P192:
+	case __OPTEE_ALG_ECDSA_P224:
+	case __OPTEE_ALG_ECDSA_P256:
+	case __OPTEE_ALG_ECDSA_P384:
+	case __OPTEE_ALG_ECDSA_P521:
 	case TEE_ALG_SM2_DSA_SM3:
 	case TEE_ALG_ED25519:
 		if (mode == TEE_MODE_SIGN) {
@@ -212,11 +228,12 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 		break;
 
 	case TEE_ALG_DH_DERIVE_SHARED_SECRET:
-	case TEE_ALG_ECDH_P192:
-	case TEE_ALG_ECDH_P224:
-	case TEE_ALG_ECDH_P256:
-	case TEE_ALG_ECDH_P384:
-	case TEE_ALG_ECDH_P521:
+	case TEE_ALG_ECDH_DERIVE_SHARED_SECRET:
+	case __OPTEE_ALG_ECDH_P192:
+	case __OPTEE_ALG_ECDH_P224:
+	case __OPTEE_ALG_ECDH_P256:
+	case __OPTEE_ALG_ECDH_P384:
+	case __OPTEE_ALG_ECDH_P521:
 	case TEE_ALG_HKDF_MD5_DERIVE_KEY:
 	case TEE_ALG_HKDF_SHA1_DERIVE_KEY:
 	case TEE_ALG_HKDF_SHA224_DERIVE_KEY:
@@ -2051,19 +2068,34 @@ TEE_Result TEE_IsAlgorithmSupported(uint32_t alg, uint32_t element)
 			goto check_element_none;
 	}
 	if (IS_ENABLED(CFG_CRYPTO_ECC)) {
-		if ((alg == TEE_ALG_ECDH_P192 || alg == TEE_ALG_ECDSA_P192) &&
+		if ((alg == __OPTEE_ALG_ECDH_P192 ||
+		     alg == __OPTEE_ALG_ECDSA_P192 ||
+		     alg == TEE_ALG_ECDH_DERIVE_SHARED_SECRET ||
+		     alg == TEE_ALG_ECDSA_SHA1) &&
 		    element == TEE_ECC_CURVE_NIST_P192)
 			return TEE_SUCCESS;
-		if ((alg == TEE_ALG_ECDH_P224 || alg == TEE_ALG_ECDSA_P224) &&
+		if ((alg == __OPTEE_ALG_ECDH_P224 ||
+		     alg == __OPTEE_ALG_ECDSA_P224 ||
+		     alg == TEE_ALG_ECDH_DERIVE_SHARED_SECRET ||
+		     alg == TEE_ALG_ECDSA_SHA224) &&
 		    element == TEE_ECC_CURVE_NIST_P224)
 			return TEE_SUCCESS;
-		if ((alg == TEE_ALG_ECDH_P256 || alg == TEE_ALG_ECDSA_P256) &&
+		if ((alg == __OPTEE_ALG_ECDH_P256 ||
+		     alg == __OPTEE_ALG_ECDSA_P256 ||
+		     alg == TEE_ALG_ECDH_DERIVE_SHARED_SECRET ||
+		     alg == TEE_ALG_ECDSA_SHA256) &&
 		    element == TEE_ECC_CURVE_NIST_P256)
 			return TEE_SUCCESS;
-		if ((alg == TEE_ALG_ECDH_P384 || alg == TEE_ALG_ECDSA_P384) &&
+		if ((alg == __OPTEE_ALG_ECDH_P384 ||
+		     alg == __OPTEE_ALG_ECDSA_P384 ||
+		     alg == TEE_ALG_ECDH_DERIVE_SHARED_SECRET ||
+		     alg == TEE_ALG_ECDSA_SHA384) &&
 		    element == TEE_ECC_CURVE_NIST_P384)
 			return TEE_SUCCESS;
-		if ((alg == TEE_ALG_ECDH_P521 || alg == TEE_ALG_ECDSA_P521) &&
+		if ((alg == __OPTEE_ALG_ECDH_P521 ||
+		     alg == __OPTEE_ALG_ECDSA_P521 ||
+		     alg == TEE_ALG_ECDH_DERIVE_SHARED_SECRET ||
+		     alg == TEE_ALG_ECDSA_SHA512) &&
 		    element == TEE_ECC_CURVE_NIST_P521)
 			return TEE_SUCCESS;
 	}

core/lib/libtomcrypt/ecc.c
@@ -26,7 +26,7 @@ static void _ltc_ecc_free_public_key(struct ecc_public_key *s)
  * For a given TEE @curve, return key size and LTC curve name. Also check that
  * @algo is compatible with this curve.
  * @curve: TEE_ECC_CURVE_NIST_P192, ...
- * @algo: TEE_ALG_ECDSA_P192, ...
+ * @algo: TEE_ALG_ECDSA_SHA1, ...
  */
 static TEE_Result ecc_get_curve_info(uint32_t curve, uint32_t algo,
 				     size_t *key_size_bytes,
@@ -46,50 +46,45 @@ static TEE_Result ecc_get_curve_info(uint32_t curve, uint32_t algo,
 	 * respectively.
 	 */
 
-	/*
-	 * Note GPv1.1 indicates TEE_ALG_ECDH_NIST_P192_DERIVE_SHARED_SECRET
-	 * but defines TEE_ALG_ECDH_P192
-	 */
-
 	switch (curve) {
 	case TEE_ECC_CURVE_NIST_P192:
 		size_bits = 192;
 		size_bytes = 24;
 		name = "NISTP192";
-		if ((algo != 0) && (algo != TEE_ALG_ECDSA_P192) &&
-		    (algo != TEE_ALG_ECDH_P192))
+		if ((algo != 0) && (algo != TEE_ALG_ECDSA_SHA1) &&
+		    (algo != TEE_ALG_ECDH_DERIVE_SHARED_SECRET))
 			return TEE_ERROR_BAD_PARAMETERS;
 		break;
 	case TEE_ECC_CURVE_NIST_P224:
 		size_bits = 224;
 		size_bytes = 28;
 		name = "NISTP224";
-		if ((algo != 0) && (algo != TEE_ALG_ECDSA_P224) &&
-		    (algo != TEE_ALG_ECDH_P224))
+		if ((algo != 0) && (algo != TEE_ALG_ECDSA_SHA224) &&
+		    (algo != TEE_ALG_ECDH_DERIVE_SHARED_SECRET))
 			return TEE_ERROR_BAD_PARAMETERS;
 		break;
 	case TEE_ECC_CURVE_NIST_P256:
 		size_bits = 256;
 		size_bytes = 32;
 		name = "NISTP256";
-		if ((algo != 0) && (algo != TEE_ALG_ECDSA_P256) &&
-		    (algo != TEE_ALG_ECDH_P256))
+		if ((algo != 0) && (algo != TEE_ALG_ECDSA_SHA256) &&
+		    (algo != TEE_ALG_ECDH_DERIVE_SHARED_SECRET))
 			return TEE_ERROR_BAD_PARAMETERS;
 		break;
 	case TEE_ECC_CURVE_NIST_P384:
 		size_bits = 384;
 		size_bytes = 48;
 		name = "NISTP384";
-		if ((algo != 0) && (algo != TEE_ALG_ECDSA_P384) &&
-		    (algo != TEE_ALG_ECDH_P384))
+		if ((algo != 0) && (algo != TEE_ALG_ECDSA_SHA384) &&
+		    (algo != TEE_ALG_ECDH_DERIVE_SHARED_SECRET))
 			return TEE_ERROR_BAD_PARAMETERS;
 		break;
 	case TEE_ECC_CURVE_NIST_P521:
 		size_bits = 521;
 		size_bytes = 66;
 		name = "NISTP521";
-		if ((algo != 0) && (algo != TEE_ALG_ECDSA_P521) &&
-		    (algo != TEE_ALG_ECDH_P521))
+		if ((algo != 0) && (algo != TEE_ALG_ECDSA_SHA512) &&
+		    (algo != TEE_ALG_ECDH_DERIVE_SHARED_SECRET))
 			return TEE_ERROR_BAD_PARAMETERS;
 		break;
 	case TEE_ECC_CURVE_SM2:

core/tee/tee_svc_cryp.c
@@ -2591,6 +2591,30 @@ static TEE_Result tee_svc_cryp_check_key_type(const struct tee_obj *o,
 	return TEE_SUCCESS;
 }
 
+static uint32_t translate_compat_algo(uint32_t algo)
+{
+	switch (algo) {
+	case __OPTEE_ALG_ECDSA_P192:
+		return TEE_ALG_ECDSA_SHA1;
+	case __OPTEE_ALG_ECDSA_P224:
+		return TEE_ALG_ECDSA_SHA224;
+	case __OPTEE_ALG_ECDSA_P256:
+		return TEE_ALG_ECDSA_SHA256;
+	case __OPTEE_ALG_ECDSA_P384:
+		return TEE_ALG_ECDSA_SHA384;
+	case __OPTEE_ALG_ECDSA_P521:
+		return TEE_ALG_ECDSA_SHA512;
+	case __OPTEE_ALG_ECDH_P192:
+	case __OPTEE_ALG_ECDH_P224:
+	case __OPTEE_ALG_ECDH_P256:
+	case __OPTEE_ALG_ECDH_P384:
+	case __OPTEE_ALG_ECDH_P521:
+		return TEE_ALG_ECDH_DERIVE_SHARED_SECRET;
+	default:
+		return algo;
+	}
+}
+
 TEE_Result syscall_cryp_state_alloc(unsigned long algo, unsigned long mode,
 			unsigned long key1, unsigned long key2,
 			uint32_t *state)
@@ -2601,6 +2625,8 @@ TEE_Result syscall_cryp_state_alloc(unsigned long algo, unsigned long mode,
 	struct tee_cryp_state *cs = NULL;
 	struct tee_obj *o1 = NULL;
 	struct tee_obj *o2 = NULL;
+
+	algo = translate_compat_algo(algo);
 
 	if (key1 != 0) {
 		res = tee_obj_get(utc, uref_to_vaddr(key1), &o1);
@@ -3447,10 +3473,11 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 		}
 		crypto_bignum_free(pub);
 		crypto_bignum_free(ss);
-	} else if (TEE_ALG_GET_MAIN_ALG(cs->algo) == TEE_MAIN_ALGO_ECDH) {
-		struct ecc_public_key key_public;
-		uint8_t *pt_secret;
-		unsigned long pt_secret_len;
+	} else if (cs->algo == TEE_ALG_ECDH_DERIVE_SHARED_SECRET) {
+		uint32_t curve = ((struct ecc_keypair *)ko->attr)->curve;
+		struct ecc_public_key key_public = { };
+		uint8_t *pt_secret = NULL;
+		unsigned long pt_secret_len = 0;
 		uint32_t key_type = TEE_TYPE_ECDH_PUBLIC_KEY;
 
 		if (param_count != 2 ||
@@ -3460,20 +3487,20 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 			goto out;
 		}
 
-		switch (cs->algo) {
-		case TEE_ALG_ECDH_P192:
+		switch (curve) {
+		case TEE_ECC_CURVE_NIST_P192:
 			alloc_size = 192;
 			break;
-		case TEE_ALG_ECDH_P224:
+		case TEE_ECC_CURVE_NIST_P224:
 			alloc_size = 224;
 			break;
-		case TEE_ALG_ECDH_P256:
+		case TEE_ECC_CURVE_NIST_P256:
 			alloc_size = 256;
 			break;
-		case TEE_ALG_ECDH_P384:
+		case TEE_ECC_CURVE_NIST_P384:
 			alloc_size = 384;
 			break;
-		case TEE_ALG_ECDH_P521:
+		case TEE_ECC_CURVE_NIST_P521:
 			alloc_size = 521;
 			break;
 		default:
@@ -3486,7 +3513,7 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 							  alloc_size);
 		if (res != TEE_SUCCESS)
 			goto out;
-		key_public.curve = ((struct ecc_keypair *)ko->attr)->curve;
+		key_public.curve = curve;
 		crypto_bignum_bin2bn(params[0].content.ref.buffer,
 				     params[0].content.ref.length,
 				     key_public.x);
@@ -4177,11 +4204,11 @@ TEE_Result syscall_asymm_operate(unsigned long state,
 					       num_params);
 		break;
 
-	case TEE_ALG_ECDSA_P192:
-	case TEE_ALG_ECDSA_P224:
-	case TEE_ALG_ECDSA_P256:
-	case TEE_ALG_ECDSA_P384:
-	case TEE_ALG_ECDSA_P521:
+	case TEE_ALG_ECDSA_SHA1:
+	case TEE_ALG_ECDSA_SHA224:
+	case TEE_ALG_ECDSA_SHA256:
+	case TEE_ALG_ECDSA_SHA384:
+	case TEE_ALG_ECDSA_SHA512:
 	case TEE_ALG_SM2_DSA_SM3:
 		res = crypto_acipher_ecc_sign(cs->algo, o->attr, src_data,
 					      src_len, dst_data, &dlen);

lib/libutee/include/tee_api_defines.h
@@ -263,27 +263,52 @@
 #define TEE_ALG_HMAC_SHA384                     0x30000005
 #define TEE_ALG_HMAC_SHA512                     0x30000006
 #define TEE_ALG_HMAC_SM3                        0x30000007
+
 /*
- * Fix GP Internal Core API v1.1
+ * These are used in the OP-TEE ABI, due to an inconsistency in the v1.1
+ * specification the wrong values we assumed and now we're stuck with those.
+ *
+ * In GP Internal Core API v1.1
  *     "Table 6-12:  Structure of Algorithm Identifier"
  *     indicates ECDSA have the algorithm "0x41" and ECDH "0x42"
  * whereas
  *     "Table 6-11:  List of Algorithm Identifiers" defines
  *     TEE_ALG_ECDSA_P192 as 0x70001042
  *
- * We chose to define TEE_ALG_ECDSA_P192 as 0x70001041 (conform to table 6-12)
+ * We chose to define __OPTEE_TEE_ALG_ECDSA_P192 as 0x70001041 and so on
+ * to conform to table 6-12.
  */
-#define TEE_ALG_ECDSA_P192                      0x70001041
-#define TEE_ALG_ECDSA_P224                      0x70002041
-#define TEE_ALG_ECDSA_P256                      0x70003041
-#define TEE_ALG_ECDSA_P384                      0x70004041
-#define TEE_ALG_ECDSA_P521                      0x70005041
+#define __OPTEE_ALG_ECDSA_P192			0x70001041
+#define __OPTEE_ALG_ECDSA_P224			0x70002041
+#define __OPTEE_ALG_ECDSA_P256			0x70003041
+#define __OPTEE_ALG_ECDSA_P384			0x70004041
+#define __OPTEE_ALG_ECDSA_P521			0x70005041
+#define __OPTEE_ALG_ECDH_P192			0x80001042
+#define __OPTEE_ALG_ECDH_P224			0x80002042
+#define __OPTEE_ALG_ECDH_P256			0x80003042
+#define __OPTEE_ALG_ECDH_P384			0x80004042
+#define __OPTEE_ALG_ECDH_P521			0x80005042
+
+/* TEE_ALG_ECDSA_P* and TEE_ALG_ECDH_P* are deprecated */
+#define TEE_ALG_ECDSA_P192			TEE_ALG_ECDSA_SHA1
+#define TEE_ALG_ECDSA_P224			TEE_ALG_ECDSA_SHA224
+#define TEE_ALG_ECDSA_P256			TEE_ALG_ECDSA_SHA256
+#define TEE_ALG_ECDSA_P384			TEE_ALG_ECDSA_SHA384
+#define TEE_ALG_ECDSA_P521			TEE_ALG_ECDSA_SHA512
+#define TEE_ALG_ECDH_P192		TEE_ALG_ECDH_DERIVE_SHARED_SECRET
+#define TEE_ALG_ECDH_P224		TEE_ALG_ECDH_DERIVE_SHARED_SECRET
+#define TEE_ALG_ECDH_P256		TEE_ALG_ECDH_DERIVE_SHARED_SECRET
+#define TEE_ALG_ECDH_P384		TEE_ALG_ECDH_DERIVE_SHARED_SECRET
+#define TEE_ALG_ECDH_P521		TEE_ALG_ECDH_DERIVE_SHARED_SECRET
+
+#define TEE_ALG_ECDH_DERIVE_SHARED_SECRET	0x80000042 /* v1.1.2 spec */
+#define TEE_ALG_ECDSA_SHA1			0x70001042 /* v1.1.2 spec */
+#define TEE_ALG_ECDSA_SHA224			0x70002042 /* v1.1.2 spec */
+#define TEE_ALG_ECDSA_SHA256			0x70003042 /* v1.1.2 spec */
+#define TEE_ALG_ECDSA_SHA384			0x70004042 /* v1.1.2 spec */
+#define TEE_ALG_ECDSA_SHA512			0x70005042 /* v1.1.2 spec */
+
 #define TEE_ALG_ED25519                         0x70006043 /* v1.3.1 spec */
-#define TEE_ALG_ECDH_P192                       0x80001042
-#define TEE_ALG_ECDH_P224                       0x80002042
-#define TEE_ALG_ECDH_P256                       0x80003042
-#define TEE_ALG_ECDH_P384                       0x80004042
-#define TEE_ALG_ECDH_P521                       0x80005042
 #define TEE_ALG_SM2_PKE                         0x80000045
 #define TEE_ALG_SM3                             0x50000007
 #define TEE_ALG_X25519                          0x80000044

lib/libutee/include/utee_defines.h
@@ -83,6 +83,12 @@ static inline uint32_t __tee_alg_get_main_alg(uint32_t algo)
 		return TEE_MAIN_ALGO_X25519;
 	case TEE_ALG_ED25519:
 		return TEE_MAIN_ALGO_ED25519;
+	case TEE_ALG_ECDSA_SHA1:
+	case TEE_ALG_ECDSA_SHA224:
+	case TEE_ALG_ECDSA_SHA256:
+	case TEE_ALG_ECDSA_SHA384:
+	case TEE_ALG_ECDSA_SHA512:
+		return TEE_MAIN_ALGO_ECDSA;
 	default:
 		break;
 	}

commit 154c16ad3221e190331cd70f3077666101df78a8

libutee: TEE_AllocateOperation(): allow wider range of key sizes for ECDSA
The TEE Internal Core API specification v1.3.1 does not restrict the
size of key to a specific value for algorithms TEE_ALG_ECDSA_SHA*.
Table 5-9 just mentions that ECDSA keys shall be between 160 and 521
bits so check this range.

Fixes: fe2fd3ff46c0 ("GP131: Add TEE_ALG_ECDH_DERIVE_SHARED_SECRET and TEE_ALG_ECDSA_SHA*")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

lib/libutee/tee_api_operations.c
@@ -76,20 +76,26 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 		break;
 
 	case TEE_ALG_ECDSA_SHA1:
+	case TEE_ALG_ECDSA_SHA224:
+	case TEE_ALG_ECDSA_SHA256:
+	case TEE_ALG_ECDSA_SHA384:
+	case TEE_ALG_ECDSA_SHA512:
+		if (maxKeySize < 160 || maxKeySize > 521)
+			return TEE_ERROR_NOT_SUPPORTED;
+		break;
+
 	case __OPTEE_ALG_ECDSA_P192:
 	case __OPTEE_ALG_ECDH_P192:
 		if (maxKeySize != 192)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_SHA224:
 	case __OPTEE_ALG_ECDSA_P224:
 	case __OPTEE_ALG_ECDH_P224:
 		if (maxKeySize != 224)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_SHA256:
 	case __OPTEE_ALG_ECDSA_P256:
 	case __OPTEE_ALG_ECDH_P256:
 	case TEE_ALG_SM2_PKE:
@@ -104,14 +110,12 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_SHA384:
 	case __OPTEE_ALG_ECDSA_P384:
 	case __OPTEE_ALG_ECDH_P384:
 		if (maxKeySize != 384)
 			return TEE_ERROR_NOT_SUPPORTED;
 		break;
 
-	case TEE_ALG_ECDSA_SHA512:
 	case __OPTEE_ALG_ECDSA_P521:
 	case __OPTEE_ALG_ECDH_P521:
 		if (maxKeySize != 521)