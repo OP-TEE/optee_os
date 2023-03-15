52986d0273dfb2922278532ce7f26ff0f2be3faf
Xiaoxu Zeng <zengxiaoxu@huawei.com>
2023-03-15 18:51:26 +0800

    drivers: implement qm driver
    The Hisilicon QM is a Queue Management module.
    In order to unify the interface between accelerator and software,
    a unified queue management module QM is used to interact with software.
    Each accelerator module integrates a QM. Software issues tasks to the SQ,
    and the QM obtains the address of the SQE. The BD information is sent to
    the accelerator. After the task processing is complete, the accelerator
    applies for a write-back address from the QM to write back the SQ
    
    Signed-off-by: Xiaoxu Zeng <zengxiaoxu@huawei.com>

---
+++ core/drivers/crypto/kunpeng/qm/hisi_qm.h
@@ -0,0 +1,221 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2022, Huawei Technologies Co., Ltd
+ */
+#ifndef __ACC_QM_H__
+#define __ACC_QM_H__
+
+#include <stdlib.h>
+#include <string.h>
+#include <string_ext.h>
+#include <initcall.h>
+#include <io.h>
+#include <sys/queue.h>
+#include <kernel/mutex.h>
+#include <kernel/delay.h>
+#include <mm/core_memprot.h>
+
+#define QM_HW_V2 0x21
+#define QM_HW_V3 0x30
+#define QM_MAX_VFS_NUM 63
+#define QM_PF_Q_BASE 0
+#define QM_PF_Q_NUM 64
+#define QM_VF_Q_NUM 15
+#define QM_Q_DEPTH 8
+
+#define QM_ABNML_INT_MASK 0x100004
+#define QM_ABNML_INT_MASK_CFG 0x7fff
+#define QM_ABNML_INT_SRC 0x100000
+#define QM_HPRE_NFE_INT_MASK 0x6fb7
+#define QM_SEC_NFE_INT_MASK 0x6ff7
+#define QM_INVALID_DB BIT(12)
+#define QM_REVISON_ID_BASE 0x1000dc
+#define QM_REVISON_ID_MASK GENMASK_32(7, 0)
+#define upper_32_bits(n) ((uint32_t)(((n) >> 16) >> 16))
+#define lower_32_bits(n) ((uint32_t)(n))
+#define POLL_PERIOD 10
+#define POLL_TIMEOUT 1000
+#define QM_RECV_SYNC_TIMEOUT 0xfffffff
+#define QM_ALIGN128 128
+#define QM_ALIGN32 32
+
+enum qm_fun_type {
+	QM_HW_PF,
+	QM_HW_VF,
+};
+
+enum qm_sq_type {
+	QM_CHANNEL_TYPE0 = 0,
+	QM_CHANNEL_TYPE1,
+	QM_CHANNEL_TYPE2,
+};
+
+struct qm_sqc {
+	uint16_t head;
+	uint16_t tail;
+	uint32_t base_l;
+	uint32_t base_h;
+	/*
+	 * qes : 12
+	 * sqe : 4
+	 * rsv(stash_nid/stash_en) : 16
+	 */
+	uint32_t dw3;
+	uint16_t rand_data;
+	uint16_t rsv0;
+	uint16_t pasid;
+	/*
+	 * rsv : 5
+	 * head_sig : 1
+	 * tail_sig : 1
+	 * pasid_en : 1
+	 * rsv : 8
+	 */
+	uint16_t w11;
+	uint16_t cq_num;
+	/*
+	 * priority(Credit): 4
+	 * order(order/fc/close/rsv) : 4
+	 * type : 4
+	 * rsv : 4
+	 */
+	uint16_t w13;
+	uint32_t rsv1;
+};
+
+struct qm_cqc {
+	uint16_t head;
+	uint16_t tail;
+	uint32_t base_l;
+	uint32_t base_h;
+	/*
+	 * qes : 12
+	 * cqe_size : 4
+	 * rsv(stash_nid/stash_en) : 16
+	 */
+	uint32_t dw3;
+	uint16_t rand_data;
+	uint16_t rsv0;
+	uint16_t pasid;
+	/*
+	 * pasid_en : 1
+	 * rsv ：4
+	 * head_sig : 1
+	 * tail_sig : 1
+	 * rsv : 9
+	 */
+	uint16_t w11;
+	/*
+	 * phase : 1
+	 * c_flag : 1
+	 * stash_vld : 1
+	 */
+	uint32_t dw6;
+	uint32_t rsv1;
+};
+
+struct qm_cqe {
+	uint32_t rsv0;
+	uint16_t cmd_id;
+	uint16_t rsv1;
+	uint16_t sq_head;
+	uint16_t sq_id;
+	uint16_t rsv2;
+	/*
+	 * p : 1
+	 * status : 15
+	 */
+	uint16_t w7;
+};
+
+struct hisi_qp {
+	struct hisi_qm *qm;
+	uint32_t qp_id;
+	uint8_t sq_type;
+	uint16_t sq_tail;
+	uint16_t cq_head;
+	bool cqc_phase;
+
+	void *sqe;
+	struct qm_cqe *cqe;
+	paddr_t sqe_dma;
+	paddr_t cqe_dma;
+
+	int32_t (*fill_sqe)(void *sqe, void *msg);
+	int32_t (*parse_sqe)(void *sqe, void *msg);
+};
+
+struct hisi_qm {
+	enum qm_fun_type fun_type;
+	uintptr_t io_base;
+	uint32_t io_size;
+	uint32_t vfs_num;
+	uint32_t version;
+
+	struct qm_sqc *sqc;
+	struct qm_cqc *cqc;
+	paddr_t sqc_dma;
+	paddr_t cqc_dma;
+	uint32_t sqe_size;
+	uint32_t sqe_log2_size;
+	uint32_t qp_base;
+	uint32_t qp_num;
+	uint32_t qp_in_used;
+	uint32_t qp_idx;
+	struct hisi_qp *qp_array;
+	struct mutex qp_lock; /* protect the qp instance */
+
+	int32_t (*dev_status_check)(struct hisi_qm *qm);
+};
+
+enum drvcrypt_status {
+	DRVCRYPT_NO_ERR = 0,
+	DRVCRYPT_FAIL = 1,
+	DRVCRYPT_EIO = 5,
+	DRVCRYPT_EAGAIN = 11,
+	DRVCRYPT_ENOMEM = 12,
+	DRVCRYPT_EFAULT = 14,
+	DRVCRYPT_EBUSY = 16,
+	DRVCRYPT_ENODEV = 19,
+	DRVCRYPT_EINVAL = 22,
+	DRVCRYPT_ETMOUT = 110,
+	DRVCRYPT_ENOPROC,
+	DRVCRYPT_IN_EPARA,
+	DRVCRYPT_VERIFY_ERR,
+	DRVCRYPT_HW_EACCESS,
+};
+
+#define readl_relaxed_poll_timeout(addr, val, cond, delay_us, timeout_us) \
+({ \
+	uint64_t timeout = 0; \
+	while (timeout < (timeout_us)) { \
+		(val) = io_read32(addr); \
+		if (cond) \
+			break; \
+		timeout += (delay_us); \
+		udelay(delay_us); \
+	} \
+	(cond) ? 0 : -DRVCRYPT_ETMOUT; \
+})
+
+struct acc_device {
+	struct hisi_qm qm;
+	uintptr_t io_base;
+	uint32_t io_size;
+	uint32_t vfs_num;
+	uint32_t endian;
+	enum qm_fun_type fun_type;
+	SLIST_ENTRY(acc_device) link;
+};
+
+void hisi_qm_get_version(struct hisi_qm *qm);
+int32_t hisi_qm_init(struct hisi_qm *qm);
+void hisi_qm_uninit(struct hisi_qm *qm);
+int32_t hisi_qm_start(struct hisi_qm *qm);
+void hisi_qm_dev_init(struct hisi_qm *qm);
+struct hisi_qp *hisi_qm_create_qp(struct hisi_qm *qm, uint8_t sq_type);
+void hisi_qm_release_qp(struct hisi_qp *qp);
+int32_t hisi_qp_send(struct hisi_qp *qp, void *msg);
+int32_t hisi_qp_recv_sync(struct hisi_qp *qp, void *msg);
+
+#endif
