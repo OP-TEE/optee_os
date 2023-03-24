OP-TEE/optee_os

commit 6cc77cdd73aaee4693f971172f51eabb24f6e534

crypto: drivers: se050-f: ecc: can fallback to softw-ops
The SE050-F device can select to fallback to specific unsupported
operations.

This allows xtests to run to completion without errors.

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/se050/core/ecc.c
@@ -23,14 +23,35 @@
 static const struct crypto_ecc_keypair_ops *pair_ops;
 static const struct crypto_ecc_public_ops *pub_ops;
 
-static bool oefid_key_supported(size_t bits __unused)
+static bool oefid_key_supported(size_t bits)
 {
-	return true;
+	switch (se050_get_oefid()) {
+	case SE050F_ID:
+		return bits >= 224;
+	default:
+		return true;
+	}
 }
 
-static bool oefid_algo_supported(uint32_t algo __unused)
+static bool oefid_algo_supported(uint32_t algo)
 {
-	return true;
+	if (!algo)
+		return true;
+
+	switch (se050_get_oefid()) {
+	case SE050F_ID:
+		switch (algo) {
+		case TEE_ALG_ECDSA_P224:
+		case TEE_ALG_ECDSA_P256:
+		case TEE_ALG_ECDSA_P384:
+		case TEE_ALG_ECDSA_P521:
+			return true;
+		default:
+			return false;
+		}
+	default:
+		return true;
+	}
 }
 
 static uint32_t algo_tee2se050(uint32_t algo)

commit b3b79db80c48f6c390c4a72d54e142b4a5d84c84

se050: ecc: SE050-F shared secret
The SE050-F does not support shared secret generation.
Allow this operation to also fallback to its software implementation.

Fixes: 6cc77cdd73aa ("crypto: drivers: se050-f: ecc: can fallback to softw-ops")
Test: xtest regression 4009

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/drivers/crypto/se050/core/ecc.c
@@ -36,9 +36,6 @@ static bool oefid_key_supported(size_t bits)
 
 static bool oefid_algo_supported(uint32_t algo)
 {
-	if (!algo)
-		return true;
-
 	switch (se050_get_oefid()) {
 	case SE050F_ID:
 		switch (algo) {