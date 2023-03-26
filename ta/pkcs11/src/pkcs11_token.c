ff3c76b4eea7045db2373968434bbbf5908f5a5f
Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
2023-03-26 15:16:42 +0300

    ta: pkcs11: Allow authentication mode switch with PIN change
    In order to enable one to switch from PIN mode to TEE Identity mode or
    vice versa enable the mode switch when token has just been initialized but
    not taken in use (eg. no user PIN set).
    
    Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

--- ta/pkcs11/src/pkcs11_token.c
+++ ta/pkcs11/src/pkcs11_token.c
@@ -973,11 +973,58 @@ static enum pkcs11_rc set_pin(struct pkcs11_session *session,
 		return PKCS11_CKR_SESSION_READ_ONLY;
 
 	if (IS_ENABLED(CFG_PKCS11_TA_AUTH_TEE_IDENTITY) &&
-	    token->db_main->flags & PKCS11_CKFT_PROTECTED_AUTHENTICATION_PATH) {
-		rc = setup_identity_auth_from_pin(token, user_type, new_pin,
-						  new_pin_size);
-		if (rc)
+	    (token->db_main->flags &
+	     PKCS11_CKFT_PROTECTED_AUTHENTICATION_PATH)) {
+		rc = setup_identity_auth_from_pin(token, user_type,
+						  new_pin, new_pin_size);
+		if (rc == PKCS11_CKR_OK) {
+			goto update_db;
+		} else if (rc == PKCS11_CKR_PIN_INVALID &&
+			   !(token->db_main->flags &
+			     PKCS11_CKFT_USER_PIN_INITIALIZED)) {
+			/*
+			 * PIN was not compatible with TEE Identity
+			 * Authentication syntax so let's assume it might be a
+			 * new SO PIN to switch the authentication mode.
+			 */
+
+			/* Update mode flag if all PIN checks pass */
+			flags_clear |=
+				PKCS11_CKFT_PROTECTED_AUTHENTICATION_PATH;
+		} else {
 			return rc;
+		}
+	} else if ((user_type == PKCS11_CKU_SO) && !new_pin &&
+		   IS_ENABLED(CFG_PKCS11_TA_AUTH_TEE_IDENTITY) &&
+		   !(token->db_main->flags &
+		     PKCS11_CKFT_USER_PIN_INITIALIZED)) {
+		/*
+		 * Allow changing of token authentication mode before user pin
+		 * has been initialized.
+		 */
+
+		/*
+		 * Set protected authentication path temporary until
+		 * finalized.
+		 */
+		token->db_main->flags |=
+			PKCS11_CKFT_PROTECTED_AUTHENTICATION_PATH;
+
+		/*
+		 * Setup authentication from currently active TEE Client
+		 * Identity.
+		 */
+		rc = setup_identity_auth_from_pin(token, PKCS11_CKU_SO,
+						  NULL, 0);
+		if (rc) {
+			/*
+			 * Failed to setup protected authentication path so
+			 * clear the temporary flag.
+			 */
+			token->db_main->flags &=
+				~PKCS11_CKFT_PROTECTED_AUTHENTICATION_PATH;
+			return rc;
+		}
 
 		goto update_db;
 	}
@@ -994,10 +1041,10 @@ static enum pkcs11_rc set_pin(struct pkcs11_session *session,
 		if (rc)
 			return rc;
 		token->db_main->so_pin_count = 0;
-		flags_clear = PKCS11_CKFT_SO_PIN_COUNT_LOW |
-			      PKCS11_CKFT_SO_PIN_FINAL_TRY |
-			      PKCS11_CKFT_SO_PIN_LOCKED |
-			      PKCS11_CKFT_SO_PIN_TO_BE_CHANGED;
+		flags_clear |= PKCS11_CKFT_SO_PIN_COUNT_LOW |
+			       PKCS11_CKFT_SO_PIN_FINAL_TRY |
+			       PKCS11_CKFT_SO_PIN_LOCKED |
+			       PKCS11_CKFT_SO_PIN_TO_BE_CHANGED;
 		break;
 	case PKCS11_CKU_USER:
 		rc = hash_pin(user_type, new_pin, new_pin_size,
@@ -1006,11 +1053,11 @@ static enum pkcs11_rc set_pin(struct pkcs11_session *session,
 		if (rc)
 			return rc;
 		token->db_main->user_pin_count = 0;
-		flags_clear = PKCS11_CKFT_USER_PIN_COUNT_LOW |
-			      PKCS11_CKFT_USER_PIN_FINAL_TRY |
-			      PKCS11_CKFT_USER_PIN_LOCKED |
-			      PKCS11_CKFT_USER_PIN_TO_BE_CHANGED;
-		flags_set = PKCS11_CKFT_USER_PIN_INITIALIZED;
+		flags_clear |= PKCS11_CKFT_USER_PIN_COUNT_LOW |
+			       PKCS11_CKFT_USER_PIN_FINAL_TRY |
+			       PKCS11_CKFT_USER_PIN_LOCKED |
+			       PKCS11_CKFT_USER_PIN_TO_BE_CHANGED;
+		flags_set |= PKCS11_CKFT_USER_PIN_INITIALIZED;
 		break;
 	default:
 		return PKCS11_CKR_FUNCTION_FAILED;
