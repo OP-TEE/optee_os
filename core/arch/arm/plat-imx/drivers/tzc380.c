OP-TEE/optee_os

commit 97eb916803fe6b01dac779d3511eb5a0490646d5

drivers: imx: tzc380: re-configure TZ380 upon PM resume
Call the initialization function of TZC380 upon resume to reconfigure
regions and check region lockdown.

Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-imx/drivers/tzc380.c
@@ -12,6 +12,7 @@
 #include <imx-regs.h>
 #include <initcall.h>
 #include <kernel/panic.h>
+#include <kernel/pm.h>
 #include <mm/core_memprot.h>
 #include <mm/generic_ram_layout.h>
 
@@ -82,3 +83,21 @@ static TEE_Result imx_configure_tzasc(void)
 	return TEE_SUCCESS;
 }
 driver_init(imx_configure_tzasc);
+
+static TEE_Result
+pm_enter_resume(enum pm_op op, uint32_t pm_hint __unused,
+		const struct pm_callback_handle *pm_handle __unused)
+{
+	if (op == PM_OP_RESUME)
+		return imx_configure_tzasc();
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result tzasc_init(void)
+{
+	register_pm_driver_cb(pm_enter_resume, NULL, "imx-tzasc");
+
+	return imx_configure_tzasc();
+}
+driver_init(tzasc_init);

commit e57c8f5bd90e4f362b2abc23dc02719a30e75a9b

core: imx: remove duplicate driver_init() call
Remove duplicated call to driver_init().
The previous driver_init() call would only initialize the driver.
The new driver_init() call initializes the driver and its power
management callback.

Fixes: 97eb916803fe ("drivers: imx: tzc380: re-configure TZ380 upon PM resume")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-imx/drivers/tzc380.c
@@ -82,7 +82,6 @@ static TEE_Result imx_configure_tzasc(void)
 	}
 	return TEE_SUCCESS;
 }
-driver_init(imx_configure_tzasc);
 
 static TEE_Result
 pm_enter_resume(enum pm_op op, uint32_t pm_hint __unused,