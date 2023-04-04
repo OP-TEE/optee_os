OP-TEE/optee_os

commit 19b1ce2b2b2bf112be1d3d59bac8b1bc4c89e656

core: ree_fs: check ta size before use
Check that the total loaded size of a TA matches what is in the sign
header. This prevents an eventual attacker from providing arbitrary
values in the img_size field of the signed header.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Suggested-by: Asaf Modelevsky <amodele@amazon.com>
Reported-by: Asaf Modelevsky <amodele@amazon.com>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/ree_fs_ta.c
@@ -424,6 +424,14 @@ static TEE_Result ree_fs_ta_open(const TEE_UUID *uuid,
 			goto error_free_hash;
 		}
 
+		/*
+		 * This is checked further down too, but we must sanity
+		 * check shdr->img_size before it's used below.
+		 */
+		if (ta_size != sz + shdr->img_size) {
+			res = TEE_ERROR_SECURITY;
+			goto error_free_hash;
+		}
 
 		ehdr = malloc(ehdr_sz);
 		if (!ehdr) {

commit b050020478839ded7dd68813e31f9c7b84621d34

core: fix loading of encrypted TA
The total size of headers for TAs signed with a subkey varies, take that
into account when sanity checking the img_size field of the signed
header.

Fixes: 19b1ce2b2b2b ("core: ree_fs: check ta size before use")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (vexpress-qemu_armv8a)

core/kernel/ree_fs_ta.c
@@ -435,7 +435,7 @@ static TEE_Result ree_fs_ta_open(const TEE_UUID *uuid,
 		 * This is checked further down too, but we must sanity
 		 * check shdr->img_size before it's used below.
 		 */
-		if (ta_size != sz + shdr->img_size) {
+		if (ta_size != offs + ehdr_sz + shdr->img_size) {
 			res = TEE_ERROR_SECURITY;
 			goto error_free_hash;
 		}