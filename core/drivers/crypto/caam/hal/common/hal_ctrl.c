OP-TEE/optee_os

commit d538d2936c2273c669de2892b6b290d9782c1554

drivers: caam: add manufacturing protection feature
The CAAM features a "manufacturing protection" functionality.
It is a authentication process used to authenticate the chip to
the OEM's server. The authentication process can ensure the chip:
 * is a genuine NXP part
 * is a correct part type
 * has been properly fused
 * is running a authenticated software
 * runs in secure/trusted mode.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/caam/hal/common/hal_ctrl.c
@@ -9,6 +9,8 @@
 #include <caam_io.h>
 #include <caam_trace.h>
 #include <config.h>
+#include <drivers/imx_snvs.h>
+#include <platform_config.h>
 #include <registers/ctrl_regs.h>
 #include <registers/version_regs.h>
 #include <kernel/panic.h>
@@ -127,4 +129,126 @@ void caam_hal_ctrl_inc_priblob(vaddr_t baseaddr)
 		   blob);
 	if (val != blob)
 		panic("Written PRIBLOB and read PRIBLOB do not match!");
+}
+
+uint8_t caam_hal_ctrl_get_mpcurve(vaddr_t ctrl_addr)
+{
+	uint32_t val_scfgr = 0;
+
+	/*
+	 * On i.MX8MQ B0, the MP is not usable, hence
+	 * return UINT8_MAX
+	 */
+	if (soc_is_imx8mq_b0_layer())
+		return UINT8_MAX;
+
+	/*
+	 * Verify if the device is closed or not
+	 * If device is closed, check get the MPCurve
+	 */
+	if (snvs_is_device_closed()) {
+		/* Get the SCFGR content */
+		val_scfgr = io_caam_read32(ctrl_addr + SCFGR);
+
+		/* Get the MPCurve field value - 4 bits */
+		val_scfgr = (val_scfgr & BM_SCFGR_MPCURVE) >> BS_SCFGR_MPCURVE;
+
+		/*
+		 * If the device is closed and the MPCurve field is 0
+		 * return UINT8_MAX indicating that there is a problem and the
+		 * MP can not be supported.
+		 */
+		if (!val_scfgr)
+			return UINT8_MAX;
+	}
+
+	return val_scfgr;
+}
+
+TEE_Result caam_hal_ctrl_read_mpmr(vaddr_t ctrl_addr, struct caambuf *mpmr)
+{
+	unsigned int i = 0;
+	uint32_t val = 0;
+
+	if (mpmr->length < MPMR_NB_REG) {
+		mpmr->length = MPMR_NB_REG;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	/* MPMR endianness is reverted between write and read */
+	for (i = 0; i < MPMR_NB_REG; i += 4) {
+		val = io_caam_read32(ctrl_addr + MPMR + i);
+		mpmr->data[i] = (uint8_t)(val >> 24);
+		mpmr->data[i + 1] = (uint8_t)(val >> 16);
+		mpmr->data[i + 2] = (uint8_t)(val >> 8);
+		mpmr->data[i + 3] = (uint8_t)val;
+	}
+
+	mpmr->length = MPMR_NB_REG;
+	return TEE_SUCCESS;
+}
+
+bool caam_hal_ctrl_is_mp_set(vaddr_t ctrl_addr)
+{
+	return io_caam_read32(ctrl_addr + SCFGR) & BM_SCFGR_MPMRL;
+}
+
+void caam_hal_ctrl_fill_mpmr(vaddr_t ctrl_addr, struct caambuf *msg_mpmr)
+{
+	size_t i = 0;
+	vaddr_t reg = ctrl_addr + MPMR;
+	bool is_filled = false;
+	uint32_t val = 0;
+	size_t min_size = 0;
+	size_t remain_size = 0;
+
+	/* check if the MPMR is filled */
+	is_filled = caam_hal_ctrl_is_mp_set(ctrl_addr);
+
+	DMSG("is_filled = %s", is_filled ? "true" : "false");
+
+	if (!is_filled) {
+		/*
+		 * Fill the MPMR with the most significant input value and
+		 * complete with 0's if value too short.
+		 */
+		min_size = MIN(msg_mpmr->length, (size_t)MPMR_NB_REG);
+		remain_size = min_size % 4;
+
+		for (i = 0; i < min_size - remain_size; i += 4, reg += 4) {
+			val = msg_mpmr->data[i] | msg_mpmr->data[i + 1] << 8 |
+			      msg_mpmr->data[i + 2] << 16 |
+			      msg_mpmr->data[i + 3] << 24;
+			io_caam_write32(reg, val);
+		}
+
+		/* Last input bytes value */
+		if (remain_size) {
+			val = 0;
+
+			/*
+			 * Fill the MPMR with the 8 bits values
+			 * until the end of the message length
+			 */
+			for (i = 0; i < remain_size; i++)
+				val |= msg_mpmr->data[i] << (i * 8);
+			io_caam_write32(reg, val);
+			reg += 4;
+		}
+
+		/* Complete with 0's */
+		remain_size = (MPMR_NB_REG - ROUNDUP(msg_mpmr->length, 4)) / 4;
+		for (i = 0; i < remain_size; i++, reg += 4)
+			io_caam_write32(reg, 0x0);
+
+		/*
+		 * Locks the MPMR for writing and remains locked until
+		 * the next power-on session.
+		 */
+		io_caam_write32(ctrl_addr + SCFGR,
+				io_caam_read32(ctrl_addr + SCFGR) |
+				BM_SCFGR_MPMRL);
+
+		DMSG("val_scfgr = %#"PRIx32, io_caam_read32(ctrl_addr + SCFGR));
+	}
 }

core/drivers/crypto/caam/include/caam_hal_ctrl.h
@@ -7,6 +7,7 @@
 #ifndef __CAAM_HAL_CTRL_H__
 #define __CAAM_HAL_CTRL_H__
 
+#include <caam_common.h>
 #include <types_ext.h>
 
 /*
@@ -58,4 +59,38 @@ uint8_t caam_hal_ctrl_era(vaddr_t baseaddr);
  * @baseaddr  Controller base address
  */
 void caam_hal_ctrl_inc_priblob(vaddr_t baseaddr);
+
+/*
+ * Get the SCFGR content and check the MPCURVE fields.
+ * The function returns either:
+ *   - UINT8_MAX if the Manafacturing Protection is not supported
+ *   - The MP Curve Value if programmed (4 bits value)
+ *   - 0 if the MP Curve is not programmed
+ *
+ * @ctrl_addr  Controller base address
+ */
+uint8_t caam_hal_ctrl_get_mpcurve(vaddr_t ctrl_addr);
+
+/*
+ * Read the MPMR content
+ *
+ * @ctrl_addr  Controller base address
+ * @mpmr       [out] MPMR buffer read
+ */
+TEE_Result caam_hal_ctrl_read_mpmr(vaddr_t ctrl_addr, struct caambuf *mpmr);
+
+/*
+ * Fill the MPMR content then lock the register
+ *
+ * @ctrl_addr  Controller base address
+ * @msg_mpmr   Buffer with the message and length to fill the MPMR content
+ */
+void caam_hal_ctrl_fill_mpmr(vaddr_t ctrl_addr, struct caambuf *msg_mpmr);
+
+/*
+ * Indicate if the MP is set
+ *
+ * @ctrl_addr  Controller base address
+ */
+bool caam_hal_ctrl_is_mp_set(vaddr_t ctrl_addr);
 #endif /* __CAAM_HAL_CTRL_H__ */

core/drivers/crypto/caam/caam_ctrl.c
@@ -13,6 +13,7 @@
 #include <caam_hash.h>
 #include <caam_jr.h>
 #include <caam_blob.h>
+#include <caam_mp.h>
 #include <caam_pwr.h>
 #include <caam_rng.h>
 #include <drivers/imx_snvs.h>
@@ -126,6 +127,13 @@ static TEE_Result crypto_driver_init(void)
 	/* Initialize the DSA Module */
 	retstatus = caam_dsa_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
+		retresult = TEE_ERROR_GENERIC;
+		goto exit_init;
+	}
+
+	/* Initialize the Manufacturing Protection Module */
+	retstatus = caam_mp_init(jrcfg.base);
+	if (retstatus != CAAM_NO_ERROR && retstatus != CAAM_NOT_SUPPORTED) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
 	}

core/drivers/crypto/caam/caam_pwr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2019, 2021, 2023 NXP
  *
  * Brief   CAAM Power state management.
  */
@@ -8,7 +8,10 @@
 #include <caam_hal_clk.h>
 #include <caam_io.h>
 #include <caam_jr.h>
+#include <caam_mp.h>
 #include <caam_pwr.h>
+#include <caam_status.h>
+#include <caam_utils_status.h>
 #include <kernel/pm.h>
 #include <kernel/panic.h>
 #include <malloc.h>
@@ -141,6 +144,8 @@ static TEE_Result pm_enter(uint32_t pm_hint)
  */
 static TEE_Result pm_resume(uint32_t pm_hint)
 {
+	enum caam_status ret = CAAM_FAILURE;
+
 	PWR_TRACE("CAAM power mode %" PRIu32 " resume", pm_hint);
 	if (pm_hint == PM_HINT_CONTEXT_STATE) {
 		caam_hal_clk_enable(true);
@@ -149,7 +154,9 @@ static TEE_Result pm_resume(uint32_t pm_hint)
 
 	caam_jr_resume(pm_hint);
 
-	return TEE_SUCCESS;
+	ret = caam_mp_resume(pm_hint);
+
+	return caam_status_to_tee_result(ret);
 }
 
 /*

core/drivers/crypto/caam/crypto.mk
@@ -20,6 +20,7 @@ ifeq ($(CFG_NXP_CAAM),y)
 # DBG_ECC    BIT32(12) // ECC trace
 # DBG_DH     BIT32(13) // DH Trace
 # DBG_DSA    BIT32(14) // DSA trace
+# DBG_MP     BIT32(15) // MP trace
 
 CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
@@ -162,6 +163,10 @@ $(eval $(call cryphw-enable-drv-hw, DSA))
 #   Format #2: (p, q, d)
 #   Format #3: (p, q, dp, dq, qp)
 CFG_NXP_CAAM_RSA_KEY_FORMAT ?= 3
+endif
+
+ifneq ($(filter y, $(CFG_MX8QM) $(CFG_MX8QX) $(CFG_MX8DXL)), y)
+$(eval $(call cryphw-enable-drv-hw, MP))
 endif
 
 endif # !CFG_LS

core/drivers/crypto/caam/hal/imx_8m/registers/ctrl_regs.h
@@ -36,6 +36,6 @@
 
 /* Manufacturing Protection Message */
 #define MPMR				0x0380
-#define MPMR_NB_REG			0x20
+#define MPMR_NB_REG			U(32)
 
 #endif /* __CTRL_REGS_H__ */

core/drivers/crypto/caam/include/caam_desc_defines.h
@@ -349,6 +349,12 @@
 #define PROT_BLOB_FORMAT_NORMAL		0
 
 /*
+ * MP Protocol Information
+ */
+#define PROT_MP_PUBK_SGT	BIT32(31)
+#define PROT_MP_CURVE(curve)	SHIFT_U32((curve) & 0xF, 17)
+
+/*
  * Algorithm Identifier
  */
 #define OP_ALGO(algo)		SHIFT_U32((ALGO_##algo) & 0xFF, 16)
@@ -644,6 +650,9 @@
 #define PDB_RSA_KEY_N_SIZE(len)	SHIFT_U32((len) & 0x3FF, 16)
 
 /* Manufacturing Curve Select */
+#define PDB_SGT_MP_SIGN_MSG		SHIFT_U32(1, 31)
+#define PDB_SGT_MP_SIGN_C		SHIFT_U32(1, 29)
+#define PDB_SGT_MP_SIGN_D		SHIFT_U32(1, 28)
 #define PDB_MP_CSEL_P256	0x03
 #define PDB_MP_CSEL_P384	0x04
 #define PDB_MP_CSEL_P521	0x05

core/drivers/crypto/caam/include/caam_mp.h
@@ -1,0 +1,37 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2019, 2021, 2023 NXP
+ */
+#ifndef __CAAM_MP_H__
+#define __CAAM_MP_H__
+
+#include "tee_api_types.h"
+#include "types_ext.h"
+
+#ifdef CFG_NXP_CAAM_MP_DRV
+/*
+ * Initialize the MP module and generate the private key
+ *
+ * @ctrl_addr   Controller base address
+ */
+enum caam_status caam_mp_init(vaddr_t ctrl_addr);
+
+/*
+ * Power Management for MP
+ *
+ * @pm_hint   Power mode type
+ */
+enum caam_status caam_mp_resume(uint32_t pm_hint);
+#else
+static inline enum caam_status caam_mp_init(vaddr_t ctrl_addr __unused)
+{
+	return CAAM_NO_ERROR;
+}
+
+static inline enum caam_status caam_mp_resume(uint32_t pm_hint __unused)
+{
+	return CAAM_NO_ERROR;
+}
+#endif /* CFG_NXP_CAAM_MP_DRV */
+
+#endif /* __CAAM_MP_H__ */

commit ddd25190bdb66180865f892b2cacf6bfc00b90fc

drivers: caam: fix MP abstraction layer functions
Compile manufacturing HAL functions only if the platform supports it.

Fixes: d538d2936c22 ("drivers: caam: add manufacturing protection feature")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/caam/hal/common/hal_ctrl.c
@@ -131,6 +131,7 @@ void caam_hal_ctrl_inc_priblob(vaddr_t baseaddr)
 		panic("Written PRIBLOB and read PRIBLOB do not match!");
 }
 
+#ifdef CFG_NXP_CAAM_MP_DRV
 uint8_t caam_hal_ctrl_get_mpcurve(vaddr_t ctrl_addr)
 {
 	uint32_t val_scfgr = 0;
@@ -252,3 +253,4 @@ void caam_hal_ctrl_fill_mpmr(vaddr_t ctrl_addr, struct caambuf *msg_mpmr)
 		DMSG("val_scfgr = %#"PRIx32, io_caam_read32(ctrl_addr + SCFGR));
 	}
 }
+#endif /* CFG_NXP_CAAM_MP_DRV */

core/drivers/crypto/caam/include/caam_hal_ctrl.h
@@ -60,6 +60,7 @@ uint8_t caam_hal_ctrl_era(vaddr_t baseaddr);
  */
 void caam_hal_ctrl_inc_priblob(vaddr_t baseaddr);
 
+#ifdef CFG_NXP_CAAM_MP_DRV
 /*
  * Get the SCFGR content and check the MPCURVE fields.
  * The function returns either:
@@ -93,4 +94,5 @@ void caam_hal_ctrl_fill_mpmr(vaddr_t ctrl_addr, struct caambuf *msg_mpmr);
  * @ctrl_addr  Controller base address
  */
 bool caam_hal_ctrl_is_mp_set(vaddr_t ctrl_addr);
+#endif /* CFG_NXP_CAAM_MP_DRV */
 #endif /* __CAAM_HAL_CTRL_H__ */