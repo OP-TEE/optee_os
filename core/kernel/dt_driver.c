linux4sam/optee_os-at91

core: dt_driver: add support for DT_DRIVER_I2C
Integrating I2C support within the dt_driver mechanism require to change
the way controller are retrieved. Indeed, when using i2c, the children are
located under a parent I2C controller node. This implies to use another
method to parse node heriarchy and ignore the case when the provider
has no phandle.

Signed-off-by: Clément Léger <clement.leger@bootlin.com>

core/kernel/dt_driver.c
@@ -108,6 +108,7 @@ static void assert_type_is_valid(enum dt_driver_type type)
 	case DT_DRIVER_UART:
 	case DT_DRIVER_PINCTRL:
 	case DT_DRIVER_GPIO:
+	case DT_DRIVER_I2C:
 		return;
 	default:
 		assert(0);
@@ -190,6 +191,8 @@ int fdt_get_dt_driver_cells(const void *fdt, int nodeoffset,
 	case DT_DRIVER_GPIO:
 		cells_name = "#gpio-cells";
 		break;
+	case DT_DRIVER_I2C:
+		return 0;
 	default:
 		panic();
 	}
@@ -259,6 +262,28 @@ static void *device_from_provider_prop(struct dt_driver_provider *prv,
 	free(pargs);
 
 	return device;
+}
+
+void *dt_driver_device_from_parent(const void *fdt, int nodeoffset,
+				   enum dt_driver_type type, TEE_Result *res)
+{
+	int parent = -1;
+	struct dt_driver_provider *prv = NULL;
+
+	parent = fdt_parent_offset(fdt, nodeoffset);
+	if (parent < 0) {
+		*res =  TEE_ERROR_BAD_FORMAT;
+		return NULL;
+	}
+
+	prv = dt_driver_get_provider_by_node(parent, type);
+	if (!prv) {
+		/* No provider registered yet */
+		*res = TEE_ERROR_DEFER_DRIVER_INIT;
+		return NULL;
+	}
+
+	return device_from_provider_prop(prv, fdt, nodeoffset, NULL, res);
 }
 
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,

OP-TEE/optee_os

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/kernel/dt_driver.c
@@ -106,6 +106,7 @@ static void assert_type_is_valid(enum dt_driver_type type)
 	case DT_DRIVER_CLK:
 	case DT_DRIVER_RSTCTRL:
 	case DT_DRIVER_UART:
+	case DT_DRIVER_I2C:
 		return;
 	default:
 		assert(0);
@@ -172,6 +173,8 @@ int fdt_get_dt_driver_cells(const void *fdt, int nodeoffset,
 	case DT_DRIVER_RSTCTRL:
 		cells_name = "#reset-cells";
 		break;
+	case DT_DRIVER_I2C:
+		return 0;
 	default:
 		panic();
 	}
@@ -241,6 +244,30 @@ static void *device_from_provider_prop(struct dt_driver_provider *prv,
 	free(pargs);
 
 	return device;
+}
+
+void *dt_driver_device_from_parent(const void *fdt, int nodeoffset,
+				   enum dt_driver_type type, TEE_Result *res)
+{
+	int parent = -1;
+	struct dt_driver_provider *prv = NULL;
+
+	assert(fdt == get_secure_dt());
+
+	parent = fdt_parent_offset(fdt, nodeoffset);
+	if (parent < 0) {
+		*res =  TEE_ERROR_GENERIC;
+		return NULL;
+	}
+
+	prv = dt_driver_get_provider_by_node(parent, type);
+	if (!prv) {
+		/* No provider registered yet */
+		*res = TEE_ERROR_DEFER_DRIVER_INIT;
+		return NULL;
+	}
+
+	return device_from_provider_prop(prv, fdt, nodeoffset, NULL, res);
 }
 
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,