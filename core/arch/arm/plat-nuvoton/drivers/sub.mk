007c5e61ff6f9f043df4f6f75d79b87f439c8e9f
rutigl@gmail.com <rutigl@gmail.com>
2023-05-04 16:50:15 +0300

    merged with upstream v 3.21.0

--- core/arch/arm/plat-nuvoton/drivers/sub.mk
+++
@@ -1,2 +0,0 @@
-global-incdirs-y += .
-srcs-$(CFG_WITH_USER_TA) += vendor_props.c
--- core/arch/arm/plat-nuvoton/pm/sub.mk
+++
@@ -1,5 +0,0 @@
-ifeq ($(CFG_CORE_FFA),y)
-ifeq ($(PLATFORM_FLAVOR_npcm845x),y)
-srcs-y += npcm845x_spmc_pm.c
-endif
-endif
