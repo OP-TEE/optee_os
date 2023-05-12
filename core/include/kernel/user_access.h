fd593d4a52f9665d1b26af93b91383b80a48ba5e
Seonghyun Park <seonghp@amazon.com>
2023-05-12 09:29:48 +0200

    core: add more user access functions
    Add more user access functions: clear_user(), strnlen_user() and
    memdup_user(), which can be used to manipulate, check or duplicate
    given user space buffers.
    
    Signed-off-by: Seonghyun Park <seonghp@amazon.com>

--- core/include/kernel/user_access.h
+++ core/include/kernel/user_access.h
@@ -34,6 +34,35 @@ static inline TEE_Result copy_from_user(void *kaddr __unused,
 TEE_Result copy_to_user_private(void *uaddr, const void *kaddr, size_t len);
 TEE_Result copy_to_user(void *uaddr, const void *kaddr, size_t len);
 
+TEE_Result clear_user(void *uaddr, size_t n);
+
+size_t strnlen_user(const void *s, size_t n);
+
+/*
+ * memdup_user() - Duplicate a user-space buffer.
+ * @src:    Pointer to the user buffer to be duplicated.
+ * @len:    Length of the user buffer to be duplicated.
+ * @p:      Holds duplicated kernel buffer on success, or unchanged on failure.
+ *          Note that the returned buffer is allocated by malloc() and
+ *          the caller shall free() the buffer later.
+ * Return TEE_SUCCESS on success.
+ * Return TEE_ERROR_OUT_OF_MEMORY or TEE_ERROR_ACCESS_DENIEND on error.
+ */
+TEE_Result memdup_user(const void *src, size_t len, void **p);
+
+/*
+ * memdup_user_private() - Duplicate a private user-space buffer.
+ * @src:    Pointer to the user buffer to be duplicated. The buffer should
+ *          be private to current TA (i.e., !TEE_MEMORY_ACCESS_ANY_OWNER).
+ * @len:    Length of the user buffer to be duplicated.
+ * @p:      Holds duplicated kernel buffer on success, or unchanged on failure.
+ *          Note that the returned buffer is allocated by malloc() and
+ *          the caller shall free() the buffer later.
+ * Return TEE_SUCCESS on success.
+ * Return TEE_ERROR_OUT_OF_MEMORY or TEE_ERROR_ACCESS_DENIEND on error.
+ */
+TEE_Result memdup_user_private(const void *src, size_t len, void **p);
+
 TEE_Result copy_kaddr_to_uref(uint32_t *uref, void *kaddr);
 
 uint32_t kaddr_to_uref(void *kaddr);
@@ -42,5 +71,26 @@ static inline void *uref_to_kaddr(uint32_t uref)
 {
 	return (void *)uref_to_vaddr(uref);
 }
+
+#define GET_USER_SCALAR(_x, _p) ({					\
+	TEE_Result __res = TEE_SUCCESS;					\
+	__typeof((_x)) __tmp = 0;					\
+									\
+	static_assert(sizeof(_x) == sizeof(*(_p)));			\
+									\
+	__res = copy_from_user(&__tmp, (const void *)_p, sizeof(_x));	\
+	(_x) = (__tmp);							\
+	__res;								\
+})
+
+#define PUT_USER_SCALAR(_x, _p) ({					\
+	TEE_Result __res = TEE_SUCCESS;					\
+	__typeof((_x)) __x = (_x);					\
+									\
+	static_assert(sizeof(_x) == sizeof(*(_p)));			\
+									\
+	__res = copy_to_user((void *)_p, &(__x), sizeof(*(_p)));	\
+	__res;								\
+})
 
 #endif /*__KERNEL_USER_ACCESS_H*/
--- core/kernel/user_access.c
+++ core/kernel/user_access.c
@@ -17,9 +17,22 @@
 static TEE_Result check_access(uint32_t flags, const void *uaddr, size_t len)
 {
 	struct ts_session *s = ts_get_current_session();
+	struct user_mode_ctx *uctx = NULL;
 
-	return vm_check_access_rights(to_user_mode_ctx(s->ctx), flags,
-				      (vaddr_t)uaddr, len);
+	if (!is_user_mode_ctx(s->ctx)) {
+		/*
+		 * check_access() may be called within a PTA session, which
+		 * doesn't have a user_mode_ctx. Here, try to retrieve the
+		 * user_mode_ctx associated with the calling session.
+		 */
+		s = TAILQ_NEXT(s, link_tsd);
+		if (!s || !is_user_mode_ctx(s->ctx))
+			return TEE_ERROR_GENERIC;
+	}
+
+	uctx = to_user_mode_ctx(s->ctx);
+
+	return vm_check_access_rights(uctx, flags, (vaddr_t)uaddr, len);
 }
 
 TEE_Result copy_from_user(void *kaddr, const void *uaddr, size_t len)
@@ -70,6 +83,71 @@ TEE_Result copy_to_user_private(void *uaddr, const void *kaddr, size_t len)
 	res = check_access(flags, uaddr, len);
 	if (!res)
 		memcpy(uaddr, kaddr, len);
+
+	return res;
+}
+
+TEE_Result clear_user(void *uaddr, size_t n)
+{
+	uint32_t flags = TEE_MEMORY_ACCESS_WRITE | TEE_MEMORY_ACCESS_ANY_OWNER;
+	TEE_Result res = TEE_SUCCESS;
+
+	uaddr = memtag_strip_tag(uaddr);
+	res = check_access(flags, uaddr, n);
+	if (res)
+		return res;
+
+	memset(uaddr, 0, n);
+
+	return TEE_SUCCESS;
+}
+
+size_t strnlen_user(const void *uaddr, size_t len)
+{
+	uint32_t flags = TEE_MEMORY_ACCESS_READ | TEE_MEMORY_ACCESS_ANY_OWNER;
+	TEE_Result res = TEE_SUCCESS;
+	size_t n = 0;
+
+	uaddr = memtag_strip_tag_const(uaddr);
+	res = check_access(flags, uaddr, len);
+	if (!res)
+		n = strnlen(uaddr, len);
+
+	return n;
+}
+
+TEE_Result memdup_user(const void *src, size_t len, void **p)
+{
+	TEE_Result res = TEE_SUCCESS;
+	void *buf = NULL;
+
+	buf = malloc(len);
+	if (!buf)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	res = copy_from_user(buf, src, len);
+	if (res)
+		free(buf);
+	else
+		*p = buf;
+
+	return res;
+}
+
+TEE_Result memdup_user_private(const void *src, size_t len, void **p)
+{
+	TEE_Result res = TEE_SUCCESS;
+	void *buf = NULL;
+
+	buf = malloc(len);
+	if (!buf)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	res = copy_from_user_private(buf, src, len);
+	if (res)
+		free(buf);
+	else
+		*p = buf;
 
 	return res;
 }
