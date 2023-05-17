linux4sam/optee_os-at91

plat-sam: enable use of SCMI generic clock support
All clocks for the plat-sam are described using the clk framework.
Enable this option to allow using them with SCMI transparently.

Signed-off-by: Clément Léger <clement.leger@bootlin.com>

core/arch/arm/plat-sam/conf.mk
@@ -45,5 +45,6 @@ CFG_SCMI_MSG_DRIVERS ?= y
 ifeq ($(CFG_SCMI_MSG_DRIVERS),y)
 $(call force,CFG_SCMI_MSG_SMT,y)
 $(call force,CFG_SCMI_MSG_CLOCK,y)
+$(call force,CFG_SCMI_MSG_CLOCK_GENERIC,y)
 $(call force,CFG_SCMI_MSG_SMT_FASTCALL_ENTRY,y)
 endif

OP-TEE/optee_os

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Thomas Perrot <thomas.perrot@bootlin.com>

core/arch/arm/plat-sam/conf.mk
@@ -102,5 +102,6 @@ CFG_SCMI_MSG_DRIVERS ?= y
 ifeq ($(CFG_SCMI_MSG_DRIVERS),y)
 $(call force,CFG_SCMI_MSG_SMT,y)
 $(call force,CFG_SCMI_MSG_CLOCK,y)
+$(call force,CFG_SCMI_MSG_USE_CLK,y)
 $(call force,CFG_SCMI_MSG_SMT_FASTCALL_ENTRY,y)
 endif