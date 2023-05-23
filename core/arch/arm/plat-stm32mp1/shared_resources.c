2a59bfc49135c0fcea1a4aed6825abdbcf53832b
Etienne Carriere <etienne.carriere@foss.st.com>
2023-05-23 11:58:14 +0200

    plat-stm32mp1: remove unused stm32mp_gpio_bank_is_shared()
    Removes platform function stm32mp_gpio_bank_is_shared() that is not
    used.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/arch/arm/plat-stm32mp1/shared_resources.c
+++ core/arch/arm/plat-stm32mp1/shared_resources.c
@@ -433,23 +433,6 @@ bool stm32mp_periph_is_secure(enum stm32mp_shres id)
 	return shres_state[id] == SHRES_SECURE;
 }
 
-bool stm32mp_gpio_bank_is_shared(unsigned int bank)
-{
-	unsigned int not_secure = 0;
-	unsigned int pin = 0;
-
-	lock_registering();
-
-	if (bank != GPIO_BANK_Z)
-		return false;
-
-	for (pin = 0; pin < get_gpioz_nbpin(); pin++)
-		if (!stm32mp_periph_is_secure(STM32MP1_SHRES_GPIOZ(pin)))
-			not_secure++;
-
-	return not_secure > 0 && not_secure < get_gpioz_nbpin();
-}
-
 bool stm32mp_gpio_bank_is_non_secure(unsigned int bank)
 {
 	unsigned int not_secure = 0;
