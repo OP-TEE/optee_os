bb053cc1afab01d5b2bce7e1ef23a602aacfdd7c
Etienne Carriere <etienne.carriere@foss.st.com>
2023-06-01 19:24:46 +0200

    core: interrupt: remove old API functions
    Remove old itr_xxx() API functions.
    
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/include/kernel/interrupt.h
+++ core/include/kernel/interrupt.h
@@ -158,46 +158,12 @@ static inline int dt_get_irq(const void *fdt, int node)
 }
 #endif
 
-struct itr_handler *itr_alloc_add_type_prio(size_t it, itr_handler_t handler,
-					    uint32_t flags, void *data,
-					    uint32_t type, uint32_t prio);
-void itr_free(struct itr_handler *hdl);
-void itr_add_type_prio(struct itr_handler *handler, uint32_t type,
-		       uint32_t prio);
-void itr_enable(size_t it);
-void itr_disable(size_t it);
-/* raise the Peripheral Interrupt corresponding to the interrupt ID */
-void itr_raise_pi(size_t it);
-/*
- * raise the Software Generated Interrupt corresponding to the interrupt ID,
- * the cpu_mask represents which cpu interface to forward.
- */
-void itr_raise_sgi(size_t it, uint8_t cpu_mask);
-/*
- * let corresponding interrupt forward to the cpu interface
- * according to the cpu_mask.
- */
-void itr_set_affinity(size_t it, uint8_t cpu_mask);
-
 /*
  * __weak overridable function which is called when a secure interrupt is
  * received. The default function calls panic() immediately, platforms which
  * expects to receive secure interrupts should override this function.
  */
 void interrupt_main_handler(void);
-
-static inline void itr_add(struct itr_handler *handler)
-{
-	itr_add_type_prio(handler, IRQ_TYPE_NONE, 0);
-}
-
-static inline struct itr_handler *itr_alloc_add(size_t it,
-						itr_handler_t handler,
-						uint32_t flags, void *data)
-{
-	return itr_alloc_add_type_prio(it, handler, flags, data, IRQ_TYPE_NONE,
-				       0);
-}
 
 /*
  * Interrupt controller chip API functions
