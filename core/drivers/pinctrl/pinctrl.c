OP-TEE/optee_os

commit 9aec039ec0d76c2e6a496e18cf79340658013425

drivers: pinctrl: add pinctrl support
Add support for pinctrl support using device-tree. The device-tree
"pinctrl-<x>" and "pinctrl-names" properties are supported and
allows to apply a pinctrl configuration based on this. This support
also includes a way to register pin muxing controllers that can apply
these states.
A few properties of the pinctrl nodes are supported such as
"bias-disable", "bias-pull-up" and "bias-pull-down".

Signed-off-by: Clément Léger <clement.leger@bootlin.com>
Signed-off-by: Thomas Perrot <thomas.perrot@bootlin.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/pinctrl/pinctrl.c
@@ -1,0 +1,132 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2023, Microchip
+ */
+
+#include <assert.h>
+#include <drivers/pinctrl.h>
+#include <libfdt.h>
+#include <stdio.h>
+#include <tee_api_defines.h>
+#include <tee_api_defines_extensions.h>
+#include <tee_api_types.h>
+#include <util.h>
+
+static const char * const pin_modes[PINCTRL_DT_PROP_MAX] = {
+	[PINCTRL_DT_PROP_BIAS_DISABLE] = "bias-disable",
+	[PINCTRL_DT_PROP_BIAS_PULL_UP] = "bias-pull-up",
+	[PINCTRL_DT_PROP_BIAS_PULL_DOWN] = "bias-pull-down",
+};
+
+TEE_Result pinctrl_parse_dt_pin_modes(const void *fdt, int node,
+				      bitstr_t **modes)
+{
+	unsigned int i = 0;
+	bitstr_t *modes_ptr = NULL;
+
+	modes_ptr = bit_alloc(PINCTRL_DT_PROP_MAX);
+	if (!modes_ptr)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	for (i = 0; i < ARRAY_SIZE(pin_modes); i++)
+		if (fdt_getprop(fdt, node, pin_modes[i], NULL))
+			bit_set(modes_ptr, i);
+
+	*modes = modes_ptr;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result pinctrl_apply_state(struct pinctrl_state *state)
+{
+	unsigned int i = 0;
+	struct pinconf *conf = NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	for (i = 0; i < state->conf_count; i++) {
+		conf = state->confs[i];
+
+		res = conf->ops->conf_apply(conf);
+		if (res) {
+			EMSG("Failed to apply pin conf");
+			return res;
+		}
+	}
+
+	return TEE_SUCCESS;
+}
+
+void pinctrl_free_state(struct pinctrl_state *state)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < state->conf_count; i++)
+		state->confs[i]->ops->conf_free(state->confs[i]);
+
+	free(state);
+}
+
+TEE_Result pinctrl_get_state_by_idx(const void *fdt, int nodeoffset,
+				    unsigned int pinctrl_index,
+				    struct pinctrl_state **state_ret)
+{
+	int bw = 0;
+	unsigned int conf_id = 0;
+	const uint32_t *prop = NULL;
+	unsigned int conf_count = 0;
+	/* Enough char to hold "pinctrl-<max_int>" */
+	char prop_name[8 + 20 + 1] = { };
+	struct pinctrl_state *state = NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	bw = snprintf(prop_name, sizeof(prop_name), "pinctrl-%d",
+		      pinctrl_index);
+	if (bw >= (int)sizeof(prop_name))
+		return TEE_ERROR_OVERFLOW;
+
+	prop = fdt_getprop(fdt, nodeoffset, prop_name, (int *)&conf_count);
+	if (!prop)
+		return TEE_ERROR_ITEM_NOT_FOUND;
+
+	conf_count /= sizeof(uint32_t);
+	state = calloc(1, sizeof(struct pinctrl_state) +
+			  conf_count * sizeof(struct pinconf *));
+	if (!state)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	state->conf_count = conf_count;
+	for (conf_id = 0; conf_id < conf_count; conf_id++) {
+		state->confs[conf_id] =
+			dt_driver_device_from_node_idx_prop(prop_name, fdt,
+							    nodeoffset, conf_id,
+							    DT_DRIVER_PINCTRL,
+							    &res);
+		if (res) {
+			free(state);
+			return res;
+		}
+	}
+
+	*state_ret = state;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result pinctrl_get_state_by_name(const void *fdt, int nodeoffset,
+				     const char *name,
+				     struct pinctrl_state **state)
+{
+	int pinctrl_index = 0;
+
+	if (!name)
+		name = "default";
+
+	pinctrl_index = fdt_stringlist_search(fdt, nodeoffset, "pinctrl-names",
+					      name);
+	if (pinctrl_index < 0) {
+		*state = NULL;
+		return TEE_ERROR_GENERIC;
+	}
+
+	return pinctrl_get_state_by_idx(fdt, nodeoffset, pinctrl_index, state);
+}

core/drivers/pinctrl/sub.mk
@@ -1,0 +1,1 @@
+srcs-$(CFG_DRIVERS_PINCTRL) += pinctrl.c

core/drivers/sub.mk
@@ -82,6 +82,7 @@ subdirs-$(CFG_BNXT_FW) += bnxt
 subdirs-$(CFG_DRIVERS_CLK) += clk
 subdirs-$(CFG_DRIVERS_GPIO) += gpio
 subdirs-$(CFG_DRIVERS_I2C) += i2c
+subdirs-$(CFG_DRIVERS_PINCTRL) += pinctrl
 subdirs-$(CFG_DRIVERS_RSTCTRL) += rstctrl
 subdirs-$(CFG_SCMI_MSG_DRIVERS) += scmi-msg
 subdirs-y += imx

core/include/drivers/pinctrl.h
@@ -1,0 +1,176 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2022-2023, Microchip
+ */
+
+#ifndef __DRIVERS_PINCTRL_H
+#define __DRIVERS_PINCTRL_H
+
+#include <bitstring.h>
+#include <kernel/dt_driver.h>
+#include <sys/queue.h>
+#include <tee_api_types.h>
+
+enum pinctrl_dt_prop {
+	/* Property "bias-disable" found in pinctrl node */
+	PINCTRL_DT_PROP_BIAS_DISABLE,
+	/* Property "bias-pull-up" found in pinctrl node */
+	PINCTRL_DT_PROP_BIAS_PULL_UP,
+	/* Property "bias-pull-down" found in pinctrl node */
+	PINCTRL_DT_PROP_BIAS_PULL_DOWN,
+	/* Terminal ID */
+	PINCTRL_DT_PROP_MAX
+};
+
+/*
+ * struct pinconf - Pinctrl device
+ * @ops: Operation handlers
+ * @priv: Pinctrl driver private data
+ */
+struct pinconf {
+	const struct pinctrl_ops *ops;
+	void *priv;
+};
+
+/*
+ * struct pinctrl_state - Pinctrl configuration state
+ * @conf_count: Number of cells in @confs
+ * @confs: Array of pin configurations related to the pinctrl config state
+ */
+struct pinctrl_state {
+	unsigned int conf_count;
+	struct pinconf *confs[];
+};
+
+struct pinctrl_ops {
+	/* Apply a pinctrl configuration */
+	TEE_Result (*conf_apply)(struct pinconf *conf);
+	/* Release resources allocated for a pinctrl configuration */
+	void (*conf_free)(struct pinconf *conf);
+};
+
+typedef struct pinconf *(*pinctrl_dt_get_func)(struct dt_driver_phandle_args *a,
+					       void *data, TEE_Result *res);
+
+#ifdef CFG_DRIVERS_PINCTRL
+/**
+ * pinctrl_dt_register_provider - Register a pinctrl controller provider
+ *
+ * @fdt: Device tree to work on
+ * @nodeoffset: Node offset of the pin controller
+ * @get_pinctrl: Callback to match the pin controller with a struct pinconf
+ * @data: Data which will be passed to the get_pinctrl callback
+ * Return a TEE_Result compliant value
+ */
+static inline
+TEE_Result pinctrl_register_provider(const void *fdt, int nodeoffset,
+				     pinctrl_dt_get_func get_pinctrl,
+				     void *data)
+{
+	return dt_driver_register_provider(fdt, nodeoffset,
+					   (get_of_device_func)get_pinctrl,
+					   data, DT_DRIVER_PINCTRL);
+}
+
+/**
+ * pinctrl_get_state_by_name - Obtain a pinctrl state by name
+ *
+ * @fdt: Device tree to work on
+ * @nodeoffset: Node offset of the pin controller
+ * @name: name of the pinctrl state to obtain from device-tree
+ * @state: Pointer filled with the retrieved state, must be freed after use
+   using pinctrl_free_state()
+ * Return a TEE_Result compliant value
+ */
+TEE_Result pinctrl_get_state_by_name(const void *fdt, int nodeoffset,
+				     const char *name,
+				     struct pinctrl_state **state);
+
+/**
+ * pinctrl_get_state_by_idx - Obtain a pinctrl state by index
+ *
+ * @fdt: Device tree to work on
+ * @nodeoffset: Node offset of the pin controller
+ * @pinctrl_id: Index of the pinctrl state to obtain from device-tree
+ * @state: Pointer filled with the retrieved state, must be freed after use
+   using pinctrl_free_state()
+ * Return a TEE_Result compliant value
+ */
+TEE_Result pinctrl_get_state_by_idx(const void *fdt, int nodeoffset,
+				    unsigned int pinctrl_id,
+				    struct pinctrl_state **state);
+
+/**
+ * pinctrl_free_state - Free a pinctrl state that was previously obtained
+ *
+ * @state: State to be freed
+ */
+void pinctrl_free_state(struct pinctrl_state *state);
+
+/**
+ * pinctrl_apply_state - apply a pinctrl state
+ *
+ * @state: State to be applied
+ * Return a TEE_Result compliant value
+ */
+TEE_Result pinctrl_apply_state(struct pinctrl_state *state);
+
+/*
+ * pinctrl_parse_dt_pin_modes - Parse DT node properties
+ * @fdt: Device tree to work on
+ * @nodeoffset: Pinctrl node
+ * @modes: Output allocated regulator properties
+ * Return a TEE_Result compliant value
+ */
+TEE_Result pinctrl_parse_dt_pin_modes(const void *fdt, int nodeoffset,
+				      bitstr_t **modes);
+#else /* CFG_DRIVERS_PINCTRL */
+static inline
+TEE_Result pinctrl_register_provider(const void *fdt __unused,
+				     int nodeoffset __unused,
+				     pinctrl_dt_get_func get_pinctrl __unused,
+				     void *data __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+static inline
+TEE_Result pinctrl_get_state_by_name(const void *fdt __unused,
+				     int nodeoffset __unused,
+				     const char *name __unused,
+				     struct pinctrl_state **state __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+static inline
+TEE_Result pinctrl_get_state_by_idx(const void *fdt __unused,
+				    int nodeoffset __unused,
+				    unsigned int pinctrl_id __unused,
+				    struct pinctrl_state **state __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+static inline
+void pinctrl_free_state(struct pinctrl_state *state __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+static inline
+TEE_Result pinctrl_apply_state(struct pinctrl_state *state __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+static inline
+TEE_Result pinctrl_parse_dt_pin_modes(const void *fdt __unused,
+				      int nodeoffset __unused,
+				      bitstr_t **modes __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+#endif /* CFG_DRIVERS_PINCTRL */
+#endif /* __DRIVERS_PINCTRL_H */

mk/config.mk
@@ -889,6 +889,11 @@ CFG_DRIVERS_GPIO ?= n
 # When enabled, CFG_DRIVERS_I2C provides I2C controller and devices support.
 CFG_DRIVERS_I2C ?= n
 
+# When enabled, CFG_DRIVERS_PINCTRL embeds a pin muxing controller framework in
+# OP-TEE core to provide drivers a way to apply pin muxing configurations based
+# on device-tree.
+CFG_DRIVERS_PINCTRL ?= n
+
 # The purpose of this flag is to show a print when booting up the device that
 # indicates whether the board runs a standard developer configuration or not.
 # A developer configuration doesn't necessarily has to be secure. The intention

commit c3a1dfd49a6bf2b5f214aa0491d454f61c7f1652

drivers: pinctrl: pinctrl name may not be found
Changes pinctrl_get_state_by_name() to return TEE_ERROR_ITEM_NOT_FOUND
instead of TEE_ERROR_GENERIC when an pinctrl state index is not found
in the DT.

Fixes: 9aec039ec0d7 ("drivers: pinctrl: add pinctrl support")
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/pinctrl/pinctrl.c
@@ -125,7 +125,10 @@ TEE_Result pinctrl_get_state_by_name(const void *fdt, int nodeoffset,
 					      name);
 	if (pinctrl_index < 0) {
 		*state = NULL;
-		return TEE_ERROR_GENERIC;
+		if (pinctrl_index == -FDT_ERR_NOTFOUND)
+			return TEE_ERROR_ITEM_NOT_FOUND;
+		else
+			return TEE_ERROR_GENERIC;
 	}
 
 	return pinctrl_get_state_by_idx(fdt, nodeoffset, pinctrl_index, state);