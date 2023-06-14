OP-TEE/optee_os

commit 1c81e5f9458a49d24dd323d867ebb7b02826cf05

drivers: stm32_gpio: temporary GPIO configuration for STM32MP13
This temporary change set all configured GPIOs to non-secure state
on STM32MP13 platform. This change is needed while we align all
components.

Changes I2C driver to assign I2C pins to secure world for STM32MP13.

Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/stm32_i2c.c
@@ -833,6 +833,10 @@ int stm32_i2c_init(struct i2c_handle_s *hi2c,
 	if (rc)
 		DMSG("I2C analog filter error %d", rc);
 
+	if (IS_ENABLED(CFG_STM32MP13))
+		stm32_gpio_set_secure_cfg(hi2c->pinctrl->bank,
+					  hi2c->pinctrl->pin, true);
+
 	clk_disable(hi2c->clock);
 
 	return rc;
@@ -1525,6 +1529,10 @@ void stm32_i2c_resume(struct i2c_handle_s *hi2c)
 	}
 
 	restore_cfg(hi2c, &hi2c->sec_cfg);
+
+	if (IS_ENABLED(CFG_STM32MP13))
+		stm32_gpio_set_secure_cfg(hi2c->pinctrl->bank,
+					  hi2c->pinctrl->pin, true);
 
 	hi2c->i2c_state = I2C_STATE_READY;
 }

core/arch/arm/plat-stm32mp1/main.c
@@ -251,6 +251,41 @@ static TEE_Result set_etzpc_secure_configuration(void)
 
 driver_init_late(set_etzpc_secure_configuration);
 #endif /* CFG_STM32_ETZPC */
+
+#ifdef CFG_STM32_GPIO
+
+#define NB_PINS_PER_BANK		U(16)
+#define NB_PINS_BANK_H			U(14)
+#define NB_PINS_BANK_I			U(8)
+
+static TEE_Result set_all_gpios_non_secure(void)
+{
+	unsigned int bank = 0;
+	unsigned int pin = 0;
+	unsigned int nb_pin_bank = 0;
+
+	for (bank = 0; bank <= GPIO_BANK_I; bank++) {
+		switch (bank) {
+		case GPIO_BANK_H:
+			nb_pin_bank = NB_PINS_BANK_H;
+			break;
+		case GPIO_BANK_I:
+			nb_pin_bank = NB_PINS_BANK_I;
+			break;
+		default:
+			nb_pin_bank = NB_PINS_PER_BANK;
+			break;
+		}
+
+		for (pin = 0; pin <= nb_pin_bank; pin++)
+			stm32_gpio_set_secure_cfg(bank, pin, false);
+	}
+
+	return TEE_SUCCESS;
+}
+
+early_init_late(set_all_gpios_non_secure);
+#endif /* CFG_STM32_GPIO */
 #endif /* CFG_STM32MP13 */
 
 static TEE_Result init_stm32mp1_drivers(void)

commit c4435b03813fe0c36242d8a17e758b43bc8c6556

drivers: stm32_i2c: fix pin secure configuration for STM32MP13
Fixes the implementation for configuring I2C pins as secure
for STM32MP13. The implementation must consider the number of pins
in the related pinctrl instance.

Fixes: 1c81e5f9458a ("drivers: stm32_gpio: temporary GPIO configuration for STM32MP13")
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/stm32_i2c.c
@@ -834,9 +834,13 @@ int stm32_i2c_init(struct i2c_handle_s *hi2c,
 	if (rc)
 		DMSG("I2C analog filter error %d", rc);
 
-	if (IS_ENABLED(CFG_STM32MP13))
-		stm32_gpio_set_secure_cfg(hi2c->pinctrl->bank,
-					  hi2c->pinctrl->pin, true);
+	if (IS_ENABLED(CFG_STM32MP13)) {
+		size_t n = 0;
+
+		for (n = 0; n < hi2c->pinctrl_count; n++)
+			stm32_gpio_set_secure_cfg(hi2c->pinctrl[n].bank,
+						  hi2c->pinctrl[n].pin, true);
+	}
 
 	clk_disable(hi2c->clock);
 
@@ -1536,9 +1540,13 @@ void stm32_i2c_resume(struct i2c_handle_s *hi2c)
 
 	restore_cfg(hi2c, &hi2c->sec_cfg);
 
-	if (IS_ENABLED(CFG_STM32MP13))
-		stm32_gpio_set_secure_cfg(hi2c->pinctrl->bank,
-					  hi2c->pinctrl->pin, true);
+	if (IS_ENABLED(CFG_STM32MP13)) {
+		size_t n = 0;
+
+		for (n = 0; n < hi2c->pinctrl_count; n++)
+			stm32_gpio_set_secure_cfg(hi2c->pinctrl[n].bank,
+						  hi2c->pinctrl[n].pin, true);
+	}
 
 	hi2c->i2c_state = I2C_STATE_READY;
 }