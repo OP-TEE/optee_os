OP-TEE/optee_os

commit 89ba3422ee80a098147c09abcd26b695fe9cee4c

plat-stm32mp1: scmi_server: default use OP-TEE shared memory
Adds configuration switch CFG_STM32MP1_SCMI_SHM_SYSRAM that is default
disabled. When disabled, CFG_STM32MP1_SCMI_SHM_BASE defaults to 0
which means OP-TEE SMCI server uses OP-TEE native shared memory
registered by clients. When CFG_STM32MP1_SCMI_SHM_SYSRAM is enabled
CFG_STM32MP1_SCMI_SHM_BASE is force the base address of the SYRAM
last 4KByte page.

Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-stm32mp1/conf.mk
@@ -155,8 +155,17 @@ CFG_DRAM_SIZE    ?= 0x40000000
 # device memory mapped SRAM used for SCMI message transfers.
 # When CFG_STM32MP1_SCMI_SHM_BASE is set to 0, the platform uses OP-TEE
 # native shared memory for SCMI communication instead of SRAM.
-CFG_STM32MP1_SCMI_SHM_BASE ?= 0x2ffff000
-CFG_STM32MP1_SCMI_SHM_SIZE ?= 0x00001000
+#
+# When CFG_STM32MP1_SCMI_SHM_SYSRAM is enabled, OP-TEE uses the
+# last 4KB page of SYSRAM as SCMI shared memory. The switch is default
+# disabled.
+CFG_STM32MP1_SCMI_SHM_SYSRAM ?= n
+ifeq ($(CFG_STM32MP1_SCMI_SHM_SYSRAM),y)
+$(call force,CFG_STM32MP1_SCMI_SHM_BASE,0x2ffff000)
+else
+CFG_STM32MP1_SCMI_SHM_BASE ?= 0
+endif
+$(call force,CFG_STM32MP1_SCMI_SHM_SIZE,0x1000)
 
 ifeq ($(CFG_STM32MP15),y)
 CFG_TZDRAM_START ?= 0xfe000000

commit 4f4f037fcdd4b9cfef4d7986b241c1d13d93e495

plat-stm32mp1: restore SYSRAM for SCMI message on STM32MP13
Restores use of SYSRAM last page for STM32MP13 for SCMI communication
as U-Boot and Linux kernel device trees are not yet updated to use OP-TEE
native shared memory instead.

Fixes: 89ba3422ee80 ("plat-stm32mp1: scmi_server: default use OP-TEE shared memory")
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-stm32mp1/conf.mk
@@ -113,6 +113,7 @@ $(call force,CFG_TEE_CORE_NB_CORE,1)
 $(call force,CFG_WITH_NSEC_GPIOS,n)
 CFG_EXTERNAL_DT ?= n
 CFG_STM32MP_OPP_COUNT ?= 2
+CFG_STM32MP1_SCMI_SHM_SYSRAM ?= y
 CFG_WITH_PAGER ?= n
 endif # CFG_STM32MP13
 