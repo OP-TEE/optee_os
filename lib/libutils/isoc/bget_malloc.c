OP-TEE/optee_os

commit 12d739bd5028a2d420512ea2c43f0fa1cebb9da1

libutils: use raw_malloc_*() as more primitive bget wrappers
The malloc implementation is layered on top of BGET, where the
raw_malloc_*() layer is the most primitive layer on top of BGET.  This
layer is unlocked so it's up to the caller to handle eventual
synchronization.

Until now this rule hasn't been followed completely by letting
raw_malloc_get_stats() call gen_malloc_get_stats() and not the other way
around. So fix this and also provide raw_malloc_buffer_overlaps_heap()
and raw_malloc_buffer_is_within_alloced().

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/isoc/bget_malloc.c
@@ -311,8 +311,7 @@ static void gen_malloc_get_stats(struct malloc_ctx *ctx,
 {
 	uint32_t exceptions = malloc_lock(ctx);
 
-	memcpy_unchecked(stats, &ctx->mstats, sizeof(*stats));
-	stats->allocated = ctx->poolset.totalloc;
+	raw_malloc_get_stats(ctx, stats);
 	malloc_unlock(ctx, exceptions);
 }
 
@@ -865,75 +864,19 @@ void free_wipe(void *ptr)
 
 static void gen_malloc_add_pool(struct malloc_ctx *ctx, void *buf, size_t len)
 {
-	void *p;
-	size_t l;
-	uint32_t exceptions;
-	uintptr_t start = (uintptr_t)buf;
-	uintptr_t end = start + len;
-	const size_t min_len = sizeof(struct bhead) + sizeof(struct bfhead);
+	uint32_t exceptions = malloc_lock(ctx);
 
-	start = ROUNDUP(start, SizeQuant);
-	end = ROUNDDOWN(end, SizeQuant);
-
-	if (start > end || (end - start) < min_len) {
-		DMSG("Skipping too small pool");
-		return;
-	}
-
-	/* First pool requires a bigger size */
-	if (!ctx->pool_len && (end - start) < MALLOC_INITIAL_POOL_MIN_SIZE) {
-		DMSG("Skipping too small initial pool");
-		return;
-	}
-
-	exceptions = malloc_lock(ctx);
-
-	tag_asan_free((void *)start, end - start);
-	bpool((void *)start, end - start, &ctx->poolset);
-	l = ctx->pool_len + 1;
-	p = realloc_unlocked(ctx, ctx->pool, sizeof(struct malloc_pool) * l);
-	assert(p);
-	ctx->pool = p;
-	ctx->pool[ctx->pool_len].buf = (void *)start;
-	ctx->pool[ctx->pool_len].len = end - start;
-#ifdef BufStats
-	ctx->mstats.size += ctx->pool[ctx->pool_len].len;
-#endif
-	ctx->pool_len = l;
+	raw_malloc_add_pool(ctx, buf, len);
 	malloc_unlock(ctx, exceptions);
 }
 
 static bool gen_malloc_buffer_is_within_alloced(struct malloc_ctx *ctx,
 						void *buf, size_t len)
 {
-	struct bpool_iterator itr;
-	void *b;
-	uint8_t *start_buf = strip_tag(buf);
-	uint8_t *end_buf = start_buf + len;
-	bool ret = false;
 	uint32_t exceptions = malloc_lock(ctx);
+	bool ret = false;
 
-	raw_malloc_validate_pools(ctx);
-
-	/* Check for wrapping */
-	if (start_buf > end_buf)
-		goto out;
-
-	BPOOL_FOREACH(ctx, &itr, &b) {
-		uint8_t *start_b;
-		uint8_t *end_b;
-		size_t s;
-
-		start_b = get_payload_start_size(b, &s);
-		end_b = start_b + s;
-
-		if (start_buf >= start_b && end_buf <= end_b) {
-			ret = true;
-			goto out;
-		}
-	}
-
-out:
+	ret = raw_malloc_buffer_is_within_alloced(ctx, buf, len);
 	malloc_unlock(ctx, exceptions);
 
 	return ret;
@@ -942,31 +885,10 @@ out:
 static bool gen_malloc_buffer_overlaps_heap(struct malloc_ctx *ctx,
 					    void *buf, size_t len)
 {
-	uintptr_t buf_start = (uintptr_t) buf;
-	uintptr_t buf_end = buf_start + len;
-	size_t n;
 	bool ret = false;
 	uint32_t exceptions = malloc_lock(ctx);
 
-	raw_malloc_validate_pools(ctx);
-
-	for (n = 0; n < ctx->pool_len; n++) {
-		uintptr_t pool_start = (uintptr_t)ctx->pool[n].buf;
-		uintptr_t pool_end = pool_start + ctx->pool[n].len;
-
-		if (buf_start > buf_end || pool_start > pool_end) {
-			ret = true;	/* Wrapping buffers, shouldn't happen */
-			goto out;
-		}
-
-		if ((buf_start >= pool_start && buf_start < pool_end) ||
-		    (buf_end >= pool_start && buf_end < pool_end)) {
-			ret = true;
-			goto out;
-		}
-	}
-
-out:
+	ret = raw_malloc_buffer_overlaps_heap(ctx, buf, len);
 	malloc_unlock(ctx, exceptions);
 	return ret;
 }
@@ -985,13 +907,97 @@ void raw_malloc_init_ctx(struct malloc_ctx *ctx)
 
 void raw_malloc_add_pool(struct malloc_ctx *ctx, void *buf, size_t len)
 {
-	gen_malloc_add_pool(ctx, buf, len);
+	const size_t min_len = sizeof(struct bhead) + sizeof(struct bfhead);
+	uintptr_t start = (uintptr_t)buf;
+	uintptr_t end = start + len;
+	void *p = NULL;
+	size_t l = 0;
+
+	start = ROUNDUP(start, SizeQuant);
+	end = ROUNDDOWN(end, SizeQuant);
+
+	if (start > end || (end - start) < min_len) {
+		DMSG("Skipping too small pool");
+		return;
+	}
+
+	/* First pool requires a bigger size */
+	if (!ctx->pool_len && (end - start) < MALLOC_INITIAL_POOL_MIN_SIZE) {
+		DMSG("Skipping too small initial pool");
+		return;
+	}
+
+	tag_asan_free((void *)start, end - start);
+	bpool((void *)start, end - start, &ctx->poolset);
+	l = ctx->pool_len + 1;
+	p = realloc_unlocked(ctx, ctx->pool, sizeof(struct malloc_pool) * l);
+	assert(p);
+	ctx->pool = p;
+	ctx->pool[ctx->pool_len].buf = (void *)start;
+	ctx->pool[ctx->pool_len].len = end - start;
+#ifdef BufStats
+	ctx->mstats.size += ctx->pool[ctx->pool_len].len;
+#endif
+	ctx->pool_len = l;
+}
+
+bool raw_malloc_buffer_overlaps_heap(struct malloc_ctx *ctx,
+				     void *buf, size_t len)
+{
+	uintptr_t buf_start = (uintptr_t) buf;
+	uintptr_t buf_end = buf_start + len;
+	size_t n = 0;
+
+	raw_malloc_validate_pools(ctx);
+
+	for (n = 0; n < ctx->pool_len; n++) {
+		uintptr_t pool_start = (uintptr_t)ctx->pool[n].buf;
+		uintptr_t pool_end = pool_start + ctx->pool[n].len;
+
+		if (buf_start > buf_end || pool_start > pool_end)
+			return true;	/* Wrapping buffers, shouldn't happen */
+
+		if ((buf_start >= pool_start && buf_start < pool_end) ||
+		    (buf_end >= pool_start && buf_end < pool_end))
+			return true;
+	}
+
+	return false;
+}
+
+bool raw_malloc_buffer_is_within_alloced(struct malloc_ctx *ctx,
+					 void *buf, size_t len)
+{
+	struct bpool_iterator itr = { };
+	void *b = NULL;
+	uint8_t *start_buf = strip_tag(buf);
+	uint8_t *end_buf = start_buf + len;
+
+	raw_malloc_validate_pools(ctx);
+
+	/* Check for wrapping */
+	if (start_buf > end_buf)
+		return false;
+
+	BPOOL_FOREACH(ctx, &itr, &b) {
+		uint8_t *start_b = NULL;
+		uint8_t *end_b = NULL;
+		size_t s = 0;
+
+		start_b = get_payload_start_size(b, &s);
+		end_b = start_b + s;
+		if (start_buf >= start_b && end_buf <= end_b)
+			return true;
+	}
+
+	return false;
 }
 
 #ifdef CFG_WITH_STATS
 void raw_malloc_get_stats(struct malloc_ctx *ctx, struct malloc_stats *stats)
 {
-	gen_malloc_get_stats(ctx, stats);
+	memcpy_unchecked(stats, &ctx->mstats, sizeof(*stats));
+	stats->allocated = ctx->poolset.totalloc;
 }
 #endif
 

lib/libutils/isoc/include/malloc.h
@@ -169,6 +169,10 @@ void *raw_realloc(void *ptr, size_t hdr_size, size_t ftr_size,
 size_t raw_malloc_get_ctx_size(void);
 void raw_malloc_init_ctx(struct malloc_ctx *ctx);
 void raw_malloc_add_pool(struct malloc_ctx *ctx, void *buf, size_t len);
+bool raw_malloc_buffer_overlaps_heap(struct malloc_ctx *ctx,
+				     void *buf, size_t len);
+bool raw_malloc_buffer_is_within_alloced(struct malloc_ctx *ctx,
+					 void *buf, size_t len);
 #ifdef CFG_WITH_STATS
 void raw_malloc_get_stats(struct malloc_ctx *ctx, struct malloc_stats *stats);
 #endif

commit deb6c7387f35b545847ba04b580cc703f09ce4a9

libutils: malloc: fix raw_malloc_buffer_overlaps_heap()
When checking if there's an overlap between allocated buffer and
heap, raw_malloc_buffer_overlaps_heap() considers two cases: when
buffer comes before the pool and the opposite. On the first case,
overlap will happen if the buffer end after the loop start.
Since buf_end is computed as buf_start + len, it will point to
the address of the first byte after the memory region allocated to
the buffer.

Fix raw_malloc_buffer_overlaps_heap() by considering overlap only
when buffer end is bigger than the pool start.

Fixes: 12d739bd5028 ("libutils: use raw_malloc_*() as more primitive bget wrappers")
Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

lib/libutils/isoc/bget_malloc.c
@@ -960,7 +960,7 @@ bool raw_malloc_buffer_overlaps_heap(struct malloc_ctx *ctx,
 			return true;	/* Wrapping buffers, shouldn't happen */
 
 		if ((buf_start >= pool_start && buf_start < pool_end) ||
-		    (buf_end >= pool_start && buf_end < pool_end))
+		    (buf_end > pool_start && buf_end < pool_end))
 			return true;
 	}
 