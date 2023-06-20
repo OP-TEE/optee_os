994c8602d9c9e0f0b3f5bdded8b52a6596b96a65
Alvin Chang <alvinga@andestech.com>
2023-06-20 11:02:19 +0800

    core: riscv: Implement timer related functions for ftrace support
    Implement barrier_read_counter_timer() to read the timer value after a
    barrier. Implement read_cntfrq() to get the frequency of machine timer
    counter. The read_time() is moved from header to C source file to reduce
    the code size.
    
    Signed-off-by: Alvin Chang <alvinga@andestech.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Sumit Garg <sumit.garg@linaro.org>
    Reviewed-by: Marouene Boubakri <marouene.boubakri@nxp.com>

--- core/arch/riscv/kernel/tee_time_rdtime.c
+++ core/arch/riscv/kernel/tee_time_rdtime.c
@@ -5,14 +5,40 @@
 
 #include <kernel/clint.h>
 #include <kernel/tee_time.h>
-#include <kernel/time.h>
 #include <kernel/time_source.h>
+#include <riscv.h>
 #include <utee_defines.h>
+
+__noprof uint64_t read_time(void)
+{
+	uint64_t time = 0;
+	uint32_t hi __maybe_unused = 0;
+	uint32_t lo __maybe_unused = 0;
+
+#ifdef CFG_RISCV_M_MODE
+	time = clint_get_mtime();
+#endif /*CFG_RISCV_M_MODE*/
+
+#ifdef CFG_RISCV_S_MODE
+#ifdef RV32
+	do {
+		hi = read_csr(timeh);
+		lo = read_csr(time);
+	} while (hi != read_csr(timeh));
+
+	time = SHIFT_U64(hi, 32) | lo;
+#else /*RV64*/
+	time = rdtime();
+#endif /*RV32*/
+#endif /*CFG_RISCV_S_MODE*/
+
+	return time;
+}
 
 static TEE_Result riscv_get_sys_time(TEE_Time *time)
 {
 	uint64_t tm = read_time();
-	uint64_t rate = CFG_RISCV_MTIME_RATE;
+	uint64_t rate = read_cntfrq();
 
 	time->seconds = tm / rate;
 	time->millis = (tm % rate) / (rate / TEE_TIME_MILLIS_BASE);
