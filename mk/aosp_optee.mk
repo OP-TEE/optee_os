OP-TEE/optee_os

commit d1b003febbcda5d8627ad8f1da76de905cdda47a

mk: Support user static lib for aosp build
The aosp build environment don't allow writing to source folder, move
the $(libname).a in current folder to $(out-link-dir$(sm)). And since
static libraries are not needed for runtime, set the LOCAL_MODULE_CLASS
to STATIC_LIBRARIES to avoid installing by aosp build.

Signed-off-by: Tadd Kao <tadd.kao@mediatek.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

mk/aosp_optee.mk
@@ -106,7 +106,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := $(local_module)
 LOCAL_PREBUILT_MODULE_FILE := $(OPTEE_TA_OUT_DIR)/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/optee_armtz
+ifneq ($(filter %.ta, $(local_module)),)
 LOCAL_MODULE_CLASS := EXECUTABLES
+else
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+endif
 LOCAL_MODULE_TAGS := optional
 
 TA_TMP_DIR := $(subst /,_,$(LOCAL_PATH))

ta/mk/ta_dev_kit.mk
@@ -141,10 +141,10 @@ endif
 
 ifneq ($(libname),)
 # Build target is static library
-all: $(libname).a
-cleanfiles += $(libname).a
+all: $(link-out-dir$(sm))/$(libname).a
+cleanfiles += $(link-out-dir$(sm))/$(libname).a
 
-$(libname).a: $(objs)
+$(link-out-dir$(sm))/$(libname).a: $(objs)
 	@echo '  AR      $@'
 	$(q)rm -f $@ && $(AR$(sm)) rcs $@ $^
 endif

commit a01568c0e1b5bd4125e25bfb53fa2d275a9662c9

mk/aosp_optee.mk: fix build dependency for static libraries
The dependency set by $(local_module_deps) needs different file paths
for static libraries due to the commit [1]. Use the make variable,
LOCAL_REQUIRED_MODULES [2], to properly set the dependencies for TA
to link with static and shared libraries.

Fixes: d1b003febbcd ("mk: Support user static lib for aosp build")
Link: https://android.googlesource.com/platform/build/+/refs/heads/android13-release/core/build-system.html#704 [2]
Signed-off-by: Tadd Kao <tadd.kao@mediatek.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

mk/aosp_optee.mk
@@ -112,6 +112,7 @@ else
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 endif
 LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(local_module_deps)
 
 TA_TMP_DIR := $(subst /,_,$(LOCAL_PATH))
 TA_TMP_FILE := $(OPTEE_TA_OUT_DIR)/$(TA_TMP_DIR)/$(LOCAL_MODULE)
@@ -119,11 +120,6 @@ $(LOCAL_PREBUILT_MODULE_FILE): $(TA_TMP_FILE)
 	@mkdir -p $(dir $@)
 	cp -vf $< $@
 
-TA_TMP_FILE_DEPS :=
-ifneq ($(local_module_deps), )
-$(foreach dep,$(local_module_deps), $(eval TA_TMP_FILE_DEPS += $(TARGET_OUT_VENDOR)/lib/optee_armtz/$(dep)))
-endif
-$(TA_TMP_FILE): $(TA_TMP_FILE_DEPS)
 $(TA_TMP_FILE): PRIVATE_TA_SRC_DIR := $(LOCAL_PATH)
 $(TA_TMP_FILE): PRIVATE_TA_TMP_FILE := $(TA_TMP_FILE)
 $(TA_TMP_FILE): PRIVATE_TA_TMP_DIR := $(TA_TMP_DIR)