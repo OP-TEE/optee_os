OP-TEE/optee_os

commit 06ea466f9c19058ad7b1c66726acb7e72e4bc842

core: fix race in mobj_reg_shm_inc_map()
Fixes a race in mobj_reg_shm_inc_map() when mapcount is 0. The problem
goes like:

Thread 1 and 2 calls mobj_reg_shm_inc_map() at the same time and
mapcount is 0. Thread 1 takes the lock first and initializes mapcount to
1 and map the mobj etc.

When thread 2 has the lock it discovers that mapcount has been
initialize while it was waiting for the lock.

Prior to this patch we where exiting the function doing nothing more
since the mobj was mapped, but by doing so we'll miss to increase
mapcount.

Fix this by restarting the call to refcount_inc() using a loop.

Fixes: 37a6b717787b ("core: introduce CFG_CORE_DYN_SHM")
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/mobj_dyn_shm.c
@@ -114,6 +114,7 @@ static void *mobj_reg_shm_get_va(struct mobj *mobj, size_t offst, size_t len)
 
 static void reg_shm_unmap_helper(struct mobj_reg_shm *r)
 {
+	assert(r->mm);
 	assert(r->mm->pool->shift == SMALL_PAGE_SHIFT);
 	core_mmu_unmap_pages(tee_mm_get_smem(r->mm), r->mm->size);
 	tee_mm_free(r->mm);
@@ -183,28 +184,41 @@ static TEE_Result mobj_reg_shm_inc_map(struct mobj *mobj)
 	uint32_t exceptions = 0;
 	size_t sz = 0;
 
-	if (refcount_inc(&r->mapcount))
-		return TEE_SUCCESS;
+	while (true) {
+		if (refcount_inc(&r->mapcount))
+			return TEE_SUCCESS;
 
-	exceptions = cpu_spin_lock_xsave(&reg_shm_map_lock);
+		exceptions = cpu_spin_lock_xsave(&reg_shm_map_lock);
 
-	if (refcount_val(&r->mapcount))
-		goto out;
-
-	assert(!r->mm);
-	sz = ROUNDUP(mobj->size + r->page_offset, SMALL_PAGE_SIZE);
-	r->mm = tee_mm_alloc(&tee_mm_shm, sz);
-	if (!r->mm) {
-		res = TEE_ERROR_OUT_OF_MEMORY;
-		goto out;
+		if (!refcount_val(&r->mapcount))
+			break; /* continue to reinitialize */
+		/*
+		 * If another thread beat us to initialize mapcount,
+		 * restart to make sure we still increase it.
+		 */
+		cpu_spin_unlock_xrestore(&reg_shm_map_lock, exceptions);
 	}
 
-	res = core_mmu_map_pages(tee_mm_get_smem(r->mm), r->pages,
-				 sz / SMALL_PAGE_SIZE, MEM_AREA_NSEC_SHM);
-	if (res) {
-		tee_mm_free(r->mm);
-		r->mm = NULL;
-		goto out;
+	/*
+	 * If we have beated another thread calling mobj_reg_shm_dec_map()
+	 * to get the lock we need only to reinitialize mapcount to 1.
+	 */
+	if (!r->mm) {
+		sz = ROUNDUP(mobj->size + r->page_offset, SMALL_PAGE_SIZE);
+		r->mm = tee_mm_alloc(&tee_mm_shm, sz);
+		if (!r->mm) {
+			res = TEE_ERROR_OUT_OF_MEMORY;
+			goto out;
+		}
+
+		res = core_mmu_map_pages(tee_mm_get_smem(r->mm), r->pages,
+					 sz / SMALL_PAGE_SIZE,
+					 MEM_AREA_NSEC_SHM);
+		if (res) {
+			tee_mm_free(r->mm);
+			r->mm = NULL;
+			goto out;
+		}
 	}
 
 	refcount_set(&r->mapcount, 1);

commit 6859eb93f98738cc7edc67e965de268ab919c73a

core: fix race in mobj_reg_shm_dec_map()
Fixes a race in mobj_reg_shm_dec_map() when r->mm is NULL. This is
similar to the race fixed by commit 06ea466f9c19 ("core: fix race in
mobj_reg_shm_inc_map()"), but with one more possibility.

The problem goes like:
A. Thread 1 calls mobj_reg_shm_dec_map() at the same time as thread 2
   calls mobj_reg_shm_inc_map().
B. Thread 1 decreases mapcount to zero and tries to take the spinlock,
   but thread 1 is suspended before it has acquired the spinlock.
C. Thread 2 sees that mapcount is zero and takes the spinlock and maps
   the memory.
D. Thread 2 calls mobj_reg_shm_dec_map(), mapcount reaches zero again
   and the shared memory is unmapped and r->mm is set to NULL.
E. Thread 1 is finally resumed and acquires the spinlock, mapcount is still
   zero but r->mm is also NULL.

To fix the problem at step E above check that r->mm is still non-NULL.

Note that the same fix isn't needed for ffa_dec_map() since
unmap_helper() checks that mf->mm is non-NULL first.

Fixes: 06ea466f9c19 ("core: fix race in mobj_reg_shm_inc_map()")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Olivier Masse <olivier.masse@nxp.com>

core/arch/arm/mm/mobj_dyn_shm.c
@@ -200,7 +200,7 @@ static TEE_Result mobj_reg_shm_inc_map(struct mobj *mobj)
 	}
 
 	/*
-	 * If we have beated another thread calling mobj_reg_shm_dec_map()
+	 * If we have beaten another thread calling mobj_reg_shm_dec_map()
 	 * to get the lock we need only to reinitialize mapcount to 1.
 	 */
 	if (!r->mm) {
@@ -238,7 +238,14 @@ static TEE_Result mobj_reg_shm_dec_map(struct mobj *mobj)
 
 	exceptions = cpu_spin_lock_xsave(&reg_shm_map_lock);
 
-	if (!refcount_val(&r->mapcount))
+	/*
+	 * Check that another thread hasn't been able to:
+	 * - increase the mapcount
+	 * - or, increase the mapcount, decrease it again, and set r->mm to
+	 *   NULL
+	 * before we acquired the spinlock
+	 */
+	if (!refcount_val(&r->mapcount) && r->mm)
 		reg_shm_unmap_helper(r);
 
 	cpu_spin_unlock_xrestore(&reg_shm_map_lock, exceptions);