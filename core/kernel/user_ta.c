OP-TEE/optee_os

commit 376448c26af407523aede7ee310b3505dfd431f8

core: use user-access functions for passing params
Use user-access functions for parameter-passing between user TA and
the core when calling another TA from a TA and when entering a user
TA.

Signed-off-by: Seonghyun Park <seonghp@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/kernel/user_ta.c
@@ -49,12 +49,20 @@
 #include <utee_defines.h>
 #include <util.h>
 
-static void init_utee_param(struct utee_params *up,
-			const struct tee_ta_param *p, void *va[TEE_NUM_PARAMS])
+static TEE_Result init_utee_param(struct utee_params *up,
+				  const struct tee_ta_param *p,
+				  void *va[TEE_NUM_PARAMS])
 {
-	size_t n;
+	TEE_Result res = TEE_SUCCESS;
+	size_t n = 0;
+	struct utee_params *up_bbuf = NULL;
 
-	up->types = p->types;
+	up_bbuf = bb_alloc(sizeof(struct utee_params));
+	if (!up_bbuf)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	up_bbuf->types = p->types;
+
 	for (n = 0; n < TEE_NUM_PARAMS; n++) {
 		uintptr_t a;
 		uintptr_t b;
@@ -77,9 +85,15 @@ static void init_utee_param(struct utee_params *up,
 			break;
 		}
 		/* See comment for struct utee_params in utee_types.h */
-		up->vals[n * 2] = a;
-		up->vals[n * 2 + 1] = b;
+		up_bbuf->vals[n * 2] = a;
+		up_bbuf->vals[n * 2 + 1] = b;
 	}
+
+	res = copy_to_user(up, up_bbuf, sizeof(struct utee_params));
+
+	bb_free(up_bbuf, sizeof(struct utee_params));
+
+	return res;
 }
 
 static void update_from_utee_param(struct tee_ta_param *p,
@@ -159,9 +173,12 @@ static TEE_Result user_ta_enter(struct ts_session *session,
 	usr_stack -= ROUNDUP(sizeof(struct utee_params), STACK_ALIGNMENT);
 	usr_params = (struct utee_params *)usr_stack;
 	if (ta_sess->param)
-		init_utee_param(usr_params, ta_sess->param, param_va);
+		res = init_utee_param(usr_params, ta_sess->param, param_va);
 	else
-		memset(usr_params, 0, sizeof(*usr_params));
+		res = clear_user(usr_params, sizeof(*usr_params));
+
+	if (res)
+		goto out_pop_session;
 
 	res = thread_enter_user_mode(func, kaddr_to_uref(session),
 				     (vaddr_t)usr_params, cmd, usr_stack,
@@ -187,15 +204,16 @@ static TEE_Result user_ta_enter(struct ts_session *session,
 	if (ta_sess->param) {
 		/* Copy out value results */
 		update_from_utee_param(ta_sess->param, usr_params);
+	}
 
+out_pop_session:
+	if (ta_sess->param) {
 		/*
 		 * Clear out the parameter mappings added with
 		 * vm_clean_param() above.
 		 */
 		vm_clean_param(&utc->uctx);
 	}
-
-
 	ts_sess = ts_pop_current_session();
 	assert(ts_sess == session);
 

core/tee/tee_svc.c
@@ -587,13 +587,25 @@ static TEE_Result utee_param_to_param(struct user_ta_ctx *utc,
 				      struct tee_ta_param *p,
 				      struct utee_params *up)
 {
+	TEE_Result res = TEE_SUCCESS;
 	size_t n = 0;
-	uint32_t types = up->types;
+	uint64_t types = 0;
+	struct utee_params *up_bbuf = NULL;
+
+	up_bbuf = bb_alloc(sizeof(struct utee_params));
+	if (!up_bbuf)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	res = copy_from_user(up_bbuf, up, sizeof(struct utee_params));
+	if (res)
+		goto out;
+
+	types = up_bbuf->types;
 
 	p->types = types;
 	for (n = 0; n < TEE_NUM_PARAMS; n++) {
-		uintptr_t a = up->vals[n * 2];
-		size_t b = up->vals[n * 2 + 1];
+		uintptr_t a = up_bbuf->vals[n * 2];
+		size_t b = up_bbuf->vals[n * 2 + 1];
 		uint32_t flags = TEE_MEMORY_ACCESS_READ |
 				 TEE_MEMORY_ACCESS_ANY_OWNER;
 
@@ -608,16 +620,19 @@ static TEE_Result utee_param_to_param(struct user_ta_ctx *utc,
 
 			if (!p->u[n].mem.offs) {
 				/* Allow NULL memrefs if of size 0 */
-				if (p->u[n].mem.size)
-					return TEE_ERROR_BAD_PARAMETERS;
+				if (p->u[n].mem.size) {
+					res = TEE_ERROR_BAD_PARAMETERS;
+					goto out;
+				}
 				p->u[n].mem.mobj = NULL;
 				break;
 			}
 
 			p->u[n].mem.mobj = &mobj_virt;
 
-			if (vm_check_access_rights(&utc->uctx, flags, a, b))
-				return TEE_ERROR_ACCESS_DENIED;
+			res = vm_check_access_rights(&utc->uctx, flags, a, b);
+			if (res)
+				goto out;
 			break;
 		case TEE_PARAM_TYPE_VALUE_INPUT:
 		case TEE_PARAM_TYPE_VALUE_INOUT:
@@ -630,7 +645,9 @@ static TEE_Result utee_param_to_param(struct user_ta_ctx *utc,
 		}
 	}
 
-	return TEE_SUCCESS;
+out:
+	bb_free(up_bbuf, sizeof(struct utee_params));
+	return res;
 }
 
 static TEE_Result alloc_temp_sec_mem(size_t size, struct mobj **mobj,
@@ -813,11 +830,15 @@ static TEE_Result tee_svc_update_out_param(
 		size_t tmp_buf_size[TEE_NUM_PARAMS],
 		struct utee_params *usr_param)
 {
-	size_t n;
+	size_t n = 0;
 	uint64_t *vals = usr_param->vals;
-	size_t sz = 0;
+	uint64_t sz = 0;
+	uint64_t in_sz = 0;
 
 	for (n = 0; n < TEE_NUM_PARAMS; n++) {
+		TEE_Result res = TEE_SUCCESS;
+		uint64_t val_buf[2] = { };
+
 		switch (TEE_PARAM_TYPE_GET(param->types, n)) {
 		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 		case TEE_PARAM_TYPE_MEMREF_INOUT:
@@ -828,10 +849,18 @@ static TEE_Result tee_svc_update_out_param(
 			 * size needs to be updated.
 			 */
 			sz = param->u[n].mem.size;
-			if (tmp_buf_va[n] && sz <= vals[n * 2 + 1]) {
+
+			res = GET_USER_SCALAR(in_sz, &vals[n * 2 + 1]);
+			if (res)
+				return res;
+
+			if (tmp_buf_va[n] && sz <= in_sz) {
 				void *src = tmp_buf_va[n];
-				void *dst = (void *)(uintptr_t)vals[n * 2];
-				TEE_Result res = TEE_SUCCESS;
+				uint64_t dst = 0;
+
+				res = GET_USER_SCALAR(dst, &vals[n * 2]);
+				if (res)
+					return res;
 
 				/*
 				 * TA is allowed to return a size larger than
@@ -840,18 +869,28 @@ static TEE_Result tee_svc_update_out_param(
 				 * API spec.
 				 */
 				if (sz <= tmp_buf_size[n]) {
-					res = copy_to_user(dst, src, sz);
+					res = copy_to_user((void *)(vaddr_t)dst,
+							   src, sz);
 					if (res != TEE_SUCCESS)
 						return res;
 				}
 			}
-			usr_param->vals[n * 2 + 1] = sz;
+			res = PUT_USER_SCALAR(sz, &usr_param->vals[n * 2 + 1]);
+			if (res)
+				return res;
+
 			break;
 
 		case TEE_PARAM_TYPE_VALUE_OUTPUT:
 		case TEE_PARAM_TYPE_VALUE_INOUT:
-			vals[n * 2] = param->u[n].val.a;
-			vals[n * 2 + 1] = param->u[n].val.b;
+			val_buf[0] = param->u[n].val.a;
+			val_buf[1] = param->u[n].val.b;
+
+			res = copy_to_user(&vals[n * 2], val_buf,
+					   2 * sizeof(uint64_t));
+			if (res)
+				return res;
+
 			break;
 
 		default:

commit 5b200fb973ca532faaaf59bb811d85359ffa3f1e

core: fix update from user parameters with CFG_PAN=y
When CFG_PAN is enabled, OP-TEE kernel can not directly access the user
memory, otherwise an exception occurs. To fix it, we apply user-access
functions when OP-TEE kernel updates the parameters from user stack. A
bounce buffer is allocated and the user stack contents are duplicated
into the bounce buffer before OP-TEE kernel accesses them.

Fixes: 376448c26af4 ("core: use user-access functions for passing params")
Signed-off-by: Alvin Chang <alvinga@andestech.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/user_ta.c
@@ -97,27 +97,38 @@ static TEE_Result init_utee_param(struct utee_params *up,
 }
 
 static void update_from_utee_param(struct tee_ta_param *p,
-			const struct utee_params *up)
+				   const struct utee_params *up)
 {
-	size_t n;
+	TEE_Result res = TEE_SUCCESS;
+	size_t n = 0;
+	struct utee_params *up_bbuf = NULL;
+	void *bbuf = NULL;
+
+	res = bb_memdup_user(up, sizeof(*up), &bbuf);
+	if (res)
+		return;
+
+	up_bbuf = bbuf;
 
 	for (n = 0; n < TEE_NUM_PARAMS; n++) {
 		switch (TEE_PARAM_TYPE_GET(p->types, n)) {
 		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
 		case TEE_PARAM_TYPE_MEMREF_INOUT:
 			/* See comment for struct utee_params in utee_types.h */
-			p->u[n].mem.size = up->vals[n * 2 + 1];
+			p->u[n].mem.size = up_bbuf->vals[n * 2 + 1];
 			break;
 		case TEE_PARAM_TYPE_VALUE_OUTPUT:
 		case TEE_PARAM_TYPE_VALUE_INOUT:
 			/* See comment for struct utee_params in utee_types.h */
-			p->u[n].val.a = up->vals[n * 2];
-			p->u[n].val.b = up->vals[n * 2 + 1];
+			p->u[n].val.a = up_bbuf->vals[n * 2];
+			p->u[n].val.b = up_bbuf->vals[n * 2 + 1];
 			break;
 		default:
 			break;
 		}
 	}
+
+	bb_free(bbuf, sizeof(*up));
 }
 
 static bool inc_recursion(void)