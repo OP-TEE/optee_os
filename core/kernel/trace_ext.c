05b05981c601cb10e048f246ea964d162832a7d1
Etienne Carriere <etienne.carriere@foss.st.com>
2023-07-12 13:50:48 +0200

    core: kernel: allow not masking interrupts on output traces
    Add configuration switch CFG_CONSOLE_MASK_INTERRUPTS define whether
    or not interrupts are masked when console trace messages are
    emitted. The config switch is default enabled to reflect core legacy
    behavior.
    
    Disabling this configuration switch can be handy to still benefit from
    OP-TEE threads output console trace support without adding extra
    latency to native and foreign interrupts handling when trace messages
    from interruptible thread context are emitted.
    
    Enabling the configuration does not fully prevent collisions
    of trace messages as described in mk/config.mk.
    
    By the way, add an initial value to local variable p in trace_ext_puts().
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/kernel/trace_ext.c
+++ core/kernel/trace_ext.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <trace.h>
 #include <console.h>
+#include <config.h>
 #include <kernel/misc.h>
 #include <kernel/spinlock.h>
 #include <kernel/thread.h>
@@ -15,21 +16,75 @@ const char trace_ext_prefix[] = "TC";
 int trace_level __nex_data = TRACE_LEVEL;
 static unsigned int puts_lock __nex_bss = SPINLOCK_UNLOCK;
 
+/*
+ * Atomic flags for sequencing concurrent messages
+ * when CFG_CONSOLE_MASK_INTERRUPTS is disabled.
+ * itr_trace_busy tracks context always uninterruptible
+ * (when thread_is_in_normal_mode() returns false).
+ */
+static unsigned int thread_trace_busy;
+static unsigned int itr_trace_busy;
+
+static unsigned int *get_busy_state(void)
+{
+	if (thread_is_in_normal_mode())
+		return &thread_trace_busy;
+	else
+		return &itr_trace_busy;
+}
+
+static bool wait_if_trace_contended(uint32_t *itr_status)
+{
+	bool was_contended = false;
+
+	if (IS_ENABLED(CFG_CONSOLE_MASK_INTERRUPTS)) {
+		*itr_status = thread_mask_exceptions(THREAD_EXCP_ALL);
+
+		if (cpu_mmu_enabled() && !cpu_spin_trylock(&puts_lock)) {
+			was_contended = true;
+			cpu_spin_lock_no_dldetect(&puts_lock);
+		}
+	} else if (cpu_mmu_enabled()) {
+		unsigned int trace_not_busy = 0;
+
+		/* Don't mix thread traces, don't mix interrupt traces */
+		while (!atomic_cas_uint(get_busy_state(), &trace_not_busy, 1)) {
+			trace_not_busy = 0;
+			was_contended = true;
+		}
+
+		/* Don't emit a thread trace inside an interrupt trace */
+		if (thread_is_in_normal_mode())
+			while (atomic_load_uint(&itr_trace_busy))
+				was_contended = true;
+	}
+
+	return was_contended;
+}
+
+static void release_trace_contention(uint32_t itr_status)
+{
+	if (IS_ENABLED(CFG_CONSOLE_MASK_INTERRUPTS)) {
+		if (cpu_mmu_enabled())
+			cpu_spin_unlock(&puts_lock);
+
+		thread_unmask_exceptions(itr_status);
+	} else if (cpu_mmu_enabled()) {
+		atomic_store_uint(get_busy_state(), 0);
+	}
+}
+
 void __weak plat_trace_ext_puts(const char *str __unused)
 {
 }
 
 void trace_ext_puts(const char *str)
 {
-	uint32_t itr_status = thread_mask_exceptions(THREAD_EXCP_ALL);
-	bool mmu_enabled = cpu_mmu_enabled();
 	bool was_contended = false;
-	const char *p;
+	uint32_t itr_status = 0;
+	const char *p = NULL;
 
-	if (mmu_enabled && !cpu_spin_trylock(&puts_lock)) {
-		was_contended = true;
-		cpu_spin_lock_no_dldetect(&puts_lock);
-	}
+	was_contended = wait_if_trace_contended(&itr_status);
 
 	plat_trace_ext_puts(str);
 
@@ -43,10 +98,7 @@ void trace_ext_puts(const char *str)
 
 	console_flush();
 
-	if (mmu_enabled)
-		cpu_spin_unlock(&puts_lock);
-
-	thread_unmask_exceptions(itr_status);
+	release_trace_contention(itr_status);
 }
 
 int trace_ext_get_thread_id(void)
