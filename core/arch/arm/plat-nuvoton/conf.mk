3c1ad68fedcb3d5774d9722d9b49646d4670ad79
Margarita Glushkin <rutigl@gmail.com>
2023-07-18 08:46:28 +0300

    plat-nuvoton: change load address, shared memory and SDP memory
    Changes load address of OPTEE-OS from 0x36000000 to 0x02100000
    Moves shared memory to 0x06000000
    Moves SDP memory to 0x05F00000
    
    Co-developed-by: Hila Miranda-Kuzi <hila.miranda.kuzi1@gmail.com>
    Signed-off-by: Hila Miranda-Kuzi <hila.miranda.kuzi1@gmail.com>
    Signed-off-by: Margarita Glushkin <rutigl@gmail.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/plat-nuvoton/conf.mk
+++ core/arch/arm/plat-nuvoton/conf.mk
@@ -21,14 +21,15 @@ CFG_NPCM_DEBUG ?= n
 ifeq ($(PLATFORM_FLAVOR),npcm845x)
 CFG_TEE_CORE_NB_CORE ?= 4
 # [3000.0000 031f.ffff] is reserved to early boot
-CFG_TZDRAM_START ?= 0x36000000
-CFG_TZDRAM_SIZE  ?= 0x00f00000
+CFG_TZDRAM_START ?= 0x02100000
+CFG_TZDRAM_SIZE  ?= 0x03f00000
 # SHM chosen arbitrary, in a way that it does not interfere
 # with initial location of linux kernel, dtb and initrd.
-CFG_SHMEM_START ?= 0x2FD00000
+CFG_SHMEM_START ?= 0x06000000
 CFG_SHMEM_SIZE  ?= 0x00200000
 # When Secure Data Path is enable, last MByte of TZDRAM is SDP test memory.
-CFG_TEE_SDP_MEM_SIZE ?= 0x00400000
+CFG_TEE_SDP_MEM_BASE ?= 0x05F00000
+CFG_TEE_SDP_MEM_SIZE ?= 0x00100000
 $(call force,CFG_DT,y)
 CFG_DTB_MAX_SIZE ?= 0x100000
 $(call force,CFG_WITH_PAGER,n,Pager is not supported for NPCM845x)
