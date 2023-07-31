OP-TEE/optee_os

commit 84f78978974b73fb163e370301078f0f6644dcfe

core: use user-access functions for storage svc
Use user-access functions within storage service syscalls, mainly to
copy object id from user-spaced buffers.

Signed-off-by: Seonghyun Park <seonghp@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/tee/tee_svc_storage.c
@@ -176,6 +176,7 @@ TEE_Result syscall_storage_obj_open(unsigned long storage_id, void *object_id,
 	TEE_Result res = TEE_SUCCESS;
 	struct tee_pobj *po = NULL;
 	struct tee_obj *o = NULL;
+	void *oid_bbuf = NULL;
 
 	if (flags & ~valid_flags)
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -192,15 +193,16 @@ TEE_Result syscall_storage_obj_open(unsigned long storage_id, void *object_id,
 
 	object_id = memtag_strip_tag(object_id);
 	if (object_id_len) {
-		res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
-					     (uaddr_t)object_id, object_id_len);
-		if (res != TEE_SUCCESS)
-			goto err;
+		res = bb_memdup_user_private(object_id, object_id_len,
+					     &oid_bbuf);
+		if (res)
+			goto exit;
 	}
 
-	res = tee_pobj_get((void *)&sess->ctx->uuid, object_id,
+	res = tee_pobj_get((void *)&sess->ctx->uuid, oid_bbuf,
 			   object_id_len, flags, TEE_POBJ_USAGE_OPEN, fops,
 			   &po);
+	bb_free(oid_bbuf, object_id_len);
 	if (res != TEE_SUCCESS)
 		goto err;
 
@@ -326,6 +328,7 @@ TEE_Result syscall_storage_obj_create(unsigned long storage_id, void *object_id,
 	TEE_Result res = TEE_SUCCESS;
 	struct tee_pobj *po = NULL;
 	struct tee_obj *o = NULL;
+	void *oid_bbuf = NULL;
 
 	if (flags & ~valid_flags)
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -340,15 +343,16 @@ TEE_Result syscall_storage_obj_create(unsigned long storage_id, void *object_id,
 	data = memtag_strip_tag(data);
 
 	if (object_id_len) {
-		res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
-					     (uaddr_t)object_id, object_id_len);
-		if (res != TEE_SUCCESS)
-			goto err;
+		res = bb_memdup_user_private(object_id, object_id_len,
+					     &oid_bbuf);
+		if (res)
+			return res;
 	}
 
-	res = tee_pobj_get((void *)&sess->ctx->uuid, object_id,
+	res = tee_pobj_get((void *)&sess->ctx->uuid, oid_bbuf,
 			   object_id_len, flags, TEE_POBJ_USAGE_CREATE,
 			   fops, &po);
+	bb_free(oid_bbuf, object_id_len);
 	if (res != TEE_SUCCESS)
 		goto err;
 
@@ -493,6 +497,7 @@ TEE_Result syscall_storage_obj_rename(unsigned long obj, void *object_id,
 	struct tee_obj *o = NULL;
 	char *new_file = NULL;
 	char *old_file = NULL;
+	void *oid_bbuf = NULL;
 
 	if (object_id_len > TEE_OBJECT_ID_MAX_LEN)
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -518,17 +523,18 @@ TEE_Result syscall_storage_obj_rename(unsigned long obj, void *object_id,
 
 	object_id = memtag_strip_tag(object_id);
 	if (object_id_len) {
-		res = vm_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ,
-					     (uaddr_t)object_id, object_id_len);
-		if (res != TEE_SUCCESS)
+		res = bb_memdup_user_private(object_id, object_id_len,
+					     &oid_bbuf);
+		if (res)
 			goto exit;
 	}
 
 	/* reserve dest name */
 	fops = o->pobj->fops;
-	res = tee_pobj_get((void *)&sess->ctx->uuid, object_id,
+	res = tee_pobj_get((void *)&sess->ctx->uuid, oid_bbuf,
 			   object_id_len, TEE_DATA_FLAG_ACCESS_WRITE_META,
 			   TEE_POBJ_USAGE_RENAME, fops, &po);
+	bb_free(oid_bbuf, object_id_len);
 	if (res != TEE_SUCCESS)
 		goto exit;
 
@@ -642,6 +648,7 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 	TEE_Result res = TEE_SUCCESS;
 	struct tee_obj *o = NULL;
 	uint64_t l = 0;
+	struct utee_object_info bbuf = { };
 
 	res = tee_svc_storage_get_enum(utc, uref_to_vaddr(obj_enum), &e);
 	if (res != TEE_SUCCESS)
@@ -688,7 +695,7 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	*info = (struct utee_object_info){
+	bbuf = (struct utee_object_info){
 		.obj_type = o->info.objectType,
 		.obj_size = o->info.objectSize,
 		.max_obj_size = o->info.maxObjectSize,
@@ -697,7 +704,13 @@ TEE_Result syscall_storage_next_enum(unsigned long obj_enum,
 		.data_pos = o->info.dataPosition,
 		.handle_flags = o->info.handleFlags,
 	};
-	memcpy(obj_id, o->pobj->obj_id, o->pobj->obj_id_len);
+	res = copy_to_user(info, &bbuf, sizeof(bbuf));
+	if (res)
+		goto exit;
+
+	res = copy_to_user(obj_id, o->pobj->obj_id, o->pobj->obj_id_len);
+	if (res)
+		goto exit;
 
 	l = o->pobj->obj_id_len;
 	res = copy_to_user_private(len, &l, sizeof(*len));

commit b3d4327ab6e9243b59b2678e3d179ac12bc8dce9

core: syscall_storage_obj_rename(): fix direct user memory access
Fixes a direct user memory access in syscall_storage_obj_rename() which
can lead to a data abort if PAN is enabled.

Fixes: 84f78978974b ("core: use user-access functions for storage svc")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/tee/tee_svc_storage.c
@@ -543,7 +543,7 @@ TEE_Result syscall_storage_obj_rename(unsigned long obj, void *object_id,
 	if (res)
 		goto exit;
 
-	res = tee_pobj_rename(o->pobj, object_id, object_id_len);
+	res = tee_pobj_rename(o->pobj, po->obj_id, po->obj_id_len);
 
 exit:
 	tee_pobj_release(po);