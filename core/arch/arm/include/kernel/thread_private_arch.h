OP-TEE/optee_os

commit 6fa59c9a70dc15e204c9215a7a21ff2cb7f59cb2

arm64: Introduce permissive PAN implementation
Privileged Access Never (PAN) is a part of ARMv8.1 extension that
restricts accesses to unprivileged memory from privileged mode
in order to prevent unintended accesses to potentially malicious
memory.

This introduces configuration of PAN and helper functions
enter_user_access() and exit_user_access() that toggles PSTATE.PAN
that controls the behavior of PAN.

Current OP-TEE impelmentation is not ready to apply strict PAN policy
due to missing user-access function uses, etc.

Hence, this patch takes a very permissive approach (yet better
than nothing), where PAN is deactivated in the entire lifetime of
thread_svc_handler (i.e., system call).

Signed-off-by: Seonghyun Park <seonghp@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/thread.c
@@ -23,6 +23,7 @@
 #include <kernel/tee_ta_manager.h>
 #include <kernel/thread.h>
 #include <kernel/thread_private.h>
+#include <kernel/user_access.h>
 #include <kernel/user_mode_ctx_struct.h>
 #include <kernel/virtualization.h>
 #include <mm/core_memprot.h>
@@ -1102,6 +1103,8 @@ void __weak thread_scall_handler(struct thread_scall_regs *regs)
 	struct ts_session *sess = NULL;
 	uint32_t state = 0;
 
+	enter_user_access();
+
 	/* Enable native interrupts */
 	state = thread_get_exceptions();
 	thread_unmask_exceptions(state & ~THREAD_EXCP_NATIVE_INTR);
@@ -1126,6 +1129,8 @@ void __weak thread_scall_handler(struct thread_scall_regs *regs)
 		/* We're returning from __thread_enter_user_mode() */
 		setup_unwind_user_mode(regs);
 	}
+
+	exit_user_access();
 }
 
 #ifdef CFG_WITH_ARM_TRUSTED_FW

core/arch/arm/include/arm64.h
@@ -271,6 +271,8 @@
 #define ID_AA64ISAR1_APA_ARCH_EPAC2_FPAC	U(0x4)
 #define ID_AA64ISAR1_APA_ARCH_EPAC2_FPAC_CMB	U(0x5)
 
+#define ID_MMFR3_EL1_PAN_SHIFT			U(16)
+
 #define GCR_EL1_RRND				BIT64(16)
 
 #ifndef __ASSEMBLER__
@@ -460,6 +462,21 @@ DEFINE_REG_WRITE_FUNC_(icc_eoir0, uint32_t, S3_0_c12_c8_1)
 DEFINE_REG_WRITE_FUNC_(icc_eoir1, uint32_t, S3_0_c12_c12_1)
 DEFINE_REG_WRITE_FUNC_(icc_igrpen0, uint32_t, S3_0_C12_C12_6)
 DEFINE_REG_WRITE_FUNC_(icc_igrpen1, uint32_t, S3_0_C12_C12_7)
+
+DEFINE_REG_WRITE_FUNC_(pan, uint64_t, S3_0_c4_c2_3)
+
+static inline void write_pan_enable(void)
+{
+	/* msr pan, #1 */
+	asm volatile("msr	S0_0_c4_c1_4, xzr" ::: "memory" );
+}
+
+static inline void write_pan_disable(void)
+{
+	/* msr pan, #0 */
+	asm volatile("msr	S0_0_c4_c0_4, xzr" ::: "memory" );
+}
+
 #endif /*__ASSEMBLER__*/
 
 #endif /*ARM64_H*/

core/arch/arm/include/arm64_macros.S
@@ -148,3 +148,18 @@
 	.macro write_apiakeyhi reg
 	msr	S3_0_c2_c1_1, \reg
 	.endm
+
+	.macro write_pan reg
+	/* msr pan, \reg */
+	msr	S3_0_c4_c2_3, \reg
+	.endm
+
+	.macro write_pan_enable
+	/* msr pan, #1 */
+	msr	S0_0_c4_c1_4, xzr
+	.endm
+
+	.macro write_pan_disable
+	/* msr pan, #0 */
+	msr	S0_0_c4_c0_4, xzr
+	.endm

core/arch/arm/include/kernel/user_access_arch.h
@@ -1,0 +1,28 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2023, Amazon.com Inc. or its affiliates. All rights Reserved.
+ */
+
+#ifndef __KERNEL_USER_ACCESS_ARCH_H
+#define __KERNEL_USER_ACCESS_ARCH_H
+
+#include <arm.h>
+
+#ifdef CFG_PAN
+/* Enter a section where user mode access is temporarily enabled. */
+static inline void enter_user_access(void)
+{
+	write_pan_disable();
+}
+
+/* Exit from the section where user mode access was temporarily enabled. */
+static inline void exit_user_access(void)
+{
+	write_pan_enable();
+}
+#else
+static inline void enter_user_access(void) {}
+static inline void exit_user_access(void) {}
+#endif /* CFG_PAN */
+
+#endif /* __KERNEL_USER_ACCESS_ARCH_H */

core/arch/arm/kernel/entry_a64.S
@@ -51,6 +51,11 @@
 		ubfx	\reg, \reg, #ID_AA64PFR1_EL1_MTE_SHIFT, #4
 	.endm
 
+	.macro read_feat_pan reg
+		mrs	\reg, id_mmfr3_el1
+		ubfx	\reg, \reg, #ID_MMFR3_EL1_PAN_SHIFT, #4
+	.endm
+
 	.macro set_sctlr_el1
 		mrs	x0, sctlr_el1
 		orr	x0, x0, #SCTLR_I
@@ -143,6 +148,17 @@
 		isb
 	.endm
 
+	.macro init_pan
+		read_feat_pan x0
+		cmp	x0, #0
+		b.eq	1f
+		mrs	x0, sctlr_el1
+		bic	x0, x0, #SCTLR_SPAN
+		msr	sctlr_el1, x0
+		write_pan_enable
+	1:
+	.endm
+
 FUNC _start , :
 	/*
 	 * Register use:
@@ -172,6 +188,10 @@ FUNC _start , :
 	adr	x0, reset_vect_table
 	msr	vbar_el1, x0
 	isb
+
+#ifdef CFG_PAN
+	init_pan
+#endif
 
 	set_sctlr_el1
 	isb
@@ -604,6 +624,10 @@ FUNC cpu_on_handler , :
 
 	set_sctlr_el1
 	isb
+
+#ifdef CFG_PAN
+	init_pan
+#endif
 
 	/* Enable aborts now that we can receive exceptions */
 	msr	daifclr, #DAIFBIT_ABT

core/arch/riscv/include/kernel/user_access_arch.h
@@ -1,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2023, Amazon.com Inc. or its affiliates. All rights Reserved.
+ */
+
+#ifndef __KERNEL_USER_ACCESS_ARCH_H
+#define __KERNEL_USER_ACCESS_ARCH_H
+
+/* Enter a section where user mode access is temporarily enabled. */
+static inline void enter_user_access(void) {}
+
+/* Exit from the section where user mode access was temporarily enabled. */
+static inline void exit_user_access(void) {}
+
+#endif /* __KERNEL_USER_ACCESS_ARCH_H */

core/include/kernel/user_access.h
@@ -7,6 +7,7 @@
 #define __KERNEL_USER_ACCESS_H
 
 #include <assert.h>
+#include <kernel/user_access_arch.h>
 #include <tee_api_types.h>
 #include <types_ext.h>
 

mk/config.mk
@@ -927,6 +927,12 @@ ifeq (y-y,$(CFG_WITH_PAGER)-$(CFG_MEMTAG))
 $(error CFG_WITH_PAGER and CFG_MEMTAG are not compatible)
 endif
 
+# Privileged Access Never (PAN, part of the ARMv8.1 Extensions) can be
+# used to restrict accesses to unprivileged memory from privileged mode.
+CFG_PAN ?= n
+
+$(eval $(call cfg-depends-all,CFG_PAN,CFG_ARM64_core))
+
 # CFG_CORE_ASYNC_NOTIF is defined by the platform to enable support
 # for sending asynchronous notifications to normal world. Note that an
 # interrupt ID must be configurged by the platform too. Currently is only

commit 07f94ac933bc2e6150c82b44eee2d2d4f3b04bd5

core: arm64: preserve PSTATE.PAN when making SPSR
When setup_unwind_user_mode() prepares to resume execution after
syscall_sys_return() or when a thread is suspended a new SPSR is
fabricated base on the current PSTATE.

Until now when remaining in S-EL1 to fabricate an SPSR only the
PSTATE.DAIF bits had to be taken into account. However, with PSTATE.PAN
there's yet another bit to consider. Since PSTATE has a few more bits
and more may be added as AArch64 evolves this problem is only going to
get worse. So implement this in a single internal C function to replace
current open codes C and assembly versions.

The AArch64 assembly versions of thread_rpc() are renamed to
thread_rpc_spsr() to indicate that SPSR is passed in the second argument
instead of having it open coded internally in the assembly function.

New C wrapper functions are added to preserve the old thread_rpc()
interface as needed.

handle_user_mode_panic() is still basing its created SPSR on the saved
SPSR from S-EL0, but now PAN bit is copied too.

Fixes: 6fa59c9a70dc ("arm64: Introduce permissive PAN implementation")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/include/kernel/thread_private_arch.h
@@ -191,13 +191,18 @@ void thread_resume_from_rpc(uint32_t thread_id, uint32_t a0, uint32_t a1,
 			    uint32_t a2, uint32_t a3);
 
 /*
- * Suspends current thread and temorarily exits to non-secure world.
- * This function returns later when non-secure world returns.
+ * The thread_rpc() function suspends current thread and temporarily exits
+ * to non-secure world.  This function returns later when non-secure world
+ * returns.
  *
  * The purpose of this function is to request services from non-secure
  * world.
  */
 #define THREAD_RPC_NUM_ARGS     4
+#ifdef ARM64
+void thread_rpc_spsr(uint32_t rv[THREAD_RPC_NUM_ARGS], uint64_t spsr);
+void __thread_rpc(uint32_t rv[THREAD_RPC_NUM_ARGS]);
+
 #ifdef CFG_CORE_FFA
 struct thread_rpc_arg {
 	union {
@@ -216,8 +221,18 @@ struct thread_rpc_arg {
 	};
 };
 
-void thread_rpc(struct thread_rpc_arg *rpc_arg);
+static inline void thread_rpc(struct thread_rpc_arg *rpc_arg)
+{
+	__thread_rpc(rpc_arg->pad);
+}
 #else
+static inline void thread_rpc(uint32_t rv[THREAD_RPC_NUM_ARGS])
+{
+	__thread_rpc(rv);
+}
+#endif
+#endif
+#ifdef ARM32
 void thread_rpc(uint32_t rv[THREAD_RPC_NUM_ARGS]);
 #endif
 

core/arch/arm/kernel/abort.c
@@ -359,6 +359,7 @@ static void handle_user_mode_panic(struct abort_info *ai)
 {
 	struct thread_ctx *tc __maybe_unused = NULL;
 	uint32_t daif = 0;
+	uint32_t pan_bit = 0;
 
 	/*
 	 * It was a user exception, stop user execution and return
@@ -380,9 +381,12 @@ static void handle_user_mode_panic(struct abort_info *ai)
 	ai->regs->apiakey_lo = tc->keys.apia_lo;
 #endif
 
+	if (IS_ENABLED(CFG_PAN) && feat_pan_implemented() && read_pan())
+		pan_bit = SPSR_64_PAN;
 	daif = (ai->regs->spsr >> SPSR_32_AIF_SHIFT) & SPSR_32_AIF_MASK;
 	/* XXX what about DAIF_D? */
-	ai->regs->spsr = SPSR_64(SPSR_64_MODE_EL1, SPSR_64_MODE_SP_EL0, daif);
+	ai->regs->spsr = SPSR_64(SPSR_64_MODE_EL1, SPSR_64_MODE_SP_EL0, daif) |
+			 pan_bit;
 }
 #endif /*ARM64*/
 

core/arch/arm/kernel/thread.c
@@ -412,6 +412,22 @@ void thread_resume_from_rpc(uint32_t thread_id, uint32_t a0, uint32_t a1,
 }
 
 #ifdef ARM64
+static uint64_t spsr_from_pstate(void)
+{
+	uint64_t spsr = SPSR_64(SPSR_64_MODE_EL1, SPSR_64_MODE_SP_EL0, 0);
+
+	spsr |= read_daif();
+	if (IS_ENABLED(CFG_PAN) && feat_pan_implemented() && read_pan())
+		spsr |= SPSR_64_PAN;
+
+	return spsr;
+}
+
+void __thread_rpc(uint32_t rv[THREAD_RPC_NUM_ARGS])
+{
+	thread_rpc_spsr(rv, spsr_from_pstate());
+}
+
 vaddr_t thread_get_saved_thread_sp(void)
 {
 	struct thread_core_local *l = thread_get_core_local();
@@ -1072,8 +1088,7 @@ static void setup_unwind_user_mode(struct thread_scall_regs *regs)
 #endif
 #ifdef ARM64
 	regs->elr = (uintptr_t)thread_unwind_user_mode;
-	regs->spsr = SPSR_64(SPSR_64_MODE_EL1, SPSR_64_MODE_SP_EL0, 0);
-	regs->spsr |= read_daif();
+	regs->spsr = spsr_from_pstate();
 	/*
 	 * Regs is the value of stack pointer before calling the SVC
 	 * handler.  By the addition matches for the reserved space at the

core/arch/arm/kernel/thread_optee_smc_a64.S
@@ -177,12 +177,8 @@ FUNC thread_std_smc_entry , :
 	panic_at_smc_return
 END_FUNC thread_std_smc_entry
 
-/* void thread_rpc(uint32_t rv[THREAD_RPC_NUM_ARGS]) */
-FUNC thread_rpc , :
-	/* Read daif and create an SPSR */
-	mrs	x1, daif
-	orr	x1, x1, #(SPSR_64_MODE_EL1 << SPSR_64_MODE_EL_SHIFT)
-
+/* void thread_rpc_spsr(uint32_t rv[THREAD_RPC_NUM_ARGS], uint64_t spsr) */
+FUNC thread_rpc_spsr , :
 	/* Mask all maskable exceptions before switching to temporary stack */
 	msr	daifset, #DAIFBIT_ALL
 	push	x0, xzr
@@ -236,8 +232,8 @@ FUNC thread_rpc , :
 	pop	x16, xzr	/* Get pointer to rv[] */
 	store_wregs x16, 0, 0, 3	/* Store w0-w3 into rv[] */
 	ret
-END_FUNC thread_rpc
-DECLARE_KEEP_PAGER thread_rpc
+END_FUNC thread_rpc_spsr
+DECLARE_KEEP_PAGER thread_rpc_spsr
 
 /*
  * void thread_foreign_intr_exit(uint32_t thread_index)

core/arch/arm/kernel/thread_spmc_a64.S
@@ -115,12 +115,8 @@ FUNC spmc_sp_thread_entry , :
 END_FUNC spmc_sp_thread_entry
 #endif
 
-/* void thread_rpc(struct thread_rpc_arg *rpc_arg) */
-FUNC thread_rpc , :
-	/* Read daif and create an SPSR */
-	mrs	x1, daif
-	orr	x1, x1, #(SPSR_64_MODE_EL1 << SPSR_64_MODE_EL_SHIFT)
-
+/* void thread_rpc_spsr(uint32_t rv[THREAD_RPC_NUM_ARGS], uint64_t spsr) */
+FUNC thread_rpc_spsr , :
 	/* Mask all maskable exceptions before switching to temporary stack */
 	msr	daifset, #DAIFBIT_ALL
 	push	x0, xzr
@@ -176,7 +172,7 @@ FUNC thread_rpc , :
 	pop	x16, xzr	/* Get pointer to rv[] */
 	store_wregs x16, 0, 0, 3	/* Store w0-w3 into rv[] */
 	ret
-END_FUNC thread_rpc
+END_FUNC thread_rpc_spsr
 
 /*
  * void thread_foreign_intr_exit(uint32_t thread_index)