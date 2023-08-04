OP-TEE/optee_os

commit f6e2b9e2d1a270542c6f6f5e36ed4e36abe18256

drivers: crypto: implement crypto driver - DH
Add a generic cryptographic DH driver interface connecting
TEE Crypto generic APIs to HW driver interface

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/drivers/crypto/crypto_api/acipher/dh.c
@@ -1,0 +1,97 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2021 NXP
+ *
+ * Crypto DH interface implementation to enable HW driver.
+ */
+#include <crypto/crypto.h>
+#include <drvcrypt.h>
+#include <drvcrypt_acipher.h>
+#include <malloc.h>
+
+TEE_Result crypto_acipher_alloc_dh_keypair(struct dh_keypair *key,
+					   size_t size_bits)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_dh *dh = NULL;
+
+	if (!key || !size_bits) {
+		CRYPTO_TRACE("Param error key @%#" PRIxPTR " size %zu bits",
+			     (uintptr_t)key, size_bits);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	dh = drvcrypt_get_ops(CRYPTO_DH);
+	if (dh)
+		ret = dh->alloc_keypair(key, size_bits);
+
+	CRYPTO_TRACE("DH Keypair (%zu bits) alloc ret = 0x%" PRIx32, size_bits,
+		     ret);
+	return ret;
+}
+
+TEE_Result crypto_acipher_gen_dh_key(struct dh_keypair *key, struct bignum *q,
+				     size_t xbits, size_t key_size)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_dh *dh = NULL;
+
+	if (!key) {
+		CRYPTO_TRACE("Parameters error key is NULL");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (key_size != 8 * crypto_bignum_num_bytes(key->p))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	dh = drvcrypt_get_ops(CRYPTO_DH);
+	if (dh)
+		ret = dh->gen_keypair(key, q, xbits);
+
+	CRYPTO_TRACE("DH Keypair (%zu bits) generate ret = 0x%" PRIx32,
+		     key_size, ret);
+
+	return ret;
+}
+
+TEE_Result crypto_acipher_dh_shared_secret(struct dh_keypair *private_key,
+					   struct bignum *public_key,
+					   struct bignum *secret)
+{
+	TEE_Result ret = TEE_ERROR_BAD_PARAMETERS;
+	struct drvcrypt_dh *dh = NULL;
+	struct drvcrypt_secret_data sdata = { };
+	uint8_t *secret_buf = NULL;
+
+	if (!private_key || !public_key || !secret) {
+		CRYPTO_TRACE("Input parameters reference error");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	dh = drvcrypt_get_ops(CRYPTO_DH);
+	if (dh) {
+		/* Allocate the binary Secret buffer */
+		sdata.secret.length = crypto_bignum_num_bytes(public_key);
+		secret_buf = malloc(sdata.secret.length);
+		if (!secret_buf)
+			return TEE_ERROR_OUT_OF_MEMORY;
+
+		/* Prepare the Secret structure data */
+		sdata.key_priv = private_key;
+		sdata.key_pub = public_key;
+		sdata.secret.data = secret_buf;
+
+		ret = dh->shared_secret(&sdata);
+		if (ret == TEE_SUCCESS)
+			ret = crypto_bignum_bin2bn(secret_buf,
+						   sdata.secret.length, secret);
+
+		free(secret_buf);
+	} else {
+		ret = TEE_ERROR_NOT_IMPLEMENTED;
+	}
+
+	CRYPTO_TRACE("Shared Secret returned 0x%" PRIx32, ret);
+
+	return ret;
+}

core/drivers/crypto/crypto_api/acipher/sub.mk
@@ -1,2 +1,3 @@
 srcs-$(CFG_CRYPTO_DRV_RSA) += rsa.c rsamgf.c rsassa.c
 srcs-$(CFG_CRYPTO_DRV_ECC) += ecc.c
+srcs-$(CFG_CRYPTO_DRV_DH)  += dh.c

core/drivers/crypto/crypto_api/include/drvcrypt.h
@@ -55,6 +55,7 @@ enum drvcrypt_algo_id {
 	CRYPTO_MATH,	 /* Mathematical driver */
 	CRYPTO_CIPHER,   /* Cipher driver */
 	CRYPTO_ECC,      /* Asymmetric ECC driver */
+	CRYPTO_DH,       /* Asymmetric DH driver */
 	CRYPTO_MAX_ALGO  /* Maximum number of algo supported */
 };
 

core/drivers/crypto/crypto_api/include/drvcrypt_acipher.h
@@ -163,4 +163,27 @@ static inline TEE_Result drvcrypt_register_ecc(struct drvcrypt_ecc *ops)
 	return drvcrypt_register(CRYPTO_ECC, (void *)ops);
 }
 
+/*
+ * Crypto Library DH driver operations
+ */
+struct drvcrypt_dh {
+	/* Allocates the DH keypair */
+	TEE_Result (*alloc_keypair)(struct dh_keypair *key, size_t size_bits);
+	/* Generates the DH keypair */
+	TEE_Result (*gen_keypair)(struct dh_keypair *key, struct bignum *q,
+				  size_t size_bits);
+	/* DH Shared Secret */
+	TEE_Result (*shared_secret)(struct drvcrypt_secret_data *sdata);
+};
+
+/*
+ * Register a DH processing driver in the crypto API
+ *
+ * @ops - Driver operations in the HW layer
+ */
+static inline TEE_Result drvcrypt_register_dh(struct drvcrypt_dh *ops)
+{
+	return drvcrypt_register(CRYPTO_DH, (void *)ops);
+}
+
 #endif /* __DRVCRYPT_ACIPHER_H__ */

commit 83810c63aba401bace5905ab0be9e69bd5bcba2c

drivers: crypto: fix to extract DH secret length from modulus
Prior to this patch, the DH secret, due to its mechanism, becomes a value
less than the key length of the Private key. Consequently, when obtaining
the maximum size of the secret from the current key length of the Public
key, the secret length falls short. So change this to extract DH secret
length from modulus instead of public key length.

Fixes: f6e2b9e2d1a2 ("drivers: crypto: implement crypto driver - DH")
Signed-off-by: Katsunori Kikuchi <Katsunori.Kikuchi@sony.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/crypto_api/acipher/dh.c
@@ -71,7 +71,7 @@ TEE_Result crypto_acipher_dh_shared_secret(struct dh_keypair *private_key,
 	dh = drvcrypt_get_ops(CRYPTO_DH);
 	if (dh) {
 		/* Allocate the binary Secret buffer */
-		sdata.secret.length = crypto_bignum_num_bytes(public_key);
+		sdata.secret.length = crypto_bignum_num_bytes(private_key->p);
 		secret_buf = malloc(sdata.secret.length);
 		if (!secret_buf)
 			return TEE_ERROR_OUT_OF_MEMORY;