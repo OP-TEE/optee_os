fe6832dfbe6dbb43dbd56fdbf45daaf4705f8cd6
Clement Faure <clement.faure@nxp.com>
2023-09-07 11:25:52 +0200

    core: imx: re-work GPCv2 driver
    Re-work GPCv2 driver:
     * use io_clr/set functions
     * use timeout for register polling
    Remove imx_gpcv2_set_core1_pdn_by_software() function.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-imx/pm/gpcv2.c
+++ core/arch/arm/plat-imx/pm/gpcv2.c
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (C) 2017 NXP
+ * Copyright (C) 2017, 2023 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
 
-#include <imx.h>
 #include <io.h>
+#include <kernel/delay_arch.h>
 #include <mm/core_memprot.h>
-#include <platform_config.h>
-#include <stdint.h>
+
+#include "local.h"
 
 #define GPC_PGC_C1			0x840
-#define GPC_PGC_C1_PUPSCR		0x844
 #define GPC_PGC_PCG_MASK		BIT(0)
 
 #define GPC_CPU_PGC_SW_PUP_REQ		0xf0
@@ -21,51 +20,31 @@
 #define GPC_PU_PGC_SW_PDN_REQ		0x104
 #define GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK BIT(1)
 
-static vaddr_t gpc_base(void)
-{
-	return core_mmu_get_va(GPC_BASE, MEM_AREA_IO_SEC, 1);
-}
-
 static void imx_gpcv2_set_core_pgc(bool enable, uint32_t offset)
 {
-	uint32_t val = io_read32(gpc_base() + offset) & (~GPC_PGC_PCG_MASK);
+	vaddr_t va = core_mmu_get_va(GPC_BASE, MEM_AREA_IO_SEC, GPC_SIZE);
 
 	if (enable)
-		val |= GPC_PGC_PCG_MASK;
-
-	io_write32(gpc_base() + offset, val);
-}
-
-void imx_gpcv2_set_core1_pdn_by_software(void)
-{
-	uint32_t val = io_read32(gpc_base() + GPC_CPU_PGC_SW_PDN_REQ);
-
-	imx_gpcv2_set_core_pgc(true, GPC_PGC_C1);
-
-	val |= GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK;
-
-	io_write32(gpc_base() + GPC_CPU_PGC_SW_PDN_REQ, val);
-
-	while ((io_read32(gpc_base() + GPC_CPU_PGC_SW_PDN_REQ) &
-	       GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK) != 0)
-		;
-
-	imx_gpcv2_set_core_pgc(false, GPC_PGC_C1);
+		io_setbits32(va + offset, GPC_PGC_PCG_MASK);
+	else
+		io_clrbits32(va + offset, GPC_PGC_PCG_MASK);
 }
 
 void imx_gpcv2_set_core1_pup_by_software(void)
 {
-	uint32_t val = io_read32(gpc_base() + GPC_CPU_PGC_SW_PUP_REQ);
+	vaddr_t va = core_mmu_get_va(GPC_BASE, MEM_AREA_IO_SEC, GPC_SIZE);
+	uint64_t timeout = timeout_init_us(10 * 1000);
 
 	imx_gpcv2_set_core_pgc(true, GPC_PGC_C1);
 
-	val |= GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK;
+	io_setbits32(va + GPC_CPU_PGC_SW_PUP_REQ,
+		     GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK);
 
-	io_write32(gpc_base() + GPC_CPU_PGC_SW_PUP_REQ, val);
-
-	while ((io_read32(gpc_base() + GPC_CPU_PGC_SW_PUP_REQ) &
-	       GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK) != 0)
-		;
+	while ((io_read32(va + GPC_CPU_PGC_SW_PUP_REQ) &
+		GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK)) {
+		if (timeout_elapsed(timeout))
+			return;
+	}
 
 	imx_gpcv2_set_core_pgc(false, GPC_PGC_C1);
 }
