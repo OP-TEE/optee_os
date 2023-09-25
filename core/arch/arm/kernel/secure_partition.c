OP-TEE/optee_os

commit ef44161f847bb1f17e6c803e70047d749e7b394e

core: update ts_store API with user space buffer
Updates the read() function pointer in struct ts_store_ops to take an
user space buffer in addition to the previous core buffer. Core buffers
are normal secure memory while user space buffers should only be accessed
using the user_access.h functions.

The different TA storage implementations are updated accordingly.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/kernel/secure_partition.c
@@ -471,7 +471,7 @@ static TEE_Result load_binary_sp(struct ts_session *s,
 		goto err_free_mobj;
 
 	/* Read SP binary into the previously mapped memory area */
-	res = store_ops->read(handle, (void *)va, bin_size);
+	res = store_ops->read(handle, NULL, (void *)va, bin_size);
 	if (res)
 		goto err_unmap;
 

core/include/kernel/embedded_ts.h
@@ -22,7 +22,8 @@ struct embedded_ts {
 
 struct ts_store_handle;
 
-TEE_Result emb_ts_read(struct ts_store_handle *h, void *data, size_t len);
+TEE_Result emb_ts_read(struct ts_store_handle *h, void *data_core,
+		       void *data_user, size_t len);
 void emb_ts_close(struct ts_store_handle *h);
 
 TEE_Result emb_ts_open(const TEE_UUID *uuid,

core/include/kernel/ts_store.h
@@ -40,12 +40,17 @@ struct ts_store_ops {
 	 * unless an error occurs. Therefore, an implementation may rely on the
 	 * condition (current offset == total size) to detect the last call to
 	 * this function.
-	 * @data: pointer to secure memory where the TS bytes should be copied.
-	 * If @data == NULL and @len != 0, the function should just skip @len
-	 * bytes.
+	 * @data_core: pointer to secure memory where the TS bytes should be
+	 *             copied.
+	 * @data_user: pointer to user memory where the TS bytes should be
+	 *             copied.
+	 * At least one of @data_core and @data_user are normally NULL, but
+	 * both are also permitted to be non-NULL.
+	 * If @data_core == NULL and @data_user == NULL and @len != 0, the
+	 * function should just skip @len bytes.
 	 */
-	TEE_Result (*read)(struct ts_store_handle *h, void *data,
-			   size_t len);
+	TEE_Result (*read)(struct ts_store_handle *h, void *data_core,
+			   void *data_user, size_t len);
 	/*
 	 * Close a TS handle. Do nothing if @h == NULL.
 	 */

core/include/tee/tadb.h
@@ -58,8 +58,8 @@ const struct tee_tadb_property *
 tee_tadb_ta_get_property(struct tee_tadb_ta_read *ta);
 TEE_Result tee_tadb_get_tag(struct tee_tadb_ta_read *ta, uint8_t *tag,
 			    unsigned int *tag_len);
-TEE_Result tee_tadb_ta_read(struct tee_tadb_ta_read *ta, void *buf,
-			    size_t *len);
+TEE_Result tee_tadb_ta_read(struct tee_tadb_ta_read *ta, void *buf_core,
+			    void *buf_user, size_t *len);
 void tee_tadb_ta_close(struct tee_tadb_ta_read *ta);
 
 

core/kernel/embedded_ts.c
@@ -7,6 +7,7 @@
 #include <initcall.h>
 #include <kernel/embedded_ts.h>
 #include <kernel/ts_store.h>
+#include <kernel/user_access.h>
 #include <mempool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -118,49 +119,48 @@ out:
 	return res;
 }
 
-static TEE_Result read_uncompressed(struct ts_store_handle *h, void *data,
-				    size_t len)
+static TEE_Result read_uncompressed(struct ts_store_handle *h, void *data_core,
+				    void *data_user, size_t len)
 {
+	TEE_Result res = TEE_SUCCESS;
 	uint8_t *src = (uint8_t *)h->ts->ts + h->offs;
 	size_t next_offs = 0;
 
 	if (ADD_OVERFLOW(h->offs, len, &next_offs) ||
 	    next_offs > h->ts->size)
 		return TEE_ERROR_BAD_PARAMETERS;
-	if (data)
-		memcpy(data, src, len);
-	h->offs = next_offs;
+	if (data_core)
+		memcpy(data_core, src, len);
+	if (data_user)
+		res = copy_to_user(data_user, src, len);
+	if (!res)
+		h->offs = next_offs;
 
-	return TEE_SUCCESS;
+	return res;
 }
 
-static TEE_Result read_compressed(struct ts_store_handle *h, void *data,
-				  size_t len)
+static TEE_Result read_compressed(struct ts_store_handle *h, void *data_core,
+				  void *data_user, size_t len)
 {
+	TEE_Result res = TEE_SUCCESS;
 	z_stream *strm = &h->strm;
 	size_t total = 0;
-	uint8_t *tmpbuf = NULL;
-	TEE_Result ret = TEE_SUCCESS;
+	uint8_t *bb = NULL;
+	size_t bb_len = 0;
 	size_t out = 0;
 	int st = Z_OK;
 
-	if (data) {
-		strm->next_out = data;
-		strm->avail_out = len;
-	} else {
-		/*
-		 * inflate() does not support a NULL strm->next_out. So, to
-		 * discard data, we have to allocate a temporary buffer. 1K
-		 * seems reasonable.
-		 */
-		strm->avail_out = MIN(len, 1024U);
-		tmpbuf = malloc(strm->avail_out);
-		if (!tmpbuf) {
-			EMSG("Out of memory");
-			return TEE_ERROR_OUT_OF_MEMORY;
-		}
-		strm->next_out = tmpbuf;
+	/* Inflate into a 1kB bounce buffer */
+	bb_len = MIN(len, 1024U);
+	bb = bb_alloc(bb_len);
+	if (!bb) {
+		EMSG("Out of memory");
+		return TEE_ERROR_OUT_OF_MEMORY;
 	}
+
+	strm->avail_out = bb_len;
+	strm->next_out = bb;
+
 	/*
 	 * Loop until we get as many bytes as requested, or an error occurs.
 	 * inflate() returns:
@@ -175,36 +175,43 @@ static TEE_Result read_compressed(struct ts_store_handle *h, void *data,
 		out = strm->total_out;
 		st = inflate(strm, Z_SYNC_FLUSH);
 		out = strm->total_out - out;
-		total += out;
 		FMSG("%zu bytes", out);
-		if (!data) {
-			/*
-			 * Reset the pointer to throw away what we've just read
-			 * and read again as much as possible.
-			 */
-			strm->next_out = tmpbuf;
-			strm->avail_out = MIN(len - total, 1024U);
+		if (data_core)
+			memcpy((uint8_t *)data_core + total, bb, out);
+		if (data_user) {
+			res = copy_to_user((uint8_t *)data_user + total, bb,
+					   out);
+			if (res)
+				goto out;
 		}
+		total += out;
+		/*
+		 * Reset the pointer since we've just copied out the last
+		 * data.
+		 */
+		strm->next_out = bb;
+		strm->avail_out = MIN(len - total, bb_len);
 	} while ((st == Z_OK || st == Z_BUF_ERROR) && (total != len));
 
 	if (st != Z_OK && st != Z_STREAM_END) {
 		EMSG("Decompression error (%d)", st);
-		ret = TEE_ERROR_GENERIC;
+		res = TEE_ERROR_GENERIC;
 		goto out;
 	}
-	ret = TEE_SUCCESS;
+	res = TEE_SUCCESS;
 out:
-	free(tmpbuf);
+	bb_free(bb, bb_len);
 
-	return ret;
+	return res;
 }
 
-TEE_Result emb_ts_read(struct ts_store_handle *h, void *data, size_t len)
+TEE_Result emb_ts_read(struct ts_store_handle *h, void *data_core,
+		       void *data_user, size_t len)
 {
 	if (h->ts->uncompressed_size)
-		return read_compressed(h, data, len);
+		return read_compressed(h, data_core, data_user, len);
 	else
-		return read_uncompressed(h, data, len);
+		return read_uncompressed(h, data_core, data_user, len);
 }
 
 void emb_ts_close(struct ts_store_handle *h)

core/kernel/ldelf_syscalls.c
@@ -206,7 +206,7 @@ TEE_Result ldelf_syscall_close_bin(unsigned long handle)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	if (binh->offs_bytes < binh->size_bytes)
-		res = binh->op->read(binh->h, NULL,
+		res = binh->op->read(binh->h, NULL, NULL,
 				     binh->size_bytes - binh->offs_bytes);
 
 	bin_close(binh);
@@ -219,8 +219,9 @@ TEE_Result ldelf_syscall_close_bin(unsigned long handle)
 	return res;
 }
 
-static TEE_Result binh_copy_to(struct bin_handle *binh, vaddr_t va,
-			       size_t offs_bytes, size_t num_bytes)
+static TEE_Result binh_copy_to(struct bin_handle *binh, vaddr_t va_core,
+			       vaddr_t va_user, size_t offs_bytes,
+			       size_t num_bytes)
 {
 	TEE_Result res = TEE_SUCCESS;
 	size_t next_offs = 0;
@@ -232,7 +233,7 @@ static TEE_Result binh_copy_to(struct bin_handle *binh, vaddr_t va,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	if (offs_bytes > binh->offs_bytes) {
-		res = binh->op->read(binh->h, NULL,
+		res = binh->op->read(binh->h, NULL, NULL,
 				     offs_bytes - binh->offs_bytes);
 		if (res)
 			return res;
@@ -242,13 +243,22 @@ static TEE_Result binh_copy_to(struct bin_handle *binh, vaddr_t va,
 	if (next_offs > binh->size_bytes) {
 		size_t rb = binh->size_bytes - binh->offs_bytes;
 
-		res = binh->op->read(binh->h, (void *)va, rb);
+		res = binh->op->read(binh->h, (void *)va_core,
+				     (void *)va_user, rb);
 		if (res)
 			return res;
-		memset((uint8_t *)va + rb, 0, num_bytes - rb);
+		if (va_core)
+			memset((uint8_t *)va_core + rb, 0, num_bytes - rb);
+		if (va_user) {
+			res = clear_user((uint8_t *)va_user + rb,
+					 num_bytes - rb);
+			if (res)
+				return res;
+		}
 		binh->offs_bytes = binh->size_bytes;
 	} else {
-		res = binh->op->read(binh->h, (void *)va, num_bytes);
+		res = binh->op->read(binh->h, (void *)va_core,
+				     (void *)va_user, num_bytes);
 		if (res)
 			return res;
 		binh->offs_bytes = next_offs;
@@ -378,7 +388,7 @@ TEE_Result ldelf_syscall_map_bin(vaddr_t *va, size_t num_bytes,
 		mobj_put(mobj);
 		if (res)
 			goto err;
-		res = binh_copy_to(binh, *va, offs_bytes, num_bytes);
+		res = binh_copy_to(binh, *va, 0, offs_bytes, num_bytes);
 		if (res)
 			goto err_unmap_va;
 		res = vm_set_prot(uctx, *va, num_rounded_bytes,
@@ -443,7 +453,7 @@ TEE_Result ldelf_syscall_copy_from_bin(void *dst, size_t offs, size_t num_bytes,
 	if (!binh)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	return binh_copy_to(binh, (vaddr_t)dst, offs, num_bytes);
+	return binh_copy_to(binh, 0, (vaddr_t)dst, offs, num_bytes);
 }
 
 TEE_Result ldelf_syscall_set_prot(unsigned long va, size_t num_bytes,

core/kernel/ree_fs_ta.c
@@ -41,6 +41,7 @@
 #include <initcall.h>
 #include <kernel/thread.h>
 #include <kernel/ts_store.h>
+#include <kernel/user_access.h>
 #include <mm/core_memprot.h>
 #include <mm/mobj.h>
 #include <mm/tee_mm.h>
@@ -542,64 +543,68 @@ out:
 	return res;
 }
 
-static TEE_Result ree_fs_ta_read(struct ts_store_handle *h, void *data,
-				 size_t len)
+static TEE_Result ree_fs_ta_read(struct ts_store_handle *h, void *data_core,
+				 void *data_user, size_t len)
 {
 	struct ree_fs_ta_handle *handle = (struct ree_fs_ta_handle *)h;
-
 	uint8_t *src = (uint8_t *)handle->nw_ta + handle->offs;
+	size_t bb_len = MIN(1024U, len);
 	size_t next_offs = 0;
-	uint8_t *dst = src;
 	TEE_Result res = TEE_SUCCESS;
+	size_t num_bytes = 0;
+	size_t dst_len = 0;
+	void *dst = NULL;
+	void *bb = NULL;
+
 
 	if (ADD_OVERFLOW(handle->offs, len, &next_offs) ||
 	    next_offs > handle->nw_ta_size)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	if (handle->shdr->img_type == SHDR_ENCRYPTED_TA) {
-		if (data) {
-			dst = data; /* Hash secure buffer */
-			res = tee_ta_decrypt_update(handle->enc_ctx, dst, src,
-						    len);
-			if (res != TEE_SUCCESS)
-				return TEE_ERROR_SECURITY;
-		} else {
-			size_t num_bytes = 0;
-			size_t b_size = MIN(1024U, len);
-			uint8_t *b = malloc(b_size);
-
-			if (!b)
-				return TEE_ERROR_OUT_OF_MEMORY;
-
-			dst = NULL;
-			while (num_bytes < len) {
-				size_t n = MIN(b_size, len - num_bytes);
-
-				res = tee_ta_decrypt_update(handle->enc_ctx, b,
-							    src + num_bytes, n);
-				if (res)
-					break;
-				num_bytes += n;
-
-				res = crypto_hash_update(handle->hash_ctx, b,
-							 n);
-				if (res)
-					break;
-			}
-
-			free(b);
-			if (res != TEE_SUCCESS)
-				return TEE_ERROR_SECURITY;
-		}
-	} else if (data) {
-		dst = data; /* Hash secure buffer (shm might be modified) */
-		memcpy(dst, src, len);
+	if (data_core) {
+		dst = data_core;
+		dst_len = len;
+	} else {
+		bb = bb_alloc(bb_len);
+		if (!bb)
+			return TEE_ERROR_OUT_OF_MEMORY;
+		dst = bb;
+		dst_len = bb_len;
 	}
 
-	if (dst) {
-		res = crypto_hash_update(handle->hash_ctx, dst, len);
-		if (res != TEE_SUCCESS)
-			return TEE_ERROR_SECURITY;
+	/*
+	 * This loop will only run once if data_core is non-NULL, but as
+	 * many times as needed if the bounce buffer bb is used. That's why
+	 * dst doesn't need to be updated in the loop.
+	 */
+	while (num_bytes < len) {
+		size_t n = MIN(dst_len, len - num_bytes);
+
+		if (handle->shdr->img_type == SHDR_ENCRYPTED_TA) {
+			res = tee_ta_decrypt_update(handle->enc_ctx, dst,
+						    src + num_bytes, n);
+			if (res) {
+				res = TEE_ERROR_SECURITY;
+				goto out;
+			}
+		} else {
+			memcpy(dst, src + num_bytes, n);
+		}
+
+		res = crypto_hash_update(handle->hash_ctx, dst, n);
+		if (res) {
+			res = TEE_ERROR_SECURITY;
+			goto out;
+		}
+		if (data_user) {
+			res = copy_to_user((uint8_t *)data_user + num_bytes,
+					   dst, n);
+			if (res) {
+				res = TEE_ERROR_SECURITY;
+				goto out;
+			}
+		}
+		num_bytes += n;
 	}
 
 	handle->offs = next_offs;
@@ -611,8 +616,10 @@ static TEE_Result ree_fs_ta_read(struct ts_store_handle *h, void *data,
 			 */
 			res = tee_ta_decrypt_final(handle->enc_ctx,
 						   handle->ehdr, NULL, NULL, 0);
-			if (res != TEE_SUCCESS)
-				return TEE_ERROR_SECURITY;
+			if (res != TEE_SUCCESS) {
+				res = TEE_ERROR_SECURITY;
+				goto out;
+			}
 		}
 		/*
 		 * Last read: time to check if our digest matches the expected
@@ -620,13 +627,15 @@ static TEE_Result ree_fs_ta_read(struct ts_store_handle *h, void *data,
 		 */
 		res = check_digest(handle);
 		if (res != TEE_SUCCESS)
-			return res;
+			goto out;
 
 		if (handle->bs_hdr)
 			res = check_update_version(ta_ver_db,
 						   handle->bs_hdr->uuid,
 						   handle->bs_hdr->ta_version);
 	}
+out:
+	bb_free(bb, bb_len);
 	return res;
 }
 
@@ -724,7 +733,7 @@ static TEE_Result buf_ta_open(const TEE_UUID *uuid,
 	}
 
 	FTMN_PUSH_LINKED_CALL(&ftmn, FTMN_FUNC_HASH("check_digest"));
-	res = ree_fs_ta_read(handle->h, handle->buf, handle->ta_size);
+	res = ree_fs_ta_read(handle->h, handle->buf, NULL, handle->ta_size);
 	if (!res)
 		FTMN_SET_CHECK_RES_FROM_CALL(&ftmn, FTMN_INCR0, res);
 	FTMN_POP_LINKED_CALL(&ftmn);
@@ -754,19 +763,25 @@ static TEE_Result buf_ta_get_size(const struct ts_store_handle *h,
 	return TEE_SUCCESS;
 }
 
-static TEE_Result buf_ta_read(struct ts_store_handle *h, void *data,
-			      size_t len)
+static TEE_Result buf_ta_read(struct ts_store_handle *h, void *data_core,
+			      void *data_user, size_t len)
 {
 	struct buf_ree_fs_ta_handle *handle = (struct buf_ree_fs_ta_handle *)h;
 	uint8_t *src = handle->buf + handle->offs;
+	TEE_Result res = TEE_SUCCESS;
 	size_t next_offs = 0;
 
 	if (ADD_OVERFLOW(handle->offs, len, &next_offs) ||
 	    next_offs > handle->ta_size)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	if (data)
-		memcpy(data, src, len);
+	if (data_core)
+		memcpy(data_core, src, len);
+	if (data_user) {
+		res = copy_to_user(data_user, src, len);
+		if (res)
+			return res;
+	}
 	handle->offs = next_offs;
 	return TEE_SUCCESS;
 }

core/kernel/secstor_ta.c
@@ -22,7 +22,7 @@ static TEE_Result secstor_ta_open(const TEE_UUID *uuid,
 	prop = tee_tadb_ta_get_property(ta);
 
 	l = prop->custom_size;
-	res = tee_tadb_ta_read(ta, NULL, &l);
+	res = tee_tadb_ta_read(ta, NULL, NULL, &l);
 	if (res)
 		goto err;
 	if (l != prop->custom_size) {
@@ -55,12 +55,12 @@ static TEE_Result secstor_ta_get_tag(const struct ts_store_handle *h,
 	return tee_tadb_get_tag((struct tee_tadb_ta_read *)h, tag, tag_len);
 }
 
-static TEE_Result secstor_ta_read(struct ts_store_handle *h, void *data,
-				  size_t len)
+static TEE_Result secstor_ta_read(struct ts_store_handle *h, void *data_core,
+				  void *data_user, size_t len)
 {
 	struct tee_tadb_ta_read *ta = (struct tee_tadb_ta_read *)h;
 	size_t l = len;
-	TEE_Result res = tee_tadb_ta_read(ta, data, &l);
+	TEE_Result res = tee_tadb_ta_read(ta, data_core, data_user, &l);
 
 	if (res)
 		return res;

commit c4694772bff910ad5a29dfc8354e439e09178583

core: sp: fix raw binary format SP loader
Loading a compressed raw binary format SP fails when read_compressed()
in embedded_ts.c is trying to allocate memory using bb_alloc(), since
the bounce buffer in this user_mode_ctx is uninitialized. For ELF format
SPs ldelf is taking care of this, let's add the necessary initialization
to the raw binary format loader too.

Fixes: ef44161f847b ("core: update ts_store API with user space buffer")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Balint Dobszay <balint.dobszay@arm.com>

core/arch/arm/kernel/secure_partition.c
@@ -32,6 +32,8 @@
 #include <util.h>
 #include <zlib.h>
 
+#define BOUNCE_BUFFER_SIZE		4096
+
 #define SP_MANIFEST_ATTR_READ		BIT(0)
 #define SP_MANIFEST_ATTR_WRITE		BIT(1)
 #define SP_MANIFEST_ATTR_EXEC		BIT(2)
@@ -408,10 +410,13 @@ static TEE_Result load_binary_sp(struct ts_session *s,
 				 struct user_mode_ctx *uctx)
 {
 	size_t bin_size = 0, bin_size_rounded = 0, bin_page_count = 0;
+	size_t bb_size = ROUNDUP(BOUNCE_BUFFER_SIZE, SMALL_PAGE_SIZE);
+	size_t bb_num_pages = bb_size / SMALL_PAGE_SIZE;
 	const struct ts_store_ops *store_ops = NULL;
 	struct ts_store_handle *handle = NULL;
 	TEE_Result res = TEE_SUCCESS;
 	tee_mm_entry_t *mm = NULL;
+	struct fobj *fobj = NULL;
 	struct mobj *mobj = NULL;
 	uaddr_t base_addr = 0;
 	uint32_t vm_flags = 0;
@@ -422,6 +427,21 @@ static TEE_Result load_binary_sp(struct ts_session *s,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	DMSG("Loading raw binary format SP %pUl", &uctx->ts_ctx->uuid);
+
+	/* Initialize the bounce buffer */
+	fobj = fobj_sec_mem_alloc(bb_num_pages);
+	mobj = mobj_with_fobj_alloc(fobj, NULL, TEE_MATTR_MEM_TYPE_TAGGED);
+	fobj_put(fobj);
+	if (!mobj)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	res = vm_map(uctx, &va, bb_size, TEE_MATTR_PRW, 0, mobj, 0);
+	mobj_put(mobj);
+	if (res)
+		return res;
+
+	uctx->bbuf = (uint8_t *)va;
+	uctx->bbuf_size = BOUNCE_BUFFER_SIZE;
 
 	vm_set_ctx(uctx->ts_ctx);
 
@@ -465,6 +485,7 @@ static TEE_Result load_binary_sp(struct ts_session *s,
 		goto err_free_mobj;
 
 	/* Map memory area for the SP binary */
+	va = 0;
 	res = vm_map(uctx, &va, bin_size_rounded, TEE_MATTR_URWX,
 		     vm_flags, mobj, 0);
 	if (res)