OP-TEE/optee_os

commit c02f9fb09df2b5b82748b17ae6a7dcc3998b91b9

arm: add auto generated asm-defines.h
Adds defines for assembly access to:
* struct thread_ctx
* struct thread_user_mode_rec
* struct thread_core_local
* struct thread_smc_args
* struct thread_abort_regs
* struct thread_user_mode_rec
as needed from assembly code replacing previous definitions in
kernel/thread.h and thread_private.h.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (HiKey)
Tested-by: Jens Wiklander <jens.wiklander@linaro.org> (QEMU, FVP)
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/asm-defines.c
@@ -25,6 +25,9 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <types_ext.h>
+#include <kernel/thread.h>
+#include "thread_private.h"
 
 #define DEFINES void __defines(void); void __defines(void)
 
@@ -33,4 +36,60 @@
 
 DEFINES
 {
+#ifdef ARM32
+	DEFINE(THREAD_SVC_REG_R0, offsetof(struct thread_svc_regs, r0));
+	DEFINE(THREAD_SVC_REG_R5, offsetof(struct thread_svc_regs, r5));
+	DEFINE(THREAD_SVC_REG_R6, offsetof(struct thread_svc_regs, r6));
+#endif /*ARM32*/
+
+#ifdef ARM64
+	DEFINE(THREAD_SMC_ARGS_X0, offsetof(struct thread_smc_args, a0));
+	DEFINE(THREAD_SMC_ARGS_SIZE, sizeof(struct thread_smc_args));
+
+	DEFINE(THREAD_SVC_REG_X0, offsetof(struct thread_svc_regs, x0));
+	DEFINE(THREAD_SVC_REG_X5, offsetof(struct thread_svc_regs, x5));
+	DEFINE(THREAD_SVC_REG_X6, offsetof(struct thread_svc_regs, x6));
+	DEFINE(THREAD_SVC_REG_X30, offsetof(struct thread_svc_regs, x30));
+	DEFINE(THREAD_SVC_REG_ELR, offsetof(struct thread_svc_regs, elr));
+	DEFINE(THREAD_SVC_REG_SPSR, offsetof(struct thread_svc_regs, spsr));
+	DEFINE(THREAD_SVC_REG_SP_EL0, offsetof(struct thread_svc_regs, sp_el0));
+	DEFINE(THREAD_SVC_REG_SIZE, sizeof(struct thread_svc_regs));
+
+	/* struct thread_abort_regs */
+	DEFINE(THREAD_ABT_REG_X0, offsetof(struct thread_abort_regs, x0));
+	DEFINE(THREAD_ABT_REG_X2, offsetof(struct thread_abort_regs, x2));
+	DEFINE(THREAD_ABT_REG_X30, offsetof(struct thread_abort_regs, x30));
+	DEFINE(THREAD_ABT_REG_SPSR, offsetof(struct thread_abort_regs, spsr));
+	DEFINE(THREAD_ABT_REGS_SIZE, sizeof(struct thread_abort_regs));
+
+	/* struct thread_ctx */
+	DEFINE(THREAD_CTX_KERN_SP, offsetof(struct thread_ctx, kern_sp));
+	DEFINE(THREAD_CTX_SIZE, sizeof(struct thread_ctx));
+
+	/* struct thread_ctx_regs */
+	DEFINE(THREAD_CTX_REGS_SP, offsetof(struct thread_ctx_regs, sp));
+	DEFINE(THREAD_CTX_REGS_X0, offsetof(struct thread_ctx_regs, x[0]));
+	DEFINE(THREAD_CTX_REGS_X1, offsetof(struct thread_ctx_regs, x[1]));
+	DEFINE(THREAD_CTX_REGS_X4, offsetof(struct thread_ctx_regs, x[4]));
+	DEFINE(THREAD_CTX_REGS_X19, offsetof(struct thread_ctx_regs, x[19]));
+
+	/* struct thread_user_mode_rec */
+	DEFINE(THREAD_USER_MODE_REC_EXIT_STATUS0_PTR,
+		offsetof(struct thread_user_mode_rec, exit_status0_ptr));
+	DEFINE(THREAD_USER_MODE_REC_X19,
+		offsetof(struct thread_user_mode_rec, x[0]));
+	DEFINE(THREAD_USER_MODE_REC_SIZE, sizeof(struct thread_user_mode_rec));
+
+	/* struct thread_core_local */
+	DEFINE(THREAD_CORE_LOCAL_TMP_STACK_VA_END,
+		offsetof(struct thread_core_local, tmp_stack_va_end));
+	DEFINE(THREAD_CORE_LOCAL_CURR_THREAD,
+		offsetof(struct thread_core_local, curr_thread));
+	DEFINE(THREAD_CORE_LOCAL_FLAGS,
+		offsetof(struct thread_core_local, flags));
+	DEFINE(THREAD_CORE_LOCAL_ABT_STACK_VA_END,
+		offsetof(struct thread_core_local, abt_stack_va_end));
+	DEFINE(THREAD_CORE_LOCAL_X0, offsetof(struct thread_core_local, x[0]));
+	DEFINE(THREAD_CORE_LOCAL_X2, offsetof(struct thread_core_local, x[2]));
+#endif /*ARM64*/
 }

core/arch/arm/include/kernel/thread.h
@@ -174,43 +174,6 @@ struct thread_svc_regs {
 #endif /*ARM64*/
 #endif /*ASM*/
 
-
-/*
- * Correctness of these defines are asserted with COMPILE_TIME_ASSERT in
- * thread_init_handlers().
- */
-#ifdef ARM32
-#define THREAD_SVC_REG_SPSR_OFFS	(0 * 4)
-#define THREAD_SVC_REG_R0_OFFS		(1 * 4)
-#define THREAD_SVC_REG_R1_OFFS		(2 * 4)
-#define THREAD_SVC_REG_R2_OFFS		(3 * 4)
-#define THREAD_SVC_REG_R3_OFFS		(4 * 4)
-#define THREAD_SVC_REG_R4_OFFS		(5 * 4)
-#define THREAD_SVC_REG_R5_OFFS		(6 * 4)
-#define THREAD_SVC_REG_R6_OFFS		(7 * 4)
-#define THREAD_SVC_REG_R7_OFFS		(8 * 4)
-#define THREAD_SVC_REG_LR_OFFS		(9 * 4)
-#endif /*ARM32*/
-
-#ifdef ARM64
-#define THREAD_ABT_REG_X_OFFS(x)	((x) * 8)
-#define THREAD_ABT_REG_ELR_OFFS		(THREAD_ABT_REG_X_OFFS(30) + 1 * 8)
-#define THREAD_ABT_REG_SPSR_OFFS	(THREAD_ABT_REG_X_OFFS(30) + 2 * 8)
-#define THREAD_ABT_REG_SP_EL0_OFFS	(THREAD_ABT_REG_X_OFFS(30) + 3 * 8)
-#define THREAD_ABT_REGS_SIZE		(THREAD_ABT_REG_X_OFFS(30) + 4 * 8)
-
-#define THREAD_SMC_ARGS_X_OFFS(x)	((x) * 8)
-#define THREAD_SMC_ARGS_SIZE		THREAD_SMC_ARGS_X_OFFS(8)
-
-#define THREAD_SVC_REG_ELR_OFFS		(8 * 0)
-#define THREAD_SVC_REG_SPSR_OFFS	(8 * 1)
-#define THREAD_SVC_REG_X_OFFS(x)	(8 * (2 + (x)))
-#define THREAD_SVC_REG_X30_OFFS		THREAD_SVC_REG_X_OFFS(15)
-#define THREAD_SVC_REG_SP_EL0_OFFS	THREAD_SVC_REG_X_OFFS(16)
-#define THREAD_SVC_REG_SIZE		THREAD_SVC_REG_X_OFFS(18)
-
-#endif /*ARM64*/
-
 #ifndef ASM
 typedef void (*thread_smc_handler_t)(struct thread_smc_args *args);
 typedef void (*thread_fiq_handler_t)(void);

core/arch/arm/kernel/thread.c
@@ -781,97 +781,6 @@ static void init_thread_stacks(void)
 
 void thread_init_primary(const struct thread_handlers *handlers)
 {
-	/*
-	 * The COMPILE_TIME_ASSERT only works in function context. These
-	 * checks verifies that the offsets used in assembly code matches
-	 * what's used in C code.
-	 */
-#ifdef ARM32
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r0) ==
-				THREAD_SVC_REG_R0_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r1) ==
-				THREAD_SVC_REG_R1_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r2) ==
-				THREAD_SVC_REG_R2_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r3) ==
-				THREAD_SVC_REG_R3_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r4) ==
-				THREAD_SVC_REG_R4_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r5) ==
-				THREAD_SVC_REG_R5_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r6) ==
-				THREAD_SVC_REG_R6_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, r7) ==
-				THREAD_SVC_REG_R7_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, lr) ==
-				THREAD_SVC_REG_LR_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_svc_regs, spsr) ==
-				THREAD_SVC_REG_SPSR_OFFS);
-#endif /*ARM32*/
-#ifdef ARM64
-	/* struct thread_abort_regs */
-	COMPILE_TIME_ASSERT(offsetof(struct thread_abort_regs, x22) ==
-			    THREAD_ABT_REG_X_OFFS(22));
-	COMPILE_TIME_ASSERT(offsetof(struct thread_abort_regs, elr) ==
-			    THREAD_ABT_REG_ELR_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_abort_regs, spsr) ==
-			    THREAD_ABT_REG_SPSR_OFFS);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_abort_regs, sp_el0) ==
-			    THREAD_ABT_REG_SP_EL0_OFFS);
-	COMPILE_TIME_ASSERT(sizeof(struct thread_abort_regs) ==
-			    THREAD_ABT_REGS_SIZE);
-
-	/* struct thread_ctx */
-	COMPILE_TIME_ASSERT(offsetof(struct thread_ctx, kern_sp) ==
-			    THREAD_CTX_KERN_SP_OFFSET);
-	COMPILE_TIME_ASSERT(sizeof(struct thread_ctx) == THREAD_CTX_SIZE);
-
-	/* struct thread_ctx_regs */
-	COMPILE_TIME_ASSERT(offsetof(struct thread_ctx_regs, sp) ==
-			    THREAD_CTX_REGS_SP_OFFSET);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_ctx_regs, pc) ==
-			    THREAD_CTX_REGS_PC_OFFSET);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_ctx_regs, cpsr) ==
-			    THREAD_CTX_REGS_SPSR_OFFSET);
-	COMPILE_TIME_ASSERT(offsetof(struct thread_ctx_regs, x[23]) ==
-			    THREAD_CTX_REGS_X_OFFSET(23));
-	COMPILE_TIME_ASSERT(sizeof(struct thread_ctx_regs) ==
-			    THREAD_CTX_REGS_SIZE);
-
-	/* struct thread_user_mode_rec */
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_user_mode_rec, exit_status0_ptr) ==
-		THREAD_USER_MODE_REC_EXIT_STATUS0_PTR_OFFSET);
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_user_mode_rec, exit_status1_ptr) ==
-		THREAD_USER_MODE_REC_EXIT_STATUS1_PTR_OFFSET);
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_user_mode_rec, x[1]) ==
-		THREAD_USER_MODE_REC_X_OFFSET(20));
-	COMPILE_TIME_ASSERT(sizeof(struct thread_user_mode_rec) ==
-			    THREAD_USER_MODE_REC_SIZE);
-
-	/* struct thread_core_local */
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_core_local, tmp_stack_va_end) ==
-		THREAD_CORE_LOCAL_TMP_STACK_VA_END_OFFSET);
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_core_local, curr_thread) ==
-		THREAD_CORE_LOCAL_CURR_THREAD_OFFSET);
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_core_local, flags) ==
-		THREAD_CORE_LOCAL_FLAGS_OFFSET);
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_core_local, abt_stack_va_end) ==
-		THREAD_CORE_LOCAL_ABT_STACK_VA_END_OFFSET);
-	COMPILE_TIME_ASSERT(
-		offsetof(struct thread_core_local, x[3]) ==
-		THREAD_CORE_LOCAL_X_OFFSET(3));
-	COMPILE_TIME_ASSERT(sizeof(struct thread_core_local) ==
-		THREAD_CORE_LOCAL_SIZE);
-
-#endif /*ARM64*/
-
 	init_handlers(handlers);
 
 	/* Initialize canaries around the stacks */

core/arch/arm/kernel/thread_a64.S
@@ -31,13 +31,13 @@
 #include <sm/teesmc.h>
 #include <sm/teesmc_opteed_macros.h>
 #include <sm/teesmc_opteed.h>
+#include <asm-defines.h>
 #include <kernel/thread_defs.h>
-#include <kernel/thread.h>
 #include "thread_private.h"
 
 	.macro get_thread_ctx core_local, res, tmp0, tmp1
 		ldr	w\tmp0, [\core_local, \
-				#THREAD_CORE_LOCAL_CURR_THREAD_OFFSET]
+				#THREAD_CORE_LOCAL_CURR_THREAD]
 		adr	x\res, threads
 		mov	x\tmp1, #THREAD_CTX_SIZE
 		madd	x\res, x\tmp0, x\tmp1, x\res
@@ -46,7 +46,7 @@
 	.section .text.thread_asm
 LOCAL_FUNC vector_std_smc_entry , :
 	sub	sp, sp, #THREAD_SMC_ARGS_SIZE
-	store_xregs sp, THREAD_SMC_ARGS_X_OFFS(0), 0, 7
+	store_xregs sp, THREAD_SMC_ARGS_X0, 0, 7
 	mov	x0, sp
 	bl	thread_handle_std_smc
 	/*
@@ -55,7 +55,7 @@ LOCAL_FUNC vector_std_smc_entry , :
 	 * hasn't switched stack (error detected) it will do a normal "C"
 	 * return.
 	 */
-	load_xregs sp, THREAD_SMC_ARGS_X_OFFS(0), 1, 8
+	load_xregs sp, THREAD_SMC_ARGS_X0, 1, 8
 	add	sp, sp, #THREAD_SMC_ARGS_SIZE
 	ldr	x0, =TEESMC_OPTEED_RETURN_CALL_DONE
 	smc	#0
@@ -64,10 +64,10 @@ END_FUNC vector_std_smc_entry
 
 LOCAL_FUNC vector_fast_smc_entry , :
 	sub	sp, sp, #THREAD_SMC_ARGS_SIZE
-	store_xregs sp, THREAD_SMC_ARGS_X_OFFS(0), 0, 7
+	store_xregs sp, THREAD_SMC_ARGS_X0, 0, 7
 	mov	x0, sp
 	bl	thread_handle_fast_smc
-	load_xregs sp, THREAD_SMC_ARGS_X_OFFS(0), 1, 8
+	load_xregs sp, THREAD_SMC_ARGS_X0, 1, 8
 	add	sp, sp, #THREAD_SMC_ARGS_SIZE
 	ldr	x0, =TEESMC_OPTEED_RETURN_CALL_DONE
 	smc	#0
@@ -167,19 +167,19 @@ END_FUNC thread_vector_table
 
 /* void thread_resume(struct thread_ctx_regs *regs) */
 FUNC thread_resume , :
-	load_xregs x0, THREAD_CTX_REGS_SP_OFFSET, 1, 3
+	load_xregs x0, THREAD_CTX_REGS_SP, 1, 3
 	mov	sp, x1
 	msr	elr_el1, x2
 	msr	spsr_el1, x3
-	load_xregs x0, THREAD_CTX_REGS_X_OFFSET(1), 1, 30
-	ldr	x0, [x0, THREAD_CTX_REGS_X_OFFSET(0)]
+	load_xregs x0, THREAD_CTX_REGS_X1, 1, 30
+	ldr	x0, [x0, THREAD_CTX_REGS_X0]
 	eret
 END_FUNC thread_resume
 
 FUNC thread_std_smc_entry , :
 	/* pass x0-x7 in a struct thread_smc_args */
 	sub	sp, sp, #THREAD_SMC_ARGS_SIZE
-	store_xregs sp, THREAD_SMC_ARGS_X_OFFS(0), 0, 7
+	store_xregs sp, THREAD_SMC_ARGS_X0, 0, 7
 	mov	x0, sp
 
 	/* Call the registered handler */
@@ -190,7 +190,7 @@ FUNC thread_std_smc_entry , :
 	 * "returned" x4-x7 since they will not be returned to normal
 	 * world.
 	 */
-	load_xregs sp, THREAD_SMC_ARGS_X_OFFS(0), 20, 23
+	load_xregs sp, THREAD_SMC_ARGS_X0, 20, 23
 	add	sp, sp, #THREAD_SMC_ARGS_SIZE
 
 	/* Disable interrupts before switching to temporary stack */
@@ -220,13 +220,13 @@ FUNC thread_rpc , :
 	push	x1, x30
 	bl	thread_get_ctx_regs
 	ldr	x30, [sp, #8]
-	store_xregs x0, THREAD_CTX_REGS_X_OFFSET(19), 19, 30
+	store_xregs x0, THREAD_CTX_REGS_X19, 19, 30
 	mov	x19, x0
 
 	bl	thread_get_tmp_sp
 	pop	x1, xzr		/* Match "push x1, x30" above */
 	mov	x2, sp
-	str	x2, [x19, #THREAD_CTX_REGS_SP_OFFSET]
+	str	x2, [x19, #THREAD_CTX_REGS_SP]
 	ldr	x20, [sp]	/* Get pointer to rv[] */
 	mov	sp, x0		/* Switch to tmp stack */
 
@@ -272,8 +272,8 @@ FUNC __thread_enter_user_mode , :
 	 * Create the and fill in the struct thread_user_mode_rec
 	 */
 	sub	sp, sp, #THREAD_USER_MODE_REC_SIZE
-	store_xregs sp, THREAD_USER_MODE_REC_EXIT_STATUS0_PTR_OFFSET, 7, 8
-	store_xregs sp, THREAD_USER_MODE_REC_X_OFFSET(19), 19, 30
+	store_xregs sp, THREAD_USER_MODE_REC_EXIT_STATUS0_PTR, 7, 8
+	store_xregs sp, THREAD_USER_MODE_REC_X19, 19, 30
 
 	/*
 	 * Switch to SP_EL1
@@ -293,7 +293,7 @@ FUNC __thread_enter_user_mode , :
 	 * Save kernel stack pointer to ensure that el0_svc() uses
 	 * correct stack pointer
 	 */
-	str	x19, [x21, #THREAD_CTX_KERN_SP_OFFSET]
+	str	x19, [x21, #THREAD_CTX_KERN_SP]
 
 	/*
 	 * Initialize SPSR, ELR_EL1, and SP_EL0 to enter user mode
@@ -316,11 +316,11 @@ END_FUNC __thread_enter_user_mode
  */
 FUNC thread_unwind_user_mode , :
 	/* Store the exit status */
-	ldp	x3, x4, [sp, #THREAD_USER_MODE_REC_EXIT_STATUS0_PTR_OFFSET]
+	ldp	x3, x4, [sp, #THREAD_USER_MODE_REC_EXIT_STATUS0_PTR]
 	str	w1, [x3]
 	str	w2, [x4]
 	/* Restore x19..x30 */
-	load_xregs sp, THREAD_USER_MODE_REC_X_OFFSET(19), 19, 30
+	load_xregs sp, THREAD_USER_MODE_REC_X19, 19, 30
 	add	sp, sp, #THREAD_USER_MODE_REC_SIZE
 	/* Return from the call of thread_enter_user_mode() */
 	ret
@@ -347,19 +347,19 @@ LOCAL_FUNC thread_vect_table , :
 	 */
 	.align	7
 sync_el1_sp0:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	el1_sync_abort
 	check_vector_size sync_el1_sp0
 
 	.align	7
 irq_el1_sp0:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	elx_irq
 	check_vector_size irq_el1_sp0
 
 	.align	7
 fiq_el1_sp0:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	elx_fiq
 	check_vector_size fiq_el1_sp0
 
@@ -398,7 +398,7 @@ SErrorSPx:
 	 */
 	.align	7
 el0_sync_a64:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	mrs	x2, esr_el1
 	mrs	x3, sp_el0
 	lsr	x2, x2, #ESR_EC_SHIFT
@@ -409,13 +409,13 @@ el0_sync_a64:
 
 	.align	7
 el0_irq_a64:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	elx_irq
 	check_vector_size el0_irq_a64
 
 	.align	7
 el0_fiq_a64:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	elx_irq
 	check_vector_size el0_fiq_a64
 
@@ -430,7 +430,7 @@ SErrorA64:
 	 */
 	.align	7
 el0_sync_a32:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	mrs	x2, esr_el1
 	mrs	x3, sp_el0
 	lsr	x2, x2, #ESR_EC_SHIFT
@@ -441,13 +441,13 @@ el0_sync_a32:
 
 	.align	7
 el0_irq_a32:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	elx_irq
 	check_vector_size el0_irq_a32
 
 	.align	7
 el0_fiq_a32:
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	b	elx_fiq
 	check_vector_size el0_fiq_a32
 
@@ -462,7 +462,7 @@ LOCAL_FUNC el0_svc , :
 	/* get pointer to current thread context in x0 */
 	get_thread_ctx sp, 0, 1, 2
 	/* load saved kernel sp */
-	ldr	x0, [x0, #THREAD_CTX_KERN_SP_OFFSET]
+	ldr	x0, [x0, #THREAD_CTX_KERN_SP]
 	/* Keep pointer to initial recod in x1 */
 	mov	x1, sp
 	/* Switch to SP_EL0 and restore kernel sp */
@@ -472,17 +472,17 @@ LOCAL_FUNC el0_svc , :
 
 	/* Make room for struct thread_svc_regs */
 	sub	sp, sp, #THREAD_SVC_REG_SIZE
-	stp	x30,x2, [sp, #THREAD_SVC_REG_X30_OFFS]
+	stp	x30,x2, [sp, #THREAD_SVC_REG_X30]
 
 	/* Restore x0-x3 */
-	ldp	x2, x3, [x1, #THREAD_CORE_LOCAL_X_OFFSET(2)]
-	ldp	x0, x1, [x1, #THREAD_CORE_LOCAL_X_OFFSET(0)]
+	ldp	x2, x3, [x1, #THREAD_CORE_LOCAL_X2]
+	ldp	x0, x1, [x1, #THREAD_CORE_LOCAL_X0]
 
 	/* Prepare the argument for the handler */
-	store_xregs sp, THREAD_SVC_REG_X_OFFS(0), 0, 14
+	store_xregs sp, THREAD_SVC_REG_X0, 0, 14
 	mrs	x0, elr_el1
 	mrs	x1, spsr_el1
-	store_xregs sp, THREAD_SVC_REG_ELR_OFFS, 0, 1
+	store_xregs sp, THREAD_SVC_REG_ELR, 0, 1
 	mov	x0, sp
 
 	/*
@@ -508,18 +508,18 @@ LOCAL_FUNC el0_svc , :
 	get_thread_ctx sp, 0, 1, 2
 	msr	spsel, #0
 	add	x1, sp, #THREAD_SVC_REG_SIZE
-	str	x1, [x0, #THREAD_CTX_KERN_SP_OFFSET]
+	str	x1, [x0, #THREAD_CTX_KERN_SP]
 
 	/* Restore registers to the required state and return*/
-	load_xregs sp, THREAD_SVC_REG_ELR_OFFS, 0, 1
+	load_xregs sp, THREAD_SVC_REG_ELR, 0, 1
 	msr	elr_el1, x0
 	msr	spsr_el1, x1
-	load_xregs sp, THREAD_SVC_REG_X_OFFS(0), 0, 14
+	load_xregs sp, THREAD_SVC_REG_X0, 0, 14
 	mov	x30, sp
-	ldr	x0, [x30, #THREAD_SVC_REG_SP_EL0_OFFS]
+	ldr	x0, [x30, #THREAD_SVC_REG_SP_EL0]
 	mov	sp, x0
-	ldr	x0, [x30, THREAD_SVC_REG_X_OFFS(0)]
-	ldr	x30, [x30, #THREAD_SVC_REG_X30_OFFS]
+	ldr	x0, [x30, THREAD_SVC_REG_X0]
+	ldr	x30, [x30, #THREAD_SVC_REG_X30]
 
 	eret
 END_FUNC el0_svc
@@ -533,24 +533,24 @@ LOCAL_FUNC el1_sync_abort , :
 	 * Update core local flags.
 	 * flags = (flags << THREAD_CLF_SAVED_SHIFT) | THREAD_CLF_ABORT;
 	 */
-	ldr	w1, [x0, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w1, [x0, #THREAD_CORE_LOCAL_FLAGS]
 	lsl	w1, w1, #THREAD_CLF_SAVED_SHIFT
 	orr	w1, w1, #THREAD_CLF_ABORT
 	tbnz	w1, #(THREAD_CLF_SAVED_SHIFT + THREAD_CLF_ABORT_SHIFT), \
 			.Lsel_tmp_sp
 
 	/* Select abort stack */
-	ldr	x2, [x0, #THREAD_CORE_LOCAL_ABT_STACK_VA_END_OFFSET]
+	ldr	x2, [x0, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
 	b	.Lset_sp
 
 .Lsel_tmp_sp:
 	/* Select tmp stack */
-	ldr	x2, [x0, #THREAD_CORE_LOCAL_TMP_STACK_VA_END_OFFSET]
+	ldr	x2, [x0, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
 	orr	w1, w1, #THREAD_CLF_TMP	/* flags |= THREAD_CLF_TMP; */
 
 .Lset_sp:
 	mov	sp, x2
-	str	w1, [x0, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w1, [x0, #THREAD_CORE_LOCAL_FLAGS]
 
 	/*
 	 * Save state on stack
@@ -558,16 +558,16 @@ LOCAL_FUNC el1_sync_abort , :
 	sub	sp, sp, #THREAD_ABT_REGS_SIZE
 	mrs	x2, spsr_el1
 	/* Store spsr, sp_el0 */
-	stp	x2, x3, [sp, #THREAD_ABT_REG_SPSR_OFFS]
+	stp	x2, x3, [sp, #THREAD_ABT_REG_SPSR]
 	/* Store original x0, x1 */
-	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X_OFFSET(0)]
-	stp	x2, x3, [sp, #THREAD_ABT_REG_X_OFFS(0)]
+	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X0]
+	stp	x2, x3, [sp, #THREAD_ABT_REG_X0]
 	/* Store original x2, x3 and x4 to x29 */
-	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X_OFFSET(2)]
-	store_xregs sp, THREAD_ABT_REG_X_OFFS(2), 2, 29
+	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X2]
+	store_xregs sp, THREAD_ABT_REG_X2, 2, 29
 	/* Store x30, elr_el1 */
 	mrs	x0, elr_el1
-	stp	x30, x0, [sp, #THREAD_ABT_REG_X_OFFS(30)]
+	stp	x30, x0, [sp, #THREAD_ABT_REG_X30]
 
 	/*
 	 * Call handler
@@ -580,27 +580,27 @@ LOCAL_FUNC el1_sync_abort , :
 	 * Restore state from stack
 	 */
 	/* Load x30, elr_el1 */
-	ldp	x30, x0, [sp, #THREAD_ABT_REG_X_OFFS(30)]
+	ldp	x30, x0, [sp, #THREAD_ABT_REG_X30]
 	msr	elr_el1, x0
 	/* Load x0 to x29 */
-	load_xregs sp, THREAD_ABT_REG_X_OFFS(0), 0, 29
+	load_xregs sp, THREAD_ABT_REG_X0, 0, 29
 	/* Switch to SP_EL1 */
 	msr	spsel, #1
 	/* Save x0 to x3 in CORE_LOCAL */
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	/* Restore spsr_el1 and sp_el0 */
 	mrs	x3, sp_el0
-	ldp	x0, x1, [x3, #THREAD_ABT_REG_SPSR_OFFS]
+	ldp	x0, x1, [x3, #THREAD_ABT_REG_SPSR]
 	msr	spsr_el1, x0
 	msr	sp_el0, x1
 
 	/* Update core local flags */
-	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsr	w0, w0, #THREAD_CLF_SAVED_SHIFT
-	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 
 	/* Restore x0 to x3 */
-	load_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	load_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 
 	/* Return from exception */
 	eret
@@ -611,17 +611,17 @@ LOCAL_FUNC el0_sync_abort , :
 	/*
 	 * Update core local flags
 	 */
-	ldr	w1, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w1, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsl	w1, w1, #THREAD_CLF_SAVED_SHIFT
 	orr	w1, w1, #THREAD_CLF_ABORT
-	str	w1, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w1, [sp, #THREAD_CORE_LOCAL_FLAGS]
 
 	/*
 	 * Save state on stack
 	 */
 
 	/* load abt_stack_va_end */
-	ldr	x1, [sp, #THREAD_CORE_LOCAL_ABT_STACK_VA_END_OFFSET]
+	ldr	x1, [sp, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
 	/* Keep pointer to initial record in x0 */
 	mov	x0, sp
 	/* Switch to SP_EL0 */
@@ -630,16 +630,16 @@ LOCAL_FUNC el0_sync_abort , :
 	sub	sp, sp, #THREAD_ABT_REGS_SIZE
 	mrs	x2, spsr_el1
 	/* Store spsr, sp_el0 */
-	stp	x2, x3, [sp, #THREAD_ABT_REG_SPSR_OFFS]
+	stp	x2, x3, [sp, #THREAD_ABT_REG_SPSR]
 	/* Store original x0, x1 */
-	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X_OFFSET(0)]
-	stp	x2, x3, [sp, #THREAD_ABT_REG_X_OFFS(0)]
+	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X0]
+	stp	x2, x3, [sp, #THREAD_ABT_REG_X0]
 	/* Store original x2, x3 and x4 to x29 */
-	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X_OFFSET(2)]
-	store_xregs sp, THREAD_ABT_REG_X_OFFS(2), 2, 29
+	ldp	x2, x3, [x0, #THREAD_CORE_LOCAL_X2]
+	store_xregs sp, THREAD_ABT_REG_X2, 2, 29
 	/* Store x30, elr_el1 */
 	mrs	x0, elr_el1
-	stp	x30, x0, [sp, #THREAD_ABT_REG_X_OFFS(30)]
+	stp	x30, x0, [sp, #THREAD_ABT_REG_X30]
 
 	/*
 	 * Call handler
@@ -653,27 +653,27 @@ LOCAL_FUNC el0_sync_abort , :
 	 */
 
 	/* Load x30, elr_el1 */
-	ldp	x30, x0, [sp, #THREAD_ABT_REG_X_OFFS(30)]
+	ldp	x30, x0, [sp, #THREAD_ABT_REG_X30]
 	msr	elr_el1, x0
 	/* Load x0 to x29 */
-	load_xregs sp, THREAD_ABT_REG_X_OFFS(0), 0, 29
+	load_xregs sp, THREAD_ABT_REG_X0, 0, 29
 	/* Switch to SP_EL1 */
 	msr	spsel, #1
 	/* Save x0 to x3 in EL1_REC */
-	store_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	store_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 	/* Restore spsr_el1 and sp_el0 */
 	mrs	x3, sp_el0
-	ldp	x0, x1, [x3, #THREAD_ABT_REG_SPSR_OFFS]
+	ldp	x0, x1, [x3, #THREAD_ABT_REG_SPSR]
 	msr	spsr_el1, x0
 	msr	sp_el0, x1
 
 	/* Update core local flags */
-	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsr	w0, w0, #THREAD_CLF_SAVED_SHIFT
-	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 
 	/* Restore x0 to x3 */
-	load_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	load_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 
 	/* Return from exception */
 	eret
@@ -683,11 +683,11 @@ LOCAL_FUNC elx_irq , :
 	/*
 	 * Update core local flags
 	 */
-	ldr	w1, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w1, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsl	w1, w1, #THREAD_CLF_SAVED_SHIFT
 	orr	w1, w1, #THREAD_CLF_TMP
 	orr	w1, w1, #THREAD_CLF_IRQ
-	str	w1, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w1, [sp, #THREAD_CORE_LOCAL_FLAGS]
 
 	/* get pointer to current thread context in x0 */
 	get_thread_ctx sp, 0, 1, 2
@@ -695,16 +695,16 @@ LOCAL_FUNC elx_irq , :
 	mrs	x2, sp_el0
 
 	/* Store original sp_el0 */
-	str	x2, [x0, #THREAD_CTX_REGS_SP_OFFSET]
+	str	x2, [x0, #THREAD_CTX_REGS_SP]
 	/* store x4..x30 */
-	store_xregs x0, THREAD_CTX_REGS_X_OFFSET(4), 4, 30
+	store_xregs x0, THREAD_CTX_REGS_X4, 4, 30
 	/* Load original x0..x3 into x10..x13 */
-	load_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 10, 13
+	load_xregs sp, THREAD_CORE_LOCAL_X0, 10, 13
 	/* Save original x0..x3 */
-	store_xregs x0, THREAD_CTX_REGS_X_OFFSET(0), 10, 13
+	store_xregs x0, THREAD_CTX_REGS_X0, 10, 13
 
 	/* load tmp_stack_va_end */
-	ldr	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END_OFFSET]
+	ldr	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
 	/* Switch to SP_EL0 */
 	msr	spsel, #0
 	mov	sp, x1
@@ -721,9 +721,9 @@ LOCAL_FUNC elx_irq , :
 	/* Update core local flags */
 	/* Switch to SP_EL1 */
 	msr	spsel, #1
-	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsr	w0, w0, #THREAD_CLF_SAVED_SHIFT
-	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	msr	spsel, #0
 
 	/*
@@ -751,23 +751,23 @@ END_FUNC elx_irq
  * 	uint64_t sp_el0;
  * };
  */
-#define ELX_FIQ_REC_X_OFFSET(x)		(8 * ((x) - 4))
-#define ELX_FIQ_REC_LR_OFFSET		(8 + ELX_FIQ_REC_X_OFFSET(19))
-#define ELX_FIQ_REC_SP_EL0_OFFSET	(8 + ELX_FIQ_REC_LR_OFFSET)
-#define ELX_FIQ_REC_SIZE		(8 + ELX_FIQ_REC_SP_EL0_OFFSET)
+#define ELX_FIQ_REC_X(x)		(8 * ((x) - 4))
+#define ELX_FIQ_REC_LR			(8 + ELX_FIQ_REC_X(19))
+#define ELX_FIQ_REC_SP_EL0		(8 + ELX_FIQ_REC_LR)
+#define ELX_FIQ_REC_SIZE		(8 + ELX_FIQ_REC_SP_EL0)
 
 LOCAL_FUNC elx_fiq , :
 	/*
 	 * Update core local flags
 	 */
-	ldr	w1, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w1, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsl	w1, w1, #THREAD_CLF_SAVED_SHIFT
 	orr	w1, w1, #THREAD_CLF_FIQ
 	orr	w1, w1, #THREAD_CLF_TMP
-	str	w1, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w1, [sp, #THREAD_CORE_LOCAL_FLAGS]
 
 	/* load tmp_stack_va_end */
-	ldr	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END_OFFSET]
+	ldr	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
 	/* Keep original SP_EL0 */
 	mrs	x2, sp_el0
 	/* Switch to SP_EL0 */
@@ -781,9 +781,9 @@ LOCAL_FUNC elx_fiq , :
 	/* Make room for struct elx_fiq_rec */
 	sub	sp, sp, #ELX_FIQ_REC_SIZE
 	/* Store x4..x18 */
-	store_xregs sp, ELX_FIQ_REC_X_OFFSET(4), 4, 18
+	store_xregs sp, ELX_FIQ_REC_X(4), 4, 18
 	/* Store lr and original sp_el0 */
-	stp	x30, x2, [sp, #ELX_FIQ_REC_LR_OFFSET]
+	stp	x30, x2, [sp, #ELX_FIQ_REC_LR]
 
 	bl	thread_check_canaries
 	adr	x16, thread_fiq_handler_ptr
@@ -794,21 +794,21 @@ LOCAL_FUNC elx_fiq , :
 	 * Restore registers
 	 */
 	/* Restore x4..x18 */
-	load_xregs sp, ELX_FIQ_REC_X_OFFSET(4), 4, 18
+	load_xregs sp, ELX_FIQ_REC_X(4), 4, 18
 	/* Load  lr and original sp_el0 */
-	ldp	x30, x2, [sp, #ELX_FIQ_REC_LR_OFFSET]
+	ldp	x30, x2, [sp, #ELX_FIQ_REC_LR]
 	/* Restore SP_El0 */
 	mov	sp, x2
 	/* Switch back to SP_EL1 */
 	msr	spsel, #1
 
 	/* Update core local flags */
-	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	ldr	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 	lsr	w0, w0, #THREAD_CLF_SAVED_SHIFT
-	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS_OFFSET]
+	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
 
 	/* Restore x0..x3 */
-	load_xregs sp, THREAD_CORE_LOCAL_X_OFFSET(0), 0, 3
+	load_xregs sp, THREAD_CORE_LOCAL_X0, 0, 3
 
 	/* Return from exception */
 	eret

core/arch/arm/kernel/thread_private.h
@@ -32,6 +32,8 @@
 
 #include <mm/core_mmu.h>
 #include <kernel/vfp.h>
+#include <kernel/mutex.h>
+#include <kernel/thread.h>
 
 enum thread_state {
 	THREAD_STATE_FREE,
@@ -143,24 +145,6 @@ struct thread_core_local {
 #define THREAD_VFP_STATE_SIZE				0
 #endif
 
-#define THREAD_CTX_KERN_SP_OFFSET			\
-		(THREAD_CTX_REGS_SIZE + (4 + 2 + 1) * 8)
-#define THREAD_CTX_SIZE					\
-		(THREAD_CTX_KERN_SP_OFFSET + THREAD_VFP_STATE_SIZE + 5 * 8)
-
-#define THREAD_CTX_REGS_SP_OFFSET			(8 * 0)
-#define THREAD_CTX_REGS_PC_OFFSET			(8 * 1)
-#define THREAD_CTX_REGS_SPSR_OFFSET			(8 * 2)
-#define THREAD_CTX_REGS_X_OFFSET(x)			(8 * (3 + (x)))
-#define THREAD_CTX_REGS_SIZE			THREAD_CTX_REGS_X_OFFSET(31)
-
-#define THREAD_CORE_LOCAL_TMP_STACK_VA_END_OFFSET	(8 * 0)
-#define THREAD_CORE_LOCAL_CURR_THREAD_OFFSET		(8 * 1)
-#define THREAD_CORE_LOCAL_FLAGS_OFFSET			(8 * 1 + 4)
-#define THREAD_CORE_LOCAL_ABT_STACK_VA_END_OFFSET	(8 * 2)
-#define THREAD_CORE_LOCAL_X_OFFSET(x)			(8 * (3 + (x)))
-#define THREAD_CORE_LOCAL_SIZE			THREAD_CORE_LOCAL_X_OFFSET(5)
-
 /* Describes the flags field of struct thread_core_local */
 #define THREAD_CLF_SAVED_SHIFT			4
 #define THREAD_CLF_CURR_SHIFT			0
@@ -174,11 +158,6 @@ struct thread_core_local {
 #define THREAD_CLF_ABORT			(1 << THREAD_CLF_ABORT_SHIFT)
 #define THREAD_CLF_IRQ				(1 << THREAD_CLF_IRQ_SHIFT)
 #define THREAD_CLF_FIQ				(1 << THREAD_CLF_FIQ_SHIFT)
-
-#define THREAD_USER_MODE_REC_EXIT_STATUS0_PTR_OFFSET	(0)
-#define THREAD_USER_MODE_REC_EXIT_STATUS1_PTR_OFFSET	(8 * 1)
-#define THREAD_USER_MODE_REC_X_OFFSET(x)		(8 * (2 + (x) - 19))
-#define THREAD_USER_MODE_REC_SIZE   THREAD_USER_MODE_REC_X_OFFSET(31)
 
 #endif /*ARM64*/
 

core/arch/arm/tee/arch_svc_a32.S
@@ -31,6 +31,7 @@
 #include <tee_api_defines.h>
 #include <kernel/thread.h>
 #include <kernel/unwind.h>
+#include <asm-defines.h>
 
 	.section .text.arch_svc_asm
 
@@ -46,8 +47,8 @@ UNWIND(	.save	{r5-r9, lr})
 	mov	r7, sp
 	mov	r8, r0
 	mov	r9, r1
-	ldr	r5, [r8, #THREAD_SVC_REG_R5_OFFS]
-	ldr	r6, [r8, #THREAD_SVC_REG_R6_OFFS]
+	ldr	r5, [r8, #THREAD_SVC_REG_R5]
+	ldr	r6, [r8, #THREAD_SVC_REG_R6]
 
 	/*
 	 * Copy eventual arguments passed on the user stack.
@@ -74,7 +75,7 @@ UNWIND(	.save	{r5-r9, lr})
 
 .Lno_args:
 	/* Load arguments to function */
-	add	lr, r8, #THREAD_SVC_REG_R0_OFFS
+	add	lr, r8, #THREAD_SVC_REG_R0
 	ldm	lr, {r0-r3}
 	blx	r9
 .Lret:

commit ab2fefe1f08cf3f0065bd44c7cc5c3166b8aaf8a

core: arm: fix style issues in asm-defines.c
Fixing a few cases of alignment that doesn't match open parenthesis.

Fixes: c02f9fb09df2 ("arm: add auto generated asm-defines.h")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/asm-defines.c
@@ -134,13 +134,13 @@ DEFINES
 
 	/* struct thread_core_local */
 	DEFINE(THREAD_CORE_LOCAL_TMP_STACK_VA_END,
-		offsetof(struct thread_core_local, tmp_stack_va_end));
+	       offsetof(struct thread_core_local, tmp_stack_va_end));
 	DEFINE(THREAD_CORE_LOCAL_CURR_THREAD,
-		offsetof(struct thread_core_local, curr_thread));
+	       offsetof(struct thread_core_local, curr_thread));
 	DEFINE(THREAD_CORE_LOCAL_FLAGS,
-		offsetof(struct thread_core_local, flags));
+	       offsetof(struct thread_core_local, flags));
 	DEFINE(THREAD_CORE_LOCAL_ABT_STACK_VA_END,
-		offsetof(struct thread_core_local, abt_stack_va_end));
+	       offsetof(struct thread_core_local, abt_stack_va_end));
 #if defined(ARM64) && defined(CFG_CORE_FFA)
 	DEFINE(THREAD_CORE_LOCAL_DIRECT_RESP_FID,
 	       offsetof(struct thread_core_local, direct_resp_fid));