OP-TEE/optee_os

commit 15da69cff2ca5ff639fbf9439be1f1a3449bc6b4

core: ffa: Enable handling 64-bit direct messages
Enable using the 64-bit version of the FF-A direct request and response
calls.

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jelle Sels <jelle.sels@arm.com>

core/arch/arm/kernel/thread_spmc.c
@@ -975,6 +975,9 @@ void thread_spmc_msg_recv(struct thread_smc_args *args)
 		itr_core_handler();
 		spmc_set_args(args, FFA_MSG_WAIT, 0, 0, 0, 0, 0);
 		break;
+#ifdef ARM64
+	case FFA_MSG_SEND_DIRECT_REQ_64:
+#endif
 	case FFA_MSG_SEND_DIRECT_REQ_32:
 		if (IS_ENABLED(CFG_SECURE_PARTITION) &&
 		    FFA_DST(args->a1) != my_endpoint_id) {

core/arch/arm/kernel/spmc_sp_handler.c
@@ -913,9 +913,15 @@ void spmc_sp_msg_handler(struct thread_smc_args *args,
 	thread_check_canaries();
 	do {
 		switch (args->a0) {
+#ifdef ARM64
+		case FFA_MSG_SEND_DIRECT_REQ_64:
+#endif
 		case FFA_MSG_SEND_DIRECT_REQ_32:
 			caller_sp = ffa_handle_sp_direct_req(args, caller_sp);
 			break;
+#ifdef ARM64
+		case FFA_MSG_SEND_DIRECT_RESP_64:
+#endif
 		case FFA_MSG_SEND_DIRECT_RESP_32:
 			caller_sp = ffa_handle_sp_direct_resp(args, caller_sp);
 			break;

commit dafebf2f88dab6e9cc37bf01bf0fb5edc1b46738

core: ffa: preserve 64bit smccc when possible
Prior to this patch when a FFA_MSG_SEND_DIRECT_REQ_64 was received the
response was sent as FFA_MSG_SEND_DIRECT_RESP_32. While not breaking
with the FF-A specification, it's still a bit unexpected and will cause
an error in the FF-A framework driver. So fix this by keeping track of
the SMCCC (SMC Calling Convention) used during the current
FFA_MSG_SEND_DIRECT_REQ and respond with matching SMCCC.

This has no impact on AArch32 mode since only the 32-bit SMCCC is valid
in that case. The greatest impact on AArch64 is that we must be able to
find out the current SMCCC during RPC, this means storing it in struct
thread_core_local to be able to access it in assembly low level
routines.

Support for FFA_MSG_SEND_DIRECT_REQ_64 is also advertised in
FFA_FEATURES.

Fixes: 15da69cff2ca ("core: ffa: Enable handling 64-bit direct messages")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/include/kernel/thread_arch.h
@@ -58,6 +58,10 @@ struct thread_core_local {
 #ifdef CFG_TEE_CORE_DEBUG
 	unsigned int locked_count; /* Number of spinlocks held */
 #endif
+#if defined(ARM64) && defined(CFG_CORE_FFA)
+	/* Function ID to use for a direct response, 32-bit vs 64-bit */
+	uint32_t direct_resp_fid;
+#endif
 #if defined(ARM64) && defined(CFG_CORE_WORKAROUND_SPECTRE_BP_SEC)
 	uint8_t bhb_loop_count;
 #endif

core/arch/arm/kernel/asm-defines.c
@@ -141,6 +141,10 @@ DEFINES
 		offsetof(struct thread_core_local, flags));
 	DEFINE(THREAD_CORE_LOCAL_ABT_STACK_VA_END,
 		offsetof(struct thread_core_local, abt_stack_va_end));
+#if defined(ARM64) && defined(CFG_CORE_FFA)
+	DEFINE(THREAD_CORE_LOCAL_DIRECT_RESP_FID,
+	       offsetof(struct thread_core_local, direct_resp_fid));
+#endif
 
 	DEFINE(STACK_TMP_GUARD, STACK_CANARY_SIZE / 2 + STACK_TMP_OFFS);
 

core/arch/arm/kernel/thread_spmc.c
@@ -23,6 +23,7 @@
 #include <optee_ffa.h>
 #include <optee_msg.h>
 #include <optee_rpc_cmd.h>
+#include <sm/optee_smc.h>
 #include <string.h>
 #include <sys/queue.h>
 #include <tee/entry_std.h>
@@ -179,6 +180,7 @@ static void handle_features(struct thread_smc_args *args)
 	case FFA_SPM_ID_GET:
 	case FFA_MEM_FRAG_TX:
 	case FFA_MEM_RECLAIM:
+	case FFA_MSG_SEND_DIRECT_REQ_64:
 	case FFA_MSG_SEND_DIRECT_REQ_32:
 	case FFA_INTERRUPT:
 	case FFA_PARTITION_INFO_GET:
@@ -580,11 +582,17 @@ out:
 }
 #endif /*CFG_CORE_SEL1_SPMC*/
 
-static void handle_yielding_call(struct thread_smc_args *args)
+static void handle_yielding_call(struct thread_smc_args *args,
+				 uint32_t direct_resp_fid)
 {
 	TEE_Result res = 0;
 
 	thread_check_canaries();
+
+#ifdef ARM64
+	/* Saving this for an eventual RPC */
+	thread_get_core_local()->direct_resp_fid = direct_resp_fid;
+#endif
 
 	if (args->a3 == OPTEE_FFA_YIELDING_CALL_RESUME) {
 		/* Note connection to struct thread_rpc_arg::ret */
@@ -596,8 +604,8 @@ static void handle_yielding_call(struct thread_smc_args *args)
 				     args->a6, args->a7);
 		res = TEE_ERROR_BUSY;
 	}
-	spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
-		      swap_src_dst(args->a1), 0, res, 0, 0);
+	spmc_set_args(args, direct_resp_fid, swap_src_dst(args->a1),
+		      0, res, 0, 0);
 }
 
 static uint32_t handle_unregister_shm(uint32_t a4, uint32_t a5)
@@ -619,30 +627,27 @@ static uint32_t handle_unregister_shm(uint32_t a4, uint32_t a5)
 	}
 }
 
-static void handle_blocking_call(struct thread_smc_args *args)
+static void handle_blocking_call(struct thread_smc_args *args,
+				 uint32_t direct_resp_fid)
 {
 	switch (args->a3) {
 	case OPTEE_FFA_GET_API_VERSION:
-		spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
-			      swap_src_dst(args->a1), 0,
+		spmc_set_args(args, direct_resp_fid, swap_src_dst(args->a1), 0,
 			      OPTEE_FFA_VERSION_MAJOR, OPTEE_FFA_VERSION_MINOR,
 			      0);
 		break;
 	case OPTEE_FFA_GET_OS_VERSION:
-		spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
-			      swap_src_dst(args->a1), 0,
+		spmc_set_args(args, direct_resp_fid, swap_src_dst(args->a1), 0,
 			      CFG_OPTEE_REVISION_MAJOR,
 			      CFG_OPTEE_REVISION_MINOR, TEE_IMPL_GIT_SHA1);
 		break;
 	case OPTEE_FFA_EXCHANGE_CAPABILITIES:
-		spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
-			      swap_src_dst(args->a1), 0, 0,
-			      THREAD_RPC_MAX_NUM_PARAMS,
+		spmc_set_args(args, direct_resp_fid, swap_src_dst(args->a1), 0,
+			      0, THREAD_RPC_MAX_NUM_PARAMS,
 			      OPTEE_FFA_SEC_CAP_ARG_OFFSET);
 		break;
 	case OPTEE_FFA_UNREGISTER_SHM:
-		spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
-			      swap_src_dst(args->a1), 0,
+		spmc_set_args(args, direct_resp_fid, swap_src_dst(args->a1), 0,
 			      handle_unregister_shm(args->a4, args->a5), 0, 0);
 		break;
 	default:
@@ -653,7 +658,8 @@ static void handle_blocking_call(struct thread_smc_args *args)
 }
 
 static void handle_framework_direct_request(struct thread_smc_args *args,
-					    struct ffa_rxtx *rxtx)
+					    struct ffa_rxtx *rxtx,
+					    uint32_t direct_resp_fid)
 {
 	uint32_t w0 = FFA_ERROR;
 	uint32_t w1 = FFA_PARAM_MBZ;
@@ -666,7 +672,7 @@ static void handle_framework_direct_request(struct thread_smc_args *args,
 			uint16_t guest_id = args->a5;
 			TEE_Result res = virt_guest_created(guest_id);
 
-			w0 = FFA_MSG_SEND_DIRECT_RESP_32;
+			w0 = direct_resp_fid;
 			w1 = swap_src_dst(args->a1);
 			w2 = FFA_MSG_FLAG_FRAMEWORK | FFA_MSG_RESP_VM_CREATED;
 			if (res == TEE_SUCCESS)
@@ -682,7 +688,7 @@ static void handle_framework_direct_request(struct thread_smc_args *args,
 			uint16_t guest_id = args->a5;
 			TEE_Result res = virt_guest_destroyed(guest_id);
 
-			w0 = FFA_MSG_SEND_DIRECT_RESP_32;
+			w0 = direct_resp_fid;
 			w1 = swap_src_dst(args->a1);
 			w2 = FFA_MSG_FLAG_FRAMEWORK | FFA_MSG_RESP_VM_DESTROYED;
 			if (res == TEE_SUCCESS)
@@ -692,7 +698,7 @@ static void handle_framework_direct_request(struct thread_smc_args *args,
 		}
 		break;
 	case FFA_MSG_VERSION_REQ:
-		w0 = FFA_MSG_SEND_DIRECT_RESP_32;
+		w0 = direct_resp_fid;
 		w1 = swap_src_dst(args->a1);
 		w2 = FFA_MSG_FLAG_FRAMEWORK | FFA_MSG_VERSION_RESP;
 		w3 = spmc_exchange_version(args->a3, rxtx);
@@ -706,29 +712,35 @@ static void handle_framework_direct_request(struct thread_smc_args *args,
 static void handle_direct_request(struct thread_smc_args *args,
 				  struct ffa_rxtx *rxtx)
 {
+	uint32_t direct_resp_fid = 0;
+
 	if (IS_ENABLED(CFG_SECURE_PARTITION) &&
 	    FFA_DST(args->a1) != my_endpoint_id) {
 		spmc_sp_start_thread(args);
 		return;
 	}
 
+	if (OPTEE_SMC_IS_64(args->a0))
+		direct_resp_fid = FFA_MSG_SEND_DIRECT_RESP_64;
+	else
+		direct_resp_fid = FFA_MSG_SEND_DIRECT_RESP_32;
+
 	if (args->a2 & FFA_MSG_FLAG_FRAMEWORK) {
-		handle_framework_direct_request(args, rxtx);
+		handle_framework_direct_request(args, rxtx, direct_resp_fid);
 		return;
 	}
 
 	if (IS_ENABLED(CFG_NS_VIRTUALIZATION) &&
 	    virt_set_guest(get_sender_id(args->a1))) {
-		spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
-			      swap_src_dst(args->a1), 0,
+		spmc_set_args(args, direct_resp_fid, swap_src_dst(args->a1), 0,
 			      TEE_ERROR_ITEM_NOT_FOUND, 0, 0);
 		return;
 	}
 
 	if (args->a3 & BIT32(OPTEE_FFA_YIELDING_CALL_BIT))
-		handle_yielding_call(args);
+		handle_yielding_call(args, direct_resp_fid);
 	else
-		handle_blocking_call(args);
+		handle_blocking_call(args, direct_resp_fid);
 
 	/*
 	 * Note that handle_yielding_call() typically only returns if a

core/arch/arm/kernel/thread_spmc_a64.S
@@ -42,7 +42,9 @@ END_FUNC thread_ffa_msg_wait
 
 	/* Caller provides x1, x3-x7 params */
 LOCAL_FUNC ffa_msg_send_direct_resp , :
-	mov_imm	x0, FFA_MSG_SEND_DIRECT_RESP_32		/* FID */
+	msr	spsel, #1
+	ldr	w0, [sp, #THREAD_CORE_LOCAL_DIRECT_RESP_FID]
+	msr	spsel, #0
 	mov	x2, #FFA_PARAM_MBZ			/* RES MBZ */
 
 .ffa_msg_loop:
@@ -149,14 +151,12 @@ FUNC thread_rpc_spsr , :
 	adr	x2, .thread_rpc_return
 	mov	w0, #THREAD_FLAGS_COPY_ARGS_ON_RETURN
 	bl	thread_state_suspend
-	mov	w7, w0		/* Supply thread index */
-	ldr	w0, =FFA_MSG_SEND_DIRECT_RESP_32
 	mov	w1, w21
-	mov	w2, #FFA_PARAM_MBZ
 	mov	w3, #0		/* Error code = 0 */
 	mov	w4, w22
 	mov	w5, w23
 	mov	w6, w24
+	mov	w7, w0		/* Supply thread index */
 	b	ffa_msg_send_direct_resp
 
 .thread_rpc_return:
@@ -203,7 +203,6 @@ FUNC thread_foreign_intr_exit , :
 	and     w2, w2, #THREAD_FLAGS_FFA_ONLY
 	cbnz	w2, thread_ffa_interrupt
 #endif /* CFG_SECURE_PARTITION */
-	mov	x2, #FFA_PARAM_MBZ
 	mov	w3, #FFA_PARAM_MBZ
 	mov	w4, #OPTEE_FFA_YIELDING_CALL_RETURN_INTERRUPT
 	mov	x5, #FFA_PARAM_MBZ