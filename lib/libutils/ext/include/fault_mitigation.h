OP-TEE/optee_os

commit ce56605a0ede428379531a49ae766ec3b16aab9a

core: support fault mitigations in non-threaded code
Fault mitigation won't work in non-threaded code due to the following
error:
assertion 'ct >= 0 && ct < CFG_NUM_THREADS' failed at core/arch/arm/kernel
/thread.c:799 <thread_get_id>

The problem is in  __ftmn_get_tsd_func_arg_pp which calls thread_get_tsd
which thread_get_id. The reason is that the interrupt handler is not
associated with any thread, so the ct (current_thread_id) value is -1 which
would cause an assert problem.

The fix is to add ftmn_arg to thread_core_local and the new variable would
be used when the current thread is < 0.

Signed-off-by: Sichun Qin <sichun.qin@amlogic.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/ext/include/fault_mitigation.h
@@ -250,7 +250,10 @@ extern struct ftmn_func_arg *__ftmn_global_func_arg;
 static inline struct ftmn_func_arg **__ftmn_get_tsd_func_arg_pp(void)
 {
 #if defined(CFG_FAULT_MITIGATION) && defined(__KERNEL__)
-	return &thread_get_tsd()->ftmn_arg;
+    if (thread_get_id_may_fail() >= 0)
+		return &thread_get_tsd()->ftmn_arg;
+	else
+		return &thread_get_core_local()->ftmn_arg;
 #elif defined(CFG_FAULT_MITIGATION)
 	return &__ftmn_global_func_arg;
 #else

core/arch/arm/include/kernel/thread_arch.h
@@ -64,6 +64,9 @@ struct thread_core_local {
 #ifdef CFG_CORE_DEBUG_CHECK_STACKS
 	bool stackcheck_recursion;
 #endif
+#ifdef CFG_FAULT_MITIGATION
+	struct ftmn_func_arg *ftmn_arg;
+#endif
 } THREAD_CORE_LOCAL_ALIGNED;
 
 struct thread_vector_table {

commit 96ff4e336d842e67cdebcfb2273a893517983369

libutils: fault_mitigation.h: Fix indentation
Indentation with tab instead of space.

Fixes: ce56605a0ede ("core: support fault mitigations in non-threaded code")
Signed-off-by: Alvin Chang <alvinga@andestech.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/ext/include/fault_mitigation.h
@@ -250,7 +250,7 @@ extern struct ftmn_func_arg *__ftmn_global_func_arg;
 static inline struct ftmn_func_arg **__ftmn_get_tsd_func_arg_pp(void)
 {
 #if defined(CFG_FAULT_MITIGATION) && defined(__KERNEL__)
-    if (thread_get_id_may_fail() >= 0)
+	if (thread_get_id_may_fail() >= 0)
 		return &thread_get_tsd()->ftmn_arg;
 	else
 		return &thread_get_core_local()->ftmn_arg;