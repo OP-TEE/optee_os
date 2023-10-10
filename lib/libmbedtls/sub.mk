OP-TEE/optee_os

commit dfafe507bbef8bc523ac454e27353e3e0e18a387

Import mbedtls-3.4.0
Imports Mbed TLS 3.4.0 from https://github.com/Mbed-TLS/mbedtls.git
tags mbedtls-3.4.0, v3.4.0.

Files that are not needed are removed:

cd lib/libmbedtls
rm -rf mbedtls
cp -R path/to/mbedtls-3.4.0/mbedtls .
cd mbedtls
rm CMakeLists.txt DartConfiguration.tcl Makefile
rm .gitignore .travis.yml .pylintrc .globalrc .mypy.ini BRANCHES.md
rm include/.gitignore include/CMakeLists.txt library/.gitignore
rm library/CMakeLists.txt library/Makefile
rm mbedtls/cmake/MbedTLSConfig.cmake.in
rm -rf .git .github doxygen configs programs scripts tests visualc
rm -rf 3rdparty ChangeLog.d docs
rm library/psa_* library/mps_*
cd ..
git add mbedtls

This is a complete overwrite of previous code so earlier changes in the
previous branch import/mbedtls-2.28.1 will be added on top of this commit.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libmbedtls/sub.mk
@@ -5,14 +5,15 @@ global-incdirs-y += mbedtls/include
 SRCS_CRYPTO :=
 ifneq ($(sm),core)
 SRCS_CRYPTO += aes.c
+SRCS_CRYPTO += aesce.c
 SRCS_CRYPTO += aesni.c
-SRCS_CRYPTO += arc4.c
 SRCS_CRYPTO += aria.c
 SRCS_CRYPTO += asn1parse.c
 SRCS_CRYPTO += asn1write.c
 SRCS_CRYPTO += base64.c
 SRCS_CRYPTO += bignum.c
-SRCS_CRYPTO += blowfish.c
+SRCS_CRYPTO += bignum_core.c
+SRCS_CRYPTO += bignum_mod_raw.c
 SRCS_CRYPTO += camellia.c
 SRCS_CRYPTO += ccm.c
 SRCS_CRYPTO += chacha20.c
@@ -33,12 +34,12 @@ SRCS_CRYPTO += entropy.c
 SRCS_CRYPTO += entropy_poll.c
 SRCS_CRYPTO += error.c
 SRCS_CRYPTO += gcm.c
-SRCS_CRYPTO += havege.c
+SRCS_CRYPTO += hash_info.c
 SRCS_CRYPTO += hkdf.c
 SRCS_CRYPTO += hmac_drbg.c
+SRCS_CRYPTO += lmots.c
+SRCS_CRYPTO += lms.c
 SRCS_CRYPTO += md.c
-SRCS_CRYPTO += md2.c
-SRCS_CRYPTO += md4.c
 SRCS_CRYPTO += md5.c
 SRCS_CRYPTO += memory_buffer_alloc.c
 SRCS_CRYPTO += nist_kw.c
@@ -47,6 +48,7 @@ SRCS_CRYPTO += padlock.c
 SRCS_CRYPTO += pem.c
 SRCS_CRYPTO += pk.c
 SRCS_CRYPTO += pk_wrap.c
+SRCS_CRYPTO += pkcs7.c
 SRCS_CRYPTO += pkcs12.c
 SRCS_CRYPTO += pkcs5.c
 SRCS_CRYPTO += pkparse.c
@@ -55,8 +57,8 @@ SRCS_CRYPTO += platform.c
 SRCS_CRYPTO += platform_util.c
 SRCS_CRYPTO += poly1305.c
 SRCS_CRYPTO += ripemd160.c
-SRCS_CRYPTO += rsa_internal.c
 SRCS_CRYPTO += rsa.c
+SRCS_CRYPTO += rsa_alt_helpers.c
 SRCS_CRYPTO += sha1.c
 SRCS_CRYPTO += sha256.c
 SRCS_CRYPTO += sha512.c
@@ -64,13 +66,14 @@ SRCS_CRYPTO += threading.c
 SRCS_CRYPTO += timing.c
 SRCS_CRYPTO += version.c
 SRCS_CRYPTO += version_features.c
-SRCS_CRYPTO += xtea.c
 else
 SRCS_CRYPTO += aes.c
 SRCS_CRYPTO += aesni.c
 SRCS_CRYPTO += asn1parse.c
 SRCS_CRYPTO += asn1write.c
 SRCS_CRYPTO += bignum.c
+SRCS_CRYPTO += bignum_core.c
+SRCS_CRYPTO += bignum_mod_raw.c
 SRCS_CRYPTO += cipher.c
 SRCS_CRYPTO += cipher_wrap.c
 SRCS_CRYPTO += cmac.c
@@ -82,6 +85,7 @@ SRCS_CRYPTO += ecdh.c
 SRCS_CRYPTO += ecdsa.c
 SRCS_CRYPTO += ecp.c
 SRCS_CRYPTO += ecp_curves.c
+SRCS_CRYPTO += hash_info.c
 SRCS_CRYPTO += md.c
 SRCS_CRYPTO += md5.c
 SRCS_CRYPTO += oid.c
@@ -89,8 +93,8 @@ SRCS_CRYPTO += pk.c
 SRCS_CRYPTO += pk_wrap.c
 SRCS_CRYPTO += platform.c
 SRCS_CRYPTO += platform_util.c
-SRCS_CRYPTO += rsa_internal.c
 SRCS_CRYPTO += rsa.c
+SRCS_CRYPTO += rsa_alt_helpers.c
 SRCS_CRYPTO += sha1.c
 SRCS_CRYPTO += sha256.c
 SRCS_CRYPTO += sha512.c
@@ -98,8 +102,6 @@ endif
 
 # OBJS_X509
 SRCS_X509 :=
-SRCS_X509 += certs.c
-SRCS_X509 += pkcs11.c
 SRCS_X509 += x509.c
 SRCS_X509 += x509_create.c
 SRCS_X509 += x509_crl.c
@@ -113,12 +115,17 @@ SRCS_TLS :=
 SRCS_TLS += debug.c
 SRCS_TLS += net_sockets.c
 SRCS_TLS += ssl_cache.c
+SRCS_TLS += ssl_client.c
 SRCS_TLS += ssl_ciphersuites.c
-SRCS_TLS += ssl_cli.c
 SRCS_TLS += ssl_cookie.c
-SRCS_TLS += ssl_srv.c
+SRCS_TLS += ssl_debug_helpers_generated.c
 SRCS_TLS += ssl_ticket.c
 SRCS_TLS += ssl_tls.c
+SRCS_TLS += ssl_tls12_client.c
+SRCS_TLS += ssl_tls12_server.c
+SRCS_TLS += ssl_tls13_client.c
+SRCS_TLS += ssl_tls13_generic.c
+SRCS_TLS += ssl_tls13_server.c
 
 srcs-y += $(addprefix mbedtls/library/, $(SRCS_CRYPTO))
 srcs-$(sm-$(ta-target)) += $(addprefix mbedtls/library/, $(SRCS_X509))
@@ -127,6 +134,7 @@ srcs-$(sm-$(ta-target)) += $(addprefix mbedtls/library/, $(SRCS_TLS))
 cflags-lib-y += -Wno-redundant-decls
 cflags-lib-y += -Wno-switch-default
 cflags-lib-y += -Wno-declaration-after-statement
+cflags-lib-y += -Wno-unused-functions
 
 ifeq ($(CFG_CRYPTOLIB_NAME_mbedtls),y)
 subdirs-$(sm-core) += core

commit 8225713449d3df7b8945e14e6c4728a3c9a24dd4

libmbedtls: fix unrecognized compiler option
Prior to this patch when compiling with clang there an error:
error: unknown warning option '-Wno-unused-functions'; did you mean '-Wno-unused-function'? [-Werror,-Wunknown-warning-option]

Fix this by using -Wno-unused-function instead.

Fixes: dfafe507bbef ("Import mbedtls-3.4.0")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

lib/libmbedtls/sub.mk
@@ -134,7 +134,7 @@ srcs-$(sm-$(ta-target)) += $(addprefix mbedtls/library/, $(SRCS_TLS))
 cflags-lib-y += -Wno-redundant-decls
 cflags-lib-y += -Wno-switch-default
 cflags-lib-y += -Wno-declaration-after-statement
-cflags-lib-y += -Wno-unused-functions
+cflags-lib-y += -Wno-unused-function
 
 ifeq ($(CFG_CRYPTOLIB_NAME_mbedtls),y)
 subdirs-$(sm-core) += core