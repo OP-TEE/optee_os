309420e9bee95b167f541f602486fabbbf2a1828
Gatien Chevallier <gatien.chevallier@foss.st.com>
2023-10-11 16:59:30 +0200

    plat-stm32mp2: add platform-specific abort handler
    When a data abort occurs and its fault type is FAULT_TYPE_IGNORE, it
    may be an abort generated by the SERC hardware block. Check if a
    SERC Illegal Access was caught and print the SERC register and panic()
    if that is the case.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>

--- core/arch/arm/plat-stm32mp2/main.c
+++ core/arch/arm/plat-stm32mp2/main.c
@@ -7,8 +7,10 @@
 #include <console.h>
 #include <drivers/gic.h>
 #include <drivers/stm32_rif.h>
+#include <drivers/stm32_serc.h>
 #include <drivers/stm32_uart.h>
 #include <initcall.h>
+#include <kernel/abort.h>
 #include <kernel/boot.h>
 #include <kernel/dt.h>
 #include <kernel/interrupt.h>
@@ -171,4 +173,10 @@ bool stm32mp_allow_probe_shared_device(const void *fdt, int node)
 		return true;
 
 	return false;
+}
+
+void plat_abort_handler(struct thread_abort_regs *regs __unused)
+{
+	/* If fault is ignored, it could be due to a SERC event */
+	stm32_serc_handle_ilac();
 }
