OP-TEE/optee_os

commit 67fec989b586467cdb322e22d0567f8c4a654817

core: ffa: correct response to FFA_INTERRUPT
When a secure interrupts is delivered by the SPM to OP-TEE the correct
response is FFA_MSG_WAIT, not FFA_SUCCESS_32 as was used previously. So
fix this.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/thread_spmc.c
@@ -972,7 +972,7 @@ void thread_spmc_msg_recv(struct thread_smc_args *args)
 #endif /*CFG_CORE_SEL1_SPMC*/
 	case FFA_INTERRUPT:
 		itr_core_handler();
-		spmc_set_args(args, FFA_SUCCESS_32, args->a1, 0, 0, 0, 0);
+		spmc_set_args(args, FFA_MSG_WAIT, 0, 0, 0, 0, 0);
 		break;
 	case FFA_MSG_SEND_DIRECT_REQ_32:
 		if (IS_ENABLED(CFG_SECURE_PARTITION) &&

commit d5d40aae898f3b630733efbbefde8a7c18cc062d

core: ffa: use FFA_NORMAL_WORLD_RESUME
Prior to this FFA_INTERRUPT was always completed using FFA_MSG_WAIT,
but at S-EL1 FFA_NORMAL_WORLD_RESUME should be used instead. So fix this
by completing a secure interrupt that has preempted the normal world with
FFA_NORMAL_WORLD_RESUME if configured with SPMC at S-EL1.

Fixes: 67fec989b586 ("core: ffa: correct response to FFA_INTERRUPT")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Balint Dobszay <balint.dobszay@arm.com>

core/arch/arm/include/ffa.h
@@ -75,6 +75,7 @@
 #define FFA_MEM_RECLAIM			U(0x84000077)
 #define FFA_MEM_FRAG_RX			U(0x8400007A)
 #define FFA_MEM_FRAG_TX			U(0x8400007B)
+#define FFA_NORMAL_WORLD_RESUME		U(0x8400007C)
 #define FFA_NOTIFICATION_BITMAP_CREATE	U(0x8400007D)
 #define FFA_NOTIFICATION_BITMAP_DESTROY	U(0x8400007E)
 #define FFA_NOTIFICATION_BIND		U(0x8400007F)

core/arch/arm/kernel/thread_spmc.c
@@ -1340,7 +1340,11 @@ void thread_spmc_msg_recv(struct thread_smc_args *args)
 #endif /*CFG_CORE_SEL1_SPMC*/
 	case FFA_INTERRUPT:
 		interrupt_main_handler();
-		spmc_set_args(args, FFA_MSG_WAIT, 0, 0, 0, 0, 0);
+		if (IS_ENABLED(CFG_CORE_SEL1_SPMC))
+			spmc_set_args(args, FFA_NORMAL_WORLD_RESUME, 0, 0, 0,
+				      0, 0);
+		else
+			spmc_set_args(args, FFA_MSG_WAIT, 0, 0, 0, 0, 0);
 		break;
 #ifdef ARM64
 	case FFA_MSG_SEND_DIRECT_REQ_64: