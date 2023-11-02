43e0957ad66eef409607e885865afdb78a9121e6
Etienne Carriere <etienne.carriere@foss.st.com>
2023-11-02 22:41:03 +0100

    plat-stm32mp1: syscfg: HLSV mode for IO domains
    Add platform API functions stm32mp_set_hslv_state() and
    stm32mp_enable_fixed_vdd_hslv() to configure High Speed Low Voltage
    mode of IO domains.
    
    Platform function stm32mp_enable_fixed_vdd_hslv() is designed for
    fixed voltage IO domains that need to be enable at boot time only
    since the supply voltage level never changes.
    
    On STM32MP13 variants, SDMMC IO domains may not be supplied by fixed
    voltage VDD but rather by a supply which voltage level can change
    at runtime for example to support SD/MMC normative 1.8V and 3.3V voltage
    modes. Therefore these IO domains require a runtime configuration
    function implemented by stm32mp_set_hslv_state().
    
    Acked-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Co-developed-by: Pascal Paillet <p.paillet@foss.st.com>
    Signed-off-by: Pascal Paillet <p.paillet@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c
+++ core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c
@@ -18,11 +18,22 @@
 /*
  * SYSCFG register offsets (base relative)
  */
+#define SYSCFG_IOCTRLSETR			U(0x18)
 #define SYSCFG_CMPCR				U(0x20)
 #define SYSCFG_CMPENSETR			U(0x24)
 #define SYSCFG_CMPSD1CR				U(0x30)
 #define SYSCFG_CMPSD2CR				U(0x40)
+#define SYSCFG_HSLVEN0R				U(0x50)
 #define SYSCFG_IOSIZE				U(0x400)
+
+/*
+ * SYSCFG_IOCTRLSETR Register for STM32MP15 variants
+ */
+#define SYSCFG_IOCTRLSETR_HSLVEN_TRACE		BIT(0)
+#define SYSCFG_IOCTRLSETR_HSLVEN_QUADSPI	BIT(1)
+#define SYSCFG_IOCTRLSETR_HSLVEN_ETH		BIT(2)
+#define SYSCFG_IOCTRLSETR_HSLVEN_SDMMC		BIT(3)
+#define SYSCFG_IOCTRLSETR_HSLVEN_SPI		BIT(4)
 
 /*
  * SYSCFG_CMPCR Register
@@ -43,6 +54,12 @@
  * SYSCFG_CMPENSETR Register
  */
 #define SYSCFG_CMPENSETR_MPU_EN			BIT(0)
+
+/*
+ * HSLV definitions
+ */
+#define SYSCFG_HSLV_MASK			GENMASK_32(15, 0)
+#define SYSCFG_HSLV_KEY				U(0x1018)
 
 static vaddr_t get_syscfg_base(void)
 {
@@ -85,16 +102,16 @@ static __maybe_unused void disable_io_compensation(int cmpcr_offset)
 	      value_cmpcr2 & SYSCFG_CMPENSETR_MPU_EN))
 		return;
 
-	/* Copy apsrc / ansrc in ransrc /rapsrc */
+	/* Copy APSRC (resp. ANSRC) in RAPSRC (resp. RANSRC) */
 	apsrc_ansrc = value_cmpcr >> SYSCFG_CMPCR_ANSRC_SHIFT;
 	value_cmpcr &= ~(SYSCFG_CMPCR_RANSRC | SYSCFG_CMPCR_RAPSRC);
 	value_cmpcr |= SHIFT_U32(apsrc_ansrc, SYSCFG_CMPCR_RANSRC_SHIFT);
 
 	io_write32(cmpcr_base, value_cmpcr | SYSCFG_CMPCR_SW_CTRL);
 
-	DMSG("SYSCFG.cmpcr = %#"PRIx32, io_read32(cmpcr_base));
-
 	io_setbits32(cmpcr_base + CMPENCLRR_OFFSET, SYSCFG_CMPENSETR_MPU_EN);
+
+	DMSG("SYSCFG.cmpcr = %#"PRIx32, io_read32(cmpcr_base));
 }
 
 static TEE_Result stm32mp1_iocomp(void)
@@ -131,4 +148,52 @@ void stm32mp_set_vddsd_comp_state(enum stm32mp13_vddsd_comp_id id, bool enable)
 	else
 		disable_io_compensation(cmpcr_offset);
 }
+
+void stm32mp_set_hslv_state(enum stm32mp13_hslv_id id, bool enable)
+{
+	size_t hslvenxr_offset = 0;
+	uint32_t hlvs_value = 0;
+
+	assert(id < SYSCFG_HSLV_COUNT);
+
+	if (enable)
+		hlvs_value = SYSCFG_HSLV_KEY;
+
+	/* IDs are indices of SYSCFG_HSLVENxR registers */
+	hslvenxr_offset = SYSCFG_HSLVEN0R + id * sizeof(uint32_t);
+
+	io_write32(get_syscfg_base() + hslvenxr_offset, hlvs_value);
+
+	/* Value read shall be 1 on enable and 0 on disable */
+	hlvs_value = io_read32(get_syscfg_base() + hslvenxr_offset) &
+		     SYSCFG_HSLV_MASK;
+	if (enable != hlvs_value)
+		panic();
+}
+
+void stm32mp_enable_fixed_vdd_hslv(void)
+{
+	enum stm32mp13_hslv_id id = SYSCFG_HSLV_COUNT;
+
+	for (id = SYSCFG_HSLV_IDX_TPIU; id < SYSCFG_HSLV_COUNT; id++) {
+		/* SDMMCs domains may not be supplied by VDD */
+		if (id == SYSCFG_HSLV_IDX_SDMMC1 ||
+		    id == SYSCFG_HSLV_IDX_SDMMC2)
+			continue;
+
+		stm32mp_set_hslv_state(id, true);
+	}
+}
 #endif /* CFG_STM32MP13 */
+
+#ifdef CFG_STM32MP15
+void stm32mp_enable_fixed_vdd_hslv(void)
+{
+	io_write32(get_syscfg_base() + SYSCFG_IOCTRLSETR,
+		   SYSCFG_IOCTRLSETR_HSLVEN_TRACE |
+		   SYSCFG_IOCTRLSETR_HSLVEN_QUADSPI |
+		   SYSCFG_IOCTRLSETR_HSLVEN_ETH |
+		   SYSCFG_IOCTRLSETR_HSLVEN_SDMMC |
+		   SYSCFG_IOCTRLSETR_HSLVEN_SPI);
+}
+#endif
