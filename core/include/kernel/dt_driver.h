OP-TEE/optee_os

commit b357d34fe91f4e7f6e0eacea17a7fbe5f6c01e7e

core: dt_driver: swap TEE_result and retrieved device reference
Changes dt_driver callback function to return a TEE_Result value
and pass retrieved device reference by a output argument rather
than the opposite.

This change updates dt_driver.c, dt_driver.h and all drivers
implementing related dt_driver callback function.

As a consequence, this change removes all type definition related
to device specific callback handler function types which are useless
as all these now comply with type dt_driver_probe_func defined in
dt_driver.h.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/kernel/dt_driver.h
@@ -108,8 +108,8 @@ struct dt_pargs {
  * Return a device opaque reference, e.g. a struct clk pointer for a clock
  * driver, or NULL if not found in which case @res provides the error code.
  */
-typedef void *(*get_of_device_func)(struct dt_pargs *parg, void *data,
-				    TEE_Result *res);
+typedef TEE_Result (*get_of_device_func)(struct dt_pargs *parg, void *data,
+					 void **out_device);
 
 /**
  * dt_driver_register_provider - Register a driver provider
@@ -137,20 +137,21 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
  * @nodeoffset: node offset in the FDT
  * @prop_idx: index of the phandle data in the property
  * @type: Driver type
- * @res: Output result code of the operation:
- *	TEE_SUCCESS in case of success
- *	TEE_ERROR_DEFER_DRIVER_INIT if device driver is not yet initialized
- *	TEE_ERROR_ITEM_NOT_FOUND if prop_name does not match a property's name
- *	Any TEE_Result compliant code in case of error.
- *
- * Return a device opaque reference, e.g. a struct clk pointer for a clock
- * driver, or NULL if not found in which case @res provides the error code.
+ * @out_device: output device opaque reference upon support, for example
+ *	a struct clk pointer for a clock driver.
+
+ * Return code:
+ * TEE_SUCCESS in case of success,
+ * TEE_ERROR_DEFER_DRIVER_INIT if device driver is not yet initialized
+ * TEE_ERROR_ITEM_NOT_FOUND if @prop_name does not match a property's name
+ *	or @prop_idx does not match any index in @prop_name phandle list
+ * Any TEE_Result compliant code in case of error.
  */
-void *dt_driver_device_from_node_idx_prop(const char *prop_name,
-					  const void *fdt, int nodeoffset,
-					  unsigned int prop_idx,
-					  enum dt_driver_type type,
-					  TEE_Result *res);
+TEE_Result dt_driver_device_from_node_idx_prop(const char *prop_name,
+					       const void *fdt, int nodeoffset,
+					       unsigned int prop_idx,
+					       enum dt_driver_type type,
+					       void **out_device);
 
 /*
  * dt_driver_device_from_parent - Return a device instance based on the parent.
@@ -160,16 +161,17 @@ void *dt_driver_device_from_node_idx_prop(const char *prop_name,
  * @fdt: FDT base address
  * @nodeoffset: node offset in the FDT
  * @type: Driver type
- * @res: Output result code of the operation:
- *	TEE_SUCCESS in case of success
- *	TEE_ERROR_DEFER_DRIVER_INIT if device driver is not yet initialized
- *	Any TEE_Result compliant code in case of error.
+ * @dout_device: output device opaque reference upon success, for example
+ *	a struct i2c_dev pointer for a I2C bus driver
  *
- * Return a device opaque reference, e.g. a struct i2c_dev pointer for a I2C bus
- * driver, or NULL if not found in which case @res provides the error code.
+ * Return code:
+ * TEE_SUCCESS in case of success,
+ * TEE_ERROR_DEFER_DRIVER_INIT if device driver is not yet initialized
+ * Any TEE_Result compliant code in case of error.
  */
-void *dt_driver_device_from_parent(const void *fdt, int nodeoffset,
-				   enum dt_driver_type type, TEE_Result *res);
+TEE_Result dt_driver_device_from_parent(const void *fdt, int nodeoffset,
+					enum dt_driver_type type,
+					void **out_device);
 
 /*
  * dt_driver_device_from_node_idx_prop_phandle() - Same as
@@ -180,12 +182,13 @@ void *dt_driver_device_from_parent(const void *fdt, int nodeoffset,
  * property carries the interrupt information but not the interrupt controller
  * phandle which is found in a specific property (here "interrupt-parent").
  */
-void *dt_driver_device_from_node_idx_prop_phandle(const char *prop_name,
-						  const void *fdt, int nodeoffs,
-						  unsigned int prop_index,
-						  enum dt_driver_type type,
-						  uint32_t phandle,
-						  TEE_Result *res);
+TEE_Result dt_driver_device_from_node_idx_prop_phandle(const char *prop_name,
+						       const void *fdt,
+						       int nodeoffs,
+						       unsigned int prop_index,
+						       enum dt_driver_type type,
+						       uint32_t phandle,
+						       void **out_device);
 
 /*
  * dt_driver_get_crypto() - Request crypto support for driver initialization

core/drivers/atmel_piobu.c
@@ -204,24 +204,26 @@ static const struct gpio_ops atmel_piobu_ops = {
 	.set_interrupt = secumod_gpio_set_interrupt,
 };
 
-static struct gpio *secumod_dt_get(struct dt_pargs *pargs, void *data,
-				   TEE_Result *res)
+static TEE_Result secumod_dt_get(struct dt_pargs *pargs, void *data,
+				 struct gpio **out_gpio)
 {
+	TEE_Result res = TEE_ERROR_GENERIC;
 	struct gpio *gpio = NULL;
 	struct gpio_chip *chip = data;
 
-	gpio = gpio_dt_alloc_pin(pargs, res);
-	if (*res)
-		return NULL;
+	res = gpio_dt_alloc_pin(pargs, &gpio);
+	if (res)
+		return res;
 
 	if (gpio_protected & BIT32(gpio->pin)) {
 		free(gpio);
-		return NULL;
+		return TEE_ERROR_GENERIC;
 	}
 
 	gpio->chip = chip;
+	*out_gpio = gpio;
 
-	return gpio;
+	return TEE_SUCCESS;
 }
 
 static enum itr_return secumod_it_handler(struct itr_handler *handler __unused)

core/drivers/clk/clk-stm32-core.c
@@ -544,23 +544,23 @@ struct clk *stm32mp_rcc_clock_id_to_clk(unsigned long clock_id)
 	return priv->clk_refs[clock_id];
 }
 
-static struct clk *stm32mp_clk_dt_get_clk(struct dt_pargs *pargs,
-					  void *data __unused, TEE_Result *res)
+static TEE_Result stm32mp_clk_dt_get_clk(struct dt_pargs *pargs,
+					 void *data __unused,
+					 struct clk **out_clk)
 {
 	unsigned long clock_id = pargs->args[0];
 	struct clk *clk = NULL;
 
-	*res = TEE_ERROR_BAD_PARAMETERS;
-
 	if (pargs->args_count != 1)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
 	clk = stm32mp_rcc_clock_id_to_clk(clock_id);
 	if (!clk)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	*res = TEE_SUCCESS;
-	return clk;
+	*out_clk = clk;
+
+	return TEE_SUCCESS;
 }
 
 static void clk_stm32_register_clocks(struct clk_stm32_priv *priv)

core/drivers/clk/clk-stm32mp15.c
@@ -1499,23 +1499,23 @@ static TEE_Result register_stm32mp1_clocks(void)
 	return TEE_SUCCESS;
 }
 
-static struct clk *stm32mp1_clk_dt_get_clk(struct dt_pargs *pargs,
-					   void *data __unused, TEE_Result *res)
+static TEE_Result stm32mp1_clk_dt_get_clk(struct dt_pargs *pargs,
+					  void *data __unused,
+					  struct clk **out_clk)
 {
 	unsigned long clock_id = pargs->args[0];
 	struct clk *clk = NULL;
 
-	*res = TEE_ERROR_BAD_PARAMETERS;
-
 	if (pargs->args_count != 1)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
 	clk = clock_id_to_clk(clock_id);
 	if (!clk)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	*res = TEE_SUCCESS;
-	return clk;
+	*out_clk = clk;
+
+	return TEE_SUCCESS;
 }
 
 /* Non-null reference for compat data */

core/drivers/clk/clk_dt.c
@@ -32,10 +32,14 @@ static TEE_Result clk_dt_get_by_idx_prop(const char *prop_name, const void *fdt,
 					 struct clk **clk)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
+	void *out_clk = NULL;
 
-	*clk = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
-						   clk_idx, DT_DRIVER_CLK,
-						   &res);
+	res = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
+						  clk_idx, DT_DRIVER_CLK,
+						  &out_clk);
+	if (!res)
+		*clk = out_clk;
+
 	return res;
 }
 

core/drivers/clk/sam/at91_clk.h
@@ -130,7 +130,7 @@ struct pmc_data *pmc_data_allocate(unsigned int ncore, unsigned int nsystem,
 				   unsigned int nperiph, unsigned int ngck,
 				   unsigned int npck);
 
-struct clk *clk_dt_pmc_get(struct dt_pargs *args, void *data, TEE_Result *res);
+TEE_Result clk_dt_pmc_get(struct dt_pargs *args, void *data, struct clk **clk);
 
 struct clk *pmc_clk_get_by_name(struct pmc_clk *clks, unsigned int nclk,
 				const char *name);

core/drivers/clk/sam/at91_pmc.c
@@ -80,22 +80,17 @@ TEE_Result pmc_clk_get(struct pmc_data *pmc, unsigned int type,
 	return TEE_SUCCESS;
 }
 
-struct clk *clk_dt_pmc_get(struct dt_pargs *clkspec, void *data,
-			   TEE_Result *res)
+TEE_Result clk_dt_pmc_get(struct dt_pargs *clkspec, void *data,
+			  struct clk **out_clk)
 {
 	unsigned int type = clkspec->args[0];
 	unsigned int idx = clkspec->args[1];
 	struct pmc_data *pmc_data = data;
-	struct clk *clk = NULL;
 
-	if (clkspec->args_count != 2) {
-		*res = TEE_ERROR_BAD_PARAMETERS;
-		return NULL;
-	}
+	if (clkspec->args_count != 2)
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	*res = pmc_clk_get(pmc_data, type, idx, &clk);
-
-	return clk;
+	return pmc_clk_get(pmc_data, type, idx, out_clk);
 }
 
 struct pmc_data *pmc_data_allocate(unsigned int ncore, unsigned int nsystem,

core/drivers/gpio/gpio.c
@@ -11,26 +11,23 @@
 #include <tee_api_types.h>
 #include <util.h>
 
-struct gpio *gpio_dt_alloc_pin(struct dt_pargs *pargs, TEE_Result *res)
+TEE_Result gpio_dt_alloc_pin(struct dt_pargs *pargs, struct gpio **out_gpio)
 {
 	struct gpio *gpio = NULL;
 
-	if (pargs->args_count != 2) {
-		*res = TEE_ERROR_BAD_PARAMETERS;
-		return NULL;
-	}
+	if (pargs->args_count != 2)
+		return TEE_ERROR_BAD_PARAMETERS;
 
 	gpio = calloc(1, sizeof(struct gpio));
-	if (!gpio) {
-		*res = TEE_ERROR_OUT_OF_MEMORY;
-		return NULL;
-	}
+	if (!gpio)
+		return TEE_ERROR_OUT_OF_MEMORY;
 
 	gpio->pin = pargs->args[0];
 	gpio->dt_flags = pargs->args[1];
 
-	*res = TEE_SUCCESS;
-	return gpio;
+	*out_gpio = gpio;
+
+	return TEE_SUCCESS;
 }
 
 static char *gpio_get_dt_prop_name(const char *gpio_name)
@@ -53,15 +50,18 @@ TEE_Result gpio_dt_get_by_index(const void *fdt, int nodeoffset,
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
 	char *prop_name = NULL;
+	void *out_gpio = NULL;
 
 	prop_name = gpio_get_dt_prop_name(gpio_name);
 	if (!prop_name)
 		return TEE_ERROR_OUT_OF_MEMORY;
 
-	*gpio = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
-						    index, DT_DRIVER_GPIO,
-						    &res);
+	res = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
+						  index, DT_DRIVER_GPIO,
+						  &out_gpio);
 	free(prop_name);
+	if (!res)
+		*gpio = out_gpio;
 
 	return res;
 }

commit 1058d764534b2b29e70aaff521329ebbccc9bef5

core: kernel: interrupt: fix inline description
Fixes the inline description comment for type get_of_device_func
and API function dt_driver_device_from_parent().

Fixes: b357d34fe91f ("core: dt_driver: swap TEE_result and retrieved device reference")
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/kernel/dt_driver.h
@@ -102,13 +102,13 @@ struct dt_pargs {
  *
  * @parg: phandle argument(s) referencing the device in the FDT.
  * @data: driver private data registered in struct dt_driver.
- * @res: Output result code of the operation:
- *	TEE_SUCCESS in case of success
- *	TEE_ERROR_DEFER_DRIVER_INIT if device driver is not yet initialized
- *	Any TEE_Result compliant code in case of error.
+ * @out_device: output device reference upon success, e.g. a struct clk
+ *	pointer for a clock driver.
  *
- * Return a device opaque reference, e.g. a struct clk pointer for a clock
- * driver, or NULL if not found in which case @res provides the error code.
+ * Return code:
+ * TEE_SUCCESS in case of success
+ * TEE_ERROR_DEFER_DRIVER_INIT if device driver is not yet initialized
+ * Any TEE_Result compliant code in case of error.
  */
 typedef TEE_Result (*get_of_device_func)(struct dt_pargs *parg, void *data,
 					 void **out_device);
@@ -163,7 +163,7 @@ TEE_Result dt_driver_device_from_node_idx_prop(const char *prop_name,
  * @fdt: FDT base address
  * @nodeoffset: node offset in the FDT
  * @type: Driver type
- * @dout_device: output device opaque reference upon success, for example
+ * @out_device: output device opaque reference upon success, for example
  *	a struct i2c_dev pointer for a I2C bus driver
  *
  * Return code: