OP-TEE/optee_os

commit 97ea199a2ae8c2c4662db64c821a887deb1d9b5b

core: io: IO_READ32_POLL_TIMEOUT()
Implement Polling Read Register Interface inspired by linux kernel.

Signed-off-by: Xiaoxu Zeng <zengxiaoxu@huawei.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/io.h
@@ -273,4 +273,31 @@ static inline void io_clrsetbits8(vaddr_t addr, uint8_t clear_mask,
 	io_write8(addr, (io_read8(addr) & ~clear_mask) | set_mask);
 }
 
+/*
+ * Poll on a IO memory content or timeout
+ *
+ * @_addr is the address of the memory cell accessed
+ * @_val represents the val of the memory cell accessed
+ * @_cond represents the condition to get the correct value
+ * @_delay_us represents the read interval in mircorseconds
+ * @_timeout_us represents the timeout period in microseconds
+ *
+ * @return nonzero value means timeout, 0 means got right value
+ */
+#define IO_READ32_POLL_TIMEOUT(_addr, _val, _cond, _delay_us, _timeout_us) \
+	({ \
+		uint32_t __timeout = 0; \
+		uint32_t __delay = (_delay_us); \
+		\
+		while (__timeout < (_timeout_us)) { \
+			(_val) = io_read32(_addr); \
+			if (_cond) \
+				break; \
+			__timeout += (__delay); \
+			udelay(__delay); \
+		} \
+		(_val) = io_read32(_addr); \
+		!(_cond); \
+	})
+
 #endif /*IO_H*/

commit ff09957914180fa21c43b933ed5bb26873c8a3cb

core: io: fix missing include of delay.h
Adds missing include of kernel/delay.h for udelay() used in
IO_READ32_POLL_TIMEOUT() macro.

Fixes: 97ea199a2ae8 ("core: io: IO_READ32_POLL_TIMEOUT()")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/io.h
@@ -6,6 +6,7 @@
 #define __IO_H
 
 #include <compiler.h>
+#include <kernel/delay.h>
 #include <stdint.h>
 #include <types_ext.h>
 #include <utee_defines.h>