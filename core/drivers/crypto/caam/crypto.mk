OP-TEE/optee_os

commit b21f122096717b506cd188776655ca6d1c2d132f

drivers: crypto: caam: use job ring 3 on i.mx8dxlevk
Use the job ring #3 on i.mx8dxl to avoid resource conflict with other
software stacks.

Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/drivers/crypto/caam/crypto.mk
@@ -71,7 +71,7 @@ caam-crypto-drivers += RSA DSA ECC DH MATH
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8dxl-flavorlist)))
 $(call force, CFG_CAAM_SIZE_ALIGN,4)
 $(call force, CFG_JR_BLOCK_SIZE,0x10000)
-$(call force, CFG_JR_INDEX,2)
+$(call force, CFG_JR_INDEX,3)
 $(call force, CFG_JR_INT,355)
 $(call force, CFG_NXP_CAAM_SGT_V1,y)
 $(call force, CFG_CAAM_JR_DISABLE_NODE,n)

commit a314452b31257cd5abe70899595be9dc91474655

drivers: crypto: caam: fix job ring interruption number
The job ring interruption number is 356 for job ring 3.

Fixes: b21f12209671 ("drivers: crypto: caam: use job ring 3 on i.mx8dxlevk")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/drivers/crypto/caam/crypto.mk
@@ -72,7 +72,7 @@ else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8dxl-flavorlist)))
 $(call force, CFG_CAAM_SIZE_ALIGN,4)
 $(call force, CFG_JR_BLOCK_SIZE,0x10000)
 $(call force, CFG_JR_INDEX,3)
-$(call force, CFG_JR_INT,355)
+$(call force, CFG_JR_INT,356)
 $(call force, CFG_NXP_CAAM_SGT_V1,y)
 $(call force, CFG_CAAM_JR_DISABLE_NODE,n)
 caam-crypto-drivers += RSA DSA ECC DH MATH