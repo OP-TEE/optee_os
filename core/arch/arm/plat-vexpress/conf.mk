OP-TEE/optee_os

commit 24475b562b81adfa5bf0653554cefb1dc38f2c9e

plat-vexpress: move CFG_TEE_CORE_NB_CORE to platform conf.mk
Aggregate juno config.
Use same platform ordering in conf.mk and platform_config.h.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Joakim Bech <joakim.bech@linaro.org>

core/arch/arm/plat-vexpress/conf.mk
@@ -13,7 +13,6 @@ platform-debugger-arm := 1
 endif
 ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
 include core/arch/arm/cpu/cortex-armv8-0.mk
-$(call force,CFG_DT,y)
 endif
 
 
@@ -45,11 +44,21 @@ endif
 CFG_WITH_STACK_CANARIES ?= y
 CFG_WITH_STATS ?= y
 
+ifeq ($(PLATFORM_FLAVOR),fvp)
+CFG_TEE_CORE_NB_CORE = 8
+# DRAM1 is defined above 4G
+$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+endif
+
 ifeq ($(PLATFORM_FLAVOR),juno)
+CFG_TEE_CORE_NB_CORE = 6
+# DRAM1 is defined above 4G
+$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
 CFG_CRYPTO_WITH_CE ?= y
 endif
 
 ifeq ($(PLATFORM_FLAVOR),qemu_virt)
+CFG_TEE_CORE_NB_CORE = 4
 ifeq ($(CFG_CORE_SANITIZE_KADDRESS),y)
 # CFG_ASAN_SHADOW_OFFSET is calculated as:
 # (&__asan_shadow_start - (TEE_RAM_VA_START / 8)
@@ -67,12 +76,7 @@ CFG_SE_API_SELF_TEST ?= y
 CFG_PCSC_PASSTHRU_READER_DRV ?= n
 endif
 
-ifeq ($(PLATFORM_FLAVOR),fvp)
-# DRAM1 is defined above 4G
-$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
-endif
-
-ifeq ($(PLATFORM_FLAVOR),juno)
-# DRAM1 is defined above 4G
-$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
+CFG_TEE_CORE_NB_CORE = 2
+$(call force,CFG_DT,y)
 endif

core/arch/arm/plat-vexpress/platform_config.h
@@ -106,8 +106,6 @@
 
 #endif /*CFG_WITH_PAGER*/
 
-#define CFG_TEE_CORE_NB_CORE	8
-
 #define TEE_SHMEM_START		(DRAM0_BASE + 0x3000000)
 #define TEE_SHMEM_SIZE		0x200000
 
@@ -149,8 +147,6 @@
 #define TZDRAM_SIZE		0x00E00000
 #endif /*CFG_WITH_PAGER*/
 
-#define CFG_TEE_CORE_NB_CORE	6
-
 #define TEE_SHMEM_START		(DRAM0_BASE + DRAM0_SIZE - TEE_SHMEM_SIZE)
 #define TEE_SHMEM_SIZE		0x200000
 
@@ -190,8 +186,6 @@
 
 #endif /* CFG_WITH_PAGER */
 
-#define CFG_TEE_CORE_NB_CORE	4
-
 #define TEE_SHMEM_START		(DRAM0_TEERES_BASE + \
 					(DRAM0_TEERES_SIZE - TEE_SHMEM_SIZE))
 #define TEE_SHMEM_SIZE		0x200000
@@ -207,7 +201,6 @@
 
 #define SECRAM_BASE		0x0e000000
 #define SECRAM_SIZE		0x01000000
-
 
 #ifdef CFG_WITH_PAGER
 
@@ -226,8 +219,6 @@
 #define TZDRAM_SIZE		(SECRAM_SIZE - 0x00100000)
 
 #endif /* CFG_WITH_PAGER */
-
-#define CFG_TEE_CORE_NB_CORE	2
 
 /*
  * TEE_SHMEM_START chosen arbitrary, in a way that it does not interfere

commit b2e7a7790c5b9674b13af2b78ecfe64d91313de0

plat-vexpress: relax CFG_TEE_CORE_NB_CORE configuration value
Fixes CFG_TEE_CORE_NB_CORE configuration value for all vexpress
platform flavors. Before this change CFG_TEE_CORE_NB_CORE variable
needed a specific scope to override the default value set by platform
conf.mk file.

Fixes: 24475b562b81 ("plat-vexpress: move CFG_TEE_CORE_NB_CORE to platform conf.mk")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-vexpress/conf.mk
@@ -64,7 +64,7 @@ endif
 
 ifeq ($(PLATFORM_FLAVOR),fvp)
 CFG_HALT_CORES_ON_PANIC ?= y
-CFG_TEE_CORE_NB_CORE = 8
+CFG_TEE_CORE_NB_CORE ?= 8
 ifeq ($(CFG_CORE_SEL2_SPMC),y)
 CFG_TZDRAM_START ?= 0x06281000
 CFG_TZDRAM_SIZE  ?= 0x01D80000
@@ -84,7 +84,7 @@ endif
 
 ifeq ($(PLATFORM_FLAVOR),juno)
 CFG_HALT_CORES_ON_PANIC ?= y
-CFG_TEE_CORE_NB_CORE = 6
+CFG_TEE_CORE_NB_CORE ?= 6
 CFG_TZDRAM_START ?= 0xff000000
 CFG_TZDRAM_SIZE  ?= 0x00ff8000
 CFG_SHMEM_START  ?= 0xfee00000
@@ -99,7 +99,7 @@ endif
 
 ifeq ($(PLATFORM_FLAVOR),qemu_virt)
 CFG_HALT_CORES_ON_PANIC ?= y
-CFG_TEE_CORE_NB_CORE = 4
+CFG_TEE_CORE_NB_CORE ?= 4
 # [0e00.0000 0e0f.ffff] is reserved to early boot
 CFG_TZDRAM_START ?= 0x0e100000
 CFG_TZDRAM_SIZE  ?= 0x00f00000
@@ -127,7 +127,7 @@ endif
 
 ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
 CFG_HALT_CORES_ON_PANIC ?= y
-CFG_TEE_CORE_NB_CORE = 4
+CFG_TEE_CORE_NB_CORE ?= 4
 ifneq ($(CFG_CORE_SEL2_SPMC),y)
 # [0e00.0000 0e0f.ffff] is reserved to early boot
 CFG_TZDRAM_START ?= 0x0e100000