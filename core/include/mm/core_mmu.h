38bb5f2b6f04a3370c771367fbb0f5151d785326
Etienne Carriere <etienne.carriere@foss.st.com>
2023-11-20 11:43:57 +0100

    core: MEM_AREA_ROM_SEC maps secure read only cached memory
    Define memory area mapping identifier MEM_AREA_ROM_SEC to map read-only
    secure cached memory.
    
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/include/mm/core_mmu.h
+++ core/include/mm/core_mmu.h
@@ -73,6 +73,7 @@
  * MEM_AREA_NEX_NSEC_SHM: nexus non-secure shared RAM between NSec and TEE.
  * MEM_AREA_RAM_NSEC: NonSecure RAM storing data
  * MEM_AREA_RAM_SEC:  Secure RAM storing some secrets
+ * MEM_AREA_ROM_SEC:  Secure read only memory storing some secrets
  * MEM_AREA_IO_NSEC:  NonSecure HW mapped registers
  * MEM_AREA_IO_SEC:   Secure HW mapped registers
  * MEM_AREA_EXT_DT:   Memory loads external device tree
@@ -103,6 +104,7 @@ enum teecore_memtypes {
 	MEM_AREA_NEX_NSEC_SHM,
 	MEM_AREA_RAM_NSEC,
 	MEM_AREA_RAM_SEC,
+	MEM_AREA_ROM_SEC,
 	MEM_AREA_IO_NSEC,
 	MEM_AREA_IO_SEC,
 	MEM_AREA_EXT_DT,
@@ -138,6 +140,7 @@ static inline const char *teecore_memtype_name(enum teecore_memtypes type)
 		[MEM_AREA_NEX_NSEC_SHM] = "NEX_NSEC_SHM",
 		[MEM_AREA_RAM_NSEC] = "RAM_NSEC",
 		[MEM_AREA_RAM_SEC] = "RAM_SEC",
+		[MEM_AREA_ROM_SEC] = "ROM_SEC",
 		[MEM_AREA_IO_NSEC] = "IO_NSEC",
 		[MEM_AREA_IO_SEC] = "IO_SEC",
 		[MEM_AREA_EXT_DT] = "EXT_DT",
