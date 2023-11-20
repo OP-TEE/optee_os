OP-TEE/optee_os

commit 24475b562b81adfa5bf0653554cefb1dc38f2c9e

plat-vexpress: move CFG_TEE_CORE_NB_CORE to platform conf.mk
Aggregate juno config.
Use same platform ordering in conf.mk and platform_config.h.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Joakim Bech <joakim.bech@linaro.org>

core/arch/arm/plat-vexpress/conf.mk
@@ -13,7 +13,6 @@ platform-debugger-arm := 1
 endif
 ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
 include core/arch/arm/cpu/cortex-armv8-0.mk
-$(call force,CFG_DT,y)
 endif
 
 
@@ -45,11 +44,21 @@ endif
 CFG_WITH_STACK_CANARIES ?= y
 CFG_WITH_STATS ?= y
 
+ifeq ($(PLATFORM_FLAVOR),fvp)
+CFG_TEE_CORE_NB_CORE = 8
+# DRAM1 is defined above 4G
+$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+endif
+
 ifeq ($(PLATFORM_FLAVOR),juno)
+CFG_TEE_CORE_NB_CORE = 6
+# DRAM1 is defined above 4G
+$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
 CFG_CRYPTO_WITH_CE ?= y
 endif
 
 ifeq ($(PLATFORM_FLAVOR),qemu_virt)
+CFG_TEE_CORE_NB_CORE = 4
 ifeq ($(CFG_CORE_SANITIZE_KADDRESS),y)
 # CFG_ASAN_SHADOW_OFFSET is calculated as:
 # (&__asan_shadow_start - (TEE_RAM_VA_START / 8)
@@ -67,12 +76,7 @@ CFG_SE_API_SELF_TEST ?= y
 CFG_PCSC_PASSTHRU_READER_DRV ?= n
 endif
 
-ifeq ($(PLATFORM_FLAVOR),fvp)
-# DRAM1 is defined above 4G
-$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
-endif
-
-ifeq ($(PLATFORM_FLAVOR),juno)
-# DRAM1 is defined above 4G
-$(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
+CFG_TEE_CORE_NB_CORE = 2
+$(call force,CFG_DT,y)
 endif

core/arch/arm/plat-vexpress/platform_config.h
@@ -106,8 +106,6 @@
 
 #endif /*CFG_WITH_PAGER*/
 
-#define CFG_TEE_CORE_NB_CORE	8
-
 #define TEE_SHMEM_START		(DRAM0_BASE + 0x3000000)
 #define TEE_SHMEM_SIZE		0x200000
 
@@ -149,8 +147,6 @@
 #define TZDRAM_SIZE		0x00E00000
 #endif /*CFG_WITH_PAGER*/
 
-#define CFG_TEE_CORE_NB_CORE	6
-
 #define TEE_SHMEM_START		(DRAM0_BASE + DRAM0_SIZE - TEE_SHMEM_SIZE)
 #define TEE_SHMEM_SIZE		0x200000
 
@@ -190,8 +186,6 @@
 
 #endif /* CFG_WITH_PAGER */
 
-#define CFG_TEE_CORE_NB_CORE	4
-
 #define TEE_SHMEM_START		(DRAM0_TEERES_BASE + \
 					(DRAM0_TEERES_SIZE - TEE_SHMEM_SIZE))
 #define TEE_SHMEM_SIZE		0x200000
@@ -207,7 +201,6 @@
 
 #define SECRAM_BASE		0x0e000000
 #define SECRAM_SIZE		0x01000000
-
 
 #ifdef CFG_WITH_PAGER
 
@@ -226,8 +219,6 @@
 #define TZDRAM_SIZE		(SECRAM_SIZE - 0x00100000)
 
 #endif /* CFG_WITH_PAGER */
-
-#define CFG_TEE_CORE_NB_CORE	2
 
 /*
  * TEE_SHMEM_START chosen arbitrary, in a way that it does not interfere

commit 88fbe59ddc6e43bbb478d0782265e72b5aceff5e

plat-vexpress: relax CFG_ASAN_SHADOW_OFFSET configuration value
Fixes CFG_ASAN_SHADOW_OFFSET configuration value for vexpress
platform qemu* flavors. Before this change CFG_ASAN_SHADOW_OFFSET
variable needed a specific scope to override the default value set
by platform conf.mk file.

Fixes: 24475b562b81 ("plat-vexpress: move CFG_TEE_CORE_NB_CORE to platform conf.mk")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-vexpress/conf.mk
@@ -115,7 +115,7 @@ ifeq ($(CFG_CORE_SANITIZE_KADDRESS),y)
 # This is unfortunately currently not possible to do in make so we have to
 # calculate it offline, there's some asserts in
 # core/arch/arm/kernel/generic_boot.c to check that we got it right
-CFG_ASAN_SHADOW_OFFSET = 0xc6a71c0
+CFG_ASAN_SHADOW_OFFSET ?= 0xc6a71c0
 endif
 $(call force,CFG_BOOT_SECONDARY_REQUEST,y)
 $(call force,CFG_PSCI_ARM32,y)
@@ -140,7 +140,7 @@ CFG_SHMEM_SIZE  ?= 0x00200000
 CFG_TEE_SDP_MEM_SIZE ?= 0x00400000
 ifeq ($(CFG_CORE_SANITIZE_KADDRESS),y)
 # See comment above
-CFG_ASAN_SHADOW_OFFSET = 0xc6a71c0
+CFG_ASAN_SHADOW_OFFSET ?= 0xc6a71c0
 endif
 endif
 $(call force,CFG_DT,y)