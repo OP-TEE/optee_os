c7f9abcee87f21b078ba707781af45c487b4025b
Xiaoxu Zeng <zengxiaoxu@huawei.com>
2023-11-21 21:20:08 +0800

    drivers: implement HiSilicon Queue Management (QM) module
    The Hisilicon QM is a Queue Management module.
    In order to unify the interface between accelerator and software,
    a unified queue management module QM is used to interact with software.
    Each accelerator module integrates a QM. Software issues tasks to the SQ
    (Submmision Queue),and the QM obtains the address of the SQE (Submmision
    Queue Element). The BD (Buffer Description, same as SQE) information is
    sent to the accelerator. After the task processing is complete, the
    accelerator applies for a write-back address from the QM to write back
    the SQ.
    
    Signed-off-by: Xiaoxu Zeng <zengxiaoxu@huawei.com>
    Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/drivers/crypto/sub.mk
+++ core/drivers/crypto/sub.mk
@@ -11,3 +11,5 @@ subdirs-$(CFG_STM32_CRYPTO_DRIVER) += stm32
 subdirs-$(CFG_ASPEED_CRYPTO_DRIVER) += aspeed
 
 subdirs-$(CFG_VERSAL_CRYPTO_DRIVER) += versal
+
+subdirs-$(CFG_HISILICON_CRYPTO_DRIVER) += hisilicon
