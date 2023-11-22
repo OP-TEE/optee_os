6c6d5191ce8dc4c102fbc15a85c15551fbbe8d06
Etienne Carriere <etienne.carriere@foss.st.com>
2023-11-22 13:44:35 +0100

    pta: stats: export PTA API definition to TA devkit
    Move statistics PTA API definitions and description to a header
    file that is exported to the TA devkit. This change makes the
    description that PTA API more consistent regarding other PTAs.
    
    Explicitly include tee_api_types.h from stats.c source file.
    
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/pta/stats.c
+++ core/pta/stats.c
@@ -8,48 +8,14 @@
 #include <malloc.h>
 #include <mm/tee_mm.h>
 #include <mm/tee_pager.h>
+#include <pta_stats.h>
 #include <stdio.h>
 #include <string.h>
 #include <string_ext.h>
+#include <tee_api_types.h>
 #include <trace.h>
 
 #define TA_NAME		"stats.ta"
-
-#define STATS_UUID \
-		{ 0xd96a5b40, 0xe2c7, 0xb1af, \
-			{ 0x87, 0x94, 0x10, 0x02, 0xa5, 0xd5, 0xc6, 0x1b } }
-
-#define STATS_CMD_PAGER_STATS		0
-#define STATS_CMD_ALLOC_STATS		1
-#define STATS_CMD_MEMLEAK_STATS		2
-/*
- * UTEE_ENTRY_FUNC_DUMP_MEMSTATS
- * [out]    memref[0]        Array of context information of loaded TAs
- *
- * Each cell of the TA information array contains:
- * TEE_UUID    TA UUID
- * uint32_t    Non zero if TA panicked, 0 otherwise
- * uint32_t    Number of sessions opened by the TA
- * uint32_t    Byte size currently allocated in TA heap
- * uint32_t    Max bytes allocated since last stats reset
- * uint32_t    TA heap pool byte size
- * uint32_t    Number of failed allocation requests
- * uint32_t    Biggest byte size which allocation failed
- * uint32_t    Biggest byte size which allocation succeeded
- */
-#define STATS_CMD_TA_STATS		3
-
-/*
- * STATS_CMD_GET_TIME - Get both REE time and TEE time
- *
- * [out]    value[0].a        REE time as seen by OP-TEE in seconds
- * [out]    value[0].b        REE time as seen by OP-TEE, milliseconds part
- * [out]    value[1].a        TEE system time in seconds
- * [out]    value[1].b        TEE system time, milliseconds part
- */
-#define STATS_CMD_GET_TIME		4
-
-#define STATS_NB_POOLS			4
 
 static TEE_Result get_alloc_stats(uint32_t type, TEE_Param p[TEE_NUM_PARAMS])
 {
