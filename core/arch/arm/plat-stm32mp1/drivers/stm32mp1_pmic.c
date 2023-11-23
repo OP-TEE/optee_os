OP-TEE/optee_os

commit 43c155ba111d44d4013c0ad52f6067e2ddcce5cf

drivers: regulator: list supported levels
Adds regulator API function regulator_supported_voltages() to get the
list of the voltage levels supported by the regulator.

Voltage level array is either an array of increasing ordered levels, in
microvolt, or is a triplet [min, max, step] for linear step incremental
levels.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/regulator/regulator.c
@@ -212,6 +212,26 @@ TEE_Result regulator_set_voltage(struct regulator *regulator, int level_uv)
 	return TEE_SUCCESS;
 }
 
+TEE_Result regulator_supported_voltages(struct regulator *regulator,
+					struct regulator_voltages **voltages)
+{
+	assert(regulator && voltages);
+
+	if (regulator->ops->supported_voltages) {
+		TEE_Result res = TEE_ERROR_GENERIC;
+
+		res = regulator->ops->supported_voltages(regulator, voltages);
+		if (res == TEE_SUCCESS)
+			return TEE_SUCCESS;
+		if (res != TEE_ERROR_NOT_SUPPORTED)
+			return res;
+	}
+
+	*voltages = &regulator->voltages_fallback.desc;
+
+	return TEE_SUCCESS;
+}
+
 TEE_Result regulator_register(struct regulator *regulator)
 {
 	TEE_Result res = TEE_SUCCESS;
@@ -248,6 +268,18 @@ TEE_Result regulator_register(struct regulator *regulator)
 		res = regulator_refcnt_enable(regulator);
 		if (res)
 			return res;
+	}
+
+	/* Preset voltage list in case ops::supported_voltages is NULL */
+	if (regulator->min_uv == regulator->max_uv) {
+		regulator->voltages_fallback.desc.type = VOLTAGE_TYPE_FULL_LIST;
+		regulator->voltages_fallback.desc.num_levels = 1;
+		regulator->voltages_fallback.levels[0] = regulator->min_uv;
+	} else {
+		regulator->voltages_fallback.desc.type = VOLTAGE_TYPE_INCREMENT;
+		regulator->voltages_fallback.levels[0] = regulator->min_uv;
+		regulator->voltages_fallback.levels[1] = regulator->max_uv;
+		regulator->voltages_fallback.levels[2] = 1;
 	}
 
 	SLIST_INSERT_HEAD(&regulator_device_list, regulator, link);

core/include/drivers/regulator.h
@@ -40,6 +40,37 @@ struct regu_dt_desc {
 };
 
 /*
+ * Defines the format of struct voltages::entries
+ *
+ * If regulator_voltages::type is VOLTAGE_TYPE_FULL_LIST, then
+ * regulator_voltages@entries stores regulator_voltages::num_levels cells,
+ * listing supported voltage levels in uV from lowest to highest value.
+ *
+ * If regulator_voltages::type is VOLTAGE_TYPE_INCREMENT, then
+ * regulator_voltages::entries stores 3 cells: min level, max level and
+ * level increment step, all in uV. When so, regulator_voltages::num_levels
+ * is meaningless.
+ */
+enum voltage_type {
+	VOLTAGE_TYPE_INVALID = 0,
+	VOLTAGE_TYPE_FULL_LIST, /* extensive list in uV */
+	VOLTAGE_TYPE_INCREMENT  /* min, max, increment (in uV) */
+};
+
+/*
+ * struct regulator_voltages - Voltage levels description
+ * @type: Type of level description
+ * @num_levels: Number of cells of @entries when @type is VOLTAGE_TYPE_FULL_LIST
+ * @entries: Voltage level information in uV
+ *
+ */
+struct regulator_voltages {
+	enum voltage_type type;
+	size_t num_levels;
+	int entries[];
+};
+
+/*
  * struct regulator - A regulator instance
  * @ops: Operation handlers for the regulator
  * @supply: Regulator supply reference or NULL if none
@@ -51,6 +82,7 @@ struct regu_dt_desc {
  * @flags: REGULATOR_* property flags
  * @refcount: Regulator enable request reference counter
  * @lock: Mutex for concurrent access protection
+ * @voltages_fallback: Default supported voltage range description
  * @link: Link in initialized regulator list
  */
 struct regulator {
@@ -66,6 +98,11 @@ struct regulator {
 	unsigned int flags;
 	unsigned int refcount;
 	struct mutex lock;	/* Concurrent access protection */
+	struct voltages_fallback {
+		struct regulator_voltages desc;
+		int levels[3];
+	} voltages_fallback;
+	size_t levels_count_fallback;
 	SLIST_ENTRY(regulator) link;
 };
 
@@ -76,6 +113,7 @@ struct regulator {
  * @get_state: Get regulator effective state
  * @set_voltage: Set voltage level in microvolt (uV)
  * @get_voltage: Get current voltage in microvolt (uV)
+ * @supported_voltages: Get supported levels description
  * @supplied_init: Optional, finalize initialization once supply is ready
  */
 struct regulator_ops {
@@ -83,6 +121,8 @@ struct regulator_ops {
 	TEE_Result (*get_state)(struct regulator *r, bool *enabled);
 	TEE_Result (*set_voltage)(struct regulator *r, int level_uv);
 	TEE_Result (*get_voltage)(struct regulator *r, int *level_uv);
+	TEE_Result (*supported_voltages)(struct regulator *r,
+					 struct regulator_voltages **voltages);
 	TEE_Result (*supplied_init)(struct regulator *r, const void *fdt,
 				    int node);
 };
@@ -241,4 +281,12 @@ static inline void regulator_get_range(struct regulator *regulator, int *min_uv,
 	if (max_uv)
 		*max_uv = regulator->max_uv;
 }
+
+/*
+ * regulator_supported_voltages() - Get regulator supported levels in microvolt
+ * @regulator: Regulator reference
+ * @voltages: Output description supported voltage levels
+ */
+TEE_Result regulator_supported_voltages(struct regulator *regulator,
+					struct regulator_voltages **voltages);
 #endif /* DRIVERS_REGULATOR_H */

commit ecbc2c07435d3eb2c24bc1d4a18f49448c4fd98b

[P-R#6487] drivers: regulator: fix variable sized voltages fallback
WARNING: Change under review: https://github.com/OP-TEE/optee_os/pull/6487

Fix build issue reported by Clang on variable size field desc
not being located at the end of struct voltages_fallback. The
error was reported with a trace message like below:

core/include/drivers/regulator.h:118:4: warning: field 'voltages_fallback' with variable sized type 'struct voltages_fallback' not at the end of a struct or class is a GNU extension [-Wgnu-variable-sized-type-not-at-end]
        } voltages_fallback;
          ^
core/drivers/regulator/regulator_fixed.c:27:19: warning: field 'regulator' with variable sized type 'struct regulator' not at the end of a struct or class is a GNU extension [-Wgnu-variable-sized-type-not-at-end]
        struct regulator regulator;
                         ^
2 warnings generated.

To achieve this, struct regulator_voltages_hdr is added to store the
voltage level description header information and the fallback level
field uses dedicated struct voltages_fallback which is designed to
store at most 3 entries cells.

Impacted sources files are updated accordingly.

Fixes: 43c155ba111d ("drivers: regulator: list supported levels")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.c
@@ -398,7 +398,7 @@ static int cmp_int_value(const void *a, const void *b)
 static size_t refine_levels_array(struct regulator_voltages *voltages)
 {
 	int *levels_uv = voltages->entries;
-	size_t count = voltages->num_levels;
+	size_t count = voltages->hdr.num_levels;
 	size_t n = 0;
 	size_t m = 0;
 
@@ -443,7 +443,7 @@ static TEE_Result pmic_list_voltages(struct regulator *regulator,
 		for (n = 0; n < level_count; n++)
 			voltages->entries[n] = level_ref[n] * 1000;
 
-		voltages->num_levels = level_count;
+		voltages->hdr.num_levels = level_count;
 		level_count = refine_levels_array(voltages);
 
 		voltages_s = realloc(voltages,
@@ -454,8 +454,8 @@ static TEE_Result pmic_list_voltages(struct regulator *regulator,
 			return TEE_ERROR_OUT_OF_MEMORY;
 		}
 
-		voltages_s->type = VOLTAGE_TYPE_FULL_LIST;
-		voltages_s->num_levels = level_count;
+		voltages_s->hdr.type = VOLTAGE_TYPE_FULL_LIST;
+		voltages_s->hdr.num_levels = level_count;
 		priv->voltages = voltages_s;
 	}
 

core/arch/arm/plat-stm32mp1/scmi_server.c
@@ -646,7 +646,8 @@ int32_t plat_scmi_voltd_levels_array(unsigned int channel_id,
 			return SCMI_NOT_SUPPORTED;
 		if (res)
 			return SCMI_GENERIC_ERROR;
-		if (!voltages || voltages->type != VOLTAGE_TYPE_FULL_LIST) {
+		if (!voltages ||
+		    voltages->hdr.type != VOLTAGE_TYPE_FULL_LIST) {
 			/*
 			 * Triplet min/max/step description. Caller should use
 			 * plat_scmi_voltd_levels_by_step().
@@ -655,7 +656,7 @@ int32_t plat_scmi_voltd_levels_array(unsigned int channel_id,
 		}
 
 		ref = voltages->entries;
-		ref_count = voltages->num_levels;
+		ref_count = voltages->hdr.num_levels;
 
 		/* Bound according to regulator registered min/max levels */
 		for (n = ref_count; n > 0; n--)
@@ -708,7 +709,8 @@ int32_t plat_scmi_voltd_levels_by_step(unsigned int channel_id,
 			return SCMI_NOT_SUPPORTED;
 		if (res)
 			return SCMI_GENERIC_ERROR;
-		if (!voltages || voltages->type != VOLTAGE_TYPE_INCREMENT) {
+		if (!voltages ||
+		    voltages->hdr.type != VOLTAGE_TYPE_INCREMENT) {
 			/*
 			 * Triplet min/max/step description. Caller should use
 			 * plat_scmi_voltd_levels_by_step().

core/drivers/regulator/regulator.c
@@ -215,6 +215,15 @@ TEE_Result regulator_set_voltage(struct regulator *regulator, int level_uv)
 TEE_Result regulator_supported_voltages(struct regulator *regulator,
 					struct regulator_voltages **voltages)
 {
+	struct voltages_fallback __unused fb = { };
+
+	/* struct voltages_fallback and struct voltages_fallback must match */
+	static_assert(!offsetof(struct voltages_fallback, hdr) &&
+		      !offsetof(struct voltages_fallback, hdr) &&
+		      offsetof(struct voltages_fallback, entries) ==
+		      offsetof(struct voltages_fallback, entries) &&
+		      sizeof(*fb.entries) == sizeof(*(*voltages)->entries));
+
 	assert(regulator && voltages);
 
 	if (regulator->ops->supported_voltages) {
@@ -227,13 +236,14 @@ TEE_Result regulator_supported_voltages(struct regulator *regulator,
 			return res;
 	}
 
-	*voltages = &regulator->voltages_fallback.desc;
+	*voltages = (void *)(vaddr_t)&regulator->voltages_fallback;
 
 	return TEE_SUCCESS;
 }
 
 TEE_Result regulator_register(struct regulator *regulator)
 {
+	struct regulator_voltages __unused voltages = { };
 	TEE_Result res = TEE_SUCCESS;
 	int min_uv = 0;
 	int max_uv = 0;
@@ -272,14 +282,14 @@ TEE_Result regulator_register(struct regulator *regulator)
 
 	/* Preset voltage list in case ops::supported_voltages is NULL */
 	if (regulator->min_uv == regulator->max_uv) {
-		regulator->voltages_fallback.desc.type = VOLTAGE_TYPE_FULL_LIST;
-		regulator->voltages_fallback.desc.num_levels = 1;
-		regulator->voltages_fallback.levels[0] = regulator->min_uv;
+		regulator->voltages_fallback.hdr.type = VOLTAGE_TYPE_FULL_LIST;
+		regulator->voltages_fallback.hdr.num_levels = 1;
+		regulator->voltages_fallback.entries[0] = regulator->min_uv;
 	} else {
-		regulator->voltages_fallback.desc.type = VOLTAGE_TYPE_INCREMENT;
-		regulator->voltages_fallback.levels[0] = regulator->min_uv;
-		regulator->voltages_fallback.levels[1] = regulator->max_uv;
-		regulator->voltages_fallback.levels[2] = 1;
+		regulator->voltages_fallback.hdr.type = VOLTAGE_TYPE_INCREMENT;
+		regulator->voltages_fallback.entries[0] = regulator->min_uv;
+		regulator->voltages_fallback.entries[1] = regulator->max_uv;
+		regulator->voltages_fallback.entries[2] = 1;
 	}
 
 	SLIST_INSERT_HEAD(&regulator_device_list, regulator, link);

core/drivers/regulator/stm32_vrefbuf.c
@@ -48,8 +48,8 @@ struct vrefbuf_compat {
  * @clock: VREFBUF access bus clock
  * @regulator: Preallocated instance for the regulator
  * @compat: Compatibility data
- * @voltages_desc: Supported voltage level description
- * @voltages_level: Supplorted levels, must follow @voltages_desc
+ * @voltages_header: Supported voltage level description
+ * @voltages_level: Supplorted levels, must follow @voltages_header
  */
 struct vrefbuf_regul {
 	vaddr_t base;
@@ -57,7 +57,8 @@ struct vrefbuf_regul {
 	uint64_t disable_timeout;
 	struct regulator regulator;
 	const struct vrefbuf_compat *compat;
-	struct regulator_voltages voltages_desc;
+	/* @supported_levels must follow @voltages_header */
+	struct regulator_voltages_hdr voltages_header;
 	int supported_levels[VREFBUF_LEVELS_COUNT];
 };
 
@@ -211,7 +212,12 @@ static TEE_Result vrefbuf_list_voltages(struct regulator *regulator __unused,
 {
 	struct vrefbuf_regul *vr = regulator_to_vr(regulator);
 
-	if (!vr->voltages_desc.type) {
+	/* ::supported_levels must follow ::voltages_header */
+	static_assert(offsetof(typeof(*vr), voltages_header) +
+		      sizeof(vr->voltages_header) ==
+		      offsetof(typeof(*vr), supported_levels));
+
+	if (!vr->voltages_header.type) {
 		size_t num_levels = ARRAY_SIZE(vr->compat->voltages);
 		unsigned int index_high = num_levels - 1;
 		unsigned int index_low = 0;
@@ -234,14 +240,14 @@ static TEE_Result vrefbuf_list_voltages(struct regulator *regulator __unused,
 
 		count = index_high - index_low + 1;
 
-		vr->voltages_desc.type = VOLTAGE_TYPE_FULL_LIST;
-		vr->voltages_desc.num_levels = count;
+		vr->voltages_header.type = VOLTAGE_TYPE_FULL_LIST;
+		vr->voltages_header.num_levels = count;
 		for (n = 0; n < count; n++)
 			vr->supported_levels[n] =
 				vr->compat->voltages[index_low + n];
 	}
 
-	*voltages = &vr->voltages_desc;
+	*voltages = (void *)(vaddr_t)&vr->voltages_header;
 
 	return TEE_SUCCESS;
 }

core/include/drivers/regulator.h
@@ -49,16 +49,16 @@ struct regu_dt_desc {
 };
 
 /*
- * Defines the format of struct voltages::entries
+ * Defines the format of struct regulator_voltages::entries
+ * and struct voltages_fallback::entries.
  *
- * If regulator_voltages::type is VOLTAGE_TYPE_FULL_LIST, then
- * regulator_voltages@entries stores regulator_voltages::num_levels cells,
+ * If regulator_voltages_desc::type is VOLTAGE_TYPE_FULL_LIST, then
+ * @entries cell stores regulator_voltages_desc::num_levels cells,
  * listing supported voltage levels in uV from lowest to highest value.
  *
- * If regulator_voltages::type is VOLTAGE_TYPE_INCREMENT, then
- * regulator_voltages::entries stores 3 cells: min level, max level and
- * level increment step, all in uV. When so, regulator_voltages::num_levels
- * is meaningless.
+ * If regulator_voltages_desc::type is VOLTAGE_TYPE_INCREMENT, then
+ * @entries stores 3 cells: min level, max level and level increment
+ * step, all in uV. When so, regulator_voltages::num_levels is meaningless.
  */
 enum voltage_type {
 	VOLTAGE_TYPE_INVALID = 0,
@@ -67,16 +67,33 @@ enum voltage_type {
 };
 
 /*
- * struct regulator_voltages - Voltage levels description
+ * struct regulator_voltages_hdr - Voltage levels description header
  * @type: Type of level description
- * @num_levels: Number of cells of @entries when @type is VOLTAGE_TYPE_FULL_LIST
- * @entries: Voltage level information in uV
- *
+ * @num_levels: Number levels described when @type is VOLTAGE_TYPE_FULL_LIST
  */
-struct regulator_voltages {
+struct regulator_voltages_hdr {
 	enum voltage_type type;
 	size_t num_levels;
+};
+
+/*
+ * struct regulator_voltages - Variable sized voltage levels description
+ * @hdr: Header of voltage levels description
+ * @entries: Voltage level information in uV
+ */
+struct regulator_voltages {
+	struct regulator_voltages_hdr hdr;
 	int entries[];
+};
+
+/*
+ * struct struct voltages_fallback - Default Voltage levels description
+ * @hdr: Header of fallback voltage levels description
+ * @entries: Voltage level information in uV
+ */
+struct voltages_fallback {
+	struct regulator_voltages_hdr hdr;
+	int entries[3];
 };
 
 /*
@@ -107,10 +124,7 @@ struct regulator {
 	unsigned int flags;
 	unsigned int refcount;
 	struct mutex lock;	/* Concurrent access protection */
-	struct voltages_fallback {
-		struct regulator_voltages desc;
-		int levels[3];
-	} voltages_fallback;
+	struct voltages_fallback voltages_fallback;
 	size_t levels_count_fallback;
 	SLIST_ENTRY(regulator) link;
 };