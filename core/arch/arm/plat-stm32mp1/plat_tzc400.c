OP-TEE/optee_os

commit 59c253f92c6c09f4aefb0f841a280663b399e0d4

plat-stm32mp1: check TZC400 configuration
Core checks TZC400 configuration during initialization to ensure
DDR firewall expectations are satisfied.

Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-stm32mp1/plat_tzc400.c
@@ -1,0 +1,110 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2019-2020, STMicroelectronics
+ */
+
+#include <assert.h>
+#include <config.h>
+#include <drivers/tzc400.h>
+#include <initcall.h>
+#include <kernel/interrupt.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+#include <trace.h>
+#include <util.h>
+
+static enum itr_return tzc_it_handler(struct itr_handler *handler __unused)
+{
+	EMSG("TZC permission failure");
+	tzc_fail_dump();
+
+	if (IS_ENABLED(CFG_STM32MP_PANIC_ON_TZC_PERM_VIOLATION))
+		panic();
+	else
+		tzc_int_clear();
+
+	return ITRR_HANDLED;
+}
+
+static struct itr_handler tzc_itr_handler = {
+	.it = STM32MP1_IRQ_TZC,
+	.handler = tzc_it_handler,
+};
+DECLARE_KEEP_PAGER(tzc_itr_handler);
+
+static bool tzc_region_is_non_secure(unsigned int i, vaddr_t base, size_t size)
+{
+	struct tzc_region_config region_cfg = { };
+	uint32_t ns_cpu_mask = TZC_REGION_ACCESS_RDWR(STM32MP1_TZC_A7_ID);
+	uint32_t filters_mask = GENMASK_32(1, 0);
+
+	if (tzc_get_region_config(i, &region_cfg))
+		panic();
+
+	return region_cfg.base == base && region_cfg.top == (base + size - 1) &&
+	       region_cfg.sec_attr == TZC_REGION_S_NONE &&
+	       (region_cfg.ns_device_access & ns_cpu_mask) == ns_cpu_mask &&
+	       region_cfg.filters == filters_mask;
+}
+
+static bool tzc_region_is_secure(unsigned int i, vaddr_t base, size_t size)
+{
+	struct tzc_region_config region_cfg = { };
+	uint32_t filters_mask = GENMASK_32(1, 0);
+
+	if (tzc_get_region_config(i, &region_cfg))
+		panic();
+
+	return region_cfg.base == base && region_cfg.top == (base + size - 1) &&
+	       region_cfg.sec_attr == TZC_REGION_S_RDWR &&
+	       region_cfg.ns_device_access == 0 &&
+	       region_cfg.filters == filters_mask;
+}
+
+static TEE_Result init_stm32mp1_tzc(void)
+{
+	void *base = phys_to_virt(TZC_BASE, MEM_AREA_IO_SEC);
+	unsigned int region_index = 1;
+	const uint64_t dram_start = DDR_BASE;
+	const uint64_t dram_end = dram_start + CFG_DRAM_SIZE;
+	const uint64_t tzdram_start = CFG_TZDRAM_START;
+	const uint64_t tzdram_size = CFG_TZDRAM_SIZE;
+	const uint64_t tzdram_end = tzdram_start + tzdram_size;
+
+	assert(base);
+
+	tzc_init((vaddr_t)base);
+	tzc_dump_state();
+
+	/*
+	 * Early boot stage is in charge of configuring memory regions
+	 * OP-TEE hence here only check this complies with static Core
+	 * expectations.
+	 */
+	if (dram_start < tzdram_start) {
+		if (!tzc_region_is_non_secure(region_index, dram_start,
+					      tzdram_start - dram_start))
+			panic("Unexpected TZC area on non-secure region");
+
+		region_index++;
+	}
+
+	if (!tzc_region_is_secure(region_index, tzdram_start, tzdram_size))
+		panic("Unexpected TZC configuration on secure region");
+
+	if (tzdram_end < dram_end) {
+		region_index++;
+
+		if (!tzc_region_is_non_secure(region_index, tzdram_end,
+					      dram_end - tzdram_end))
+			panic("Unexpected TZC area on non-secure region");
+	}
+
+	itr_add(&tzc_itr_handler);
+	itr_enable(tzc_itr_handler.it);
+	tzc_set_action(TZC_ACTION_INT);
+
+	return TEE_SUCCESS;
+}
+driver_init(init_stm32mp1_tzc);

core/arch/arm/plat-stm32mp1/conf.mk
@@ -83,11 +83,15 @@ CFG_STM32_RNG ?= y
 CFG_STM32_RNG ?= y
 CFG_STM32_UART ?= y
 CFG_STPMIC1 ?= y
+CFG_TZC400 ?= y
 
 ifeq ($(CFG_STPMIC1),y)
 $(call force,CFG_STM32_I2C,y)
 $(call force,CFG_STM32_GPIO,y)
 endif
+
+# Platform specific configuration
+CFG_STM32MP_PANIC_ON_TZC_PERM_VIOLATION ?= y
 
 # Default enable some test facitilites
 CFG_TEE_CORE_EMBED_INTERNAL_TESTS ?= y

core/arch/arm/plat-stm32mp1/main.c
@@ -54,6 +54,7 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC, PWR_BASE, SMALL_PAGE_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, RCC_BASE, SMALL_PAGE_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, RNG1_BASE, SMALL_PAGE_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, TAMP_BASE, SMALL_PAGE_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, TZC_BASE, SMALL_PAGE_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, USART1_BASE, SMALL_PAGE_SIZE);
 
 #if DDR_BASE < CFG_TZDRAM_START

core/arch/arm/plat-stm32mp1/platform_config.h
@@ -41,6 +41,7 @@
 #define SPI6_BASE			0x5c001000
 #define SYSRAM_BASE			0x2ffc0000
 #define TAMP_BASE			0x5c00a000
+#define TZC_BASE			0x5c006000
 #define UART1_BASE			0x5c000000
 #define UART2_BASE			0x4000e000
 #define UART3_BASE			0x4000f000
@@ -116,6 +117,21 @@
 
 /* TAMP resources */
 #define TAMP_BKP_REGISTER_OFF		0x100
+
+/* TZC resources */
+#define STM32MP1_IRQ_TZC		36
+
+#define STM32MP1_TZC_A7_ID		0
+#define STM32MP1_TZC_M4_ID		1
+#define STM32MP1_TZC_LCD_ID		3
+#define STM32MP1_TZC_GPU_ID		4
+#define STM32MP1_TZC_MDMA_ID		5
+#define STM32MP1_TZC_DMA_ID		6
+#define STM32MP1_TZC_USB_HOST_ID	7
+#define STM32MP1_TZC_USB_OTG_ID		8
+#define STM32MP1_TZC_SDMMC_ID		9
+#define STM32MP1_TZC_ETH_ID		10
+#define STM32MP1_TZC_DAP_ID		15
 
 /* USART/UART resources */
 #define USART1_BASE			UART1_BASE

core/arch/arm/plat-stm32mp1/sub.mk
@@ -5,6 +5,7 @@ srcs-y += reset.S
 srcs-$(CFG_STM32_RNG) += rng_seed.c
 srcs-y += scmi_server.c
 srcs-y += shared_resources.c
+srcs-$(CFG_TZC400) += plat_tzc400.c
 
 subdirs-y += drivers
 subdirs-y += nsec-service

commit ace8ddf2202b9bb6a140a20b1b3095f73ceb6e80

plat-stm32mp1: fix warning trace on TZC configuration check
Fix build warning reported by recent toolchains when TZDRAM
memory ends at the UINT32_MAX. This happends for example when
building for the stm32mp1-157C_EV1 platform. In such case was GCC
to emit the following warning trace:

core/arch/arm/plat-stm32mp1/plat_tzc400.c: In function ‘init_stm32mp1_tzc’:
core/arch/arm/plat-stm32mp1/plat_tzc400.c:107:61: warning: conversion from ‘uint64_t’ {aka ‘long long unsigned int’} to ‘vaddr_t’ {aka ‘long unsigned int’} changes value from ‘4294967296’ to ‘0’ [-Woverflow]
  107 |                 if (!tzc_region_is_non_secure(region_index, tzdram_end,
      |                                                             ^~~~~~~~~~

Fixes: 59c253f92c6c ("plat-stm32mp1: check TZC400 configuration")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-stm32mp1/plat_tzc400.c
@@ -40,11 +40,12 @@ static struct itr_handler tzc_itr_handler = {
 };
 DECLARE_KEEP_PAGER(tzc_itr_handler);
 
-static bool tzc_region_is_non_secure(unsigned int i, vaddr_t base, size_t size)
+static bool tzc_region_is_non_secure(unsigned int i, uint64_t pa, size_t size)
 {
 	struct tzc_region_config region_cfg = { };
 	uint32_t ns_cpu_mask = TZC_REGION_ACCESS_RDWR(STM32MP1_TZC_A7_ID);
 	uint32_t filters_mask = TZC_FILTERS_MASK;
+	vaddr_t base = pa;
 
 	if (tzc_get_region_config(i, &region_cfg))
 		panic();