OP-TEE/optee_os

commit fb5592f9cfebbc6aa0eec6b3cf8a04b14da1a896

core: drivers: add HiSilicon TRNG implementation
Based on HiSilicon hardware, a matching TRNG module is added.
The driver is enabled for the D06 platform (PLATFORM=d06).

Signed-off-by: loubaihui <loubaihui1@huawei.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Xiaoxu Zeng <zengxiaoxu@huawei.com>
[jf: amend commit description]
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

core/drivers/hisi_trng.c
@@ -1,0 +1,94 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2023 HiSilicon Limited. */
+
+#include <initcall.h>
+#include <io.h>
+#include <kernel/spinlock.h>
+#include <malloc.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+#include <rng_support.h>
+
+#define HTRNG_RANDATA_REG 0xF0
+#define HTRNG_BYTES 4
+
+#define POLL_PERIOD 10
+#define POLL_TIMEOUT 1000
+
+struct hisi_trng {
+	vaddr_t base;
+};
+
+static unsigned int trng_lock = SPINLOCK_UNLOCK;
+static struct hisi_trng *trng_dev;
+
+static TEE_Result trng_read(uint32_t *val)
+{
+	TEE_Result ret = TEE_SUCCESS;
+	uint32_t exceptions = 0;
+
+	exceptions = cpu_spin_lock_xsave(&trng_lock);
+	if (IO_READ32_POLL_TIMEOUT(trng_dev->base + HTRNG_RANDATA_REG,
+				   val, val, POLL_PERIOD, POLL_TIMEOUT)) {
+		EMSG("Hardware busy.");
+		ret = TEE_ERROR_BUSY;
+	}
+	cpu_spin_unlock_xrestore(&trng_lock, exceptions);
+
+	return ret;
+}
+
+TEE_Result hw_get_random_bytes(void *buf, size_t len)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+	size_t current_len = 0;
+	uint32_t val = 0;
+
+	if (!trng_dev) {
+		EMSG("No valid TRNG device.");
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	if (!buf || !len) {
+		EMSG("Invalid input parameter.");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	while (current_len < len) {
+		ret = trng_read(&val);
+		if (ret)
+			return TEE_ERROR_BUSY;
+
+		size = MIN(HTRNG_BYTES, len - current_len);
+		memcpy((uint8_t *)buf + current_len, &val, size);
+		current_len += size;
+	}
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result trng_init(void)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+
+	IMSG("TRNG driver init start.");
+	trng_dev = calloc(1, sizeof(struct hisi_trng));
+	if (!trng_dev) {
+		EMSG("Fail to calloc trng device.");
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+	trng->base = (vaddr_t)phys_to_virt_io(HISI_TRNG_BASE, HISI_TRNG_SIZE);
+	if (!trng_dev->base) {
+		EMSG("Fail to get trng io_base.");
+		free(trng_dev);
+		trng_dev = NULL;
+		return TEE_ERROR_ACCESS_DENIED;
+	}
+
+	DMSG("TRNG driver init done.");
+
+	return TEE_SUCCESS;
+}
+
+early_init(trng_init);

core/arch/arm/plat-d06/conf.mk
@@ -5,7 +5,7 @@ CFG_NUM_THREADS ?= 96
 CFG_CRYPTO_WITH_CE ?= y
 
 CFG_WITH_PAGER ?= n
-CFG_WITH_SOFTWARE_PRNG ?= y
+CFG_WITH_SOFTWARE_PRNG ?= n
 CFG_WITH_STATS ?= y
 CFG_TEE_CORE_EMBED_INTERNAL_TESTS ?= y
 
@@ -26,3 +26,5 @@ CFG_TZDRAM_START ?=    0x20C0000000
 CFG_TZDRAM_SIZE ?=     0x32000000
 CFG_SHMEM_START ?=     0x50000000
 CFG_SHMEM_SIZE ?=      0x04000000
+
+CFG_HISILICON_CRYPTO_DRIVER ?= y

core/arch/arm/plat-d06/platform_config.h
@@ -17,4 +17,8 @@
 #define CONSOLE_BAUDRATE	115200
 #define CONSOLE_UART_CLK_IN_HZ	200
 
+/* HISI_TRNG */
+#define HISI_TRNG_BASE		0x2010C0000
+#define HISI_TRNG_SIZE		0x100
+
 #endif /* PLATFORM_CONFIG_H */

core/drivers/sub.mk
@@ -77,6 +77,7 @@ srcs-$(CFG_VERSAL_PUF) += versal_puf.c
 srcs-$(CFG_VERSAL_HUK) += versal_huk.c
 srcs-$(CFG_CBMEM_CONSOLE) += cbmem_console.c
 srcs-$(CFG_RISCV_PLIC) += plic.c
+srcs-$(CFG_HISILICON_CRYPTO_DRIVER) += hisi_trng.c
 subdirs-y += crypto
 subdirs-$(CFG_BNXT_FW) += bnxt
 subdirs-$(CFG_DRIVERS_CLK) += clk

commit d5928515bc26dc095c2d627dd27d68c6dec3f126

core: drivers: fix random number reading errors in hisi_trng
Fixes arguments passed to IO_READ32_POLL_TIMEOUT() macro
and missing local variable definition in hisi_trng driver.

Fixes: fb5592f9cfeb ("core: drivers: add HiSilicon TRNG implementation")
Signed-off-by: loubaihui <loubaihui1@huawei.com>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/hisi_trng.c
@@ -8,9 +8,10 @@
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 #include <rng_support.h>
+#include <string.h>
 
 #define HTRNG_RANDATA_REG 0xF0
-#define HTRNG_BYTES 4
+#define HTRNG_BYTES 4U
 
 #define POLL_PERIOD 10
 #define POLL_TIMEOUT 1000
@@ -29,7 +30,7 @@ static TEE_Result trng_read(uint32_t *val)
 
 	exceptions = cpu_spin_lock_xsave(&trng_lock);
 	if (IO_READ32_POLL_TIMEOUT(trng_dev->base + HTRNG_RANDATA_REG,
-				   val, val, POLL_PERIOD, POLL_TIMEOUT)) {
+				   *val, *val, POLL_PERIOD, POLL_TIMEOUT)) {
 		EMSG("Hardware busy");
 		ret = TEE_ERROR_BUSY;
 	}
@@ -43,6 +44,7 @@ TEE_Result hw_get_random_bytes(void *buf, size_t len)
 	TEE_Result ret = TEE_ERROR_GENERIC;
 	size_t current_len = 0;
 	uint32_t val = 0;
+	size_t size = 0;
 
 	if (!trng_dev) {
 		EMSG("No valid TRNG device");
@@ -69,8 +71,6 @@ TEE_Result hw_get_random_bytes(void *buf, size_t len)
 
 static TEE_Result trng_init(void)
 {
-	TEE_Result ret = TEE_ERROR_GENERIC;
-
 	DMSG("TRNG driver init start");
 	trng_dev = calloc(1, sizeof(struct hisi_trng));
 	if (!trng_dev) {
@@ -78,7 +78,8 @@ static TEE_Result trng_init(void)
 		return TEE_ERROR_OUT_OF_MEMORY;
 	}
 
-	trng->base = (vaddr_t)phys_to_virt_io(HISI_TRNG_BASE, HISI_TRNG_SIZE);
+	trng_dev->base = (vaddr_t)phys_to_virt_io(HISI_TRNG_BASE,
+						  HISI_TRNG_SIZE);
 	if (!trng_dev->base) {
 		EMSG("Fail to get trng io_base");
 		free(trng_dev);