OP-TEE/optee_os

commit 2305544b3b9bce764141c863985a7ab44c98657c

drivers: clk: add generic clock framework
In order to ease clock support in OP-TEE, add a generic clock framework
which allows to add clocks driver and handle clock hierarchy.
This clock framework provides various functions to enable/disable clock
and to get their rate. Some basic behavior are supported such as gating
when parent or rate is set. This option is enabled using
CFG_DRIVERS_CLK which is disabled by default.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Clément Léger <clement.leger@bootlin.com>

core/drivers/clk/clk.c
@@ -1,0 +1,298 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2021, Bootlin
+ */
+
+#include <drivers/clk.h>
+#include <kernel/boot.h>
+#include <kernel/panic.h>
+#include <kernel/spinlock.h>
+#include <libfdt.h>
+#include <malloc.h>
+#include <stddef.h>
+
+/* Global clock tree lock */
+static unsigned int clk_lock = SPINLOCK_UNLOCK;
+
+struct clk *clk_alloc(const char *name, const struct clk_ops *ops,
+		      struct clk **parent_clks, size_t parent_count)
+{
+	struct clk *clk = NULL;
+	size_t parent = 0;
+
+	clk = calloc(1, sizeof(*clk) + parent_count * sizeof(clk));
+	if (!clk)
+		return NULL;
+
+	clk->num_parents = parent_count;
+	for (parent = 0; parent < parent_count; parent++)
+		clk->parents[parent] = parent_clks[parent];
+
+	clk->name = name;
+	clk->ops = ops;
+	refcount_set(&clk->enabled_count, 0);
+
+	return clk;
+}
+
+void clk_free(struct clk *clk)
+{
+	free(clk);
+}
+
+static bool __maybe_unused clk_check(struct clk *clk)
+{
+	if (!clk->ops)
+		return false;
+
+	if (clk->ops->set_parent && !clk->ops->get_parent)
+		return false;
+
+	if (clk->num_parents > 1 && !clk->ops->get_parent)
+		return false;
+
+	return true;
+}
+
+static void clk_compute_rate_no_lock(struct clk *clk)
+{
+	unsigned long parent_rate = 0;
+
+	if (clk->parent)
+		parent_rate = clk->parent->rate;
+
+	if (clk->ops->get_rate)
+		clk->rate = clk->ops->get_rate(clk, parent_rate);
+	else
+		clk->rate = parent_rate;
+}
+
+struct clk *clk_get_parent_by_index(struct clk *clk, size_t pidx)
+{
+	if (pidx >= clk->num_parents)
+		return NULL;
+
+	return clk->parents[pidx];
+}
+
+static void clk_init_parent(struct clk *clk)
+{
+	size_t pidx = 0;
+
+	switch (clk->num_parents) {
+	case 0:
+		break;
+	case 1:
+		clk->parent = clk->parents[0];
+		break;
+	default:
+		pidx = clk->ops->get_parent(clk);
+		assert(pidx < clk->num_parents);
+
+		clk->parent = clk->parents[pidx];
+		break;
+	}
+}
+
+TEE_Result clk_register(struct clk *clk)
+{
+	assert(clk_check(clk));
+
+	clk_init_parent(clk);
+	clk_compute_rate_no_lock(clk);
+
+	DMSG("Registered clock %s, freq %lu", clk->name, clk_get_rate(clk));
+
+	return TEE_SUCCESS;
+}
+
+static bool clk_is_enabled_no_lock(struct clk *clk)
+{
+	return refcount_val(&clk->enabled_count) != 0;
+}
+
+static void clk_disable_no_lock(struct clk *clk)
+{
+	struct clk *parent = NULL;
+
+	if (!refcount_dec(&clk->enabled_count))
+		return;
+
+	if (clk->ops->disable)
+		clk->ops->disable(clk);
+
+	parent = clk_get_parent(clk);
+	if (parent)
+		clk_disable_no_lock(parent);
+}
+
+static TEE_Result clk_enable_no_lock(struct clk *clk)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct clk *parent = NULL;
+
+	if (refcount_inc(&clk->enabled_count))
+		return TEE_SUCCESS;
+
+	parent = clk_get_parent(clk);
+	if (parent) {
+		res = clk_enable_no_lock(parent);
+		if (res)
+			return res;
+	}
+
+	if (clk->ops->enable) {
+		res = clk->ops->enable(clk);
+		if (res) {
+			if (parent)
+				clk_disable_no_lock(parent);
+
+			return res;
+		}
+	}
+
+	refcount_set(&clk->enabled_count, 1);
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result clk_enable(struct clk *clk)
+{
+	uint32_t exceptions = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	exceptions = cpu_spin_lock_xsave(&clk_lock);
+	res = clk_enable_no_lock(clk);
+	cpu_spin_unlock_xrestore(&clk_lock, exceptions);
+
+	return res;
+}
+
+void clk_disable(struct clk *clk)
+{
+	uint32_t exceptions = 0;
+
+	exceptions = cpu_spin_lock_xsave(&clk_lock);
+	clk_disable_no_lock(clk);
+	cpu_spin_unlock_xrestore(&clk_lock, exceptions);
+}
+
+unsigned long clk_get_rate(struct clk *clk)
+{
+	return clk->rate;
+}
+
+static TEE_Result clk_set_rate_no_lock(struct clk *clk, unsigned long rate)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	unsigned long parent_rate = 0;
+
+	if (clk->parent)
+		parent_rate = clk_get_rate(clk->parent);
+
+	res = clk->ops->set_rate(clk, rate, parent_rate);
+	if (res)
+		return res;
+
+	clk_compute_rate_no_lock(clk);
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result clk_set_rate(struct clk *clk, unsigned long rate)
+{
+	uint32_t exceptions = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	if (!clk->ops->set_rate)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	exceptions =  cpu_spin_lock_xsave(&clk_lock);
+
+	if (clk->flags & CLK_SET_RATE_GATE && clk_is_enabled_no_lock(clk))
+		res = TEE_ERROR_BAD_STATE;
+	else
+		res = clk_set_rate_no_lock(clk, rate);
+
+	cpu_spin_unlock_xrestore(&clk_lock, exceptions);
+
+	return res;
+}
+
+struct clk *clk_get_parent(struct clk *clk)
+{
+	return clk->parent;
+}
+
+static TEE_Result clk_get_parent_idx(struct clk *clk, struct clk *parent,
+				     size_t *pidx)
+{
+	size_t i = 0;
+
+	for (i = 0; i < clk_get_num_parents(clk); i++) {
+		if (clk_get_parent_by_index(clk, i) == parent) {
+			*pidx = i;
+			return TEE_SUCCESS;
+		}
+	}
+	EMSG("Clock %s is not a parent of clock %s", parent->name, clk->name);
+
+	return TEE_ERROR_BAD_PARAMETERS;
+}
+
+static TEE_Result clk_set_parent_no_lock(struct clk *clk, struct clk *parent,
+					 size_t pidx)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	bool was_enabled = false;
+
+	/* Requested parent is already the one set */
+	if (clk->parent == parent)
+		return TEE_SUCCESS;
+
+	was_enabled = clk_is_enabled_no_lock(clk);
+	/* Call is needed to decrement refcount on current parent tree */
+	if (was_enabled)
+		clk_disable_no_lock(clk);
+
+	res = clk->ops->set_parent(clk, pidx);
+	if (res)
+		goto out;
+
+	clk->parent = parent;
+
+	/* The parent changed and the rate might also have changed */
+	clk_compute_rate_no_lock(clk);
+
+out:
+	/* Call is needed to increment refcount on the new parent tree */
+	if (was_enabled) {
+		res = clk_enable_no_lock(clk);
+		if (res)
+			panic("Failed to re-enable clock after setting parent");
+	}
+
+	return res;
+}
+
+TEE_Result clk_set_parent(struct clk *clk, struct clk *parent)
+{
+	size_t pidx = 0;
+	uint32_t exceptions = 0;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	if (clk_get_parent_idx(clk, parent, &pidx) || !clk->ops->set_parent)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	exceptions = cpu_spin_lock_xsave(&clk_lock);
+	if (clk->flags & CLK_SET_PARENT_GATE && clk_is_enabled_no_lock(clk)) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
+
+	res = clk_set_parent_no_lock(clk, parent, pidx);
+out:
+	cpu_spin_unlock_xrestore(&clk_lock, exceptions);
+
+	return res;
+}

core/drivers/clk/sub.mk
@@ -1,0 +1,1 @@
+srcs-y += clk.c

core/drivers/sub.mk
@@ -40,5 +40,6 @@ srcs-$(CFG_IMX_OCOTP) += imx_ocotp.c
 
 subdirs-y += crypto
 subdirs-$(CFG_BNXT_FW) += bnxt
+subdirs-$(CFG_DRIVERS_CLK) += clk
 subdirs-$(CFG_SCMI_MSG_DRIVERS) += scmi-msg
 subdirs-y += imx

core/include/drivers/clk.h
@@ -1,0 +1,171 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, Bootlin
+ */
+
+#ifndef __DRIVERS_CLK_H
+#define __DRIVERS_CLK_H
+
+#include <kernel/refcount.h>
+#include <stdint.h>
+#include <tee_api_types.h>
+
+/* Flags for clock */
+#define CLK_SET_RATE_GATE	BIT(0) /* must be gated across rate change */
+#define CLK_SET_PARENT_GATE	BIT(1) /* must be gated across re-parent */
+
+/**
+ * struct clk - Clock structure
+ *
+ * @name: Clock name
+ * @priv: Private data for the clock provider
+ * @ops: Clock operations
+ * @parent: Current parent
+ * @rate: Current clock rate (cached after init or rate change)
+ * @flags: Specific clock flags
+ * @enabled_count: Enable/disable reference counter
+ * @num_parents: Number of parents
+ * @parents: Array of possible parents of the clock
+ */
+struct clk {
+	const char *name;
+	void *priv;
+	const struct clk_ops *ops;
+	struct clk *parent;
+	unsigned long rate;
+	unsigned int flags;
+	struct refcount enabled_count;
+	size_t num_parents;
+	struct clk *parents[];
+};
+
+/**
+ * struct clk_ops
+ *
+ * @enable: Enable the clock
+ * @disable: Disable the clock
+ * @set_parent: Set the clock parent based on index
+ * @get_parent: Get the current parent index of the clock
+ * @set_rate: Set the clock rate
+ * @get_rate: Get the clock rate
+ */
+struct clk_ops {
+	TEE_Result (*enable)(struct clk *clk);
+	void (*disable)(struct clk *clk);
+	TEE_Result (*set_parent)(struct clk *clk, size_t index);
+	size_t (*get_parent)(struct clk *clk);
+	TEE_Result (*set_rate)(struct clk *clk, unsigned long rate,
+			       unsigned long parent_rate);
+	unsigned long (*get_rate)(struct clk *clk,
+				  unsigned long parent_rate);
+};
+
+/**
+ * Return the clock name
+ *
+ * @clk: Clock for which the name is needed
+ * Return a const char * pointing to the clock name
+ */
+static inline const char *clk_get_name(struct clk *clk)
+{
+	return clk->name;
+}
+
+/**
+ * clk_alloc - Allocate a clock structure
+ *
+ * @name: Clock name
+ * @ops: Clock operations
+ * @parent_clks: Parents of the clock
+ * @parent_count: Number of parents of the clock
+ *
+ * Return a clock struct properly initialized or NULL if allocation failed
+ */
+struct clk *clk_alloc(const char *name, const struct clk_ops *ops,
+		      struct clk **parent_clks, size_t parent_count);
+
+/**
+ * clk_free - Free a clock structure
+ *
+ * @clk: Clock to be freed or NULL
+ */
+void clk_free(struct clk *clk);
+
+/**
+ * clk_register - Register a clock within the clock framework
+ *
+ * @clk: Clock struct to be registered
+ * Return a TEE_Result compliant value
+ */
+TEE_Result clk_register(struct clk *clk);
+
+/**
+ * clk_get_rate - Get clock rate
+ *
+ * @clk: Clock for which the rate is needed
+ * Return the clock rate in Hz
+ */
+unsigned long clk_get_rate(struct clk *clk);
+
+/**
+ * clk_set_rate - Set a clock rate
+ *
+ * @clk: Clock to be set with the rate
+ * @rate: Rate to set in Hz
+ * Return a TEE_Result compliant value
+ */
+TEE_Result clk_set_rate(struct clk *clk, unsigned long rate);
+
+/**
+ * clk_enable - Enable a clock and its ascendance
+ *
+ * @clk: Clock to be enabled
+ * Return a TEE_Result compliant value
+ */
+TEE_Result clk_enable(struct clk *clk);
+
+/**
+ * clk_disable - Disable a clock
+ *
+ * @clk: Clock to be disabled
+ */
+void clk_disable(struct clk *clk);
+
+/**
+ * clk_get_parent - Get the current clock parent
+ *
+ * @clk: Clock for which the parent is needed
+ * Return the clock parent or NULL if there is no parent
+ */
+struct clk *clk_get_parent(struct clk *clk);
+
+/**
+ * clk_get_num_parents - Get the number of parents for a clock
+ *
+ * @clk: Clock for which the number of parents is needed
+ * Return the number of parents
+ */
+static inline size_t clk_get_num_parents(struct clk *clk)
+{
+	return clk->num_parents;
+}
+
+/**
+ * Get a clock parent by its index
+ *
+ * @clk: Clock for which the parent is needed
+ * @pidx: Parent index for the clock
+ * Return the clock parent at index @pidx or NULL if out of bound
+ */
+struct clk *clk_get_parent_by_index(struct clk *clk, size_t pidx);
+
+/**
+ * clk_set_parent - Set the current clock parent
+ *
+ * @clk: Clock for which the parent should be set
+ * @parent: Parent clock to set
+ * Return a TEE_Result compliant value
+ */
+TEE_Result clk_set_parent(struct clk *clk, struct clk *parent);
+
+#endif /* __DRIVERS_CLK_H */

core/include/kernel/dt.h
@@ -62,6 +62,7 @@ struct dt_device_match {
 enum dt_driver_type {
 	DT_DRIVER_NOTYPE,
 	DT_DRIVER_UART,
+	DT_DRIVER_CLK,
 };
 
 struct dt_driver {

mk/config.mk
@@ -713,3 +713,8 @@ ifeq ($(CFG_WITH_PAGER),y)
 CFG_PREALLOC_RPC_CACHE ?= n
 endif
 CFG_PREALLOC_RPC_CACHE ?= y
+
+# When enabled, CFG_DRIVERS_CLK embeds a clock framework in OP-TEE core.
+# This clock framework allows to describe clock tree and provides functions to
+# get and configure the clocks.
+CFG_DRIVERS_CLK ?= n

commit f09476f924d4328dcd8fdb4e18ba1da9b6b1a24e

drivers: clk: assert registered clock pointer is not NULL
Fix clk_register() assertion entry instruction to print an assertion
error message on NULL clock pointer instead of issuing a segmentation
fault.

Fixes: 2305544b3b9b ("drivers: clk: add generic clock framework")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/clk/clk.c
@@ -49,7 +49,7 @@ void clk_free(struct clk *clk)
 
 static bool __maybe_unused clk_check(struct clk *clk)
 {
-	if (!clk->ops)
+	if (!clk || !clk->ops)
 		return false;
 
 	if (clk->ops->set_parent && !clk->ops->get_parent)