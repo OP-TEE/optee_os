OP-TEE/optee_os

commit 2b719df0fc1c2fc22ee5fa94bcd0dafd696f1384

core: interrupt: halt other cores when one is panicking
When one core panics, send an SGI (CFG_CORE_HALT_CORES_ON_PANIC_SGI)
to halt other cores if CFG_CORE_HALT_CORES_ON_PANIC is enabled.

Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/panic.c
@@ -4,10 +4,57 @@
  * Copyright (c) 2014, STMicroelectronics International N.V.
  */
 
+#include <initcall.h>
+#include <kernel/interrupt.h>
+#include <kernel/misc.h>
 #include <kernel/panic.h>
 #include <kernel/thread.h>
 #include <kernel/unwind.h>
 #include <trace.h>
+
+/* SGI number chosen to halt other cores must be in the secure SGI range */
+static_assert(!IS_ENABLED(CFG_HALT_CORES_ON_PANIC) ||
+	      (CFG_HALT_CORES_ON_PANIC_SGI >= 8 &&
+	       CFG_HALT_CORES_ON_PANIC_SGI < 16));
+
+static enum itr_return __noreturn
+multi_core_panic_it_handler(struct itr_handler *hdl __unused)
+{
+	IMSG("Halting CPU %zu", get_core_pos());
+
+	while (true)
+		cpu_idle();
+}
+
+static struct itr_handler multi_core_panic_handler = {
+	.it = CFG_HALT_CORES_ON_PANIC_SGI,
+	.handler = multi_core_panic_it_handler,
+};
+DECLARE_KEEP_PAGER(multi_core_panic_handler);
+
+static void notify_other_cores(void)
+{
+	interrupt_raise_sgi(interrupt_get_main_chip(),
+			    CFG_HALT_CORES_ON_PANIC_SGI,
+			    ITR_CPU_MASK_TO_OTHER_CPUS);
+}
+
+static TEE_Result init_multi_core_panic_handler(void)
+{
+	if (!IS_ENABLED(CFG_HALT_CORES_ON_PANIC) || CFG_TEE_CORE_NB_CORE == 1)
+		return TEE_SUCCESS;
+
+	if (interrupt_add_handler_with_chip(interrupt_get_main_chip(),
+					    &multi_core_panic_handler))
+		panic();
+
+	interrupt_enable(interrupt_get_main_chip(),
+			 multi_core_panic_handler.it);
+
+	return TEE_SUCCESS;
+}
+
+boot_final(init_multi_core_panic_handler);
 
 void __do_panic(const char *file __maybe_unused,
 		const int line __maybe_unused,
@@ -16,8 +63,6 @@ void __do_panic(const char *file __maybe_unused,
 {
 	/* disable prehemption */
 	(void)thread_mask_exceptions(THREAD_EXCP_ALL);
-
-	/* TODO: notify other cores */
 
 	/* trace: Panic ['panic-string-message' ]at FILE:LINE [<FUNCTION>]" */
 	if (!file && !func && !msg)
@@ -29,6 +74,10 @@ void __do_panic(const char *file __maybe_unused,
 			 func ? "<" : "", func ? func : "", func ? ">" : "");
 
 	print_kernel_stack();
+
+	if (IS_ENABLED(CFG_HALT_CORES_ON_PANIC) && CFG_TEE_CORE_NB_CORE > 1)
+		notify_other_cores();
+
 	/* abort current execution */
 	while (1)
 		cpu_idle();

mk/config.mk
@@ -1077,3 +1077,10 @@ CFG_HMAC_64_1024_RANGE ?= n
 # By default use standard pbkdf2 implementation
 CFG_CRYPTO_HW_PBKDF2 ?= n
 $(eval $(call cfg-depends-all,CFG_CRYPTO_HW_PBKDF2,CFG_CRYPTO_PBKDF2))
+
+# CFG_HALT_CORES_ON_PANIC, when enabled, makes any call to panic() halt the
+# other cores. The feature currently relies on GIC device to trap the other
+# cores using an SGI interrupt specified by CFG_HALT_CORES_ON_PANIC_SGI.
+CFG_HALT_CORES_ON_PANIC ?= n
+CFG_HALT_CORES_ON_PANIC_SGI ?= 15
+$(eval $(call cfg-depends-all,CFG_HALT_CORES_ON_PANIC,CFG_GIC))

commit 984ceb1c822349c5e7b19ba0c819ac9e7a58ede1

core: panic(): avoid panic in panic
When configured with CFG_HALT_CORES_ON_PANIC=y panic() sends a secure
SGI to all other CPUs to trap all of them, but if interrupts haven't
been configured yet it triggers a cascade of panics. So add a check to
see if interrupts are configured before trying to trap the other CPUs.

Fixes: 2b719df0fc1c ("core: interrupt: halt other cores when one is panicking")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>

core/kernel/panic.c
@@ -34,9 +34,13 @@ DECLARE_KEEP_PAGER(multi_core_panic_handler);
 
 static void notify_other_cores(void)
 {
-	interrupt_raise_sgi(interrupt_get_main_chip(),
-			    CFG_HALT_CORES_ON_PANIC_SGI,
-			    ITR_CPU_MASK_TO_OTHER_CPUS);
+	struct itr_chip *chip = interrupt_get_main_chip_may_fail();
+
+	if (chip)
+		interrupt_raise_sgi(chip, CFG_HALT_CORES_ON_PANIC_SGI,
+				    ITR_CPU_MASK_TO_OTHER_CPUS);
+	else
+		EMSG("Can't notify other cores, main interrupt chip not set");
 }
 
 static TEE_Result init_multi_core_panic_handler(void)