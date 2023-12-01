OP-TEE/optee_os

commit a22e85b257727087c0255d715c585487b603cacc

core: dt_driver: factorize clk_dt_get_from_provider()
Implement dt_driver_device_from_node_idx_prop() for
clk_dt_get_by_idx_prop() to get target reference instance.

Move/rename clk_dt_get_from_provider() to
dt_driver_device_from_provider_prop()

Acked-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Clément Léger <clement.leger@bootlin.com>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/kernel/dt_driver.c
@@ -126,3 +126,63 @@ struct dt_driver_provider *dt_driver_get_provider_by_phandle(uint32_t phandle)
 
 	return NULL;
 }
+
+static void *device_from_provider_prop(struct dt_driver_provider *prv,
+				       const uint32_t *prop)
+{
+	struct dt_driver_phandle_args *pargs = NULL;
+	unsigned int n = 0;
+	void *device = NULL;
+
+	pargs = calloc(1, prv->provider_cells * sizeof(uint32_t *) +
+		       sizeof(*pargs));
+	if (!pargs)
+		return NULL;
+
+	pargs->args_count = prv->provider_cells;
+	for (n = 0; n < prv->provider_cells; n++)
+		pargs->args[n] = fdt32_to_cpu(prop[n + 1]);
+
+	device = prv->get_of_device(pargs, prv->priv_data);
+
+	free(pargs);
+
+	return device;
+}
+
+void *dt_driver_device_from_node_idx_prop(const char *prop_name,
+					  const void *fdt, int nodeoffset,
+					  unsigned int prop_idx)
+{
+	int len = 0;
+	int idx = 0;
+	int idx32 = 0;
+	int prv_cells = 0;
+	uint32_t phandle = 0;
+	const uint32_t *prop = NULL;
+	struct dt_driver_provider *prv = NULL;
+
+	prop = fdt_getprop(fdt, nodeoffset, prop_name, &len);
+	if (!prop)
+		return NULL;
+
+	while (idx < len) {
+		idx32 = idx / sizeof(uint32_t);
+		phandle = fdt32_to_cpu(prop[idx32]);
+
+		prv = dt_driver_get_provider_by_phandle(phandle);
+		if (!prv)
+			return NULL;
+
+		prv_cells = dt_driver_provider_cells(prv);
+		if (prop_idx) {
+			prop_idx--;
+			idx += sizeof(phandle) + prv_cells * sizeof(uint32_t);
+			continue;
+		}
+
+		return device_from_provider_prop(prv, prop + idx32);
+	}
+
+	return NULL;
+}

core/drivers/clk/clk_dt.c
@@ -13,30 +13,6 @@
 #include <libfdt.h>
 #include <stddef.h>
 
-static struct clk *clk_dt_get_from_provider(struct dt_driver_provider *prv,
-					    unsigned int clock_cells,
-					    const uint32_t *prop)
-{
-	unsigned int arg = 0;
-	struct clk *clk = NULL;
-	struct dt_driver_phandle_args *pargs = NULL;
-
-	pargs = calloc(1, clock_cells * sizeof(uint32_t *) +
-		       sizeof(*pargs));
-	if (!pargs)
-		return NULL;
-
-	pargs->args_count = clock_cells;
-	for (arg = 0; arg < clock_cells; arg++)
-		pargs->args[arg] = fdt32_to_cpu(prop[arg + 1]);
-
-	clk = prv->get_of_device(pargs, prv->priv_data);
-
-	free(pargs);
-
-	return clk;
-}
-
 struct clk *clk_dt_get_by_name(const void *fdt, int nodeoffset,
 			       const char *name)
 {
@@ -53,37 +29,10 @@ static struct clk *clk_dt_get_by_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
 					  unsigned int clk_idx)
 {
-	int len = 0;
-	int idx = 0;
-	int idx32 = 0;
-	int clock_cells = 0;
-	uint32_t phandle = 0;
-	const uint32_t *prop = NULL;
-	struct dt_driver_provider *prv = NULL;
+	void *device = dt_driver_device_from_node_idx_prop(prop_name, fdt,
+							   nodeoffset, clk_idx);
 
-	prop = fdt_getprop(fdt, nodeoffset, prop_name, &len);
-	if (!prop)
-		return NULL;
-
-	while (idx < len) {
-		idx32 = idx / sizeof(uint32_t);
-		phandle = fdt32_to_cpu(prop[idx32]);
-
-		prv = dt_driver_get_provider_by_node(phandle);
-		if (!prv)
-			return NULL;
-
-		clock_cells = prv->provider_cells;
-		if (clk_idx) {
-			clk_idx--;
-			idx += sizeof(phandle) + clock_cells * sizeof(uint32_t);
-			continue;
-		}
-
-		return clk_dt_get_from_provider(prv, clock_cells, &prop[idx32]);
-	}
-
-	return NULL;
+	return (struct clk *)device;
 }
 
 struct clk *clk_dt_get_by_idx(const void *fdt, int nodeoffset,

core/include/kernel/dt_driver.h
@@ -80,6 +80,22 @@ TEE_Result dt_driver_register_provider(const void *fdt, int nodeoffset,
 				       void *data, enum dt_driver_type type);
 
 /*
+ * dt_driver_device_from_node_idx_prop - Return a device instance based on a
+ *	property name and FDT information
+ *
+ * @prop_name: DT property name, e.g. "clocks" for clock resources
+ * @fdt: FDT base address
+ * @nodeoffset: node offset in the FDT
+ * @prop_idx: index of the phandle data in the property
+ *
+ * Return a device opaque reference, e.g. a struct clk pointer for a clock
+ * driver, or NULL if not found.
+ */
+void *dt_driver_device_from_node_idx_prop(const char *prop_name,
+					  const void *fdt, int nodeoffset,
+					  unsigned int prop_idx);
+
+/*
  * Return driver provider reference from its node offset value in the FDT
  */
 struct dt_driver_provider *dt_driver_get_provider_by_node(int nodeoffset);

commit 5e13d9732575424a08920e7ccbe9e9294fe5d8a2

core: dt_driver: do not defer probe on skip phandler arguments
Prevent dt_driver_device_from_node_idx_prop() to request driver probe
deferral when needing to skip phandle arguments cells related to
providers not yet registered. When skipping these phandle argument cells
we don't really need the skipped provider is registered, we can read
the #xxx-cells property in the provider DT node straight.

For example, consider a driver which DT node defines:
  clocks = <&foo_clock 1 2 3>, <&bar_clock 2>;
  clock-names = "foo", "bar";
If driver calls clk_get_by_name(fdt, node, "bar"), it does not need to
wait &foo_clock related driver to be already probed, it does not even
need the driver to be ever probed.

Fixes: a22e85b25772 ("core: dt_driver: factorize clk_dt_get_from_provider()")
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/kernel/dt_driver.c
@@ -383,20 +383,44 @@ TEE_Result dt_driver_device_from_node_idx_prop(const char *prop_name,
 				return TEE_ERROR_GENERIC;
 
 			prv = dt_driver_get_provider_by_node(nodeoffset, type);
-			if (!prv)
-				return TEE_ERROR_DEFER_DRIVER_INIT;
 		} else {
 			prv = dt_driver_get_provider_by_phandle(phandle, type);
-			if (!prv)
-				return TEE_ERROR_DEFER_DRIVER_INIT;
 		}
 
-		prv_cells = dt_driver_provider_cells(prv);
+		if (prv) {
+			prv_cells = dt_driver_provider_cells(prv);
+		} else if (prop_idx) {
+			/*
+			 * When we need to skip another provider phandle
+			 * arguments cells (aka when prop_idx != 0), we don't
+			 * really need the skipped provider to be already
+			 * registered, we can look straight in its DT node.
+			 */
+			phandle_node = fdt_node_offset_by_phandle(fdt, phandle);
+			if (phandle_node < 0) {
+				DMSG("Can't find node for phandle %"PRIu32,
+				     phandle);
+				return TEE_ERROR_GENERIC;
+			}
+
+			prv_cells = fdt_get_dt_driver_cells(fdt, phandle_node,
+							    type);
+			if (prv_cells < 0) {
+				DMSG("Can't find cells count on node %s: %d",
+				     fdt_get_name(fdt, phandle_node, NULL),
+				     prv_cells);
+				return TEE_ERROR_GENERIC;
+			}
+		}
+
 		if (prop_idx) {
 			prop_idx--;
 			idx += sizeof(phandle) + prv_cells * sizeof(uint32_t);
 			continue;
 		}
+
+		if (!prv)
+			return TEE_ERROR_DEFER_DRIVER_INIT;
 
 		/* Skip property cell with the phandle, already handled */
 		idx32++;