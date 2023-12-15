OP-TEE/optee_os

commit 508e2476b232f3f055fd2e17d42c2884731e9436

core: update transfer list header and signature
Add tl->flags and TL_FLAGS_HAS_CHECKSUM to align to the latest FW
Handoff spec update.
Bypass checksum verifying and updating if TL_FLAGS_HAS_CHECKSUM bit
is not set in tl->flags.
Update TL signature to 4a0f_b10b to align to the latest FW Handoff
spec update.

Note: It causes Transfer List ABI breakage due to FW Handoff spec
stays with same rev number (v0.9) with above changes.

Signed-off-by: Raymond Mao <raymond.mao@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/transfer_list.h
@@ -6,7 +6,7 @@
 #ifndef __KERNEL_TRANSFER_LIST_H
 #define __KERNEL_TRANSFER_LIST_H
 
-#define TRANSFER_LIST_SIGNATURE		U(0x006ed0ff)
+#define TRANSFER_LIST_SIGNATURE		U(0x4a0fb10b)
 #define TRANSFER_LIST_VERSION		U(0x0001)
 
 /*
@@ -23,6 +23,8 @@
  * Set to 1 for both AArch64 and AArch32 according to fw handoff spec v0.9
  */
 #define REG_CONVENTION_VER_MASK BIT(24)
+
+#define TL_FLAGS_HAS_CHECKSUM BIT(0)
 
 /* Transfer list operation codes */
 #define TL_OPS_NONE	U(0) /* invalid for any operation */
@@ -54,6 +56,7 @@ struct transfer_list_header {
 	uint8_t alignment;	/* max alignment of transfer entry data */
 	uint32_t size;		/* TL header + all transfer entries */
 	uint32_t max_size;
+	uint32_t flags;
 	/*
 	 * Commented out element used to visualize dynamic part of the
 	 * data structure.

core/kernel/transfer_list.c
@@ -101,6 +101,7 @@ void transfer_list_dump(struct transfer_list_header *tl)
 	DMSG("alignment  %#"PRIx8, tl->alignment);
 	DMSG("size       %#"PRIx32, tl->size);
 	DMSG("max_size   %#"PRIx32, tl->max_size);
+	DMSG("flags      %#"PRIx32, tl->flags);
 	while (true) {
 		tl_e = transfer_list_next(tl, tl_e);
 		if (!tl_e)
@@ -145,6 +146,7 @@ struct transfer_list_header *transfer_list_init(paddr_t pa, size_t max_size)
 	tl->alignment = TRANSFER_LIST_INIT_MAX_ALIGN; /* initial max align */
 	tl->size = sizeof(*tl); /* initial size is the size of header */
 	tl->max_size = max_size;
+	tl->flags = TL_FLAGS_HAS_CHECKSUM;
 
 	transfer_list_update_checksum(tl);
 
@@ -298,9 +300,6 @@ static uint8_t calc_byte_sum(const struct transfer_list_header *tl)
 	uint8_t cs = 0;
 	size_t n = 0;
 
-	if (!tl)
-		return 0;
-
 	for (n = 0; n < tl->size; n++)
 		cs += b[n];
 
@@ -316,7 +315,7 @@ void transfer_list_update_checksum(struct transfer_list_header *tl)
 {
 	uint8_t cs = 0;
 
-	if (!tl)
+	if (!tl || !(tl->flags & TL_FLAGS_HAS_CHECKSUM))
 		return;
 
 	cs = calc_byte_sum(tl);
@@ -333,6 +332,12 @@ void transfer_list_update_checksum(struct transfer_list_header *tl)
  ******************************************************************************/
 bool transfer_list_verify_checksum(const struct transfer_list_header *tl)
 {
+	if (!tl)
+		return false;
+
+	if (!(tl->flags & TL_FLAGS_HAS_CHECKSUM))
+		return true;
+
 	return !calc_byte_sum(tl);
 }
 

commit 86d2f350f44cb79a5f0d4e224ed279f27c98f59c

core: fixup of transfer list header size
Add 4 reserved bytes at the tail of the transfer list header.
This fixes a non-8-bytes aligned header when "flags" was introduced
into the header.

Fixes: 508e2476b232 ("core: update transfer list header and signature")
Signed-off-by: Raymond Mao <raymond.mao@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/transfer_list.h
@@ -57,6 +57,7 @@ struct transfer_list_header {
 	uint32_t size;		/* TL header + all transfer entries */
 	uint32_t max_size;
 	uint32_t flags;
+	uint32_t reserved;	/* spare bytes */
 	/*
 	 * Commented out element used to visualize dynamic part of the
 	 * data structure.