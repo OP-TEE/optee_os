OP-TEE/optee_os

commit 462028ede02dfe60aac2de12b214187fb666fe23

qemu_armv8a: add GIC v3 redistributor base address
Adds and configures the GIC v3 redistributor base address.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-vexpress/platform_config.h
@@ -126,15 +126,17 @@
 
 #define GICD_OFFSET		0
 #define GICC_OFFSET		0x10000
+#ifdef CFG_ARM_GICV3
+#define GIC_REDIST_BASE		0x080A0000
+#define GIC_REDIST_SIZE		0x00F60000
+#endif
 
 #else
 #error "Unknown platform flavor"
 #endif
 
-#ifdef GIC_BASE
 #define GICD_BASE		(GIC_BASE + GICD_OFFSET)
 #define GICC_BASE		(GIC_BASE + GICC_OFFSET)
-#endif
 
 #ifndef UART_BAUDRATE
 #define UART_BAUDRATE		115200

core/arch/arm/plat-vexpress/main.c
@@ -44,12 +44,20 @@ register_ddr(DRAM1_BASE, DRAM1_SIZE);
 #endif
 
 #ifdef CFG_GIC
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICC_BASE, GIC_CPU_REG_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICD_BASE, GIC_DIST_REG_SIZE);
-register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICC_BASE, GIC_DIST_REG_SIZE);
+#ifdef GIC_REDIST_BASE
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, GIC_REDIST_BASE, GIC_REDIST_SIZE);
+#endif
 
 void boot_primary_init_intc(void)
 {
+#ifdef GIC_REDIST_BASE
+	gic_init_v3(GIC_BASE + GICC_OFFSET, GIC_BASE + GICD_OFFSET,
+		    GIC_REDIST_BASE);
+#else
 	gic_init(GIC_BASE + GICC_OFFSET, GIC_BASE + GICD_OFFSET);
+#endif
 }
 
 #if !defined(CFG_WITH_ARM_TRUSTED_FW)

commit 829db0d72e98b66059945d4687ea8a763ea3bed8

plat-vexpress: fvp: configure GIC redistributor base address
Configure GIC redistributor base address needed with GICv3.

Fixes: 462028ede02d ("qemu_armv8a: add GIC v3 redistributor base address")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-vexpress/platform_config.h
@@ -96,6 +96,10 @@
 
 #define GICC_OFFSET		0x0
 #define GICD_OFFSET		0x3000000
+#ifdef CFG_ARM_GICV3
+#define GIC_REDIST_BASE		0x2F100000
+#define GIC_REDIST_SIZE		0x00100000
+#endif
 
 #elif defined(PLATFORM_FLAVOR_juno)
 /*