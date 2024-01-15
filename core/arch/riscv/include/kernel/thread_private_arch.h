472c70be15692f8f9fe747691b26fc9943035c8a
Alvin Chang <alvinga@andestech.com>
2024-01-15 13:22:27 +0800

    core: riscv: Rename thread_return_to_ree() to thread_return_to_udomain()
    The terminology "udomain", which means untrusted domain, is more precise
    terminology for RISC-V architecture, where "domain" has already been
    defined in RISC-V CoVE and Smmtt specifications.
    
    Signed-off-by: Alvin Chang <alvinga@andestech.com>
    Reviewed-by: Marouene Boubakri <marouene.boubakri@nxp.com>

--- core/arch/riscv/include/kernel/thread_private_arch.h
+++ core/arch/riscv/include/kernel/thread_private_arch.h
@@ -59,9 +59,9 @@ void thread_init_tvec(void);
 void thread_trap_vect(void);
 void thread_trap_vect_end(void);
 
-void thread_return_to_ree(unsigned long arg0, unsigned long arg1,
-			  unsigned long arg2, unsigned long arg3,
-			  unsigned long arg4, unsigned long arg5);
+void thread_return_to_udomain(unsigned long arg0, unsigned long arg1,
+			      unsigned long arg2, unsigned long arg3,
+			      unsigned long arg4, unsigned long arg5);
 
 void __panic_at_abi_return(void);
 
