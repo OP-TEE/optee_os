e25a1b91428790de467610dd785f18c7caf3913b
Tony Han <tony.han@microchip.com>
2024-01-15 15:40:24 +0800

    drivers: clk: sam: update UTMI clock for sama7g5
    The frequency of the parent clock for UTMI is different with sama5d2.
    The control of UTMI clock is different with sama5d2.
    
    Signed-off-by: Tony Han <tony.han@microchip.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/drivers/clk/sam/at91_clk.h
+++ core/drivers/clk/sam/at91_clk.h
@@ -163,6 +163,10 @@ struct clk *
 at91_clk_register_utmi(struct pmc_data *pmc, const char *name,
 		       struct clk *parent);
 
+struct clk *at91_clk_sama7g5_register_utmi(struct pmc_data *pmc,
+					   const char *name,
+					   struct clk *parent);
+
 /* Master */
 struct clk *
 at91_clk_register_master_pres(struct pmc_data *pmc,
