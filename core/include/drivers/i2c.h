OP-TEE/optee_os

commit b357d34fe91f4e7f6e0eacea17a7fbe5f6c01e7e

core: dt_driver: swap TEE_result and retrieved device reference
Changes dt_driver callback function to return a TEE_Result value
and pass retrieved device reference by a output argument rather
than the opposite.

This change updates dt_driver.c, dt_driver.h and all drivers
implementing related dt_driver callback function.

As a consequence, this change removes all type definition related
to device specific callback handler function types which are useless
as all these now comply with type dt_driver_probe_func defined in
dt_driver.h.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/drivers/i2c.h
@@ -183,12 +183,16 @@ static inline TEE_Result i2c_smbus_raw(struct i2c_dev *i2c_dev,
  * Return a TEE_Result compliant code in case of error
  */
 static inline TEE_Result i2c_dt_get_dev(const void *fdt, int nodeoffset,
-					struct i2c_dev **i2c_dev)
+					struct i2c_dev **out_i2c_dev)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
+	void *i2c_dev = NULL;
 
-	*i2c_dev = dt_driver_device_from_parent(fdt, nodeoffset, DT_DRIVER_I2C,
-						&res);
+	res = dt_driver_device_from_parent(fdt, nodeoffset, DT_DRIVER_I2C,
+					   &i2c_dev);
+	if (!res)
+		*out_i2c_dev = i2c_dev;
+
 	return res;
 }
 #else
@@ -219,7 +223,6 @@ static inline TEE_Result i2c_dt_get_dev(const void *fdt __unused,
 					int nodeoffset __unused,
 					struct i2c_dev **i2c_dev)
 {
-	*i2c_dev = NULL;
 	return TEE_ERROR_NOT_SUPPORTED;
 }
 #endif
@@ -230,17 +233,10 @@ static inline TEE_Result i2c_dt_get_dev(const void *fdt __unused,
  *
  * @args: Pointer to devicetree description of the I2C bus device to parse
  * @data: Pointer to data given at i2c_dt_register_provider() call
- * @res: Output result code of the operation:
- *	TEE_SUCCESS in case of success
- *	TEE_ERROR_DEFER_DRIVER_INIT if I2C controller is not initialized
- *	Any TEE_Result compliant code in case of error.
- *
- * Returns a struct i2c pointer pointing to a I2C bus device matching
- * the devicetree description or NULL if invalid description in which case
- * @res provides the error code.
+ * @out_device: Output pointer to I2C device upon success
  */
-typedef struct i2c_dev *(*i2c_dt_get_func)(struct dt_pargs *args, void *data,
-					   TEE_Result *res);
+typedef TEE_Result (*i2c_dt_get_func)(struct dt_pargs *args, void *data,
+				      struct i2c_dev **out_device);
 
 /**
  * i2c_dt_register_provider - Register a I2C controller provider and add all the
@@ -252,9 +248,9 @@ typedef struct i2c_dev *(*i2c_dt_get_func)(struct dt_pargs *args, void *data,
  * @data: Data which will be passed to the get_dt_i2c callback
  * Returns TEE_Result value
  */
-static inline
-TEE_Result i2c_register_provider(const void *fdt, int nodeoffset,
-				 i2c_dt_get_func get_dt_i2c, void *data)
+static inline TEE_Result i2c_register_provider(const void *fdt, int nodeoffset,
+					       i2c_dt_get_func get_dt_i2c,
+					       void *data)
 {
 	int subnode = -1;
 	TEE_Result res = TEE_ERROR_GENERIC;

core/drivers/atmel_piobu.c
@@ -204,24 +204,26 @@ static const struct gpio_ops atmel_piobu_ops = {
 	.set_interrupt = secumod_gpio_set_interrupt,
 };
 
-static struct gpio *secumod_dt_get(struct dt_pargs *pargs, void *data,
-				   TEE_Result *res)
+static TEE_Result secumod_dt_get(struct dt_pargs *pargs, void *data,
+				 struct gpio **out_gpio)
 {
+	TEE_Result res = TEE_ERROR_GENERIC;
 	struct gpio *gpio = NULL;
 	struct gpio_chip *chip = data;
 
-	gpio = gpio_dt_alloc_pin(pargs, res);
-	if (*res)
-		return NULL;
+	res = gpio_dt_alloc_pin(pargs, &gpio);
+	if (res)
+		return res;
 
 	if (gpio_protected & BIT32(gpio->pin)) {
 		free(gpio);
-		return NULL;
+		return TEE_ERROR_GENERIC;
 	}
 
 	gpio->chip = chip;
+	*out_gpio = gpio;
 
-	return gpio;
+	return TEE_SUCCESS;
 }
 
 static enum itr_return secumod_it_handler(struct itr_handler *handler __unused)

core/drivers/clk/clk-stm32-core.c
@@ -544,23 +544,23 @@ struct clk *stm32mp_rcc_clock_id_to_clk(unsigned long clock_id)
 	return priv->clk_refs[clock_id];
 }
 
-static struct clk *stm32mp_clk_dt_get_clk(struct dt_pargs *pargs,
-					  void *data __unused, TEE_Result *res)
+static TEE_Result stm32mp_clk_dt_get_clk(struct dt_pargs *pargs,
+					 void *data __unused,
+					 struct clk **out_clk)
 {
 	unsigned long clock_id = pargs->args[0];
 	struct clk *clk = NULL;
 
-	*res = TEE_ERROR_BAD_PARAMETERS;
-
 	if (pargs->args_count != 1)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
 	clk = stm32mp_rcc_clock_id_to_clk(clock_id);
 	if (!clk)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	*res = TEE_SUCCESS;
-	return clk;
+	*out_clk = clk;
+
+	return TEE_SUCCESS;
 }
 
 static void clk_stm32_register_clocks(struct clk_stm32_priv *priv)

core/drivers/clk/clk-stm32mp15.c
@@ -1499,23 +1499,23 @@ static TEE_Result register_stm32mp1_clocks(void)
 	return TEE_SUCCESS;
 }
 
-static struct clk *stm32mp1_clk_dt_get_clk(struct dt_pargs *pargs,
-					   void *data __unused, TEE_Result *res)
+static TEE_Result stm32mp1_clk_dt_get_clk(struct dt_pargs *pargs,
+					  void *data __unused,
+					  struct clk **out_clk)
 {
 	unsigned long clock_id = pargs->args[0];
 	struct clk *clk = NULL;
 
-	*res = TEE_ERROR_BAD_PARAMETERS;
-
 	if (pargs->args_count != 1)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
 	clk = clock_id_to_clk(clock_id);
 	if (!clk)
-		return NULL;
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	*res = TEE_SUCCESS;
-	return clk;
+	*out_clk = clk;
+
+	return TEE_SUCCESS;
 }
 
 /* Non-null reference for compat data */

core/drivers/clk/clk_dt.c
@@ -32,10 +32,14 @@ static TEE_Result clk_dt_get_by_idx_prop(const char *prop_name, const void *fdt,
 					 struct clk **clk)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
+	void *out_clk = NULL;
 
-	*clk = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
-						   clk_idx, DT_DRIVER_CLK,
-						   &res);
+	res = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
+						  clk_idx, DT_DRIVER_CLK,
+						  &out_clk);
+	if (!res)
+		*clk = out_clk;
+
 	return res;
 }
 

core/drivers/clk/sam/at91_clk.h
@@ -130,7 +130,7 @@ struct pmc_data *pmc_data_allocate(unsigned int ncore, unsigned int nsystem,
 				   unsigned int nperiph, unsigned int ngck,
 				   unsigned int npck);
 
-struct clk *clk_dt_pmc_get(struct dt_pargs *args, void *data, TEE_Result *res);
+TEE_Result clk_dt_pmc_get(struct dt_pargs *args, void *data, struct clk **clk);
 
 struct clk *pmc_clk_get_by_name(struct pmc_clk *clks, unsigned int nclk,
 				const char *name);

core/drivers/clk/sam/at91_pmc.c
@@ -80,22 +80,17 @@ TEE_Result pmc_clk_get(struct pmc_data *pmc, unsigned int type,
 	return TEE_SUCCESS;
 }
 
-struct clk *clk_dt_pmc_get(struct dt_pargs *clkspec, void *data,
-			   TEE_Result *res)
+TEE_Result clk_dt_pmc_get(struct dt_pargs *clkspec, void *data,
+			  struct clk **out_clk)
 {
 	unsigned int type = clkspec->args[0];
 	unsigned int idx = clkspec->args[1];
 	struct pmc_data *pmc_data = data;
-	struct clk *clk = NULL;
 
-	if (clkspec->args_count != 2) {
-		*res = TEE_ERROR_BAD_PARAMETERS;
-		return NULL;
-	}
+	if (clkspec->args_count != 2)
+		return TEE_ERROR_BAD_PARAMETERS;
 
-	*res = pmc_clk_get(pmc_data, type, idx, &clk);
-
-	return clk;
+	return pmc_clk_get(pmc_data, type, idx, out_clk);
 }
 
 struct pmc_data *pmc_data_allocate(unsigned int ncore, unsigned int nsystem,

core/drivers/gpio/gpio.c
@@ -11,26 +11,23 @@
 #include <tee_api_types.h>
 #include <util.h>
 
-struct gpio *gpio_dt_alloc_pin(struct dt_pargs *pargs, TEE_Result *res)
+TEE_Result gpio_dt_alloc_pin(struct dt_pargs *pargs, struct gpio **out_gpio)
 {
 	struct gpio *gpio = NULL;
 
-	if (pargs->args_count != 2) {
-		*res = TEE_ERROR_BAD_PARAMETERS;
-		return NULL;
-	}
+	if (pargs->args_count != 2)
+		return TEE_ERROR_BAD_PARAMETERS;
 
 	gpio = calloc(1, sizeof(struct gpio));
-	if (!gpio) {
-		*res = TEE_ERROR_OUT_OF_MEMORY;
-		return NULL;
-	}
+	if (!gpio)
+		return TEE_ERROR_OUT_OF_MEMORY;
 
 	gpio->pin = pargs->args[0];
 	gpio->dt_flags = pargs->args[1];
 
-	*res = TEE_SUCCESS;
-	return gpio;
+	*out_gpio = gpio;
+
+	return TEE_SUCCESS;
 }
 
 static char *gpio_get_dt_prop_name(const char *gpio_name)
@@ -53,15 +50,18 @@ TEE_Result gpio_dt_get_by_index(const void *fdt, int nodeoffset,
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
 	char *prop_name = NULL;
+	void *out_gpio = NULL;
 
 	prop_name = gpio_get_dt_prop_name(gpio_name);
 	if (!prop_name)
 		return TEE_ERROR_OUT_OF_MEMORY;
 
-	*gpio = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
-						    index, DT_DRIVER_GPIO,
-						    &res);
+	res = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
+						  index, DT_DRIVER_GPIO,
+						  &out_gpio);
 	free(prop_name);
+	if (!res)
+		*gpio = out_gpio;
 
 	return res;
 }

commit 12e59f1d5294827224aa402b156cc9bf9659ec1d

drivers: i2c: add missing __unused in stubbed function
Fix statc inline stub implementation of i2c_dt_get_dev() that
lacks a __unused attribute on an unused argument.

Fixes: b357d34fe91f ("core: dt_driver: swap TEE_result and retrieved device reference")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/drivers/i2c.h
@@ -221,7 +221,7 @@ static inline TEE_Result i2c_smbus_raw(struct i2c_dev *i2c_dev __unused,
 
 static inline TEE_Result i2c_dt_get_dev(const void *fdt __unused,
 					int nodeoffset __unused,
-					struct i2c_dev **i2c_dev)
+					struct i2c_dev **i2c_dev __unused)
 {
 	return TEE_ERROR_NOT_SUPPORTED;
 }