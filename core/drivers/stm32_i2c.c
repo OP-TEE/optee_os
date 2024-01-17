OP-TEE/optee_os

commit 73ba32eb0f6cbb6ebbc59f13ea7eee44b387fe48

drivers: stm32_i2c: support CFG_DRIVERS_PINCTRL
Updates stm32_i2c driver for when CFG_DRIVERS_PINCTRL is enabled making
I2C driver to get pin control configuration using the generic pin
control framework. When enabled, stm32_i2c driver get the active and
sleep pin control configuration from the device tree. Sleep pinctrl
configuration is optional.

SE050 and STM32MP1 PMIC drivers that use the stm32_i2c bus are both
updated accordingly.

Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/stm32_i2c.c
@@ -12,6 +12,8 @@
 #include <arm.h>
 #include <drivers/clk.h>
 #include <drivers/clk_dt.h>
+#include <drivers/pinctrl.h>
+#include <drivers/stm32_gpio.h>
 #include <drivers/stm32_i2c.h>
 #include <io.h>
 #include <kernel/delay.h>
@@ -684,13 +686,19 @@ static int i2c_config_analog_filter(struct i2c_handle_s *hi2c,
 
 TEE_Result stm32_i2c_get_setup_from_fdt(void *fdt, int node,
 					struct stm32_i2c_init_s *init,
+#ifdef CFG_DRIVERS_PINCTRL
+					struct pinctrl_state **pinctrl,
+					struct pinctrl_state **pinctrl_sleep
+#else
 					struct stm32_pinctrl **pinctrl,
-					size_t *pinctrl_count)
+					size_t *pinctrl_count
+#endif
+					)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
 	const fdt32_t *cuint = NULL;
 	struct dt_node_info info = { .status = 0 };
-	int count = 0;
+	int __maybe_unused count = 0;
 
 	/* Default STM32 specific configs caller may need to overwrite */
 	memset(init, 0, sizeof(*init));
@@ -732,6 +740,22 @@ TEE_Result stm32_i2c_get_setup_from_fdt(void *fdt, int node,
 		init->bus_rate = I2C_STANDARD_RATE;
 	}
 
+#ifdef CFG_DRIVERS_PINCTRL
+	if (pinctrl) {
+		res = pinctrl_get_state_by_name(fdt, node, "default", pinctrl);
+		if (res)
+			return res;
+	}
+
+	if (pinctrl_sleep) {
+		res = pinctrl_get_state_by_name(fdt, node, "sleep",
+						pinctrl_sleep);
+		if (res == TEE_ERROR_ITEM_NOT_FOUND)
+			res = TEE_SUCCESS;
+		if (res)
+			return res;
+	}
+#else
 	count = stm32_pinctrl_fdt_get_pinctrl(fdt, node, NULL, 0);
 	if (count <= 0) {
 		*pinctrl = NULL;
@@ -752,6 +776,7 @@ TEE_Result stm32_i2c_get_setup_from_fdt(void *fdt, int node,
 	*pinctrl_count = stm32_pinctrl_fdt_get_pinctrl(fdt, node,
 						       *pinctrl, count);
 	assert(*pinctrl_count == (unsigned int)count);
+#endif /*CFG_DRIVERS_PINCTRL*/
 
 	return TEE_SUCCESS;
 }
@@ -834,6 +859,10 @@ int stm32_i2c_init(struct i2c_handle_s *hi2c,
 	if (rc)
 		DMSG("I2C analog filter error %d", rc);
 
+#ifdef CFG_DRIVERS_PINCTRL
+	if (IS_ENABLED(CFG_STM32MP13))
+		stm32_pinctrl_set_secure_cfg(hi2c->pinctrl, true);
+#else
 	if (IS_ENABLED(CFG_STM32MP13)) {
 		size_t n = 0;
 
@@ -841,6 +870,7 @@ int stm32_i2c_init(struct i2c_handle_s *hi2c,
 			stm32_gpio_set_secure_cfg(hi2c->pinctrl[n].bank,
 						  hi2c->pinctrl[n].pin, true);
 	}
+#endif
 
 	clk_disable(hi2c->clock);
 
@@ -1531,7 +1561,12 @@ void stm32_i2c_resume(struct i2c_handle_s *hi2c)
 	    (hi2c->i2c_state != I2C_STATE_SUSPENDED))
 		panic();
 
+#ifdef CFG_DRIVERS_PINCTRL
+	if (pinctrl_apply_state(hi2c->pinctrl))
+		panic();
+#else
 	stm32_pinctrl_load_active_cfg(hi2c->pinctrl, hi2c->pinctrl_count);
+#endif
 
 	if (hi2c->i2c_state == I2C_STATE_RESET) {
 		/* There is no valid I2C configuration to be loaded yet */
@@ -1540,6 +1575,10 @@ void stm32_i2c_resume(struct i2c_handle_s *hi2c)
 
 	restore_cfg(hi2c, &hi2c->sec_cfg);
 
+#ifdef CFG_DRIVERS_PINCTRL
+	if (IS_ENABLED(CFG_STM32MP13))
+		stm32_pinctrl_set_secure_cfg(hi2c->pinctrl, true);
+#else
 	if (IS_ENABLED(CFG_STM32MP13)) {
 		size_t n = 0;
 
@@ -1547,6 +1586,7 @@ void stm32_i2c_resume(struct i2c_handle_s *hi2c)
 			stm32_gpio_set_secure_cfg(hi2c->pinctrl[n].bank,
 						  hi2c->pinctrl[n].pin, true);
 	}
+#endif
 
 	hi2c->i2c_state = I2C_STATE_READY;
 }
@@ -1560,7 +1600,13 @@ void stm32_i2c_suspend(struct i2c_handle_s *hi2c)
 		panic();
 
 	save_cfg(hi2c, &hi2c->sec_cfg);
+
+#ifdef CFG_DRIVERS_PINCTRL
+	if (hi2c->pinctrl_sleep && pinctrl_apply_state(hi2c->pinctrl_sleep))
+		panic();
+#else
 	stm32_pinctrl_load_standby_cfg(hi2c->pinctrl, hi2c->pinctrl_count);
+#endif
 
 	hi2c->i2c_state = I2C_STATE_SUSPENDED;
 }

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.c
@@ -460,8 +460,13 @@ static void parse_regulator_fdt_nodes(void)
  * Return 0 on success, 1 if no PMIC node found and a negative value otherwise
  */
 static int dt_pmic_i2c_config(struct dt_node_info *i2c_info,
+#ifdef CFG_DRIVERS_PINCTRL
+			      struct pinctrl_state **pinctrl_active,
+			      struct pinctrl_state **pinctrl_sleep,
+#else
 			      struct stm32_pinctrl **pinctrl,
 			      size_t *pinctrl_count,
+#endif
 			      struct stm32_i2c_init_s *init)
 {
 	int pmic_node = 0;
@@ -493,9 +498,15 @@ static int dt_pmic_i2c_config(struct dt_node_info *i2c_info,
 	if (!i2c_info->reg)
 		return -FDT_ERR_NOTFOUND;
 
+#ifdef CFG_DRIVERS_PINCTRL
+	if (stm32_i2c_get_setup_from_fdt(fdt, i2c_node, init, pinctrl_active,
+					 pinctrl_sleep))
+		panic();
+#else
 	if (stm32_i2c_get_setup_from_fdt(fdt, i2c_node, init,
 					 pinctrl, pinctrl_count))
 		panic();
+#endif
 
 	return 0;
 }
@@ -510,17 +521,22 @@ static bool initialize_pmic_i2c(void)
 	int ret = 0;
 	struct dt_node_info i2c_info = { };
 	struct i2c_handle_s *i2c = &i2c_handle;
-	struct stm32_pinctrl *pinctrl = NULL;
-	size_t pin_count = 0;
 	struct stm32_i2c_init_s i2c_init = { };
 
-	ret = dt_pmic_i2c_config(&i2c_info, &pinctrl, &pin_count, &i2c_init);
+#ifdef CFG_DRIVERS_PINCTRL
+	if (dt_pmic_i2c_config(&i2c_info, &i2c->pinctrl, &i2c->pinctrl_sleep,
+			       &i2c_init))
+		panic();
+#else
+	ret = dt_pmic_i2c_config(&i2c_info, &i2c->pinctrl, &i2c->pinctrl_count,
+				 &i2c_init);
 	if (ret < 0) {
 		EMSG("I2C configuration failed %d", ret);
 		panic();
 	}
 	if (ret)
 		return false;
+#endif
 
 	/* Initialize PMIC I2C */
 	i2c->base.pa = i2c_info.reg;
@@ -532,9 +548,6 @@ static bool initialize_pmic_i2c(void)
 	i2c_init.own_address1 = pmic_i2c_addr;
 	i2c_init.analog_filter = true;
 	i2c_init.digital_filter_coef = 0;
-
-	i2c->pinctrl = pinctrl;
-	i2c->pinctrl_count = pin_count;
 
 	stm32mp_get_pmic();
 
@@ -586,26 +599,38 @@ void stm32mp_put_pmic(void)
 
 static void register_non_secure_pmic(void)
 {
-	size_t n = 0;
+	size_t __maybe_unused n = 0;
 
 	/* Allow this function to be called when STPMIC1 not used */
 	if (!i2c_handle.base.pa)
 		return;
 
+#ifdef CFG_DRIVERS_PINCTRL
+	stm32mp_register_non_secure_pinctrl(i2c_handle.pinctrl);
+	if (i2c_handle.pinctrl_sleep)
+		stm32mp_register_non_secure_pinctrl(i2c_handle.pinctrl_sleep);
+#else
 	for (n = 0; n < i2c_handle.pinctrl_count; n++)
 		stm32mp_register_non_secure_gpio(i2c_handle.pinctrl[n].bank,
 						 i2c_handle.pinctrl[n].pin);
+#endif
 
 	stm32mp_register_non_secure_periph_iomem(i2c_handle.base.pa);
 }
 
 static void register_secure_pmic(void)
 {
-	size_t n = 0;
+	size_t __maybe_unused n = 0;
 
+#ifdef CFG_DRIVERS_PINCTRL
+	stm32mp_register_secure_pinctrl(i2c_handle.pinctrl);
+	if (i2c_handle.pinctrl_sleep)
+		stm32mp_register_secure_pinctrl(i2c_handle.pinctrl_sleep);
+#else
 	for (n = 0; n < i2c_handle.pinctrl_count; n++)
 		stm32mp_register_secure_gpio(i2c_handle.pinctrl[n].bank,
 					     i2c_handle.pinctrl[n].pin);
+#endif
 
 	stm32mp_register_secure_periph_iomem(i2c_handle.base.pa);
 	register_pm_driver_cb(pmic_pm, NULL, "stm32mp1-pmic");

core/drivers/crypto/se050/glue/i2c_stm32.c
@@ -5,6 +5,7 @@
  */
 
 #include <assert.h>
+#include <drivers/pinctrl.h>
 #include <drivers/stm32_i2c.h>
 #include <i2c_native.h>
 #include <kernel/boot.h>
@@ -33,7 +34,13 @@ TEE_Result native_i2c_transfer(struct rpc_i2c_request *req, size_t *bytes)
 }
 
 static int dt_i2c_bus_config(struct stm32_i2c_init_s *init,
-			     struct stm32_pinctrl **pctrl,  size_t *pcnt)
+#ifdef CFG_DRIVERS_PINCTRL
+			     struct pinctrl_state **pinctrl_active,
+			     struct pinctrl_state **pinctrl_sleep
+#else
+			     struct stm32_pinctrl **pctrl,  size_t *pcnt
+#endif
+			     )
 {
 	const fdt32_t *cuint = NULL;
 	const char *path = NULL;
@@ -61,29 +68,35 @@ static int dt_i2c_bus_config(struct stm32_i2c_init_s *init,
 	else if (I2C_STANDARD_RATE != CFG_CORE_SE05X_BAUDRATE)
 		IMSG("SE05x ignoring CFG_CORE_SE05X_BAUDRATE, use built-in");
 
+#ifdef CFG_DRIVERS_PINCTRL
+	return stm32_i2c_get_setup_from_fdt(fdt, node, init, pinctrl_active,
+					    pinctrl_sleep);
+#else
 	return stm32_i2c_get_setup_from_fdt(fdt, node, init, pctrl, pcnt);
+#endif
 }
 
 int native_i2c_init(void)
 {
 	struct stm32_i2c_init_s i2c_init = { };
-	struct stm32_pinctrl *pinctrl = NULL;
-	size_t pin_count = 0;
 
 	/* No need to re-initialize */
 	if (i2c.base.pa)
 		return 0;
 
 	/* Support only one device on the platform */
+#ifdef CFG_DRIVERS_PINCTRL
+	if (dt_i2c_bus_config(&i2c_init, &i2c.pinctrl, &i2c.pinctrl_sleep))
+		return -1;
+#else
 	if (dt_i2c_bus_config(&i2c_init, &pinctrl, &pin_count))
 		return -1;
+#endif
 
 	/* Probe the device */
 	i2c_init.own_address1 = SMCOM_I2C_ADDRESS;
 	i2c_init.digital_filter_coef = 0;
 	i2c_init.analog_filter = true;
-	i2c.pinctrl_count = pin_count;
-	i2c.pinctrl = pinctrl;
 
 	stm32_i2c_resume(&i2c);
 

core/include/drivers/stm32_i2c.h
@@ -1,12 +1,13 @@
 /* SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause) */
 /*
- * Copyright (c) 2017-2019, STMicroelectronics
+ * Copyright (c) 2017-2023, STMicroelectronics
  */
 
-#ifndef __STM32_I2C_H
-#define __STM32_I2C_H
+#ifndef DRIVERS_STM32_I2C_H
+#define DRIVERS_STM32_I2C_H
 
 #include <drivers/clk.h>
+#include <drivers/pinctrl.h>
 #include <drivers/stm32_gpio.h>
 #include <kernel/dt.h>
 #include <mm/core_memprot.h>
@@ -111,6 +112,10 @@ struct i2c_cfg {
  * @saved_timing: Saved timing value if already computed
  * @saved_frequency: Saved frequency value if already computed
  * @sec_cfg: I2C registers configuration storage
+ * Case CFG_DRIVERS_PINCTRL
+ * @pinctrl: Pin control configuration for the I2C bus in active state
+ * @pinctrl_sleep: Pin control configuration for the I2C bus in standby state
+ * Case !CFG_DRIVERS_PINCTRL
  * @pinctrl: PINCTRLs configuration for the I2C PINs
  * @pinctrl_count: Number of PINCTRLs elements
  */
@@ -124,8 +129,13 @@ struct i2c_handle_s {
 	uint32_t saved_timing;
 	unsigned long saved_frequency;
 	struct i2c_cfg sec_cfg;
+#ifdef CFG_DRIVERS_PINCTRL
+	struct pinctrl_state *pinctrl;
+	struct pinctrl_state *pinctrl_sleep;
+#else
 	struct stm32_pinctrl *pinctrl;
 	size_t pinctrl_count;
+#endif
 };
 
 /* STM32 specific defines */
@@ -135,6 +145,22 @@ struct i2c_handle_s {
 #define STM32_I2C_ANALOG_FILTER_DELAY_MAX	U(260)	/* ns */
 #define STM32_I2C_DIGITAL_FILTER_MAX		U(16)
 
+#ifdef CFG_DRIVERS_PINCTRL
+/*
+ * Fill struct stm32_i2c_init_s from DT content for a given I2C node
+ *
+ * @fdt: Reference to DT
+ * @node: Target I2C node in the DT
+ * @init: Output stm32_i2c_init_s structure
+ * @pinctrl_active: Output active I2C pinctrl state
+ * @pinctrl_sleep: Output suspended I2C pinctrl state
+ * Return a TEE_Result compliant value
+ */
+TEE_Result stm32_i2c_get_setup_from_fdt(void *fdt, int node,
+					struct stm32_i2c_init_s *init,
+					struct pinctrl_state **pinctrl_active,
+					struct pinctrl_state **pinctrl_sleep);
+#else
 /*
  * Fill struct stm32_i2c_init_s from DT content for a given I2C node
  *
@@ -149,6 +175,7 @@ TEE_Result stm32_i2c_get_setup_from_fdt(void *fdt, int node,
 					struct stm32_i2c_init_s *init,
 					struct stm32_pinctrl **pinctrl,
 					size_t *pinctrl_count);
+#endif /*CFG_DRIVERS_PINCTRL*/
 
 /*
  * Initialize I2C bus handle from input configuration directives
@@ -265,4 +292,4 @@ static inline bool i2c_is_secure(struct i2c_handle_s *hi2c)
 	return hi2c->dt_status == DT_STATUS_OK_SEC;
 }
 
-#endif /* __STM32_I2C_H */
+#endif /* DRIVERS_STM32_I2C_H*/

commit a3de6929951944f9672752dd1759956b34576a3a

drivers: stm32_i2c: apply pinctrl config at init
Add missing load of stm32_i2c pinctrl state at driver init.

Fixes: 73ba32eb0f6c ("drivers: stm32_i2c: support CFG_DRIVERS_PINCTRL")
Reviewed-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/stm32_i2c.c
@@ -821,6 +821,9 @@ int stm32_i2c_init(struct i2c_handle_s *hi2c,
 
 	clk_disable(hi2c->clock);
 
+	if (hi2c->pinctrl && pinctrl_apply_state(hi2c->pinctrl))
+		return -1;
+
 	return 0;
 }
 