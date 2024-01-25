4ff6772837926233095da34ba1c154c453276080
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2024-01-25 11:46:37 +0100

    drivers: rstctrl: add reset controller for STM32MP21 platforms
    Implement the STM32MP21 reset controller device by embedding it
    with CFG_STM32_RSTCTRL=y and CFG_STM32MP21_RSTCTRL=y.
    
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>

--- core/drivers/rstctrl/sub.mk
+++ core/drivers/rstctrl/sub.mk
@@ -1,4 +1,5 @@
 srcs-y += rstctrl.c
 srcs-$(CFG_STM32_RSTCTRL) += stm32_rstctrl.c
 srcs-$(CFG_STM32MP1_RSTCTRL) += stm32mp1_rstctrl.c
+srcs-$(CFG_STM32MP21_RSTCTRL) += stm32mp21_rstctrl.c
 srcs-$(CFG_STM32MP25_RSTCTRL) += stm32mp25_rstctrl.c
