OP-TEE/optee_os

commit a8a14b78eef61b92105d1e420ca0709ac6410bf1

core: ls: lx2160ardb: Add regions for dynamic shared memory
To enable use of dynamic shared memory, DDR regions
need to be added in the platform MMU map.

Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
Acked-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-ls/conf.mk
@@ -91,9 +91,12 @@ include core/arch/arm/cpu/cortex-armv8-0.mk
 $(call force,CFG_CAAM_LITTLE_ENDIAN,y)
 $(call force,CFG_TEE_CORE_NB_CORE,16)
 $(call force,CFG_DRAM0_SIZE,0x80000000)
+$(call force,CFG_DRAM1_BASE,0x2080000000)
+$(call force,CFG_DRAM1_SIZE,0x1F80000000)
 $(call force,CFG_CORE_CLUSTER_SHIFT,1)
 $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_PL011,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,48)
 CFG_SHMEM_SIZE ?= 0x00200000
 endif
 

core/arch/arm/plat-ls/main.c
@@ -59,6 +59,13 @@ register_phys_mem_pgdir(MEM_AREA_IO_NSEC, CONSOLE_UART_BASE,
 			CORE_MMU_PGDIR_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GIC_BASE, CORE_MMU_PGDIR_SIZE);
 
+#if defined(PLATFORM_FLAVOR_lx2160ardb)
+register_ddr(CFG_DRAM0_BASE, (CFG_TZDRAM_START - CFG_DRAM0_BASE));
+#ifdef CFG_DRAM1_BASE
+register_ddr(CFG_DRAM1_BASE, CFG_DRAM1_SIZE);
+#endif
+#endif
+
 #ifdef CFG_ARM32_core
 void plat_primary_init_early(void)
 {

commit 2005b462bcd7b19db6cb0971c4f4c8413e89bbd8

core: ls: correct CFG_CORE_ARM64_PA_BITS for LX2160A-RDB/QDS
Correct CFG_CORE_ARM64_PA_BITS for LX2160A-RDB/QDS

Fixes: a8a14b78eef6 ("core: ls: lx2160ardb: Add regions for dynamic shared memory")
Fixes: 1a1214016d1c ("core: ls: add LX2160A-QDS platform")
Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-ls/conf.mk
@@ -65,7 +65,7 @@ $(call force,CFG_DRAM1_SIZE,0x1F80000000)
 $(call force,CFG_CORE_CLUSTER_SHIFT,1)
 $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_PL011,y)
-$(call force,CFG_CORE_ARM64_PA_BITS,48)
+$(call force,CFG_CORE_ARM64_PA_BITS,40)
 $(call force,CFG_EMBED_DTB,y)
 $(call force,CFG_EMBED_DTB_SOURCE_FILE,fsl-lx2160a-qds.dts)
 CFG_LS_I2C ?= y
@@ -83,7 +83,7 @@ $(call force,CFG_DRAM1_SIZE,0x1F80000000)
 $(call force,CFG_CORE_CLUSTER_SHIFT,1)
 $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_PL011,y)
-$(call force,CFG_CORE_ARM64_PA_BITS,48)
+$(call force,CFG_CORE_ARM64_PA_BITS,40)
 $(call force,CFG_EMBED_DTB,y)
 $(call force,CFG_EMBED_DTB_SOURCE_FILE,fsl-lx2160a-rdb.dts)
 CFG_LS_I2C ?= y