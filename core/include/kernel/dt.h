OP-TEE/optee_os

commit 4e45454a85d31d3cdf87b938ddc5ff467bfd6b27

core: add dt_getprop_as_number()
Adds dt_getprop_as_number() to read a property and parse it as a number
returned as a uint64_t. The size of the property determines if it's read
as an unsigned 32-bit or 64-bit integer.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/dt.h
@@ -73,6 +73,20 @@ enum dt_map_dev_directive {
 	DT_MAP_NON_SECURE
 };
 
+/*
+ * dt_getprop_as_number() - get a DT property a unsigned number
+ * @fdt: DT base address
+ * @nodeoffset: node offset
+ * @name: property string name
+ * @num: output number read
+ * Return 0 on success and a negative FDT error value on error
+ *
+ * The size of the property determines if it is read as an unsigned 32-bit
+ * or 64-bit integer.
+ */
+int dt_getprop_as_number(const void *fdt, int nodeoffset, const char *name,
+			 uint64_t *num);
+
 #ifdef CFG_DT
 /*
  * Find a driver that is suitable for the given DT node, that is, with

core/kernel/dt.c
@@ -415,3 +415,25 @@ int fdt_get_reg_props_by_name(const void *fdt, int node, const char *name,
 
 	return fdt_get_reg_props_by_index(fdt, node, index, base, size);
 }
+
+int dt_getprop_as_number(const void *fdt, int nodeoffset, const char *name,
+			 uint64_t *num)
+{
+	const void *prop = NULL;
+	int len = 0;
+
+	prop = fdt_getprop(fdt, nodeoffset, name, &len);
+	if (!prop)
+		return len;
+
+	switch (len) {
+	case sizeof(uint32_t):
+		*num = fdt32_ld(prop);
+		return 0;
+	case sizeof(uint64_t):
+		*num = fdt64_ld(prop);
+		return 0;
+	default:
+		return -FDT_ERR_BADVALUE;
+	}
+}

commit 3138ebce4374c149192b8d832dd9e95e76e0d4ed

core: dt: provide stubbed dt_getprop_as_number()
When compiled with CFG_DT=n link the following error can occur:
aarch64-linux-gnu-ld.bfd: out/arm/core/arch/arm/kernel/boot.o: in function `get_sec_mem_from_manifest':
core/arch/arm/kernel/boot.c:1511: undefined reference to `dt_getprop_as_number'
aarch64-linux-gnu-ld.bfd: core/arch/arm/kernel/boot.c:1519: undefined reference to `dt_getprop_as_number'
aarch64-linux-gnu-ld.bfd: out/arm/core/mm/core_mmu.o: in function `collect_device_mem_ranges':
core/mm/core_mmu.c:982: undefined reference to `dt_getprop_as_number'
aarch64-linux-gnu-ld.bfd: core/mm/core_mmu.c:993: undefined reference to `dt_getprop_as_number'

Fix this by adding a stubbed version of dt_getprop_as_number() when
CFG_DT=n.

Link: https://github.com/OP-TEE/optee_os/issues/6537
Fixes: 4e45454a85d3 ("core: add dt_getprop_as_number()")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/include/kernel/dt.h
@@ -86,6 +86,7 @@ struct dt_descriptor {
 
 extern uint8_t embedded_secure_dtb[];
 
+#ifdef CFG_DT
 /*
  * dt_getprop_as_number() - get a DT property a unsigned number
  * @fdt: DT base address
@@ -100,7 +101,6 @@ extern uint8_t embedded_secure_dtb[];
 int dt_getprop_as_number(const void *fdt, int nodeoffset, const char *name,
 			 uint64_t *num);
 
-#ifdef CFG_DT
 /*
  * Find a driver that is suitable for the given DT node, that is, with
  * a matching "compatible" property.
@@ -391,6 +391,14 @@ static inline int fdt_get_reg_props_by_name(const void *fdt __unused,
 					    const char *name __unused,
 					    paddr_t *base __unused,
 					    size_t *size __unused)
+{
+	return -1;
+}
+
+static inline int dt_getprop_as_number(const void *fdt __unused,
+				       int nodeoffset __unused,
+				       const char *name __unused,
+				       uint64_t *num __unused)
 {
 	return -1;
 }