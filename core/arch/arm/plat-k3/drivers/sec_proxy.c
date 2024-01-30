OP-TEE/optee_os

commit cf20f0a4f77eddf47f278f3d20dc786793542ab4

plat-k3: drivers: Add secure proxy driver for communication with System Controller
Secure Proxy is a communication scheme in Texas Instrument's devices
intended to provide an unique communication path from various processors
in the System on Chip(SoC) to a central System Controller.

Secure Proxy instance exposes "threads" or "proxies" which is
primary representation of "a" communication channel. Each thread is
preconfigured by System controller configuration based on SoC usage
requirements. Secure proxy by itself represents a single "queue" of
communication but allows the proxies to be independently operated.

For communication with TISCI, Secure Proxy driver is required in OP-TEE.

NOTE: Secure proxy configuration is only done by System Controller,
hence these are assumed to be pre-configured instances.

Provide the driver support for Secure Proxy and thread instances.

Tested on AM65x.

See AM65x Technical Reference Manual (SPRUID7E, April 2018)
for further details: https://www.ti.com/lit/ug/spruid7e/spruid7e.pdf

Signed-off-by: Manorit Chawdhry <m-chawdhry@ti.com>
Acked-by: Andrew Davis <afd@ti.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-k3/drivers/sec_proxy.c
@@ -1,0 +1,292 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Texas Instruments K3 Secure Proxy Driver
+ *
+ * Copyright (C) 2022 Texas Instruments Incorporated - https://www.ti.com/
+ *	Manorit Chawdhry <m-chawdhry@ti.com>
+ */
+
+#include <io.h>
+#include <kernel/delay.h>
+#include <mm/core_memprot.h>
+#include <mm/core_mmu.h>
+#include <platform_config.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <trace.h>
+
+#include "sec_proxy.h"
+
+/* SEC PROXY RT THREAD STATUS */
+#define RT_THREAD_STATUS_REG            0x0
+#define RT_THREAD_STATUS_ERROR_MASK     BIT(31)
+#define RT_THREAD_STATUS_CUR_CNT_MASK   GENMASK_32(7, 0)
+
+/* SEC PROXY SCFG THREAD CTRL */
+#define SCFG_THREAD_CTRL_REG            0x1000
+#define SCFG_THREAD_CTRL_DIR_SHIFT      31
+#define SCFG_THREAD_CTRL_DIR_MASK       BIT(31)
+
+/* SECURE PROXY GENERIC HELPERS */
+enum threads {
+	SEC_PROXY_TX_THREAD,
+	SEC_PROXY_RX_THREAD,
+	SEC_PROXY_MAX_THREADS
+};
+
+#define SEC_PROXY_THREAD(base, x)       ((base) + (0x1000 * (x)))
+#define SEC_PROXY_DATA_START_OFFS       0x4
+#define SEC_PROXY_DATA_END_OFFS         0x3c
+
+#define THREAD_DIR_TX (0)
+#define THREAD_DIR_RX (1)
+
+/**
+ * struct k3_sec_proxy_thread - Description of a Secure Proxy Thread
+ * @id:		Thread ID
+ * @data:	Thread Data path region for target
+ * @scfg:	Secure Config Region for Thread
+ * @rt:		RealTime Region for Thread
+ */
+struct k3_sec_proxy_thread {
+	const char *name;
+	vaddr_t data;
+	vaddr_t scfg;
+	vaddr_t rt;
+} spts[SEC_PROXY_MAX_THREADS];
+
+/**
+ * k3_sec_proxy_verify_thread() - Verify thread status before
+ *				  sending/receiving data
+ * @dir: Direction of the thread
+ */
+static TEE_Result k3_sec_proxy_verify_thread(uint32_t dir)
+{
+	struct k3_sec_proxy_thread *spt = &spts[dir];
+	uint64_t timeout = 0;
+	uint32_t val = 0;
+	unsigned int retry = 2;
+
+	FMSG("Check for thread corruption");
+	val = io_read32(spt->rt + RT_THREAD_STATUS_REG);
+
+	/* Check for any errors already available */
+	while ((val & RT_THREAD_STATUS_ERROR_MASK) && retry--) {
+		if (!retry) {
+			EMSG("Thread %s is corrupted, cannot send data.",
+			     spt->name);
+			return TEE_ERROR_BAD_STATE;
+		}
+
+		/* Write Bit 0 to this location */
+		IMSG("Resetting proxy thread %s", spt->name);
+		val ^= RT_THREAD_STATUS_ERROR_MASK;
+		io_write32(spt->rt + RT_THREAD_STATUS_REG, val);
+	}
+
+	FMSG("Check for thread direction");
+	/* Make sure thread is configured for right direction */
+	if ((io_read32(spt->scfg + SCFG_THREAD_CTRL_REG) &
+	     SCFG_THREAD_CTRL_DIR_MASK) >> SCFG_THREAD_CTRL_DIR_SHIFT != dir) {
+		if (dir == SEC_PROXY_TX_THREAD)
+			EMSG("Trying to receive data on tx Thread %s",
+			     spt->name);
+		else
+			EMSG("Trying to send data on rx Thread %s", spt->name);
+		return TEE_ERROR_COMMUNICATION;
+	}
+
+	FMSG("Check for thread queue");
+	/* Check the message queue before sending/receiving data */
+	timeout = timeout_init_us(SEC_PROXY_TIMEOUT_US);
+	while (!(io_read32(spt->rt + RT_THREAD_STATUS_REG) &
+		 RT_THREAD_STATUS_CUR_CNT_MASK)) {
+		DMSG("Waiting for thread %s to %s", spt->name,
+		     (dir == THREAD_DIR_TX) ? "empty" : "fill");
+		if (timeout_elapsed(timeout)) {
+			EMSG("Queue is busy");
+			return TEE_ERROR_BUSY;
+		}
+	}
+
+	FMSG("Success");
+	return TEE_SUCCESS;
+}
+
+/**
+ * k3_sec_proxy_send() - Send data over a Secure Proxy thread
+ * @msg: Pointer to k3_sec_proxy_msg
+ */
+TEE_Result k3_sec_proxy_send(const struct k3_sec_proxy_msg *msg)
+{
+	struct k3_sec_proxy_thread *spt = &spts[SEC_PROXY_TX_THREAD];
+	int num_words = 0;
+	int trail_bytes = 0;
+	int i = 0;
+	uintptr_t data_reg = 0;
+	uint32_t data_word = 0;
+	TEE_Result ret = TEE_SUCCESS;
+
+	FMSG("Verifying the thread");
+	ret = k3_sec_proxy_verify_thread(THREAD_DIR_TX);
+	if (ret) {
+		EMSG("Thread %s verification failed. ret = %d", spt->name, ret);
+		return ret;
+	}
+
+	/* Check the message size. */
+	if (msg->len > SEC_PROXY_MAX_MSG_SIZE) {
+		EMSG("Thread %s message length %zu > max msg size %d",
+		     spt->name, msg->len, SEC_PROXY_MAX_MSG_SIZE);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	/* Send the message */
+	data_reg = spt->data + SEC_PROXY_DATA_START_OFFS;
+	num_words = msg->len / sizeof(uint32_t);
+	for (i = 0; i < num_words; i++) {
+		memcpy(&data_word, &msg->buf[i * 4], sizeof(uint32_t));
+		io_write32(data_reg, data_word);
+		data_reg += sizeof(uint32_t);
+	}
+
+	trail_bytes = msg->len % sizeof(uint32_t);
+	if (trail_bytes) {
+		uint32_t data_trail = 0;
+
+		i = msg->len - trail_bytes;
+		while (trail_bytes--) {
+			data_trail <<= 8;
+			data_trail |= msg->buf[i++];
+		}
+
+		io_write32(data_reg, data_trail);
+		data_reg += sizeof(uint32_t);
+	}
+
+	/*
+	 * 'data_reg' indicates next register to write. If we did not already
+	 * write on tx complete reg(last reg), we must do so for transmit
+	 */
+	if (data_reg <= (spt->data + SEC_PROXY_DATA_END_OFFS))
+		io_write32(spt->data + SEC_PROXY_DATA_END_OFFS, 0);
+
+	return TEE_SUCCESS;
+}
+
+/**
+ * k3_sec_proxy_recv() - Receive data from a Secure Proxy thread
+ * @msg: Pointer to k3_sec_proxy_msg
+ */
+TEE_Result k3_sec_proxy_recv(struct k3_sec_proxy_msg *msg)
+{
+	struct k3_sec_proxy_thread *spt = &spts[SEC_PROXY_RX_THREAD];
+	int num_words = 0;
+	int i = 0;
+	int trail_bytes = 0;
+	uint32_t data_trail = 0;
+	uint32_t data_word = 0;
+	uintptr_t data_reg = 0;
+	TEE_Result ret = TEE_SUCCESS;
+
+	FMSG("Verifying thread");
+	ret = k3_sec_proxy_verify_thread(THREAD_DIR_RX);
+	if (ret) {
+		EMSG("Thread %s verification failed. ret = %d", spt->name, ret);
+		return ret;
+	}
+
+	/* Receive the message */
+	data_reg = spt->data + SEC_PROXY_DATA_START_OFFS;
+	num_words = msg->len / sizeof(uint32_t);
+	for (i = 0; i < num_words; i++) {
+		data_word = io_read32(data_reg);
+		memcpy(&msg->buf[i * 4], &data_word, sizeof(uint32_t));
+		data_reg += sizeof(uint32_t);
+	}
+
+	trail_bytes = msg->len % sizeof(uint32_t);
+	if (trail_bytes) {
+		data_trail = io_read32(data_reg);
+		data_reg += sizeof(uint32_t);
+
+		i = msg->len - trail_bytes;
+		while (trail_bytes--) {
+			msg->buf[i] = data_trail & 0xff;
+			data_trail >>= 8;
+		}
+	}
+
+	/*
+	 * 'data_reg' indicates next register to read. If we did not already
+	 * read on rx complete reg(last reg), we must do so for receive
+	 */
+	if (data_reg <= (spt->data + SEC_PROXY_DATA_END_OFFS))
+		io_read32(spt->data + SEC_PROXY_DATA_END_OFFS);
+
+	return TEE_SUCCESS;
+}
+
+/**
+ * k3_sec_proxy_init() - Initialize the secure proxy threads
+ */
+TEE_Result k3_sec_proxy_init(void)
+{
+	struct k3_sec_proxy_thread *thread;
+	int rx_thread = SEC_PROXY_RESPONSE_THREAD;
+	int tx_thread = SEC_PROXY_REQUEST_THREAD;
+	uint32_t target_data = 0;
+	uint32_t cfg_scfg = 0;
+	uint32_t cfg_rt = 0;
+
+	DMSG("tx_thread: %d, rx_thread: %d", tx_thread, rx_thread);
+
+	/* TX_THREAD */
+	target_data = SEC_PROXY_THREAD(SEC_PROXY_DATA_BASE, tx_thread);
+	cfg_scfg = SEC_PROXY_THREAD(SEC_PROXY_SCFG_BASE, tx_thread);
+	cfg_rt = SEC_PROXY_THREAD(SEC_PROXY_RT_BASE, tx_thread);
+
+	thread = &spts[SEC_PROXY_TX_THREAD];
+	thread->name = "SEC_PROXY_LOW_PRIORITY_THREAD";
+
+	thread->data = core_mmu_get_va(target_data, MEM_AREA_IO_SEC,
+				       SEC_PROXY_DATA_SIZE);
+	if (!thread->data)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	thread->scfg = core_mmu_get_va(cfg_scfg, MEM_AREA_IO_SEC,
+				       SEC_PROXY_SCFG_SIZE);
+	if (!thread->scfg)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	thread->rt = core_mmu_get_va(cfg_rt, MEM_AREA_IO_SEC,
+				     SEC_PROXY_RT_SIZE);
+	if (!thread->rt)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	/* RX_THREAD */
+	target_data = SEC_PROXY_THREAD(SEC_PROXY_DATA_BASE, rx_thread);
+	cfg_scfg = SEC_PROXY_THREAD(SEC_PROXY_SCFG_BASE, rx_thread);
+	cfg_rt = SEC_PROXY_THREAD(SEC_PROXY_RT_BASE, rx_thread);
+
+	thread = &spts[SEC_PROXY_RX_THREAD];
+	thread->name = "SEC_PROXY_RESPONSE_THREAD";
+
+	thread->data = core_mmu_get_va(target_data, MEM_AREA_IO_SEC,
+				       SEC_PROXY_DATA_SIZE);
+	if (!thread->data)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	thread->scfg = core_mmu_get_va(cfg_scfg, MEM_AREA_IO_SEC,
+				       SEC_PROXY_SCFG_SIZE);
+	if (!thread->scfg)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	thread->rt = core_mmu_get_va(cfg_rt, MEM_AREA_IO_SEC,
+				     SEC_PROXY_RT_SIZE);
+	if (!thread->rt)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	return TEE_SUCCESS;
+}

core/arch/arm/plat-k3/drivers/sec_proxy.h
@@ -1,0 +1,47 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Texas Instruments K3 Secure Proxy Driver
+ *
+ * Copyright (C) 2022 Texas Instruments Incorporated - https://www.ti.com/
+ *	Manorit Chawdhry <m-chawdhry@ti.com>
+ */
+
+#ifndef SEC_PROXY_H
+#define SEC_PROXY_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <tee_api_types.h>
+
+#define SEC_PROXY_MAX_MSG_SIZE 60
+
+/**
+ * struct k3_sec_proxy_msg - Secure proxy message structure
+ * @len: Length of data in the Buffer
+ * @buf: Buffer pointer
+ *
+ * This is the structure for data used in k3_sec_proxy_{send,recv}()
+ */
+struct k3_sec_proxy_msg {
+	size_t len;
+	uint8_t *buf;
+};
+
+/**
+ * k3_sec_proxy_send() - Send data over a Secure Proxy thread
+ * @msg: Pointer to k3_sec_proxy_msg
+ */
+TEE_Result k3_sec_proxy_send(const struct k3_sec_proxy_msg *msg);
+
+/**
+ * k3_sec_proxy_recv() - Receive data from a Secure Proxy thread
+ * @msg: Pointer to k3_sec_proxy_msg
+ */
+TEE_Result k3_sec_proxy_recv(struct k3_sec_proxy_msg *msg);
+
+/**
+ * k3_sec_proxy_init() - Initialize the secure proxy threads
+ */
+TEE_Result k3_sec_proxy_init(void);
+
+#endif /* __SEC_PROXY_H */

core/arch/arm/plat-k3/drivers/sub.mk
@@ -1,0 +1,1 @@
+srcs-y += sec_proxy.c

core/arch/arm/plat-k3/main.c
@@ -24,6 +24,11 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICC_BASE, GICC_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICD_BASE, GICD_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_NSEC, CONSOLE_UART_BASE,
 		  SERIAL8250_UART_REG_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_DATA_BASE,
+			SEC_PROXY_DATA_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_SCFG_BASE,
+			SEC_PROXY_SCFG_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_PROXY_RT_BASE, SEC_PROXY_RT_SIZE);
 
 void main_init_gic(void)
 {

core/arch/arm/plat-k3/platform_config.h
@@ -27,6 +27,27 @@
 #define GICD_OFFSET     0x0
 #define GICD_SIZE       0x10000
 #endif
+#if defined(PLATFORM_FLAVOR_am65x) || defined(PLATFORM_FLAVOR_j721e)
+#define SEC_PROXY_DATA_BASE             0x32c00000
+#define SEC_PROXY_DATA_SIZE             0x100000
+#define SEC_PROXY_SCFG_BASE             0x32800000
+#define SEC_PROXY_SCFG_SIZE             0x100000
+#define SEC_PROXY_RT_BASE               0x32400000
+#define SEC_PROXY_RT_SIZE               0x100000
+#define SEC_PROXY_RESPONSE_THREAD       6
+#define SEC_PROXY_REQUEST_THREAD        7
+#else
+#define SEC_PROXY_DATA_BASE             0x4d000000
+#define SEC_PROXY_DATA_SIZE             0x80000
+#define SEC_PROXY_SCFG_BASE             0x4a400000
+#define SEC_PROXY_SCFG_SIZE             0x80000
+#define SEC_PROXY_RT_BASE               0x4a600000
+#define SEC_PROXY_RT_SIZE               0x80000
+#define SEC_PROXY_RESPONSE_THREAD       10
+#define SEC_PROXY_REQUEST_THREAD        11
+#endif
+#define OPTEE_HOST_ID                   11
+#define SEC_PROXY_TIMEOUT_US            1000000
 #define GICC_BASE       (SCU_BASE + GICC_OFFSET)
 #define GICD_BASE       (SCU_BASE + GICD_OFFSET)
 

core/arch/arm/plat-k3/sub.mk
@@ -1,2 +1,3 @@
 global-incdirs-y += .
 srcs-y += main.c
+subdirs-y += drivers

commit 08a0b7e9735ef57e20ce7cfd2484bbf9ae5f8746

plat-k3: drivers: sec_proxy: increment while reading trail bytes
The trail bytes from the secure proxy driver were being overwritten,
increase the count each time to not overwrite the existing data and not
get the end data corrupted from secure proxy.

Fixes: cf20f0a4f77e ("plat-k3: drivers: Add secure proxy driver for communication with System Controller")
Signed-off-by: Manorit Chawdhry <m-chawdhry@ti.com>
Acked-by: Andrew Davis <afd@ti.com>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Dhruva Gole <d-gole@ti.com>

core/arch/arm/plat-k3/drivers/sec_proxy.c
@@ -213,7 +213,7 @@ TEE_Result k3_sec_proxy_recv(struct k3_sec_proxy_msg *msg)
 
 		i = msg->len - trail_bytes;
 		while (trail_bytes--) {
-			msg->buf[i] = data_trail & 0xff;
+			msg->buf[i++] = data_trail & 0xff;
 			data_trail >>= 8;
 		}
 	}