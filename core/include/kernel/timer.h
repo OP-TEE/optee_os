c41db53b5f259f7122fa673410f1033ddda1d2c8
Jens Wiklander <jens.wiklander@linaro.org>
2024-02-01 15:10:55 +0100

    core: define generic callout service initializer
    Add a generic timer_init_callout_service() to be implemented in
    architecture or platform specific code. The function shall provide an
    implementation of the abstract timer interface needed by the callout
    service and register a timer interrupt callback to call
    callout_service_cb().
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/include/kernel/timer.h
+++ core/include/kernel/timer.h
@@ -1,15 +1,34 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (C) 2018, Linaro Limited
+ * Copyright (C) 2018, 2024, Linaro Limited
  */
 
 #ifndef __KERNEL_TIMER_H
 #define __KERNEL_TIMER_H
+
+#include <kernel/interrupt.h>
+#include <types_ext.h>
 
 void generic_timer_start(uint32_t time_ms);
 void generic_timer_stop(void);
 
 /* Handler for timer expiry interrupt */
 void generic_timer_handler(uint32_t time_ms);
+
+/*
+ * timer_init_callout_service() - Initializes the callout service
+ * @itr_chip:	Interrupt chip, typically interrupt_get_main_chip()
+ * @itr_number:	Interrupt number in @itr_chip space for the timer.
+ *
+ * This function starts the callout service via interrupts from the timer.
+ * The platform or architecture specific code provides the implementation
+ * of this function. The interrupt callback function for the timer calls
+ * callout_service_cb() to drive the callout service.
+ *
+ * Note that usage of this function is incompatible with usage of the
+ * generic_timer_start(), generic_timer_stop() and generic_timer_handler()
+ * functions.
+ */
+void timer_init_callout_service(struct itr_chip *itr_chip, size_t itr_number);
 
 #endif /* __KERNEL_TIMER_H */
