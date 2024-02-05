OP-TEE/optee_os

commit ba6b29591828040f913d336d5825fd0772e5b3d7

core: arm64: Add Secure EL1 physical timer framework
As an implementation of generic timer, arm64 platforms provides secure
EL1 physical timer. So enable corresponding framework. For more
information refer to section: D6.1.5 Timers - ARMv8-A RM.

Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
Acked-by: Joakim Bech <joakim.bech@linaro.org>

core/arch/arm/kernel/timer_a64.c
@@ -1,0 +1,68 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2018, Linaro Limited
+ */
+
+#include <arm64.h>
+#include <kernel/spinlock.h>
+#include <kernel/timer.h>
+
+static unsigned int timer_lock = SPINLOCK_UNLOCK;
+static bool timer_running;
+
+void generic_timer_start(uint32_t time_ms)
+{
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_ALL);
+	uint32_t timer_ticks;
+
+	cpu_spin_lock(&timer_lock);
+
+	if (timer_running == true)
+		goto exit;
+
+	/* The timer will fire time_ms from now */
+	timer_ticks = (read_cntfrq() * time_ms) / 1000;
+	write_cntps_tval(timer_ticks);
+
+	/* Enable the secure physical timer */
+	write_cntps_ctl(1);
+
+	timer_running = true;
+
+exit:
+	cpu_spin_unlock(&timer_lock);
+	thread_set_exceptions(exceptions);
+}
+
+void generic_timer_stop(void)
+{
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_ALL);
+
+	cpu_spin_lock(&timer_lock);
+
+	/* Disable the timer */
+	write_cntps_ctl(0);
+
+	timer_running = false;
+
+	cpu_spin_unlock(&timer_lock);
+	thread_set_exceptions(exceptions);
+}
+
+void generic_timer_handler(uint32_t time_ms)
+{
+	uint32_t timer_ticks;
+
+	/* Ensure that the timer did assert the interrupt */
+	assert((read_cntps_ctl() >> 2));
+
+	/* Disable the timer */
+	write_cntps_ctl(0);
+
+	/* Reconfigure timer to fire time_ms from now */
+	timer_ticks = (read_cntfrq() * time_ms) / 1000;
+	write_cntps_tval(timer_ticks);
+
+	/* Enable the secure physical timer */
+	write_cntps_ctl(1);
+}

core/arch/arm/include/arm64.h
@@ -305,6 +305,10 @@ DEFINE_REG_READ_FUNC_(cntfrq, uint32_t, cntfrq_el0)
 DEFINE_REG_READ_FUNC_(cntpct, uint64_t, cntpct_el0)
 DEFINE_REG_READ_FUNC_(cntkctl, uint32_t, cntkctl_el1)
 DEFINE_REG_WRITE_FUNC_(cntkctl, uint32_t, cntkctl_el1)
+DEFINE_REG_READ_FUNC_(cntps_ctl, uint32_t, cntps_ctl_el1)
+DEFINE_REG_WRITE_FUNC_(cntps_ctl, uint32_t, cntps_ctl_el1)
+DEFINE_REG_READ_FUNC_(cntps_tval, uint32_t, cntps_tval_el1)
+DEFINE_REG_WRITE_FUNC_(cntps_tval, uint32_t, cntps_tval_el1)
 
 DEFINE_REG_READ_FUNC_(pmccntr, uint64_t, pmccntr_el0)
 

core/arch/arm/include/kernel/timer.h
@@ -1,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2018, Linaro Limited
+ */
+
+#ifndef __TIMER_H
+#define __TIMER_H
+
+void generic_timer_start(uint32_t time_ms);
+void generic_timer_stop(void);
+
+/* Handler for timer expiry interrupt */
+void generic_timer_handler(uint32_t time_ms);
+
+#endif /* __TIMER_H */

core/arch/arm/kernel/sub.mk
@@ -13,6 +13,7 @@ srcs-y += delay.c
 
 srcs-$(CFG_SECURE_TIME_SOURCE_CNTPCT) += tee_time_arm_cntpct.c
 srcs-$(CFG_SECURE_TIME_SOURCE_REE) += tee_time_ree.c
+srcs-$(CFG_ARM64_core) += timer_a64.c
 
 srcs-$(CFG_ARM32_core) += proc_a32.S
 srcs-$(CFG_ARM32_core) += spin_lock_a32.S

commit 5bb9c6c4e660c34f223320bd54bd669ffad81dfc

core: arm: fix integer overflow in generic_timer_{handler,start}()
In generic_timer_handler() and generic_timer_start(), read_cntfrq() can
return a pretty large 32-bit number, multiplying that with a delay of
1000 ms can overflow. Fix that by casting the result from read_cntfrq()
to a uint64_t to avoid overflow during the calculation.

Fixes: ba6b29591828 ("core: arm64: Add Secure EL1 physical timer framework")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/timer_a64.c
@@ -13,7 +13,7 @@ static bool timer_running;
 void generic_timer_start(uint32_t time_ms)
 {
 	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_ALL);
-	uint32_t timer_ticks;
+	uint32_t timer_ticks = 0;
 
 	cpu_spin_lock(&timer_lock);
 
@@ -21,7 +21,7 @@ void generic_timer_start(uint32_t time_ms)
 		goto exit;
 
 	/* The timer will fire time_ms from now */
-	timer_ticks = (read_cntfrq() * time_ms) / 1000;
+	timer_ticks = ((uint64_t)read_cntfrq() * time_ms) / 1000;
 	write_cntps_tval(timer_ticks);
 
 	/* Enable the secure physical timer */
@@ -51,7 +51,7 @@ void generic_timer_stop(void)
 
 void generic_timer_handler(uint32_t time_ms)
 {
-	uint32_t timer_ticks;
+	uint32_t timer_ticks = 0;
 
 	/* Ensure that the timer did assert the interrupt */
 	assert((read_cntps_ctl() >> 2));
@@ -60,7 +60,7 @@ void generic_timer_handler(uint32_t time_ms)
 	write_cntps_ctl(0);
 
 	/* Reconfigure timer to fire time_ms from now */
-	timer_ticks = (read_cntfrq() * time_ms) / 1000;
+	timer_ticks = ((uint64_t)read_cntfrq() * time_ms) / 1000;
 	write_cntps_tval(timer_ticks);
 
 	/* Enable the secure physical timer */