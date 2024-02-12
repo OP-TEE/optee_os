OP-TEE/optee_os

commit 5516c6cd78da05b01b15c85aed8a1ab98e96051b

core: ecc: support the crypto driver
Provide an interface that cryptographic IC drivers can use to fallback
to the software implementation of the algorithms.

This supports the use case where vulnerabilities in the IC algorithms
can be addressed without a respin of the hardware.

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/crypto/crypto_impl.h
@@ -355,6 +355,12 @@ struct crypto_ecc_keypair_ops {
 };
 
 #ifdef CFG_CRYPTO_ECC
+const struct crypto_ecc_keypair_ops *
+crypto_asym_get_ecc_keypair_ops(uint32_t key_type);
+
+const struct crypto_ecc_public_ops *
+crypto_asym_get_ecc_public_ops(uint32_t key_type);
+
 TEE_Result crypto_asym_alloc_ecc_public_key(struct ecc_public_key *key,
 					    uint32_t key_type,
 					    size_t key_size_bits);
@@ -368,6 +374,18 @@ crypto_asym_alloc_ecc_public_key(struct ecc_public_key *key __unused,
 				 size_t key_size_bits __unused)
 {
 	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+
+static inline const struct crypto_ecc_keypair_ops *
+crypto_asym_get_keypair_ops(uint32_t key_type __unused)
+{
+	return NULL;
+}
+
+static inline const struct crypto_ecc_public_ops *
+crypto_asym_get_ecc_public_ops(uint32_t key_type __unused)
+{
+	return NULL;
 }
 
 static inline TEE_Result

commit 06f72a33f501fff58d7caa2e214fd765a85d52f3

core: crypto: fix crypto_asym_get_ecc_keypair_ops() stub
Correct definition of crypto_asym_get_ecc_keypair_ops() stub inline
function when CFG_CRYPTO_ECC is disabled. The definition used a wrong
function label.

Fixes: 5516c6cd78da ("core: ecc: support the crypto driver")
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/crypto/crypto_impl.h
@@ -463,7 +463,7 @@ crypto_asym_alloc_ecc_public_key(struct ecc_public_key *key __unused,
 }
 
 static inline const struct crypto_ecc_keypair_ops *
-crypto_asym_get_keypair_ops(uint32_t key_type __unused)
+crypto_asym_get_ecc_keypair_ops(uint32_t key_type __unused)
 {
 	return NULL;
 }