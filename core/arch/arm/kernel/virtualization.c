OP-TEE/optee_os

commit a65dd3a6b64ddf6b3377babcb123c99d9b782d28

core: spmc: support virtualization with SPMC at S-EL1
Adds support for virtualization with OP-TEE as SPMC at S-EL1. This if
the FF-A counterpart of SMC based ABI with virtualization.

Reviewed-by: Balint Dobszay <balint.dobszay@arm.com>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/virtualization.c
@@ -42,6 +42,10 @@ struct guest_partition {
 	bool runtime_initialized;
 	uint16_t id;
 	struct refcount refc;
+#ifdef CFG_CORE_SEL1_SPMC
+	uint64_t cookies[64];
+	uint8_t cookie_count;
+#endif
 };
 
 struct guest_partition *current_partition[CFG_TEE_CORE_NB_CORE] __nex_bss;
@@ -432,3 +436,83 @@ void virt_get_ta_ram(vaddr_t *start, vaddr_t *end)
 				       tee_mm_get_bytes(prtn->ta_ram));
 	*end = *start + tee_mm_get_bytes(prtn->ta_ram);
 }
+
+#ifdef CFG_CORE_SEL1_SPMC
+static int find_cookie(struct guest_partition *prtn, uint64_t cookie)
+{
+	int i = 0;
+
+	for (i = 0; i < prtn->cookie_count; i++)
+		if (prtn->cookies[i] == cookie)
+			return i;
+	return -1;
+}
+
+static struct guest_partition *find_prtn_cookie(uint64_t cookie, int *idx)
+{
+	struct guest_partition *prtn = NULL;
+	int i = 0;
+
+	LIST_FOREACH(prtn, &prtn_list, link) {
+		i = find_cookie(prtn, cookie);
+		if (i >= 0) {
+			if (idx)
+				*idx = i;
+			return prtn;
+		}
+	}
+
+	return NULL;
+}
+
+TEE_Result virt_add_cookie_to_current_guest(uint64_t cookie)
+{
+	TEE_Result res = TEE_ERROR_ACCESS_DENIED;
+	struct guest_partition *prtn = NULL;
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+
+	if (find_prtn_cookie(cookie, NULL))
+		goto out;
+
+	prtn = current_partition[get_core_pos()];
+	if (prtn->cookie_count < ARRAY_SIZE(prtn->cookies)) {
+		prtn->cookies[prtn->cookie_count] = cookie;
+		prtn->cookie_count++;
+		res = TEE_SUCCESS;
+	}
+out:
+	thread_unmask_exceptions(exceptions);
+
+	return res;
+}
+
+void virt_remove_cookie(uint64_t cookie)
+{
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+	struct guest_partition *prtn = NULL;
+	int i = 0;
+
+	prtn = find_prtn_cookie(cookie, &i);
+	if (prtn) {
+		memmove(prtn->cookies + i, prtn->cookies + i + 1,
+			sizeof(uint64_t) * (prtn->cookie_count - i - 1));
+		prtn->cookie_count--;
+	}
+	thread_unmask_exceptions(exceptions);
+}
+
+uint16_t virt_find_guest_by_cookie(uint64_t cookie)
+{
+	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+	struct guest_partition *prtn = NULL;
+	uint16_t ret = 0;
+
+	prtn = find_prtn_cookie(cookie, NULL);
+	if (prtn)
+		ret = prtn->id;
+
+	thread_unmask_exceptions(exceptions);
+
+	return ret;
+}
+#endif /*CFG_CORE_SEL1_SPMC*/

core/arch/arm/include/ffa.h
@@ -169,9 +169,17 @@
 #define FFA_PART_PROP_NOTIF_DESTROYED	BIT(7)
 #define FFA_PART_PROP_AARCH64_STATE	BIT(8)
 
+#define FFA_MEMORY_HANDLE_HYPERVISOR_BIT	BIT64(63)
+#define FFA_MEMORY_HANDLE_SECURE_BIT		BIT64(45)
+#define FFA_MEMORY_HANDLE_NON_SECURE_BIT	BIT64(44)
+/*
+ * Codes the OP-TEE partition/guest ID into a cookie in order to know which
+ * partition to activate when reclaiming the shared memory. This field is 0
+ * unless CFG_NS_VIRTUALIZATION is enabled.
+ */
+#define FFA_MEMORY_HANDLE_PRTN_SHIFT		16
+#define FFA_MEMORY_HANDLE_PRTN_MASK		GENMASK_32(16, 0)
 
-#define FFA_MEMORY_HANDLE_SECURE_BIT	BIT64(45)
-#define FFA_MEMORY_HANDLE_NONE_SECURE_BIT	BIT64(44)
 
 #define FFA_BOOT_INFO_NAME_LEN		U(16)
 

core/arch/arm/kernel/thread_spmc.c
@@ -17,6 +17,7 @@
 #include <kernel/thread.h>
 #include <kernel/thread_private.h>
 #include <kernel/thread_spmc.h>
+#include <kernel/virtualization.h>
 #include <mm/core_mmu.h>
 #include <mm/mobj.h>
 #include <optee_ffa.h>
@@ -49,6 +50,10 @@ static uint16_t my_endpoint_id __nex_bss;
 #ifdef CFG_CORE_SEL1_SPMC
 static const uint32_t my_part_props = FFA_PART_PROP_DIRECT_REQ_RECV |
 				      FFA_PART_PROP_DIRECT_REQ_SEND |
+#ifdef CFG_NS_VIRTUALIZATION
+				      FFA_PART_PROP_NOTIF_CREATED |
+				      FFA_PART_PROP_NOTIF_DESTROYED |
+#endif
 #ifdef ARM64
 				      FFA_PART_PROP_AARCH64_STATE |
 #endif
@@ -81,7 +86,7 @@ static uint32_t my_uuid_words[] = {
  * the lock.
  */
 
-static struct ffa_rxtx nw_rxtx;
+static struct ffa_rxtx nw_rxtx __nex_bss;
 
 static bool is_nw_buf(struct ffa_rxtx *rxtx)
 {
@@ -99,6 +104,11 @@ static struct ffa_rxtx nw_rxtx = { .rx = __rx_buf, .tx = __tx_buf };
 static uint32_t swap_src_dst(uint32_t src_dst)
 {
 	return (src_dst >> 16) | (src_dst << 16);
+}
+
+static uint16_t get_sender_id(uint32_t src_dst)
+{
+	return src_dst >> 16;
 }
 
 void spmc_set_args(struct thread_smc_args *args, uint32_t fid, uint32_t src_dst,
@@ -256,13 +266,54 @@ void spmc_handle_rxtx_map(struct thread_smc_args *args, struct ffa_rxtx *rxtx)
 	 * mapped.
 	 */
 	if (is_nw_buf(rxtx)) {
-		rc = map_buf(tx_pa, sz, &tx);
-		if (rc)
-			goto out;
-		rc = map_buf(rx_pa, sz, &rx);
-		if (rc) {
-			unmap_buf(tx, sz);
-			goto out;
+		if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+			enum teecore_memtypes mt = MEM_AREA_NEX_NSEC_SHM;
+			bool tx_alloced = false;
+
+			/*
+			 * With virtualization we establish this mapping in
+			 * the nexus mapping which then is replicated to
+			 * each partition.
+			 *
+			 * This means that this mapping must be done before
+			 * any partition is created and then must not be
+			 * changed.
+			 */
+
+			/*
+			 * core_mmu_add_mapping() may reuse previous
+			 * mappings. First check if there's any mappings to
+			 * reuse so we know how to clean up in case of
+			 * failure.
+			 */
+			tx = phys_to_virt(tx_pa, mt, sz);
+			rx = phys_to_virt(rx_pa, mt, sz);
+			if (!tx) {
+				tx = core_mmu_add_mapping(mt, tx_pa, sz);
+				if (!tx) {
+					rc = FFA_NO_MEMORY;
+					goto out;
+				}
+				tx_alloced = true;
+			}
+			if (!rx)
+				rx = core_mmu_add_mapping(mt, rx_pa, sz);
+
+			if (!rx) {
+				if (tx_alloced && tx)
+					core_mmu_remove_mapping(mt, tx, sz);
+				rc = FFA_NO_MEMORY;
+				goto out;
+			}
+		} else {
+			rc = map_buf(tx_pa, sz, &tx);
+			if (rc)
+				goto out;
+			rc = map_buf(rx_pa, sz, &rx);
+			if (rc) {
+				unmap_buf(tx, sz);
+				goto out;
+			}
 		}
 		rxtx->tx = tx;
 		rxtx->rx = rx;
@@ -580,6 +631,22 @@ static void handle_framework_direct_request(struct thread_smc_args *args,
 	uint32_t w3 = FFA_PARAM_MBZ;
 
 	switch (args->a2 & FFA_MSG_TYPE_MASK) {
+	case FFA_MSG_SEND_VM_CREATED:
+		if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+			uint16_t guest_id = args->a5;
+			TEE_Result res = virt_guest_created(guest_id);
+
+			w0 = FFA_MSG_SEND_DIRECT_RESP_32;
+			w1 = swap_src_dst(args->a1);
+			w2 = FFA_MSG_FLAG_FRAMEWORK | FFA_MSG_RESP_VM_CREATED;
+			if (res == TEE_SUCCESS)
+				w3 = FFA_OK;
+			else if (res == TEE_ERROR_OUT_OF_MEMORY)
+				w3 = FFA_DENIED;
+			else
+				w3 = FFA_INVALID_PARAMETERS;
+		}
+		break;
 	case FFA_MSG_VERSION_REQ:
 		w0 = FFA_MSG_SEND_DIRECT_RESP_32;
 		w1 = swap_src_dst(args->a1);
@@ -606,10 +673,25 @@ static void handle_direct_request(struct thread_smc_args *args,
 		return;
 	}
 
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION) &&
+	    virt_set_guest(get_sender_id(args->a1))) {
+		spmc_set_args(args, FFA_MSG_SEND_DIRECT_RESP_32,
+			      swap_src_dst(args->a1), 0,
+			      TEE_ERROR_ITEM_NOT_FOUND, 0, 0);
+		return;
+	}
+
 	if (args->a3 & BIT32(OPTEE_FFA_YIELDING_CALL_BIT))
 		handle_yielding_call(args);
 	else
 		handle_blocking_call(args);
+
+	/*
+	 * Note that handle_yielding_call() typically only returns if a
+	 * thread cannot be allocated or found. virt_unset_guest() is also
+	 * called from thread_state_suspend() and thread_state_free().
+	 */
+	virt_unset_guest();
 }
 
 int spmc_read_mem_transaction(uint32_t ffa_vers, void *buf, size_t blen,
@@ -829,6 +911,7 @@ static int add_mem_share(struct ffa_mem_transaction_x *mem_trans,
 	int rc = 0;
 	struct mem_share_state share = { };
 	size_t addr_range_offs = 0;
+	uint64_t cookie = OPTEE_MSG_FMEM_INVALID_GLOBAL_ID;
 	size_t n = 0;
 
 	rc = mem_share_init(mem_trans, buf, flen, &share.page_count,
@@ -841,7 +924,9 @@ static int add_mem_share(struct ffa_mem_transaction_x *mem_trans,
 	    ADD_OVERFLOW(n, addr_range_offs, &n) || n > blen)
 		return FFA_INVALID_PARAMETERS;
 
-	share.mf = mobj_ffa_sel1_spmc_new(share.page_count);
+	if (mem_trans->global_handle)
+		cookie = mem_trans->global_handle;
+	share.mf = mobj_ffa_sel1_spmc_new(cookie, share.page_count);
 	if (!share.mf)
 		return FFA_NO_MEMORY;
 
@@ -921,9 +1006,13 @@ static int handle_mem_share_tmem(paddr_t pbuf, size_t blen, size_t flen,
 
 	cpu_spin_lock(&rxtx->spinlock);
 	rc = spmc_read_mem_transaction(rxtx->ffa_vers, buf, flen, &mem_trans);
+	if (!rc && IS_ENABLED(CFG_NS_VIRTUALIZATION) &&
+	    virt_set_guest(mem_trans.sender_id))
+		rc = FFA_DENIED;
 	if (!rc)
 		rc = add_mem_share(&mem_trans, mm, buf, blen, flen,
 				   global_handle);
+	virt_unset_guest();
 	cpu_spin_unlock(&rxtx->spinlock);
 	if (rc > 0)
 		return rc;
@@ -956,8 +1045,14 @@ static int handle_mem_share_rxbuf(size_t blen, size_t flen,
 		goto out;
 	}
 
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION) &&
+	    virt_set_guest(mem_trans.sender_id))
+		goto out;
+
 	rc = add_mem_share(&mem_trans, NULL, rxtx->rx, blen, flen,
 			   global_handle);
+
+	virt_unset_guest();
 
 out:
 	cpu_spin_unlock(&rxtx->spinlock);
@@ -1032,10 +1127,9 @@ static struct mem_frag_state *get_frag_state(uint64_t global_handle)
 static void handle_mem_frag_tx(struct thread_smc_args *args,
 			       struct ffa_rxtx *rxtx)
 {
-	int rc = 0;
-	uint64_t global_handle = reg_pair_to_64(READ_ONCE(args->a2),
-						READ_ONCE(args->a1));
-	size_t flen = READ_ONCE(args->a3);
+	uint64_t global_handle = reg_pair_to_64(args->a2, args->a1);
+	size_t flen = args->a3;
+	uint32_t endpoint_id = args->a4;
 	struct mem_frag_state *s = NULL;
 	tee_mm_entry_t *mm = NULL;
 	unsigned int page_count = 0;
@@ -1044,6 +1138,16 @@ static void handle_mem_frag_tx(struct thread_smc_args *args,
 	uint32_t ret_w2 = 0;
 	uint32_t ret_w3 = 0;
 	uint32_t ret_fid = 0;
+	int rc = 0;
+
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+		uint16_t guest_id = endpoint_id >> 16;
+
+		if (!guest_id || virt_set_guest(guest_id)) {
+			rc = FFA_INVALID_PARAMETERS;
+			goto out_set_rc;
+		}
+	}
 
 	/*
 	 * Currently we're only doing this for fragmented FFA_MEM_SHARE_*
@@ -1076,6 +1180,7 @@ static void handle_mem_frag_tx(struct thread_smc_args *args,
 
 	rc = add_mem_share_frag(s, buf, flen);
 out:
+	virt_unset_guest();
 	cpu_spin_unlock(&rxtx->spinlock);
 
 	if (rc <= 0 && mm) {
@@ -1083,6 +1188,7 @@ out:
 		tee_mm_free(mm);
 	}
 
+out_set_rc:
 	if (rc < 0) {
 		ret_fid = FFA_ERROR;
 		ret_w2 = rc;
@@ -1108,6 +1214,19 @@ static void handle_mem_reclaim(struct thread_smc_args *args)
 		goto out;
 
 	cookie = reg_pair_to_64(args->a2, args->a1);
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+		uint16_t guest_id = 0;
+
+		if (cookie & FFA_MEMORY_HANDLE_HYPERVISOR_BIT) {
+			guest_id = virt_find_guest_by_cookie(cookie);
+		} else {
+			guest_id = (cookie >> FFA_MEMORY_HANDLE_PRTN_SHIFT) &
+				   FFA_MEMORY_HANDLE_PRTN_MASK;
+		}
+		if (!guest_id || virt_set_guest(guest_id))
+			goto out;
+	}
+
 	switch (mobj_ffa_sel1_spmc_reclaim(cookie)) {
 	case TEE_SUCCESS:
 		ret_fid = FFA_SUCCESS_32;
@@ -1122,6 +1241,9 @@ static void handle_mem_reclaim(struct thread_smc_args *args)
 		ret_val = FFA_DENIED;
 		break;
 	}
+
+	virt_unset_guest();
+
 out:
 	spmc_set_args(args, ret_fid, ret_val, 0, 0, 0, 0);
 }
@@ -1224,6 +1346,7 @@ static TEE_Result yielding_call_with_arg(uint64_t cookie, uint32_t offset)
 	if (!thr->rpc_arg)
 		goto out_dec_map;
 
+	virt_on_stdcall();
 	res = tee_entry_std(arg, num_params);
 
 	thread_rpc_shm_cache_clear(&thr->shm_cache);
@@ -1782,7 +1905,7 @@ static TEE_Result spmc_init(void)
  * to initialize via boot_final() to make sure we have a value assigned
  * before it's used the first time.
  */
-#ifdef CFG_VIRTUALIZATION
+#ifdef CFG_NS_VIRTUALIZATION
 boot_final(spmc_init);
 #else
 service_init(spmc_init);

core/arch/arm/mm/mobj_ffa.c
@@ -5,11 +5,13 @@
 
 #include <assert.h>
 #include <bitstring.h>
+#include <config.h>
 #include <ffa.h>
 #include <initcall.h>
 #include <kernel/refcount.h>
 #include <kernel/spinlock.h>
 #include <kernel/thread_spmc.h>
+#include <kernel/virtualization.h>
 #include <mm/mobj.h>
 #include <sys/queue.h>
 
@@ -83,26 +85,44 @@ static struct mobj_ffa *ffa_new(unsigned int num_pages)
 }
 
 #ifdef CFG_CORE_SEL1_SPMC
-struct mobj_ffa *mobj_ffa_sel1_spmc_new(unsigned int num_pages)
+struct mobj_ffa *mobj_ffa_sel1_spmc_new(uint64_t cookie,
+					unsigned int num_pages)
 {
 	struct mobj_ffa *mf = NULL;
 	uint32_t exceptions = 0;
 	int i = 0;
 
+	if (cookie != OPTEE_MSG_FMEM_INVALID_GLOBAL_ID) {
+		if (!(cookie & FFA_MEMORY_HANDLE_HYPERVISOR_BIT))
+			return NULL;
+		if (virt_add_cookie_to_current_guest(cookie))
+			return NULL;
+	}
+
 	mf = ffa_new(num_pages);
-	if (!mf)
+	if (!mf) {
+		if (cookie != OPTEE_MSG_FMEM_INVALID_GLOBAL_ID)
+			virt_remove_cookie(cookie);
 		return NULL;
+	}
+
+	if (cookie != OPTEE_MSG_FMEM_INVALID_GLOBAL_ID) {
+		mf->cookie = cookie;
+		return mf;
+	}
 
 	exceptions = cpu_spin_lock_xsave(&shm_lock);
 	bit_ffc(shm_bits, NUM_SHMS, &i);
 	if (i != -1) {
 		bit_set(shm_bits, i);
+		mf->cookie = i;
+		mf->cookie |= FFA_MEMORY_HANDLE_NON_SECURE_BIT;
 		/*
-		 * Setting bit 44 to use one of the upper 32 bits too for
-		 * testing.
+		 * Encode the partition ID into the handle so we know which
+		 * partition to switch to when reclaiming a handle.
 		 */
-		mf->cookie = i | FFA_MEMORY_HANDLE_NONE_SECURE_BIT;
-
+		mf->cookie |= SHIFT_U64(virt_get_current_guest_id(),
+					FFA_MEMORY_HANDLE_PRTN_SHIFT);
 	}
 	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
 
@@ -175,17 +195,26 @@ static struct mobj_ffa *find_in_list(struct mobj_ffa_head *head,
 #if defined(CFG_CORE_SEL1_SPMC)
 void mobj_ffa_sel1_spmc_delete(struct mobj_ffa *mf)
 {
-	int i = mf->cookie & ~BIT64(44);
-	uint32_t exceptions = 0;
 
-	assert(i >= 0 && i < NUM_SHMS);
+	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION) ||
+	    !(mf->cookie & FFA_MEMORY_HANDLE_HYPERVISOR_BIT)) {
+		uint64_t mask = FFA_MEMORY_HANDLE_NON_SECURE_BIT;
+		uint32_t exceptions = 0;
+		int64_t i = 0;
 
-	exceptions = cpu_spin_lock_xsave(&shm_lock);
-	assert(bit_test(shm_bits, i));
-	bit_clear(shm_bits, i);
+		if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
+			mask |= SHIFT_U64(FFA_MEMORY_HANDLE_PRTN_MASK,
+					  FFA_MEMORY_HANDLE_PRTN_SHIFT);
+		i = mf->cookie & ~mask;
+		assert(i >= 0 && i < NUM_SHMS);
+
+		exceptions = cpu_spin_lock_xsave(&shm_lock);
+		assert(bit_test(shm_bits, i));
+		bit_clear(shm_bits, i);
+		cpu_spin_unlock_xrestore(&shm_lock, exceptions);
+	}
+
 	assert(!mf->mm);
-	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
-
 	free(mf);
 }
 #else /* !defined(CFG_CORE_SEL1_SPMC) */
@@ -295,8 +324,10 @@ TEE_Result mobj_ffa_sel1_spmc_reclaim(uint64_t cookie)
 	res = TEE_SUCCESS;
 out:
 	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
-	if (!res)
+	if (!res) {
 		mobj_ffa_sel1_spmc_delete(mf);
+		virt_remove_cookie(cookie);
+	}
 	return res;
 }
 #endif /*CFG_CORE_SEL1_SPMC*/

core/include/kernel/virtualization.h
@@ -121,4 +121,18 @@ static inline void virt_init_memory(struct tee_mmap_region *memory_map __unused,
 static inline uint16_t virt_get_current_guest_id(void) { return 0; }
 #endif /*CFG_NS_VIRTUALIZATION*/
 
+#if defined(CFG_CORE_SEL1_SPMC) && defined(CFG_NS_VIRTUALIZATION)
+TEE_Result virt_add_cookie_to_current_guest(uint64_t cookie);
+void virt_remove_cookie(uint64_t cookie);
+uint16_t virt_find_guest_by_cookie(uint64_t cookie);
+
+#else
+static inline TEE_Result
+virt_add_cookie_to_current_guest(uint64_t cookie __unused)
+{ return TEE_ERROR_NOT_SUPPORTED; }
+static inline void virt_remove_cookie(uint64_t cookie __unused) { }
+static inline uint16_t virt_find_guest_by_cookie(uint64_t cookie __unused)
+{ return 0; }
+#endif
+
 #endif	/* KERNEL_VIRTUALIZATION_H */

core/include/mm/mobj.h
@@ -240,7 +240,8 @@ TEE_Result mobj_ffa_unregister_by_cookie(uint64_t cookie);
 
 /* Functions for SPMC */
 #ifdef CFG_CORE_SEL1_SPMC
-struct mobj_ffa *mobj_ffa_sel1_spmc_new(unsigned int num_pages);
+struct mobj_ffa *mobj_ffa_sel1_spmc_new(uint64_t cookie,
+					unsigned int num_pages);
 void mobj_ffa_sel1_spmc_delete(struct mobj_ffa *mobj);
 TEE_Result mobj_ffa_sel1_spmc_reclaim(uint64_t cookie);
 #else

commit a7ae7285c834a62c8bfb867d384667c0645a4bcb

core: arm: fix lock in virt_add_cookie_to_current_guest()
Prior to this patch was virt_add_cookie_to_current_guest() only masking
interrupts while adding a shared memory cookie to the list of cookies.
Proper locking is needed to serialize access to the cookie list, replace
the interrupt masking with a cpu_spin_lock_xsave().

Fixes: a65dd3a6b64d ("core: spmc: support virtualization with SPMC at S-EL1")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/virtualization.c
@@ -467,8 +467,9 @@ TEE_Result virt_add_cookie_to_current_guest(uint64_t cookie)
 {
 	TEE_Result res = TEE_ERROR_ACCESS_DENIED;
 	struct guest_partition *prtn = NULL;
-	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+	uint32_t exceptions = 0;
 
+	exceptions = cpu_spin_lock_xsave(&prtn_list_lock);
 	if (find_prtn_cookie(cookie, NULL))
 		goto out;
 
@@ -479,37 +480,39 @@ TEE_Result virt_add_cookie_to_current_guest(uint64_t cookie)
 		res = TEE_SUCCESS;
 	}
 out:
-	thread_unmask_exceptions(exceptions);
+	cpu_spin_unlock_xrestore(&prtn_list_lock, exceptions);
 
 	return res;
 }
 
 void virt_remove_cookie(uint64_t cookie)
 {
-	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
 	struct guest_partition *prtn = NULL;
+	uint32_t exceptions = 0;
 	int i = 0;
 
+	exceptions = cpu_spin_lock_xsave(&prtn_list_lock);
 	prtn = find_prtn_cookie(cookie, &i);
 	if (prtn) {
 		memmove(prtn->cookies + i, prtn->cookies + i + 1,
 			sizeof(uint64_t) * (prtn->cookie_count - i - 1));
 		prtn->cookie_count--;
 	}
-	thread_unmask_exceptions(exceptions);
+	cpu_spin_unlock_xrestore(&prtn_list_lock, exceptions);
 }
 
 uint16_t virt_find_guest_by_cookie(uint64_t cookie)
 {
-	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
 	struct guest_partition *prtn = NULL;
+	uint32_t exceptions = 0;
 	uint16_t ret = 0;
 
+	exceptions = cpu_spin_lock_xsave(&prtn_list_lock);
 	prtn = find_prtn_cookie(cookie, NULL);
 	if (prtn)
 		ret = prtn->id;
 
-	thread_unmask_exceptions(exceptions);
+	cpu_spin_unlock_xrestore(&prtn_list_lock, exceptions);
 
 	return ret;
 }