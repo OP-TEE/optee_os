ac5330fb020073ea8e4eacc8be4175326f244f4d
Jerome Forissier <jerome.forissier@linaro.org>
2024-02-22 13:27:09 +0100

    libunw: arm: unwind_arm32.c should be compiled only for Arm
    unwind_arm32.c contains functions to unwind arm32 code. This is used
    when libunw is built for arm32 or arm64, in other words when ARCH=arm.
    Other architectures (such as ARCH=riscv) obviously don't need it.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libunw/sub.mk
+++ lib/libunw/sub.mk
@@ -1,6 +1,8 @@
 global-incdirs-y += include
 ifeq ($(CFG_UNWIND),y)
+ifeq (arm,$(ARCH))
 srcs-y += unwind_arm32.c
+endif
 srcs-$(CFG_ARM64_$(sm)) += unwind_arm64.c
 ifneq (,$(filter y,$(CFG_RV32_$(sm)) $(CFG_RV64_$(sm))))
 srcs-y += unwind_riscv.c
