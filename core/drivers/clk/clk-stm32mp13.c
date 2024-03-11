OP-TEE/optee_os

commit 95f2142bf848294a04ba34d68dc86523dd322c1f

drivers: clk: clk-stm32mp13: don't gate/ungate oscillators not wired
If an oscillator is not wired we shouldn't gate it to avoid a panic.
For example the external LSE oscillator may not be supported on a board
in which case node named clk-lse is disabled in the board DTS file.

Reviewed-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>

core/drivers/clk/clk-stm32mp13.c
@@ -1486,8 +1486,14 @@ static int stm32_clk_parse_fdt_all_oscillator(const void *fdt,
 
 		fdt_err = clk_stm32_parse_oscillator_fdt(fdt, osc_node,
 							 osc_data->name, osci);
-		if (fdt_err < 0)
-			panic();
+		if (fdt_err) {
+			if (fdt_err == -FDT_ERR_NOTFOUND) {
+				/* Oscillator not found means it is not wired */
+				osci->freq = 0;
+			} else {
+				panic();
+			}
+		}
 	}
 
 	return 0;
@@ -1970,11 +1976,47 @@ const struct clk_ops ck_timer_ops = {
 	}
 
 /* Oscillator clocks */
-static STM32_GATE_READY(ck_hsi, NULL, 0, GATE_HSI);
-static STM32_GATE_READY(ck_hse, NULL, 0, GATE_HSE);
-static STM32_GATE_READY(ck_csi, NULL, 0, GATE_CSI);
-static STM32_GATE_READY(ck_lsi, NULL, 0, GATE_LSI);
-static STM32_GATE_READY(ck_lse, NULL, 0, GATE_LSE);
+
+static TEE_Result clk_stm32_oscillator_enable(struct clk *clk)
+{
+	struct clk_stm32_gate_cfg *cfg = clk->priv;
+
+	if (clk->rate == 0U)
+		return TEE_SUCCESS;
+
+	return stm32_gate_rdy_enable(cfg->gate_id);
+}
+
+static void clk_stm32_oscillator_disable(struct clk *clk)
+{
+	struct clk_stm32_gate_cfg *cfg = clk->priv;
+
+	if (clk->rate == 0U)
+		return;
+
+	if (stm32_gate_rdy_disable(cfg->gate_id))
+		panic();
+}
+
+static const struct clk_ops clk_stm32_oscillator_ops = {
+	.enable		= clk_stm32_oscillator_enable,
+	.disable	= clk_stm32_oscillator_disable,
+};
+
+#define STM32_OSCILLATOR(_name, _gate_id)\
+	struct clk _name = {\
+		.ops = &clk_stm32_oscillator_ops,\
+		.priv = &(struct clk_stm32_gate_cfg) {\
+			.gate_id = (_gate_id),\
+		},\
+		.name = #_name,\
+	}
+
+static STM32_OSCILLATOR(ck_hsi, GATE_HSI);
+static STM32_OSCILLATOR(ck_hse, GATE_HSE);
+static STM32_OSCILLATOR(ck_csi, GATE_CSI);
+static STM32_OSCILLATOR(ck_lsi, GATE_LSI);
+static STM32_OSCILLATOR(ck_lse, GATE_LSE);
 
 static STM32_FIXED_FACTOR(ck_i2sckin, NULL, 0, 1, 1);
 static STM32_FIXED_FACTOR(ck_hse_div2, &ck_hse, 0, 1, 2);

commit b2851c7b8a5e267b0546cbe63df64258c87bc922

drivers: clk: clk-stm32mp13: fix memory corruption on oscillator parent
Fix oscillators struct clk instances for STM32MP13 clock driver. These
clocks have 1 parent that is set during driver initialization, based on
device tree content, whereas referred bugged commit defined 0 parents
and did not allocate memory for the parent reference.

Fixes: 95f2142bf848 ("drivers: clk: clk-stm32mp13: don't gate/ungate oscillators not wired")
Tested-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
Acked-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/clk/clk-stm32mp13.c
@@ -2026,6 +2026,10 @@ static const struct clk_ops clk_stm32_oscillator_ops = {
 	.disable	= clk_stm32_oscillator_disable,
 };
 
+/*
+ * Each oscillator has 1 parent which reference is NULL here
+ * but set during initialization.
+ */
 #define STM32_OSCILLATOR(_name, _gate_id)\
 	struct clk _name = {\
 		.ops = &clk_stm32_oscillator_ops,\
@@ -2033,6 +2037,8 @@ static const struct clk_ops clk_stm32_oscillator_ops = {
 			.gate_id = (_gate_id),\
 		},\
 		.name = #_name,\
+		.num_parents = 1, \
+		.parents = { NULL }, \
 	}
 
 static STM32_OSCILLATOR(ck_hsi, GATE_HSI);