f833111f28d40fba31a5038455377a3ddfd1a21a
Tony Han <tony.han@microchip.com>
2024-03-20 16:49:29 +0800

    plat-sam: rename filename for sama5d2 functions to 'platform_sama5d2.c'
    Rename 'main.c' to 'platform_sama5d2.c' in 'core/arch/arm/plat-sam'.
    Update the makefile accordingly.
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- core/arch/arm/plat-sam/sub.mk
+++ core/arch/arm/plat-sam/sub.mk
@@ -1,5 +1,6 @@
 global-incdirs-y += .
-srcs-y += main.c freq.c sam_sfr.c
+srcs-y += freq.c sam_sfr.c
+srcs-$(CFG_SAMA5D2) += platform_sama5d2.c
 srcs-$(CFG_AT91_MATRIX) += matrix.c
 srcs-$(CFG_PL310) += sam_pl310.c
 srcs-$(CFG_SCMI_MSG_DRIVERS) += scmi_server.c
