778a36bf91d2271146e6ed57c0cfe925872b2ab6
Gatien Chevallier <gatien.chevallier@foss.st.com>
2024-03-27 16:33:45 +0100

    drivers: add FMC driver with RIF support
    This driver implements RIF configuration for FMC, which is a RIF aware IP.
    It means that the FMC driver is in charge of configuring its own RIF
    restrictions and that the FMC has dedicated RIF configuration registers.
    
    Additional check on RIF configuration is added for this IP when debug is
    on.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -36,6 +36,7 @@ srcs-$(CFG_AMLOGIC_UART) += amlogic_uart.c
 srcs-$(CFG_MVEBU_UART) += mvebu_uart.c
 srcs-$(CFG_STM32_BSEC) += stm32_bsec.c
 srcs-$(CFG_STM32_ETZPC) += stm32_etzpc.c
+srcs-$(CFG_STM32_FMC) += stm32_fmc.c
 srcs-$(CFG_STM32_GPIO) += stm32_gpio.c
 srcs-$(CFG_STM32_IWDG) += stm32_iwdg.c
 srcs-$(CFG_STM32_I2C) += stm32_i2c.c
