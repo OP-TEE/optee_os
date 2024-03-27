OP-TEE/optee_os

commit 1506f47af9175e18e93476596074a267c2340d5a

drivers: firewall: add stm32_rif driver for common RIF features
The resource isolation framework (RIF) is a comprehensive set of
hardware blocks designed to enforce and manage the isolation of
STM32MP25xx hardware resources, like memories and peripherals.

The RIF manages security and privilege levels as well as compartment
filtering. Each compartment is identified by a Compartment ID (CID).

Therefore, the access filtering can be, depending on the case:
• restricted to none, one or more than one CID
• secure-only, non-secure only, or both
• privileged-only or privileged/unprivileged
• read-only, write-only, or read/write

Add a firewall driver folder that contains firewall drivers.
This RIF driver contains generic features shared between all drivers
managing RIF configuration.

Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/drivers/stm32_rif.h
@@ -1,0 +1,206 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2022-2024, STMicroelectronics
+ */
+
+#ifndef __DRIVERS_STM32_RIF_H
+#define __DRIVERS_STM32_RIF_H
+
+#include <dt-bindings/firewall/stm32mp25-rif.h>
+#include <tee_api_types.h>
+#include <types_ext.h>
+#include <util.h>
+
+/*
+ * CIDCFGR register
+ */
+#define _CIDCFGR_CFEN			BIT(0)
+#define _CIDCFGR_SEMEN			BIT(1)
+#define _CIDCFGR_SEMWL(x)		BIT(SEMWL_SHIFT + (x))
+
+/*
+ * SEMCR register
+ */
+#define _SEMCR_MUTEX			BIT(0)
+#define _SEMCR_SEMCID_SHIFT		U(4)
+#define _SEMCR_SEMCID_MASK		GENMASK_32(6, 4)
+
+/*
+ * Miscellaneous
+ */
+#define MAX_CID_SUPPORTED		U(8)
+
+#define SCID_SHIFT			U(4)
+#define SEMWL_SHIFT			U(16)
+#define RIF_ID_SHIFT			U(24)
+
+#define RIF_ID_MASK			GENMASK_32(31, 24)
+#define RIF_CHANNEL_ID(x)		((RIF_ID_MASK & (x)) >> RIF_ID_SHIFT)
+
+#define RIFPROT_SEC			BIT(8)
+#define RIFPROT_PRIV			BIT(9)
+#define RIFPROT_LOCK			BIT(10)
+
+/**
+ * struct rif_conf_data - Structure containing RIF configuration data
+ *
+ * @access_mask: Array of the masks of the registers which will be configured.
+ * @sec_conf: Secure configuration registers.
+ * @priv_conf: Privilege configuration registers.
+ * @cid_confs: CID filtering configuration register value for a peripheral
+ *             resource (e.g: GPIO pins, FMC controllers)
+ * @lock_conf: RIF configuration locking registers
+ *
+ * For a hardware block having 56 channels, there will be 56 cid_confs
+ * registers and 2 sec_conf and priv_conf registers
+ */
+struct rif_conf_data {
+	uint32_t *access_mask;
+	uint32_t *sec_conf;
+	uint32_t *priv_conf;
+	uint32_t *cid_confs;
+	uint32_t *lock_conf;
+};
+
+#ifdef CFG_STM32_RIF
+/**
+ * stm32_rif_scid_ok() - Check if a given static CID configuration authorizes
+ *			 access to a given CID
+ *
+ * @cidcfgr: Value of the CIDCFGR register
+ * @scid_m: Mask of the static CID in the register
+ * @cid_to_check: CID of the target compartment
+ *
+ * Returns true if given CID is authorized, false otherwise.
+ */
+static inline bool stm32_rif_scid_ok(uint32_t cidcfgr, uint32_t scid_m,
+				     uint32_t cid_to_check)
+{
+	return (cidcfgr & scid_m) == SHIFT_U32(cid_to_check, SCID_SHIFT) &&
+	       !(cidcfgr & _CIDCFGR_SEMEN);
+}
+
+/**
+ * stm32_rif_semaphore_enabled_and_ok() - Check if semaphore mode is enabled and
+ *					  that a given CID can request the
+ *					  semaphore ownership
+ *
+ * @cidcfgr: Value of the cidcfgr register
+ * @cid_bf_to_check: Bitfield of CIDs to check
+ *
+ * Returns true if all requested CIDs can request the semaphore ownership,
+ * false otherwise.
+ */
+static inline bool stm32_rif_semaphore_enabled_and_ok(uint32_t cidcfgr,
+						      uint32_t cid_bf_to_check)
+{
+	return (cidcfgr & _CIDCFGR_CFEN) && (cidcfgr & _CIDCFGR_SEMEN) &&
+	       (cidcfgr & _CIDCFGR_SEMWL(cid_bf_to_check));
+}
+
+/**
+ * stm32_rif_check_access() - Test peripheral access for a given compartment
+ *
+ * @cidcfgr: CIDCFGR configuration register value
+ * @semcr: SEMCR register value
+ * @nb_cid_supp: Number of supported CID for the peripheral
+ * @cid_to_check: CID of the target compartment
+ *
+ * Returns TEE_SUCCESS if access is authorized, a TEE_Result error value
+ * otherwise.
+ */
+TEE_Result stm32_rif_check_access(uint32_t cidcfgr,
+				  uint32_t semcr,
+				  unsigned int nb_cid_supp,
+				  unsigned int cid_to_check);
+
+/**
+ * stm32_rif_parse_cfg() - Parse RIF config from Device Tree extracted
+ *			   information
+ *
+ * @rif_conf: Configuration read in the device tree
+ * @conf_data: Buffer containing the RIF configuration to apply for a peripheral
+ * @nb_cid_supp: Number of supported CID for the peripheral
+ * @nb_channel: Number of channels for the peripheral
+ */
+void stm32_rif_parse_cfg(uint32_t rif_conf,
+			 struct rif_conf_data *conf_data,
+			 unsigned int nb_cid_supp,
+			 unsigned int nb_channel);
+
+/**
+ * stm32_rif_semaphore_is_available() - Checks if the _SEMCR_MUTEX bit is set
+ *
+ * @addr: Address of the register to read from
+ */
+bool stm32_rif_semaphore_is_available(vaddr_t addr);
+
+/**
+ * stm32_rif_semaphore_is_available() - Acquires the semaphore by setting the
+ *					_SEMCR_MUTEX bit
+ *
+ * @addr: Address of the register to write to
+ * @nb_cid_supp: Number of CID supported
+ */
+TEE_Result stm32_rif_acquire_semaphore(vaddr_t addr,
+				       unsigned int nb_cid_supp);
+
+/**
+ * stm32_rif_semaphore_is_available() - Releases the semaphore by clearing the
+ *					_SEMCR_MUTEX bit
+ *
+ * @addr: Address of the register to write to
+ * @nb_cid_supp: Number of CID supported
+ */
+TEE_Result stm32_rif_release_semaphore(vaddr_t addr,
+				       unsigned int nb_cid_supp);
+#else
+static inline bool stm32_rif_scid_ok(uint32_t cidcfgr, uint32_t scid_m,
+				     uint32_t cid_to_check)
+{
+	return true;
+}
+
+static inline bool stm32_rif_semaphore_enabled_and_ok(uint32_t cidcfgr,
+						      uint32_t cid_bf_to_check)
+{
+	return true;
+}
+
+static inline TEE_Result
+stm32_rif_check_access(uint32_t cidcfgr __unused,
+		       uint32_t semcr __unused,
+		       unsigned int nb_cid_supp __unused,
+		       unsigned int cid_to_check __unused)
+{
+	return TEE_SUCCESS;
+}
+
+static inline void
+stm32_rif_parse_cfg(uint32_t rif_conf __unused,
+		    struct rif_conf_data *conf_data __unused,
+		    unsigned int nb_cid_supp __unused,
+		    unsigned int nb_channel __unused)
+{
+}
+
+static inline bool stm32_rif_semaphore_is_available(vaddr_t addr __unused)
+{
+	return true;
+}
+
+static inline TEE_Result
+stm32_rif_acquire_semaphore(vaddr_t addr __unused,
+			    unsigned int nb_cid_supp __unused)
+{
+	return TEE_SUCCESS;
+}
+
+static inline TEE_Result
+stm32_rif_release_semaphore(vaddr_t addr __unused,
+			    unsigned int nb_cid_supp __unused)
+{
+	return TEE_SUCCESS;
+}
+#endif /* CFG_STM32_RIF */
+#endif /* __DRIVERS_STM32_RIF_H */

core/drivers/firewall/stm32_rif.c
@@ -1,0 +1,138 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2021-2024, STMicroelectronics
+ */
+
+#include <assert.h>
+#include <drivers/stm32_rif.h>
+#include <drivers/stm32mp_dt_bindings.h>
+#include <io.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+
+#define MAX_CID_BITFIELD	U(3)
+
+/**
+ * get_scid_mask() - Get the static CID mask according to the number of
+ *		     supported CIDs
+ *
+ * @nb_cid_supp: Number of CIDs supported. Cannot be 0.
+ */
+static uint32_t get_scid_mask(unsigned int nb_cid_supp)
+{
+	uint32_t msb_nb_cid_supp = 0;
+
+	assert(nb_cid_supp);
+
+	msb_nb_cid_supp = sizeof(nb_cid_supp) * 8 -
+			  __builtin_clz((nb_cid_supp - 1) | 1);
+
+	/* SCID bitfield highend can't be > SCID_SHIFT + MAX_CID_BITFIELD */
+	assert(msb_nb_cid_supp <= MAX_CID_BITFIELD);
+
+	return GENMASK_32(SCID_SHIFT + msb_nb_cid_supp - 1, SCID_SHIFT);
+}
+
+TEE_Result stm32_rif_check_access(uint32_t cidcfgr,
+				  uint32_t semcr,
+				  unsigned int nb_cid_supp,
+				  unsigned int cid_to_check)
+{
+	uint32_t scid_mask = get_scid_mask(nb_cid_supp);
+
+	if (!(cidcfgr & _CIDCFGR_CFEN))
+		return TEE_SUCCESS;
+
+	if (stm32_rif_scid_ok(cidcfgr, scid_mask, cid_to_check))
+		return TEE_SUCCESS;
+
+	if (stm32_rif_semaphore_enabled_and_ok(cidcfgr, cid_to_check)) {
+		if (!(semcr & _SEMCR_MUTEX) ||
+		    ((semcr & scid_mask) >> SCID_SHIFT) == cid_to_check) {
+			return TEE_SUCCESS;
+		}
+	}
+
+	return TEE_ERROR_ACCESS_DENIED;
+}
+
+void stm32_rif_parse_cfg(uint32_t rif_conf,
+			 struct rif_conf_data *conf_data,
+			 unsigned int nb_cid_supp,
+			 unsigned int nb_channel)
+{
+	uint32_t scid_mask = get_scid_mask(nb_cid_supp);
+	uint32_t cidcfdg_conf_mask = 0;
+	uint32_t channel_id = 0;
+	uint32_t semwl_mask = 0;
+	unsigned int conf_index = 0;
+
+	semwl_mask = GENMASK_32(SEMWL_SHIFT + nb_cid_supp - 1, SEMWL_SHIFT);
+
+	cidcfdg_conf_mask = scid_mask | semwl_mask | _CIDCFGR_CFEN |
+			    _CIDCFGR_SEMEN;
+
+	/* Shift corresponding to the desired resources */
+	channel_id = RIF_CHANNEL_ID(rif_conf);
+	if (channel_id >= nb_channel)
+		panic("Bad RIF controllers number");
+
+	/* Some peripherals have more than 32 RIF channels */
+	conf_index = channel_id / 32;
+
+	/* Privilege configuration */
+	if (rif_conf & RIFPROT_PRIV)
+		conf_data->priv_conf[conf_index] |= BIT(channel_id);
+
+	/* Security RIF configuration */
+	if (rif_conf & RIFPROT_SEC)
+		conf_data->sec_conf[conf_index] |= BIT(channel_id);
+
+	/* RIF configuration lock */
+	if (rif_conf & RIFPROT_LOCK && conf_data->lock_conf)
+		conf_data->lock_conf[conf_index] |= BIT(channel_id);
+
+	/* CID configuration */
+	conf_data->cid_confs[channel_id] = rif_conf & cidcfdg_conf_mask;
+
+	/* Store that this RIF resource is to be configured */
+	conf_data->access_mask[conf_index] |= BIT(channel_id);
+}
+
+bool stm32_rif_semaphore_is_available(vaddr_t addr)
+{
+	return !(io_read32(addr) & _SEMCR_MUTEX);
+}
+
+TEE_Result stm32_rif_acquire_semaphore(vaddr_t addr, unsigned int nb_cid_supp)
+{
+	uint32_t scid_mask = get_scid_mask(nb_cid_supp);
+
+	/* Take the semaphore */
+	io_setbits32(addr, _SEMCR_MUTEX);
+
+	/* Check that the Cortex-A has the semaphore */
+	if (stm32_rif_semaphore_is_available(addr) ||
+	    ((io_read32(addr) & scid_mask) >> SCID_SHIFT) != RIF_CID1)
+		return TEE_ERROR_ACCESS_DENIED;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result stm32_rif_release_semaphore(vaddr_t addr, unsigned int nb_cid_supp)
+{
+	uint32_t scid_mask = get_scid_mask(nb_cid_supp);
+
+	if (stm32_rif_semaphore_is_available(addr))
+		return TEE_SUCCESS;
+
+	/* Release the semaphore */
+	io_clrbits32(addr, _SEMCR_MUTEX);
+
+	/* Check that current compartment no more owns the semaphore */
+	if (!stm32_rif_semaphore_is_available(addr) &&
+	    ((io_read32(addr) & scid_mask) >> SCID_SHIFT) == RIF_CID1)
+		return TEE_ERROR_ACCESS_DENIED;
+
+	return TEE_SUCCESS;
+}

core/drivers/firewall/sub.mk
@@ -1,0 +1,1 @@
+srcs-$(CFG_STM32_RIF) += stm32_rif.c

core/drivers/sub.mk
@@ -95,3 +95,4 @@ subdirs-y += imx
 subdirs-y += pm
 subdirs-y += wdt
 subdirs-y += rtc
+subdirs-y += firewall

commit d5bdb661c8be9fb4954950451445caaa85982892

drivers: firewall: fix stm32_rif_semaphore_enabled_and_ok() prototype
The stm32_rif_semaphore_enabled_and_ok() function checks a single CID
at a time, not a bit field.

Fixes: 1506f47af917 ("drivers: firewall: add stm32_rif driver for common RIF features")
Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/drivers/stm32_rif.h
@@ -86,16 +86,16 @@ static inline bool stm32_rif_scid_ok(uint32_t cidcfgr, uint32_t scid_m,
  *					  semaphore ownership
  *
  * @cidcfgr: Value of the cidcfgr register
- * @cid_bf_to_check: Bitfield of CIDs to check
+ * @cid_to_check: CID to check
  *
- * Returns true if all requested CIDs can request the semaphore ownership,
+ * Returns true if the requested CID can request the semaphore ownership,
  * false otherwise.
  */
 static inline bool stm32_rif_semaphore_enabled_and_ok(uint32_t cidcfgr,
-						      uint32_t cid_bf_to_check)
+						      uint32_t cid_to_check)
 {
 	return (cidcfgr & _CIDCFGR_CFEN) && (cidcfgr & _CIDCFGR_SEMEN) &&
-	       (cidcfgr & _CIDCFGR_SEMWL(cid_bf_to_check));
+	       (cidcfgr & _CIDCFGR_SEMWL(cid_to_check));
 }
 
 /**
@@ -172,7 +172,7 @@ static inline bool stm32_rif_scid_ok(uint32_t cidcfgr, uint32_t scid_m,
 }
 
 static inline bool stm32_rif_semaphore_enabled_and_ok(uint32_t cidcfgr,
-						      uint32_t cid_bf_to_check)
+						      uint32_t cid_to_check)
 {
 	return true;
 }