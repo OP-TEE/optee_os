a4365e096ba7e6bb81b3b56b73696fd6c64247f7
Tony Han <tony.han@microchip.com>
2024-03-28 12:36:48 +0800

    drivers: pm: sam: add code for the phy of sama7g5 dram controller
    Add and initilate the variable for the start address of sama7g5
    dram controller phy.
    
    Signed-off-by: Tony Han <tony.han@microchip.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/drivers/pm/sam/pm-defines.c
+++ core/drivers/pm/sam/pm-defines.c
@@ -12,6 +12,8 @@ DEFINES
 {
 	DEFINE(PM_DATA_PMC,		offsetof(struct at91_pm_data, pmc));
 	DEFINE(PM_DATA_RAMC0,		offsetof(struct at91_pm_data, ramc));
+	DEFINE(PM_DATA_RAMC_PHY,	offsetof(struct at91_pm_data,
+						 ramc_phy));
 	DEFINE(PM_DATA_MODE,		offsetof(struct at91_pm_data, mode));
 	DEFINE(PM_DATA_SHDWC,		offsetof(struct at91_pm_data, shdwc));
 	DEFINE(PM_DATA_SFRBU,		offsetof(struct at91_pm_data, sfrbu));
