1dd1efa95c190ec521e94efcf3801dcdbdd5e34b
Jeremie Corbier <jeremie.corbier@provenrun.com>
2024-04-02 17:12:42 +0200

    fixup! core: pta: add Versal FPGA PTA
     - s!invokeCommandEntryPoint!invoke_command!
     - Return TEE_ERROR_NOT_SUPPORTED rather than BAD_PARAMS in case an
       invalid command ID is used
     - Re-order includes as requested
    
    Signed-off-by: Jeremie Corbier <jeremie.corbier@provenrun.com>

--- core/pta/versal/fpga_pta.c
+++ core/pta/versal/fpga_pta.c
@@ -2,13 +2,13 @@
 /*
  * Copyright 2023 ProvenRun SAS
  */
-#include <kernel/pseudo_ta.h>
 #include <drivers/versal_pm.h>
+#include <kernel/pseudo_ta.h>
 #include <mm/core_memprot.h>
-#include <tee/cache.h>
-#include <pta_versal_fpga.h>
 #include <platform_config.h>
+#include <pta_versal_fpga.h>
 #include <string.h>
+#include <tee/cache.h>
 
 #define FPGA_PTA_NAME "fpga.pta"
 
@@ -37,19 +37,19 @@ static TEE_Result pta_versal_fpga_write(uint32_t param_types,
 	return versal_write_fpga(virt_to_phys(buf));
 }
 
-static TEE_Result invokeCommandEntryPoint(void *sess_ctx __unused,
-					  uint32_t cmd_id,
-					  uint32_t param_types,
-					  TEE_Param params[TEE_NUM_PARAMS])
+static TEE_Result invoke_command(void *sess_ctx __unused,
+				 uint32_t cmd_id,
+				 uint32_t param_types,
+				 TEE_Param params[TEE_NUM_PARAMS])
 {
 	switch (cmd_id) {
 	case PTA_VERSAL_FPGA_WRITE:
 		return pta_versal_fpga_write(param_types, params);
 	default:
-		return TEE_ERROR_BAD_PARAMETERS;
+		return TEE_ERROR_NOT_SUPPORTED;
 	}
 }
 
 pseudo_ta_register(.uuid = PTA_VERSAL_FPGA_UUID, .name = FPGA_PTA_NAME,
 		   .flags = PTA_DEFAULT_FLAGS,
-		   .invoke_command_entry_point = invokeCommandEntryPoint);
+		   .invoke_command_entry_point = invoke_command);
