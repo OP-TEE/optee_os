OP-TEE/optee_os

commit af5b9881111c129396d68bdb54e1476e9f3f3d02

drivers: regulator: supported voltage consider levels bounds
Assert regulator_supported_voltages() provides a list that takes into
account the min/max voltage levels possibly set from the secure DT.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/regulator/regulator.c
@@ -223,14 +223,19 @@ TEE_Result regulator_supported_voltages(struct regulator *regulator,
 
 		res = regulator->ops->supported_voltages(regulator, desc,
 							 levels);
-		if (res == TEE_SUCCESS)
-			return TEE_SUCCESS;
 		if (res != TEE_ERROR_NOT_SUPPORTED)
 			return res;
+	} else {
+		*desc = &regulator->voltages_fallback.desc;
+		*levels = regulator->voltages_fallback.levels;
 	}
 
-	*desc = &regulator->voltages_fallback.desc;
-	*levels = regulator->voltages_fallback.levels;
+	assert(((*desc)->type == VOLTAGE_TYPE_FULL_LIST &&
+		(*levels)[0] >= regulator->min_uv && (*desc)->num_levels &&
+		(*levels)[(*desc)->num_levels - 1] <= regulator->max_uv) ||
+	       ((*desc)->type == VOLTAGE_TYPE_INCREMENT &&
+		(*levels)[0] >= regulator->min_uv &&
+		(*levels)[1] <= regulator->max_uv));
 
 	return TEE_SUCCESS;
 }

commit 6b0921ffe9fa9b393f4f95d36da26cac768c335a

drivers: regulator: fix error case in regulator_supported_voltages()
Fix case when .supported_voltages() handler is supported. If successful
it shall assert voltage description data. If it returns
TEE_ERROR_NOT_SUPPORTED, it shall use the pre-filled fallback description.

Fixes: af5b9881111c ("drivers: regulator: supported voltage consider levels bounds")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Acked-by: Patrick Delaunay <patrick.delaunay@foss.st.com>

core/drivers/regulator/regulator.c
@@ -216,18 +216,18 @@ TEE_Result regulator_supported_voltages(struct regulator *regulator,
 					struct regulator_voltages_desc **desc,
 					const int **levels)
 {
+	TEE_Result res = TEE_ERROR_NOT_SUPPORTED;
+
 	assert(regulator && desc && levels);
 
-	if (regulator->ops->supported_voltages) {
-		TEE_Result res = TEE_ERROR_GENERIC;
-
+	if (regulator->ops->supported_voltages)
 		res = regulator->ops->supported_voltages(regulator, desc,
 							 levels);
-		if (res != TEE_ERROR_NOT_SUPPORTED)
-			return res;
-	} else {
+	if (res == TEE_ERROR_NOT_SUPPORTED) {
 		*desc = &regulator->voltages_fallback.desc;
 		*levels = regulator->voltages_fallback.levels;
+	} else if (res) {
+		return res;
 	}
 
 	assert(((*desc)->type == VOLTAGE_TYPE_FULL_LIST &&