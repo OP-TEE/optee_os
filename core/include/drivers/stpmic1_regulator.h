OP-TEE/optee_os

commit 9cb0d51670f24088c4e9528d4c0a15fcb3c3d3d5

drivers: stpmic1: export regulators API in a specific header file
Split stpmic1.h in 2 parts, one specifically for STPMIC1 regulator
interface.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/include/drivers/stpmic1_regulator.h
@@ -1,0 +1,57 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021-2022, STMicroelectronics
+ */
+
+#ifndef __DRIVERS_STPMIC1_REGULATOR_H
+#define __DRIVERS_STPMIC1_REGULATOR_H
+
+#include <stdint.h>
+
+/*
+ * Return true if @name refers to a knwon regulator, return false otherwise
+ */
+bool stpmic1_regulator_is_valid(const char *name);
+
+/*
+ * Enable STPMIC1 regulator identified by @name.
+ * Return 0 on success and a non-0 value if failing
+ */
+int stpmic1_regulator_enable(const char *name);
+
+/*
+ * Disable STPMIC1 regulator identified by @name.
+ * Return 0 on success and a non-0 value if failing
+ */
+int stpmic1_regulator_disable(const char *name);
+
+/*
+ * Return true if regulator identified by @name is enabled and false otherwise.
+ * Return 0 on success and a non-0 value if failing
+ */
+bool stpmic1_is_regulator_enabled(const char *name);
+
+/*
+ * Retrieve regulator levels array (in millivolts) and/or levels count
+ * @name: regulator identifier
+ * @levels: output reference for an arrays of the supported levels, or NULL
+ * @levels_count: output reference for number of supported levels, or NULL
+ */
+void stpmic1_regulator_levels_mv(const char *name, const uint16_t **levels,
+				 size_t *levels_count);
+
+/*
+ * Set voltage level @millivolt for target regulator @name
+ * @name: regulator identifier
+ * @millivot: target voltage level, in mV
+ */
+int stpmic1_regulator_voltage_set(const char *name, uint16_t millivolts);
+
+/*
+ * Get current voltage level (in millivolt) for target regulator @name
+ * @name: regulator identifier
+ * Return a positive millivolt level on success or a negative value on error
+ */
+int stpmic1_regulator_voltage_get(const char *name);
+
+#endif /*__DRIVERS_STPMIC1_REGULATOR_H*/

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.c
@@ -6,6 +6,7 @@
 #include <drivers/stm32_i2c.h>
 #include <drivers/stm32mp1_pmic.h>
 #include <drivers/stpmic1.h>
+#include <drivers/stpmic1_regulator.h>
 #include <io.h>
 #include <keep.h>
 #include <kernel/delay.h>

core/arch/arm/plat-stm32mp1/scmi_server.c
@@ -13,6 +13,7 @@
 #include <drivers/stm32mp1_pmic.h>
 #include <drivers/stm32mp1_pwr.h>
 #include <drivers/stpmic1.h>
+#include <drivers/stpmic1_regulator.h>
 #include <dt-bindings/clock/stm32mp1-clks.h>
 #include <dt-bindings/regulator/st,stm32mp15-regulator.h>
 #include <dt-bindings/reset/stm32mp1-resets.h>

core/drivers/stpmic1.c
@@ -5,6 +5,7 @@
 
 #include <assert.h>
 #include <drivers/stpmic1.h>
+#include <drivers/stpmic1_regulator.h>
 #include <kernel/panic.h>
 #include <platform_config.h>
 #include <stdint.h>

core/include/drivers/stpmic1.h
@@ -173,19 +173,6 @@ int stpmic1_register_read(uint8_t register_id, uint8_t *value);
 int stpmic1_register_write(uint8_t register_id, uint8_t value);
 int stpmic1_register_update(uint8_t register_id, uint8_t value, uint8_t mask);
 
-/* API for gating of regulators driven from STPMIC1 device */
-bool stpmic1_regulator_is_valid(const char *name);
-int stpmic1_regulator_enable(const char *name);
-int stpmic1_regulator_disable(const char *name);
-bool stpmic1_is_regulator_enabled(const char *name);
-
-void stpmic1_regulator_levels_mv(const char *name,
-				 const uint16_t **levels,
-				 size_t *levels_count);
-
-/* API for voltage cnotrol of regulators driven from STPMIC1 device */
-int stpmic1_regulator_voltage_set(const char *name, uint16_t millivolts);
-int stpmic1_regulator_voltage_get(const char *name);
 int stpmic1_regulator_mask_reset_set(const char *name);
 
 /* API for low power configuration of regulators driven from STPMIC1 device */

commit 3910d23f7d861f66eb8407f285e0eb9f4f95ab8f

drivers: include standard header files from stpmic1_regulator.h
Add missing inclusion of stdbool.h and stddef.h in stpmic1_regulator.h.
The issue was revealed when upgrading to latest SCP-firmware source tree.

Fixes: 9cb0d51670f2 ("drivers: stpmic1: export regulators API in a specific header file")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/include/drivers/stpmic1_regulator.h
@@ -1,11 +1,13 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2021-2022, STMicroelectronics
+ * Copyright (c) 2021-2024, STMicroelectronics
  */
 
 #ifndef __DRIVERS_STPMIC1_REGULATOR_H
 #define __DRIVERS_STPMIC1_REGULATOR_H
 
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 /*