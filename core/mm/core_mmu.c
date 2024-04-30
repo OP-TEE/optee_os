OP-TEE/optee_os

commit 72a6827a635378fe41a06169a8161abdad018540

core: arm: SPMC obtain device memory info from DTB
When CFG_CORE_SEL2_SPMC = y, obtain device memory info from
the SP manifest DTB.

Signed-off-by: leisen <leisen1@huawei.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/mm/core_mmu.c
@@ -954,6 +954,48 @@ static void check_sec_nsec_mem_config(void)
 	}
 }
 
+static void collect_device_mem_ranges(struct tee_mmap_region *memory_map,
+				      size_t num_elems, size_t *last)
+{
+	const char *compatible = "arm,ffa-manifest-device-regions";
+	void *fdt = get_manifest_dt();
+	const char *name = NULL;
+	uint64_t page_count = 0;
+	uint64_t base = 0;
+	int subnode = 0;
+	int node = 0;
+
+	node = fdt_node_offset_by_compatible(fdt, 0, compatible);
+	if (node < 0)
+		return;
+
+	fdt_for_each_subnode(subnode, fdt, node) {
+		name = fdt_get_name(fdt, subnode, NULL);
+		if (!name)
+			continue;
+
+		if (dt_getprop_as_number(fdt, subnode, "base-address",
+					 &base)) {
+			EMSG("Mandatory field is missing: base-address");
+			continue;
+		}
+
+		if (base & SMALL_PAGE_MASK) {
+			EMSG("base-address is not page aligned");
+			continue;
+		}
+
+		if (dt_getprop_as_number(fdt, subnode, "pages-count",
+					 &page_count)) {
+			EMSG("Mandatory field is missing: pages-count");
+			continue;
+		}
+
+		add_phys_mem(memory_map, num_elems, name, MEM_AREA_IO_SEC,
+			     base, base + page_count * SMALL_PAGE_SIZE, last);
+	}
+}
+
 static size_t collect_mem_ranges(struct tee_mmap_region *memory_map,
 				 size_t num_elems)
 {
@@ -1019,6 +1061,10 @@ static size_t collect_mem_ranges(struct tee_mmap_region *memory_map,
 	}
 
 #undef ADD_PHYS_MEM
+
+	/* Collect device memory info from SP manifest */
+	if (IS_ENABLED(CFG_CORE_SEL2_SPMC))
+		collect_device_mem_ranges(memory_map, num_elems, &last);
 
 	for (mem = phys_mem_map_begin; mem < phys_mem_map_end; mem++) {
 		/* Only unmapped virtual range may have a null phys addr */

commit c35ac89c5c01d6dee4f69c4af5c2f3d4d68e33d0

core: mmu: fix memory regions found from ff-a manifest
Fix the 6th parameter of add_phys_mem() in collect_device_mem_ranges()
that has to be the size of memory region and not the end address of the
region.

Fixes: 72a6827a6353 ("core: arm: SPMC obtain device memory info from DTB")
Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/core_mmu.c
@@ -997,7 +997,7 @@ static void collect_device_mem_ranges(struct tee_mmap_region *memory_map,
 		}
 
 		add_phys_mem(memory_map, num_elems, name, MEM_AREA_IO_SEC,
-			     base, base + page_count * SMALL_PAGE_SIZE, last);
+			     base, page_count * SMALL_PAGE_SIZE, last);
 	}
 }
 