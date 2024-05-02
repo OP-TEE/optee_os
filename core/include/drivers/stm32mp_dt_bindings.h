8944654933888a024d7130eec05f084f48eca28e
Gatien Chevallier <gatien.chevallier@foss.st.com>
2024-05-02 16:51:17 +0200

    dt-bindings: add platform specific ETZPC bindings
    Define ETZPC bindings for STM32MP15 and STM32MP13 and add these
    header files into the stm32mp_dt_bindings helper. While there, also
    update some includes to fix the path errors.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/include/drivers/stm32mp_dt_bindings.h
+++ core/include/drivers/stm32mp_dt_bindings.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2022, STMicroelectronics
+ * Copyright (c) 2022-2024, STMicroelectronics
  */
 #ifndef __DRIVERS_STM32MP_DT_BINDINGS_H
 #define __DRIVERS_STM32MP_DT_BINDINGS_H
@@ -8,12 +8,14 @@
 #ifdef CFG_STM32MP13
 #include <dt-bindings/clock/stm32mp13-clks.h>
 #include <dt-bindings/clock/stm32mp13-clksrc.h>
+#include <dt-bindings/firewall/stm32mp13-etzpc.h>
 #include <dt-bindings/regulator/st,stm32mp13-regulator.h>
 #include <dt-bindings/reset/stm32mp13-resets.h>
 #endif
 
 #ifdef CFG_STM32MP15
 #include <dt-bindings/clock/stm32mp1-clks.h>
+#include <dt-bindings/firewall/stm32mp15-etzpc.h>
 #include <dt-bindings/regulator/st,stm32mp15-regulator.h>
 #include <dt-bindings/reset/stm32mp1-resets.h>
 #endif
