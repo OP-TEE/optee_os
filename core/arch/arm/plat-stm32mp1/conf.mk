348af9732e2c65dfa71e0fe6637b8fa01a21104d
Gatien Chevallier <gatien.chevallier@foss.st.com>
2024-05-02 16:51:17 +0200

    plat-stm32mp1: default enable CFG_DRIVERS_FIREWALL
    Default enable the CFG_DRIVERS_FIREWALL switch that is used to enable
    the support of the firewall framework.
    
    On this platform, only the ETZPC is a firewall controller for now.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -411,3 +411,8 @@ CFG_STM32_DEBUG_ACCESS ?= $(CFG_TEE_CORE_DEBUG)
 ifeq ($(call cfg-all-enabled,CFG_STM32MP15 CFG_STM32MP13),y)
 $(error CFG_STM32MP13_CLK and CFG_STM32MP15_CLK are exclusive)
 endif
+
+CFG_DRIVERS_FIREWALL ?= y
+ifeq ($(CFG_STM32_ETZPC),y)
+$(call force,CFG_DRIVERS_FIREWALL,y)
+endif
