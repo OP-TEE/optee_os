OP-TEE/optee_os

commit cf707bd0d6955a19c8b024acae3c46f78088648f

core: add callout service
Add a callout service to call registered callback functions at a given
time from now. A callout is periodic or oneshot depending on how the
callback function returns. Callback functions execute in nexus and
interrupt context.

The callout service is initialized with callout_service_init() and
depends on an abstract timer interface to manage the hardware timer.
callout_service_cb() needs to be called from the timer interrupt handler
to drive the callout service.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/kernel/callout.c
@@ -1,0 +1,188 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2024, Linaro Limited
+ */
+
+#include <kernel/callout.h>
+#include <kernel/misc.h>
+#include <kernel/spinlock.h>
+#include <mm/core_memprot.h>
+
+TAILQ_HEAD(callout_head, callout);
+
+static unsigned int callout_sched_lock __nex_data = SPINLOCK_UNLOCK;
+static size_t callout_sched_core __nex_bss;
+static unsigned int callout_lock __nex_data = SPINLOCK_UNLOCK;
+static const struct callout_timer_desc *callout_desc __nex_bss;
+static struct callout_head callout_head __nex_data =
+	TAILQ_HEAD_INITIALIZER(callout_head);
+
+static void insert_callout(struct callout *co)
+{
+	struct callout *co2 = NULL;
+
+	TAILQ_FOREACH(co2, &callout_head, link) {
+		if (co->expiry_value < co2->expiry_value) {
+			TAILQ_INSERT_BEFORE(co2, co, link);
+			return;
+		}
+	}
+
+	TAILQ_INSERT_TAIL(&callout_head, co, link);
+}
+
+static void schedule_next_timeout(void)
+{
+	const struct callout_timer_desc *desc = callout_desc;
+	struct callout *co = TAILQ_FIRST(&callout_head);
+
+	if (co)
+		desc->set_next_timeout(desc, co->expiry_value);
+	else
+		desc->disable_timeout(desc);
+
+	if (desc->is_per_cpu) {
+		/*
+		 * Remember which core is supposed to receive the next
+		 * timer interrupt. This will not disable timers on other
+		 * CPUs, instead they will be ignored as a spurious call.
+		 */
+		cpu_spin_lock(&callout_sched_lock);
+		callout_sched_core = get_core_pos();
+		cpu_spin_unlock(&callout_sched_lock);
+	}
+}
+
+static bool callout_is_active(struct callout *co)
+{
+	struct callout *co2 = NULL;
+
+	TAILQ_FOREACH(co2, &callout_head, link)
+		if (co2 == co)
+			return true;
+
+	return false;
+}
+
+void callout_rem(struct callout *co)
+{
+	uint32_t state = 0;
+
+	state = cpu_spin_lock_xsave(&callout_lock);
+
+	if (callout_is_active(co)) {
+		TAILQ_REMOVE(&callout_head, co, link);
+		schedule_next_timeout();
+	}
+
+	cpu_spin_unlock_xrestore(&callout_lock, state);
+}
+
+void callout_add(struct callout *co, bool (*callback)(struct callout *co),
+		 uint32_t ms)
+{
+	const struct callout_timer_desc *desc = callout_desc;
+	uint32_t state = 0;
+
+	state = cpu_spin_lock_xsave(&callout_lock);
+
+	assert(is_nexus(co) && !callout_is_active(co));
+	*co = (struct callout){ .callback = callback, };
+
+	if (desc) {
+		co->period = desc->ms_to_ticks(desc, ms);
+		co->expiry_value = desc->get_now(desc) + co->period;
+	} else {
+		/* This will be converted to ticks in callout_service_init(). */
+		co->period = ms;
+	}
+
+	insert_callout(co);
+	if (desc && co == TAILQ_FIRST(&callout_head))
+		schedule_next_timeout();
+
+	cpu_spin_unlock_xrestore(&callout_lock, state);
+}
+
+void callout_set_next_timeout(struct callout *co, uint32_t ms)
+{
+	co->period = callout_desc->ms_to_ticks(callout_desc, ms);
+}
+
+void callout_service_init(const struct callout_timer_desc *desc)
+{
+	struct callout_head tmp_head = TAILQ_HEAD_INITIALIZER(tmp_head);
+	struct callout *co = NULL;
+	uint32_t state = 0;
+	uint64_t now = 0;
+
+	state = cpu_spin_lock_xsave(&callout_lock);
+
+	assert(!callout_desc);
+	assert(is_nexus(desc) && is_unpaged(desc->disable_timeout) &&
+	       is_unpaged(desc->set_next_timeout) &&
+	       is_unpaged(desc->ms_to_ticks) && is_unpaged(desc->get_now));
+
+	callout_desc = desc;
+	now = desc->get_now(desc);
+
+	TAILQ_CONCAT(&tmp_head, &callout_head, link);
+	while (!TAILQ_EMPTY(&tmp_head)) {
+		co = TAILQ_FIRST(&tmp_head);
+		TAILQ_REMOVE(&tmp_head, co, link);
+
+		/*
+		 * Periods set before the timer descriptor are in
+		 * milliseconds since the frequency of the timer isn't
+		 * available at that point. So update it to ticks now.
+		 */
+		co->period = desc->ms_to_ticks(desc, co->period);
+		co->expiry_value = now + co->period;
+		insert_callout(co);
+	}
+	schedule_next_timeout();
+
+	cpu_spin_unlock_xrestore(&callout_lock, state);
+}
+
+void callout_service_cb(void)
+{
+	const struct callout_timer_desc *desc = callout_desc;
+	struct callout *co = NULL;
+	uint64_t now = 0;
+
+	if (desc->is_per_cpu) {
+		bool do_callout = false;
+
+		/*
+		 * schedule_next_timeout() saves the core it was last
+		 * called on. If there's a mismatch here it means that
+		 * another core has been scheduled for the next callout, so
+		 * there's no work to be done for this core.
+		 */
+		cpu_spin_lock(&callout_sched_lock);
+		do_callout = (get_core_pos() == callout_sched_core);
+		cpu_spin_unlock(&callout_sched_lock);
+		if (!do_callout)
+			return;
+	}
+
+	cpu_spin_lock(&callout_lock);
+
+	now = desc->get_now(desc);
+	while (!TAILQ_EMPTY(&callout_head)) {
+		co = TAILQ_FIRST(&callout_head);
+		if (co->expiry_value > now)
+			break;
+
+		TAILQ_REMOVE(&callout_head, co, link);
+
+		if (co->callback(co)) {
+			co->expiry_value += co->period;
+			insert_callout(co);
+		}
+	}
+	schedule_next_timeout();
+
+	cpu_spin_unlock(&callout_lock);
+}

core/include/kernel/callout.h
@@ -1,0 +1,134 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2024, Linaro Limited
+ */
+
+#ifndef __KERNEL_CALLOUT_H
+#define __KERNEL_CALLOUT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/queue.h>
+
+/*
+ * struct callout - callout reference
+ * @callback:	  function to be called when a callout expires
+ * @expiry_value: callout expiry time counter value
+ * @period:	  ticks to next timeout
+ * @link:	  linked list element
+ *
+ * @callback is called from an interrupt handler so thread resources must
+ * not be used. The main callout service lock is held while @callback is
+ * called so callout_rem() and callout_add() can't be used, but it is safe
+ * to call callout_set_next_timeout() if the call period should be changed.
+ * @callback returns true if it should be called again in @period ticks
+ * or false if the callout should be removed and inactivated. Returning
+ * false from @callback is the equivalent of calling callout_rem() on the
+ * callout reference.
+ */
+struct callout {
+	bool (*callback)(struct callout *co);
+	uint64_t expiry_value;
+	uint64_t period;
+	TAILQ_ENTRY(callout) link;
+};
+
+/*
+ * callout_add() - Add a callout
+ * @co:		callout reference
+ * @callback:	callback function accociated with the callout
+ * @ms:		time to next callout in milliseconds
+ *
+ * Adds a callout to the callout service with an associated callback
+ * function @callback that is to be called in @ms milliseconds.
+ *
+ * If callout_add() is called before callout_service_init() has been called
+ * then it will be called @ms milliseconds after callout_service_init() has
+ * been called.
+ *
+ * The callout structure can reside in global data or on the heap. It's
+ * safe to embed it inside another struct, but it must not be freed until
+ * removed with callout_rem() or equivalent.
+ *
+ * The function takes the main callout service for synchronization so it
+ * can't be called from within a callback function in a callout or there's
+ * deadlock.
+ */
+void callout_add(struct callout *co, bool (*callback)(struct callout *co),
+		 uint32_t ms);
+
+/*
+ * callout_rem() - Remove a callout
+ * @co:		callout reference
+ *
+ * Removes a callout previously added to the callout service with
+ * callout_add(). Note that when the callback function in a callout
+ * returns false the callout is also removed.
+ *
+ * It's safe to try to remove a callback even if it isn't active any
+ * longer. Nothing will happen in that case, but it's guaranteed to be
+ * inactive and it's safe to free the memory after callout_rem() has
+ * returned.
+ */
+void callout_rem(struct callout *co);
+
+/*
+ * callout_set_next_timeout() - set time to next callout
+ * @co:		callout reference
+ * @ms:		time to next callout in milliseconds
+ *
+ * Updates the @co->ticks field with the new number of ticks based on @ms.
+ * This value is used to when to calculate the time of the next callout
+ * following then one already set.
+ *
+ * Must only be called from @co->callback() when the callout is triggered.
+ */
+void callout_set_next_timeout(struct callout *co, uint32_t ms);
+
+/*
+ * struct callout_timer_desc - callout timer descriptor
+ * @disable_timeout:	disables the timer from triggering an interrupt
+ * @set_next_timeout:	sets the next timeout and enables the timer
+ * @ms_to_ticks:	converts milliseconds to ticks, the counter value
+ *			unit
+ * @get_now:		get the current counter value
+ * @is_per_cpu:		flag to indicate if this timer is per CPU (true) or
+ *			global (false).
+ *
+ * This descriptor provides an abstract timer interface first used by
+ * callout_service_init() and then stored to be used by
+ * callout_service_cb().
+ *
+ * When @is_per_cpu is true there is one private timer per CPU so
+ * @disable_timeout() and @set_next_timeout() only affects the timer on the
+ * current CPU. If for instance @set_next_timeout() is called on a new CPU
+ * compared to last time the timer on the old CPU will remain unchanged.
+ * Timer interrupts may trigger based on obsolete configuration, the
+ * callout service is expected to handle this gracefully.
+ */
+struct callout_timer_desc {
+	void (*disable_timeout)(const struct callout_timer_desc *desc);
+	void (*set_next_timeout)(const struct callout_timer_desc *desc,
+				 uint64_t expiry_value);
+	uint64_t (*ms_to_ticks)(const struct callout_timer_desc *desc,
+				uint32_t ms);
+	uint64_t (*get_now)(const struct callout_timer_desc *desc);
+	bool is_per_cpu;
+};
+
+/*
+ * callout_service_init() - Initialize the callout service
+ * @desc:	Pointer to the timer interface
+ *
+ * The callout service is initialized with the supplied timer interface
+ */
+void callout_service_init(const struct callout_timer_desc *desc);
+
+/*
+ * callout_service_cb() - Callout service callback
+ *
+ * Called from interrupt service function for the timer.
+ */
+void callout_service_cb(void);
+
+#endif /*__KERNEL_CALLOUT_H*/

core/kernel/sub.mk
@@ -63,3 +63,4 @@ ifeq ($(CFG_SYSCALL_FTRACE),y)
 cflags-remove-spin_lock_debug.c-$(CFG_TEE_CORE_DEBUG) += -pg
 endif
 srcs-y += nv_counter.c
+srcs-y += callout.c

commit 8956d243ee958e5e2c78f20ea815492648f01c26

core: callout: disable obsolete timeouts
In callout_service_cb() when a timeout interrupt is received there's a
check to see if this is the last scheduled CPU. If not the interrupt is
ignored, but not disabled causing it to trigger again and again. So fix
this by disabling the timeout too.

Fixes: cf707bd0d695 ("core: add callout service")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/kernel/callout.c
@@ -158,10 +158,13 @@ void callout_service_cb(void)
 		 * schedule_next_timeout() saves the core it was last
 		 * called on. If there's a mismatch here it means that
 		 * another core has been scheduled for the next callout, so
-		 * there's no work to be done for this core.
+		 * there's no work to be done for this core and we can
+		 * disable the timeout on this CPU.
 		 */
 		cpu_spin_lock(&callout_sched_lock);
 		do_callout = (get_core_pos() == callout_sched_core);
+		if (!do_callout)
+			desc->disable_timeout(desc);
 		cpu_spin_unlock(&callout_sched_lock);
 		if (!do_callout)
 			return;