OP-TEE/optee_os

commit 19ad526cb1391f8489ea75c89ae7f97c8a630ffa

core: spmc, sp: cleanup FF-A ID handling
When OP-TEE implements the S-EL1 SPMC, from an FF-A point-of-view the
core OP-TEE functionality is running in a logical SP that resides at the
same exception level as the SPMC. This means that the SPMC and the SP
should have separate FF-A IDs, i.e. the SPMC ID and a normal endpoint ID
for the SP. The SPMC ID is described in the SPMC manifest which gets
parsed by the SPMD, so this ID should be queried from the SPMD. OP-TEE's
endpoint ID is assigned by the SPMC.

Currently OP-TEE's FF-A endpoint ID and the SPMC ID are mixed together
and hardcoded, this patch implements the correct ID handling mechanism
as described above.

Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Balint Dobszay <balint.dobszay@arm.com>

core/arch/arm/kernel/thread_spmc.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2020-2023, Linaro Limited.
- * Copyright (c) 2019-2021, Arm Limited. All rights reserved.
+ * Copyright (c) 2019-2024, Arm Limited. All rights reserved.
  */
 
 #include <assert.h>
@@ -53,8 +53,10 @@ static uint16_t notif_vm_id;
 static bool spmc_notif_is_ready;
 
 /* Initialized in spmc_init() below */
-static uint16_t my_endpoint_id __nex_bss;
+uint16_t optee_endpoint_id __nex_bss;
+uint16_t spmc_id __nex_bss;
 #ifdef CFG_CORE_SEL1_SPMC
+uint16_t spmd_id __nex_bss;
 static const uint32_t my_part_props = FFA_PART_PROP_DIRECT_REQ_RECV |
 				      FFA_PART_PROP_DIRECT_REQ_SEND |
 #ifdef CFG_NS_VIRTUALIZATION
@@ -303,9 +305,9 @@ static int map_buf(paddr_t pa, unsigned int sz, void **va_ret)
 	return 0;
 }
 
-static void handle_spm_id_get(struct thread_smc_args *args)
+void spmc_handle_spm_id_get(struct thread_smc_args *args)
 {
-	spmc_set_args(args, FFA_SUCCESS_32, FFA_PARAM_MBZ, my_endpoint_id,
+	spmc_set_args(args, FFA_SUCCESS_32, FFA_PARAM_MBZ, spmc_id,
 		      FFA_PARAM_MBZ, FFA_PARAM_MBZ, FFA_PARAM_MBZ);
 }
 
@@ -525,7 +527,7 @@ static int handle_partition_info_get_all(size_t *elem_count,
 	if (!count_only) {
 		/* Add OP-TEE SP */
 		if (spmc_fill_partition_entry(rxtx->ffa_vers, rxtx->tx,
-					      rxtx->size, 0, my_endpoint_id,
+					      rxtx->size, 0, optee_endpoint_id,
 					      CFG_TEE_CORE_NB_CORE,
 					      my_part_props, my_uuid_words))
 			return FFA_NO_MEMORY;
@@ -580,7 +582,7 @@ void spmc_handle_partition_info_get(struct thread_smc_args *args,
 		if (!count_only) {
 			res = spmc_fill_partition_entry(rxtx->ffa_vers,
 							rxtx->tx, rxtx->size, 0,
-							my_endpoint_id,
+							optee_endpoint_id,
 							CFG_TEE_CORE_NB_CORE,
 							my_part_props,
 							my_uuid_words);
@@ -638,7 +640,7 @@ static void spmc_handle_run(struct thread_smc_args *args)
 	uint16_t thread_id = FFA_TARGET_INFO_GET_VCPU_ID(args->a1);
 	uint32_t rc = FFA_OK;
 
-	if (endpoint != my_endpoint_id) {
+	if (endpoint != optee_endpoint_id) {
 		/*
 		 * The endpoint should be an SP, try to resume the SP from
 		 * preempted into busy state.
@@ -833,7 +835,7 @@ static void handle_direct_request(struct thread_smc_args *args,
 	uint32_t direct_resp_fid = 0;
 
 	if (IS_ENABLED(CFG_SECURE_PARTITION) &&
-	    FFA_DST(args->a1) != my_endpoint_id) {
+	    FFA_DST(args->a1) != optee_endpoint_id) {
 		spmc_sp_start_thread(args);
 		return;
 	}
@@ -945,7 +947,7 @@ static int get_acc_perms(vaddr_t mem_acc_base, unsigned int mem_access_size,
 	for (n = 0; n < mem_access_count; n++) {
 		mem_acc = (void *)(mem_acc_base + mem_access_size * n);
 		descr = &mem_acc->access_perm;
-		if (READ_ONCE(descr->endpoint_id) == my_endpoint_id) {
+		if (READ_ONCE(descr->endpoint_id) == optee_endpoint_id) {
 			*acc_perms = READ_ONCE(descr->perm);
 			*region_offs = READ_ONCE(mem_acc[n].region_offs);
 			return 0;
@@ -1075,7 +1077,7 @@ static bool is_sp_share(struct ffa_mem_transaction_x *mem_trans,
 	 * OP-TEE. We do read it later on again, but there are some additional
 	 * checks there to make sure that the data is correct.
 	 */
-	return READ_ONCE(perm->endpoint_id) != my_endpoint_id;
+	return READ_ONCE(perm->endpoint_id) != optee_endpoint_id;
 }
 
 static int add_mem_share(struct ffa_mem_transaction_x *mem_trans,
@@ -1645,7 +1647,7 @@ void notif_send_async(uint32_t value)
 
 	assert(value == NOTIF_VALUE_DO_BOTTOM_HALF && spmc_notif_is_ready &&
 	       do_bottom_half_value >= 0);
-	res = ffa_set_notification(notif_vm_id, my_endpoint_id, flags,
+	res = ffa_set_notification(notif_vm_id, optee_endpoint_id, flags,
 				   BIT64(do_bottom_half_value));
 	if (res) {
 		EMSG("notification set failed with error %d", res);
@@ -1665,7 +1667,7 @@ void thread_spmc_msg_recv(struct thread_smc_args *args)
 		handle_features(args);
 		break;
 	case FFA_SPM_ID_GET:
-		handle_spm_id_get(args);
+		spmc_handle_spm_id_get(args);
 		break;
 #ifdef ARM64
 	case FFA_RXTX_MAP_64:
@@ -2068,11 +2070,66 @@ void thread_spmc_register_secondary_ep(vaddr_t ep)
 		EMSG("FFA_SECONDARY_EP_REGISTER_64 ret %#lx", ret);
 }
 
+static uint16_t ffa_id_get(void)
+{
+	/*
+	 * Ask the SPM component running at a higher EL to return our FF-A ID.
+	 * This can either be the SPMC ID (if the SPMC is enabled in OP-TEE) or
+	 * the partition ID (if not).
+	 */
+	struct thread_smc_args args = {
+		.a0 = FFA_ID_GET,
+	};
+
+	thread_smccc(&args);
+	if (!is_ffa_success(args.a0)) {
+		if (args.a0 == FFA_ERROR)
+			EMSG("Get id failed with error %ld", args.a2);
+		else
+			EMSG("Get id failed");
+		panic();
+	}
+
+	return args.a2;
+}
+
+static uint16_t ffa_spm_id_get(void)
+{
+	/*
+	 * Ask the SPM component running at a higher EL to return its ID.
+	 * If OP-TEE implements the S-EL1 SPMC, this will get the SPMD ID.
+	 * If not, the ID of the SPMC will be returned.
+	 */
+	struct thread_smc_args args = {
+		.a0 = FFA_SPM_ID_GET,
+	};
+
+	thread_smccc(&args);
+	if (!is_ffa_success(args.a0)) {
+		if (args.a0 == FFA_ERROR)
+			EMSG("Get spm id failed with error %ld", args.a2);
+		else
+			EMSG("Get spm id failed");
+		panic();
+	}
+
+	return args.a2;
+}
+
 #if defined(CFG_CORE_SEL1_SPMC)
 static TEE_Result spmc_init(void)
 {
-	my_endpoint_id = SPMC_ENDPOINT_ID;
-	DMSG("My endpoint ID %#x", my_endpoint_id);
+	spmd_id = ffa_spm_id_get();
+	DMSG("SPMD ID %#"PRIx16, spmd_id);
+
+	spmc_id = ffa_id_get();
+	DMSG("SPMC ID %#"PRIx16, spmc_id);
+
+	optee_endpoint_id = FFA_SWD_ID_MIN;
+	while (optee_endpoint_id == spmd_id || optee_endpoint_id == spmc_id)
+		optee_endpoint_id++;
+
+	DMSG("OP-TEE endpoint ID %#"PRIx16, optee_endpoint_id);
 
 	/*
 	 * If SPMD think we are version 1.0 it will report version 1.0 to
@@ -2110,24 +2167,6 @@ static void spmc_rxtx_map(struct ffa_rxtx *rxtx)
 			EMSG("rxtx map failed");
 		panic();
 	}
-}
-
-static uint16_t get_my_id(void)
-{
-	struct thread_smc_args args = {
-		.a0 = FFA_ID_GET,
-	};
-
-	thread_smccc(&args);
-	if (!is_ffa_success(args.a0)) {
-		if (args.a0 == FFA_ERROR)
-			EMSG("Get id failed with error %ld", args.a2);
-		else
-			EMSG("Get id failed");
-		panic();
-	}
-
-	return args.a2;
 }
 
 static uint32_t get_ffa_version(uint32_t my_version)
@@ -2190,7 +2229,7 @@ static void *spmc_retrieve_req(uint64_t cookie,
 	acc_descr_array->region_offs = 0;
 	acc_descr_array->reserved = 0;
 	perm_descr = &acc_descr_array->access_perm;
-	perm_descr->endpoint_id = my_endpoint_id;
+	perm_descr->endpoint_id = optee_endpoint_id;
 	perm_descr->perm = FFA_MEM_ACC_RW;
 	perm_descr->flags = 0;
 
@@ -2228,7 +2267,7 @@ void thread_spmc_relinquish(uint64_t cookie)
 	relinquish_desc->handle = cookie;
 	relinquish_desc->flags = 0;
 	relinquish_desc->endpoint_count = 1;
-	relinquish_desc->endpoint_id_array[0] = my_endpoint_id;
+	relinquish_desc->endpoint_id_array[0] = optee_endpoint_id;
 	thread_smccc(&args);
 	if (!is_ffa_success(args.a0))
 		EMSG("Failed to relinquish cookie %#"PRIx64, cookie);
@@ -2329,8 +2368,12 @@ static TEE_Result spmc_init(void)
 	my_rxtx.ffa_vers = my_vers;
 
 	spmc_rxtx_map(&my_rxtx);
-	my_endpoint_id = get_my_id();
-	DMSG("My endpoint ID %#x", my_endpoint_id);
+
+	spmc_id = ffa_spm_id_get();
+	DMSG("SPMC ID %#"PRIx16, spmc_id);
+
+	optee_endpoint_id = ffa_id_get();
+	DMSG("OP-TEE endpoint ID %#"PRIx16, optee_endpoint_id);
 
 	if (!ffa_features(FFA_NOTIFICATION_SET)) {
 		spmc_notif_is_ready = true;

core/arch/arm/include/kernel/thread_spmc.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2021, Arm Limited.
+ * Copyright (c) 2021-2024, Arm Limited.
  * Copyright (c) 2023, Linaro Limited
  */
 #ifndef __KERNEL_THREAD_SPMC_H
@@ -11,8 +11,28 @@
 #include <kernel/panic.h>
 #include <kernel/thread.h>
 
-/* FF-A endpoint base ID when OP-TEE is used as a S-EL1 endpoint */
-#define SPMC_ENDPOINT_ID        0x8001
+/* The FF-A ID of Secure World components should be between these limits */
+#define FFA_SWD_ID_MIN	0x8000
+#define FFA_SWD_ID_MAX	UINT16_MAX
+
+/*
+ * OP-TEE FF-A partition ID. This is valid both when
+ * - the SPMC is implemented by OP-TEE and the core OP-TEE functionality runs
+ *   in a logical SP that resides at the same exception level as the SPMC, or,
+ * - the SPMC is at a higher EL and OP-TEE is running as a standalone S-EL1 SP.
+ */
+extern uint16_t optee_endpoint_id;
+
+/*
+ * FF-A ID of the SPMC. This is valid both when the SPMC is implemented in
+ * OP-TEE or at a higher EL.
+ */
+extern uint16_t spmc_id;
+
+#if defined(CFG_CORE_SEL1_SPMC)
+/* FF-A ID of the SPMD. This is only valid when OP-TEE is the S-EL1 SPMC. */
+extern uint16_t spmd_id;
+#endif
 
 #define SPMC_CORE_SEL1_MAX_SHM_COUNT	64
 
@@ -25,6 +45,7 @@ struct ffa_rxtx {
 	bool tx_is_mine;
 };
 
+void spmc_handle_spm_id_get(struct thread_smc_args *args);
 void spmc_handle_rxtx_map(struct thread_smc_args *args, struct ffa_rxtx *buf);
 void spmc_handle_rxtx_unmap(struct thread_smc_args *args, struct ffa_rxtx *buf);
 void spmc_handle_rx_release(struct thread_smc_args *args, struct ffa_rxtx *buf);

core/arch/arm/kernel/secure_partition.c
@@ -169,23 +169,29 @@ bool sp_has_exclusive_access(struct sp_mem_map_region *mem,
 	return !sp_mem_is_shared(mem);
 }
 
+static bool endpoint_id_is_valid(uint32_t id)
+{
+	/*
+	 * These IDs are assigned at the SPMC init so already have valid values
+	 * by the time this function gets first called
+	 */
+	return id != spmd_id && id != spmc_id && id != optee_endpoint_id &&
+	       id >= FFA_SWD_ID_MIN && id <= FFA_SWD_ID_MAX;
+}
+
 static TEE_Result new_session_id(uint16_t *endpoint_id)
 {
-	struct sp_session *session = NULL;
-
-	*endpoint_id = SPMC_ENDPOINT_ID;
+	uint32_t id = 0;
 
 	/* Find the first available endpoint id */
-	do {
-		if (*endpoint_id == UINT16_MAX)
-			return TEE_ERROR_BAD_FORMAT;
+	for (id = FFA_SWD_ID_MIN; id <= FFA_SWD_ID_MAX; id++) {
+		if (endpoint_id_is_valid(id) && !sp_get_session(id)) {
+			*endpoint_id = id;
+			return TEE_SUCCESS;
+		}
+	}
 
-		(*endpoint_id)++;
-
-		session = sp_get_session(*endpoint_id);
-	} while (session);
-
-	return TEE_SUCCESS;
+	return TEE_ERROR_BAD_FORMAT;
 }
 
 static TEE_Result sp_create_ctx(const TEE_UUID *bin_uuid, struct sp_session *s)
@@ -1088,8 +1094,10 @@ static TEE_Result read_manifest_endpoint_id(struct sp_session *s)
 	if (!sp_dt_get_u32(s->fdt, 0, "id", &endpoint_id)) {
 		TEE_Result res = TEE_ERROR_GENERIC;
 
-		if (endpoint_id <= SPMC_ENDPOINT_ID)
+		if (!endpoint_id_is_valid(endpoint_id)) {
+			EMSG("Invalid endpoint ID 0x%"PRIx32, endpoint_id);
 			return TEE_ERROR_BAD_FORMAT;
+		}
 
 		res = swap_sp_endpoints(endpoint_id, s->endpoint_id);
 		if (res)

core/arch/arm/kernel/spmc_sp_handler.c
@@ -983,12 +983,6 @@ static void handle_features(struct thread_smc_args *args)
 		      FFA_PARAM_MBZ, FFA_PARAM_MBZ);
 }
 
-static void handle_spm_id_get(struct thread_smc_args *args)
-{
-	spmc_set_args(args, FFA_SUCCESS_32, FFA_PARAM_MBZ, SPMC_ENDPOINT_ID,
-		      FFA_PARAM_MBZ, FFA_PARAM_MBZ, FFA_PARAM_MBZ);
-}
-
 static void handle_mem_perm_get(struct thread_smc_args *args,
 				struct sp_session *sp_s)
 {
@@ -1219,7 +1213,7 @@ void spmc_sp_msg_handler(struct thread_smc_args *args,
 			sp_enter(args, caller_sp);
 			break;
 		case FFA_SPM_ID_GET:
-			handle_spm_id_get(args);
+			spmc_handle_spm_id_get(args);
 			sp_enter(args, caller_sp);
 			break;
 		case FFA_PARTITION_INFO_GET:

commit 88f2eea64845c3b2590b42da2b959ffa855ab4fb

core: spmc: fix direct request handler
The FF-A direct request handling has an error: if the destination ID is
the SPMC ID, the handler is trying to forward the message to an SP with
this ID, which is obviously non-existent so this gives an error.

Fixes: 19ad526cb139 ("core: spmc, sp: cleanup FF-A ID handling")
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Balint Dobszay <balint.dobszay@arm.com>

core/arch/arm/kernel/thread_spmc.c
@@ -836,6 +836,7 @@ static void handle_direct_request(struct thread_smc_args *args,
 	uint32_t direct_resp_fid = 0;
 
 	if (IS_ENABLED(CFG_SECURE_PARTITION) &&
+	    FFA_DST(args->a1) != spmc_id &&
 	    FFA_DST(args->a1) != optee_endpoint_id) {
 		spmc_sp_start_thread(args);
 		return;