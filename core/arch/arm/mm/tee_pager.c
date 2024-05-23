OP-TEE/optee_os

commit 60e3671460429e85d61334da126e1294e985f20b

core: pager fix alloc_merged_pgt_array()
Fix the logic for how a shared pgt is detected in
alloc_merged_pgt_array().  Without this there's a buffer overrun in the
pgt_array plus of course not quite right pgt pointers in that array.

Fixes: 4a3f6ad054d4 ("core: pager: let struct tee_pager_area span multiple translation tables")
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/tee_pager.c
@@ -912,16 +912,24 @@ static struct pgt **alloc_merged_pgt_array(struct vm_paged_region *a,
 {
 	size_t a_next_pgt_count = get_pgt_count(a_next->base, a_next->size);
 	size_t a_pgt_count = get_pgt_count(a->base, a->size);
+	size_t pgt_count = get_pgt_count(a->base, a->size + a_next->size);
 	struct pgt **pgt_array = NULL;
+	bool have_shared_pgt = false;
+
+	have_shared_pgt = ((a->base + a->size) & ~CORE_MMU_PGDIR_MASK) ==
+			  (a_next->base & ~CORE_MMU_PGDIR_MASK);
+
+	if (have_shared_pgt)
+		assert(pgt_count == a_pgt_count + a_next_pgt_count - 1);
+	else
+		assert(pgt_count == a_pgt_count + a_next_pgt_count);
 
 	/* In case there's a shared pgt they must match */
-	if ((a->base & ~CORE_MMU_PGDIR_MASK) ==
-	    (a_next->base & ~CORE_MMU_PGDIR_MASK) &&
+	if (have_shared_pgt &&
 	    a->pgt_array[a_pgt_count - 1] != a_next->pgt_array[0])
 		return NULL;
 
-	pgt_array = calloc(sizeof(struct pgt *),
-			   get_pgt_count(a->base, a->size + a_next->size));
+	pgt_array = calloc(sizeof(struct pgt *), pgt_count);
 	if (!pgt_array)
 		return NULL;
 
@@ -930,8 +938,7 @@ static struct pgt **alloc_merged_pgt_array(struct vm_paged_region *a,
 	 * where a pgt is shared.
 	 */
 	memcpy(pgt_array, a->pgt_array, a_pgt_count * sizeof(struct pgt *));
-	if ((a->base & ~CORE_MMU_PGDIR_MASK) ==
-	    (a_next->base & ~CORE_MMU_PGDIR_MASK))
+	if (have_shared_pgt)
 		memcpy(pgt_array + a_pgt_count, a_next->pgt_array + 1,
 		       (a_next_pgt_count - 1) * sizeof(struct pgt *));
 	else

commit 60ea5f9851331dcd0ec5318a39dd245cc3299af1

core: pager: fix arguments passed to calloc in alloc_merged_pgt_array()
An error was reported when compiling with GCC14 on this calloc:

core/arch/arm/mm/tee_pager.c: In function 'alloc_merged_pgt_array':
core/arch/arm/mm/tee_pager.c:934:35: warning: 'calloc' sizes specified
with 'sizeof' in the earlier argument and not in the later argument
[-Wcalloc-transposed-args]
   934 |         pgt_array = calloc(sizeof(struct pgt *), pgt_count);
       |                                   ^~~~~~

Looking at the code, it seems that pgt_count and sizeof(struct pgt *)
are inverted.

Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Fixes: 60e367146042 ("core: pager fix alloc_merged_pgt_array()")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/mm/tee_pager.c
@@ -931,7 +931,7 @@ static struct pgt **alloc_merged_pgt_array(struct vm_paged_region *a,
 	    a->pgt_array[a_pgt_count - 1] != a_next->pgt_array[0])
 		return NULL;
 
-	pgt_array = calloc(sizeof(struct pgt *), pgt_count);
+	pgt_array = calloc(pgt_count, sizeof(struct pgt *));
 	if (!pgt_array)
 		return NULL;
 