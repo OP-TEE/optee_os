OP-TEE/optee_os

commit 58dbe3dff53066c8180b49450d2254b8d6ae5ea6

plat-mediatek: add support for MT7988 SoC
Add OP-TEE support for the MT7988 SoC.

Signed-off-by: guan-gm.lin <guan-gm.lin@mediatek.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-mediatek/conf.mk
@@ -78,3 +78,15 @@ CFG_TZDRAM_SIZE ?=  0x00a00000
 CFG_SHMEM_START ?= ($(CFG_TZDRAM_START) + $(CFG_TZDRAM_SIZE))
 CFG_SHMEM_SIZE ?= 0x00200000
 endif
+
+ifeq ($(PLATFORM_FLAVOR),mt7988)
+$(call force,CFG_TEE_CORE_NB_CORE,4)
+$(call force,CFG_CORE_CLUSTER_SHIFT,1)
+$(call force,CFG_ARM_GICV3,y)
+$(call force,CFG_GIC,y)
+$(call force,CFG_WITH_SOFTWARE_PRNG,Y)
+CFG_TZDRAM_START ?= 0x43041000
+CFG_TZDRAM_SIZE ?=  0x04ff000
+CFG_SHMEM_START ?= ($(CFG_TZDRAM_START) + $(CFG_TZDRAM_SIZE))
+CFG_SHMEM_SIZE ?= 0x00200000
+endif

core/arch/arm/plat-mediatek/platform_config.h
@@ -105,6 +105,20 @@
 #define CONSOLE_BAUDRATE	115200
 #define CONSOLE_UART_CLK_IN_HZ	26000000
 
+#elif defined(PLATFORM_FLAVOR_mt7988)
+
+#define GIC_BASE		0x0C000000
+#define GICC_OFFSET		0x400000
+#define GICD_OFFSET		0x000000
+
+#define UART0_BASE		0x11000000
+#define UART1_BASE		0x11000100
+#define UART2_BASE		0x11000200
+
+#define CONSOLE_UART_BASE	UART0_BASE
+#define CONSOLE_BAUDRATE	115200
+#define CONSOLE_UART_CLK_IN_HZ	40000000
+
 #else
 #error "Unknown platform flavor"
 #endif

commit b73dcd9c123c52feda7b079aeca48e6ac5cc9284

core: plat-mediatek: fix enforcement of SWRNG configuration on MT7988
Small fix: $(call force,CFG_WITH_SOFTWARE_PRNG,Y) should be lowercase
'y' so that the conditional check in core/crypto/sub.mk includes
the correct source file rng_fortuna.c instead of rng_hw.c, which is
unimplemented for this platform and causes build failure.

Fixes: 58dbe3dff530 ("plat-mediatek: add support for MT7988 SoC")
Signed-off-by: Charles Herz <herzc@umich.edu>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-mediatek/conf.mk
@@ -84,7 +84,7 @@ $(call force,CFG_TEE_CORE_NB_CORE,4)
 $(call force,CFG_CORE_CLUSTER_SHIFT,1)
 $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_GIC,y)
-$(call force,CFG_WITH_SOFTWARE_PRNG,Y)
+$(call force,CFG_WITH_SOFTWARE_PRNG,y)
 CFG_TZDRAM_START ?= 0x43041000
 CFG_TZDRAM_SIZE ?=  0x04ff000
 CFG_SHMEM_START ?= ($(CFG_TZDRAM_START) + $(CFG_TZDRAM_SIZE))