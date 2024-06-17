86861ecf0db42b29e92d8c66498fac9b526f94fd
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2024-06-17 11:10:18 +0200

    clk: stm32mp25: Introduce STM32MP25 clocks platform
    This driver is based on clk-stm32-core API to manage STM32 gates, dividers
    and muxes.
    
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
    Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/include/drivers/stm32mp2_rcc_util.h
+++ core/include/drivers/stm32mp2_rcc_util.h
@@ -6,9 +6,13 @@
 #ifndef __DRIVERS_STM32MP2_RCC_UTIL_H__
 #define __DRIVERS_STM32MP2_RCC_UTIL_H__
 
+#include <drivers/clk.h>
 #include <types_ext.h>
 
 /* Return RCC base address */
 vaddr_t stm32_rcc_base(void);
+
+/* Return the clock handle related to a clock DT binding ID */
+struct clk *stm32mp_rcc_clock_id_to_clk(unsigned long clock_id);
 
 #endif /*__DRIVERS_STM32MP2_RCC_UTIL_H__*/
