OP-TEE/optee_os

commit 138c5102ef4c8b77ed31fdb7aa98c7c3d4173478

GP131: Add TA property gpd.ta.doesNotCloseHandleOnCorruptObject
Adds the TA property gpd.ta.doesNotCloseHandleOnCorruptObject.

All syscalls operating on an object handle and can return
TEE_ERROR_CORRUPT_OBJECT must also do special treatment when
TEE_ERROR_CORRUPT_OBJECT is returned. Prior to
gpd.ta.doesNotCloseHandleOnCorruptObject this meant removing the object
and closing the object handle. With the
gpd.ta.doesNotCloseHandleOnCorruptObject property the object handle
shouldn't be close if this the current TA has the property set to true.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/include/user_ta_header.h
@@ -10,27 +10,30 @@
 #include <tee_api_types.h>
 #include <util.h>
 
-#define TA_FLAG_USER_MODE		0	 /* Deprecated, was (1 << 0) */
-#define TA_FLAG_EXEC_DDR		0	 /* Deprecated, was (1 << 1) */
-#define TA_FLAG_SINGLE_INSTANCE		(1 << 2)
-#define TA_FLAG_MULTI_SESSION		(1 << 3)
-#define TA_FLAG_INSTANCE_KEEP_ALIVE	(1 << 4) /* remains after last close */
-#define TA_FLAG_SECURE_DATA_PATH	(1 << 5) /* accesses SDP memory */
-#define TA_FLAG_REMAP_SUPPORT		0	 /* Deprecated, was (1 << 6) */
-#define TA_FLAG_CACHE_MAINTENANCE	(1 << 7) /* use cache flush syscall */
+#define TA_FLAG_USER_MODE		0	 /* Deprecated, was BIT32(0) */
+#define TA_FLAG_EXEC_DDR		0	 /* Deprecated, was BIT32(1) */
+#define TA_FLAG_SINGLE_INSTANCE		BIT32(2)
+#define TA_FLAG_MULTI_SESSION		BIT32(3)
+#define TA_FLAG_INSTANCE_KEEP_ALIVE	BIT32(4) /* remains after last close */
+#define TA_FLAG_SECURE_DATA_PATH	BIT32(5) /* accesses SDP memory */
+#define TA_FLAG_REMAP_SUPPORT		0	 /* Deprecated, was BIT32(6) */
+#define TA_FLAG_CACHE_MAINTENANCE	BIT32(7) /* use cache flush syscall */
 	/*
 	 * TA instance can execute multiple sessions concurrently
 	 * (pseudo-TAs only).
 	 */
-#define TA_FLAG_CONCURRENT		(1 << 8)
+#define TA_FLAG_CONCURRENT		BIT32(8)
 	/*
 	 * Device enumeration is done in two stages by the normal world, first
 	 * before the tee-supplicant has started and then once more when the
 	 * tee-supplicant is started. The flags below control if the TA should
 	 * be reported in the first or second or case.
 	 */
-#define TA_FLAG_DEVICE_ENUM		(1 << 9)  /* without tee-supplicant */
-#define TA_FLAG_DEVICE_ENUM_SUPP	(1 << 10) /* with tee-supplicant */
+#define TA_FLAG_DEVICE_ENUM		BIT32(9)  /* without tee-supplicant */
+#define TA_FLAG_DEVICE_ENUM_SUPP	BIT32(10) /* with tee-supplicant */
+	/* See also "gpd.ta.doesNotCloseHandleOnCorruptObject" */
+#define TA_FLAG_DONT_CLOSE_HANDLE_ON_CORRUPT_OBJECT \
+					BIT32(11)
 
 #define TA_FLAGS_MASK			GENMASK_32(10, 0)
 
@@ -116,6 +119,8 @@ extern struct __elf_phdr_info __elf_phdr_info;
 #define TA_PROP_STR_VERSION		"gpd.ta.version"
 #define TA_PROP_STR_DESCRIPTION		"gpd.ta.description"
 #define TA_PROP_STR_ENDIAN		"gpd.ta.endian"
+#define TA_PROP_STR_DOES_NOT_CLOSE_HANDLE_ON_CORRUPT_OBJECT \
+	"gpd.ta.doesNotCloseHandleOnCorruptObject"
 
 enum user_ta_prop_type {
 	USER_TA_PROP_TYPE_BOOL,	/* bool */

core/tee/tee_svc_storage.c
@@ -74,13 +74,11 @@ static TEE_Result tee_svc_close_enum(struct user_ta_ctx *utc,
 	return TEE_SUCCESS;
 }
 
-static TEE_Result tee_svc_storage_remove_corrupt_obj(struct ts_session *sess,
-						     struct tee_obj *o)
+static void remove_corrupt_obj(struct user_ta_ctx *utc, struct tee_obj *o)
 {
 	o->pobj->fops->remove(o->pobj);
-	tee_obj_close(to_user_ta_ctx(sess->ctx), o);
-
-	return TEE_SUCCESS;
+	if (!(utc->ta_ctx.flags & TA_FLAG_DONT_CLOSE_HANDLE_ON_CORRUPT_OBJECT))
+		tee_obj_close(utc, o);
 }
 
 static TEE_Result tee_svc_storage_read_head(struct tee_obj *o)
@@ -241,7 +239,7 @@ err:
 	if (res == TEE_ERROR_NO_DATA || res == TEE_ERROR_BAD_FORMAT)
 		res = TEE_ERROR_CORRUPT_OBJECT;
 	if (res == TEE_ERROR_CORRUPT_OBJECT && o)
-		tee_svc_storage_remove_corrupt_obj(sess, o);
+		remove_corrupt_obj(utc, o);
 
 exit:
 	return res;
@@ -763,7 +761,7 @@ TEE_Result syscall_storage_obj_read(unsigned long obj, void *data, size_t len,
 	if (res != TEE_SUCCESS) {
 		if (res == TEE_ERROR_CORRUPT_OBJECT) {
 			EMSG("Object corrupt");
-			tee_svc_storage_remove_corrupt_obj(sess, o);
+			remove_corrupt_obj(utc, o);
 		}
 		goto exit;
 	}
@@ -819,7 +817,7 @@ TEE_Result syscall_storage_obj_write(unsigned long obj, void *data, size_t len)
 	if (res != TEE_SUCCESS) {
 		if (res == TEE_ERROR_CORRUPT_OBJECT) {
 			EMSG("Object corrupt");
-			tee_svc_storage_remove_corrupt_obj(sess, o);
+			remove_corrupt_obj(utc, o);
 		}
 		goto exit;
 	}
@@ -874,7 +872,7 @@ TEE_Result syscall_storage_obj_trunc(unsigned long obj, size_t len)
 		break;
 	case TEE_ERROR_CORRUPT_OBJECT:
 		EMSG("Object corruption");
-		(void)tee_svc_storage_remove_corrupt_obj(sess, o);
+		remove_corrupt_obj(to_user_ta_ctx(sess->ctx), o);
 		break;
 	default:
 		res = TEE_ERROR_GENERIC;

ta/arch/arm/user_ta_header.c
@@ -155,6 +155,10 @@ const struct user_ta_property ta_props[] = {
 	/* Only little-endian supported */
 	{TA_PROP_STR_ENDIAN, USER_TA_PROP_TYPE_U32, &(const uint32_t){0}},
 
+	{TA_PROP_STR_DOES_NOT_CLOSE_HANDLE_ON_CORRUPT_OBJECT,
+	 USER_TA_PROP_TYPE_BOOL,
+	 &(const bool){TA_FLAGS & TA_FLAG_DONT_CLOSE_HANDLE_ON_CORRUPT_OBJECT}},
+
 /*
  * Extended propietary properties, name of properties must not begin with
  * "gpd."

commit 96cf17fc0aa93c5fc9cd709d0ab5c19ef752ba2b

Fix TA_FLAGS_MASK
Add missing TA_FLAG_DONT_CLOSE_HANDLE_ON_CORRUPT_OBJECT to
TA_FLAGS_MASK.

Fixes: 138c5102ef4c ("GP131: Add TA property gpd.ta.doesNotCloseHandleOnCorruptObject")
Suggested-by: Mikko Rapeli <mikko.rapeli@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

lib/libutee/include/user_ta_header.h
@@ -35,7 +35,7 @@
 #define TA_FLAG_DONT_CLOSE_HANDLE_ON_CORRUPT_OBJECT \
 					BIT32(11)
 
-#define TA_FLAGS_MASK			GENMASK_32(10, 0)
+#define TA_FLAGS_MASK			GENMASK_32(11, 0)
 
 struct ta_head {
 	TEE_UUID uuid;