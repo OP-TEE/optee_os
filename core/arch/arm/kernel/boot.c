OP-TEE/optee_os

commit 508e2476b232f3f055fd2e17d42c2884731e9436

core: update transfer list header and signature
Add tl->flags and TL_FLAGS_HAS_CHECKSUM to align to the latest FW
Handoff spec update.
Bypass checksum verifying and updating if TL_FLAGS_HAS_CHECKSUM bit
is not set in tl->flags.
Update TL signature to 4a0f_b10b to align to the latest FW Handoff
spec update.

Note: It causes Transfer List ABI breakage due to FW Handoff spec
stays with same rev number (v0.9) with above changes.

Signed-off-by: Raymond Mao <raymond.mao@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/transfer_list.h
@@ -6,7 +6,7 @@
 #ifndef __KERNEL_TRANSFER_LIST_H
 #define __KERNEL_TRANSFER_LIST_H
 
-#define TRANSFER_LIST_SIGNATURE		U(0x006ed0ff)
+#define TRANSFER_LIST_SIGNATURE		U(0x4a0fb10b)
 #define TRANSFER_LIST_VERSION		U(0x0001)
 
 /*
@@ -23,6 +23,8 @@
  * Set to 1 for both AArch64 and AArch32 according to fw handoff spec v0.9
  */
 #define REG_CONVENTION_VER_MASK BIT(24)
+
+#define TL_FLAGS_HAS_CHECKSUM BIT(0)
 
 /* Transfer list operation codes */
 #define TL_OPS_NONE	U(0) /* invalid for any operation */
@@ -54,6 +56,7 @@ struct transfer_list_header {
 	uint8_t alignment;	/* max alignment of transfer entry data */
 	uint32_t size;		/* TL header + all transfer entries */
 	uint32_t max_size;
+	uint32_t flags;
 	/*
 	 * Commented out element used to visualize dynamic part of the
 	 * data structure.

core/kernel/transfer_list.c
@@ -101,6 +101,7 @@ void transfer_list_dump(struct transfer_list_header *tl)
 	DMSG("alignment  %#"PRIx8, tl->alignment);
 	DMSG("size       %#"PRIx32, tl->size);
 	DMSG("max_size   %#"PRIx32, tl->max_size);
+	DMSG("flags      %#"PRIx32, tl->flags);
 	while (true) {
 		tl_e = transfer_list_next(tl, tl_e);
 		if (!tl_e)
@@ -145,6 +146,7 @@ struct transfer_list_header *transfer_list_init(paddr_t pa, size_t max_size)
 	tl->alignment = TRANSFER_LIST_INIT_MAX_ALIGN; /* initial max align */
 	tl->size = sizeof(*tl); /* initial size is the size of header */
 	tl->max_size = max_size;
+	tl->flags = TL_FLAGS_HAS_CHECKSUM;
 
 	transfer_list_update_checksum(tl);
 
@@ -298,9 +300,6 @@ static uint8_t calc_byte_sum(const struct transfer_list_header *tl)
 	uint8_t cs = 0;
 	size_t n = 0;
 
-	if (!tl)
-		return 0;
-
 	for (n = 0; n < tl->size; n++)
 		cs += b[n];
 
@@ -316,7 +315,7 @@ void transfer_list_update_checksum(struct transfer_list_header *tl)
 {
 	uint8_t cs = 0;
 
-	if (!tl)
+	if (!tl || !(tl->flags & TL_FLAGS_HAS_CHECKSUM))
 		return;
 
 	cs = calc_byte_sum(tl);
@@ -333,6 +332,12 @@ void transfer_list_update_checksum(struct transfer_list_header *tl)
  ******************************************************************************/
 bool transfer_list_verify_checksum(const struct transfer_list_header *tl)
 {
+	if (!tl)
+		return false;
+
+	if (!(tl->flags & TL_FLAGS_HAS_CHECKSUM))
+		return true;
+
 	return !calc_byte_sum(tl);
 }
 

commit a3e9748f645ff05a0e00f2c5356bd4fb03359e8d

core: kernel: Fix checking register convention r1/x1 value of transfer_list
According to recently firmware handsoff spec [1]'s
"Register usage at handoff boundary", Transfer List's signature value
was changed from 0x40_b10b (3 bytes) to 4a0f_b10b (4 bytes).

As updating of TL's signature, register value of x1/r1 should be:

In aarch32's r1 value should be
    R1[23:0]: set to the TL signature (4a0f_b10b->masked value: 0f_b10b)
    R1[31:24]: version of the register convention ==  1
and
In aarch64's x1 value should be
    X1[31:0]: set to the TL signature (4a0f_b10b)
    X1[39:32]: version of the register convention ==  1
    X1[63:40]: MBZ
(See the [2] and [3]).

Therefore, it requires to separate mask and shift value for register
convention version field when checking each r1/x1 value.

This patch fix two problems:
   1. breaking X1 value with updated specification in aarch64
        - change of length of signature field.

   2. previous error value set in R1 in arm32.
        - length of signature should be 24, but it uses 32bit signature.

This patch is a breaking change. It works only TF-A is updated.

Link: https://github.com/FirmwareHandoff/firmware_handoff [1]
Link: https://github.com/FirmwareHandoff/firmware_handoff/issues/32 [2]
Link: https://github.com/FirmwareHandoff/firmware_handoff/commit/5aa7aa1d3a1db75213e458d392b751f0707de027 [3]
Fixes: 508e2476b232 ("core: update transfer list header and signature")
Signed-off-by: Levi Yun <yeoreum.yun@arm.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/boot.c
@@ -1381,22 +1381,25 @@ void __weak boot_save_args(unsigned long a0, unsigned long a1,
 	 *       Transfer List
 	 * a0	- DTB address or 0 (AArch64)
 	 *	- must be 0 (AArch32)
-	 * a1	- TRANSFER_LIST_SIGNATURE | REG_CONVENTION_VER_MASK
+	 * a1	- 1 << 32 | TRANSFER_LIST_SIGNATURE[0:31] (AArch64)
+	 *	- 1 << 24 | TRANSFER_LIST_SIGNATURE[0:23] (AArch32)
 	 * a2	- must be 0 (AArch64)
 	 *	- DTB address or 0 (AArch32)
 	 * a3	- Transfer list base address
 	 * a4	- Not used
 	 */
 
-	if (IS_ENABLED(CFG_TRANSFER_LIST) &&
-	    a1 == (TRANSFER_LIST_SIGNATURE | REG_CONVENTION_VER_MASK)) {
-		if (IS_ENABLED(CFG_ARM64_core)) {
+	if (IS_ENABLED(CFG_TRANSFER_LIST)) {
+		if (IS_ENABLED(CFG_ARM64_core) &&
+		    a1 == TL_HANDOFF_X1_VALUE(TL_REG_CONVENTION_VER)) {
 			boot_save_transfer_list(a2, a3, a0);
 			boot_arg_fdt = a0;
-		} else {
+		} else if (IS_ENABLED(CFG_ARM32_core) &&
+			   a1 == TL_HANDOFF_R1_VALUE(TL_REG_CONVENTION_VER)) {
 			boot_save_transfer_list(a0, a3, a2);
 			boot_arg_fdt = a2;
 		}
+
 		return;
 	}
 

core/include/kernel/transfer_list.h
@@ -22,7 +22,22 @@
  * Version of the register convention used.
  * Set to 1 for both AArch64 and AArch32 according to fw handoff spec v0.9
  */
-#define REG_CONVENTION_VER_MASK BIT(24)
+#define TL_REG_CONVENTION_VER_SHIFT_64	UL(32)
+#define TL_REG_CONVENTION_VER_SHIFT_32	UL(24)
+#define TL_REG_CONVENTION_VER_MASK	UL(0xff)
+#define TL_REG_CONVENTION_VER	UL(1)
+
+#define TL_HANDOFF_X1_VALUE(__version)	\
+	((TRANSFER_LIST_SIGNATURE &	\
+	((BIT64(TL_REG_CONVENTION_VER_SHIFT_64)) - 1)) |	\
+	(((__version) & TL_REG_CONVENTION_VER_MASK) <<	\
+	 TL_REG_CONVENTION_VER_SHIFT_64))
+
+#define TL_HANDOFF_R1_VALUE(__version)	\
+	((TRANSFER_LIST_SIGNATURE &	\
+	((BIT32(TL_REG_CONVENTION_VER_SHIFT_32)) - 1)) |	\
+	(((__version) & TL_REG_CONVENTION_VER_MASK) <<	\
+	 TL_REG_CONVENTION_VER_SHIFT_32))
 
 #define TL_FLAGS_HAS_CHECKSUM BIT(0)
 