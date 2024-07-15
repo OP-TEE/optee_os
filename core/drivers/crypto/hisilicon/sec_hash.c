OP-TEE/optee_os

commit 94c8a3397ec48cff142d602a18579053a9fb4996

drivers: crypto: hisilicon:Add HASH and HMAC algorithm
Add HASH and HMAC algorithm by SEC, and support SHA1,
SHA224, SHA256, SHA384, SHA512, MD5, SM3, and HMAC
algorithms based on these algorithms.

Signed-off-by: leisen <leisen1@huawei.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/hisilicon/sec_hash.c
@@ -1,0 +1,663 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2022-2024 HiSilicon Limited.
+ * Kunpeng hardware accelerator sec hash algorithm implementation.
+ */
+
+#include <drvcrypt_hash.h>
+#include <initcall.h>
+#include <sec_hash.h>
+#include <sec_main.h>
+
+static enum hisi_drv_status sec_digest_set_hmac_key(struct hashctx *ctx,
+						    struct hisi_sec_sqe *sqe)
+{
+	if (ctx->key_len > SEC_DIGEST_MAX_KEY_SIZE || !ctx->key_len) {
+		EMSG("Invalid digest key len(%ld)", ctx->key_len);
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+
+	/* If the length of key is not word-aligned, increment by 1 */
+	sqe->type2.mac_key_alg |= SHIFT_U64(DIV_ROUND_UP(ctx->key_len,
+							 SEC_ENCODE_BYTES),
+					    SEC_AKEY_OFFSET);
+	sqe->type2.a_key_addr = ctx->key_dma;
+
+	return HISI_QM_DRVCRYPT_NO_ERR;
+}
+
+static void sec_digest_fill_long_bd2(struct hashctx *ctx,
+				     struct hisi_sec_sqe *sqe)
+{
+	uint64_t total_bits = 0;
+
+	if (ctx->has_next && !ctx->iv_len) {
+		/* LONG BD FIRST */
+		sqe->ai_apd_cs |= AI_GEN_INNER;
+		sqe->ai_apd_cs |= SHIFT_U32(AUTHPAD_NOPAD, SEC_APAD_OFFSET);
+		ctx->iv_len = ctx->mac_len;
+	} else if (ctx->has_next && ctx->iv_len) {
+		/* LONG BD MIDDLE */
+		sqe->ai_apd_cs |= AI_GEN_IVIN_ADDR;
+		sqe->ai_apd_cs |= SHIFT_U32(AUTHPAD_NOPAD, SEC_APAD_OFFSET);
+		sqe->type2.a_ivin_addr = sqe->type2.mac_addr;
+		ctx->iv_len = ctx->mac_len;
+	} else if (!ctx->has_next && ctx->iv_len) {
+		/* LONG BD END */
+		sqe->ai_apd_cs |= AI_GEN_IVIN_ADDR;
+		sqe->ai_apd_cs |= SHIFT_U32(AUTHPAD_PAD, SEC_APAD_OFFSET);
+		sqe->type2.a_ivin_addr = sqe->type2.mac_addr;
+		total_bits = ctx->long_data_len * BYTE_BITS;
+		sqe->type2.long_a_data_len = total_bits;
+		ctx->iv_len = 0;
+	} else {
+		/* SHORT BD */
+		ctx->iv_len = 0;
+	}
+}
+
+static struct crypto_hash *to_hash_ctx(struct crypto_hash_ctx *ctx)
+{
+	return container_of(ctx, struct crypto_hash, hash_ctx);
+}
+
+static uint32_t sec_digest_get_alg_type(uint32_t algo)
+{
+	switch (algo) {
+	case TEE_ALG_MD5:
+		return A_ALG_MD5;
+	case TEE_ALG_HMAC_MD5:
+		return A_ALG_HMAC_MD5;
+	case TEE_ALG_SHA1:
+		return A_ALG_SHA1;
+	case TEE_ALG_HMAC_SHA1:
+		return A_ALG_HMAC_SHA1;
+	case TEE_ALG_SHA224:
+		return A_ALG_SHA224;
+	case TEE_ALG_HMAC_SHA224:
+		return A_ALG_HMAC_SHA224;
+	case TEE_ALG_SM3:
+		return A_ALG_SM3;
+	case TEE_ALG_HMAC_SM3:
+		return A_ALG_HMAC_SM3;
+	case TEE_ALG_SHA256:
+		return A_ALG_SHA256;
+	case TEE_ALG_HMAC_SHA256:
+		return A_ALG_HMAC_SHA256;
+	case TEE_ALG_SHA384:
+		return A_ALG_SHA384;
+	case TEE_ALG_HMAC_SHA384:
+		return A_ALG_HMAC_SHA384;
+	case TEE_ALG_SHA512:
+		return A_ALG_SHA512;
+	case TEE_ALG_HMAC_SHA512:
+		return A_ALG_HMAC_SHA512;
+	default:
+		return A_ALG_MAX;
+	}
+}
+
+static enum hisi_drv_status sec_digest_fill_sqe(void *bd, void *msg)
+{
+	enum hisi_drv_status ret = HISI_QM_DRVCRYPT_NO_ERR;
+	struct hisi_sec_sqe *sqe = bd;
+	struct hashctx *ctx = msg;
+	uint32_t alg_type = 0;
+
+	if (!ctx->in_len) {
+		EMSG("Digest bd2 not support 0 packet");
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+
+	sqe->type_auth_cipher = BD_TYPE2;
+	sqe->sds_sa_type = SHIFT_U32(SCENE_NOTHING, SEC_SCENE_OFFSET);
+	sqe->type_auth_cipher |= SHIFT_U32(AUTH_MAC_CALCULATE, SEC_AUTH_OFFSET);
+	sqe->type2.alen_ivllen = ctx->in_len;
+
+	sqe->type2.data_src_addr = ctx->in_dma;
+	sqe->type2.mac_addr = ctx->out_dma;
+	sqe->type2.mac_key_alg |= ctx->mac_len / SEC_ENCODE_BYTES;
+
+	if (ctx->mode == WCRYPTO_DIGEST_HMAC) {
+		ret = sec_digest_set_hmac_key(ctx, sqe);
+		if (ret)
+			return ret;
+	}
+
+	alg_type = sec_digest_get_alg_type(ctx->algo);
+	if (alg_type >= A_ALG_MAX) {
+		EMSG("Fail to get digest alg type");
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+	sqe->type2.mac_key_alg |= SHIFT_U32(alg_type, SEC_AEAD_ALG_OFFSET);
+
+	sec_digest_fill_long_bd2(ctx, sqe);
+
+	return ret;
+}
+
+static enum hisi_drv_status
+sec_digest_set_hmac_bd3_key(struct hashctx *ctx, struct hisi_sec_bd3_sqe *sqe)
+{
+	if (ctx->key_len > SEC_DIGEST_MAX_KEY_SIZE || !ctx->key_len) {
+		EMSG("Invalid digest key len(%ld)", ctx->key_len);
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+
+	/* If the length of key is not word-aligned, increment by 1 */
+	sqe->auth_mac_key |= SHIFT_U64(DIV_ROUND_UP(ctx->key_len,
+						    SEC_ENCODE_BYTES),
+				       SEC_AKEY_OFFSET_V3);
+	sqe->a_key_addr = ctx->key_dma;
+
+	return HISI_QM_DRVCRYPT_NO_ERR;
+}
+
+static void sec_digest_fill_long_bd3(struct hashctx *ctx,
+				     struct hisi_sec_bd3_sqe *sqe)
+{
+	uint64_t total_bits = 0;
+
+	if (ctx->has_next && !ctx->iv_len) {
+		/* LONG BD FIRST */
+		sqe->auth_mac_key |= SHIFT_U32(AI_GEN_INNER,
+					       SEC_AI_GEN_OFFSET_V3);
+		sqe->stream_scene.auth_pad = AUTHPAD_NOPAD;
+		ctx->iv_len = ctx->mac_len;
+	} else if (ctx->has_next && ctx->iv_len) {
+		/* LONG BD MIDDLE */
+		sqe->auth_mac_key |= SHIFT_U32(AI_GEN_IVIN_ADDR,
+					       SEC_AI_GEN_OFFSET_V3);
+		sqe->stream_scene.auth_pad = AUTHPAD_NOPAD;
+		sqe->a_ivin_addr = sqe->mac_addr;
+		ctx->iv_len = ctx->mac_len;
+	} else if (!ctx->has_next && ctx->iv_len) {
+		/* LONG BD END */
+		sqe->auth_mac_key |= SHIFT_U32(AI_GEN_IVIN_ADDR,
+					       SEC_AI_GEN_OFFSET_V3);
+		sqe->stream_scene.auth_pad = AUTHPAD_PAD;
+		sqe->a_ivin_addr = sqe->mac_addr;
+		total_bits = ctx->long_data_len * BYTE_BITS;
+		sqe->stream_scene.long_a_data_len = total_bits;
+		ctx->iv_len = 0;
+	} else {
+		/* SHORT BD */
+		ctx->iv_len = 0;
+	}
+}
+
+static enum hisi_drv_status sec_digest_fill_bd3_sqe(void *bd, void *msg)
+{
+	enum hisi_drv_status ret = HISI_QM_DRVCRYPT_NO_ERR;
+	struct hisi_sec_bd3_sqe *sqe = bd;
+	struct hashctx *ctx = msg;
+	uint32_t alg_type = 0;
+
+	sqe->bd_param = BD_TYPE3 | SHIFT_U32(ctx->scene, SEC_SCENE_OFFSET_V3);
+	sqe->a_len_key = ctx->in_len;
+	sqe->auth_mac_key = AUTH_MAC_CALCULATE;
+	sqe->data_src_addr = ctx->in_dma;
+	sqe->mac_addr = ctx->out_dma;
+
+	if (ctx->mode == WCRYPTO_DIGEST_HMAC) {
+		ret = sec_digest_set_hmac_bd3_key(ctx, sqe);
+		if (ret)
+			return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+
+	sqe->auth_mac_key |= SHIFT_U64(ctx->mac_len / SEC_ENCODE_BYTES,
+				       SEC_MAC_OFFSET_V3);
+	alg_type = sec_digest_get_alg_type(ctx->algo);
+	if (alg_type >= A_ALG_MAX) {
+		EMSG("Fail to get digest bd3 alg");
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+	sqe->auth_mac_key |= SHIFT_U32(alg_type, SEC_AUTH_ALG_OFFSET_V3);
+	sec_digest_fill_long_bd3(ctx, sqe);
+
+	return HISI_QM_DRVCRYPT_NO_ERR;
+}
+
+static TEE_Result sec_digest_do_task(struct hisi_qp *qp, void *msg)
+{
+	TEE_Result ret = TEE_SUCCESS;
+
+	ret = hisi_qp_send(qp, msg);
+	if (ret) {
+		EMSG("Fail to send task, ret=%d", ret);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	ret = hisi_qp_recv_sync(qp, msg);
+	if (ret) {
+		EMSG("Recv task error, ret=%d", ret);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	return TEE_SUCCESS;
+}
+
+static enum hisi_drv_status sec_parse_digest_sqe(void *bd, void *msg __unused)
+{
+	struct hisi_sec_sqe *sqe = bd;
+	uint16_t done = 0;
+
+	done = SEC_GET_FIELD(sqe->type2.done_flag, SEC_DONE_MASK, 0);
+	if (done != SEC_HW_TASK_DONE || sqe->type2.error_type) {
+		EMSG("SEC BD2 fail! done=%#"PRIx16", etype=%#"PRIx8,
+		     done, sqe->type2.error_type);
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+
+	return HISI_QM_DRVCRYPT_NO_ERR;
+}
+
+static enum hisi_drv_status sec_parse_digest_bd3_sqe(void *bd,
+						     void *msg __unused)
+{
+	struct hisi_sec_bd3_sqe *sqe = bd;
+	uint16_t done = 0;
+
+	done = SEC_GET_FIELD(sqe->done_flag, SEC_DONE_MASK, 0);
+	if (done != SEC_HW_TASK_DONE || sqe->error_type) {
+		EMSG("SEC BD3 fail! done=%#"PRIx16", etype=%#"PRIx8,
+		     done, sqe->error_type);
+		return HISI_QM_DRVCRYPT_IN_EPARA;
+	}
+
+	return HISI_QM_DRVCRYPT_NO_ERR;
+}
+
+TEE_Result hisi_sec_digest_ctx_init(struct hashctx *hash_ctx,
+				    const uint8_t *key, size_t len)
+{
+	if (!hash_ctx) {
+		EMSG("Input hash_ctx is NULL");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	hash_ctx->in_len = 0;
+	hash_ctx->iv_len = 0;
+	hash_ctx->has_next = false;
+	hash_ctx->long_data_len = 0;
+	hash_ctx->scene = SCENE_NOTHING;
+
+	/*
+	 * In reset ctx scenarios, sec_hash_initialize will be called.
+	 * To ensure in data is NULL, reset ctx need to free in data
+	 * which is not NULL.
+	 */
+	free(hash_ctx->in);
+	hash_ctx->in = NULL;
+
+	if (len) {
+		hash_ctx->key_len = len;
+		memcpy(hash_ctx->key, key, len);
+	}
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result sec_hash_initialize(struct crypto_hash_ctx *ctx)
+{
+	struct crypto_hash *hash = NULL;
+	struct hashctx *hash_ctx = NULL;
+
+	if (!ctx) {
+		EMSG("Input ctx is NULL");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	hash = to_hash_ctx(ctx);
+	hash_ctx = hash->ctx;
+
+	return hisi_sec_digest_ctx_init(hash_ctx, NULL, 0);
+}
+
+TEE_Result hisi_sec_digest_do_update(struct hashctx *hash_ctx,
+				     const uint8_t *data, size_t len)
+{
+	TEE_Result ret = TEE_SUCCESS;
+	size_t left_size = 0;
+
+	hash_ctx->long_data_len += len;
+
+	if (!hash_ctx->in) {
+		if (len <= SMALL_BUF_SIZE)
+			hash_ctx->buf_len = SMALL_BUF_SIZE;
+		else if (len <= MAX_AUTH_LENGTH)
+			hash_ctx->buf_len = ROUNDUP(len, HISI_QM_ALIGN128);
+		else
+			hash_ctx->buf_len = MAX_AUTH_LENGTH;
+
+		hash_ctx->in_len = 0;
+		hash_ctx->in = malloc(hash_ctx->buf_len);
+		if (!hash_ctx->in) {
+			EMSG("Fail to alloc in data buf");
+			return TEE_ERROR_STORAGE_NO_SPACE;
+		}
+		hash_ctx->in_dma = virt_to_phys(hash_ctx->in);
+		if (!hash_ctx->in_dma) {
+			free(hash_ctx->in);
+			hash_ctx->in = NULL;
+			EMSG("Fail to get in_dma");
+			return TEE_ERROR_STORAGE_NO_SPACE;
+		}
+	}
+
+	while (len > 0) {
+		if (hash_ctx->in_len + len <= hash_ctx->buf_len) {
+			memcpy(hash_ctx->in + hash_ctx->in_len, data, len);
+			hash_ctx->in_len += len;
+			len = 0;
+		} else {
+			left_size = hash_ctx->buf_len - hash_ctx->in_len;
+			memcpy(hash_ctx->in + hash_ctx->in_len, data,
+			       left_size);
+			hash_ctx->in_len = hash_ctx->buf_len;
+			hash_ctx->scene = SCENE_STREAM;
+			hash_ctx->has_next = true;
+			data += left_size;
+			len -= left_size;
+			ret = sec_digest_do_task(hash_ctx->qp, hash_ctx);
+			if (ret) {
+				EMSG("Fail to do digest task! ret = %#"PRIx32,
+				     ret);
+				return ret;
+			}
+			hash_ctx->iv_len = hash_ctx->mac_len;
+			hash_ctx->in_len = 0;
+		}
+	}
+	return TEE_SUCCESS;
+}
+
+static TEE_Result sec_hash_do_update(struct crypto_hash_ctx *ctx,
+				     const uint8_t *data, size_t len)
+{
+	struct crypto_hash *hash = NULL;
+	struct hashctx *hash_ctx = NULL;
+
+	if (!len) {
+		IMSG("This is 0 len task, skip");
+		return TEE_SUCCESS;
+	}
+
+	if (!ctx || (!data && len)) {
+		EMSG("Invalid input parameters");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	hash = to_hash_ctx(ctx);
+	hash_ctx = hash->ctx;
+
+	return hisi_sec_digest_do_update(hash_ctx, data, len);
+}
+
+TEE_Result hisi_sec_digest_do_final(struct hashctx *hash_ctx, uint8_t *digest,
+				    size_t len)
+{
+	TEE_Result ret = TEE_SUCCESS;
+
+	if (!digest || len == 0) {
+		EMSG("Invalid input parameters");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (hash_ctx->mac_len & WORD_ALIGNMENT_MASK) {
+		EMSG("Invalid digest out_bytes");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	hash_ctx->has_next = false;
+	ret = sec_digest_do_task(hash_ctx->qp, hash_ctx);
+	if (ret) {
+		EMSG("Fail to do digest task! ret = %#"PRIx32, ret);
+		return ret;
+	}
+
+	memcpy(digest, hash_ctx->out, MIN(hash_ctx->mac_len, len));
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result sec_hash_do_final(struct crypto_hash_ctx *ctx,
+				    uint8_t *digest, size_t len)
+{
+	struct crypto_hash *hash = to_hash_ctx(ctx);
+	struct hashctx *hash_ctx = hash->ctx;
+
+	return hisi_sec_digest_do_final(hash_ctx, digest, len);
+}
+
+void hisi_sec_digest_ctx_free(struct hashctx *hash_ctx)
+{
+	hisi_qm_release_qp(hash_ctx->qp);
+
+	free(hash_ctx->in);
+	hash_ctx->in = NULL;
+
+	memzero_explicit(hash_ctx->key, SEC_DIGEST_MAX_KEY_SIZE);
+
+	free(hash_ctx);
+}
+
+static void sec_hash_ctx_free(struct crypto_hash_ctx *ctx)
+{
+	struct crypto_hash *hash = NULL;
+	struct hashctx *hash_ctx = NULL;
+
+	if (!ctx)
+		return;
+
+	hash = to_hash_ctx(ctx);
+	hash_ctx = hash->ctx;
+	if (!hash_ctx)
+		return;
+	hisi_sec_digest_ctx_free(hash_ctx);
+
+	hash->ctx = NULL;
+
+	free(hash);
+}
+
+void hisi_sec_digest_copy_state(struct hashctx *out_hash_ctx,
+				struct hashctx *in_hash_ctx)
+{
+	out_hash_ctx->iv_len = in_hash_ctx->iv_len;
+	out_hash_ctx->buf_len = in_hash_ctx->buf_len;
+	out_hash_ctx->key_len = in_hash_ctx->key_len;
+	out_hash_ctx->has_next = in_hash_ctx->has_next;
+	out_hash_ctx->long_data_len = in_hash_ctx->long_data_len;
+
+	if (in_hash_ctx->in) {
+		out_hash_ctx->in = malloc(out_hash_ctx->buf_len);
+		if (!out_hash_ctx->in) {
+			EMSG("Fail to alloc in buf");
+			return;
+		}
+		out_hash_ctx->in_dma = virt_to_phys(out_hash_ctx->in);
+		if (!out_hash_ctx->in_dma) {
+			free(out_hash_ctx->in);
+			out_hash_ctx->in = NULL;
+			EMSG("Fail to get in_dma");
+			return;
+		}
+		out_hash_ctx->in_len = in_hash_ctx->in_len;
+		memcpy(out_hash_ctx->in, in_hash_ctx->in,
+		       out_hash_ctx->buf_len);
+	}
+
+	memcpy(out_hash_ctx->iv, in_hash_ctx->iv, out_hash_ctx->iv_len);
+	memcpy(out_hash_ctx->key, in_hash_ctx->key, out_hash_ctx->key_len);
+}
+
+static void sec_hash_copy_state(struct crypto_hash_ctx *out_ctx,
+				struct crypto_hash_ctx *in_ctx)
+{
+	struct crypto_hash *out_hash = NULL;
+	struct crypto_hash *in_hash = NULL;
+	struct hashctx *out_hash_ctx = NULL;
+	struct hashctx *in_hash_ctx = NULL;
+
+	if (!out_ctx || !in_ctx) {
+		EMSG("Invalid input parameters");
+		return;
+	}
+
+	out_hash = to_hash_ctx(out_ctx);
+	in_hash = to_hash_ctx(in_ctx);
+
+	out_hash_ctx = out_hash->ctx;
+	in_hash_ctx = in_hash->ctx;
+
+	hisi_sec_digest_copy_state(out_hash_ctx, in_hash_ctx);
+}
+
+static struct crypto_hash_ops hash_ops = {
+	.init = sec_hash_initialize,
+	.update = sec_hash_do_update,
+	.final = sec_hash_do_final,
+	.free_ctx = sec_hash_ctx_free,
+	.copy_state = sec_hash_copy_state,
+};
+
+static size_t sec_hash_get_mac_len(uint32_t type)
+{
+	switch (type) {
+	case TEE_ALG_MD5:
+	case TEE_ALG_HMAC_MD5:
+		return HASH_MAC_LEN128;
+	case TEE_ALG_SHA1:
+	case TEE_ALG_HMAC_SHA1:
+		return HASH_MAC_LEN160;
+	case TEE_ALG_SHA224:
+	case TEE_ALG_HMAC_SHA224:
+		return HASH_MAC_LEN224;
+	case TEE_ALG_SM3:
+	case TEE_ALG_HMAC_SM3:
+	case TEE_ALG_SHA256:
+	case TEE_ALG_HMAC_SHA256:
+		return HASH_MAC_LEN256;
+	case TEE_ALG_SHA384:
+	case TEE_ALG_HMAC_SHA384:
+		return HASH_MAC_LEN384;
+	case TEE_ALG_SHA512:
+	case TEE_ALG_HMAC_SHA512:
+		return HASH_MAC_LEN512;
+	default:
+		return 0;
+	}
+}
+
+static TEE_Result sec_hash_get_dma(struct hashctx *hash_ctx)
+{
+	hash_ctx->key_dma = virt_to_phys(hash_ctx->key);
+	if (!hash_ctx->key_dma) {
+		EMSG("Fail to get key_dma");
+		return TEE_ERROR_STORAGE_NO_SPACE;
+	}
+
+	hash_ctx->iv_dma = virt_to_phys(hash_ctx->iv);
+	if (!hash_ctx->iv_dma) {
+		EMSG("Fail to get iv_dma");
+		return TEE_ERROR_STORAGE_NO_SPACE;
+	}
+
+	hash_ctx->out_dma = virt_to_phys(hash_ctx->out);
+	if (!hash_ctx->out_dma) {
+		EMSG("Fail to get out_dma");
+		return TEE_ERROR_STORAGE_NO_SPACE;
+	}
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result hisi_sec_hash_ctx_init(struct hashctx *hash_ctx, uint32_t algo)
+{
+	TEE_Result ret = TEE_SUCCESS;
+
+	hash_ctx->mac_len = sec_hash_get_mac_len(algo);
+	if (!hash_ctx->mac_len) {
+		EMSG("Invalid algo type %#"PRIx32, algo);
+		return TEE_ERROR_NOT_IMPLEMENTED;
+	}
+
+	hash_ctx->algo = algo;
+	hash_ctx->mode = algo >> HASH_MODE_OFFSET;
+
+	ret = sec_hash_get_dma(hash_ctx);
+	if (ret)
+		return ret;
+
+	hash_ctx->qp = sec_create_qp(HISI_QM_CHANNEL_TYPE0);
+	if (!hash_ctx->qp) {
+		EMSG("Fail to create hash qp");
+		return TEE_ERROR_BUSY;
+	}
+
+	if (hash_ctx->qp->qm->version == HISI_QM_HW_V2) {
+		hash_ctx->qp->fill_sqe = sec_digest_fill_sqe;
+		hash_ctx->qp->parse_sqe = sec_parse_digest_sqe;
+	} else {
+		hash_ctx->qp->fill_sqe = sec_digest_fill_bd3_sqe;
+		hash_ctx->qp->parse_sqe = sec_parse_digest_bd3_sqe;
+	}
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result sec_hash_ctx_allocate(struct crypto_hash_ctx **ctx,
+					uint32_t algo)
+{
+	struct crypto_hash *hash = NULL;
+	struct hashctx *hash_ctx = NULL;
+	TEE_Result ret = TEE_SUCCESS;
+
+	if (!ctx) {
+		EMSG("Ctx is NULL");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	hash = calloc(1, sizeof(*hash));
+	if (!hash) {
+		EMSG("Fail to alloc hash");
+		return TEE_ERROR_STORAGE_NO_SPACE;
+	}
+
+	hash_ctx = calloc(1, sizeof(*hash_ctx));
+	if (!hash_ctx) {
+		EMSG("Fail to alloc hash_ctx");
+		ret = TEE_ERROR_STORAGE_NO_SPACE;
+		goto free_hash;
+	}
+
+	ret = hisi_sec_hash_ctx_init(hash_ctx, algo);
+	if (ret)
+		goto free_ctx;
+
+	hash->hash_ctx.ops = &hash_ops;
+	hash->ctx = hash_ctx;
+	*ctx = &hash->hash_ctx;
+
+	return TEE_SUCCESS;
+
+free_ctx:
+	free(hash_ctx);
+free_hash:
+	free(hash);
+
+	return ret;
+}
+
+static TEE_Result sec_hash_init(void)
+{
+	TEE_Result ret = TEE_SUCCESS;
+
+	ret = drvcrypt_register_hash(&sec_hash_ctx_allocate);
+	if (ret)
+		EMSG("Sec hash register to crypto fail");
+
+	return ret;
+}
+driver_init(sec_hash_init);

core/drivers/crypto/hisilicon/crypto.mk
@@ -1,8 +1,10 @@
-ifeq ($(CFG_HISILICON_ACC_V3), y)
+ifeq ($(CFG_HISILICON_CRYPTO_DRIVER), y)
 $(call force,CFG_CRYPTO_DRIVER,y)
 CFG_CRYPTO_DRIVER_DEBUG ?= 0
 
 $(call force, CFG_CRYPTO_DRV_ACIPHER,y,Mandated by CFG_HISILICON_CRYPTO_DRIVER)
+$(call force, CFG_CRYPTO_DRV_HASH,y,Mandated by CFG_HISILICON_CRYPTO_DRIVER)
+$(call force, CFG_CRYPTO_DRV_MAC,y,Mandated by CFG_HISILICON_CRYPTO_DRIVER)
 
 ifeq ($(CFG_HISILICON_ACC_V3), y)
 $(call force, CFG_CRYPTO_DRV_DH,y,Mandated by CFG_HISILICON_ACC_V3)

core/drivers/crypto/hisilicon/include/sec_hash.h
@@ -1,0 +1,114 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2022-2024 HiSilicon Limited. */
+#ifndef __SEC_HASH_H__
+#define __SEC_HASH_H__
+
+#include <stdint.h>
+#include <tee_api_types.h>
+
+#define WORD_ALIGNMENT_MASK		0x3
+#define HASH_MODE_OFFSET		28
+#define WCRYPTO_DIGEST_HMAC		3
+#define WCRYPTO_DIGEST_NORMAL		5
+#define MAX_AUTH_LENGTH			16776704
+#define HASH_MAC_LEN128			16
+#define HASH_MAC_LEN160			20
+#define HASH_MAC_LEN224			28
+#define HASH_MAC_LEN256			32
+#define HASH_MAC_LEN384			48
+#define HASH_MAC_LEN512			64
+#define SEC_DIGEST_MAX_KEY_SIZE		128
+#define SEC_DIGEST_MAX_MAC_SIZE		64
+#define SEC_DIGEST_MAX_IV_SIZE		64
+#define SMALL_BUF_SIZE			0x1000
+
+struct hashctx {
+	uint8_t key[SEC_DIGEST_MAX_KEY_SIZE];
+	uint8_t iv[SEC_DIGEST_MAX_IV_SIZE];
+	uint8_t out[SEC_DIGEST_MAX_MAC_SIZE];
+	bool has_next;
+	uint8_t mode;
+	uint32_t algo;
+	uint32_t scene;
+	struct hisi_qp *qp;
+	uint8_t *in;
+	uint64_t in_dma;
+	size_t buf_len;
+	size_t in_len;
+	uint64_t out_dma;
+	size_t mac_len;
+	uint64_t key_dma;
+	size_t key_len;
+	uint64_t iv_dma;
+	size_t iv_len;
+	uint64_t long_data_len;
+};
+
+/*
+ * Format the hash context to keep the reference to the
+ * operation driver
+ */
+struct crypto_hash {
+	struct crypto_hash_ctx hash_ctx; /* Crypto Hash API context */
+	struct hashctx *ctx; /* Hash Context */
+};
+
+/*
+ * Format the hmac context to keep the reference to the
+ * operation driver
+ */
+struct crypto_hmac {
+	struct crypto_mac_ctx hmac_op; /* Crypto Hash API context */
+	struct hashctx *ctx; /* Hash Context */
+};
+
+enum A_ALG {
+	A_ALG_SHA1     = 0x0,
+	A_ALG_SHA256   = 0x1,
+	A_ALG_MD5      = 0x2,
+	A_ALG_SHA224   = 0x3,
+	A_ALG_SHA384   = 0x4,
+	A_ALG_SHA512   = 0x5,
+	A_ALG_SHA512_224 = 0x6,
+	A_ALG_SHA512_256 = 0x7,
+	A_ALG_HMAC_SHA1   = 0x10,
+	A_ALG_HMAC_SHA256 = 0x11,
+	A_ALG_HMAC_MD5      = 0x12,
+	A_ALG_HMAC_SHA224 = 0x13,
+	A_ALG_HMAC_SHA384 = 0x14,
+	A_ALG_HMAC_SHA512 = 0x15,
+	A_ALG_HMAC_SHA512_224 = 0x16,
+	A_ALG_HMAC_SHA512_256 = 0x17,
+	A_ALG_AES_XCBC_MAC_96  = 0x20,
+	A_ALG_AES_XCBC_PRF_128 = 0x20,
+	A_ALG_AES_CMAC = 0x21,
+	A_ALG_AES_GMAC = 0x22,
+	A_ALG_SM3       = 0x25,
+	A_ALG_HMAC_SM3 = 0x26,
+	A_ALG_MAX
+};
+
+enum {
+	AI_GEN_INNER        = 0x0,
+	AI_GEN_IVIN_ADDR    = 0x1,
+	AI_GEN_CAL_IV_ADDR  = 0x2,
+	AI_GEN_TRNG         = 0x3,
+};
+
+enum {
+	AUTHPAD_PAD,
+	AUTHPAD_NOPAD,
+};
+
+TEE_Result hisi_sec_hash_ctx_init(struct hashctx *hash_ctx, uint32_t algo);
+TEE_Result hisi_sec_digest_ctx_init(struct hashctx *hash_ctx,
+				    const uint8_t *key, size_t len);
+TEE_Result hisi_sec_digest_do_update(struct hashctx *hashctx,
+				     const uint8_t *data, size_t len);
+TEE_Result hisi_sec_digest_do_final(struct hashctx *hashctx, uint8_t *digest,
+				    size_t len);
+void hisi_sec_digest_ctx_free(struct hashctx *hash_ctx);
+void hisi_sec_digest_copy_state(struct hashctx *out_hash_ctx,
+				struct hashctx *in_hash_ctx);
+
+#endif

core/drivers/crypto/hisilicon/include/sec_main.h
@@ -49,6 +49,293 @@
 #define SEC_ICV_LEN_OFFSET_V3	4
 #define SEC_DK_LEN_OFFSET_V3	16
 #define SEC_KEY_SEL_OFFSET_V3	21
+#define SEC_GET_FIELD(val, mask, shift) (((val) & (mask)) >> (shift))
+
+struct hisi_sec_sqe_type2 {
+	/*
+	 * mac_len: 0~4 bits
+	 * a_key_len: 5~10 bits
+	 * a_alg: 11~16 bits
+	 */
+	uint32_t mac_key_alg;
+
+	/*
+	 * c_icv_len: 0~5 bits
+	 * c_width: 6~8 bits
+	 * c_key_len: 9~11 bits
+	 * c_mode: 12~15 bits
+	 */
+	uint16_t icvw_kmode;
+
+	/* c_alg: 0~3 bits */
+	uint8_t c_alg;
+
+	uint8_t rsvd4;
+	/*
+	 * a_len: 0~23 bits
+	 * iv_offset_l: 24~31 bits
+	 */
+	uint32_t alen_ivllen;
+
+	/*
+	 * c_len: 0~23 bits
+	 * iv_offset_h: 24~31 bits
+	 */
+	uint32_t clen_ivhlen;
+
+	uint16_t auth_src_offset;
+	uint16_t cipher_src_offset;
+	uint16_t cs_ip_header_offset;
+	uint16_t cs_udp_header_offset;
+	uint16_t pass_word_len;
+	uint16_t dk_len;
+	uint8_t salt3;
+	uint8_t salt2;
+	uint8_t salt1;
+	uint8_t salt0;
+
+	uint16_t tag;
+	uint16_t rsvd5;
+
+	/*
+	 * c_pad_type: 0~3 bits
+	 * c_pad_len: 4~11 bits
+	 * c_pad_data_type: 12~15 bits
+	 */
+	uint16_t cph_pad;
+	/* c_pad_len_field: 0~1 bits */
+	uint16_t c_pad_len_field;
+
+	uint64_t long_a_data_len;
+	uint64_t a_ivin_addr;
+	uint64_t a_key_addr;
+	uint64_t mac_addr;
+	uint64_t c_ivin_addr;
+	uint64_t c_key_addr;
+	uint64_t data_src_addr;
+	uint64_t data_dst_addr;
+
+	/*
+	 * done: 0 bit
+	 * icv: 1~3 bits
+	 * csc: 4~6 bits
+	 * flag: 7~10 bits
+	 */
+	uint16_t done_flag;
+
+	uint8_t error_type;
+	uint8_t warning_type;
+	uint8_t mac_i3;
+	uint8_t mac_i2;
+	uint8_t mac_i1;
+	uint8_t mac_i0;
+	uint16_t check_sum_i;
+	uint8_t tls_pad_len_i;
+	uint8_t rsvd12;
+	uint32_t counter;
+};
+
+struct hisi_sec_sqe {
+	/*
+	 * type:  0~3 bits;
+	 * cipher: 4~5 bits;
+	 * auth: 6~7 bits;
+	 */
+	uint8_t type_auth_cipher;
+	/*
+	 * seq: 0 bits;
+	 * de: 1~2 bits;
+	 * scene: 3~6 bits;
+	 * src_addr_type: 7 bits;
+	 */
+	uint8_t sds_sa_type;
+	/*
+	 * src_addr_type: 0~1 bits not used now.
+	 * dst_addr_type: 2~4 bits;
+	 * mac_addr_type: 5~7 bits;
+	 */
+	uint8_t sdm_addr_type;
+
+	uint8_t rsvd0;
+	/*
+	 * nonce_len(type): 0~3 bits;
+	 * huk: 4 bit;
+	 * key_s: 5 bit
+	 * ci_gen: 6~7 bits
+	 */
+	uint8_t huk_ci_key;
+	/*
+	 * ai_gen: 0~1 bits;
+	 * a_pad : 2~3 bits;
+	 * c_s : 4~5 bits;
+	 */
+	uint8_t ai_apd_cs;
+	/*
+	 * rhf(type2): 0 bit;
+	 * c_key_type: 1~2 bits;
+	 * a_key_type: 3~4 bits
+	 * write_frame_len(type2): 5~7bits;
+	 */
+	uint8_t rca_key_frm;
+
+	uint8_t iv_tls_ld;
+	struct hisi_sec_sqe_type2 type2; /* the other scene */
+};
+
+struct bd3_stream_scene {
+	uint64_t c_ivin_addr;
+	uint64_t long_a_data_len;
+
+	/*
+	 * auth_pad: 0~1 bits
+	 * stream_protocol: 2~4 bits
+	 * reserved: 5~7 bits
+	 */
+	uint8_t auth_pad;
+	uint8_t plaintext_type;
+	uint16_t pad_len_1p3;
+} __packed __aligned(4);
+
+struct bd3_no_scene {
+	uint64_t c_ivin_addr;
+	uint32_t rsvd0;
+	uint32_t rsvd1;
+	uint32_t rsvd2;
+} __packed __aligned(4);
+
+struct bd3_pbkdf2_scene {
+	uint64_t c_ivin_addr;
+
+	/*
+	 * pbkdf2_salt_len: 0~23 bits
+	 * rsvd0: 24~31 bits
+	 */
+	uint32_t pbkdf2_salt_len;
+
+	/*
+	 * c_num: 0~23 bits
+	 * rsvd1: 24~31 bits
+	 */
+	uint32_t c_num;
+
+	/*
+	 * pass_word_len: 0~15 bits
+	 * dk_len: 16~31 bits
+	 */
+	uint32_t pass_word_dk_len;
+} __packed __aligned(4);
+
+struct hisi_sec_bd3_sqe {
+	/*
+	 * type: 0~3 bit
+	 * bd_invalid: 4 bit
+	 * scene: 5~8 bit
+	 * de: 9~10 bit
+	 * src_addr_type: 11~13 bit
+	 * dst_addr_type: 14~16 bit
+	 * mac_addr_type: 17~19 bit
+	 * reserved: 20~31 bits
+	 */
+	uint32_t bd_param;
+
+	/*
+	 * cipher: 0~1 bits
+	 * ci_gen: 2~3 bit
+	 * c_icv_len: 4~9 bit
+	 * c_width: 10~12 bits
+	 * c_key_len: 13~15 bits
+	 */
+	uint16_t c_icv_key;
+
+	/*
+	 * c_mode : 0~3 bits
+	 * c_alg : 4~7 bits
+	 */
+	uint8_t c_mode_alg;
+
+	/*
+	 * nonce_len : 0~3 bits
+	 * huk : 4 bits
+	 * cal_iv_addr_en : 5 bits
+	 * seq : 6 bits
+	 * reserved : 7 bits
+	 */
+	uint8_t huk_iv_seq;
+
+	uint64_t tag;
+	uint64_t data_src_addr;
+	uint64_t a_key_addr;
+	uint64_t a_ivin_addr;
+	uint64_t rsvd;
+	uint64_t c_key_addr;
+
+	/*
+	 * auth: 0~1 bits
+	 * ai_gen: 2~3 bits
+	 * mac_len: 4~8 bits
+	 * akey_len: 9~14 bits
+	 * a_alg: 15~20 bits
+	 * key_sel: 21~24 bits
+	 * ctr_count_mode/sm4_xts: 25~26 bits
+	 * sva_prefetch: 27 bits
+	 * key_wrap_num:28~30 bits
+	 * update_key: 31 bits
+	 */
+	uint32_t auth_mac_key;
+	uint32_t salt;
+	uint16_t auth_src_offset;
+	uint16_t cipher_src_offset;
+
+	/*
+	 * auth_len: 0~23 bit
+	 * auth_key_offset: 24~31 bits
+	 */
+	uint32_t a_len_key;
+
+	/*
+	 * cipher_len: 0~23 bit
+	 * auth_ivin_offset: 24~31 bits
+	 */
+	uint32_t c_len_ivin;
+	uint64_t data_dst_addr;
+	uint64_t mac_addr;
+	union {
+		struct bd3_stream_scene stream_scene;
+		struct bd3_no_scene no_scene;
+		struct bd3_pbkdf2_scene pbkdf2_scene;
+	};
+
+	/*
+	 * done: 0 bit
+	 * icv: 1~3 bit
+	 * csc: 4~6 bit
+	 * flag: 7~10 bit
+	 * reserved: 11~15 bit
+	 */
+	uint16_t done_flag;
+	uint8_t error_type;
+	uint8_t warning_type;
+	uint64_t kek_key_addr;
+	uint32_t counter;
+} __packed __aligned(4);
+
+enum sec_bd_type {
+	BD_TYPE1 = 0x1,
+	BD_TYPE2 = 0x2,
+	BD_TYPE3 = 0x3,
+};
+
+enum sec_bd_scene {
+	SCENE_NOTHING = 0x0,
+	SCENE_STREAM = 0x7,
+	SCENE_PBKDF2 = 0x8,
+};
+
+enum sec_auth_dir {
+	NO_AUTH,
+	AUTH_MAC_CALCULATE,
+	AUTH_MAC_VERIFY,
+};
 
 /*
  * Create task queue pair for SEC.

commit 2c833f568eec05c922a7d8ef4114edd37c849f41

drivers: crypto: hisilicon: fix incorrect use of error code
Fix incorrect use of memory-related error code in hash algorithm.

Fixes: 94c8a3397ec4 ("drivers: crypto: hisilicon:Add HASH and HMAC algorithm")
Signed-off-by: leisen <leisen1@huawei.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/crypto/hisilicon/sec_hash.c
@@ -335,15 +335,9 @@ TEE_Result hisi_sec_digest_do_update(struct hashctx *hash_ctx,
 		hash_ctx->in = malloc(hash_ctx->buf_len);
 		if (!hash_ctx->in) {
 			EMSG("Fail to alloc in data buf");
-			return TEE_ERROR_STORAGE_NO_SPACE;
+			return TEE_ERROR_OUT_OF_MEMORY;
 		}
 		hash_ctx->in_dma = virt_to_phys(hash_ctx->in);
-		if (!hash_ctx->in_dma) {
-			free(hash_ctx->in);
-			hash_ctx->in = NULL;
-			EMSG("Fail to get in_dma");
-			return TEE_ERROR_STORAGE_NO_SPACE;
-		}
 	}
 
 	while (len > 0) {
@@ -478,12 +472,6 @@ void hisi_sec_digest_copy_state(struct hashctx *out_hash_ctx,
 			return;
 		}
 		out_hash_ctx->in_dma = virt_to_phys(out_hash_ctx->in);
-		if (!out_hash_ctx->in_dma) {
-			free(out_hash_ctx->in);
-			out_hash_ctx->in = NULL;
-			EMSG("Fail to get in_dma");
-			return;
-		}
 		out_hash_ctx->in_len = in_hash_ctx->in_len;
 		memcpy(out_hash_ctx->in, in_hash_ctx->in,
 		       out_hash_ctx->buf_len);
@@ -556,19 +544,19 @@ static TEE_Result sec_hash_get_dma(struct hashctx *hash_ctx)
 	hash_ctx->key_dma = virt_to_phys(hash_ctx->key);
 	if (!hash_ctx->key_dma) {
 		EMSG("Fail to get key_dma");
-		return TEE_ERROR_STORAGE_NO_SPACE;
+		return TEE_ERROR_GENERIC;
 	}
 
 	hash_ctx->iv_dma = virt_to_phys(hash_ctx->iv);
 	if (!hash_ctx->iv_dma) {
 		EMSG("Fail to get iv_dma");
-		return TEE_ERROR_STORAGE_NO_SPACE;
+		return TEE_ERROR_GENERIC;
 	}
 
 	hash_ctx->out_dma = virt_to_phys(hash_ctx->out);
 	if (!hash_ctx->out_dma) {
 		EMSG("Fail to get out_dma");
-		return TEE_ERROR_STORAGE_NO_SPACE;
+		return TEE_ERROR_GENERIC;
 	}
 
 	return TEE_SUCCESS;
@@ -623,13 +611,13 @@ static TEE_Result sec_hash_ctx_allocate(struct crypto_hash_ctx **ctx,
 	hash = calloc(1, sizeof(*hash));
 	if (!hash) {
 		EMSG("Fail to alloc hash");
-		return TEE_ERROR_STORAGE_NO_SPACE;
+		return TEE_ERROR_OUT_OF_MEMORY;
 	}
 
 	hash_ctx = calloc(1, sizeof(*hash_ctx));
 	if (!hash_ctx) {
 		EMSG("Fail to alloc hash_ctx");
-		ret = TEE_ERROR_STORAGE_NO_SPACE;
+		ret = TEE_ERROR_OUT_OF_MEMORY;
 		goto free_hash;
 	}
 
@@ -657,7 +645,7 @@ static TEE_Result sec_hash_init(void)
 
 	ret = drvcrypt_register_hash(&sec_hash_ctx_allocate);
 	if (ret)
-		EMSG("Sec hash register to crypto fail");
+		EMSG("Sec hash register to crypto fail ret=%#"PRIx32, ret);
 
 	return ret;
 }