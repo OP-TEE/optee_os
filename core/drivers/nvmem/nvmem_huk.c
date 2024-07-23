OP-TEE/optee_os

commit fc7169686724f2614043200f79a90f36c0459c49

drivers: nvmem: add nvmem-huk driver
This driver is meant to read the OTP unique hardware key from a
NVMEM controller. It uses the nvmem framework to read the NVMEM
cells from the device tree.

Signed-off-by: Thomas Perrot <thomas.perrot@bootlin.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/drivers/nvmem/nvmem_huk.c
@@ -1,0 +1,62 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2023, Microchip
+ */
+
+#include <drivers/nvmem.h>
+#include <io.h>
+#include <kernel/dt.h>
+#include <kernel/huk_subkey.h>
+#include <kernel/tee_common_otp.h>
+#include <malloc.h>
+#include <tee_api_defines.h>
+#include <tee_api_types.h>
+#include <types_ext.h>
+
+static struct nvmem_cell *huk_cell;
+
+TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	uint8_t *huk = NULL;
+	size_t len = 0;
+
+	res = nvmem_cell_malloc_and_read(huk_cell, &huk);
+	if (res)
+		goto out_free_cell;
+
+	if (len != HW_UNIQUE_KEY_LENGTH) {
+		res = TEE_ERROR_GENERIC;
+		goto out_free_cell;
+	}
+
+	memcpy(hwkey->data, huk, HW_UNIQUE_KEY_LENGTH);
+
+out_free_cell:
+	nvmem_put_cell(huk_cell);
+
+	return res;
+}
+
+static TEE_Result nvmem_huk_get_cell(const void *fdt, int node)
+{
+	return nvmem_get_cell_by_name(fdt, node, "hw_unique_key", &huk_cell);
+}
+
+static TEE_Result nvmem_huk_probe(const void *fdt, int node,
+				  const void *compat_data __unused)
+{
+	return nvmem_huk_get_cell(fdt, node);
+}
+
+static const struct dt_device_match nvmem_huk_match_table[] = {
+	{ .compatible = "optee,nvmem-huk" },
+	{ }
+};
+
+DEFINE_DT_DRIVER(nvmem_huk_dt_driver) = {
+	.name = "nvmem_huk",
+	.type = DT_DRIVER_NVMEM,
+	.match_table = nvmem_huk_match_table,
+	.probe = nvmem_huk_probe,
+};

core/drivers/nvmem/sub.mk
@@ -1,3 +1,4 @@
 srcs-y += nvmem.c
 srcs-$(CFG_ATMEL_SFC) += atmel_sfc.c
 srcs-$(CFG_NVMEM_DIE_ID) += nvmem_die_id.c
+srcs-$(CFG_NVMEM_HUK) += nvmem_huk.c

commit e5d1f8fec68189e61271cab8de584df3f3d8b8c9

drivers: nvmem: nvmem-huk: fix cell size and memory allocation issues
No longer free the HUK cell after at the end of the
"tee_otp_get_hw_unique_key" execution, because for the regression
test 4013, the "tee_otp_get_hw_unique_key" callback isn't
re-called then an invalid huk_cell pointer is used, leading the
following issue:

E/TC:0 0 Core data-abort at address 0x0 (translation fault)
E/TC:0 0  fsr 0x00000005  ttbr0 0x20087859  ttbr1 0x20080059  cidr 0x2
E/TC:0 0  cpu #0          cpsr 0x60000133
E/TC:0 0  r0 0x2007d8e8      r4 0x2007d8e8    r8 0x00102010   r12 0xfffc2a40
E/TC:0 0  r1 0x20089528      r5 0x2008954c    r9 0x00000010    sp 0x20089508
E/TC:0 0  r2 0x20059164      r6 0x20060188   r10 0x00000020    lr 0x2000bcb3
E/TC:0 0  r3 0x00000000      r7 0x2008954c   r11 0x20089684    pc 0x2000baa2
E/TC:0 0 TEE load address @ 0x20000000
E/TC:0 0 Call stack:
E/TC:0 0  0x2000baa2 nvmem_cell_malloc_and_read at core/drivers/nvmem/nvmem.c:62
E/TC:0 0  0x2000bcb3 tee_otp_get_hw_unique_key at core/drivers/nvmem/nvmem_huk.c:23
E/TC:0 0  0x20010e9f __huk_subkey_derive at core/kernel/huk_subkey.c:78
E/TC:0 0  0x200178c9 system_derive_ta_unique_key at core/pta/system.c:122
E/TC:0 0  0x2001382b pseudo_ta_enter_invoke_cmd at core/kernel/pseudo_ta.c:209
E/TC:0 0  0x20010d95 tee_ta_invoke_command at core/kernel/tee_ta_manager.c:765
E/TC:0 0  0x20019567 syscall_invoke_ta_command at core/tee/tee_svc.c:871
E/TC:0 0  0x20002178 scall_do_call at core/arch/arm/kernel/arch_scall_a32.S:54
E/TC:0 0 Panic 'unhandled pageable abort' at core/arch/arm/kernel/abort.c:582 <abort_handler>
E/TC:0 0 TEE load address @ 0x20000000
E/TC:0 0 Call stack:
E/TC:0 0  0x200039b1 print_kernel_stack at core/arch/arm/kernel/unwind_arm32.c:109
E/TC:0 0  0x2000ff05 __do_panic at core/kernel/panic.c:80
E/TC:0 0  0x20002f01 get_fault_type at core/arch/arm/kernel/abort.c:498
E/TC:0 0  0x2000077c thread_excp_vect at core/arch/arm/kernel/thread_a32.S:758

Moreover, the length of the HW unique key must be 16 bytes.
But the size of NVMEM cells can be larger than 16 bytes, for
example on samad5 the SFC is using 32 bytes cells. These changes
also allow the use of a NVMEM cell larger to the HW unique key
length, using the first 16 bytes of a cell.

Finally, the instance of the NVMEM cell data will be allocated once,
in the probe. Before, a new instance was allocated at each call
to tee_otp_get_hw_unique_key().

Fixes: fc7169686724 ("drivers: nvmem: add nvmem-huk driver")

core/drivers/nvmem/nvmem_huk.c
@@ -8,45 +8,55 @@
 #include <kernel/dt.h>
 #include <kernel/huk_subkey.h>
 #include <kernel/tee_common_otp.h>
+#include <libfdt.h>
 #include <malloc.h>
 #include <tee_api_defines.h>
 #include <tee_api_types.h>
+#include <trace.h>
 #include <types_ext.h>
 
-static struct nvmem_cell *huk_cell;
+static uint8_t *huk;
 
 TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
 {
-	TEE_Result res = TEE_ERROR_GENERIC;
-	uint8_t *huk = NULL;
-	size_t len = 0;
-
-	res = nvmem_cell_malloc_and_read(huk_cell, &huk);
-	if (res)
-		goto out_free_cell;
-
-	if (len != HW_UNIQUE_KEY_LENGTH) {
-		res = TEE_ERROR_GENERIC;
-		goto out_free_cell;
+	if (!huk) {
+		EMSG("no HUK");
+		return TEE_ERROR_GENERIC;
 	}
 
 	memcpy(hwkey->data, huk, HW_UNIQUE_KEY_LENGTH);
 
-out_free_cell:
-	nvmem_put_cell(huk_cell);
-
-	return res;
-}
-
-static TEE_Result nvmem_huk_get_cell(const void *fdt, int node)
-{
-	return nvmem_get_cell_by_name(fdt, node, "hw_unique_key", &huk_cell);
+	return TEE_SUCCESS;
 }
 
 static TEE_Result nvmem_huk_probe(const void *fdt, int node,
 				  const void *compat_data __unused)
 {
-	return nvmem_huk_get_cell(fdt, node);
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct nvmem_cell *cell = NULL;
+	uint8_t *data = NULL;
+
+	res = nvmem_get_cell_by_name(fdt, node, "hw_unique_key", &cell);
+	if (res)
+		return res;
+
+	if (cell->len < HW_UNIQUE_KEY_LENGTH) {
+		EMSG("cell %s is too small", fdt_get_name(fdt, node, NULL));
+		nvmem_put_cell(cell);
+		return TEE_ERROR_GENERIC;
+	}
+
+	if (cell->len > HW_UNIQUE_KEY_LENGTH)
+		IMSG("nvmem_huk: HUK truncated from %zu to %u bytes",
+		     cell->len, HW_UNIQUE_KEY_LENGTH);
+
+	res = nvmem_cell_malloc_and_read(cell, &data);
+	if (!res)
+		huk = data;
+
+	nvmem_put_cell(cell);
+
+	return res;
 }
 
 static const struct dt_device_match nvmem_huk_match_table[] = {