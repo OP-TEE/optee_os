OP-TEE/optee_os

commit a7990eb02b82e88f8474d7704713a6c207962cfb

plat-stm32mp1: set voltage list at pmic driver init
Bound stm32mp1_pmic supported voltage levels list to min/max voltage
level values set from the DT. This change free quite a bit of byte in
the heap for this platform.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.c
@@ -397,7 +397,8 @@ static int cmp_int_value(const void *a, const void *b)
 	return CMP_TRILEAN(*ia, *ib);
 }
 
-static size_t refine_levels_array(size_t count, int *levels_uv)
+static size_t refine_levels_array(size_t count, int *levels_uv,
+				  int min_uv, int max_uv)
 {
 	size_t n = 0;
 	size_t m = 0;
@@ -413,8 +414,21 @@ static size_t refine_levels_array(size_t count, int *levels_uv)
 			m++;
 		}
 	}
+	count = m + 1;
 
-	return m + 1;
+	for (n = count; n; n--)
+		if (levels_uv[n - 1] <= max_uv)
+			break;
+	count = n;
+
+	for (n = 0; n < count; n++)
+		if (levels_uv[n] >= min_uv)
+			break;
+	count -= n;
+
+	memmove(levels_uv, levels_uv + n, count * sizeof(*levels_uv));
+
+	return count;
 }
 
 static TEE_Result pmic_list_voltages(struct regulator *regulator,
@@ -443,7 +457,9 @@ static TEE_Result pmic_list_voltages(struct regulator *regulator,
 		for (n = 0; n < level_count; n++)
 			levels[n] = level_ref[n] * 1000;
 
-		level_count = refine_levels_array(level_count, levels);
+		level_count = refine_levels_array(level_count, levels,
+						  regulator->min_uv,
+						  regulator->max_uv);
 
 		/* Shrink levels array to not waste heap memory */
 		levels2 = realloc(levels, sizeof(*levels) * level_count);

commit 910a472f86055bca2e0d0a4a21f30fef474eed6b

plat-stm32mp1: fix use after free in PMIC driver
Fix PMIC regulator levels arrays handling that missed a pointer reset
after the buffer is freed. At runtime, pmic_list_voltages() handler
function uses that reference and is expected to allocate back the
buffer in case non-secure world requests voltage enumeration for the
related regulator.

Fixes: a7990eb02b82 ("plat-stm32mp1: set voltage list at pmic driver init")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.c
@@ -542,8 +542,10 @@ static TEE_Result release_voltage_lists(void)
 	for (n = 0; n < ARRAY_SIZE(pmic_regulators); n++) {
 		struct pmic_regulator_data *priv = pmic_regulators[n].priv;
 
-		if (priv && priv->voltages_level)
+		if (priv && priv->voltages_level) {
 			free(priv->voltages_level);
+			priv->voltages_level = NULL;
+		}
 	}
 
 	return TEE_SUCCESS;