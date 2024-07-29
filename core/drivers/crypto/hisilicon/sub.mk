2b1f0ea5a9905b38458e3a1cb8aa5efd5f9ca0e0
leisen <leisen1@huawei.com>
2024-07-29 20:29:12 +0800

    drivers: crypto: hisilicon: add authenc algorithm
    Add authenc algorithm for hisilicon SEC driver
    
    Signed-off-by: leisen <leisen1@huawei.com>

--- core/drivers/crypto/hisilicon/sub.mk
+++ core/drivers/crypto/hisilicon/sub.mk
@@ -3,6 +3,7 @@ srcs-y += sec_main.c
 srcs-y += sec_hash.c
 srcs-y += sec_hmac.c
 srcs-y += sec_cipher.c
+srcs-y += sec_authenc.c
 srcs-$(CFG_HISILICON_ACC_V3) += hpre_main.c
 srcs-$(CFG_HISILICON_ACC_V3) += hpre_dh.c
 srcs-$(CFG_HISILICON_ACC_V3) += hpre_ecc.c
