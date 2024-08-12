537a6576f0bbe70be63f98494dc291c57d4b9652
Jens Wiklander <jens.wiklander@linaro.org>
2024-08-12 20:05:05 +0200

    core: add call_{early,service,driver}_initcalls()
    Add more fine-grained replacements for call_initcalls() to enable
    initcalls at several separate stages.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/kernel/initcall.c
+++ core/kernel/initcall.c
@@ -8,23 +8,60 @@
 #include <trace.h>
 #include <kernel/linker.h>
 
+static void do_init_calls(const char *type __maybe_unused,
+			  const struct initcall *begin,
+			  const struct initcall *end)
+{
+	const struct initcall *call = NULL;
+	TEE_Result ret = TEE_SUCCESS;
+
+	for (call = begin; call < end; call++) {
+		DMSG("%s level %d %s()", type, call->level, call->func_name);
+		ret = call->func();
+		if (ret) {
+			EMSG("%s __text_start + 0x%08"PRIxVA" failed",
+			     type, (vaddr_t)call - VCORE_START_VA);
+		}
+	}
+}
+
+#define DO_INIT_CALLS(name) \
+	do_init_calls(#name, name##_begin, name##_end)
+
 /*
  * Note: this function is weak just to make it possible to exclude it from
  * the unpaged area.
  */
 void __weak call_preinitcalls(void)
 {
-	const struct initcall *call = NULL;
-	TEE_Result ret = TEE_SUCCESS;
+	DO_INIT_CALLS(preinitcall);
+}
 
-	for (call = preinitcall_begin; call < preinitcall_end; call++) {
-		DMSG("level %d %s()", call->level, call->func_name);
-		ret = call->func();
-		if (ret != TEE_SUCCESS) {
-			EMSG("Preinitcall __text_start + 0x%08" PRIxVA
-			     " failed", (vaddr_t)call - VCORE_START_VA);
-		}
-	}
+/*
+ * Note: this function is weak just to make it possible to exclude it from
+ * the unpaged area.
+ */
+void __weak call_early_initcalls(void)
+{
+	DO_INIT_CALLS(early_initcall);
+}
+
+/*
+ * Note: this function is weak just to make it possible to exclude it from
+ * the unpaged area.
+ */
+void __weak call_service_initcalls(void)
+{
+	DO_INIT_CALLS(service_initcall);
+}
+
+/*
+ * Note: this function is weak just to make it possible to exclude it from
+ * the unpaged area.
+ */
+void __weak call_driver_initcalls(void)
+{
+	DO_INIT_CALLS(driver_initcall);
 }
 
 /*
@@ -33,17 +70,9 @@ void __weak call_preinitcalls(void)
  */
 void __weak call_initcalls(void)
 {
-	const struct initcall *call = NULL;
-	TEE_Result ret = TEE_SUCCESS;
-
-	for (call = initcall_begin; call < initcall_end; call++) {
-		DMSG("level %d %s()", call->level, call->func_name);
-		ret = call->func();
-		if (ret != TEE_SUCCESS) {
-			EMSG("Initcall __text_start + 0x%08" PRIxVA
-			     " failed", (vaddr_t)call - VCORE_START_VA);
-		}
-	}
+	call_early_initcalls();
+	call_service_initcalls();
+	call_driver_initcalls();
 }
 
 /*
@@ -52,15 +81,5 @@ void __weak call_initcalls(void)
  */
 void __weak call_finalcalls(void)
 {
-	const struct initcall *call = NULL;
-	TEE_Result ret = TEE_SUCCESS;
-
-	for (call = finalcall_begin; call < finalcall_end; call++) {
-		DMSG("level %d %s()", call->level, call->func_name);
-		ret = call->func();
-		if (ret != TEE_SUCCESS) {
-			EMSG("Finalcall __text_start + 0x%08" PRIxVA
-			     " failed", (vaddr_t)call - VCORE_START_VA);
-		}
-	}
+	DO_INIT_CALLS(finalcall);
 }
