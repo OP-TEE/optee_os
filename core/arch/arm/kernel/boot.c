27ed6973310a7bc160eced27019624ebac2055d6
Jens Wiklander <jens.wiklander@linaro.org>
2024-08-13 08:55:34 +0200

    core: arm: call call_driver_initcalls() late
    Calls call_early_initcalls() and call_service_initcalls() directly
    instead of call_initcalls() from init_tee_runtime(). This allows
    call_driver_initcalls() to be called with PAUTH enabled.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/arch/arm/kernel/boot.c
+++ core/arch/arm/kernel/boot.c
@@ -929,7 +929,8 @@ void init_tee_runtime(void)
 	 */
 	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION))
 		call_preinitcalls();
-	call_initcalls();
+	call_early_initcalls();
+	call_service_initcalls();
 
 	/*
 	 * These two functions uses crypto_rng_read() to initialize the
@@ -1069,6 +1070,8 @@ void __weak boot_init_primary_late(unsigned long fdt __unused,
  */
 void __weak boot_init_primary_final(void)
 {
+	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		call_driver_initcalls();
 	call_finalcalls();
 	IMSG("Primary CPU switching to normal world boot");
 }
