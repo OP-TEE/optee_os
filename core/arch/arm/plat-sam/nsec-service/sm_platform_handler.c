952dbec78443ecbe0e9ea6c748b1f1c6082beeab
Tony Han <tony.han@microchip.com>
2024-08-21 09:52:00 +0800

    plat-sam: implement PL310 SMC protocol
    When Linux runs in normal world, it expects the PL310 to be initially
    disabled, and then invokes SMCs to enable it.
    Let CFG_PL310_SIP_PROTOCOL=y, and the L2 cache will be left untouched
    until the OS enables it.
    
    Signed-off-by: Tony Han <tony.han@microchip.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-sam/nsec-service/sm_platform_handler.c
+++ core/arch/arm/plat-sam/nsec-service/sm_platform_handler.c
@@ -10,13 +10,34 @@
 #include <kernel/tee_misc.h>
 #include <mm/core_memprot.h>
 #include <sam_sfr.h>
+#include <sam_pl310.h>
 #include <sm/optee_smc.h>
 #include <sm/sm.h>
 #include <smc_ids.h>
 
 static enum sm_handler_ret sam_sip_handler(struct thread_smc_args *args)
 {
+	/*
+	 * As all sama5 SoCs are single-core ones, check the code compiled for a
+	 * single core. No serializations done to protect against concurrency.
+	 */
+	static_assert(CFG_TEE_CORE_NB_CORE == 1);
+
 	switch (OPTEE_SMC_FUNC_NUM(args->a0)) {
+#ifdef CFG_PL310_SIP_PROTOCOL
+	case SAM_SMC_SIP_PL310_ENABLE:
+		args->a0 = pl310_enable();
+		break;
+	case SAM_SMC_SIP_PL310_DISABLE:
+		args->a0 = pl310_disable();
+		break;
+	case SAM_SMC_SIP_PL310_EN_WRITEBACK:
+		args->a0 = pl310_enable_writeback();
+		break;
+	case SAM_SMC_SIP_PL310_DIS_WRITEBACK:
+		args->a0 = pl310_disable_writeback();
+		break;
+#endif
 	case SAMA5_SMC_SIP_SFR_SET_USB_SUSPEND:
 		atmel_sfr_set_usb_suspend(args->a1);
 		args->a0 = SAMA5_SMC_SIP_RETURN_SUCCESS;
