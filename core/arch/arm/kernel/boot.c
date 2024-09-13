f12843460d47d97ed5b33bd11e9c7c49b0928169
Jens Wiklander <jens.wiklander@linaro.org>
2024-09-13 11:48:33 +0200

    core: mm: allocate temporary memory map array
    With CFG_BOOT_MEM enabled, allocate a temporary memory map array using
    boot_mem_alloc_tmp() instead of using the global static_mmap_regions[].
    core_mmu_save_mem_map() is added and called from
    boot_init_primary_late() before the temporary memory is reused.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/arch/arm/kernel/boot.c
+++ core/arch/arm/kernel/boot.c
@@ -938,6 +938,7 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
 #endif
 	IMSG_RAW("\n");
 
+	core_mmu_save_mem_map();
 	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
 		IMSG("Initializing virtualization support");
 		core_mmu_init_virtualization();
--- core/include/mm/core_mmu.h
+++ core/include/mm/core_mmu.h
@@ -295,6 +295,11 @@ extern const unsigned long core_mmu_tee_load_pa;
 
 void core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg);
 void core_init_mmu_regs(struct core_mmu_config *cfg);
+/*
+ * Copy static memory map from temporary boot_mem to heap when CFG_BOOT_MEM
+ * is enabled.
+ */
+void core_mmu_save_mem_map(void);
 
 /* Arch specific function to help optimizing 1 MMU xlat table */
 bool core_mmu_prefer_tee_ram_at_top(paddr_t paddr);
