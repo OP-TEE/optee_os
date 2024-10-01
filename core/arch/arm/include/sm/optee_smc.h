b21f080c2d04c3f0d8e3e1c36f9e244320ed9032
Jens Wiklander <jens.wiklander@linaro.org>
2024-10-01 14:47:27 +0200

    core: arm: SMC ABI to return protected memory configuration
    Extend the SMC ABI to return the protect memory configuration.
    
    A capability bit, OPTEE_SMC_SEC_CAP_PROTMEM, is added to announce that
    the new SMC fast call OPTEE_SMC_GET_PROTMEM_CONFIG is available.
    OPTEE_SMC_GET_PROTMEM_CONFIG returns the physical memory range of
    reserved protected memory.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

--- core/arch/arm/include/sm/optee_smc.h
+++ core/arch/arm/include/sm/optee_smc.h
@@ -318,6 +318,8 @@
 #define OPTEE_SMC_SEC_CAP_RPC_ARG		BIT(6)
 /* Secure world supports probing for RPMB device if needed */
 #define OPTEE_SMC_SEC_CAP_RPMB_PROBE		BIT(7)
+/* Secure world supports protected memory */
+#define OPTEE_SMC_SEC_CAP_PROTMEM		BIT(8)
 
 #define OPTEE_SMC_FUNCID_EXCHANGE_CAPABILITIES	U(9)
 #define OPTEE_SMC_EXCHANGE_CAPABILITIES \
@@ -560,6 +562,32 @@
 
 /* See OPTEE_SMC_CALL_WITH_REGD_ARG above */
 #define OPTEE_SMC_FUNCID_CALL_WITH_REGD_ARG	U(19)
+
+/*
+ * Get protected memory config
+ *
+ * Returns the protected memory config.
+ *
+ * Call register usage:
+ * a0   SMC Function ID, OPTEE_SMC_GET_PROTMEM_CONFIG
+ * a2-6	Not used, must be zero
+ * a7	Hypervisor Client ID register
+ *
+ * Have config return register usage:
+ * a0	OPTEE_SMC_RETURN_OK
+ * a1	Physical address of start of protected memory
+ * a2	Size of protected memory
+ * a3	PA width, max 64
+ * a4-7	Preserved
+ *
+ * Not available register usage:
+ * a0	OPTEE_SMC_RETURN_ENOTAVAIL
+ * a1-3 Not used
+ * a4-7	Preserved
+ */
+#define OPTEE_SMC_FUNCID_GET_PROTMEM_CONFIG		U(20)
+#define OPTEE_SMC_GET_PROTMEM_CONFIG \
+	OPTEE_SMC_FAST_CALL_VAL(OPTEE_SMC_FUNCID_GET_PROTMEM_CONFIG)
 
 /*
  * Resume from RPC (for example after processing a foreign interrupt)
