d735136fcc0b3ccaf43c422c3d32398279ecef34
Etienne Carriere <etienne.carriere@foss.st.com>
2024-10-31 16:32:00 +0100

    drivers: firewall: stm32_etzpc: explicit index in DECPROT string names
    Explicit the indices assigned to DECPROT helper string names.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Reviewed-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
    Reviewed-by: Gatien Chevallier <gatien.chevallier@foss.st.com>

--- core/drivers/firewall/stm32_etzpc.c
+++ core/drivers/firewall/stm32_etzpc.c
@@ -112,10 +112,10 @@ struct etzpc_device {
 static struct etzpc_device *etzpc_device;
 
 static const char *const etzpc_decprot_strings[] __maybe_unused = {
-	"ETZPC_DECPROT_S_RW",
-	"ETZPC_DECPROT_NS_R_S_W",
-	"ETZPC_DECPROT_MCU_ISOLATION",
-	"ETZPC_DECPROT_NS_RW",
+	[ETZPC_DECPROT_S_RW] = "ETZPC_DECPROT_S_RW",
+	[ETZPC_DECPROT_NS_R_S_W] = "ETZPC_DECPROT_NS_R_S_W",
+	[ETZPC_DECPROT_MCU_ISOLATION] = "ETZPC_DECPROT_MCU_ISOLATION",
+	[ETZPC_DECPROT_NS_RW] = "ETZPC_DECPROT_NS_RW",
 };
 
 static uint32_t etzpc_lock(void)
