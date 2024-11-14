6e15f94659b54dd4471607528780c5d199de6c87
Gatien Chevallier <gatien.chevallier@foss.st.com>
2024-11-14 12:08:52 +0100

    drivers: stm32_fmc: use PM_HINT_IS_STATE macro in pm callbacks
    Use dedicated PM_HINT_IS_STATE macro in pm callbacks to determine
    the targeted pm state.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/drivers/stm32_fmc.c
+++ core/drivers/stm32_fmc.c
@@ -447,7 +447,7 @@ static void fmc_suspend(void)
 static TEE_Result fmc_pm(enum pm_op op, unsigned int pm_hint,
 			 const struct pm_callback_handle *pm_handle __unused)
 {
-	if (pm_hint != PM_HINT_CONTEXT_STATE)
+	if (!PM_HINT_IS_STATE(pm_hint, CONTEXT))
 		return TEE_SUCCESS;
 
 	if (op == PM_OP_RESUME)
