OP-TEE/optee_os

commit c596d8359eb30c3151ff183691d11d895d4d2c59

core: add phys_mem allocation functions
Add nex_phys_mem and phys_mem allocation functions. These functions are
intended to replace the previous calls to tee_mm functions on with the
virt_mapper_pool or tee_mm_sec_ddr as arguments.

The pool of physical memory is divided into two parts, core and ta. All
physical TA memory allocations are done from the core pool if a ta pool
isn't added. This might be the case if core and ta physical memory
resides in the same physical memory range.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/mm/phys_mem.c
@@ -1,0 +1,213 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2024, Linaro Limited
+ */
+
+#include <kernel/panic.h>
+#include <kernel/tee_misc.h>
+#include <mm/core_mmu.h>
+#include <mm/phys_mem.h>
+#include <mm/tee_mm.h>
+#include <string.h>
+#include <types_ext.h>
+
+static tee_mm_pool_t *nex_core_pool __nex_bss;
+static tee_mm_pool_t *nex_ta_pool __nex_bss;
+
+static tee_mm_pool_t *init_pool(paddr_t b, paddr_size_t sz, uint32_t flags)
+{
+	tee_mm_pool_t *pool = NULL;
+
+	if (!b && !sz)
+		return NULL;
+
+	if (!b || (b & CORE_MMU_USER_CODE_MASK) ||
+	    !sz || (sz & CORE_MMU_USER_CODE_MASK))
+		panic("invalid phys mem");
+
+	if (flags & TEE_MM_POOL_NEX_MALLOC)
+		pool = nex_malloc(sizeof(*pool));
+	else
+		pool = malloc(sizeof(*pool));
+	if (!pool)
+		panic();
+
+	tee_mm_init(pool, b, sz, CORE_MMU_USER_CODE_SHIFT, flags);
+	return pool;
+}
+
+void nex_phys_mem_init(paddr_t core_base, paddr_size_t core_size,
+		       paddr_t ta_base, paddr_size_t ta_size)
+{
+	uint32_t flags = TEE_MM_POOL_NEX_MALLOC;
+
+	nex_core_pool = init_pool(core_base, core_size, flags);
+	nex_ta_pool = init_pool(ta_base, ta_size, flags);
+}
+
+paddr_size_t nex_phys_mem_get_ta_size(void)
+{
+	if (nex_ta_pool)
+		return nex_ta_pool->size;
+	assert(nex_core_pool);
+	return nex_core_pool->size - TEE_RAM_VA_SIZE;
+}
+
+paddr_t nex_phys_mem_get_ta_base(void)
+{
+	if (nex_ta_pool)
+		return nex_ta_pool->lo;
+	assert(nex_core_pool);
+	return nex_core_pool->lo;
+}
+
+static bool is_in_pool_range(tee_mm_pool_t *pool, paddr_t addr)
+{
+	return pool && core_is_buffer_inside(addr, 1, pool->lo, pool->size);
+}
+
+static tee_mm_entry_t *mm_find(tee_mm_pool_t *p0, tee_mm_pool_t *p1,
+			       paddr_t addr)
+{
+	if (is_in_pool_range(p0, addr))
+		return tee_mm_find(p0, addr);
+	if (is_in_pool_range(p1, addr))
+		return tee_mm_find(p1, addr);
+	return NULL;
+}
+
+tee_mm_entry_t *nex_phys_mem_mm_find(paddr_t addr)
+{
+	return mm_find(nex_core_pool, nex_ta_pool, addr);
+}
+
+static tee_mm_entry_t *mm_alloc(tee_mm_pool_t *p0, tee_mm_pool_t *p1,
+				size_t size)
+{
+	tee_mm_entry_t *mm = NULL;
+
+	if (p0)
+		mm = tee_mm_alloc(p0, size);
+	if (!mm && p1)
+		mm = tee_mm_alloc(p1, size);
+
+	return mm;
+}
+
+tee_mm_entry_t *nex_phys_mem_core_alloc(size_t size)
+{
+	return mm_alloc(nex_core_pool, NULL, size);
+}
+
+tee_mm_entry_t *nex_phys_mem_ta_alloc(size_t size)
+{
+	return mm_alloc(nex_ta_pool, nex_core_pool, size);
+}
+
+static tee_mm_entry_t *mm_alloc2(tee_mm_pool_t *p0, tee_mm_pool_t *p1,
+				 paddr_t base, size_t size)
+{
+	if (is_in_pool_range(p0, base))
+		return tee_mm_alloc2(p0, base, size);
+	if (is_in_pool_range(p1, base))
+		return tee_mm_alloc2(p1, base, size);
+	return NULL;
+}
+
+tee_mm_entry_t *nex_phys_mem_alloc2(paddr_t base, size_t size)
+{
+	return mm_alloc2(nex_core_pool, nex_ta_pool, base, size);
+}
+
+static void partial_carve_out(tee_mm_pool_t *pool, paddr_t base, size_t size)
+{
+	if (pool &&
+	    core_is_buffer_intersect(base, size, pool->lo, pool->size)) {
+		tee_mm_entry_t *mm __maybe_unused = NULL;
+		paddr_t end_pa = 0;
+		paddr_t pa = 0;
+		size_t sz = 0;
+
+		pa = MAX(base, pool->lo);
+		end_pa = MIN(base + size - 1, pool->lo + pool->size - 1);
+		sz = end_pa - pa + 1;
+
+		mm = tee_mm_alloc2(pool, pa, sz);
+		assert(mm);
+	}
+}
+
+void nex_phys_mem_partial_carve_out(paddr_t base, size_t size)
+{
+	partial_carve_out(nex_core_pool, base, size);
+	partial_carve_out(nex_ta_pool, base, size);
+}
+
+#ifdef CFG_WITH_STATS
+static void add_pool_stats(tee_mm_pool_t *pool, struct pta_stats_alloc *stats,
+			   bool reset)
+{
+	if (pool) {
+		struct pta_stats_alloc s = { };
+
+		tee_mm_get_pool_stats(pool, &s, reset);
+		stats->size += s.size;
+		if (s.max_allocated > stats->max_allocated)
+			stats->max_allocated = s.max_allocated;
+		stats->allocated += s.allocated;
+	}
+}
+
+void nex_phys_mem_stats(struct pta_stats_alloc *stats, bool reset)
+{
+	memset(stats, 0, sizeof(*stats));
+
+	add_pool_stats(nex_core_pool, stats, reset);
+	add_pool_stats(nex_ta_pool, stats, reset);
+}
+#endif /*CFG_WITH_STATS*/
+
+#if defined(CFG_NS_VIRTUALIZATION)
+
+static tee_mm_pool_t *core_pool;
+static tee_mm_pool_t *ta_pool;
+
+void phys_mem_init(paddr_t core_base, paddr_size_t core_size,
+		   paddr_t ta_base, paddr_size_t ta_size)
+{
+	uint32_t flags = TEE_MM_POOL_NO_FLAGS;
+
+	core_pool = init_pool(core_base, core_size, flags);
+	ta_pool = init_pool(ta_base, ta_size, flags);
+}
+
+tee_mm_entry_t *phys_mem_mm_find(paddr_t addr)
+{
+	return mm_find(core_pool, ta_pool, addr);
+}
+
+tee_mm_entry_t *phys_mem_core_alloc(size_t size)
+{
+	return mm_alloc(core_pool, NULL, size);
+}
+
+tee_mm_entry_t *phys_mem_ta_alloc(size_t size)
+{
+	return mm_alloc(ta_pool, core_pool, size);
+}
+
+tee_mm_entry_t *phys_mem_alloc2(paddr_t base, size_t size)
+{
+	return mm_alloc2(core_pool, ta_pool, base, size);
+}
+
+#ifdef CFG_WITH_STATS
+void phys_mem_stats(struct pta_stats_alloc *stats, bool reset)
+{
+	memset(stats, 0, sizeof(*stats));
+
+	add_pool_stats(core_pool, stats, reset);
+	add_pool_stats(ta_pool, stats, reset);
+}
+#endif /*CFG_WITH_STATS*/
+#endif /*CFG_NS_VIRTUALIZATION*/

core/include/mm/phys_mem.h
@@ -1,0 +1,70 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2024, Linaro Limited
+ */
+
+#ifndef __MM_PHYS_MEM_H
+#define __MM_PHYS_MEM_H
+
+#include <mm/tee_mm.h>
+#include <types_ext.h>
+
+void nex_phys_mem_init(paddr_t core_base, paddr_size_t core_size,
+		       paddr_t ta_base, paddr_size_t ta_size);
+paddr_size_t nex_phys_mem_get_ta_size(void);
+paddr_t nex_phys_mem_get_ta_base(void);
+tee_mm_entry_t *nex_phys_mem_mm_find(paddr_t addr);
+tee_mm_entry_t *nex_phys_mem_core_alloc(size_t size);
+tee_mm_entry_t *nex_phys_mem_ta_alloc(size_t size);
+tee_mm_entry_t *nex_phys_mem_alloc2(paddr_t base, size_t size);
+void nex_phys_mem_partial_carve_out(paddr_t base, size_t size);
+#ifdef CFG_WITH_STATS
+void nex_phys_mem_stats(struct pta_stats_alloc *stats, bool reset);
+#endif
+
+#ifdef CFG_NS_VIRTUALIZATION
+void phys_mem_init(paddr_t core_base, paddr_size_t core_size,
+		   paddr_t ta_base, paddr_size_t ta_size);
+tee_mm_entry_t *phys_mem_mm_find(paddr_t addr);
+tee_mm_entry_t *phys_mem_core_alloc(size_t size);
+tee_mm_entry_t *phys_mem_ta_alloc(size_t size);
+tee_mm_entry_t *phys_mem_alloc2(paddr_t base, size_t size);
+#ifdef CFG_WITH_STATS
+void phys_mem_stats(struct pta_stats_alloc *stats, bool reset);
+#endif
+#else
+static inline void phys_mem_init(paddr_t core_base, paddr_size_t core_size,
+				 paddr_t ta_base, paddr_size_t ta_size)
+{
+	nex_phys_mem_init(core_base, core_size, ta_base, ta_size);
+}
+
+static inline tee_mm_entry_t *phys_mem_mm_find(paddr_t addr)
+{
+	return nex_phys_mem_mm_find(addr);
+}
+
+static inline tee_mm_entry_t *phys_mem_core_alloc(size_t size)
+{
+	return nex_phys_mem_core_alloc(size);
+}
+
+static inline tee_mm_entry_t *phys_mem_ta_alloc(size_t size)
+{
+	return nex_phys_mem_ta_alloc(size);
+}
+
+static inline tee_mm_entry_t *phys_mem_alloc2(paddr_t base, size_t size)
+{
+	return nex_phys_mem_alloc2(base, size);
+}
+
+#ifdef CFG_WITH_STATS
+static inline void phys_mem_stats(struct pta_stats_alloc *stats, bool reset)
+{
+	return nex_phys_mem_stats(stats, reset);
+}
+#endif
+#endif
+
+#endif /*__MM_PHYS_MEM_H*/

core/mm/sub.mk
@@ -6,6 +6,7 @@ srcs-y += vm.c
 srcs-y += core_mmu.c
 srcs-y += pgt_cache.c
 srcs-y += tee_mm.c
+srcs-y += phys_mem.c
 ifneq ($(CFG_CORE_FFA),y)
 srcs-$(CFG_CORE_DYN_SHM) += mobj_dyn_shm.c
 endif

commit 92985519d99f959688930a8563d24944925244d5

core: mm: check return value from tee_mm_init()
Check return value from tee_mm_init() function.

Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Fixes: c596d8359eb3 ("core: add phys_mem allocation functions")

core/mm/phys_mem.c
@@ -32,7 +32,9 @@ static tee_mm_pool_t *init_pool(paddr_t b, paddr_size_t sz, uint32_t flags)
 	if (!pool)
 		panic();
 
-	tee_mm_init(pool, b, sz, CORE_MMU_USER_CODE_SHIFT, flags);
+	if (!tee_mm_init(pool, b, sz, CORE_MMU_USER_CODE_SHIFT, flags))
+		panic();
+
 	return pool;
 }
 