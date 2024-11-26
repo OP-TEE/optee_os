OP-TEE/optee_os

commit 2a12ae23779651fa99b83082585aae1739331850

drivers: caam: add CAAM secure memory driver
Add CAAM secure memory support. The CAAM secure memory is an embedded
memory within the CAAM used for data protection and special operations.

Enable the allocation of secure memory pages and partitions used by job
rings as input/output for special cryptographic operations.

Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/caam/caam_sm.c
@@ -1,0 +1,182 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2019, 2023 NXP
+ */
+#include <caam_common.h>
+#include <caam_hal_ctrl.h>
+#include <caam_hal_sm.h>
+#include <caam_jr.h>
+#include <caam_status.h>
+#include <caam_sm.h>
+#include <caam_utils_status.h>
+#include <tee_api_defines.h>
+
+/*
+ * Secure memory module private data
+ */
+static struct sm_privdata {
+	vaddr_t baseaddr;  /* Secure memory base address */
+	vaddr_t ctrl_addr; /* CAAM base address */
+	vaddr_t jr_addr;   /* Job Ring base address */
+	paddr_t jr_offset; /* Job Ring offset */
+} sm_privdata;
+
+enum caam_status caam_sm_alloc(const struct caam_sm_page_desc *page_desc,
+			       struct caam_sm_page_addr *page_addr)
+{
+	enum caam_status ret = CAAM_FAILURE;
+
+	if (!page_desc || !page_addr)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	ret = caam_hal_sm_check_page_partition(sm_privdata.jr_addr, page_desc);
+	if (ret != CAAM_NO_ERROR) {
+		SM_TRACE("Pages %u to %u or partition %u are out of bounds",
+			 page_desc->page,
+			 page_desc->page + page_desc->page_count - 1,
+			 page_desc->partition);
+		return ret;
+	}
+
+	/* Check if partition is already allocated */
+	if (!caam_hal_sm_prtn_is_free(sm_privdata.jr_addr,
+				      page_desc->partition)) {
+		SM_TRACE("Partition %u not available", page_desc->partition);
+		return CAAM_BUSY;
+	}
+
+	/* Open secure memory partition to all groups */
+	caam_hal_sm_open_access_perm(sm_privdata.jr_addr, page_desc->partition);
+	caam_hal_sm_set_access_all_group(sm_privdata.jr_addr,
+					 page_desc->partition);
+
+	ret = caam_hal_sm_allocate_page(sm_privdata.jr_addr, page_desc);
+	if (ret != CAAM_NO_ERROR) {
+		SM_TRACE("Error allocation Pages %u to %u into partition %u",
+			 page_desc->page,
+			 page_desc->page + page_desc->page_count - 1,
+			 page_desc->partition);
+
+		/* Free all potientiel pages allocated before failure */
+		return caam_hal_sm_deallocate_pages(sm_privdata.jr_addr,
+						    page_desc);
+	}
+
+	page_addr->paddr = caam_hal_ctrl_get_smvaddr(sm_privdata.ctrl_addr,
+						     sm_privdata.jr_offset) +
+			   caam_hal_sm_get_pages_size(sm_privdata.jr_addr,
+						      page_desc->page);
+	page_addr->vaddr = sm_privdata.baseaddr +
+			   caam_hal_sm_get_pages_size(sm_privdata.jr_addr,
+						      page_desc->page);
+
+	SM_TRACE("Partition %u Pages %u to %u allocated @0x%" PRIxVA
+		 " (phys 0x@%" PRIxPA,
+		 page_desc->partition, page_desc->page,
+		 page_desc->page + page_desc->page_count - 1, page_addr->vaddr,
+		 page_addr->paddr);
+
+	return CAAM_NO_ERROR;
+}
+
+enum caam_status caam_sm_free(const struct caam_sm_page_desc *page_desc)
+{
+	enum caam_status ret = CAAM_FAILURE;
+
+	SM_TRACE("Free Secure Memory pages %u to %u from partition %u",
+		 page_desc->page, page_desc->page + page_desc->page_count,
+		 page_desc->partition);
+
+	/*
+	 * De-allocate partition. It automatically releases partition's pages
+	 * to the pool of available pages. if the partition if marked as CSP,
+	 * pages will be zeroized. If the partition is marked as PSP,
+	 * partition and pages will not be de-allocated and a PSP will be
+	 * returned.
+	 */
+	if (!caam_hal_sm_prtn_is_owned(sm_privdata.jr_addr,
+				       page_desc->partition)) {
+		SM_TRACE("Partition %u not owned by used JR",
+			 page_desc->partition);
+		return TEE_ERROR_ACCESS_DENIED;
+	}
+
+	ret = caam_hal_sm_deallocate_pages(sm_privdata.jr_addr, page_desc);
+	if (ret) {
+		SM_TRACE("De-alloc pages %u to %u error 0x%" PRIx32,
+			 page_desc->page,
+			 page_desc->page + page_desc->page_count, ret);
+
+		return ret;
+	}
+
+	ret = caam_hal_sm_deallocate_partition(sm_privdata.jr_addr,
+					       page_desc->partition);
+	if (ret) {
+		SM_TRACE("De-alloc partition %u error 0x%" PRIx32,
+			 page_desc->partition, ret);
+		return ret;
+	}
+
+	return CAAM_NO_ERROR;
+}
+
+enum caam_status
+caam_sm_set_access_perm(const struct caam_sm_page_desc *page_desc,
+			unsigned int grp1_perm, unsigned int grp2_perm)
+{
+	uint32_t grp1 = UINT32_MAX;
+	uint32_t grp2 = UINT32_MAX;
+
+	if (!page_desc)
+		return CAAM_BAD_PARAM;
+
+	/* Check if the partition is already owned */
+	if (!caam_hal_sm_prtn_is_owned(sm_privdata.jr_addr,
+				       page_desc->partition)) {
+		SM_TRACE("Partition %d not owned by current JR",
+			 page_desc->partition);
+		return CAAM_FAILURE;
+	}
+
+	/*
+	 * Set ourself to access Secure Memory group 1 and/or group 2
+	 * function if @grp1_perm and/or @grp2_perm not equal 0.
+	 *
+	 * The Access Group is related to the Job Ring owner setting without
+	 * the Secure Bit setting already managed by the Job Ring.
+	 */
+	if (grp1_perm)
+		grp1 = JROWN_ARM_NS;
+
+	if (grp2_perm)
+		grp2 = JROWN_ARM_NS;
+
+	caam_hal_sm_set_access_group(sm_privdata.jr_addr, page_desc->partition,
+				     grp1, grp2);
+	caam_hal_sm_set_access_perm(sm_privdata.jr_addr, page_desc->partition,
+				    grp1_perm, grp2_perm);
+
+	return CAAM_NO_ERROR;
+}
+
+enum caam_status caam_sm_init(struct caam_jrcfg *jrcfg)
+{
+	if (!jrcfg)
+		return CAAM_FAILURE;
+
+	sm_privdata.ctrl_addr = jrcfg->base;
+	sm_privdata.jr_addr = jrcfg->base + jrcfg->offset;
+	sm_privdata.jr_offset = jrcfg->offset;
+	sm_privdata.baseaddr = caam_hal_sm_get_base();
+
+	if (!sm_privdata.baseaddr)
+		return CAAM_FAILURE;
+
+	SM_TRACE("Secure Memory Base address = 0x%" PRIxVA,
+		 sm_privdata.baseaddr);
+	SM_TRACE("CAAM controller address = 0x%" PRIxVA, sm_privdata.ctrl_addr);
+	SM_TRACE("CAAM Job Ring address = 0x%" PRIxVA, sm_privdata.jr_addr);
+
+	return CAAM_NO_ERROR;
+}

core/drivers/crypto/caam/caam_ctrl.c
@@ -17,6 +17,7 @@
 #include <caam_mp.h>
 #include <caam_pwr.h>
 #include <caam_rng.h>
+#include <caam_sm.h>
 #include <drivers/imx_snvs.h>
 #include <initcall.h>
 #include <kernel/panic.h>
@@ -143,6 +144,13 @@ static TEE_Result crypto_driver_init(void)
 	/* Initialize the Manufacturing Protection Module */
 	retstatus = caam_mp_init(jrcfg.base);
 	if (retstatus != CAAM_NO_ERROR && retstatus != CAAM_NOT_SUPPORTED) {
+		retresult = TEE_ERROR_GENERIC;
+		goto exit_init;
+	}
+
+	/* Initialize the secure memory */
+	retstatus = caam_sm_init(&jrcfg);
+	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
 	}

core/drivers/crypto/caam/include/caam_sm.h
@@ -1,0 +1,72 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2019, 2023 NXP
+ */
+#ifndef __CAAM_SM_H__
+#define __CAAM_SM_H__
+
+#include "caam_jr.h"
+#include "drivers/caam_extension.h"
+
+/*
+ * Secure Memory data
+ */
+struct caam_sm_page_addr {
+	paddr_t paddr; /* Secure memory base address */
+	vaddr_t vaddr; /* Secure memory virtual base address */
+};
+
+/*
+ * Secure Memory Page(s)/Partition definition
+ */
+struct caam_sm_page_desc {
+	unsigned int partition; /* Partition number */
+	unsigned int page; /* Page number */
+	unsigned int page_count; /* Number of pages used */
+};
+
+#ifdef CFG_NXP_CAAM_SM_DRV
+/*
+ * CAAM Secure memory module initialization
+ *
+ * @jrcfg  JR configuration structure
+ */
+enum caam_status caam_sm_init(struct caam_jrcfg *jrcfg);
+
+/*
+ * Allocate page(s) to one partition in the CAAM secure memory.
+ * Reset the group access and permission access to remove restrictions.
+ *
+ * @sm_page_descriptor  Secure Memory page
+ * @sm_page_addr [out]  Secure Memory page addresses
+ */
+enum caam_status
+caam_sm_alloc(const struct caam_sm_page_desc *sm_page_descriptor,
+	      struct caam_sm_page_addr *sm_page_addr);
+
+/*
+ * Set the Secure Memory group 1 and group 2 access rights to allocated
+ * partition and lock configuration.
+ *
+ * @page_desc  Secure Memory page
+ * @grp1_perm  Group 1 Permission value
+ * @grp2_perm  Group 2 Permission value
+ */
+enum caam_status
+caam_sm_set_access_perm(const struct caam_sm_page_desc *page_desc,
+			unsigned int grp1_perm, unsigned int grp2_perm);
+
+/*
+ * Free a Secure Memory pages
+ *
+ * @sm_page_descriptor    Secure Memory page
+ */
+TEE_Result caam_sm_free(const struct caam_sm_page_desc *sm_page_descriptor);
+
+#else
+static inline enum caam_status caam_sm_init(struct caam_jrcfg *jrcfg __unused)
+{
+	return CAAM_NO_ERROR;
+}
+#endif /* CFG_NXP_CAAM_SM_DRV */
+#endif /* __CAAM_SM_H__ */

core/drivers/crypto/caam/include/caam_trace.h
@@ -41,6 +41,7 @@
 #define DBG_TRACE_DH	 BIT32(13) /* DH trace */
 #define DBG_TRACE_DSA	 BIT32(14) /* DSA trace */
 #define DBG_TRACE_MP	 BIT32(15) /* MP trace */
+#define DBG_TRACE_SM	 BIT32(16) /* Secure Memory trace */
 
 /* HAL */
 #if CAAM_DBG_TRACE(HAL)
@@ -320,6 +321,13 @@
 #define BLOB_TRACE(...)
 #define BLOB_DUMPDESC(desc)
 #define BLOB_DUMPBUF(...)
+#endif
+
+/* Secure Memory */
+#if CAAM_DBG_TRACE(SM)
+#define SM_TRACE DRV_TRACE
+#else
+#define SM_TRACE(...)
 #endif
 
 #endif /* CAAM_TRACE_H__ */

core/drivers/crypto/caam/sub.mk
@@ -8,6 +8,7 @@ srcs-y += caam_ctrl.c
 srcs-y += caam_jr.c
 srcs-y += caam_rng.c
 srcs-y += caam_desc.c
+srcs-$(CFG_NXP_CAAM_SM_DRV) += caam_sm.c
 subdirs-$(call cfg-one-enabled, CFG_NXP_CAAM_HASH_DRV CFG_NXP_CAAM_HMAC_DRV) += hash
 subdirs-$(call cfg-one-enabled, CFG_NXP_CAAM_CIPHER_DRV CFG_NXP_CAAM_CMAC_DRV) += cipher
 subdirs-y += acipher

commit 3f465336bd04f588a74d2a0b2164a70207d3243a

drivers: caam: sm: fix SM partition permission in SMAG registers
SM partition SMAG1 permissions were wrongly set for generating DEK blob
which results in error while decapsulating DEK blob during HAB
encrypted boot.
Setting the permissions correctly fix this issue.

Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Fixes: 2a12ae237796 ("drivers: caam: add CAAM secure memory driver")

core/drivers/crypto/caam/caam_sm.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2019, 2023 NXP
+ * Copyright 2019, 2023-2024 NXP
  */
 #include <caam_common.h>
 #include <caam_hal_ctrl.h>
@@ -124,8 +124,8 @@ enum caam_status
 caam_sm_set_access_perm(const struct caam_sm_page_desc *page_desc,
 			unsigned int grp1_perm, unsigned int grp2_perm)
 {
-	uint32_t grp1 = UINT32_MAX;
-	uint32_t grp2 = UINT32_MAX;
+	uint32_t grp1 = 0;
+	uint32_t grp2 = 0;
 
 	if (!page_desc)
 		return CAAM_BAD_PARAM;
@@ -144,12 +144,18 @@ caam_sm_set_access_perm(const struct caam_sm_page_desc *page_desc,
 	 *
 	 * The Access Group is related to the Job Ring owner setting without
 	 * the Secure Bit setting already managed by the Job Ring.
+	 *
+	 * If any group permissions are set, need to enable Secure World MID
+	 * access in SMAG1/2 registers.
+	 * Since both Non-Secure/Secure world has same MID, using JROWN_ARM_NS
+	 * and if any grp1_perm/grp2_perm is set, need to enable permission
+	 * for Secure World for partition in SMAG1/2 Registers.
 	 */
 	if (grp1_perm)
-		grp1 = JROWN_ARM_NS;
+		grp1 = SHIFT_U32(1, JROWN_ARM_NS);
 
 	if (grp2_perm)
-		grp2 = JROWN_ARM_NS;
+		grp2 = SHIFT_U32(1, JROWN_ARM_NS);
 
 	caam_hal_sm_set_access_group(sm_privdata.jr_addr, page_desc->partition,
 				     grp1, grp2);

core/drivers/crypto/caam/hal/common/hal_sm.c
@@ -101,13 +101,8 @@ void caam_hal_sm_set_access_group(vaddr_t jr_base, unsigned int partition,
 	if (!jr_base)
 		return;
 
-	if (grp1 != UINT32_MAX)
-		io_caam_write32(jr_base + SM_SMAG1(partition),
-				SHIFT_U32(1, grp1));
-
-	if (grp2 != UINT32_MAX)
-		io_caam_write32(jr_base + SM_SMAG2(partition),
-				SHIFT_U32(1, grp2));
+	io_caam_write32(jr_base + SM_SMAG1(partition), grp1);
+	io_caam_write32(jr_base + SM_SMAG2(partition), grp2);
 }
 
 void caam_hal_sm_open_access_perm(vaddr_t jr_base, unsigned int partition)