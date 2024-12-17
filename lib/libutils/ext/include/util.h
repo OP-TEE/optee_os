0b9aff61944a215725b494e7c87fb812d44d4a6d
Etienne Carriere <etienne.carriere@foss.st.com>
2024-12-17 23:23:00 +0100

    libutils: util.h: fix DIV_ROUND_UP() inline description
    Remove trailing space char in inline description comment of
    DIV_ROUND_UP() macro.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- lib/libutils/ext/include/util.h
+++ lib/libutils/ext/include/util.h
@@ -144,7 +144,7 @@
 	}))
 
 /*
- * Round up the result of x / y to the nearest upper integer if result is not 
+ * Round up the result of x / y to the nearest upper integer if result is not
  * already an integer.
  */
 #define DIV_ROUND_UP(x, y) (((x) + (y) - 1) / (y))
