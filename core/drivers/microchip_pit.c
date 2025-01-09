OP-TEE/optee_os

commit 8796ab4a984ffb6e34f43870b381385063103482

drivers: microchip_pit: add driver for sama7g54's pit64b
Add support for the peripheral PIT64B in sama7g54. In the driver the clocks
are initialized for PIT64B.

Signed-off-by: Tony Han <tony.han@microchip.com>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/microchip_pit.c
@@ -1,0 +1,64 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2024, Microchip
+ */
+
+#include <assert.h>
+#include <drivers/clk.h>
+#include <drivers/clk_dt.h>
+#include <dt_driver.h>
+#include <string.h>
+
+#define MCHP_PIT64B_FREQ		UL(5000000)	/* 5 MHz */
+
+static TEE_Result microchip_pit_probe(const void *fdt, int node,
+				      const void *compat_data __unused)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct clk *parent = NULL;
+	struct clk *pclk = NULL;
+	struct clk *gclk = NULL;
+	size_t i = 0;
+
+	res = clk_dt_get_by_name(fdt, node, "pclk", &pclk);
+	if (res)
+		return res;
+
+	res = clk_dt_get_by_name(fdt, node, "gclk", &gclk);
+	if (res)
+		return res;
+
+	res = clk_enable(pclk);
+	if (res)
+		panic();
+
+	do {
+		parent = clk_get_parent_by_index(gclk, i++);
+		if (!memcmp("syspll", clk_get_name(parent), sizeof("syspll")))
+			break;
+	} while (parent);
+	if (!parent)
+		panic();
+
+	res = clk_set_parent(gclk, parent);
+	if (res)
+		panic();
+
+	res = clk_set_rate(gclk, MCHP_PIT64B_FREQ);
+	if (res)
+		panic();
+
+	return clk_enable(gclk);
+}
+
+static const struct dt_device_match microchip_pit_match_table[] = {
+	{ .compatible = "microchip,sama7g5-pit64b" },
+	{ }
+};
+
+DEFINE_DT_DRIVER(microchip_pit_dt_driver) = {
+	.name = "microchip_pit",
+	.type = DT_DRIVER_NOTYPE,
+	.match_table = microchip_pit_match_table,
+	.probe = microchip_pit_probe,
+};

core/arch/arm/plat-sam/conf.mk
@@ -20,6 +20,7 @@ include core/arch/arm/cpu/cortex-a7.mk
 $(call force,CFG_SAMA7G5,y)
 $(call force,CFG_GIC,y)
 $(call force,CFG_TZC400,y)
+$(call force,CFG_MICROCHIP_PIT,y)
 else
 include core/arch/arm/cpu/cortex-a5.mk
 $(call force,CFG_SAMA5D2,y)

core/drivers/sub.mk
@@ -32,6 +32,7 @@ srcs-$(CFG_ATMEL_WDT) += atmel_wdt.c
 srcs-$(CFG_ATMEL_RTC) += atmel_rtc.c
 srcs-$(CFG_ATMEL_PIOBU) += atmel_piobu.c
 srcs-$(CFG_ATMEL_TCB) += atmel_tcb.c
+srcs-$(CFG_MICROCHIP_PIT) += microchip_pit.c
 srcs-$(CFG_AMLOGIC_UART) += amlogic_uart.c
 srcs-$(CFG_MVEBU_UART) += mvebu_uart.c
 srcs-$(CFG_STM32_BSEC) += stm32_bsec.c

commit 24616bf2b8059f154025d10aace4877bf04577f2

drivers: microchip_pit: fix the path of "dt_driver.h" included
change "#include <dt_driver.h>" to "#include <kernel/dt_driver.h>" due to
"dt_driver.h" is located at "core/include/kernel/" and the path is not in
the include paths which would cause compile errors.

Fixes: 8796ab4a984f ("drivers: microchip_pit: add driver for sama7g54's pit64b")
Signed-off-by: Tony Han <tony.han@microchip.com>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/microchip_pit.c
@@ -6,7 +6,7 @@
 #include <assert.h>
 #include <drivers/clk.h>
 #include <drivers/clk_dt.h>
-#include <dt_driver.h>
+#include <kernel/dt_driver.h>
 #include <string.h>
 
 #define MCHP_PIT64B_FREQ		UL(5000000)	/* 5 MHz */