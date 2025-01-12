39217e3e692b14492f802ccccabc4405f08eb7f2
Yu-Chien Peter Lin <peter.lin@sifive.com>
2025-01-12 03:25:29 -0800

    riscv: kernel: sbi: introduce sbi_hsm_hart_get_status() function
    Introduce sbi_hsm_hart_get_status() function and add comment for
    sbi_hsm_hart_start().
    
    Signed-off-by: Yu-Chien Peter Lin <peter.lin@sifive.com>
    Reviewed-by: Samuel Holland <samuel.holland@sifive.com>
    Reviewed-by: Zong Li <zong.li@sifive.com>

--- core/arch/riscv/kernel/sbi.c
+++ core/arch/riscv/kernel/sbi.c
@@ -69,6 +69,15 @@ int sbi_dbcn_write_byte(unsigned char ch)
 	return ret.error;
 }
 
+/**
+ * sbi_hsm_hart_start() - Start target hart at OP-TEE entry in S-mode
+ * @hartid:     Target hart id
+ * @start_addr: Physical address of OP-TEE entry
+ * @arg:        opaque parameter, typically used as the physical
+ *              address of device-tree passed via $a1
+ *
+ * Return:      SBI error code (SBI_SUCCESS = 0 on success)
+ */
 int sbi_hsm_hart_start(uint32_t hartid, paddr_t start_addr, unsigned long arg)
 {
 	struct sbiret ret = { };
@@ -77,4 +86,22 @@ int sbi_hsm_hart_start(uint32_t hartid, paddr_t start_addr, unsigned long arg)
 			arg);
 
 	return ret.error;
+}
+
+/**
+ * sbi_hsm_hart_get_status() - Get the current HSM state of given hart
+ * @hartid:         Target hart id
+ *
+ * Return:          HSM state on success, SBI error code otherwise
+ */
+int sbi_hsm_hart_get_status(uint32_t hartid)
+{
+	struct sbiret ret = { };
+
+	ret = sbi_ecall(SBI_EXT_HSM, SBI_EXT_HSM_HART_GET_STATUS, hartid);
+
+	if (ret.error)
+		return ret.error;
+	else
+		return ret.value;
 }
