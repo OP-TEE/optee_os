OP-TEE/optee_os

commit a96033ca7bee4c892d1436592872be52fe0c3f5c

core: add flag to enumerate TAs when secure storage is ready
Add TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE with similar usage as
TA_FLAG_DEVICE_ENUM_SUPP except that it is used to add an enumeration
point when secure storage is ready.

The new TA function PTA_CMD_GET_DEVICES_RPMB is added to the device PTA
to report TAs depending on RPMB.

PTA_CMD_GET_DEVICES_RPMB will if CFG_REE_FS!=y and CFG_RPMB_FS=y first
try to initialize RPMB and only if successful will it return eventual
TAs with the TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE flag set.

PTA_CMD_GET_DEVICES_SUPP will if CFG_REE_FS=y include TAs with the
TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE flag set in the list of TAs to
return.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/pta/device.c
@@ -15,6 +15,7 @@
 #include <kernel/stmm_sp.h>
 #include <kernel/tee_ta_manager.h>
 #include <pta_device.h>
+#include <tee/tee_fs.h>
 #include <tee/uuid.h>
 #include <user_ta_header.h>
 
@@ -23,8 +24,11 @@
 static void add_ta(uint32_t flags, const TEE_UUID *uuid, uint8_t *buf,
 		   uint32_t blen, uint32_t *pos, uint32_t rflags)
 {
-	if ((flags & TA_FLAG_DEVICE_ENUM) &&
-	    (flags & TA_FLAG_DEVICE_ENUM_SUPP)) {
+	flags &= (TA_FLAG_DEVICE_ENUM | TA_FLAG_DEVICE_ENUM_SUPP |
+		  TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE);
+	if (flags && flags != TA_FLAG_DEVICE_ENUM &&
+	    flags != TA_FLAG_DEVICE_ENUM_SUPP &&
+	    flags != TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE) {
 		EMSG(PTA_NAME ": skipping TA %pUl, inconsistent flags", uuid);
 		return;
 	}
@@ -82,18 +86,31 @@ static TEE_Result invoke_command(void *pSessionContext __unused,
 				 uint32_t nCommandID, uint32_t nParamTypes,
 				 TEE_Param pParams[TEE_NUM_PARAMS])
 {
+	TEE_Result res = TEE_SUCCESS;
+	uint32_t rflags = 0;
+
 	switch (nCommandID) {
 	case PTA_CMD_GET_DEVICES:
-		return get_devices(nParamTypes, pParams,
-				   TA_FLAG_DEVICE_ENUM);
-	case PTA_CMD_GET_DEVICES_SUPP:
-		return get_devices(nParamTypes, pParams,
-				   TA_FLAG_DEVICE_ENUM_SUPP);
-	default:
+		rflags = TA_FLAG_DEVICE_ENUM;
 		break;
+	case PTA_CMD_GET_DEVICES_SUPP:
+		rflags = TA_FLAG_DEVICE_ENUM_SUPP;
+		if (IS_ENABLED(CFG_REE_FS))
+			rflags |= TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE;
+		break;
+	case PTA_CMD_GET_DEVICES_RPMB:
+		if (!IS_ENABLED(CFG_REE_FS)) {
+			res = tee_rpmb_reinit();
+			if (res)
+				return TEE_ERROR_STORAGE_NOT_AVAILABLE;
+			rflags = TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE;
+		}
+		break;
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
 	}
 
-	return TEE_ERROR_NOT_IMPLEMENTED;
+	return get_devices(nParamTypes, pParams, rflags);
 }
 
 pseudo_ta_register(.uuid = PTA_DEVICE_UUID, .name = PTA_NAME,

lib/libutee/include/pta_device.h
@@ -25,5 +25,6 @@
  */
 #define PTA_CMD_GET_DEVICES		0x0 /* before tee-supplicant run */
 #define PTA_CMD_GET_DEVICES_SUPP	0x1 /* after tee-supplicant run */
+#define PTA_CMD_GET_DEVICES_RPMB	0x2 /* probe for RPMB */
 
 #endif /* __PTA_DEVICE_H */

lib/libutee/include/user_ta_header.h
@@ -24,16 +24,34 @@
 	 */
 #define TA_FLAG_CONCURRENT		BIT32(8)
 	/*
-	 * Device enumeration is done in two stages by the normal world, first
-	 * before the tee-supplicant has started and then once more when the
-	 * tee-supplicant is started. The flags below control if the TA should
-	 * be reported in the first or second or case.
+	 * Device enumeration is initiated at multiple stages by the normal
+	 * world:
+	 * 1. First when the kernel driver has initialized
+	 * 2. When RPMB is available via inkernel RPMB routing
+	 * 3. When the tee-supplicant is started
+	 *
+	 * The flags below control at which stage a TA will be enumerated:
+	 * TA_FLAG_DEVICE_ENUM - at stage 1
+	 * TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE -
+	 *      when secure storage is available, at stage 2 or 3 depending
+	 *      on whether TEE_STORAGE_PRIVATE is using RPMB FS
+	 *      (CFG_REE_FS=n CFG_RPMB_FS=y) or REE FS (CFG_REE_FS=y). The
+	 *      former utilizes in kernel RPMB routing, and the latter
+	 *      depends on tee-supplicant to access secure storage.
+	 * TA_FLAG_DEVICE_ENUM_SUPP - at stage 3
+	 *
+	 * The TA is enumerated at stage 2 if
+	 * TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE is set and
+	 * TEE_STORAGE_PRIVATE is using RPMB FS, or if it's using REE FS it
+	 * will be enumerated at stage 3.
 	 */
 #define TA_FLAG_DEVICE_ENUM		BIT32(9)  /* without tee-supplicant */
 #define TA_FLAG_DEVICE_ENUM_SUPP	BIT32(10) /* with tee-supplicant */
 	/* See also "gpd.ta.doesNotCloseHandleOnCorruptObject" */
 #define TA_FLAG_DONT_CLOSE_HANDLE_ON_CORRUPT_OBJECT \
 					BIT32(11)
+#define TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE	\
+					BIT32(12) /* with TEE_STORAGE_PRIVATE */
 
 #define TA_FLAGS_MASK			GENMASK_32(11, 0)
 

commit 82d8a42ed67722992904eb19ffde20b2d384b87f

core: pta: device: fix enumeration for PTA_CMD_GET_DEVICES_SUPP
TAs which depend on TEE_STORAGE_PRIVATE do need the TEE supplicant if
REE FS is disabled (in which case secure storage can only be RPMB) and
RPMB is not routed via the kernel.

Fixes: a96033ca7bee ("core: add flag to enumerate TAs when secure storage is ready")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/pta/device.c
@@ -88,6 +88,11 @@ static TEE_Result invoke_command(void *pSessionContext __unused,
 {
 	TEE_Result res = TEE_SUCCESS;
 	uint32_t rflags = 0;
+	/*
+	 * This should also be true if CFG_RPMB_ANNOUNCE_PROBE_CAP is
+	 * enabled when the kernel does not support OP-TEE RPMB operations.
+	 */
+	bool rpmb_needs_supp = !IS_ENABLED(CFG_RPMB_ANNOUNCE_PROBE_CAP);
 
 	switch (nCommandID) {
 	case PTA_CMD_GET_DEVICES:
@@ -95,7 +100,8 @@ static TEE_Result invoke_command(void *pSessionContext __unused,
 		break;
 	case PTA_CMD_GET_DEVICES_SUPP:
 		rflags = TA_FLAG_DEVICE_ENUM_SUPP;
-		if (IS_ENABLED(CFG_REE_FS))
+		if (IS_ENABLED(CFG_REE_FS) ||
+		    (IS_ENABLED(CFG_RPMB_FS) && rpmb_needs_supp))
 			rflags |= TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE;
 		break;
 	case PTA_CMD_GET_DEVICES_RPMB: