a1a55532e8634d2a2a264260194407f5ed628084
Akshay Belsare <akshay.belsare@amd.com>
2025-01-28 09:36:57 +0000

    plat-versal2: enable AMD mailbox driver
    Add AMD mailbox driver support for AMD Versal Gen 2 platform.
    Keeping the config CFG_MAILBOX_DRIVER disabled by default.
    Once a driver using the mailbox service is added and enabled
    for this platform the configuration to be updated.
    
    Signed-off-by: Akshay Belsare <akshay.belsare@amd.com>

---
+++ core/arch/arm/plat-versal2/plat_ipi.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+ */
+#ifndef PLAT_IPI_H
+#define PLAT_IPI_H
+
+#define PLAT_IPI_BASE_ADDR	0xEB3F0000
+
+#define PLAT_SMC_SIP_IPI	0xC2001000
+
+#endif /* PLAT_IPI_H */
