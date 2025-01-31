OP-TEE/optee_os

commit 14885eb1688bb0826c53522d4c3d99ef9c461f25

drivers: gic: register to dt_driver
Registers GIC driver as an interrupt controller in DT_DRIVER
providers when DT is supported. This change allows interrupt
consumer nodes to leverage interrupts and interrupts-extended
properties DT bindings for their device drivers to retrieve
their interrupts.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/gic.c
@@ -11,6 +11,7 @@
 #include <drivers/gic.h>
 #include <keep.h>
 #include <kernel/dt.h>
+#include <kernel/dt_driver.h>
 #include <kernel/interrupt.h>
 #include <kernel/panic.h>
 #include <mm/core_memprot.h>
@@ -656,3 +657,64 @@ static void gic_op_set_affinity(struct itr_chip *chip, size_t it,
 
 	gic_it_set_cpu_mask(gd, it, cpu_mask);
 }
+
+#ifdef CFG_DT
+/* Callback for "interrupts" and "interrupts-extended" DT node properties */
+static TEE_Result dt_get_gic_chip_cb(struct dt_pargs *arg, void *priv_data,
+				     struct itr_desc *itr_desc)
+{
+	int itr_num = DT_INFO_INVALID_INTERRUPT;
+	struct itr_chip *chip = priv_data;
+	uint32_t phandle_args[2] = { };
+	uint32_t type = 0;
+	uint32_t prio = 0;
+
+	assert(arg && itr_desc);
+
+	/*
+	 * gic_dt_get_irq() expects phandle arguments passed are still in DT
+	 * format (big-endian) whereas struct dt_pargs carries converted
+	 * formats. Therefore swap again phandle arguments. gic_dt_get_irq()
+	 * consumes only the 2 first arguments.
+	 */
+	if (arg->args_count < 2)
+		return TEE_ERROR_GENERIC;
+	phandle_args[0] = cpu_to_fdt32(arg->args[0]);
+	phandle_args[1] = cpu_to_fdt32(arg->args[1]);
+
+	itr_num = gic_dt_get_irq((const void *)phandle_args, 2, &type, &prio);
+	if (itr_num == DT_INFO_INVALID_INTERRUPT)
+		return TEE_ERROR_GENERIC;
+
+	gic_op_add(chip, itr_num, type, prio);
+
+	itr_desc->chip = chip;
+	itr_desc->itr_num = itr_num;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result gic_probe(const void *fdt, int offs, const void *cd __unused)
+{
+	if (interrupt_register_provider(fdt, offs, dt_get_gic_chip_cb,
+					&gic_data.chip))
+		panic();
+
+	return TEE_SUCCESS;
+}
+
+static const struct dt_device_match gic_match_table[] = {
+	{ .compatible = "arm,cortex-a15-gic" },
+	{ .compatible = "arm,cortex-a7-gic" },
+	{ .compatible = "arm,cortex-a5-gic" },
+	{ .compatible = "arm,cortex-a9-gic" },
+	{ .compatible = "arm,gic-400" },
+	{ }
+};
+
+DEFINE_DT_DRIVER(gic_dt_driver) = {
+	.name = "gic",
+	.match_table = gic_match_table,
+	.probe = gic_probe,
+};
+#endif /*CFG_DT*/

commit b30e5ea4a71c35b8ddf97b6fd1653d1746db46ff

drivers: gic: allow detection type configuration
Allow consumers to configure SPI interrupts when the provided type
is not IRQ_TYPE_NONE. The allowed value are then IRQ_TYPE_EDGE_RISING
and IRQ_TYPE_LEVEL_HIGH, as per Arm GIC specification.

This change fixes an issue when using interrupt_dt_get_by_index()
or interrupt_dt_get_by_name() since these API functions tell the
interrupt is configured from DT phandle arguments whereas GIC driver
did not consider the interrupt detection type even if set in the
consumer DT node property.

Fixes: 14885eb1688b ("drivers: gic: register to dt_driver")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/gic.c
@@ -45,6 +45,7 @@
 #define GICD_ICPENDR(n)		(0x280 + (n) * 4)
 #define GICD_IPRIORITYR(n)	(0x400 + (n) * 4)
 #define GICD_ITARGETSR(n)	(0x800 + (n) * 4)
+#define GICD_ICFGR(n)		(0xc00 + (n) * 4)
 #define GICD_IGROUPMODR(n)	(0xd00 + (n) * 4)
 #define GICD_SGIR		(0xF00)
 
@@ -123,6 +124,14 @@
 #define GICD_SGIR_TARGET_LIST_FILTER_SHIFT	24
 #define GICD_SGIR_NSATT_SHIFT			15
 #define GICD_SGIR_CPU_TARGET_LIST_SHIFT		16
+
+/* GICD ICFGR bit fields */
+#define GICD_ICFGR_TYPE_EDGE		2
+#define GICD_ICFGR_TYPE_LEVEL		0
+#define GICD_ICFGR_FIELD_BITS		2
+#define GICD_ICFGR_FIELD_MASK		0x3
+#define GICD_ICFGR_NUM_INTS_PER_REG	(NUM_INTS_PER_REG / \
+					 GICD_ICFGR_FIELD_BITS)
 
 struct gic_data {
 	vaddr_t gicc_base;
@@ -423,28 +432,48 @@ static int gic_dt_get_irq(const uint32_t *properties, int count, uint32_t *type,
 			  uint32_t *prio)
 {
 	int it_num = DT_INFO_INVALID_INTERRUPT;
+	uint32_t detection_type = IRQ_TYPE_NONE;
+	uint32_t interrupt_type = GIC_PPI;
 
-	if (type)
-		*type = IRQ_TYPE_NONE;
-
-	if (prio)
-		*prio = 0;
-
-	if (!properties || count < 2)
+	if (!properties || count < 2 || count > 3)
 		return DT_INFO_INVALID_INTERRUPT;
 
-	it_num = fdt32_to_cpu(properties[1]);
+	interrupt_type = fdt32_to_cpu(properties[0]);
+	it_num = (int)fdt32_to_cpu(properties[1]);
 
-	switch (fdt32_to_cpu(properties[0])) {
+	if (count == 3) {
+		detection_type = fdt32_to_cpu(properties[2]) & GENMASK_32(3, 0);
+		if (interrupt_type == GIC_PPI &&
+		    detection_type != IRQ_TYPE_EDGE_RISING) {
+			EMSG("PPI must be edge rising");
+			return DT_INFO_INVALID_INTERRUPT;
+		}
+
+		if (interrupt_type == GIC_SPI &&
+		    (detection_type != IRQ_TYPE_EDGE_RISING &&
+		     detection_type != IRQ_TYPE_LEVEL_HIGH)) {
+			EMSG("SPI must be edge rising or high level");
+			return DT_INFO_INVALID_INTERRUPT;
+		}
+	}
+
+	switch (interrupt_type) {
 	case GIC_PPI:
 		it_num += 16;
+		detection_type = IRQ_TYPE_EDGE_RISING;
 		break;
 	case GIC_SPI:
 		it_num += 32;
 		break;
 	default:
-		it_num = DT_INFO_INVALID_INTERRUPT;
+		return DT_INFO_INVALID_INTERRUPT;
 	}
+
+	if (type)
+		*type = detection_type;
+
+	if (prio)
+		*prio = 0;
 
 	return it_num;
 }
@@ -669,6 +698,25 @@ static void gic_it_set_prio(struct gic_data *gd, size_t it, uint8_t prio)
 	DMSG("prio: writing 0x%x to 0x%" PRIxVA,
 		prio, gd->gicd_base + GICD_IPRIORITYR(0) + it);
 	io_write8(gd->gicd_base + GICD_IPRIORITYR(0) + it, prio);
+}
+
+static void gic_it_set_type(struct gic_data *gd, size_t it, uint32_t type)
+{
+	size_t index = it / GICD_ICFGR_NUM_INTS_PER_REG;
+	uint32_t shift = (it % GICD_ICFGR_NUM_INTS_PER_REG) *
+			 GICD_ICFGR_FIELD_BITS;
+	uint32_t icfg = 0;
+
+	assert(type == IRQ_TYPE_EDGE_RISING || type == IRQ_TYPE_LEVEL_HIGH);
+
+	if (type == IRQ_TYPE_EDGE_RISING)
+		icfg = GICD_ICFGR_TYPE_EDGE;
+	else
+		icfg = GICD_ICFGR_TYPE_LEVEL;
+
+	io_mask32(gd->gicd_base + GICD_ICFGR(index),
+		  SHIFT_U32(icfg, shift),
+		  SHIFT_U32(GICD_ICFGR_FIELD_MASK, shift));
 }
 
 static void gic_it_enable(struct gic_data *gd, size_t it)
@@ -916,8 +964,7 @@ void interrupt_main_handler(void)
 #endif /*CFG_CORE_WORKAROUND_ARM_NMFI*/
 
 static void gic_op_add(struct itr_chip *chip, size_t it,
-		       uint32_t type __unused,
-		       uint32_t prio __unused)
+		       uint32_t type, uint32_t prio __unused)
 {
 	struct gic_data *gd = container_of(chip, struct gic_data, chip);
 
@@ -959,6 +1006,8 @@ static void gic_op_add(struct itr_chip *chip, size_t it,
 		/* Set the CPU mask to deliver interrupts to any online core */
 		gic_it_set_cpu_mask(gd, it, 0xff);
 		gic_it_set_prio(gd, it, 0x1);
+		if (type != IRQ_TYPE_NONE)
+			gic_it_set_type(gd, it, type);
 	}
 }
 
@@ -1048,7 +1097,7 @@ static TEE_Result dt_get_gic_chip_cb(struct dt_pargs *arg, void *priv_data,
 {
 	int itr_num = DT_INFO_INVALID_INTERRUPT;
 	struct itr_chip *chip = priv_data;
-	uint32_t phandle_args[2] = { };
+	uint32_t phandle_args[3] = { };
 	uint32_t type = 0;
 	uint32_t prio = 0;
 
@@ -1062,10 +1111,14 @@ static TEE_Result dt_get_gic_chip_cb(struct dt_pargs *arg, void *priv_data,
 	 */
 	if (arg->args_count < 2)
 		return TEE_ERROR_GENERIC;
+
 	phandle_args[0] = cpu_to_fdt32(arg->args[0]);
 	phandle_args[1] = cpu_to_fdt32(arg->args[1]);
+	if (arg->args_count >= 3)
+		phandle_args[2] = cpu_to_fdt32(arg->args[2]);
 
-	itr_num = gic_dt_get_irq((const void *)phandle_args, 2, &type, &prio);
+	itr_num = gic_dt_get_irq((const void *)phandle_args, arg->args_count,
+				 &type, &prio);
 	if (itr_num == DT_INFO_INVALID_INTERRUPT)
 		return TEE_ERROR_GENERIC;
 