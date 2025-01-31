OP-TEE/optee_os

commit e9376d025eb57424056b012335aaa5f812691259

core: interrupt: add interrupt_create_handler()
Adds interrupt_create_handler() API function in interrupt framework.
The function is to be used with interrupt controls obtained
from the DT with  interrupt_dt_get() interrupt_dt_get_by_index() or
interrupt_dt_get_by_name().

The function differs from legacy interrupt_add_handler() in that
this latter always reconfigure the interrupt while new
interrupt_create_handler() function assumes the interrupt was configured
from interrupt_dt_get() or friends.

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/include/kernel/interrupt.h
@@ -346,6 +346,25 @@ static inline TEE_Result interrupt_add_handler(struct itr_handler *hdl)
 }
 
 /*
+ * interrupt_create_handler() - Allocate/register an interrupt callback handler
+ * @itr_chip Interrupt chip obtained from dt_get_interrupt_by_*()
+ * @itr_num Interrupt number obtained from dt_get_interrupt_by_*()
+ * @callback Callback handler function
+ * @priv Private dat to pssa to @callback
+ * @flags INTERRUPT_FLAGS_* or 0
+ * @out_hdl Output allocated and registered handler or NULL
+ *
+ * This function  differs from interrupt_add_handler() in that the
+ * interrupt is not reconfigured. interrupt_create_handler() expects
+ * @itr_desc was obtained from a call to dt_get_interrupt_by_index()
+ * or dt_get_interrupt_by_name(). That call configured the interrupt.
+ */
+TEE_Result interrupt_create_handler(struct itr_chip *itr_chip, size_t itr_num,
+				    itr_handler_t callback, void *priv,
+				    uint32_t flags,
+				    struct itr_handler **out_hdl);
+
+/*
  * interrupt_add_handler_with_chip() - Register an interrupt handler providing
  *	the interrupt chip reference in specific argument @chip.
  * @chip	Interrupt controller

core/kernel/interrupt.c
@@ -104,8 +104,9 @@ TEE_Result interrupt_configure(struct itr_chip *chip, size_t itr_num,
 	return TEE_SUCCESS;
 }
 
-TEE_Result interrupt_add_configure_handler(struct itr_handler *hdl,
-					   uint32_t type, uint32_t prio)
+static TEE_Result add_configure_handler(struct itr_handler *hdl,
+					uint32_t type, uint32_t prio,
+					bool configure)
 {
 	struct itr_handler *h = NULL;
 
@@ -122,9 +123,48 @@ TEE_Result interrupt_add_configure_handler(struct itr_handler *hdl,
 		}
 	}
 
-	interrupt_configure(hdl->chip, hdl->it, type, prio);
+	if (configure)
+		interrupt_configure(hdl->chip, hdl->it, type, prio);
 
 	SLIST_INSERT_HEAD(&hdl->chip->handlers, hdl, link);
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result interrupt_add_configure_handler(struct itr_handler *hdl,
+					   uint32_t type, uint32_t prio)
+{
+	return add_configure_handler(hdl, type, prio, true /* configure */);
+}
+
+TEE_Result interrupt_create_handler(struct itr_chip *itr_chip, size_t itr_num,
+				    itr_handler_t callback, void *priv,
+				    uint32_t flags,
+				    struct itr_handler **out_hdl)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct itr_handler *itr_hdl = NULL;
+
+	itr_hdl = calloc(1, sizeof(*itr_hdl));
+	if (!itr_hdl)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	*itr_hdl = (struct itr_handler){
+		.chip = itr_chip,
+		.it = itr_num,
+		.flags = flags,
+		.handler = callback,
+		.data = priv,
+	};
+
+	res = add_configure_handler(itr_hdl, 0, 0, false /* configure */);
+	if (res) {
+		free(itr_hdl);
+		return res;
+	}
+
+	if (out_hdl)
+		*out_hdl = itr_hdl;
 
 	return TEE_SUCCESS;
 }

commit a3e7b35989ed89d5ce88e5efbee52ddaf3fdeba9

core: interrupt: fix interrupt_create_handler() description
Correct inline description comment of interrupt_create_handler()
that referred deprecated API function labels dt_get_interrupt_by_*()
and mention argument itr_desc that was reaplce with the 2 arguments
itr_chip and itr_num.

By the way, indent functions argument description for consistency
with the other function inline description comments in this header file.

Fixes: e9376d025eb5 ("core: interrupt: add interrupt_create_handler()")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/include/kernel/interrupt.h
@@ -349,17 +349,18 @@ static inline TEE_Result interrupt_add_handler(struct itr_handler *hdl)
 
 /*
  * interrupt_create_handler() - Allocate/register an interrupt callback handler
- * @itr_chip Interrupt chip obtained from dt_get_interrupt_by_*()
- * @itr_num Interrupt number obtained from dt_get_interrupt_by_*()
- * @callback Callback handler function
- * @priv Private dat to pssa to @callback
- * @flags INTERRUPT_FLAGS_* or 0
- * @out_hdl Output allocated and registered handler or NULL
+ * @itr_chip	Interrupt chip obtained from interrupt_dt_get_by_*() or like
+ * @itr_num	Interrupt number obtained from interrupt_dt_get_by_*() or like
+ * @callback	Callback handler function
+ * @priv	Private dat to pssa to @callback
+ * @flags	INTERRUPT_FLAGS_* or 0
+ * @out_hdl	Output allocated and registered handler or NULL
  *
- * This function  differs from interrupt_add_handler() in that the
+ * This function differs from interrupt_add_handler() in that the
  * interrupt is not reconfigured. interrupt_create_handler() expects
- * @itr_desc was obtained from a call to dt_get_interrupt_by_index()
- * or dt_get_interrupt_by_name(). That call configured the interrupt.
+ * @itr_chip and @itr_num were obtained from a call
+ * to interrupt_dt_get_by_index() or interrupt_dt_get_by_name() that
+ * are in charge of configuring the interrupt according to its DT property.
  */
 TEE_Result interrupt_create_handler(struct itr_chip *itr_chip, size_t itr_num,
 				    itr_handler_t callback, void *priv,