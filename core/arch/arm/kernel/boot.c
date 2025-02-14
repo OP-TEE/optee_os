OP-TEE/optee_os

commit 259c34df294fecd1a40823e9a40f18f737a80d3e

core: arm: boot: enable native interrupts before initcalls
Enable native interrupts on Arm architectures when executing
initcalls (early,service,driver,final). This change allows drivers
to leverage interrupts during OP-TEE core initialization, as for example
detecting a firewall access violation when it occurs. Before this change,
OP-TEE needed to complete its initialization and return the secure
monitor for secure interrupts to be handled.

Note that when CFG_NS_VIRTUALIZATION is enabled, initcalls called from
virt_on_stdcall() is not changed here since they are executing from a
standard call entry hence with native and foreign interrupts already
default unmasked.

Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/boot.c
@@ -1069,8 +1069,14 @@ void __weak boot_init_primary_late(unsigned long fdt __unused,
 
 	boot_primary_init_intc();
 	init_vfp_nsec();
-	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION))
+	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+		/* Unmask native interrupts during driver initcalls */
+		thread_set_exceptions(thread_get_exceptions() &
+				      ~THREAD_EXCP_NATIVE_INTR);
 		init_tee_runtime();
+		thread_set_exceptions(thread_get_exceptions() |
+				      THREAD_EXCP_NATIVE_INTR);
+	}
 }
 
 /*
@@ -1082,10 +1088,19 @@ void __weak boot_init_primary_final(void)
 	if (!IS_ENABLED(CFG_WITH_PAGER))
 		boot_mem_release_tmp_alloc();
 
+	/* Unmask native interrupts during init/finalcalls */
+	thread_set_exceptions(thread_get_exceptions() &
+			      ~THREAD_EXCP_NATIVE_INTR);
+
 	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION))
 		call_driver_initcalls();
+
 	call_finalcalls();
+
 	IMSG("Primary CPU switching to normal world boot");
+
+	thread_set_exceptions(thread_get_exceptions() |
+			      THREAD_EXCP_NATIVE_INTR);
 }
 
 static void init_secondary_helper(unsigned long nsec_entry)

commit f7f3608be4ffd5f6e6c2c86c1b5ac6e5e643a095

core: arm: boot: mask native interrupts for virtualization
Native interrupts are prior to this patch unmasked while processing
initcalls. This is only permitted if the temporary stack isn't used.
That's not true when CFG_NS_VIRTUALIZATION=y so fix this by only
unmasking when NS-virtualization isn't enabled.

Fixes: 259c34df294f ("core: arm: boot: enable native interrupts before initcalls")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/boot.c
@@ -1073,20 +1073,20 @@ void __weak boot_init_primary_final(void)
 	boot_primary_init_intc();
 	init_vfp_nsec();
 	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
-		/* Unmask native interrupts during driver initcalls */
+		/*
+		 * Unmask native interrupts during driver initcalls.
+		 *
+		 * NS-virtualization still uses the temporary stack also
+		 * used for exception handling so it must still have native
+		 * interrupts masked.
+		 */
 		thread_set_exceptions(thread_get_exceptions() &
 				      ~THREAD_EXCP_NATIVE_INTR);
 		init_tee_runtime();
-		thread_set_exceptions(thread_get_exceptions() |
-				      THREAD_EXCP_NATIVE_INTR);
 	}
 
 	if (!IS_ENABLED(CFG_WITH_PAGER))
 		boot_mem_release_tmp_alloc();
-
-	/* Unmask native interrupts during init/finalcalls */
-	thread_set_exceptions(thread_get_exceptions() &
-			      ~THREAD_EXCP_NATIVE_INTR);
 
 	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION))
 		call_driver_initcalls();
@@ -1095,8 +1095,10 @@ void __weak boot_init_primary_final(void)
 
 	IMSG("Primary CPU switching to normal world boot");
 
-	thread_set_exceptions(thread_get_exceptions() |
-			      THREAD_EXCP_NATIVE_INTR);
+	/* Mask native interrupts before switching to the normal world */
+	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		thread_set_exceptions(thread_get_exceptions() |
+				      THREAD_EXCP_NATIVE_INTR);
 }
 
 static void init_secondary_helper(void)