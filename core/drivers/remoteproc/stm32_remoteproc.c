OP-TEE/optee_os

commit a03044318866a9975808f548a4f2dcd9083da923

drivers: stm32_remote_proc: add stm32_rproc_is_secure()
Add stm32_remoteproc driver API function stm32_rproc_is_secure()
that return whether of not remote processor management shall be
handled through OP-TEE remoteproc secure services.

Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>

core/drivers/remoteproc/stm32_remoteproc.c
@@ -39,6 +39,7 @@ struct stm32_rproc_mem {
  * @regions:	memory regions used
  * @mcu_rst:	remote processor reset control
  * @hold_boot:	remote processor hold boot control
+ * @ns_loading: True if supports non-secure firmware loading, false otherwise
  */
 struct stm32_rproc_instance {
 	const struct stm32_rproc_compat_data *cdata;
@@ -47,6 +48,7 @@ struct stm32_rproc_instance {
 	struct stm32_rproc_mem *regions;
 	struct rstctrl *mcu_rst;
 	struct rstctrl *hold_boot;
+	bool ns_loading;
 };
 
 /**
@@ -70,6 +72,16 @@ void *stm32_rproc_get(uint32_t rproc_id)
 			break;
 
 	return rproc;
+}
+
+bool stm32_rproc_is_secure(uint32_t rproc_id)
+{
+	struct stm32_rproc_instance *rproc = stm32_rproc_get(rproc_id);
+
+	if (rproc)
+		return !rproc->cdata->ns_loading;
+
+	return false;
 }
 
 TEE_Result stm32_rproc_start(uint32_t rproc_id)
@@ -400,6 +412,7 @@ err:
 
 static const struct stm32_rproc_compat_data stm32_rproc_m4_compat = {
 	.rproc_id = STM32_M4_RPROC_ID,
+	.ns_loading = false,
 };
 
 static const struct dt_device_match stm32_rproc_match_table[] = {

core/include/drivers/stm32_remoteproc.h
@@ -74,4 +74,14 @@ TEE_Result stm32_rproc_stop(uint32_t rproc_id);
  */
 TEE_Result stm32_rproc_clean_up_memories(uint32_t rproc_id);
 
+#ifdef CFG_STM32MP_REMOTEPROC
+/* Return true is secure loading of remoteproc firmware is enabled */
+bool stm32_rproc_is_secure(uint32_t rproc_id);
+#else
+static inline bool stm32_rproc_is_secure(uint32_t rproc_id __unused)
+{
+	return false;
+}
+#endif
+
 #endif /* __DRIVERS_STM32_REMOTEPROC_H */

commit aee5a185ede03589be1f5f710b698d559dead67d

drivers: stm32_remote_proc: fix definition of stm32_rproc_compat_data
Fix compilation error of core/drivers/remoteproc/stm32_remoteproc.c
Move bool ns_loading from "struct stm32_rproc_instance" to
"struct stm32_rproc_compat_data".

Fixes: a03044318866 ("drivers: stm32_remote_proc: add stm32_rproc_is_secure()")
Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>

core/drivers/remoteproc/stm32_remoteproc.c
@@ -43,7 +43,6 @@ struct stm32_rproc_mem {
  * @regions:	memory regions used
  * @mcu_rst:	remote processor reset control
  * @hold_boot:	remote processor hold boot control
- * @ns_loading: True if supports non-secure firmware loading, false otherwise
  */
 struct stm32_rproc_instance {
 	const struct stm32_rproc_compat_data *cdata;
@@ -52,16 +51,17 @@ struct stm32_rproc_instance {
 	struct stm32_rproc_mem *regions;
 	struct rstctrl *mcu_rst;
 	struct rstctrl *hold_boot;
-	bool ns_loading;
 };
 
 /**
  * struct stm32_rproc_compat_data - rproc associated data for compatible list
  *
  * @rproc_id:	identifies the remote processor
+ * @ns_loading: True if supports non-secure firmware loading, false otherwise
  */
 struct stm32_rproc_compat_data {
 	uint32_t rproc_id;
+	bool ns_loading;
 };
 
 static SLIST_HEAD(, stm32_rproc_instance) rproc_list =