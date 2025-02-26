b462b6811872eb91c58959828a8ac2fa0a99efc8
Jens Wiklander <jens.wiklander@linaro.org>
2025-02-26 16:06:21 +0100

    Use malloc flags MAF_* in tee_mm.h
    Switch to use the malloc flags MAF_* in tee_mm.h replacing the previous
    TEE_MM_POOL_* flags. TEE_MM_POOL_* flags are kept defined using MAF_*
    flags to for easier transition. The TEE_MM_POOL_* flags can be moved
    gradually after this commit.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- lib/libutils/isoc/include/malloc_flags.h
+++ lib/libutils/isoc/include/malloc_flags.h
@@ -22,9 +22,14 @@
  * avoid needless translation of one class of flags to another class of
  * flags.
  */
-#define MAF_NULL	0x0	/* Passed if no flags are needed */
-#define MAF_ZERO_INIT	0x1	/* Zero initialize the allocated buffer */
-#define MAF_NEX		0x2	/* Allocate from nexus heap */
-#define MAF_FREE_WIPE	0x4	/* Free wipes allocated buffer */
+#define MAF_NULL	0x00	/* Passed if no flags are needed */
+#define MAF_ZERO_INIT	0x01	/* Zero initialize the allocated buffer */
+#define MAF_NEX		0x02	/* Allocate from nexus heap */
+#define MAF_FREE_WIPE	0x04	/* Free wipes allocated buffer */
+/*
+ * Used by tee_mm_init() to indicatate that the pool should allocate
+ * from high address to low address.
+ */
+#define MAF_HI_ALLOC	0x10
 
 #endif /*__MALLOC_FLAGS_H*/
