43730326be0aef1afbc3fa5dc00cbe65bb73b47f
Huang Borong <huangborong@bosc.ac.cn>
2025-02-28 11:32:23 +0800

    riscv: plat-virt: add APLIC and IMSIC support for QEMU virt platform
    - Add APLIC and IMSIC configurations for the QEMU virt platform.
    - Override the interrupt controller initialization and interrupt handler
      functions when using APLIC or IMSIC.
    
    Signed-off-by: Huang Borong <huangborong@bosc.ac.cn>
    Reviewed-by: Alvin Chang <alvinga@andestech.com>
    Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/arch/riscv/plat-virt/main.c
+++ core/arch/riscv/plat-virt/main.c
@@ -4,6 +4,8 @@
  */
 
 #include <console.h>
+#include <drivers/aplic.h>
+#include <drivers/imsic.h>
 #include <drivers/ns16550.h>
 #include <drivers/plic.h>
 #include <kernel/boot.h>
@@ -17,6 +19,15 @@ register_phys_mem_pgdir(MEM_AREA_IO_NSEC, UART0_BASE, CORE_MMU_PGDIR_SIZE);
 
 register_ddr(DRAM_BASE, DRAM_SIZE);
 
+#if defined(CFG_RISCV_APLIC) || defined(CFG_RISCV_APLIC_MSI)
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, APLIC_BASE,
+			APLIC_SIZE);
+#endif
+#if defined(CFG_RISCV_APLIC_MSI) && defined(CFG_RISCV_IMSIC)
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, IMSIC_BASE,
+			IMSIC_SIZE);
+#endif
+
 #ifdef CFG_RISCV_PLIC
 void boot_primary_init_intc(void)
 {
@@ -28,6 +39,32 @@ void boot_secondary_init_intc(void)
 	plic_hart_init();
 }
 #endif /* CFG_RISCV_PLIC */
+
+#ifdef CFG_RISCV_APLIC
+void boot_primary_init_intc(void)
+{
+	aplic_init(APLIC_BASE);
+}
+
+void boot_secondary_init_intc(void)
+{
+	aplic_init_per_hart();
+}
+#endif /* CFG_RISCV_APLIC */
+
+#if defined(CFG_RISCV_APLIC_MSI) && defined(CFG_RISCV_IMSIC)
+void boot_primary_init_intc(void)
+{
+	aplic_init(APLIC_BASE);
+	imsic_init(IMSIC_BASE);
+}
+
+void boot_secondary_init_intc(void)
+{
+	aplic_init_per_hart();
+	imsic_init_per_hart();
+}
+#endif
 
 #ifdef CFG_16550_UART
 void plat_console_init(void)
@@ -41,4 +78,9 @@ void interrupt_main_handler(void)
 {
 	if (IS_ENABLED(CFG_RISCV_PLIC))
 		plic_it_handle();
+	else if (IS_ENABLED(CFG_RISCV_APLIC))
+		aplic_it_handle();
+	else if (IS_ENABLED(CFG_RISCV_APLIC_MSI) &&
+		 IS_ENABLED(CFG_RISCV_IMSIC))
+		imsic_it_handle();
 }
