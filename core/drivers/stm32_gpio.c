OP-TEE/optee_os

commit 1001585e2e563a2ebefd3b10d6f0c6f97a7e306f

drivers: stm32_gpio: remove GPIO access specific API functions
Removes stm32_gpio API functions to access GPIOs as the driver has
moved to the generic GPIO framework and consumer driver should use the
generic API to access GPIOs. The driver now expects CFG_DRIVERS_GPIO
is enabled.

Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/stm32_gpio.c
@@ -3,7 +3,6 @@
  * Copyright (c) 2017-2023, STMicroelectronics
  *
  * STM32 GPIO driver is used as pin controller for stm32mp SoCs.
- * The driver API is defined in header file stm32_gpio.h.
  */
 
 #include <assert.h>
@@ -23,6 +22,10 @@
 #include <sys/queue.h>
 #include <trace.h>
 #include <util.h>
+
+#ifndef CFG_DRIVERS_GPIO
+#error stm32_gpio driver expects CFG_DRIVERS_GPIO
+#endif
 
 #define GPIO_PIN_MAX		15
 
@@ -763,58 +766,6 @@ int stm32_get_gpio_count(void *fdt, int pinctrl_node, unsigned int bank)
 	}
 
 	return -1;
-}
-
-static __maybe_unused bool valid_gpio_config(unsigned int bank_id,
-					     unsigned int pin, bool input)
-{
-	struct stm32_gpio_bank *bank = stm32_gpio_get_bank(bank_id);
-	uint32_t mode = (io_read32(bank->base + GPIO_MODER_OFFSET) >>
-			 (pin << 1)) & GPIO_MODE_MASK;
-
-	if (pin > GPIO_PIN_MAX)
-		return false;
-
-	if (input)
-		return mode == GPIO_MODE_INPUT;
-	else
-		return mode == GPIO_MODE_OUTPUT;
-}
-
-int stm32_gpio_get_input_level(unsigned int bank_id, unsigned int pin)
-{
-	struct stm32_gpio_bank *bank = stm32_gpio_get_bank(bank_id);
-	int rc = 0;
-
-	if (clk_enable(bank->clock))
-		panic();
-
-	assert(valid_gpio_config(bank_id, pin, true));
-
-	if (io_read32(bank->base + GPIO_IDR_OFFSET) == BIT(pin))
-		rc = 1;
-
-	clk_disable(bank->clock);
-
-	return rc;
-}
-
-void stm32_gpio_set_output_level(unsigned int bank_id, unsigned int pin,
-				 int level)
-{
-	struct stm32_gpio_bank *bank = stm32_gpio_get_bank(bank_id);
-
-	if (clk_enable(bank->clock))
-		panic();
-
-	assert(valid_gpio_config(bank_id, pin, false));
-
-	if (level)
-		io_write32(bank->base + GPIO_BSRR_OFFSET, BIT(pin));
-	else
-		io_write32(bank->base + GPIO_BSRR_OFFSET, BIT(pin + 16));
-
-	clk_disable(bank->clock);
 }
 
 void stm32_gpio_set_secure_cfg(unsigned int bank_id, unsigned int pin,

core/include/drivers/stm32_gpio.h
@@ -111,47 +111,6 @@ void stm32_pinctrl_store_standby_cfg(struct stm32_pinctrl *pinctrl, size_t cnt);
 int stm32_pinctrl_fdt_get_pinctrl(void *fdt, int node,
 				  struct stm32_pinctrl *pinctrl, size_t count);
 
-/*
- * Set target output GPIO pin to high or low level
- *
- * @bank: GPIO bank identifier as assigned by the platform
- * @pin: GPIO pin position in the GPIO bank
- * @high: 1 to set GPIO to high level, 0 to set to GPIO low level
- */
-void stm32_gpio_set_output_level(unsigned int bank, unsigned int pin, int high);
-
-/*
- * Set output GPIO pin referenced by @pinctrl to high or low level
- *
- * @pinctrl: Reference to pinctrl
- * @high: 1 to set GPIO to high level, 0 to set to GPIO low level
- */
-static inline void stm32_pinctrl_set_gpio_level(struct stm32_pinctrl *pinctrl,
-						int high)
-{
-	stm32_gpio_set_output_level(pinctrl->bank, pinctrl->pin, high);
-}
-
-/*
- * Get input GPIO pin current level, high or low
- *
- * @bank: GPIO bank identifier as assigned by the platform
- * @pin: GPIO pin position in the GPIO bank
- * Return 1 if GPIO level is high, 0 if it is low
- */
-int stm32_gpio_get_input_level(unsigned int bank, unsigned int pin);
-
-/*
- * Set target output GPIO pin to high or low level
- *
- * @pinctrl: Reference to pinctrl
- * Return 1 if GPIO level is high, 0 if it is low
- */
-static inline int stm32_pinctrl_get_gpio_level(struct stm32_pinctrl *pinctrl)
-{
-	return stm32_gpio_get_input_level(pinctrl->bank, pinctrl->pin);
-}
-
 #ifdef CFG_STM32_GPIO
 /*
  * Configure pin muxing access permission: can be secure or not

commit e72fe04b87eb1a1da7b8624ee7bd695d9a60bd30

drivers: stm32_gpio: remove test on CFG_DRIVERS_GPIO
Remove test on CFG_DRIVERS_GPIO inside stm32_gpio.c C source file.
CFG_* dependencies are addressed in makefile file (e.g. plat-*/conf.mk),
not in the driver source file.

Fixes: 1001585e2e56 ("drivers: stm32_gpio: remove GPIO access specific API functions")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/stm32_gpio.c
@@ -31,10 +31,6 @@
 #include <trace.h>
 #include <util.h>
 
-#ifndef CFG_DRIVERS_GPIO
-#error stm32_gpio driver expects CFG_DRIVERS_GPIO
-#endif
-
 #define GPIO_PIN_MAX		15
 
 #define GPIO_MODER_OFFSET	U(0x00)