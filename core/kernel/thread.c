OP-TEE/optee_os

commit b5ec8152f3e5ad8cc111952f0483f5cf903aac7c

core: arm: refactor boot
Introduce CFG_BOOT_INIT_THREAD_CORE_LOCAL0 to indicate that
thread_core_local[0] is initialized before the boot_init_* functions are
called.

thread_init_core_local_stacks() and thread_init_thread_core_local() are
replaced by a new version of thread_init_thread_core_local() for
CFG_BOOT_INIT_THREAD_CORE_LOCAL0=y.

Move initialization of thread_core_local[] from very early to
boot_init_primary_late() where various DTBs containing run-time
configuration are available. This will be needed in later patches when
the number of configured cores can be read from DT or some other
run-time configuration.

Move the "OP-TEE version" print and following code from
boot_init_primary_late() to boot_init_primary_final()

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/entry_a32.S
@@ -4,13 +4,14 @@
  * Copyright (c) 2021-2023, Arm Limited
  */
 
-#include <arm32_macros.S>
 #include <arm.h>
+#include <arm32_macros.S>
 #include <asm.S>
 #include <generated/asm-defines.h>
 #include <keep.h>
 #include <kernel/asan.h>
 #include <kernel/cache_helpers.h>
+#include <kernel/thread.h>
 #include <kernel/thread_private.h>
 #include <mm/core_mmu.h>
 #include <platform_config.h>
@@ -448,7 +449,34 @@ shadow_stack_access_ok:
 
 	set_sp
 
-	bl	thread_init_thread_core_local
+	/* Initialize thread_core_local[0] for early boot */
+	bl	thread_get_core_local
+	push	{r0,r1}
+	bl	thread_get_abt_stack
+	pop	{r1,r2}
+	mov	r3, sp
+
+	cps	#CPSR_MODE_IRQ
+	mov	sp, r3
+	cps	#CPSR_MODE_FIQ
+	mov	sp, r3
+	cps	#CPSR_MODE_ABT
+	mov	sp, r1
+	cps	#CPSR_MODE_UND
+	mov	sp, r1
+	cps	#CPSR_MODE_SVC
+
+#ifdef CFG_CORE_DEBUG_CHECK_STACKS
+	mov	r2, #1
+	strb	r2, [r1, #THREAD_CORE_LOCAL_STACKCHECK_RECURSION]
+#endif
+
+	str	sp, [r1, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
+	str	r0, [r1, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
+	mov	r0, #THREAD_ID_INVALID
+	str	r0, [r1, #THREAD_CORE_LOCAL_CURR_THREAD]
+	mov	r0, #THREAD_CLF_TMP
+	str	r0, [r1, #THREAD_CORE_LOCAL_FLAGS]
 
 	/* complete ARM secure MP common configuration */
 	bl	plat_primary_init_early
@@ -516,6 +544,14 @@ shadow_stack_access_ok:
 
 #ifdef CFG_CORE_ASLR
 	/*
+	 * Save a pointer to thread_core_local[core_pos] since we can't
+	 * call thread_get_core_local() again before the recorded end_va's
+	 * have been updated below.
+	 */
+	bl	thread_get_core_local
+	mov	r4, r0
+
+	/*
 	 * Process relocation information for updating with the virtual map
 	 * offset.  We're doing this now before MMU is enabled as some of
 	 * the memory will become write protected.
@@ -541,6 +577,38 @@ shadow_stack_access_ok:
 	ldr	r0, [r0, #CORE_MMU_CONFIG_MAP_OFFSET]
 	bl	boot_mem_relocate
 	/*
+	 * Update recorded end_va, we depend on r4 pointing to the
+	 * pre-relocated thread_core_local[core_pos].
+	 */
+	ldr	r1, =boot_mmu_config
+	ldr	r1, [r1, #CORE_MMU_CONFIG_MAP_OFFSET]
+	add	r4, r4, r1
+	ldr	r0, [r4, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
+	add	r0, r0, r1
+	str	r0, [r4, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
+	ldr	r0, [r4, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
+	add	r0, r0, r1
+	str	r0, [r4, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
+
+	cps	#CPSR_MODE_IRQ
+	mov	sp, r0
+	cps	#CPSR_MODE_FIQ
+	mov	sp, r0
+	cps	#CPSR_MODE_ABT
+	mov	sp, r4
+	cps	#CPSR_MODE_UND
+	mov	sp, r4
+	cps	#CPSR_MODE_SVC
+
+#if defined(CFG_CORE_DEBUG_CHECK_STACKS)
+	/*
+	 * Clear thread_core_local[0].stackcheck_recursion now that the
+	 * stack pointer matches recorded information.
+	 */
+	mov	r2, #0
+	strb	r2, [r4, #THREAD_CORE_LOCAL_STACKCHECK_RECURSION]
+#endif
+	/*
 	 * Reinitialize console, since register_serial_console() has
 	 * previously registered a PA and with ASLR the VA is different
 	 * from the PA.
@@ -557,6 +625,7 @@ shadow_stack_access_ok:
 #endif
 
 	bl	boot_init_primary_early
+	bl	boot_init_primary_late
 #ifndef CFG_NS_VIRTUALIZATION
 	mov	r9, sp
 	ldr	r0, =threads
@@ -567,7 +636,6 @@ shadow_stack_access_ok:
 	mov	r0, #0
 	str	r0, [r8, #THREAD_CORE_LOCAL_FLAGS]
 #endif
-	bl	boot_init_primary_late
 	bl	boot_init_primary_final
 #ifndef CFG_NS_VIRTUALIZATION
 	mov	r0, #THREAD_CLF_TMP

core/arch/arm/kernel/entry_a64.S
@@ -11,6 +11,7 @@
 #include <asm.S>
 #include <generated/asm-defines.h>
 #include <keep.h>
+#include <kernel/thread.h>
 #include <kernel/thread_private.h>
 #include <mm/core_mmu.h>
 #include <sm/optee_smc.h>
@@ -284,7 +285,21 @@ clear_nex_bss:
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
 	set_sp
 
-	bl	thread_init_thread_core_local
+	/* Initialize thread_core_local[0] for early boot */
+	bl	thread_get_abt_stack
+	mov	x1, sp
+	msr	spsel, #1
+	str	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
+	str	x0, [sp, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
+#ifdef CFG_CORE_DEBUG_CHECK_STACKS
+	mov	x0, #1
+	strb	w0, [sp, #THREAD_CORE_LOCAL_STACKCHECK_RECURSION]
+#endif
+	mov	x0, #THREAD_ID_INVALID
+	str	x0, [sp, #THREAD_CORE_LOCAL_CURR_THREAD]
+	mov	w0, #THREAD_CLF_TMP
+	str	w0, [sp, #THREAD_CORE_LOCAL_FLAGS]
+	msr	spsel, #0
 
 	/* Enable aborts now that we can receive exceptions */
 	msr	daifclr, #DAIFBIT_ABT
@@ -376,6 +391,22 @@ clear_nex_bss:
 	ldr	x0, [x0, #CORE_MMU_CONFIG_MAP_OFFSET]
 	bl	boot_mem_relocate
 	/*
+	 * Update recorded end_va.
+	 */
+	adr_l	x0, boot_mmu_config
+	ldr	x0, [x0, #CORE_MMU_CONFIG_MAP_OFFSET]
+	msr	spsel, #1
+	ldr	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
+	add	x1, x1, x0
+	str	x1, [sp, #THREAD_CORE_LOCAL_TMP_STACK_VA_END]
+	ldr	x1, [sp, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
+	add	x1, x1, x0
+	str	x1, [sp, #THREAD_CORE_LOCAL_ABT_STACK_VA_END]
+#ifdef CFG_CORE_DEBUG_CHECK_STACKS
+	strb	wzr, [sp, #THREAD_CORE_LOCAL_STACKCHECK_RECURSION]
+#endif
+	msr	spsel, #0
+	/*
 	 * Reinitialize console, since register_serial_console() has
 	 * previously registered a PA and with ASLR the VA is different
 	 * from the PA.
@@ -400,7 +431,7 @@ clear_nex_bss:
 #ifdef CFG_MEMTAG
 	init_memtag_per_cpu
 #endif
-
+	bl	boot_init_primary_late
 #ifndef CFG_NS_VIRTUALIZATION
 	mov	x23, sp
 	adr_l	x0, threads
@@ -410,7 +441,6 @@ clear_nex_bss:
 	mov	x24, x0
 	str	wzr, [x24, #THREAD_CORE_LOCAL_FLAGS]
 #endif
-	bl	boot_init_primary_late
 #ifdef CFG_CORE_PAUTH
 	adr_l	x0, threads
 	ldp	x1, x2, [x0, #THREAD_CTX_KEYS]

core/kernel/thread.c
@@ -473,6 +473,26 @@ void thread_init_threads(void)
 		TAILQ_INIT(&threads[n].tsd.sess_stack);
 }
 
+vaddr_t __nostackcheck thread_get_abt_stack(void)
+{
+	return GET_STACK_BOTTOM(stack_abt, get_core_pos());
+}
+
+#ifdef CFG_BOOT_INIT_THREAD_CORE_LOCAL0
+void thread_init_thread_core_local(void)
+{
+	size_t n = 0;
+	struct thread_core_local *tcl = thread_core_local;
+
+	for (n = 1; n < CFG_TEE_CORE_NB_CORE; n++) {
+		tcl[n].curr_thread = THREAD_ID_INVALID;
+		tcl[n].flags = THREAD_CLF_TMP;
+		tcl[n].tmp_stack_va_end = GET_STACK_BOTTOM(stack_tmp, n) -
+					  STACK_TMP_OFFS;
+		tcl[n].abt_stack_va_end = GET_STACK_BOTTOM(stack_abt, n);
+	}
+}
+#else
 void __nostackcheck thread_init_thread_core_local(void)
 {
 	size_t n = 0;
@@ -496,6 +516,7 @@ void __nostackcheck thread_init_core_local_stacks(void)
 		tcl[n].abt_stack_va_end = GET_STACK_BOTTOM(stack_abt, n);
 	}
 }
+#endif /*CFG_BOOT_INIT_THREAD_CORE_LOCAL0*/
 
 #if defined(CFG_CORE_PAUTH)
 void thread_init_thread_pauth_keys(void)

mk/config.mk
@@ -1265,10 +1265,14 @@ CFG_CORE_UNSAFE_MODEXP ?= n
 CFG_TA_MEBDTLS_UNSAFE_MODEXP ?= n
 
 # CFG_BOOT_MEM, when enabled, adds stack like memory allocation during boot.
+# CFG_BOOT_INIT_THREAD_CORE_LOCAL0, when enabled, initializes
+# thread_core_local[0] before calling C code.
 ifeq ($(ARCH),arm)
 $(call force,CFG_BOOT_MEM,y)
+$(call force,CFG_BOOT_INIT_THREAD_CORE_LOCAL0,y)
 else
 CFG_BOOT_MEM ?= n
+CFG_BOOT_INIT_THREAD_CORE_LOCAL0 ?= n
 endif
 
 # CFG_DYN_CONFIG, when enabled, use dynamic memory allocation for translation

core/arch/arm/kernel/asm-defines.c
@@ -146,6 +146,10 @@ DEFINES
 	DEFINE(THREAD_CORE_LOCAL_DIRECT_RESP_FID,
 	       offsetof(struct thread_core_local, direct_resp_fid));
 #endif
+#if defined(CFG_CORE_DEBUG_CHECK_STACKS)
+	DEFINE(THREAD_CORE_LOCAL_STACKCHECK_RECURSION,
+	       offsetof(struct thread_core_local, stackcheck_recursion));
+#endif
 
 	DEFINE(STACK_TMP_GUARD, STACK_CANARY_SIZE / 2 + STACK_TMP_OFFS);
 

core/arch/arm/kernel/boot.c
@@ -909,7 +909,6 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
 {
 	vaddr_t va = 0;
 
-	thread_init_core_local_stacks();
 	/*
 	 * Mask asynchronous exceptions before switch to the thread vector
 	 * as the thread handler requires those to be masked while
@@ -970,17 +969,6 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
 		init_pager_runtime(pageable_part);
 	}
 
-	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
-		/*
-		 * Virtualization: We can't initialize threads right now because
-		 * threads belong to "tee" part and will be initialized
-		 * separately per each new virtual guest. So, we'll clear
-		 * "curr_thread" and call it done.
-		 */
-		thread_get_core_local()->curr_thread = -1;
-	} else {
-		thread_init_boot_thread();
-	}
 	thread_init_primary();
 	thread_init_per_cpu();
 	init_sec_mon(nsec_entry);
@@ -1040,6 +1028,22 @@ void __weak boot_init_primary_late(unsigned long fdt __unused,
 	update_external_dt();
 	configure_console_from_dt();
 
+	thread_init_thread_core_local();
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
+		/*
+		 * Virtualization: We can't initialize threads right now because
+		 * threads belong to "tee" part and will be initialized
+		 * separately per each new virtual guest. So, we'll clear
+		 * "curr_thread" and call it done.
+		 */
+		thread_get_core_local()->curr_thread = -1;
+	} else {
+		thread_init_boot_thread();
+	}
+}
+
+void __weak boot_init_primary_final(void)
+{
 	IMSG("OP-TEE version: %s", core_v_str);
 	if (IS_ENABLED(CFG_INSECURE)) {
 		IMSG("WARNING: This OP-TEE configuration might be insecure!");
@@ -1077,14 +1081,7 @@ void __weak boot_init_primary_late(unsigned long fdt __unused,
 		thread_set_exceptions(thread_get_exceptions() |
 				      THREAD_EXCP_NATIVE_INTR);
 	}
-}
 
-/*
- * Note: this function is weak just to make it possible to exclude it from
- * the unpaged area.
- */
-void __weak boot_init_primary_final(void)
-{
 	if (!IS_ENABLED(CFG_WITH_PAGER))
 		boot_mem_release_tmp_alloc();
 

core/include/kernel/thread.h
@@ -67,6 +67,8 @@ struct thread_core_local *thread_get_core_local(void);
  */
 void thread_init_threads(void);
 
+vaddr_t thread_get_abt_stack(void);
+
 /*
  * Called by the init CPU. Sets temporary stack mode for all CPUs
  * (curr_thread = -1 and THREAD_CLF_TMP) and sets the temporary stack limit for

commit a5d086f48be5480ca4b3d4eb8fea01686d24dc70

core: fix CFG_BOOT_INIT_THREAD_CORE_LOCAL0
CFG_BOOT_INIT_THREAD_CORE_LOCAL0 is misleading since it's concerning the
core id of the boot CPU. So rename the configuration flag to
CFG_BOOT_INIT_CURRENT_THREAD_CORE_LOCAL and update the code as needed.
Only thread_init_thread_core_local() has a change of behaviour where the
boot CPU now can have any core id.

Fixes: b5ec8152f3e5 ("core: arm: refactor boot")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/entry_a32.S
@@ -449,7 +449,7 @@ shadow_stack_access_ok:
 
 	set_sp
 
-	/* Initialize thread_core_local[0] for early boot */
+	/* Initialize thread_core_local[current_cpu_id] for early boot */
 	bl	thread_get_core_local
 	push	{r0,r1}
 	bl	thread_get_abt_stack
@@ -602,8 +602,8 @@ shadow_stack_access_ok:
 
 #if defined(CFG_CORE_DEBUG_CHECK_STACKS)
 	/*
-	 * Clear thread_core_local[0].stackcheck_recursion now that the
-	 * stack pointer matches recorded information.
+	 * Clear thread_core_local[current_cpu_id].stackcheck_recursion now
+	 * that the stack pointer matches recorded information.
 	 */
 	mov	r2, #0
 	strb	r2, [r4, #THREAD_CORE_LOCAL_STACKCHECK_RECURSION]

core/arch/arm/kernel/entry_a64.S
@@ -285,7 +285,7 @@ clear_nex_bss:
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */
 	set_sp
 
-	/* Initialize thread_core_local[0] for early boot */
+	/* Initialize thread_core_local[current_cpu_id] for early boot */
 	bl	thread_get_abt_stack
 	mov	x1, sp
 	msr	spsel, #1

core/kernel/thread.c
@@ -538,14 +538,17 @@ vaddr_t __nostackcheck thread_get_abt_stack(void)
 	return GET_STACK_BOTTOM(stack_abt, get_core_pos());
 }
 
-#ifdef CFG_BOOT_INIT_THREAD_CORE_LOCAL0
+#ifdef CFG_BOOT_INIT_CURRENT_THREAD_CORE_LOCAL
 void thread_init_thread_core_local(void)
 {
 	struct thread_core_local *tcl = thread_core_local;
+	const size_t core_pos = get_core_pos();
 	vaddr_t va = 0;
 	size_t n = 0;
 
-	for (n = 1; n < CFG_TEE_CORE_NB_CORE; n++) {
+	for (n = 0; n < CFG_TEE_CORE_NB_CORE; n++) {
+		if (n == core_pos)
+			continue;	/* Already initialized */
 		tcl[n].curr_thread = THREAD_ID_INVALID;
 		tcl[n].flags = THREAD_CLF_TMP;
 
@@ -583,7 +586,7 @@ void __nostackcheck thread_init_core_local_stacks(void)
 		tcl[n].abt_stack_va_end = GET_STACK_BOTTOM(stack_abt, n);
 	}
 }
-#endif /*CFG_BOOT_INIT_THREAD_CORE_LOCAL0*/
+#endif /*CFG_BOOT_INIT_CURRENT_THREAD_CORE_LOCAL*/
 
 #if defined(CFG_CORE_PAUTH)
 void thread_init_thread_pauth_keys(void)

mk/config.mk
@@ -1264,12 +1264,12 @@ CFG_CORE_UNSAFE_MODEXP ?= n
 # exponentiation algorithm.
 CFG_TA_MEBDTLS_UNSAFE_MODEXP ?= n
 
-# CFG_BOOT_INIT_THREAD_CORE_LOCAL0, when enabled, initializes
-# thread_core_local[0] before calling C code.
+# CFG_BOOT_INIT_CURRENT_THREAD_CORE_LOCAL, when enabled, initializes
+# thread_core_local[current_core_pos] before calling C code.
 ifeq ($(ARCH),arm)
-$(call force,CFG_BOOT_INIT_THREAD_CORE_LOCAL0,y)
+$(call force,CFG_BOOT_INIT_CURRENT_THREAD_CORE_LOCAL,y)
 else
-CFG_BOOT_INIT_THREAD_CORE_LOCAL0 ?= n
+CFG_BOOT_INIT_CURRENT_THREAD_CORE_LOCAL ?= n
 endif
 
 # CFG_DYN_CONFIG, when enabled, use dynamic memory allocation for translation