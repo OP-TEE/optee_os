48bd9d990cb8cbdc6fe80aca46f5caf7e15297f0
Yu-Chien Peter Lin <peter.lin@sifive.com>
2025-03-15 03:27:45 -0700

    core: riscv: mm: add per-mapping VA range validation
    Replace single maximum VA check with individual VA
    range validation for each memory map entry during MMU
    initialization, providing earlier detection of invalid
    mappings.
    
    Signed-off-by: Yu-Chien Peter Lin <peter.lin@sifive.com>
    Reviewed-by: Alvin Chang <alvinga@andestech.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/riscv/mm/core_mmu_arch.c
+++ core/arch/riscv/mm/core_mmu_arch.c
@@ -1015,7 +1015,6 @@ void core_init_mmu_prtn(struct mmu_partition *prtn, struct memory_map *mem_map)
 void core_init_mmu(struct memory_map *mem_map)
 {
 	struct mmu_partition *prtn = &default_partition;
-	uint64_t max_va = 0;
 	size_t n = 0;
 
 	if (IS_ENABLED(CFG_DYN_CONFIG)) {
@@ -1041,17 +1040,15 @@ void core_init_mmu(struct memory_map *mem_map)
 	core_init_mmu_prtn_tee(prtn, mem_map);
 
 	for (n = 0; n < mem_map->count; n++) {
-		vaddr_t va_end = mem_map->map[n].va + mem_map->map[n].size - 1;
-
-		if (va_end > max_va)
-			max_va = va_end;
+		if (!core_mmu_va_is_valid(mem_map->map[n].va) ||
+		    !core_mmu_va_is_valid(mem_map->map[n].va +
+					  mem_map->map[n].size - 1))
+			panic("Invalid VA range in memory map");
 	}
 
 	set_user_va_idx(prtn);
 
 	core_init_mmu_prtn_ta(prtn);
-
-	assert(max_va < BIT64(RISCV_MMU_VA_WIDTH));
 }
 
 void core_init_mmu_regs(struct core_mmu_config *cfg)
