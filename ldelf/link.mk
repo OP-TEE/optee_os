e59663eb6ad2d73bebc0b956a0691e16ccb81db6
Thomas Bourgoin <thomas.bourgoin@foss.st.com>
2025-04-03 17:24:13 +0200

    tree wide: fix header files dependencies in linker files
    When linking with a generated linker script like kern.ld.S, dependencies
    with header file are not regenerated.
    Same issue as
    commit acdc32afe18f ("mk/compile.mk: fix header dependency in .d file")
    
    Add option -MP used to fix error generated when removing header files.
    
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

--- ldelf/link.mk
+++ ldelf/link.mk
@@ -44,7 +44,7 @@ $(link-script-pp$(sm)): $(link-script$(sm)) $(conf-file) \
 			$(link-script-pp-makefiles$(sm))
 	@$(cmd-echo-silent) '  CPP     $$@'
 	$(q)mkdir -p $$(dir $$@)
-	$(q)$(CPP$(sm)) -P -MT $$@ -MD -MF $(link-script-dep$(sm)) \
+	$(q)$(CPP$(sm)) -P -MT $$@ -MD -MP -MF $(link-script-dep$(sm)) \
 		$(link-script-cppflags-$(sm)) $$< -o $$@
 
 $(link-out-dir$(sm))/ldelf.elf: $(objs) $(libdeps) $(link-script-pp$(sm))
