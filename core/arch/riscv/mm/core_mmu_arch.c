1da541250186769ef5c225060ab98e7e5ede414f
Alvin Chang <alvinga@andestech.com>
2025-04-11 13:49:24 +0800

    riscv: mm: Explicitly set user VA range inside L2(RV64) and L1(RV32) VPN
    Add macros for level and bit shift of virtual page number (VPN) encoded
    in RISC-V virtual address. Explicitly set range of user virtual address
    inside level 2 VPN by giving CORE_MMU_VPN2_SHIFT since the
    CORE_MMU_BASE_TABLE_SHIFT is not always based on level 2 VPN if the MMU
    scheme is not Sv39.
    
    For RV32, there is only two-level VPN. The user VA range would be inside
    level 1 VPN.
    
    Signed-off-by: Alvin Chang <alvinga@andestech.com>
    Reviewed-by: Yu-Chien Peter Lin <peter.lin@sifive.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/riscv/mm/core_mmu_arch.c
+++ core/arch/riscv/mm/core_mmu_arch.c
@@ -746,11 +746,17 @@ void core_mmu_get_user_va_range(vaddr_t *base, size_t *size)
 {
 	assert(core_mmu_user_va_range_is_defined());
 
+#ifdef RV64
 	if (base)
-		*base = SHIFT_U64(user_va_idx, CORE_MMU_BASE_TABLE_SHIFT);
-
+		*base = SHIFT_U64(user_va_idx, CORE_MMU_VPN2_SHIFT);
 	if (size)
-		*size =  BIT64(CORE_MMU_BASE_TABLE_SHIFT);
+		*size =  BIT64(CORE_MMU_VPN2_SHIFT);
+#else
+	if (base)
+		*base = SHIFT_U64(user_va_idx, CORE_MMU_VPN1_SHIFT);
+	if (size)
+		*size =  BIT64(CORE_MMU_VPN1_SHIFT);
+#endif
 }
 
 void core_mmu_get_user_pgdir(struct core_mmu_table_info *pgd_info)
