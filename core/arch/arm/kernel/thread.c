OP-TEE/optee_os

commit 099918f6744c37ce693c38562f11466b19d573c9

ftrace: Add support for syscall function tracer
This patch adds support for syscall tracing in TEE core. It complements
existing ftrace support for user TAs via adding trace for syscalls that
are invoked by user TAs into the TEE core.

And after this patch ftrace will cover both TA and TEE core code. So lets
rename config option from CFG_TA_FTRACE_SUPPORT to CFG_FTRACE_SUPPORT.

It is optional to enable syscall trace via CFG_SYSCALL_FTRACE=y config
option in addition to CFG_FTRACE_SUPPORT=y config option.

Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/kernel/thread.c
@@ -483,6 +483,38 @@ static bool is_from_user(uint32_t cpsr)
 }
 #endif
 
+#ifdef CFG_SYSCALL_FTRACE
+static void __noprof ftrace_suspend(void)
+{
+	struct tee_ta_session *s = TAILQ_FIRST(&thread_get_tsd()->sess_stack);
+
+	if (!s)
+		return;
+
+	if (s->fbuf)
+		s->fbuf->syscall_trace_suspended = true;
+}
+
+static void __noprof ftrace_resume(void)
+{
+	struct tee_ta_session *s = TAILQ_FIRST(&thread_get_tsd()->sess_stack);
+
+	if (!s)
+		return;
+
+	if (s->fbuf)
+		s->fbuf->syscall_trace_suspended = false;
+}
+#else
+static void __noprof ftrace_suspend(void)
+{
+}
+
+static void __noprof ftrace_resume(void)
+{
+}
+#endif
+
 static bool is_user_mode(struct thread_ctx_regs *regs)
 {
 	return is_from_user((uint32_t)regs->cpsr);
@@ -530,6 +562,9 @@ void thread_resume_from_rpc(uint32_t thread_id, uint32_t a0, uint32_t a1,
 	}
 
 	thread_lazy_save_ns_vfp();
+
+	if (threads[n].have_user_map)
+		ftrace_resume();
 
 	thread_resume(&threads[n].regs);
 	/*NOTREACHED*/
@@ -658,6 +693,9 @@ int thread_state_suspend(uint32_t flags, uint32_t cpsr, vaddr_t pc)
 	int ct = l->curr_thread;
 
 	assert(ct != -1);
+
+	if (core_mmu_user_mapping_is_active())
+		ftrace_suspend();
 
 	thread_check_canaries();
 
@@ -972,7 +1010,7 @@ void thread_init_per_cpu(void)
 
 	thread_init_vbar(get_excp_vect());
 
-#ifdef CFG_TA_FTRACE_SUPPORT
+#ifdef CFG_FTRACE_SUPPORT
 	/*
 	 * Enable accesses to frequency register and physical counter
 	 * register in EL0/PL0 required for timestamping during

lib/libutils/ext/ftrace/ftrace.c
@@ -15,6 +15,9 @@
 #if defined(__KERNEL__)
 #include <arm.h>
 #include <kernel/panic.h>
+#include <kernel/tee_ta_manager.h>
+#include <kernel/thread.h>
+#include <mm/core_mmu.h>
 #else
 #include <arm_user_sysreg.h>
 #include <setjmp.h>
@@ -29,7 +32,28 @@ static const char hex_str[] = "0123456789abcdef";
 static __noprof struct ftrace_buf *get_fbuf(void)
 {
 #if defined(__KERNEL__)
-	return NULL;
+	int ct = thread_get_id_may_fail();
+	struct tee_ta_session *s = NULL;
+	struct thread_specific_data *tsd = NULL;
+
+	if (ct == -1)
+		return NULL;
+
+	if (!(core_mmu_user_va_range_is_defined() &&
+	      core_mmu_user_mapping_is_active()))
+		return NULL;
+
+	tsd = thread_get_tsd();
+	s = TAILQ_FIRST(&tsd->sess_stack);
+
+	if (!s || tsd->ctx != s->ctx)
+		return NULL;
+
+	if (s->fbuf && s->fbuf->syscall_trace_enabled &&
+	    !s->fbuf->syscall_trace_suspended)
+		return s->fbuf;
+	else
+		return NULL;
 #else
 	return &__ftrace_buf_start;
 #endif

ldelf/ftrace.c
@@ -57,6 +57,8 @@ bool ftrace_init(struct ftrace_buf **fbuf_ptr)
 	fbuf->buf_off = fbuf->head_off + count;
 	fbuf->curr_size = 0;
 	fbuf->max_size = fbuf_size - sizeof(struct ftrace_buf) - count;
+	fbuf->syscall_trace_enabled = false;
+	fbuf->syscall_trace_suspended = false;
 
 	*fbuf_ptr = fbuf;
 

core/arch/arm/arm.mk
@@ -149,7 +149,11 @@ core-platform-cflags += $(arm32-platform-cflags-no-hard-float)
 ifeq ($(CFG_UNWIND),y)
 core-platform-cflags += -funwind-tables
 endif
+ifeq ($(CFG_SYSCALL_FTRACE),y)
+core-platform-cflags += $(arm32-platform-cflags-generic-arm)
+else
 core-platform-cflags += $(arm32-platform-cflags-generic-thumb)
+endif
 core-platform-aflags += $(core_arm32-platform-aflags)
 core-platform-aflags += $(arm32-platform-aflags)
 endif
@@ -181,7 +185,7 @@ ta_arm32-platform-cflags += -fpic
 # Thumb mode doesn't support function graph tracing due to missing
 # frame pointer support required to trace function call chain. So
 # rather compile in ARM mode if function tracing is enabled.
-ifeq ($(CFG_TA_FTRACE_SUPPORT),y)
+ifeq ($(CFG_FTRACE_SUPPORT),y)
 ta_arm32-platform-cflags += $(arm32-platform-cflags-generic-arm)
 else
 ta_arm32-platform-cflags += $(arm32-platform-cflags-generic-thumb)

core/arch/arm/include/kernel/misc.h
@@ -13,7 +13,7 @@
 
 size_t __get_core_pos(void);
 
-static inline size_t get_core_pos(void)
+static inline size_t __noprof get_core_pos(void)
 {
 	/*
 	 * Foreign interrupts must be disabled before playing with current

core/arch/arm/include/kernel/user_ta.h
@@ -45,7 +45,7 @@ SLIST_HEAD(load_seg_head, load_seg);
 struct user_ta_ctx {
 	uaddr_t entry_func;
 	uaddr_t dump_entry_func;
-#ifdef CFG_TA_FTRACE_SUPPORT
+#ifdef CFG_FTRACE_SUPPORT
 	uaddr_t ftrace_entry_func;
 #endif
 	uaddr_t dl_entry_func;

core/arch/arm/include/mm/core_mmu.h
@@ -303,7 +303,7 @@ struct core_mmu_user_map {
 #ifdef CFG_WITH_LPAE
 bool core_mmu_user_va_range_is_defined(void);
 #else
-static inline bool core_mmu_user_va_range_is_defined(void)
+static inline bool __noprof core_mmu_user_va_range_is_defined(void)
 {
 	return true;
 }

core/arch/arm/include/sm/sm.h
@@ -37,7 +37,7 @@ struct sm_unbanked_regs {
 #ifdef CFG_SM_NO_CYCLE_COUNTING
 	uint32_t pmcr;
 #endif
-#ifdef CFG_TA_FTRACE_SUPPORT
+#ifdef CFG_FTRACE_SUPPORT
 	uint32_t cntkctl;
 	uint32_t pad;
 #endif

commit f3ab4e49293381e80bb437c6deb1618d2da192e4

core: fix crash during syscall ftrace
Syscall ftrace collects data during a syscall. get_fbuf() checks if
thread_get_id_may_fail() != -1 to see if a function is called under
normal thread execution. This can lead to an inconsistent state if a
native interrupt occur while ftrace_enter() or ftrace_return() is
recording data in the ftrace buffer. So fix this by using
thread_is_in_normal_mode() to exclude ftrace during interrupt
processing.

Reported-by: Jerome Forissier <jerome.forissier@linaro.org>
Closes: https://github.com/OP-TEE/optee_os/issues/7216
Fixes: 099918f6744c ("ftrace: Add support for syscall function tracer")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (vexpress-qemu_armv8a)

core/arch/arm/kernel/thread.c
@@ -439,7 +439,7 @@ vaddr_t thread_get_saved_thread_sp(void)
 #endif /*ARM64*/
 
 #ifdef ARM32
-bool thread_is_in_normal_mode(void)
+bool __noprof thread_is_in_normal_mode(void)
 {
 	return (read_cpsr() & ARM32_CPSR_MODE_MASK) == ARM32_CPSR_MODE_SVC;
 }

core/kernel/thread.c
@@ -426,7 +426,7 @@ bool thread_is_from_abort_mode(void)
  * This function should always be accurate, but it might be possible to
  * implement a more efficient depending on cpu architecture.
  */
-bool __weak thread_is_in_normal_mode(void)
+bool __weak __noprof thread_is_in_normal_mode(void)
 {
 	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
 	struct thread_core_local *l = thread_get_core_local();

lib/libutils/ext/ftrace/ftrace.c
@@ -37,11 +37,10 @@
 static __noprof struct ftrace_buf *get_fbuf(void)
 {
 #if defined(__KERNEL__)
-	short int ct = thread_get_id_may_fail();
 	struct ts_session *s = NULL;
 	struct thread_specific_data *tsd = NULL;
 
-	if (ct == -1)
+	if (!thread_is_in_normal_mode())
 		return NULL;
 
 	if (!(core_mmu_user_va_range_is_defined() &&