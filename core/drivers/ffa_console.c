2e4a52ccb58285439a1a146c4bf2abe0cb1874c3
Sungbae Yoo <sungbaey@nvidia.com>
2025-04-19 16:03:36 +0900

    drivers: ffa_console: register a DT_DRIVER_UART driver
    This registers ffa_console driver as a DT_DRIVER_UART driver.
    
    Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- core/drivers/ffa_console.c
+++ core/drivers/ffa_console.c
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2024, NVIDIA CORPORATION
+ * Copyright (c) 2024-2025, NVIDIA CORPORATION
  */
 
 #include <compiler.h>
 #include <console.h>
 #include <drivers/ffa_console.h>
 #include <drivers/serial.h>
+#include <kernel/dt_driver.h>
 #include <kernel/thread_arch.h>
 
 #define FFA_CONSOLE_LOG_32		(0x8400008A)
@@ -29,3 +30,41 @@ void ffa_console_init(void)
 {
 	register_serial_console(&ffa_console);
 }
+
+#ifdef CFG_DT
+
+static struct serial_chip *ffa_console_dev_alloc(void)
+{
+	return &ffa_console;
+}
+
+static int ffa_console_dev_init(struct serial_chip *chip __unused,
+				const void *fdt __unused, int offs __unused,
+				const char *params __unused)
+{
+	return 0;
+}
+
+static void ffa_console_dev_free(struct serial_chip *chip __unused)
+{
+}
+
+static const struct serial_driver ffa_console_driver = {
+	.dev_alloc = ffa_console_dev_alloc,
+	.dev_init = ffa_console_dev_init,
+	.dev_free = ffa_console_dev_free,
+};
+
+static const struct dt_device_match ffa_console_match_table[] = {
+	{ .compatible = "arm,ffa-console" },
+	{ }
+};
+
+DEFINE_DT_DRIVER(ffa_console_dt_driver) = {
+	.name = "ffa-console",
+	.type = DT_DRIVER_UART,
+	.match_table = ffa_console_match_table,
+	.driver = &ffa_console_driver,
+};
+
+#endif /* CFG_DT */
