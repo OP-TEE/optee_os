58ad77ff9d270e56111d7eec91a1c2ee28b92808
Bryan Brattlof <bb@ti.com>
2025-04-23 12:16:53 -0500

    plat-k3: drivers: add platform flavors for 62A and 62P devices
    Even though the SA2UL integration on the AM62Ax and AM62Px platforms are
    functionally identical to the AM62x platforms many, when building
    OP-TEE manually, are using the platform name they are building for and
    not 'am62x' which leaves SA2UL_BASE undefined and to failed builds:
    
        In file included from core/include/mm/core_memprot.h:9,
                         from core/include/kernel/interrupt.h:10,
                         from core/arch/arm/plat-k3/drivers/sa2ul_rng.c:12:
        core/arch/arm/plat-k3/./platform_config.h:91:34: error: ‘SA2UL_BASE’ undeclared here (not in a function); did you mean ‘SCU_BASE’?
           91 | #define RNG_BASE                (SA2UL_BASE + 0x10000)
              |                                  ^~~~~~~~~~
    
    For now let's just define the AM62Ax and AM62Px platform flavors
    identical to how AM62x is defined and include an #else statement to
    catch when a undefined platform flavor tries to build the SA2UL driver
    
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Andrew Davis <afd@ti.com>
    Signed-off-by: Bryan Brattlof <bb@ti.com>

--- core/arch/arm/plat-k3/platform_config.h
+++ core/arch/arm/plat-k3/platform_config.h
@@ -79,18 +79,29 @@
 #define SA2UL_TI_SCI_DEV_ID	133
 #define SA2UL_TI_SCI_FW_ID	35
 #define SA2UL_TI_SCI_FW_RGN_ID	0
-#elif defined(PLATFORM_FLAVOR_am62x)
+#elif defined(PLATFORM_FLAVOR_am62x) || \
+	defined(PLATFORM_FLAVOR_am62ax) || \
+	defined(PLATFORM_FLAVOR_am62px)
 #define SA2UL_BASE		0x40900000
 #define SA2UL_TI_SCI_DEV_ID	-1
 #define SA2UL_TI_SCI_FW_ID	66
 #define SA2UL_TI_SCI_FW_RGN_ID	1
+#elif !defined(CFG_WITH_SOFTWARE_PRNG)
+/*
+ * If we got here we're trying to build a hardware based RNG driver
+ * but are missing some crticial definitions. This is usually because
+ * we're using the wrong platform flavor.
+ */
+#error "Unknown platform flavor! No SA2UL_BASE address is defined"
 #endif
 #define SA2UL_REG_SIZE		0x1000
 
 /* RNG */
 #define RNG_BASE		(SA2UL_BASE + 0x10000)
 #define RNG_REG_SIZE		0x1000
-#if defined(PLATFORM_FLAVOR_am62x)
+#if defined(PLATFORM_FLAVOR_am62x) || \
+	defined(PLATFORM_FLAVOR_am62ax) || \
+	defined(PLATFORM_FLAVOR_am62px)
 #define RNG_TI_SCI_FW_RGN_ID	2
 #else
 #define RNG_TI_SCI_FW_RGN_ID	3
