OP-TEE/optee_os

commit 414123ae8ca55dcc0536fd95aace052f8c28e749

core: ffa: reserve physical memory for manifest
With CFG_CORE_SEL2_SPMC=y (Hafnium as SPMC at S-EL2), the FF-A manifest
passed to OP-TEE resides in the memory reserved for OP-TEE just before
the load address. The physical memory pool is initialized with the entire
range of secure memory, with holes carved out for already used memory.

Temporarily allocate the physical memory used by the manifest until it's
not needed any longer and released by release_manifest_dt().

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/kernel/dt.c
@@ -12,6 +12,7 @@
 #include <libfdt.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
+#include <mm/phys_mem.h>
 #include <stdio.h>
 #include <string.h>
 #include <trace.h>
@@ -20,6 +21,7 @@ static struct dt_descriptor external_dt __nex_bss;
 
 #if defined(CFG_CORE_FFA)
 static void *manifest_dt __nex_bss;
+static size_t manifest_max_size __nex_bss;
 #endif
 
 const struct dt_driver *dt_find_compatible_driver(const void *fdt, int offs)
@@ -972,29 +974,44 @@ int add_res_mem_dt_node(struct dt_descriptor *dt, const char *name,
 }
 
 #if defined(CFG_CORE_FFA)
-void init_manifest_dt(void *fdt)
+void init_manifest_dt(void *fdt, size_t max_size)
 {
 	manifest_dt = fdt;
+	manifest_max_size = max_size;
 }
 
 void reinit_manifest_dt(void)
 {
-	paddr_t pa = (unsigned long)manifest_dt;
+	paddr_t end_pa = 0;
 	void *fdt = NULL;
+	paddr_t pa = 0;
 	int ret = 0;
 
-	if (!pa) {
+	if (!manifest_dt) {
 		EMSG("No manifest DT found");
 		return;
 	}
 
-	fdt = core_mmu_add_mapping(MEM_AREA_MANIFEST_DT, pa, CFG_DTB_MAX_SIZE);
+	if (IS_ENABLED(CFG_CORE_SEL2_SPMC)) {
+		pa = (unsigned long)manifest_dt;
+		end_pa = pa + manifest_max_size;
+		pa = ROUNDDOWN(pa, SMALL_PAGE_SIZE);
+		end_pa = ROUNDUP(end_pa, SMALL_PAGE_SIZE);
+		if (!phys_mem_alloc2(pa, end_pa - pa)) {
+			EMSG("Failed to reserve manifest DT physical memory %#"PRIxPA"..%#"PRIxPA" len %#zx",
+			     pa, end_pa - 1, end_pa - pa);
+			panic();
+		}
+	}
+
+	pa = (unsigned long)manifest_dt;
+	fdt = core_mmu_add_mapping(MEM_AREA_MANIFEST_DT, pa, manifest_max_size);
 	if (!fdt)
 		panic("Failed to map manifest DT");
 
 	manifest_dt = fdt;
 
-	ret = fdt_check_full(fdt, CFG_DTB_MAX_SIZE);
+	ret = fdt_check_full(fdt, manifest_max_size);
 	if (ret < 0) {
 		EMSG("Invalid manifest Device Tree at %#lx: error %d", pa, ret);
 		panic();
@@ -1010,20 +1027,28 @@ void *get_manifest_dt(void)
 
 static TEE_Result release_manifest_dt(void)
 {
+	paddr_t pa = 0;
+
 	if (!manifest_dt)
 		return TEE_SUCCESS;
 
+	if (IS_ENABLED(CFG_CORE_SEL2_SPMC))
+		pa = virt_to_phys(manifest_dt);
+
 	if (core_mmu_remove_mapping(MEM_AREA_MANIFEST_DT, manifest_dt,
-				    CFG_DTB_MAX_SIZE))
+				    manifest_max_size))
 		panic("Failed to remove temporary manifest DT mapping");
 	manifest_dt = NULL;
+
+	if (IS_ENABLED(CFG_CORE_SEL2_SPMC))
+		tee_mm_free(phys_mem_mm_find(pa));
 
 	return TEE_SUCCESS;
 }
 
 boot_final(release_manifest_dt);
 #else
-void init_manifest_dt(void *fdt __unused)
+void init_manifest_dt(void *fdt __unused, size_t max_size __unused)
 {
 }
 

core/arch/arm/kernel/boot.c
@@ -1316,7 +1316,8 @@ static void *get_fdt_from_boot_info(struct ffa_boot_info_header_1_1 *hdr)
 	return fdt;
 }
 
-static void get_sec_mem_from_manifest(void *fdt, paddr_t *base, size_t *size)
+static void get_sec_mem_from_manifest(void *fdt, paddr_t *base,
+				      paddr_size_t *size)
 {
 	int ret = 0;
 	uint64_t num = 0;
@@ -1411,20 +1412,30 @@ void __weak boot_save_args(unsigned long a0, unsigned long a1,
 	}
 
 	if (IS_ENABLED(CFG_CORE_FFA)) {
+		size_t fdt_max_size = CFG_DTB_MAX_SIZE;
+		void *fdt = NULL;
+
 		if (IS_ENABLED(CFG_CORE_SEL2_SPMC) ||
 		    IS_ENABLED(CFG_CORE_EL3_SPMC))
-			init_manifest_dt(get_fdt_from_boot_info((void *)a0));
+			fdt = get_fdt_from_boot_info((void *)a0);
 		else
-			init_manifest_dt((void *)a0);
-		if (IS_ENABLED(CFG_CORE_SEL2_SPMC) &&
-		    IS_ENABLED(CFG_CORE_PHYS_RELOCATABLE)) {
+			fdt = (void *)a0;
+		if (IS_ENABLED(CFG_CORE_SEL2_SPMC)) {
+			paddr_size_t size = 0;
 			paddr_t base = 0;
-			size_t size = 0;
 
-			get_sec_mem_from_manifest(get_manifest_dt(),
-						  &base, &size);
-			core_mmu_set_secure_memory(base, size);
+			if (IS_ENABLED(CFG_CORE_PHYS_RELOCATABLE)) {
+				get_sec_mem_from_manifest(fdt, &base, &size);
+				core_mmu_set_secure_memory(base, size);
+			} else {
+				core_mmu_get_secure_memory(&base, &size);
+			}
+			assert((unsigned long)fdt >= base);
+			assert((unsigned long)fdt <= base + size);
+			assert((unsigned long)fdt < VCORE_START_VA);
+			fdt_max_size = VCORE_START_VA - (unsigned long)fdt;
 		}
+		init_manifest_dt(fdt, fdt_max_size);
 	} else {
 		if (IS_ENABLED(CFG_WITH_PAGER)) {
 #if defined(CFG_PAGEABLE_ADDR)

core/include/kernel/dt.h
@@ -320,15 +320,17 @@ int add_res_mem_dt_node(struct dt_descriptor *dt, const char *name,
 /*
  * init_manifest_dt() - Initialize the manifest DTB to given address.
  * @fdt:	Physical address where the manifest DTB located.
+ * @max_size:	Maximum size of the DTB
  *
  * Initialize the manifest DTB to physical address
  */
-void init_manifest_dt(void *fdt);
+void init_manifest_dt(void *fdt, size_t max_size);
 
 /*
  * reinit_manifest_dt() - Reinitialize the manifest DTB
  *
- * Add MMU mapping of the manifest DTB and initialize device tree overlay
+ * Reserve used physical memory, add MMU mapping of the manifest DTB, and
+ * initialize device tree overlay
  */
 void reinit_manifest_dt(void);
 
@@ -492,7 +494,8 @@ static inline int add_res_mem_dt_node(struct dt_descriptor *dt __unused,
 	return -1;
 }
 
-static inline void init_manifest_dt(void *fdt __unused)
+static inline void init_manifest_dt(void *fdt __unused,
+				    size_t max_size __unused)
 {
 }
 

commit e7b391f33efc2f0e9493168d8ff23904e4d31089

core: ffa: reserve physical nexus memory for manifest
In reinit_manifest_dt() when reserving physical memory for the manifest,
allocate from physical nexus memory instead to support ns-virtualization.

Fixes: 414123ae8ca5 ("core: ffa: reserve physical memory for manifest")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/kernel/dt.c
@@ -997,7 +997,7 @@ void reinit_manifest_dt(void)
 		end_pa = pa + manifest_max_size;
 		pa = ROUNDDOWN(pa, SMALL_PAGE_SIZE);
 		end_pa = ROUNDUP(end_pa, SMALL_PAGE_SIZE);
-		if (!phys_mem_alloc2(pa, end_pa - pa)) {
+		if (!nex_phys_mem_alloc2(pa, end_pa - pa)) {
 			EMSG("Failed to reserve manifest DT physical memory %#"PRIxPA"..%#"PRIxPA" len %#zx",
 			     pa, end_pa - 1, end_pa - pa);
 			panic();
@@ -1041,7 +1041,7 @@ static TEE_Result release_manifest_dt(void)
 	manifest_dt = NULL;
 
 	if (IS_ENABLED(CFG_CORE_SEL2_SPMC))
-		tee_mm_free(phys_mem_mm_find(pa));
+		tee_mm_free(nex_phys_mem_mm_find(pa));
 
 	return TEE_SUCCESS;
 }