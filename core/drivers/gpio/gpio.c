00fe336d492e79faa8160768861afae1e4762efa
Patrick Delaunay <patrick.delaunay@foss.st.com>
2025-05-06 18:53:28 +0200

    drivers: gpio: support gpio suffix in device tree
    Align the OP-TEE GPIO driver on the Linux GPIO lib behavior for the gpio
    properties in device tree: search  "gpios" or "gpio" when gpio_name is
    omitted and "foo-gpios" or "foo-gpio" when and if the gpio_name="foo".
    
    This patch allows use of the function gpio_dt_get_by_index()
    by GPIO consumers for all supported bindings.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>

--- core/drivers/gpio/gpio.c
+++ core/drivers/gpio/gpio.c
@@ -10,6 +10,9 @@
 #include <tee_api_types.h>
 #include <util.h>
 
+/* gpio suffixes used for device tree lookup */
+static const char * const gpio_suffixes[] = { "gpios", "gpio" };
+
 TEE_Result gpio_dt_alloc_pin(struct dt_pargs *pargs, struct gpio **out_gpio)
 {
 	struct gpio *gpio = NULL;
@@ -29,41 +32,33 @@ TEE_Result gpio_dt_alloc_pin(struct dt_pargs *pargs, struct gpio **out_gpio)
 	return TEE_SUCCESS;
 }
 
-static char *gpio_get_dt_prop_name(const char *gpio_name)
-{
-	int ret = 0;
-	char *prop_name = NULL;
-	size_t max_len = strlen(gpio_name) + strlen("-gpios") + 1;
-
-	prop_name = calloc(1, max_len);
-	if (!prop_name)
-		return NULL;
-
-	ret = snprintf(prop_name, max_len, "%s-gpios", gpio_name);
-	if (ret < 0 || (size_t)ret >= max_len) {
-		free(prop_name);
-		return NULL;
-	}
-
-	return prop_name;
-}
-
 TEE_Result gpio_dt_get_by_index(const void *fdt, int nodeoffset,
 				unsigned int index, const char *gpio_name,
 				struct gpio **gpio)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
-	char *prop_name = NULL;
+	char prop_name[32]; /* 32 is max size of property name in DT */
 	void *out_gpio = NULL;
+	unsigned int i = 0;
 
-	prop_name = gpio_get_dt_prop_name(gpio_name);
-	if (!prop_name)
-		return TEE_ERROR_OUT_OF_MEMORY;
+	/* Try GPIO properties "foo-gpios" and "foo-gpio" */
+	for (i = 0; i < ARRAY_SIZE(gpio_suffixes); i++) {
+		if (gpio_name)
+			snprintf(prop_name, sizeof(prop_name), "%s-%s",
+				 gpio_name, gpio_suffixes[i]);
+		else
+			snprintf(prop_name, sizeof(prop_name), "%s",
+				 gpio_suffixes[i]);
 
-	res = dt_driver_device_from_node_idx_prop(prop_name, fdt, nodeoffset,
-						  index, DT_DRIVER_GPIO,
-						  &out_gpio);
-	free(prop_name);
+		res = dt_driver_device_from_node_idx_prop(prop_name, fdt,
+							  nodeoffset,
+							  index, DT_DRIVER_GPIO,
+							  &out_gpio);
+
+		if (res != TEE_ERROR_ITEM_NOT_FOUND)
+			break;
+	}
+
 	if (!res)
 		*gpio = out_gpio;
 
