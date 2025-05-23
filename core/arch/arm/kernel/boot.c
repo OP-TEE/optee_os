OP-TEE/optee_os

commit 1c1f8b65b5c67ed04adb6fd5c4eb7595930e1f5b

core: mm: unify secure core and TA memory
In configurations where secure core and TA memory is allocated from the
same contiguous physical memory block, carve out the memory needed by
OP-TEE core and make the rest available as TA memory.

This is needed by later patches where more core memory is allocated as
needed from the pool of TA memory.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/mm/core_mmu.c
@@ -2627,6 +2627,15 @@ static TEE_Result teecore_init_pub_ram(void)
 early_init(teecore_init_pub_ram);
 #endif /*CFG_CORE_RESERVED_SHM*/
 
+static void __maybe_unused carve_out_core_mem(paddr_t pa, paddr_t end_pa)
+{
+	tee_mm_entry_t *mm __maybe_unused = NULL;
+
+	DMSG("%#"PRIxPA" .. %#"PRIxPA, pa, end_pa);
+	mm = phys_mem_alloc2(pa, end_pa - pa);
+	assert(mm);
+}
+
 void core_mmu_init_phys_mem(void)
 {
 	paddr_t ps = 0;
@@ -2643,10 +2652,48 @@ void core_mmu_init_phys_mem(void)
 		virt_get_ta_ram(&s, &e);
 		ps = virt_to_phys((void *)s);
 		size = e - s;
-
+		phys_mem_init(0, 0, ps, size);
 	} else {
-		core_mmu_get_ta_range(&ps, &size);
-	}
+#ifdef CFG_WITH_PAGER
+		/*
+		 * The pager uses all core memory so there's no need to add
+		 * it to the pool.
+		 */
+		static_assert(ARRAY_SIZE(secure_only) == 2);
+		phys_mem_init(0, 0, secure_only[1].paddr, secure_only[1].size);
+#else /*!CFG_WITH_PAGER*/
+		size_t align = BIT(CORE_MMU_USER_CODE_SHIFT);
+		paddr_t end_pa = 0;
+		paddr_t pa = 0;
 
-	phys_mem_init(0, 0, ps, size);
+		static_assert(ARRAY_SIZE(secure_only) <= 2);
+		if (ARRAY_SIZE(secure_only) == 2) {
+			ps = secure_only[1].paddr;
+			size = secure_only[1].size;
+		}
+		phys_mem_init(secure_only[0].paddr, secure_only[0].size,
+			      ps, size);
+
+		/*
+		 * The VCORE macros are relocatable so we need to translate
+		 * the addresses now that the MMU is enabled.
+		 */
+		end_pa = vaddr_to_phys(ROUNDUP(VCORE_FREE_END_PA,
+					       align) - 1) + 1;
+		/* Carve out the part used by OP-TEE core */
+		carve_out_core_mem(vaddr_to_phys(VCORE_UNPG_RX_PA), end_pa);
+		if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS)) {
+			pa = vaddr_to_phys(ROUNDUP(ASAN_MAP_PA, align));
+			carve_out_core_mem(pa, pa + ASAN_MAP_SZ);
+		}
+
+		/* Carve out test SDP memory */
+#ifdef TEE_SDP_TEST_MEM_BASE
+		if (TEE_SDP_TEST_MEM_SIZE) {
+			pa = vaddr_to_phys(TEE_SDP_TEST_MEM_BASE);
+			carve_out_core_mem(pa, pa + TEE_SDP_TEST_MEM_SIZE);
+		}
+#endif
+#endif /*!CFG_WITH_PAGER*/
+	}
 }

commit 6e2a69e0ceb4ab7754e62c3d50dbf41eaa68e6e6

core: fix asan for CFG_WITH_PAGER=n
Some fixes are needed to make CFG_CORE_SANITIZE_KADDRESS=y work both
with and without CFG_DYN_STACK_CONFIG=y.

Sanitizing stack addresses aren't supported with CFG_DYN_STACK_CONFIG=y
since it requires extensive changes in the ASAN framework.

The VCORE_FREE area is moved right before the .asan_shadow area.

init_asan() calls boot_mem_init_asan() to tag access to already
allocated boot memory.

entry_a32.S is updated to skip allowing access to stacks in the
.asan_shadow area for CFG_DYN_STACK_CONFIG=y since stacks are stored
elsewhere in that configuration.

entry_a64.S is updated to initialize the .asan_shadow area in the same
way as in entry_a32.S.

The .asan_shadow area is mapped explicitly in collect_mem_ranges()
instead of relying on the now non-existent coverage of
MEM_AREA_TEE_RAM_RW.

CFG_DYN_STACK_CONFIG=y and CFG_WITH_PAGER=y is not yet known to work.

Fixes: 1c1f8b65b5c6 ("core: mm: unify secure core and TA memory")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/boot.c
@@ -326,6 +326,7 @@ static void init_asan(void)
 	 * Add access to areas that aren't opened automatically by a
 	 * constructor.
 	 */
+	boot_mem_init_asan();
 	asan_tag_access(&__ctor_list, &__ctor_end);
 	asan_tag_access(__rodata_start, __rodata_end);
 #ifdef CFG_WITH_PAGER

core/arch/arm/kernel/entry_a32.S
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2014, Linaro Limited
+ * Copyright (c) 2014-2025, Linaro Limited
  * Copyright (c) 2021-2023, Arm Limited
  */
 
@@ -433,6 +433,7 @@ shadow_no_access:
 	cmp	r0, r1
 	bls	shadow_no_access
 
+#if !defined(CFG_DYN_STACK_CONFIG)
 	/* Mark the entire stack area as OK */
 	ldr	r2, =CFG_ASAN_SHADOW_OFFSET
 	ldr	r0, =__nozi_stack_start	/* start */
@@ -446,6 +447,7 @@ shadow_stack_access_ok:
 	strb	r2, [r0], #1
 	cmp	r0, r1
 	bls	shadow_stack_access_ok
+#endif
 #endif
 
 #if defined(CFG_DYN_STACK_CONFIG)

core/arch/arm/kernel/entry_a64.S
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2015-2022, Linaro Limited
+ * Copyright (c) 2015-2025, Linaro Limited
  * Copyright (c) 2021-2023, Arm Limited
  */
 
@@ -11,6 +11,7 @@
 #include <asm.S>
 #include <generated/asm-defines.h>
 #include <keep.h>
+#include <kernel/asan.h>
 #include <kernel/thread.h>
 #include <kernel/thread_private.h>
 #include <kernel/thread_private_arch.h>
@@ -280,6 +281,31 @@ clear_nex_bss:
 	cbz	x0, 1f
 	bl	relocate
 1:
+#endif
+
+#ifdef CFG_CORE_SANITIZE_KADDRESS
+	/* Initialize the entire shadow area with no access */
+	adr_l	x0, __asan_shadow_start	/* start */
+	adr_l	x1, __asan_shadow_end	/* limit */
+	mov	x2, #ASAN_DATA_RED_ZONE
+1:	str	x2, [x0], #8
+	cmp	x0, x1
+	bls	1b
+
+#if !defined(CFG_DYN_STACK_CONFIG)
+	/* Mark the entire stack area as OK */
+	mov_imm	x2, CFG_ASAN_SHADOW_OFFSET
+	adr_l	x0, __nozi_stack_start	/* start */
+	lsr	x0, x0, #ASAN_BLOCK_SHIFT
+	add	x0, x0, x2
+	adr_l	x1, __nozi_stack_end	/* limit */
+	lsr	x1, x1, #ASAN_BLOCK_SHIFT
+	add	x1, x1, x2
+	mov	w2, #0
+1:	strb	w2, [x0], #1
+	cmp	x0, x1
+	bls	1b
+#endif
 #endif
 
 	/* Setup SP_EL0 and SP_EL1, SP will be set to SP_EL0 */

core/arch/arm/kernel/kern.ld.S
@@ -386,8 +386,18 @@ SECTIONS
 #endif /*CFG_WITH_PAGER*/
 
 #ifdef CFG_CORE_SANITIZE_KADDRESS
+	/*
+	 * Guard against moving the location counter backwards in the assignment
+	 * below.
+	 */
+	ASSERT(. <= (TEE_RAM_START + (TEE_RAM_VA_SIZE * 8) / 9 - 8),
+		"TEE_RAM_VA_SIZE is too small")
 	. = TEE_RAM_START + (TEE_RAM_VA_SIZE * 8) / 9 - 8;
 	. = ALIGN(8);
+#ifndef CFG_WITH_PAGER
+	__flatmap_free_size = LD_ROUNDDOWN(. - __flatmap_free_start,
+					   SMALL_PAGE_SIZE);
+#endif
 	.asan_shadow : {
 		__asan_shadow_start = .;
 		. += TEE_RAM_VA_SIZE / 9;
@@ -413,7 +423,9 @@ SECTIONS
 
 #ifndef CFG_WITH_PAGER
 	__get_tee_init_end = .;
+#ifndef CFG_CORE_SANITIZE_KADDRESS
 	__flatmap_free_size = _end_of_ram - __flatmap_free_start;
+#endif
 #endif
 
 	/*

core/core.mk
@@ -62,8 +62,11 @@ $(error error: CFG_CORE_SANITIZE_KADDRESS not supported with Clang)
 endif
 cflags_kasan	+= -fsanitize=kernel-address \
 		   -fasan-shadow-offset=$(CFG_ASAN_SHADOW_OFFSET)\
-		   --param asan-stack=1 --param asan-globals=1 \
+		   --param asan-globals=1 \
 		   --param asan-instrumentation-with-call-threshold=0
+ifneq ($(CFG_DYN_STACK_CONFIG),y)
+cflags_kasan	+= --param asan-stack=1
+endif
 cflags$(sm)	+= $(cflags_kasan)
 endif
 ifeq ($(CFG_CORE_DEBUG_CHECK_STACKS),y)

core/mm/core_mmu.c
@@ -1179,14 +1179,8 @@ static void collect_mem_ranges(struct memory_map *mem_map)
 		ADD_PHYS_MEM(MEM_AREA_SEC_RAM_OVERALL, secure_only[n].paddr,
 			     secure_only[n].size);
 
-	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS) &&
-	    IS_ENABLED(CFG_WITH_PAGER)) {
-		/*
-		 * Asan ram is part of MEM_AREA_TEE_RAM_RW when pager is
-		 * disabled.
-		 */
+	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS))
 		ADD_PHYS_MEM(MEM_AREA_TEE_ASAN, ASAN_MAP_PA, ASAN_MAP_SZ);
-	}
 
 #undef ADD_PHYS_MEM
 