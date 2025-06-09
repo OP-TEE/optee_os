OP-TEE/optee_os

commit bd8bea6f45b004aa153fa8d7922dec6a655f50e3

core: boot_mem: enable asan support
Add boot_mem_init_asan() to tag all allocated memory with
asan_tag_access().

Allocations with boot_mem_alloc() and boot_mem_alloc_tmp() are tagged
with asan_tag_access().

boot_mem_foreach_padding() temporarily allow access to paddings for the
callback and restores no-access if the callback returns false to tell
that the padding wasn't consumed.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/mm/boot_mem.c
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2024, Linaro Limited
+ * Copyright (c) 2024-2025, Linaro Limited
  */
 
 #include <assert.h>
+#include <kernel/asan.h>
 #include <kernel/boot.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
@@ -61,6 +62,9 @@ static void *mem_alloc_tmp(struct boot_mem_desc *desc, size_t len, size_t align)
 {
 	vaddr_t va = 0;
 
+	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS))
+		align = MAX(align, ASAN_BLOCK_SIZE);
+
 	assert(desc && desc->mem_start && desc->mem_end);
 	assert(IS_POWER_OF_TWO(align) && !(len % align));
 	if (SUB_OVERFLOW(desc->mem_end, len, &va))
@@ -69,18 +73,26 @@ static void *mem_alloc_tmp(struct boot_mem_desc *desc, size_t len, size_t align)
 	if (va < desc->mem_start)
 		panic();
 	desc->mem_end = va;
+
+	asan_tag_access((void *)va, (void *)(va + len));
+
 	return (void *)va;
 }
 
 static void add_padding(struct boot_mem_desc *desc, vaddr_t va)
 {
 	struct boot_mem_padding *pad = NULL;
-	vaddr_t rounded = ROUNDUP(desc->mem_start, alignof(*pad));
+	vaddr_t start = desc->mem_start;
+	vaddr_t rounded = 0;
 
+	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS))
+		start = ROUNDUP(start, ASAN_BLOCK_SIZE);
+
+	rounded = ROUNDUP(start, alignof(*pad));
 	if (rounded < va && va - rounded > sizeof(*pad)) {
 		pad = (struct boot_mem_padding *)rounded;
-		pad->start = desc->mem_start;
-		pad->len = va - desc->mem_start;
+		pad->start = start;
+		pad->len = va - start;
 		DMSG("%#"PRIxVA" len %#zx", pad->start, pad->len);
 		pad->next = desc->padding;
 		desc->padding = pad;
@@ -92,6 +104,9 @@ static void *mem_alloc(struct boot_mem_desc *desc, size_t len, size_t align)
 	vaddr_t va = 0;
 	vaddr_t ve = 0;
 
+	if (IS_ENABLED(CFG_CORE_SANITIZE_KADDRESS))
+		align = MAX(align, ASAN_BLOCK_SIZE);
+
 	runtime_assert(!IS_ENABLED(CFG_WITH_PAGER));
 	assert(desc && desc->mem_start && desc->mem_end);
 	assert(IS_POWER_OF_TWO(align) && !(len % align));
@@ -102,6 +117,9 @@ static void *mem_alloc(struct boot_mem_desc *desc, size_t len, size_t align)
 		panic();
 	add_padding(desc, va);
 	desc->mem_start = ve;
+
+	asan_tag_access((void *)va, (void *)(va + len));
+
 	return (void *)va;
 }
 
@@ -120,6 +138,28 @@ void boot_mem_init(vaddr_t start, vaddr_t end, vaddr_t orig_end)
 					     sizeof(*boot_mem_desc->reloc),
 					     alignof(*boot_mem_desc->reloc));
 	memset(boot_mem_desc->reloc, 0, sizeof(*boot_mem_desc->reloc));
+}
+
+static bool tag_padding_no_access(vaddr_t va __unused, size_t len __unused,
+				  void *ptr __unused)
+{
+	/*
+	 * No need to do anything since boot_mem_foreach_padding() calls
+	 * asan_tag_access() before this function is called and calls
+	 * asan_tag_no_access() after this function has returned false.
+	 */
+	return false;
+}
+
+void boot_mem_init_asan(void)
+{
+	asan_tag_access((void *)ROUNDDOWN(boot_mem_desc->orig_mem_start,
+					  ASAN_BLOCK_SIZE),
+			(void *)boot_mem_desc->mem_start);
+	asan_tag_access((void *)ROUNDDOWN(boot_mem_desc->mem_end,
+					  ASAN_BLOCK_SIZE),
+			(void *)boot_mem_desc->orig_mem_end);
+	boot_mem_foreach_padding(tag_padding_no_access, NULL);
 }
 
 void boot_mem_add_reloc(void *ptr)
@@ -216,9 +256,13 @@ void boot_mem_foreach_padding(bool (*func)(vaddr_t va, size_t len, void *ptr),
 
 	prev = &boot_mem_desc->padding;
 	for (pad = boot_mem_desc->padding; pad; pad = next) {
-		vaddr_t start = pad->start;
-		size_t len = pad->len;
+		vaddr_t start = 0;
+		size_t len = 0;
 
+		asan_tag_access(pad, (uint8_t *)pad + sizeof(*pad));
+		start = pad->start;
+		len = pad->len;
+		asan_tag_access((void *)start, (void *)(start + len));
 		next = pad->next;
 		if (func(start, len, ptr)) {
 			DMSG("consumed %p %#"PRIxVA" len %#zx",
@@ -228,6 +272,8 @@ void boot_mem_foreach_padding(bool (*func)(vaddr_t va, size_t len, void *ptr),
 			DMSG("keeping %p %#"PRIxVA" len %#zx",
 			     pad, start, len);
 			prev = &pad->next;
+			asan_tag_no_access((void *)start,
+					   (void *)(start + len));
 		}
 	}
 }

core/include/kernel/boot.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2015-2020, Linaro Limited
+ * Copyright (c) 2015-2025, Linaro Limited
  * Copyright (c) 2021-2023, Arm Limited
  */
 #ifndef __KERNEL_BOOT_H
@@ -115,6 +115,7 @@ int mark_static_shm_as_reserved(struct dt_descriptor *dt);
  * free physical memory once MMU is initialized.
  */
 void boot_mem_init(vaddr_t start, vaddr_t end, vaddr_t orig_end);
+void boot_mem_init_asan(void);
 void boot_mem_foreach_padding(bool (*func)(vaddr_t va, size_t len, void *ptr),
 			      void *ptr);
 void boot_mem_add_reloc(void *ptr);

commit 995554285a7decbe74ee43c62a7196204e2a58ee

core: boot_mem: remove multiple of align restriction
The two internal boot mem functions mem_alloc_tmp() and mem_alloc() has
until now required the length of the request to be a multiple of align.
While it may minimize amount of wasted padding it serves no real purpose
and with the commit bd8bea6f45b0 ("core: boot_mem: enable asan support")
the alignment is increased internally with CFG_CORE_SANITIZE_KADDRESS=y.
This makes it virtually impossible for the caller always supply correct
lengths. So fix this by removing that restriction.

Fixes: bd8bea6f45b0 ("core: boot_mem: enable asan support")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/boot_mem.c
@@ -66,7 +66,7 @@ static void *mem_alloc_tmp(struct boot_mem_desc *desc, size_t len, size_t align)
 		align = MAX(align, ASAN_BLOCK_SIZE);
 
 	assert(desc && desc->mem_start && desc->mem_end);
-	assert(IS_POWER_OF_TWO(align) && !(len % align));
+	assert(IS_POWER_OF_TWO(align));
 	if (SUB_OVERFLOW(desc->mem_end, len, &va))
 		panic();
 	va = ROUNDDOWN2(va, align);
@@ -109,7 +109,7 @@ static void *mem_alloc(struct boot_mem_desc *desc, size_t len, size_t align)
 
 	runtime_assert(!IS_ENABLED(CFG_WITH_PAGER));
 	assert(desc && desc->mem_start && desc->mem_end);
-	assert(IS_POWER_OF_TWO(align) && !(len % align));
+	assert(IS_POWER_OF_TWO(align));
 	va = ROUNDUP2(desc->mem_start, align);
 	if (ADD_OVERFLOW(va, len, &ve))
 		panic();