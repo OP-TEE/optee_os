OP-TEE/optee_os

commit 2b06f9dede33f6b73063938fa8cbc16ee46d3d44

Add basic pointer authentication support for TA's
APIAKey is used for usespace TA's where these keys are generated
for every TA at load time. The TEE core maintains the key value
for each TA is responsible for storing/restorign them during
switch to EL0 and back.

Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/user_ta.c
@@ -442,6 +442,10 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	if (res)
 		goto out;
 
+#ifdef CFG_TA_PAUTH
+	crypto_rng_read(&utc->uctx.keys, sizeof(utc->uctx.keys));
+#endif
+
 	mutex_lock(&tee_ta_mutex);
 	s->ts_sess.ctx = &utc->ta_ctx.ts_ctx;
 	s->ts_sess.handle_svc = s->ts_sess.ctx->ops->handle_svc;

core/arch/arm/arm.mk
@@ -297,12 +297,23 @@ ta_arm64-platform-cxxflags += -fpic
 ta_arm64-platform-cxxflags += $(platform-cflags-optimization)
 ta_arm64-platform-cxxflags += $(platform-cflags-debug-info)
 
-ifeq ($(CFG_TA_BTI),y)
-bti-ta-opt := $(call cc-option,-mbranch-protection=bti)
-ifeq (,$(bti-ta-opt))
-$(error -mbranch-protection=bti not supported)
+ifeq ($(CFG_TA_PAUTH),y)
+bp-ta-opt := $(call cc-option,-mbranch-protection=pac-ret+leaf)
 endif
-ta_arm64-platform-cflags += $(bti-ta-opt)
+
+ifeq ($(CFG_TA_BTI),y)
+bp-ta-opt := $(call cc-option,-mbranch-protection=bti)
+endif
+
+ifeq (y-y,$(CFG_TA_PAUTH)-$(CFG_TA_BTI))
+bp-ta-opt := $(call cc-option,-mbranch-protection=pac-ret+leaf+bti)
+endif
+
+ifeq (y,$(filter $(CFG_TA_BTI) $(CFG_TA_PAUTH),y))
+ifeq (,$(bp-ta-opt))
+$(error -mbranch-protection not supported)
+endif
+ta_arm64-platform-cflags += $(bp-ta-opt)
 endif
 
 ta-mk-file-export-vars-ta_arm64 += CFG_ARM64_ta_arm64

core/arch/arm/include/arm64_macros.S
@@ -132,3 +132,19 @@
 		b	.
 #endif
 	.endm
+
+	.macro read_apiakeylo reg
+	mrs	\reg, S3_0_c2_c1_0
+	.endm
+
+	.macro read_apiakeyhi reg
+	mrs	\reg, S3_0_c2_c1_1
+	.endm
+
+	.macro write_apiakeylo reg
+	msr	S3_0_c2_c1_0, \reg
+	.endm
+
+	.macro write_apiakeyhi reg
+	msr	S3_0_c2_c1_1, \reg
+	.endm

core/arch/arm/include/kernel/thread.h
@@ -73,6 +73,11 @@ struct thread_user_vfp_state {
 	bool saved;
 };
 
+struct thread_pauth_keys {
+	uint64_t hi;
+	uint64_t lo;
+};
+
 #ifdef ARM32
 struct thread_smc_args {
 	uint32_t a0;	/* SMC function ID */
@@ -211,6 +216,10 @@ struct thread_svc_regs {
 	uint64_t x28;
 	uint64_t x29;
 #endif
+#ifdef CFG_TA_PAUTH
+	uint64_t apiakey_hi;
+	uint64_t apiakey_lo;
+#endif
 	uint64_t pad;
 } __aligned(16);
 #endif /*ARM64*/
@@ -247,6 +256,10 @@ struct thread_ctx_regs {
 	uint64_t cpsr;
 	uint64_t x[31];
 	uint64_t tpidr_el0;
+#ifdef CFG_TA_PAUTH
+	uint64_t apiakey_hi;
+	uint64_t apiakey_lo;
+#endif
 };
 #endif /*ARM64*/
 

core/arch/arm/kernel/asm-defines.c
@@ -58,6 +58,10 @@ DEFINES
 	DEFINE(THREAD_SVC_REG_ELR, offsetof(struct thread_svc_regs, elr));
 	DEFINE(THREAD_SVC_REG_SPSR, offsetof(struct thread_svc_regs, spsr));
 	DEFINE(THREAD_SVC_REG_SP_EL0, offsetof(struct thread_svc_regs, sp_el0));
+#ifdef CFG_TA_PAUTH
+	DEFINE(THREAD_SVC_REG_APIAKEY_HI, offsetof(struct thread_svc_regs,
+						   apiakey_hi));
+#endif
 	DEFINE(THREAD_SVC_REG_SIZE, sizeof(struct thread_svc_regs));
 
 	/* struct thread_abort_regs */
@@ -81,6 +85,10 @@ DEFINES
 	DEFINE(THREAD_CTX_REGS_X19, offsetof(struct thread_ctx_regs, x[19]));
 	DEFINE(THREAD_CTX_REGS_TPIDR_EL0, offsetof(struct thread_ctx_regs,
 						   tpidr_el0));
+#ifdef CFG_TA_PAUTH
+	DEFINE(THREAD_CTX_REGS_APIAKEY_HI, offsetof(struct thread_ctx_regs,
+						    apiakey_hi));
+#endif
 
 	/* struct thread_user_mode_rec */
 	DEFINE(THREAD_USER_MODE_REC_CTX_REGS_PTR,

core/arch/arm/kernel/thread.c
@@ -1435,7 +1435,8 @@ static bool get_spsr(bool is_32bit, unsigned long entry_func, uint32_t *spsr)
 static void set_ctx_regs(struct thread_ctx_regs *regs, unsigned long a0,
 			 unsigned long a1, unsigned long a2, unsigned long a3,
 			 unsigned long user_sp, unsigned long entry_func,
-			 uint32_t spsr)
+			 uint32_t spsr,
+			 struct thread_pauth_keys *keys __maybe_unused)
 {
 	/*
 	 * First clear all registers to avoid leaking information from
@@ -1461,8 +1462,26 @@ static void set_ctx_regs(struct thread_ctx_regs *regs, unsigned long a0,
 	regs->cpsr = spsr;
 	regs->x[13] = user_sp;	/* Used when running TA in Aarch32 */
 	regs->sp = user_sp;	/* Used when running TA in Aarch64 */
+#ifdef CFG_TA_PAUTH
+	assert(keys);
+	regs->apiakey_hi = keys->hi;
+	regs->apiakey_lo = keys->lo;
+#endif
 	/* Set frame pointer (user stack can't be unwound past this point) */
 	regs->x[29] = 0;
+#endif
+}
+
+static struct thread_pauth_keys *thread_get_pauth_keys(void)
+{
+#if defined(CFG_TA_PAUTH)
+	struct ts_session *s = ts_get_current_session();
+	/* Only user TA's support the PAUTH keys */
+	struct user_ta_ctx *utc = to_user_ta_ctx(s->ctx);
+
+	return &utc->uctx.keys;
+#else
+	return NULL;
 #endif
 }
 
@@ -1475,8 +1494,11 @@ uint32_t thread_enter_user_mode(unsigned long a0, unsigned long a1,
 	uint32_t exceptions = 0;
 	uint32_t rc = 0;
 	struct thread_ctx_regs *regs = NULL;
+	struct thread_pauth_keys *keys = NULL;
 
 	tee_ta_update_session_utime_resume();
+
+	keys = thread_get_pauth_keys();
 
 	/* Derive SPSR from current CPSR/PSTATE readout. */
 	if (!get_spsr(is_32bit, entry_func, &spsr)) {
@@ -1493,7 +1515,7 @@ uint32_t thread_enter_user_mode(unsigned long a0, unsigned long a1,
 	 * unmasked when user mode has been entered.
 	 */
 	regs = thread_get_ctx_regs();
-	set_ctx_regs(regs, a0, a1, a2, a3, user_sp, entry_func, spsr);
+	set_ctx_regs(regs, a0, a1, a2, a3, user_sp, entry_func, spsr, keys);
 	rc = __thread_enter_user_mode(regs, exit_status0, exit_status1);
 	thread_unmask_exceptions(exceptions);
 	return rc;

core/arch/arm/kernel/thread_a64.S
@@ -36,6 +36,28 @@
 		b.eq	\label
 	.endm
 
+	.macro disable_pauth reg
+#ifdef	CFG_TA_PAUTH
+		mrs	\reg, sctlr_el1
+		bic     \reg, \reg, #SCTLR_ENIA
+#ifdef CFG_TA_BTI
+		orr     \reg, \reg, #(SCTLR_BT0 | SCTLR_BT1)
+#endif
+		msr	sctlr_el1, \reg
+#endif
+	.endm
+
+	.macro enable_pauth reg
+#ifdef	CFG_TA_PAUTH
+		mrs	\reg, sctlr_el1
+		orr     \reg, \reg, #SCTLR_ENIA
+#ifdef CFG_TA_BTI
+		bic     \reg, \reg, #(SCTLR_BT0 | SCTLR_BT1)
+#endif
+		msr	sctlr_el1, \reg
+#endif
+	.endm
+
 /* void thread_resume(struct thread_ctx_regs *regs) */
 FUNC thread_resume , :
 	load_xregs x0, THREAD_CTX_REGS_SP, 1, 3
@@ -52,7 +74,15 @@ FUNC thread_resume , :
 	ldr	x0, [x0, THREAD_CTX_REGS_X0]
 	return_from_exception
 
-1:	load_xregs x0, THREAD_CTX_REGS_X1, 1, 3
+1:
+#ifdef	CFG_TA_PAUTH
+	/* Restore PAC keys before return to el0 */
+	load_xregs x0, THREAD_CTX_REGS_APIAKEY_HI, 1, 2
+	write_apiakeyhi	x1
+	write_apiakeylo	x2
+#endif
+
+	load_xregs x0, THREAD_CTX_REGS_X1, 1, 3
 	ldr	x0, [x0, THREAD_CTX_REGS_X0]
 
 	msr	spsel, #1
@@ -126,6 +156,13 @@ FUNC __thread_enter_user_mode , :
 	msr	sp_el0, x1
 	msr	elr_el1, x2
 	msr	spsr_el1, x3
+
+#ifdef	CFG_TA_PAUTH
+	/* Load APIAKEY */
+	load_xregs x0, THREAD_CTX_REGS_APIAKEY_HI, 1, 2
+	write_apiakeyhi	x1
+	write_apiakeylo	x2
+#endif
 
 	/*
 	 * Save the values for x0 and x1 in struct thread_core_local to be
@@ -284,6 +321,7 @@ el1_serror_sp1:
 	.balign	128, INV_INSN
 el0_sync_a64:
 	restore_mapping
+	/* PAuth will be disabled later else check_vector_size will fail */
 
 	mrs	x2, esr_el1
 	mrs	x3, sp_el0
@@ -296,6 +334,7 @@ el0_sync_a64:
 	.balign	128, INV_INSN
 el0_irq_a64:
 	restore_mapping
+	disable_pauth x1
 
 	b	elx_irq
 	check_vector_size el0_irq_a64
@@ -303,6 +342,7 @@ el0_irq_a64:
 	.balign	128, INV_INSN
 el0_fiq_a64:
 	restore_mapping
+	disable_pauth x1
 
 	b	elx_fiq
 	check_vector_size el0_fiq_a64
@@ -466,6 +506,7 @@ workaround_el0_serror_a32:
  * that it's always available.
  */
 eret_to_el0:
+	enable_pauth x1
 
 #ifdef CFG_CORE_UNMAP_CORE_AT_EL0
 	/* Point to the vector into the reduced mapping */
@@ -614,6 +655,7 @@ thread_excp_vect_end:
 END_FUNC thread_excp_vect
 
 LOCAL_FUNC el0_svc , :
+	disable_pauth x1
 	/* get pointer to current thread context in x0 */
 	get_thread_ctx sp, 0, 1, 2
 	mrs	x1, tpidr_el0
@@ -640,6 +682,14 @@ LOCAL_FUNC el0_svc , :
 	mrs	x0, elr_el1
 	mrs	x1, spsr_el1
 	store_xregs sp, THREAD_SVC_REG_ELR, 0, 1
+
+#ifdef CFG_TA_PAUTH
+	/* Save APIAKEY */
+	read_apiakeyhi	x0
+	read_apiakeylo	x1
+	store_xregs sp, THREAD_SVC_REG_APIAKEY_HI, 0, 1
+#endif
+
 	mov	x0, sp
 
 	/*
@@ -687,7 +737,15 @@ LOCAL_FUNC el0_svc , :
 
 	return_from_exception
 
-1:	ldp	x0, x1, [x30, THREAD_SVC_REG_X0]
+1:
+#ifdef	CFG_TA_PAUTH
+	/* Restore APIAKEY */
+	load_xregs x30, THREAD_SVC_REG_APIAKEY_HI, 0, 1
+	write_apiakeyhi	x0
+	write_apiakeylo	x1
+#endif
+
+	ldp	x0, x1, [x30, THREAD_SVC_REG_X0]
 	ldr	x30, [x30, #THREAD_SVC_REG_X30]
 
 	msr	spsel, #1
@@ -779,6 +837,7 @@ END_FUNC el1_sync_abort
 
 	/* sp_el0 in x3 */
 LOCAL_FUNC el0_sync_abort , :
+	disable_pauth x1
 	/*
 	 * Update core local flags
 	 */

core/include/kernel/user_mode_ctx_struct.h
@@ -16,6 +16,7 @@
  * @vm_info:		Virtual memory map of this context
  * @regions:		Memory regions registered by pager
  * @vfp:		State of VFP registers
+ * @keys:		Pointer authentication keys
  * @ts_ctx:		Generic TS context
  * @entry_func:		Entry address in TS
  * @dump_entry_func:	Entry address in TS for dumping address mappings
@@ -33,6 +34,9 @@ struct user_mode_ctx {
 	struct vm_paged_region_head *regions;
 #if defined(CFG_WITH_VFP)
 	struct thread_user_vfp_state vfp;
+#endif
+#if defined(CFG_TA_PAUTH)
+	struct thread_pauth_keys keys;
 #endif
 	struct ts_ctx *ts_ctx;
 	uaddr_t entry_func;

commit 077e727a12030eb6991807de33673ac03c591a3b

core: user_ta: PAUTH key initialization may fail
Test crypto_rng_read() return value when initializing user TA pointer
authentication. For sake of simplicity get random bytes before user TA
context starts to be initialized.

Fixes: 2b06f9dede33 ("Add basic pointer authentication support for TA's")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/user_ta.c
@@ -468,6 +468,14 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	if (!utc)
 		return TEE_ERROR_OUT_OF_MEMORY;
 
+#ifdef CFG_TA_PAUTH
+	res = crypto_rng_read(&utc->uctx.keys, sizeof(utc->uctx.keys));
+	if (res) {
+		free(utc);
+		return res;
+	}
+#endif
+
 	TAILQ_INIT(&utc->open_sessions);
 	TAILQ_INIT(&utc->cryp_states);
 	TAILQ_INIT(&utc->objects);
@@ -490,10 +498,6 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	}
 
 	utc->ta_ctx.is_initializing = true;
-
-#ifdef CFG_TA_PAUTH
-	crypto_rng_read(&utc->uctx.keys, sizeof(utc->uctx.keys));
-#endif
 
 	assert(!mutex_trylock(&tee_ta_mutex));
 