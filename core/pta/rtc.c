OP-TEE/optee_os

commit cea1eb0bc90e1a8eb683767fba0e77bba4afe3e1

pta: add PTA for RTC
On some systems, when the RTC is secured, there is no way for the
normal world to access it. This PTA uses the RTC API to allow a
Linux OP-TEE based RTC driver to communicate with the RTC that is
secured.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Clément Léger <clement.leger@bootlin.com>

core/pta/rtc.c
@@ -1,0 +1,195 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2018, Linaro Limited
+ * Copyright (c) 2021, EPAM Systems. All rights reserved.
+ * Copyright (c) 2022, Microchip
+ *
+ */
+
+#include <drivers/rtc.h>
+#include <kernel/pseudo_ta.h>
+#include <pta_rtc.h>
+#include <string.h>
+#include <tee_api_defines.h>
+#include <tee_api_defines_extensions.h>
+
+#define PTA_NAME "rtc.pta"
+
+static void rtc_pta_copy_time_from_optee(struct pta_rtc_time *pta_time,
+					 struct optee_rtc_time *optee_time)
+{
+	pta_time->tm_sec = optee_time->tm_sec;
+	pta_time->tm_min = optee_time->tm_min;
+	pta_time->tm_hour = optee_time->tm_hour;
+	pta_time->tm_mday = optee_time->tm_mday;
+	pta_time->tm_mon = optee_time->tm_mon;
+	pta_time->tm_year = optee_time->tm_year;
+	pta_time->tm_wday = optee_time->tm_wday;
+}
+
+static TEE_Result rtc_pta_get_time(uint32_t types,
+				   TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct optee_rtc_time time = { };
+	struct pta_rtc_time *pta_time = NULL;
+
+	if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_OUTPUT,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	pta_time = params[0].memref.buffer;
+	if (!pta_time || params[0].memref.size != sizeof(*pta_time))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = rtc_get_time(&time);
+	if (res)
+		return res;
+
+	rtc_pta_copy_time_from_optee(pta_time, &time);
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result rtc_pta_set_time(uint32_t types,
+				   TEE_Param params[TEE_NUM_PARAMS])
+{
+	struct optee_rtc_time time = { };
+	struct pta_rtc_time *pta_time = NULL;
+
+	if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	pta_time = params[0].memref.buffer;
+	if (!pta_time || params[0].memref.size != sizeof(*pta_time))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	time.tm_sec = pta_time->tm_sec;
+	time.tm_min = pta_time->tm_min;
+	time.tm_hour = pta_time->tm_hour;
+	time.tm_mday = pta_time->tm_mday;
+	time.tm_mon = pta_time->tm_mon;
+	time.tm_year = pta_time->tm_year;
+	time.tm_wday = pta_time->tm_wday;
+
+	return rtc_set_time(&time);
+}
+
+static TEE_Result rtc_pta_set_offset(uint32_t types,
+				     TEE_Param params[TEE_NUM_PARAMS])
+{
+	if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	return rtc_set_offset((int32_t)params[0].value.a);
+}
+
+static TEE_Result rtc_pta_get_offset(uint32_t types,
+				     TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	long offset = 0;
+
+	if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_OUTPUT,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = rtc_get_offset(&offset);
+	if (res)
+		return res;
+
+	if (offset > INT32_MAX || offset < INT32_MIN)
+		return TEE_ERROR_OVERFLOW;
+
+	params[0].value.a = (uint32_t)offset;
+
+	return res;
+}
+
+static TEE_Result rtc_pta_get_info(uint32_t types,
+				   TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct pta_rtc_info *info = NULL;
+	struct optee_rtc_time range_min = { };
+	struct optee_rtc_time range_max = { };
+	uint64_t features = 0;
+
+	if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_OUTPUT,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE,
+				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	info = params[0].memref.buffer;
+	if (!info || params[0].memref.size != sizeof(*info))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	memset(info, 0, sizeof(*info));
+
+	res = rtc_get_info(&features, &range_min, &range_max);
+	if (res)
+		return res;
+
+	info->version = PTA_RTC_INFO_VERSION;
+
+	if (features & RTC_CORRECTION_FEATURE)
+		info->features |= PTA_RTC_FEATURE_CORRECTION;
+
+	rtc_pta_copy_time_from_optee(&info->range_min, &range_min);
+	rtc_pta_copy_time_from_optee(&info->range_max, &range_max);
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result open_session(uint32_t ptypes __unused,
+			       TEE_Param par[TEE_NUM_PARAMS] __unused,
+			       void **session __unused)
+{
+	struct ts_session *ts = ts_get_current_session();
+	struct tee_ta_session *ta_session = to_ta_session(ts);
+
+	/* Only REE kernel is allowed to access RTC */
+	if (ta_session->clnt_id.login != TEE_LOGIN_REE_KERNEL)
+		return TEE_ERROR_ACCESS_DENIED;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result invoke_command(void *session __unused,
+				 uint32_t cmd, uint32_t ptypes,
+				 TEE_Param params[TEE_NUM_PARAMS])
+{
+	switch (cmd) {
+	case PTA_CMD_RTC_GET_INFO:
+		return rtc_pta_get_info(ptypes, params);
+	case PTA_CMD_RTC_GET_TIME:
+		return rtc_pta_get_time(ptypes, params);
+	case PTA_CMD_RTC_SET_TIME:
+		return rtc_pta_set_time(ptypes, params);
+	case PTA_CMD_RTC_GET_OFFSET:
+		return rtc_pta_get_offset(ptypes, params);
+	case PTA_CMD_RTC_SET_OFFSET:
+		return rtc_pta_set_offset(ptypes, params);
+	default:
+		break;
+	}
+
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+
+pseudo_ta_register(.uuid = PTA_RTC_UUID, .name = PTA_NAME,
+		   .flags = PTA_DEFAULT_FLAGS | TA_FLAG_CONCURRENT |
+			    TA_FLAG_DEVICE_ENUM,
+		   .open_session_entry_point = open_session,
+		   .invoke_command_entry_point = invoke_command);

core/pta/sub.mk
@@ -12,5 +12,6 @@ srcs-$(CFG_SCP03_PTA) += scp03.c
 srcs-$(CFG_APDU_PTA) += apdu.c
 srcs-$(CFG_SCMI_PTA) += scmi.c
 srcs-$(CFG_HWRNG_PTA) += hwrng.c
+srcs-$(CFG_RTC_PTA) += rtc.c
 
 subdirs-y += bcm

lib/libutee/include/pta_rtc.h
@@ -1,0 +1,103 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2022, Microchip
+ */
+#ifndef __PTA_RTC_H
+#define __PTA_RTC_H
+
+#include <tee_api_types.h>
+
+#define PTA_RTC_UUID { 0xf389f8c8, 0x845f, 0x496c, \
+		{ 0x8b, 0xbe, 0xd6, 0x4b, 0xd2, 0x4c, 0x92, 0xfd } }
+
+#define PTA_RTC_INFO_VERSION		0x1
+
+/*
+ * RTC provides set/get offset and thus command PTA_CMD_RTC_GET_OFFSET and
+ * PTA_CMD_RTC_SET_OFFSET might be called
+ */
+#define PTA_RTC_FEATURE_CORRECTION	BIT(0)
+
+struct pta_rtc_time {
+	uint32_t tm_sec;
+	uint32_t tm_min;
+	uint32_t tm_hour;
+	uint32_t tm_mday;
+	uint32_t tm_mon;
+	uint32_t tm_year;
+	uint32_t tm_wday;
+};
+
+/*
+ * struct pta_rtc_info - RTC service information
+ * @version - 1st 64bit cell, version of the structure: PTA_RTC_INFO_VERSION
+ * @features - 64bit flag mask related to PTA_RTC_FEATURE_*
+ * @range_min - Minima time reference the RTC can be programmed to
+ * @range_max - Maxima time reference the RTC can reach
+ */
+struct pta_rtc_info {
+	uint64_t version;
+	uint64_t features;
+	struct pta_rtc_time range_min;
+	struct pta_rtc_time range_max;
+};
+
+/*
+ * PTA_CMD_RTC_GET_INFO - Get RTC information
+ *
+ * [out]        memref[0]  RTC buffer memory reference containing a struct
+ *			   pta_rtc_info
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_CMD_RTC_GET_INFO	0x0
+
+/*
+ * PTA_CMD_RTC_GET_TIME - Get time from RTC
+ *
+ * [out]    memref[0]  RTC buffer memory reference containing a struct
+ *		       pta_rtc_time
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_CMD_RTC_GET_TIME		0x1
+
+/*
+ * PTA_CMD_RTC_SET_TIME - Set time from RTC
+ *
+ * [in]     memref[0]  RTC buffer memory reference containing a struct
+ *                     pta_rtc_time to be used as RTC time
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_CMD_RTC_SET_TIME		0x2
+
+/*
+ * PTA_CMD_RTC_GET_OFFSET - Get RTC offset
+ *
+ * [out]    value[0].a  RTC offset (signed 32bit value)
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_CMD_RTC_GET_OFFSET		0x3
+
+/*
+ * PTA_CMD_RTC_SET_OFFSET - Set RTC offset
+ *
+ * [in]     value[0].a  RTC offset to be set (signed 32bit value)
+ *
+ * Return codes:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_CMD_RTC_SET_OFFSET		0x4
+
+#endif /* __PTA_RTC_H */

mk/config.mk
@@ -822,3 +822,6 @@ CFG_CORE_HAS_GENERIC_TIMER ?= y
 
 # Enable RTC API
 CFG_DRIVERS_RTC ?= n
+
+# Enable PTA for RTC access from non-secure world
+CFG_RTC_PTA ?= n

commit aad0f76d3dc9d9fce316ae68ff45b3179978e5ed

core: pta: check struct alignment in RTC PTA API
Check buffer alignment against its pretended type before assignation.

Fixes: cea1eb0bc90e ("pta: add PTA for RTC")
Signed-off-by: Clément Le Goffic <clement.legoffic@foss.st.com>

core/pta/rtc.c
@@ -40,6 +40,9 @@ static TEE_Result rtc_pta_get_time(uint32_t types,
 				     TEE_PARAM_TYPE_NONE))
 		return TEE_ERROR_BAD_PARAMETERS;
 
+	if (!IS_ALIGNED_WITH_TYPE(params[0].memref.buffer, typeof(*pta_time)))
+		return TEE_ERROR_BAD_PARAMETERS;
+
 	pta_time = params[0].memref.buffer;
 	if (!pta_time || params[0].memref.size != sizeof(*pta_time))
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -63,6 +66,9 @@ static TEE_Result rtc_pta_set_time(uint32_t types,
 				     TEE_PARAM_TYPE_NONE,
 				     TEE_PARAM_TYPE_NONE,
 				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (!IS_ALIGNED_WITH_TYPE(params[0].memref.buffer, typeof(*pta_time)))
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	pta_time = params[0].memref.buffer;
@@ -129,6 +135,9 @@ static TEE_Result rtc_pta_get_info(uint32_t types,
 				     TEE_PARAM_TYPE_NONE,
 				     TEE_PARAM_TYPE_NONE,
 				     TEE_PARAM_TYPE_NONE))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (!IS_ALIGNED_WITH_TYPE(params[0].memref.buffer, typeof(*info)))
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	info = params[0].memref.buffer;