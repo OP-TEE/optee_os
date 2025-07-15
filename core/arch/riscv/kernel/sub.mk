00eea9243ce73ce7a191f087b0bab460b70be0d3
Marouene Boubakri <marouene.boubakri@nxp.com>
2025-07-15 03:34:27 +0200

    core: riscv: introduce CFG_RISCV_SBI_MPXY flag for MPXY SBI support
    Add a new build-time configuration flag CFG_RISCV_SBI_MPXY to enable
    compilation of MPXY SBI extension support in OP-TEE. When enabled, this
    automatically forces CFG_RISCV_SBI to ensure the SBI infrastructure is
    included.
    
    Also update the build system to conditionally compile sbi_mpxy.c based
    on this flag.
    
    Enable CFG_RISCV_SBI_MPXY by default for the virt platform.
    
    Signed-off-by: Marouene Boubakri <marouene.boubakri@nxp.com>
    Reviewed-by: Yu-Chien Peter Lin <peter.lin@sifive.com>
    Reviewed-by: Alvin Chang <alvinga@andestech.com>

--- core/arch/riscv/kernel/sub.mk
+++ core/arch/riscv/kernel/sub.mk
@@ -5,6 +5,7 @@ srcs-y += idle.c
 srcs-$(CFG_RISCV_TIME_SOURCE_RDTIME) += tee_time_rdtime.c
 srcs-$(CFG_RISCV_SBI) += sbi.c
 srcs-$(CFG_RISCV_SBI_CONSOLE) += sbi_console.c
+srcs-$(CFG_RISCV_SBI_MPXY) += sbi_mpxy.c
 srcs-y += boot.c
 srcs-y += entry.S
 srcs-y += abort.c
