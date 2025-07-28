OP-TEE/optee_os

commit 941a58d78c99c4754fbd4ec3079ec9e1d596af8f

Add optee.ta.instanceKeepCrashed property
Add the optee.ta.instanceKeepCrashed property to prevent a TA with
gpd.ta.instanceKeepAlive=true to be restarted. This prevents unexpected
resetting of the state of the TA.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Alex Lewontin <alex.lewontin@canonical.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/kernel/tee_ta_manager.c
@@ -455,6 +455,7 @@ TEE_Result tee_ta_close_session(struct tee_ta_session *csess,
 	struct tee_ta_session *sess = NULL;
 	struct tee_ta_ctx *ctx = NULL;
 	struct ts_ctx *ts_ctx = NULL;
+	bool keep_crashed = false;
 	bool keep_alive = false;
 
 	DMSG("csess 0x%" PRIxVA " id %u",
@@ -501,9 +502,12 @@ TEE_Result tee_ta_close_session(struct tee_ta_session *csess,
 		panic();
 
 	ctx->ref_count--;
-	keep_alive = (ctx->flags & TA_FLAG_INSTANCE_KEEP_ALIVE) &&
-			(ctx->flags & TA_FLAG_SINGLE_INSTANCE);
-	if (!ctx->ref_count && (ctx->panicked || !keep_alive)) {
+	if (ctx->flags & TA_FLAG_SINGLE_INSTANCE)
+		keep_alive = ctx->flags & TA_FLAG_INSTANCE_KEEP_ALIVE;
+	if (keep_alive)
+		keep_crashed = ctx->flags & TA_FLAG_INSTANCE_KEEP_CRASHED;
+	if (!ctx->ref_count &&
+	    ((ctx->panicked && !keep_crashed) || !keep_alive)) {
 		if (!ctx->is_releasing) {
 			TAILQ_REMOVE(&tee_ctxes, ctx, link);
 			ctx->is_releasing = true;

lib/libutee/include/user_ta_header.h
@@ -52,8 +52,13 @@
 					BIT32(11)
 #define TA_FLAG_DEVICE_ENUM_TEE_STORAGE_PRIVATE	\
 					BIT32(12) /* with TEE_STORAGE_PRIVATE */
+/*
+ * Don't restart a TA with TA_FLAG_INSTANCE_KEEP_ALIVE set if it has
+ * crashed.
+ */
+#define TA_FLAG_INSTANCE_KEEP_CRASHED	BIT32(13)
 
-#define TA_FLAGS_MASK			GENMASK_32(12, 0)
+#define TA_FLAGS_MASK			GENMASK_32(13, 0)
 
 struct ta_head {
 	TEE_UUID uuid;
@@ -133,6 +138,7 @@ extern struct __elf_phdr_info __elf_phdr_info;
 #define TA_PROP_STR_SINGLE_INSTANCE	"gpd.ta.singleInstance"
 #define TA_PROP_STR_MULTI_SESSION	"gpd.ta.multiSession"
 #define TA_PROP_STR_KEEP_ALIVE		"gpd.ta.instanceKeepAlive"
+#define TA_PROP_STR_KEEP_CRASHED	"optee.ta.instanceKeepCrashed"
 #define TA_PROP_STR_DATA_SIZE		"gpd.ta.dataSize"
 #define TA_PROP_STR_STACK_SIZE		"gpd.ta.stackSize"
 #define TA_PROP_STR_VERSION		"gpd.ta.version"

ta/user_ta_header.c
@@ -142,6 +142,9 @@ const struct user_ta_property ta_props[] = {
 	{TA_PROP_STR_KEEP_ALIVE, USER_TA_PROP_TYPE_BOOL,
 	 &(const bool){(TA_FLAGS & TA_FLAG_INSTANCE_KEEP_ALIVE) != 0}},
 
+	{TA_PROP_STR_KEEP_CRASHED, USER_TA_PROP_TYPE_BOOL,
+	 &(const bool){(TA_FLAGS & TA_FLAG_INSTANCE_KEEP_CRASHED) != 0}},
+
 	{TA_PROP_STR_DATA_SIZE, USER_TA_PROP_TYPE_U32,
 	 &(const uint32_t){TA_DATA_SIZE}},
 

commit 197ae8979354f4051369719815c3b3d2d8d90bbb

core: maintain the panicked TAs with instanceKeepCrashed property
TA context lists should retain panicked TAs (those with the
TA_FLAG_SINGLE_INSTANCE, TA_FLAG_INSTANCE_KEEP_ALIVE, and
TA_FLAG_INSTANCE_KEEP_CRASHED flags) to maintain their panicked state and
prevent respawning.

Fixes: 941a58d78c99 ("Add optee.ta.instanceKeepCrashed property")
Signed-off-by: Joseph Lo <josephl@nvidia.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/kernel/tee_ta_manager.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
  * Copyright (c) 2020, Arm Limited
+ * Copyright (c) 2025, NVIDIA Corporation & AFFILIATES.
  */
 
 #include <assert.h>
@@ -663,21 +664,35 @@ err_mutex_unlock:
 	return res;
 }
 
-static void release_ta_ctx(struct tee_ta_ctx *ctx)
+static void maybe_release_ta_ctx(struct tee_ta_ctx *ctx)
 {
 	bool was_releasing = false;
+	bool keep_crashed = false;
+	bool keep_alive = false;
 
-	mutex_lock(&tee_ta_mutex);
-	was_releasing = ctx->is_releasing;
-	ctx->is_releasing = true;
-	if (!was_releasing) {
-		DMSG("Releasing panicked TA ctx");
-		TAILQ_REMOVE(&tee_ctxes, ctx, link);
+	if (ctx->flags & TA_FLAG_SINGLE_INSTANCE)
+		keep_alive = ctx->flags & TA_FLAG_INSTANCE_KEEP_ALIVE;
+	if (keep_alive)
+		keep_crashed = ctx->flags & TA_FLAG_INSTANCE_KEEP_CRASHED;
+
+	/*
+	 * Keep panicked TAs with SINGLE_INSTANCE, KEEP_ALIVE, and KEEP_CRASHED
+	 * flags in the context list to maintain their panicked status and
+	 * prevent respawning.
+	 */
+	if (!keep_crashed) {
+		mutex_lock(&tee_ta_mutex);
+		was_releasing = ctx->is_releasing;
+		ctx->is_releasing = true;
+		if (!was_releasing) {
+			DMSG("Releasing panicked TA ctx");
+			TAILQ_REMOVE(&tee_ctxes, ctx, link);
+		}
+		mutex_unlock(&tee_ta_mutex);
+
+		if (!was_releasing)
+			ctx->ts_ctx.ops->release_state(&ctx->ts_ctx);
 	}
-	mutex_unlock(&tee_ta_mutex);
-
-	if (!was_releasing)
-		ctx->ts_ctx.ops->release_state(&ctx->ts_ctx);
 }
 
 TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
@@ -719,7 +734,7 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 
 		panicked = ctx->panicked;
 		if (panicked) {
-			release_ta_ctx(ctx);
+			maybe_release_ta_ctx(ctx);
 			res = TEE_ERROR_TARGET_DEAD;
 		}
 
@@ -782,7 +797,7 @@ TEE_Result tee_ta_invoke_command(TEE_ErrorOrigin *err,
 
 	panicked = ta_ctx->panicked;
 	if (panicked) {
-		release_ta_ctx(ta_ctx);
+		maybe_release_ta_ctx(ta_ctx);
 		res = TEE_ERROR_TARGET_DEAD;
 	}
 
@@ -832,7 +847,7 @@ static TEE_Result dump_ta_memstats(struct tee_ta_session *s,
 
 		panicked = ctx->panicked;
 		if (panicked) {
-			release_ta_ctx(ctx);
+			maybe_release_ta_ctx(ctx);
 			res = TEE_ERROR_TARGET_DEAD;
 		}
 