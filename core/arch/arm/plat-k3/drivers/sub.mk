3ff25b4f45537538a86610e0bf5611688bf72056
Aniket Sarkar <a-sarkar1@ti.com>
2025-07-28 16:38:23 +0530

    plat-k3: drivers: Add support for TI mailbox driver
    New devices like the AM62L use a mailbox to communicate with the
    security firmware. Add mailbox driver here to support the mailbox
    hardware.
    
    Signed-off-by: Aniket Sarkar <a-sarkar1@ti.com>
    Signed-off-by: Suhaas Joshi <s-joshi@ti.com>
    Reviewed-by: Andrew Davis <afd@ti.com>

--- core/arch/arm/plat-k3/drivers/sub.mk
+++ core/arch/arm/plat-k3/drivers/sub.mk
@@ -1,5 +1,11 @@
-srcs-y += sec_proxy.c
+
 srcs-y += ti_sci.c
 srcs-$(CFG_EIP76D_TRNG) += eip76d_trng.c
+ifeq ($(PLATFORM_FLAVOR),am62lx)
+srcs-y += mailbox.c
+else
+srcs-y += sec_proxy.c
+endif
+
 srcs-$(CFG_SA2UL) += sa2ul.c
 srcs-$(CFG_DTHEV2) += dthev2.c
