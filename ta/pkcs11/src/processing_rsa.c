OP-TEE/optee_os

commit 3dc4089afde26a069a24e4649f8d6e6612df1239

ta: pkcs11: correct RSA keys extended attributes sanitation
Fix RSA key attributes function load_tee_rsa_key_attrs() that badly
checks that the 5 extended RSA attributes are found in the key object.

Link: https://github.com/OP-TEE/optee_test/issues/721#issuecomment-2068055537
Link: https://github.com/OP-TEE/optee_test/issues/721#issuecomment-2072064963
Fixes: 0442c956edfb ("ta: pkcs11: Add support for RSA signing & verification")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

ta/pkcs11/src/processing_rsa.c
@@ -569,6 +569,8 @@ enum pkcs11_rc load_tee_rsa_key_attrs(TEE_Attribute **tee_attrs,
 			break;
 		}
 
+		rc = PKCS11_CKR_GENERAL_ERROR;
+
 		if (pkcs2tee_load_attr(&attrs[count], TEE_ATTR_RSA_PRIME1, obj,
 				       PKCS11_CKA_PRIME_1))
 			count++;

commit e2d12c39f36227ce2c43da1fbc8e92c5c3ac87dc

ta: pkcs11: be flexible on RSA private key optional attributes
Allow RSA private key to partially store the RSA private key optional
attributes without facing complaints from the GP TEE API.

On one hand, in the PKCS#11 specification, RSA private key attributes
CKA_PRIME_1, CKA_PRIME_2, CKA_EXPONENT_1, CKA_EXPONENT_2 and
CKA_COEFFICIENT are optional and the spec does not add much constraints
on their presence.

On the other hand, the GP TEE Internal Core API requests that these
5 optional attributes are all present or none is present at all.

As a trade-off, allow PKCS#11 client to partially provide them but do not
load them into the TEE object unless they are all present.

Fixes: 3dc4089afde2 ("ta: pkcs11: correct RSA keys extended attributes sanitation")
Closes: https://github.com/OP-TEE/optee_os/issues/5418
Closes: https://github.com/OP-TEE/optee_os/issues/7520
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

ta/pkcs11/src/processing_rsa.c
@@ -511,7 +511,6 @@ enum pkcs11_rc load_tee_rsa_key_attrs(TEE_Attribute **tee_attrs,
 	TEE_Attribute *attrs = NULL;
 	size_t count = 0;
 	enum pkcs11_rc rc = PKCS11_CKR_GENERAL_ERROR;
-	void *a_ptr = NULL;
 
 	assert(get_key_type(obj->attributes) == PKCS11_CKK_RSA);
 
@@ -559,18 +558,13 @@ enum pkcs11_rc load_tee_rsa_key_attrs(TEE_Attribute **tee_attrs,
 		if (count != 3)
 			break;
 
-		/* If pre-computed values are present load those */
-		rc = get_attribute_ptr(obj->attributes, PKCS11_CKA_PRIME_1,
-				       &a_ptr, NULL);
-		if (rc != PKCS11_CKR_OK && rc != PKCS11_RV_NOT_FOUND)
-			break;
-		if (rc == PKCS11_RV_NOT_FOUND || !a_ptr) {
-			rc = PKCS11_CKR_OK;
-			break;
-		}
 
-		rc = PKCS11_CKR_GENERAL_ERROR;
-
+		/*
+		 * If pre-computed values are present load those
+		 * but only if they are all present since the GP TEE
+		 * specification expects either the 5 are present
+		 * or none is present.
+		 */
 		if (pkcs2tee_load_attr(&attrs[count], TEE_ATTR_RSA_PRIME1, obj,
 				       PKCS11_CKA_PRIME_1))
 			count++;
@@ -591,9 +585,15 @@ enum pkcs11_rc load_tee_rsa_key_attrs(TEE_Attribute **tee_attrs,
 				       obj, PKCS11_CKA_COEFFICIENT))
 			count++;
 
-		if (count == 8)
-			rc = PKCS11_CKR_OK;
+		/*
+		 * As mentioned above, if at least 1of these optional
+		 * attribute is missing, keep only the 3 first already
+		 * loaded above.
+		 */
+		if (count != 8)
+			count = 3;
 
+		rc = PKCS11_CKR_OK;
 		break;
 
 	default: