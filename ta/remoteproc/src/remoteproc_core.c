OP-TEE/optee_os

commit 07917406b1062f56503c3f0cdc0bea63df438273

remoteproc: fix potential overflows in TLV parsing
This commit is fixing two issues:
- Verify that the end of the buffer is not reached before reading the
tag and value.
- Verify that the entire TLV fits into the TLV chunk

Signed-off-by: Alexandre Gonzalo <alexandre.gonzalo@arm.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

ta/remoteproc/src/remoteproc_core.c
@@ -232,28 +232,32 @@ static TEE_Result remoteproc_get_tlv(void *tlv_chunk, size_t tlv_size,
 	uint32_t tlv_type = 0;
 	uint32_t tlv_length = 0;
 	uint32_t tlv_v = 0;
+	uintptr_t tmp_p_tlv = 0;
 
 	*value = NULL;
 	*length = 0;
 
 	/* Parse the TLV area */
-	while (p_tlv < p_end_tlv) {
+	while (p_tlv + RPROC_TLV_VALUE_OF < p_end_tlv) {
 		memcpy(&tlv_v, p_tlv, sizeof(tlv_v));
 		tlv_type = TEE_U32_FROM_LITTLE_ENDIAN(tlv_v);
 		memcpy(&tlv_v, p_tlv + RPROC_TLV_LENGTH_OF, sizeof(tlv_v));
+		p_tlv += RPROC_TLV_VALUE_OF;
 		tlv_length = TEE_U32_FROM_LITTLE_ENDIAN(tlv_v);
-		if (tlv_type == type) {
+		if (ADD_OVERFLOW((uintptr_t)p_tlv, tlv_length, &tmp_p_tlv))
+			break;
+		if (tmp_p_tlv <= (uintptr_t)p_end_tlv && tlv_type == type) {
 			/* The specified TLV has been found */
 			DMSG("TLV type %#"PRIx32" found, size %#"PRIx32,
 			     type, tlv_length);
-			*value = &p_tlv[RPROC_TLV_VALUE_OF];
+			*value = p_tlv;
 			*length = tlv_length;
 			if (tlv_length)
 				return TEE_SUCCESS;
 			else
 				return TEE_ERROR_NO_DATA;
 		}
-		p_tlv += ROUNDUP_64(sizeof(struct remoteproc_tlv) + tlv_length);
+		p_tlv += ROUNDUP_64(tlv_length);
 	}
 
 	return TEE_ERROR_NO_DATA;

commit e2334dd9bdfab91114c30cd6c707517cb38ddbf1

ta: remoteproc: Fix case empty key info is last TLV cell
Fix case when remote processor firmware key info TLV RPROC_TLV_PKEYINFO
is present but empty (size = 0) and is placed last in the TLVs memory
area hence its value cell start address matches the TLV area end address.

Fixes: 07917406b106 ("remoteproc: fix potential overflows in TLV parsing")
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

ta/remoteproc/src/remoteproc_core.c
@@ -238,7 +238,7 @@ static TEE_Result remoteproc_get_tlv(void *tlv_chunk, size_t tlv_size,
 	*length = 0;
 
 	/* Parse the TLV area */
-	while (p_tlv + RPROC_TLV_VALUE_OF < p_end_tlv) {
+	while (p_tlv + RPROC_TLV_VALUE_OF <= p_end_tlv) {
 		memcpy(&tlv_v, p_tlv, sizeof(tlv_v));
 		tlv_type = TEE_U32_FROM_LITTLE_ENDIAN(tlv_v);
 		memcpy(&tlv_v, p_tlv + RPROC_TLV_LENGTH_OF, sizeof(tlv_v));