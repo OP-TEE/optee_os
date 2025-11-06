OP-TEE/optee_os

commit 5a9d570a8d7ff1cf7adfcf332c4a1ea0e539481b

core: fs_htree: re-init hash tree when both tag and counter are zero
Creating and update a hash tree involves several RPC commands.
If a power loss occurs during the creation flow, it may result
in a hash tree with an incomplete header and a counter value of 0.
If attempting to read this file subsequently leads to a
TEE_ERROR_CORRUPT_OBJECT error.

Instead of returning TEE_ERROR_CORRUPT_OBJECT, continue the
initialization flow to support subsequent functionality.

Link: https://github.com/OP-TEE/optee_os/issues/7512
Fixes: 50a814981d8b ("core: provide a hash tree for secure storage")

Signed-off-by: Ox Yeh <ox.yeh@mediatek.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/tee/fs_htree.c
@@ -630,6 +630,47 @@ static TEE_Result init_root_node(struct tee_fs_htree *ht)
 	return res;
 }
 
+static TEE_Result create_and_sync(struct tee_fs_htree *ht,
+				  uint8_t *hash,
+				  uint32_t min_counter)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	const struct tee_fs_htree_image dummy_head = {
+		.counter = min_counter,
+	};
+
+	if (!ht)
+		return TEE_ERROR_GENERIC;
+
+	res = crypto_rng_read(ht->fek, sizeof(ht->fek));
+	if (res != TEE_SUCCESS)
+		return res;
+
+	res = tee_fs_fek_crypt(ht->uuid, TEE_MODE_ENCRYPT, ht->fek,
+			       sizeof(ht->fek), ht->head.enc_fek);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	res = init_root_node(ht);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	ht->dirty = true;
+	res = tee_fs_htree_sync_to_storage(&ht, hash, NULL);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	return rpc_write_head(ht, 0, &dummy_head);
+}
+
+static bool ht_head_is_partially_done(const struct tee_fs_htree_image *head)
+{
+	uint8_t zero_tag[TEE_FS_HTREE_TAG_SIZE] = {0};
+
+	return head->counter == 0 &&
+	       !memcmp(head->tag, zero_tag, sizeof(head->tag));
+}
+
 TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 			     const TEE_UUID *uuid,
 			     const struct tee_fs_htree_storage *stor,
@@ -646,32 +687,22 @@ TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 	ht->stor_aux = stor_aux;
 
 	if (create) {
-		const struct tee_fs_htree_image dummy_head = {
-			.counter = min_counter,
-		};
-
-		res = crypto_rng_read(ht->fek, sizeof(ht->fek));
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		res = tee_fs_fek_crypt(ht->uuid, TEE_MODE_ENCRYPT, ht->fek,
-				       sizeof(ht->fek), ht->head.enc_fek);
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		res = init_root_node(ht);
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		ht->dirty = true;
-		res = tee_fs_htree_sync_to_storage(&ht, hash, NULL);
-		if (res != TEE_SUCCESS)
-			goto out;
-		res = rpc_write_head(ht, 0, &dummy_head);
+		res = create_and_sync(ht, hash, min_counter);
 	} else {
 		res = init_head_from_data(ht, hash, min_counter);
 		if (res != TEE_SUCCESS)
 			goto out;
+
+		/*
+		 * If a power loss occurred during hash tree creation, the
+		 * head may not have been written and counter is still 0.
+		 * Re-initialze the hash tree.
+		 */
+		if (ht_head_is_partially_done(&ht->head)) {
+			res = create_and_sync(ht, hash, min_counter);
+			if (res != TEE_SUCCESS)
+				goto out;
+		}
 
 		res = verify_root(ht);
 		if (res != TEE_SUCCESS)

commit 9c9f39063c0a7d9ebd9926cb365c43eb3fabc0db

core: fs_htree: fix incorrect data type in create_and_sync
Fixes an incorrect data type used in the create_and_sync function.
When this function calls tee_fs_htree_sync_to_storage, it may invoke
tee_fs_htree_close to free the input hash tree in error cases.
Therefore, the input type should be struct tee_fs_htree **ht_arg
to handle these scenarios correctly.

Link: https://github.com/OP-TEE/optee_os/issues/7512
Fixes: 5a9d570a8d7f ("core: fs_htree: re-init hash tree when both tag and counter are zero")

Signed-off-by: Ox Yeh <ox.yeh@mediatek.com>
Reviewed-by: Menson Chen <menson.chen@mediatek.com>
Reviewed-by: Rayan Hu <rayan.hu@mediatek.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/fs_htree.c
@@ -630,11 +630,12 @@ static TEE_Result init_root_node(struct tee_fs_htree *ht)
 	return res;
 }
 
-static TEE_Result create_and_sync(struct tee_fs_htree *ht,
+static TEE_Result create_and_sync(struct tee_fs_htree **ht_arg,
 				  uint8_t *hash,
 				  uint32_t min_counter)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
+	struct tee_fs_htree *ht = *ht_arg;
 	const struct tee_fs_htree_image dummy_head = {
 		.counter = min_counter,
 	};
@@ -656,11 +657,11 @@ static TEE_Result create_and_sync(struct tee_fs_htree *ht,
 		return res;
 
 	ht->dirty = true;
-	res = tee_fs_htree_sync_to_storage(&ht, hash, NULL);
+	res = tee_fs_htree_sync_to_storage(ht_arg, hash, NULL);
 	if (res != TEE_SUCCESS)
 		return res;
 
-	return rpc_write_head(ht, 0, &dummy_head);
+	return rpc_write_head(*ht_arg, 0, &dummy_head);
 }
 
 static bool ht_head_is_partially_done(const struct tee_fs_htree_image *head)
@@ -687,7 +688,7 @@ TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 	ht->stor_aux = stor_aux;
 
 	if (create) {
-		res = create_and_sync(ht, hash, min_counter);
+		res = create_and_sync(&ht, hash, min_counter);
 	} else {
 		res = init_head_from_data(ht, hash, min_counter);
 		if (res != TEE_SUCCESS)
@@ -699,7 +700,7 @@ TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 		 * Re-initialze the hash tree.
 		 */
 		if (ht_head_is_partially_done(&ht->head)) {
-			res = create_and_sync(ht, hash, min_counter);
+			res = create_and_sync(&ht, hash, min_counter);
 			if (res != TEE_SUCCESS)
 				goto out;
 		}