5186e4fd5dfe12ff9b3168da0ba86a2abd55ee8e
Hugues KAMBA MPIANA <hugues.kambampiana@arm.com>
2025-11-11 08:09:40 +0000

    plat-corstone1000: Add Cortex-A320 support
    Convert arm64-platform-cpuarch from a hard-coded cortex-a35 into a “?=”
    (default) assignment so users can override it (for example to
    cortex-a320) via the make command line.
    
    The Cortex-A320 core is not yet supported via -mcpu=cortex-a320.
    When arm64-platform-cpuarch is set to cortex-a320, switch to
    -march=armv9.2-a.
    
    The new Corstone-1000 variant with Cortex-A320 replaces the original
    GIC-400 (v2) interrupt controller with a GIC-600, which is
    architecturally compliant with GICv3. Since OP-TEE already provides
    a generic GICv3 driver, only minimal platform changes are needed
    to expose the updated register map and initialize the GICv3 interface.
    
    **Changes introduced**
    
    * When `cortex-a320` is selected:
      * Force `CFG_ARM_GICV3=y`.
    * Map the Redistributor region (`GICR_BASE`).
    * Use `gic_init_v3(…)` instead of the v2 helper for Cortex-A320 builds.
    * Add `GICR_BASE`, `GIC_REDIST_REG_SIZE`, and related offsets.
    * Retain legacy `GICC_BASE` definitions under the GICv2 path so that
      the Cortex-A35 + GIC-400 variant continues to build unchanged.
    
    Signed-off-by: Hugues KAMBA MPIANA <hugues.kambampiana@arm.com>
    Signed-off-by: Harsimran Singh Tungal <harsimransingh.tungal@arm.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-corstone1000/main.c
+++ core/arch/arm/plat-corstone1000/main.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2022, Arm Limited
+ * Copyright (c) 2022, 2025, Arm Limited
  */
 
 #include <console.h>
@@ -18,11 +18,20 @@ register_ddr(DRAM0_BASE, DRAM0_SIZE);
 
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, CONSOLE_UART_BASE, PL011_REG_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICD_BASE, GIC_DIST_REG_SIZE);
+
+#ifdef CFG_ARM_GICV3
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICR_BASE, GIC_REDIST_REG_SIZE);
+#else
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICC_BASE, GIC_CPU_REG_SIZE);
+#endif
 
 void boot_primary_init_intc(void)
 {
+#ifdef CFG_ARM_GICV3
+	gic_init_v3(0, GICD_BASE, GICR_BASE);
+#else
 	gic_init(GICC_BASE, GICD_BASE);
+#endif
 }
 
 void boot_secondary_init_intc(void)
