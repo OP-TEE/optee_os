283c8326f2b813b635ee54b39896f29690466f55
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-11-28 08:33:44 +0100

    drivers: qti: ramblur: configure pIMEM access
    Configure memory access to enable execution of Trusted Applications.
    
    OP-TEE and its Trusted Applications execute from pIMEM, a region protected
    by the RAMBLUR IP block.
    
    RAMBLUR provides anti-rollback protection as well as confidentiality and
    integrity guarantees for the memory region under its control.
    
    Any agent accessing the pIMEM-protected region performs normal reads or
    writes to the pIMEM address range in the SNoC. The SNoC routes these
    transactions to the pIMEM slave port, and pIMEM remasters them to DDR.
    
    For write transactions, pIMEM applies the required cryptographic
    operations before committing data to DDR.
    
    For read transactions, pIMEM applies the corresponding cryptographic
    operations before returning the data from DDR to the requesting master.
    
    The reserved DDR region used by pIMEM to store cryptographically
    processed data and associated cryptographic state is referred to as the
    pIMEM vault.
    
    With the current U-Boot (tag 2026.01-rc3), the pIMEM Vault DDR
    reservation is derived from the TZ node in U-Boot’s built-in device tree
    (specifically the trusted_apps_mem reserved-memory node).
    
    U-Boot uses this node to construct the EFI memory map that is later
    passed to the kernel.
    
    A future update will remove this dependency on the built-in device tree.
    Instead, U-Boot will obtain the memory configuration directly from SMEM.
    Because of this transition, the current version of the driver does not
    generate a DT overlay for U-Boot to consume.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

---
+++ core/drivers/qcom/ramblur/ramblur_pimem_v3.c
@@ -0,0 +1,361 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <assert.h>
+#include <config.h>
+#include <initcall.h>
+#include <io.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+#include <ramblur_pimem_hwio.h>
+#include <string.h>
+
+#define ANTIROLLBACK	(UINT32_C(1) << \
+	RAMBLUR_PIMEM_WIN0_ALGORITHM_CONFIG_WIN_ANTIROLLBACK_ENABLE_SHFT)
+#define INTEGRITY	(UINT32_C(1) << \
+	RAMBLUR_PIMEM_WIN0_ALGORITHM_CONFIG_WIN_INTEGRITY_ENABLE_SHFT)
+#define CONFIDENTIALITY	(UINT32_C(1) << \
+	RAMBLUR_PIMEM_WIN0_ALGORITHM_CONFIG_WIN_CONFIDENTIALITY_ENABLE_SHFT)
+
+static vaddr_t ramblur_va;
+static struct {
+	uint32_t major;
+	uint32_t minor;
+	uint32_t step;
+} ramblur_version;
+
+/* Some of these functions are used by macros in the auto generated header */
+static inline uint16_t in_word(uint32_t offset)
+{
+	return io_read16(ramblur_va + (vaddr_t)offset);
+}
+
+static inline uint32_t in_dword(uint32_t offset)
+{
+	return io_read32(ramblur_va + (vaddr_t)offset);
+}
+
+static inline void out_dword(uint32_t offset, uint32_t val)
+{
+	io_write32(ramblur_va + (vaddr_t)offset, val);
+}
+
+static inline uint32_t in_dword_masked(uint32_t offset, uint32_t mask)
+{
+	return in_dword(offset) & mask;
+}
+
+static inline void out_dword_masked_ns(uint32_t offset, uint32_t mask,
+				       uint32_t val, uint32_t current_val)
+{
+	uint32_t new_val;
+
+	new_val = (current_val & ~mask) | (val & mask);
+	out_dword(offset, new_val);
+}
+
+static inline void readback_sync(uint32_t reg, uint32_t val, uint32_t mask,
+				 uint32_t shift)
+{
+	if (shift > 31)
+		panic();
+
+	dsb();
+	while (val != (in_dword_masked(reg, mask) >> shift))
+		; /* spin */
+}
+
+static void enable(int window)
+{
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+
+	mask = RAMBLUR_PIMEM_WINn_CTL_WIN_ENABLE_REQ_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_CTL_ADDR(window);
+	val = BIT(RAMBLUR_PIMEM_WINn_CTL_WIN_ENABLE_REQ_SHFT);
+
+	out_dword_masked_ns(reg, mask, val, RAMBLUR_PIMEM_WINn_CTL_INI(window));
+
+	reg = RAMBLUR_PIMEM_WINn_STATUS_ADDR(window);
+	mask = RAMBLUR_PIMEM_WINn_STATUS_WIN_ENABLE_STATUS_BMSK;
+
+	do {
+		val = in_dword_masked(reg, mask) >>
+			RAMBLUR_PIMEM_WINn_STATUS_WIN_ENABLE_STATUS_SHFT;
+	} while (val != 1U);
+}
+
+static void disable_sw_init_mode(int window)
+{
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+
+	mask = RAMBLUR_PIMEM_WINn_CTL_SW_INIT_MODE_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_CTL_ADDR(window);
+	val = 0U;
+
+	out_dword_masked_ns(reg, mask, val, RAMBLUR_PIMEM_WINn_CTL_INI(window));
+	readback_sync(reg, val, mask, RAMBLUR_PIMEM_WINn_CTL_SW_INIT_MODE_SHFT);
+}
+
+static void disable(int window)
+{
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+
+	mask = RAMBLUR_PIMEM_WINn_CTL_WIN_DISABLE_REQ_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_CTL_ADDR(window);
+	val = BIT(RAMBLUR_PIMEM_WINn_CTL_WIN_DISABLE_REQ_SHFT);
+
+	out_dword_masked_ns(reg, mask, val, RAMBLUR_PIMEM_WINn_CTL_INI(window));
+
+	mask = RAMBLUR_PIMEM_WINn_STATUS_WIN_ENABLE_STATUS_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_STATUS_ADDR(window);
+
+	do {
+		val = in_dword_masked(reg, mask) >>
+			RAMBLUR_PIMEM_WINn_STATUS_WIN_ENABLE_STATUS_SHFT;
+	} while (val != 0U);
+}
+
+static void set_hw_init(int window, uint32_t offset)
+{
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+	uint32_t cur = 0;
+
+	mask = RAMBLUR_PIMEM_WINn_HW_INIT_START_OFFSET_INIT_START_OFFSET_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_HW_INIT_START_OFFSET_ADDR(window);
+	cur = RAMBLUR_PIMEM_WINn_HW_INIT_START_OFFSET_INI(window);
+	val = offset;
+
+	out_dword_masked_ns(reg, mask, val, cur);
+	readback_sync(reg, val & mask, mask, 0);
+}
+
+static void initialize_hardware_v3_0(int window)
+{
+	int skip = 6; /* stabilize output */
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+
+	/*
+	 * pIMEM 3.0: HW_INIT_IN_PROGRESS updates only after ~6 cycles.
+	 * Because the first write is posted, the immediate read may return 0.
+	 * Issuing a few back-to-back reads ensures the bit has settled.
+	 */
+	mask = RAMBLUR_PIMEM_WINn_STATUS_HW_INIT_IN_PROGRESS_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_STATUS_ADDR(window);
+
+	do {
+		val = in_dword_masked(reg, mask) >>
+		      RAMBLUR_PIMEM_WINn_STATUS_HW_INIT_IN_PROGRESS_SHFT;
+
+		if (skip > 0) {
+			skip--;
+			continue;
+		}
+
+	} while (val != 0);
+}
+
+static void initialize_hardware_v3_x(int window)
+{
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+
+	mask = RAMBLUR_PIMEM_WINn_STATUS_HW_INIT_DONE_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_STATUS_ADDR(window);
+
+	do {
+		val = in_dword_masked(reg, mask) >>
+			RAMBLUR_PIMEM_WINn_STATUS_HW_INIT_DONE_SHFT;
+	} while (val != 1U);
+}
+
+static void initialize_hardware(int window)
+{
+	uint32_t mask = 0;
+	uint32_t reg = 0;
+	uint32_t val = 0;
+
+	mask = RAMBLUR_PIMEM_WINn_CTL_START_HW_INIT_BMSK;
+	reg = RAMBLUR_PIMEM_WINn_CTL_ADDR(window);
+	val = BIT(RAMBLUR_PIMEM_WINn_CTL_START_HW_INIT_SHFT);
+
+	out_dword_masked_ns(reg, mask, val, RAMBLUR_PIMEM_WINn_CTL_INI(window));
+
+	if (ramblur_version.minor > 0)
+		return initialize_hardware_v3_x(window);
+
+	return initialize_hardware_v3_0(window);
+}
+
+static void initialize(int window)
+{
+	const uint32_t offset = 0U;
+
+	disable_sw_init_mode(window);
+	disable(window);
+	set_hw_init(window, offset);
+	initialize_hardware(window);
+}
+
+static void set_security(int window, uint32_t algo)
+{
+	uint32_t reg = RAMBLUR_PIMEM_WINn_ALGORITHM_CONFIG_ADDR(window);
+
+	out_dword(reg, algo);
+	readback_sync(reg, algo, UINT32_MAX, 0);
+}
+
+static uint32_t get_size(int window)
+{
+	uint32_t reg = RAMBLUR_PIMEM_WINn_SIZE_ADDR(window);
+
+	return in_dword_masked(reg, RAMBLUR_PIMEM_WINn_SIZE_RMSK);
+}
+
+static void set_size(int window, size_t size)
+{
+	uint32_t reg = RAMBLUR_PIMEM_WINn_SIZE_ADDR(window);
+	uint32_t win_size = 0;
+
+	win_size = get_size(window);
+	if (!win_size) {
+		/* Initialize */
+		out_dword(reg, (uint32_t)size);
+		readback_sync(reg, size, RAMBLUR_PIMEM_WINn_SIZE_RMSK, 0);
+	} else if (win_size < size) {
+		/* We can not resize in this code path */
+		panic("Can't increase pIMEM");
+	} else {
+		/* Requested fits what is already configured */
+		IMSG("Ramblur pIMEM size reused");
+	}
+}
+
+static void configure_vault(int window, uintptr_t addr)
+{
+	uint32_t hi = (uint32_t)(addr >> 32);
+	uint32_t lo = (uint32_t)addr;
+	uint32_t reg = 0;
+
+	hi &= RAMBLUR_PIMEM_WINn_DATA_VAULT_START_ADDR_HI_RMSK;
+	reg = RAMBLUR_PIMEM_WINn_DATA_VAULT_START_ADDR_HI_ADDR(window);
+	out_dword(reg, hi);
+	readback_sync(reg, hi, UINT32_MAX, 0);
+
+	lo &= RAMBLUR_PIMEM_WINn_DATA_VAULT_START_ADDR_LOW_RMSK;
+	reg = RAMBLUR_PIMEM_WINn_DATA_VAULT_START_ADDR_LOW_ADDR(window);
+	out_dword(reg, lo);
+	readback_sync(reg, lo, UINT32_MAX, 0);
+}
+
+static void get_hardware_version(uint32_t *major, uint32_t *minor,
+				 uint32_t *step)
+{
+	uint32_t val = in_dword(RAMBLUR_PIMEM_VERSION_ADDR);
+
+	if (major) {
+		*major = (val & RAMBLUR_PIMEM_VERSION_MAJOR_VERSION_BMSK) >>
+			RAMBLUR_PIMEM_VERSION_MAJOR_VERSION_SHFT;
+	}
+
+	if (minor) {
+		*minor = (val & RAMBLUR_PIMEM_VERSION_MINOR_VERSION_BMSK) >>
+			RAMBLUR_PIMEM_VERSION_MINOR_VERSION_SHFT;
+	}
+
+	if (step) {
+		*step = (val & RAMBLUR_PIMEM_VERSION_STEP_VERSION_BMSK) >>
+			RAMBLUR_PIMEM_VERSION_STEP_VERSION_SHFT;
+	}
+}
+
+static void pre_initialize_3_0_0(int window)
+{
+	uint32_t regs[2] = {
+		RAMBLUR_PIMEM_WINn_DATA_TXN_QSB_CTL_ADDR(window),
+		RAMBLUR_PIMEM_WINn_OVERHEAD_TXN_QSB_CTL_ADDR(window),
+	};
+	uint32_t masks[2] = {
+		RAMBLUR_PIMEM_WINn_DATA_TXN_QSB_CTL_AINNERCACHEABLE_BMSK,
+		RAMBLUR_PIMEM_WINn_OVERHEAD_TXN_QSB_CTL_AINNERCACHEABLE_BMSK,
+	};
+	uint32_t inis[2] = {
+		RAMBLUR_PIMEM_WINn_DATA_TXN_QSB_CTL_INI(window),
+		RAMBLUR_PIMEM_WINn_OVERHEAD_TXN_QSB_CTL_INI(window),
+	};
+	uint32_t shift[2] = {
+		RAMBLUR_PIMEM_WINn_DATA_TXN_QSB_CTL_AINNERCACHEABLE_SHFT,
+		RAMBLUR_PIMEM_WINn_OVERHEAD_TXN_QSB_CTL_AINNERCACHEABLE_SHFT,
+	};
+
+	for (int i = 0; i < 2; i++) {
+		out_dword_masked_ns(regs[i], masks[i], 0x0, inis[i]);
+		readback_sync(regs[i], 0, masks[i], shift[i]);
+	}
+}
+
+/* This function might not return (panic) */
+static TEE_Result initialize_window(int window, size_t size, uintptr_t vault)
+{
+	if (ramblur_version.major == 3 &&
+	    ramblur_version.minor == 0 &&
+	    ramblur_version.step == 0)
+		pre_initialize_3_0_0(window);
+
+	configure_vault(window, vault);
+	set_size(window, size);
+	set_security(window, ANTIROLLBACK | INTEGRITY | CONFIDENTIALITY);
+
+	/* Wipes out the full window to 0x0 */
+	initialize(window);
+	enable(window);
+
+	return TEE_SUCCESS;
+}
+
+/*
+ * Trusted Applications run from protected IMEM.
+ * If this region cannot be configured, abort the boot sequence.
+ */
+static TEE_Result qti_ramblur_pimem_init(void)
+{
+	if (!core_mmu_add_mapping(MEM_AREA_IO_SEC, RAMBLUR_PIMEM_REG_BASE,
+				  RAMBLUR_PIMEM_REG_SIZE))
+		panic("Can't add Ramblur pIMEM");
+
+	ramblur_va = (vaddr_t)phys_to_virt(RAMBLUR_PIMEM_REG_BASE,
+					   MEM_AREA_IO_SEC,
+					   RAMBLUR_PIMEM_REG_SIZE);
+	if (!ramblur_va)
+		panic("Can't get Ramblur virtual");
+
+	get_hardware_version(&ramblur_version.major,
+			     &ramblur_version.minor,
+			     &ramblur_version.step);
+
+	DMSG("Ramblur pIMEM v%d.%d.%d (window=%d)",
+	     ramblur_version.major,
+	     ramblur_version.minor,
+	     ramblur_version.step,
+	     CFG_QCOM_RAMBLUR_TA_WINDOW_ID);
+
+	if (ramblur_version.major != 3)
+		panic();
+
+	return initialize_window(CFG_QCOM_RAMBLUR_TA_WINDOW_ID,
+				 RAMBLUR_PIMEM_VAULT_TA_SIZE,
+				 RAMBLUR_PIMEM_VAULT_TA_BASE);
+}
+early_init(qti_ramblur_pimem_init);
