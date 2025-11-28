63a2fda1616d784926496159e0db2ab57666ce35
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-11-28 08:33:44 +0100

    drivers: qcom: ramblur: configure pIMEM access
    Configure memory access to enable execution of Trusted Applications.
    
    OP-TEE and its Trusted Applications execute from pIMEM, a region protected
    by the RAMBLUR IP block.
    
    RAMBLUR provides anti-rollback protection as well as confidentiality and
    integrity guarantees for the memory region under its control.
    
    Any agent accessing the pIMEM-protected region performs normal reads or
    writes to the pIMEM address range in the SNoC. The SNoC routes these
    transactions to the pIMEM slave port, and pIMEM remasters them to DDR.
    
    For write transactions, pIMEM applies the required cryptographic
    operations before committing data to DDR.
    
    For read transactions, pIMEM applies the corresponding cryptographic
    operations before returning the data from DDR to the requesting master.
    
    The reserved DDR region used by pIMEM to store cryptographically
    processed data and associated cryptographic state is referred to as the
    pIMEM vault.
    
    With the current U-Boot (tag 2026.01-rc3), the pIMEM Vault DDR
    reservation is derived from the TZ node in U-Boot’s built-in device tree
    (specifically the trusted_apps_mem reserved-memory node).
    
    U-Boot uses this node to construct the EFI memory map that is later
    passed to the kernel.
    
    A future update will remove this dependency on the built-in device tree.
    Instead, U-Boot will obtain the memory configuration directly from SMEM.
    Because of this transition, the current version of the driver does not
    generate a DT overlay for U-Boot to consume.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Reviewed-by: Tony Hamilton <tonyh@qti.qualcomm.com>

---
+++ core/drivers/qcom/sub.mk
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+#
+#
+
+srcs-$(CFG_QCOM_RAMBLUR_PIMEM_V3) += ramblur/ramblur_pimem_v3.c
