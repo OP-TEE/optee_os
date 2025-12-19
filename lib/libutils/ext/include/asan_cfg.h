a0a34a52863fb80ba193dc914987f148c7dd8ba5
Aleksandr Iashchenko <aleksandr.iashchenko@linutronix.de>
2025-12-19 17:45:04 +0100

    ldelf: asan: add support and user shadow mapping infrastructure
    This commit enables ASan support in ldelf and introduces infrastructure
    for mapping ASan shadow regions in user space. With these changes,
    ASan built with CFG_CORE_SANITIZE_KADDRESS is no longer limited to
    the core and can also operate in ldelf.
    
    Signed-off-by: Aleksandr Iashchenko <aleksandr.iashchenko@linutronix.de>

---
+++ lib/libutils/ext/include/asan_cfg.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#pragma once
+
+#if ((defined(__KERNEL__) || defined(__LDELF__)) && defined(CFG_CORE_SANITIZE_KADDRESS))
+#define CFG_ASAN_ENABLED 1
+#else
+#define CFG_ASAN_ENABLED 0
+#endif
