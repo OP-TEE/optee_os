OP-TEE/optee_os

commit 614b28146e96edda3e92c316eb78b592e00072c2

core: user_ta: PAUTH key initialization may fail
Test crypto_rng_read() return value when initializing user TA pointer
authentication. For sake of simplicity get random bytes before user TA
context starts to be initialized.

Fixes: 2b06f9dede33 ("Add basic pointer authentication support for TA's")
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/user_ta.c
@@ -468,6 +468,14 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	if (!utc)
 		return TEE_ERROR_OUT_OF_MEMORY;
 
+#ifdef CFG_TA_PAUTH
+	res = crypto_rng_read(&utc->uctx.keys, sizeof(utc->uctx.keys));
+	if (res) {
+		free(utc);
+		return res;
+	}
+#endif
+
 	TAILQ_INIT(&utc->open_sessions);
 	TAILQ_INIT(&utc->cryp_states);
 	TAILQ_INIT(&utc->objects);
@@ -490,10 +498,6 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	}
 
 	utc->ta_ctx.is_initializing = true;
-
-#ifdef CFG_TA_PAUTH
-	crypto_rng_read(&utc->uctx.keys, sizeof(utc->uctx.keys));
-#endif
 
 	assert(!mutex_trylock(&tee_ta_mutex));
 

commit 6573dde4356aeef472042e02a4abb2c28ec27488

core: user_ta: fix cleared userspace PAUTH keys
Restore pointer authentication keys that were cleared when commit
referenced below was integrated since vm_info_init(), called after
the keys are generated, resets the user context structure.

Closes: https://github.com/OP-TEE/optee_os/issues/7659
Fixes: 614b28146e96 ("core: user_ta: PAUTH key initialization may fail")
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-by: Rouven Czerwinski <rouven.czerwinski@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/user_ta.c
@@ -33,6 +33,7 @@
 #include <printk.h>
 #include <signed_hdr.h>
 #include <stdlib.h>
+#include <string_ext.h>
 #include <sys/queue.h>
 #include <tee/tee_cryp_utl.h>
 #include <tee/tee_obj.h>
@@ -467,6 +468,9 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 {
 	TEE_Result res = TEE_SUCCESS;
 	struct user_ta_ctx *utc = NULL;
+#ifdef CFG_TA_PAUTH
+	uint8_t pauth_keys[sizeof(utc->uctx.keys)] = { };
+#endif
 
 	/*
 	 * Caller is expected to hold tee_ta_mutex for safe changes
@@ -479,7 +483,7 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 		return TEE_ERROR_OUT_OF_MEMORY;
 
 #ifdef CFG_TA_PAUTH
-	res = crypto_rng_read(&utc->uctx.keys, sizeof(utc->uctx.keys));
+	res = crypto_rng_read(pauth_keys, sizeof(pauth_keys));
 	if (res) {
 		free(utc);
 		return res;
@@ -510,6 +514,11 @@ TEE_Result tee_ta_init_user_ta_session(const TEE_UUID *uuid,
 	utc->ta_ctx.is_initializing = true;
 
 	assert(!mutex_trylock(&tee_ta_mutex));
+
+#ifdef CFG_TA_PAUTH
+	memcpy(&utc->uctx.keys, pauth_keys, sizeof(pauth_keys));
+	memzero_explicit(pauth_keys, sizeof(pauth_keys));
+#endif
 
 	s->ts_sess.ctx = &utc->ta_ctx.ts_ctx;
 	s->ts_sess.handle_scall = s->ts_sess.ctx->ops->handle_scall;