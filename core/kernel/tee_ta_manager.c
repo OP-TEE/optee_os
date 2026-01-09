OP-TEE/optee_os

commit 17513217b24c180ad44d8904d6c7be5ea6868352

ftrace: dump ftrace after every ta_entry
This patch implements the feature to dump ftrace buffer to
tee_supplicant after every entry to the ta.
To implement the feature, this patch does some modification to the
ftrace dumping process and add a new config CFG_FTRACE_DUMP_EVERY_ENTRY
to control this behavior.
This can reduce the chance of losing the ftrace data due to not
enough ftrace buffer and make debugging long-lived TA possible.

Signed-off-by: Leo Chen <shf.chen@mediatek.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

core/kernel/tee_ta_manager.c
@@ -295,17 +295,23 @@ static void tee_ta_unlink_session(struct tee_ta_session *s,
 	mutex_unlock(&tee_ta_mutex);
 }
 
-static void destroy_session(struct tee_ta_session *s,
-			    struct tee_ta_session_head *open_sessions)
+static void dump_ftrace(struct tee_ta_session *s __maybe_unused)
 {
 #if defined(CFG_FTRACE_SUPPORT)
-	if (s->ts_sess.ctx && s->ts_sess.ctx->ops->dump_ftrace) {
+	struct ts_ctx *ts_ctx = s->ts_sess.ctx;
+
+	if (ts_ctx && ts_ctx->ops->dump_ftrace) {
 		ts_push_current_session(&s->ts_sess);
-		s->ts_sess.fbuf = NULL;
-		s->ts_sess.ctx->ops->dump_ftrace(s->ts_sess.ctx);
+		ts_ctx->ops->dump_ftrace(ts_ctx);
 		ts_pop_current_session();
 	}
 #endif
+}
+
+static void destroy_session(struct tee_ta_session *s,
+			    struct tee_ta_session_head *open_sessions)
+{
+	dump_ftrace(s);
 
 	tee_ta_unlink_session(s, open_sessions);
 #if defined(CFG_TA_GPROF_SUPPORT)
@@ -736,6 +742,9 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 		if (panicked) {
 			maybe_release_ta_ctx(ctx);
 			res = TEE_ERROR_TARGET_DEAD;
+		} else {
+			if (IS_ENABLED(CFG_FTRACE_DUMP_EVERY_ENTRY))
+				dump_ftrace(s);
 		}
 
 		tee_ta_clear_busy(ctx);
@@ -799,6 +808,9 @@ TEE_Result tee_ta_invoke_command(TEE_ErrorOrigin *err,
 	if (panicked) {
 		maybe_release_ta_ctx(ta_ctx);
 		res = TEE_ERROR_TARGET_DEAD;
+	} else {
+		if (IS_ENABLED(CFG_FTRACE_DUMP_EVERY_ENTRY))
+			dump_ftrace(sess);
 	}
 
 	tee_ta_clear_busy(ta_ctx);

core/arch/arm/kernel/abort.c
@@ -267,10 +267,8 @@ void abort_print_current_ts(void)
 	s->ctx->ops->dump_state(s->ctx);
 
 #if defined(CFG_FTRACE_SUPPORT)
-	if (s->ctx->ops->dump_ftrace) {
-		s->fbuf = NULL;
+	if (s->ctx->ops->dump_ftrace)
 		s->ctx->ops->dump_ftrace(s->ctx);
-	}
 #endif
 }
 

core/arch/riscv/kernel/abort.c
@@ -178,10 +178,8 @@ void abort_print_current_ts(void)
 	s->ctx->ops->dump_state(s->ctx);
 
 #if defined(CFG_FTRACE_SUPPORT)
-	if (s->ctx->ops->dump_ftrace) {
-		s->fbuf = NULL;
+	if (s->ctx->ops->dump_ftrace)
 		s->ctx->ops->dump_ftrace(s->ctx);
-	}
 #endif
 }
 

core/kernel/ldelf_loader.c
@@ -321,6 +321,7 @@ TEE_Result ldelf_dump_ftrace(struct user_mode_ctx *uctx,
 	uint32_t panicked = 0;
 	size_t *arg = NULL;
 	struct ts_session *sess = NULL;
+	struct ftrace_buf *saved_fbuf = NULL;
 
 	if (!uctx->ftrace_entry_func)
 		return TEE_ERROR_NOT_SUPPORTED;
@@ -342,10 +343,28 @@ TEE_Result ldelf_dump_ftrace(struct user_mode_ctx *uctx,
 	sess = ts_get_current_session();
 	sess->handle_scall = scall_handle_ldelf;
 
+	/*
+	 * This function is called twice during every ftrace dumping
+	 * process (first to obtain the size of the buffer, then to
+	 * get the actual data). The data is required to be the same
+	 * during the two calls.
+	 * Since ldelf uses syscall_sys_return() to return from the
+	 * userspace, the syscall ftrace would be enabled after that,
+	 * modifying the ftrace data.
+	 * Therefore, we need to null the fbuf pointer before entering
+	 * userspace to prevent the ftrace data is modified.
+	 */
+	saved_fbuf = sess->fbuf;
+	sess->fbuf = NULL;
+
 	res = thread_enter_user_mode((vaddr_t)buf, (vaddr_t)arg, 0, 0,
 				     usr_stack, uctx->ftrace_entry_func,
 				     is_32bit, &panicked, &panic_code);
 
+	/* Disable syscall trace again before restoring the pointer */
+	if (saved_fbuf)
+		saved_fbuf->syscall_trace_enabled = false;
+	sess->fbuf = saved_fbuf;
 	sess->handle_scall = sess->ctx->ops->handle_scall;
 	thread_user_clear_vfp(uctx);
 	ldelf_sess_cleanup(sess);

core/kernel/user_ta.c
@@ -288,6 +288,7 @@ static void user_ta_dump_state(struct ts_ctx *ctx)
 static void user_ta_dump_ftrace(struct ts_ctx *ctx)
 {
 	uint32_t prot = TEE_MATTR_URW;
+	struct ts_session *sess = ts_get_current_session();
 	struct user_ta_ctx *utc = to_user_ta_ctx(ctx);
 	struct thread_param params[3] = { };
 	TEE_Result res = TEE_SUCCESS;
@@ -296,6 +297,7 @@ static void user_ta_dump_ftrace(struct ts_ctx *ctx)
 	void *buf = NULL;
 	size_t pl_sz = 0;
 	size_t blen = 0, ld_addr_len = 0;
+	uint32_t dump_id = 0;
 	vaddr_t va = 0;
 
 	res = ldelf_dump_ftrace(&utc->uctx, NULL, &blen);
@@ -325,9 +327,13 @@ static void user_ta_dump_ftrace(struct ts_ctx *ctx)
 	memcpy(ubuf, &ctx->uuid, sizeof(TEE_UUID));
 	ubuf += sizeof(TEE_UUID);
 
-	ld_addr_len = snprintk((char *)ubuf, LOAD_ADDR_DUMP_SIZE,
-			       "TEE load address @ %#"PRIxVA"\n",
-			       VCORE_START_VA);
+	if (sess->fbuf)
+		dump_id = sess->fbuf->dump_id;
+	/* only print the header when this is a new dump */
+	if (!dump_id)
+		ld_addr_len = snprintk((char *)ubuf, LOAD_ADDR_DUMP_SIZE,
+				       "TEE load address @ %#"PRIxVA"\n",
+				       VCORE_START_VA);
 	ubuf += ld_addr_len;
 
 	res = ldelf_dump_ftrace(&utc->uctx, ubuf, &blen);
@@ -336,14 +342,18 @@ static void user_ta_dump_ftrace(struct ts_ctx *ctx)
 		goto out_unmap_pl;
 	}
 
-	params[0] = THREAD_PARAM_VALUE(INOUT, 0, 0, 0);
+	params[0] = THREAD_PARAM_VALUE(INOUT, dump_id, 0, 0);
 	params[1] = THREAD_PARAM_MEMREF(IN, mobj, 0, sizeof(TEE_UUID));
 	params[2] = THREAD_PARAM_MEMREF(IN, mobj, sizeof(TEE_UUID),
 					blen + ld_addr_len);
 
 	res = thread_rpc_cmd(OPTEE_RPC_CMD_FTRACE, 3, params);
-	if (res)
+	if (res) {
 		EMSG("Ftrace thread_rpc_cmd res: %#"PRIx32, res);
+		goto out_unmap_pl;
+	}
+	if (sess->fbuf)
+		sess->fbuf->dump_id = params[0].u.value.a;
 
 out_unmap_pl:
 	res = vm_unmap(&utc->uctx, va, mobj->size);

ldelf/ftrace.c
@@ -107,13 +107,30 @@ void ftrace_copy_buf(void *pctx, void (*copy_func)(void *pctx, void *b,
 		end = hstart + dump_size;
 
 		/* Header */
-		copy_func(pctx, hstart, fbuf->buf_off - fbuf->head_off);
+		if (!fbuf->dump_id)
+			copy_func(pctx, hstart, fbuf->buf_off - fbuf->head_off);
 		if (fbuf->overflow) {
 			/* From current index to end of circular buffer */
 			copy_func(pctx, ccurr, end - ccurr);
 		}
 		/* From start of circular buffer to current index */
 		copy_func(pctx, cstart, ccurr - cstart);
+	}
+}
+
+uint32_t ftrace_get_dump_id(void)
+{
+	if (fbuf)
+		return fbuf->dump_id;
+	else
+		return 0;
+}
+
+void ftrace_reset_buf(void)
+{
+	if (fbuf) {
+		fbuf->curr_idx = 0;
+		fbuf->overflow = false;
 	}
 }
 

ldelf/ftrace.h
@@ -13,6 +13,8 @@
 bool ftrace_init(struct ftrace_buf **fbuf_ptr);
 void ftrace_copy_buf(void *pctx, void (*copy_func)(void *pctx, void *b,
 						   size_t bl));
+uint32_t ftrace_get_dump_id(void);
+void ftrace_reset_buf(void);
 void ftrace_map_lr(uint64_t *lr);
 #else
 static inline void ftrace_map_lr(uint64_t *lr __unused)
@@ -21,4 +23,3 @@ static inline void ftrace_map_lr(uint64_t *lr __unused)
 #endif
 
 #endif /*FTRACE_H*/
-

ldelf/main.c
@@ -103,9 +103,18 @@ static void __noreturn ftrace_dump(void *buf, size_t *blen)
 {
 	struct print_buf_ctx pbuf = { .buf = buf, .blen = *blen };
 
-	ta_elf_print_mappings(&pbuf, print_to_pbuf, &main_elf_queue,
-			      0, NULL, mpool_base);
+	/* only print the header when this is a new dump */
+	if (!ftrace_get_dump_id())
+		ta_elf_print_mappings(&pbuf, print_to_pbuf, &main_elf_queue,
+				      0, NULL, mpool_base);
 	ftrace_copy_buf(&pbuf, copy_to_pbuf);
+	/*
+	 * Reset the buffer after dump if this is the actual write
+	 * The OS may call this function with buf == NULL,
+	 * in order to get the length required to write ftrace data.
+	 */
+	if (buf)
+		ftrace_reset_buf();
 	*blen = pbuf.ret;
 	sys_return_cleanup();
 }

commit 5f55472b493a61a29e7479eb69ef4c22591635e9

core: only dump ftrace buffer with TA mapped
The ftrace buffer is mapped in secure user space. The dump_ftrace()
callback must only be called if the buffer is mapped. During TA panic
the dump_ftrace() might get called as part of the TA context cleanup and
cause a crash. So fix this by skipping the dump_ftrace() callback during
those occasions.

Fixes: 17513217b24c ("ftrace: dump ftrace after every ta_entry")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Rouven Czerwinski <rouven.czerwinski@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Etienne Carriere <etienne.carriere@st.com>

core/kernel/tee_ta_manager.c
@@ -300,7 +300,8 @@ static void dump_ftrace(struct tee_ta_session *s __maybe_unused)
 #if defined(CFG_FTRACE_SUPPORT)
 	struct ts_ctx *ts_ctx = s->ts_sess.ctx;
 
-	if (ts_ctx && ts_ctx->ops->dump_ftrace) {
+	if (ts_ctx && ts_ctx->ops->dump_ftrace &&
+	    core_mmu_user_mapping_is_active()) {
 		ts_push_current_session(&s->ts_sess);
 		ts_ctx->ops->dump_ftrace(ts_ctx);
 		ts_pop_current_session();