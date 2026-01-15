47de86d65b3346f0ed9c5f84e9e30f6cf6a71729
Hugues KAMBA MPIANA <hugues.kambampiana@arm.com>
2026-01-15 18:33:04 +0000

    plat-corstone1000: specify GIC version in plat specific conf.mk
    The Generic Interrupt Controller architecture version is not core
    specific. Therefore move the CFG_ARM_GICV3 definition from
    cortex-a320.mk file to the Corstone-1000 specific file.
    
    Signed-off-by: Hugues KAMBA MPIANA <hugues.kambampiana@arm.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/arch/arm/plat-corstone1000/conf.mk
+++ core/arch/arm/plat-corstone1000/conf.mk
@@ -33,3 +33,9 @@ CFG_DDR_SIZE ?= 0x7f000000
 CFG_DT_ADDR ?= 0x82100000
 CFG_DTB_MAX_SIZE ?= 0x100000
 CFG_CORE_HEAP_SIZE ?= 131072
+
+# Corstone-1000 with Cortex-A320 uses GIC-600 which supports
+# the GICv3 architecture
+ifeq ($(arm64-platform-cpuarch),cortex-a320)
+$(call force,CFG_ARM_GICV3,y)
+endif
