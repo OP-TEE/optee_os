OP-TEE/optee_os

commit ddcd07a27aa6876bd7503f4ddec12edb75e1c393

ta: avb: copy data to temporary buffers
Use intermediate temporary buffers instead of directly supplying
non-secure buffers to TEE_ReadObjectData()/TEE_CreatePersistentObject().
This fixes TA panics while accessing persistent objects:

E/TC:? 0 TA panicked with code 0xffff0001
E/LD:  Status of TA 023f8f1a-292a-432b-8fc4-de8471358067
...
D/TC:? 0 user_ta_enter:176 tee_user_ta_enter: TA panicked with
code 0xffff0001

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/avb/entry.c
@@ -267,13 +267,18 @@ static TEE_Result write_persist_value(uint32_t pt,
 	char *name_buf = params[0].memref.buffer;
 	uint32_t name_buf_sz = params[0].memref.size;
 
-	char *value = params[1].memref.buffer;
 	uint32_t value_sz = params[1].memref.size;
+	char *value = TEE_Malloc(value_sz, 0);
+
+	if (!value)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	TEE_MemMove(value, params[1].memref.buffer, value_sz);
 
 	res = get_named_object_name(name_buf, name_buf_sz,
 				    name_full, &name_full_sz);
 	if (res)
-		return res;
+		goto out;
 
 	res = TEE_CreatePersistentObject(storageid, name_full,
 					 name_full_sz,
@@ -283,6 +288,8 @@ static TEE_Result write_persist_value(uint32_t pt,
 		EMSG("Can't create named object value, res = 0x%x", res);
 
 	TEE_CloseObject(h);
+out:
+	TEE_Free(value);
 
 	return res;
 }
@@ -309,19 +316,22 @@ static TEE_Result read_persist_value(uint32_t pt,
 	char *name_buf = params[0].memref.buffer;
 	uint32_t name_buf_sz = params[0].memref.size;
 
-	char *value = params[1].memref.buffer;
 	uint32_t value_sz = params[1].memref.size;
+	char *value = TEE_Malloc(value_sz, 0);
+
+	if (!value)
+		return TEE_ERROR_OUT_OF_MEMORY;
 
 	res = get_named_object_name(name_buf, name_buf_sz,
 				    name_full, &name_full_sz);
 	if (res)
-		return res;
+		goto out_free;
 
 	res = TEE_OpenPersistentObject(storageid, name_full,
 				       name_full_sz, flags, &h);
 	if (res) {
 		EMSG("Can't open named object value, res = 0x%x", res);
-		return res;
+		goto out_free;
 	}
 
 	res =  TEE_ReadObjectData(h, value, value_sz, &count);
@@ -330,9 +340,14 @@ static TEE_Result read_persist_value(uint32_t pt,
 		goto out;
 	}
 
+	TEE_MemMove(params[1].memref.buffer, value,
+		    value_sz);
+
 	params[1].memref.size = count;
 out:
 	TEE_CloseObject(h);
+out_free:
+	TEE_Free(value);
 
 	return res;
 }

commit 00f4c352d59d705c576d589b4cd321d8da78aa97

ta: avb: fix memory copied in read_persist_value()
read_persist_value() allocates a temporary buffer and reads persistent
value from secure storage into that buffer. Next it copies the content
of the buffer into an out memref, but it copies the number of allocated
bytes instead of the size of the persistent value. No unintended
information leaks though, since Temporary buffer was zero-initialized by
TEE_Malloc(). To avoid unnecessary copying, copy only the number of read
bytes from secure storage.

Fixes: ddcd07a27aa6 ("ta: avb: copy data to temporary buffers")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>
Reviewed-by: Etienne Carriere <etienne.carriere@st.com>

ta/avb/entry.c
@@ -345,9 +345,7 @@ static TEE_Result read_persist_value(uint32_t pt,
 		goto out;
 	}
 
-	TEE_MemMove(params[1].memref.buffer, value,
-		    value_sz);
-
+	TEE_MemMove(params[1].memref.buffer, value, count);
 	params[1].memref.size = count;
 out:
 	TEE_CloseObject(h);