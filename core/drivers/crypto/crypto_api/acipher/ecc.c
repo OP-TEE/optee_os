OP-TEE/optee_os

commit f4f85ac774af1653362ab4ab4df2a43f2cc04154

drivers: crypto: add SM2 ECC encrypt and decrypt
Adds operation handlers for decryption with ECC public keys and
encryption with ECC private keys and implements SM2 curves asymmetric
ciphering.

Signed-off-by: Zexi Yu <yuzexi@hisilicon.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Clement Faure <clement.faure@nxp.com>

core/drivers/crypto/crypto_api/acipher/ecc.c
@@ -340,10 +340,156 @@ static TEE_Result ecc_shared_secret(struct ecc_keypair *private_key,
 	return ret;
 }
 
+static TEE_Result ecc_sm2_encrypt(struct ecc_public_key *key,
+				  const uint8_t *src, size_t src_len,
+				  uint8_t *dst, size_t *dst_len)
+{
+	TEE_Result ret = TEE_ERROR_BAD_PARAMETERS;
+	struct drvcrypt_ecc_ed cdata = { };
+	struct drvcrypt_ecc *ecc = NULL;
+	size_t ciphertext_len = 0;
+	size_t size_bytes = 0;
+
+	ecc = drvcrypt_get_ops(CRYPTO_ECC);
+
+	size_bytes = get_ecc_key_size_bytes(key->curve);
+	if (!size_bytes) {
+		CRYPTO_TRACE("Curve 0x%08"PRIx32" not supported", key->curve);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	/* Uncompressed form indicator */
+	dst[0] = 0x04;
+
+	ciphertext_len = 2 * size_bytes + src_len + TEE_SM3_HASH_SIZE;
+
+	cdata.key = key;
+	cdata.size_sec = size_bytes;
+	cdata.plaintext.data = (uint8_t *)src;
+	cdata.plaintext.length = src_len;
+	cdata.ciphertext.data = dst + 1;
+	cdata.ciphertext.length = ciphertext_len;
+
+	ret = ecc->encrypt(&cdata);
+
+	if (!ret || ret == TEE_ERROR_SHORT_BUFFER)
+		*dst_len = cdata.ciphertext.length + 1;
+
+	return ret;
+}
+
+static TEE_Result ecc_encrypt(struct ecc_public_key *key,
+			      const uint8_t *src, size_t src_len,
+			      uint8_t *dst, size_t *dst_len)
+{
+	struct drvcrypt_ecc *ecc = NULL;
+
+	if (!key || !src || !dst) {
+		CRYPTO_TRACE("Input parameters reference error");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	ecc = drvcrypt_get_ops(CRYPTO_ECC);
+	if (!ecc || !ecc->encrypt)
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	switch (key->curve) {
+	case TEE_ECC_CURVE_SM2:
+		return ecc_sm2_encrypt(key, src, src_len, dst, dst_len);
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
+	}
+}
+
+static TEE_Result ecc_sm2_decrypt(struct ecc_keypair *key,
+				  const uint8_t *src, size_t src_len,
+				  uint8_t *dst, size_t *dst_len)
+{
+	TEE_Result ret = TEE_ERROR_BAD_PARAMETERS;
+	struct drvcrypt_ecc_ed cdata = { };
+	struct drvcrypt_ecc *ecc = NULL;
+	uint8_t *ciphertext = NULL;
+	size_t ciphertext_len = 0;
+	size_t size_bytes = 0;
+	size_t plaintext_len = 0;
+	/* Point Compression */
+	uint8_t pc = 0;
+
+	ecc = drvcrypt_get_ops(CRYPTO_ECC);
+
+	size_bytes = get_ecc_key_size_bytes(key->curve);
+	if (!size_bytes) {
+		CRYPTO_TRACE("Curve 0x%08"PRIx32" not supported", key->curve);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	pc = src[0];
+	switch (pc) {
+	case 0x02:
+	case 0x03:
+		/* Compressed form */
+		return TEE_ERROR_NOT_SUPPORTED;
+	case 0x04:
+		/* Uncompressed form */
+		ciphertext = (uint8_t *)src + 1;
+		ciphertext_len = src_len - 1;
+		break;
+	case 0x06:
+	case 0x07:
+		/* Hybrid form */
+		return TEE_ERROR_NOT_SUPPORTED;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (SUB_OVERFLOW(ciphertext_len, 2 * size_bytes + TEE_SM3_HASH_SIZE,
+			 &plaintext_len))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	cdata.key = key;
+	cdata.size_sec = size_bytes;
+	cdata.ciphertext.data = ciphertext;
+	cdata.ciphertext.length = ciphertext_len;
+	cdata.plaintext.data = dst;
+	cdata.plaintext.length = plaintext_len;
+
+	ret = ecc->decrypt(&cdata);
+
+	/* Set the plaintext length */
+	if (!ret || ret == TEE_ERROR_SHORT_BUFFER)
+		*dst_len = cdata.plaintext.length;
+
+	return ret;
+}
+
+static TEE_Result ecc_decrypt(struct ecc_keypair *key,
+			      const uint8_t *src, size_t src_len,
+			      uint8_t *dst, size_t *dst_len)
+{
+	struct drvcrypt_ecc *ecc = NULL;
+
+	if (!key || !src || !dst) {
+		CRYPTO_TRACE("Input parameters reference error");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	ecc = drvcrypt_get_ops(CRYPTO_ECC);
+	if (!ecc || !ecc->decrypt)
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	switch (key->curve) {
+	case TEE_ECC_CURVE_SM2:
+		return ecc_sm2_decrypt(key, src, src_len, dst, dst_len);
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
+	}
+}
+
 static const struct crypto_ecc_keypair_ops ecc_keypair_ops = {
 	.generate = ecc_generate_keypair,
 	.sign = ecc_sign,
 	.shared_secret = ecc_shared_secret,
+	.decrypt = ecc_decrypt,
 };
 
 TEE_Result drvcrypt_asym_alloc_ecc_keypair(struct ecc_keypair *key,
@@ -361,6 +507,7 @@ TEE_Result drvcrypt_asym_alloc_ecc_keypair(struct ecc_keypair *key,
 	switch (type) {
 	case TEE_TYPE_ECDSA_KEYPAIR:
 	case TEE_TYPE_ECDH_KEYPAIR:
+	case TEE_TYPE_SM2_PKE_KEYPAIR:
 		ecc = drvcrypt_get_ops(CRYPTO_ECC);
 		break;
 	default:
@@ -370,8 +517,17 @@ TEE_Result drvcrypt_asym_alloc_ecc_keypair(struct ecc_keypair *key,
 	if (ecc)
 		ret = ecc->alloc_keypair(key, size_bits);
 
-	if (!ret)
+	if (!ret) {
 		key->ops = &ecc_keypair_ops;
+
+		switch (type) {
+		case TEE_TYPE_SM2_PKE_KEYPAIR:
+			key->curve = TEE_ECC_CURVE_SM2;
+			break;
+		default:
+			break;
+		}
+	}
 
 	CRYPTO_TRACE("ECC Keypair (%zu bits) alloc ret = 0x%" PRIx32, size_bits,
 		     ret);
@@ -381,6 +537,7 @@ TEE_Result drvcrypt_asym_alloc_ecc_keypair(struct ecc_keypair *key,
 static const struct crypto_ecc_public_ops ecc_public_key_ops = {
 	.free = ecc_free_public_key,
 	.verify = ecc_verify,
+	.encrypt = ecc_encrypt,
 };
 
 TEE_Result drvcrypt_asym_alloc_ecc_public_key(struct ecc_public_key *key,
@@ -398,6 +555,7 @@ TEE_Result drvcrypt_asym_alloc_ecc_public_key(struct ecc_public_key *key,
 	switch (type) {
 	case TEE_TYPE_ECDSA_PUBLIC_KEY:
 	case TEE_TYPE_ECDH_PUBLIC_KEY:
+	case TEE_TYPE_SM2_PKE_PUBLIC_KEY:
 		ecc = drvcrypt_get_ops(CRYPTO_ECC);
 		break;
 	default:
@@ -407,8 +565,17 @@ TEE_Result drvcrypt_asym_alloc_ecc_public_key(struct ecc_public_key *key,
 	if (ecc)
 		ret = ecc->alloc_publickey(key, size_bits);
 
-	if (!ret)
+	if (!ret) {
 		key->ops = &ecc_public_key_ops;
+
+		switch (type) {
+		case TEE_TYPE_SM2_PKE_PUBLIC_KEY:
+			key->curve = TEE_ECC_CURVE_SM2;
+			break;
+		default:
+			break;
+		}
+	}
 
 	CRYPTO_TRACE("ECC Public Key (%zu bits) alloc ret = 0x%" PRIx32,
 		     size_bits, ret);

core/drivers/crypto/crypto_api/include/drvcrypt_acipher.h
@@ -134,6 +134,17 @@ struct drvcrypt_secret_data {
 };
 
 /*
+ * Encrypt/Decrypt data
+ */
+struct drvcrypt_ecc_ed {
+	uint32_t algo;                  /* Operation algorithm */
+	void *key;                      /* Public or Private Key */
+	size_t size_sec;                /* Security size in bytes */
+	struct drvcrypt_buf plaintext;  /* Clear text message */
+	struct drvcrypt_buf ciphertext; /* Encrypted message */
+};
+
+/*
  * Crypto ECC driver operations
  */
 struct drvcrypt_ecc {
@@ -152,6 +163,10 @@ struct drvcrypt_ecc {
 	TEE_Result (*verify)(struct drvcrypt_sign_data *sdata);
 	/* ECC Shared Secret */
 	TEE_Result (*shared_secret)(struct drvcrypt_secret_data *sdata);
+	/* ECC Encrypt */
+	TEE_Result (*encrypt)(struct drvcrypt_ecc_ed *cdata);
+	/* ECC Decrypt */
+	TEE_Result (*decrypt)(struct drvcrypt_ecc_ed *cdata);
 };
 
 /*

commit 8d70da4696c0b7333c880297350265f5ec2ad2ac

drivers: crypto: fix SM2 ECC encrypt and decrypt
Adds checks that the destination buffer has room for the result in
ecc_sm2_decrypt() and ecc_sm2_encrypt(). Note that these two functions
not reachable upstream since none of the crypto drivers registers ECC
encrypt or decrypt drivers. So fix this before it becomes a problem.

Fixes: f4f85ac774af ("drivers: crypto: add SM2 ECC encrypt and decrypt")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Zexi Yu <yuzexi@hisilicon.com>

core/drivers/crypto/crypto_api/acipher/ecc.c
@@ -365,10 +365,15 @@ static TEE_Result ecc_sm2_encrypt(struct ecc_public_key *key,
 		return TEE_ERROR_BAD_PARAMETERS;
 	}
 
-	/* Uncompressed form indicator */
-	dst[0] = 0x04;
 
 	ciphertext_len = 2 * size_bytes + src_len + TEE_SM3_HASH_SIZE;
+	if (*dst_len < ciphertext_len + 1) {
+		*dst_len = ciphertext_len + 1;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	/* Uncompressed form indicator */
+	dst[0] = 0x04;
 
 	cdata.key = key;
 	cdata.size_sec = size_bytes;
@@ -452,6 +457,11 @@ static TEE_Result ecc_sm2_decrypt(struct ecc_keypair *key,
 	if (SUB_OVERFLOW(ciphertext_len, 2 * size_bytes + TEE_SM3_HASH_SIZE,
 			 &plaintext_len))
 		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (*dst_len < plaintext_len) {
+		*dst_len = plaintext_len;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
 
 	cdata.key = key;
 	cdata.size_sec = size_bytes;