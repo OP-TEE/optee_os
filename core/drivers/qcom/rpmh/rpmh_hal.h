96896b747a49586c13a93c94bef211e91812b773
Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
2026-02-20 12:52:15 +0530

    drivers: qcom: rpmh: add RPMH client driver
    Add Resource Power Manager Hardened (RPMH) client driver for
    Qualcomm platforms.
    
    RPMH provides a communication interface to the Always-On Processor
    for power management operations. It uses Trigger Command Sets (TCS)
    to send resource state requests (e.g., voltage levels, clock states)
    to the AOP, which then programs the hardware accordingly.
    
    The driver implements:
    - Client handle management for Secure driver ID
    - Active set command submission (sleep/wake not needed for Secure DRV)
    - TCS hardware sequencing and completion barriers
    - Resource command tracking and caching
    - Hardware abstraction layer for register access
    
    This driver is essential for controlling voltage rails during
    QFPROM fuse provisioning, which requires precise voltage levels
    (0.95V on MX rail) to safely program one-time-programmable fuses.
    
    Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

---
+++ core/drivers/qcom/rpmh/rpmh_hal.h
@@ -0,0 +1,88 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __RPMH_HAL_H__
+#define __RPMH_HAL_H__
+
+#include <drivers/qcom/rpmh/rpmh_client.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <util.h>
+
+enum hal_status {
+	HAL_STATUS_SUCCESS = 0,
+	HAL_STATUS_ERROR = 1,
+	HAL_STATUS_INVALID_PARAM = 2,
+};
+
+#define DRV_STRIDE			0x10000
+#define RSC_DRV_IRQ_ENABLE		0x0d00
+#define RSC_DRV_IRQ_STATUS		0x0d04
+#define RSC_DRV_IRQ_CLEAR		0x0d08
+
+#define RSC_DRV_TCS_CONFIG		0x0C
+#define TCS_BASE_OFFSET			0x0D10  /* CMD_WAIT_FOR_CMPL base */
+#define TCS_STRIDE			0x2A0
+
+#define TCS_CONTROL_OFFSET		0x04
+#define TCS_STATUS_OFFSET		0x08
+#define TCS_CMD_ENABLE_OFFSET		0x0C
+
+#define TCS_CMD_BASE_OFFSET		0x20
+#define TCS_CMDn_MSGID_OFFSET		0x00
+#define TCS_CMDn_ADDR_OFFSET		0x04
+#define TCS_CMDn_DATA_OFFSET		0x08
+#define TCS_CMD_STRIDE			0x14
+
+#define TCS_CONTROL_AMC_MODE_TRIGGER	BIT(24)
+#define TCS_CONTROL_AMC_MODE_EN		BIT(16)
+
+#define TCS_STATUS_CONTROLLER_IDLE	BIT(0)
+
+#define RSC_DRV_ERROR_IRQ_STATUS	0xD0
+#define RSC_DRV_ERROR_IRQ_ENABLE	0xD8
+#define RSC_DRV_ERROR_IRQ_CLEAR		0xD4
+
+#define EPCB_TIMEOUT_IRQ_EN_MASK	BIT(20)
+#define EPCB_TIMEOUT_THRESHOLD_SHIFT	0x0
+#define EPCB_TIMEOUT_THRESHOLD_MASK	0xFFFF
+
+#define MSGID_READ_OR_WRITE_SHIFT	0x10
+#define MSGID_RES_REQ_SHIFT		0x8
+#define MSGID_MSG_LENGTH_SHIFT		0x0
+
+#define ADDR_SLV_ID_SHIFT		0x10
+#define ADDR_OFFSET_SHIFT		0x0
+
+enum hal_status hal_rpmh_init(vaddr_t rsc_base);
+enum hal_status hal_rpmh_register_drv(enum rsc_drv_id drv_id);
+enum hal_status hal_rpmh_read_config(enum rsc_drv_id drv_id,
+				     uint32_t *tcs, uint32_t *cmds);
+enum hal_status hal_rpmh_convert_to_amc(enum rsc_drv_id drv_id,
+					uint32_t tcs_id);
+enum hal_status hal_rpmh_convert_to_tcs(enum rsc_drv_id drv_id,
+					uint32_t tcs_id);
+enum hal_status hal_rpmh_enable_amc_status(enum rsc_drv_id drv_id,
+					   uint32_t tcs_id);
+enum hal_status hal_rpmh_clear_amc_status(enum rsc_drv_id drv_id,
+					  uint32_t tcs_id);
+enum hal_status hal_rpmh_is_tcs_idle(enum rsc_drv_id drv_id,
+				     uint32_t tcs_id, bool *idle);
+enum hal_status hal_rpmh_get_amc_status(enum rsc_drv_id drv_id,
+					uint32_t tcs_id,
+					bool *finished);
+enum hal_status hal_rpmh_send_tcs(enum rsc_drv_id drv_id,
+				  uint32_t tcs_id,
+				  uint32_t enable_mask);
+enum hal_status hal_rpmh_write_cmd(enum rsc_drv_id drv_id,
+				   uint32_t tcs_id, uint32_t cmd_idx,
+				   uint32_t addr, uint32_t data,
+				   bool completion);
+enum hal_status hal_rpmh_update_epcb_timeout(enum rsc_drv_id drv_id,
+					     uint32_t threshold);
+enum hal_status hal_rpmh_toggle_epcb_timeout(enum rsc_drv_id drv_id,
+					     bool enable);
+
+#endif /* __RPMH_HAL_H__ */
---
+++ core/drivers/qcom/rpmh/rpmh_resource_commands.h
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __RPMH_RESOURCE_COMMANDS_H__
+#define __RPMH_RESOURCE_COMMANDS_H__
+
+#include <drivers/qcom/rpmh/rpmh_client.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <tee_api_types.h>
+
+enum rpmh_rc_state {
+	RPMH_RC_STATE_IDLE = 0,
+	RPMH_RC_STATE_IN_PROGRESS,
+	RPMH_RC_STATE_MAX
+};
+
+enum rpmh_rc_dirty {
+	RPMH_RC_CLEAN = 0,
+	RPMH_RC_CLEAN_EXPLICIT_VOTE = 1,
+	RPMH_RC_LAST_CLEAN = 1,
+	RPMH_RC_DIRTY = 2,
+	RPMH_RC_DIRTY_MOL = 3,
+	RPMH_RC_DIRTY_USE_ACTIVE = 4,
+	RPMH_RC_DIRTY_MAX
+};
+
+struct rpmh_vote {
+	uint32_t data;
+	bool valid;
+	bool completion;
+	enum rpmh_rc_dirty dirty;
+};
+
+enum rpmh_rc_priority {
+	RPMH_RC_PRIORITY_USE_CASE = 0,
+	RPMH_RC_PRIORITY_SUBSYSTEM,
+	RPMH_RC_PRIORITY_MAX
+};
+
+struct drv_votes {
+	enum rsc_drv_id drv_id;
+	enum rpmh_rc_state state;
+	enum rpmh_rc_priority priority;
+	struct rpmh_vote vote_at_rpmh;
+	struct rpmh_vote local_votes[RPMH_NUM_SETS];
+	struct drv_votes *next;
+};
+
+struct rpmh_resource_command {
+	uint32_t address;
+	struct drv_votes *drv_votes;
+};
+
+/* Initialize resource command */
+void rpmh_resource_command_init(struct rpmh_resource_command *rc,
+				uint32_t address);
+
+/* Update resource command vote, returns true if set is dirty */
+bool rpmh_resource_command_update(struct rpmh_resource_command *rc,
+				  enum rpmh_set set,
+				  uint32_t data, bool completion,
+				  enum rsc_drv_id drv_id,
+				  bool explicit_cmd);
+
+/* Find resource command by address */
+struct rpmh_resource_command *rpmh_find_resource_command(uint32_t address);
+
+#endif /* __RPMH_RESOURCE_COMMANDS_H__ */
