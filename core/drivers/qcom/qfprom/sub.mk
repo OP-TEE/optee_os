27e94084735d248674570a6a023aef214966ed99
Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
2026-02-20 13:34:38 +0530

    drivers: qcom: qfprom: add Kodiak platform configuration
    Add Kodiak-specific QFPROM configuration with 31 fuse region
    definitions including base addresses, sizes, FEC settings, and
    permission masks.
    
    Regions include security keys, anti-rollback counters, OEM
    configuration, and calibration data.
    
    Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
    Reviewed-by: Kishore Batta <kishore.batta@oss.qualcomm.com>
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

--- core/drivers/qcom/qfprom/sub.mk
+++ core/drivers/qcom/qfprom/sub.mk
@@ -6,3 +6,7 @@
 srcs-y += qfprom_core.c
 srcs-y += qfprom_hal.c
 srcs-y += qfprom_target.c
+srcs-y += $(PLATFORM_FLAVOR)/qfprom_fuse_region.c
+
+global-incdirs-y += .
+global-incdirs-y += $(PLATFORM_FLAVOR)
