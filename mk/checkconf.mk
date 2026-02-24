b1de71c81c2d226f5b56785c052e422d800da892
Marco Felsch <m.felsch@pengutronix.de>
2026-02-24 15:41:48 +0100

    checkconf.mk: add cfg-opt-dis macro
    Add a convenient macro to disable CFG_BAR if CFG_FOO is enabled.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

--- mk/checkconf.mk
+++ mk/checkconf.mk
@@ -188,3 +188,15 @@ endef
 define cfg-remove-quotes
 $(patsubst "%",%,$($(1)))
 endef
+
+# Return 'n' if symbol ($1) equals 'y' else return 'y'
+# $(call cfg-opt-dis,CFG_FOO)
+define cfg-opt-dis
+$(call cfg-ifdef,$($(1)),n,y)
+endef
+
+# Return value ($2) if symbol ($1) equals 'y' else return value $(3)
+# CFG_BAR ?= $(call cfg-ifdef,CFG_FOO,yes,no)
+define cfg-ifdef
+$(if $(filter y,$($(1))),$(2),$(3))
+endef
