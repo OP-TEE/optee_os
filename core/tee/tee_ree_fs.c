OP-TEE/optee_os

commit b2284b11a961871ca8670fd8506327daab9ba870

core: update FS storage API with user space buffer
Updates the create(), read(), and write() function pointers in struct
ts_store_ops to take a user space buffer in addition to the previous
core buffer. Core buffers are normal secure memory while user space
buffers should only be accessed using the user_access.h functions.

The different FS storage implementations are updated accordingly.

Note that the RPMB FS storage implementation resorts to using
enter_user_access() and exit_user_access() due to internal complexities.

Fixes: 4e154320e47c ("core: Apply finer-grained PAN")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/tee/tee_ree_fs.c
@@ -8,6 +8,7 @@
 #include <kernel/mutex.h>
 #include <kernel/panic.h>
 #include <kernel/thread.h>
+#include <kernel/user_access.h>
 #include <mempool.h>
 #include <mm/core_memprot.h>
 #include <mm/tee_pager.h>
@@ -62,13 +63,15 @@ static void put_tmp_block(void *tmp_block)
 }
 
 static TEE_Result out_of_place_write(struct tee_fs_fd *fdp, size_t pos,
-				     const void *buf, size_t len)
+				     const void *buf_core,
+				     const void *buf_user, size_t len)
 {
 	TEE_Result res;
 	size_t start_block_num = pos_to_block_num(pos);
 	size_t end_block_num = pos_to_block_num(pos + len - 1);
 	size_t remain_bytes = len;
-	uint8_t *data_ptr = (uint8_t *)buf;
+	uint8_t *data_core_ptr = (uint8_t *)buf_core;
+	uint8_t *data_user_ptr = (uint8_t *)buf_user;
 	uint8_t *block;
 	struct tee_fs_htree_meta *meta = tee_fs_htree_get_meta(fdp->ht);
 
@@ -101,18 +104,26 @@ static TEE_Result out_of_place_write(struct tee_fs_fd *fdp, size_t pos,
 			memset(block, 0, BLOCK_SIZE);
 		}
 
-		if (data_ptr)
-			memcpy(block + offset, data_ptr, size_to_write);
-		else
+		if (data_core_ptr) {
+			memcpy(block + offset, data_core_ptr, size_to_write);
+		} else if (data_user_ptr) {
+			res = copy_from_user(block + offset, data_user_ptr,
+					     size_to_write);
+			if (res)
+				return res;
+		} else {
 			memset(block + offset, 0, size_to_write);
+		}
 
 		res = tee_fs_htree_write_block(&fdp->ht, start_block_num,
 					       block);
 		if (res != TEE_SUCCESS)
 			goto exit;
 
-		if (data_ptr)
-			data_ptr += size_to_write;
+		if (data_core_ptr)
+			data_core_ptr += size_to_write;
+		if (data_user_ptr)
+			data_user_ptr += size_to_write;
 		remain_bytes -= size_to_write;
 		start_block_num++;
 		pos += size_to_write;
@@ -265,7 +276,8 @@ static TEE_Result ree_fs_ftruncate_internal(struct tee_fs_fd *fdp,
 	if ((size_t)new_file_len > meta->length) {
 		size_t ext_len = new_file_len - meta->length;
 
-		res = out_of_place_write(fdp, meta->length, NULL, ext_len);
+		res = out_of_place_write(fdp, meta->length, NULL, NULL,
+					 ext_len);
 		if (res != TEE_SUCCESS)
 			return res;
 	} else {
@@ -296,16 +308,21 @@ static TEE_Result ree_fs_ftruncate_internal(struct tee_fs_fd *fdp,
 }
 
 static TEE_Result ree_fs_read_primitive(struct tee_file_handle *fh, size_t pos,
-					void *buf, size_t *len)
+					void *buf_core, void *buf_user,
+					size_t *len)
 {
 	TEE_Result res;
 	int start_block_num;
 	int end_block_num;
 	size_t remain_bytes;
-	uint8_t *data_ptr = buf;
+	uint8_t *data_core_ptr = buf_core;
+	uint8_t *data_user_ptr = buf_user;
 	uint8_t *block = NULL;
 	struct tee_fs_fd *fdp = (struct tee_fs_fd *)fh;
 	struct tee_fs_htree_meta *meta = tee_fs_htree_get_meta(fdp->ht);
+
+	/* One of buf_core and buf_user must be NULL */
+	assert(!buf_core || !buf_user);
 
 	remain_bytes = *len;
 	if ((pos + remain_bytes) < remain_bytes || pos > meta->length)
@@ -340,9 +357,17 @@ static TEE_Result ree_fs_read_primitive(struct tee_file_handle *fh, size_t pos,
 		if (res != TEE_SUCCESS)
 			goto exit;
 
-		memcpy(data_ptr, block + offset, size_to_read);
+		if (data_core_ptr) {
+			memcpy(data_core_ptr, block + offset, size_to_read);
+			data_core_ptr += size_to_read;
+		} else if (data_user_ptr) {
+			res = copy_to_user(data_user_ptr, block + offset,
+					   size_to_read);
+			if (res)
+				goto exit;
+			data_user_ptr += size_to_read;
+		}
 
-		data_ptr += size_to_read;
 		remain_bytes -= size_to_read;
 		pos += size_to_read;
 
@@ -356,23 +381,27 @@ exit:
 }
 
 static TEE_Result ree_fs_read(struct tee_file_handle *fh, size_t pos,
-			      void *buf, size_t *len)
+			      void *buf_core, void *buf_user, size_t *len)
 {
 	TEE_Result res;
 
 	mutex_lock(&ree_fs_mutex);
-	res = ree_fs_read_primitive(fh, pos, buf, len);
+	res = ree_fs_read_primitive(fh, pos, buf_core, buf_user, len);
 	mutex_unlock(&ree_fs_mutex);
 
 	return res;
 }
 
 static TEE_Result ree_fs_write_primitive(struct tee_file_handle *fh, size_t pos,
-					 const void *buf, size_t len)
+					 const void *buf_core,
+					 const void *buf_user, size_t len)
 {
 	TEE_Result res;
 	struct tee_fs_fd *fdp = (struct tee_fs_fd *)fh;
 	size_t file_size;
+
+	/* One of buf_core and buf_user must be NULL */
+	assert(!buf_core || !buf_user);
 
 	if (!len)
 		return TEE_SUCCESS;
@@ -388,7 +417,7 @@ static TEE_Result ree_fs_write_primitive(struct tee_file_handle *fh, size_t pos,
 			return res;
 	}
 
-	return out_of_place_write(fdp, pos, buf, len);
+	return out_of_place_write(fdp, pos, buf_core, buf_user, len);
 }
 
 static TEE_Result ree_fs_open_primitive(bool create, uint8_t *hash,
@@ -461,11 +490,23 @@ static TEE_Result ree_dirf_commit_writes(struct tee_file_handle *fh,
 	return res;
 }
 
+static TEE_Result dirf_read(struct tee_file_handle *fh, size_t pos, void *buf,
+			    size_t *len)
+{
+	return ree_fs_read_primitive(fh, pos, buf, NULL, len);
+}
+
+static TEE_Result dirf_write(struct tee_file_handle *fh, size_t pos,
+			     const void *buf, size_t len)
+{
+	return ree_fs_write_primitive(fh, pos, buf, NULL, len);
+}
+
 static const struct tee_fs_dirfile_operations ree_dirf_ops = {
 	.open = ree_fs_open_primitive,
 	.close = ree_fs_close_primitive,
-	.read = ree_fs_read_primitive,
-	.write = ree_fs_write_primitive,
+	.read = dirf_read,
+	.write = dirf_write,
 	.commit_writes = ree_dirf_commit_writes,
 };
 
@@ -486,7 +527,7 @@ static TEE_Result open_dirh(struct tee_fs_dirfile_dirh **dirh)
 	if (!res) {
 		size_t l = sizeof(hash);
 
-		res = rpmb_fs_ops.read(ree_fs_rpmb_fh, 0, hash, &l);
+		res = rpmb_fs_ops.read(ree_fs_rpmb_fh, 0, hash, NULL, &l);
 		if (res)
 			return res;
 		if (l == sizeof(hash))
@@ -534,7 +575,7 @@ static TEE_Result commit_dirh_writes(struct tee_fs_dirfile_dirh *dirh)
 	res = tee_fs_dirfile_commit_writes(dirh, hash);
 	if (res)
 		return res;
-	return rpmb_fs_ops.write(ree_fs_rpmb_fh, 0, hash, sizeof(hash));
+	return rpmb_fs_ops.write(ree_fs_rpmb_fh, 0, hash, NULL, sizeof(hash));
 }
 
 static void close_dirh(struct tee_fs_dirfile_dirh **dirh)
@@ -705,14 +746,17 @@ static void ree_fs_close(struct tee_file_handle **fh)
 static TEE_Result ree_fs_create(struct tee_pobj *po, bool overwrite,
 				const void *head, size_t head_size,
 				const void *attr, size_t attr_size,
-				const void *data, size_t data_size,
-				struct tee_file_handle **fh)
+				const void *data_core, const void *data_user,
+				size_t data_size, struct tee_file_handle **fh)
 {
 	struct tee_fs_fd *fdp;
 	struct tee_fs_dirfile_dirh *dirh = NULL;
 	struct tee_fs_dirfile_fileh dfh;
 	TEE_Result res;
 	size_t pos = 0;
+
+	/* One of data_core and data_user must be NULL */
+	assert(!data_core || !data_user);
 
 	*fh = NULL;
 	mutex_lock(&ree_fs_mutex);
@@ -730,21 +774,22 @@ static TEE_Result ree_fs_create(struct tee_pobj *po, bool overwrite,
 		goto out;
 
 	if (head && head_size) {
-		res = ree_fs_write_primitive(*fh, pos, head, head_size);
+		res = ree_fs_write_primitive(*fh, pos, head, NULL, head_size);
 		if (res)
 			goto out;
 		pos += head_size;
 	}
 
 	if (attr && attr_size) {
-		res = ree_fs_write_primitive(*fh, pos, attr, attr_size);
+		res = ree_fs_write_primitive(*fh, pos, attr, NULL, attr_size);
 		if (res)
 			goto out;
 		pos += attr_size;
 	}
 
-	if (data && data_size) {
-		res = ree_fs_write_primitive(*fh, pos, data, data_size);
+	if ((data_core || data_user) && data_size) {
+		res = ree_fs_write_primitive(*fh, pos, data_core, data_user,
+					     data_size);
 		if (res)
 			goto out;
 	}
@@ -770,11 +815,15 @@ out:
 }
 
 static TEE_Result ree_fs_write(struct tee_file_handle *fh, size_t pos,
-			       const void *buf, size_t len)
+			       const void *buf_core, const void *buf_user,
+			       size_t len)
 {
 	TEE_Result res;
 	struct tee_fs_dirfile_dirh *dirh = NULL;
 	struct tee_fs_fd *fdp = (struct tee_fs_fd *)fh;
+
+	/* One of buf_core and buf_user must be NULL */
+	assert(!buf_core || !buf_user);
 
 	mutex_lock(&ree_fs_mutex);
 
@@ -782,7 +831,7 @@ static TEE_Result ree_fs_write(struct tee_file_handle *fh, size_t pos,
 	if (res)
 		goto out;
 
-	res = ree_fs_write_primitive(fh, pos, buf, len);
+	res = ree_fs_write_primitive(fh, pos, buf_core, buf_user, len);
 	if (res)
 		goto out;
 

core/arch/arm/kernel/stmm_sp.c
@@ -649,7 +649,6 @@ static TEE_Result sec_storage_obj_read(unsigned long storage_id, char *obj_id,
 	TEE_Result res = TEE_ERROR_BAD_STATE;
 	struct ts_session *sess = NULL;
 	struct tee_file_handle *fh = NULL;
-	struct stmm_ctx *spc = NULL;
 	struct tee_pobj *po = NULL;
 	size_t file_size = 0;
 	size_t read_len = 0;
@@ -662,13 +661,6 @@ static TEE_Result sec_storage_obj_read(unsigned long storage_id, char *obj_id,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	sess = ts_get_current_session();
-	spc = to_stmm_ctx(sess->ctx);
-	res = vm_check_access_rights(&spc->uctx,
-				     TEE_MEMORY_ACCESS_WRITE |
-				     TEE_MEMORY_ACCESS_ANY_OWNER,
-				     (uaddr_t)data, len);
-	if (res != TEE_SUCCESS)
-		return res;
 
 	res = tee_pobj_get(&sess->ctx->uuid, obj_id, obj_id_len, flags,
 			   false, fops, &po);
@@ -680,7 +672,7 @@ static TEE_Result sec_storage_obj_read(unsigned long storage_id, char *obj_id,
 		goto out;
 
 	read_len = len;
-	res = po->fops->read(fh, offset, data, &read_len);
+	res = po->fops->read(fh, offset, NULL, data, &read_len);
 	if (res == TEE_ERROR_CORRUPT_OBJECT) {
 		EMSG("Object corrupt");
 		po->fops->remove(po);
@@ -709,7 +701,6 @@ static TEE_Result sec_storage_obj_write(unsigned long storage_id, char *obj_id,
 	const struct tee_file_operations *fops = NULL;
 	struct ts_session *sess = NULL;
 	struct tee_file_handle *fh = NULL;
-	struct stmm_ctx *spc = NULL;
 	TEE_Result res = TEE_SUCCESS;
 	struct tee_pobj *po = NULL;
 
@@ -721,13 +712,6 @@ static TEE_Result sec_storage_obj_write(unsigned long storage_id, char *obj_id,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	sess = ts_get_current_session();
-	spc = to_stmm_ctx(sess->ctx);
-	res = vm_check_access_rights(&spc->uctx,
-				     TEE_MEMORY_ACCESS_READ |
-				     TEE_MEMORY_ACCESS_ANY_OWNER,
-				     (uaddr_t)data, len);
-	if (res != TEE_SUCCESS)
-		return res;
 
 	res = tee_pobj_get(&sess->ctx->uuid, obj_id, obj_id_len, flags,
 			   false, fops, &po);
@@ -736,10 +720,10 @@ static TEE_Result sec_storage_obj_write(unsigned long storage_id, char *obj_id,
 
 	res = po->fops->open(po, NULL, &fh);
 	if (res == TEE_ERROR_ITEM_NOT_FOUND)
-		res = po->fops->create(po, false, NULL, 0, NULL, 0, NULL, 0,
-				       &fh);
+		res = po->fops->create(po, false, NULL, 0, NULL, 0,
+				       NULL, NULL, 0, &fh);
 	if (res == TEE_SUCCESS) {
-		res = po->fops->write(fh, offset, data, len);
+		res = po->fops->write(fh, offset, NULL, data, len);
 		po->fops->close(&fh);
 	}
 

core/drivers/crypto/se050/core/storage.c
@@ -38,7 +38,8 @@ TEE_Result crypto_storage_obj_del(struct tee_obj *o)
 		return TEE_ERROR_OUT_OF_MEMORY;
 
 	/* Read the object into memory */
-	ret = o->pobj->fops->read(o->fh, o->info.dataPosition, data, &len);
+	ret = o->pobj->fops->read(o->fh, o->info.dataPosition,
+				  data, NULL, &len);
 	if (ret) {
 		EMSG("se05x: can not read the object prior removal");
 		free(data);

core/include/tee/fs_dirfile.h
@@ -38,8 +38,8 @@ struct tee_fs_dirfile_operations {
 			   struct tee_fs_dirfile_fileh *dfh,
 			   struct tee_file_handle **fh);
 	void (*close)(struct tee_file_handle *fh);
-	TEE_Result (*read)(struct tee_file_handle *fh, size_t pos,
-			   void *buf, size_t *len);
+	TEE_Result (*read)(struct tee_file_handle *fh, size_t pos, void *buf,
+			   size_t *len);
 	TEE_Result (*write)(struct tee_file_handle *fh, size_t pos,
 			    const void *buf, size_t len);
 	TEE_Result (*commit_writes)(struct tee_file_handle *fh, uint8_t *hash);

core/include/tee/tee_fs.h
@@ -34,13 +34,14 @@ struct tee_file_operations {
 	TEE_Result (*create)(struct tee_pobj *po, bool overwrite,
 			     const void *head, size_t head_size,
 			     const void *attr, size_t attr_size,
-			     const void *data, size_t data_size,
-			     struct tee_file_handle **fh);
+			     const void *data_core, const void *data_user,
+			     size_t data_size, struct tee_file_handle **fh);
 	void (*close)(struct tee_file_handle **fh);
 	TEE_Result (*read)(struct tee_file_handle *fh, size_t pos,
-			   void *buf, size_t *len);
+			   void *buf_core, void *buf_user, size_t *len);
 	TEE_Result (*write)(struct tee_file_handle *fh, size_t pos,
-			    const void *buf, size_t len);
+			    const void *buf_core, const void *buf_user,
+			    size_t len);
 	TEE_Result (*rename)(struct tee_pobj *old_po, struct tee_pobj *new_po,
 			     bool overwrite);
 	TEE_Result (*remove)(struct tee_pobj *po);

core/kernel/ree_fs_ta.c
@@ -110,17 +110,18 @@ static TEE_Result check_update_version(const char *db_name,
 		goto out;
 
 	if (res == TEE_ERROR_ITEM_NOT_FOUND) {
-		res = ops->create(&pobj, false, NULL, 0, NULL, 0, NULL, 0, &fh);
+		res = ops->create(&pobj, false, NULL, 0, NULL, 0, NULL, NULL,
+				   0, &fh);
 		if (res != TEE_SUCCESS)
 			goto out;
 
-		res = ops->write(fh, 0, &db_hdr, sizeof(db_hdr));
+		res = ops->write(fh, 0, &db_hdr, NULL, sizeof(db_hdr));
 		if (res != TEE_SUCCESS)
 			goto out;
 	} else {
 		len = sizeof(db_hdr);
 
-		res = ops->read(fh, 0, &db_hdr, &len);
+		res = ops->read(fh, 0, &db_hdr, NULL, &len);
 		if (res != TEE_SUCCESS) {
 			goto out;
 		} else if (len != sizeof(db_hdr)) {
@@ -133,7 +134,7 @@ static TEE_Result check_update_version(const char *db_name,
 		len = sizeof(db_entry);
 
 		res = ops->read(fh, sizeof(db_hdr) + (i * len), &db_entry,
-				&len);
+				NULL, &len);
 		if (res != TEE_SUCCESS) {
 			goto out;
 		} else if (len != sizeof(db_entry)) {
@@ -156,7 +157,7 @@ static TEE_Result check_update_version(const char *db_name,
 			db_entry.version = version;
 			len = sizeof(db_entry);
 			res = ops->write(fh, sizeof(db_hdr) + (i * len),
-					 &db_entry, len);
+					 &db_entry, NULL, len);
 			if (res != TEE_SUCCESS)
 				goto out;
 		}
@@ -165,12 +166,12 @@ static TEE_Result check_update_version(const char *db_name,
 		db_entry.version = version;
 		len = sizeof(db_entry);
 		res = ops->write(fh, sizeof(db_hdr) + (db_hdr.nb_entries * len),
-				 &db_entry, len);
+				 &db_entry, NULL, len);
 		if (res != TEE_SUCCESS)
 			goto out;
 
 		db_hdr.nb_entries++;
-		res = ops->write(fh, 0, &db_hdr, sizeof(db_hdr));
+		res = ops->write(fh, 0, &db_hdr, NULL, sizeof(db_hdr));
 		if (res != TEE_SUCCESS)
 			goto out;
 	}

core/pta/attestation.c
@@ -232,7 +232,7 @@ static TEE_Result sec_storage_obj_read(TEE_UUID *uuid, uint32_t storage_id,
 		goto out;
 
 	read_len = *len;
-	res = po->fops->read(fh, offset, data, &read_len);
+	res = po->fops->read(fh, offset, data, NULL, &read_len);
 	if (res == TEE_ERROR_CORRUPT_OBJECT) {
 		EMSG("Object corrupt");
 		po->fops->remove(po);
@@ -273,10 +273,10 @@ static TEE_Result sec_storage_obj_write(TEE_UUID *uuid, uint32_t storage_id,
 
 	res = po->fops->open(po, NULL, &fh);
 	if (res == TEE_ERROR_ITEM_NOT_FOUND)
-		res = po->fops->create(po, false, NULL, 0, NULL, 0, NULL, 0,
-				       &fh);
+		res = po->fops->create(po, false, NULL, 0, NULL, 0,
+				       NULL, NULL, 0, &fh);
 	if (!res) {
-		res = po->fops->write(fh, offset, data, len);
+		res = po->fops->write(fh, offset, data, NULL, len);
 		po->fops->close(&fh);
 	}
 

core/tee/fs_dirfile.c
@@ -113,8 +113,8 @@ static TEE_Result write_dent(struct tee_fs_dirfile_dirh *dirh, size_t n,
 {
 	TEE_Result res;
 
-	res = dirh->fops->write(dirh->fh, sizeof(*dent) * n,
-				dent, sizeof(*dent));
+	res = dirh->fops->write(dirh->fh, sizeof(*dent) * n, dent,
+				sizeof(*dent));
 	if (!res && n >= dirh->ndents)
 		dirh->ndents = n + 1;
 

commit 93e9b8694fc88795047611e7ae981d683b3969e6

core: tee_ree_fs: fix possible mempool leak
In out_of_place_write() if copy_from_user() fails, the function returns
directly instead of using the common cleanup path. If this happens a
temporary block is leaked from the default memory pool. Fix this by
using the common exit path.

Fixes: b2284b11a961 ("core: update FS storage API with user space buffer")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Rouven Czerwinski <rouven.czerwinski@linaro.org>

core/tee/tee_ree_fs.c
@@ -109,7 +109,7 @@ static TEE_Result out_of_place_write(struct tee_fs_fd *fdp, size_t pos,
 			res = copy_from_user(block + offset, data_user_ptr,
 					     size_to_write);
 			if (res)
-				return res;
+				goto exit;
 		} else {
 			memset(block + offset, 0, size_to_write);
 		}