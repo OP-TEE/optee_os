OP-TEE/optee_os

commit 4237855ad63e53d0dc0e9b2914d0cccfb7507746

plat-d06: Add support for HIP08A
HIP08A is another form of the D06 development board and equipped by
Hisilicon.

Signed-off-by: zhaozheng7 <zhaozheng96@outlook.com>
Reviewed-by: Yuan Wang <wangyuan46@huawei.com>
Acked-by: Zexi Yu <yuzexi@hisilicon.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-d06/conf.mk
@@ -16,17 +16,40 @@ $(call force,CFG_ARM64_core,y)
 $(call force,CFG_WITH_LPAE,y)
 $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_LPAE_ADDR_SPACE_BITS,48)
-$(call force,CFG_LPC_UART,y)
 $(call force,CFG_CRYPTO_PBKDF2,y)
 $(call force,CFG_CRYPTO_HW_PBKDF2,y)
+$(call force,CFG_CORE_IRQ_IS_NATIVE_INTR,y)
+$(call force,CFG_CORE_UNMAP_CORE_AT_EL0,n)
+$(call force,CFG_ATTESTATION_PTA,y)
+$(call force,CFG_DYN_CONFIG,y)
 
 CFG_TEE_CORE_LOG_LEVEL ?= 4
-
-CFG_CORE_HEAP_SIZE ?= 0x008000000
 CFG_CORE_ARM64_PA_BITS ?= 40
-CFG_TEE_RAM_VA_SIZE ?= 0x009000000
-CFG_TZDRAM_START ?=    0x20C0000000
-CFG_TZDRAM_SIZE ?=     0x32000000
-CFG_SHMEM_START ?=     0x50000000
-CFG_SHMEM_SIZE ?=      0x04000000
 
+CFG_TEE_DYN_VASPACE_SIZE ?= (1024 * 1024 * 5)
+PLATFORM_FLAVOR ?= d06
+CFG_TZDRAM_START ?=	0x20C0000000
+CFG_TZDRAM_SIZE ?=	0x32000000
+CFG_SHMEM_START ?=	0x50000000
+CFG_SHMEM_SIZE ?=	0x04000000
+
+ifeq (${PLATFORM_FLAVOR}, d06)
+$(call force,CFG_LPC_UART,y)
+CFG_CORE_HEAP_SIZE ?=	0x008000000
+CFG_TEE_RAM_VA_SIZE ?=	0x009000000
+CFG_TZDRAM_START ?=	0x20C0000000
+CFG_TZDRAM_SIZE ?=	0x32000000
+CFG_SHMEM_START ?=	0x50000000
+CFG_SHMEM_SIZE ?=	0x04000000
+endif # PLATFORM_FLAVOR = d06
+
+ifeq (${PLATFORM_FLAVOR}, HIP08A)
+$(call force,CFG_PL011,y)
+CFG_CRYPTO_HW_PBKDF2_WITH_EFUSE ?= y
+CFG_CORE_HEAP_SIZE ?=	0x008000000
+CFG_TEE_RAM_VA_SIZE ?=	0x009000000
+CFG_TZDRAM_START ?=	0x2140000000
+CFG_TZDRAM_SIZE ?=	0x40000000
+CFG_SHMEM_START ?=	0x50200000
+CFG_SHMEM_SIZE ?=	0x04000000
+endif # PLATFORM_FLAVOR = HIP08A

core/arch/arm/plat-d06/main.c
@@ -5,16 +5,33 @@
  */
 #include <console.h>
 #include <drivers/lpc_uart.h>
+#include <drivers/pl011.h>
 #include <platform_config.h>
 
+#if defined(PLATFORM_FLAVOR_d06)
 static struct lpc_uart_data console_data __nex_bss;
-
 register_phys_mem_pgdir(MEM_AREA_IO_NSEC, LPC_BASE, LPC_SIZE);
+#endif
+
+#if defined(PLATFORM_FLAVOR_HIP08A)
+static struct pl011_data console_data __nex_bss;
+register_phys_mem_pgdir(MEM_AREA_IO_NSEC, UART_BASE, PL011_REG_SIZE);
+#endif
+
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, HISI_SEC_BASE, HISI_SEC_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, HISI_TRNG_BASE, HISI_TRNG_SIZE);
 
 void plat_console_init(void)
 {
+#if defined(PLATFORM_FLAVOR_d06)
 	lpc_uart_init(&console_data, LPC_BASE, CONSOLE_UART_CLK_IN_HZ,
 		      CONSOLE_BAUDRATE);
-
 	register_serial_console(&console_data.chip);
+#endif
+
+#if defined(PLATFORM_FLAVOR_HIP08A)
+	pl011_init(&console_data, UART_BASE, CONSOLE_UART_CLK_IN_HZ,
+		   CONSOLE_BAUDRATE);
+	register_serial_console(&console_data.chip);
+#endif
 }

core/arch/arm/plat-d06/platform_config.h
@@ -13,12 +13,24 @@
 #define STACK_ALIGNMENT		64
 
 /* UART */
+#if defined(PLATFORM_FLAVOR_d06)
 #define UART_BASE		0x2f8
-#define CONSOLE_BAUDRATE	115200
 #define CONSOLE_UART_CLK_IN_HZ	200
+#endif
+
+#if defined(PLATFORM_FLAVOR_HIP08A)
+#define UART_BASE		0x94080000
+#define CONSOLE_UART_CLK_IN_HZ	200000000
+#endif
+
+#define CONSOLE_BAUDRATE	115200
 
 /* HISI_TRNG */
 #define HISI_TRNG_BASE		0x2010C0000
 #define HISI_TRNG_SIZE		0x100
+
+/* SEC */
+#define HISI_SEC_BASE		0x141800000
+#define HISI_SEC_SIZE		0x400000
 
 #endif /* PLATFORM_CONFIG_H */

commit ef843805f615a5440fdfa8670cc0b327970323ec

plat-d06: fix d06 platform bug
Delete unnecessary configuration information to prevent the failure of
correct value assignment.

Fixes: 4237855ad63e ("plat-d06: Add support for HIP08A")
Signed-off-by: zhaozheng7 <zhaozheng96@outlook.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-d06/conf.mk
@@ -28,10 +28,6 @@ CFG_CORE_ARM64_PA_BITS ?= 40
 
 CFG_TEE_DYN_VASPACE_SIZE ?= (1024 * 1024 * 5)
 PLATFORM_FLAVOR ?= d06
-CFG_TZDRAM_START ?=	0x20C0000000
-CFG_TZDRAM_SIZE ?=	0x32000000
-CFG_SHMEM_START ?=	0x50000000
-CFG_SHMEM_SIZE ?=	0x04000000
 
 ifeq (${PLATFORM_FLAVOR}, d06)
 $(call force,CFG_LPC_UART,y)