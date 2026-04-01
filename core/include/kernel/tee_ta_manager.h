OP-TEE/optee_os

commit 096cbcddbe21a0cf0ae1f553f267f56befb6ac13

Align Session handle for generic ta interface entry
TEE session handle is now used by all tee_dispatch_xx
function. uint32_t type ID parameter has be removed for
the tee_dispatch_close_session() function.

Signed-off-by: Jean-Michel Delorme <jean-michel.delorme@st.com>
Reviewed-by: Pascal BRAND <pascal.brand@st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Tested-by: Pascal Brand <pascal.brand@linaro.org> (STM platform)

core/include/kernel/tee_ta_manager.h
@@ -89,7 +89,7 @@ TEE_Result tee_ta_cancel_command(TEE_ErrorOrigin *err,
  * Returns:
  *        TEE_Result
  *---------------------------------------------------------------------------*/
-TEE_Result tee_ta_close_session(uint32_t id,
+TEE_Result tee_ta_close_session(struct tee_ta_session *sess,
 				struct tee_ta_session_head *open_sessions,
 				const TEE_Identity *clnt_id);
 

core/tee/tee_svc.c
@@ -612,7 +612,8 @@ TEE_Result tee_svc_close_ta_session(TEE_TASessionHandle ta_sess)
 	memcpy(&clnt_id.uuid, &sess->ctx->head->uuid, sizeof(TEE_UUID));
 
 	tee_ta_set_current_session(NULL);
-	res = tee_ta_close_session((vaddr_t)ta_sess, &sess->ctx->open_sessions,
+	res = tee_ta_close_session((struct tee_ta_session *)ta_sess,
+			&sess->ctx->open_sessions,
 				   &clnt_id);
 	tee_ta_set_current_session(sess);
 	return res;

core/arch/arm/kernel/tee_ta_manager.c
@@ -992,7 +992,7 @@ static void tee_ta_destroy_context(struct tee_ta_ctx *ctx)
 	 * from the ctx->open_sessions list.
 	 */
 	while (!TAILQ_EMPTY(&ctx->open_sessions)) {
-		tee_ta_close_session((vaddr_t)TAILQ_FIRST(&ctx->open_sessions),
+		tee_ta_close_session(TAILQ_FIRST(&ctx->open_sessions),
 				     &ctx->open_sessions, KERN_IDENTITY);
 	}
 
@@ -1043,24 +1043,25 @@ static TEE_Result check_client(struct tee_ta_session *s, const TEE_Identity *id)
 /*-----------------------------------------------------------------------------
  * Close a Trusted Application and free available resources
  *---------------------------------------------------------------------------*/
-TEE_Result tee_ta_close_session(uint32_t id,
+TEE_Result tee_ta_close_session(struct tee_ta_session *csess,
 				struct tee_ta_session_head *open_sessions,
 				const TEE_Identity *clnt_id)
 {
 	struct tee_ta_session *sess;
 	struct tee_ta_ctx *ctx;
 
-	DMSG("tee_ta_close_session(%x)", (unsigned int)id);
+	DMSG("tee_ta_close_session(0x%" PRIxVA ")",  (vaddr_t)csess);
 
-	if (id == 0)
+	if (!csess)
 		return TEE_ERROR_ITEM_NOT_FOUND;
 
 	TAILQ_FOREACH(sess, open_sessions, link) {
-		if (id == (vaddr_t)sess)
+		if (csess == sess)
 			break;
 	}
 	if (!sess) {
-		EMSG(" .... Session 0x%x to removed is not found", id);
+		EMSG("session 0x%" PRIxVA " to removed is not found",
+		     (vaddr_t)csess);
 		return TEE_ERROR_ITEM_NOT_FOUND;
 	}
 
@@ -1330,7 +1331,7 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 
 	if (ctx->panicked) {
 		DMSG("panicked, call tee_ta_close_session()");
-		tee_ta_close_session((vaddr_t)s, open_sessions, KERN_IDENTITY);
+		tee_ta_close_session(s, open_sessions, KERN_IDENTITY);
 		*err = TEE_ORIGIN_TEE;
 		return TEE_ERROR_TARGET_DEAD;
 	}
@@ -1364,7 +1365,7 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	panicked = ctx->panicked;
 
 	if (panicked || (res != TEE_SUCCESS))
-		tee_ta_close_session((vaddr_t)s, open_sessions, KERN_IDENTITY);
+		tee_ta_close_session(s, open_sessions, KERN_IDENTITY);
 
 	/*
 	 * Origin error equal to TEE_ORIGIN_TRUSTED_APP for "regular" error,

core/arch/arm/tee/entry.c
@@ -206,7 +206,7 @@ static void entry_close_session(struct thread_smc_args *args,
 		struct tee_close_session_in in;
 		uint32_t ret;
 
-		in.sess = arg32->session;
+		in.sess = (TEE_Session *)(vaddr_t)arg32->session;
 		ret = tee_dispatch_close_session(&in);
 		if (ret == TEE_ERROR_SYSTEM_BUSY) {
 			args->a0 = TEESMC_RETURN_EBUSY;

core/include/kernel/tee_dispatch.h
@@ -87,7 +87,7 @@ struct tee_dispatch_cancel_command_out {
 
 /* Input arg structure specific to TEE service 'close session'. */
 struct tee_close_session_in {
-	uint32_t sess;
+	TEE_Session *sess;
 };
 
 /* Input arg structure specific to TEE service 'register shared memory'. */

core/kernel/tee_dispatch.c
@@ -143,8 +143,8 @@ TEE_Result tee_dispatch_close_session(struct tee_close_session_in *in)
 {
 	inject_entropy_with_timestamp();
 
-	return tee_ta_close_session(in->sess, &tee_open_sessions,
-				    NSAPP_IDENTITY);
+	return tee_ta_close_session((struct tee_ta_session *)in->sess,
+				    &tee_open_sessions, NSAPP_IDENTITY);
 }
 
 TEE_Result tee_dispatch_invoke_command(struct tee_dispatch_invoke_command_in *
@@ -153,7 +153,7 @@ TEE_Result tee_dispatch_invoke_command(struct tee_dispatch_invoke_command_in *
 				       out)
 {
 	struct tee_ta_param param;
-	struct tee_ta_session *sess; /*= (struct tee_ta_session *)arg->sess; */
+	struct tee_ta_session *sess;
 	TEE_Result res;
 	TEE_ErrorOrigin err = TEE_ORIGIN_TEE;
 

commit 143581ac7370e096fb7cb0c13f53dd5052ff5539

core: pass session ID to tee_ta_close_session()
tee_ta_close_session() previously took a session pointer. When called
from entry_close_session(), this pointer was acquired without bumping
the reference count, creating a race condition where the session could
be freed by another thread before being used. This race is harmless
unless ASAN or memory tagging is enabled.

Refactor tee_ta_close_session() to take a session ID instead of a
pointer to avoid possible races.

Fixes: 096cbcddbe21 ("Align Session handle for generic ta interface entry")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

core/include/kernel/tee_ta_manager.h
@@ -128,7 +128,7 @@ bool tee_ta_session_is_cancelled(struct tee_ta_session *s, TEE_Time *curr_time);
  * Returns:
  *        TEE_Result
  *---------------------------------------------------------------------------*/
-TEE_Result tee_ta_close_session(struct tee_ta_session *sess,
+TEE_Result tee_ta_close_session(uint32_t id,
 				struct tee_ta_session_head *open_sessions,
 				const TEE_Identity *clnt_id);
 

core/kernel/tee_ta_manager.c
@@ -456,7 +456,7 @@ infinite:
 /*-----------------------------------------------------------------------------
  * Close a Trusted Application and free available resources
  *---------------------------------------------------------------------------*/
-TEE_Result tee_ta_close_session(struct tee_ta_session *csess,
+TEE_Result tee_ta_close_session(uint32_t id,
 				struct tee_ta_session_head *open_sessions,
 				const TEE_Identity *clnt_id)
 {
@@ -466,17 +466,12 @@ TEE_Result tee_ta_close_session(struct tee_ta_session *csess,
 	bool keep_crashed = false;
 	bool keep_alive = false;
 
-	DMSG("csess 0x%" PRIxVA " id %u",
-	     (vaddr_t)csess, csess ? csess->id : UINT_MAX);
+	DMSG("id %"PRIu32, id);
 
-	if (!csess)
-		return TEE_ERROR_ITEM_NOT_FOUND;
-
-	sess = tee_ta_get_session(csess->id, true, open_sessions);
+	sess = tee_ta_get_session(id, true, open_sessions);
 
 	if (!sess) {
-		EMSG("session 0x%" PRIxVA " to be removed is not found",
-		     (vaddr_t)csess);
+		EMSG("session id %"PRIu32" to be removed is not found", id);
 		return TEE_ERROR_ITEM_NOT_FOUND;
 	}
 
@@ -716,6 +711,7 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	struct ts_ctx *ts_ctx = NULL;
 	bool panicked = false;
 	bool was_busy = false;
+	uint32_t id = 0;
 
 	res = tee_ta_init_session(err, open_sessions, uuid, &s);
 	if (res != TEE_SUCCESS) {
@@ -764,9 +760,10 @@ TEE_Result tee_ta_open_session(TEE_ErrorOrigin *err,
 	else
 		*err = s->err_origin;
 
+	id = s->id;
 	tee_ta_put_session(s);
 	if (panicked || res != TEE_SUCCESS)
-		tee_ta_close_session(s, open_sessions, KERN_IDENTITY);
+		tee_ta_close_session(id, open_sessions, KERN_IDENTITY);
 
 	if (!res)
 		*sess = s;

core/kernel/user_ta.c
@@ -383,7 +383,7 @@ static void release_utc_state(struct user_ta_ctx *utc)
 	 * from the utc->open_sessions list.
 	 */
 	while (!TAILQ_EMPTY(&utc->open_sessions)) {
-		tee_ta_close_session(TAILQ_FIRST(&utc->open_sessions),
+		tee_ta_close_session(TAILQ_FIRST(&utc->open_sessions)->id,
 				     &utc->open_sessions, KERN_IDENTITY);
 	}
 

core/tee/entry_std.c
@@ -411,8 +411,7 @@ out:
 
 static void entry_close_session(struct optee_msg_arg *arg, uint32_t num_params)
 {
-	TEE_Result res;
-	struct tee_ta_session *s;
+	TEE_Result res = TEE_SUCCESS;
 
 	if (num_params) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -422,8 +421,8 @@ static void entry_close_session(struct optee_msg_arg *arg, uint32_t num_params)
 	plat_prng_add_jitter_entropy(CRYPTO_RNG_SRC_JITTER_SESSION,
 				     &session_pnum);
 
-	s = tee_ta_find_session(arg->session, &tee_open_sessions);
-	res = tee_ta_close_session(s, &tee_open_sessions, NSAPP_IDENTITY);
+	res = tee_ta_close_session(arg->session, &tee_open_sessions,
+				   NSAPP_IDENTITY);
 out:
 	arg->ret = res;
 	arg->ret_origin = TEE_ORIGIN_TEE;

core/tee/tee_svc.c
@@ -832,14 +832,11 @@ TEE_Result syscall_close_ta_session(unsigned long ta_sess)
 	struct ts_session *sess = ts_get_current_session();
 	struct user_ta_ctx *utc = to_user_ta_ctx(sess->ctx);
 	TEE_Identity clnt_id = { };
-	struct tee_ta_session *s = NULL;
-
-	s = tee_ta_find_session(ta_sess, &utc->open_sessions);
 
 	clnt_id.login = TEE_LOGIN_TRUSTED_APP;
 	memcpy(&clnt_id.uuid, &sess->ctx->uuid, sizeof(TEE_UUID));
 
-	return tee_ta_close_session(s, &utc->open_sessions, &clnt_id);
+	return tee_ta_close_session(ta_sess, &utc->open_sessions, &clnt_id);
 }
 
 TEE_Result syscall_invoke_ta_command(unsigned long ta_sess,