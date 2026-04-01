a78388c06ef4539359c2ac853c747e5338ab6c62
Sunil Vaghela <Sunilkumar.JagdishbhaiVaghela@amd.com>
2026-04-01 15:01:45 +0530

    crypto: asu: ECC and ECDH crypto driver support
    Add ECC and ECDH support through the AMD ASU crypto firmware driver path.
    This driver enables hardware-accelerated ECDSA and ECDH operations for
    the following curves:
    - NIST P-192
    - NIST P-224
    - NIST P-256
    - NIST P-384
    - NIST P-521
    
    Signed-off-by: Sunil Vaghela <Sunilkumar.JagdishbhaiVaghela@amd.com>
    Signed-off-by: Shubham Soni <shubham.manojsoni@amd.com>
    Signed-off-by: Akshay Belsare <akshay.belsare@amd.com>
    Acked-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

--- core/drivers/crypto/asu_driver/sub.mk
+++ core/drivers/crypto/asu_driver/sub.mk
@@ -8,3 +8,16 @@
 srcs-$(CFG_AMD_ASU_HASH) += asu_hash.c
 srcs-$(CFG_AMD_ASU_TRNG) += asu_trng.c
 srcs-$(CFG_AMD_ASU_HUK) += asu_huk.c
+srcs-$(CFG_AMD_ASU_ECC) += asu_ecc.c
+
+ifeq ($(CFG_AMD_ASU_ECC),y)
+# This curves configuration aligns enabled ECC/ECDH curves with ASU FW default
+# configurations. For the disabled curves, driver will use software fallback
+# operations. Make sure to configure ASU FW with enabled curves to prevent
+# any testcase failures
+CFG_AMD_ASU_ECC_CURVE_NIST_P192 ?= n
+CFG_AMD_ASU_ECC_CURVE_NIST_P224 ?= n
+CFG_AMD_ASU_ECC_CURVE_NIST_P256 ?= y
+CFG_AMD_ASU_ECC_CURVE_NIST_P384 ?= n
+CFG_AMD_ASU_ECC_CURVE_NIST_P521 ?= n
+endif
