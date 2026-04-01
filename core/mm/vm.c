OP-TEE/optee_os

commit 89c9728d981ff0f4a8edecc325858537441d721e

core: replace tee_mmu prefix with vm
Replaces the tee_mmu prefix with vm. tee_mmu.h is renamed to vm.h and
core/arch/arm/mm/tee_mmu.c is moved to core/mm/vm.c. Public functions
belonging to these files are renamed with a vm prefix.

Introduces: vm_map_param(), vm_clean_param(),
vm_buf_is_inside_private(), vm_buf_intersects_private(),
vm_buf_to_mboj_offs(), vm_buf_is_inside_um_private(),
vm_buf_intersects_um_private(), vm_add_rwmem(), vm_rem_rwmem(),
vm_va2pa(), vm_pa2va(), vm_check_access_rights(), vm_set_ctx() replacing
their tee_mmu_*() counterpart.

Acked-by: Joakim Bech <joakim.bech@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/mm/vm.c
@@ -19,9 +19,9 @@
 #include <mm/mobj.h>
 #include <mm/pgt_cache.h>
 #include <mm/tee_mm.h>
-#include <mm/tee_mmu.h>
 #include <mm/tee_mmu_types.h>
 #include <mm/tee_pager.h>
+#include <mm/vm.h>
 #include <sm/optee_smc.h>
 #include <stdlib.h>
 #include <tee_api_defines_extensions.h>
@@ -310,7 +310,7 @@ TEE_Result vm_map_pad(struct user_mode_ctx *uctx, vaddr_t *va, size_t len,
 	 * the mapping.
 	 */
 	if (thread_get_tsd()->ctx == uctx->ts_ctx)
-		tee_mmu_set_ctx(uctx->ts_ctx);
+		vm_set_ctx(uctx->ts_ctx);
 
 	*va = reg->va;
 
@@ -550,7 +550,7 @@ TEE_Result vm_remap(struct user_mode_ctx *uctx, vaddr_t *new_va, vaddr_t old_va,
 	 * Synchronize change to translation tables. Even though the pager
 	 * case unmaps immediately we may still free a translation table.
 	 */
-	tee_mmu_set_ctx(uctx->ts_ctx);
+	vm_set_ctx(uctx->ts_ctx);
 
 	r_first = TAILQ_FIRST(&regs);
 	while (!TAILQ_EMPTY(&regs)) {
@@ -603,7 +603,7 @@ TEE_Result vm_remap(struct user_mode_ctx *uctx, vaddr_t *new_va, vaddr_t old_va,
 
 	fobj_put(fobj);
 
-	tee_mmu_set_ctx(uctx->ts_ctx);
+	vm_set_ctx(uctx->ts_ctx);
 	*new_va = r_first->va;
 
 	return TEE_SUCCESS;
@@ -623,7 +623,7 @@ err_restore_map:
 			panic("Cannot restore mapping");
 	}
 	fobj_put(fobj);
-	tee_mmu_set_ctx(uctx->ts_ctx);
+	vm_set_ctx(uctx->ts_ctx);
 
 	return res;
 }
@@ -716,7 +716,7 @@ TEE_Result vm_set_prot(struct user_mode_ctx *uctx, vaddr_t va, size_t len,
 
 	if (need_sync) {
 		/* Synchronize changes to translation tables */
-		tee_mmu_set_ctx(uctx->ts_ctx);
+		vm_set_ctx(uctx->ts_ctx);
 	}
 
 	for (r = r0; r; r = TAILQ_NEXT(r, link)) {
@@ -787,7 +787,7 @@ TEE_Result vm_unmap(struct user_mode_ctx *uctx, vaddr_t va, size_t len)
 	 * Synchronize change to translation tables. Even though the pager
 	 * case unmaps immediately we may still free a translation table.
 	 */
-	tee_mmu_set_ctx(uctx->ts_ctx);
+	vm_set_ctx(uctx->ts_ctx);
 
 	return TEE_SUCCESS;
 }
@@ -836,7 +836,7 @@ TEE_Result vm_info_init(struct user_mode_ctx *uctx)
 	return res;
 }
 
-void tee_mmu_clean_param(struct user_mode_ctx *uctx)
+void vm_clean_param(struct user_mode_ctx *uctx)
 {
 	struct vm_region *next_r;
 	struct vm_region *r;
@@ -916,9 +916,8 @@ static int cmp_param_mem(const void *a0, const void *a1)
 	return CMP_TRILEAN(m0->size, m1->size);
 }
 
-TEE_Result tee_mmu_map_param(struct user_mode_ctx *uctx,
-			     struct tee_ta_param *param,
-			     void *param_va[TEE_NUM_PARAMS])
+TEE_Result vm_map_param(struct user_mode_ctx *uctx, struct tee_ta_param *param,
+			void *param_va[TEE_NUM_PARAMS])
 {
 	TEE_Result res = TEE_SUCCESS;
 	size_t n;
@@ -1012,13 +1011,13 @@ TEE_Result tee_mmu_map_param(struct user_mode_ctx *uctx,
 	res = alloc_pgt(uctx);
 out:
 	if (res)
-		tee_mmu_clean_param(uctx);
+		vm_clean_param(uctx);
 
 	return res;
 }
 
-TEE_Result tee_mmu_add_rwmem(struct user_mode_ctx *uctx, struct mobj *mobj,
-			     vaddr_t *va)
+TEE_Result vm_add_rwmem(struct user_mode_ctx *uctx, struct mobj *mobj,
+			vaddr_t *va)
 {
 	TEE_Result res;
 	struct vm_region *reg = calloc(1, sizeof(*reg));
@@ -1050,8 +1049,7 @@ TEE_Result tee_mmu_add_rwmem(struct user_mode_ctx *uctx, struct mobj *mobj,
 	return res;
 }
 
-void tee_mmu_rem_rwmem(struct user_mode_ctx *uctx, struct mobj *mobj,
-		       vaddr_t va)
+void vm_rem_rwmem(struct user_mode_ctx *uctx, struct mobj *mobj, vaddr_t va)
 {
 	struct vm_region *r = NULL;
 
@@ -1082,8 +1080,8 @@ void vm_info_final(struct user_mode_ctx *uctx)
 }
 
 /* return true only if buffer fits inside TA private memory */
-bool tee_mmu_is_vbuf_inside_um_private(const struct user_mode_ctx *uctx,
-				       const void *va, size_t size)
+bool vm_buf_is_inside_um_private(const struct user_mode_ctx *uctx,
+				 const void *va, size_t size)
 {
 	struct vm_region *r = NULL;
 
@@ -1098,8 +1096,8 @@ bool tee_mmu_is_vbuf_inside_um_private(const struct user_mode_ctx *uctx,
 }
 
 /* return true only if buffer intersects TA private memory */
-bool tee_mmu_is_vbuf_intersect_um_private(const struct user_mode_ctx *uctx,
-					  const void *va, size_t size)
+bool vm_buf_intersects_um_private(const struct user_mode_ctx *uctx,
+				  const void *va, size_t size)
 {
 	struct vm_region *r = NULL;
 
@@ -1113,9 +1111,9 @@ bool tee_mmu_is_vbuf_intersect_um_private(const struct user_mode_ctx *uctx,
 	return false;
 }
 
-TEE_Result tee_mmu_vbuf_to_mobj_offs(const struct user_mode_ctx *uctx,
-				     const void *va, size_t size,
-				     struct mobj **mobj, size_t *offs)
+TEE_Result vm_buf_to_mboj_offs(const struct user_mode_ctx *uctx,
+			       const void *va, size_t size,
+			       struct mobj **mobj, size_t *offs)
 {
 	struct vm_region *r = NULL;
 
@@ -1182,14 +1180,12 @@ static TEE_Result tee_mmu_user_va2pa_attr(const struct user_mode_ctx *uctx,
 	return TEE_ERROR_ACCESS_DENIED;
 }
 
-TEE_Result tee_mmu_user_va2pa_helper(const struct user_mode_ctx *uctx, void *ua,
-				     paddr_t *pa)
+TEE_Result vm_va2pa(const struct user_mode_ctx *uctx, void *ua, paddr_t *pa)
 {
 	return tee_mmu_user_va2pa_attr(uctx, ua, pa, NULL);
 }
 
-TEE_Result tee_mmu_user_pa2va_helper(const struct user_mode_ctx *uctx,
-				     paddr_t pa, void **va)
+TEE_Result vm_pa2va(const struct user_mode_ctx *uctx, paddr_t pa, void **va)
 {
 	TEE_Result res = TEE_SUCCESS;
 	paddr_t p = 0;
@@ -1238,9 +1234,8 @@ TEE_Result tee_mmu_user_pa2va_helper(const struct user_mode_ctx *uctx,
 	return TEE_ERROR_ACCESS_DENIED;
 }
 
-TEE_Result tee_mmu_check_access_rights(const struct user_mode_ctx *uctx,
-				       uint32_t flags, uaddr_t uaddr,
-				       size_t len)
+TEE_Result vm_check_access_rights(const struct user_mode_ctx *uctx,
+				  uint32_t flags, uaddr_t uaddr, size_t len)
 {
 	uaddr_t a = 0;
 	uaddr_t end_addr = 0;
@@ -1259,7 +1254,7 @@ TEE_Result tee_mmu_check_access_rights(const struct user_mode_ctx *uctx,
 	 * to TA or not.
 	 */
 	if (!(flags & TEE_MEMORY_ACCESS_ANY_OWNER) &&
-	   !tee_mmu_is_vbuf_inside_um_private(uctx, (void *)uaddr, len))
+	   !vm_buf_is_inside_um_private(uctx, (void *)uaddr, len))
 		return TEE_ERROR_ACCESS_DENIED;
 
 	for (a = ROUNDDOWN(uaddr, addr_incr); a < end_addr; a += addr_incr) {
@@ -1287,7 +1282,7 @@ TEE_Result tee_mmu_check_access_rights(const struct user_mode_ctx *uctx,
 	return TEE_SUCCESS;
 }
 
-void tee_mmu_set_ctx(struct ts_ctx *ctx)
+void vm_set_ctx(struct ts_ctx *ctx)
 {
 	struct thread_specific_data *tsd = thread_get_tsd();
 

core/arch/arm/kernel/secure_partition.c
@@ -11,7 +11,7 @@
 #include <kernel/user_mode_ctx.h>
 #include <mm/fobj.h>
 #include <mm/mobj.h>
-#include <mm/tee_mmu.h>
+#include <mm/vm.h>
 #include <pta_stmm.h>
 #include <tee_api_defines_extensions.h>
 #include <tee/tee_pobj.h>
@@ -200,7 +200,7 @@ static TEE_Result load_stmm(struct sec_part_ctx *spc)
 	stack_addr = heap_addr + stmm_heap_size;
 	sec_buf_addr = stack_addr + stmm_stack_size;
 
-	tee_mmu_set_ctx(&spc->ta_ctx.ts_ctx);
+	vm_set_ctx(&spc->ta_ctx.ts_ctx);
 	uncompress_image((void *)image_addr, stmm_image_uncompressed_size,
 			 stmm_image, stmm_image_size);
 
@@ -284,7 +284,7 @@ TEE_Result sec_part_init_session(const TEE_UUID *uuid,
 	ts_push_current_session(&sess->ts_sess);
 	res = load_stmm(spc);
 	ts_pop_current_session();
-	tee_mmu_set_ctx(NULL);
+	vm_set_ctx(NULL);
 	if (res) {
 		sess->ts_sess.ctx = NULL;
 		spc->ta_ctx.ts_ctx.ops->destroy(&spc->ta_ctx.ts_ctx);
@@ -551,10 +551,10 @@ static TEE_Result sec_storage_obj_read(unsigned long storage_id, char *obj_id,
 
 	sess = ts_get_current_session();
 	spc = to_sec_part_ctx(sess->ctx);
-	res = tee_mmu_check_access_rights(&spc->uctx,
-					  TEE_MEMORY_ACCESS_WRITE |
-					  TEE_MEMORY_ACCESS_ANY_OWNER,
-					  (uaddr_t)data, len);
+	res = vm_check_access_rights(&spc->uctx,
+				     TEE_MEMORY_ACCESS_WRITE |
+				     TEE_MEMORY_ACCESS_ANY_OWNER,
+				     (uaddr_t)data, len);
 	if (res != TEE_SUCCESS)
 		return res;
 
@@ -610,10 +610,10 @@ static TEE_Result sec_storage_obj_write(unsigned long storage_id, char *obj_id,
 
 	sess = ts_get_current_session();
 	spc = to_sec_part_ctx(sess->ctx);
-	res = tee_mmu_check_access_rights(&spc->uctx,
-					  TEE_MEMORY_ACCESS_READ |
-					  TEE_MEMORY_ACCESS_ANY_OWNER,
-					  (uaddr_t)data, len);
+	res = vm_check_access_rights(&spc->uctx,
+				     TEE_MEMORY_ACCESS_READ |
+				     TEE_MEMORY_ACCESS_ANY_OWNER,
+				     (uaddr_t)data, len);
 	if (res != TEE_SUCCESS)
 		return res;
 

core/arch/arm/kernel/thread.c
@@ -25,8 +25,8 @@
 #include <mm/core_memprot.h>
 #include <mm/mobj.h>
 #include <mm/tee_mm.h>
-#include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <mm/vm.h>
 #include <smccc.h>
 #include <sm/sm.h>
 #include <trace.h>

core/arch/arm/kernel/user_ta.c
@@ -27,8 +27,8 @@
 #include <mm/mobj.h>
 #include <mm/pgt_cache.h>
 #include <mm/tee_mm.h>
-#include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <mm/vm.h>
 #include <optee_rpc_cmd.h>
 #include <printk.h>
 #include <signed_hdr.h>
@@ -162,7 +162,7 @@ static TEE_Result user_ta_enter(struct ts_session *session,
 	}
 	if (ta_sess->param) {
 		/* Map user space memory */
-		res = tee_mmu_map_param(&utc->uctx, ta_sess->param, param_va);
+		res = vm_map_param(&utc->uctx, ta_sess->param, param_va);
 		if (res != TEE_SUCCESS)
 			goto out;
 	}
@@ -206,9 +206,9 @@ static TEE_Result user_ta_enter(struct ts_session *session,
 
 		/*
 		 * Clear out the parameter mappings added with
-		 * tee_mmu_map_param() above.
+		 * vm_clean_param() above.
 		 */
-		tee_mmu_clean_param(&utc->uctx);
+		vm_clean_param(&utc->uctx);
 	}
 
 
@@ -258,9 +258,10 @@ static TEE_Result init_with_ldelf(struct ts_session *sess __maybe_unused,
 		return res;
 	}
 
-	res = tee_mmu_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ |
-					  TEE_MEMORY_ACCESS_ANY_OWNER,
-					  (uaddr_t)arg, sizeof(*arg));
+	res = vm_check_access_rights(&utc->uctx,
+				     TEE_MEMORY_ACCESS_READ |
+				     TEE_MEMORY_ACCESS_ANY_OWNER,
+				     (uaddr_t)arg, sizeof(*arg));
 	if (res)
 		return res;
 
@@ -327,9 +328,10 @@ static TEE_Result dump_state_ldelf_dbg(struct user_ta_ctx *utc)
 			     STACK_ALIGNMENT);
 	arg = (struct dump_entry_arg *)usr_stack;
 
-	res = tee_mmu_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ |
-					  TEE_MEMORY_ACCESS_ANY_OWNER,
-					  (uaddr_t)arg, sizeof(*arg));
+	res = vm_check_access_rights(&utc->uctx,
+				     TEE_MEMORY_ACCESS_READ |
+				     TEE_MEMORY_ACCESS_ANY_OWNER,
+				     (uaddr_t)arg, sizeof(*arg));
 	if (res) {
 		EMSG("ldelf stack is inaccessible!");
 		return res;
@@ -457,9 +459,10 @@ static TEE_Result dump_ftrace(struct user_ta_ctx *utc, void *buf, size_t *blen)
 	usr_stack -= ROUNDUP(sizeof(*arg), STACK_ALIGNMENT);
 	arg = (size_t *)usr_stack;
 
-	res = tee_mmu_check_access_rights(&utc->uctx, TEE_MEMORY_ACCESS_READ |
-					  TEE_MEMORY_ACCESS_ANY_OWNER,
-					  (uaddr_t)arg, sizeof(*arg));
+	res = vm_check_access_rights(&utc->uctx,
+				     TEE_MEMORY_ACCESS_READ |
+				     TEE_MEMORY_ACCESS_ANY_OWNER,
+				     (uaddr_t)arg, sizeof(*arg));
 	if (res) {
 		EMSG("ldelf stack is inaccessible!");
 		return res;
@@ -702,7 +705,7 @@ static TEE_Result load_ldelf(struct user_ta_ctx *utc)
 	if (res)
 		return res;
 
-	tee_mmu_set_ctx(&utc->ta_ctx.ts_ctx);
+	vm_set_ctx(&utc->ta_ctx.ts_ctx);
 
 	memcpy((void *)code_addr, ldelf_data, ldelf_code_size);
 	memcpy((void *)rw_addr, ldelf_data + ldelf_code_size, ldelf_data_size);

core/arch/arm/mm/core_mmu.c
@@ -8,8 +8,8 @@
 #include <assert.h>
 #include <bitstring.h>
 #include <config.h>
-#include <kernel/cache_helpers.h>
 #include <kernel/boot.h>
+#include <kernel/cache_helpers.h>
 #include <kernel/linker.h>
 #include <kernel/panic.h>
 #include <kernel/spinlock.h>
@@ -25,8 +25,8 @@
 #include <mm/core_mmu.h>
 #include <mm/mobj.h>
 #include <mm/pgt_cache.h>
-#include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <mm/vm.h>
 #include <platform_config.h>
 #include <stdlib.h>
 #include <trace.h>
@@ -2022,7 +2022,6 @@ static void check_pa_matches_va(void *va, paddr_t pa)
 	vaddr_t v = (vaddr_t)va;
 	paddr_t p = 0;
 	struct core_mmu_table_info ti __maybe_unused = { };
-	struct user_mode_ctx *uctx = NULL;
 
 	if (core_mmu_user_va_range_is_defined()) {
 		vaddr_t user_va_base;
@@ -2037,8 +2036,8 @@ static void check_pa_matches_va(void *va, paddr_t pa)
 				return;
 			}
 
-			uctx = to_user_mode_ctx(thread_get_tsd()->ctx);
-			res = tee_mmu_user_va2pa_helper(uctx, va, &p);
+			res = vm_va2pa(to_user_mode_ctx(thread_get_tsd()->ctx),
+				       va, &p);
 			if (res == TEE_SUCCESS && pa != p)
 				panic("bad pa");
 			if (res != TEE_SUCCESS && pa)
@@ -2136,8 +2135,7 @@ static void *phys_to_virt_ta_vaspace(paddr_t pa)
 	if (!core_mmu_user_mapping_is_active())
 		return NULL;
 
-	res = tee_mmu_user_pa2va_helper(to_user_mode_ctx(thread_get_tsd()->ctx),
-					pa, &va);
+	res = vm_pa2va(to_user_mode_ctx(thread_get_tsd()->ctx), pa, &va);
 	if (res != TEE_SUCCESS)
 		return NULL;
 	return va;

core/arch/arm/mm/mobj.c
@@ -14,8 +14,8 @@
 #include <kernel/tee_misc.h>
 #include <mm/core_mmu.h>
 #include <mm/mobj.h>
-#include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <mm/vm.h>
 #include <optee_msg.h>
 #include <sm/optee_smc.h>
 #include <stdlib.h>
@@ -451,7 +451,7 @@ static void mobj_seccpy_shm_free(struct mobj *mobj)
 	struct mobj_seccpy_shm *m = to_mobj_seccpy_shm(mobj);
 
 	tee_pager_rem_um_region(&m->utc->uctx, m->va, mobj->size);
-	tee_mmu_rem_rwmem(&m->utc->uctx, mobj, m->va);
+	vm_rem_rwmem(&m->utc->uctx, mobj, m->va);
 	fobj_put(m->fobj);
 	free(m);
 }
@@ -492,7 +492,7 @@ struct mobj *mobj_seccpy_shm_alloc(size_t size)
 	m->mobj.ops = &mobj_seccpy_shm_ops;
 	refcount_set(&m->mobj.refc, 1);
 
-	if (tee_mmu_add_rwmem(&utc->uctx, &m->mobj, &va) != TEE_SUCCESS)
+	if (vm_add_rwmem(&utc->uctx, &m->mobj, &va) != TEE_SUCCESS)
 		goto bad;
 
 	m->fobj = fobj_rw_paged_alloc(ROUNDUP(size, SMALL_PAGE_SIZE) /
@@ -506,7 +506,7 @@ struct mobj *mobj_seccpy_shm_alloc(size_t size)
 	return &m->mobj;
 bad:
 	if (va)
-		tee_mmu_rem_rwmem(&utc->uctx, &m->mobj, va);
+		vm_rem_rwmem(&utc->uctx, &m->mobj, va);
 	fobj_put(m->fobj);
 	free(m);
 	return NULL;

core/arch/arm/mm/mobj_dyn_shm.c
@@ -14,8 +14,8 @@
 #include <kernel/tee_misc.h>
 #include <mm/core_mmu.h>
 #include <mm/mobj.h>
-#include <mm/tee_mmu.h>
 #include <mm/tee_pager.h>
+#include <mm/vm.h>
 #include <optee_msg.h>
 #include <sm/optee_smc.h>
 #include <stdlib.h>

core/arch/arm/mm/sub.mk
@@ -1,6 +1,5 @@
 srcs-y += core_mmu.c
 srcs-$(CFG_WITH_PAGER) += tee_pager.c
-srcs-y += tee_mmu.c
 ifeq ($(CFG_WITH_LPAE),y)
 srcs-y += core_mmu_lpae.c
 else

commit 60367377f1459470026aa8a89e98e5d60d94b064

core: vm_buf_intersects_um_private(): fix flag check
vm_buf_intersects_um_private() checks if a memory range intersects with
already registered memory ranges with the flag VM_FLAGS_NONPRIV set. However,
until now, the function was checking the flag against the attr field instead
of the correct flags field. In practise this results in the function always
returning false. Fix this by checking against the correct field.

Fixes: 89c9728d981f ("core: replace tee_mmu prefix with vm")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>
Acked-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

core/mm/vm.c
@@ -1117,7 +1117,7 @@ bool vm_buf_intersects_um_private(const struct user_mode_ctx *uctx,
 	struct vm_region *r = NULL;
 
 	TAILQ_FOREACH(r, &uctx->vm_info.regions, link) {
-		if (r->attr & VM_FLAGS_NONPRIV)
+		if (r->flags & VM_FLAGS_NONPRIV)
 			continue;
 		if (core_is_buffer_intersect((vaddr_t)va, size, r->va, r->size))
 			return true;