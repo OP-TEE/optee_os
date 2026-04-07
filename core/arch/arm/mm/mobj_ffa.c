OP-TEE/optee_os

commit 003383344c26be3589383acc87c1ebb2860e9317

core: support dynamic protected memory lending
With CFG_CORE_DYN_PROTMEM=y support dynamic protected memory lending.

A new internal struct mobj_ffa_rsm is added to handle dynamic protected
memory for FF-A.

A new internal struct mobj_protmem is add to handle dynamic protected
memory without FF-A.

Lending non-secure memory to OP-TEE to use it as protected memory means
that it should to become inaccessible by the normal world as part of the
process. This part is currently not supported, since it must be done in
a platform specific way for platforms that support that. QEMU don't
support that.

Adding two platform specific functions, plat_get_protmem_config() and
plat_set_protmem_range() for dynamic protected memory. The functions has
__weak implementation to allow easier testing. However,
plat_set_protmem_range() requires CFG_INSECURE=y since it doesn't change
memory protection.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/mm/mobj_ffa.c
@@ -10,6 +10,7 @@
 #include <initcall.h>
 #include <kernel/refcount.h>
 #include <kernel/spinlock.h>
+#include <kernel/tee_misc.h>
 #include <kernel/thread_spmc.h>
 #include <kernel/virtualization.h>
 #include <mm/mobj.h>
@@ -92,6 +93,13 @@ struct mobj_ffa_shm {
 	paddr_t pages[];
 };
 
+struct mobj_ffa_prm {
+	struct mobj_ffa mf;
+	paddr_t pa;
+	enum mobj_use_case use_case;
+	bool assigned_use_case;
+};
+
 SLIST_HEAD(mobj_ffa_head, mobj_ffa);
 
 #ifdef CFG_CORE_SEL1_SPMC
@@ -117,8 +125,9 @@ static struct mobj_ffa_head shm_inactive_head =
 static unsigned int shm_lock = SPINLOCK_UNLOCK;
 
 static const struct mobj_ops mobj_ffa_shm_ops;
+static const struct mobj_ops mobj_ffa_prm_ops;
 
-static bool __maybe_unused is_mobj_ffa_shm(struct mobj *mobj)
+static bool is_mobj_ffa_shm(struct mobj *mobj)
 {
 	return mobj->ops == &mobj_ffa_shm_ops;
 }
@@ -127,6 +136,17 @@ static struct mobj_ffa_shm *to_mobj_ffa_shm(struct mobj *mobj)
 {
 	assert(is_mobj_ffa_shm(mobj));
 	return container_of(mobj, struct mobj_ffa_shm, mf.mobj);
+}
+
+static bool is_mobj_ffa_prm(struct mobj *mobj)
+{
+	return mobj->ops == &mobj_ffa_prm_ops;
+}
+
+static struct mobj_ffa_prm *to_mobj_ffa_prm(struct mobj *mobj)
+{
+	assert(is_mobj_ffa_prm(mobj));
+	return container_of(mobj, struct mobj_ffa_prm, mf.mobj);
 }
 
 static size_t shm_size(size_t num_pages)
@@ -140,7 +160,7 @@ static size_t shm_size(size_t num_pages)
 	return s;
 }
 
-static struct mobj_ffa_shm *ffa_shm_new(unsigned int num_pages)
+static struct mobj_ffa *ffa_shm_new(unsigned int num_pages)
 {
 	struct mobj_ffa_shm *m = NULL;
 	size_t s = 0;
@@ -161,14 +181,39 @@ static struct mobj_ffa_shm *ffa_shm_new(unsigned int num_pages)
 	refcount_set(&m->mf.mobj.refc, 0);
 	m->mf.inactive_refs = 0;
 
-	return m;
+	return &m->mf;
+}
+
+static struct mobj_ffa *ffa_prm_new(unsigned int num_pages,
+				    enum mobj_use_case use_case)
+{
+	struct mobj_ffa_prm *m = NULL;
+	size_t sz = 0;
+
+	if (!num_pages || MUL_OVERFLOW(num_pages, SMALL_PAGE_SIZE, &sz) ||
+	    use_case == MOBJ_USE_CASE_NS_SHM)
+		return NULL;
+
+	m = calloc(1, sizeof(*m));
+	if (!m)
+		return NULL;
+
+	m->mf.mobj.ops = &mobj_ffa_prm_ops;
+	m->mf.mobj.size = sz;
+	m->mf.mobj.phys_granule = SMALL_PAGE_SIZE;
+	refcount_set(&m->mf.mobj.refc, 0);
+	m->mf.inactive_refs = 0;
+	m->use_case = use_case;
+
+	return &m->mf;
 }
 
 #ifdef CFG_CORE_SEL1_SPMC
 struct mobj_ffa *mobj_ffa_sel1_spmc_new(uint64_t cookie,
-					unsigned int num_pages)
+					unsigned int num_pages,
+					enum mobj_use_case use_case)
 {
-	struct mobj_ffa_shm *m = NULL;
+	struct mobj_ffa *m = NULL;
 	bitstr_t *shm_bits = NULL;
 	uint32_t exceptions = 0;
 	int i = 0;
@@ -180,7 +225,17 @@ struct mobj_ffa *mobj_ffa_sel1_spmc_new(uint64_t cookie,
 			return NULL;
 	}
 
-	m = ffa_shm_new(num_pages);
+	switch (use_case) {
+	case MOBJ_USE_CASE_NS_SHM:
+		m = ffa_shm_new(num_pages);
+		break;
+	case MOBJ_USE_CASE_SEC_VIDEO_PLAY:
+	case MOBJ_USE_CASE_TRUSED_UI:
+		m = ffa_prm_new(num_pages, use_case);
+		break;
+	default:
+		break;
+	}
 	if (!m) {
 		if (cookie != OPTEE_MSG_FMEM_INVALID_GLOBAL_ID)
 			virt_remove_cookie(cookie);
@@ -188,8 +243,8 @@ struct mobj_ffa *mobj_ffa_sel1_spmc_new(uint64_t cookie,
 	}
 
 	if (cookie != OPTEE_MSG_FMEM_INVALID_GLOBAL_ID) {
-		m->mf.cookie = cookie;
-		return &m->mf;
+		m->cookie = cookie;
+		return m;
 	}
 
 	shm_bits = get_shm_bits();
@@ -197,23 +252,23 @@ struct mobj_ffa *mobj_ffa_sel1_spmc_new(uint64_t cookie,
 	bit_ffc(shm_bits, SPMC_CORE_SEL1_MAX_SHM_COUNT, &i);
 	if (i != -1) {
 		bit_set(shm_bits, i);
-		m->mf.cookie = i;
-		m->mf.cookie |= FFA_MEMORY_HANDLE_NON_SECURE_BIT;
+		m->cookie = i;
+		m->cookie |= FFA_MEMORY_HANDLE_NON_SECURE_BIT;
 		/*
 		 * Encode the partition ID into the handle so we know which
 		 * partition to switch to when reclaiming a handle.
 		 */
-		m->mf.cookie |= SHIFT_U64(virt_get_current_guest_id(),
-					  FFA_MEMORY_HANDLE_PRTN_SHIFT);
+		m->cookie |= SHIFT_U64(virt_get_current_guest_id(),
+				       FFA_MEMORY_HANDLE_PRTN_SHIFT);
 	}
 	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
 
 	if (i == -1) {
-		free(m);
+		mobj_ffa_sel1_spmc_delete(m);
 		return NULL;
 	}
 
-	return &m->mf;
+	return m;
 }
 #endif /*CFG_CORE_SEL1_SPMC*/
 
@@ -230,6 +285,47 @@ static bool cmp_cookie(struct mobj_ffa *mf, uint64_t cookie)
 static bool cmp_ptr(struct mobj_ffa *mf, uint64_t ptr)
 {
 	return mf == (void *)(vaddr_t)ptr;
+}
+
+static bool check_shm_overlaps_prm(struct mobj_ffa_shm *shm,
+				   struct mobj_ffa_prm *prm)
+{
+	size_t n = 0;
+
+	for (n = 0; n < shm->mf.mobj.size / SMALL_PAGE_SIZE; n++)
+		if (core_is_buffer_intersect(prm->pa, prm->mf.mobj.size,
+					     shm->pages[n], SMALL_PAGE_SIZE))
+			return true;
+
+	return false;
+}
+
+static bool cmp_pa_overlap(struct mobj_ffa *mf, uint64_t ptr)
+{
+	struct mobj_ffa *mf2 = (void *)(vaddr_t)ptr;
+	bool mf_is_shm = is_mobj_ffa_shm(&mf->mobj);
+	bool mf2_is_shm = is_mobj_ffa_shm(&mf2->mobj);
+
+	if (mf_is_shm && mf2_is_shm) {
+		/*
+		 * Not a security issue and might be too expensive to check
+		 * if we have many pages in each registered shared memory
+		 * object.
+		 */
+		return false;
+	}
+
+	if (mf_is_shm)
+		return check_shm_overlaps_prm(to_mobj_ffa_shm(&mf->mobj),
+					      to_mobj_ffa_prm(&mf2->mobj));
+	if (mf2_is_shm)
+		return check_shm_overlaps_prm(to_mobj_ffa_shm(&mf2->mobj),
+					      to_mobj_ffa_prm(&mf->mobj));
+
+	return core_is_buffer_intersect(to_mobj_ffa_prm(&mf->mobj)->pa,
+					mf->mobj.size,
+					to_mobj_ffa_prm(&mf2->mobj)->pa,
+					mf2->mobj.size);
 }
 
 static struct mobj_ffa *pop_from_list(struct mobj_ffa_head *head,
@@ -277,8 +373,6 @@ static struct mobj_ffa *find_in_list(struct mobj_ffa_head *head,
 #if defined(CFG_CORE_SEL1_SPMC)
 void mobj_ffa_sel1_spmc_delete(struct mobj_ffa *mf)
 {
-	struct mobj_ffa_shm *m = NULL;
-
 	if (!IS_ENABLED(CFG_NS_VIRTUALIZATION) ||
 	    !(mf->cookie & FFA_MEMORY_HANDLE_HYPERVISOR_BIT)) {
 		uint64_t mask = FFA_MEMORY_HANDLE_NON_SECURE_BIT;
@@ -298,33 +392,44 @@ void mobj_ffa_sel1_spmc_delete(struct mobj_ffa *mf)
 		cpu_spin_unlock_xrestore(&shm_lock, exceptions);
 	}
 
-	m = to_mobj_ffa_shm(&mf->mobj);
-	assert(!m->mm);
-	free(m);
+	if (is_mobj_ffa_shm(&mf->mobj)) {
+		struct mobj_ffa_shm *m = to_mobj_ffa_shm(&mf->mobj);
+
+		assert(!m->mm);
+		free(m);
+	} else {
+		free(to_mobj_ffa_prm(&mf->mobj));
+	}
 }
 #else /* !defined(CFG_CORE_SEL1_SPMC) */
-struct mobj_ffa *mobj_ffa_spmc_new(uint64_t cookie, unsigned int num_pages)
+struct mobj_ffa *mobj_ffa_spmc_new(uint64_t cookie, unsigned int num_pages,
+				   enum mobj_use_case use_case)
 {
-	struct mobj_ffa_shm *m = NULL;
+	struct mobj_ffa *mf = NULL;
 
 	assert(cookie != OPTEE_MSG_FMEM_INVALID_GLOBAL_ID);
-	m = ffa_shm_new(num_pages);
-	if (m)
-		m->mf.cookie = cookie;
-	return &m->mf;
+	if (use_case == MOBJ_USE_CASE_NS_SHM)
+		mf = ffa_shm_new(num_pages);
+	else
+		mf = ffa_prm_new(num_pages, use_case);
+	if (mf)
+		mf->cookie = cookie;
+	return mf;
 }
 
 void mobj_ffa_spmc_delete(struct mobj_ffa *mf)
 {
-	free(to_mobj_ffa_shm(&mf->mobj));
+	if (is_mobj_ffa_shm(&mf->mobj))
+		free(to_mobj_ffa_shm(&mf->mobj));
+	else
+		free(to_mobj_ffa_prm(&mf->mobj));
 }
 #endif /* !defined(CFG_CORE_SEL1_SPMC) */
 
 TEE_Result mobj_ffa_add_pages_at(struct mobj_ffa *mf, unsigned int *idx,
 				 paddr_t pa, unsigned int num_pages)
 {
-	struct mobj_ffa_shm *mfs = to_mobj_ffa_shm(&mf->mobj);
-	size_t tot_page_count = get_page_count(mf);
+	size_t tot_page_count = tot_page_count = get_page_count(mf);
 	unsigned int n = 0;
 
 	if (ADD_OVERFLOW(*idx, num_pages, &n) || n > tot_page_count)
@@ -334,10 +439,22 @@ TEE_Result mobj_ffa_add_pages_at(struct mobj_ffa *mf, unsigned int *idx,
 	    !core_pbuf_is(CORE_MEM_NON_SEC, pa, num_pages * SMALL_PAGE_SIZE))
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	for (n = 0; n < num_pages; n++)
-		mfs->pages[n + *idx] = pa + n * SMALL_PAGE_SIZE;
+	if (is_mobj_ffa_shm(&mf->mobj)) {
+		struct mobj_ffa_shm *mfs = to_mobj_ffa_shm(&mf->mobj);
+
+		for (n = 0; n < num_pages; n++)
+			mfs->pages[n + *idx] = pa + n * SMALL_PAGE_SIZE;
+	} else {
+		struct mobj_ffa_prm *mfr = to_mobj_ffa_prm(&mf->mobj);
+
+		if (!*idx)
+			mfr->pa = pa;
+		else if (mfr->pa != pa + *idx * SMALL_PAGE_SIZE)
+			return TEE_ERROR_BAD_PARAMETERS;
+	}
 
 	(*idx) += n;
+
 	return TEE_SUCCESS;
 }
 
@@ -346,18 +463,50 @@ uint64_t mobj_ffa_get_cookie(struct mobj_ffa *mf)
 	return mf->cookie;
 }
 
-uint64_t mobj_ffa_push_to_inactive(struct mobj_ffa *mf)
+static TEE_Result protect_mem(struct mobj_ffa_prm *m)
 {
+	DMSG("use_case %d pa %#"PRIxPA", size %#zx cookie %#"PRIx64,
+	     m->use_case, m->pa, m->mf.mobj.size, m->mf.cookie);
+
+	return plat_set_protmem_range(m->use_case, m->pa, m->mf.mobj.size);
+}
+
+static TEE_Result __maybe_unused restore_mem(struct mobj_ffa_prm *m)
+{
+	DMSG("use_case %d pa %#" PRIxPA ", size %#zx cookie %#"PRIx64,
+	     m->use_case, m->pa, m->mf.mobj.size, m->mf.cookie);
+
+	return plat_set_protmem_range(MOBJ_USE_CASE_NS_SHM, m->pa,
+				      m->mf.mobj.size);
+}
+
+TEE_Result mobj_ffa_push_to_inactive(struct mobj_ffa *mf)
+{
+	TEE_Result res = TEE_SUCCESS;
 	uint32_t exceptions = 0;
 
 	exceptions = cpu_spin_lock_xsave(&shm_lock);
 	assert(!find_in_list(&shm_inactive_head, cmp_ptr, (vaddr_t)mf));
 	assert(!find_in_list(&shm_inactive_head, cmp_cookie, mf->cookie));
 	assert(!find_in_list(&shm_head, cmp_cookie, mf->cookie));
+
+	if (find_in_list(&shm_inactive_head, cmp_pa_overlap, (vaddr_t)mf) ||
+	    find_in_list(&shm_head, cmp_pa_overlap, (vaddr_t)mf)) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+	if (is_mobj_ffa_prm(&mf->mobj)) {
+		res = protect_mem(to_mobj_ffa_prm(&mf->mobj));
+		if (res)
+			goto out;
+	}
+
 	SLIST_INSERT_HEAD(&shm_inactive_head, mf, link);
+
+out:
 	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
 
-	return mf->cookie;
+	return res;
 }
 
 static void unmap_helper(struct mobj_ffa_shm *m)
@@ -408,7 +557,10 @@ TEE_Result mobj_ffa_sel1_spmc_reclaim(uint64_t cookie)
 
 	if (!pop_from_list(&shm_inactive_head, cmp_ptr, (vaddr_t)mf))
 		panic();
-	res = TEE_SUCCESS;
+	if (is_mobj_ffa_prm(&mf->mobj))
+		res = restore_mem(to_mobj_ffa_prm(&mf->mobj));
+	else
+		res = TEE_SUCCESS;
 out:
 	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
 	if (!res) {
@@ -497,8 +649,10 @@ struct mobj *mobj_ffa_get_by_cookie(uint64_t cookie,
 	exceptions = cpu_spin_lock_xsave(&shm_lock);
 	mf = find_in_list(&shm_head, cmp_cookie, cookie);
 	if (mf) {
-		mfs = to_mobj_ffa_shm(&mf->mobj);
-		offs = mfs->page_offset;
+		if (is_mobj_ffa_shm(&mf->mobj))
+			offs = to_mobj_ffa_shm(&mf->mobj)->page_offset;
+		else
+			offs = 0;
 		if (offs == internal_offs) {
 			if (!refcount_inc(&mf->mobj.refc)) {
 				/*
@@ -525,9 +679,11 @@ struct mobj *mobj_ffa_get_by_cookie(uint64_t cookie,
 		if (mf) {
 			DMSG("cookie %#"PRIx64" resurrecting", cookie);
 		} else {
+			enum mobj_use_case uc = MOBJ_USE_CASE_NS_SHM;
+
 			DMSG("Populating mobj from rx buffer, cookie %#"PRIx64,
 			     cookie);
-			mf = thread_spmc_populate_mobj_from_rx(cookie);
+			mf = thread_spmc_populate_mobj_from_rx(cookie, uc);
 		}
 #endif
 		if (mf) {
@@ -540,26 +696,33 @@ struct mobj *mobj_ffa_get_by_cookie(uint64_t cookie,
 			assert(refcount_val(&mf->mobj.refc) == 0);
 			refcount_set(&mf->mobj.refc, 1);
 			mf->inactive_refs++;
-			mfs = to_mobj_ffa_shm(&mf->mobj);
-			refcount_set(&mfs->mapcount, 0);
+			if (is_mobj_ffa_shm(&mf->mobj)) {
+				mfs = to_mobj_ffa_shm(&mf->mobj);
+				refcount_set(&mfs->mapcount, 0);
 
-			/*
-			 * mf->page_offset is offset into the first page.
-			 * This offset is assigned from the internal_offs
-			 * parameter to this function.
-			 *
-			 * While a mobj_ffa is active (ref_count > 0) this
-			 * will not change, but when being pushed to the
-			 * inactive list it can be changed again.
-			 *
-			 * So below we're backing out the old
-			 * mf->page_offset and then assigning a new from
-			 * internal_offset.
-			 */
-			mf->mobj.size += mfs->page_offset;
-			assert(!(mf->mobj.size & SMALL_PAGE_MASK));
-			mf->mobj.size -= internal_offs;
-			mfs->page_offset = internal_offs;
+				/*
+				 * mfs->page_offset is offset into the
+				 * first page.  This offset is assigned
+				 * from the internal_offs parameter to this
+				 * function.
+				 *
+				 * While a mobj_ffa is active (ref_count >
+				 * 0) this will not change, but when being
+				 * pushed to the inactive list it can be
+				 * changed again.
+				 *
+				 * So below we're backing out the old
+				 * mfs->page_offset and then assigning a
+				 * new from internal_offset.
+				 */
+				mf->mobj.size += mfs->page_offset;
+				assert(!(mf->mobj.size & SMALL_PAGE_MASK));
+				mf->mobj.size -= internal_offs;
+				mfs->page_offset = internal_offs;
+			} else if (is_mobj_ffa_prm(&mf->mobj) &&
+				   internal_offs) {
+				mf = NULL;
+			}
 
 			SLIST_INSERT_HEAD(&shm_head, mf, link);
 		}
@@ -623,9 +786,8 @@ static void *ffa_shm_get_va(struct mobj *mobj, size_t offset, size_t len)
 	return (void *)(tee_mm_get_smem(m->mm) + offset + m->page_offset);
 }
 
-static void ffa_shm_inactivate(struct mobj *mobj)
+static void ffa_inactivate(struct mobj_ffa *mf)
 {
-	struct mobj_ffa_shm *m = to_mobj_ffa_shm(mobj);
 	uint32_t exceptions = 0;
 
 	exceptions = cpu_spin_lock_xsave(&shm_lock);
@@ -634,8 +796,8 @@ static void ffa_shm_inactivate(struct mobj *mobj)
 	 * shm_head after the mobj_put() that put us here and before we got
 	 * the lock.
 	 */
-	if (refcount_val(&mobj->refc)) {
-		DMSG("cookie %#"PRIx64" was resurrected", m->mf.cookie);
+	if (refcount_val(&mf->mobj.refc)) {
+		DMSG("cookie %#"PRIx64" was resurrected", mf->cookie);
 		goto out;
 	}
 
@@ -650,15 +812,21 @@ static void ffa_shm_inactivate(struct mobj *mobj)
 	 * that the mobj can't be freed until it reaches 0.
 	 * At this point the mobj is in the inactive list.
 	 */
-	if (pop_from_list(&shm_head, cmp_ptr, (vaddr_t)&m->mf)) {
-		unmap_helper(m);
-		SLIST_INSERT_HEAD(&shm_inactive_head, &m->mf, link);
+	if (pop_from_list(&shm_head, cmp_ptr, (vaddr_t)mf)) {
+		if (is_mobj_ffa_shm(&mf->mobj))
+			unmap_helper(to_mobj_ffa_shm(&mf->mobj));
+		SLIST_INSERT_HEAD(&shm_inactive_head, mf, link);
 	}
 out:
-	if (!m->mf.inactive_refs)
+	if (!mf->inactive_refs)
 		panic();
-	m->mf.inactive_refs--;
+	mf->inactive_refs--;
 	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
+}
+
+static void ffa_shm_inactivate(struct mobj *mobj)
+{
+	ffa_inactivate(&to_mobj_ffa_shm(mobj)->mf);
 }
 
 static TEE_Result ffa_shm_get_mem_type(struct mobj *mobj __unused, uint32_t *mt)
@@ -783,3 +951,162 @@ static const struct mobj_ops mobj_ffa_shm_ops = {
 };
 
 preinit(mapped_shm_init);
+
+#ifdef CFG_CORE_DYN_PROTMEM
+static TEE_Result ffa_prm_get_pa(struct mobj *mobj, size_t offset,
+				 size_t granule, paddr_t *pa)
+{
+	struct mobj_ffa_prm *m = to_mobj_ffa_prm(mobj);
+	paddr_t p;
+
+	if (!pa || offset >= mobj->size)
+		return TEE_ERROR_GENERIC;
+
+	p = m->pa + offset;
+
+	if (granule) {
+		if (granule != SMALL_PAGE_SIZE &&
+		    granule != CORE_MMU_PGDIR_SIZE)
+			return TEE_ERROR_GENERIC;
+		p &= ~(granule - 1);
+	}
+
+	*pa = p;
+	return TEE_SUCCESS;
+}
+
+static TEE_Result ffa_prm_get_mem_type(struct mobj *mobj __maybe_unused,
+				       uint32_t *mt)
+{
+	assert(is_mobj_ffa_prm(mobj));
+
+	if (!mt)
+		return TEE_ERROR_GENERIC;
+
+	*mt = TEE_MATTR_MEM_TYPE_CACHED;
+
+	return TEE_SUCCESS;
+}
+
+static bool ffa_prm_matches(struct mobj *mobj __maybe_unused,
+			    enum buf_is_attr attr)
+{
+	assert(is_mobj_ffa_prm(mobj));
+
+	return attr == CORE_MEM_SEC || attr == CORE_MEM_SDP_MEM;
+}
+
+static void ffa_prm_inactivate(struct mobj *mobj)
+{
+	ffa_inactivate(&to_mobj_ffa_prm(mobj)->mf);
+}
+
+static uint64_t ffa_prm_get_cookie(struct mobj *mobj)
+{
+	return to_mobj_ffa_prm(mobj)->mf.cookie;
+}
+
+static TEE_Result ffa_prm_no_map(struct mobj *mobj __maybe_unused)
+{
+	assert(is_mobj_ffa_prm(mobj));
+
+	return TEE_ERROR_GENERIC;
+}
+
+static const struct mobj_ops mobj_ffa_prm_ops = {
+	.get_pa = ffa_prm_get_pa,
+	.get_mem_type = ffa_prm_get_mem_type,
+	.matches = ffa_prm_matches,
+	.free = ffa_prm_inactivate,
+	.get_cookie = ffa_prm_get_cookie,
+	.inc_map = ffa_prm_no_map,
+	.dec_map = ffa_prm_no_map,
+};
+
+static bool cmp_protmem_pa(struct mobj_ffa *mf, uint64_t pa)
+{
+	struct mobj_ffa_prm *m = NULL;
+
+	if (!is_mobj_ffa_prm(&mf->mobj))
+		return false;
+
+	m = to_mobj_ffa_prm(&mf->mobj);
+	return pa >= m->pa && pa < m->pa + m->mf.mobj.size;
+}
+
+struct mobj *mobj_ffa_protmem_get_by_pa(paddr_t pa, paddr_size_t size)
+{
+	struct mobj_ffa_prm *m = NULL;
+	struct mobj_ffa *mf = NULL;
+	struct mobj *mobj = NULL;
+	uint32_t exceptions = 0;
+
+	if (!size)
+		size = 1;
+
+	exceptions = cpu_spin_lock_xsave(&shm_lock);
+
+	mf = find_in_list(&shm_head, cmp_protmem_pa, pa);
+	if (mf) {
+		m = to_mobj_ffa_prm(&mf->mobj);
+		if (core_is_buffer_inside(pa, size, m->pa, m->mf.mobj.size))
+			mobj = mobj_get(&mf->mobj);
+	}
+
+	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
+	return mobj;
+}
+
+TEE_Result mobj_ffa_assign_protmem(uint64_t cookie, enum mobj_use_case use_case)
+{
+	TEE_Result res = TEE_SUCCESS;
+	struct mobj_ffa_prm *m = NULL;
+	struct mobj_ffa *mf = NULL;
+	uint32_t exceptions = 0;
+
+	exceptions = cpu_spin_lock_xsave(&shm_lock);
+	mf = find_in_list(&shm_inactive_head, cmp_cookie, cookie);
+	if (mf) {
+		if (!is_mobj_ffa_prm(&mf->mobj)) {
+			res = TEE_ERROR_ITEM_NOT_FOUND;
+			goto out;
+		}
+		m = to_mobj_ffa_prm(&mf->mobj);
+		if (m->assigned_use_case) {
+			res = TEE_ERROR_BUSY;
+			goto out;
+		}
+		if (m->use_case != use_case) {
+			res = TEE_ERROR_BAD_PARAMETERS;
+			goto out;
+		}
+		m->assigned_use_case = true;
+		goto out;
+	}
+	mf = find_in_list(&shm_head, cmp_cookie, cookie);
+	if (mf) {
+		if (!is_mobj_ffa_prm(&mf->mobj))
+			res = TEE_ERROR_BUSY;
+		else
+			res = TEE_ERROR_ITEM_NOT_FOUND;
+		goto out;
+	}
+#if !defined(CFG_CORE_SEL1_SPMC)
+	/* Try to retrieve it from the SPM at S-EL2 */
+	DMSG("Populating mobj from rx buffer, cookie %#"PRIx64" use-case %d",
+	     cookie, use_case);
+	mf = thread_spmc_populate_mobj_from_rx(cookie, use_case);
+	if (mf) {
+		SLIST_INSERT_HEAD(&shm_inactive_head, mf, link);
+	} else {
+		EMSG("Failed to assign use-case %d to cookie %#"PRIx64"",
+		     use_case, cookie);
+		res = TEE_ERROR_ITEM_NOT_FOUND;
+		goto out;
+	}
+#endif
+out:
+	cpu_spin_unlock_xrestore(&shm_lock, exceptions);
+	return res;
+}
+#endif /*CFG_CORE_DYN_PROTMEM*/

core/arch/arm/include/ffa.h
@@ -161,6 +161,8 @@
 
 /* Share memory transaction */
 #define FFA_MEMORY_REGION_TRANSACTION_TYPE_SHARE SHIFT_U32(1, 3)
+/* Lend memory transaction */
+#define FFA_MEMORY_REGION_TRANSACTION_TYPE_LEND SHIFT_U32(2, 3)
 /* Relayer must choose the alignment boundary */
 #define FFA_MEMORY_REGION_FLAG_ANY_ALIGNMENT	0
 

core/arch/arm/include/kernel/thread_spmc.h
@@ -10,6 +10,7 @@
 #include <ffa.h>
 #include <kernel/panic.h>
 #include <kernel/thread.h>
+#include <mm/mobj.h>
 #include <sys/queue.h>
 #include <tee_api_types.h>
 #include <types_ext.h>
@@ -74,8 +75,26 @@ thread_spmc_set_async_notif_intid(int intid __unused)
 {
 	panic();
 }
-struct mobj_ffa *thread_spmc_populate_mobj_from_rx(uint64_t cookie);
+
+struct mobj_ffa *thread_spmc_populate_mobj_from_rx(uint64_t cookie,
+						   enum mobj_use_case use_case);
 void thread_spmc_relinquish(uint64_t memory_region_handle);
 #endif
+
+#if defined(CFG_CORE_DYN_PROTMEM) && defined(CFG_CORE_FFA)
+TEE_Result thread_spmc_get_protmem_config(enum mobj_use_case use_case,
+					  void *buf, size_t *buf_sz,
+					  size_t *min_mem_sz,
+					  size_t *min_mem_align);
+#else
+static inline TEE_Result
+thread_spmc_get_protmem_config(enum mobj_use_case use_case __unused,
+			       void *buf __unused, size_t *buf_sz __unused,
+			       size_t *min_mem_sz __unused,
+			       size_t *min_mem_align __unused)
+{
+	return TEE_ERROR_NOT_SUPPORTED;
+}
+#endif /*CFG_CORE_DYN_PROTMEM*/
 
 #endif /* __KERNEL_THREAD_SPMC_H */

commit 0082bc3e83dcf8f4f0615169b6b776386642f5cd

core: ffa: fix memory range tracking in mobj_ffa_add_pages_at()
In mobj_ffa_add_pages_at(), the PA check for protected memory checked
the wrong offset and idx was incremented by the wrong variable. Fix both
for correct validation.

Fixes: 003383344c26 ("core: support dynamic protected memory lending")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>

core/arch/arm/mm/mobj_ffa.c
@@ -429,7 +429,7 @@ void mobj_ffa_spmc_delete(struct mobj_ffa *mf)
 TEE_Result mobj_ffa_add_pages_at(struct mobj_ffa *mf, unsigned int *idx,
 				 paddr_t pa, unsigned int num_pages)
 {
-	size_t tot_page_count = tot_page_count = get_page_count(mf);
+	size_t tot_page_count = get_page_count(mf);
 	unsigned int n = 0;
 
 	if (ADD_OVERFLOW(*idx, num_pages, &n) || n > tot_page_count)
@@ -449,11 +449,11 @@ TEE_Result mobj_ffa_add_pages_at(struct mobj_ffa *mf, unsigned int *idx,
 
 		if (!*idx)
 			mfr->pa = pa;
-		else if (mfr->pa != pa + *idx * SMALL_PAGE_SIZE)
+		else if (pa != mfr->pa + *idx * SMALL_PAGE_SIZE)
 			return TEE_ERROR_BAD_PARAMETERS;
 	}
 
-	(*idx) += n;
+	(*idx) += num_pages;
 
 	return TEE_SUCCESS;
 }