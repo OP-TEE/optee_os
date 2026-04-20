OP-TEE/optee_os

commit 653409a236196e56e3204db4f00b312171ed1fdd

core: pta: add helper functions to support calls from TA when CFG_PAN=y
Introduce helper function to make it easier to use bounce buffers in a
PTA entry point to support calls with memrefs from user space TAs when
PAN is enabled.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/kernel/pseudo_ta.c
@@ -9,6 +9,7 @@
 #include <kernel/panic.h>
 #include <kernel/pseudo_ta.h>
 #include <kernel/tee_ta_manager.h>
+#include <kernel/user_access.h>
 #include <mm/core_memprot.h>
 #include <mm/mobj.h>
 #include <stdlib.h>
@@ -326,4 +327,96 @@ TEE_Result tee_ta_init_pseudo_ta_session(const TEE_UUID *uuid,
 	DMSG("%s : %pUl", stc->pseudo_ta->name, (void *)&ctx->ts_ctx.uuid);
 
 	return TEE_SUCCESS;
+}
+
+TEE_Result to_bounce_params(uint32_t param_types,
+			    TEE_Param params[TEE_NUM_PARAMS],
+			    TEE_Param bparams[TEE_NUM_PARAMS],
+			    TEE_Param **oparams)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	void *kptr = NULL;
+	void *uptr = NULL;
+	size_t size = 0;
+	int i = 0;
+
+	if (!is_caller_ta_with_pan()) {
+		*oparams = params;
+		return TEE_SUCCESS;
+	}
+
+	for (i = 0; i < TEE_NUM_PARAMS; i++) {
+		switch (TEE_PARAM_TYPE_GET(param_types, i)) {
+		case TEE_PARAM_TYPE_MEMREF_INPUT:
+		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
+		case TEE_PARAM_TYPE_MEMREF_INOUT:
+			size = params[i].memref.size;
+			uptr = params[i].memref.buffer;
+			kptr = bb_alloc(size);
+			if (!kptr)
+				return TEE_ERROR_OUT_OF_MEMORY;
+			bparams[i].memref.buffer = kptr;
+			bparams[i].memref.size = size;
+			break;
+		default:
+			break;
+		}
+		switch (TEE_PARAM_TYPE_GET(param_types, i)) {
+		case TEE_PARAM_TYPE_MEMREF_INPUT:
+		case TEE_PARAM_TYPE_MEMREF_INOUT:
+			res = copy_from_user(kptr, uptr, size);
+			if (res)
+				return res;
+			break;
+		case TEE_PARAM_TYPE_VALUE_INPUT:
+		case TEE_PARAM_TYPE_VALUE_INOUT:
+			bparams[i].value.a = params[i].value.a;
+			bparams[i].value.b = params[i].value.b;
+			break;
+		default:
+			break;
+		}
+	}
+	*oparams = bparams;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result from_bounce_params(uint32_t param_types,
+			      TEE_Param params[TEE_NUM_PARAMS],
+			      TEE_Param bparams[TEE_NUM_PARAMS],
+			      TEE_Param *eparams)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	void *kptr = NULL;
+	void *uptr = NULL;
+	size_t size = 0;
+	int i = 0;
+
+	if (eparams == params)
+		return TEE_SUCCESS;
+
+	for (i = 0; i < TEE_NUM_PARAMS; i++) {
+		switch (TEE_PARAM_TYPE_GET(param_types, i)) {
+		case TEE_PARAM_TYPE_MEMREF_OUTPUT:
+		case TEE_PARAM_TYPE_MEMREF_INOUT:
+			uptr = params[i].memref.buffer;
+			kptr = bparams[i].memref.buffer;
+			size = bparams[i].memref.size;
+			res = copy_to_user(uptr, kptr, size);
+			if (res)
+				return res;
+			params[i].memref.size = size;
+			break;
+		case TEE_PARAM_TYPE_VALUE_OUTPUT:
+		case TEE_PARAM_TYPE_VALUE_INOUT:
+			params[i].value.a = bparams[i].value.a;
+			params[i].value.b = bparams[i].value.b;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return res;
 }

core/include/kernel/pseudo_ta.h
@@ -7,7 +7,9 @@
 
 #include <assert.h>
 #include <compiler.h>
+#include <config.h>
 #include <kernel/tee_ta_manager.h>
+#include <kernel/user_ta.h>
 #include <scattered_array.h>
 #include <tee_api_types.h>
 #include <user_ta_header.h>
@@ -59,6 +61,39 @@ static inline struct pseudo_ta_ctx *to_pseudo_ta_ctx(struct ts_ctx *ctx)
 
 TEE_Result tee_ta_init_pseudo_ta_session(const TEE_UUID *uuid,
 			struct tee_ta_session *s);
+
+/*
+ * Helper functions for PTAs to support calls from a TA when CFG_PAN=y
+ */
+
+static inline bool is_caller_ta_with_pan(void)
+{
+	struct ts_session *s = NULL;
+
+	if (!IS_ENABLED(CFG_PAN))
+		return false;
+	s = ts_get_calling_session();
+	return s && is_user_ta_ctx(s->ctx);
+}
+
+/*
+ * If caller is a TA and PAN is enabled, allocate bounce buffers for each
+ * memref in @params and build @bparams, then make *@oparams point to @bparams.
+ * Otherwise just make *@oparams point to @params.
+ */
+TEE_Result to_bounce_params(uint32_t param_types,
+			    TEE_Param params[TEE_NUM_PARAMS],
+			    TEE_Param bparams[TEE_NUM_PARAMS],
+			    TEE_Param **oparams);
+
+/*
+ * If @eparams == @bparams, copy data from @bparams to @params. Otherwise, do
+ * nothing.
+ */
+TEE_Result from_bounce_params(uint32_t param_types,
+			      TEE_Param params[TEE_NUM_PARAMS],
+			      TEE_Param bparams[TEE_NUM_PARAMS],
+			      TEE_Param *eparams);
 
 #endif /* KERNEL_PSEUDO_TA_H */
 

commit b226369a1f63a74aa222b717fd3cf4c087669bad

core: pseudo_ta: fix spurious error from from_bounce_params()
from_bounce_params() initializes res to TEE_ERROR_GENERIC and only
updates it when a MEMREF_OUTPUT or MEMREF_INOUT parameter is processed
via copy_to_user(). For any other parameter combination - value outputs,
input-only, or no params - the loop completes without touching res and
the function returns TEE_ERROR_GENERIC even though everything succeeded.

This causes pseudo-TA invocations from TAs on PAN-enabled hardware to
fail spuriously when the call has no memory reference output parameters.

The same function to_bounce_params() in the same file correctly ends
with return TEE_SUCCESS, making the asymmetry a straightforward mistake.

Fix by explicitly returning TEE_SUCCESS at the end of the function.

Fixes: 653409a23619 ("core: pta: add helper functions to support calls from TA when CFG_PAN=y")
Signed-off-by: Ashutosh Desai <ashutoshdesai993@gmail.com>

core/kernel/pseudo_ta.c
@@ -421,5 +421,5 @@ TEE_Result from_bounce_params(uint32_t param_types,
 		}
 	}
 
-	return res;
+	return TEE_SUCCESS;
 }