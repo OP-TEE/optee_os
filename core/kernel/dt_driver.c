OP-TEE/optee_os

commit 50dd2af06ae420fe19ee027d21d8282a2578509f

core: dt_driver: add helper for old fashion interrupt bindings
Adds a helper function dt_driver_device_from_node_idx_prop_phandle()
in device tree driver probing framework for when a DT node property
contains a resource references but not the related device phandle as
first property cell, as for property "interrupts" which should get the
interrupt controller phandle from property "interrupt-parent". This
change aims at supporting "interrupts" property DT bindings.

Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

core/kernel/dt_driver.c
@@ -292,6 +292,47 @@ void *dt_driver_device_from_parent(const void *fdt, int nodeoffset,
 	return device_from_provider_prop(prv, fdt, nodeoffset, NULL, res);
 }
 
+void *dt_driver_device_from_node_idx_prop_phandle(const char *prop_name,
+						  const void *fdt, int nodeoffs,
+						  unsigned int prop_index,
+						  enum dt_driver_type type,
+						  uint32_t phandle,
+						  TEE_Result *res)
+{
+	int len = 0;
+	const uint32_t *prop = NULL;
+	int phandle_node_unused = -1;
+	struct dt_driver_provider *prv = NULL;
+
+	prop = fdt_getprop(fdt, nodeoffs, prop_name, &len);
+	if (!prop) {
+		if (len != -FDT_ERR_NOTFOUND) {
+			DMSG("Corrupted node %s", prop_name);
+			*res = TEE_ERROR_GENERIC;
+		} else {
+			DMSG("Property %s missing in node %s", prop_name,
+			     fdt_get_name(fdt, nodeoffs, NULL));
+			*res = TEE_ERROR_ITEM_NOT_FOUND;
+		}
+		return NULL;
+	}
+
+	prv = dt_driver_get_provider_by_phandle(phandle, type);
+	if (!prv) {
+		*res = TEE_ERROR_DEFER_DRIVER_INIT;
+		return NULL;
+	}
+
+	prop_index *= dt_driver_provider_cells(prv);
+	if ((prop_index + 1) * sizeof(*prop) > (size_t)len) {
+		*res = TEE_ERROR_ITEM_NOT_FOUND;
+		return NULL;
+	}
+
+	return device_from_provider_prop(prv, fdt, phandle_node_unused,
+					 prop + prop_index, res);
+}
+
 void *dt_driver_device_from_node_idx_prop(const char *prop_name,
 					  const void *fdt, int nodeoffset,
 					  unsigned int prop_idx,

core/include/kernel/dt_driver.h
@@ -172,6 +172,22 @@ void *dt_driver_device_from_parent(const void *fdt, int nodeoffset,
 				   enum dt_driver_type type, TEE_Result *res);
 
 /*
+ * dt_driver_device_from_node_idx_prop_phandle() - Same as
+ *	dt_driver_device_from_node_idx_prop() but phandle is not the first
+ *	cells in property @prop_name but is passed as an argument.
+ *
+ * This function is used for DT bindings as "interrupts" property where the
+ * property carries the interrupt information but not the interrupt controller
+ * phandle which is found in a specific property (here "interrupt-parent").
+ */
+void *dt_driver_device_from_node_idx_prop_phandle(const char *prop_name,
+						  const void *fdt, int nodeoffs,
+						  unsigned int prop_index,
+						  enum dt_driver_type type,
+						  uint32_t phandle,
+						  TEE_Result *res);
+
+/*
  * dt_driver_get_crypto() - Request crypto support for driver initialization
  *
  * Return TEE_SUCCESS if cryptography services are initialized, otherwise return

commit c39a968d4158e128fb224d0d670cb86db6e2dad6

core: dt_driver: fix under-counted bounds check in prop_phandle helper
dt_driver_device_from_node_idx_prop_phandle() scales prop_index to a
cell offset by multiplying by provider_cells, then checks that only
(prop_index + 1) cells fit within the property buffer. However,
device_from_provider_prop() subsequently reads provider_cells values
starting at prop + prop_index, not just one cell.

For any provider with more than one cell (e.g. an interrupt controller
with #interrupt-cells = 2 or 3) the bounds check is insufficient and
the subsequent read in device_from_provider_prop() can access memory
beyond the end of the property buffer.

Fix by checking (prop_index + dt_driver_provider_cells(prv)) cells
rather than (prop_index + 1).

Fixes: 50dd2af06ae4 ("core: dt_driver: add helper for old fashion interrupt bindings")
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Ashutosh Desai <ashutoshdesai993@gmail.com>

core/kernel/dt_driver.c
@@ -338,7 +338,8 @@ TEE_Result dt_driver_device_from_node_idx_prop_phandle(const char *prop_name,
 		return TEE_ERROR_DEFER_DRIVER_INIT;
 
 	prop_index *= dt_driver_provider_cells(prv);
-	if ((prop_index + 1) * sizeof(*prop) > (size_t)len)
+	if ((prop_index + dt_driver_provider_cells(prv)) *
+	    sizeof(*prop) > (size_t)len)
 		return TEE_ERROR_ITEM_NOT_FOUND;
 
 	phandle_node = fdt_node_offset_by_phandle(fdt, phandle);