0e164c01845876dfb88950dfa2438f979f970e33
Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
2026-05-07 11:31:22 +0200

    drivers: versal_net: pki: Implement FIPS186-5 rejection sampling on key generation
    FIPS186-5 basically offers two options when it comes to private key
    generation:
    
    1. Acquire a random number at least 64 bits larger/wider than the prime
       order of the curve (521 bit in case of P-521) and do additional
       "mod" math on that wider number to end up in a valid key
       ("extra bits" method).
    
    Or
    
    2. Loop over potentially multiple random numbers of the same width as the
       prime order of the curve until one fits certain conditions
       ("testing candidates" or "rejection sampling" method).
    
    Neither method was fully implemented in the previous version of the PKI
    driver. This commit chooses the second method, due to limitations of the
    PKI Accelerator's "modular reduction" operator (supporting odd modulus
    only).
    
    Signed-off-by: Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
    Reviewed-by: Nathan Menhorn <nathan.menhorn@amd.com>
    Acked-by: Jens Wiklander <jenswi@kernel.org>

--- core/drivers/crypto/versal/pki/ecc_pki_engine.c
+++ core/drivers/crypto/versal/pki/ecc_pki_engine.c
@@ -216,6 +216,35 @@ static struct versal_pki versal_pki;
 
 #define PKI_RESET_DELAY_US		10
 
+/*
+ * Helper function to check if the large number 'val1' is greater than or
+ * equal to 'val2'. Both numbers are assumed to be 'len' bytes long and
+ * formatted in little-endian byte order.
+ */
+static bool pki_largenum_is_ge_le(const uint8_t *val1, const uint8_t *val2,
+				  size_t len)
+{
+	for (ssize_t i = len - 1; i >= 0; i--) {
+		if (val1[i] > val2[i])
+			return true;
+		if (val1[i] < val2[i])
+			return false;
+	}
+	return true;
+}
+
+/*
+ * Helper function to check if the large number 'val', which is assumed to be
+ * 'len' bytes long, is 0.
+ */
+static bool pki_largenum_is_zero(const uint8_t *val, size_t len)
+{
+	for (size_t idx = 0; idx < len; idx++)
+		if (val[idx])
+			return false;
+	return true;
+}
+
 static TEE_Result pki_get_opsize(uint32_t curve, uint32_t op, size_t *in_sz,
 				 size_t *out_sz)
 {
@@ -702,43 +731,59 @@ static TEE_Result versal_ecc_gen_private_key(uint32_t curve, uint8_t *priv,
 
 	/* Copy curve order N */
 	memcpy(addr, order, bytes);
-	addr += bytes;
 
-	/* Copy A = random */
-	ret = versal_ecc_trng_get_random_bytes(addr, bytes);
-	if (ret)
-		return ret;
-	addr += bytes;
+	while (true) {
+		addr = versal_pki.rq_in + bytes;
 
-	/* Copy B = 1 */
-	addr[0] = 1;
-	memset(addr + 1, 0, bytes - 1);
-	addr += bytes;
+		/* Copy A = random */
+		ret = versal_ecc_trng_get_random_bytes(addr, bytes);
+		if (ret)
+			return ret;
 
-	if (curve == TEE_ECC_CURVE_NIST_P521) {
-		memset(addr, 0, PKI_SIGN_P521_PADD_BYTES);
-		addr += PKI_SIGN_P521_PADD_BYTES;
+		if (curve == TEE_ECC_CURVE_NIST_P521)
+			addr[bytes - 1] &= 0x01;
+
+		/* rejections sampling */
+		if (pki_largenum_is_ge_le(addr, order, bytes))
+			continue;
+
+		addr += bytes;
+
+		/* Copy B = 1 */
+		addr[0] = 1;
+		memset(addr + 1, 0, bytes - 1);
+		addr += bytes;
+
+		if (curve == TEE_ECC_CURVE_NIST_P521) {
+			memset(addr, 0, PKI_SIGN_P521_PADD_BYTES);
+			addr += PKI_SIGN_P521_PADD_BYTES;
+		}
+
+		/* Build descriptors */
+		if (!IS_ALIGNED_WITH_TYPE(addr, uint64_t))
+			return TEE_ERROR_BAD_PARAMETERS;
+
+		ret = pki_build_descriptors(curve, PKI_DESC_OPTYPE_MOD_ADD,
+					    (void *)addr);
+		if (ret)
+			return ret;
+
+		/* Use PKI engine to compute A+B mod N */
+		ret = pki_start_operation(PKI_NEW_REQUEST_MASK & (vaddr_t)addr);
+		if (ret)
+			return ret;
+
+		ret = pki_check_status();
+		if (ret)
+			return ret;
+
+		cache_operation(TEE_CACHEFLUSH, versal_pki.rq_out,
+				PKI_QUEUE_BUF_SIZE);
+
+		/* rejections sampling */
+		if (!pki_largenum_is_zero(versal_pki.rq_out, bytes))
+			break;
 	}
-
-	/* Build descriptors */
-	if (!IS_ALIGNED_WITH_TYPE(addr, uint64_t))
-		return TEE_ERROR_BAD_PARAMETERS;
-
-	ret = pki_build_descriptors(curve, PKI_DESC_OPTYPE_MOD_ADD,
-				    (void *)addr);
-	if (ret)
-		return ret;
-
-	/* Use PKI engine to compute A+B mod N */
-	ret = pki_start_operation(PKI_NEW_REQUEST_MASK & (vaddr_t)addr);
-	if (ret)
-		return ret;
-
-	ret = pki_check_status();
-	if (ret)
-		return ret;
-
-	cache_operation(TEE_CACHEFLUSH, versal_pki.rq_out, PKI_QUEUE_BUF_SIZE);
 
 	/* Copy back result */
 	memcpy(priv, versal_pki.rq_out, bytes);
