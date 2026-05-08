b29376404c0e3bc110db8c0c43ec66fa4cbbe6e4
Ralph Siemsen <ralph.siemsen@linaro.org>
2026-05-07 22:09:13 -0400

    drivers: eip76d_trng: move into core/drivers
    Commit 5b6c7df764e5 ("plat-k3: drivers: Refactor SA2UL driver")
    made the eip76_trng driver independent of SA2UL/SA3UL.
    
    Move the driver from core/arch/arm/plat-k3 into core/drivers,
    so that it can be used on other platforms.
    
    Signed-off-by: Ralph Siemsen <ralph.siemsen@linaro.org>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -23,6 +23,7 @@ srcs-$(CFG_HI16XX_RNG) += hi16xx_rng.c
 srcs-$(CFG_LPC_UART) += lpc_uart.c
 srcs-$(CFG_SCIF) += scif.c
 srcs-$(CFG_DRA7_RNG) += dra7_rng.c
+srcs-$(CFG_EIP76D_TRNG) += eip76d_trng.c
 srcs-$(CFG_STIH_UART) += stih_asc.c
 srcs-$(CFG_ATMEL_UART) += atmel_uart.c
 srcs-$(CFG_ATMEL_TRNG) += atmel_trng.c
