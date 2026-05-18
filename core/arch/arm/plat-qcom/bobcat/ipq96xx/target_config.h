d11bc38a89c404cc7399fddafb5730a1618c6441
Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
2026-05-18 13:16:06 +0530

    plat-qcom: Add BOBCAT architecture with IPQ96xx platform support
    Introduce support for the BOBCAT chipset family, starting with the
    IPQ96xx platform. This extends the architecture-based hierarchy
    previously established for HOYA to accommodate the new family.
    
    BOBCAT Architecture Framework:
    The conf.mk is updated to detect BOBCAT chipsets alongside HOYA and
    load the appropriate architecture and chipset configurations using
    the same variable-based include mechanism. Architecture-level
    configuration common to all BOBCAT chipsets is placed in
    bobcat/arch_config.h, while per-chipset differences are captured in
    chipset-specific target.mk and target_config.h files.
    
    IPQ96xx Platform Configuration:
    IPQ96xx is a 5-core SoC (4x Cortex-A55 + 1x Cortex-A78) with the
    following configuration:
    - 5 CPU cores with GICv3 interrupt controller
    - TZDRAM at 0x8A680000 (2.5MB)
    - Dual DRAM regions (2GB + 14GB)
    - GENI UART at 0x01A98000
    
    Signed-off-by: Harikrishna <hart@qti.qualcomm.com>
    Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

---
+++ core/arch/arm/plat-qcom/bobcat/ipq96xx/target_config.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef TARGET_CONFIG_H
+#define TARGET_CONFIG_H
+
+#define GENI_UART_REG_BASE		UL(0x1A98000)
+
+#define DRAM0_BASE			UL(0x80000000)
+#define DRAM0_SIZE			UL(0x80000000)
+#define DRAM1_BASE			ULL(0x800000000)
+#define DRAM1_SIZE			ULL(0x380000000)
+
+#define GICD_BASE			UL(0xF200000)
+#define GICR_BASE			UL(0xF240000)
+
+#endif /* TARGET_CONFIG_H */
