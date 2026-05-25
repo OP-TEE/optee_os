OP-TEE/optee_os

commit b65298cd84133f509e015ff1ea9b82e9998ec53d

core: ffa: Deny unsupported memory sharing operations
Fragmented memory sharing operations and memory sharing where the
transaction descriptors are forwarded in a custom buffer are not
supported for SP destinations. Return early FFA_ERROR if these
conditions are detected.
Add CFG_NS_VIRTUALIZATION condition for virt_unset_guest calls in
thread_spmc.c as a minor refactoring step.

Signed-off-by: Imre Kis <imre.kis@arm.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/kernel/thread_spmc.c
@@ -869,7 +869,8 @@ static void handle_direct_request(struct thread_smc_args *args,
 	 * thread cannot be allocated or found. virt_unset_guest() is also
 	 * called from thread_state_suspend() and thread_state_free().
 	 */
-	virt_unset_guest();
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		virt_unset_guest();
 }
 
 int spmc_read_mem_transaction(uint32_t ffa_vers, void *buf, size_t blen,
@@ -1184,13 +1185,27 @@ static int handle_mem_share_tmem(paddr_t pbuf, size_t blen, size_t flen,
 
 	cpu_spin_lock(&rxtx->spinlock);
 	rc = spmc_read_mem_transaction(rxtx->ffa_vers, buf, flen, &mem_trans);
-	if (!rc && IS_ENABLED(CFG_NS_VIRTUALIZATION) &&
-	    virt_set_guest(mem_trans.sender_id))
+	if (rc)
+		goto unlock;
+
+	if (is_sp_share(&mem_trans, buf)) {
+		rc = spmc_sp_add_share(&mem_trans, buf, blen, flen,
+				       global_handle, NULL);
+		goto unlock;
+	}
+
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION) &&
+	    virt_set_guest(mem_trans.sender_id)) {
 		rc = FFA_DENIED;
-	if (!rc)
-		rc = add_mem_share(&mem_trans, mm, buf, blen, flen,
-				   global_handle);
-	virt_unset_guest();
+		goto unlock;
+	}
+
+	rc = add_mem_share(&mem_trans, mm, buf, blen, flen, global_handle);
+
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		virt_unset_guest();
+
+unlock:
 	cpu_spin_unlock(&rxtx->spinlock);
 	if (rc > 0)
 		return rc;
@@ -1218,7 +1233,7 @@ static int handle_mem_share_rxbuf(size_t blen, size_t flen,
 	if (rc)
 		goto out;
 	if (is_sp_share(&mem_trans, rxtx->rx)) {
-		rc = spmc_sp_add_share(&mem_trans, rxtx, blen,
+		rc = spmc_sp_add_share(&mem_trans, rxtx, blen, flen,
 				       global_handle, NULL);
 		goto out;
 	}
@@ -1230,7 +1245,8 @@ static int handle_mem_share_rxbuf(size_t blen, size_t flen,
 	rc = add_mem_share(&mem_trans, NULL, rxtx->rx, blen, flen,
 			   global_handle);
 
-	virt_unset_guest();
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		virt_unset_guest();
 
 out:
 	cpu_spin_unlock(&rxtx->spinlock);
@@ -1358,7 +1374,9 @@ static void handle_mem_frag_tx(struct thread_smc_args *args,
 
 	rc = add_mem_share_frag(s, buf, flen);
 out:
-	virt_unset_guest();
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		virt_unset_guest();
+
 	cpu_spin_unlock(&rxtx->spinlock);
 
 	if (rc <= 0 && mm) {
@@ -1424,7 +1442,8 @@ static void handle_mem_reclaim(struct thread_smc_args *args)
 		break;
 	}
 
-	virt_unset_guest();
+	if (IS_ENABLED(CFG_NS_VIRTUALIZATION))
+		virt_unset_guest();
 
 out:
 	set_simple_ret_val(args, rc);

core/arch/arm/include/kernel/spmc_sp_handler.h
@@ -23,7 +23,7 @@ bool ffa_mem_reclaim(struct thread_smc_args *args,
 #ifdef CFG_SECURE_PARTITION
 void spmc_sp_start_thread(struct thread_smc_args *args);
 int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
-		      struct ffa_rxtx *rxtx, size_t blen,
+		      struct ffa_rxtx *rxtx, size_t blen, size_t flen,
 		      uint64_t *global_handle, struct sp_session *owner_sp);
 void spmc_sp_set_to_preempted(struct ts_session *ts_sess);
 int spmc_sp_resume_from_preempted(uint16_t endpoint_id);
@@ -35,7 +35,7 @@ static inline void spmc_sp_start_thread(struct thread_smc_args *args __unused)
 static inline int
 spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans __unused,
 		  struct ffa_rxtx *rxtx __unused, size_t blen __unused,
-		  uint64_t *global_handle __unused,
+		  size_t flen __unused, uint64_t *global_handle __unused,
 		  struct sp_session *owner_sp __unused)
 {
 	return FFA_NOT_SUPPORTED;

core/arch/arm/kernel/spmc_sp_handler.c
@@ -142,8 +142,8 @@ static void spmc_sp_handle_mem_share(struct thread_smc_args *args,
 
 	cpu_spin_lock(&rxtx->spinlock);
 
-	/* Descriptor fragments aren't supported yet. */
-	if (frag_len != tot_len)
+	/* Descriptor fragments or custom buffers aren't supported yet. */
+	if (frag_len != tot_len || args->a3 || args->a4)
 		res = FFA_NOT_SUPPORTED;
 	else if (frag_len > rxtx->size)
 		res = FFA_INVALID_PARAMETERS;
@@ -151,7 +151,7 @@ static void spmc_sp_handle_mem_share(struct thread_smc_args *args,
 		res = spmc_read_mem_transaction(rxtx->ffa_vers, rxtx->rx,
 						frag_len, &mem_trans);
 	if (!res)
-		res = spmc_sp_add_share(&mem_trans, rxtx, tot_len,
+		res = spmc_sp_add_share(&mem_trans, rxtx, tot_len, frag_len,
 					&global_handle, owner_sp);
 	if (!res) {
 		args->a3 = high32_from_64(global_handle);
@@ -286,7 +286,7 @@ clean_up:
 }
 
 int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
-		      struct ffa_rxtx *rxtx, size_t blen,
+		      struct ffa_rxtx *rxtx, size_t blen, size_t flen,
 		      uint64_t *global_handle, struct sp_session *owner_sp)
 {
 	int res = FFA_INVALID_PARAMETERS;
@@ -297,9 +297,15 @@ int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
 	size_t addr_range_offs = 0;
 	struct ffa_mem_region *mem_reg = NULL;
 	uint8_t highest_permission = 0;
-	struct sp_mem *smem = sp_mem_new();
+	struct sp_mem *smem = NULL;
 	uint16_t sender_id = mem_trans->sender_id;
 
+	if (blen != flen) {
+		DMSG("Fragmented memory share is not supported for SPs");
+		return FFA_NOT_SUPPORTED;
+	}
+
+	smem = sp_mem_new();
 	if (!smem)
 		return FFA_NO_MEMORY;
 

commit d0751c71b368b37dd2c0b9cee80c8d8b079ff61a

core: ffa: deny dynamic memory sharing to S-EL0 SPs
Deny sharing memory with S-EL0 SPs using a dynamically allocated buffer.
This avoids a bug where a owner allocated buffer was passed instead of
struct ffa_rxtx pointer, possibly leading to crash in OP-TEE core at
S-EL1.

Fixes: b65298cd8413 ("core: ffa: Deny unsupported memory sharing operations")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@arm.com>

core/arch/arm/kernel/thread_spmc.c
@@ -1298,12 +1298,7 @@ static int handle_mem_op_tmem(bool share_mem, paddr_t pbuf, size_t tot_len,
 		goto unlock;
 
 	if (is_sp_op(&mem_trans, buf)) {
-		if (!share_mem) {
-			rc = FFA_DENIED;
-			goto unlock;
-		}
-		rc = spmc_sp_add_share(&mem_trans, buf, tot_len, frag_len,
-				       global_handle, NULL);
+		rc = FFA_INVALID_PARAMETERS;
 		goto unlock;
 	}
 