d643ed16787c6593e420796d2cd3b1b4f37069a7
Vitor Sato Eschholz <vsatoes@baylibre.com>
2026-05-29 11:32:22 +0200

    plat: rcar: prepare support of new gen
    Support for the next generation of Renesas automotive chips is coming.
    
    To prepare the support of different number of cores and a different
    memory layout, place related configs inside a generation specif block.
    
    No behavior change is expected with this commit.
    
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- core/arch/arm/plat-rcar/conf.mk
+++ core/arch/arm/plat-rcar/conf.mk
@@ -7,8 +7,6 @@ $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_SCIF,y)
 $(call force,CFG_GIC,y)
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
-$(call force,CFG_CORE_ARM64_PA_BITS,36)
-$(call force,CFG_TEE_CORE_NB_CORE,8)
 $(call force,CFG_ARM64_core,y)
 $(call force,CFG_WITH_LPAE,y)
 
@@ -18,13 +16,15 @@ else
 $(call force,CFG_RCAR_GEN3, y)
 endif
 
-CFG_TZDRAM_START ?= 0x44100000
-CFG_TZDRAM_SIZE	 ?= 0x03D00000
-CFG_TEE_RAM_VA_SIZE ?= 0x100000
 supported-ta-targets = ta_arm64
 
 ifeq ($(CFG_RCAR_GEN3), y)
+CFG_TZDRAM_START ?= 0x44100000
+CFG_TZDRAM_SIZE  ?= 0x03D00000
+CFG_TEE_RAM_VA_SIZE ?= 0x100000
 CFG_DT ?= y
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
+$(call force,CFG_TEE_CORE_NB_CORE,8)
 ifeq ($(CFG_RCAR_GEN3_HWRNG), y)
 $(warning "Warning: Use of HWRNG can cause crashes on some Renesas SoCs")
 CFG_WITH_SOFTWARE_PRNG ?= n
@@ -35,10 +35,14 @@ endif
 endif
 
 ifeq ($(CFG_RCAR_GEN4), y)
+CFG_TZDRAM_START ?= 0x44100000
+CFG_TZDRAM_SIZE  ?= 0x2200000
+CFG_TEE_RAM_VA_SIZE ?= 0x100000
 # 1xx - for SCIFxx
 # 2xx - for HSCIFxx
-CFG_TZDRAM_SIZE	= 0x2200000
 CFG_RCAR_UART ?= 200
+$(call force,CFG_CORE_ARM64_PA_BITS,36)
+$(call force,CFG_TEE_CORE_NB_CORE,8)
 $(call force,CFG_RCAR_ROMAPI, n)
 $(call force,CFG_CORE_CLUSTER_SHIFT, 1)
 $(call force,CFG_ARM_GICV3, y)
