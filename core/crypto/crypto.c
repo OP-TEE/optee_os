OP-TEE/optee_os

commit 543d7e742a3f5246f72aaf071fceecd92be0ecc7

ECC: ECDH at GP level
Following derive key algorithm of Global Platform Internal Core API v1.1
are implemented:
    TEE_ALG_ECDH_P192
    TEE_ALG_ECDH_P224
    TEE_ALG_ECDH_P256
    TEE_ALG_ECDH_P384
    TEE_ALG_ECDH_P521

Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Cedric Chaumont <cedric.chaumont@linaro.org>
Tested-by: Pascal Brand <pascal.brand@linaro.org> (QEMU platform)
Signed-off-by: Pascal Brand <pascal.brand@st.com>

core/tee/tee_svc_cryp.c
@@ -2654,6 +2654,78 @@ TEE_Result tee_svc_cryp_derive_key(uint32_t state,
 		}
 		crypto_ops.bignum.free(pub);
 		crypto_ops.bignum.free(ss);
+	} else if (TEE_ALG_GET_MAIN_ALG(cs->algo) == TEE_MAIN_ALGO_ECDH) {
+		size_t alloc_size;
+		struct ecc_public_key key_public;
+		uint8_t *pt_secret;
+		unsigned long pt_secret_len;
+
+		if (!crypto_ops.bignum.allocate ||
+		    !crypto_ops.bignum.free ||
+		    !crypto_ops.bignum.bin2bn ||
+		    !crypto_ops.bignum.bn2bin ||
+		    !crypto_ops.bignum.num_bytes ||
+		    !crypto_ops.acipher.alloc_ecc_public_key ||
+		    !crypto_ops.acipher.ecc_shared_secret) {
+			res = TEE_ERROR_NOT_IMPLEMENTED;
+			goto out;
+		}
+		if (param_count != 2 ||
+		    params[0].attributeID != TEE_ATTR_ECC_PUBLIC_VALUE_X ||
+		    params[1].attributeID != TEE_ATTR_ECC_PUBLIC_VALUE_Y) {
+			res = TEE_ERROR_BAD_PARAMETERS;
+			goto out;
+		}
+
+		switch (cs->algo) {
+		case TEE_ALG_ECDH_P192:
+			alloc_size = 192;
+			break;
+		case TEE_ALG_ECDH_P224:
+			alloc_size = 224;
+			break;
+		case TEE_ALG_ECDH_P256:
+			alloc_size = 256;
+			break;
+		case TEE_ALG_ECDH_P384:
+			alloc_size = 384;
+			break;
+		case TEE_ALG_ECDH_P521:
+			alloc_size = 521;
+			break;
+		default:
+			res = TEE_ERROR_NOT_IMPLEMENTED;
+			goto out;
+		}
+
+		/* Create the public key */
+		res = crypto_ops.acipher.alloc_ecc_public_key(&key_public,
+							      alloc_size);
+		if (res != TEE_SUCCESS)
+			goto out;
+		key_public.curve = ((struct ecc_keypair *)ko->data)->curve;
+		crypto_ops.bignum.bin2bn(params[0].content.ref.buffer,
+					 params[0].content.ref.length,
+					 key_public.x);
+		crypto_ops.bignum.bin2bn(params[1].content.ref.buffer,
+					 params[1].content.ref.length,
+					 key_public.y);
+
+		pt_secret = (uint8_t *)(sk + 1);
+		pt_secret_len = so->data_size -
+				sizeof(struct tee_cryp_obj_secret);
+		res = crypto_ops.acipher.ecc_shared_secret(ko->data,
+				&key_public, pt_secret, &pt_secret_len);
+
+		if (res == TEE_SUCCESS) {
+			sk->key_size = pt_secret_len;
+			so->info.handleFlags |= TEE_HANDLE_FLAG_INITIALIZED;
+			SET_ATTRIBUTE(so, type_props, TEE_ATTR_SECRET_VALUE);
+		}
+
+		/* free the public key */
+		crypto_ops.bignum.free(key_public.x);
+		crypto_ops.bignum.free(key_public.y);
 	}
 #if defined(CFG_CRYPTO_HKDF)
 	else if (TEE_ALG_GET_MAIN_ALG(cs->algo) == TEE_MAIN_ALGO_HKDF) {

commit c2d64e1390587b39e47d7eb618b5fa4e5c7639e0

core: validate ECC public keys are on the curve
Adjusts the derive_key syscall to validate that the ECC public key that
will be used for a ECDH shared secret operation is a valid point on the
correct curve. This is required to avoid invalid curve attacks that
allow an attacker to recover the private key with a few tens of derive
key operations. mbedtls and STM32 already had checks for this and
should be unaffected.

To avoid situations where implementations of the derive key operation
forget to do the check it is placed early in the shared path. To allow
for hardware acceleration the operation is passed to the drvcrypt layer
when it is enabled. If drvcrypt is disabled, or the driver in use
doesn't implement the check, crypto_asym_get_ecc_public_ops is used to
get a software fallback from either libtomcrypt or mbed tls.

For this patch no changes have been made to the drivers. For STM32 this
means the existing hardware validation now happens in addition to the
software validation. For CAAM, a hardware implementation is possible.

Fixes: CVE-2026-45614
Fixes: 543d7e742a3f ("ECC: ECDH at GP level")
Signed-off-by: Martin Nyhus <martin@nyhus.dev>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/crypto/crypto.c
@@ -796,6 +796,16 @@ TEE_Result crypto_acipher_ecc_shared_secret(struct ecc_keypair *private_key,
 					       secret_len);
 }
 
+TEE_Result crypto_acipher_verify_ecc_public_key(struct ecc_public_key *key)
+{
+	assert(key && key->ops);
+
+	if (!key->ops->validate)
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
+	return key->ops->validate(key);
+}
+
 TEE_Result crypto_acipher_sm2_pke_decrypt(struct ecc_keypair *key,
 					  const uint8_t *src, size_t src_len,
 					  uint8_t *dst, size_t *dst_len)

core/drivers/crypto/crypto_api/acipher/ecc.c
@@ -118,6 +118,36 @@ static void ecc_free_public_key(struct ecc_public_key *key)
 }
 
 /*
+ * Check that an ECC public key is valid for use in ECDH
+ *
+ * @key   Public Key
+ */
+static TEE_Result ecc_validate_public_key(struct ecc_public_key *key)
+{
+	const struct crypto_ecc_public_ops *fallback = NULL;
+	const struct drvcrypt_ecc *driver_impl = NULL;
+
+	if (!key) {
+		CRYPTO_TRACE("Parameters error key is NULL");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	driver_impl = drvcrypt_get_ops(CRYPTO_ECC);
+	if (driver_impl && driver_impl->validate_publickey) {
+		CRYPTO_TRACE("ECC Public Key validate");
+		return driver_impl->validate_publickey(key);
+	}
+
+	fallback = crypto_asym_get_ecc_public_ops(TEE_TYPE_ECDH_PUBLIC_KEY);
+	if (fallback && fallback->validate) {
+		CRYPTO_TRACE("ECC Public Key validate (fallback)");
+		return fallback->validate(key);
+	}
+
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+
+/*
  * Generates an ECC keypair
  *
  * @key        Keypair
@@ -556,6 +586,7 @@ TEE_Result drvcrypt_asym_alloc_ecc_keypair(struct ecc_keypair *key,
 
 static const struct crypto_ecc_public_ops ecc_public_key_ops = {
 	.free = ecc_free_public_key,
+	.validate = ecc_validate_public_key,
 	.verify = ecc_verify,
 	.encrypt = ecc_encrypt,
 };

core/drivers/crypto/crypto_api/include/drvcrypt_acipher.h
@@ -158,6 +158,8 @@ struct drvcrypt_ecc {
 				      size_t size_bits);
 	/* Free ECC public key */
 	void (*free_publickey)(struct ecc_public_key *key);
+	/* Check that an ECC public key is valid */
+	TEE_Result (*validate_publickey)(struct ecc_public_key *key);
 	/* Generates the ECC keypair */
 	TEE_Result (*gen_keypair)(struct ecc_keypair *key, size_t size_bits);
 	/* ECC Sign a message and returns the signature */

core/include/crypto/crypto.h
@@ -292,6 +292,7 @@ TEE_Result crypto_acipher_ecc_sign(uint32_t algo, struct ecc_keypair *key,
 TEE_Result crypto_acipher_ecc_verify(uint32_t algo, struct ecc_public_key *key,
 				     const uint8_t *msg, size_t msg_len,
 				     const uint8_t *sig, size_t sig_len);
+TEE_Result crypto_acipher_verify_ecc_public_key(struct ecc_public_key *key);
 TEE_Result crypto_acipher_ecc_shared_secret(struct ecc_keypair *private_key,
 					    struct ecc_public_key *public_key,
 					    void *secret,

core/include/crypto/crypto_impl.h
@@ -405,12 +405,14 @@ drvcrypt_authenc_alloc_ctx(struct crypto_authenc_ctx **ctx __unused,
  * crypto_acipher_free_ecc_*() functions.
  * Reference set in ecc_public_key when key allocated.
  *
- * @free    is mandatory
- * @verify  is optional
- * @encrypt is optional
+ * @free     is mandatory
+ * @validate is optional
+ * @verify   is optional
+ * @encrypt  is optional
  */
 struct crypto_ecc_public_ops {
 	void (*free)(struct ecc_public_key *key);
+	TEE_Result (*validate)(struct ecc_public_key *key);
 	TEE_Result (*verify)(uint32_t algo, struct ecc_public_key *key,
 			     const uint8_t *msg, size_t msg_len,
 			     const uint8_t *sig, size_t sig_len);

core/lib/libtomcrypt/ecc.c
@@ -22,6 +22,25 @@ static void _ltc_ecc_free_public_key(struct ecc_public_key *s)
 	crypto_bignum_free(&s->y);
 }
 
+static TEE_Result _ltc_ecc_validate_public_key(struct ecc_public_key *key)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	ecc_key ltc_public_key = { };
+
+	res = ecc_populate_ltc_public_key(&ltc_public_key, key, 0, NULL);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	if (ltc_ecc_verify_key(&ltc_public_key) != CRYPT_OK) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+
+out:
+	ecc_free(&ltc_public_key);
+	return res;
+}
+
 /*
  * For a given TEE @curve, return key size and LTC curve name. Also check that
  * @algo is compatible with this curve.
@@ -357,6 +376,7 @@ static const struct crypto_ecc_keypair_ops ecc_keypair_ops = {
 
 static const struct crypto_ecc_public_ops ecc_public_key_ops = {
 	.free = _ltc_ecc_free_public_key,
+	.validate = _ltc_ecc_validate_public_key,
 	.verify = _ltc_ecc_verify,
 };
 

core/lib/libtomcrypt/sub.mk
@@ -339,6 +339,7 @@ srcs-$(_CFG_CORE_LTC_ECC) += src/pk/ecc/ltc_ecc_mul2add.c
 srcs-$(_CFG_CORE_LTC_ECC) += src/pk/ecc/ltc_ecc_points.c
 srcs-$(_CFG_CORE_LTC_ECC) += src/pk/ecc/ltc_ecc_projective_add_point.c
 srcs-$(_CFG_CORE_LTC_ECC) += src/pk/ecc/ltc_ecc_projective_dbl_point.c
+srcs-$(_CFG_CORE_LTC_ECC) += src/pk/ecc/ltc_ecc_verify_key.c
 
 ifneq (,$(filter y,$(_CFG_CORE_LTC_SM2_DSA) $(_CFG_CORE_LTC_SM2_PKE)))
    cppflags-lib-y += -DLTC_ECC_SM2

core/tee/tee_svc_cryp.c
@@ -3844,6 +3844,10 @@ dh_out:
 		crypto_bignum_bin2bn(y_bbuf, params[1].content.ref.length,
 				     key_public.y);
 
+		res = crypto_acipher_verify_ecc_public_key(&key_public);
+		if (res != TEE_SUCCESS)
+			goto ecdh_out;
+
 		pt_secret = (uint8_t *)(sk + 1);
 		pt_secret_len = sk->alloc_size;
 		res = crypto_acipher_ecc_shared_secret(ko->attr, &key_public,
@@ -3856,6 +3860,7 @@ dh_out:
 			set_attribute(so, type_props, TEE_ATTR_SECRET_VALUE);
 		}
 
+ecdh_out:
 		/* free the public key */
 		crypto_acipher_free_ecc_public_key(&key_public);
 	}

lib/libmbedtls/core/ecc.c
@@ -35,6 +35,26 @@ static TEE_Result get_tee_result(int lmd_res)
 	}
 }
 
+static mbedtls_ecp_group_id curve_to_group_id(uint32_t curve)
+{
+	switch (curve) {
+	case TEE_ECC_CURVE_NIST_P192:
+		return MBEDTLS_ECP_DP_SECP192R1;
+	case TEE_ECC_CURVE_NIST_P224:
+		return MBEDTLS_ECP_DP_SECP224R1;
+	case TEE_ECC_CURVE_NIST_P256:
+		return MBEDTLS_ECP_DP_SECP256R1;
+	case TEE_ECC_CURVE_NIST_P384:
+		return MBEDTLS_ECP_DP_SECP384R1;
+	case TEE_ECC_CURVE_NIST_P521:
+		return MBEDTLS_ECP_DP_SECP521R1;
+	case TEE_ECC_CURVE_SM2:
+		return MBEDTLS_ECP_DP_SM2;
+	default:
+		return MBEDTLS_ECP_DP_NONE;
+	}
+}
+
 static void ecc_free_public_key(struct ecc_public_key *s)
 {
 	if (!s)
@@ -42,6 +62,49 @@ static void ecc_free_public_key(struct ecc_public_key *s)
 
 	crypto_bignum_free(&s->x);
 	crypto_bignum_free(&s->y);
+}
+
+static TEE_Result ecc_verify_public_key(struct ecc_public_key *key)
+{
+	mbedtls_ecp_group_id gid = MBEDTLS_ECP_DP_NONE;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	mbedtls_ecp_point point = { };
+	mbedtls_ecp_group grp = { };
+	uint8_t one[1] = { 1 };
+	int lmd_res = 0;
+
+	mbedtls_ecp_group_init(&grp);
+	mbedtls_ecp_point_init(&point);
+
+	gid = curve_to_group_id(key->curve);
+	lmd_res = mbedtls_ecp_group_load(&grp, gid);
+	if (lmd_res != 0) {
+		res = TEE_ERROR_NOT_SUPPORTED;
+		goto out;
+	}
+
+	point.X = *(mbedtls_mpi *)key->x;
+	point.Y = *(mbedtls_mpi *)key->y;
+	if (mbedtls_mpi_read_binary(&point.Z, one, sizeof(one))) {
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
+	lmd_res = mbedtls_ecp_check_pubkey(&grp, &point);
+	if (lmd_res != 0) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+
+	res = TEE_SUCCESS;
+
+out:
+	/* Reset X and Y since they are shallow copies of the input key */
+	mbedtls_mpi_init(&point.X);
+	mbedtls_mpi_init(&point.Y);
+	mbedtls_ecp_point_free(&point);
+	mbedtls_ecp_group_free(&grp);
+	return res;
 }
 
 static TEE_Result ecc_get_keysize(uint32_t curve, uint32_t algo,
@@ -82,26 +145,6 @@ static TEE_Result ecc_get_keysize(uint32_t curve, uint32_t algo,
 	}
 
 	return TEE_SUCCESS;
-}
-
-static mbedtls_ecp_group_id curve_to_group_id(uint32_t curve)
-{
-	switch (curve) {
-	case TEE_ECC_CURVE_NIST_P192:
-		return MBEDTLS_ECP_DP_SECP192R1;
-	case TEE_ECC_CURVE_NIST_P224:
-		return MBEDTLS_ECP_DP_SECP224R1;
-	case TEE_ECC_CURVE_NIST_P256:
-		return MBEDTLS_ECP_DP_SECP256R1;
-	case TEE_ECC_CURVE_NIST_P384:
-		return MBEDTLS_ECP_DP_SECP384R1;
-	case TEE_ECC_CURVE_NIST_P521:
-		return MBEDTLS_ECP_DP_SECP521R1;
-	case TEE_ECC_CURVE_SM2:
-		return MBEDTLS_ECP_DP_SM2;
-	default:
-		return MBEDTLS_ECP_DP_NONE;
-	}
 }
 
 static TEE_Result ecc_generate_keypair(struct ecc_keypair *key, size_t key_size)
@@ -453,6 +496,7 @@ err:
 
 static const struct crypto_ecc_public_ops ecc_public_key_ops = {
 	.free = ecc_free_public_key,
+	.validate = ecc_verify_public_key,
 	.verify = ecc_verify,
 };
 