OP-TEE/optee_os

commit 1478437e65c44163f7c96f8a4c5d1532a9312bc3

core: ltc: use SHA-3 crypto accelerated function
Uses the recently provided accelerated SHA-3 function in LTC

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/lib/libtomcrypt/sha3_accel.c
@@ -1,0 +1,245 @@
+/* LibTomCrypt, modular cryptographic library -- Tom St Denis */
+/* SPDX-License-Identifier: Unlicense */
+/*
+ * Copyright (c) 2023, Linaro Limited
+ */
+
+/* based on https://github.com/brainhub/SHA3IUF (public domain) */
+
+#include <crypto/crypto_accel.h>
+#include <io.h>
+#include <tomcrypt_private.h>
+
+#ifdef LTC_SHA3
+
+const struct ltc_hash_descriptor sha3_224_desc =
+{
+   "sha3-224",                  /* name of hash */
+   17,                          /* internal ID */
+   28,                          /* Size of digest in octets */
+   144,                         /* Input block size in octets */
+   { 2,16,840,1,101,3,4,2,7 },  /* ASN.1 OID */
+   9,                           /* Length OID */
+   &sha3_224_init,
+   &sha3_process,
+   &sha3_done,
+   &sha3_224_test,
+   NULL
+};
+
+const struct ltc_hash_descriptor sha3_256_desc =
+{
+   "sha3-256",                  /* name of hash */
+   18,                          /* internal ID */
+   32,                          /* Size of digest in octets */
+   136,                         /* Input block size in octets */
+   { 2,16,840,1,101,3,4,2,8 },  /* ASN.1 OID */
+   9,                           /* Length OID */
+   &sha3_256_init,
+   &sha3_process,
+   &sha3_done,
+   &sha3_256_test,
+   NULL
+};
+
+const struct ltc_hash_descriptor sha3_384_desc =
+{
+   "sha3-384",                  /* name of hash */
+   19,                          /* internal ID */
+   48,                          /* Size of digest in octets */
+   104,                         /* Input block size in octets */
+   { 2,16,840,1,101,3,4,2,9 },  /* ASN.1 OID */
+   9,                           /* Length OID */
+   &sha3_384_init,
+   &sha3_process,
+   &sha3_done,
+   &sha3_384_test,
+   NULL
+};
+
+const struct ltc_hash_descriptor sha3_512_desc =
+{
+   "sha3-512",                  /* name of hash */
+   20,                          /* internal ID */
+   64,                          /* Size of digest in octets */
+   72,                          /* Input block size in octets */
+   { 2,16,840,1,101,3,4,2,10 }, /* ASN.1 OID */
+   9,                           /* Length OID */
+   &sha3_512_init,
+   &sha3_process,
+   &sha3_done,
+   &sha3_512_test,
+   NULL
+};
+
+/* Public Inteface */
+
+int sha3_224_init(hash_state *md)
+{
+   LTC_ARGCHK(md != NULL);
+   XMEMSET(&md->sha3, 0, sizeof(md->sha3));
+   md->sha3.capacity_words = 2 * 224 / (8 * sizeof(ulong64));
+   return CRYPT_OK;
+}
+
+int sha3_256_init(hash_state *md)
+{
+   LTC_ARGCHK(md != NULL);
+   XMEMSET(&md->sha3, 0, sizeof(md->sha3));
+   md->sha3.capacity_words = 2 * 256 / (8 * sizeof(ulong64));
+   return CRYPT_OK;
+}
+
+int sha3_384_init(hash_state *md)
+{
+   LTC_ARGCHK(md != NULL);
+   XMEMSET(&md->sha3, 0, sizeof(md->sha3));
+   md->sha3.capacity_words = 2 * 384 / (8 * sizeof(ulong64));
+   return CRYPT_OK;
+}
+
+int sha3_512_init(hash_state *md)
+{
+   LTC_ARGCHK(md != NULL);
+   XMEMSET(&md->sha3, 0, sizeof(md->sha3));
+   md->sha3.capacity_words = 2 * 512 / (8 * sizeof(ulong64));
+   return CRYPT_OK;
+}
+
+int sha3_shake_init(hash_state *md, int num)
+{
+   LTC_ARGCHK(md != NULL);
+   if (num != 128 && num != 256) return CRYPT_INVALID_ARG;
+   XMEMSET(&md->sha3, 0, sizeof(md->sha3));
+   md->sha3.capacity_words = (unsigned short)(2 * num / (8 * sizeof(ulong64)));
+   return CRYPT_OK;
+}
+
+int sha3_process(hash_state *md, const unsigned char *in, unsigned long inlen)
+{
+	unsigned int digest_size = 0;
+	unsigned int block_count = 0;
+	unsigned int block_size = 0;
+	void *state = NULL;
+	unsigned int l = 0;
+
+	if (!inlen)
+		return CRYPT_OK;
+	LTC_ARGCHK(md);
+	LTC_ARGCHK(in);
+
+	block_size = 200 - md->sha3.capacity_words * 8;
+	digest_size = md->sha3.capacity_words * 8 / 2;
+	state = md->sha3.s;
+
+	if (md->sha3.byte_index) {
+		l = MIN(block_size - md->sha3.byte_index, inlen);
+		memcpy(md->sha3.sb + md->sha3.byte_index, in, l);
+		in += l;
+		inlen -= l;
+		md->sha3.byte_index += l;
+		if (md->sha3.byte_index == block_size) {
+			crypto_accel_sha3_compress(state, md->sha3.sb, 1,
+						   digest_size);
+			md->sha3.byte_index = 0;
+		}
+
+		if (!inlen)
+			return CRYPT_OK;
+	}
+
+	if (inlen > block_size) {
+		block_count = inlen / block_size;
+		crypto_accel_sha3_compress(state, in, block_count,
+					   digest_size);
+		in += block_count * block_size;
+		inlen -= block_count * block_size;
+	}
+
+	memcpy(md->sha3.sb + md->sha3.byte_index, in, inlen);
+	md->sha3.byte_index += inlen;
+
+	return CRYPT_OK;
+}
+
+static void copy_out_digest(ulong64 *s, unsigned int digest_size,
+			    unsigned char *out)
+{
+	unsigned int n = 0;
+
+	for (n = 0; n < digest_size / sizeof(uint64_t); n++) {
+		put_unaligned_le64(out, s[n]);
+		out += sizeof(uint64_t);
+	}
+
+	if (digest_size % sizeof(uint64_t))
+		put_unaligned_le32(out, s[n]);
+}
+
+int sha3_done(hash_state *md, unsigned char *out)
+{
+	unsigned int digest_size = 0;
+	unsigned int block_size = 0;
+	void *state = NULL;
+	uint8_t *buf = NULL;
+
+	LTC_ARGCHK(md   != NULL);
+	LTC_ARGCHK(out != NULL);
+
+	block_size = 200 - md->sha3.capacity_words * 8;
+	digest_size = md->sha3.capacity_words * 8 / 2;
+	state = md->sha3.s;
+	buf = md->sha3.sb;
+
+	buf[md->sha3.byte_index++] = 0x06;
+	memset(buf + md->sha3.byte_index, 0, block_size - md->sha3.byte_index);
+	buf[block_size - 1] |= 0x80;
+	crypto_accel_sha3_compress(state, buf, 1, digest_size);
+
+	copy_out_digest(state, digest_size, out);
+
+	return CRYPT_OK;
+}
+
+
+int sha3_shake_done(hash_state *md, unsigned char *out, unsigned long outlen)
+{
+	unsigned int digest_size = 0;
+	unsigned int block_size = 0;
+	void *state = NULL;
+	uint8_t *buf = NULL;
+	unsigned int n = 0;
+
+	LTC_ARGCHK(md   != NULL);
+	LTC_ARGCHK(out != NULL);
+
+	block_size = 200 - md->sha3.capacity_words * 8;
+	digest_size = md->sha3.capacity_words * 8 / 2;
+	state = md->sha3.s;
+	buf = md->sha3.sb;
+
+	if (!md->sha3.xof_flag) {
+		buf[md->sha3.byte_index++] = 0x1f;
+		memset(buf + md->sha3.byte_index, 0,
+		       block_size - md->sha3.byte_index);
+		buf[block_size - 1] |= 0x80;
+		crypto_accel_sha3_compress(state, buf, 1, digest_size);
+		md->sha3.byte_index = 0;
+		copy_out_digest(state, block_size, buf);
+		md->sha3.xof_flag = 1;
+	}
+
+	for (n = 0; n < outlen; n++) {
+		if (md->sha3.byte_index >= block_size) {
+			memset(buf, 0, block_size);
+			crypto_accel_sha3_compress(state, buf, 1, digest_size);
+			md->sha3.byte_index = 0;
+			copy_out_digest(state, block_size, buf);
+		}
+		out[n] = buf[md->sha3.byte_index];
+		md->sha3.byte_index++;
+	}
+
+	return CRYPT_OK;
+}
+#endif

core/crypto.mk
@@ -204,6 +204,7 @@ _CFG_CORE_LTC_AES_ACCEL := $(CFG_CORE_CRYPTO_AES_ACCEL)
 _CFG_CORE_LTC_SHA1_ACCEL := $(CFG_CORE_CRYPTO_SHA1_ACCEL)
 _CFG_CORE_LTC_SHA256_ACCEL := $(CFG_CORE_CRYPTO_SHA256_ACCEL)
 _CFG_CORE_LTC_SHA512_ACCEL := $(CFG_CORE_CRYPTO_SHA512_ACCEL)
+_CFG_CORE_LTC_SHA3_ACCEL := $(CFG_CORE_CRYPTO_SHA3_ACCEL)
 endif
 
 ###############################################################
@@ -229,9 +230,6 @@ _CFG_CORE_LTC_SHA3_384 := $(CFG_CRYPTO_SHA3_384)
 _CFG_CORE_LTC_SHA3_512 := $(CFG_CRYPTO_SHA3_512)
 _CFG_CORE_LTC_SHAKE128 := $(CFG_CRYPTO_SHAKE128)
 _CFG_CORE_LTC_SHAKE256 := $(CFG_CRYPTO_SHAKE256)
-_CFG_CORE_LTC_HMAC := $(call cfg-one-enabled, _CFG_CORE_LTC_SHA3_224 \
-			_CFG_CORE_LTC_SHA3_256 _CFG_CORE_LTC_SHA3_384 \
-			_CFG_CORE_LTC_SHA3_512)
 endif
 
 ###############################################################
@@ -249,10 +247,10 @@ _CFG_CORE_LTC_SHA512_DESC := $(call cfg-one-enabled, _CFG_CORE_LTC_SHA512_DESC \
 _CFG_CORE_LTC_AES_DESC := $(call cfg-one-enabled, _CFG_CORE_LTC_AES_DESC \
 						  _CFG_CORE_LTC_AES)
 
-_CFG_CORE_LTC_SHA3 := $(call cfg-one-enabled, _CFG_CORE_LTC_SHA3_224 \
-			_CFG_CORE_LTC_SHA3_256 _CFG_CORE_LTC_SHA3_384 \
-			_CFG_CORE_LTC_SHA3_512 _CFG_CORE_LTC_SHAKE128 \
-			_CFG_CORE_LTC_SHAKE256)
+_CFG_CORE_LTC_SHA3_DESC := $(call cfg-one-enabled, _CFG_CORE_LTC_SHA3_224 \
+			     _CFG_CORE_LTC_SHA3_256 _CFG_CORE_LTC_SHA3_384 \
+			     _CFG_CORE_LTC_SHA3_512 _CFG_CORE_LTC_SHAKE128 \
+			     _CFG_CORE_LTC_SHAKE256)
 
 # Assign system variables
 _CFG_CORE_LTC_CE := $(CFG_CRYPTO_WITH_CE)
@@ -275,6 +273,12 @@ _CFG_CORE_LTC_CIPHER := $(call ltc-one-enabled, AES_DESC DES)
 _CFG_CORE_LTC_HASH := $(call ltc-one-enabled, MD5 SHA1 SHA224 SHA256 SHA384 \
 					      SHA512 SHA3_224 SHA3_256 \
 					      SHA3_384 SHA3_512)
+ifeq ($(CFG_CRYPTO_HMAC),y)
+_CFG_CORE_LTC_HMAC := $(call ltc-one-enabled, MD5 SHA1 SHA224 SHA256 SHA384 \
+					      SHA512 SHA3_224 SHA3_256 \
+					      SHA3_384 SHA3_512)
+endif
+
 _CFG_CORE_LTC_MAC := $(call ltc-one-enabled, HMAC CMAC CBC_MAC)
 _CFG_CORE_LTC_CBC := $(call ltc-one-enabled, CBC CBC_MAC)
 _CFG_CORE_LTC_ASN1 := $(call ltc-one-enabled, RSA DSA ECC)

core/lib/libtomcrypt/src/hashes/sub.mk
@@ -6,6 +6,9 @@ srcs-y += sha1.c
 endif
 endif
 
-srcs-$(_CFG_CORE_LTC_SHA3) += sha3.c sha3_test.c
+ifneq ($(_CFG_CORE_LTC_SHA3_ACCEL),y)
+srcs-$(_CFG_CORE_LTC_SHA3_DESC) += sha3.c
+endif
+srcs-$(_CFG_CORE_LTC_SHA3_DESC) += sha3_test.c
 subdirs-y += helper
 subdirs-y += sha2

core/lib/libtomcrypt/sub.mk
@@ -57,6 +57,8 @@ endif
 ifeq ($(_CFG_CORE_LTC_SHA512_256),y)
 	cppflags-lib-y += -DLTC_SHA512_256
 endif
+cppflags-lib-$(_CFG_CORE_LTC_SHA3_DESC) += -DLTC_SHA3
+
 
 cppflags-lib-y += -DLTC_NO_MACS
 
@@ -107,8 +109,6 @@ endif
 cppflags-lib-$(_CFG_CORE_LTC_X25519) += -DLTC_CURVE25519
 cppflags-lib-$(_CFG_CORE_LTC_ED25519) += -DLTC_CURVE25519
 
-cppflags-lib-$(_CFG_CORE_LTC_SHA3) += -DLTC_SHA3
-
 cppflags-lib-y += -DLTC_NO_PRNGS -DLTC_FORTUNA
 
 cflags-lib-$(_CFG_CORE_LTC_SIZE_OPTIMIZATION) += -Os
@@ -116,7 +116,6 @@ cflags-lib-$(_CFG_CORE_LTC_SIZE_OPTIMIZATION) += -Os
 subdirs-y += src
 
 srcs-$(_CFG_CORE_LTC_HASH) += hash.c
-srcs-$(_CFG_CORE_LTC_SHA3) += shake.c
 srcs-$(_CFG_CORE_LTC_HMAC) += hmac.c
 srcs-$(_CFG_CORE_LTC_CMAC) += cmac.c
 srcs-$(_CFG_CORE_LTC_ECB) += ecb.c
@@ -137,6 +136,10 @@ srcs-$(_CFG_CORE_LTC_SHA256_ACCEL) += sha256_accel.c
 endif
 ifeq ($(_CFG_CORE_LTC_SHA512_DESC),y)
 srcs-$(_CFG_CORE_LTC_SHA512_ACCEL) += sha512_accel.c
+endif
+ifeq ($(_CFG_CORE_LTC_SHA3_DESC),y)
+srcs-y += shake.c
+srcs-$(_CFG_CORE_LTC_SHA3_ACCEL) += sha3_accel.c
 endif
 srcs-$(_CFG_CORE_LTC_SM2_DSA) += sm2-dsa.c
 srcs-$(_CFG_CORE_LTC_SM2_PKE) += sm2-pke.c

core/lib/libtomcrypt/tomcrypt.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2014, Linaro Limited
+ * Copyright (c) 2014-2023, Linaro Limited
  */
 
 #include <crypto/crypto.h>
@@ -117,16 +117,16 @@ static void tee_ltc_reg_algs(void)
 #if defined(_CFG_CORE_LTC_SHA512) || defined(_CFG_CORE_LTC_SHA512_DESC)
 	register_hash(&sha512_desc);
 #endif
-#if defined(_CFG_CORE_LTC_SHA3_224)
+#if defined(_CFG_CORE_LTC_SHA3_224) || defined(_CFG_CORE_LTC_SHA3_224_DESC)
 	register_hash(&sha3_224_desc);
 #endif
-#if defined(_CFG_CORE_LTC_SHA3_256)
+#if defined(_CFG_CORE_LTC_SHA3_256) || defined(_CFG_CORE_LTC_SHA3_256_DESC)
 	register_hash(&sha3_256_desc);
 #endif
-#if defined(_CFG_CORE_LTC_SHA3_384)
+#if defined(_CFG_CORE_LTC_SHA3_384) || defined(_CFG_CORE_LTC_SHA3_384_DESC)
 	register_hash(&sha3_384_desc);
 #endif
-#if defined(_CFG_CORE_LTC_SHA3_512)
+#if defined(_CFG_CORE_LTC_SHA3_512) || defined(_CFG_CORE_LTC_SHA3_512_DESC)
 	register_hash(&sha3_512_desc);
 #endif
 #if defined(_CFG_CORE_LTC_ACIPHER) || defined(_CFG_CORE_LTC_EC25519)

commit 245b2929448f2df6470c170fba191612c535e859

core: ltc: fix boundary condition in sha3_accel()
Before this patch, sha3_accel() had an off-by-one error where a full
block bypassed hardware compression, leaving the internal byte_index
equal to block_size. During finalization, this invalid state caused an
integer underflow in the padding length calculation, resulting in an
out-of-bounds memory write.

Fix this off-by-one error and add sanity checks in the finalization
functions to prevent future overflows.

Fixes: 1478437e65c4 ("core: ltc: use SHA-3 crypto accelerated function")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>

core/lib/libtomcrypt/sha3_accel.c
@@ -148,7 +148,7 @@ int sha3_process(hash_state *md, const unsigned char *in, unsigned long inlen)
 			return CRYPT_OK;
 	}
 
-	if (inlen > block_size) {
+	if (inlen >= block_size) {
 		block_count = inlen / block_size;
 		crypto_accel_sha3_compress(state, in, block_count,
 					   digest_size);
@@ -191,6 +191,8 @@ int sha3_done(hash_state *md, unsigned char *out)
 	state = md->sha3.s;
 	buf = md->sha3.sb;
 
+	LTC_ARGCHK(md->sha3.byte_index < block_size);
+
 	buf[md->sha3.byte_index++] = 0x06;
 	memset(buf + md->sha3.byte_index, 0, block_size - md->sha3.byte_index);
 	buf[block_size - 1] |= 0x80;
@@ -217,6 +219,8 @@ int sha3_shake_done(hash_state *md, unsigned char *out, unsigned long outlen)
 	digest_size = md->sha3.capacity_words * 8 / 2;
 	state = md->sha3.s;
 	buf = md->sha3.sb;
+
+	LTC_ARGCHK(md->sha3.byte_index < block_size);
 
 	if (!md->sha3.xof_flag) {
 		buf[md->sha3.byte_index++] = 0x1f;