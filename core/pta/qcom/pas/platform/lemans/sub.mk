ldts/optee_os_qcom

pta: qcom: pas: lemans: add LPASS (ADSP) bring-up
Add LPASS / ADSP (QDSP6 v68/v69) PAS bring-up for the Lemans platform
(IQ-9075-EVK), following the existing Lemans CDSP0/1 PAS + clock-driver
pattern and the Kodiak LPASS PTA layout.

Unlike the Turing NSP, LPASS configures its Q6 PLL and switches the core
RCG during the clock setup stage (before the boot FSM), mirroring
Setup_LPASSProcessor in the reference ClockPIL. qcom_clock_enable_pas
runs the setup; lpass_fw_start programs RST_EVB / EFUSE EVB-select then
triggers the boot FSM; pas_reset and enable_pas_processor are no-ops for
LPASS. The IOMMU resource table is translated from the reference TZ SMMU
static config (adsp_q6_elf[]).

PAS ID for ADSP/LPASS is PAS_ID_QDSP6 (1).
Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

core/pta/qcom/pas/platform/lemans/sub.mk
@@ -1,3 +1,3 @@
-srcs-y += entry.c cdsp0.c cdsp1.c
+srcs-y += entry.c cdsp0.c cdsp1.c lpass.c
 incdirs-y += .
 incdirs-y += ../

OP-TEE/optee_os

core/pta/qcom/pas/platform/lemans/sub.mk
@@ -1,3 +1,3 @@
-srcs-y += subsys.c cdsp0.c cdsp1.c
+srcs-y += subsys.c cdsp0.c cdsp1.c lpass.c
 incdirs-y += .
 incdirs-y += ../