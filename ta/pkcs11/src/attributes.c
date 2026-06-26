OP-TEE/optee_os

commit 63f89caa9022ecf51d1b82dc78af35ba9e38466d

ta: pkcs11: attribute helper functions
* Helper functions for object attributes management.
* Helper functions to safely parse client attributes template
  to create a list of attributes for a object in the PKCS11 ta.
* Helper functions for assigning or checking object attributes
  according to PKCS#11 specification.
* Add id-to-string conversion for attribute/class/key types.
* Helper functions to analyze object attributes.

Reviewed-by: Ricardo Salveti <ricardo@foundries.io>
Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/pkcs11/src/attributes.c
@@ -1,0 +1,299 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#include <assert.h>
+#include <compiler.h>
+#include <pkcs11_ta.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string_ext.h>
+#include <tee_internal_api.h>
+#include <tee_internal_api_extensions.h>
+#include <trace.h>
+#include <util.h>
+
+#include "attributes.h"
+#include "pkcs11_helpers.h"
+#include "serializer.h"
+
+enum pkcs11_rc init_attributes_head(struct obj_attrs **head)
+{
+	*head = TEE_Malloc(sizeof(**head), TEE_MALLOC_FILL_ZERO);
+	if (!*head)
+		return PKCS11_CKR_DEVICE_MEMORY;
+
+	return PKCS11_CKR_OK;
+}
+
+enum pkcs11_rc add_attribute(struct obj_attrs **head, uint32_t attribute,
+			     void *data, size_t size)
+{
+	size_t buf_len = sizeof(struct obj_attrs) + (*head)->attrs_size;
+	char **bstart = (void *)head;
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	uint32_t data32 = 0;
+
+	data32 = attribute;
+	rc = serialize(bstart, &buf_len, &data32, sizeof(uint32_t));
+	if (rc)
+		return rc;
+
+	data32 = size;
+	rc = serialize(bstart, &buf_len, &data32, sizeof(uint32_t));
+	if (rc)
+		return rc;
+
+	rc = serialize(bstart, &buf_len, data, size);
+	if (rc)
+		return rc;
+
+	/* Alloced buffer is always well aligned */
+	head = (void *)bstart;
+	(*head)->attrs_size += 2 * sizeof(uint32_t) + size;
+	(*head)->attrs_count++;
+
+	return rc;
+}
+
+void get_attribute_ptrs(struct obj_attrs *head, uint32_t attribute,
+			void **attr, uint32_t *attr_size, size_t *count)
+{
+	char *cur = (char *)head + sizeof(struct obj_attrs);
+	char *end = cur + head->attrs_size;
+	size_t next_off = 0;
+	size_t max_found = *count;
+	size_t found = 0;
+	void **attr_ptr = attr;
+	uint32_t *attr_size_ptr = attr_size;
+
+	for (; cur < end; cur += next_off) {
+		/* Structure aligned copy of the pkcs11_ref in the object */
+		struct pkcs11_attribute_head pkcs11_ref = { };
+
+		TEE_MemMove(&pkcs11_ref, cur, sizeof(pkcs11_ref));
+		next_off = sizeof(pkcs11_ref) + pkcs11_ref.size;
+
+		if (pkcs11_ref.id != attribute)
+			continue;
+
+		found++;
+
+		if (!max_found)
+			continue;	/* only count matching attributes */
+
+		if (attr)
+			*attr_ptr++ = cur + sizeof(pkcs11_ref);
+
+		if (attr_size)
+			*attr_size_ptr++ = pkcs11_ref.size;
+
+		if (found == max_found)
+			break;
+	}
+
+	/* Sanity */
+	if (cur > end) {
+		DMSG("Exceeding serial object length");
+		TEE_Panic(0);
+	}
+
+	*count = found;
+}
+
+enum pkcs11_rc get_attribute_ptr(struct obj_attrs *head, uint32_t attribute,
+				 void **attr_ptr, uint32_t *attr_size)
+{
+	size_t count = 1;
+
+	get_attribute_ptrs(head, attribute, attr_ptr, attr_size, &count);
+
+	if (!count)
+		return PKCS11_RV_NOT_FOUND;
+
+	if (count != 1)
+		return PKCS11_CKR_GENERAL_ERROR;
+
+	return PKCS11_CKR_OK;
+}
+
+enum pkcs11_rc get_attribute(struct obj_attrs *head, uint32_t attribute,
+			     void *attr, uint32_t *attr_size)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	void *attr_ptr = NULL;
+	uint32_t size = 0;
+
+	rc = get_attribute_ptr(head, attribute, &attr_ptr, &size);
+	if (rc)
+		return rc;
+
+	if (attr_size && *attr_size != size) {
+		*attr_size = size;
+		/* This reuses buffer-to-small for any bad size matching */
+		return PKCS11_CKR_BUFFER_TOO_SMALL;
+	}
+
+	if (attr)
+		TEE_MemMove(attr, attr_ptr, size);
+
+	if (attr_size)
+		*attr_size = size;
+
+	return PKCS11_CKR_OK;
+}
+
+bool get_bool(struct obj_attrs *head, uint32_t attribute)
+{
+	enum pkcs11_rc rc = PKCS11_CKR_OK;
+	uint8_t bbool = 0;
+	uint32_t size = sizeof(bbool);
+
+	rc = get_attribute(head, attribute, &bbool, &size);
+
+	if (rc == PKCS11_RV_NOT_FOUND)
+		return false;
+
+	assert(rc == PKCS11_CKR_OK);
+	return bbool;
+}
+
+#if CFG_TEE_TA_LOG_LEVEL > 0
+/*
+ * Debug: dump CK attribute array to output trace
+ */
+#define ATTR_TRACE_FMT	"%s attr %s / %s\t(0x%04"PRIx32" %"PRIu32"-byte"
+#define ATTR_FMT_0BYTE	ATTR_TRACE_FMT ")"
+#define ATTR_FMT_1BYTE	ATTR_TRACE_FMT ": %02x)"
+#define ATTR_FMT_2BYTE	ATTR_TRACE_FMT ": %02x %02x)"
+#define ATTR_FMT_3BYTE	ATTR_TRACE_FMT ": %02x %02x %02x)"
+#define ATTR_FMT_4BYTE	ATTR_TRACE_FMT ": %02x %02x %02x %02x)"
+#define ATTR_FMT_ARRAY	ATTR_TRACE_FMT ": %02x %02x %02x %02x ...)"
+
+static void __trace_attributes(char *prefix, void *src, void *end)
+{
+	size_t next_off = 0;
+	char *prefix2 = NULL;
+	size_t prefix_len = strlen(prefix);
+	char *cur = src;
+
+	/* append 4 spaces to the prefix plus terminal '\0' */
+	prefix2 = TEE_Malloc(prefix_len + 1 + 4, TEE_MALLOC_FILL_ZERO);
+	if (!prefix2)
+		return;
+
+	TEE_MemMove(prefix2, prefix, prefix_len + 1);
+	TEE_MemFill(prefix2 + prefix_len, ' ', 4);
+	*(prefix2 + prefix_len + 4) = '\0';
+
+	for (; cur < (char *)end; cur += next_off) {
+		struct pkcs11_attribute_head pkcs11_ref = { };
+		uint8_t data[4] = { 0 };
+
+		TEE_MemMove(&pkcs11_ref, cur, sizeof(pkcs11_ref));
+		TEE_MemMove(&data[0], cur + sizeof(pkcs11_ref),
+			    MIN(pkcs11_ref.size, sizeof(data)));
+
+		next_off = sizeof(pkcs11_ref) + pkcs11_ref.size;
+
+		switch (pkcs11_ref.size) {
+		case 0:
+			IMSG_RAW(ATTR_FMT_0BYTE,
+				 prefix, id2str_attr(pkcs11_ref.id), "*",
+				 pkcs11_ref.id, pkcs11_ref.size);
+			break;
+		case 1:
+			IMSG_RAW(ATTR_FMT_1BYTE,
+				 prefix, id2str_attr(pkcs11_ref.id),
+				 id2str_attr_value(pkcs11_ref.id,
+						   pkcs11_ref.size,
+						   cur + sizeof(pkcs11_ref)),
+				 pkcs11_ref.id, pkcs11_ref.size, data[0]);
+			break;
+		case 2:
+			IMSG_RAW(ATTR_FMT_2BYTE,
+				 prefix, id2str_attr(pkcs11_ref.id),
+				 id2str_attr_value(pkcs11_ref.id,
+						   pkcs11_ref.size,
+						   cur + sizeof(pkcs11_ref)),
+				 pkcs11_ref.id, pkcs11_ref.size, data[0],
+				 data[1]);
+			break;
+		case 3:
+			IMSG_RAW(ATTR_FMT_3BYTE,
+				 prefix, id2str_attr(pkcs11_ref.id),
+				 id2str_attr_value(pkcs11_ref.id,
+						   pkcs11_ref.size,
+						   cur + sizeof(pkcs11_ref)),
+				 pkcs11_ref.id, pkcs11_ref.size,
+				 data[0], data[1], data[2]);
+			break;
+		case 4:
+			IMSG_RAW(ATTR_FMT_4BYTE,
+				 prefix, id2str_attr(pkcs11_ref.id),
+				 id2str_attr_value(pkcs11_ref.id,
+						   pkcs11_ref.size,
+						   cur + sizeof(pkcs11_ref)),
+				 pkcs11_ref.id, pkcs11_ref.size,
+				 data[0], data[1], data[2], data[3]);
+			break;
+		default:
+			IMSG_RAW(ATTR_FMT_ARRAY,
+				 prefix, id2str_attr(pkcs11_ref.id),
+				 id2str_attr_value(pkcs11_ref.id,
+						   pkcs11_ref.size,
+						   cur + sizeof(pkcs11_ref)),
+				 pkcs11_ref.id, pkcs11_ref.size,
+				 data[0], data[1], data[2], data[3]);
+			break;
+		}
+
+		switch (pkcs11_ref.id) {
+		case PKCS11_CKA_WRAP_TEMPLATE:
+		case PKCS11_CKA_UNWRAP_TEMPLATE:
+		case PKCS11_CKA_DERIVE_TEMPLATE:
+			trace_attributes(prefix2, cur + sizeof(pkcs11_ref));
+			break;
+		default:
+			break;
+		}
+	}
+
+	/* Sanity */
+	if (cur != end)
+		EMSG("Warning: unexpected alignment in object attributes");
+
+	TEE_Free(prefix2);
+}
+
+void trace_attributes(const char *prefix, void *ref)
+{
+	struct obj_attrs head;
+	char *pre = NULL;
+
+	TEE_MemMove(&head, ref, sizeof(head));
+
+	pre = TEE_Malloc(prefix ? strlen(prefix) + 2 : 2, TEE_MALLOC_FILL_ZERO);
+	if (!pre) {
+		EMSG("%s: out of memory", prefix);
+		return;
+	}
+
+	if (prefix)
+		TEE_MemMove(pre, prefix, strlen(prefix));
+
+	IMSG_RAW("%s,--- (serial object) Attributes list --------", pre);
+	IMSG_RAW("%s| %"PRIu32" item(s) - %"PRIu32" bytes",
+		 pre, head.attrs_count, head.attrs_size);
+
+	pre[prefix ? strlen(prefix) : 0] = '|';
+	__trace_attributes(pre, (char *)ref + sizeof(head),
+			   (char *)ref + sizeof(head) + head.attrs_size);
+
+	IMSG_RAW("%s`-----------------------", prefix ? prefix : "");
+
+	TEE_Free(pre);
+}
+#endif /*CFG_TEE_TA_LOG_LEVEL*/

ta/pkcs11/src/attributes.h
@@ -1,0 +1,248 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#ifndef PKCS11_TA_ATTRIBUTES_H
+#define PKCS11_TA_ATTRIBUTES_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <util.h>
+
+#include "pkcs11_helpers.h"
+
+/*
+ * Boolean property attributes (BPA): bit position in a 64 bit mask
+ * for boolean properties object can mandate as attribute, depending
+ * on the object. These attributes are often accessed and it is
+ * quicker to get them from a 64 bit field in the object instance
+ * rather than searching into the object attributes.
+ */
+#define PKCS11_BOOLPROPS_BASE		0
+#define PKCS11_BOOLPROPS_MAX_COUNT	64
+
+enum boolprop_attr {
+	BPA_TOKEN = 0,
+	BPA_PRIVATE,
+	BPA_TRUSTED,
+	BPA_SENSITIVE,
+	BPA_ENCRYPT,
+	BPA_DECRYPT,
+	BPA_WRAP,
+	BPA_UNWRAP,
+	BPA_SIGN,
+	BPA_SIGN_RECOVER,
+	BPA_VERIFY,
+	BPA_VERIFY_RECOVER,
+	BPA_DERIVE,
+	BPA_EXTRACTABLE,
+	BPA_LOCAL,
+	BPA_NEVER_EXTRACTABLE,
+	BPA_ALWAYS_SENSITIVE,
+	BPA_MODIFIABLE,
+	BPA_COPYABLE,
+	BPA_DESTROYABLE,
+	BPA_ALWAYS_AUTHENTICATE,
+	BPA_WRAP_WITH_TRUSTED,
+};
+
+/*
+ * Header of a serialized memory object inside PKCS11 TA.
+ *
+ * @attrs_size:	 byte size of the serialized data
+ * @attrs_count: number of items in the blob
+ * @attrs:	 then starts the blob binary data
+ */
+struct obj_attrs {
+	uint32_t attrs_size;
+	uint32_t attrs_count;
+	uint8_t attrs[];
+};
+
+/*
+ * init_attributes_head() - Allocate a reference for serialized attributes
+ * @head:	*@head holds the retrieved pointer
+ *
+ * Retrieved pointer can be freed from a simple TEE_Free(reference).
+ *
+ * Return a PKCS11_OK on success or a PKCS11 return code.
+ */
+enum pkcs11_rc init_attributes_head(struct obj_attrs **head);
+
+/*
+ * add_attribute() - Update serialized attributes to add an entry.
+ *
+ * @head:	*@head points to serialized attributes,
+ *		can be reallocated as attributes are added
+ * @attribute:	Attribute ID to add
+ * @data:	Opaque data of attribute
+ * @size:	Size of data
+ *
+ * Return a PKCS11_OK on success or a PKCS11 return code.
+ */
+enum pkcs11_rc add_attribute(struct obj_attrs **head, uint32_t attribute,
+			     void *data, size_t size);
+
+/*
+ * get_attribute_ptrs() - Get pointers to attributes with a given ID
+ * @head:	Pointer to serialized attributes
+ * @attribute:	Attribute ID to look for
+ * @attr:	Array of pointers to the data inside @head
+ * @attr_size:	Array of uint32_t holding the sizes of each value pointed to
+ *		by @attr
+ * @count:	Number of elements in the arrays above
+ *
+ * If *count == 0, count and return in *count the number of attributes matching
+ * the input attribute ID.
+ *
+ * If *count != 0, return the address and size of the attributes found, up to
+ * the occurrence number *count. attr and attr_size are expected large
+ * enough. attr is the output array of the values found. attr_size is the
+ * output array of the size of each value found.
+ *
+ * If attr_size != NULL, return in *attr_size attribute value size.
+ * If attr != NULL return in *attr the address of the attribute value.
+ */
+void get_attribute_ptrs(struct obj_attrs *head, uint32_t attribute,
+			void **attr, uint32_t *attr_size, size_t *count);
+
+/*
+ * get_attribute_ptrs() - Get pointer to the attribute of a given ID
+ * @head:	Pointer to serialized attributes
+ * @attribute:	Attribute ID
+ * @attr:	*@attr holds the retrieved pointer to the attribute value
+ * @attr_size:	Size of the attribute value
+ *
+ * If no matching attributes is found return PKCS11_RV_NOT_FOUND.
+ * If attr_size != NULL, return in *attr_size attribute value size.
+ * If attr != NULL, return in *attr the address of the attribute value.
+ *
+ * Return a PKCS11_OK or PKCS11_RV_NOT_FOUND on success, or a PKCS11 return
+ * code.
+ */
+enum pkcs11_rc get_attribute_ptr(struct obj_attrs *head, uint32_t attribute,
+				 void **attr_ptr, uint32_t *attr_size);
+/*
+ * get_attribute() - Copy out the attribute of a given ID
+ * @head:	Pointer to serialized attributes
+ * @attribute:	Attribute ID to look for
+ * @attr:	holds the retrieved attribute value
+ * @attr_size:	Size of the attribute value
+ *
+ * If attribute is not found, return PKCS11_RV_NOT_FOUND.
+ * If attr_size != NULL, check *attr_size matches attributes size and return
+ * PKCS11_CKR_BUFFER_TOO_SMALL with expected size in *attr_size.
+ * If attr != NULL and attr_size is NULL or gives expected buffer size,
+ * copy attribute value into attr.
+ *
+ * Return a PKCS11_OK or PKCS11_RV_NOT_FOUND on success, or a PKCS11 return
+ * code.
+ */
+enum pkcs11_rc get_attribute(struct obj_attrs *head, uint32_t attribute,
+			     void *attr, uint32_t *attr_size);
+
+/*
+ * get_u32_attribute() - Copy out the 32-bit attribute value of a given ID
+ * @head:	Pointer to serialized attributes
+ * @attribute:	Attribute ID
+ * @attr:	holds the retrieved 32-bit attribute value
+ *
+ * If attribute is not found, return PKCS11_RV_NOT_FOUND.
+ * If the retreived attribute doesn't have a 4 byte sized value
+ * PKCS11_CKR_GENERAL_ERROR is returned.
+ *
+ * Return a PKCS11_OK or PKCS11_RV_NOT_FOUND on success, or a PKCS11 return
+ * code.
+ */
+
+static inline enum pkcs11_rc get_u32_attribute(struct obj_attrs *head,
+					       uint32_t attribute,
+					       uint32_t *attr)
+{
+	uint32_t size = sizeof(uint32_t);
+	enum pkcs11_rc rc = get_attribute(head, attribute, attr, &size);
+
+	if (!rc && size != sizeof(uint32_t))
+		return PKCS11_CKR_GENERAL_ERROR;
+
+	return rc;
+}
+
+/*
+ * get_class() - Get class ID of an object
+ * @head:	Pointer to serialized attributes
+ *
+ * Returns the class ID of an object on succes or returns
+ * PKCS11_CKO_UNDEFINED_ID on error.
+ */
+static inline enum pkcs11_class_id get_class(struct obj_attrs *head)
+{
+	uint32_t class = 0;
+	uint32_t size = sizeof(class);
+
+	if (get_attribute(head, PKCS11_CKA_CLASS, &class, &size))
+		return PKCS11_CKO_UNDEFINED_ID;
+
+	return class;
+}
+
+/*
+ * get_key_type() - Get the key type of an object
+ * @head:	Pointer to serialized attributes
+ *
+ * Returns the key type of an object on success or returns
+ * PKCS11_CKK_UNDEFINED_ID on error.
+ */
+static inline enum pkcs11_key_type get_key_type(struct obj_attrs *head)
+{
+	uint32_t type = 0;
+	uint32_t size = sizeof(type);
+
+	if (get_attribute(head, PKCS11_CKA_KEY_TYPE, &type, &size))
+		return PKCS11_CKK_UNDEFINED_ID;
+
+	return type;
+}
+
+/*
+ * get_mechanism_type() - Get the mechanism type of an object
+ * @head:	Pointer to serialized attributes
+ *
+ * Returns the mechanism type of an object on success or returns
+ * PKCS11_CKM_UNDEFINED_ID on error.
+ */
+static inline enum pkcs11_mechanism_id get_mechanism_type(struct obj_attrs *head)
+{
+	uint32_t type = 0;
+	uint32_t size = sizeof(type);
+
+	if (get_attribute(head, PKCS11_CKA_MECHANISM_TYPE, &type, &size))
+		return PKCS11_CKM_UNDEFINED_ID;
+
+	return type;
+}
+
+/*
+ * get_bool() - Get the bool value of an attribute
+ * @head:	Pointer to serialized attributes
+ * @attribute:	Attribute ID to look for
+ *
+ * May assert if attribute ID isn't of the boolean type.
+ *
+ * Returns the bool value of the supplied attribute ID on success if found
+ * else false.
+ */
+bool get_bool(struct obj_attrs *head, uint32_t attribute);
+
+#if CFG_TEE_TA_LOG_LEVEL > 0
+/* Debug: dump object attributes to IMSG() trace console */
+void trace_attributes(const char *prefix, void *ref);
+#else
+static inline void trace_attributes(const char *prefix __unused,
+				    void *ref __unused)
+{
+}
+#endif /*CFG_TEE_TA_LOG_LEVEL*/
+#endif /*PKCS11_TA_ATTRIBUTES_H*/

commit eb3145160d20a6a8b5673b461f0b1a1c0478cf36

ta: pkcs#11: limit indirect attribute recursion depth
Add a depth parameter for recursive calls handling indirect attributes.
Most notable is the mutual recursion between sanitize_indirect_attr()
and sanitize_client_object(). A few functions for debug prints are also
equipped with a depth parameter to limit the recursion depth.

Recursion depth is set to 5 as a reasonable maximum limit.

Fixes: 63f89caa9022 ("ta: pkcs11: attribute helper functions")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@arm.com>
Reviewed-by: Etienne Carriere <etienne.carriere@st.com>

ta/pkcs11/src/attributes.c
@@ -328,12 +328,18 @@ enum pkcs11_rc attributes_match_add_reference(struct obj_attrs **head,
 #define ATTR_FMT_4BYTE	ATTR_TRACE_FMT ": %02x %02x %02x %02x)"
 #define ATTR_FMT_ARRAY	ATTR_TRACE_FMT ": %02x %02x %02x %02x ...)"
 
-static void __trace_attributes(char *prefix, void *src, void *end)
+static void __trace_attributes(char *prefix, void *src, void *end,
+			       unsigned int depth)
 {
 	size_t next_off = 0;
 	char *prefix2 = NULL;
 	size_t prefix_len = strlen(prefix);
 	char *cur = src;
+
+	if (depth >= PKCS11_MAX_INDIRECT_DEPTH) {
+		EMSG("Too deep indirection");
+		return;
+	}
 
 	/* append 4 spaces to the prefix plus terminal '\0' */
 	prefix2 = TEE_Malloc(prefix_len + 1 + 4, TEE_MALLOC_FILL_ZERO);
@@ -412,7 +418,8 @@ static void __trace_attributes(char *prefix, void *src, void *end)
 		case PKCS11_CKA_DERIVE_TEMPLATE:
 			if (pkcs11_ref.size)
 				trace_attributes(prefix2,
-						 cur + sizeof(pkcs11_ref));
+						 cur + sizeof(pkcs11_ref),
+						 depth + 1);
 			break;
 		default:
 			break;
@@ -426,7 +433,7 @@ static void __trace_attributes(char *prefix, void *src, void *end)
 	TEE_Free(prefix2);
 }
 
-void trace_attributes(const char *prefix, void *ref)
+void trace_attributes(const char *prefix, void *ref, unsigned int depth)
 {
 	struct obj_attrs head = { };
 	char *pre = NULL;
@@ -451,7 +458,7 @@ void trace_attributes(const char *prefix, void *ref)
 
 	pre[prefix ? strlen(prefix) : 0] = '|';
 	__trace_attributes(pre, (char *)ref + sizeof(head),
-			   (char *)ref + sizeof(head) + head.attrs_size);
+			   (char *)ref + sizeof(head) + head.attrs_size, depth);
 
 	IMSG_RAW("%s`-----------------------", prefix ? prefix : "");
 

ta/pkcs11/src/attributes.h
@@ -23,6 +23,8 @@
 #define PKCS11_BOOLPROPS_BASE		0
 #define PKCS11_BOOLPROPS_MAX_COUNT	64
 
+#define PKCS11_MAX_INDIRECT_DEPTH 5
+
 enum boolprop_attr {
 	BPA_TOKEN = 0,
 	BPA_PRIVATE,
@@ -312,10 +314,11 @@ bool get_bool(struct obj_attrs *head, uint32_t attribute);
 
 #if CFG_TEE_TA_LOG_LEVEL > 0
 /* Debug: dump object attributes to IMSG() trace console */
-void trace_attributes(const char *prefix, void *ref);
+void trace_attributes(const char *prefix, void *ref, unsigned int depth);
 #else
 static inline void trace_attributes(const char *prefix __unused,
-				    void *ref __unused)
+				    void *ref __unused,
+				    unsigned int depth __unused)
 {
 }
 #endif /*CFG_TEE_TA_LOG_LEVEL*/

ta/pkcs11/src/object.c
@@ -542,7 +542,8 @@ enum pkcs11_rc entry_find_objects_init(struct pkcs11_client *client,
 
 	rc = sanitize_client_object(&req_attrs, template,
 				    sizeof(*template) + template->attrs_size,
-				    PKCS11_UNDEFINED_ID, PKCS11_UNDEFINED_ID);
+				    PKCS11_UNDEFINED_ID, PKCS11_UNDEFINED_ID,
+				    0);
 	if (rc)
 		goto out;
 

ta/pkcs11/src/pkcs11_attributes.c
@@ -1263,7 +1263,7 @@ create_attributes_from_template(struct obj_attrs **out, void *template,
 	 * template
 	 */
 	rc = sanitize_client_object(&temp, template, template_size, class,
-				    type);
+				    type, 0);
 	if (rc)
 		goto out;
 

ta/pkcs11/src/processing_ec.c
@@ -606,8 +606,8 @@ enum pkcs11_rc generate_ec_keys(struct pkcs11_attribute_head *proc_params,
 	    remove_empty_attribute(priv_head, PKCS11_CKA_VALUE) ||
 	    remove_empty_attribute(priv_head, PKCS11_CKA_EC_PARAMS)) {
 		EMSG("Unexpected attribute(s) found");
-		trace_attributes("public-key", *pub_head);
-		trace_attributes("private-key", *priv_head);
+		trace_attributes("public-key", *pub_head, 0);
+		trace_attributes("private-key", *priv_head, 0);
 		return PKCS11_CKR_TEMPLATE_INCONSISTENT;
 	}
 
@@ -741,8 +741,8 @@ enum pkcs11_rc generate_eddsa_keys(struct pkcs11_attribute_head *proc_params,
 	    remove_empty_attribute(priv_head, PKCS11_CKA_EC_POINT) ||
 	    remove_empty_attribute(priv_head, PKCS11_CKA_EC_PARAMS)) {
 		EMSG("Unexpected attribute(s) found");
-		trace_attributes("public-key", *pub_head);
-		trace_attributes("private-key", *priv_head);
+		trace_attributes("public-key", *pub_head, 0);
+		trace_attributes("private-key", *priv_head, 0);
 		return PKCS11_CKR_TEMPLATE_INCONSISTENT;
 	}
 

ta/pkcs11/src/sanitize_object.c
@@ -159,7 +159,7 @@ static enum pkcs11_rc sanitize_class_and_type(struct obj_attrs **dst, void *src,
 	return PKCS11_CKR_OK;
 
 err:
-	trace_attributes_from_api_head("bad-template", src, src_size);
+	trace_attributes_from_api_head("bad-template", src, src_size, 0);
 
 	return rc;
 }
@@ -224,17 +224,20 @@ static enum pkcs11_rc sanitize_boolprops(struct obj_attrs **dst, void *src,
 
 static uint32_t sanitize_indirect_attr(struct obj_attrs **dst,
 				       struct pkcs11_attribute_head *cli_ref,
-				       char *data)
+				       char *data, unsigned int depth)
 {
 	struct obj_attrs *obj2 = NULL;
 	enum pkcs11_rc rc = PKCS11_CKR_OK;
+
+	if (depth >= PKCS11_MAX_INDIRECT_DEPTH)
+		return PKCS11_CKR_TEMPLATE_INCONSISTENT;
 
 	assert(pkcs11_attr_has_indirect_attributes(cli_ref->id));
 
 	/* Build a new serial object while sanitizing the attributes list */
 	rc = sanitize_client_object(&obj2, data, cli_ref->size,
 				    PKCS11_CKO_UNDEFINED_ID,
-				    PKCS11_UNDEFINED_ID);
+				    PKCS11_UNDEFINED_ID, depth + 1);
 	if (rc)
 		goto out;
 
@@ -247,7 +250,7 @@ out:
 
 enum pkcs11_rc sanitize_client_object(struct obj_attrs **dst, void *src,
 				      size_t size, uint32_t class_hint,
-				      uint32_t type_hint)
+				      uint32_t type_hint, unsigned int depth)
 {
 	struct pkcs11_attribute_head cli_ref = { };
 	struct pkcs11_object_head head = { };
@@ -289,7 +292,7 @@ enum pkcs11_rc sanitize_client_object(struct obj_attrs **dst, void *src,
 			continue;
 
 		if (pkcs11_attr_has_indirect_attributes(cli_ref.id)) {
-			rc = sanitize_indirect_attr(dst, &cli_ref, data);
+			rc = sanitize_indirect_attr(dst, &cli_ref, data, depth);
 			if (rc)
 				return rc;
 
@@ -313,12 +316,18 @@ enum pkcs11_rc sanitize_client_object(struct obj_attrs **dst, void *src,
  * Debug: dump object attribute array to output trace
  */
 
-static void __trace_attributes(char *prefix, void *src, void *end)
+static void __trace_attributes(char *prefix, void *src, void *end,
+			       unsigned int depth)
 {
 	size_t next = 0;
 	char *prefix2 = NULL;
 	size_t prefix_len = strlen(prefix);
 	char *cur = src;
+
+	if (depth >= PKCS11_MAX_INDIRECT_DEPTH) {
+		EMSG("Too deep indirection");
+		return;
+	}
 
 	/* append 4 spaces to the prefix plus terminal '\0' */
 	prefix2 = TEE_Malloc(prefix_len + 1 + 4, TEE_MALLOC_FILL_ZERO);
@@ -401,7 +410,8 @@ static void __trace_attributes(char *prefix, void *src, void *end)
 			if (avail >= sizeof(struct pkcs11_object_head))
 				trace_attributes_from_api_head(prefix2, start,
 							       (char *)end -
-							       start);
+							       start,
+							       depth + 1);
 			break;
 		default:
 			break;
@@ -415,7 +425,8 @@ static void __trace_attributes(char *prefix, void *src, void *end)
 	TEE_Free(prefix2);
 }
 
-void trace_attributes_from_api_head(const char *prefix, void *ref, size_t size)
+void trace_attributes_from_api_head(const char *prefix, void *ref, size_t size,
+				    unsigned int depth)
 {
 	struct pkcs11_object_head head = { };
 	char *pre = NULL;
@@ -444,7 +455,7 @@ void trace_attributes_from_api_head(const char *prefix, void *ref, size_t size)
 	offset = sizeof(head);
 	pre[prefix ? strlen(prefix) : 0] = '|';
 	__trace_attributes(pre, (char *)ref + offset,
-			   (char *)ref + offset + head.attrs_size);
+			   (char *)ref + offset + head.attrs_size, depth);
 
 	DMSG_RAW("%s`-----------------------", prefix ? prefix : "");
 

ta/pkcs11/src/sanitize_object.h
@@ -26,6 +26,7 @@ bool sanitize_consistent_class_and_type(struct obj_attrs *attrs);
  *               in serialized object.
  * @type_hint - Hint for type to be added to template if not presnet
  *               in serialized object.
+ * @depth - recursion depth.
  *
  * This function copies an attribute list from a client API attribute head
  * into a PKCS11 TA internal attribute structure. It generates a serialized
@@ -38,9 +39,10 @@ bool sanitize_consistent_class_and_type(struct obj_attrs *attrs);
  */
 enum pkcs11_rc sanitize_client_object(struct obj_attrs **dst, void *head,
 				      size_t size, uint32_t class_hint,
-				      uint32_t type_hint);
+				      uint32_t type_hint, unsigned int depth);
 
 /* Debug: dump attribute content as debug traces */
-void trace_attributes_from_api_head(const char *prefix, void *ref, size_t size);
+void trace_attributes_from_api_head(const char *prefix, void *ref, size_t size,
+				    unsigned int depth);
 
 #endif /*PKCS11_TA_SANITIZE_OBJECT_H*/