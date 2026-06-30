OP-TEE/optee_os

commit c34d0d917ff5f71c2f7c10d677c769c428ee90bc

core: support loading TAs signed with a subkey
Adds support to load TAs signed with subkey or a chain of subkeys. This
allows delegation of TA signing without distributing the root key. TAs
signed with a subkey are confined to the UUID-V5 namespace of the subkey
to avoid TA UUID clashes with different subkeys.

SHDR_SUBKEY is a type of header which enables chains of public keys.
The public root key is used to verify the first public subkey, which
then is used to verify the next public subkey and so on.

The TA is finally verified using the last subkey. All these headers are
added in front of the TA binary so everything needed to verify the TA is
available when it's loaded into memory.

For example:
Subkey
 struct shdr
  magic:      0x4f545348
  img_type:   3 (SHDR_SUBKEY)
  img_size:   320 bytes
  algo:       0x70414930 (TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256)
  hash_size:  32 bytes
  sig_size:   256 bytes
  hash:       f573f329fe77be686ce71647909c4ea35b5e1cd7de86369bd7d9fca31f6a4d65
 struct shdr_subkey
  uuid:       f04fa996-148a-453c-b037-1dcfbad120a6
  name_size:  64
  subkey_version: 1
  max_depth:  4
  algo:       0x70414930 (TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256)
  attr_count: 2
  next name:  "mid_level_subkey"
Next header at offset: 692 (0x2b4)
Subkey
 struct shdr
  magic:      0x4f545348
  img_type:   3 (SHDR_SUBKEY)
  img_size:   320 bytes
  algo:       0x70414930 (TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256)
  hash_size:  32 bytes
  sig_size:   256 bytes
  hash:       233a6dcf1a2cf69e50cde8e20c4129157da707c76fa86ce12ee31037edef02d7
 struct shdr_subkey
  uuid:       1a5948c5-1aa0-518c-86f4-be6f6a057b16
  name_size:  64
  subkey_version: 1
  max_depth:  3
  algo:       0x70414930 (TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256)
  attr_count: 2
  next name:  "subkey1_ta"
Next header at offset: 1384 (0x568)
Bootstrap TA
 struct shdr
  magic:      0x4f545348
  img_type:   1 (SHDR_BOOTSTRAP_TA)
  img_size:   84576 bytes

core/crypto/signed_hdr.c
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2015-2017, Linaro Limited
+ * Copyright (c) 2015-2022, Linaro Limited
  */
 
 #include <crypto/crypto.h>
+#include <kernel/panic.h>
+#include <mempool.h>
 #include <signed_hdr.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ta_pub_key.h>
 #include <tee_api_types.h>
 #include <tee/tee_cryp_utl.h>
+#include <tee/uuid.h>
 #include <utee_defines.h>
 #include <util.h>
 
@@ -85,5 +88,268 @@ out:
 	crypto_acipher_free_rsa_public_key(&key);
 	if (res)
 		return TEE_ERROR_SECURITY;
+	return TEE_SUCCESS;
+}
+
+static const struct shdr_subkey_attr *
+find_attr(const struct shdr_subkey *subkey, uint32_t id)
+{
+	size_t n = 0;
+
+	for (n = 0; n < subkey->attr_count; n++)
+		if (subkey->attrs[n].id == id)
+			return subkey->attrs + n;
+
+	return NULL;
+}
+
+static TEE_Result load_rsa_key(const struct shdr_subkey *subkey,
+			       struct rsa_public_key **key_pp)
+{
+	const uint8_t *base = (const uint8_t *)subkey;
+	const struct shdr_subkey_attr *pub_exp = NULL;
+	const struct shdr_subkey_attr *modulus = NULL;
+	struct rsa_public_key *key = NULL;
+	TEE_Result res = TEE_SUCCESS;
+
+	pub_exp = find_attr(subkey, TEE_ATTR_RSA_PUBLIC_EXPONENT);
+	if (!pub_exp)
+		return TEE_ERROR_SECURITY;
+	modulus = find_attr(subkey, TEE_ATTR_RSA_MODULUS);
+	if (!modulus)
+		return TEE_ERROR_SECURITY;
+
+	key = calloc(1, sizeof(*key));
+	if (!key)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	res = crypto_acipher_alloc_rsa_public_key(key, modulus->size * 8);
+	if (res)
+		goto err_key;
+
+	res = crypto_bignum_bin2bn(base + pub_exp->offs, pub_exp->size, key->e);
+	if (res)
+		goto err;
+	res = crypto_bignum_bin2bn(base + modulus->offs, modulus->size, key->n);
+	if (res)
+		goto err;
+
+	*key_pp = key;
+	return TEE_SUCCESS;
+err:
+	crypto_acipher_free_rsa_public_key(key);
+err_key:
+	free(key);
+	return TEE_ERROR_SECURITY;
+}
+
+static TEE_Result check_attrs(const struct shdr_subkey *subkey, size_t img_size)
+{
+	const struct shdr_subkey_attr *attrs = subkey->attrs;
+	size_t end = 0;
+	size_t n = 0;
+
+	if (MUL_OVERFLOW(subkey->attr_count, sizeof(*attrs), &end) ||
+	    ADD_OVERFLOW(end, sizeof(*subkey), &end) ||
+	    end > img_size)
+		return TEE_ERROR_SECURITY;
+
+	for (n = 0; n < subkey->attr_count; n++)
+		if (ADD_OVERFLOW(attrs[n].offs, attrs[n].size, &end) ||
+		    end > img_size)
+			return TEE_ERROR_SECURITY;
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result calc_next_uuid(uint8_t uuid[sizeof(TEE_UUID)],
+				 const uint8_t my_uuid[sizeof(TEE_UUID)],
+				 const void *ns_name, size_t name_size)
+{
+	TEE_Result res = TEE_ERROR_SECURITY;
+	void *ctx = NULL;
+	struct {
+		uint8_t digest[TEE_SHA1_HASH_SIZE];
+		TEE_UUID uuid;
+		char name_str[];
+	} *tmp = NULL;
+
+	if (!name_size) {
+		memcpy(uuid, my_uuid, sizeof(TEE_UUID));
+		return TEE_SUCCESS;
+	}
+
+	/*
+	 * RFC 4122 requires a SHA-1 digest for UUID v5. Use SHA-512
+	 * instead for better collision resistance.
+	 */
+	if (crypto_hash_alloc_ctx(&ctx, TEE_ALG_SHA512))
+		return TEE_ERROR_SECURITY;
+
+	tmp = mempool_alloc(mempool_default, sizeof(*tmp) + name_size);
+	if (!tmp)
+		goto out_ctx;
+	memcpy(tmp->name_str, ns_name, name_size);
+
+	if (crypto_hash_init(ctx) ||
+	    crypto_hash_update(ctx, my_uuid, sizeof(TEE_UUID)) ||
+	    crypto_hash_update(ctx, (const void *)tmp->name_str,
+			       strnlen(tmp->name_str, name_size)) ||
+	    crypto_hash_final(ctx, tmp->digest, sizeof(tmp->digest)))
+		goto out_mempool;
+
+	tee_uuid_from_octets(&tmp->uuid, tmp->digest);
+	/*
+	 * Set the four most significant bits (bits 12 through 15) of the
+	 * time_hi_and_version field to 5.
+	 */
+	tmp->uuid.timeHiAndVersion &= ~SHIFT_U32(0xf, 12);
+	tmp->uuid.timeHiAndVersion |= SHIFT_U32(5, 12);
+	/*
+	 * Set the two most significant bits (bits 6 and 7) of the
+	 * clock_seq_hi_and_reserved to zero and one, respectively.
+	 */
+	tmp->uuid.clockSeqAndNode[0] &= ~BIT(6);
+	tmp->uuid.clockSeqAndNode[0] |= BIT(7);
+
+	tee_uuid_to_octets(uuid, &tmp->uuid);
+	res = TEE_SUCCESS;
+
+out_mempool:
+	mempool_free(mempool_default, tmp);
+out_ctx:
+	crypto_hash_free_ctx(ctx);
+
+	return res;
+}
+
+TEE_Result shdr_load_pub_key(const struct shdr *shdr, size_t offs,
+			     const uint8_t *ns_img, size_t ns_img_size,
+			     const uint8_t next_uuid[sizeof(TEE_UUID)],
+			     uint32_t max_depth, struct shdr_pub_key *key)
+{
+	struct shdr_subkey *subkey = NULL;
+	TEE_Result res = TEE_SUCCESS;
+	void *digest = NULL;
+	uint8_t *img = NULL;
+	void *ctx = NULL;
+	size_t end = 0;
+
+	if (shdr->img_type != SHDR_SUBKEY)
+		return TEE_ERROR_SECURITY;
+
+	if (shdr->img_size < sizeof(*subkey))
+		return TEE_ERROR_SECURITY;
+
+	if (ADD_OVERFLOW(shdr->img_size, offs, &end) || end > ns_img_size)
+		return TEE_ERROR_SECURITY;
+
+	img = mempool_alloc(mempool_default, shdr->img_size + shdr->hash_size);
+	if (!img)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	memcpy(img + shdr->hash_size, ns_img + offs, shdr->img_size);
+	subkey = (void *)(img + shdr->hash_size);
+	digest = img;
+
+	if (crypto_hash_alloc_ctx(&ctx, TEE_DIGEST_HASH_TO_ALGO(shdr->algo))) {
+		res = TEE_ERROR_SECURITY;
+		goto out_mempool;
+	}
+
+	if (crypto_hash_init(ctx) ||
+	    crypto_hash_update(ctx, (const void *)shdr, sizeof(*shdr)) ||
+	    crypto_hash_update(ctx, (const void *)subkey, shdr->img_size) ||
+	    crypto_hash_final(ctx, digest, shdr->hash_size) ||
+	    memcmp(digest, SHDR_GET_HASH(shdr), shdr->hash_size)) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+
+	res = check_attrs(subkey, shdr->img_size);
+	if (res)
+		goto out_ctx;
+
+	if (subkey->max_depth >= max_depth) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+	if (next_uuid && memcmp(next_uuid, subkey->uuid, sizeof(TEE_UUID))) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+
+	key->max_depth = subkey->max_depth;
+	key->name_size = subkey->name_size;
+	memcpy(key->uuid, subkey->uuid, sizeof(TEE_UUID));
+	if (ADD_OVERFLOW(key->name_size, offs + shdr->img_size, &end) ||
+	    end > ns_img_size) {
+		res = TEE_ERROR_SECURITY;
+		goto out_ctx;
+	}
+	res = calc_next_uuid(key->next_uuid, key->uuid,
+			     ns_img + offs + shdr->img_size, key->name_size);
+	if (res)
+		goto out_ctx;
+
+	key->main_algo = TEE_ALG_GET_MAIN_ALG(subkey->algo);
+	switch (key->main_algo) {
+	case TEE_MAIN_ALGO_RSA:
+		res = load_rsa_key(subkey, &key->pub_key.rsa);
+		break;
+	default:
+		res = TEE_ERROR_SECURITY;
+		break;
+	}
+
+out_ctx:
+	crypto_hash_free_ctx(ctx);
+out_mempool:
+	mempool_free(mempool_default, img);
+	return res;
+}
+
+void shdr_free_pub_key(struct shdr_pub_key *key)
+{
+	if (key) {
+		switch (key->main_algo) {
+		case TEE_MAIN_ALGO_RSA:
+			crypto_acipher_free_rsa_public_key(key->pub_key.rsa);
+			free(key->pub_key.rsa);
+			break;
+		default:
+			panic();
+		}
+	}
+}
+
+TEE_Result shdr_verify_signature2(struct shdr_pub_key *key,
+				  const struct shdr *shdr)
+{
+	size_t hash_size = 0;
+
+	if (shdr->magic != SHDR_MAGIC)
+		return TEE_ERROR_SECURITY;
+
+	if (TEE_ALG_GET_MAIN_ALG(shdr->algo) != key->main_algo)
+		return TEE_ERROR_SECURITY;
+
+	if (tee_alg_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(shdr->algo),
+				    &hash_size) ||
+	    hash_size != shdr->hash_size)
+		return TEE_ERROR_SECURITY;
+
+	switch (key->main_algo) {
+	case TEE_MAIN_ALGO_RSA:
+		if (crypto_acipher_rsassa_verify(shdr->algo, key->pub_key.rsa,
+						 shdr->hash_size,
+						 SHDR_GET_HASH(shdr),
+						 shdr->hash_size,
+						 SHDR_GET_SIG(shdr),
+						 shdr->sig_size))
+			return TEE_ERROR_SECURITY;
+		break;
+	default:
+		panic();
+	}
+
 	return TEE_SUCCESS;
 }

core/include/signed_hdr.h
@@ -14,6 +14,7 @@ enum shdr_img_type {
 	SHDR_TA = 0,
 	SHDR_BOOTSTRAP_TA = 1,
 	SHDR_ENCRYPTED_TA = 2,
+	SHDR_SUBKEY = 3,
 };
 
 #define SHDR_MAGIC	0x4f545348
@@ -63,6 +64,62 @@ static inline size_t shdr_get_size(const struct shdr *shdr)
 #define SHDR_GET_SIZE(x)	shdr_get_size((x))
 #define SHDR_GET_HASH(x)	(uint8_t *)(((struct shdr *)(x)) + 1)
 #define SHDR_GET_SIG(x)		(SHDR_GET_HASH(x) + (x)->hash_size)
+
+/**
+ * struct shdr_subkey - subkey header
+ * @uuid:		UUID of the subkey
+ * @name_size:		The size of a name field that follows right
+ *			after this header, before the next signed header.
+ * @subkey_version:	Version of the subkey
+ * @max_depth:		Maximum depth supported below this subkey
+ * @algo:		Algorithm, defined by public key algorithms TEE_ALG_*
+ *			from TEE Internal API specification
+ * @attr_count:		Number of attributes for the public key matching
+ *			@algo.
+ * @attrs:		Attributes for the public key matching @algo.
+ * @attrs[].id:		Attribute ID TEE_ATTR_* from GlobalPlatform
+ * @attrs[].offs:	Offset of the attribute value from start of
+ *			struct shdr_subkey
+ * @attrs[].size:	Attribute size
+ *
+ * The @uuid defines UUID URN Namespace (RFC4122), the next UUID after this
+ * header (another subkey or a TA) must be in the namespace of this UUID.
+ * This means that further subkeys or TAs have their UUID fixed in the
+ * hierarchy and cannot be moved up or below another subkey.
+ *
+ * If @name_size is non-zero it indicates that a name field of this size
+ * exists and is used to generate the UUID of the following TA or subkey.
+ * If it's zero the following TA or subkey must have a matching UUID.
+ *
+ * The @subkey_version field is used as a rollback measure. The version is
+ * checked against earlier saved values of this subkey. If the latest known
+ * version is less than this the stored value is updated. If the latest
+ * known version is larger than this then the subkey is refused.
+ *
+ * The @max_depth defines how many levels are allowed below this subkey,
+ * the value 0 means only TAs are allowed below. The value 1 means that
+ * eventual subkeys below must have the value 0 in their @max_depth field.
+ *
+ * Each attribute of @attrs must be within range of the image size of this
+ * header defined in the preceding struct shdr.
+ *
+ * The next struct shdr is found right after the indicated end of the
+ * previous struct shdr. Signature verification starts over with the
+ * next struct shdr using this subkey instead of the root key.
+ */
+struct shdr_subkey {
+	uint8_t uuid[sizeof(TEE_UUID)];
+	uint32_t name_size;
+	uint32_t subkey_version;
+	uint32_t max_depth;
+	uint32_t algo;
+	uint32_t attr_count;
+	struct shdr_subkey_attr {
+		uint32_t id;
+		uint32_t offs;
+		uint32_t size;
+	} attrs[];
+};
 
 /**
  * struct shdr_bootstrap_ta - bootstrap TA subheader
@@ -137,6 +194,26 @@ static inline void shdr_free(struct shdr *shdr)
 {
 	free(shdr);
 }
+
+struct shdr_pub_key {
+	uint32_t main_algo;
+	uint8_t uuid[sizeof(TEE_UUID)];
+	uint8_t next_uuid[sizeof(TEE_UUID)];
+	uint32_t max_depth;
+	uint32_t name_size;
+	uint32_t version;
+	union {
+		struct rsa_public_key *rsa;
+	} pub_key;
+};
+
+TEE_Result shdr_load_pub_key(const struct shdr *shdr, size_t offs,
+			     const uint8_t *ns_img, size_t ns_img_size,
+			     const uint8_t next_uuid[sizeof(TEE_UUID)],
+			     uint32_t max_depth, struct shdr_pub_key *key);
+void shdr_free_pub_key(struct shdr_pub_key *key);
+TEE_Result shdr_verify_signature2(struct shdr_pub_key *key,
+				  const struct shdr *shdr);
 
 /*
  * Verifies the signature in the @shdr.

core/kernel/ree_fs_ta.c
@@ -78,7 +78,107 @@ struct ver_db_hdr {
 };
 
 static const char ta_ver_db[] = "ta_ver.db";
+static const char subkey_ver_db[] = "subkey_ver.db";
 static struct mutex ver_db_mutex = MUTEX_INITIALIZER;
+
+static TEE_Result check_update_version(const char *db_name,
+				       const uint8_t uuid[sizeof(TEE_UUID)],
+				       uint32_t version)
+{
+	struct ver_db_entry db_entry = { };
+	const struct tee_file_operations *ops = NULL;
+	struct tee_file_handle *fh = NULL;
+	TEE_Result res = TEE_SUCCESS;
+	bool entry_found = false;
+	size_t len = 0;
+	unsigned int i = 0;
+	struct ver_db_hdr db_hdr = { };
+	struct tee_pobj pobj = {
+		.obj_id = (void *)db_name,
+		.obj_id_len = strlen(db_name) + 1,
+	};
+
+	ops = tee_svc_storage_file_ops(TEE_STORAGE_PRIVATE);
+	if (!ops)
+		return TEE_SUCCESS; /* Compiled with no secure storage */
+
+	mutex_lock(&ver_db_mutex);
+
+	res = ops->open(&pobj, NULL, &fh);
+	if (res != TEE_SUCCESS && res != TEE_ERROR_ITEM_NOT_FOUND)
+		goto out;
+
+	if (res == TEE_ERROR_ITEM_NOT_FOUND) {
+		res = ops->create(&pobj, false, NULL, 0, NULL, 0, NULL, 0, &fh);
+		if (res != TEE_SUCCESS)
+			goto out;
+
+		res = ops->write(fh, 0, &db_hdr, sizeof(db_hdr));
+		if (res != TEE_SUCCESS)
+			goto out;
+	} else {
+		len = sizeof(db_hdr);
+
+		res = ops->read(fh, 0, &db_hdr, &len);
+		if (res != TEE_SUCCESS) {
+			goto out;
+		} else if (len != sizeof(db_hdr)) {
+			res = TEE_ERROR_BAD_STATE;
+			goto out;
+		}
+	}
+
+	for (i = 0; i < db_hdr.nb_entries; i++) {
+		len = sizeof(db_entry);
+
+		res = ops->read(fh, sizeof(db_hdr) + (i * len), &db_entry,
+				&len);
+		if (res != TEE_SUCCESS) {
+			goto out;
+		} else if (len != sizeof(db_entry)) {
+			res = TEE_ERROR_BAD_STATE;
+			goto out;
+		}
+
+		if (!memcmp(uuid, db_entry.uuid, sizeof(TEE_UUID))) {
+			entry_found = true;
+			break;
+		}
+	}
+
+	if (entry_found) {
+		if (db_entry.version > version) {
+			res = TEE_ERROR_ACCESS_CONFLICT;
+			goto out;
+		} else if (db_entry.version < version) {
+			memcpy(db_entry.uuid, uuid, sizeof(TEE_UUID));
+			db_entry.version = version;
+			len = sizeof(db_entry);
+			res = ops->write(fh, sizeof(db_hdr) + (i * len),
+					 &db_entry, len);
+			if (res != TEE_SUCCESS)
+				goto out;
+		}
+	} else {
+		memcpy(db_entry.uuid, uuid, sizeof(TEE_UUID));
+		db_entry.version = version;
+		len = sizeof(db_entry);
+		res = ops->write(fh, sizeof(db_hdr) + (db_hdr.nb_entries * len),
+				 &db_entry, len);
+		if (res != TEE_SUCCESS)
+			goto out;
+
+		db_hdr.nb_entries++;
+		res = ops->write(fh, 0, &db_hdr, sizeof(db_hdr));
+		if (res != TEE_SUCCESS)
+			goto out;
+	}
+
+out:
+	ops->close(&fh);
+	mutex_unlock(&ver_db_mutex);
+	return res;
+}
 
 /*
  * Load a TA via RPC with UUID defined by input param @uuid. The virtual
@@ -132,7 +232,9 @@ exit:
 static TEE_Result ree_fs_ta_open(const TEE_UUID *uuid,
 				 struct ts_store_handle **h)
 {
+	uint8_t next_uuid[sizeof(TEE_UUID)] = { };
 	struct ree_fs_ta_handle *handle;
+	uint8_t *next_uuid_ptr = NULL;
 	struct shdr *shdr = NULL;
 	struct mobj *mobj = NULL;
 	void *hash_ctx = NULL;
@@ -143,6 +245,7 @@ static TEE_Result ree_fs_ta_open(const TEE_UUID *uuid,
 	struct shdr_bootstrap_ta *bs_hdr = NULL;
 	struct shdr_encrypted_ta *ehdr = NULL;
 	size_t shdr_sz = 0;
+	uint32_t max_depth = UINT32_MAX;
 
 	handle = calloc(1, sizeof(*handle));
 	if (!handle)
@@ -164,10 +267,87 @@ static TEE_Result ree_fs_ta_open(const TEE_UUID *uuid,
 	res = shdr_verify_signature(shdr);
 	if (res != TEE_SUCCESS)
 		goto error_free_payload;
+
+	shdr_sz = SHDR_GET_SIZE(shdr);
+	if (!shdr_sz) {
+		res = TEE_ERROR_SECURITY;
+		goto error_free_payload;
+	}
+	offs = shdr_sz;
+
+	while (shdr->img_type == SHDR_SUBKEY) {
+		struct shdr_pub_key pub_key = { };
+
+		if (offs > ta_size) {
+			res = TEE_ERROR_SECURITY;
+			goto error_free_payload;
+		}
+
+		res = shdr_load_pub_key(shdr, offs, (const void *)ta,
+					ta_size, next_uuid_ptr, max_depth,
+					&pub_key);
+		if (res)
+			goto error_free_payload;
+
+		if (ADD_OVERFLOW(offs, shdr->img_size, &offs) ||
+		    ADD_OVERFLOW(offs, pub_key.name_size, &offs) ||
+		    offs > ta_size) {
+			res = TEE_ERROR_SECURITY;
+			goto error_free_payload;
+		}
+		max_depth = pub_key.max_depth;
+		memcpy(next_uuid, pub_key.next_uuid, sizeof(TEE_UUID));
+		next_uuid_ptr = next_uuid;
+
+		res = check_update_version(subkey_ver_db, pub_key.uuid,
+					   pub_key.version);
+		if (res) {
+			res = TEE_ERROR_SECURITY;
+			shdr_free_pub_key(&pub_key);
+			goto error_free_payload;
+		}
+
+		shdr_free(shdr);
+		shdr = shdr_alloc_and_copy(offs, ta, ta_size);
+		res = TEE_ERROR_SECURITY;
+		if (shdr)
+			res = shdr_verify_signature2(&pub_key, shdr);
+		shdr_free_pub_key(&pub_key);
+		if (res)
+			goto error_free_payload;
+
+		shdr_sz = SHDR_GET_SIZE(shdr);
+		if (!shdr_sz) {
+			res = TEE_ERROR_SECURITY;
+			goto error_free_payload;
+		}
+		offs += shdr_sz;
+		if (offs > ta_size) {
+			res = TEE_ERROR_SECURITY;
+			goto error_free_payload;
+		}
+	}
+
 	if (shdr->img_type != SHDR_TA && shdr->img_type != SHDR_BOOTSTRAP_TA &&
 	    shdr->img_type != SHDR_ENCRYPTED_TA) {
 		res = TEE_ERROR_SECURITY;
 		goto error_free_payload;
+	}
+
+	/*
+	 * If we're verifying this TA using a subkey, make sure that
+	 * the UUID of the TA belongs to the namespace defined by the subkey.
+	 * The namespace is defined as in RFC4122, that is, valid UUID
+	 * is calculated as a V5 UUID SHA-512(subkey UUID, "name string").
+	 */
+	if (next_uuid_ptr) {
+		TEE_UUID check_uuid = { };
+
+		tee_uuid_from_octets(&check_uuid, next_uuid_ptr);
+		if (memcmp(&check_uuid, uuid, sizeof(*uuid))) {
+			res = TEE_ERROR_SECURITY;
+			goto error_free_payload;
+		}
 	}
 
 	/*
@@ -184,12 +364,6 @@ static TEE_Result ree_fs_ta_open(const TEE_UUID *uuid,
 	res = crypto_hash_update(hash_ctx, (uint8_t *)shdr, sizeof(*shdr));
 	if (res != TEE_SUCCESS)
 		goto error_free_hash;
-	shdr_sz = SHDR_GET_SIZE(shdr);
-	if (!shdr_sz) {
-		res = TEE_ERROR_SECURITY;
-		goto error_free_hash;
-	}
-	offs = shdr_sz;
 
 	if (shdr->img_type == SHDR_BOOTSTRAP_TA ||
 	    shdr->img_type == SHDR_ENCRYPTED_TA) {
@@ -340,105 +514,6 @@ static TEE_Result check_digest(struct ree_fs_ta_handle *h)
 		res = TEE_ERROR_SECURITY;
 out:
 	free(digest);
-	return res;
-}
-
-static TEE_Result check_update_version(const char *db_name,
-				       const uint8_t uuid[sizeof(TEE_UUID)],
-				       uint32_t version)
-{
-	struct ver_db_entry db_entry = { };
-	const struct tee_file_operations *ops = NULL;
-	struct tee_file_handle *fh = NULL;
-	TEE_Result res = TEE_SUCCESS;
-	bool entry_found = false;
-	size_t len = 0;
-	unsigned int i = 0;
-	struct ver_db_hdr db_hdr = { };
-	struct tee_pobj pobj = {
-		.obj_id = (void *)db_name,
-		.obj_id_len = strlen(db_name) + 1,
-	};
-
-	ops = tee_svc_storage_file_ops(TEE_STORAGE_PRIVATE);
-	if (!ops)
-		return TEE_SUCCESS; /* Compiled with no secure storage */
-
-	mutex_lock(&ver_db_mutex);
-
-	res = ops->open(&pobj, NULL, &fh);
-	if (res != TEE_SUCCESS && res != TEE_ERROR_ITEM_NOT_FOUND)
-		goto out;
-
-	if (res == TEE_ERROR_ITEM_NOT_FOUND) {
-		res = ops->create(&pobj, false, NULL, 0, NULL, 0, NULL, 0, &fh);
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		res = ops->write(fh, 0, &db_hdr, sizeof(db_hdr));
-		if (res != TEE_SUCCESS)
-			goto out;
-	} else {
-		len = sizeof(db_hdr);
-
-		res = ops->read(fh, 0, &db_hdr, &len);
-		if (res != TEE_SUCCESS) {
-			goto out;
-		} else if (len != sizeof(db_hdr)) {
-			res = TEE_ERROR_BAD_STATE;
-			goto out;
-		}
-	}
-
-	for (i = 0; i < db_hdr.nb_entries; i++) {
-		len = sizeof(db_entry);
-
-		res = ops->read(fh, sizeof(db_hdr) + (i * len), &db_entry,
-				&len);
-		if (res != TEE_SUCCESS) {
-			goto out;
-		} else if (len != sizeof(db_entry)) {
-			res = TEE_ERROR_BAD_STATE;
-			goto out;
-		}
-
-		if (!memcmp(uuid, db_entry.uuid, sizeof(TEE_UUID))) {
-			entry_found = true;
-			break;
-		}
-	}
-
-	if (entry_found) {
-		if (db_entry.version > version) {
-			res = TEE_ERROR_ACCESS_CONFLICT;
-			goto out;
-		} else if (db_entry.version < version) {
-			memcpy(db_entry.uuid, uuid, sizeof(TEE_UUID));
-			db_entry.version = version;
-			len = sizeof(db_entry);
-			res = ops->write(fh, sizeof(db_hdr) + (i * len),
-					 &db_entry, len);
-			if (res != TEE_SUCCESS)
-				goto out;
-		}
-	} else {
-		memcpy(db_entry.uuid, uuid, sizeof(TEE_UUID));
-		db_entry.version = version;
-		len = sizeof(db_entry);
-		res = ops->write(fh, sizeof(db_hdr) + (db_hdr.nb_entries * len),
-				 &db_entry, len);
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		db_hdr.nb_entries++;
-		res = ops->write(fh, 0, &db_hdr, sizeof(db_hdr));
-		if (res != TEE_SUCCESS)
-			goto out;
-	}
-
-out:
-	ops->close(&fh);
-	mutex_unlock(&ver_db_mutex);
 	return res;
 }
 

mk/config.mk
@@ -228,6 +228,16 @@ _CFG_WITH_SECURE_STORAGE := $(call cfg-one-enabled,CFG_REE_FS CFG_RPMB_FS)
 TA_SIGN_KEY ?= keys/default_ta.pem
 TA_PUBLIC_KEY ?= $(TA_SIGN_KEY)
 
+# Subkeys is a complement to the normal TA_SIGN_KEY where a subkey is used
+# to verify a TA instead. To sign a TA using a previously prepared subkey
+# two new options are added, TA_SUBKEY_ARGS and TA_SUBKEY_DEPS.  It is
+# typically used by assigning the following in the TA Makefile:
+# BINARY = <TA-uuid-string>
+# TA_SIGN_KEY = subkey.pem
+# TA_SUBKEY_ARGS = --subkey subkey.bin --name subkey_ta
+# TA_SUBKEY_DEPS = subkey.bin
+# See the documentation for more details on subkeys.
+
 # Include lib/libutils/isoc in the build? Most platforms need this, but some
 # may not because they obtain the isoc functions from elsewhere
 CFG_LIBUTILS_WITH_ISOC ?= y

ta/arch/arm/link.mk
@@ -108,10 +108,11 @@ cmd-echo$(user-ta-uuid) := SIGNENC
 endif
 $(link-out-dir$(sm))/$(user-ta-uuid).ta: \
 			$(link-out-dir$(sm))/$(user-ta-uuid).stripped.elf \
-			$(TA_SIGN_KEY) \
+			$(TA_SIGN_KEY) $(TA_SUBKEY_DEPS) \
 			$(lastword $(SIGN_ENC))
 	@$(cmd-echo-silent) '  $$(cmd-echo$(user-ta-uuid)) $$@'
-	$(q)$(SIGN_ENC) --key $(TA_SIGN_KEY) $$(crypt-args$(user-ta-uuid)) \
+	$(q)$(SIGN_ENC) --key $(TA_SIGN_KEY) $(TA_SUBKEY_ARGS) \
+		$$(crypt-args$(user-ta-uuid)) \
 		--uuid $(user-ta-uuid) --ta-version $(user-ta-version) \
 		--in $$< --out $$@
 endef

ta/arch/arm/link_shlib.mk
@@ -48,7 +48,7 @@ $(link-out-dir)/$(shlibuuid).elf: $(link-out-dir)/$(shlibname).so
 	$(q)ln -sf $(<F) $@
 
 $(link-out-dir)/$(shlibuuid).ta: $(link-out-dir)/$(shlibname).stripped.so \
-				$(TA_SIGN_KEY)
+				$(TA_SIGN_KEY) $(TA_SUBKEY_DEPS)
 	@$(cmd-echo-silent) '  SIGN    $@'
-	$(q)$(PYTHON3) $(SIGN) --key $(TA_SIGN_KEY) --uuid $(shlibuuid) \
-		--in $< --out $@
+	$(q)$(PYTHON3) $(SIGN) --key $(TA_SIGN_KEY) $(TA_SUBKEY_ARGS) \
+		--uuid $(shlibuuid) --in $< --out $@

commit 43ed01d216a9586e7e7591dff1f7fbf80923f93f

core: copy subkey_version in shdr_load_pub_key()
Prior to this patch shdr_load_pub_key() didn't copy the subkey version
when loading a subkey, effectively always reporting it as version 0 and
disabling rollback protection. Fix this by extracting the subkey version
when extracting the other field to enable the rollback protection.

Fixes: c34d0d917ff5 ("core: support loading TAs signed with a subkey")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>

core/crypto/signed_hdr.c
@@ -317,6 +317,7 @@ TEE_Result shdr_load_pub_key(const struct shdr *shdr, size_t offs,
 
 	key->max_depth = subkey->max_depth;
 	key->name_size = subkey->name_size;
+	key->version = subkey->subkey_version;
 	memcpy(key->uuid, subkey->uuid, sizeof(TEE_UUID));
 	if (ADD_OVERFLOW(key->name_size, offs + shdr->img_size, &end) ||
 	    end > ns_img_size) {