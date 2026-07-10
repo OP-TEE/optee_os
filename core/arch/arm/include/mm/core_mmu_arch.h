OP-TEE/optee_os

commit 2317a39482d9d26258ac81e74fd451e426f1c38d

core: lpae: search TAs VA base in the regions [1GB, 4GB[
In order to support 32-bit TAs we will have to find
a TAs VA base in the region of [1GB, 4GB[.
The 4GB limit wasn't enforced before this patch.

Signed-off-by: Izik Dubnov <izik@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/mm/core_mmu_lpae.c
@@ -484,6 +484,45 @@ void core_init_mmu_prtn(struct mmu_partition *prtn, struct tee_mmap_region *mm)
 	}
 }
 
+/*
+ * In order to support 32-bit TAs we will have to find
+ * a user VA base in the region [1GB, 4GB[.
+ * Due to OP-TEE design limitation, TAs page table should be an entry
+ * inside a level 1 page table.
+ *
+ * Available options are only these:
+ * - base level 0 entry 0 - [0GB, 512GB[
+ *   - level 1 entry 0 - [0GB, 1GB[
+ *   - level 1 entry 1 - [1GB, 2GB[           <----
+ *   - level 1 entry 2 - [2GB, 3GB[           <----
+ *   - level 1 entry 3 - [3GB, 4GB[           <----
+ *   - level 1 entry 4 - [4GB, 5GB[
+ *   - ...
+ * - ...
+ */
+static void set_user_va_idx(struct mmu_partition *prtn)
+{
+	uint64_t *tbl = NULL;
+	unsigned int n = 0;
+
+	assert(prtn);
+
+	tbl = prtn->base_tables[0][get_core_pos()];
+
+	/*
+	 * Search level 1 table (i.e. 1GB mapping per entry) for
+	 * an empty entry in the range [1GB, 4GB[.
+	 */
+	for (n = 1; n < 4; n++) {
+		if ((tbl[n] & DESC_ENTRY_TYPE_MASK) == INVALID_DESC) {
+			user_va_idx = n;
+			break;
+		}
+	}
+
+	assert(user_va_idx != -1);
+}
+
 void core_init_mmu(struct tee_mmap_region *mm)
 {
 	uint64_t max_va = 0;
@@ -507,13 +546,7 @@ void core_init_mmu(struct tee_mmap_region *mm)
 			max_va = va_end;
 	}
 
-	for (n = 1; n < NUM_BASE_LEVEL_ENTRIES; n++) {
-		if (!default_partition.base_tables[0][0][n]) {
-			user_va_idx = n;
-			break;
-		}
-	}
-	assert(user_va_idx != -1);
+	set_user_va_idx(&default_partition);
 
 	COMPILE_TIME_ASSERT(CFG_LPAE_ADDR_SPACE_BITS > L1_XLAT_ADDRESS_SHIFT);
 	assert(max_va < BIT64(CFG_LPAE_ADDR_SPACE_BITS));

commit 8b4e2d1c03559f4430bcd0e095079c6c786fadba

core: lpae: retry ASLR when no user mapping entry remains
With LPAE, the user mapping must occupy an unused level 1 entry in the
32-bit VA range [1GB, 4GB[. An identity mapping can consume one of these
entries while a randomized core mapping crosses a 1GB boundary and
consumes the other two. The mappings do not overlap, so init_mem_map()
accepts the layout, but set_user_va_idx() later asserts because no entry
remains for a user mapping.

Add an architecture-specific check for each tentative ASLR memory map,
including the pending identity mapping. Reject candidates that leave no
room for a user mapping and retry with the next candidate. Non-LPAE
configurations retain their existing behavior.

Fixes: 2317a39482d9 ("core: lpae: search TAs VA base in the regions [1GB, 4GB[")
Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
Reviewed-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

core/arch/arm/include/mm/core_mmu_arch.h
@@ -206,6 +206,19 @@ static inline bool core_mmu_va_is_valid(vaddr_t va)
 	return va < BIT64(core_mmu_get_va_width());
 }
 
+#ifdef CFG_WITH_LPAE
+bool arch_mem_map_allows_user_va(const struct memory_map *mem_map,
+				 vaddr_t id_map_start, vaddr_t id_map_end);
+#else
+static inline bool
+arch_mem_map_allows_user_va(const struct memory_map *mem_map __unused,
+			    vaddr_t id_map_start __unused,
+			    vaddr_t id_map_end __unused)
+{
+	return true;
+}
+#endif
+
 static inline bool core_mmu_level_in_range(unsigned int level)
 {
 #if CORE_MMU_BASE_TABLE_LEVEL == 0

core/arch/arm/mm/core_mmu_lpae.c
@@ -68,6 +68,7 @@
 #include <kernel/linker.h>
 #include <kernel/misc.h>
 #include <kernel/panic.h>
+#include <kernel/tee_misc.h>
 #include <kernel/thread.h>
 #include <kernel/tlb_helpers.h>
 #include <memtag.h>
@@ -290,6 +291,13 @@ static uint64_t xlat_tables_ul1[XLAT_TABLE_SIZE * CFG_NUM_THREADS /
 static l1_idx_t user_l1_table_idx[NUM_BASE_TABLES * CFG_TEE_CORE_NB_CORE];
 #endif
 #endif
+
+/*
+ * Level 1 entries at this index and above start outside the 32-bit VA space.
+ * Use it as the exclusive upper bound when selecting a slot for 32-bit TAs.
+ */
+#define ARM32_USER_VA_SLOT_LIMIT \
+	((UINT32_MAX >> L1_XLAT_ADDRESS_SHIFT) + 1)
 
 /*
  * TAs page table entry inside a level 1 page table.
@@ -887,6 +895,37 @@ static void core_init_mmu_prtn_tee(struct mmu_partition *prtn,
 	}
 }
 
+bool arch_mem_map_allows_user_va(const struct memory_map *mem_map,
+				 vaddr_t id_map_start, vaddr_t id_map_end)
+{
+	const size_t slot_size = BIT64(L1_XLAT_ADDRESS_SHIFT);
+	vaddr_t id_start = ROUNDDOWN(id_map_start, SMALL_PAGE_SIZE);
+	size_t id_size = ROUNDUP(id_map_end, SMALL_PAGE_SIZE) - id_start;
+	unsigned int slot = 0;
+	size_t n = 0;
+
+	/*
+	 * The user mapping must occupy an unused level 1 entry in the 32-bit
+	 * VA range [1GB, 4GB[. Account for the identity mapping too, since it
+	 * has not yet been added to @mem_map.
+	 */
+	for (slot = 1; slot < ARM32_USER_VA_SLOT_LIMIT; slot++) {
+		vaddr_t slot_base = (vaddr_t)slot * slot_size;
+		bool used = core_is_buffer_intersect(slot_base, slot_size,
+						     id_start, id_size);
+
+		for (n = 0; !used && n < mem_map->count; n++)
+			used = core_is_buffer_intersect(slot_base, slot_size,
+							mem_map->map[n].va,
+							mem_map->map[n].size);
+
+		if (!used)
+			return true;
+	}
+
+	return false;
+}
+
 /*
  * In order to support 32-bit TAs we will have to find
  * a user VA base in the region [1GB, 4GB[.
@@ -944,7 +983,7 @@ static void set_user_va_idx(struct mmu_partition *prtn)
 	 * Search level 1 table (i.e. 1GB mapping per entry) for
 	 * an empty entry in the range [1GB, 4GB[.
 	 */
-	for (n = 1; n < 4; n++) {
+	for (n = 1; n < ARM32_USER_VA_SLOT_LIMIT; n++) {
 		if ((tbl[n] & DESC_ENTRY_TYPE_MASK) == INVALID_DESC) {
 			user_va_idx = n;
 			break;

core/arch/riscv/include/mm/core_mmu_arch.h
@@ -223,6 +223,19 @@ static inline bool core_mmu_va_is_valid(vaddr_t va)
 #endif
 }
 
+static inline bool
+arch_mem_map_allows_user_va(const struct memory_map *mem_map __unused,
+			    vaddr_t id_map_start __unused,
+			    vaddr_t id_map_end __unused)
+{
+	/*
+	 * RV32 TAs on an RV64 core aren't currently supported, so the user VA
+	 * isn't restricted to the 32-bit VA range. The final selector may use
+	 * any unused VPN[2] entry without an additional layout constraint.
+	 */
+	return true;
+}
+
 static inline bool core_mmu_level_in_range(unsigned int level)
 {
 	return level <= CORE_MMU_BASE_TABLE_LEVEL;

core/mm/core_mmu.c
@@ -1540,6 +1540,9 @@ static struct memory_map *init_mem_map(struct memory_map *mem_map,
 		for (n = 0; n < 3; n++) {
 			ba = arch_aslr_base_addr(start_addr, seed, n);
 			if (assign_mem_va(ba, mem_map) &&
+			    arch_mem_map_allows_user_va(mem_map,
+							id_map_start,
+							id_map_end) &&
 			    mem_map_add_id_map(mem_map, id_map_start,
 					       id_map_end)) {
 				offs = ba - start_addr;