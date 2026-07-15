0164f1472593fbd0d0186c3d59ae1d17afcb1d03
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2026-07-15 15:47:56 +0200

    pta: qcom: pas: kodiak: rename venus to iris
    The subsystem manages the Iris video core, so name the file and its
    symbols accordingly to match the hardware it drives.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Acked-by: Jerome Forissier <jerome.forissier@arm.com>

--- core/pta/qcom/pas/platform/kodiak/iris.h
+++ core/pta/qcom/pas/platform/kodiak/iris.h
@@ -3,14 +3,14 @@
  * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
-#ifndef _VENUS_H_
-#define _VENUS_H_
+#ifndef _IRIS_H_
+#define _IRIS_H_
 
 #include "pas_subsys.h"
 
 #define IRIS_WRAPPER_TOP_TZ_REG_BASE	0x000c0000
 #define IRIS_WRAPPER_TOP_REG_BASE	0x000b0000
 
-extern const struct qcom_pas_ops venus_ops;
+extern const struct qcom_pas_ops iris_ops;
 
-#endif /* _VENUS_H_ */
+#endif /* _IRIS_H_ */
