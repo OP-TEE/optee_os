OP-TEE/optee_os

commit f12843460d47d97ed5b33bd11e9c7c49b0928169

core: mm: allocate temporary memory map array
With CFG_BOOT_MEM enabled, allocate a temporary memory map array using
boot_mem_alloc_tmp() instead of using the global static_mmap_regions[].
core_mmu_save_mem_map() is added and called from
boot_init_primary_late() before the temporary memory is reused.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/mm/core_mmu.c
@@ -62,6 +62,9 @@ unsigned long default_nsec_shm_size __nex_bss;
 unsigned long default_nsec_shm_paddr __nex_bss;
 #endif
 
+#ifdef CFG_BOOT_MEM
+static struct memory_map static_memory_map __nex_bss;
+#else
 static struct tee_mmap_region static_mmap_regions[CFG_MMAP_REGIONS
 #if defined(CFG_CORE_ASLR) || defined(CFG_CORE_PHYS_RELOCATABLE)
 						+ 1
@@ -71,6 +74,8 @@ static struct memory_map static_memory_map __nex_data = {
 	.map = static_mmap_regions,
 	.alloc_count = ARRAY_SIZE(static_mmap_regions),
 };
+#endif
+void (*memory_map_realloc_func)(struct memory_map *mem_map) __nex_bss;
 
 /* Offset of the first TEE RAM mapping from start of secure RAM */
 static size_t tee_ram_initial_offs __nex_bss;
@@ -126,11 +131,43 @@ static void mmu_unlock(uint32_t exceptions)
 	cpu_spin_unlock_xrestore(&mmu_spinlock, exceptions);
 }
 
+static void heap_realloc_memory_map(struct memory_map *mem_map)
+{
+	struct tee_mmap_region *m = NULL;
+	struct tee_mmap_region *old = mem_map->map;
+	size_t old_sz = sizeof(*old) * mem_map->alloc_count;
+	size_t sz = old_sz + sizeof(*m);
+
+	assert(nex_malloc_buffer_is_within_alloced(old, old_sz));
+	m = nex_realloc(old, sz);
+	if (!m)
+		panic();
+	mem_map->map = m;
+	mem_map->alloc_count++;
+}
+
+static void boot_mem_realloc_memory_map(struct memory_map *mem_map)
+{
+	struct tee_mmap_region *m = NULL;
+	struct tee_mmap_region *old = mem_map->map;
+	size_t old_sz = sizeof(*old) * mem_map->alloc_count;
+	size_t sz = old_sz * 2;
+
+	m = boot_mem_alloc_tmp(sz, alignof(*m));
+	memcpy(m, old, old_sz);
+	mem_map->map = m;
+	mem_map->alloc_count *= 2;
+}
+
 static void grow_mem_map(struct memory_map *mem_map)
 {
 	if (mem_map->count == mem_map->alloc_count) {
-		EMSG("Out of entries (%zu) in mem_map", mem_map->alloc_count);
-		panic();
+		if (!memory_map_realloc_func) {
+			EMSG("Out of entries (%zu) in mem_map",
+			     mem_map->alloc_count);
+			panic();
+		}
+		memory_map_realloc_func(mem_map);
 	}
 	mem_map->count++;
 }
@@ -1576,7 +1613,16 @@ void __weak core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg)
 
 	check_sec_nsec_mem_config();
 
-	mem_map = static_memory_map;
+	if (IS_ENABLED(CFG_BOOT_MEM)) {
+		mem_map.alloc_count = CFG_MMAP_REGIONS;
+		mem_map.map = boot_mem_alloc_tmp(mem_map.alloc_count *
+							sizeof(*mem_map.map),
+						 alignof(*mem_map.map));
+		memory_map_realloc_func = boot_mem_realloc_memory_map;
+	} else {
+		mem_map = static_memory_map;
+	}
+
 	static_memory_map = (struct memory_map){
 		.map = &tmp_mmap_region,
 		.alloc_count = 1,
@@ -1603,6 +1649,25 @@ void __weak core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg)
 	core_init_mmu_regs(cfg);
 	cfg->map_offset = offs;
 	static_memory_map = mem_map;
+	boot_mem_add_reloc(&static_memory_map.map);
+}
+
+void core_mmu_save_mem_map(void)
+{
+	if (IS_ENABLED(CFG_BOOT_MEM)) {
+		size_t alloc_count = static_memory_map.count + 5;
+		size_t elem_sz = sizeof(*static_memory_map.map);
+		void *p = NULL;
+
+		p = nex_calloc(alloc_count, elem_sz);
+		if (!p)
+			panic();
+		memcpy(p, static_memory_map.map,
+		       static_memory_map.count * elem_sz);
+		static_memory_map.map = p;
+		static_memory_map.alloc_count = alloc_count;
+		memory_map_realloc_func = heap_realloc_memory_map;
+	}
 }
 
 bool core_mmu_mattr_is_ok(uint32_t mattr)

core/arch/arm/kernel/boot.c
@@ -938,6 +938,7 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
 #endif
 	IMSG_RAW("\n");
 
+	core_mmu_save_mem_map();
 	if (IS_ENABLED(CFG_NS_VIRTUALIZATION)) {
 		IMSG("Initializing virtualization support");
 		core_mmu_init_virtualization();

core/include/mm/core_mmu.h
@@ -295,6 +295,11 @@ extern const unsigned long core_mmu_tee_load_pa;
 
 void core_init_mmu_map(unsigned long seed, struct core_mmu_config *cfg);
 void core_init_mmu_regs(struct core_mmu_config *cfg);
+/*
+ * Copy static memory map from temporary boot_mem to heap when CFG_BOOT_MEM
+ * is enabled.
+ */
+void core_mmu_save_mem_map(void);
 
 /* Arch specific function to help optimizing 1 MMU xlat table */
 bool core_mmu_prefer_tee_ram_at_top(paddr_t paddr);

commit fdc1195749c50138936a8a0ae7dc0572fdfdd65f

core: mm: grow memory map in core_mmu_add_mapping()
core_mmu_save_mem_map() reserves five extra entries when moving the
memory map to heap storage. core_mmu_add_mapping() currently returns
NULL when these entries are exhausted, even though grow_mem_map() can
expand the heap-backed memory map.

Use grow_mem_map() before appending a new mapping. Since growing the
map may reallocate its backing array, save the index of the
MEM_AREA_RES_VASPACE entry and refresh its pointer afterwards.

This was tested on an ARM32 platform with more than five late mappings.

Fixes: f12843460d47 ("core: mm: allocate temporary memory map array")
Signed-off-by: Miaobing Ye <yemiaobing@gmail.com>

core/mm/core_mmu.c
@@ -268,9 +268,10 @@ TEE_Result core_mmu_for_each_map(void *ptr,
 	return TEE_SUCCESS;
 }
 
-static struct tee_mmap_region *find_map_by_type(enum teecore_memtypes type)
+static struct tee_mmap_region *
+find_map_by_type_in_map(struct memory_map *mem_map,
+			enum teecore_memtypes type)
 {
-	struct memory_map *mem_map = get_memory_map();
 	size_t n = 0;
 
 	for (n = 0; n < mem_map->count; n++) {
@@ -278,6 +279,11 @@ static struct tee_mmap_region *find_map_by_type(enum teecore_memtypes type)
 			return mem_map->map + n;
 	}
 	return NULL;
+}
+
+static struct tee_mmap_region *find_map_by_type(enum teecore_memtypes type)
+{
+	return find_map_by_type_in_map(get_memory_map(), type);
 }
 
 static struct tee_mmap_region *
@@ -2430,6 +2436,7 @@ void *core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	vaddr_t end = 0;
 	paddr_t p = 0;
 	size_t l = 0;
+	size_t res_map_idx = 0;
 
 	if (!len)
 		return NULL;
@@ -2443,7 +2450,7 @@ void *core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 		return (void *)(vaddr_t)(map->va + addr - map->pa);
 
 	/* Find the reserved va space used for late mappings */
-	map = find_map_by_type(MEM_AREA_RES_VASPACE);
+	map = find_map_by_type_in_map(mem_map, MEM_AREA_RES_VASPACE);
 	if (!map)
 		return NULL;
 
@@ -2458,10 +2465,15 @@ void *core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	if (map->size < l)
 		return NULL;
 
-	if (static_memory_map.count >= static_memory_map.alloc_count)
-		return NULL;
+	/*
+	 * grow_mem_map() may realloc the map, so refresh the RES_VASPACE
+	 * pointer before using it below.
+	 */
+	res_map_idx = map - mem_map->map;
+	grow_mem_map(mem_map);
+	map = mem_map->map + res_map_idx;
 
-	mem_map->map[mem_map->count] = (struct tee_mmap_region){
+	mem_map->map[mem_map->count - 1] = (struct tee_mmap_region){
 		.va = map->va,
 		.size = l,
 		.type = type,
@@ -2471,8 +2483,7 @@ void *core_mmu_add_mapping(enum teecore_memtypes type, paddr_t addr, size_t len)
 	};
 	map->va += l;
 	map->size -= l;
-	map = mem_map->map + mem_map->count;
-	mem_map->count++;
+	map = mem_map->map + mem_map->count - 1;
 
 	if (ADD_OVERFLOW(map->va, map->size, &end))
 		panic("VA overflow in add_mapping");