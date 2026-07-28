OP-TEE/optee_os

commit de66193d98491779bdbcc647d85bf6fb9a4d5e41

core: FF-A: ADD FFA_MEM_RETRIEVE for SPs
FFA_MEM_RETRIEVE is used by a SP to retrieve a FF-A memory share with a
handle. Once the FFA_MEM_RETRIEVE message is handled correctly the
corresponding memory region is mapped into the SPs memory

Signed-off-by: Jelle Sels <jelle.sels@arm.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/kernel/spmc_sp_handler.c
@@ -17,6 +17,8 @@
 #include <string.h>
 #include "thread_private.h"
 
+static unsigned int mem_ref_lock = SPINLOCK_UNLOCK;
+
 void spmc_sp_start_thread(struct thread_smc_args *args)
 {
 	thread_sp_alloc_and_run(args);
@@ -358,6 +360,228 @@ cleanup:
 	return res;
 }
 
+static bool check_rxtx(struct ffa_rxtx *rxtx)
+{
+	return rxtx && rxtx->rx && rxtx->tx && rxtx->size > 0;
+}
+
+static TEE_Result check_retrieve_request(struct sp_mem_receiver *receiver,
+					 struct ffa_mem_transaction *retr_dsc,
+					 struct sp_mem *smem, int64_t tx_len)
+{
+	struct ffa_mem_access *retr_access = NULL;
+	uint8_t share_perm = receiver->perm.perm;
+	uint32_t retr_perm = 0;
+	uint32_t retr_flags = READ_ONCE(retr_dsc->tag);
+	uint64_t retr_tag = READ_ONCE(retr_dsc->flags);
+	struct sp_mem_map_region *reg = NULL;
+
+	/*
+	 * The request came from the endpoint. It should only have one
+	 * ffa_mem_access element
+	 */
+	if (READ_ONCE(retr_dsc->mem_access_count) != 1)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	retr_access = retr_dsc->mem_access_array;
+	retr_perm = READ_ONCE(retr_access->access_perm.perm);
+
+	/* Check if tag is correct */
+	if (receiver->smem->tag != retr_tag) {
+		EMSG("Incorrect tag %#"PRIx64" %#"PRIx64, receiver->smem->tag,
+		     retr_tag);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	/* Check permissions and flags */
+	if ((retr_perm & FFA_MEM_ACC_RW) &&
+	    !(share_perm & FFA_MEM_ACC_RW)) {
+		DMSG("Incorrect memshare permission set");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if ((retr_perm & FFA_MEM_ACC_EXE) &&
+	    !(share_perm & FFA_MEM_ACC_EXE)) {
+		DMSG("Incorrect memshare permission set");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (retr_flags & FFA_MEMORY_REGION_FLAG_CLEAR_RELINQUISH) {
+		DMSG("CLEAR_RELINQUISH is not allowed for FFA_SHARE");
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	/*
+	 * Check if there is enough space in the tx buffer to send the respons.
+	 */
+	tx_len -= sizeof(struct ffa_mem_transaction) +
+		  sizeof(struct ffa_mem_access) +
+		  sizeof(struct ffa_mem_region);
+
+	if (tx_len < 0)
+		return FFA_NO_MEMORY;
+
+	SLIST_FOREACH(reg, &smem->regions, link) {
+		tx_len -= sizeof(struct ffa_address_range);
+		if (tx_len < 0)
+			return FFA_NO_MEMORY;
+	}
+
+	return TEE_SUCCESS;
+}
+
+static void create_retrieve_response(void *dst_buffer,
+				     struct sp_mem_receiver *receiver,
+				     struct sp_mem *smem, struct sp_session *s)
+{
+	size_t off = 0;
+	struct ffa_mem_region *dst_region =  NULL;
+	struct ffa_mem_transaction *d_ds = dst_buffer;
+	struct ffa_address_range *addr_dst = NULL;
+	struct sp_mem_map_region *reg = NULL;
+
+	/*
+	 * We respond with a FFA_MEM_RETRIEVE_RESP which defines the
+	 * following data in the rx buffer of the SP.
+	 * struct mem_transaction_descr
+	 * struct mem_access_descr (always 1 Element)
+	 * struct mem_region_descr
+	 */
+	/* Copy the mem_transaction_descr */
+	d_ds->sender_id = receiver->smem->sender_id;
+	d_ds->mem_reg_attr = receiver->smem->mem_reg_attr;
+	d_ds->flags = receiver->smem->flags;
+	d_ds->tag = receiver->smem->tag;
+
+	off = sizeof(struct ffa_mem_transaction) +
+	      sizeof(struct ffa_mem_access);
+
+	d_ds->mem_access_count = 1;
+
+	/* Copy the mem_accsess_descr */
+	d_ds->mem_access_array[0].region_offs = off;
+	memcpy(&d_ds->mem_access_array[0].access_perm,
+	       &receiver->perm, sizeof(struct ffa_mem_access_perm));
+
+	/* Copy the mem_region_descr */
+	dst_region = (struct ffa_mem_region *)((vaddr_t)d_ds + off);
+
+	dst_region->address_range_count = 0;
+	dst_region->total_page_count = 0;
+
+	addr_dst = dst_region->address_range_array;
+
+	SLIST_FOREACH(reg, &smem->regions, link) {
+		uint32_t offset = reg->page_offset;
+		struct sp_ctx *ctx = to_sp_ctx(s->ts_sess.ctx);
+
+		addr_dst->address = (uint64_t)sp_mem_get_va(&ctx->uctx,
+							    offset,
+							    reg->mobj);
+		addr_dst->page_count = reg->page_count;
+		dst_region->address_range_count++;
+
+		dst_region->total_page_count += addr_dst->page_count;
+	}
+}
+
+static void ffa_mem_retrieve(struct thread_smc_args *args,
+			     struct sp_session *caller_sp,
+			     struct ffa_rxtx *rxtx)
+{
+	int ret = FFA_OK;
+	size_t tx_len = 0;
+	struct ffa_mem_transaction *retr_dsc = NULL;
+	struct ffa_mem_region *mem_region = NULL;
+	uint64_t va = 0;
+	struct sp_mem *smem = NULL;
+	struct sp_mem_receiver *receiver = NULL;
+	uint32_t exceptions = 0;
+	uint32_t address_offset = 0;
+	size_t needed_size = 0;
+
+	if (!check_rxtx(rxtx) || !rxtx->tx_is_mine) {
+		ret = FFA_DENIED;
+		goto err;
+	}
+
+	tx_len = rxtx->size;
+	retr_dsc = rxtx->rx;
+
+	smem = sp_mem_get(retr_dsc->global_handle);
+	if (!smem) {
+		DMSG("Incorrect handle");
+		ret = FFA_DENIED;
+		goto err;
+	}
+
+	receiver = sp_mem_get_receiver(caller_sp->endpoint_id, smem);
+
+	address_offset = READ_ONCE(retr_dsc->mem_access_array[0].region_offs);
+
+	if (ADD_OVERFLOW(address_offset, sizeof(struct ffa_mem_region),
+			 &needed_size) || needed_size > tx_len) {
+		ret = FFA_INVALID_PARAMETERS;
+		goto err;
+	}
+
+	if (check_retrieve_request(receiver, retr_dsc, smem, tx_len) !=
+	    TEE_SUCCESS) {
+		ret = FFA_INVALID_PARAMETERS;
+		goto err;
+	}
+
+	exceptions = cpu_spin_lock_xsave(&mem_ref_lock);
+
+	if (receiver->ref_count == UINT8_MAX) {
+		ret = FFA_DENIED;
+		cpu_spin_unlock_xrestore(&mem_ref_lock, exceptions);
+		goto err;
+	}
+
+	receiver->ref_count++;
+
+	/* We only need to map the region the first time we request it. */
+	if (receiver->ref_count == 1) {
+		TEE_Result ret_map = TEE_SUCCESS;
+
+		cpu_spin_unlock_xrestore(&mem_ref_lock, exceptions);
+
+		/*
+		 * Try to map the memory linked to the handle in
+		 * sp_mem_access_descr.
+		 */
+		mem_region = (struct ffa_mem_region *)((vaddr_t)retr_dsc +
+						       address_offset);
+
+		va = READ_ONCE(mem_region->address_range_array[0].address);
+		ret_map = sp_map_shared(caller_sp, receiver, smem,  &va);
+
+		if (ret_map) {
+			EMSG("Could not map memory region: %#"PRIx32, ret_map);
+			exceptions = cpu_spin_lock_xsave(&mem_ref_lock);
+			receiver->ref_count--;
+			cpu_spin_unlock_xrestore(&mem_ref_lock, exceptions);
+			ret = FFA_DENIED;
+			goto err;
+		}
+	} else {
+		cpu_spin_unlock_xrestore(&mem_ref_lock, exceptions);
+	}
+
+	create_retrieve_response(rxtx->tx, receiver, smem, caller_sp);
+
+	args->a0 = FFA_MEM_RETRIEVE_RESP;
+	args->a1 = tx_len;
+	args->a2 = tx_len;
+
+	rxtx->tx_is_mine = false;
+
+	return;
+err:
+	ffa_set_error(args, ret);
+}
+
 static struct sp_session *
 ffa_handle_sp_direct_req(struct thread_smc_args *args,
 			 struct sp_session *caller_sp)
@@ -595,6 +819,15 @@ void spmc_sp_msg_handler(struct thread_smc_args *args,
 			ts_push_current_session(&caller_sp->ts_sess);
 			spmc_sp_handle_mem_share(args, &caller_sp->rxtx,
 						 caller_sp);
+			ts_pop_current_session();
+			sp_enter(args, caller_sp);
+			break;
+#ifdef ARM64
+		case FFA_MEM_RETRIEVE_REQ_64:
+#endif
+		case FFA_MEM_RETRIEVE_REQ_32:
+			ts_push_current_session(&caller_sp->ts_sess);
+			ffa_mem_retrieve(args, caller_sp, &caller_sp->rxtx);
 			ts_pop_current_session();
 			sp_enter(args, caller_sp);
 			break;

core/arch/arm/include/kernel/secure_partition.h
@@ -81,6 +81,10 @@ TEE_Result sp_partition_info_get_all(struct ffa_partition_info *fpi,
 TEE_Result sp_find_session_id(const TEE_UUID *uuid, uint32_t *session_id);
 bool sp_has_exclusive_access(struct sp_mem_map_region *mem,
 			     struct user_mode_ctx *uctx);
+TEE_Result sp_map_shared(struct sp_session *s,
+			 struct sp_mem_receiver *receiver,
+			 struct sp_mem *mem,
+			 uint64_t *va);
 
 #define for_each_secure_partition(_sp) \
 	SCATTERED_ARRAY_FOREACH(_sp, sp_images, struct embedded_ts)

core/arch/arm/kernel/secure_partition.c
@@ -232,6 +232,50 @@ static TEE_Result sp_init_set_registers(struct sp_ctx *ctx)
 	return TEE_SUCCESS;
 }
 
+TEE_Result sp_map_shared(struct sp_session *s,
+			 struct sp_mem_receiver *receiver,
+			 struct sp_mem *smem,
+			 uint64_t *va)
+{
+	TEE_Result res = TEE_SUCCESS;
+	struct sp_ctx *ctx = NULL;
+	uint32_t perm = TEE_MATTR_UR;
+	struct sp_mem_map_region *reg = NULL;
+
+	ctx = to_sp_ctx(s->ts_sess.ctx);
+
+	/* Get the permission */
+	if (receiver->perm.perm & FFA_MEM_ACC_EXE)
+		perm |= TEE_MATTR_UX;
+
+	if (receiver->perm.perm & FFA_MEM_ACC_RW) {
+		if (receiver->perm.perm & FFA_MEM_ACC_EXE)
+			return TEE_ERROR_ACCESS_CONFLICT;
+
+		perm |= TEE_MATTR_UW;
+	}
+	/*
+	 * Currently we don't support passing a va. We can't guarantee that the
+	 * full region will be mapped in a contiguous region. A smem->region can
+	 * have multiple mobj for one share. Currently there doesn't seem to be
+	 * an option to guarantee that these will be mapped in a contiguous va
+	 * space.
+	 */
+	if (*va)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	SLIST_FOREACH(reg, &smem->regions, link) {
+		res = vm_map(&ctx->uctx, va, reg->page_count * SMALL_PAGE_SIZE,
+			     perm, 0, reg->mobj, reg->page_offset);
+
+		if (res != TEE_SUCCESS) {
+			EMSG("Failed to map memory region %#"PRIx32, res);
+			return res;
+		}
+	}
+	return TEE_SUCCESS;
+}
+
 static TEE_Result sp_open_session(struct sp_session **sess,
 				  struct sp_sessions_head *open_sessions,
 				  const TEE_UUID *uuid)

core/arch/arm/mm/sp_mem.c
@@ -17,6 +17,7 @@ static unsigned int sp_mem_lock = SPINLOCK_UNLOCK;
 /* mem_shares stores all active FF-A shares. */
 SLIST_HEAD(sp_mem_head, sp_mem);
 static struct sp_mem_head mem_shares = SLIST_HEAD_INITIALIZER(sp_mem_head);
+/* Weak instance of mobj_sp_ops mandates it is not static */
 const struct mobj_ops mobj_sp_ops;
 
 struct mobj_sp {
@@ -162,6 +163,43 @@ const struct mobj_ops mobj_sp_ops __weak __rodata_unpaged("mobj_sp_ops") = {
 	.matches = mobj_sp_matches,
 	.free = inactivate,
 };
+
+struct sp_mem_receiver *sp_mem_get_receiver(uint32_t s_id, struct sp_mem *smem)
+{
+	struct sp_mem_receiver *r = NULL;
+
+	SLIST_FOREACH(r, &smem->receivers, link) {
+		if (r->perm.endpoint_id == s_id)
+			return r;
+	}
+	return NULL;
+}
+
+struct sp_mem *sp_mem_get(uint64_t handle)
+{
+	struct sp_mem *smem = NULL;
+	uint32_t exceptions = cpu_spin_lock_xsave(&sp_mem_lock);
+
+	SLIST_FOREACH(smem, &mem_shares, link) {
+		if (smem->global_handle == handle)
+			break;
+	}
+
+	cpu_spin_unlock_xrestore(&sp_mem_lock, exceptions);
+	return smem;
+}
+
+void *sp_mem_get_va(const struct user_mode_ctx *uctx, size_t offset,
+		    struct mobj *mobj)
+{
+	struct vm_region *region = NULL;
+
+	TAILQ_FOREACH(region, &uctx->vm_info.regions, link) {
+		if (region->mobj == mobj && region->offset == offset)
+			return (void *)region->va;
+	}
+	return NULL;
+}
 
 struct sp_mem *sp_mem_new(void)
 {

commit 6e595c8959b96a19d5c114942c6c32a4636872fa

core: spmc: fix missing addr_dst increment in retrieve response
create_retrieve_response() in core/arch/arm/kernel/spmc_sp_handler.c
sets addr_dst to the start of dst_region->address_range_array before
the SLIST_FOREACH loop, but never advances the pointer inside the
loop body. As a result, every iteration overwrites slot [0] with the
current region's address and page_count, while address_range_count is
correctly incremented to N.

For single-region shares (e.g. all NW-originated shares, which always
produce exactly one region via spmc_sp_add_nw_region()) the bug is
invisible. For multi-region SP-to-SP shares produced by
spmc_sp_add_sp_region() the receiving SP gets a descriptor that
declares N regions but only carries valid data in slot [0]; slots
[1..N-1] remain zero-initialised.

total_page_count is unaffected because it sums addr_dst->page_count,
which always reflects the current iteration.

Fix: advance addr_dst at the end of the loop body, mirroring the
per-region descriptor write pattern used elsewhere in the SPMC.

Fixes: de66193d9849 ("core: FF-A: ADD FFA_MEM_RETRIEVE for SPs")
Signed-off-by: Mattijs van Ommeren <mattijs@qlibr.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@st.com>

core/arch/arm/kernel/spmc_sp_handler.c
@@ -612,6 +612,7 @@ static void create_retrieve_response(uint32_t ffa_vers, void *dst_buffer,
 		dst_region->address_range_count++;
 
 		dst_region->total_page_count += addr_dst->page_count;
+		addr_dst++;
 	}
 }
 