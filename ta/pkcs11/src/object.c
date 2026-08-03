d46e13db40fa59913b3a1c72b1ea4c85e32dcabc
Sungbae Yoo <sungbaey@nvidia.com>
2026-08-03 09:37:58 +0000

    ta: pkcs11: invalidate handles before freeing token objects
    Move token_invalidate_object_handles() before cleanup_persistent_object()
    so that handles are invalidated while obj is still valid.
    
    cleanup_persistent_object() frees obj through cleanup_volatile_obj_ref().
    Calling token_invalidate_object_handles() afterwards therefore passes a
    pointer to an object whose lifetime has ended.
    
    Fixes: bb5d18259197 ("ta: pkcs11: invalidate handle of destroyed token objects")
    Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
    Acked-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

--- ta/pkcs11/src/object.c
+++ ta/pkcs11/src/object.c
@@ -159,9 +159,9 @@ void destroy_object(struct pkcs11_session *session, struct pkcs11_object *obj,
 
 		handle_put(get_object_handle_db(session),
 			   pkcs11_object2handle(obj, session));
-		cleanup_persistent_object(obj, session->token);
-
 		token_invalidate_object_handles(obj);
+
+		cleanup_persistent_object(obj, session->token);
 	} else {
 		handle_put(get_object_handle_db(session),
 			   pkcs11_object2handle(obj, session));
