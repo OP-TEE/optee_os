OP-TEE/optee_os

commit f5a70e3efb80be4b9bff2c9c811ddc139058e05a

drivers: crypto: generic resources for crypto device driver - RSA
TEE Crypto generic APIs to HW driver interface

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/crypto_api/acipher/rsa.c
@@ -1,0 +1,458 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * Crypto RSA interface implementation to enable HW driver.
+ */
+#include <drvcrypt.h>
+#include <crypto/crypto.h>
+#include <tee_api_defines_extensions.h>
+#include <tee/tee_cryp_utl.h>
+#include <utee_defines.h>
+
+#include "local.h"
+
+TEE_Result crypto_acipher_alloc_rsa_keypair(struct rsa_keypair *key,
+					    size_t size_bits)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+
+	struct drvcrypt_rsa *rsa = NULL;
+
+	if (!key || !size_bits) {
+		CRYPTO_TRACE("Parameters error (key @%p) (size %zu bits)", key,
+			     size_bits);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa)
+		ret = rsa->alloc_keypair(key, size_bits);
+
+	CRYPTO_TRACE("RSA Keypair (%zu bits) alloc ret = 0x%" PRIx32, size_bits,
+		     ret);
+	return ret;
+}
+
+TEE_Result crypto_acipher_alloc_rsa_public_key(struct rsa_public_key *key,
+					       size_t size_bits)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_rsa *rsa = NULL;
+
+	if (!key || !size_bits) {
+		CRYPTO_TRACE("Parameters error (key @%p) (size %zu bits)", key,
+			     size_bits);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa)
+		ret = rsa->alloc_publickey(key, size_bits);
+
+	CRYPTO_TRACE("RSA Public Key (%zu bits) alloc ret = 0x%" PRIx32,
+		     size_bits, ret);
+	return ret;
+}
+
+void crypto_acipher_free_rsa_public_key(struct rsa_public_key *key)
+{
+	struct drvcrypt_rsa *rsa = NULL;
+
+	if (key) {
+		rsa = drvcrypt_get_ops(CRYPTO_RSA);
+		if (rsa) {
+			CRYPTO_TRACE("RSA Public Key free");
+			rsa->free_publickey(key);
+		}
+	}
+}
+
+TEE_Result crypto_acipher_gen_rsa_key(struct rsa_keypair *key, size_t size_bits)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_rsa *rsa = NULL;
+
+	if (!key || !size_bits) {
+		CRYPTO_TRACE("Parameters error (key @%p) (size %zu bits) ",
+			     key, size_bits);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa)
+		ret = rsa->gen_keypair(key, size_bits);
+
+	CRYPTO_TRACE("RSA Keypair (%zu bits) generate ret = 0x%" PRIx32,
+		     size_bits, ret);
+
+	return ret;
+}
+
+TEE_Result crypto_acipher_rsanopad_decrypt(struct rsa_keypair *key,
+					   const uint8_t *cipher,
+					   size_t cipher_len, uint8_t *msg,
+					   size_t *msg_len)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_rsa *rsa = NULL;
+	struct drvcrypt_rsa_ed rsa_data = { };
+
+	if (!key || !msg || !cipher || !msg_len) {
+		CRYPTO_TRACE("Parameters error (key @%p)\n"
+			     "(msg @%p size %zu bytes)\n"
+			     "(cipher @0%p size %zu bytes)",
+			     key, msg, *msg_len, cipher, cipher_len);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa_data.key.key = key;
+	rsa_data.key.isprivate = true;
+	rsa_data.key.n_size = crypto_bignum_num_bytes(key->n);
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa) {
+		rsa_data.rsa_id = DRVCRYPT_RSA_NOPAD;
+		rsa_data.message.data = msg;
+		rsa_data.message.length = *msg_len;
+		rsa_data.cipher.data = (uint8_t *)cipher;
+		rsa_data.cipher.length = cipher_len;
+
+		ret = rsa->decrypt(&rsa_data);
+
+		*msg_len = rsa_data.message.length;
+	}
+
+	CRYPTO_TRACE("RSA Decrypt NO PAD ret = 0x%" PRIx32, ret);
+
+	return ret;
+}
+
+TEE_Result crypto_acipher_rsanopad_encrypt(struct rsa_public_key *key,
+					   const uint8_t *msg, size_t msg_len,
+					   uint8_t *cipher, size_t *cipher_len)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_rsa *rsa = NULL;
+	struct drvcrypt_rsa_ed rsa_data = { };
+
+	if (!key || !msg || !cipher || !cipher_len) {
+		CRYPTO_TRACE("Parameters error (key @%p)\n"
+			     "(msg @%p size %zu bytes)\n"
+			     "(cipher @%p size %zu bytes)",
+			     key, msg, msg_len, cipher, *cipher_len);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa_data.key.key = key;
+	rsa_data.key.isprivate = false;
+	rsa_data.key.n_size = crypto_bignum_num_bytes(key->n);
+
+	if (rsa_data.key.n_size > *cipher_len) {
+		CRYPTO_TRACE("Cipher length (%zu) too short expected %zu bytes",
+			     *cipher_len, rsa_data.key.n_size);
+		*cipher_len = rsa_data.key.n_size;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa) {
+		/* Prepare the encryption data parameters */
+		rsa_data.rsa_id = DRVCRYPT_RSA_NOPAD;
+		rsa_data.message.data = (uint8_t *)msg;
+		rsa_data.message.length = msg_len;
+		rsa_data.cipher.data = cipher;
+		rsa_data.cipher.length = *cipher_len;
+
+		ret = rsa->encrypt(&rsa_data);
+
+		/* Set the cipher size */
+		*cipher_len = rsa_data.cipher.length;
+	}
+
+	CRYPTO_TRACE("RSA Encrypt NO PAD ret = 0x%" PRIx32, ret);
+
+	return ret;
+}
+
+TEE_Result crypto_acipher_rsaes_decrypt(uint32_t algo, struct rsa_keypair *key,
+					const uint8_t *label, size_t label_len,
+					const uint8_t *cipher,
+					size_t cipher_len, uint8_t *msg,
+					size_t *msg_len)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_rsa *rsa = NULL;
+	struct drvcrypt_rsa_ed rsa_data = { };
+
+	if (!key || !msg || !cipher || !msg_len || (!label && label_len)) {
+		CRYPTO_TRACE("Parameters error (key @%p)\n"
+			     "(msg @%p size %zu bytes)\n"
+			     "(cipher @%p size %zu bytes)\n"
+			     "(label @%p size %zu bytes)",
+			     key, msg, *msg_len, cipher, cipher_len, label,
+			     label_len);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa) {
+		/* Prepare the encryption data parameters */
+		if (algo == TEE_ALG_RSAES_PKCS1_V1_5) {
+			rsa_data.rsa_id = DRVCRYPT_RSA_PKCS_V1_5;
+		} else {
+			rsa_data.rsa_id = DRVCRYPT_RSA_OAEP;
+			rsa_data.hash_algo = TEE_INTERNAL_HASH_TO_ALGO(algo);
+
+			ret = tee_hash_get_digest_size(rsa_data.hash_algo,
+						       &rsa_data.digest_size);
+			if (ret != TEE_SUCCESS)
+				return ret;
+
+			rsa_data.mgf = &drvcrypt_rsa_mgf1;
+		}
+
+		rsa_data.key.key = key;
+		rsa_data.key.isprivate = true;
+		rsa_data.key.n_size = crypto_bignum_num_bytes(key->n);
+
+		rsa_data.message.data = msg;
+		rsa_data.message.length = *msg_len;
+		rsa_data.cipher.data = (uint8_t *)cipher;
+		rsa_data.cipher.length = cipher_len;
+		rsa_data.label.data =
+			((label_len > 0) ? (uint8_t *)label : NULL);
+		rsa_data.label.length = label_len;
+
+		ret = rsa->decrypt(&rsa_data);
+
+		/* Set the message size */
+		*msg_len = rsa_data.message.length;
+	}
+
+	CRYPTO_TRACE("RSAES Decrypt ret = 0x%" PRIx32, ret);
+
+	return ret;
+}
+
+TEE_Result crypto_acipher_rsaes_encrypt(uint32_t algo,
+					struct rsa_public_key *key,
+					const uint8_t *label, size_t label_len,
+					const uint8_t *msg, size_t msg_len,
+					uint8_t *cipher, size_t *cipher_len)
+{
+	TEE_Result ret = TEE_ERROR_NOT_IMPLEMENTED;
+	struct drvcrypt_rsa *rsa = NULL;
+	struct drvcrypt_rsa_ed rsa_data = { };
+
+	if (!key || !msg || !cipher || !cipher_len || (!label && label_len)) {
+		CRYPTO_TRACE("Parameters error (key @%p\n"
+			     "(msg @%p size %zu bytes)\n"
+			     "(cipher @%p size %zu bytes)\n"
+			     "(label @%p size %zu bytes)",
+			     key, msg, msg_len, cipher, *cipher_len, label,
+			     label_len);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	rsa_data.key.key = key;
+	rsa_data.key.isprivate = false;
+	rsa_data.key.n_size = crypto_bignum_num_bytes(key->n);
+
+	if (rsa_data.key.n_size > *cipher_len) {
+		CRYPTO_TRACE("Cipher length (%zu) too short expected %zu bytes",
+			     *cipher_len, rsa_data.key.n_size);
+		*cipher_len = rsa_data.key.n_size;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa) {
+		/* Prepare the encryption data parameters */
+		if (algo == TEE_ALG_RSAES_PKCS1_V1_5) {
+			rsa_data.rsa_id = DRVCRYPT_RSA_PKCS_V1_5;
+
+			/* Message length <= (modulus_size - 11) */
+			if (msg_len > rsa_data.key.n_size - 11)
+				return TEE_ERROR_BAD_PARAMETERS;
+
+		} else {
+			rsa_data.rsa_id = DRVCRYPT_RSA_OAEP;
+			rsa_data.hash_algo = TEE_INTERNAL_HASH_TO_ALGO(algo);
+
+			/* Message length <= (modulus_size - 2 * hLength - 2) */
+			ret = tee_hash_get_digest_size(rsa_data.hash_algo,
+						       &rsa_data.digest_size);
+			if (ret != TEE_SUCCESS)
+				return ret;
+
+			if (msg_len >
+			    rsa_data.key.n_size - 2 * rsa_data.digest_size - 2)
+				return TEE_ERROR_BAD_PARAMETERS;
+
+			rsa_data.mgf = &drvcrypt_rsa_mgf1;
+		}
+
+		rsa_data.message.data = (uint8_t *)msg;
+		rsa_data.message.length = msg_len;
+		rsa_data.cipher.data = cipher;
+		rsa_data.cipher.length = rsa_data.key.n_size;
+		rsa_data.label.data = (label_len > 0) ? (uint8_t *)label : NULL;
+		rsa_data.label.length = label_len;
+
+		ret = rsa->encrypt(&rsa_data);
+
+		/* Set the cipher size */
+		*cipher_len = rsa_data.cipher.length;
+	}
+
+	CRYPTO_TRACE("RSAES Encrypt ret = 0x%" PRIx32, ret);
+
+	return ret;
+}
+
+TEE_Result crypto_acipher_rsassa_sign(uint32_t algo, struct rsa_keypair *key,
+				      int salt_len, const uint8_t *msg,
+				      size_t msg_len, uint8_t *sig,
+				      size_t *sig_len)
+{
+	TEE_Result ret = TEE_ERROR_BAD_PARAMETERS;
+	struct drvcrypt_rsa *rsa = NULL;
+	struct drvcrypt_rsa_ssa rsa_ssa = { };
+
+	if (!key || !msg || !sig || !sig_len) {
+		CRYPTO_TRACE("Input parameters reference error");
+		return ret;
+	}
+
+	if (algo != TEE_ALG_RSASSA_PKCS1_V1_5) {
+		/* Prepare the Digest */
+		rsa_ssa.hash_algo = TEE_DIGEST_HASH_TO_ALGO(algo);
+
+		/* Check if the message length is digest hash size */
+		ret = tee_hash_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(algo),
+					       &rsa_ssa.digest_size);
+		if (ret != TEE_SUCCESS)
+			return ret;
+
+		if (msg_len != rsa_ssa.digest_size) {
+			CRYPTO_TRACE("Msg length (%zu expected %zu)", msg_len,
+				     rsa_ssa.digest_size);
+			return TEE_ERROR_BAD_PARAMETERS;
+		}
+	} else {
+		rsa_ssa.hash_algo = 0;
+		rsa_ssa.digest_size = 0;
+	}
+
+	/* Prepare the Key */
+	rsa_ssa.key.key = key;
+	rsa_ssa.key.isprivate = true;
+	rsa_ssa.key.n_size = crypto_bignum_num_bytes(key->n);
+
+	if (rsa_ssa.key.n_size > *sig_len) {
+		CRYPTO_TRACE("Sign length (%zu) too short must be %zu bytes",
+			     *sig_len, rsa_ssa.key.n_size);
+		*sig_len = rsa_ssa.key.n_size;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa) {
+		/* Prepare the Encoded Signature structure data */
+		rsa_ssa.algo = algo;
+		rsa_ssa.message.data = (uint8_t *)msg;
+		rsa_ssa.message.length = msg_len;
+		rsa_ssa.signature.data = (uint8_t *)sig;
+		rsa_ssa.signature.length = rsa_ssa.key.n_size;
+		rsa_ssa.salt_len = salt_len;
+		rsa_ssa.mgf = &drvcrypt_rsa_mgf1;
+
+		if (rsa->optional.ssa_sign)
+			ret = rsa->optional.ssa_sign(&rsa_ssa);
+		else
+			ret = drvcrypt_rsassa_sign(&rsa_ssa);
+
+		/* Set the signature length */
+		*sig_len = rsa_ssa.signature.length;
+	} else {
+		ret = TEE_ERROR_NOT_IMPLEMENTED;
+	}
+
+	CRYPTO_TRACE("Encode signature algo (0x%" PRIx32
+		     ") returned 0x%" PRIx32,
+		     algo, ret);
+	return ret;
+}
+
+TEE_Result crypto_acipher_rsassa_verify(uint32_t algo,
+					struct rsa_public_key *key,
+					int salt_len, const uint8_t *msg,
+					size_t msg_len, const uint8_t *sig,
+					size_t sig_len)
+{
+	TEE_Result ret = TEE_ERROR_BAD_PARAMETERS;
+	struct drvcrypt_rsa *rsa = NULL;
+	struct drvcrypt_rsa_ssa rsa_ssa = { };
+
+	if (!key || !msg || !sig) {
+		CRYPTO_TRACE("Input parameters reference error");
+		return ret;
+	}
+
+	if (algo != TEE_ALG_RSASSA_PKCS1_V1_5) {
+		/* Prepare the Digest */
+		rsa_ssa.hash_algo = TEE_DIGEST_HASH_TO_ALGO(algo);
+
+		/* Check if the message length is digest hash size */
+		ret = tee_hash_get_digest_size(TEE_DIGEST_HASH_TO_ALGO(algo),
+					       &rsa_ssa.digest_size);
+		if (ret != TEE_SUCCESS)
+			return ret;
+
+		if (msg_len != rsa_ssa.digest_size) {
+			CRYPTO_TRACE("Input msg length (%zu expected %zu)",
+				     msg_len, rsa_ssa.digest_size);
+			return TEE_ERROR_BAD_PARAMETERS;
+		}
+	} else {
+		rsa_ssa.hash_algo = 0;
+		rsa_ssa.digest_size = 0;
+	}
+
+	/* Prepare the Key */
+	rsa_ssa.key.key = key;
+	rsa_ssa.key.isprivate = false;
+	rsa_ssa.key.n_size = crypto_bignum_num_bytes(key->n);
+
+	if (rsa_ssa.key.n_size > sig_len) {
+		CRYPTO_TRACE("Signature length expected %zu",
+			     rsa_ssa.key.n_size);
+		return TEE_ERROR_SIGNATURE_INVALID;
+	}
+
+	rsa = drvcrypt_get_ops(CRYPTO_RSA);
+	if (rsa) {
+		/* Prepare the Encoded Signature structure data */
+		rsa_ssa.algo = algo;
+		rsa_ssa.message.data = (uint8_t *)msg;
+		rsa_ssa.message.length = msg_len;
+		rsa_ssa.signature.data = (uint8_t *)sig;
+		rsa_ssa.signature.length = sig_len;
+		rsa_ssa.salt_len = salt_len;
+		rsa_ssa.mgf = &drvcrypt_rsa_mgf1;
+
+		if (rsa->optional.ssa_verify)
+			ret = rsa->optional.ssa_verify(&rsa_ssa);
+		else
+			ret = drvcrypt_rsassa_verify(&rsa_ssa);
+
+	} else {
+		ret = TEE_ERROR_NOT_IMPLEMENTED;
+	}
+
+	CRYPTO_TRACE("Signature verif algo (0x%" PRIx32 ") returned 0x%" PRIx32,
+		     algo, ret);
+
+	return ret;
+}

core/arch/arm/plat-imx/conf.mk
@@ -407,7 +407,9 @@ $(call force,CFG_IMX_CAAM,n)
 # it with generic crypto API can be enabled.
 CFG_CRYPTO_DRIVER ?= y
 # Crypto Driver Debug
-CFG_CRYPTO_DRIVER_DEBUG ?= n
+# DRV_DBG_TRACE BIT32(0) // Driver trace
+# DRV_DBG_BUF   BIT32(1) // Driver dump Buffer
+CFG_CRYPTO_DRIVER_DEBUG ?= 0
 else
 $(call force,CFG_CRYPTO_DRIVER,n)
 $(call force,CFG_WITH_SOFTWARE_PRNG,y)

core/drivers/crypto/crypto_api/acipher/local.h
@@ -1,0 +1,34 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * Definition of the functions shared locally.
+ */
+#ifndef __LOCAL_H__
+#define __LOCAL_H__
+
+#include <drvcrypt_acipher.h>
+
+/*
+ * Mask Generation function. Use a Hash operation
+ * to generate an output @mask from a input @seed
+ *
+ * @mgf_data  [in/out] MGF data
+ */
+TEE_Result drvcrypt_rsa_mgf1(struct drvcrypt_rsa_mgf *mgf_data);
+
+/*
+ * PKCS#1 - Signature of RSA message and encodes the signature.
+ *
+ * @ssa_data   [in/out] RSA data to sign / Signature
+ */
+TEE_Result drvcrypt_rsassa_sign(struct drvcrypt_rsa_ssa *ssa_data);
+
+/*
+ * PKCS#1 - Verification the encoded signature of RSA message.
+ *
+ * @ssa_data   RSA Encoded signature data
+ */
+TEE_Result drvcrypt_rsassa_verify(struct drvcrypt_rsa_ssa *ssa_data);
+
+#endif /* __LOCAL_H__ */

core/drivers/crypto/crypto_api/acipher/rsamgf.c
@@ -1,0 +1,105 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * RSA Mask Generation function implementation.
+ */
+#include <drvcrypt.h>
+#include <malloc.h>
+#include <string.h>
+#include <utee_defines.h>
+
+#include "local.h"
+
+TEE_Result drvcrypt_rsa_mgf1(struct drvcrypt_rsa_mgf *mgf_data)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+	void *ctx = NULL;
+	size_t lastBlock_size = 0;
+	size_t nbBlock = 0;
+	uint32_t counter = 0;
+	uint32_t swapcount = 0;
+	uint8_t *cur_mask = mgf_data->mask.data;
+	uint8_t *tmpdigest = NULL;
+
+	CRYPTO_TRACE("Generate Mask (%zu bytes) with seed of %zu bytes",
+		     mgf_data->mask.length, mgf_data->seed.length);
+
+	/* Calculate the number of complet hash digest */
+	lastBlock_size = mgf_data->mask.length % mgf_data->digest_size;
+	if (lastBlock_size) {
+		/* Allocate a digest buffer for the last block */
+		tmpdigest = malloc(mgf_data->digest_size);
+		if (!tmpdigest)
+			return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+	/* Allocate the Hash Context */
+	ret = crypto_hash_alloc_ctx(&ctx, mgf_data->hash_algo);
+	if (ret != TEE_SUCCESS)
+		goto exit_mgf;
+
+	nbBlock = (mgf_data->mask.length - lastBlock_size) /
+		  mgf_data->digest_size;
+
+	CRYPTO_TRACE("Nb Loop (%zu bytes) = %zu, last Block = %zu bytes",
+		     mgf_data->digest_size, nbBlock, lastBlock_size);
+
+	for (; counter < nbBlock;
+	     counter++, cur_mask += mgf_data->digest_size) {
+		swapcount = TEE_U32_TO_BIG_ENDIAN(counter);
+
+		ret = crypto_hash_init(ctx);
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		ret = crypto_hash_update(ctx, mgf_data->seed.data,
+					 mgf_data->seed.length);
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		ret = crypto_hash_update(ctx, (uint8_t *)&swapcount,
+					 sizeof(swapcount));
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		ret = crypto_hash_final(ctx, cur_mask, mgf_data->digest_size);
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+	}
+
+	if (lastBlock_size) {
+		CRYPTO_TRACE("Last Block = %zu bytes", lastBlock_size);
+
+		swapcount = TEE_U32_TO_BIG_ENDIAN(counter);
+
+		ret = crypto_hash_init(ctx);
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		ret = crypto_hash_update(ctx, mgf_data->seed.data,
+					 mgf_data->seed.length);
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		ret = crypto_hash_update(ctx, (uint8_t *)&swapcount,
+					 sizeof(swapcount));
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		ret = crypto_hash_final(ctx, tmpdigest, mgf_data->digest_size);
+		if (ret != TEE_SUCCESS)
+			goto exit_mgf;
+
+		memcpy(cur_mask, tmpdigest, lastBlock_size);
+	}
+
+	ret = TEE_SUCCESS;
+
+exit_mgf:
+	crypto_hash_free_ctx(ctx);
+	free(tmpdigest);
+
+	CRYPTO_TRACE("return 0x%08" PRIx32, ret);
+	return ret;
+}

core/drivers/crypto/crypto_api/acipher/sub.mk
@@ -1,0 +1,1 @@
+srcs-$(CFG_CRYPTO_DRV_RSA) += rsa.c rsamgf.c rsassa.c

core/drivers/crypto/crypto_api/include/drvcrypt.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2020 NXP
  *
  * Brief   Crypto Driver exported constants and interfaces.
  */
@@ -9,18 +9,48 @@
 
 #include <tee_api_types.h>
 #include <trace.h>
+#include <util.h>
 
-#ifdef CFG_CRYPTO_DRIVER_DEBUG
-#define CRYPTO_TRACE	DMSG
+/*
+ * Debug Macros function of Crypto Driver Debug Level setting
+ * The CFG_CRYPTO_DRV_DBG is a bit mask 32 bits value defined
+ * as followed:
+ */
+#define DRV_DBG_TRACE BIT32(0) /* Driver trace */
+#define DRV_DBG_BUF   BIT32(1) /* Driver dump Buffer */
+
+#if (CFG_CRYPTO_DRIVER_DEBUG & DRV_DBG_TRACE)
+#define CRYPTO_TRACE DMSG
 #else
 #define CRYPTO_TRACE(...)
 #endif
+#if (CFG_CRYPTO_DRIVER_DEBUG & DRV_DBG_BUF)
+#define CRYPTO_DUMPBUF(title, buf, len)                                        \
+	do {                                                                   \
+		__typeof__(buf) _buf = (buf);                                  \
+		__typeof__(len) _len = (len);                                  \
+		CRYPTO_TRACE("%s @%p: %zu", title, _buf, _len);                \
+		dhex_dump(NULL, 0, 0, _buf, _len);                             \
+	} while (0)
+#else
+#define CRYPTO_DUMPBUF(...)
+#endif
+
+/*
+ * Definition of a crypto buffer type
+ */
+struct drvcrypt_buf {
+	uint8_t *data;
+	size_t length;
+};
 
 /*
  * Crypto Library Algorithm enumeration
  */
 enum drvcrypt_algo_id {
 	CRYPTO_HASH = 0, /* Hash driver */
+	CRYPTO_RSA,      /* Asymmetric RSA driver */
+	CRYPTO_MATH,	 /* Mathematical driver */
 	CRYPTO_MAX_ALGO  /* Maximum number of algo supported */
 };
 

core/drivers/crypto/crypto_api/include/drvcrypt_acipher.h
@@ -1,0 +1,112 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * Brief   Asymmetric Cipher interface calling the HW crypto driver.
+ */
+#ifndef __DRVCRYPT_ACIPHER_H__
+#define __DRVCRYPT_ACIPHER_H__
+
+#include <crypto/crypto.h>
+#include <tee_api_types.h>
+
+/*
+ * Assymetric Cipher RSA Algorithm enumerate
+ */
+enum drvcrypt_rsa_id {
+	DRVCRYPT_RSA_NOPAD = 0,	   /* RSA Algo mode NO PAD */
+	DRVCRYPT_RSA_OAEP,	   /* RSA Algo mode OAEP */
+	DRVCRYPT_RSA_PKCS_V1_5,	   /* RSA Algo mode PKCSv1.5 */
+	DRVCRYPT_RSASSA_PKCS_V1_5, /* RSA Signature Algo mode PKCSv1.5 */
+	DRVCRYPT_RSASSA_PSS,	   /* RSA Signature Algo mode PSS */
+};
+
+/*
+ * RSA Key object
+ */
+struct drvcrypt_rsakey {
+	void *key;	/* Public or Private key */
+	size_t n_size;	/* Size in bytes of the Modulus N */
+	bool isprivate; /* True if private key */
+};
+
+/*
+ * RSA Mask Generation data
+ */
+struct drvcrypt_rsa_mgf {
+	uint32_t hash_algo;	  /* HASH Algorithm */
+	size_t digest_size;	  /* Hash Digest Size */
+	struct drvcrypt_buf seed; /* Seed to generate mask */
+	struct drvcrypt_buf mask; /* Mask generated */
+};
+
+/*
+ * RSA Encoded Signature data
+ */
+struct drvcrypt_rsa_ssa {
+	uint32_t algo;		       /* Operation algorithm */
+	uint32_t hash_algo;	       /* HASH Algorithm */
+	size_t digest_size;	       /* Hash Digest Size */
+	struct drvcrypt_rsakey key;    /* Public or Private Key */
+	struct drvcrypt_buf message;   /* Message to sign or signed */
+	struct drvcrypt_buf signature; /* Signature of the message */
+	size_t salt_len;	       /* Signature Salt length */
+
+	/* RSA Mask Generation function */
+	TEE_Result (*mgf)(struct drvcrypt_rsa_mgf *mgf_data);
+};
+
+/*
+ * RSA Encrypt/Decript data
+ */
+struct drvcrypt_rsa_ed {
+	enum drvcrypt_rsa_id rsa_id; /* RSA Algorithm Id */
+	uint32_t hash_algo;	     /* HASH Algorithm */
+	size_t digest_size;	     /* Hash Digest Size */
+	struct drvcrypt_rsakey key;  /* Public or Private key */
+	struct drvcrypt_buf message; /* Message to encrypt or decrypted */
+	struct drvcrypt_buf cipher;  /* Cipher encrypted or to decrypt */
+	struct drvcrypt_buf label;   /* Additional Label (RSAES) */
+
+	/* RSA Mask Generation function */
+	TEE_Result (*mgf)(struct drvcrypt_rsa_mgf *mgf_data);
+};
+
+/*
+ * Crypto Library RSA driver operations
+ */
+struct drvcrypt_rsa {
+	/* Allocates the RSA keypair */
+	TEE_Result (*alloc_keypair)(struct rsa_keypair *key, size_t size_bits);
+	/* Allocates the RSA public key */
+	TEE_Result (*alloc_publickey)(struct rsa_public_key *key,
+				      size_t size_bits);
+	/* Free RSA public key */
+	void (*free_publickey)(struct rsa_public_key *key);
+	/* Generates the RSA keypair */
+	TEE_Result (*gen_keypair)(struct rsa_keypair *key, size_t size_bits);
+
+	/* RSA Encryption */
+	TEE_Result (*encrypt)(struct drvcrypt_rsa_ed *rsa_data);
+	/* RSA Decryption */
+	TEE_Result (*decrypt)(struct drvcrypt_rsa_ed *rsa_data);
+
+	struct {
+		/* RSA Sign a message and encode the signature */
+		TEE_Result (*ssa_sign)(struct drvcrypt_rsa_ssa *ssa_data);
+		/* RSA Encoded Signature Verification */
+		TEE_Result (*ssa_verify)(struct drvcrypt_rsa_ssa *ssa_data);
+	} optional;
+};
+
+/*
+ * Register a RSA processing driver in the crypto API
+ *
+ * @ops - Driver operations in the HW layer
+ */
+static inline TEE_Result drvcrypt_register_rsa(const struct drvcrypt_rsa *ops)
+{
+	return drvcrypt_register(CRYPTO_RSA, (void *)ops);
+}
+
+#endif /* __DRVCRYPT_ACIPHER_H__ */

commit 6244b54e57c94ea568053e67aa00e8258880ca2f

drivers: crypto: harden crypto_acipher_rsaes_{de,en}crypt()
Adds input length validation against the RSA modulus size to avoid
propagating invalid arguments. Also validates the RSAES padding overhead
before subtracting it from the modulus size to avoid unsigned underflows.

Fixes: f5a70e3efb80 ("drivers: crypto: generic resources for crypto device driver - RSA")
Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
Acked-by: Jerome Forissier <jerome.forissier@arm.com>

core/drivers/crypto/crypto_api/acipher/rsa.c
@@ -244,6 +244,8 @@ TEE_Result crypto_acipher_rsaes_decrypt(uint32_t algo, struct rsa_keypair *key,
 		rsa_data.key.key = key;
 		rsa_data.key.isprivate = true;
 		rsa_data.key.n_size = crypto_bignum_num_bytes(key->n);
+		if (cipher_len > rsa_data.key.n_size)
+			return TEE_ERROR_BAD_PARAMETERS;
 
 		rsa_data.message.data = msg;
 		rsa_data.message.length = *msg_len;
@@ -310,7 +312,8 @@ TEE_Result crypto_acipher_rsaes_encrypt(uint32_t algo,
 			rsa_data.rsa_id = DRVCRYPT_RSA_PKCS_V1_5;
 
 			/* Message length <= (modulus_size - 11) */
-			if (msg_len > rsa_data.key.n_size - 11)
+			if (rsa_data.key.n_size < 11 ||
+			    msg_len > rsa_data.key.n_size - 11)
 				return TEE_ERROR_BAD_PARAMETERS;
 
 		} else {
@@ -323,7 +326,8 @@ TEE_Result crypto_acipher_rsaes_encrypt(uint32_t algo,
 			if (ret != TEE_SUCCESS)
 				return ret;
 
-			if (2 * rsa_data.digest_size >= rsa_data.key.n_size - 2)
+			if (rsa_data.key.n_size <=
+			    2 * rsa_data.digest_size + 2)
 				return TEE_ERROR_BAD_PARAMETERS;
 
 			if (msg_len >