OP-TEE/optee_os

commit 88796f89484340777b3a507cd78b0896b7ab105b

ldelf: remap first page of libraries
Remaps first page of libraries to a suitable location once the amount of
required virtual memory is known.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf.c
@@ -178,7 +178,6 @@ static void init_elf(struct ta_elf *elf)
 	TEE_Result res = TEE_SUCCESS;
 	vaddr_t va = 0;
 	uint32_t flags = PTA_SYSTEM_MAP_FLAG_SHAREABLE;
-	const size_t max_align = 0x10000;
 
 	res = sys_open_ta_bin(&elf->uuid, &elf->handle);
 	if (res)
@@ -190,13 +189,7 @@ static void init_elf(struct ta_elf *elf)
 	 */
 	if (!elf->is_main)
 		flags |= PTA_SYSTEM_MAP_FLAG_EXECUTABLE;
-	/*
-	 * Add 1Mb pad at end in case a library with this large alignment
-	 * has been mapped before. We want to avoid ending up in a hole in
-	 * the mapping of a library.
-	 */
-	res = sys_map_ta_bin(&va, SMALL_PAGE_SIZE, flags, elf->handle, 0, 0,
-			     max_align);
+	res = sys_map_ta_bin(&va, SMALL_PAGE_SIZE, flags, elf->handle, 0, 0, 0);
 	if (res)
 		err(res, "sys_map_ta_bin");
 	elf->ehdr_addr = va;
@@ -548,8 +541,35 @@ static void populate_segments(struct ta_elf *elf)
 
 static void map_segments(struct ta_elf *elf)
 {
+	TEE_Result res = TEE_SUCCESS;
+
 	parse_load_segments(elf);
 	adjust_segments(elf);
+	if (TAILQ_FIRST(&elf->segs)->offset < SMALL_PAGE_SIZE) {
+		vaddr_t va = 0;
+		size_t sz = elf->max_addr - elf->load_addr;
+		struct segment *seg = TAILQ_LAST(&elf->segs, segment_head);
+
+		/*
+		 * We're loading a library, if not other parts of the code
+		 * need to be updated too.
+		 */
+		assert(!elf->is_main);
+
+		/*
+		 * Now that we know how much virtual memory is needed move
+		 * the already mapped part to a location which can
+		 * accommodate us.
+		 */
+		res = sys_remap(elf->load_addr, &va, sz, 0,
+				roundup(seg->vaddr + seg->memsz));
+		if (res)
+			err(res, "sys_remap");
+		elf->ehdr_addr = va;
+		elf->load_addr = va;
+		elf->max_addr = va + sz;
+		elf->phdr = (void *)(va + elf->e_phoff);
+	}
 	if (elf->is_legacy)
 		populate_segments_legacy(elf);
 	else

ldelf/sys.c
@@ -205,3 +205,31 @@ TEE_Result sys_set_prot(vaddr_t va, size_t num_bytes, uint32_t flags)
 
 	return invoke_sys_ta(PTA_SYSTEM_SET_PROT, &params);
 }
+
+TEE_Result sys_remap(vaddr_t old_va, vaddr_t *new_va, size_t num_bytes,
+		     size_t pad_begin, size_t pad_end)
+{
+	TEE_Result res = TEE_SUCCESS;
+	struct utee_params params = {
+		.types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+					 TEE_PARAM_TYPE_VALUE_INPUT,
+					 TEE_PARAM_TYPE_VALUE_INOUT,
+					 TEE_PARAM_TYPE_VALUE_INPUT),
+	};
+	uint32_t r[2] = { 0 };
+
+	params.vals[0] = num_bytes;
+	reg_pair_from_64(old_va, r, r + 1);
+	params.vals[2] = r[0];
+	params.vals[3] = r[1];
+	reg_pair_from_64(*new_va, r, r + 1);
+	params.vals[4] = r[0];
+	params.vals[5] = r[1];
+	params.vals[6] = pad_begin;
+	params.vals[7] = pad_end;
+
+	res = invoke_sys_ta(PTA_SYSTEM_REMAP, &params);
+	if (!res)
+		*new_va = reg_pair_to_64(params.vals[4], params.vals[5]);
+	return res;
+}

ldelf/sys.h
@@ -44,5 +44,7 @@ TEE_Result sys_map_ta_bin(vaddr_t *va, size_t num_bytes, uint32_t flags,
 TEE_Result sys_copy_from_ta_bin(void *dst, size_t num_bytes, uint32_t handle,
 				size_t offs);
 TEE_Result sys_set_prot(vaddr_t va, size_t num_bytes, uint32_t flags);
+TEE_Result sys_remap(vaddr_t old_va, vaddr_t *new_va, size_t num_bytes,
+		     size_t pad_begin, size_t pad_end);
 
 #endif /*SYS_H*/

commit a0dc20b1b3b70c8f75d17382f0d73bd533534a0a

ldelf: reject ELF files without loadable segments
A malformed ELF file without a PT_LOAD program header leaves the
segment list empty. map_segments() then dereferences the missing first
segment.

Reject such files as having a bad format before dereferencing the first
segment.

Fixes: 88796f894843 ("ldelf: remap first page of libraries")
Signed-off-by: Sungbae Yoo <sungbaey@nvidia.com>
Reviewed-by: Lars Persson <larper@axis.com>
Reviewed-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

ldelf/ta_elf.c
@@ -1002,6 +1002,9 @@ static void map_segments(struct ta_elf *elf)
 	TEE_Result res = TEE_SUCCESS;
 
 	parse_load_segments(elf);
+	if (TAILQ_EMPTY(&elf->segs))
+		err(TEE_ERROR_BAD_FORMAT, "No loadable segments");
+
 	adjust_segments(elf);
 	if (TAILQ_FIRST(&elf->segs)->offset < SMALL_PAGE_SIZE) {
 		vaddr_t va = 0;