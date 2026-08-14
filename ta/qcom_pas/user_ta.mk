86b3e54dddc3ccdfab5ce4e33497f27a1230515e
Jens Wiklander <jens.wiklander@oss.qualcomm.com>
2026-08-14 10:23:31 +0200

    ta: qcom_pas: restrict to Qualcomm platforms
    The Qualcomm PAS TA isn't useful for other platforms. To avoid
    unnecessary building and maintenance, restrict the TA to only be built
    for Qualcomm platforms.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
    Reviewed-by: Rouven Czerwinski <rouven.czerwinski@linaro.org>

--- ta/qcom_pas/user_ta.mk
+++ ta/qcom_pas/user_ta.mk
@@ -1,4 +1,8 @@
 user-ta-uuid := cff7d191-7ca0-4784-af13-48223b9a4fbe
 
+ifneq ($(PLATFORM),qcom)
+user-ta-skip := y
+endif
+
 # PAS TA heap size can be customized if 4kB is not enough
 CFG_PAS_TA_HEAP_SIZE ?= (4 * 1024)
