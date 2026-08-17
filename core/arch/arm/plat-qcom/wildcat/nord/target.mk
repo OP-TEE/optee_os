09b483551eee56ac862e88fdf2d75299a43e8700
Pawan Rai <pawarai@qti.qualcomm.com>
2026-08-17 14:41:56 +0530

    plat-qcom: wildcat: nord: move Nord-specific addresses to its target files
    Nord was the only Wildcat chip in the tree, so its GIC base addresses
    and DARE-TZ TZDRAM region settings were placed in the shared Wildcat
    architecture layer.  Adding a second Wildcat chip with different
    addresses makes the architecture layer the wrong home for them.
    
    Move GICD_BASE and GICR_BASE from arch_config.h to
    nord/target_config.h, and move the DARE-TZ TZDRAM region configuration
    from qcom-arch.mk to nord/target.mk, so the shared Wildcat layer stays
    chip-agnostic.  Add SPDX licence identifier to qcom-arch.mk while there.
    
    Signed-off-by: Pawan Rai <pawarai@qti.qualcomm.com>
    Reviewed-by: Harshal Dev <harshal.dev@oss.qualcomm.com>
    Tested-by: Harshal Dev <harshal.dev@oss.qualcomm.com>
    Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
    Acked-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

--- core/arch/arm/plat-qcom/wildcat/nord/target.mk
+++ core/arch/arm/plat-qcom/wildcat/nord/target.mk
@@ -3,3 +3,11 @@
 # Threads are expensive in OP-TEE, so they don't have
 # to be same as number of cores.
 $(call force,CFG_TEE_CORE_NB_CORE,18)
+
+# DARE-TZ secure memory regions. DARE is another in-line
+# memory encryption IP similar to pIMEM but on wildcat arch
+# it is setup by TME root-of-trust, no specific driver needed
+# in OP-TEE for that.
+CFG_TZDRAM_START ?= 0xBC280000
+CFG_TEE_RAM_VA_SIZE ?= 0x00200000
+CFG_TA_RAM_VA_SIZE ?= 0x07B80000
