d71b58a9eb28751092789765b57bb207e6ca5251
Selvam Sathappan <speriaka@qti.qualcomm.com>
2026-08-20 20:55:31 +0530

    Revert "ta: qcom_pas: add Multiboot Binary (MBN) hash-segment parser"
    This reverts commit c297c66d1e22f76d74396dbee098b4f3dc433310.

--- ta/qcom_pas/src/sub.mk
+++ ta/qcom_pas/src/sub.mk
@@ -1,3 +1,1 @@
-global-incdirs-y += ../include
 srcs-y += qcom_pas.c
-srcs-$(CFG_QCOM_PAS_AUTH) += pas_mbn_parser.c
--- ta/qcom_pas/user_ta.mk
+++ ta/qcom_pas/user_ta.mk
@@ -1,9 +1,4 @@
 user-ta-uuid := cff7d191-7ca0-4784-af13-48223b9a4fbe
 
-ifeq ($(CFG_QCOM_PAS_AUTH),y)
-CFG_PAS_TA_HEAP_SIZE ?= (512 * 1024)
-else
+# PAS TA heap size can be customized if 4kB is not enough
 CFG_PAS_TA_HEAP_SIZE ?= (4 * 1024)
-endif
-
-CFG_QCOM_PAS_AUTH ?= n
