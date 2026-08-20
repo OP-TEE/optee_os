OP-TEE/optee_os

commit 9e47f7e14776d71a94a7197675907117ceef0292

core: derive key: check provided out key size
syscall_cryp_derive_key() takes a derived_key parameter where the
derived key data is stored. The algorithms DH_DERIVE_SHARED_SECRET,
HKDF, CONCAT_KDF, and PBKDF2 doesn't check the output key size. Fix this
by adding or fixing the needed checks. Add an extra check to make sure
that the provided output key is a simple symmetric key.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@arm.com>
Acked-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

core/tee/tee_svc_cryp.c
@@ -3719,15 +3719,24 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 	if (res != TEE_SUCCESS)
 		goto out;
 
-	/* Find information needed about the object to initialize */
-	sk = so->attr;
-
 	/* Find description of object */
 	type_props = tee_svc_find_type_props(so->info.objectType);
 	if (!type_props) {
 		res = TEE_ERROR_NOT_SUPPORTED;
 		goto out;
 	}
+
+	/*
+	 * The key type must be a simple symmetric key since sk represents
+	 * such a key type.
+	 */
+	if (type_props->type_attrs != tee_cryp_obj_secret_value_attrs) {
+		res = TEE_ERROR_NOT_SUPPORTED;
+		goto out;
+	}
+
+	/* Find information needed about the object to initialize */
+	sk = so->attr;
 
 	if (cs->algo == TEE_ALG_DH_DERIVE_SHARED_SECRET) {
 		struct bignum *pub = NULL;
@@ -3755,21 +3764,24 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 
 		pub = crypto_bignum_allocate(alloc_size);
 		ss = crypto_bignum_allocate(alloc_size);
-		if (pub && ss) {
-			crypto_bignum_bin2bn(bbuf, bin_size, pub);
-			res = crypto_acipher_dh_shared_secret(ko->attr,
-							      pub, ss);
-			if (res == TEE_SUCCESS) {
-				sk->key_size = crypto_bignum_num_bytes(ss);
-				crypto_bignum_bn2bin(ss, (uint8_t *)(sk + 1));
-				so->info.handleFlags |=
-						TEE_HANDLE_FLAG_INITIALIZED;
-				set_attribute(so, type_props,
-					      TEE_ATTR_SECRET_VALUE);
-			}
-		} else {
+		if (!pub || !ss) {
 			res = TEE_ERROR_OUT_OF_MEMORY;
+			goto dh_out;
 		}
+		crypto_bignum_bin2bn(bbuf, bin_size, pub);
+		res = crypto_acipher_dh_shared_secret(ko->attr,
+						      pub, ss);
+		if (res)
+			goto dh_out;
+		if (crypto_bignum_num_bytes(ss) > sk->alloc_size) {
+			res = TEE_ERROR_BAD_PARAMETERS;
+			goto dh_out;
+		}
+		sk->key_size = crypto_bignum_num_bytes(ss);
+		crypto_bignum_bn2bin(ss, (uint8_t *)(sk + 1));
+		so->info.handleFlags |= TEE_HANDLE_FLAG_INITIALIZED;
+		set_attribute(so, type_props, TEE_ATTR_SECRET_VALUE);
+dh_out:
 		crypto_bignum_free(&pub);
 		crypto_bignum_free(&ss);
 	} else if (cs->algo == TEE_ALG_ECDH_DERIVE_SHARED_SECRET) {
@@ -3862,7 +3874,7 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 			goto out;
 
 		/* Requested size must fit into the output object's buffer */
-		if (okm_len > ik->alloc_size) {
+		if (okm_len > sk->alloc_size) {
 			res = TEE_ERROR_BAD_PARAMETERS;
 			goto out;
 		}
@@ -3891,7 +3903,7 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 			goto out;
 
 		/* Requested size must fit into the output object's buffer */
-		if (derived_key_len > ss->alloc_size) {
+		if (derived_key_len > sk->alloc_size) {
 			res = TEE_ERROR_BAD_PARAMETERS;
 			goto out;
 		}
@@ -3920,7 +3932,7 @@ TEE_Result syscall_cryp_derive_key(unsigned long state,
 			goto out;
 
 		/* Requested size must fit into the output object's buffer */
-		if (derived_key_len > ss->alloc_size) {
+		if (derived_key_len > sk->alloc_size) {
 			res = TEE_ERROR_BAD_PARAMETERS;
 			goto out;
 		}

commit 8d80d6e70d46345c928dd7d8ac7e0ef16e4f7a8f

core: derive key: fix out-of-bounds write in SM2-KEP derivation
In syscall_cryp_derive_key(), the TEE_ALG_SM2_KEP branch sets the KEP
output length from so->info.maxObjectSize, which is expressed in bits,
and passes it as the number of bytes written by sm2_kdf() into the
derived key's secret buffer. This writes 8x the intended size and
overruns the sk->alloc_size allocation (up to ~3.5 KiB for a 4096-bit
object).

The sibling key-derivation branches (HKDF, Concat KDF, PBKDF2) already
bound the derived length against sk->alloc_size; the SM2-KEP branch was
missed when that check was added.

Convert the output length from bits to bytes and reject an output that
would exceed the allocated buffer, matching the other branches.

Fixes: 9e47f7e14 ("core: derive key: check provided out key size")
Signed-off-by: Gyeongsik Song <secretpack97@gmail.com>

core/tee/tee_svc_cryp.c
@@ -3958,9 +3958,14 @@ ecdh_out:
 		struct ecc_public_key peer_key = { };
 		struct sm2_kep_parms kep_parms = {
 			.out = (uint8_t *)(sk + 1),
-			.out_len = so->info.maxObjectSize,
+			.out_len = so->info.maxObjectSize / 8,
 		};
 		struct tee_obj *ko2 = NULL;
+
+		if (kep_parms.out_len > sk->alloc_size) {
+			res = TEE_ERROR_BAD_PARAMETERS;
+			goto out;
+		}
 
 		res = tee_obj_get(utc, cs->key2, &ko2);
 		if (res != TEE_SUCCESS)