OP-TEE/optee_os

commit 7509ff7ce5e5a7679319e6fa059b71dd6f6cc8b9

Add user mode ELF loader
Adds ldelf which loads user mode TAs while in user mode. The TA ELF file is
loaded and relocated by ldelf before the TA can be executed.

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf_rel.c
@@ -1,0 +1,332 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2019, Linaro Limited
+ */
+
+#include <assert.h>
+#include <elf32.h>
+#include <elf64.h>
+#include <elf_common.h>
+#include <string.h>
+#include <tee_api_types.h>
+#include <util.h>
+
+#include "sys.h"
+#include "ta_elf.h"
+
+static bool __resolve_sym(struct ta_elf *elf, unsigned int bind,
+			  size_t st_shndx, size_t st_name, size_t st_value,
+			  const char *name, vaddr_t *val)
+{
+	if (bind != STB_GLOBAL)
+		return false;
+	if (st_shndx == SHN_UNDEF || st_shndx == SHN_XINDEX)
+		return false;
+	if (!st_name)
+		return false;
+	if (st_name > elf->dynstr_size)
+		err(TEE_ERROR_BAD_FORMAT, "Symbol out of range");
+
+	if (strcmp(name, elf->dynstr + st_name))
+		return false;
+
+	*val = st_value + elf->load_addr;
+	return true;
+}
+
+static void resolve_sym(const char *name, vaddr_t *val)
+{
+	struct ta_elf *elf = NULL;
+	size_t n = 0;
+
+	TAILQ_FOREACH(elf, &main_elf_queue, link) {
+		if (elf->is_32bit) {
+			Elf32_Sym *sym = elf->dynsymtab;
+
+			for (n = 0; n < elf->num_dynsyms; n++) {
+				if (__resolve_sym(elf,
+						  ELF32_ST_BIND(sym[n].st_info),
+						  sym[n].st_shndx,
+						  sym[n].st_name,
+						  sym[n].st_value, name, val))
+					return;
+			}
+		} else {
+			Elf64_Sym *sym = elf->dynsymtab;
+
+			for (n = 0; n < elf->num_dynsyms; n++) {
+				if (__resolve_sym(elf,
+						  ELF64_ST_BIND(sym[n].st_info),
+						  sym[n].st_shndx,
+						  sym[n].st_name,
+						  sym[n].st_value, name, val))
+					return;
+			}
+		}
+	}
+	err(TEE_ERROR_ITEM_NOT_FOUND, "Symbol %s not found", name);
+}
+
+static void e32_process_dyn_rel(const Elf32_Sym *sym_tab, size_t num_syms,
+				const char *str_tab, size_t str_tab_size,
+				Elf32_Rel *rel, Elf32_Addr *where)
+{
+	size_t sym_idx = 0;
+	const char *name = NULL;
+	vaddr_t val = 0;
+	size_t name_idx = 0;
+
+	sym_idx = ELF32_R_SYM(rel->r_info);
+	assert(sym_idx < num_syms);
+
+	name_idx = sym_tab[sym_idx].st_name;
+	assert(name_idx < str_tab_size);
+	name = str_tab + name_idx;
+
+	resolve_sym(name, &val);
+	*where = val;
+}
+
+static void e32_relocate(struct ta_elf *elf, unsigned int rel_sidx)
+{
+	Elf32_Shdr *shdr = elf->shdr;
+	Elf32_Rel *rel = NULL;
+	Elf32_Rel *rel_end = NULL;
+	size_t sym_tab_idx = 0;
+	Elf32_Sym *sym_tab = NULL;
+	size_t num_syms = 0;
+	size_t sh_end = 0;
+	const char *str_tab = NULL;
+	size_t str_tab_size = 0;
+
+	assert(shdr[rel_sidx].sh_type == SHT_REL);
+
+	assert(shdr[rel_sidx].sh_entsize == sizeof(Elf32_Rel));
+
+	sym_tab_idx = shdr[rel_sidx].sh_link;
+	if (sym_tab_idx) {
+		size_t str_tab_idx = 0;
+
+		assert(sym_tab_idx < elf->e_shnum);
+
+		assert(shdr[sym_tab_idx].sh_entsize == sizeof(Elf32_Sym));
+
+		/* Check the address is inside ELF memory */
+		if (ADD_OVERFLOW(shdr[sym_tab_idx].sh_addr,
+				 shdr[sym_tab_idx].sh_size, &sh_end))
+			err(TEE_ERROR_SECURITY, "Overflow");
+		assert(sh_end < (elf->max_addr - elf->load_addr));
+
+		sym_tab = (Elf32_Sym *)(elf->load_addr +
+					shdr[sym_tab_idx].sh_addr);
+
+		num_syms = shdr[sym_tab_idx].sh_size / sizeof(Elf32_Sym);
+
+		str_tab_idx = shdr[sym_tab_idx].sh_link;
+		if (str_tab_idx) {
+			/* Check the address is inside ELF memory */
+			if (ADD_OVERFLOW(shdr[str_tab_idx].sh_addr,
+					 shdr[str_tab_idx].sh_size, &sh_end))
+				err(TEE_ERROR_SECURITY, "Overflow");
+			assert(sh_end < (elf->max_addr - elf->load_addr));
+
+			str_tab = (const char *)(elf->load_addr +
+						 shdr[str_tab_idx].sh_addr);
+			str_tab_size = shdr[str_tab_idx].sh_size;
+		}
+	}
+
+	/* Check the address is inside TA memory */
+	assert(shdr[rel_sidx].sh_addr < (elf->max_addr - elf->load_addr));
+	rel = (Elf32_Rel *)(elf->load_addr + shdr[rel_sidx].sh_addr);
+
+	/* Check the address is inside TA memory */
+	if (ADD_OVERFLOW(shdr[rel_sidx].sh_addr, shdr[rel_sidx].sh_size,
+			 &sh_end))
+		err(TEE_ERROR_SECURITY, "Overflow");
+	assert(sh_end < (elf->max_addr - elf->load_addr));
+	rel_end = rel + shdr[rel_sidx].sh_size / sizeof(Elf32_Rel);
+	for (; rel < rel_end; rel++) {
+		Elf32_Addr *where = NULL;
+		size_t sym_idx = 0;
+
+		/* Check the address is inside TA memory */
+		assert(rel->r_offset < (elf->max_addr - elf->load_addr));
+		where = (Elf32_Addr *)(elf->load_addr + rel->r_offset);
+
+		switch (ELF32_R_TYPE(rel->r_info)) {
+		case R_ARM_ABS32:
+			sym_idx = ELF32_R_SYM(rel->r_info);
+			assert(sym_idx < num_syms);
+			if (sym_tab[sym_idx].st_shndx == SHN_UNDEF) {
+				/* Symbol is external */
+				e32_process_dyn_rel(sym_tab, num_syms, str_tab,
+						    str_tab_size, rel, where);
+			} else {
+				*where += elf->load_addr +
+					  sym_tab[sym_idx].st_value;
+			}
+			break;
+		case R_ARM_REL32:
+			sym_idx = ELF32_R_SYM(rel->r_info);
+			assert(sym_idx < num_syms);
+			*where += sym_tab[sym_idx].st_value - rel->r_offset;
+			break;
+		case R_ARM_RELATIVE:
+			*where += elf->load_addr;
+			break;
+		case R_ARM_GLOB_DAT:
+		case R_ARM_JUMP_SLOT:
+			e32_process_dyn_rel(sym_tab, num_syms, str_tab,
+					    str_tab_size, rel, where);
+			break;
+		default:
+			err(TEE_ERROR_BAD_FORMAT, "Unknown relocation type %d",
+			     ELF32_R_TYPE(rel->r_info));
+		}
+	}
+}
+
+#ifdef ARM64
+static void e64_process_dyn_rela(const Elf64_Sym *sym_tab, size_t num_syms,
+				 const char *str_tab, size_t str_tab_size,
+				 Elf64_Rela *rela, Elf64_Addr *where)
+{
+	size_t sym_idx = 0;
+	const char *name = NULL;
+	uintptr_t val = 0;
+	size_t name_idx = 0;
+
+	sym_idx = ELF64_R_SYM(rela->r_info);
+	assert(sym_idx < num_syms);
+
+	name_idx = sym_tab[sym_idx].st_name;
+	assert(name_idx < str_tab_size);
+	name = str_tab + name_idx;
+
+	resolve_sym(name, &val);
+	*where = val;
+}
+
+static void e64_relocate(struct ta_elf *elf, unsigned int rel_sidx)
+{
+	Elf64_Shdr *shdr = elf->shdr;
+	Elf64_Rela *rela = NULL;
+	Elf64_Rela *rela_end = NULL;
+	size_t sym_tab_idx = 0;
+	Elf64_Sym *sym_tab = NULL;
+	size_t num_syms = 0;
+	size_t sh_end = 0;
+	const char *str_tab = NULL;
+	size_t str_tab_size = 0;
+
+	assert(shdr[rel_sidx].sh_type == SHT_RELA);
+
+	assert(shdr[rel_sidx].sh_entsize == sizeof(Elf64_Rela));
+
+	sym_tab_idx = shdr[rel_sidx].sh_link;
+	if (sym_tab_idx) {
+		size_t str_tab_idx = 0;
+
+		assert(sym_tab_idx < elf->e_shnum);
+
+		assert(shdr[sym_tab_idx].sh_entsize == sizeof(Elf64_Sym));
+
+		/* Check the address is inside TA memory */
+		if (ADD_OVERFLOW(shdr[sym_tab_idx].sh_addr,
+				 shdr[sym_tab_idx].sh_size, &sh_end))
+			err(TEE_ERROR_SECURITY, "Overflow");
+		assert(sh_end < (elf->max_addr - elf->load_addr));
+
+		sym_tab = (Elf64_Sym *)(elf->load_addr +
+					shdr[sym_tab_idx].sh_addr);
+
+		num_syms = shdr[sym_tab_idx].sh_size / sizeof(Elf64_Sym);
+
+		str_tab_idx = shdr[sym_tab_idx].sh_link;
+		if (str_tab_idx) {
+			/* Check the address is inside ELF memory */
+			if (ADD_OVERFLOW(shdr[str_tab_idx].sh_addr,
+					 shdr[str_tab_idx].sh_size, &sh_end))
+				err(TEE_ERROR_SECURITY, "Overflow");
+			assert(sh_end < (elf->max_addr - elf->load_addr));
+
+			str_tab = (const char *)(elf->load_addr +
+						 shdr[str_tab_idx].sh_addr);
+			str_tab_size = shdr[str_tab_idx].sh_size;
+		}
+	}
+
+	/* Check the address is inside TA memory */
+	assert(shdr[rel_sidx].sh_addr < (elf->max_addr - elf->load_addr));
+	rela = (Elf64_Rela *)(elf->load_addr + shdr[rel_sidx].sh_addr);
+
+	/* Check the address is inside TA memory */
+	if (ADD_OVERFLOW(shdr[rel_sidx].sh_addr, shdr[rel_sidx].sh_size,
+			 &sh_end))
+		err(TEE_ERROR_SECURITY, "Overflow");
+	assert(sh_end < (elf->max_addr - elf->load_addr));
+	rela_end = rela + shdr[rel_sidx].sh_size / sizeof(Elf64_Rela);
+	for (; rela < rela_end; rela++) {
+		Elf64_Addr *where = NULL;
+		size_t sym_idx = 0;
+
+		/* Check the address is inside TA memory */
+		assert(rela->r_offset < (elf->max_addr - elf->load_addr));
+
+		where = (Elf64_Addr *)(elf->load_addr + rela->r_offset);
+
+		switch (ELF64_R_TYPE(rela->r_info)) {
+		case R_AARCH64_ABS64:
+			sym_idx = ELF64_R_SYM(rela->r_info);
+			assert(sym_idx < num_syms);
+			if (sym_tab[sym_idx].st_shndx == SHN_UNDEF) {
+				/* Symbol is external */
+				e64_process_dyn_rela(sym_tab, num_syms, str_tab,
+						     str_tab_size, rela, where);
+			} else {
+				*where = rela->r_addend + elf->load_addr +
+					 sym_tab[sym_idx].st_value;
+			}
+			break;
+		case R_AARCH64_RELATIVE:
+			*where = rela->r_addend + elf->load_addr;
+			break;
+		case R_AARCH64_GLOB_DAT:
+		case R_AARCH64_JUMP_SLOT:
+			e64_process_dyn_rela(sym_tab, num_syms, str_tab,
+					     str_tab_size, rela, where);
+			break;
+		default:
+			err(TEE_ERROR_BAD_FORMAT, "Unknown relocation type %zd",
+			     ELF64_R_TYPE(rela->r_info));
+		}
+	}
+}
+#else /*ARM64*/
+static void e64_relocate(struct ta_elf *elf __unused,
+			 unsigned int rel_sidx __unused)
+{
+	err(TEE_ERROR_NOT_SUPPORTED, "arm64 not supported");
+}
+#endif /*ARM64*/
+
+void ta_elf_relocate(struct ta_elf *elf)
+{
+	size_t n = 0;
+
+	if (elf->is_32bit) {
+		Elf32_Shdr *shdr = elf->shdr;
+
+		for (n = 0; n < elf->e_shnum; n++)
+			if (shdr[n].sh_type == SHT_REL)
+				e32_relocate(elf, n);
+	} else {
+		Elf64_Shdr *shdr = elf->shdr;
+
+		for (n = 0; n < elf->e_shnum; n++)
+			if (shdr[n].sh_type == SHT_RELA)
+				e64_relocate(elf, n);
+
+	}
+}

Makefile
@@ -77,6 +77,7 @@ ta-targets ?= invalid
 default-user-ta-target ?= $(firstword $(ta-targets))
 
 ifeq ($(CFG_WITH_USER_TA),y)
+include ldelf/ldelf.mk
 define build-ta-target
 ta-target := $(1)
 include ta/ta.mk

ldelf/include/elf_common.h
@@ -37,6 +37,7 @@
  * ELF definitions that are independent of architecture or word size.
  */
 
+#ifndef ASM
 /*
  * Note header.  The ".note" section contains an array of notes.  Each
  * begins with this header, aligned to a word boundary.  Immediately
@@ -62,6 +63,7 @@ typedef struct {
 	uint32_t	gh_maskwords;	/* #maskwords used in bloom filter. */
 	uint32_t	gh_shift2;	/* Bloom filter shift count. */
 } Elf_GNU_Hash_Header;
+#endif /*ASM*/
 
 /* Indexes into the e_ident array.  Keep synced with
    http://www.sco.com/developers/gabi/latest/ch4.eheader.html */

ldelf/include/ldelf.h
@@ -1,0 +1,43 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2019, Linaro Limited
+ */
+
+#ifndef __LDELF_H
+#define __LDELF_H
+
+#include <stdint.h>
+#include <tee_api_types.h>
+
+/* Size of stack for TEE Core to allocate */
+#define LDELF_STACK_SIZE	(4096 * 2)
+
+/*
+ * struct ldelf_arg - argument for ldelf
+ * @uuid:	[in] UUID of TA to load
+ * @is_32bit:	[out] 1 if a 32bit TA or 0 if a 64bit TA
+ * @flags:	[out] Flags field of TA header
+ * @entry_func:	[out] TA entry function
+ * @stack_ptr:	[out] TA stack pointer
+ */
+struct ldelf_arg {
+	TEE_UUID uuid;
+	uint32_t is_32bit;
+	uint32_t flags;
+	uint64_t entry_func;
+	uint64_t stack_ptr;
+};
+
+/*
+ * ldelf is loaded into memory by TEE Core. BSS is initialized and a
+ * stack is allocated and supplied in SP register. A struct ldelf_arg
+ * is placed in the stack and a pointer to the struct is provided in
+ * r0/x0.
+ *
+ * ldelf relocates itself to the address where it is loaded before the main
+ * C routine is called.
+ *
+ * In the main C routine the TA is loaded using the PTA System interface.
+ */
+
+#endif /*__LDELF_H*/

ldelf/ldelf.ld.S
@@ -1,0 +1,80 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2014, STMicroelectronics International N.V.
+ * Copyright (c) 2016-2019, Linaro Limited
+ */
+
+#ifdef ARM32
+OUTPUT_FORMAT("elf32-littlearm")
+OUTPUT_ARCH(arm)
+#endif
+#ifdef ARM64
+OUTPUT_FORMAT("elf64-littleaarch64")
+OUTPUT_ARCH(aarch64)
+#endif
+
+ENTRY(_ldelf_start)
+SECTIONS {
+	.text : {
+		__text_start = .;
+		*(.text._ldelf_start .text .text.*)
+		*(.stub)
+		*(.glue_7)
+		*(.glue_7t)
+		*(.gnu.linkonce.t.*)
+		/* Workaround for an erratum in ARM's VFP11 coprocessor */
+		*(.vfp11_veneer)
+		__text_end = .;
+	}
+        .plt : { *(.plt) }
+
+	.eh_frame : { *(.eh_frame) }
+	.rodata : {
+		*(.gnu.linkonce.r.*)
+		*(.rodata .rodata.*)
+	}
+	/* .ARM.exidx is sorted, so has to go in its own output section.  */
+	.ARM.exidx : { *(.ARM.exidx* .gnu.linkonce.armexidx.*) }
+	.ARM.extab : { *(.ARM.extab*) }
+        .ctors : { *(.ctors) }
+        .dtors : { *(.dtors) }
+	.dynsym : { *(.dynsym) }
+	.dynstr : { *(.dynstr) }
+	.hash : { *(.hash) }
+
+	/* Page align to allow dropping execute bit for RW data */
+	. = ALIGN(4096);
+
+	.got : { *(.got.plt) *(.got) }
+	__reloc_begin = .;
+	.rel.got : { *(.rel.got) }
+	.rela.got : { *(.rela.got) }
+	.rel.text : { *(.rel.text) *(.rel.gnu.linkonce.t*) }
+	.rela.text : { *(.rela.text) *(.rela.gnu.linkonce.t*) }
+	.rel.data : { *(.rel.data) *(.rel.gnu.linkonce.d*) }
+	.rela.data : { *(.rela.data) *(.rela.gnu.linkonce.d*) }
+	.rel.rodata : { *(.rel.rodata) *(.rel.gnu.linkonce.r*) }
+	.rela.rodata : { *(.rela.rodata) *(.rela.gnu.linkonce.r*) }
+	.rel.dyn : { *(.rel.dyn) }
+	.rela.dyn : { *(.rela.dyn) }
+	.rel.ctors : { *(.rel.ctors) }
+	.rela.ctors : { *(.rela.ctors) }
+	.rel.dtors : { *(.rel.dtors) }
+	.rela.dtors : { *(.rela.dtors) }
+	.rel.init : { *(.rel.init) }
+	.rela.init : { *(.rela.init) }
+	.rel.fini : { *(.rel.fini) }
+	.rela.fini : { *(.rela.fini) }
+	.rel.bss : { *(.rel.bss) }
+	.rela.bss : { *(.rela.bss) }
+	.rel.plt : { *(.rel.plt) }
+	.rela.plt : { *(.rela.plt) }
+	__reloc_end = .;
+
+	.dynamic : { *(.dynamic) }
+
+	.data : { *(.data .data.* .gnu.linkonce.d.*) }
+	.bss : { *(.bss .bss.* .gnu.linkonce.b.* COMMON) }
+
+	/DISCARD/ : { *(.interp) }
+}

ldelf/ldelf.mk
@@ -1,0 +1,46 @@
+include mk/cleanvars.mk
+
+# Set current submodule (used for module specific flags compile result etc)
+sm := ldelf
+sm-$(sm) := y
+
+link-out-dir$(sm) := $(out-dir)/$(sm)
+
+cppflags$(sm)	:= $(core-platform-cppflags)
+cflags$(sm)	:= $(core-platform-cflags) -fpie -fvisibility=hidden
+aflags$(sm)	:= $(core-platform-aflags)
+
+# ldelf is compiled for the same arch or register width as core
+ifeq ($(CFG_ARM64_core),y)
+CFG_ARM64_$(sm) := y
+endif
+ifeq ($(CFG_ARM32_core),y)
+CFG_ARM32_$(sm) := y
+endif
+
+cppflags$(sm)	+= -include $(conf-file)
+cppflags$(sm)	+= -DTRACE_LEVEL=$(CFG_TEE_CORE_LOG_LEVEL)
+
+# Use same compiler as for core
+CROSS_COMPILE_$(sm)	:= $(CROSS_COMPILE_core)
+COMPILER_$(sm)		:= $(COMPILER_core)
+include mk/$(COMPILER_$(sm)).mk
+
+base-prefix := $(sm)-
+
+libname = utils
+libdir = lib/libutils
+include mk/lib.mk
+
+libname = utee
+libdir = lib/libutee
+include mk/lib.mk
+
+base-prefix :=
+
+subdirs = ldelf
+include mk/subdir.mk
+
+include mk/compile.mk
+
+include ldelf/link.mk

ldelf/link.mk
@@ -1,0 +1,52 @@
+link-script$(sm) = ldelf/ldelf.ld.S
+link-script-pp$(sm) = $(link-out-dir$(sm))/ldelf.lds
+link-script-dep$(sm) = $(link-out-dir$(sm))/.ldelf.ld.d
+
+.PHONY: ldelf
+ldelf: $(link-out-dir$(sm))/ldelf.dmp
+ldelf: $(link-out-dir$(sm))/ldelf.elf
+all: ldelf
+
+cleanfiles += $(link-out-dir$(sm))/ldelf.dmp
+cleanfiles += $(link-out-dir$(sm))/ldelf.elf
+cleanfiles += $(link-script-pp$(sm)) $(link-script-dep$(sm))
+
+link-ldflags  = -pie -static --gc-sections
+link-ldflags += -T $(link-script-pp$(sm))
+link-ldflags += -Map=$(link-out-dir$(sm))/ldelf.map
+link-ldflags += --sort-section=alignment
+link-ldflags += -z max-page-size=4096 # OP-TEE always uses 4K alignment
+link-ldflags += $(link-ldflags$(sm))
+
+link-ldadd  = $(addprefix -L,$(libdirs))
+link-ldadd += --start-group $(addprefix -l,$(libnames)) --end-group
+ldargs-ldelf.elf := $(link-ldflags) $(objs) $(link-ldadd)
+
+link-script-cppflags-$(sm) := -DASM=1 \
+	$(filter-out $(CPPFLAGS_REMOVE) $(cppflags-remove), \
+		$(nostdinc$(sm)) $(CPPFLAGS) \
+		$(addprefix -I,$(incdirs$(sm)) $(link-out-dir$(sm))) \
+		$(cppflags$(sm)))
+
+-include $(link-script-dep$(sm))
+
+link-script-pp-makefiles$(sm) = $(filter-out %.d %.cmd,$(MAKEFILE_LIST))
+
+define gen-link-t
+$(link-script-pp$(sm)): $(link-script$(sm)) $(conf-file) \
+			$(link-script-pp-makefiles$(sm))
+	@$(cmd-echo-silent) '  CPP     $$@'
+	$(q)mkdir -p $$(dir $$@)
+	$(q)$(CPP$(sm)) -Wp,-P,-MT,$$@,-MD,$(link-script-dep$(sm)) \
+		$(link-script-cppflags-$(sm)) $$< > $$@
+
+$(link-out-dir$(sm))/ldelf.elf: $(objs) $(libdeps) $(link-script-pp$(sm))
+	@$(cmd-echo-silent) '  LD      $$@'
+	$(q)$(LD$(sm)) $(ldargs-ldelf.elf) -o $$@
+
+$(link-out-dir$(sm))/ldelf.dmp: $(link-out-dir$(sm))/ldelf.elf
+	@$(cmd-echo-silent) '  OBJDUMP $$@'
+	$(q)$(OBJDUMP$(sm)) -l -x -d $$< > $$@
+endef
+
+$(eval $(call gen-link-t))

ldelf/main.c
@@ -1,0 +1,67 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2019, Linaro Limited
+ */
+
+#include <ldelf.h>
+#include <malloc.h>
+#include <sys/queue.h>
+#include <tee_api_types.h>
+#include <trace.h>
+#include <types_ext.h>
+
+#include "ta_elf.h"
+#include "sys.h"
+
+static size_t mpool_size = 2 * SMALL_PAGE_SIZE;
+static vaddr_t mpool_base;
+
+/*
+ * ldelf()- Loads ELF into memory
+ * @arg:	Argument passing to/from TEE Core
+ *
+ * Only called from assembly
+ */
+void __noreturn ldelf(struct ldelf_arg *arg);
+void ldelf(struct ldelf_arg *arg)
+{
+	TEE_Result res = TEE_SUCCESS;
+	struct ta_elf *elf = NULL;
+
+	DMSG("Loading TA %pUl", (void *)&arg->uuid);
+	res = sys_map_zi(mpool_size, 0, &mpool_base, 0, 0);
+	if (res) {
+		EMSG("sys_map_zi(%zu): result %"PRIx32, mpool_size, res);
+		panic();
+	}
+	malloc_add_pool((void *)mpool_base, mpool_size);
+
+	/* Load the main binary and get a list of dependencies, if any. */
+	ta_elf_load_main(&arg->uuid, &arg->is_32bit, &arg->entry_func,
+			 &arg->stack_ptr, &arg->flags);
+
+	/*
+	 * Load binaries, ta_elf_load() may add external libraries to the
+	 * list, so the loop will end when all the dependencies are
+	 * satisfied.
+	 */
+	TAILQ_FOREACH(elf, &main_elf_queue, link)
+		ta_elf_load_dependency(elf, arg->is_32bit);
+
+	TAILQ_FOREACH(elf, &main_elf_queue, link) {
+		ta_elf_relocate(elf);
+		ta_elf_finalize_mappings(elf);
+	}
+
+	TAILQ_FOREACH(elf, &main_elf_queue, link)
+		DMSG("ELF (%pUl) at %#"PRIxVA,
+		     (void *)&elf->uuid, elf->load_addr);
+
+	res = sys_unmap(mpool_base, mpool_size);
+	if (res) {
+		EMSG("sys_unmap(%p, %zu): result %"PRIx32,
+		     (void *)mpool_base, mpool_size, res);
+		panic();
+	}
+	sys_return_cleanup();
+}

commit 39c4965be0134e2291be6410f4498ae15aa8e503

ldelf: fix off-by-one bounds check on symbol name offset
sym_compare() rejects st_name only when it is strictly greater than
elf->dynstr_size, so a crafted TA can set st_name == dynstr_size. This
makes elf->dynstr + st_name point one byte past the end of the
validated .dynstr range, and the following strcmp() then reads
forward until it happens to find a NUL byte, an unbounded out-of-bounds
read past the string table.

The equivalent checks in e32_get_sym_name() and e64_get_sym_name() in
this file already use the correct ">=" comparison.

Fixes: 7509ff7ce5e5 ("Add user mode ELF loader")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Assisted-by: Claude:sonnet-5

ldelf/ta_elf_rel.c
@@ -52,7 +52,7 @@ static bool sym_compare(struct ta_elf *elf, unsigned int st_bind,
 
 	if (!st_name)
 		return false;
-	if (st_name > elf->dynstr_size)
+	if (st_name >= elf->dynstr_size)
 		err(TEE_ERROR_BAD_FORMAT, "Symbol name out of range");
 	if (strcmp(name, elf->dynstr + st_name))
 		return false;