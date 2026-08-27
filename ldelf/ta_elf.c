OP-TEE/optee_os

commit 9d224046e71083e0fe3e8c26c22f729399c2c950

ldelf, libutee: add minimal Thread Local Storage support
Preparing for C++ support in TAs.

Adds enough runtime Thread Local Storage (TLS) support for the GNU C++
compilers (arm-linux-gnueabihf-g++, aarch64-linux-gnu-g++) to work with
OP-TEE. That is:

 - A Thread Control Block,
 - The __tls_get_addr() and dl_iterate_phdr() functions.

Note that __tls_get_addr() is an ABI helper so it has no prototype in a
user-accessible header file. dl_iterate_phdr() however is defined in
<link.h> and may be used in a TA. The file lib/libutee/include/link.h is
borrowed from Android's Bionic [1] with minor changes (added the
required #include statement and named the function parameters). A
similar <link.h> header is provided by other C libraries such as GNU
libc, musl and FreeBSD/NetBSD/OpenBSD.

Link: [1] https://android.googlesource.com/platform/bionic/+/master/libc/include/link.h
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf.c
@@ -9,6 +9,7 @@
 #include <elf64.h>
 #include <elf_common.h>
 #include <ldelf.h>
+#include <link.h>
 #include <pta_system.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +24,21 @@
 #include "sys.h"
 #include "ta_elf.h"
 #include "unwind.h"
+
+/*
+ * Layout of a 32-bit struct dl_phdr_info for a 64-bit ldelf to access a 32-bit
+ * TA
+ */
+struct dl_phdr_info32 {
+	uint32_t dlpi_addr;
+	uint32_t dlpi_name;
+	uint32_t dlpi_phdr;
+	uint16_t dlpi_phnum;
+	uint64_t dlpi_adds;
+	uint64_t dlpi_subs;
+	uint32_t dlpi_tls_modid;
+	uint32_t dlpi_tls_data;
+};
 
 static vaddr_t ta_stack;
 static vaddr_t ta_stack_size;
@@ -271,6 +287,64 @@ static void save_hashtab(struct ta_elf *elf)
 	check_hashtab(elf, elf->hashtab, hashtab[0], hashtab[1]);
 }
 
+static void save_soname_from_segment(struct ta_elf *elf, unsigned int type,
+				     vaddr_t addr, size_t memsz)
+{
+	size_t dyn_entsize = 0;
+	size_t num_dyns = 0;
+	size_t n = 0;
+	unsigned int tag = 0;
+	size_t val = 0;
+	char *str_tab = NULL;
+
+	if (type != PT_DYNAMIC)
+		return;
+
+	if (elf->is_32bit)
+		dyn_entsize = sizeof(Elf32_Dyn);
+	else
+		dyn_entsize = sizeof(Elf64_Dyn);
+
+	assert(!(memsz % dyn_entsize));
+	num_dyns = memsz / dyn_entsize;
+
+	for (n = 0; n < num_dyns; n++) {
+		read_dyn(elf, addr, n, &tag, &val);
+		if (tag == DT_STRTAB) {
+			str_tab = (char *)(val + elf->load_addr);
+			break;
+		}
+	}
+	for (n = 0; n < num_dyns; n++) {
+		read_dyn(elf, addr, n, &tag, &val);
+		if (tag == DT_SONAME) {
+			elf->soname = str_tab + val;
+			break;
+		}
+	}
+}
+
+static void save_soname(struct ta_elf *elf)
+{
+	size_t n = 0;
+
+	if (elf->is_32bit) {
+		Elf32_Phdr *phdr = elf->phdr;
+
+		for (n = 0; n < elf->e_phnum; n++)
+			save_soname_from_segment(elf, phdr[n].p_type,
+						 phdr[n].p_vaddr,
+						 phdr[n].p_memsz);
+	} else {
+		Elf64_Phdr *phdr = elf->phdr;
+
+		for (n = 0; n < elf->e_phnum; n++)
+			save_soname_from_segment(elf, phdr[n].p_type,
+						 phdr[n].p_vaddr,
+						 phdr[n].p_memsz);
+	}
+}
+
 static void e32_save_symtab(struct ta_elf *elf, size_t tab_idx)
 {
 	Elf32_Shdr *shdr = elf->shdr;
@@ -349,6 +423,7 @@ static void save_symtab(struct ta_elf *elf)
 	}
 
 	save_hashtab(elf);
+	save_soname(elf);
 }
 
 static void init_elf(struct ta_elf *elf)
@@ -1073,6 +1148,9 @@ void ta_elf_finalize_load_main(uint64_t *entry)
 	res = ta_elf_set_init_fini_info(elf->is_32bit);
 	if (res)
 		err(res, "ta_elf_set_init_fini_info");
+	res = ta_elf_set_elf_phdr_info(elf->is_32bit);
+	if (res)
+		err(res, "ta_elf_set_elf_phdr_info");
 
 	if (elf->is_legacy)
 		*entry = elf->head->depr_entry;
@@ -1329,6 +1407,7 @@ void ta_elf_stack_trace_a64(uint64_t fp, uint64_t sp, uint64_t pc)
 
 TEE_Result ta_elf_add_library(const TEE_UUID *uuid)
 {
+	TEE_Result res = TEE_ERROR_GENERIC;
 	struct ta_elf *ta = TAILQ_FIRST(&main_elf_queue);
 	struct ta_elf *lib = ta_elf_find_elf(uuid);
 	struct ta_elf *elf = NULL;
@@ -1352,7 +1431,11 @@ TEE_Result ta_elf_add_library(const TEE_UUID *uuid)
 		DMSG("ELF (%pUl) at %#"PRIxVA,
 		     (void *)&elf->uuid, elf->load_addr);
 
-	return ta_elf_set_init_fini_info(ta->is_32bit);
+	res = ta_elf_set_init_fini_info(ta->is_32bit);
+	if (res)
+		return res;
+
+	return ta_elf_set_elf_phdr_info(ta->is_32bit);
 }
 
 /* Get address/size of .init_array and .fini_array from the dynamic segment */
@@ -1563,4 +1646,121 @@ TEE_Result ta_elf_set_init_fini_info(bool is_32bit)
 err:
 	free(info);
 	return res;
+}
+
+static TEE_Result realloc_elf_phdr_info(vaddr_t va, size_t cnt, bool is_32bit)
+{
+	struct __elf_phdr_info32 *info32 = (struct __elf_phdr_info32 *)va;
+	struct __elf_phdr_info *info = (struct __elf_phdr_info *)va;
+	struct dl_phdr_info32 *dlpi32 = NULL;
+	struct dl_phdr_info *dlpi = NULL;
+	size_t prev_cnt = 0;
+	void *ptr = NULL;
+
+	if (is_32bit) {
+		ptr = (void *)(vaddr_t)info32->dlpi;
+		ptr = realloc(ptr, cnt * sizeof(*dlpi32));
+		if (!ptr)
+			return TEE_ERROR_OUT_OF_MEMORY;
+		dlpi32 = ptr;
+		prev_cnt = info32->count;
+		if (cnt > prev_cnt)
+			memset(dlpi32 + prev_cnt, 0,
+			       (cnt - prev_cnt) * sizeof(*dlpi32));
+		info32->dlpi = (uint32_t)(vaddr_t)dlpi32;
+		info32->count = cnt;
+	} else {
+		ptr = realloc(info->dlpi, cnt * sizeof(*dlpi));
+		if (!ptr)
+			return TEE_ERROR_OUT_OF_MEMORY;
+		dlpi = ptr;
+		prev_cnt = info->count;
+		if (cnt > prev_cnt)
+			memset(dlpi + prev_cnt, 0,
+			       (cnt - prev_cnt) * sizeof(*dlpi));
+		info->dlpi = dlpi;
+		info->count = cnt;
+	}
+
+	return TEE_SUCCESS;
+}
+
+static void fill_elf_phdr_info(vaddr_t va, size_t idx, struct ta_elf *elf,
+			       bool is_32bit)
+{
+	struct __elf_phdr_info32 *info32 = (struct __elf_phdr_info32 *)va;
+	struct __elf_phdr_info *info = (struct __elf_phdr_info *)va;
+	struct dl_phdr_info32 *dlpi32 = NULL;
+	struct dl_phdr_info *dlpi = NULL;
+
+	if (is_32bit) {
+		assert(idx < info32->count);
+		dlpi32 = (struct dl_phdr_info32 *)(vaddr_t)info32->dlpi + idx;
+
+		dlpi32->dlpi_addr = elf->load_addr;
+		if (elf->soname)
+			dlpi32->dlpi_name = (vaddr_t)elf->soname;
+		else
+			dlpi32->dlpi_name = (vaddr_t)&info32->zero;
+		dlpi32->dlpi_phdr = (vaddr_t)elf->phdr;
+		dlpi32->dlpi_phnum = elf->e_phnum;
+		dlpi32->dlpi_adds = 1; /* No unloading on dlclose() currently */
+		dlpi32->dlpi_subs = 0; /* No unloading on dlclose() currently */
+		dlpi32->dlpi_tls_modid = elf->tls_mod_id;
+		dlpi32->dlpi_tls_data = elf->tls_start;
+	} else {
+		assert(idx < info->count);
+		dlpi = info->dlpi + idx;
+
+		dlpi->dlpi_addr = elf->load_addr;
+		if (elf->soname)
+			dlpi->dlpi_name = elf->soname;
+		else
+			dlpi->dlpi_name = &info32->zero;
+		dlpi->dlpi_phdr = elf->phdr;
+		dlpi->dlpi_phnum = elf->e_phnum;
+		dlpi->dlpi_adds = 1; /* No unloading on dlclose() currently */
+		dlpi->dlpi_subs = 0; /* No unloading on dlclose() currently */
+		dlpi->dlpi_tls_modid = elf->tls_mod_id;
+		dlpi->dlpi_tls_data = (void *)elf->tls_start;
+	}
+}
+
+/* Set or update __elf_hdr_info in the TA with information from the ELF queue */
+TEE_Result ta_elf_set_elf_phdr_info(bool is_32bit)
+{
+	struct __elf_phdr_info *info = NULL;
+	TEE_Result res = TEE_SUCCESS;
+	struct ta_elf *elf = NULL;
+	vaddr_t info_va = 0;
+	size_t cnt = 0;
+
+	res = ta_elf_resolve_sym("__elf_phdr_info", &info_va, NULL, NULL);
+	if (res) {
+		if (res == TEE_ERROR_ITEM_NOT_FOUND) {
+			/* Older TA */
+			return TEE_SUCCESS;
+		}
+		return res;
+	}
+	assert(info_va);
+
+	info = (struct __elf_phdr_info *)info_va;
+	if (info->reserved)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	TAILQ_FOREACH(elf, &main_elf_queue, link)
+		cnt++;
+
+	res = realloc_elf_phdr_info(info_va, cnt, is_32bit);
+	if (res)
+		return res;
+
+	cnt = 0;
+	TAILQ_FOREACH(elf, &main_elf_queue, link) {
+		fill_elf_phdr_info(info_va, cnt, elf, is_32bit);
+		cnt++;
+	}
+
+	return TEE_SUCCESS;
 }

ldelf/ta_elf.h
@@ -59,6 +59,9 @@ struct ta_elf {
 	/* DT_HASH hash table for faster resolution of external symbols */
 	void *hashtab;
 
+	/* DT_SONAME */
+	char *soname;
+
 	struct segment_head segs;
 
 	vaddr_t exidx_start;
@@ -117,5 +120,6 @@ TEE_Result ta_elf_resolve_sym(const char *name, vaddr_t *val,
 			      struct ta_elf **found_elf, struct ta_elf *elf);
 TEE_Result ta_elf_add_library(const TEE_UUID *uuid);
 TEE_Result ta_elf_set_init_fini_info(bool is_32bit);
+TEE_Result ta_elf_set_elf_phdr_info(bool is_32bit);
 
 #endif /*TA_ELF_H*/

ldelf/ta_elf_rel.c
@@ -421,7 +421,7 @@ static void e64_process_tls_rela(const Elf64_Sym *sym_tab, size_t num_syms,
 	vaddr_t symval = 0;
 
 	e64_get_sym_name(sym_tab, num_syms, str_tab, str_tab_size, rela, &name);
-	ta_elf_resolve_sym(name, &symval, &mod, NULL, NULL);
+	resolve_sym(name, &symval, &mod);
 	*where = symval + mod->tls_tcb_offs + rela->r_addend;
 }
 

lib/libdl/dlfcn.c
@@ -65,6 +65,7 @@ void *dlopen(const char *filename, int flags)
 		if (res)
 			goto err;
 
+		__utee_tcb_init();
 		__utee_call_elf_init_fn();
 	}
 

lib/libutee/arch/arm/sub.mk
@@ -4,6 +4,7 @@ srcs-$(CFG_ARM32_$(sm)) += utee_syscalls_a32.S
 srcs-$(CFG_ARM64_$(sm)) += utee_syscalls_a64.S
 
 ifneq ($(sm),ldelf)
+srcs-y += tcb.c
 srcs-y += user_ta_entry.c
 subdirs-y += gprof
 endif #$(sm-$(sm)-is-ld)

lib/libutee/arch/arm/tcb.c
@@ -1,0 +1,207 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+/*
+ * Support for Thread-Local Storage (TLS) ABIs for ARMv7/Aarch32 and Aarch64.
+ *
+ * TAs are currently single-threaded, so the only benefit of implementing these
+ * ABIs is to support toolchains that need them even when the target program is
+ * single-threaded. Such as, the g++ compiler from the GCC toolchain targeting a
+ * "Posix thread" Linux runtime, which OP-TEE has been using for quite some time
+ * (arm-linux-gnueabihf-* and aarch64-linux-gnu-*). This allows building C++ TAs
+ * without having to build a specific toolchain with --disable-threads.
+ *
+ * This implementation is based on [1].
+ *
+ *  - "TLS data structures variant 1" (section 3): the AArch64 compiler uses the
+ *    TPIDR_EL0 to access TLS data directly. This assumes a specific layout for
+ *    the TCB, and (for shared objects) the use of R_AARCH64_TLS_TPREL
+ *    relocations.
+ *  - The "General Dynamic access model" (section 4.1): the ARMv7/Aarch32
+ *    compiler inserts calls to the __tls_get_addr() function which has to be
+ *    implemented by the runtime library. The function takes a module ID and an
+ *    offset parameter, which are provided thanks to R_ARM_TLS_DTPMOD32 and
+ *    R_ARM_TLS_DTPOFF32 relocations.
+ *
+ * In addition, dl_iterate_phdr() is implemented here, because it is used by the
+ * g++ Aarch64 exception handling and it does use the TCB to provide TLS
+ * information to the caller.
+ *
+ * [1] "ELF Handling For Thread-Local Storage"
+ *     https://www.akkadia.org/drepper/tls.pdf
+ */
+
+#include <arm64_user_sysreg.h>
+#include <assert.h>
+#include <link.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/queue.h>
+#include "user_ta_header.h"
+
+/* DTV - Dynamic Thread Vector
+ *
+ * Maintains an array of pointers to TLS data for each module in the TCB. Each
+ * module that has a TLS segment has an entry (and consequently, some space in
+ * the tcb_head::tls buffer). The index is the "module ID".
+ * dtv[0].size is the number of elements in the vector
+ * dtv[1].tls points to TLS for the main executable (may be NULL)
+ * tls[2 .. (size-1)] are for shared libraries
+ */
+union dtv {
+	unsigned long size;
+	uint8_t *tls;
+};
+
+#define DTV_SIZE(size) (sizeof(union dtv) + (size))
+
+/* Thread Control Block */
+struct tcb_head {
+	/* Two words are reserved as per the "TLS variant 1" ABI */
+	union dtv *dtv;
+	unsigned long reserved;
+	/*
+	 * The rest of the structure contains the TLS blocks for each ELF module
+	 * having a PT_TLS segment. Each block is a copy of the .tdata section
+	 * plus some zero-initialized space for .tbss.
+	 */
+	uint8_t tls[];
+};
+
+/*
+ * Since TAs are single threaded, only one TCB is needed. This would need to
+ * change if multi-threading is introduced.
+ */
+static struct tcb_head *_tcb;
+static size_t _tls_size;
+
+#define TCB_SIZE(tls_size) (sizeof(*_tcb) + (tls_size))
+
+/*
+ * Initialize or update the TCB.
+ * Called on application initialization and when additional shared objects are
+ * loaded via dlopen().
+ */
+void __utee_tcb_init(void)
+{
+	struct dl_phdr_info *dlpi = NULL;
+	const Elf_Phdr *phdr = NULL;
+	size_t total_size = 0;
+	size_t size = 0;
+	size_t i = 0;
+	size_t j = 0;
+
+	/* Compute the size needed for all the TLS blocks */
+	for (i = 0; i < __elf_phdr_info.count; i++) {
+		dlpi = __elf_phdr_info.dlpi + i;
+		for (j = 0; j < dlpi->dlpi_phnum; j++) {
+			phdr = dlpi->dlpi_phdr + j;
+			if (phdr->p_type == PT_TLS) {
+				total_size += phdr->p_memsz;
+				break;
+			}
+		}
+	}
+
+	/* ELF modules currently cannot be unmapped */
+	assert(total_size >= _tls_size);
+
+	if (total_size == _tls_size)
+		return;
+
+	/* (Re-)allocate the TCB */
+	_tcb = realloc(_tcb, TCB_SIZE(total_size));
+	if (!_tcb) {
+		EMSG("TCB allocation failed (%zu bytes)", TCB_SIZE(total_size));
+		abort();
+	}
+
+	/* (Re-)allocate the DTV. + 1 since dtv[0] holds the size */
+	size = DTV_SIZE((__elf_phdr_info.count + 1) * sizeof(union dtv));
+	_tcb->dtv = realloc(_tcb->dtv, size);
+	if (!_tcb->dtv) {
+		EMSG("DTV allocation failed (%zu bytes)", size);
+		abort();
+	}
+
+	/* Copy TLS data to the TCB */
+	size = 0;
+	for (i = 0; i < __elf_phdr_info.count; i++) {
+		dlpi = __elf_phdr_info.dlpi + i;
+		for (j = 0; j < dlpi->dlpi_phnum; j++) {
+			phdr = dlpi->dlpi_phdr + j;
+			if (phdr->p_type != PT_TLS)
+				continue;
+			if (size + phdr->p_memsz <= _tls_size) {
+				/* Already copied */
+				break;
+			}
+			_tcb->dtv[i + 1].tls = _tcb->tls + size;
+			/* Copy .tdata */
+			memcpy(_tcb->tls + size,
+			       (void *)(dlpi->dlpi_addr + phdr->p_vaddr),
+			       phdr->p_filesz);
+			/* Initialize .tbss */
+			memset(_tcb->tls + size + phdr->p_filesz, 0,
+			       phdr->p_memsz - phdr->p_filesz);
+			size += phdr->p_memsz;
+		}
+	}
+	_tcb->dtv[0].size = i;
+
+	_tls_size = total_size;
+#ifdef ARM64
+	/*
+	 * Aarch64 ABI requirement: the thread pointer shall point to the
+	 * thread's TCB. ARMv7 and Aarch32 access the TCB via _tls_get_addr().
+	 */
+	write_tpidr_el0((vaddr_t)_tcb);
+#endif
+}
+
+struct tls_index {
+	unsigned long module;
+	unsigned long offset;
+};
+
+void *__tls_get_addr(struct tls_index *ti);
+
+void *__tls_get_addr(struct tls_index *ti)
+{
+	return _tcb->dtv[ti->module].tls + ti->offset;
+}
+
+int dl_iterate_phdr(int (*callback)(struct dl_phdr_info *, size_t, void *),
+		    void *data)
+{
+	struct dl_phdr_info *dlpi = NULL;
+	size_t id = 0;
+	size_t i = 0;
+	int st = 0;
+
+	/*
+	 * dlpi_tls_data is thread-specific so if we were to support
+	 * multi-threading, we would need one copy of struct dl_phdr_info per
+	 * thread. Could be a pre-allocated area, or could be allocated on the
+	 * heap. Doing the latter here so that it would at least work if/when we
+	 * add thread support. Further optimization can always come later.
+	 */
+	dlpi = calloc(1, sizeof(*dlpi));
+	if (!dlpi) {
+		EMSG("dl_phdr_info allocation failed");
+		abort();
+	}
+
+	for (i = 0; i < __elf_phdr_info.count; i++) {
+		memcpy(dlpi, __elf_phdr_info.dlpi + i, sizeof(*dlpi));
+		dlpi->dlpi_tls_data = NULL;
+		id = dlpi->dlpi_tls_modid;
+		if (id)
+			dlpi->dlpi_tls_data = _tcb->dtv[id].tls;
+		st = callback(dlpi, sizeof(*dlpi), data);
+	}
+
+	free(dlpi);
+	return st;
+}

lib/libutee/arch/arm/user_ta_entry.c
@@ -34,6 +34,7 @@ extern struct ta_head ta_head;
 uint32_t ta_param_types;
 TEE_Param ta_params[TEE_NUM_PARAMS];
 struct __init_fini_info __init_fini_info;
+struct __elf_phdr_info __elf_phdr_info;
 
 void __utee_call_elf_init_fn(void)
 {
@@ -76,6 +77,7 @@ static TEE_Result init_instance(void)
 	__utee_gprof_init();
 	malloc_add_pool(ta_heap, ta_heap_size);
 	_TEE_MathAPI_Init();
+	__utee_tcb_init();
 	__utee_call_elf_init_fn();
 	return TA_CreateEntryPoint();
 }

lib/libutee/include/link.h
@@ -1,0 +1,27 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ */
+
+#ifndef _LINK_H_
+#define	_LINK_H_
+
+#include <elf.h>
+#include <stddef.h>
+
+struct dl_phdr_info {
+	Elf_Addr dlpi_addr;			/* module relocation base */
+	const char *dlpi_name;			/* module name */
+	const Elf_Phdr *dlpi_phdr;		/* pointer to module's phdr */
+	Elf_Half dlpi_phnum;			/* number of entries in phdr */
+	unsigned long long dlpi_adds;		/* total # of loads */
+	unsigned long long dlpi_subs;		/* total # of unloads */
+	size_t dlpi_tls_modid;
+	void *dlpi_tls_data;
+};
+
+int dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info, size_t size,
+				    void *data),
+		    void *data);
+
+#endif /* _LINK_H_ */

commit 341401cb658fe017a845230c314fc352b112bc4b

ldelf: bounds-check DT_SONAME offset in save_soname_from_segment()
save_soname_from_segment() reads DT_STRTAB and uses the DT_SONAME
offset into it without ever reading DT_STRSZ or checking the offset
against the string table size, unlike the equivalent DT_NEEDED
handling in add_deps_from_segment(). A crafted TA can supply an
out-of-bounds DT_SONAME offset, making elf->soname point outside the
TA's mapped memory.

Add the same PT_DYNAMIC range check, .dynstr/STRTAB range check, and
DT_SONAME bounds check already used in add_deps_from_segment().

Fixes: 9d224046e710 ("ldelf, libutee: add minimal Thread Local Storage support")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Assisted-by: Claude:sonnet-5

ldelf/ta_elf.c
@@ -352,9 +352,12 @@ static void save_soname_from_segment(struct ta_elf *elf, unsigned int type,
 	unsigned int tag = 0;
 	size_t val = 0;
 	char *str_tab = NULL;
+	size_t str_tab_sz = 0;
 
 	if (type != PT_DYNAMIC)
 		return;
+
+	check_phdr_in_range(elf, type, addr, memsz);
 
 	if (elf->is_32bit)
 		dyn_entsize = sizeof(Elf32_Dyn);
@@ -364,16 +367,21 @@ static void save_soname_from_segment(struct ta_elf *elf, unsigned int type,
 	assert(!(memsz % dyn_entsize));
 	num_dyns = memsz / dyn_entsize;
 
-	for (n = 0; n < num_dyns; n++) {
+	for (n = 0; n < num_dyns && !(str_tab && str_tab_sz); n++) {
 		read_dyn(elf, addr, n, &tag, &val);
-		if (tag == DT_STRTAB) {
+		if (tag == DT_STRTAB)
 			str_tab = (char *)(val + elf->load_addr);
-			break;
-		}
+		else if (tag == DT_STRSZ)
+			str_tab_sz = val;
 	}
+	check_range(elf, ".dynstr/STRTAB", str_tab, str_tab_sz);
+
 	for (n = 0; n < num_dyns; n++) {
 		read_dyn(elf, addr, n, &tag, &val);
 		if (tag == DT_SONAME) {
+			if (val >= str_tab_sz)
+				err(TEE_ERROR_BAD_FORMAT,
+				    "Offset into .dynstr/STRTAB out of range");
 			elf->soname = str_tab + val;
 			break;
 		}