OP-TEE/optee_os

commit d45fc1401c575c4273f684571b4150d6ff89982f

core: ffa: add missing field in memory access descriptor
FF-A v1.2 introduced a 16 byte implementation-defined field in the
endpoint memory access descriptor. Update all handling of struct
ffa_mem_access to for correct access regardless of FF-A version.

With this patch, OP-TEE will use the updated memory access descriptor,
but ignore the impdef field.

Suggested-by: Olivier Deprez <olivier.deprez@arm.com>
Fixes: bef959c837fe ("core: arm: ffa: switch to FF-A version 1.2")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/kernel/spmc_sp_handler.c
@@ -89,10 +89,9 @@ static struct sp_mem_receiver *find_sp_mem_receiver(struct sp_session *s,
 	return receiver;
 }
 
-static int add_mem_region_to_sp(struct ffa_mem_access *mem_acc,
+static int add_mem_region_to_sp(struct ffa_mem_access_perm *access_perm,
 				struct sp_mem *smem)
 {
-	struct ffa_mem_access_perm *access_perm = &mem_acc->access_perm;
 	struct sp_session *s = NULL;
 	struct sp_mem_receiver *receiver = NULL;
 	uint8_t perm = READ_ONCE(access_perm->perm);
@@ -288,7 +287,7 @@ int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
 	int res = FFA_INVALID_PARAMETERS;
 	unsigned int num_mem_accs = 0;
 	unsigned int i = 0;
-	struct ffa_mem_access *mem_acc = NULL;
+	struct ffa_mem_access_common *mem_acc = NULL;
 	size_t needed_size = 0;
 	size_t addr_range_offs = 0;
 	struct ffa_mem_region *mem_reg = NULL;
@@ -299,6 +298,8 @@ int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
 	struct ffa_address_range *addr_range = NULL;
 	size_t total_page_count = 0;
 	size_t page_count_sum = 0;
+	vaddr_t mem_acc_base = 0;
+	size_t mem_acc_size = 0;
 
 	if (blen != flen) {
 		DMSG("Fragmented memory share is not supported for SPs");
@@ -315,8 +316,9 @@ int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
 		goto cleanup;
 	}
 
+	mem_acc_size = mem_trans->mem_access_size;
 	num_mem_accs = mem_trans->mem_access_count;
-	mem_acc = (void *)((vaddr_t)rxtx->rx + mem_trans->mem_access_offs);
+	mem_acc_base = (vaddr_t)rxtx->rx + mem_trans->mem_access_offs;
 
 	if (!num_mem_accs) {
 		res = FFA_INVALID_PARAMETERS;
@@ -329,18 +331,20 @@ int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
 	smem->flags = mem_trans->flags;
 	smem->tag = mem_trans->tag;
 
-	if (MUL_OVERFLOW(num_mem_accs, sizeof(*mem_acc), &needed_size) ||
+	if (MUL_OVERFLOW(num_mem_accs, mem_acc_size, &needed_size) ||
 	    ADD_OVERFLOW(needed_size, mem_trans->mem_access_offs,
 			 &needed_size) || needed_size > blen) {
 		res = FFA_INVALID_PARAMETERS;
 		goto cleanup;
 	}
 
-	for (i = 0; i < num_mem_accs; i++)
-		highest_permission |= READ_ONCE(mem_acc[i].access_perm.perm);
+	for (i = 0; i < num_mem_accs; i++) {
+		mem_acc = (void *)(mem_acc_base + i * mem_acc_size);
+		highest_permission |= READ_ONCE(mem_acc->access_perm.perm);
+	}
 
 	/* Check if the memory region array fits into the buffer */
-	addr_range_offs = READ_ONCE(mem_acc[0].region_offs);
+	addr_range_offs = READ_ONCE(mem_acc->region_offs);
 
 	if (ADD_OVERFLOW(addr_range_offs, sizeof(*mem_reg), &needed_size) ||
 	    needed_size > blen) {
@@ -408,7 +412,8 @@ int spmc_sp_add_share(struct ffa_mem_transaction_x *mem_trans,
 
 	/* Add the memory address to the SP */
 	for (i = 0; i < num_mem_accs; i++) {
-		res = add_mem_region_to_sp(&mem_acc[i], smem);
+		mem_acc = (void *)(mem_acc_base + i * mem_acc_size);
+		res = add_mem_region_to_sp(&mem_acc->access_perm, smem);
 		if (res)
 			goto cleanup;
 	}
@@ -462,7 +467,7 @@ check_retrieve_request(struct sp_mem_receiver *receiver, uint32_t ffa_vers,
 		       struct ffa_mem_transaction_x *mem_trans,
 		       void *rx, struct sp_mem *smem, int64_t tx_len)
 {
-	struct ffa_mem_access *retr_access = NULL;
+	struct ffa_mem_access_common *retr_access = NULL;
 	uint8_t share_perm = receiver->perm.perm;
 	uint32_t retr_perm = 0;
 	uint32_t retr_flags = mem_trans->flags;
@@ -511,8 +516,7 @@ check_retrieve_request(struct sp_mem_receiver *receiver, uint32_t ffa_vers,
 		tx_len -= sizeof(struct ffa_mem_transaction_1_0);
 	else
 		tx_len -= sizeof(struct ffa_mem_transaction_1_1);
-	tx_len -= sizeof(struct ffa_mem_access) +
-		  sizeof(struct ffa_mem_region);
+	tx_len -= mem_trans->mem_access_size + sizeof(struct ffa_mem_region);
 
 	if (tx_len < 0)
 		return FFA_NO_MEMORY;
@@ -534,7 +538,13 @@ static void create_retrieve_response(uint32_t ffa_vers, void *dst_buffer,
 	struct ffa_mem_region *dst_region =  NULL;
 	struct ffa_address_range *addr_dst = NULL;
 	struct sp_mem_map_region *reg = NULL;
-	struct ffa_mem_access *mem_acc = NULL;
+	struct ffa_mem_access_common *mem_acc = NULL;
+	size_t mem_acc_size = 0;
+
+	if (ffa_vers <= FFA_VERSION_1_1)
+		mem_acc_size = sizeof(struct ffa_mem_access_1_0);
+	else
+		mem_acc_size = sizeof(struct ffa_mem_access_1_2);
 
 	/*
 	 * we respond with a ffa_mem_retrieve_resp which defines the
@@ -549,7 +559,7 @@ static void create_retrieve_response(uint32_t ffa_vers, void *dst_buffer,
 		memset(d_ds, 0, sizeof(*d_ds));
 
 		off = sizeof(*d_ds);
-		mem_acc = d_ds->mem_access_array;
+		mem_acc = (void *)d_ds->mem_access_array;
 
 		/* copy the mem_transaction_descr */
 		d_ds->sender_id = receiver->smem->sender_id;
@@ -569,17 +579,17 @@ static void create_retrieve_response(uint32_t ffa_vers, void *dst_buffer,
 		d_ds->mem_reg_attr = receiver->smem->mem_reg_attr;
 		d_ds->flags = FFA_MEMORY_TRANSACTION_TYPE_SHARE;
 		d_ds->tag = receiver->smem->tag;
-		d_ds->mem_access_size = sizeof(*mem_acc);
+		d_ds->mem_access_size = mem_acc_size;
 		d_ds->mem_access_count = 1;
 		d_ds->mem_access_offs = off;
 	}
 
-	off += sizeof(struct ffa_mem_access);
-	dst_region = (struct ffa_mem_region *)(mem_acc + 1);
+	off += mem_acc_size;
+	dst_region = (struct ffa_mem_region *)((vaddr_t)dst_buffer + off);
 
 	/* Copy the mem_accsess_descr */
-	mem_acc[0].region_offs = off;
-	memcpy(&mem_acc[0].access_perm, &receiver->perm,
+	mem_acc->region_offs = off;
+	memcpy(&mem_acc->access_perm, &receiver->perm,
 	       sizeof(struct ffa_mem_access_perm));
 
 	/* Copy the mem_region_descr */
@@ -612,7 +622,7 @@ static void ffa_mem_retrieve(struct thread_smc_1_2_regs *args,
 	uint32_t frag_len = args->a2;
 	int ret = FFA_OK;
 	size_t tx_len = 0;
-	struct ffa_mem_access *mem_acc = NULL;
+	struct ffa_mem_access_common *mem_acc = NULL;
 	struct ffa_mem_region *mem_region = NULL;
 	uint64_t va = 0;
 	struct sp_mem *smem = NULL;
@@ -652,8 +662,7 @@ static void ffa_mem_retrieve(struct thread_smc_1_2_regs *args,
 	receiver = sp_mem_get_receiver(caller_sp->endpoint_id, smem);
 
 	mem_acc = (void *)((vaddr_t)rxtx->rx + mem_trans.mem_access_offs);
-
-	address_offset = READ_ONCE(mem_acc[0].region_offs);
+	address_offset = READ_ONCE(mem_acc->region_offs);
 
 	if (ADD_OVERFLOW(address_offset, sizeof(struct ffa_mem_region),
 			 &needed_size) || needed_size > tx_len) {

core/arch/arm/include/ffa.h
@@ -278,11 +278,28 @@ struct ffa_mem_access_perm {
 	uint8_t flags;
 };
 
-/* Endpoint memory access descriptor */
-struct ffa_mem_access {
+/* Endpoint memory access descriptor up to version 1.1 */
+struct ffa_mem_access_1_0 {
 	struct ffa_mem_access_perm access_perm;
 	uint32_t region_offs;
 	uint64_t reserved;
+};
+
+/* Endpoint memory access descriptor from version 1.2 */
+struct ffa_mem_access_1_2 {
+	struct ffa_mem_access_perm access_perm;
+	uint32_t region_offs;
+	uint8_t impdef[16];
+	uint64_t reserved;
+};
+
+/*
+ * A common memory access descriptor where only the first two fields need
+ * to be accessed.
+ */
+struct ffa_mem_access_common {
+	struct ffa_mem_access_perm access_perm;
+	uint32_t region_offs;
 };
 
 /* Lend, donate or share memory transaction descriptor */
@@ -295,7 +312,7 @@ struct ffa_mem_transaction_1_0 {
 	uint64_t tag;
 	uint32_t reserved1;
 	uint32_t mem_access_count;
-	struct ffa_mem_access mem_access_array[];
+	struct ffa_mem_access_1_0 mem_access_array[];
 };
 
 struct ffa_mem_transaction_1_1 {

core/arch/arm/kernel/thread_spmc.c
@@ -994,7 +994,7 @@ int spmc_read_mem_transaction(uint32_t ffa_vers, void *buf, size_t blen,
 		trans->tag = READ_ONCE(descr->tag);
 
 		count = READ_ONCE(descr->mem_access_count);
-		size = sizeof(struct ffa_mem_access);
+		size = sizeof(descr->mem_access_array[0]);
 		offs = offsetof(struct ffa_mem_transaction_1_0,
 				mem_access_array);
 	}
@@ -1021,8 +1021,8 @@ static int get_acc_perms(vaddr_t mem_acc_base, unsigned int mem_access_size,
 			 unsigned int mem_access_count, uint8_t *acc_perms,
 			 unsigned int *region_offs)
 {
+	struct ffa_mem_access_common *mem_acc = NULL;
 	struct ffa_mem_access_perm *descr = NULL;
-	struct ffa_mem_access *mem_acc = NULL;
 	unsigned int n = 0;
 
 	for (n = 0; n < mem_access_count; n++) {
@@ -1145,8 +1145,8 @@ static int add_mem_op_frag(struct mem_frag_state *s, void *buf, size_t flen)
 
 static bool is_sp_op(struct ffa_mem_transaction_x *mem_trans, void *buf)
 {
+	struct ffa_mem_access_common *mem_acc = NULL;
 	struct ffa_mem_access_perm *perm = NULL;
-	struct ffa_mem_access *mem_acc = NULL;
 
 	if (!IS_ENABLED(CFG_SECURE_PARTITION))
 		return false;
@@ -2607,20 +2607,26 @@ static uint32_t get_ffa_version(uint32_t my_version)
 static void *spmc_retrieve_req(struct ffa_mem_transaction_x *trans)
 {
 	uint64_t cookie __maybe_unused = trans->global_handle;
-	struct ffa_mem_access *acc_descr_array = NULL;
+	struct ffa_mem_access_common *mem_acc = NULL;
 	struct ffa_mem_access_perm *perm_descr = NULL;
 	struct thread_smc_args args = {
 		.a0 = FFA_MEM_RETRIEVE_REQ_32,
 		.a3 =	0,	/* Address, Using TX -> MBZ */
 		.a4 =   0,	/* Using TX -> MBZ */
 	};
+	size_t mem_acc_size = 0;
 	size_t size = 0;
 	int rc = 0;
+
+	if (my_rxtx.ffa_vers <= FFA_VERSION_1_1)
+		mem_acc_size = sizeof(struct ffa_mem_access_1_0);
+	else
+		mem_acc_size = sizeof(struct ffa_mem_access_1_2);
 
 	if (my_rxtx.ffa_vers == FFA_VERSION_1_0) {
 		struct ffa_mem_transaction_1_0 *trans_descr = my_rxtx.tx;
 
-		size = sizeof(*trans_descr) + 1 * sizeof(struct ffa_mem_access);
+		size = sizeof(*trans_descr) + 1 * mem_acc_size;
 		memset(trans_descr, 0, size);
 		trans_descr->sender_id = trans->sender_id;
 		trans_descr->mem_reg_attr = trans->mem_reg_attr;
@@ -2628,11 +2634,11 @@ static void *spmc_retrieve_req(struct ffa_mem_transaction_x *trans)
 		trans_descr->tag = trans->tag;
 		trans_descr->flags = trans->flags;
 		trans_descr->mem_access_count = 1;
-		acc_descr_array = trans_descr->mem_access_array;
+		mem_acc = (void *)trans_descr->mem_access_array;
 	} else {
 		struct ffa_mem_transaction_1_1 *trans_descr = my_rxtx.tx;
 
-		size = sizeof(*trans_descr) + 1 * sizeof(struct ffa_mem_access);
+		size = sizeof(*trans_descr) + 1 * mem_acc_size;
 		memset(trans_descr, 0, size);
 		trans_descr->sender_id = trans->sender_id;
 		trans_descr->mem_reg_attr = trans->mem_reg_attr;
@@ -2641,13 +2647,11 @@ static void *spmc_retrieve_req(struct ffa_mem_transaction_x *trans)
 		trans_descr->flags = trans->flags;
 		trans_descr->mem_access_count = 1;
 		trans_descr->mem_access_offs = sizeof(*trans_descr);
-		trans_descr->mem_access_size = sizeof(struct ffa_mem_access);
-		acc_descr_array = (void *)((vaddr_t)my_rxtx.tx +
-					   sizeof(*trans_descr));
+		trans_descr->mem_access_size = mem_acc_size;
+		mem_acc = (void *)((vaddr_t)my_rxtx.tx + sizeof(*trans_descr));
 	}
-	acc_descr_array->region_offs = 0;
-	acc_descr_array->reserved = 0;
-	perm_descr = &acc_descr_array->access_perm;
+	mem_acc->region_offs = 0;
+	perm_descr = &mem_acc->access_perm;
 	perm_descr->endpoint_id = optee_core_lsp.sp_id;
 	perm_descr->perm = FFA_MEM_ACC_RW;
 	perm_descr->flags = 0;
@@ -2718,7 +2722,7 @@ struct mobj_ffa *thread_spmc_populate_mobj_from_rx(uint64_t cookie,
 {
 	struct mobj_ffa *ret = NULL;
 	struct ffa_mem_transaction_x retrieve_desc = { .tag = use_case};
-	struct ffa_mem_access *descr_array = NULL;
+	struct ffa_mem_access_common *mem_acc = NULL;
 	struct ffa_mem_region *descr = NULL;
 	struct mobj_ffa *mf = NULL;
 	unsigned int num_pages = 0;
@@ -2748,8 +2752,8 @@ struct mobj_ffa *thread_spmc_populate_mobj_from_rx(uint64_t cookie,
 		return NULL;
 	}
 
-	descr_array = (void *)((vaddr_t)buf + retrieve_desc.mem_access_offs);
-	offs = READ_ONCE(descr_array->region_offs);
+	mem_acc = (void *)((vaddr_t)buf + retrieve_desc.mem_access_offs);
+	offs = READ_ONCE(mem_acc->region_offs);
 	descr = (struct ffa_mem_region *)((vaddr_t)buf + offs);
 
 	num_pages = READ_ONCE(descr->total_page_count);

commit e8655df782c8db99a79e644137fd363bbd5f631a

core: spmc: fix tx buffer overrun in the retrieve response
check_retrieve_request() decides whether the FFA_MEM_RETRIEVE_RESP fits
in the endpoint's tx buffer by subtracting the requester-supplied
mem_access_size from the buffer length. create_retrieve_response()
instead writes the response using the SPMC's own access descriptor size
(struct ffa_mem_access_1_0 for FF-A <= 1.1, struct ffa_mem_access_1_2
for 1.2), and its address range loop writes one struct ffa_address_range
per shared region without checking the space left.

An endpoint that retrieves with a mem_access_size smaller than the size
the SPMC writes makes the estimate over-count the free space, so the
check passes while the response is written past the end of the tx
buffer. A malicious S-EL0 SP can drive this by negotiating FF-A 1.2,
using a mem_access_size of sizeof(struct ffa_mem_access_common) and
controlling how many regions the memory was shared with, causing the
S-EL1 SPMC to write out of bounds into the SP's tx buffer.

Reserve the space using the same access descriptor size
create_retrieve_response() writes, so the estimate matches the write.

Fixes: d45fc1401c57 ("core: ffa: add missing field in memory access descriptor")
Signed-off-by: Guled Hasan <Law.Zoldyck@proton.me>
Reviewed-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

core/arch/arm/kernel/spmc_sp_handler.c
@@ -492,6 +492,7 @@ check_retrieve_request(struct sp_mem_receiver *receiver, uint32_t ffa_vers,
 	uint32_t retr_flags = mem_trans->flags;
 	uint64_t retr_tag = mem_trans->tag;
 	struct sp_mem_map_region *reg = NULL;
+	size_t mem_acc_size = 0;
 
 	/*
 	 * The request came from the endpoint. It should only have one
@@ -528,14 +529,17 @@ check_retrieve_request(struct sp_mem_receiver *receiver, uint32_t ffa_vers,
 		return TEE_ERROR_BAD_PARAMETERS;
 	}
 
-	/*
-	 * Check if there is enough space in the tx buffer to send the respons.
-	 */
+	/* Check that the response fits in the tx buffer. */
+	if (ffa_vers <= FFA_VERSION_1_1)
+		mem_acc_size = sizeof(struct ffa_mem_access_1_0);
+	else
+		mem_acc_size = sizeof(struct ffa_mem_access_1_2);
+
 	if (ffa_vers <= FFA_VERSION_1_0)
 		tx_len -= sizeof(struct ffa_mem_transaction_1_0);
 	else
 		tx_len -= sizeof(struct ffa_mem_transaction_1_1);
-	tx_len -= mem_trans->mem_access_size + sizeof(struct ffa_mem_region);
+	tx_len -= mem_acc_size + sizeof(struct ffa_mem_region);
 
 	if (tx_len < 0)
 		return FFA_NO_MEMORY;