ec45bced3b9a9e763973009c1d27b76798ab6a58
Mateusz Nowicki <mateusz.nowicki@posteo.net>
2026-09-01 12:47:16 +0200

    plat-stm32mp2: add BSEC3 OTP driver and enable BSEC PTA
    STM32MP25 embeds the BSEC3 OTP controller, not compatible with the
    STM32MP1 BSEC driver. Add a read-only driver implementing the
    stm32_bsec.h API used by the BSEC PTA and enable the PTA on stm32mp2.
    Main consumers are U-Boot and Linux (nvmem stm32-romem in TA mode).
    
    OTP programming, non-secure access to upper words (OTP 256 and above)
    and HDP/coresight state are not supported in this first version.
    
    Tested on STM32MP257F-DK: U-Boot reads part number, revision and board
    ID through the PTA.
    
    Signed-off-by: Mateusz Nowicki <mateusz.nowicki@posteo.net>

--- core/arch/arm/plat-stm32mp2/stm32_util.h
+++ core/arch/arm/plat-stm32mp2/stm32_util.h
@@ -20,4 +20,19 @@ bool stm32mp_allow_probe_shared_device(const void *fdt, int node);
 /* Print a message and reset the system */
 void __noreturn do_reset(const char *str);
 
+/*
+ * Structure and API function for BSEC driver to get some platform data.
+ *
+ * @base: BSEC interface registers physical base address
+ * @upper_start: Base ID for the BSEC upper words in the platform
+ * @max_id: Max value for BSEC word ID for the platform
+ */
+struct stm32_bsec3_static_cfg {
+	paddr_t base;
+	unsigned int upper_start;
+	unsigned int max_id;
+};
+
+void stm32mp_get_bsec3_static_cfg(struct stm32_bsec3_static_cfg *cfg);
+
 #endif /*__STM32_UTIL_H__*/
