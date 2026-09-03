OP-TEE/optee_os

commit 6915bbbb5b56e147ee652b98f6172f4dfbb325b9

GP 1.1: Check parameters annotated in the specification
Checks that all the function parameters which are annotated in the
specification [1] are compliant with regards to memory access and memory
location. In case the check fails the TA panics to help debugging. The
more precise and expensive checks can be disabled with
CFG_TA_STRICT_ANNOTATION_CHECKS=n.

TEE_Realloc(), TEE_MemMove(), TEE_MemCompare(), TEE_MemFill() are
skipped for performance reasons. The TA will instead die with a fatal
exception if buffers supplied to these functions do not follow the
annotation rules.

[1]: GlobalPlatform TEE Internal Core API Specification v1.1

Reviewed-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/tee_api_operations.c
@@ -389,8 +389,7 @@ void TEE_GetOperationInfo(TEE_OperationHandle operation,
 	if (operation == TEE_HANDLE_NULL)
 		TEE_Panic(0);
 
-	if (!operationInfo)
-		TEE_Panic(0);
+	__utee_check_out_annotation(operationInfo, sizeof(*operationInfo));
 
 	*operationInfo = operation->info;
 }
@@ -410,15 +409,7 @@ TEE_Result TEE_GetOperationInfoMultiple(TEE_OperationHandle operation,
 		goto out;
 	}
 
-	if (!operationInfoMultiple) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
-
-	if (!operationSize) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	__utee_check_outbuf_annotation(operationInfoMultiple, operationSize);
 
 	num_of_keys = (*operationSize-sizeof(TEE_OperationInfoMultiple))/
 			sizeof(TEE_OperationInfoKey);
@@ -801,12 +792,11 @@ TEE_Result TEE_DigestDoFinal(TEE_OperationHandle operation, const void *chunk,
 
 	if ((operation == TEE_HANDLE_NULL) ||
 	    (!chunk && chunkLen) ||
-	    !hash ||
-	    !hashLen ||
 	    (operation->info.operationClass != TEE_OPERATION_DIGEST)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_inout_annotation(hashLen, sizeof(*hashLen));
 
 	hl = *hashLen;
 	res = _utee_hash_final(operation->state, chunk, chunkLen, hash, &hl);
@@ -962,13 +952,11 @@ TEE_Result TEE_CipherUpdate(TEE_OperationHandle operation, const void *srcData,
 	size_t req_dlen;
 	uint64_t dl;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL ||
-	    (destData == NULL && *destLen != 0)) {
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_inout_annotation(destLen, sizeof(*destLen));
 
 	if (operation->info.operationClass != TEE_OPERATION_CIPHER) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -1042,19 +1030,18 @@ TEE_Result TEE_CipherDoFinal(TEE_OperationHandle operation,
 			     const void *srcData, uint32_t srcLen,
 			     void *destData, uint32_t *destLen)
 {
-	TEE_Result res;
+	TEE_Result res = TEE_SUCCESS;
 	uint8_t *dst = destData;
 	size_t acc_dlen = 0;
-	uint64_t tmp_dlen;
-	size_t req_dlen;
+	uint64_t tmp_dlen = 0;
+	size_t req_dlen = 0;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL ||
-	    (destData == NULL && *destLen != 0)) {
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	if (destLen)
+		__utee_check_inout_annotation(destLen, sizeof(*destLen));
 
 	if (operation->info.operationClass != TEE_OPERATION_CIPHER) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -1100,13 +1087,15 @@ TEE_Result TEE_CipherDoFinal(TEE_OperationHandle operation,
 	} else {
 		req_dlen = srcLen;
 	}
-	if (*destLen < req_dlen) {
-		*destLen = req_dlen;
+	if (destLen)
+		tmp_dlen = *destLen;
+	if (tmp_dlen < req_dlen) {
+		if (destLen)
+			*destLen = req_dlen;
 		res = TEE_ERROR_SHORT_BUFFER;
 		goto out;
 	}
 
-	tmp_dlen = *destLen - acc_dlen;
 	if (operation->block_size > 1) {
 		res = tee_buffer_update(operation, _utee_cipher_update,
 					srcData, srcLen, dst, &tmp_dlen);
@@ -1128,7 +1117,8 @@ TEE_Result TEE_CipherDoFinal(TEE_OperationHandle operation,
 		goto out;
 
 	acc_dlen += tmp_dlen;
-	*destLen = acc_dlen;
+	if (destLen)
+		*destLen = acc_dlen;
 
 	operation->info.handleState &= ~TEE_HANDLE_FLAG_INITIALIZED;
 
@@ -1193,13 +1183,11 @@ TEE_Result TEE_MACComputeFinal(TEE_OperationHandle operation,
 	TEE_Result res;
 	uint64_t ml;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (message == NULL && messageLen != 0) ||
-	    mac == NULL ||
-	    macLen == NULL) {
+	if (operation == TEE_HANDLE_NULL || (!message && messageLen)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_inout_annotation(macLen, sizeof(*macLen));
 
 	if (operation->info.operationClass != TEE_OPERATION_MAC) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -1364,17 +1352,15 @@ void TEE_AEUpdateAAD(TEE_OperationHandle operation, const void *AADdata,
 TEE_Result TEE_AEUpdate(TEE_OperationHandle operation, const void *srcData,
 			uint32_t srcLen, void *destData, uint32_t *destLen)
 {
-	TEE_Result res;
-	size_t req_dlen;
-	uint64_t dl;
+	TEE_Result res = TEE_SUCCESS;
+	size_t req_dlen = 0;
+	uint64_t dl = 0;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL ||
-	    (destData == NULL && *destLen != 0)) {
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_inout_annotation(destLen, sizeof(*destLen));
 
 	if (operation->info.operationClass != TEE_OPERATION_AE) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -1404,13 +1390,13 @@ TEE_Result TEE_AEUpdate(TEE_OperationHandle operation, const void *srcData,
 		req_dlen = srcLen;
 	}
 
-	if (*destLen < req_dlen) {
+	dl = *destLen;
+	if (dl < req_dlen) {
 		*destLen = req_dlen;
 		res = TEE_ERROR_SHORT_BUFFER;
 		goto out;
 	}
 
-	dl = *destLen;
 	if (operation->block_size > 1) {
 		res = tee_buffer_update(operation, _utee_authenc_update_payload,
 					srcData, srcLen, destData, &dl);
@@ -1451,14 +1437,12 @@ TEE_Result TEE_AEEncryptFinal(TEE_OperationHandle operation,
 	size_t req_dlen;
 	uint64_t tl;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL ||
-	    (destData == NULL && *destLen != 0) ||
-	    tag == NULL || tagLen == NULL) {
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_inout_annotation(destLen, sizeof(*destLen));
+	__utee_check_inout_annotation(tagLen, sizeof(*tagLen));
 
 	if (operation->info.operationClass != TEE_OPERATION_AE) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -1545,14 +1529,11 @@ TEE_Result TEE_AEDecryptFinal(TEE_OperationHandle operation,
 	uint64_t tmp_dlen;
 	size_t req_dlen;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL ||
-	    (destData == NULL && *destLen != 0) ||
-	    (tag == NULL && tagLen != 0)) {
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_inout_annotation(destLen, sizeof(*destLen));
 
 	if (operation->info.operationClass != TEE_OPERATION_AE) {
 		res = TEE_ERROR_BAD_PARAMETERS;
@@ -1627,15 +1608,16 @@ TEE_Result TEE_AsymmetricEncrypt(TEE_OperationHandle operation,
 				 uint32_t srcLen, void *destData,
 				 uint32_t *destLen)
 {
-	TEE_Result res;
+	TEE_Result res = TEE_SUCCESS;
 	struct utee_attribute ua[paramCount];
-	uint64_t dl;
+	uint64_t dl = 0;
 
-	if (operation == TEE_HANDLE_NULL || (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL || (destData == NULL && *destLen != 0))
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen))
 		TEE_Panic(0);
-	if (params == NULL && paramCount != 0)
-		TEE_Panic(0);
+
+	__utee_check_attr_in_annotation(params, paramCount);
+	__utee_check_inout_annotation(destLen, sizeof(*destLen));
+
 	if (!operation->key1)
 		TEE_Panic(0);
 	if (operation->info.operationClass != TEE_OPERATION_ASYMMETRIC_CIPHER)
@@ -1663,15 +1645,16 @@ TEE_Result TEE_AsymmetricDecrypt(TEE_OperationHandle operation,
 				 uint32_t srcLen, void *destData,
 				 uint32_t *destLen)
 {
-	TEE_Result res;
+	TEE_Result res = TEE_SUCCESS;
 	struct utee_attribute ua[paramCount];
-	uint64_t dl;
+	uint64_t dl = 0;
 
-	if (operation == TEE_HANDLE_NULL || (srcData == NULL && srcLen != 0) ||
-	    destLen == NULL || (destData == NULL && *destLen != 0))
+	if (operation == TEE_HANDLE_NULL || (!srcData && srcLen))
 		TEE_Panic(0);
-	if (params == NULL && paramCount != 0)
-		TEE_Panic(0);
+
+	__utee_check_attr_in_annotation(params, paramCount);
+	__utee_check_inout_annotation(destLen, sizeof(*destLen));
+
 	if (!operation->key1)
 		TEE_Panic(0);
 	if (operation->info.operationClass != TEE_OPERATION_ASYMMETRIC_CIPHER)
@@ -1699,16 +1682,16 @@ TEE_Result TEE_AsymmetricSignDigest(TEE_OperationHandle operation,
 				    uint32_t digestLen, void *signature,
 				    uint32_t *signatureLen)
 {
-	TEE_Result res;
+	TEE_Result res = TEE_SUCCESS;
 	struct utee_attribute ua[paramCount];
-	uint64_t sl;
+	uint64_t sl = 0;
 
-	if (operation == TEE_HANDLE_NULL ||
-	    (digest == NULL && digestLen != 0) ||
-	    !signatureLen || (!signature && *signatureLen != 0))
+	if (operation == TEE_HANDLE_NULL || (!digest && digestLen))
 		TEE_Panic(0);
-	if (params == NULL && paramCount != 0)
-		TEE_Panic(0);
+
+	__utee_check_attr_in_annotation(params, paramCount);
+	__utee_check_inout_annotation(signatureLen, sizeof(*signatureLen));
+
 	if (!operation->key1)
 		TEE_Panic(0);
 	if (operation->info.operationClass !=
@@ -1743,8 +1726,9 @@ TEE_Result TEE_AsymmetricVerifyDigest(TEE_OperationHandle operation,
 	    (digest == NULL && digestLen != 0) ||
 	    (signature == NULL && signatureLen != 0))
 		TEE_Panic(0);
-	if (params == NULL && paramCount != 0)
-		TEE_Panic(0);
+
+	__utee_check_attr_in_annotation(params, paramCount);
+
 	if (!operation->key1)
 		TEE_Panic(0);
 	if (operation->info.operationClass !=
@@ -1775,8 +1759,9 @@ void TEE_DeriveKey(TEE_OperationHandle operation,
 
 	if (operation == TEE_HANDLE_NULL || derivedKey == 0)
 		TEE_Panic(0);
-	if (params == NULL && paramCount != 0)
-		TEE_Panic(0);
+
+	__utee_check_attr_in_annotation(params, paramCount);
+
 	if (TEE_ALG_GET_CLASS(operation->info.algorithm) !=
 	    TEE_OPERATION_KEY_DERIVATION)
 		TEE_Panic(0);

lib/libutee/tee_api.c
@@ -153,6 +153,12 @@ TEE_Result TEE_OpenTASession(const TEE_UUID *destination,
 	size_t tmp_len = 0;
 	void *tmp_va[TEE_NUM_PARAMS] = { NULL };
 
+	if (paramTypes)
+		__utee_check_inout_annotation(params,
+					      sizeof(TEE_Param) *
+					      TEE_NUM_PARAMS);
+	__utee_check_out_annotation(session, sizeof(*session));
+
 	res = copy_param(&up, paramTypes, params, &tmp_buf, &tmp_len, tmp_va);
 	if (res)
 		goto out;
@@ -201,6 +207,14 @@ TEE_Result TEE_InvokeTACommand(TEE_TASessionHandle session,
 	void *tmp_buf = NULL;
 	size_t tmp_len = 0;
 	void *tmp_va[TEE_NUM_PARAMS] = { NULL };
+
+	if (paramTypes)
+		__utee_check_inout_annotation(params,
+					      sizeof(TEE_Param) *
+					      TEE_NUM_PARAMS);
+	if (returnOrigin)
+		__utee_check_out_annotation(returnOrigin,
+					    sizeof(*returnOrigin));
 
 	res = copy_param(&up, paramTypes, params, &tmp_buf, &tmp_len, tmp_va);
 	if (res)

lib/libutee/tee_api_objects.c
@@ -115,6 +115,8 @@ TEE_Result TEE_GetObjectBufferAttribute(TEE_ObjectHandle object,
 	TEE_ObjectInfo info;
 	uint64_t sz;
 
+	__utee_check_inout_annotation(size, sizeof(*size));
+
 	res = _utee_cryp_obj_get_info((unsigned long)object, &info);
 	if (res != TEE_SUCCESS)
 		goto exit;
@@ -149,6 +151,11 @@ TEE_Result TEE_GetObjectValueAttribute(TEE_ObjectHandle object,
 	TEE_ObjectInfo info;
 	uint32_t buf[2];
 	uint64_t size = sizeof(buf);
+
+	if (a)
+		__utee_check_out_annotation(a, sizeof(*a));
+	if (b)
+		__utee_check_out_annotation(b, sizeof(*b));
 
 	res = _utee_cryp_obj_get_info((unsigned long)object, &info);
 	if (res != TEE_SUCCESS)
@@ -203,6 +210,8 @@ TEE_Result TEE_AllocateTransientObject(TEE_ObjectType objectType,
 {
 	TEE_Result res;
 	uint32_t obj;
+
+	__utee_check_out_annotation(object, sizeof(*object));
 
 	res = _utee_cryp_obj_alloc(objectType, maxKeySize, &obj);
 
@@ -265,6 +274,8 @@ TEE_Result TEE_PopulateTransientObject(TEE_ObjectHandle object,
 	TEE_ObjectInfo info;
 	struct utee_attribute ua[attrCount];
 
+	__utee_check_attr_in_annotation(attrs, attrCount);
+
 	res = _utee_cryp_obj_get_info((unsigned long)object, &info);
 	if (res != TEE_SUCCESS)
 		TEE_Panic(res);
@@ -287,8 +298,8 @@ TEE_Result TEE_PopulateTransientObject(TEE_ObjectHandle object,
 void TEE_InitRefAttribute(TEE_Attribute *attr, uint32_t attributeID,
 			  const void *buffer, uint32_t length)
 {
-	if (attr == NULL)
-		TEE_Panic(0);
+	__utee_check_out_annotation(attr, sizeof(*attr));
+
 	if ((attributeID & TEE_ATTR_BIT_VALUE) != 0)
 		TEE_Panic(0);
 	attr->attributeID = attributeID;
@@ -299,8 +310,8 @@ void TEE_InitRefAttribute(TEE_Attribute *attr, uint32_t attributeID,
 void TEE_InitValueAttribute(TEE_Attribute *attr, uint32_t attributeID,
 			    uint32_t a, uint32_t b)
 {
-	if (attr == NULL)
-		TEE_Panic(0);
+	__utee_check_out_annotation(attr, sizeof(*attr));
+
 	if ((attributeID & TEE_ATTR_BIT_VALUE) == 0)
 		TEE_Panic(0);
 	attr->attributeID = attributeID;
@@ -371,6 +382,8 @@ TEE_Result TEE_GenerateKey(TEE_ObjectHandle object, uint32_t keySize,
 	TEE_Result res;
 	struct utee_attribute ua[paramCount];
 
+	__utee_check_attr_in_annotation(params, paramCount);
+
 	__utee_from_attr(ua, params, paramCount);
 	res = _utee_cryp_obj_generate_key((unsigned long)object, keySize,
 					  ua, paramCount);
@@ -392,11 +405,6 @@ TEE_Result TEE_OpenPersistentObject(uint32_t storageID, const void *objectID,
 
 	if (!objectID) {
 		res = TEE_ERROR_ITEM_NOT_FOUND;
-		goto exit;
-	}
-
-	if (objectIDLen > TEE_OBJECT_ID_MAX_LEN) {
-		res = TEE_ERROR_BAD_PARAMETERS;
 		goto exit;
 	}
 
@@ -435,10 +443,7 @@ TEE_Result TEE_CreatePersistentObject(uint32_t storageID, const void *objectID,
 		goto exit;
 	}
 
-	if (objectIDLen > TEE_OBJECT_ID_MAX_LEN) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto exit;
-	}
+	__utee_check_out_annotation(object, sizeof(*object));
 
 	res = _utee_storage_obj_create(storageID, objectID, objectIDLen, flags,
 				       (unsigned long)attributes, initialData,
@@ -509,16 +514,6 @@ TEE_Result TEE_RenamePersistentObject(TEE_ObjectHandle object,
 		goto out;
 	}
 
-	if (!newObjectID) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
-
-	if (newObjectIDLen > TEE_OBJECT_ID_MAX_LEN) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
-
 	res = _utee_storage_obj_rename((unsigned long)object, newObjectID,
 				       newObjectIDLen);
 
@@ -538,8 +533,8 @@ TEE_Result TEE_AllocatePersistentObjectEnumerator(TEE_ObjectEnumHandle *
 	TEE_Result res;
 	uint32_t oe;
 
-	if (!objectEnumerator)
-		return TEE_ERROR_BAD_PARAMETERS;
+	__utee_check_out_annotation(objectEnumerator,
+				    sizeof(*objectEnumerator));
 
 	res = _utee_storage_alloc_enum(&oe);
 
@@ -608,12 +603,11 @@ TEE_Result TEE_GetNextPersistentObject(TEE_ObjectEnumHandle objectEnumerator,
 	TEE_ObjectInfo local_info;
 	TEE_ObjectInfo *pt_info;
 
-	if (!objectID) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	if (objectInfo)
+		__utee_check_out_annotation(objectInfo, sizeof(*objectInfo));
+	__utee_check_out_annotation(objectIDLen, sizeof(*objectIDLen));
 
-	if (!objectIDLen) {
+	if (!objectID) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
@@ -649,6 +643,7 @@ TEE_Result TEE_ReadObjectData(TEE_ObjectHandle object, void *buffer,
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	__utee_check_out_annotation(count, sizeof(*count));
 
 	cnt64 = *count;
 	res = _utee_storage_obj_read((unsigned long)object, buffer, size,

lib/libutee/tee_api_panic.c
@@ -1,13 +1,87 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
+ * Copyright (c) 2020, Linaro Limited
  */
+
+#include <config.h>
+#include <string.h>
 #include <tee_api.h>
 #include <utee_syscalls.h>
+#include <util.h>
+
+#include "tee_api_private.h"
+
+#define ACCESS_RW	(TEE_MEMORY_ACCESS_READ | TEE_MEMORY_ACCESS_WRITE)
+#define ACCESS_W_ANY	(TEE_MEMORY_ACCESS_WRITE | TEE_MEMORY_ACCESS_ANY_OWNER)
+#define ACCESS_R	TEE_MEMORY_ACCESS_READ
+#define ACCESS_W	TEE_MEMORY_ACCESS_WRITE
 
 /* System API - Misc */
 
 void TEE_Panic(TEE_Result panicCode)
 {
 	_utee_panic(panicCode);
+}
+
+static void check_res(const char *msg __maybe_unused, TEE_Result res)
+{
+	if (res) {
+		DMSG("%s: error %#"PRIx32, msg, res);
+		TEE_Panic(0);
+	}
+}
+
+static TEE_Result check_access(uint32_t flags, void *buf, size_t len)
+{
+	if (!len)
+		return TEE_SUCCESS;
+
+	if (!buf)
+		return TEE_ERROR_SECURITY;
+
+	if (IS_ENABLED(CFG_TA_STRICT_ANNOTATION_CHECKS))
+		return TEE_CheckMemoryAccessRights(flags, buf, len);
+
+	return TEE_SUCCESS;
+}
+
+void __utee_check_outbuf_annotation(void *buf, uint32_t *len)
+{
+	check_res("[outbuf] len",
+		  check_access(ACCESS_RW, len, sizeof(*len)));
+	check_res("[outbuf] buf",
+		  check_access(ACCESS_W_ANY, buf, *len));
+}
+
+void __utee_check_instring_annotation(const char *buf)
+{
+	check_res("[instring]",
+		  check_access(ACCESS_R, (char *)buf, strlen(buf) + 1));
+}
+
+void __utee_check_outstring_annotation(char *buf, uint32_t *len)
+{
+	check_res("[outstring] len",
+		  check_access(ACCESS_RW, len, sizeof(*len)));
+	check_res("[outstring] buf",
+		  check_access(ACCESS_W_ANY, buf, *len));
+}
+
+void __utee_check_out_annotation(void *buf, const size_t len)
+{
+	check_res("[out]",
+		  check_access(ACCESS_W, buf, len));
+}
+
+void __utee_check_attr_in_annotation(const TEE_Attribute *attr, size_t count)
+{
+	check_res("[in] attr",
+		  check_access(ACCESS_R, (void *)attr, sizeof(*attr) * count));
+}
+
+void __utee_check_inout_annotation(void *buf, const size_t len)
+{
+	check_res("[inout]",
+		  check_access(ACCESS_RW, buf, len));
 }

lib/libutee/tee_api_private.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2015, Linaro Limited
+ * Copyright (c) 2015-2020, Linaro Limited
  */
 #ifndef TEE_API_PRIVATE
 #define TEE_API_PRIVATE
@@ -24,5 +24,16 @@ static inline void __utee_gprof_init(void) {}
 static inline void __utee_gprof_fini(void) {}
 #endif
 
-#endif /*TEE_API_PRIVATE*/
+/*
+ * The functions help checking that the pointers comply with the parameters
+ * annotation as described in the spec. Any descrepency results in a panic
+ * of the TA.
+ */
+void __utee_check_out_annotation(void *buf, const size_t len);
+void __utee_check_inout_annotation(void *buf, const size_t len);
+void __utee_check_attr_in_annotation(const TEE_Attribute *attr, size_t count);
+void __utee_check_outbuf_annotation(void *buf, uint32_t *len);
+void __utee_check_instring_annotation(const char *buf);
+void __utee_check_outstring_annotation(char *buf, uint32_t *len);
 
+#endif /*TEE_API_PRIVATE*/

lib/libutee/tee_api_property.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2014, STMicroelectronics International N.V.
- * Copyright (c) 2017, Linaro Limited
+ * Copyright (c) 2017-2020, Linaro Limited
  */
 #include <printk.h>
 #include <stdio.h>
@@ -17,8 +17,9 @@
 #include <utee_syscalls.h>
 #include <util.h>
 
-#include "string_ext.h"
 #include "base64.h"
+#include "string_ext.h"
+#include "tee_api_private.h"
 
 #define PROP_STR_MAX    80
 
@@ -123,6 +124,13 @@ static TEE_Result propget_get_ext_prop(const struct user_ta_property *ep,
 	return TEE_SUCCESS;
 }
 
+static bool is_propset_pseudo_handle(TEE_PropSetHandle h)
+{
+	return h == TEE_PROPSET_CURRENT_TA ||
+	       h == TEE_PROPSET_CURRENT_CLIENT ||
+	       h == TEE_PROPSET_TEE_IMPLEMENTATION;
+}
+
 static TEE_Result propget_get_property(TEE_PropSetHandle h, const char *name,
 				       enum user_ta_prop_type *type,
 				       void *buf, uint32_t *len)
@@ -133,8 +141,7 @@ static TEE_Result propget_get_property(TEE_PropSetHandle h, const char *name,
 	uint32_t prop_type;
 	uint32_t index;
 
-	if (h == TEE_PROPSET_CURRENT_TA || h == TEE_PROPSET_CURRENT_CLIENT ||
-	    h == TEE_PROPSET_TEE_IMPLEMENTATION) {
+	if (is_propset_pseudo_handle(h)) {
 		size_t n;
 
 		res = propset_get(h, &eps, &eps_len);
@@ -193,10 +200,9 @@ TEE_Result TEE_GetPropertyAsString(TEE_PropSetHandle propsetOrEnumerator,
 	bool bool_val;
 	TEE_Identity *p_identity_val;
 
-	if (!value || !value_len) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	if (is_propset_pseudo_handle(propsetOrEnumerator))
+		__utee_check_instring_annotation(name);
+	__utee_check_outstring_annotation(value, value_len);
 
 	tmp_len = *value_len;
 	if (tmp_len < sizeof(TEE_Identity))
@@ -288,10 +294,10 @@ TEE_Result TEE_GetPropertyAsBool(TEE_PropSetHandle propsetOrEnumerator,
 	TEE_Result res;
 	enum user_ta_prop_type type;
 	uint32_t bool_len = sizeof(bool);
-	if (value == NULL) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+
+	if (is_propset_pseudo_handle(propsetOrEnumerator))
+		__utee_check_instring_annotation(name);
+	__utee_check_out_annotation(value, sizeof(*value));
 
 	type = USER_TA_PROP_TYPE_BOOL;
 	res = propget_get_property(propsetOrEnumerator, name, &type,
@@ -317,10 +323,9 @@ TEE_Result TEE_GetPropertyAsU32(TEE_PropSetHandle propsetOrEnumerator,
 	enum user_ta_prop_type type;
 	uint32_t uint32_len = sizeof(uint32_t);
 
-	if (!value) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	if (is_propset_pseudo_handle(propsetOrEnumerator))
+		__utee_check_instring_annotation(name);
+	__utee_check_out_annotation(value, sizeof(*value));
 
 	type = USER_TA_PROP_TYPE_U32;
 	res = propget_get_property(propsetOrEnumerator, name, &type,
@@ -328,7 +333,6 @@ TEE_Result TEE_GetPropertyAsU32(TEE_PropSetHandle propsetOrEnumerator,
 	if (type != USER_TA_PROP_TYPE_U32)
 		res = TEE_ERROR_BAD_FORMAT;
 
-out:
 	if (res != TEE_SUCCESS &&
 	    res != TEE_ERROR_ITEM_NOT_FOUND &&
 	    res != TEE_ERROR_BAD_FORMAT)
@@ -344,10 +348,9 @@ TEE_Result TEE_GetPropertyAsBinaryBlock(TEE_PropSetHandle propsetOrEnumerator,
 	TEE_Result res;
 	enum user_ta_prop_type type;
 
-	if (!value_len) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	if (is_propset_pseudo_handle(propsetOrEnumerator))
+		__utee_check_instring_annotation(name);
+	__utee_check_outbuf_annotation(value, value_len);
 
 	type = USER_TA_PROP_TYPE_BINARY_BLOCK;
 	res = propget_get_property(propsetOrEnumerator, name, &type,
@@ -355,7 +358,6 @@ TEE_Result TEE_GetPropertyAsBinaryBlock(TEE_PropSetHandle propsetOrEnumerator,
 	if (type != USER_TA_PROP_TYPE_BINARY_BLOCK)
 		res = TEE_ERROR_BAD_FORMAT;
 
-out:
 	if (res != TEE_SUCCESS &&
 	    res != TEE_ERROR_ITEM_NOT_FOUND &&
 	    res != TEE_ERROR_BAD_FORMAT &&
@@ -372,10 +374,9 @@ TEE_Result TEE_GetPropertyAsUUID(TEE_PropSetHandle propsetOrEnumerator,
 	enum user_ta_prop_type type;
 	uint32_t uuid_len = sizeof(TEE_UUID);
 
-	if (!value) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	if (is_propset_pseudo_handle(propsetOrEnumerator))
+		__utee_check_instring_annotation(name);
+	__utee_check_out_annotation(value, sizeof(*value));
 
 	type = USER_TA_PROP_TYPE_UUID;
 	res = propget_get_property(propsetOrEnumerator, name, &type,
@@ -383,7 +384,6 @@ TEE_Result TEE_GetPropertyAsUUID(TEE_PropSetHandle propsetOrEnumerator,
 	if (type != USER_TA_PROP_TYPE_UUID)
 		res = TEE_ERROR_BAD_FORMAT;
 
-out:
 	if (res != TEE_SUCCESS &&
 	    res != TEE_ERROR_ITEM_NOT_FOUND &&
 	    res != TEE_ERROR_BAD_FORMAT)
@@ -399,10 +399,9 @@ TEE_Result TEE_GetPropertyAsIdentity(TEE_PropSetHandle propsetOrEnumerator,
 	enum user_ta_prop_type type;
 	uint32_t identity_len = sizeof(TEE_Identity);
 
-	if (!value) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto out;
-	}
+	if (is_propset_pseudo_handle(propsetOrEnumerator))
+		__utee_check_instring_annotation(name);
+	__utee_check_out_annotation(value, sizeof(*value));
 
 	type = USER_TA_PROP_TYPE_IDENTITY;
 	res = propget_get_property(propsetOrEnumerator, name, &type,
@@ -410,7 +409,6 @@ TEE_Result TEE_GetPropertyAsIdentity(TEE_PropSetHandle propsetOrEnumerator,
 	if (type != USER_TA_PROP_TYPE_IDENTITY)
 		res = TEE_ERROR_BAD_FORMAT;
 
-out:
 	if (res != TEE_SUCCESS &&
 	    res != TEE_ERROR_ITEM_NOT_FOUND &&
 	    res != TEE_ERROR_BAD_FORMAT)
@@ -424,10 +422,7 @@ TEE_Result TEE_AllocatePropertyEnumerator(TEE_PropSetHandle *enumerator)
 	TEE_Result res;
 	struct prop_enumerator *pe;
 
-	if (!enumerator) {
-		res = TEE_ERROR_BAD_PARAMETERS;
-		goto err;
-	}
+	__utee_check_out_annotation(enumerator, sizeof(*enumerator));
 
 	pe = TEE_Malloc(sizeof(struct prop_enumerator),
 			TEE_USER_MEM_HINT_NO_FILL_ZERO);
@@ -485,10 +480,11 @@ TEE_Result TEE_GetPropertyName(TEE_PropSetHandle enumerator,
 	const char *str;
 	size_t bufferlen;
 
-	if (!pe || !name || !name_len) {
+	if (!pe) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto err;
 	}
+	__utee_check_outstring_annotation(name, name_len);
 
 	bufferlen = *name_len;
 	res = propset_get(pe->prop_set, &eps, &eps_len);

mk/config.mk
@@ -602,3 +602,9 @@ endif
 ifeq ($(CFG_WITH_SECURE_PARTITION),y)
 $(call force,CFG_ZLIB,y)
 endif
+
+# When enabled checks that buffers passed to the GP Internal Core API
+# comply with the rules added as annotations as part of the definition of
+# the API. For example preventing buffers in non-secure shared memory when
+# not allowed.
+CFG_TA_STRICT_ANNOTATION_CHECKS ?= y

commit afdf9067d852a9f9f7fdb0acfab37b973441e041

libutee: check destData against NULL in TEE_AEDecryptFinal()
destData is [outbufopt] but was never checked for NULL, unlike srcData.
Use __utee_check_outbuf_annotation() instead, matching
TEE_CipherDoFinal() and TEE_AEEncryptFinal().

Fixes: 6915bbbb5b56 ("GP 1.1: Check parameters annotated in the specification")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Assisted-by: Claude:sonnet-5
Reviewed-by: Guled Hasan <Law.Zoldyck@proton.me>

lib/libutee/tee_api_operations.c
@@ -2144,7 +2144,7 @@ TEE_Result TEE_AEDecryptFinal(TEE_OperationHandle operation,
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
-	__utee_check_inout_annotation(destLen, sizeof(*destLen));
+	__utee_check_outbuf_annotation(destData, destLen);
 
 	if (operation->info.operationClass != TEE_OPERATION_AE) {
 		res = TEE_ERROR_BAD_PARAMETERS;