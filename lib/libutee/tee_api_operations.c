OP-TEE/optee_os

commit 24ea76132a1203c2c46113512f7e5afa4c689f7d

GP131: add TEE_DigestExtract()
Adds TEE_DigestExtract() to support Extendable Output Functions, that
is, for now TEE_ALG_SHAKE128 and TEE_ALG_SHAKE256.

Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutee/tee_api_operations.c
@@ -3,6 +3,7 @@
  * Copyright (c) 2014, STMicroelectronics International N.V.
  * Copyright (c) 2021, SumUp Services GmbH
  */
+#include <assert.h>
 #include <config.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,7 +21,12 @@ struct __TEE_OperationHandle {
 	TEE_ObjectHandle key1;
 	TEE_ObjectHandle key2;
 	uint32_t operationState;/* Operation state : INITIAL or ACTIVE */
-	uint8_t *buffer;	/* buffer to collect complete blocks */
+
+	/*
+	 * buffer to collect complete blocks or to keep a complete digest
+	 * for TEE_DigestExtract().
+	 */
+	uint8_t *buffer;
 	bool buffer_two_blocks;	/* True if two blocks need to be buffered */
 	size_t block_size;	/* Block size of cipher */
 	size_t buffer_offs;	/* Offset in buffer */
@@ -260,6 +266,12 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 	case TEE_ALG_SHA256:
 	case TEE_ALG_SHA384:
 	case TEE_ALG_SHA512:
+	case TEE_ALG_SHA3_224:
+	case TEE_ALG_SHA3_256:
+	case TEE_ALG_SHA3_384:
+	case TEE_ALG_SHA3_512:
+	case TEE_ALG_SHAKE128:
+	case TEE_ALG_SHAKE256:
 	case TEE_ALG_SM3:
 		if (mode != TEE_MODE_DIGEST)
 			return TEE_ERROR_NOT_SUPPORTED;
@@ -313,6 +325,13 @@ TEE_Result TEE_AllocateOperation(TEE_OperationHandle *operation,
 	op->info.maxKeySize = maxKeySize;
 	op->info.requiredKeyUsage = req_key_usage;
 	op->info.handleState = handle_state;
+
+	/*
+	 * Needed to buffer the digest if TEE_DigestExtract() doesn't
+	 * retrieve the entire digest in one go.
+	 */
+	if (op->info.operationClass == TEE_OPERATION_DIGEST)
+		block_size = op->info.digestLength;
 
 	if (block_size > 1) {
 		size_t buffer_size = block_size;
@@ -799,10 +818,14 @@ void TEE_CopyOperation(TEE_OperationHandle dst_op, TEE_OperationHandle src_op)
 		TEE_Panic(0);
 
 	if (dst_op->buffer != NULL) {
+		size_t sz = src_op->block_size;
+
 		if (src_op->buffer == NULL)
 			TEE_Panic(0);
 
-		memcpy(dst_op->buffer, src_op->buffer, src_op->buffer_offs);
+		if (src_op->buffer_two_blocks)
+			sz *= 2;
+		memcpy(dst_op->buffer, src_op->buffer, sz);
 		dst_op->buffer_offs = src_op->buffer_offs;
 	} else if (src_op->buffer != NULL) {
 		TEE_Panic(0);
@@ -856,8 +879,9 @@ void __GP11_TEE_DigestUpdate(TEE_OperationHandle operation,
 TEE_Result TEE_DigestDoFinal(TEE_OperationHandle operation, const void *chunk,
 			     size_t chunkLen, void *hash, size_t *hashLen)
 {
-	TEE_Result res;
-	uint64_t hl;
+	TEE_Result res = TEE_SUCCESS;
+	uint64_t hl = 0;
+	size_t len = 0;
 
 	if ((operation == TEE_HANDLE_NULL) ||
 	    (!chunk && chunkLen) ||
@@ -865,13 +889,31 @@ TEE_Result TEE_DigestDoFinal(TEE_OperationHandle operation, const void *chunk,
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto out;
 	}
+	if (operation->operationState == TEE_OPERATION_STATE_EXTRACTING &&
+	    chunkLen) {
+		res = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
 	__utee_check_inout_annotation(hashLen, sizeof(*hashLen));
 
-	hl = *hashLen;
-	res = _utee_hash_final(operation->state, chunk, chunkLen, hash, &hl);
-	*hashLen = hl;
-	if (res != TEE_SUCCESS)
-		goto out;
+	if (operation->operationState == TEE_OPERATION_STATE_EXTRACTING &&
+	    operation->buffer) {
+		/*
+		 * This is not an Extendable-Output Function and we have
+		 * already started extracting
+		 */
+		len = MIN(operation->block_size - operation->buffer_offs,
+			  *hashLen);
+		memcpy(hash, operation->buffer + operation->buffer_offs, len);
+		*hashLen = len;
+	} else {
+		hl = *hashLen;
+		res = _utee_hash_final(operation->state, chunk, chunkLen, hash,
+				       &hl);
+		*hashLen = hl;
+		if (res)
+			goto out;
+	}
 
 	/* Reset operation state */
 	init_hash_operation(operation, NULL, 0);
@@ -898,6 +940,52 @@ TEE_Result __GP11_TEE_DigestDoFinal(TEE_OperationHandle operation,
 	res = TEE_DigestDoFinal(operation, chunk, chunkLen, hash, &l);
 	*hashLen = l;
 	return res;
+}
+
+TEE_Result TEE_DigestExtract(TEE_OperationHandle operation, void *hash,
+			     size_t *hashLen)
+{
+	TEE_Result res = TEE_SUCCESS;
+	uint64_t hl = 0;
+	size_t len = 0;
+
+	if (operation == TEE_HANDLE_NULL ||
+	    operation->info.operationClass != TEE_OPERATION_DIGEST)
+		TEE_Panic(0);
+	__utee_check_inout_annotation(hashLen, sizeof(*hashLen));
+
+	if (!operation->buffer) {
+		/* This is an Extendable-Output Function */
+		operation->info.handleState |= TEE_HANDLE_FLAG_EXTRACTING;
+		operation->operationState = TEE_OPERATION_STATE_EXTRACTING;
+		hl = *hashLen;
+		res = _utee_hash_final(operation->state, NULL, 0, hash, &hl);
+		if (res)
+			TEE_Panic(0);
+		*hashLen = hl;
+
+		return TEE_SUCCESS;
+	}
+
+	if (operation->operationState != TEE_OPERATION_STATE_EXTRACTING) {
+		hl = operation->block_size;
+		res = _utee_hash_final(operation->state, NULL, 0,
+				       operation->buffer, &hl);
+		if (res)
+			TEE_Panic(0);
+		if (hl != operation->block_size)
+			TEE_Panic(0);
+		assert(!operation->buffer_offs);
+		operation->info.handleState |= TEE_HANDLE_FLAG_EXTRACTING;
+		operation->operationState = TEE_OPERATION_STATE_EXTRACTING;
+	}
+
+	len = MIN(operation->block_size - operation->buffer_offs, *hashLen);
+	memcpy(hash, operation->buffer + operation->buffer_offs, len);
+	*hashLen = len;
+	operation->buffer_offs += len;
+
+	return TEE_SUCCESS;
 }
 
 /* Cryptographic Operations API - Symmetric Cipher Functions */

lib/libutee/include/tee_api_defines.h
@@ -208,6 +208,7 @@
 #define TEE_HANDLE_FLAG_INITIALIZED        0x00020000
 #define TEE_HANDLE_FLAG_KEY_SET            0x00040000
 #define TEE_HANDLE_FLAG_EXPECT_TWO_KEYS    0x00080000
+#define TEE_HANDLE_FLAG_EXTRACTING         0x00100000
 #define TEE_OPERATION_CIPHER               1
 #define TEE_OPERATION_MAC                  3
 #define TEE_OPERATION_AE                   4
@@ -217,6 +218,7 @@
 #define TEE_OPERATION_KEY_DERIVATION       8
 #define TEE_OPERATION_STATE_INITIAL        0x00000000
 #define TEE_OPERATION_STATE_ACTIVE         0x00000001
+#define TEE_OPERATION_STATE_EXTRACTING     0x00000002
 
 /* Algorithm Identifiers */
 #define TEE_ALG_AES_ECB_NOPAD                   0x10000010

lib/libutee/include/tee_internal_api.h
@@ -335,6 +335,9 @@ TEE_Result __GP11_TEE_DigestDoFinal(TEE_OperationHandle operation,
 				    const void *chunk, uint32_t chunkLen,
 				    void *hash, uint32_t *hashLen);
 
+TEE_Result TEE_DigestExtract(TEE_OperationHandle operation, void *hash,
+			     size_t *hashLen);
+
 /* Cryptographic Operations API - Symmetric Cipher Functions */
 
 void TEE_CipherInit(TEE_OperationHandle operation, const void *IV,

lib/libutee/include/utee_defines.h
@@ -202,20 +202,24 @@ static inline size_t __tee_alg_get_digest_size(uint32_t algo)
 	case TEE_ALG_DSA_SHA1:
 		return TEE_SHA1_HASH_SIZE;
 	case TEE_ALG_SHA224:
+	case TEE_ALG_SHA3_224:
 	case TEE_ALG_HMAC_SHA224:
 	case TEE_ALG_HMAC_SHA3_224:
 	case TEE_ALG_DSA_SHA224:
 		return TEE_SHA224_HASH_SIZE;
 	case TEE_ALG_SHA256:
+	case TEE_ALG_SHA3_256:
 	case TEE_ALG_HMAC_SHA256:
 	case TEE_ALG_HMAC_SHA3_256:
 	case TEE_ALG_DSA_SHA256:
 		return TEE_SHA256_HASH_SIZE;
 	case TEE_ALG_SHA384:
+	case TEE_ALG_SHA3_384:
 	case TEE_ALG_HMAC_SHA384:
 	case TEE_ALG_HMAC_SHA3_384:
 		return TEE_SHA384_HASH_SIZE;
 	case TEE_ALG_SHA512:
+	case TEE_ALG_SHA3_512:
 	case TEE_ALG_HMAC_SHA512:
 	case TEE_ALG_HMAC_SHA3_512:
 		return TEE_SHA512_HASH_SIZE;

commit fd5a211fd7e7b95c26587c911b645367adbc9f5e

libutee: fix digest final short-buffer handling after extraction
For a non-XOF operation in extracting state, TEE_DigestDoFinal()
currently truncates the remaining digest to the output buffer size and
returns success. This finalizes the operation even though the caller
cannot retrieve the complete digest.

GP TEE Internal Core API v1.3.1 §6.3.2 requires non-XOF operations to
return TEE_ERROR_SHORT_BUFFER without finalizing in this case.

Return TEE_ERROR_SHORT_BUFFER with the required remaining length before
copying, leaving the operation in extracting state for a retry.

Fixes: 24ea76132a12 ("GP131: add TEE_DigestExtract()")
Signed-off-by: Jaehun Lee <gsojc234@gmail.com>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>
Reviewed-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

lib/libutee/tee_api_operations.c
@@ -950,8 +950,13 @@ TEE_Result TEE_DigestDoFinal(TEE_OperationHandle operation, const void *chunk,
 		 * This is not an Extendable-Output Function and we have
 		 * already started extracting
 		 */
-		len = MIN(operation->block_size - operation->buffer_offs,
-			  *hashLen);
+		len = operation->block_size - operation->buffer_offs;
+		if (*hashLen < len) {
+			*hashLen = len;
+			res = TEE_ERROR_SHORT_BUFFER;
+			goto out;
+		}
+
 		memcpy(hash, operation->buffer + operation->buffer_offs, len);
 		*hashLen = len;
 	} else {