OP-TEE/optee_os

commit c999bfc60869d707a2db42ec1b704e90b2755029

shdr: add check for weak key sizes
Add a function is_weak_key_size(...), which checks whether a given key
size (in bits) complies with current security standards. If the key size
is lower than 2048, then it's considered deprecated and will make
signature verification fail. Note that this only affects verifying TA
and subkey signatures.

This change aligns with GlobalPlatform's decision, influenced by
feedback from ANSSI, BSI, SOGIS, and NIST. For further details on the
GlobalPlatform's cryptographic algorithm recommendations, see [1].

Link: https://globalplatform.org/specs-library/globalplatform-technology-cryptographic-algorithm-recommendations/ [1]
Signed-off-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

core/crypto/signed_hdr.c
@@ -55,6 +55,18 @@ static bool is_weak_hash_algo(uint32_t algo)
 	       algo == TEE_ALG_MD5SHA1 || algo == TEE_ALG_SHA224;
 }
 
+/*
+ * Checks for key sizes in bits, that are depracted.
+ */
+static bool is_weak_key_size(uint32_t algo, size_t key_size_bits)
+{
+	if (TEE_ALG_GET_MAIN_ALG(algo) == TEE_MAIN_ALGO_RSA &&
+	    key_size_bits < 2048)
+		return true;
+
+	return false;
+}
+
 TEE_Result shdr_verify_signature(const struct shdr *shdr)
 {
 	struct rsa_public_key key = { };
@@ -73,6 +85,9 @@ TEE_Result shdr_verify_signature(const struct shdr *shdr)
 
 	hash_algo = TEE_DIGEST_HASH_TO_ALGO(shdr->algo);
 	if (is_weak_hash_algo(hash_algo))
+		goto err;
+
+	if (is_weak_key_size(shdr->algo, ta_pub_key_modulus_size * 8))
 		goto err;
 
 	res = tee_alg_get_digest_size(hash_algo, &hash_size);
@@ -359,6 +374,9 @@ TEE_Result shdr_verify_signature2(struct shdr_pub_key *key,
 
 	hash_algo = TEE_DIGEST_HASH_TO_ALGO(shdr->algo);
 	if (is_weak_hash_algo(hash_algo))
+		goto err;
+
+	if (is_weak_key_size(shdr->algo, ta_pub_key_modulus_size * 8))
 		goto err;
 
 	if (tee_alg_get_digest_size(hash_algo, &hash_size) ||

commit ba35e3c9c7cb0c9d17686bed9ef3981232a49416

core: crypto: check subkey's own size in shdr_verify_signature2()
shdr_verify_signature2()'s weak-key check used the root key's
modulus size (ta_pub_key_modulus_size) instead of the size of the
subkey it is actually about to verify with, making the check a
no-op for the subkey it applies to.

Use crypto_bignum_num_bits() on the loaded subkey's modulus
instead.

Fixes: c999bfc60869 ("shdr: add check for weak key sizes")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Assisted-by: Claude:sonnet-5
Reviewed-by: Guled Hasan <Law.Zoldyck@proton.me>

core/crypto/signed_hdr.c
@@ -382,21 +382,24 @@ TEE_Result shdr_verify_signature2(struct shdr_pub_key *key,
 	if (is_weak_hash_algo(hash_algo))
 		goto err;
 
-	if (is_weak_key_size(shdr->algo, ta_pub_key_modulus_size * 8))
-		goto err;
-
 	if (tee_alg_get_digest_size(hash_algo, &hash_size) ||
 	    hash_size != shdr->hash_size)
 		goto err;
 
 	switch (key->main_algo) {
-	case TEE_MAIN_ALGO_RSA:
+	case TEE_MAIN_ALGO_RSA: {
+		size_t n_bits = crypto_bignum_num_bits(key->pub_key.rsa->n);
+
+		if (is_weak_key_size(shdr->algo, n_bits))
+			goto err;
+
 		FTMN_CALL_FUNC(res, &ftmn, FTMN_INCR0,
 			       crypto_acipher_rsassa_verify, shdr->algo,
 			       key->pub_key.rsa, shdr->hash_size,
 			       SHDR_GET_HASH(shdr), shdr->hash_size,
 			       SHDR_GET_SIG(shdr), shdr->sig_size);
 		break;
+	}
 	default:
 		panic();
 	}