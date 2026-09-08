OP-TEE/optee_os

commit fa5813256d0c15892403c9d56ab3dc8280a7705f

plat-qcom: Add support for Nord based on Wildcat arch
Nord is also referenced as IQ-10 as the marketing name, more details
about the platform can be found on link below. It is based on wildcat
architecture and base support is being added for 18 Oryon cores along
with GICv4.

Assisted-by: Claude:claude-opus-4-8
Link: https://www.qualcomm.com/internet-of-things/products/iq10-series
Co-developed-by: Tyler Baker <tyler.baker@oss.qualcomm.com>
Signed-off-by: Tyler Baker <tyler.baker@oss.qualcomm.com>
Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

core/arch/arm/plat-qcom/wildcat/nord/target_config.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef TARGET_CONFIG_H
+#define TARGET_CONFIG_H
+
+#define GENI_UART_REG_BASE		UL(0x884000)
+
+#define DRAM0_BASE			UL(0x80000000)
+#define DRAM0_SIZE			UL(0x380000000)
+#define DRAM1_BASE			ULL(0x800000000)
+#define DRAM1_SIZE			ULL(0x800000000)
+
+#endif /* TARGET_CONFIG_H */

core/arch/arm/plat-qcom/bobcat/sub.mk
@@ -1,0 +1,1 @@
+# Placeholder for Qcom architecture specific makefile

core/arch/arm/plat-qcom/conf.mk
@@ -31,11 +31,14 @@ supported-ta-targets ?= ta_arm64
 # Architecture family mapping
 HOYA_ARCH_CHIPSETS := kodiak lemans
 BOBCAT_ARCH_CHIPSETS := ipq96xx ipq52xx
+WILDCAT_ARCH_CHIPSETS := nord
 
 ifneq (,$(filter $(PLATFORM_FLAVOR),$(HOYA_ARCH_CHIPSETS)))
 QCOM_ARCH_FAMILY := hoya
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(BOBCAT_ARCH_CHIPSETS)))
 QCOM_ARCH_FAMILY := bobcat
+else ifneq (,$(filter $(PLATFORM_FLAVOR),$(WILDCAT_ARCH_CHIPSETS)))
+QCOM_ARCH_FAMILY := wildcat
 else
 $(error Unsupported PLATFORM_FLAVOR: $(PLATFORM_FLAVOR))
 endif

core/arch/arm/plat-qcom/hoya/sub.mk
@@ -1,0 +1,1 @@
+# Placeholder for Qcom architecture specific makefile

core/arch/arm/plat-qcom/sub.mk
@@ -2,7 +2,7 @@ global-incdirs-y += .
 global-incdirs-y += $(QCOM_ARCH_FAMILY)
 global-incdirs-y += $(QCOM_ARCH_FAMILY)/$(PLATFORM_FLAVOR)
 srcs-y += main.c
+srcs-$(CFG_QCOM_DIAG_LOG) += diag_log.c
 
 subdirs-$(CFG_QCOM_QFPROM_FUSEPROV) += provision
-
-srcs-$(CFG_QCOM_DIAG_LOG) += diag_log.c
+subdirs-y += $(QCOM_ARCH_FAMILY)

core/arch/arm/plat-qcom/wildcat/arch_config.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef ARCH_CONFIG_H
+#define ARCH_CONFIG_H
+
+#define GICD_BASE			UL(0x17000000)
+#define GICR_BASE			UL(0x17080000)
+
+#endif /* ARCH_CONFIG_H */

core/arch/arm/plat-qcom/wildcat/core_pos_a64.S
@@ -1,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * Nord (Oryon "NCC") get_core_pos override.
+ *
+ * Oryon MPIDR has the MT bit set but uses one thread per core with
+ * AFF0 = thread (always 0), AFF1 = core (0..5), AFF2 = cluster (0..2).
+ * The generic get_core_pos_mpidr left-shifts the MPIDR by one affinity
+ * level when MT is set; on this layout that promotes the CORE into the
+ * cluster slot and drops the real cluster, collapsing all clusters to the
+ * same value and yielding CorePos = core * 4 (so cores 2..5 map to
+ * 8/12/16/20, exceeding CFG_TEE_CORE_NB_CORE and getting their GIC
+ * redistributors skipped -> secondaries crash). Read AFF1/AFF2 from the
+ * raw MPIDR instead and produce a contiguous CorePos = cluster*6 + core
+ * (0..17) for the 3 x 6 topology.
+ */
+
+#include <asm.S>
+#include <arm.h>
+#include <arm64_macros.S>
+
+FUNC get_core_pos_mpidr , :
+	/* AFF1 = core (0..5) */
+	ubfx	x1, x0, #MPIDR_AFF1_SHIFT, #MPIDR_AFFINITY_BITS
+	/* AFF2 = cluster (0..2) */
+	ubfx	x2, x0, #MPIDR_AFF2_SHIFT, #MPIDR_AFFINITY_BITS
+	add	x2, x2, x2, lsl #1	/* x2 = cluster * 3 */
+	add	x0, x1, x2, lsl #1	/* CorePos = core + cluster * 6 */
+	ret
+END_FUNC get_core_pos_mpidr

core/arch/arm/plat-qcom/wildcat/nord/target.mk
@@ -1,0 +1,5 @@
+# Nord (SA8797P / Oryon) OP-TEE platform.
+
+# Threads are expensive in OP-TEE, so they don't have
+# to be same as number of cores.
+$(call force,CFG_TEE_CORE_NB_CORE,18)

commit ce41fbce248d1745cb724a6d7250c9a7628007e7

plat-qcom: wildcat: nord: Fix DRAM address mappings and add DRAM2 region
Fix the address mappings for DRAM on Nord IQ-10. DRAM0 has a size of
0x8000 0000 while DRAM1 starts at 0x8 8000 0000 with a size of
0x7 8000 0000. Also, there exists a DRAM2 region which the Linux kernel
maps as HIGHMEM for allocating memory for userspace applications.
Since this region starting at 0x88 0000 0000 is not mapped by OP-TEE,
TEE_IOC_SHM_REGISTER ioctl fails because OP-TEE doesn't recognize it as
Non-secure memory.

Validated by running xtest 1002 after building OP-TEE with
CFG_TEE_CORE_EMBED_INTERNAL_TESTS=y which was failing otherwise.

Fixes: fa5813256d0c ("plat-qcom: Add support for Nord based on Wildcat arch")
Signed-off-by: Harshal Dev <harshal.dev@oss.qualcomm.com>

core/arch/arm/plat-qcom/main.c
@@ -32,6 +32,9 @@ register_ddr(DRAM0_BASE, DRAM0_SIZE);
 #ifdef DRAM1_BASE
 register_ddr(DRAM1_BASE, DRAM1_SIZE);
 #endif
+#ifdef DRAM2_BASE
+register_ddr(DRAM2_BASE, DRAM2_SIZE);
+#endif
 
 static struct qcom_geni_uart_data console_data;
 

core/arch/arm/plat-qcom/wildcat/nord/target_config.h
@@ -9,8 +9,10 @@
 #define GENI_UART_REG_BASE		UL(0x884000)
 
 #define DRAM0_BASE			UL(0x80000000)
-#define DRAM0_SIZE			UL(0x380000000)
-#define DRAM1_BASE			ULL(0x800000000)
-#define DRAM1_SIZE			ULL(0x800000000)
+#define DRAM0_SIZE			UL(0x80000000)
+#define DRAM1_BASE			ULL(0x880000000)
+#define DRAM1_SIZE			ULL(0x780000000)
+#define DRAM2_BASE			ULL(0x8800000000)
+#define DRAM2_SIZE			ULL(0x3800000000)
 
 #endif /* TARGET_CONFIG_H */