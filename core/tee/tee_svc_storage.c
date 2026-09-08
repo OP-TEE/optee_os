OP-TEE/optee_os

commit e98e3c87608c16039d7e72fbda46ce739e796248

core: provide tee_svc_storage_file_ops()
Reviewed-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

core/tee/tee_svc_storage.c
@@ -41,12 +41,7 @@
 #include <tee/tee_svc_storage.h>
 #include <trace.h>
 
-/*
- * Returns the appropriate tee_file_operations for the specified storage ID.
- * The value TEE_STORAGE_PRIVATE will select the REE FS if available, otherwise
- * RPMB.
- */
-static const struct tee_file_operations *file_ops(uint32_t storage_id)
+const struct tee_file_operations *tee_svc_storage_file_ops(uint32_t storage_id)
 {
 
 	switch (storage_id) {
@@ -276,8 +271,9 @@ TEE_Result syscall_storage_obj_open(unsigned long storage_id, void *object_id,
 	char *file = NULL;
 	struct tee_pobj *po = NULL;
 	struct user_ta_ctx *utc;
-	const struct tee_file_operations *fops = file_ops(storage_id);
 	size_t attr_size;
+	const struct tee_file_operations *fops =
+			tee_svc_storage_file_ops(storage_id);
 
 	if (!fops) {
 		res = TEE_ERROR_ITEM_NOT_FOUND;
@@ -425,7 +421,8 @@ TEE_Result syscall_storage_obj_create(unsigned long storage_id, void *object_id,
 	struct tee_obj *attr_o = NULL;
 	struct tee_pobj *po = NULL;
 	struct user_ta_ctx *utc;
-	const struct tee_file_operations *fops = file_ops(storage_id);
+	const struct tee_file_operations *fops =
+			tee_svc_storage_file_ops(storage_id);
 
 	if (!fops)
 		return TEE_ERROR_ITEM_NOT_FOUND;
@@ -705,7 +702,8 @@ TEE_Result syscall_storage_start_enum(unsigned long obj_enum,
 	struct tee_storage_enum *e;
 	TEE_Result res;
 	struct tee_ta_session *sess;
-	const struct tee_file_operations *fops = file_ops(storage_id);
+	const struct tee_file_operations *fops =
+			tee_svc_storage_file_ops(storage_id);
 
 	res = tee_ta_get_current_session(&sess);
 	if (res != TEE_SUCCESS)

core/include/tee/tee_svc_storage.h
@@ -33,6 +33,13 @@
 #include <tee/tee_fs.h>
 
 /*
+ * Returns the appropriate tee_file_operations for the specified storage ID.
+ * The value TEE_STORAGE_PRIVATE will select the REE FS if available, otherwise
+ * RPMB.
+ */
+const struct tee_file_operations *tee_svc_storage_file_ops(uint32_t storage_id);
+
+/*
  * Persistant Object Functions
  */
 TEE_Result syscall_storage_obj_open(unsigned long storage_id, void *object_id,

commit ba3ec7f0afe20e0e2cfff1bf46dda22061d6fe00

core storage: clear enum state after failed start
Do not retain file operations for an enumerator whose directory could
not be opened. This makes a subsequent GetNext return ITEM_NOT_FOUND
instead of dereferencing a NULL directory handle.

Fixes: e98e3c87608c ("core: provide tee_svc_storage_file_ops()")
Signed-off-by: Jaehun Lee <gsojc234@gmail.com>
Reviewed-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

core/tee/tee_svc_storage.c
@@ -606,17 +606,20 @@ TEE_Result syscall_storage_start_enum(unsigned long obj_enum,
 	if (res != TEE_SUCCESS)
 		return res;
 
-	if (e->dir) {
+	if (e->dir)
 		e->fops->closedir(e->dir);
-		e->dir = NULL;
-	}
+
+	e->dir = NULL;
+	e->fops = NULL;
 
 	if (!fops)
 		return TEE_ERROR_ITEM_NOT_FOUND;
 
-	e->fops = fops;
+	res = fops->opendir(&sess->ctx->uuid, &e->dir);
+	if (!res)
+		e->fops = fops;
 
-	return fops->opendir(&sess->ctx->uuid, &e->dir);
+	return res;
 }
 
 TEE_Result syscall_storage_next_enum(unsigned long obj_enum,