OP-TEE/optee_os

commit 3c47d0879cd1ceea6859a4945f95a8ac76f969a6

riscv: kernel: implement boot flow in boot.c
Provide an implementation of init_tee_runtime(), plat_primary_init_early()
boot_init_primary_early(), boot_init_primary_late(), boot_init_secondary()
and helper functions.  For now init_sec_mon() is kept to be replaced later
by a routine to initialize SBI implementation (to for example, probe for
available SBI extensions).

Signed-off-by: Marouene Boubakri <marouene.boubakri@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/riscv/kernel/boot.c
@@ -1,0 +1,151 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2022-2023 NXP
+ */
+
+#include <assert.h>
+#include <compiler.h>
+#include <config.h>
+#include <console.h>
+#include <keep.h>
+#include <kernel/boot.h>
+#include <kernel/linker.h>
+#include <kernel/misc.h>
+#include <kernel/panic.h>
+#include <kernel/thread.h>
+#include <mm/core_memprot.h>
+#include <mm/core_mmu.h>
+#include <mm/tee_mm.h>
+#include <mm/tee_pager.h>
+#include <platform_config.h>
+#include <riscv.h>
+#include <sbi.h>
+#include <stdio.h>
+#include <trace.h>
+#include <util.h>
+
+#define PADDR_INVALID               ULONG_MAX
+
+paddr_t start_addr;
+unsigned long boot_args[4];
+
+uint32_t sem_cpu_sync[CFG_TEE_CORE_NB_CORE];
+
+void init_sec_mon(unsigned long nsec_entry __maybe_unused)
+{
+	assert(nsec_entry == PADDR_INVALID);
+	/* Do nothing as we don't have a secure monitor */
+}
+
+#ifdef CFG_RISCV_S_MODE
+static void start_secondary_cores(void)
+{
+	size_t i = 0;
+	size_t pos = get_core_pos();
+
+	for (i = 0; i < CFG_TEE_CORE_NB_CORE; i++)
+		if (i != pos && IS_ENABLED(CFG_RISCV_SBI) &&
+		    sbi_boot_hart(i, start_addr, i))
+			EMSG("Error starting secondary hart %zu", i);
+}
+#endif
+
+static void init_runtime(void)
+{
+	malloc_add_pool(__heap1_start, __heap1_end - __heap1_start);
+
+	IMSG_RAW("\n");
+}
+
+void init_tee_runtime(void)
+{
+	core_mmu_init_ta_ram();
+	call_preinitcalls();
+	call_initcalls();
+}
+
+static void init_primary(unsigned long nsec_entry)
+{
+	/*
+	 * Mask asynchronous exceptions before switch to the thread vector
+	 * as the thread handler requires those to be masked while
+	 * executing with the temporary stack. The thread subsystem also
+	 * asserts that the foreign interrupts are blocked when using most of
+	 * its functions.
+	 */
+	thread_set_exceptions(THREAD_EXCP_ALL);
+
+	init_runtime();
+	thread_init_boot_thread();
+	thread_init_primary();
+	thread_init_per_cpu();
+	init_sec_mon(nsec_entry);
+}
+
+/* May be overridden in plat-$(PLATFORM)/main.c */
+__weak void plat_primary_init_early(void)
+{
+}
+
+/* May be overridden in plat-$(PLATFORM)/main.c */
+__weak void main_init_plic(void)
+{
+}
+
+/* May be overridden in plat-$(PLATFORM)/main.c */
+__weak void main_secondary_init_plic(void)
+{
+}
+
+void boot_init_primary_early(unsigned long pageable_part __unused,
+			     unsigned long nsec_entry __unused)
+{
+	unsigned long e = PADDR_INVALID;
+
+	init_primary(e);
+}
+
+void boot_init_primary_late(unsigned long fdt __unused)
+{
+	IMSG("OP-TEE version: %s", core_v_str);
+	if (IS_ENABLED(CFG_WARN_INSECURE)) {
+		IMSG("WARNING: This OP-TEE configuration might be insecure!");
+		IMSG("WARNING: Please check https://optee.readthedocs.io/en/latest/architecture/porting_guidelines.html");
+	}
+	IMSG("Primary CPU initializing");
+	main_init_plic();
+	init_tee_runtime();
+	call_finalcalls();
+	IMSG("Primary CPU initialized");
+
+#ifdef CFG_RISCV_S_MODE
+	start_secondary_cores();
+#endif
+}
+
+static void init_secondary_helper(unsigned long nsec_entry)
+{
+	size_t pos = get_core_pos();
+
+	IMSG("Secondary CPU %zu initializing", pos);
+
+	/*
+	 * Mask asynchronous exceptions before switch to the thread vector
+	 * as the thread handler requires those to be masked while
+	 * executing with the temporary stack. The thread subsystem also
+	 * asserts that the foreign interrupts are blocked when using most of
+	 * its functions.
+	 */
+	thread_set_exceptions(THREAD_EXCP_ALL);
+
+	thread_init_per_cpu();
+	init_sec_mon(nsec_entry);
+	main_secondary_init_plic();
+
+	IMSG("Secondary CPU %zu initialized", pos);
+}
+
+void boot_init_secondary(unsigned long nsec_entry __unused)
+{
+	init_secondary_helper(PADDR_INVALID);
+}

core/arch/riscv/kernel/sub.mk
@@ -3,3 +3,4 @@ srcs-y += idle.c
 srcs-$(CFG_RISCV_TIME_SOURCE_RDTIME) += tee_time_rdtime.c
 srcs-$(CFG_RISCV_SBI) += sbi.c
 srcs-$(CFG_RISCV_SBI_CONSOLE) += sbi_console.c
+srcs-y += boot.c

commit 6b616ad8ed5d91c442f1e411cd4d05e663781bf0

core: riscv: boot: guard sem_cpu_sync with CFG_BOOT_SYNC_CPU
Add missing CFG_BOOT_SYNC_CPU guards to sem_cpu_sync
declaration. Also add a comment for the array.

Fixes: 3c47d0879cd1 ("riscv: kernel: implement boot flow in boot.c")
Signed-off-by: Yu-Chien Peter Lin <peter.lin@sifive.com>
Reviewed-by: Marouene Boubakri <marouene.boubakri@nxp.com>

core/arch/riscv/kernel/boot.c
@@ -34,7 +34,15 @@
 
 paddr_t start_addr;
 
+#ifdef CFG_BOOT_SYNC_CPU
+/*
+ * Array used when booting, to synchronize harts.
+ * When 0, the hart has not started.
+ * When 1, it has started
+ */
 uint32_t sem_cpu_sync[CFG_TEE_CORE_NB_CORE];
+#endif
+
 uint32_t hartids[CFG_TEE_CORE_NB_CORE];
 
 #if defined(CFG_DT)