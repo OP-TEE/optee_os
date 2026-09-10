OP-TEE/optee_os

commit e84a7da412e39afa3c71b569d07997ea455dae28

ldelf: Add ELF program property parsing support to check for BTI
ELF program properties will be needed for detecting whether to
enable optional architecture or ABI features for a new ELF process.

The PT_GNU_PROPERTY is generated by a linker to describe the
.note.gnu.property section. The definition of PT_GNU_PROPERTY can
be found in [1].

For AArch64, GNU_PROPERTY_AARCH64_FEATURE_1_AND program property type
describes a set of processor features with which an ELF object or
executable image is compatible, but does not require in order to
execute correctly. These processor features are BTI and PAC.

[1] https://github.com/hjl-tools/linux-abi/wiki/linux-abi-draft.pdf

Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf.c
@@ -5,6 +5,8 @@
  */
 
 #include <assert.h>
+#include <config.h>
+#include <confine_array_index.h>
 #include <ctype.h>
 #include <elf32.h>
 #include <elf64.h>
@@ -534,6 +536,11 @@ static void parse_load_segments(struct ta_elf *elf)
 				elf->tls_start = phdr[n].p_vaddr;
 				elf->tls_filesz = phdr[n].p_filesz;
 				elf->tls_memsz = phdr[n].p_memsz;
+			} else if (IS_ENABLED(CFG_TA_BTI) &&
+				   phdr[n].p_type == PT_GNU_PROPERTY) {
+				elf->prop_start = phdr[n].p_vaddr;
+				elf->prop_align = phdr[n].p_align;
+				elf->prop_memsz = phdr[n].p_memsz;
 			}
 	}
 }
@@ -853,6 +860,69 @@ static void populate_segments(struct ta_elf *elf)
 			elf->max_offs += filesz;
 		}
 	}
+}
+
+static void parse_property_segment(struct ta_elf *elf)
+{
+	char *desc = NULL;
+	size_t align = elf->prop_align;
+	size_t desc_offset = 0;
+	size_t prop_offset = 0;
+	vaddr_t va = 0;
+	Elf_Note *note = NULL;
+	char *name = NULL;
+
+	if (!IS_ENABLED(CFG_TA_BTI) || !elf->prop_start)
+		return;
+
+	check_phdr_in_range(elf, PT_GNU_PROPERTY, elf->prop_start,
+			    elf->prop_memsz);
+
+	va = elf->load_addr + elf->prop_start;
+	note = (void *)va;
+	name = (char *)(note + 1);
+
+	if (elf->prop_memsz < sizeof(*note) + sizeof(ELF_NOTE_GNU))
+		return;
+
+	if (note->n_type != NT_GNU_PROPERTY_TYPE_0 ||
+	    note->n_namesz != sizeof(ELF_NOTE_GNU) ||
+	    memcmp(name, ELF_NOTE_GNU, sizeof(ELF_NOTE_GNU)) ||
+	    !IS_POWER_OF_TWO(align))
+		return;
+
+	desc_offset = ROUNDUP(sizeof(*note) + sizeof(ELF_NOTE_GNU), align);
+
+	if (desc_offset > elf->prop_memsz ||
+	    ROUNDUP(desc_offset + note->n_descsz, align) > elf->prop_memsz)
+		return;
+
+	desc = (char *)(va + desc_offset);
+
+	do {
+		Elf_Prop *prop = (void *)(desc + prop_offset);
+		size_t data_offset = prop_offset + sizeof(*prop);
+
+		if (note->n_descsz < data_offset)
+			return;
+
+		data_offset = confine_array_index(data_offset, note->n_descsz);
+
+		if (prop->pr_type == GNU_PROPERTY_AARCH64_FEATURE_1_AND) {
+			uint32_t *pr_data = (void *)(desc + data_offset);
+
+			if (note->n_descsz < (data_offset + sizeof(*pr_data)) &&
+			    prop->pr_datasz != sizeof(*pr_data))
+				return;
+
+			if (*pr_data & GNU_PROPERTY_AARCH64_FEATURE_1_BTI) {
+				DMSG("BTI Feature present in note property");
+				elf->bti_enabled = true;
+			}
+		}
+
+		prop_offset += ROUNDUP(sizeof(*prop) + prop->pr_datasz, align);
+	} while (prop_offset < note->n_descsz);
 }
 
 static void map_segments(struct ta_elf *elf)

ldelf/ta_elf.h
@@ -29,6 +29,7 @@ struct ta_elf {
 	bool is_main;
 	bool is_32bit;	/* Initialized from Elf32_Ehdr/Elf64_Ehdr */
 	bool is_legacy;
+	bool bti_enabled;
 
 	vaddr_t load_addr;
 	vaddr_t max_addr;
@@ -78,6 +79,11 @@ struct ta_elf {
 	/* Offset of the copy of the TLS block in the TLS area of the TCB */
 	size_t tls_tcb_offs;
 #endif
+
+	/* PT_GNU_PROPERTY segment */
+	vaddr_t prop_start;
+	size_t prop_align;
+	size_t prop_memsz;
 
 	uint32_t handle;
 

commit 2d63fc8a8f6cad74aa742a155044deb942ab3184

ldelf: ta_elf: fix bounds check in parse_property_segment()
A malformed PT_GNU_PROPERTY note could bypass the pr_data bounds check
and be read past n_descsz, potentially influencing the BTI enablement
decision. Fix that by tightening the logic.

Fixes: e84a7da412e3 ("ldelf: Add ELF program property parsing support to check for BTI")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Assisted-by: Claude:sonnet-5
Reviewed-by: Guled Hasan <Law.Zoldyck@proton.me>

ldelf/ta_elf.c
@@ -974,16 +974,17 @@ static void parse_property_segment(struct ta_elf *elf)
 	do {
 		Elf_Prop *prop = (void *)(desc + prop_offset);
 		size_t data_offset = prop_offset + sizeof(*prop);
+		size_t cdo = 0;
 
 		if (note->n_descsz < data_offset)
 			return;
 
-		data_offset = confine_array_index(data_offset, note->n_descsz);
+		cdo = confine_array_index(data_offset, note->n_descsz);
 
 		if (prop->pr_type == GNU_PROPERTY_AARCH64_FEATURE_1_AND) {
-			uint32_t *pr_data = (void *)(desc + data_offset);
+			uint32_t *pr_data = (void *)(desc + cdo);
 
-			if (note->n_descsz < (data_offset + sizeof(*pr_data)) &&
+			if (note->n_descsz < (data_offset + sizeof(*pr_data)) ||
 			    prop->pr_datasz != sizeof(*pr_data))
 				return;
 