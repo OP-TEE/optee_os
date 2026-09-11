OP-TEE/optee_os

commit bc1d13c122371e8d16aa154f2bde93e5f28a2df9

ldelf: check that PT_DYNAMIC is in range before use
Adds checks that the ELF program header PT_DYNAMIC is in range of the
loaded ELF before they are used. This fixes an error where a malformed
ELF may cause the loader to read data from other ELF or from the loader
itself.

Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reported-by: Martijn Bogaard <martijn@riscure.com>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

ldelf/ta_elf.c
@@ -18,6 +18,7 @@
 #include <tee_internal_api_extensions.h>
 #include <user_ta_header.h>
 #include <utee_syscalls.h>
+#include <util.h>
 
 #include "sys.h"
 #include "ta_elf.h"
@@ -126,6 +127,24 @@ static TEE_Result e64_parse_ehdr(struct ta_elf *elf __unused,
 }
 #endif /*ARM64*/
 
+static void check_phdr_in_range(struct ta_elf *elf, unsigned int type,
+				vaddr_t addr, size_t memsz)
+{
+	vaddr_t max_addr = 0;
+
+	if (ADD_OVERFLOW(addr, memsz, &max_addr))
+		err(TEE_ERROR_BAD_FORMAT, "Program header %#x overflow", type);
+
+	/*
+	 * elf->load_addr and elf->max_addr are both using the
+	 * final virtual addresses, while this program header is
+	 * relative to 0.
+	 */
+	if (max_addr > elf->max_addr - elf->load_addr)
+		err(TEE_ERROR_BAD_FORMAT, "Program header %#x out of bounds",
+		    type);
+}
+
 static void read_dyn(struct ta_elf *elf, vaddr_t addr,
 		     size_t idx, unsigned int *tag, size_t *val)
 {
@@ -153,6 +172,8 @@ static void save_hashtab_from_segment(struct ta_elf *elf, unsigned int type,
 
 	if (type != PT_DYNAMIC)
 		return;
+
+	check_phdr_in_range(elf, type, addr, memsz);
 
 	if (elf->is_32bit)
 		dyn_entsize = sizeof(Elf32_Dyn);
@@ -711,6 +732,8 @@ static void add_deps_from_segment(struct ta_elf *elf, unsigned int type,
 	if (type != PT_DYNAMIC)
 		return;
 
+	check_phdr_in_range(elf, type, addr, memsz);
+
 	if (elf->is_32bit)
 		dyn_entsize = sizeof(Elf32_Dyn);
 	else
@@ -1215,6 +1238,8 @@ static void get_init_fini_array(struct ta_elf *elf, unsigned int type,
 	size_t val = 0;
 
 	assert(type == PT_DYNAMIC);
+
+	check_phdr_in_range(elf, type, addr, memsz);
 
 	if (elf->is_32bit) {
 		dyn_entsize = sizeof(Elf32_Dyn);

commit 4f409d73fb3e141fd2db06e7ba2cbd370c039598

ldelf: verify DT_STRTAB/PT_DYNAMIC ranges are covered by a PT_LOAD segment
check_range() and check_phdr_in_range() only checked against
load_addr..max_addr, but PT_LOAD segments can have unmapped gaps
between them, so a range placed in such a gap passed the check while
pointing at unmapped memory.

Fixes: bc1d13c12237 ("ldelf: check that PT_DYNAMIC is in range before use")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Assisted-by: Claude:sonnet-5
Reviewed-by: Guled Hasan <Law.Zoldyck@proton.me>

ldelf/ta_elf.c
@@ -198,6 +198,27 @@ static TEE_Result e64_parse_ehdr(struct ta_elf *elf, Elf64_Ehdr *ehdr)
 }
 #endif /* RV64 */
 
+/*
+ * Checks that [addr, addr + sz) (relative to 0, i.e., not yet offset by
+ * elf->load_addr) is fully covered by a single PT_LOAD segment. There can
+ * be unmapped gaps between PT_LOAD segments, so being within
+ * elf->load_addr..elf->max_addr is not sufficient.
+ */
+static bool range_is_in_segment(struct ta_elf *elf, vaddr_t addr, size_t sz)
+{
+	struct segment *seg = NULL;
+	vaddr_t end_addr = 0;
+
+	if (ADD_OVERFLOW(addr, sz, &end_addr))
+		return false;
+
+	TAILQ_FOREACH(seg, &elf->segs, link)
+		if (addr >= seg->vaddr && end_addr <= seg->vaddr + seg->memsz)
+			return true;
+
+	return false;
+}
+
 static void check_phdr_in_range(struct ta_elf *elf, unsigned int type,
 				vaddr_t addr, size_t memsz)
 {
@@ -213,6 +234,11 @@ static void check_phdr_in_range(struct ta_elf *elf, unsigned int type,
 	 */
 	if (max_addr > elf->max_addr - elf->load_addr)
 		err(TEE_ERROR_BAD_FORMAT, "Program header %#x out of bounds",
+		    type);
+
+	if (!range_is_in_segment(elf, addr, memsz))
+		err(TEE_ERROR_BAD_FORMAT,
+		    "Program header %#x not covered by a PT_LOAD segment",
 		    type);
 }
 
@@ -246,6 +272,11 @@ static void check_range(struct ta_elf *elf, const char *name, const void *ptr,
 	if (max_addr > elf->max_addr)
 		err(TEE_ERROR_BAD_FORMAT,
 		    "%s %p..%#zx out of range", name, ptr, max_addr);
+
+	if (!range_is_in_segment(elf, (vaddr_t)ptr - elf->load_addr, sz))
+		err(TEE_ERROR_BAD_FORMAT,
+		    "%s %p..%#zx not covered by a PT_LOAD segment",
+		    name, ptr, max_addr);
 }
 
 static void check_hashtab(struct ta_elf *elf, void *ptr, size_t num_buckets,