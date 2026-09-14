OP-TEE/optee_os

commit 463acb4d50929fc3d3c803c7de8a55c07694c951

core: ltc: acipher_helpers.h: add macro mp_to_unsigned_bin2()
Writing a bignum to a fixed size integer buffer in big endian order is
slightly cumbersome, because one has to take into account the actual
size of the bignum in order to have the zero padding on the left.
Let's say I am working with 256-bit numbers:

 unsigned char buf[32] = { };
 void *n = compute_some_bignum(...);

 mp_to_unsigned_bin(n, buf + sizeof(buf) - mp_unsigned_bin_size(n));

This commit introduces mp_to_unsigned_bin2() which can be used like so:

 unsigned char buf[32] = { };
 void *n = compute_some_bignum(...);

 mp_to_unsigned_bin2(n, buf, sizeof(buf));

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/lib/libtomcrypt/acipher_helpers.h
@@ -43,4 +43,11 @@ TEE_Result ecc_populate_ltc_public_key(ecc_key *ltc_key,
 				       uint32_t algo, size_t *key_size_bytes);
 #endif
 
+/* Write bignum to fixed size buffer in big endian order */
+#define mp_to_unsigned_bin2(a, b, c) \
+        do { \
+                void *_a = (a); \
+                mp_to_unsigned_bin(_a, (b) + (c) - mp_unsigned_bin_size(_a)); \
+        } while(0)
+
 #endif /* ACIPHER_HELPERS_H */

commit b74807e6a042c5112e965796132e55d5e7a8e1b9

core: ltc: make mp_to_unsigned_bin2() bounds checked
mp_to_unsigned_bin2() computes the output address without checking
whether the input bignum fits in the destination. An oversized value can
move the output pointer before the buffer and cause a buffer underwrite.

Replace the macro with a bounds-checked __must_check inline function,
propagate bignum export errors, and update all SM2 callers to check the
result. Keep fixed-width zero-padding at the call sites that require it.

SM2 KEP can receive TA-controlled public coordinates. As an additional
validation layer, validate all local and peer static and ephemeral public
keys before key derivation. Reject oversized and non-canonical
coordinates, the all-zero point, and points not on the SM2 curve.

Fixes: 463acb4d5092 ("core: ltc: acipher_helpers.h: add macro mp_to_unsigned_bin2()")

Signed-off-by: Jaehun Lee <gsojc234@gmail.com>
Assisted-by: Codex:gpt-5.6-sol

core/lib/libtomcrypt/acipher_helpers.h
@@ -6,6 +6,7 @@
 #ifndef ACIPHER_HELPERS_H
 #define ACIPHER_HELPERS_H
 
+#include <compiler.h>
 #include <crypto/crypto.h>
 #include <tee_api_defines.h>
 #include <tee_api_types.h>
@@ -44,12 +45,26 @@ TEE_Result ecc_populate_ltc_public_key(ecc_key *ltc_key,
 				       uint32_t algo, size_t *key_size_bytes);
 #endif
 
-/* Write bignum to fixed size buffer in big endian order */
-#define mp_to_unsigned_bin2(a, b, c) \
-        do { \
-                void *_a = (a); \
-                mp_to_unsigned_bin(_a, (b) + (c) - mp_unsigned_bin_size(_a)); \
-        } while(0)
+/*
+ * Write a bignum right-aligned in a fixed-size big-endian buffer.
+ */
+static inline TEE_Result __must_check
+mp_to_unsigned_bin2(void *num, uint8_t *buf, size_t size)
+{
+	size_t num_size = mp_unsigned_bin_size(num);
+	int ltc_res = CRYPT_OK;
+
+	if (num_size > size)
+		return TEE_ERROR_BAD_STATE;
+
+	ltc_res = mp_to_unsigned_bin(num, buf + size - num_size);
+	if (ltc_res == CRYPT_MEM)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	if (ltc_res != CRYPT_OK)
+		return TEE_ERROR_BAD_STATE;
+
+	return TEE_SUCCESS;
+}
 
 #ifdef _CFG_CORE_LTC_SM2_DSA
 TEE_Result sm2_ltc_dsa_sign(uint32_t algo, struct ecc_keypair *key,

core/lib/libtomcrypt/sm2-dsa.c
@@ -124,8 +124,13 @@ A3:
 
 	*sig_len = 2 * SM2_INT_SIZE_BYTES;
 	memset(sig, 0, *sig_len);
-	mp_to_unsigned_bin2(r, sig, SM2_INT_SIZE_BYTES);
-	mp_to_unsigned_bin2(s, sig + SM2_INT_SIZE_BYTES, SM2_INT_SIZE_BYTES);
+	res = mp_to_unsigned_bin2(r, sig, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
+	res = mp_to_unsigned_bin2(s, sig + SM2_INT_SIZE_BYTES,
+			      SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 out:
 	ecc_free(&ltc_key);
 	ltc_ecc_del_point(x1y1p);

core/lib/libtomcrypt/sm2-kep.c
@@ -18,6 +18,29 @@
 /* SM2 uses 256 bit unsigned integers in big endian format */
 #define SM2_INT_SIZE_BYTES 32
 
+static TEE_Result sm2_kep_validate_public_key(const ecc_key *key)
+{
+	const ecc_point *point = &key->pubkey;
+	int ltc_res = CRYPT_OK;
+
+	if (mp_unsigned_bin_size(point->x) > SM2_INT_SIZE_BYTES ||
+	    mp_unsigned_bin_size(point->y) > SM2_INT_SIZE_BYTES ||
+	    mp_cmp_d(point->x, 0) == LTC_MP_LT ||
+	    mp_cmp_d(point->y, 0) == LTC_MP_LT ||
+	    mp_cmp(point->x, key->dp.prime) != LTC_MP_LT ||
+	    mp_cmp(point->y, key->dp.prime) != LTC_MP_LT ||
+	    (mp_iszero(point->x) && mp_iszero(point->y)))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	ltc_res = ltc_ecc_is_point(&key->dp, point->x, point->y);
+	if (ltc_res == CRYPT_MEM)
+		return TEE_ERROR_OUT_OF_MEMORY;
+	if (ltc_res != CRYPT_OK)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	return TEE_SUCCESS;
+}
+
 /*
  * Compute a hash of a user's identity and public key
  * For user A: ZA = SM3(ENTLA || IDA || a || b || xG || yG || xA || yA)
@@ -56,32 +79,50 @@ static TEE_Result sm2_kep_compute_Z(uint8_t *Z, size_t Zlen, const uint8_t *id,
 	if (res)
 		goto out;
 
-	mp_to_unsigned_bin2(key->dp.A, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(key->dp.A, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
-	mp_to_unsigned_bin2(key->dp.B, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(key->dp.B, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
-	mp_to_unsigned_bin2(key->dp.base.x, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(key->dp.base.x, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
-	mp_to_unsigned_bin2(key->dp.base.y, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(key->dp.base.y, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
-	mp_to_unsigned_bin2(key->pubkey.x, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(key->pubkey.x, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
-	mp_to_unsigned_bin2(key->pubkey.y, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(key->pubkey.y, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
@@ -124,7 +165,10 @@ static TEE_Result sm2_kep_compute_S(uint8_t *S, size_t S_len, uint8_t flag,
 		goto out;
 
 	/* xU or xV */
-	mp_to_unsigned_bin2(UV->x, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(UV->x, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
@@ -135,29 +179,41 @@ static TEE_Result sm2_kep_compute_S(uint8_t *S, size_t S_len, uint8_t flag,
 		goto out;
 
 	/* x1 */
-	mp_to_unsigned_bin2(initiator_eph_key->pubkey.x, buf,
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(initiator_eph_key->pubkey.x, buf,
 			    SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
 	/* y1 */
-	mp_to_unsigned_bin2(initiator_eph_key->pubkey.y, buf,
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(initiator_eph_key->pubkey.y, buf,
 			    SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
 	/* x2 */
-	mp_to_unsigned_bin2(responder_eph_key->pubkey.x, buf,
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(responder_eph_key->pubkey.x, buf,
 			    SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
 
 	/* y2 */
-	mp_to_unsigned_bin2(responder_eph_key->pubkey.y, buf,
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(responder_eph_key->pubkey.y, buf,
 			   SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
@@ -178,7 +234,10 @@ static TEE_Result sm2_kep_compute_S(uint8_t *S, size_t S_len, uint8_t flag,
 		goto out;
 
 	/* yU or yV */
-	mp_to_unsigned_bin2(UV->y, buf, SM2_INT_SIZE_BYTES);
+	memset(buf, 0, sizeof(buf));
+	res = mp_to_unsigned_bin2(UV->y, buf, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	res = crypto_hash_update(ctx, buf, sizeof(buf));
 	if (res)
 		goto out;
@@ -251,87 +310,135 @@ static TEE_Result sm2_kep_derive(ecc_key *my_key, ecc_key *my_eph_key,
 	 * Step A4: (x1, y1) = RA; x1bar = 2^w + (x1 & (2^w - 1))
 	 */
 
-	mp_to_unsigned_bin2(my_eph_key->pubkey.x, tmp, SM2_INT_SIZE_BYTES);
+	memset(tmp, 0, sizeof(tmp));
+	res = mp_to_unsigned_bin2(my_eph_key->pubkey.x, tmp,
+			      SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	tmp[SM2_INT_SIZE_BYTES / 2] |= 0x80;
-	mp_read_unsigned_bin(x1bar, tmp + SM2_INT_SIZE_BYTES / 2,
-			     SM2_INT_SIZE_BYTES / 2);
+	ltc_res = mp_read_unsigned_bin(x1bar,
+				       tmp + SM2_INT_SIZE_BYTES / 2,
+				       SM2_INT_SIZE_BYTES / 2);
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
 
 	/* Step A5: tA = (dA + x1bar * rA) mod n */
 
 	ltc_res = mp_mulmod(x1bar, my_eph_key->k, n, tA);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = mp_addmod(tA, my_key->k, n, tA);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	/* Step A6: verify whether RB verifies the curve equation */
 
 	ltc_res = ltc_ecc_is_point(&peer_eph_key->dp, peer_eph_key->pubkey.x,
 				   peer_eph_key->pubkey.y);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	/* Step A6 (continued): (x2, y2) = RB; x2bar = 2^w + (x2 & (2^w - 1)) */
 
-	mp_to_unsigned_bin2(peer_eph_key->pubkey.x, tmp, SM2_INT_SIZE_BYTES);
+	memset(tmp, 0, sizeof(tmp));
+	res = mp_to_unsigned_bin2(peer_eph_key->pubkey.x, tmp,
+			      SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 	tmp[SM2_INT_SIZE_BYTES / 2] |= 0x80;
-	mp_read_unsigned_bin(x2bar, tmp + SM2_INT_SIZE_BYTES / 2,
-			     SM2_INT_SIZE_BYTES / 2);
+	ltc_res = mp_read_unsigned_bin(x2bar,
+				       tmp + SM2_INT_SIZE_BYTES / 2,
+				       SM2_INT_SIZE_BYTES / 2);
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
+		goto out;
+	}
 
 
 	/* Step A7: compute U = [h.tA](PB + [x2bar]RB) and check for infinity */
 
 	ltc_res = mp_montgomery_setup(peer_key->dp.prime, &mp);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = mp_montgomery_normalization(mu, peer_key->dp.prime);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = mp_mulmod(peer_key->dp.A, mu, peer_key->dp.prime, ma);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = mp_set_int(one, 1);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = ltc_ecc_mul2add(&peer_key->pubkey, one, &peer_eph_key->pubkey,
 				  x2bar, U, ma, peer_key->dp.prime);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = mp_set_int(h, peer_key->dp.cofactor);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = mp_mul(h, tA, htA);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = ltc_ecc_mulmod(htA, U, U, peer_key->dp.A, peer_key->dp.prime,
 				 1);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	ltc_res = ltc_ecc_is_point_at_infinity(U, peer_key->dp.prime, &inf);
-	if (ltc_res != CRYPT_OK)
+	if (ltc_res != CRYPT_OK) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
-	if (inf)
+	if (inf) {
+		res = TEE_ERROR_BAD_STATE;
 		goto out;
+	}
 
 	/* Step A8: compute KA = KDF(xU || yU || ZA || ZB, klen) */
 
 	/* xU */
-	mp_to_unsigned_bin2(U->x, xUyUZAZB, SM2_INT_SIZE_BYTES);
+	res = mp_to_unsigned_bin2(U->x, xUyUZAZB, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 
 	/* yU */
-	mp_to_unsigned_bin2(U->y, xUyUZAZB + SM2_INT_SIZE_BYTES,
+	res = mp_to_unsigned_bin2(U->y, xUyUZAZB + SM2_INT_SIZE_BYTES,
 			    SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 
 	/* ZA */
 	res = sm2_kep_compute_Z(xUyUZAZB + 2 * SM2_INT_SIZE_BYTES,
@@ -417,8 +524,16 @@ TEE_Result crypto_acipher_sm2_kep_derive(struct ecc_keypair *my_key,
 	if (res)
 		goto out;
 
+	res = sm2_kep_validate_public_key(&ltc_my_key);
+	if (res)
+		goto out;
+
 	res = ecc_populate_ltc_private_key(&ltc_my_eph_key, my_eph_key,
 					   TEE_ALG_SM2_KEP, NULL);
+	if (res)
+		goto out;
+
+	res = sm2_kep_validate_public_key(&ltc_my_eph_key);
 	if (res)
 		goto out;
 
@@ -427,8 +542,16 @@ TEE_Result crypto_acipher_sm2_kep_derive(struct ecc_keypair *my_key,
 	if (res)
 		goto out;
 
+	res = sm2_kep_validate_public_key(&ltc_peer_key);
+	if (res)
+		goto out;
+
 	res = ecc_populate_ltc_public_key(&ltc_peer_eph_key, peer_eph_key,
 					  TEE_ALG_SM2_KEP, NULL);
+	if (res)
+		goto out;
+
+	res = sm2_kep_validate_public_key(&ltc_peer_eph_key);
 	if (res)
 		goto out;
 

core/lib/libtomcrypt/sm2-pke.c
@@ -211,9 +211,13 @@ TEE_Result sm2_ltc_pke_decrypt(struct ecc_keypair *key, const uint8_t *src,
 		goto out;
 	}
 
-	mp_to_unsigned_bin2(x2y2p->x, x2y2, SM2_INT_SIZE_BYTES);
-	mp_to_unsigned_bin2(x2y2p->y, x2y2 + SM2_INT_SIZE_BYTES,
+	res = mp_to_unsigned_bin2(x2y2p->x, x2y2, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
+	res = mp_to_unsigned_bin2(x2y2p->y, x2y2 + SM2_INT_SIZE_BYTES,
 			    SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 
 	/* Step B4: t = KDF(x2 || y2, klen) */
 
@@ -306,6 +310,7 @@ out:
 static TEE_Result sm2_point_to_bytes(uint8_t *buf, size_t *size,
 				     const ecc_point *p)
 {
+	TEE_Result res = TEE_SUCCESS;
 	size_t xsize = mp_unsigned_bin_size(p->x);
 	size_t ysize = mp_unsigned_bin_size(p->y);
 	size_t sz = 2 * SM2_INT_SIZE_BYTES + 1;
@@ -316,9 +321,13 @@ static TEE_Result sm2_point_to_bytes(uint8_t *buf, size_t *size,
 
 	memset(buf, 0, sz);
 	buf[0] = 0x04;  /* Uncompressed form indicator */
-	mp_to_unsigned_bin2(p->x, buf + 1, SM2_INT_SIZE_BYTES);
-	mp_to_unsigned_bin2(p->y, buf + 1 + SM2_INT_SIZE_BYTES,
-			    SM2_INT_SIZE_BYTES);
+	res = mp_to_unsigned_bin2(p->x, buf + 1, SM2_INT_SIZE_BYTES);
+	if (res)
+		return res;
+	res = mp_to_unsigned_bin2(p->y, buf + 1 + SM2_INT_SIZE_BYTES,
+			      SM2_INT_SIZE_BYTES);
+	if (res)
+		return res;
 
 	*size = sz;
 
@@ -437,9 +446,13 @@ TEE_Result sm2_ltc_pke_encrypt(struct ecc_public_key *key, const uint8_t *src,
 		goto out;
 	}
 
-	mp_to_unsigned_bin2(x2y2p->x, x2y2, SM2_INT_SIZE_BYTES);
-	mp_to_unsigned_bin2(x2y2p->y, x2y2 + SM2_INT_SIZE_BYTES,
+	res = mp_to_unsigned_bin2(x2y2p->x, x2y2, SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
+	res = mp_to_unsigned_bin2(x2y2p->y, x2y2 + SM2_INT_SIZE_BYTES,
 			    SM2_INT_SIZE_BYTES);
+	if (res)
+		goto out;
 
 	/* Step A5: compute t = KDF(x2 || y2, klen) */
 