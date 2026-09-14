OP-TEE/optee_os

commit 5a9d570a8d7ff1cf7adfcf332c4a1ea0e539481b

core: fs_htree: re-init hash tree when both tag and counter are zero
Creating and update a hash tree involves several RPC commands.
If a power loss occurs during the creation flow, it may result
in a hash tree with an incomplete header and a counter value of 0.
If attempting to read this file subsequently leads to a
TEE_ERROR_CORRUPT_OBJECT error.

Instead of returning TEE_ERROR_CORRUPT_OBJECT, continue the
initialization flow to support subsequent functionality.

Link: https://github.com/OP-TEE/optee_os/issues/7512
Fixes: 50a814981d8b ("core: provide a hash tree for secure storage")

Signed-off-by: Ox Yeh <ox.yeh@mediatek.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/tee/fs_htree.c
@@ -630,6 +630,47 @@ static TEE_Result init_root_node(struct tee_fs_htree *ht)
 	return res;
 }
 
+static TEE_Result create_and_sync(struct tee_fs_htree *ht,
+				  uint8_t *hash,
+				  uint32_t min_counter)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	const struct tee_fs_htree_image dummy_head = {
+		.counter = min_counter,
+	};
+
+	if (!ht)
+		return TEE_ERROR_GENERIC;
+
+	res = crypto_rng_read(ht->fek, sizeof(ht->fek));
+	if (res != TEE_SUCCESS)
+		return res;
+
+	res = tee_fs_fek_crypt(ht->uuid, TEE_MODE_ENCRYPT, ht->fek,
+			       sizeof(ht->fek), ht->head.enc_fek);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	res = init_root_node(ht);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	ht->dirty = true;
+	res = tee_fs_htree_sync_to_storage(&ht, hash, NULL);
+	if (res != TEE_SUCCESS)
+		return res;
+
+	return rpc_write_head(ht, 0, &dummy_head);
+}
+
+static bool ht_head_is_partially_done(const struct tee_fs_htree_image *head)
+{
+	uint8_t zero_tag[TEE_FS_HTREE_TAG_SIZE] = {0};
+
+	return head->counter == 0 &&
+	       !memcmp(head->tag, zero_tag, sizeof(head->tag));
+}
+
 TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 			     const TEE_UUID *uuid,
 			     const struct tee_fs_htree_storage *stor,
@@ -646,32 +687,22 @@ TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 	ht->stor_aux = stor_aux;
 
 	if (create) {
-		const struct tee_fs_htree_image dummy_head = {
-			.counter = min_counter,
-		};
-
-		res = crypto_rng_read(ht->fek, sizeof(ht->fek));
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		res = tee_fs_fek_crypt(ht->uuid, TEE_MODE_ENCRYPT, ht->fek,
-				       sizeof(ht->fek), ht->head.enc_fek);
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		res = init_root_node(ht);
-		if (res != TEE_SUCCESS)
-			goto out;
-
-		ht->dirty = true;
-		res = tee_fs_htree_sync_to_storage(&ht, hash, NULL);
-		if (res != TEE_SUCCESS)
-			goto out;
-		res = rpc_write_head(ht, 0, &dummy_head);
+		res = create_and_sync(ht, hash, min_counter);
 	} else {
 		res = init_head_from_data(ht, hash, min_counter);
 		if (res != TEE_SUCCESS)
 			goto out;
+
+		/*
+		 * If a power loss occurred during hash tree creation, the
+		 * head may not have been written and counter is still 0.
+		 * Re-initialze the hash tree.
+		 */
+		if (ht_head_is_partially_done(&ht->head)) {
+			res = create_and_sync(ht, hash, min_counter);
+			if (res != TEE_SUCCESS)
+				goto out;
+		}
 
 		res = verify_root(ht);
 		if (res != TEE_SUCCESS)

commit b4846822231bc08737f6e0e3f89c83e3aa14a53a

core: fs_htree: prevent possible hash-tree reset
In tee_fs_htree_open() when checking if hash-tree head has only been
partially written, add that it can only be true without a provided hash.
A hash-tree head is always fully written if there's a hash over it.

Fixes: 5a9d570a8d7f ("core: fs_htree: re-init hash tree when both tag and counter are zero")
Signed-off-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Reviewed-by: Jerome Forissier <jerome.forissier@arm.com>

core/tee/fs_htree.c
@@ -697,10 +697,15 @@ TEE_Result tee_fs_htree_open(bool create, uint8_t *hash, uint32_t min_counter,
 		/*
 		 * If a power loss occurred during hash tree creation, the
 		 * head may not have been written and counter is still 0.
-		 * Re-initialze the hash tree.
+		 * Re-initialize the hash tree.
+		 *
+		 * If hash is non-NULL, the head must have been written
+		 * properly at least once before. This prevents reset of
+		 * the hash tree only by overwriting selected parts of the
+		 * head.
 		 */
-		if (ht_head_is_partially_done(&ht->head)) {
-			res = create_and_sync(&ht, hash, min_counter);
+		if (!hash && ht_head_is_partially_done(&ht->head)) {
+			res = create_and_sync(&ht, NULL, min_counter);
 			if (res != TEE_SUCCESS)
 				goto out;
 		}