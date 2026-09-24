OP-TEE/optee_os

commit 4516e38db43025ad9a2ea4825edc7d5fb4c06ddc

assert.h: add static_assert
Adds the static_assert macro from C11.

Acked-by: Marouene Boubakri <marouene.boubakri@nxp.com>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

mk/compile.mk
@@ -35,7 +35,8 @@ comp-cflags-warns-high = \
 	-Wmissing-prototypes -Wnested-externs -Wpointer-arith \
 	-Wshadow -Wstrict-prototypes -Wswitch-default \
 	-Wwrite-strings \
-	-Wno-missing-field-initializers -Wno-format-zero-length
+	-Wno-missing-field-initializers -Wno-format-zero-length \
+	-Wno-c2x-extensions
 ifeq ($(CFG_WARN_DECL_AFTER_STATEMENT),y)
 comp-cflags-warns-high += $(call cc-option,-Wdeclaration-after-statement)
 endif

lib/libutils/ext/include/compiler.h
@@ -82,6 +82,10 @@
 #define __HAVE_BUILTIN_OVERFLOW 1
 #endif
 
+#if __GCC_VERSION >= 90100 && !defined(__CHECKER__)
+#define __HAVE_SINGLE_ARGUMENT_STATIC_ASSERT 1
+#endif
+
 #ifdef __HAVE_BUILTIN_OVERFLOW
 #define __compiler_add_overflow(a, b, res) \
 	__builtin_add_overflow((a), (b), (res))

lib/libutils/isoc/include/assert.h
@@ -25,9 +25,30 @@ void _assert_log(const char *expr, const char *file, const int line,
 	} while (0)
 #endif
 
+/* This macro is deprecated, please use static_assert instead */
 #define COMPILE_TIME_ASSERT(x) \
 	do { \
 		switch (0) { case 0: case ((x) ? 1: 0): default : break; } \
 	} while (0)
 
+#endif
+
+#if defined(__HAVE_SINGLE_ARGUMENT_STATIC_ASSERT)
+#define static_assert _Static_assert
+#else
+/*
+ * In gcc prior to 9.1 _Static_assert requires two arguments. To allow
+ * passing a single argument to static_assert() add a workaround with
+ * macros.
+ */
+#define ___args_count(_0, _1, x, ...) x
+#define __args_count(...) ___args_count(__VA_ARGS__, 2, 1, 0)
+
+#define __static_assert_1(expr)		_Static_assert(expr, "")
+#define __static_assert_2(expr, msg)	_Static_assert(expr, msg)
+#define ___static_assert(count, ...)	__static_assert_ ## count(__VA_ARGS__)
+#define __static_assert(count, ...)	___static_assert(count, __VA_ARGS__)
+
+#define static_assert(...) \
+	__static_assert(__args_count(__VA_ARGS__), __VA_ARGS__)
 #endif

commit c0e653a2309154f306f4bb09753074a746c1d874

mk/compile.mk: pass -Wno-c2x-extensions only to a compiler that knows it
-Wno-c2x-extensions silences a clang warning about static_assert and
is passed to every compiler. GCC accepts an unknown -Wno- option
without complaint, so cc-option cannot exclude it, but GCC then adds a
note about the unrecognized option to every diagnostic it emits.

Probe -Wc2x-extensions instead and pass the negated option only when
the compiler accepts the warning.

Fixes: 4516e38db430 ("assert.h: add static_assert")
Signed-off-by: Guled Hasan <Law.Zoldyck@proton.me>
Acked-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

mk/compile.mk
@@ -35,8 +35,9 @@ comp-cflags-warns-high = \
 	-Wmissing-prototypes -Wnested-externs -Wpointer-arith \
 	-Wshadow -Wstrict-prototypes -Wswitch-default \
 	-Wwrite-strings \
-	-Wno-missing-field-initializers -Wno-format-zero-length \
-	-Wno-c2x-extensions
+	-Wno-missing-field-initializers -Wno-format-zero-length
+comp-cflags-warns-high += $(if $(call cc-option,-Wc2x-extensions),\
+				-Wno-c2x-extensions)
 comp-cflags-warns-high += $(call cc-option,-Wpacked-not-aligned)
 comp-cflags-warns-high += $(call cc-option,-Waddress-of-packed-member)
 ifeq ($(CFG_WARN_DECL_AFTER_STATEMENT),y)