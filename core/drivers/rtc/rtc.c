OP-TEE/optee_os

commit 26899ca2e2f4cbe2b26643c60891f8596c715233

drivers: rtc: add RTC functions and millisecond field
Add a millisecond field in the optee_rtc_time structure.

Add different APIs to manipulate optee_rtc_time structures:
rtc_is_a_leap_year(): Detects if the given year is a leap year
rtc_get_month_days(): Returns the number of day in the given month
rtc_timecmp(): Compare two time captures
rtc_diff_calendar_ms(): Returns the difference in milliseconds between
two time captures
rtc_diff_calendar_tick(): Returns the difference in number of ticks
between two time captures

Signed-off-by: Clément Le Goffic <clement.legoffic@foss.st.com>
Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/rtc/rtc.c
@@ -5,9 +5,116 @@
 
 #include <assert.h>
 #include <drivers/rtc.h>
-#include <tee_api_types.h>
+
+/* Time fields shift values to fit date in a 64bit word */
+#define RTC_CMP_YEAR_SHIFT	U(36)
+#define RTC_CMP_MONTH_SHIFT	U(32)
+#define RTC_CMP_DAY_SHIFT	U(27)
+#define RTC_CMP_HOUR_SHIFT	U(22)
+#define RTC_CMP_MINUTES_SHIFT	U(16)
+#define RTC_CMP_SECONDS_SHIFT	U(10)
 
 struct rtc *rtc_device;
+
+static const uint8_t rtc_months_days[] = {
+	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+static unsigned long long rtc_time_to_ms(struct optee_rtc_time *time)
+{
+	uint64_t time_ms = 0;
+	unsigned int n = 0;
+	uint64_t days = 0;
+	uint64_t year = 0;
+
+	if (!time->tm_mday || time->tm_year < rtc_device->range_min.tm_year ||
+	    time->tm_year > rtc_device->range_max.tm_year)
+		panic();
+
+	/* Elapsed milliseconds since midnight */
+	time_ms = time->tm_ms;
+	time_ms += time->tm_sec * MS_PER_SEC;
+	time_ms += time->tm_min * MS_PER_MIN;
+	time_ms += time->tm_hour * MS_PER_HOUR;
+
+	/* Elapsed days in current month and in past months of current year */
+	days = time->tm_mday - 1;
+	for (n = 0; n < time->tm_mon; n++)
+		days += rtc_get_month_days(n, time->tm_year);
+
+	/* Elapsed days pasted years */
+	year = time->tm_year;
+	days += (year * 365) + (year / 4) - (year / 100) + (year / 400);
+
+	/* Accumulate the elapsed days */
+	time_ms += days * MS_PER_DAY;
+
+	return time_ms;
+}
+
+bool rtc_is_a_leap_year(uint32_t year)
+{
+	return !(year % 4) && ((year % 100) || !(year % 400));
+}
+
+uint8_t rtc_get_month_days(uint32_t month, uint32_t year)
+{
+	if (rtc_is_a_leap_year(year) && month == 1)
+		return rtc_months_days[month] + 1;
+	else
+		return rtc_months_days[month];
+}
+
+int rtc_timecmp(struct optee_rtc_time *a, struct optee_rtc_time *b)
+{
+	uint64_t tm_a = 0;
+	uint64_t tm_b = 0;
+
+	tm_a = SHIFT_U64(a->tm_year, RTC_CMP_YEAR_SHIFT) +
+	       SHIFT_U64(a->tm_mon, RTC_CMP_MONTH_SHIFT) +
+	       SHIFT_U64(a->tm_mday, RTC_CMP_DAY_SHIFT) +
+	       SHIFT_U64(a->tm_hour, RTC_CMP_HOUR_SHIFT) +
+	       SHIFT_U64(a->tm_min, RTC_CMP_MINUTES_SHIFT) +
+	       SHIFT_U64(a->tm_sec, RTC_CMP_SECONDS_SHIFT) +
+	       a->tm_ms;
+	tm_b = SHIFT_U64(b->tm_year, RTC_CMP_YEAR_SHIFT) +
+	       SHIFT_U64(b->tm_mon, RTC_CMP_MONTH_SHIFT) +
+	       SHIFT_U64(b->tm_mday, RTC_CMP_DAY_SHIFT) +
+	       SHIFT_U64(b->tm_hour, RTC_CMP_HOUR_SHIFT) +
+	       SHIFT_U64(b->tm_min, RTC_CMP_MINUTES_SHIFT) +
+	       SHIFT_U64(b->tm_sec, RTC_CMP_SECONDS_SHIFT) +
+	       b->tm_ms;
+
+	return CMP_TRILEAN(tm_a, tm_b);
+}
+
+signed long long rtc_diff_calendar_ms(struct optee_rtc_time *ref1,
+				      struct optee_rtc_time *ref2)
+{
+	signed long long res = 0;
+
+	if (SUB_OVERFLOW(rtc_time_to_ms(ref1), rtc_time_to_ms(ref2), &res)) {
+		DMSG("Overflow while computing time diff in milliseconds");
+		return LLONG_MAX;
+	}
+
+	return res;
+}
+
+signed long long rtc_diff_calendar_tick(struct optee_rtc_time *ref1,
+					struct optee_rtc_time *ref2,
+					unsigned long long tick_rate)
+{
+	signed long long diff_ms = rtc_diff_calendar_ms(ref1, ref2);
+	signed long long res = 0;
+
+	if (diff_ms == LLONG_MAX || MUL_OVERFLOW(diff_ms, tick_rate, &res)) {
+		DMSG("Overflow while computing calendar diff in ticks");
+		return LLONG_MAX;
+	}
+
+	return res / MS_PER_SEC;
+}
 
 void rtc_register(struct rtc *rtc)
 {

core/include/drivers/rtc.h
@@ -6,20 +6,39 @@
 #ifndef __DRIVERS_RTC_H
 #define __DRIVERS_RTC_H
 
+#include <kernel/panic.h>
 #include <tee_api_types.h>
 #include <util.h>
 
 /* The RTC allows to set/get offset for correction */
 #define RTC_CORRECTION_FEATURE	BIT(0)
 
+#define MS_PER_SEC		1000
+#define MS_PER_MIN		(60 * MS_PER_SEC)
+#define MS_PER_HOUR		(60 * MS_PER_MIN)
+#define MS_PER_DAY		(24 * MS_PER_HOUR)
+
+/*
+ * struct optee_rtc_time - Time in Gregorian calendar
+ *
+ * @tm_year: Absolute year
+ * @tm_mon: Month: 0=January, 1=February, ..., 11=December
+ * @tm_mday: Month day start from 1
+ * @tm_wday: Week day: 0=Sunday, 1=Monday, ..., 6=Saturday
+ * @tm_hour: Hour in range [0 23]
+ * @tm_min: Minute in range [0 59]
+ * @tm_sec: Second in range [0 59]
+ * @tm_ms: Millisecond in range [0 999] or 0 if no applicable
+ */
 struct optee_rtc_time {
 	uint32_t tm_year;
 	uint32_t tm_mon;
 	uint32_t tm_mday;
+	uint32_t tm_wday;
 	uint32_t tm_hour;
 	uint32_t tm_min;
 	uint32_t tm_sec;
-	uint32_t tm_wday;
+	uint32_t tm_ms;
 };
 
 struct rtc {
@@ -48,6 +67,59 @@ extern struct rtc *rtc_device;
 
 /* Register a RTC device as the system RTC */
 void rtc_register(struct rtc *rtc);
+
+/**
+ * rtc_is_a_leap_year() - Check if a year is a leap year
+ * @year:	The year to check
+ *
+ * Return:	true if the year is a leap year, false otherwise
+ */
+bool rtc_is_a_leap_year(uint32_t year);
+
+/**
+ * rtc_get_month_days() - Get the number of days in a month
+ * @month:	The month to know the number of days
+ * @year:	The year of the month
+ *
+ * Return:	Number of days in the month
+ */
+uint8_t rtc_get_month_days(uint32_t month, uint32_t year);
+
+/**
+ * rtc_timecmp() - Compare two RTC time structures
+ * @a:		First RTC time
+ * @b:		Second RTC time
+ *
+ * Return a negative value if @a < @b
+ * Return 0 if @a == @b
+ * Return a positive value if @a > @b
+ */
+int rtc_timecmp(struct optee_rtc_time *a, struct optee_rtc_time *b);
+
+/**
+ * rtc_diff_calendar_ms() - Return the difference in milliseconds between
+ * two times captures.
+ * @ref1: First time capture
+ * @ref2: Second time capture
+ *
+ * Return @ref1 - @ref2 in milliseconds or LLONG_MAX in case of overflow
+ */
+signed long long rtc_diff_calendar_ms(struct optee_rtc_time *ref1,
+				      struct optee_rtc_time *ref2);
+
+/**
+ * rtc_diff_calendar_tick() - Return the difference in number of ticks between
+ * two times captures.
+ * @ref1: First time capture
+ * @ref2: Second time capture
+ * @tick_rate: Tick rate
+ *
+ * Return @ref1 - @ref2 in number of ticks. In case of tick computation
+ * overflow, return LLONG_MAX
+ */
+signed long long rtc_diff_calendar_tick(struct optee_rtc_time *ref1,
+					struct optee_rtc_time *ref2,
+					unsigned long long tick_rate);
 
 static inline TEE_Result rtc_get_info(uint64_t *features,
 				      struct optee_rtc_time *range_min,
@@ -100,6 +172,23 @@ static inline TEE_Result rtc_set_offset(long offset)
 
 static inline void rtc_register(struct rtc *rtc __unused) {}
 
+static inline bool __noreturn rtc_is_a_leap_year(uint32_t year __unused)
+{
+	panic();
+}
+
+static inline uint8_t __noreturn rtc_get_month_days(uint32_t month __unused,
+						    uint32_t year __unused)
+{
+	panic();
+}
+
+static inline int __noreturn rtc_timecmp(struct optee_rtc_time *a __unused,
+					 struct optee_rtc_time *b __unused)
+{
+	panic();
+}
+
 static inline TEE_Result rtc_get_info(uint64_t *features __unused,
 				      struct optee_rtc_time *range_min __unused,
 				      struct optee_rtc_time *range_max __unused)
@@ -125,6 +214,21 @@ static inline TEE_Result rtc_get_offset(long *offset __unused)
 static inline TEE_Result rtc_set_offset(long offset __unused)
 {
 	return TEE_ERROR_NOT_SUPPORTED;
+}
+
+static inline signed long long
+rtc_diff_calendar_ms(struct optee_rtc_time *ref1 __unused,
+		     struct optee_rtc_time *ref2 __unused)
+{
+	return LLONG_MAX;
+}
+
+static inline signed long long
+rtc_diff_calendar_tick(struct optee_rtc_time *ref1 __unused,
+		       struct optee_rtc_time *ref2 __unused,
+		       unsigned long long tick_rate __unused)
+{
+	return LLONG_MAX;
 }
 #endif
 #endif /* __DRIVERS_RTC_H */

commit efaaf2546df922aee36ea5b63a262e2cf1b410a5

drivers: rtc: count leap days of past years only
rtc_time_to_ms() adds the leap days up to and including the current
year, and the month loop adds February 29 again for a later date of a
leap year. A date in a leap year therefore lies one day late. The
difference across the start of a leap year is one day too long and the
difference across the start of the following year is one day too
short.

stm32_stgen_pm_resume() adds the suspend duration measured this way to
the system counter. A suspend across midnight into a leap year adds a
day to system time, and a suspend of less than a day across midnight
into the following year gives a negative duration and panics.

Count the leap days of past years only.

Fixes: 26899ca2e2f4 ("drivers: rtc: add RTC functions and millisecond field")
Signed-off-by: Guled Hasan <Law.Zoldyck@proton.me>
Acked-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>
Acked-by: Gatien Chevallier <gatien.chevallier.foss@gmail.com>

core/drivers/rtc/rtc.c
@@ -42,9 +42,16 @@ static unsigned long long rtc_time_to_ms(struct optee_rtc_time *time)
 	for (n = 0; n < time->tm_mon; n++)
 		days += rtc_get_month_days(n, time->tm_year);
 
-	/* Elapsed days pasted years */
+	/*
+	 * Elapsed days in past years. The leap day of the current year is
+	 * already counted above for dates after February.
+	 */
 	year = time->tm_year;
-	days += (year * 365) + (year / 4) - (year / 100) + (year / 400);
+	days += year * 365;
+	if (year) {
+		year--;
+		days += year / 4 - year / 100 + year / 400;
+	}
 
 	/* Accumulate the elapsed days */
 	time_ms += days * MS_PER_DAY;